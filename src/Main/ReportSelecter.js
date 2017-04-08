e0285516698847434d3cad2275b2d3e56415bffb
Martijn Hols <martijnhols@hotmail.com>
2017-04-08 23:34:50 +0200

    Update changelog

--- src/Main/ReportSelecter.js
+++ src/Main/ReportSelecter.js
@@ -42,7 +42,9 @@ class ReportSelecter extends Component {
               <img src="./mastery-radius.png" alt="Mastery radius" className="pull-right" style={{ margin: 15 }} />
               Use this tool to analyze your performance as a Holy Paladin based on important metrics for the spec.<br /><br />
 
-              You will need a Warcraft Logs report with advanced combat logging enabled to start. Private logs can not be used, if your guild has private logs you will have to <a href="https://www.warcraftlogs.com/help/start/">upload your own logs</a> or change the existing logs to the <i>unlisted</i> privacy option instead.
+              You will need a Warcraft Logs report with advanced combat logging enabled to start. Private logs can not be used, if your guild has private logs you will have to
+              <a href="https://www.warcraftlogs.com/help/start/">upload your own logs</a> or change the existing logs to the
+              <i>unlisted</i> privacy option instead.
             </div>
           </div>
 
@@ -53,8 +55,8 @@ class ReportSelecter extends Component {
             <div className="panel-body">
               <strong>Enter your Warcraft Logs report code.</strong><br />
               https://www.warcraftlogs.com/reports/<input type="text" name="code" className="form-control" ref={(elem) => {
-                this.codeInput = elem;
-              }} style={{ width: 175 }} placeholder="Report code" autoCorrect="off" autoCapitalize="off" spellCheck="false" />/<br /><br />
+              this.codeInput = elem;
+            }} style={{ width: 175 }} placeholder="Report code" autoCorrect="off" autoCapitalize="off" spellCheck="false" />/<br /><br />
 
               <button type="submit" className="btn btn-primary">
                 Start <span className="glyphicon glyphicon-chevron-right" aria-hidden="true" />
@@ -67,25 +69,28 @@ class ReportSelecter extends Component {
               <h2>Changes</h2>
             </div>
             <div className="panel-body text-muted">
-                08-04-2017 - Add Wowhead tooltips and show T19 4 set bonus gain.<br />
-                08-04-2017 - Add Beacon of the Lightbringer mastery radius support!<br />
-                08-04-2017 - Improve beacon healing tracking accuracy and it now works properly with Beacon of Virtue.<br />
-                08-04-2017 - Total healing done count now includes absorbed healing.<br />
-                07-04-2017 - New layout with many usability improvements!<br />
-                04-04-2017 - Add an Always Be Casting (ABC) module that checks your <i>Non healing time</i> and dead GCD time (this is shown in the tooltip).<br />
-                29-03-2017 - Fixed a bug where Maraad's healing statistic would show 0% healing after getting only 1 Maraad's charge.<br />
-                29-03-2017 - Update healing bonuses to the 7.2 values (DoS & Ilterendi nerfs).<br />
-                27-03-2017 - Added Maraad's Dying Breath healing statistic.<br />
-                27-03-2017 - Added Prydaz and Obsidian Stone Spaulders healing statistic.<br />
-                26-03-2017 - Added Chain of Thrayn healing statistic.<br />
-                26-03-2017 - Completely refactor the core, rename to Holy Paladin Analyzer.<br />
-                25-03-2017 - Added cast ratios statistic.
-              </div>
+              08-04-2017 - Added <i>Heals on beacon</i> statistic.<br />
+              08-04-2017 - Added Wowhead tooltips and show T19 4 set bonus gain.<br />
+              08-04-2017 - Added Beacon of the Lightbringer mastery radius support!<br />
+              08-04-2017 - Improve beacon healing tracking accuracy and it now works properly with Beacon of Virtue.<br />
+              08-04-2017 - Total healing done count now includes absorbed healing.<br />
+              07-04-2017 - New layout with many usability improvements!<br />
+              04-04-2017 - Add an Always Be Casting (ABC) module that checks your
+              <i>Non healing time</i> and dead GCD time (this is shown in the tooltip).<br />
+              29-03-2017 - Fixed a bug where Maraad's healing statistic would show 0% healing after getting only 1 Maraad's charge.<br />
+              29-03-2017 - Update healing bonuses to the 7.2 values (DoS & Ilterendi nerfs).<br />
+              27-03-2017 - Added Maraad's Dying Breath healing statistic.<br />
+              27-03-2017 - Added Prydaz and Obsidian Stone Spaulders healing statistic.<br />
+              26-03-2017 - Added Chain of Thrayn healing statistic.<br />
+              26-03-2017 - Completely refactor the core, rename to Holy Paladin Analyzer.<br />
+              25-03-2017 - Added cast ratios statistic.
             </div>
+          </div>
 
-            <div className="text-muted">
-              Created by Zerotorescue for the <a href="https://discordapp.com/invite/hammerofwrath">#holy Discord</a>. You can usually find helpful people there.
-            </div>
+          <div className="text-muted">
+            Created by Zerotorescue for the
+            <a href="https://discordapp.com/invite/hammerofwrath">#holy Discord</a>. You can usually find helpful people there.
+          </div>
         </form>
       </div>
     );
