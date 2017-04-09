badc1a271b19cee8c3859b53f0ebbd933304ab11
Martijn Hols <martijnhols@hotmail.com>
2017-04-09 14:04:22 +0200

    Forgot to update changelog

--- src/Main/ReportSelecter.js
+++ src/Main/ReportSelecter.js
@@ -69,6 +69,7 @@ class ReportSelecter extends Component {
               <h2>Changes</h2>
             </div>
             <div className="panel-body text-muted">
+              09-04-2017 - Added <i>Casts Per Minute</i> table with very basic recommendations.<br />
               08-04-2017 - Added <i>Heals on beacon</i> statistic.<br />
               08-04-2017 - Added Wowhead tooltips and show T19 4 set bonus gain.<br />
               08-04-2017 - Added Beacon of the Lightbringer mastery radius support!<br />
