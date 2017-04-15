6dc3ca134f4ee0437756119d86c49145047908e6
Martijn Hols <martijnhols@hotmail.com>
2017-04-15 20:06:38 +0200

    Imroved cast efficiency calculation to be less of a bitch

--- src/Main/Results.js
+++ src/Main/Results.js
@@ -172,11 +172,13 @@ class Results extends React.Component {
 
         const cooldown = ability.getCooldown(hastePercentage);
         // By dividing the fight duration by the cooldown we get the max amount of casts during this particular fight, we round this up because you would be able to cast once at the start of the fight and once at the end since abilities always start off cooldown (e.g. fight is 100 seconds long, you could cast 2 Holy Avengers with a 90 sec cooldown). Good players should be able to reasonably predict this and maximize their casts.
-        const maxCasts = Math.ceil(fightDuration / 1000 / cooldown) + (ability.charges ? ability.charges - 1 : 0);
+        const rawMaxCasts = (fightDuration / 1000 / cooldown) + (ability.charges ? ability.charges - 1 : 0);
+        const maxCasts = Math.ceil(rawMaxCasts);
         const maxCpm = cooldown === null ? null : maxCasts / minutes;
-        const castEfficiency = cpm / maxCpm;
+        // The reason cast efficiency is based on the `rawMaxCasts` is that it you usually want to save cooldowns for periods where there is a lot of damage coming. Instead of adding a static buffer, this is more dynamic in that if you had 90% of the cooldown time to cast it you probably could have found a good moment, while if you only had 10% of the time then it isn't as likely there was a good opportunity. By making this based on the cooldown time, this scales with the strength of the cooldown; a 1.5 min cooldown is weaker than a 3 min cooldown.
+        const castEfficiency = cooldown === null ? null : Math.min(1, casts / rawMaxCasts);
 
-        const canBeImproved = castEfficiency < (ability.recommendedCastEfficiency || 0.8);
+        const canBeImproved = castEfficiency !== null && castEfficiency < (ability.recommendedCastEfficiency || 0.8);
 
         if (canBeImproved) {
           this.issues.push(`Try to cast <a href="http://www.wowhead.com/spell=${ability.spellId}" target="_blank">${ability.name}</a> more often (${casts}/${maxCasts} casts: ${Math.round(castEfficiency * 100)}% cast efficiency). ${ability.extraSuggestion || ''}`);
