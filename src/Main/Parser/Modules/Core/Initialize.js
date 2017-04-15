e0e9512783ecd39fcdb266ea8d4fe631bd581b31
Martijn Hols <martijnhols@hotmail.com>
2017-04-15 23:24:34 +0200

    Improved initialization

--- src/Main/Parser/Modules/Core/Initialize.js
+++ src/Main/Parser/Modules/Core/Initialize.js
@@ -1,15 +1,30 @@
 import Module from 'Main/Parser/Module';
 
-const debug = false;
+const debug = true;
+const TIMEOUT = 1000;
 
 class Initialize extends Module {
+  encounterStarted = false;
+  firstEventTimestamp = null;
   on_event(event) {
-    if (event.type === 'combatantinfo' || event.type === 'encounterstart') {
-      debug && console.log('Still waiting...', event);
-    } else {
-      debug && console.log('Finished initializing!', event);
-      this.active = false;
-      this.owner.triggerEvent('initialized');
+    this.firstEventTimestamp = this.firstEventTimestamp || event.timestamp;
+    switch (event.type) {
+      case 'encounterstart':
+        this.encounterStarted = true;
+        debug && console.log('Encounter started!', event);
+        break;
+      case 'combatantinfo':
+        debug && console.log('Combatant info received', event);
+        break;
+      default:
+        if (this.encounterStarted || (this.firstEventTimestamp !== null && (event.timestamp - this.firstEventTimestamp) > TIMEOUT)) {
+          debug && console.log('Finished initializing!', event);
+          this.active = false;
+          this.owner.triggerEvent('initialized');
+        } else {
+          console.error('Received an unexpected event prior to `encounterstart`:', event);
+        }
+        break;
     }
   }
 }
