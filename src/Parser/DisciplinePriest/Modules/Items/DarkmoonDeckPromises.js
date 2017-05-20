27c95f6a20af87d9bff3534f76fd1782280107be
Martijn Hols <martijnhols@hotmail.com>
2017-05-20 12:14:09 +0200

    Mana reduction from Promises during Innervate is 0

--- src/Parser/DisciplinePriest/Modules/Items/DarkmoonDeckPromises.js
+++ src/Parser/DisciplinePriest/Modules/Items/DarkmoonDeckPromises.js
@@ -6,7 +6,6 @@ const debug = false;
 
 /** The amount of time during which it's impossible a second Penance could have started */
 const PENANCE_CHANNEL_TIME_BUFFER = 2500;
-const PENANCE_MANA_COST = 30800;
 
 class DarkmoonDeckPromises extends CoreDarkmoonDeckPromises {
   lastPenanceStartTimestamp = null;
@@ -14,11 +13,15 @@ class DarkmoonDeckPromises extends CoreDarkmoonDeckPromises {
     const spellId = event.ability.guid;
     let cost = super.getManaCost(event);
 
+    if (this.owner.selectedCombatant.hasBuff(SPELLS.INNERVATE_BUFF.id, event.timestamp)) {
+      return 0;
+    }
+
     // Penance does not include the mana cost in the combatlog :(
     if (spellId === SPELLS.PENANCE.id) {
       if (!this.lastPenanceStartTimestamp || (event.timestamp - this.lastPenanceStartTimestamp) > PENANCE_CHANNEL_TIME_BUFFER) {
         this.lastPenanceStartTimestamp = event.timestamp;
-        cost = PENANCE_MANA_COST;
+        cost = SPELLS.PENANCE.manaCost;
       } else {
         // This is a second bolt from Penance, it doesn't cost mana.
         cost = 0;
