e9e5e2047ff843aa0e59b74893ce4232f920d203
Martijn Hols <martijnhols@hotmail.com>
2017-05-21 14:58:37 +0200

    Fix icons

--- src/common/Icon.js
+++ src/common/Icon.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const Icon = ({ icon, alt, ...other }) => (
-  <img
-    src={`http://media.blizzard.com/wow/icons/56/${icon.replace('.jpg', '')}.jpg`}
-    alt={alt}
-    {...other}
-  />
-);
+const Icon = ({ icon, alt, ...other }) => {
+  icon = icon.replace('.jpg', '').replace('-', '');
+  if (icon === 'petbattle_healthdown') {
+    // Blizzard seems to have forgotten to remove the dash for this one... or something
+    icon = 'petbattle_health-down';
+  }
+
+  return (
+    <img
+      src={`http://media.blizzard.com/wow/icons/56/${icon}.jpg`}
+      alt={alt}
+      {...other}
+    />
+  );
+};
 Icon.propTypes = {
   icon: React.PropTypes.string.isRequired,
   alt: React.PropTypes.string.isRequired,
