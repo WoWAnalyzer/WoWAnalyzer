b7d9ce75f5caf48259a180e8ee730be736f4c564
Martijn Hols <martijnhols@hotmail.com>
2017-05-23 23:58:56 +0200

    Fix eslint / code style issues

--- src/Parser/DisciplinePriest/Modules/Spells/Atonement.js
+++ src/Parser/DisciplinePriest/Modules/Spells/Atonement.js
@@ -5,8 +5,8 @@ import Module from 'Parser/Core/Module';
 const debug = true;
 
 class Atonement extends Module {
-    healing = 0;
-    totalAtones = 0;
+  healing = 0;
+  totalAtones = 0;
   hasContrition = false;
   currentAtonementTargets = [];
   improperAtonementRefreshes = [];
@@ -15,11 +15,12 @@ class Atonement extends Module {
     return 15 + (this.hasContrition ? 3 : 0);
   }
 
-  get numAtonementsActive(){
-      return this.currentAtonementTargets.length;
+  get numAtonementsActive() {
+    return this.currentAtonementTargets.length;
   }
+
   on_initialized() {
-      this.active = true;
+    this.active = true;
     if (!this.owner.error) {
       this.hasContrition = this.owner.selectedCombatant.lv75Talent === SPELLS.CONTRITION_TALENT.id;
     }
@@ -31,16 +32,15 @@ class Atonement extends Module {
       return;
     }
 
-    var atonement = {
-        target: event.targetID,
-        lastAtonmentAppliedTimestamp: event.timestamp
+    const atonement = {
+      target: event.targetID,
+      lastAtonmentAppliedTimestamp: event.timestamp
     };
     this.currentAtonementTargets = this.currentAtonementTargets.filter(id => id.target !== atonement.target);
     this.currentAtonementTargets.push(atonement);
     this.totalAtones++;
     debug && console.log(`%c${this.owner.combatants.players[atonement.target].name} gained an atonement`, 'color:green', this.currentAtonementTargets);
     this.owner.triggerEvent('atonement_applied', event);
-
   }
   on_byPlayer_refreshbuff(event) {
     const spellId = event.ability.guid;
@@ -48,20 +48,22 @@ class Atonement extends Module {
       return;
     }
 
-    var atonement = {
-        target: event.targetID,
-        lastAtonmentAppliedTimestamp: event.timestamp
+    const atonement = {
+      target: event.targetID,
+      lastAtonmentAppliedTimestamp: event.timestamp
     };
-    var refreshedTarget = this.currentAtonementTargets.find(id => id.target == atonement.target) || {};
-    if (refreshedTarget == null){
-        refreshedTarget.target = event.targetID;
-        refreshedTarget.lastAtonmentAppliedTimestamp = this.owner.fight.start_time;
-        debug && console.warn('Atonement: was applied prior to combat');
+    let refreshedTarget = this.currentAtonementTargets.find(id => id.target === atonement.target);
+    if (!refreshedTarget) {
+      refreshedTarget = {
+        target: event.targetID,
+        lastAtonmentAppliedTimestamp: this.owner.fight.start_time,
+      };
+      debug && console.warn('Atonement: was applied prior to combat');
     }
     if ((event.timestamp - refreshedTarget.lastAtonmentAppliedTimestamp) < this.atonementDuration * 1000 - 3000) {
-        this.improperAtonementRefreshes.push(refreshedTarget)
-        debug && console.log(`%c${this.owner.combatants.players[atonement.target].name} refreshed an atonement too early %c${event.timestamp - refreshedTarget.lastAtonmentAppliedTimestamp}`, 'color:red', this.currentAtonementTargets);
-    this.owner.triggerEvent('atonement_refresh_improper', event);
+      this.improperAtonementRefreshes.push(refreshedTarget);
+      debug && console.log(`%c${this.owner.combatants.players[atonement.target].name} refreshed an atonement too early %c${event.timestamp - refreshedTarget.lastAtonmentAppliedTimestamp}`, 'color:red', this.currentAtonementTargets);
+      this.owner.triggerEvent('atonement_refresh_improper', event);
     }
     this.currentAtonementTargets = this.currentAtonementTargets.filter(id => id.target !== atonement.target);
     this.currentAtonementTargets.push(atonement);
@@ -69,21 +71,19 @@ class Atonement extends Module {
     debug && console.log(`%c${this.owner.combatants.players[atonement.target].name} refreshed an atonement`, 'color:green', this.currentAtonementTargets);
     this.owner.triggerEvent('atonement_refresh', event);
   }
-
-    on_byPlayer_removebuff(event) {
+  on_byPlayer_removebuff(event) {
     const spellId = event.ability.guid;
     if (spellId !== SPELLS.ATONEMENT.id) {
       return;
     }
-    var atonement = {
-        target: event.targetID,
-        lastAtonmentAppliedTimestamp: event.timestamp
+    const atonement = {
+      target: event.targetID,
+      lastAtonmentAppliedTimestamp: event.timestamp
     };
     this.currentAtonementTargets = this.currentAtonementTargets.filter(id => id.target !== atonement.target);
     debug && console.log(`%c${this.owner.combatants.players[atonement.target].name} lost an atonement`, 'color:yellow', this.currentAtonementTargets);
     this.owner.triggerEvent('atonement_faded', event);
   }
-
 
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
