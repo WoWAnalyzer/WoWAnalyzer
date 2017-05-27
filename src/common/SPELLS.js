732b818a1f56fcafeaace7999263ffc734ec32ec
Martijn Hols <martijnhols@hotmail.com>
2017-05-28 00:51:25 +0200

    Also put talents and traits in class spell files

--- src/common/SPELLS.js
+++ src/common/SPELLS.js
@@ -1,8 +1,6 @@
 import indexById from './indexById';
 
-import talents from './SPELLS_TALENTS';
 import others from './SPELLS_OTHERS';
-import traits from './SPELLS_TRAITS';
 import racials from './SPELLS_RACIALS';
 
 import paladin from './SPELLS_PALADIN';
@@ -11,9 +9,7 @@ import druid from './SPELLS_DRUID';
 import monk from './SPELLS_MONK';
 
 const ABILITIES = {
-  ...talents,
   ...others,
-  ...traits,
   ...racials,
   ...paladin,
   ...priest,
--- src/common/SPELLS_DRUID.js
+++ src/common/SPELLS_DRUID.js
@@ -265,4 +265,163 @@ export default {
     name: 'Spring Blossoms',
     icon: 'spell_druid_springblossoms'
   },
+
+  // Talents
+  // Resto Druid:
+  // lv15
+  PROSPERITY_TALENT: {
+    id: 200383,
+    name: 'Prosperity',
+    icon: 'spell_druid_prosperity',
+    description: 'Unfortunately <a href="http://www.wowhead.com/spell=200383" target="_blank">Properity</a> is right now shit tier, please dont use it for raids. ',
+  },
+  CENARION_WARD_TALENT: {
+    id: 102351,
+    name: 'Cenarion Ward',
+    icon: 'ability_druid_naturalperfection',
+    description: '<a href="http://www.wowhead.com/spell=102351" target="_blank">Cenarion Ward</a> is the best talent in this tier in almost all situations.'
+  },
+  ABUNDANCE_TALENT: {
+    id: 207383,
+    name: 'Abundance',
+    icon: 'spell_druid_abundance',
+    description: '<a href="http://www.wowhead.com/spell=207383" target="_blank">Abundance</a> is usually not a recommended talent for raiding, however it\'s decent for M+ dungeons',
+  },
+  // lv30
+  RENEWAL_TALENT: {
+    id: 108238,
+    name: 'Renewal',
+    icon: 'spell_druid_renewal',
+    description: '<a href="http://www.wowhead.com/spell=108238" target="_blank">Renewal</a> is a big self-heal on a long-ish cooldown which is off the global cooldown. This talent is usualy not chosen because the lack of movement utility <a href="http://www.wowhead.com/spell=108238" target="_blank">Displacer Beast</a> offer is better.'
+  },
+  DISPLACER_BEAST_TALENT: {
+    id: 102280,
+    name: 'Displacer Beast',
+    icon: 'spell_druid_displacement',
+    description: '<a href="http://www.wowhead.com/spell=108238" target="_blank">Displacer Beast</a> is the default choice for this tier. Basically a Blink with kitty Spring tagged onto the end.',
+  },
+  WILD_CHARGE_TALENT: {
+    id: 102401,
+    name: 'Wild Charge',
+    icon: 'spell_druid_wildcharge',
+    description: '<a href="http://www.wowhead.com/spell=102401" target="_blank">Wild Charge</a> is usually worse than Displacer Bear when it comes to movement utility. Why are you using Wild Charge?????',
+  },
+  // lv45
+  BALANCE_AFFINITY_TALENT: {
+    id: 197632,
+    name: 'Balance Affinity',
+    icon: 'spell_druid_balanceaffinity',
+    description: '<a href="http://www.wowhead.com/spell=197632" target="_blank">Balance Affinity</a> is useful whenever you need the extra passive 5 yard range increase. However it offers less DPS than <a href="http://www.wowhead.com/spell=197490" target="_blank">Feral Affinity</a> .',
+  },
+  FERAL_AFFINITY_TALENT: {
+    id: 197490,
+    name: 'Feral Affinity',
+    icon: 'spell_druid_feralaffinity',
+    description: '<a href="http://www.wowhead.com/spell=197490" target="_blank">Feral Affinity</a> offers passive 15% movement speed, however it offers the highest DPS by <a href="https://questionablyepic.com/restodruid-catweaving/" target="_blank">catweaving</a>. This talent is usually only taken if you need the extra dps.',
+  },
+  GUARDIAN_AFFINITY_TALENT: {
+    id: 197491,
+    name: 'Guardian Affinity',
+    icon: 'talentspec_druid_feral_bear',
+    description: '<a href="http://www.wowhead.com/spell=197491" target="_blank">Guardian Affinity</a> offers a flat 6% damage taken reduction. It also gives you <a href="http://www.wowhead.com/spell=22842" target="_blank">Frenzied Regeneration</a> which can be life saving (or good for padding healing). This is usually the default talent, great choice!',
+  },
+  // lv60
+  MIGHTY_BASH_TALENT: {
+    id: 5211,
+    name: 'Mighty Bash',
+    icon: 'spell_druid_mightybash',
+    description: 'This talent row usually has no impact on performance. This can be useful on encounters with bigger adds that you can stun or procc Sephuz Secret with',
+  },
+  MASS_ENTANGLEMENT_TALENT: {
+    id: 102359,
+    name: 'Mass Entanglement',
+    icon: 'spell_druid_massentanglement',
+    description: 'This talent row usually has no impact on performance. This can be useful if you need a spell to procc Sephuz Secret with',
+  },
+  TYPHOON_TALENT: {
+    id: 132469,
+    name: 'Typhoon',
+    icon: 'ability_druid_typhoon',
+    description: 'This talent row usually has no impact on performance. This can be useful if you need a spell to push adds e.g. Illgynoth blobs.',
+  },
+  // lv75
+  SOUL_OF_THE_FOREST_TALENT: {
+    id: 158478,
+    name: 'Soul of the Forest',
+    icon: 'spell_druid_souloftheforest',
+    description: 'You should not at any point use this talent right now. <a href="http://www.wowhead.com/spell=158478" target="_blank">Soul of the Forest</a> is unfortunately mathematically inferior to both <a href="http://www.wowhead.com/spell=200390" target="_blank">Cultivation</a> and <a href="http://www.wowhead.com/spell=33891" target="_blank">Tree of Life</a>.',
+  },
+  CULTIVATION_TALENT: {
+    id: 200390,
+    name: 'Cultivation',
+    icon: 'ability_druid_nourish',
+    description: 'This is usualy the default talent. <a href="http://www.wowhead.com/spell=200390" target="_blank">Cultivation</a> provides a free passive HoT on players actually needing heal (sub 60% hp), it also increases the overall average mastery count on players. It is also the easiest talent to play with.',
+  },
+  INCARNATION_TREE_OF_LIFE_TALENT: {
+    id: 33891,
+    name: 'Incarnation: Tree of Life',
+    icon: 'ability_druid_improvedtreeform',
+    description: '<a href="http://www.wowhead.com/spell=33891" target="_blank">Incarnation: Tree of Life</a> or simply ToL. This talent has actually been proven to be a decent or sometimes a superior choice to <a href="http://www.wowhead.com/spell=200390" target="_blank">Cultivation</a>. This talent requires some planning to be used optimally. You have to take into account the 3 minute cooldown and plan accordingly, e.g. a 2.30 minute fight you would only be able to use this ability once, but on a 3.30 minute fight you would get 2 active uses. ToL is also preferred whenever people rarely drop below 60% hp or/and you need the on demand burst healing. With the nerfs coming to cultivation in 7.2.5 this might actually be the default pick.',
+  },
+  // lv90
+  SPRING_BLOSSOMS_TALENT: {
+    id: 207385,
+    name: 'Spring Blossoms',
+    icon: 'spell_druid_springblossoms',
+    description: 'A good talent for some encounters. Spring blossom counts as a mastery stack and can be extended by <a href="http://www.wowhead.com/spell=197721" target="_blank">Flourish</a> and a well placed spring blossoms can give you around 9-12 extra mastery stacks up (depending on your haste). This talent requires some planning to get optimal value as you would need to have a high uptime (>90%) on <a href="http://www.wowhead.com/spell=145205" target="_blank">Efflorescence</a> and your raid members being grouped up most the time.',
+  },
+  INNER_PEACE_TALENT: {
+    id: 197073,
+    name: 'Inner Peace',
+    icon: 'spell_druid_innerpeace',
+    description: 'This talent requires some planning with your healing team to optimize. Useful if you can get a few extra good tranquilities in, useless otherwise.',
+  },
+  GERMINATION_TALENT: {
+    id: 155675,
+    name: 'Germination',
+    icon: 'spell_druid_germination',
+    description: 'This talent shines on encounters where you need to have extra focus on specific targets. Also if you have the T19 4P and <a href="http://www.wowhead.com/item=137072/amanthuls-wisdom" target="_blank">Amanthuls Wisdom</a> this will most likely be your default talent choice.',
+  },
+  // lv100
+  MOMENT_OF_CLARITY_TALENT: {
+    id: 155577,
+    name: 'Moment of Clarity',
+    icon: 'spell_druid_momentofclarity',
+    description: 'You are either a Korean meme-lord or you dont know how to play this game, avoid this talent.',
+  },
+  STONEBARK_TALENT: {
+    id: 197061,
+    name: 'Stonebark',
+    icon: 'spell_druid_stonebark',
+    description: 'This is a weak talent for raiding and you should not be using this, please stop using this.',
+  },
+  FLOURISH_TALENT: {
+    id: 197721,
+    name: 'Flourish',
+    icon: 'spell_druid_wildburst',
+    description: 'Default top tier talent, use it and love it. Always aim to extend <a href="http://www.wowhead.com/spell=48438" target="_blank">Wild Growth</a> and <a href="http://www.wowhead.com/spell=102351" target="_blank">Cenarion Ward</a>.',
+  },
+
+  // Traits:
+  // Resto Druid:
+  POWER_OF_THE_ARCHDRUID: {
+    id: 189870,
+    name: 'Power of the Archdruid',
+    icon: 'spell_druid_rampantgrowth',
+  },
+  NATURES_ESSENCE_TRAIT: {
+    id:189787,
+    name:'Nature\'s Essence',
+    icon: 'ability_druid_flourish'
+  },
+  DREAMWALKER_TRAIT: {
+    id:189849,
+    name:'Dreamwalker',
+    icon: 'ability_druid_healinginstincts'
+  },
+  INFUSION_OF_NATURE_TRAIT: {
+    id:189757,
+    name:'Infusion of Nature',
+    icon: 'ability_druid_flourish'
+  },
 };
