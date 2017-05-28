e8573c140a56776af4a5bc56d70082957e9861af
Martijn Hols <martijnhols@hotmail.com>
2017-05-28 10:51:21 +0200

    Use the shared Prydaz module instead

--- src/Parser/RestorationShaman/CombatLogParser.js
+++ src/Parser/RestorationShaman/CombatLogParser.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-import MainCombatLogParser from 'Parser/Core/CombatLogParser';
-import ParseResults from 'Parser/Core/ParseResults';
-import getCastEfficiency from 'Parser/Core/getCastEfficiency';
-import ISSUE_IMPORTANCE from 'Parser/Core/ISSUE_IMPORTANCE';
-
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 import SpellIcon from 'common/SpellIcon';
@@ -20,6 +15,12 @@ import CastEfficiencyTab from 'Main/CastEfficiencyTab';
 import CooldownsTab from 'Main/CooldownsTab';
 import ManaTab from 'Main/ManaTab';
 import FeedingTab from 'Main/FeedingTab';
+
+import MainCombatLogParser from 'Parser/Core/CombatLogParser';
+import ParseResults from 'Parser/Core/ParseResults';
+import getCastEfficiency from 'Parser/Core/getCastEfficiency';
+import ISSUE_IMPORTANCE from 'Parser/Core/ISSUE_IMPORTANCE';
+import Prydaz from 'Parser/Core/Modules/Items/Prydaz';
 
 import ShamanAbilityTracker from './Modules/ShamanCore/ShamanAbilityTracker';
 
@@ -37,7 +38,6 @@ import UncertainReminder from './Modules/Legendaries/UncertainReminder';
 import Jonat from './Modules/Legendaries/Jonat';
 import Nobundo from './Modules/Legendaries/Nobundo';
 import Tidecallers from './Modules/Legendaries/Tidecallers';
-import Prydaz from './Modules/Legendaries/Prydaz';
 import Restoration_Shaman_T19_2Set from './Modules/Legendaries/T19_2Set';
 
 import CPM_ABILITIES, { SPELL_CATEGORY } from './CPM_ABILITIES';
