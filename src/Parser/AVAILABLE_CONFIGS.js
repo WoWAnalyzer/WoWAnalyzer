b13d0306acc681d75e69ac021428d1e8913adbba
Martijn Hols <martijnhols@hotmail.com>
2017-05-28 10:47:31 +0200

    Update AVAILABLE_CONFIGS order

--- src/Parser/AVAILABLE_CONFIGS.js
+++ src/Parser/AVAILABLE_CONFIGS.js
@@ -4,10 +4,11 @@ import RestoDruid from './RestoDruid/CONFIG';
 import RestorationShaman from './RestorationShaman/CONFIG';
 import MistweaverMonk from './MistweaverMonk/CONFIG';
 
+// Order of this should be the order in which specs were added to production
 export default [
   HolyPaladin,
   DisciplinePriest,
   RestoDruid,
-  RestorationShaman,
   MistweaverMonk,
+  RestorationShaman,
 ];
