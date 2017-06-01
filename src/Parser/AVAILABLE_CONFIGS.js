74c877c9a70432fff6526060ec65c25fd3f0a21b
Lawrence Matacena <lmatacena@stopforschools.com>
2017-06-01 19:30:44 -0400

    started working on Marksmanship Hunter

--- src/Parser/AVAILABLE_CONFIGS.js
+++ src/Parser/AVAILABLE_CONFIGS.js
@@ -4,6 +4,7 @@ import RestoDruid from './RestoDruid/CONFIG';
 import ElementalShaman from './ElementalShaman/CONFIG';
 import RestorationShaman from './RestorationShaman/CONFIG';
 import MistweaverMonk from './MistweaverMonk/CONFIG';
+import MarksmanshipHunter from './MarksmanshipHunter/CONFIG';
 
 // Order of this should be the order in which specs were added to production
 export default [
@@ -13,4 +14,5 @@ export default [
   MistweaverMonk,
   RestorationShaman,
   ElementalShaman,
+  MarksmanshipHunter,
 ];
---
+++ src/Parser/MarksmanshipHunter/CombatLogParser.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import SpellIcon from 'common/SpellIcon';
+import Icon from 'common/Icon';
+import ITEMS from 'common/ITEMS';
+import ItemLink from 'common/ItemLink';
+import ItemIcon from 'common/ItemIcon';
+
+import StatisticBox from 'Main/StatisticBox';
+import SuggestionsTab from 'Main/SuggestionsTab';
+import TalentsTab from 'Main/TalentsTab';
+import CastEfficiencyTab from 'Main/CastEfficiencyTab';
+import CooldownsTab from 'Main/CooldownsTab';
+
+import MainCombatLogParser from 'Parser/Core/CombatLogParser';
+import ParseResults from 'Parser/Core/ParseResults';
+import getCastEfficiency from 'Parser/Core/getCastEfficiency';
+import ISSUE_IMPORTANCE from 'Parser/Core/ISSUE_IMPORTANCE';
+import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
+
+// import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
+// import CooldownTracker from './Modules/Features/CooldownTracker';
+
+// import T20 from './Modules/Legendaries/T20';
+
+// import CPM_ABILITIES, { SPELL_CATEGORY } from './CPM_ABILITIES';
