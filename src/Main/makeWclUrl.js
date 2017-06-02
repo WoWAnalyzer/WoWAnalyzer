d82e8ddc924bf35d260513e457c71cd7375d8649
Martijn Hols <martijnhols@hotmail.com>
2017-06-02 20:12:04 +0200

    Don't cache bust fights

--- src/Main/makeWclUrl.js
+++ src/Main/makeWclUrl.js
@@ -2,7 +2,7 @@ import makeUrl from './makeUrl';
 
 import WCL_API_KEY from './WCL_API_KEY';
 
-export default function makeWclUrl(base, queryParams) {
+export default function makeWclUrl(base, queryParams = {}) {
   queryParams.api_key = WCL_API_KEY;
   return makeUrl(base, queryParams);
 }
