551ba0f55e6247b7071058c7f1ab3199931ca40c
Ryan Park <parky.fp@gmail.com>
2017-06-05 19:00:13 +0100

    Changed config and added changelog

--- src/Parser/DisciplinePriest/CONFIG.js
+++ src/Parser/DisciplinePriest/CONFIG.js
@@ -9,7 +9,7 @@ import CombatLogParser from './CombatLogParser';
 export default {
   spec: SPECS.DISCIPLINE_PRIEST,
   parser: CombatLogParser,
-  maintainer: '@Zerotorescue',
+  maintainer: '@Reglitch',
   footer: (
     <div>
       Please consider donating to help out further development.<br />
