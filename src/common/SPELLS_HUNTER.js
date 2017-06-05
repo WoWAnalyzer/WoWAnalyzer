ded5c62e4fadee806d0fdfe2931832aa7dc8b7ce
Lawrence Matacena <lmatacena@stopforschools.com>
2017-06-05 15:32:06 -0400

    started adding hunter spells: t1 and t2 talents

---
+++ src/common/SPELLS_HUNTER.js
@@ -0,0 +1,48 @@
+export default {
+  // All
+  // Spec: Marksmanship
+  // Spec: Beast Mastery
+  // Spec: Survival
+  // Talents: Marksmanship
+  LONE_WOLF_TALENT: {
+    id: 155228,
+    name: 'Lone Wolf',
+    icon: 'spell_hunter_lonewolf',
+    description: 'Use at all times since it simply provides the most damage and requires no management. It also improves target switching capability by removing the pet, putting more of the total damage in the hands of the hunter.',
+  },
+  STEADY_FOCUS_TALENT: {
+    id: 193533,
+    name: 'Steady Focus',
+    icon: 'ability_hunter_improvedsteadyshot',
+    description: 'Decent choice if you enjoy having a pet, but with the new patch where Focus is not the limit as much as it is time, it is significantly worse than <a href="http://www.wowhead.com/spell=155228/lone-wolf" target="_blank">Lone Wolf</a> in terms of damage output, and even worse in terms of target switching since you will have a pet if you use this talent.',
+  },
+  CAREFUL_AIM_TALENT: {
+    id: 53238,
+    name: 'Careful Aim',
+    icon: 'ability_hunter_zenarchery',
+    description: 'Vastly inferior choice to <a href="http://www.wowhead.com/spell=155228/lone-wolf" target="_blank">Lone Wolf</a>.',
+  },
+  LOCK_AND_LOAD_TALENT: {
+    id: 194595,
+    name: 'Lock and Load',
+    icon: 'ability_hunter_lockandload',
+    description: 'Always use when there is regular target switching in the fight.',
+  },
+  BLACK_ARROW_TALENT: {
+    id: 194599,
+    name: 'Black Arrow',
+    icon: 'spell_shadow_painspike',
+    description: 'Not worth using for any PvE situation outside leveling.',
+  },
+  TRUE_AIM_TALENT: {
+    id: 199527,
+    name: 'True Aim',
+    icon: 'spell_hunter_focusingshot',
+    description: 'The best pure single-target option by a fair bit, but only really when combined with the level 100 talent <a href="http://www.wowhead.com/spell=199522/trick-shot" target="_blank">Trick Shot</a> on a pure single-target, as they add together to vastly increase Aimed Shot damage. When not using <a href="http://www.wowhead.com/spell=199522/trick-shot" target="_blank">Trick Shot</a>, <a href="http://www.wowhead.com/spell=194595/lock-and-load" target="_blank">Lock and Load</a> is always the best choice. NOTE: <a href="http://www.wowhead.com/spell=199527/true-aim" target="_blank">True Aim</a> cannot be used together with <a href="http://www.wowhead.com/spell=199522/trick-shot" target="_blank">Trick Shot</a> for situations where you will be making use of <a href="http://www.wowhead.com/spell=199522/trick-shot" target="_blank">Trick Shot</a>\'s ability to cleave, since the AoE-Aimed Shots that <a href="http://www.wowhead.com/spell=199522/trick-shot" target="_blank">Trick Shot</a> causes will cancel out <a href="http://www.wowhead.com/spell=199527/true-aim" target="_blank">True Aim</a>\'s beneficial effect on a single target, as it will constantly reproc on different targets.',
+  },
+  // Talents: Beast Mastery
+  // Talents: Survival
+  // Traits: Marksmanship
+  // Traits: Beast Mastery
+  // Traits: Survival
+};
