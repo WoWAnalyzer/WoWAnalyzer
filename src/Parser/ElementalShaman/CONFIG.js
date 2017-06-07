e606654b46b0d97ce60f71ffebf853e49bf3c3ef
Martijn Hols <martijnhols@hotmail.com>
2017-06-07 23:20:30 +0200

    Fixed Elemental Shaman footer sizing

--- src/Parser/ElementalShaman/CONFIG.js
+++ src/Parser/ElementalShaman/CONFIG.js
@@ -8,9 +8,11 @@ export default {
   parser: CombatLogParser,
   maintainer: '@fasib',
   footer: (
-    <div>
-      Based on Guides from <a href='https://www.stormearthandlava.com/'>Storm Earth and Lava</a>.<br />
-      Questions about Elementals? Visit <a href='http://www.discord.me/earthshrine'>Earthshrine</a> Discord.<br />
+    <div className="panel fade-in" style={{ margin: '15px auto 30px', maxWidth: 400, textAlign: 'center' }}>
+      <div className="panel-body text-muted">
+        Based on Guides from <a href='https://www.stormearthandlava.com/'>Storm Earth and Lava</a>.<br />
+        Questions about Elementals? Visit <a href='http://www.discord.me/earthshrine'>Earthshrine</a> Discord.<br />
+      </div>
     </div>
   ),
 };
