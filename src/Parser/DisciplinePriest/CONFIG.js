d6f44a79e2f40ada74f3dbd810c151e742748578
Martijn Hols <martijnhols@hotmail.com>
2017-06-07 23:15:32 +0200

    Removed Disc Priest footer since I'm no longer maintaining it

--- src/Parser/DisciplinePriest/CONFIG.js
+++ src/Parser/DisciplinePriest/CONFIG.js
@@ -1,8 +1,4 @@
-import React from 'react';
-
 import SPECS from 'common/SPECS';
-
-import PatreonLink from 'Main/PatreonLink';
 
 import CombatLogParser from './CombatLogParser';
 
@@ -10,10 +6,4 @@ export default {
   spec: SPECS.DISCIPLINE_PRIEST,
   parser: CombatLogParser,
   maintainer: '@Reglitch',
-  footer: (
-    <div>
-      Please consider donating to help out further development.<br />
-      <PatreonLink />
-    </div>
-  ),
 };
