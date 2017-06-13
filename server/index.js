e951ca4527c3ad328401344b684f97c182d4a61a
Martijn Hols <martijnhols@hotmail.com>
2017-06-13 23:13:04 +0200

    Fully implement server-side caching

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const querystring = require('querystring');
+const https = require('https');
 
 const cache = require('./cache');
 
@@ -21,22 +22,52 @@ app.get('/report/:reportCode([A-Za-z0-9]+)/:playerName([^/]{2,})?/:fightId([0-9]
   res.sendFile(path.join(buildFolder, 'index.html'));
 });
 app.get('/api/v1/*', function (req, res) {
-  // https:///v1/report/events/
-
+  console.time('request');
   const requestUrl = `${req.params[0]}?${querystring.stringify(req.query)}`;
-  let result = cache.get(requestUrl);
-  if (!result) {
-    // TODO: If not exist, fetch from WCL, then store in cache automatically purging oldest entry should memory be an issue
+  const json = cache.get(requestUrl);
+  if (json) {
+    console.timeEnd('request');
+    res.json(json);
+  }
+  if (!json) {
     const query = Object.assign({}, req.query, {
       api_key: '97c84db1a100b32a6d5abb763711244e',
     });
 
-    result = null;
-    // TODO: Cache must be in JS format since I want to be able to read it server-side.
-    cache.set(requestUrl, result);
+    const options = {
+      host: 'www.warcraftlogs.com',
+      path: `/v1/${req.params[0]}?${querystring.stringify(query)}`,
+      headers: {
+        'User-Agent': 'WoWAnalyzer.com API',
+      },
+    };
+    console.log('GET', options.path);
+    console.time('wclApi');
+    https.get(options, function (wclRes) {
+      let responseContent = '';
+      wclRes.on('data', function (chunk) {
+        responseContent += chunk;
+      });
+      wclRes.on('end', function () {
+        if (wclRes.statusCode === 200) {
+          console.timeEnd('wclApi');
+          try {
+            const json = JSON.parse(responseContent);
+            console.log('Request success!');
+            cache.set(requestUrl, json, 7200);
+            console.timeEnd('request');
+            res.json(json);
+          } catch (e) {
+            console.error('Error parsing JSON!', e);
+          }
+        } else {
+          console.error('Status:', wclRes.statusCode);
+        }
+      });
+    }).on('error', function (err) {
+      console.error('Error:', err);
+    });
   }
-
-  res.send(result);
 
   // TODO: Handle fights list cache busting smartly (ignore _ when writing to cache so that _-less requests get the latest version)
 });
