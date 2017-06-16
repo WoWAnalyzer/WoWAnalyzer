cc179ec398a86191f69a3a75c1986b651eecd450
Martijn Hols <martijnhols@hotmail.com>
2017-06-17 00:28:03 +0200

    If cache key exists already we have to remove it from the index to avoid it being removed early.

--- server/cache.js
+++ server/cache.js
@@ -1,7 +1,7 @@
 const NodeCache = require('node-cache');
 
 const MEMORY_LIMIT = 512 * 1024 * 1024;
-const MEMORY_LIMIT_DANGEROUS = 1024 * 1024 * 1024;
+const MEMORY_LIMIT_DANGEROUS = MEMORY_LIMIT * 2;
 function getCurrentMemoryUsage() {
   const memoryUsage = process.memoryUsage();
   return memoryUsage.rss;
@@ -14,11 +14,20 @@ class Cache extends NodeCache {
     super.set(key, val, ttl, callback);
 
     const index = this.get(INDEX_KEY) || [];
+
+    const keyExistingIndex = index.indexOf(key);
+    if (keyExistingIndex !== -1) {
+      // If key exists already we have to remove it from the index to avoid it being removed early.
+      index.splice(keyExistingIndex, 1);
+    }
+
     // If that last set pushed us over the memory limit, remove the first key. I assume here the first key is about the same size.
     const memoryUsage = getCurrentMemoryUsage();
     if (memoryUsage > MEMORY_LIMIT) {
       const firstKey = index.shift();
-      this.delete(firstKey);
+      if (firstKey) {
+        this.delete(firstKey);
+      }
     }
     if (memoryUsage > MEMORY_LIMIT_DANGEROUS) {
       this.flushAll();
