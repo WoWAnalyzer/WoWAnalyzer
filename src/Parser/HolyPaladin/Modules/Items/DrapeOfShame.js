d24ce8e661357d6f87b5f198c97bb0d4eb02a881
Martijn Hols <martijnhols@hotmail.com>
2017-06-17 22:47:58 +0200

    Deduplicate DRAPE_OF_SHAME_CRIT_EFFECT const

--- src/Parser/HolyPaladin/Modules/Items/DrapeOfShame.js
+++ src/Parser/HolyPaladin/Modules/Items/DrapeOfShame.js
@@ -2,9 +2,7 @@ import SPELLS from 'common/SPELLS';
 
 import HIT_TYPES from 'Parser/Core/HIT_TYPES';
 
-import CoreDrapeOfShame from 'Parser/Core/Modules/Items/DrapeOfShame';
-
-export const DRAPE_OF_SHAME_CRIT_EFFECT = 0.05;
+import CoreDrapeOfShame, { DRAPE_OF_SHAME_CRIT_EFFECT } from 'Parser/Core/Modules/Items/DrapeOfShame';
 
 class DrapeOfShame extends CoreDrapeOfShame {
   on_byPlayer_heal(event) {
