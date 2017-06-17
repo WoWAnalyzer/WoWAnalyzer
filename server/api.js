bd237daecf58763baea4ab04733b765b0b923012
Martijn Hols <martijnhols@hotmail.com>
2017-06-17 14:18:45 +0200

    Improve API logging

--- server/api.js
+++ server/api.js
@@ -21,15 +21,13 @@ module.exports = function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const requestUrl = `${req.params[0]}?${querystring.stringify(req.query)}`;
-  console.time('request');
   const jsonString = !cacheBust && cache.get(requestUrl);
   if (jsonString) {
-    console.timeEnd('request');
-    console.log('\x1b[32m%s\x1b[0m', 'cache hit');
+    console.log('++++++cache hit', requestUrl);
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.send(jsonString);
   } else {
-    console.log('\x1b[31m%s\x1b[0m', 'cache miss');
+    console.log('------cache miss', requestUrl);
     const query = Object.assign({}, req.query, {
       // Allow users to provide their own API key. This is required during development so that other developers don't lock out the production in case they mess something up.
       api_key: req.query.api_key || '97c84db1a100b32a6d5abb763711244e',
@@ -53,18 +51,15 @@ module.exports = function (req, res) {
           console.timeEnd('wcl');
 
           if (wclResponse.statusCode === 200) {
-            console.log('Request success!');
             cache.set(requestUrl, jsonString);
           } else {
-            console.error('Status:', wclResponse.statusCode);
+            console.error('Error status:', wclResponse.statusCode);
           }
 
           // Clone WCL response
           res.setHeader('Content-Type', wclResponse.headers['content-type']);
           res.status(wclResponse.statusCode);
           res.send(jsonString);
-
-          console.timeEnd('request');
         });
       })
       .on('error', (err) => {
