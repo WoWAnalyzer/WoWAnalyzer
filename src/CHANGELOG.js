96a39faaa2cc5493cc3e470bc221ac55cb3adf49
Anomoly <jkippley@gmail.com>
2017-06-18 09:00:14 -0400

    CHANGELOG.js updates

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+18-06-2017 - Mistweaver Monk: Implement Dead Time / Non-Healing Time.  Update Mana Tea data tip to show MP5 (by anomoly)
 18-06-2017 - Added Archive of Faith, Barbaric Mindslaver, The Deceiver's Grand Design and Sea Star of the Depthmother to all specs, trinket implementations by anomoly. (by Zerotorescue)
 17-06-2017 - Holy Paladin: Add Soul of the Highlord legendary. (by Zerotorescue)
 17-06-2017 - Disc Priest: Evangelism casts are now also shown under the cooldowns tab. Rapture now shows the total abosrbs applied and the amount of damage absorbed. Fixed a few issues that caused too much healing to be assigned to Evangelism. (by Zerotorescue)
