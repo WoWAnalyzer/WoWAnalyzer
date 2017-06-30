0c399c75e33693a58610f1e4e872692656a5b961
Martijn Hols <martijnhols@hotmail.com>
2017-06-30 20:53:44 +0200

    Remove talents from development tab since it's already visible in talents tab Show item/spell/trait name is it is known

--- src/Main/DevelopmentTab.js
+++ src/Main/DevelopmentTab.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import SPECS from 'common/SPECS';
+import ITEMS from 'common/ITEMS';
 import SPELLS from 'common/SPELLS';
 import Icon from 'common/Icon';
 import ItemLink from 'common/ItemLink';
@@ -79,88 +80,11 @@ ${item.name.toUpperCase().replace(/[^A-Z ]/g, '').replace(/ /g, '_')}: {
           </div>
           <div className="col-md-6">
             <ItemLink id={item.id} quality={item.quality} details={item}>
-              <Icon icon={item.icon} alt={item.icon} style={{ height: '1.6em' }} /> itemID: {item.id}
+              <Icon icon={item.icon} alt={item.icon} style={{ height: '1.6em' }} /> {ITEMS[item.id] ? ITEMS[item.id].name : `itemID: ${item.id}`}
             </ItemLink>
           </div>
           <div className="col-md-3">
             itemLevel: {item.itemLevel}
-          </div>
-        </div>
-
-        {this.state.expanded && (
-          <div>
-            {this.state.data ? (
-              <pre ref={elem => selectText(elem)}>
-                <code>
-                  {this.renderCode()}
-                </code>
-              </pre>
-            ) : 'Loading...'}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-class Talent extends React.PureComponent {
-  static propTypes = {
-    talent: PropTypes.object,
-    slotNo: PropTypes.number,
-  };
-
-  constructor() {
-    super();
-    this.state = {
-      expanded: false,
-      data: null,
-    };
-  }
-
-  view(id) {
-    this.setState({
-      expanded: !this.state.expanded,
-    });
-
-    if (this.state.data) {
-      return;
-    }
-
-    return fetch(`https://us.api.battle.net/wow/spell/${id}?locale=en_US&apikey=n6q3eyvqh2v4gz8t893mjjgxsf9kjdgz`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          data: data,
-        });
-      });
-  }
-
-  renderCode() {
-    const item = this.state.data;
-    return `
-${item.name.toUpperCase().replace(/[^A-Z ]/g, '').replace(/ /g, '_')}_TALENT: {
-  id: ${item.id},
-  name: '${item.name.replace("'", "\\'")}',
-  icon: '${item.icon}',
-},`;
-  }
-
-  render() {
-    const { talent, slotNo } = this.props;
-
-    return (
-      <div>
-        <div className="row" onClick={() => this.view(talent.id)}>
-          <div className="col-md-3">
-            level {Math.min(100, (slotNo + 1) * 15)}
-          </div>
-          <div className="col-md-6">
-            <SpellLink id={talent.id}>
-              <Icon icon={talent.icon} alt={talent.icon} style={{ height: '1.6em' }} /> spellID: {talent.id}
-            </SpellLink>
-          </div>
-          <div className="col-md-3">
-            {SPELLS[talent.id] && 'Already known'}
           </div>
         </div>
 
@@ -217,7 +141,7 @@ class DevelopmentTab extends React.Component {
                   return (
                     <li key={`${i}-${aura.ability}`}>
                       <SpellLink id={aura.ability}>
-                        <Icon icon={aura.icon} alt={aura.icon} style={{ height: '1.6em' }} /> spellID: {aura.ability}
+                        <Icon icon={aura.icon} alt={aura.icon} style={{ height: '1.6em' }} /> {SPELLS[aura.ability] ? SPELLS[aura.ability].name : `spellID: ${aura.ability}`}
                       </SpellLink>{' '}
                       source: <span className={specClassName}>{source ? source.name : aura.source}</span>{' '}
                     </li>
@@ -236,22 +160,12 @@ class DevelopmentTab extends React.Component {
               </ul>
             </div>
             <div className="col-md-6">
-              Talents:
-              <ul className="list">
-                {combatant._combatantInfo.talents.map((talent, i) => (
-                  <li key={`${i}-${talent.id}`}>
-                    <Talent talent={talent} slotNo={i} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="col-md-6">
               Traits:
               <ul className="list">
                 {combatant._combatantInfo.artifact.map(trait => (
                   <li key={trait.traitID}>
                     <SpellLink id={trait.spellID}>
-                      <Icon icon={trait.icon} alt={trait.icon} style={{ height: '1.6em' }} /> spellID: {trait.spellID}
+                      <Icon icon={trait.icon} alt={trait.icon} style={{ height: '1.6em' }} /> {SPELLS[trait.id] ? SPELLS[trait.id].name : `spellID: ${trait.spellID}`}
                     </SpellLink>{' '}
                     traitID: {trait.traitID}{' '}
                     rank: {trait.rank}
