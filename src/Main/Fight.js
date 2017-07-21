04c92a49eb4c6a4e085ddd2f2d0a6d4fa30bc770
Martijn Hols <martijnhols@hotmail.com>
2017-07-21 23:08:31 +0200

    Show wipe counts again

--- src/Main/Fight.js
+++ src/Main/Fight.js
@@ -26,7 +26,7 @@ const Fight = ({ difficulty, name, kill, start_time, end_time, wipes, fightPerce
   return (
     <div className="row" {...others}>
       <div className="col-xs-6 col-sm-5 col-md-4 col-lg-3">
-        {DIFFICULTIES[difficulty]} {name}
+        {DIFFICULTIES[difficulty]} {name} {!kill && `(Wipe ${wipes})`}
       </div>
       <div className={`col-xs-6 col-sm-7 col-md-8 col-lg-9 ${kill ? 'kill' : 'wipe'}`}>
         <Icon style={{ fontSize: '1.8em', display: 'inline-block', marginBottom: '-0.25em' }} />
