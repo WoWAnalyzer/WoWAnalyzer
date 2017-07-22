56d3e6b62980a8d4bbdf01d05bddb2ceb86c3f1c
AttilioLH <jomaca92@gmail.com>
2017-07-22 12:11:28 -0500

    Spell ID correction

--- src/Parser/WindwalkerMonk/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/WindwalkerMonk/Modules/Features/AlwaysBeCasting.js
@@ -5,9 +5,10 @@ import CoreAlwaysBeCasting from 'Parser/Core/Modules/AlwaysBeCasting';
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
   static ABILITIES_ON_GCD = [
     // Windwalker:
-    SPELLS.BLACK_OUT_KICK.id,
+    SPELLS.BLACKOUT_KICK.id,
     SPELLS.RISING_SUN_KICK.id,
     SPELLS.TIGER_PALM.id,
+    // To Do: Complete list of spells.
     // Monk:
 
     // Items:
