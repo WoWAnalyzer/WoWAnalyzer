610a319262c8f3d74797053f1cade67daa1e7e9a
Martijn Hols <martijnhols@hotmail.com>
2017-07-22 12:46:03 +0200

    Fix server url matching

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.static(buildFolder));
 app.get('/', function (req, res) {
   res.sendFile(path.join(buildFolder, 'index.html'));
 });
-app.get('/report/:reportCode([A-Za-z0-9]+)/:fightId([0-9]+|[0-9]+-.*)?/:playerName([^/]{2,})?/:tab([A-Za-z0-9-]+)?', function (req, res) {
+app.get('/report/:reportCode([A-Za-z0-9]+)/:fightId([0-9]+)?:fightName(-[^/]+)?/:playerName([^/]{2,})?/:tab([A-Za-z0-9-]+)?', function (req, res) {
   // TODO: Manipulate OpenGraph based on cached report info
   // TODO: Change fightId to be fight name now that it's unique with wipe numbers
   res.sendFile(path.join(buildFolder, 'index.html'));
