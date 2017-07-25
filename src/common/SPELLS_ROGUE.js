2a554eebb2f9a30e339b0158173348c1b84a62bb
zealk <zealotcs@gmail.com>
2017-07-25 04:51:57 -0700

    SubtletyRogue initail draft

---
+++ src/common/SPELLS_ROGUE.js
@@ -0,0 +1,113 @@
+export default {
+  // Defensive general spells
+  FEINT: {
+    id: 1966,
+    name: 'Feint',
+    icon: 'ability_rogue_feint',
+  },
+  CRIMSON_VAIL: {
+    id: 185311,
+    name: 'Crimson Vial',
+    icon: 'ability_rogue_crimsonvial',
+  },
+  CLOAK_OF_SHADOWS: {
+    id: 31224,
+    name: 'Cloak of Shadows',
+    icon: 'spell_shadow_nethercloak',
+  },
+  EVASION: {
+    id: 5277,
+    name: 'Evasion',
+    icon: 'spell_shadow_shadowward',
+  },
+  
+  // General spells
+  KICK: {
+    id: 1766,
+    name: 'Kick',
+    icon: 'ability_kick',
+  },
+  SPRINT: {
+    id: 2983,
+    name: 'Sprint',
+    icon: 'ability_rogue_sprint',
+  },
+  VANISH: {
+    id: 1856,
+    name: 'Vanish',
+    icon: 'ability_vanish',
+  },
+  SHADOWSTEP: {
+    id: 36554,
+    name: 'Shadowstep',
+    icon: 'ability_rogue_shadowstep',
+  },
+  
+  // Subtlety spells
+  // Combo point generating damage ability
+  BACKSTAB: {
+    id: 53,
+    name: 'Backstab',
+    icon: 'ability_backstab',
+  },
+  SHADOWSTRIKE: {
+    id: 185438,
+    name: 'Shadowstrike',
+    icon: 'ability_rogue_shadowstrike',
+  },
+  SHURIKEN_STORM: {
+    id: 197835,
+    name: 'Shuriken Storm',
+    icon: 'ability_rogue_shurikenstorm',
+  },
+  SHURIKEN_TOSS: {
+    id: 114014,
+    name: 'Shuriken Toss',
+    icon: 'inv_throwingknife_07',
+  },
+  // Main finishing move
+  NIGHTBLADE: {
+    id: 195452,
+    name: 'Nightblade',
+    icon: 'ability_rogue_nightblade',
+  },
+  EVISCERATE: {
+    id: 196819,
+    name: 'Eviscerate',
+    icon: 'ability_rogue_eviscerate',
+  },
+  // Offensive cooldown
+  SHADOW_BLADES: {
+    id: 121471,
+    name: 'Shadow Blades',
+    icon: 'inv_knife_1h_grimbatolraid_d_03',
+  },
+  SHADOW_DANCE: {
+    id: 185313,
+    name: 'Shadow Dance',
+    icon: 'ability_rogue_shadowdance',
+  },
+  SYMBOLS_OF_DEATH: {
+    id: 212283,
+    name: 'Symbols of Death',
+    icon: 'spell_shadow_rune',
+  },
+  GOREMAWS_BITE: {
+    id: 209782,
+    name: 'Goremaw\'s Bite',
+    icon: 'inv_knife_1h_artifactfangs_d_01',
+  },
+  // Triggered damage
+  SHADOW_NOVA: {
+    id: 197800,
+    name: 'Shadow Nova',
+    icon: 'spell_fire_twilightnova',
+  },
+  // Abilities form talent
+  DEATH_FROM_ABOVE: {
+    id: 178236,
+    name: 'Death from Above',
+    icon: 'spell_rogue_deathfromabove',
+  },
+
+};
