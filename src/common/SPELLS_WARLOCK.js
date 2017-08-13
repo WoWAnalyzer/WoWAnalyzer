1fd4782ff94f6663ed43f9f3c077fbc4d7a937d1
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-08-13 22:03:57 +0200

    Add warlock spells
    Added Warlock spells (unused stored elsewhere) + initial commit

---
+++ src/common/SPELLS_WARLOCK.js
@@ -0,0 +1,197 @@
+export default {
+  // Shared spells
+  LIFE_TAP: {
+    id: 1454,
+    name: 'Life Tap',
+    icon: 'spell_shadow_burningspirit',
+  },
+  SOUL_LEECH_BUFF: {
+    id: 108366,
+    name: 'Soul Leech',
+    icon: 'warlock_siphonlife',
+  },
+  SUMMON_DOOMGUARD_UNTALENTED: {
+    id: 18540,
+    name: 'Summon Doomguard',
+    icon: 'warlock_summon_doomguard',
+  },
+  SUMMON_INFERNAL_UNTALENTED: {
+    id: 1122,
+    name: 'Summon Infernal',
+    icon: 'spell_shadow_summoninfernal',
+  },
+  UNENDING_RESOLVE: {
+    id: 104773,
+    name: 'Unending Resolve',
+    icon: 'spell_shadow_demonictactics',
+  },
+
+  // Shared talents
+  BURNING_RUSH: {
+    id: 111400,
+    name: 'Burning Rush',
+    icon: 'ability_deathwing_sealarmorbreachtga',
+  },
+  EMPOWERED_LIFE_TAP_BUFF: {
+    id: 235156,
+    name: 'Empowered Life Tap',
+    icon: 'spell_shadow_manafeed',
+  },
+  MORTAL_COIL: {
+    id: 6789,
+    name: 'Mortal Coil',
+    icon: 'ability_warlock_mortalcoil',
+  },
+  MORTAL_COIL_HEAL: {
+    id: 108396,
+    name: 'Mortal Coil',
+    icon: 'ability_warlock_mortalcoil',
+  },
+  SOUL_HARVEST: {
+    id: 196098,
+    name: 'Soul Harvest',
+    icon: 'spell_warlock_demonsoul',
+  },
+  DARK_PACT: {
+    id: 108416,
+    name: 'Dark Pact',
+    icon: 'warlock_sacrificial_pact',
+  },
+  GRIMOIRE_IMP: {
+    id: 111859,
+    name: 'Grimoire: Imp',
+    icon: 'spell_shadow_summonimp',
+  },
+  GRIMOIRE_VOIDWALKER: {
+    id: 111895,
+    name: 'Grimoire: Voidwalker',
+    icon: 'spell_shadow_summonvoidwalker',
+  },
+  GRIMOIRE_FELHUNTER: {
+    id: 111897,
+    name: 'Grimoire: Felhunter',
+    icon: 'spell_shadow_summonfelhunter',
+  },
+  GRIMOIRE_SUCCUBUS: {
+    id: 111896,
+    name: 'Grimoire: Succubus',
+    icon: 'spell_shadow_summonsuccubus',
+  },
+  GRIMOIRE_OF_SACRIFICE_BUFF: {
+    id: 196099,
+    name: 'Demonic Power',
+    icon: 'warlock_grimoireofsacrifice',
+  },
+  GRIMOIRE_OF_SACRIFICE_DAMAGE: {
+    id: 196100,
+    name: 'Demonic Power',
+    icon: 'warlock_grimoireofsacrifice',
+  },
+
+  // Affliction spells
+  AGONY: {
+    id: 980,
+    name: 'Agony',
+    icon: 'spell_shadow_curseofsargeras',
+  },
+  CORRUPTION_CAST: {
+    id: 172,
+    name: 'Corruption',
+    icon: 'spell_shadow_abominationexplosion',
+  },
+  CORRUPTION_DEBUFF: {
+    id: 146739,
+    name: 'Corruption',
+    icon: 'spell_shadow_abominationexplosion',
+  },
+  DRAIN_SOUL: {
+    id: 198590,
+    name: 'Drain Soul',
+    icon: 'spell_shadow_haunting',
+  },
+  REAP_SOULS: {
+    id: 216698,
+    name: 'Reap Souls',
+    icon: 'inv_staff_2h_artifactdeadwind_d_01',
+  },
+  TORMENTED_SOULS: {
+    id: 216695,
+    name: 'Tormented Souls',
+    icon: 'inv_staff_2h_artifactdeadwind_d_01',
+  },
+  DEADWIND_HARVESTER: {
+    id: 216708,
+    name: 'Deadwind Harvester',
+    icon: 'inv_misc_2h_farmscythe_a_01',
+  },
+  SEED_OF_CORRUPTION_DEBUFF: {
+    id: 27243,
+    name: 'Seed of Corruption',
+    icon: 'spell_shadow_seedofdestruction',
+  },
+  SEED_OF_CORRUPTION_EXPLOSION: {
+    id: 27285,
+    name: 'Seed of Corruption',
+    icon: 'spell_shadow_seedofdestruction',
+  },
+  UNSTABLE_AFFLICTION_CAST: {
+    id: 30108,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  //experimentalni varianta debuffu:
+  UNSTABLE_AFFLICTION_DEBUFF: {
+    ids: [233490, 233496, 233497, 233498, 233499],
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  /*
+  UNSTABLE_AFFLICTION_DEBUFF_1: {
+    id: 233490,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  UNSTABLE_AFFLICTION_DEBUFF_2: {
+    id: 233496,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  UNSTABLE_AFFLICTION_DEBUFF_3: {
+    id: 233497,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  UNSTABLE_AFFLICTION_DEBUFF_4: {
+    id: 233498,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  UNSTABLE_AFFLICTION_DEBUFF_5: {
+    id: 233499,
+    name: 'Unstable Affliction',
+    icon: 'spell_shadow_unstableaffliction_3',
+  },
+  */
+
+  // Affliction talents
+  PHANTOM_SINGULARITY: {
+    id: 205179,
+    name: 'Phantom Singularity',
+    icon: 'inv_enchant_voidsphere',
+  },
+  PHANTOM_SINGULARITY_HEAL: {
+    id: 205246,
+    name: 'Phantom Singularity',
+    icon: 'inv_enchant_voidsphere',
+  },
+  HAUNT: {
+    id: 48181,
+    name: 'Haunt',
+    icon: 'ability_warlock_haunt',
+  },
+  SIPHON_LIFE: {
+    id: 63106,
+    name: 'Siphon Life',
+    icon: 'spell_shadow_requiem',
+  },
+};
