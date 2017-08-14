1b369a9766b5c2e5e913403ed5d5a09fc132fb4d
Martijn Hols <martijnhols@hotmail.com>
2017-08-14 22:59:04 +0200

    Update changelog

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+14-08-2017 - Major under the hood changes, you might see some things move around but no data should be lost or changed. (by Zerotorescue)
 14-08-2017 - Holy Paladin: Reworded Maraad's Dying Breath display to be less confusing and moved the total display to the tooltip since it was being used wrong. (by Zerotorescue)
 13-05-2017 - Added <span class="Shaman">Enhancement Shaman</span> support by <b>@Nighteyez07</b>! Thanks a lot for your contribution!
 13-08-2017 - Guardian Druid: Added damage type into the tooltip of damage taken, added logic for Pulverize and minor fixes. (by wopr)
