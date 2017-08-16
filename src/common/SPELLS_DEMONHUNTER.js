a063c7416ac194f05eca83a036fe290ae90aa370
Ronald Pereira <ronald.drp11@gmail.com>
2017-08-16 15:15:26 -0300

    Added Vengeance Demon Hunter spells and artifact traits

---
+++ src/common/SPELLS_DEMONHUNTER.js
@@ -0,0 +1,306 @@
+export default {
+  // Vengeance Demon Hunter:
+
+  // Tier 20 Bonus:
+  VENG_DH_T20_2SET_BONUS_BUFF: {
+    id: 242228,
+    name: 'T20 2 set bonus',
+    icon: 'ability_demonhunter_spectank',
+  },
+  VENG_DH_T20_4SET_BONUS_BUFF: {
+    id: 242229,
+    name: 'T20 4 set bonus',
+    icon: 'ability_demonhunter_spectank',
+  },
+
+  // Talents:
+  DEMONIC_INFUSION: {
+    id: 236189,
+    name: 'Demonic Infusion',
+    icon: 'spell_warlock_demonsoul',
+  },
+  LAST_RESORT: {
+    id: 209258,
+    name: 'Last Resort',
+    icon: 'inv_glaive_1h_artifactaldorchi_d_06',
+  },
+  SOUL_BARRIER: {
+    id: 227225,
+    name: 'Soul Barrier',
+    icon: 'inv_glaive_1h_artifactaldrochi_d_05',
+  },
+  BLADE_TURNING: {
+    id: 247254,
+    name: 'Blade Turning',
+    icon: 'trade_archaeology_zinrokh-sword',
+  },
+  FEL_DEVASTATION: {
+    id: 212084,
+    name: 'Fel Devastation',
+    icon: 'ability_demonhunter_feldevastation',
+  },
+  SPIRIT_BOMB: {
+    id: 247454,
+    name: 'Spirit Bomb',
+    icon: 'inv_icon_shadowcouncilorb_purple',
+  },
+  FEL_ERUPTION: {
+    id: 211881,
+    name: 'Fel Eruption',
+    icon: 'ability_bossfellord_felspike',
+  },
+  CONCENTRATED_SIGILS: {
+      id: 207666,
+      name: 'Concentrated Sigils',
+      icon: 'ability_bossfelorcs_necromancer_red',
+  },
+  QUICKENED_SIGILS: {
+    id: 209281,
+    name: 'Quickened Sigils',
+    icon: 'ability_demonhunter_concentratedsigils',
+  },
+  SIGIL_OF_CHAINS: {
+    id: 202138,
+    name: 'Sigil of Chains',
+    icon: 'ability_demonhunter_sigilofchains',
+  },
+  SIGIL_OF_MISERY: {
+    id: 207684,
+    name: 'Sigil of Misery',
+    icon: 'ability_demonhunter_sigilofmisery',
+  },
+  FEED_THE_DEMON: {
+    id: 218612,
+    name: 'Feed the Demon',
+    icon: 'spell_warlock_demonicempowerment',
+  },
+  FRACTURE: {
+    id: 209795,
+    name: 'Fracture',
+    icon: 'ability_creature_felsunder',
+  },
+  SOUL_RENDING: {
+    id: 217996,
+    name: 'Soul Rending',
+    icon: 'ability_demonhunter_soulcleave2',
+  },
+  FELBLADE: {
+    id: 232893,
+    name: 'Felblade',
+    icon: 'ability_demonhunter_felblade',
+  },
+  FLAME_CRASH: {
+    id: 227322,
+    name: 'Flame Crash',
+    icon: 'spell_fire_felhellfire',
+  },
+  BURNING_ALIVE: {
+    id: 207739,
+    name: 'Burning Alive',
+    icon: 'spell_fire_elementaldevastation',
+  },
+  FALLOUT: {
+    id: 227174,
+    name: 'Fallout',
+    icon: 'spell_volatilefiregreen',
+  },
+  FEAST_OF_SOULS: {
+    id: 207697,
+    name: 'Feast of Souls',
+    icon: 'spell_shadow_soulleech',
+  },
+  ABYSSAL_STRIKE: {
+    id: 207550,
+    name: 'Abyssal Strike',
+    icon: 'spell_warlock_summonabyssal',
+  },
+  AGONIZING_FLAMES: {
+    id: 207548,
+    name: 'Agonizing Flames',
+    icon: 'achievment_raid_houroftwilight',
+  },
+  RAZOR_SPIKES: {
+    id: 209400,
+    name: 'Razor Spikes',
+    icon: 'ability_demonhunter_demonspikes2',
+  },
+
+  // Abilities:
+  EMPOWER_WARDS: {
+    id: 218256,
+    name: 'Empower Wards',
+    icon: 'ability_demonhunter_empowerwards',
+  },
+  SIGIL_OF_SILENCE: {
+    id: 202137,
+    name: 'Sigil of Silence',
+    icon: 'ability_demonhunter_sigilofsilence',
+  },
+  DEMON_SPIKES: {
+    id: 203720,
+    name: 'Demon Spikes',
+    icon: 'ability_demonhunter_demonspikes',
+  },
+  FIERY_BRAND: {
+    id: 204021,
+    name: 'Fiery Brand',
+    icon: 'ability_demonhunter_fierybrand',
+  },
+  METAMORPHOSIS: {
+    id: 187827,
+    name: 'Metamorphosis',
+    icon: 'ability_demonhunter_metamorphasistank',
+  },
+  SIGIL_OF_FLAME: {
+    id: 204596,
+    name: 'Sigil of Flame',
+    icon: 'ability_demonhunter_sigilofinquisition',
+  },
+  SOUL_CLEAVE: {
+    id: 228477,
+    name: 'Soul Cleave',
+    icon: 'ability_demonhunter_soulcleave',
+  },
+  THROW_GLAIVE: {
+    id: 204157,
+    name: 'Throw Glaive',
+    icon: 'ability_demonhunter_throwglaive',
+  },
+  IMMOLATION_AURA: {
+    id: 178740,
+    name: 'Immolation Aura',
+    icon: 'ability_demonhunter_immolation',
+  },
+  SHEAR: {
+    id: 203782,
+    name: 'Shear',
+    icon: 'ability_demonhunter_hatefulstrike',
+  },
+  INFERNAL_STRIKE: {
+    id: 189110,
+    name: 'Infernal Strike',
+    icon: 'ability_demonhunter_infernalstrike1',
+  },
+  TORMENT: {
+    id: 185245,
+    name: 'Torment',
+    icon: 'ability_demonhunter_torment',
+  },
+
+  // Traits:
+  SOUL_CARVER: {
+    id: 207407,
+    name: 'Soul Carver',
+    icon: 'inv_glaive_1h_artifactaldrochi_d_01',
+  },
+  ALDRACHI_DESIGN: {
+    id: 207343,
+    name: 'Aldrachi Design',
+    icon: 'inv_glaive_1h_artifactaldrochi_d_01dual',
+  },
+  AURA_OF_PAIN: {
+    id: 207347,
+    name: 'Aura of Pain',
+    icon: 'ability_demonhunter_immolation',
+  },
+  CHARRED_WARBLADES: {
+    id: 213010,
+    name: 'Charred Warblades',
+    icon: 'spell_fire_incinerate',
+  },
+  CONCORDANCE_OF_THE_LEGIONFALL: {
+    id: 239042,
+    name:'Concordance of the Legionfall',
+    icon: 'trade_archaeology_shark-jaws',
+  },
+  DEFENSIVE_SPIKES: {
+    id: 212829,
+    name: 'Defensive Spikes',
+    icon: 'ability_demonhunter_demonspikes',
+  },
+  DEMONIC_FLAMES: {
+    id: 212894,
+    name: 'Demonic Flames',
+    icon: 'ability_demonhunter_fierybrand',
+  },
+  DEVOUR_SOULS: {
+    id: 212821,
+    name: 'Devour Souls',
+    icon: 'ability_demonhunter_soulcleave',
+  },
+  EMBRACE_THE_PAIN: {
+    id: 212816,
+    name: 'Embrace the Pain',
+    icon: 'ability_demonhunter_metamorphasistank',
+  },
+  ERUPTING_SOULS: {
+    id: 238082,
+    name: 'Erupting Souls',
+    icon: 'ability_demonhunter_soulcleave',
+  },
+  FIERY_DEMISE: {
+    id: 212817,
+    name: 'Fiery Demise',
+    icon: 'ability_demonhunter_fierybrand',
+  },
+  FLAMING_SOUL: {
+    id: 238118,
+    name: 'Flaming Soul',
+    icon: 'ability_demonhunter_fierybrand',
+  },
+  FUELED_BY_PAIN: {
+    id: 213017,
+    name: 'Fueled by Pain',
+    icon: 'ability_demonhunter_metamorphasistank',
+  },
+  HONED_WARBLADES: {
+    id: 207352,
+    name: 'Honed Warblades',
+    icon: 'ability_demonhunter_hatefulstrike',
+  },
+  ILLIDARI_DURABILITY: {
+    id: 241091,
+    name: 'Illidari Durability',
+    icon: 'misc_legionfall_demonhunter',
+  },
+  INFERNAL_FORCE: {
+    id: 207375,
+    name: 'Infernal Force',
+    icon: 'ability_demonhunter_infernalstrike1',
+  },
+  LINGERING_ORDEAL: {
+    id: 238046,
+    name: 'Lingering Ordeal',
+    icon: 'ability_demonhunter_metamorphasistank',
+  },
+  PAINBRINGER: {
+    id: 207387,
+    name: 'Painbringer',
+    icon: 'artifactability_vengeancedemonhunter_painbringer',
+  },
+  SHATTER_THE_SOULS: {
+    id: 212827,
+    name: 'Shatter the Souls',
+    icon: 'ability_demonhunter_shatteredsouls',
+  },
+  SIPHON_POWER: {
+    id: 218910,
+    name: 'Siphon Power',
+    icon: 'ability_demonhunter_reversemagic',
+  },
+  SOULGORGER: {
+    id: 214909,
+    name: 'Soulgorger',
+    icon: 'inv_glaive_1h_artifactaldrochi_d_02dual',
+  },
+  TORMENTED_SOULS: {
+    id:214744,
+    name:'Tormented Souls',
+    icon: 'ability_demonhunter_soulcleave',
+  },
+  WILL_OF_THE_ILLIDARI: {
+    id: 212819,
+    name: 'Will of the Illidari',
+    icon: 'ability_demonhunter_spectank',
+  },
+};
