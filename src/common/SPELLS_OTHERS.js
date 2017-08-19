f16d2702d65c04513d69e53556d1c4e1938fd71f
Ronald Pereira <ronald.drp11@gmail.com>
2017-08-19 15:31:21 -0300

    Added unbending potion to the pre-potions and second potion options for tanks

--- src/common/SPELLS_OTHERS.js
+++ src/common/SPELLS_OTHERS.js
@@ -35,6 +35,11 @@ export default {
     name: 'Ancient Mana Potion',
     icon: 'inv_alchemy_70_blue',
   },
+  UNBENDING_POTION: {
+    id: 127845,
+    name: 'Unbending Potion',
+    icon: 'inv_alchemy_70_flask04',
+  },
   // Items buffs:
   GNAWED_THUMB_RING: {
     id: 228461,
