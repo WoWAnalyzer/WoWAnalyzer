545fda9f5007d3e9c6ee2ebd0aca477e6635b0b8
WOPR <shighman@gmail.com>
2017-08-19 15:05:30 -0400

    Updated changelog

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+19-08-2017 - Guardian Druid: Added detail on Ironfur usage.
 18-09-2017 - Added Affliction Warlock support by @Chizu.
 18-08-2017 - Guardian Druid: Updates to align with new module structure and added overkill into DTPS display.
 16-08-2017 - Mistweaver Monk: Soothing Mist suggestion added (by anomoly)
