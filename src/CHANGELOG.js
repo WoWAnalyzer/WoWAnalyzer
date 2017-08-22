3263c6b8add8f628eafb8d4be876c94013306f9c
WOPR <shighman@gmail.com>
2017-08-22 14:08:11 -0400

    Changelog update

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+22-08-2017 - Guardian Druid: Fix to Ironfur uptime suggestion (by wopr).
 21-08-2017 - Affliction Warlock: Added rest of the legendaries (apart from head), some ToS trinkets and T20 set bonuses. (by Chizu)
 21-08-2017 - Brewmaster Monk: Added T20 2pc and 4pc stats (by wopr)
 20-08-2017 - Brewmaster Monk: Added more information about what occured while ISB was up or not (by wopr)
