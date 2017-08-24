9eee37ba73eef90b1690952ecd3f66d0ec8b22ce
Martijn Hols <martijnhols@hotmail.com>
2017-08-24 22:12:34 +0200

    When API runs into an error, don't throw but just log it and send a 500 reponse

--- server/api.js
+++ server/api.js
@@ -71,7 +71,8 @@ module.exports = function (req, res) {
             console.log('Finished (memory:', Math.ceil(getCurrentMemoryUsage() / 1024 / 1024), 'MB', 'wcl:', Date.now() - wclStart, 'ms', ')');
           })
           .on('error', (e) => {
-            throw e;
+            console.error(e);
+            res.status(500).send();
           });
       })
       .on('error', (err) => {
