8372b659cc13efefbaefa113b4be76d3e2820608
WOPR <shighman@gmail.com>
2017-08-24 16:10:25 -0400

    Added changelog

--- src/CHANGELOG_BREWMASTER_MONK.js
+++ src/CHANGELOG_BREWMASTER_MONK.js
@@ -1,4 +1,5 @@
 export default `
+24-08-2017 - Brewmaster Monk: Added blackout combo statistic (by wopr)
 21-08-2017 - Brewmaster Monk: Fixed bug with stagger if a tick of the dot is absorbed it will calculate correctly (by wopr)
 21-08-2017 - Brewmaster Monk: Added T20 2pc and 4pc stats (by wopr)
 20-08-2017 - Brewmaster Monk: Added more information about what occured while ISB was up or not (by wopr)
