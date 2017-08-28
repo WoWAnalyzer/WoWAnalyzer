0cfd2df2fa529d3e28a74d267697b7b2f76d9878
Hans Ripa <hans.ripa@gmail.com>
2017-08-28 10:37:18 +0200

    cleaned up pets a bit and ABC GCD list should now be complete.

--- src/Parser/ShadowPriest/Modules/Core/Pets.js
+++ src/Parser/ShadowPriest/Modules/Core/Pets.js
@@ -1,15 +1,31 @@
 import Module from 'Parser/Core/Module';
 
 class Pets extends Module {
+  _sourceId = null;
   _pets = {}
 
   on_initialized() {
     this.active = true;
     this._pets = this.owner.report.friendlyPets.filter(pet => pet.petOwner === this.owner.player.id);
+
+    if(this._pet !== undefined){
+      const pet = this.fetchPet(this._pet);
+      if(pet) this._sourceId = pet.id;
+    }
+  }
+
+  on_damage(event){
+    if(this._sourceId !== undefined && event.sourceID === this._sourceId){
+      this._damageDone += event.amount;
+    }
   }
 
   get fetchPets(){
     return this._pets;
+  }
+
+  get damageDone(){
+    return this._damageDone;
   }
 
   fetchPet(pet){
