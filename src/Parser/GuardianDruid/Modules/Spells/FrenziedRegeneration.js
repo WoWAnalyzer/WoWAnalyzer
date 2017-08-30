a34386c072d41e96c7bc5ad78d8bd02456d38955
FaideWW <michael@faide.net>
2017-08-30 15:15:34 -0600

    Add suggestion for efficiency

--- src/Parser/GuardianDruid/Modules/Spells/FrenziedRegeneration.js
+++ src/Parser/GuardianDruid/Modules/Spells/FrenziedRegeneration.js
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import SPELLS from 'common/SPELLS';
+import { formatPercentage } from 'common/format';
+import SpellLink from 'common/SpellLink';
 
 import Module from 'Parser/Core/Module';
 
@@ -6,15 +10,23 @@ const WILDFLESH_MODIFIER_PER_RANK = 0.05;
 const FR_WINDOW_MS = 5000;
 const FR_MINIMUM_HP_HEAL = 0.05;
 
+const HEAL_THRESHOLD = 0.2;
+const HP_THRESHOLD = 0.7;
+
 class FrenziedRegeneration extends Module {
-  percentHPsAtCast = [];
-  percentHealsAtCast = [];
+  castData = [];
   damageEventsInWindow = [];
   _healModifier = 0.5;
+
+  // Charges are not used by this module, but they may be used in a future Death Recap module
   _charges = 2;
 
   get healModifier() {
     return this._healModifier;
+  }
+
+  get charges() {
+    return this._charges;
   }
 
   pruneDamageEvents(currentTimestamp) {
@@ -50,6 +62,7 @@ class FrenziedRegeneration extends Module {
       this.pruneDamageEvents(event.timestamp);
       const damageTakenInWindow = this.damageEventsInWindow.reduce((total, event) => total + event.damage, 0);
 
+      // TODO: is event ordering consistent here? (this cast event needs to happen before GoE removebuff)
       const goeModifier = this.owner.selectedCombatant.hasBuff(SPELLS.GUARDIAN_OF_ELUNE.id) ? 1.2 : 1;
 
       const healAmount = damageTakenInWindow * this.healModifier * goeModifier;
@@ -59,8 +72,11 @@ class FrenziedRegeneration extends Module {
         percentHeal = healAsPercentHP;
       }
 
-      this.percentHPsAtCast.push(percentHP);
-      this.percentHealsAtCast.push(percentHeal);
+      this.castData.push({
+        percentHP,
+        percentHeal,
+        actualHeal: healAmount,
+      });
     }
   }
 
@@ -71,9 +87,28 @@ class FrenziedRegeneration extends Module {
     });
   }
 
-  on_finished() {
-    console.log(this.percentHPsAtCast);
-    console.log(this.percentHealsAtCast);
+  // A cast is considered inefficient if the expected heal is less than 20% of max HP,
+  // and the target is above 70% HP at the time of the cast.
+  get inefficientCasts() {
+    return this.castData.filter(cast => (
+      cast.percentHeal <= HEAL_THRESHOLD &&
+      cast.percentHP >= HP_THRESHOLD
+    ));
+  }
+
+  suggestions(when) {
+    const inefficiency = this.inefficientCasts.length / this.castData.length;
+    when(inefficiency).isGreaterThan(0)
+      .addSuggestion((suggest, actual, recommended) => {
+        return suggest(
+          <span>You are casting <SpellLink id={SPELLS.FRENZIED_REGENERATION.id} /> inefficiently (at high HP and after low damage intake).  It is almost always better to wait until after you have taken a big hit to cast it, even if that means spending extended periods of time at maximum charges.  If you don't already have one, consider getting an FR prediction weakaura to assist you in casting it more effectively.</span>
+        )
+          .icon(SPELLS.FRENZIED_REGENERATION.icon)
+          .actual(`${formatPercentage(actual, 0)}% of casts had a predicted heal of less than ${formatPercentage(HEAL_THRESHOLD, 0)}% and were cast above ${formatPercentage(HP_THRESHOLD, 0)}% HP`)
+          .recommended(`${recommended}% is recommended`)
+          .regular(recommended + 0.05).major(recommended + 0.1);
+
+      });
   }
 }
 
