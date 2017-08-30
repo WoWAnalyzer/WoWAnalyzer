d9c9c6296dd03976c1419fa28a91e26b7d8df2a5
Dylan Bowers <dylan.volibowers@gmail.com>
2017-08-30 16:24:23 -0400

    renew the faith

--- src/Parser/HolyPriest/Modules/Features/RenewTheFaith.js
+++ src/Parser/HolyPriest/Modules/Features/RenewTheFaith.js
@@ -1,5 +1,9 @@
-import SPELLS from 'common/SPELLS';
+import React from 'react';
+import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+import SpellIcon from 'common/SpellIcon';
+import { formatPercentage, formatNumber } from 'common/format';
 
+import SPELLS from 'common/SPELLS';
 import Module from 'Parser/Core/Module';
 
 class RenewTheFaith extends Module {
@@ -29,6 +33,28 @@ class RenewTheFaith extends Module {
       this.overhealing += event.overheal || 0;
     }
   }
+
+  statistic() {
+    const rtfPercHPS = formatPercentage(this.owner.getPercentageOfTotalHealingDone(this.healing));
+    const rtfPercOH = formatPercentage(this.overhealing / (this.healing + this.overhealing));
+    const rtfRelPercHPS = formatPercentage(this.healing / this.owner.modules.divineHymn.healing);
+
+    //
+    return this.active && (
+      <StatisticBox
+        icon={<SpellIcon id={SPELLS.DIVINE_HYMN_CAST.id} />}
+        value={`${formatNumber(this.healing)}`}
+        label={(
+          <dfn data-tip={`Benefit gained from Renew the Faith (does not include Benediction renews). Assumes that every Divine Hymn cast is fully channeled. This was ${rtfPercHPS}% of your healing, had ${rtfPercOH}% overhealing and increased your Divine Hymn healing by ${rtfRelPercHPS}%`}>
+            Renew the Faith
+          </dfn>
+        )}
+      />
+    )
+    //
+  }
+
+  statisticOrder = STATISTIC_ORDER.TRAITS(1);
 }
 
 
