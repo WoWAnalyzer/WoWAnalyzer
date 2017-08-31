f5191515265021eff799e62f03a010d34cf93b42
Martijn Hols <martijnhols@hotmail.com>
2017-08-31 11:05:24 +0200

    Add localized urls test to ReportSelector.test.js

--- src/tests/Main/ReportSelector.test.js
+++ src/tests/Main/ReportSelector.test.js
@@ -1,10 +1,10 @@
 import ReportSelecter from 'Main/ReportSelecter';
 
 describe('ReportSelector', () => {
-  test('getCode accepts report code', () => {
+  test('getCode accepts just the report code', () => {
     expect(ReportSelecter.getCode('AB1CDEf2G3HIjk4L')).toBe('AB1CDEf2G3HIjk4L');
   });
-  test('getCode accepts base url', () => {
+  test('getCode accepts base report url', () => {
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L')).toBe('AB1CDEf2G3HIjk4L');
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L/')).toBe('AB1CDEf2G3HIjk4L');
   });
@@ -20,6 +20,10 @@ describe('ReportSelector', () => {
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L#fight=6&type=healing&source=10')).toBe('AB1CDEf2G3HIjk4L');
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L/#fight=6&type=healing&source=10')).toBe('AB1CDEf2G3HIjk4L');
   });
+  test('getCode accepts localized urls', () => {
+    expect(ReportSelecter.getCode('https://de.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L')).toBe('AB1CDEf2G3HIjk4L');
+    expect(ReportSelecter.getCode('https://kr.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L/#fight=3')).toBe('AB1CDEf2G3HIjk4L');
+  });
   test('getCode knows to only match the part between reports and #', () => {
     expect(ReportSelecter.getCode('https://www.AAAAAAAAAAAAAAAA.com/reports/AB1CDEf2G3HIjk4L#fight=6&type=healing&source=10')).toBe('AB1CDEf2G3HIjk4L');
     expect(ReportSelecter.getCode('https://www.AAAAAAAAAAAAAAAA.com/reports/AB1CDEf2G3HIjk4L#fight=6&type=AAAAAAAAAAAAAAAA&source=10')).toBe('AB1CDEf2G3HIjk4L');
@@ -30,5 +34,6 @@ describe('ReportSelector', () => {
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4AA#fight=6&type=healing&source=10')).toBe(null);
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/#fight=6&type=healing&source=10')).toBe(null);
     expect(ReportSelecter.getCode('https://www.warcraftlogs.com/')).toBe(null);
+    expect(ReportSelecter.getCode('https://www.warcraftlogs.com/reports/<report code>')).toBe(null);
   });
 });
