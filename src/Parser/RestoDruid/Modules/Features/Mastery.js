e2b8531602ab23ededb1aae7e8631c4ddb9e8409
Kelton Finch <keltonfinch@gmail.com>
2017-09-01 22:32:25 -0400

    Filled out more of Resto Druid mastery analyzer

--- src/Parser/RestoDruid/Modules/Features/Mastery.js
+++ src/Parser/RestoDruid/Modules/Features/Mastery.js
@@ -1,30 +1,80 @@
 import Module from 'Parser/Core/Module';
 import SPELLS from 'common/SPELLS';
 import indexById from 'common/SPELLS';
+import Combatants from 'Parser/Core/Modules/Combatants';
 
 import {ABILITIES_AFFECTED_BY_HEALING_INCREASES} from '../../Constants';
 import {HEALS_MASTERY_STACK} from '../../Constants';
 
 class Mastery extends Module {
+  static dependencies = {
+    combatants: Combatants,
+  };
 
-  hotHealingMap = new Map();
-  for(let healId of HEALS_MASTERY_STACK) {
-    hotHealingMap.set(healId, {'name':indexById(healId).name, 'direct':0, 'mastery':0});
+  totalNoMasteryHealing = 0;
+  druidSpellNoMasteryHealing = 0;
+  masteryTimesHealing = 0;
+
+  hotHealingMap = null;
+  masteryBuffs = null;
+
+  on_initialized() {
+    // TODO use JS objects instead of Maps for hotHealingMap and masteryBuffs?
+    this.hotHealingMap = new Map();
+    for(let healId of HEALS_MASTERY_STACK) {
+      this.hotHealingMap.set(healId, {'name':indexById(healId).name, 'direct':0, 'mastery':0});
+    }
+
+    this.masteryBuffs = new Map([
+        [ SPELLS.ASTRAL_HARMONY.id, { 'spell':SPELLS.ASTRAL_HARMONY, 'amount':4000 } ],
+        [ SPELLS.JACINS_RUSE.id, { 'spell':SPELLS.JACINS_RUSE, 'amount':3000 } ],
+    ]);
+    for(let[buffId, buffObj] of this.masteryBuffs.entries()) {
+  		buffObj.attributableHealing = 0;
+  		buffObj.isActive = false; // TODO: is buff presence already handled by WoWAnalyzer?
+  	}
   }
 
-  // TODO: is this already handled by WoWAnalyzer system, can I just query for this?
-  hotsOnTarget = new Map(); // map from playerId to set of hotIds on that player
+  // TODO handle pre proc mastery buffs? Looks like pre-hots are already handled by the system.
 
-  masteryBuffs = new Map([
-      [ SPELLS.ASTRAL_HARMONY.id, { 'spell':SPELLS.ASTRAL_HARMONY, 'amount':4000 } ],
-      [ SPELLS.JACINS_RUSE.id, { 'spell':SPELLS.JACINS_RUSE, 'amount':3000 } ],
-  ]);
-  for(let[buffId, buffObj] of masteryBuffs.entries()) {
-		buffObj.attributableHealing = 0;
-		buffObj.isActive = false; // TODO: is buff presence already handled by WoWAnalyzer?
-	}
+  on_byPlayer_heal(event) {
+    const spellId = event.ability.guid;
+    const target = this.combatants.getEntity(event);
+    const amount = event.amount + (event.absorbed === undefined : 0 ? event.absorbed);
 
+    if(target === null) {
+      return;
+    }
 
+    if(this.hotHealingMap.has(spellId)) {
+      this.hotHealingMap.get(spellId).direct += amount;
+    }
+
+    if(ABILITIES_AFFECTED_BY_HEALING_INCREASES.includes(spellId)) {
+      let hotsOn = target.activeBuffs().filter(buff => HEALS_MASTERY_STACK.includes(buff.ability.guid));
+      let numHotsOn = hotsOn.length;
+      let healWoMastery = getNoMasteryHealing(amount, numHotsOn);
+
+      this.totalNoMasteryHealing += healWoMastery;
+      this.druidSpellNoMasteryHealing += healWoMastery;
+      this.masteryTimesHealing += healWoMastery * numHotsOn;
+
+      // TODO give each HoT credit for mastery boosting
+
+      // TODO attribute healing to mastery buffs that are present
+
+    } else {
+      this.totalNoMasteryHealing += amount;
+    }
+  }
+
+  // TODO also count absorbs in total no mastery healing?
+
+  // TODO build suggested results
+
+  getNoMasteryHealing(amount, numHotsOn) {
+    // TODO implement
+  }
 
 }
 
