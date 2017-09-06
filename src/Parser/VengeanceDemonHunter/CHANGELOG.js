afa94e5aa82ab1d09dd82cebbaa7eb2a1ba2da02
Ronald Pereira <ronald.drp11@gmail.com>
2017-09-06 12:24:05 -0300

    Updated changelogs

--- src/Parser/VengeanceDemonHunter/CHANGELOG.js
+++ src/Parser/VengeanceDemonHunter/CHANGELOG.js
@@ -1,4 +1,6 @@
 export default `
+06-09-2017 - Added more suggestions to Immolation Aura buff uptime. (by Mamtooth)
+06-09-2017 - Fixed Sigil of Flame debuff uptime to best fit in more cases. (by Mamtooth)
 27-08-2017 - Fixed a bug with Spirit Bomb uptime suggestions and modified the Dead GCD recommended time (ABC). (by Mamtooth)
 20-08-2017 - Vengeance Demon Hunter: Added Pain tracker chart and changed Spirit Bomb uptime buff to Frailty debuff uptime. (by Mamtooth)
 19-08-2017 - Vengeance Demon Hunter: Added more useful and specific statistics tooltips.
