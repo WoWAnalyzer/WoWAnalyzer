cb52862a524b4131e920db025612c805ae97b4be
JLassie82 <jlassie82@gmail.com>
2017-09-05 19:59:38 -0700

    fixed unused variable error

--- src/Parser/MarksmanshipHunter/CombatLogParser.js
+++ src/Parser/MarksmanshipHunter/CombatLogParser.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import ITEMS from 'common/ITEMS';
-
 import SuggestionsTab from 'Main/SuggestionsTab';
 import Tab from 'Main/Tab';
 import Talents from 'Main/Talents';
