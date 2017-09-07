e4a858f80cdf1e5f91b3b3a6e386b41faa2c7808
Ronald Pereira <ronald.drp11@gmail.com>
2017-09-07 08:57:58 -0300

    Using on_initiliazed to detect the equipped talents and changed time duration on statistic tooltips

--- src/Parser/HavocDemonHunter/Modules/Statistics/Spells/Nemesis.js
+++ src/Parser/HavocDemonHunter/Modules/Statistics/Spells/Nemesis.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import Module from 'Parser/Core/Module';
+import Combatants from 'Parser/Core/Modules/Combatants';
+import Enemies from 'Parser/Core/Modules/Enemies';
+
 import SPELLS from 'common/SPELLS';
 import Icon from 'common/Icon';
 
@@ -9,23 +12,30 @@ import { formatDuration } from 'common/format';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
 
 class Nemesis extends Module {
+  static dependencies = {
+    combatants: Combatants,
+    enemies: Enemies,
+  };
+
+  on_initialized() {
+    if (!this.owner.error) {
+      this.active = this.combatants.selected.hasTalent(SPELLS.NEMESIS_TALENT.id);
+    }
+  }
 
   statistic() {
 
-    const nemesisUptime = this.owner.modules.enemies.getBuffUptime(SPELLS.NEMESIS_TALENT.id);
+    const nemesisUptime = this.enemies.getBuffUptime(SPELLS.NEMESIS_TALENT.id);
     const nemesisUptimePercentage = nemesisUptime / this.owner.fightDuration;
 
-    if(nemesisUptime > 0)
-    {
     return (
       <StatisticBox
         icon={<Icon icon="ability_warlock_improveddemonictactics" alt="Nemesis" />}
         value={`${formatPercentage(nemesisUptimePercentage)}%`}
         label='Nemesis Uptime'
-        tooltip={`The Nemesis buff total uptime was ${formatDuration(nemesisUptime / 1000)} seconds.`}
+        tooltip={`The Nemesis buff total uptime was ${formatDuration(nemesisUptime / 1000)}.`}
       />
     );
-    }
   }
   statisticOrder = STATISTIC_ORDER.CORE(3);
 }
