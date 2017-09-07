32c9eaf147ba58db9646d9277e0de7235914a124
Lukáš Stuchlík <sMteX@users.noreply.github.com>
2017-09-07 02:50:46 +0200

    Update changelog

--- src/Parser/DestructionWarlock/CHANGELOG.js
+++ src/Parser/DestructionWarlock/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+07-09-2017 - Added Immolate uptime tracker. (by Chizu)
 04-09-2017 - Fixed the issues with Grimoire of Service and Summon Infernal/Doomguard. 
 02-09-2017 - Added <span class="Warlock">Destruction Warlock</span> support by @Chizu.
 `;
