07a07eef20fe632a1d964f122bc5382673aeb62d
Collin Cromwell <collcrom@gmail.com>
2017-09-08 17:05:07 -0400

    added damage done stat and cast effciencies

---
+++ src/Parser/RetributionPaladin/CONFIG.js
@@ -0,0 +1,11 @@
+import SPECS from 'common/SPECS';
+
+import CombatLogParser from './CombatLogParser';
+import CHANGELOG from './CHANGELOG';
+
+export default {
+  spec: SPECS.RETRIBUTION_PALADIN,
+  maintainer: '@Hewhosmites',
+  changelog: CHANGELOG,
+  parser: CombatLogParser,
+};
