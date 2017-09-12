b3e82a2b303136327980906dafaac7ba7d43cca1
Iskalla <11649446+Iskalla@users.noreply.github.com>
2017-09-12 11:11:23 +0200

    Added Damage Done box

---
+++ src/Parser/BalanceDruid/Modules/Features/DamageDone.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Icon from 'common/Icon';
+import { formatThousands, formatNumber } from 'common/format';
+
+import CoreDamageDone from 'Parser/Core/Modules/DamageDone';
+
+import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+
+class DamageDone extends CoreDamageDone {
+  statistic() {
+    return (
+      <StatisticBox
+        icon={<Icon icon="class_druid" alt="Damage done" />}
+        value={`${formatNumber(this.total.effective / this.owner.fightDuration * 1000)} DPS`}
+        label='Damage done'
+        tooltip={`The total damage done was ${formatThousands(this.total.effective)}.`}
+      />
+    );
+  }
+  statisticOrder = STATISTIC_ORDER.CORE(0);
+}
+
+export default DamageDone;
