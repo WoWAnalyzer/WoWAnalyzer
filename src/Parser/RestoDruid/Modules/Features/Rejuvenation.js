ddf2c184afa6a9784f6ada017cda7629129f15d0
Kelton Finch <keltonfinch@gmail.com>
2017-09-16 09:39:14 -0400

    [resto druid] Now counting Dreamwalker as part of avg healing per rejuv. Added framework for including Mastery, but not yet activated because in its current form will moderately overcount.

--- src/Parser/RestoDruid/Modules/Features/Rejuvenation.js
+++ src/Parser/RestoDruid/Modules/Features/Rejuvenation.js
@@ -1,6 +1,8 @@
 import SPELLS from 'common/SPELLS';
 import Module from 'Parser/Core/Module';
 
+import Mastery from './Mastery';
+
 const BASE_MANA = 220000;
 const REJUV_COST = 0.10; // % of base mana
 
@@ -8,19 +10,18 @@ const REJUV_COST = 0.10; // % of base mana
  * Backend module for calculating things about Rejuvenation, to be used by other modules.
  */
 class Rejuvenation extends Module {
+  static dependencies = {
+    mastery: Mastery,
+  };
 
-  totalRejuvHealing = 0;
   totalRejuvsCast = 0;
-  totalRejuvsApplied = 0;
+
+  dreamwalkerHealing = 0;
 
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
-    const amount = event.amount + (event.absorbed || 0);
-
-    if (spellId === SPELLS.REJUVENATION.id || spellId === SPELLS.REJUVENATION_GERMINATION.id) {
-      this.totalRejuvHealing += amount;
-    } else if (this.hasGermination && SPELLS.REJUVENATION_GERMINATION.id === spellId) {
-      this.totalRejuvHealing += amount;
+    if(spellId === SPELLS.DREAMWALKER.id) {
+      this.dreamwalkerHealing += event.amount + (event.absorbed || 0);
     }
   }
 
@@ -45,8 +46,16 @@ class Rejuvenation extends Module {
    * The average healing caused per cast of Rejuvenation
    */
   get avgRejuvHealing() {
-    // TODO take into account dreamwalker etc.
-    return this.totalRejuvsCast === 0 ? 0 : this.totalRejuvHealing / this.totalRejuvsCast;
+    const rejuvDirect = this.mastery.getDirectHealing(SPELLS.REJUVENATION.id);
+    //const rejuvMastery = this.mastery.getMasteryHealing(SPELLS.REJUVENATION.id);
+    const germDirect = this.mastery.getDirectHealing(SPELLS.REJUVENATION_GERMINATION.id);
+    //const germMastery = this.mastery.getMasteryHealing(SPELLS.REJUVENATION_GERMINATION.id);
+    const total = rejuvDirect + germDirect + this.dreamwalkerHealing;
+    // FIXME adding the direct + mastery of both moderately overcounts, because when both rejuv and germ are on same target,
+    //       there is a portion of double counting in rejuvDirect/germMastery and germDirect/rejuvMastery.
+    //       Enhancements to the Mastery module may be required.
+
+    return this.totalRejuvsCast === 0 ? 0 : total / this.totalRejuvsCast;
   }
 
   /*
