daf0d2bdafb2322927d031fdecc9e29b7a1c7ccb
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-09-16 17:27:41 +0200

    Fix CooldownTracker to track pet damage during cooldowns

--- src/Parser/DemonologyWarlock/Modules/Features/CooldownTracker.js
+++ src/Parser/DemonologyWarlock/Modules/Features/CooldownTracker.js
@@ -108,6 +108,10 @@ class CooldownTracker extends CoreCooldownTracker {
     });
     super.trackEvent(event);
   }
+
+  on_byPlayerPet_damage(event) {
+    this.trackEvent(event);
+  }
 }
 
 export default CooldownTracker;
