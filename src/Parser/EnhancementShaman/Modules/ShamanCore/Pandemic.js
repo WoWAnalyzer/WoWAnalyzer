420c7c938603ed640e766a05c28f2e1a7760f9b0
Nighteyez07 <Nighteyez07@gmail.com>
2017-09-17 19:48:33 -0700

    Adjust Pandemic based on comments
    Camel Casing, default values, specified spells

--- src/Parser/EnhancementShaman/Modules/ShamanCore/Pandemic.js
+++ src/Parser/EnhancementShaman/Modules/ShamanCore/Pandemic.js
@@ -1,52 +1,49 @@
 import SPELLS from 'common/SPELLS';
-import Combatants from 'Parser/Core/Modules/Combatants';
 
 import Module from 'Parser/Core/Module';
 
-const SPELLID = SPELLS.FLAMETONGUE_BUFF.id;
 const PANDEMIC_THRESHOLD = 11500;
-const debug = false;
+const debug = true;
 
 class Pandemic extends Module {
-  ft_timestamp = [];
-  early_refresh = [];
-  refresh_avearge;
-  static dependencies = {
-    combatants: Combatants,
-  }
+  flametongueTimestamp = 0;
+  earlyRefresh = 0;
+  refreshTimestamps = [];
 
   on_byPlayer_applybuff(event) {
-    if(event.ability.guid === SPELLID) {
-      this.ft_timestamp.push(event.timestamp);
-
+    if(event.ability.guid === SPELLS.FLAMETONGUE_BUFF.id) {
+      if(this.flametongueTimestamp === 0) {
+        this.flametongueTimestamp = event.timestamp;
+      } else {
+        if(event.timestamp - this.flametongueTimestamp < PANDEMIC_THRESHOLD) {
+          this.earlyRefresh += 1;
+          this.refreshTimestamps.push(event.timestamp);
+        }
+      }
     }
   }
 
   on_byPlayer_refreshbuff(event) {
-    if(event.ability.guid === SPELLID) {
-      this.ft_timestamp.push(event.timestamp);
+    if(event.ability.guid === SPELLS.FLAMETONGUE_BUFF.id) {
+      if(this.flametongueTimestamp === 0) {
+        this.flametongueTimestamp = event.timestamp;
+      } else {
+        if(event.timestamp - this.flametongueTimestamp < PANDEMIC_THRESHOLD) {
+          this.earlyRefresh += 1;
+          this.refreshTimestamps.push(event.timestamp);
+        }
+      }
     }
   }
 
   on_finished() {
-    let diff;
-
-    for (let i = 1; i < this.ft_timestamp.length; i++) {
-      diff = this.ft_timestamp[i] - this.ft_timestamp[i - 1];
-      if(diff < PANDEMIC_THRESHOLD) {
-        this.early_refresh.push(diff);
-      }
-    }
-    debug && console.log(this.early_refresh);
-    if(this.early_refresh.length > 0) {
-      this.refresh_avearge = this.early_refresh.reduce((a, b) => a + b) / this.early_refresh.length;
-    }
+    debug && console.log(this.refreshTimestamps);
   }
 
   suggestions(when) {
-    when(this.early_refresh.length).isGreaterThan(0)
+    when(this.earlyRefresh).isGreaterThan(0)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(`Avoid refreshing Flametonue with more then 4.5 sec left on the buff. Some early refreshes are unavoidable.`)
+        return suggest(`Avoid refreshing Flametongue with more then 4.5 sec left on the buff. Some early refreshes are unavoidable.`)
           .icon(SPELLS.FLAMETONGUE_BUFF.icon)
           .actual(`${actual} early refreshes`)
           .recommended(`${(recommended)} recommended`)
