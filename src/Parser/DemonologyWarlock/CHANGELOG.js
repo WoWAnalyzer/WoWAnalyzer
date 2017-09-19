785387365869cc58e4404951f168494fdc0e2b07
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-09-19 14:17:39 +0200

    Fixed soul shard tracker for Demo

--- src/Parser/DemonologyWarlock/CHANGELOG.js
+++ src/Parser/DemonologyWarlock/CHANGELOG.js
@@ -1,3 +1,4 @@
 export default `
-15-09-2017 - Added <span class="Warlock">Demonology Warlock</span> support by @Chizu and @WOPR.
+19-09-2017 - Added the Soul Shard tracker. (by Chizu)
+15-09-2017 - Added <span class="Warlock">Demonology Warlock</span> support by @Chizu.
 `;
