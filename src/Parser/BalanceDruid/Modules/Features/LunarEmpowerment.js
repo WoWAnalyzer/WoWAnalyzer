433ac5ce213d5fad50915110640b8863cf127a27
Iskalla <11649446+Iskalla@users.noreply.github.com>
2017-09-21 11:07:38 +0200

    Added CD LS AsP cost

--- src/Parser/BalanceDruid/Modules/Features/LunarEmpowerment.js
+++ src/Parser/BalanceDruid/Modules/Features/LunarEmpowerment.js
@@ -4,11 +4,9 @@ import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
 import { formatNumber } from 'common/format';
 import Module from 'Parser/Core/Module';
 import SPELLS from 'common/SPELLS';
+import ResourceTypes from 'common/RESOURCE_TYPES';
 
 class LEmpowerment extends Module {
-  lsEmpWasted = 0;
-  totalLSEmps = 0;
-
   isLunarStrike(event) {
     const spellId = event.ability.guid;
     return spellId === SPELLS.LUNAR_STRIKE.id;
@@ -16,12 +14,22 @@ class LEmpowerment extends Module {
   isStarsurge(event) {
     const spellId = event.ability.guid;
     return spellId === SPELLS.STARSURGE_MOONKIN.id;
+  }  
+  isCoolDown(event) {
+    const spellId = event.ability.guid;
+    return spellId === SPELLS.CELESTIAL_ALIGNMENT.id || spellId === SPELLS.INCARNATION_CHOSEN_OF_ELUNE_TALENT.id;
   }
 
   LunarEmpsActive = 0;
   LunarEmpsOver = 0;
 
+  CurrentAsP = 0;
+  MaxAsP = 0;
+
+  LSAsPGen = 150;
+
   on_byPlayer_cast(event) {
+
     if (!this.isLunarStrike(event) && !this.isStarsurge(event)) return;
 
     if (this.isLunarStrike(event) && this.LunarEmpsActive > 0){
@@ -30,19 +38,37 @@ class LEmpowerment extends Module {
     else if (this.isStarsurge(event)){
       if (this.LunarEmpsActive < 3)
         this.LunarEmpsActive++;
-      else
+      else if (this.MaxAsP - this.CurrentAsP >= this.LSAsPGen)
         this.LunarEmpsOver++;
     }
   }
 
+  on_toPlayer_energize(event) {
+    if (!event.classResources) return;
+    
+    for (let i = 0; i < event.classResources.length; i += 1) {
+      if (event.classResources[i].type === ResourceTypes.ASTRAL_POWER) {
+        this.MaxAsP = event.classResources[i].max;
+        this.CurrentAsP = event.classResources[i].amount;
+      }
+    }
+  }
+
+  on_toPlayer_applybuff(event) {
+    if (this.isCoolDown) this.LSAsPGen = 180;
+  }
+  on_toPlayer_removebuff(event) {
+    if (this.isCoolDown) this.LSAsPGen = 150;
+  }
+  
   suggestions(when) {
-    const wastedPerMin = ((this.lsEmpWasted) / (this.owner.fightDuration / 100)) * 60;
+    const wastedPerMin = ((this.LunarEmpsOver) / (this.owner.fightDuration / 100)) * 60;
     when(wastedPerMin).isGreaterThan(0)
         .addSuggestion((suggest, actual, recommended) => {
-          return suggest(<span>You overcapped {this.lsEmpWasted / 10} LS Empowerments.</span>)
+          return suggest(<span>You overcapped {this.LunarEmpsOver} Lunar Empowerments when you could have avoided it without overcapping Astral Power.</span>)
             .icon('spell_arcane_starfire')
-            .actual(`${formatNumber(actual)} overcapped LS Empowerments per minute`)
-            .recommended('0 overcapped LS Empowerments is recommended.')
+            .actual(`${formatNumber(actual)} avoidable overcapped Lunar Empowerments per minute`)
+            .recommended('0 avoidable overcapped Lunar Empowerments is recommended.')
             .regular(recommended + 4).major(recommended + 8);
         });
   }
@@ -51,9 +77,9 @@ class LEmpowerment extends Module {
     return (
       <StatisticBox
         icon={<Icon icon="spell_arcane_starfire" />}
-        value={`${this.lsEmpWasted / 10}`}
-        label="Overcapped LSEmp"
-        tooltip={'LS Empowerments overcapping is often due to mismanagement of resources, but can also be due to an overwhelming amount of OI procs.'}
+        value={`${this.LunarEmpsOver}`}
+        label="Overcapped Lunar Empowerment"
+        tooltip={'Lunar Empowerment overcapping should never occur when it\'s possible to cast a Lunar Strike without overcapping Astral Power.'}
       />
     );
   }
