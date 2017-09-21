43e478de496118e129a1bc48f5c3f5b01653826f
Collin Cromwell <collcrom@gmail.com>
2017-09-20 21:50:49 -0500

    Made some math calcuation fixes on my damage increases. Also made getting bonus damage is out method to get rid of some copy pasting

--- src/Parser/RetributionPaladin/Modules/Items/Tier20_2set.js
+++ src/Parser/RetributionPaladin/Modules/Items/Tier20_2set.js
@@ -8,6 +8,8 @@ import { formatNumber, formatPercentage } from 'common/format';
 import Module from 'Parser/Core/Module';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
+import GetDamageBonus from '../PaladinCore/GetDamageBonus';
+
 const RET_PALADIN_T20_2SET_MODIFIER = 0.2;
 
 class Tier20_2set extends Module {
@@ -31,7 +33,7 @@ class Tier20_2set extends Module {
 
   on_byPlayer_damage(event) {
     if (this.combatants.selected.hasBuff(SPELLS.RET_PALADIN_T20_2SET_BONUS_BUFF.id) && (event.ability.guid === SPELLS.BLADE_OF_JUSTICE.id || event.ability.guid === SPELLS.DIVINE_HAMMER_HIT.id)) {
-      this.damageDone += ((event.amount || 0) + (event.absorbed || 0)) * RET_PALADIN_T20_2SET_MODIFIER / (1 + RET_PALADIN_T20_2SET_MODIFIER);
+      this.damageDone += GetDamageBonus(event, RET_PALADIN_T20_2SET_MODIFIER);
     }
   }
 
