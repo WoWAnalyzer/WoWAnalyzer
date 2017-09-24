d6569081cf0d2d4da3a4aceff46ba4e268dd5d05
Martijn Hols <martijnhols@hotmail.com>
2017-09-24 11:23:01 +0200

    Holy Paladin: Change styling of cast behavior

--- src/Main/StatisticsListBox.js
+++ src/Main/StatisticsListBox.js
@@ -5,8 +5,8 @@ import './StatisticBox.css';
 
 export { default as STATISTIC_ORDER } from './STATISTIC_ORDER';
 
-const StatisticsListBox = ({ title, tooltip, children, bodyStyle, ...others }) => (
-  <div className="col-lg-4 col-sm-6 col-xs-12">
+const StatisticsListBox = ({ title, tooltip, children, bodyStyle, containerProps, ...others }) => (
+  <div className="col-lg-4 col-sm-6 col-xs-12" {...containerProps}>
     <div className="panel statistic-box statistic-list" {...others}>
       {title && (
         <div className="panel-heading">
@@ -24,6 +24,7 @@ StatisticsListBox.propTypes = {
   children: PropTypes.node.isRequired,
   tooltip: PropTypes.string,
   bodyStyle: PropTypes.object,
+  containerProps: PropTypes.object,
 };
 
 export default StatisticsListBox;
