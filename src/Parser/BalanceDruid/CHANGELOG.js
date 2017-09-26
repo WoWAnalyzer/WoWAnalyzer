50cdac245e407e24267a91a22e287ec95943dc9d
Iskalla <11649446+Iskalla@users.noreply.github.com>
2017-09-26 19:30:31 +0200

    I wasnt doing this in 2011

--- src/Parser/BalanceDruid/CHANGELOG.js
+++ src/Parser/BalanceDruid/CHANGELOG.js
@@ -5,8 +5,8 @@ export default `
 12-09-2017 - Added the Damage module and Reorder of Stat boxes
 12-09-2017 - Minor text fixes
 07-09-2017 - Fixed stackable buffs - Now the ABC module should be more reliable
-05-09-2016 - Added Moons casted module
+05-09-2017 - Added Moons casted module
 04-09-2017 - Added Wasted Astral Power module
-02-09-2011 - Added Moonfire and Sunfire uptime modules
+02-09-2017 - Added Moonfire and Sunfire uptime modules
 30-08-2017 - Added Balance Druid support by <b>@Iskalla</b>
 `;
