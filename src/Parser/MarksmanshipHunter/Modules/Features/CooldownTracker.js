c29a3d73f187fe73264c74842a032663b9272ffa
Putro <lafa16ac@student.cbs.dk>
2017-09-26 20:45:14 +0200

    Updated cast efficiency approximation for Windburst to more correctly reflect actual usage, removed spam of Windburst buff in the cooldown section, as it is completely irrelevant and only cluttered the view

--- src/Parser/MarksmanshipHunter/Modules/Features/CooldownTracker.js
+++ src/Parser/MarksmanshipHunter/Modules/Features/CooldownTracker.js
@@ -2,6 +2,8 @@ import SPELLS from 'common/SPELLS';
 
 import CoreCooldownTracker, { BUILT_IN_SUMMARY_TYPES } from 'Parser/Core/Modules/CooldownTracker';
 
+const debug = false;
+
 class CooldownTracker extends CoreCooldownTracker {
   static cooldownSpells = [
     ...CooldownTracker.cooldownSpells,
@@ -12,6 +14,19 @@ class CooldownTracker extends CoreCooldownTracker {
       ],
     },
   ];
+
+  castEventSpells = [
+    SPELLS.WINDBURST_MOVEMENT_SPEED.id,
+  ];
+
+  on_byPlayer_cast(event) {
+    const spellID = event.ability.guid;
+    if (this.castEventSpells.indexOf(spellID) !== -1)  {
+      debug && console.log('Exiting');
+      return;
+    }
+    super.on_byPlayer_cast(event);
+  }
 }
 
 export default CooldownTracker;
