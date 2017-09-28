59faceb50d40095ecf43ab12ec494161acc2a1bc
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-09-29 00:14:00 +0200

    Changelog

--- src/Parser/DemonologyWarlock/CHANGELOG.js
+++ src/Parser/DemonologyWarlock/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+29-09-2017 - Added T20 set bonuses. (by Chizu)
 25-09-2017 - Added rest of the talent modules - Hand of Doom, GoSac, GoSyn, Darkglare and Demonbolt. (by Chizu)
 23-09-2017 - Added second talent row modules - Impending Doom, Improved Dreadstalkers and Implosion. (by Chizu)
 19-09-2017 - Added first talent row modules - Shadowy Inspiration, Shadowflame and Demonic Calling. (by Chizu)
