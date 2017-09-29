5977e79cf9aaabf7b2706ded6b8a76f07a71c418
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-09-30 00:46:43 +0200

    Add base for the spec information

--- src/Parser/DemonologyWarlock/CONFIG.js
+++ src/Parser/DemonologyWarlock/CONFIG.js
@@ -4,11 +4,15 @@ import SPEC_ANALYSIS_COMPLETENESS from 'common/SPEC_ANALYSIS_COMPLETENESS';
 import CombatLogParser from './CombatLogParser';
 import CHANGELOG from './CHANGELOG';
 
+import ChizuAvatar from './Images/Chizu_avatar.jpg';
+
 export default {
   spec: SPECS.DEMONOLOGY_WARLOCK,
   maintainer: '@Chizu',
+  maintainerAvatar: ChizuAvatar,
   completeness: SPEC_ANALYSIS_COMPLETENESS.NEEDS_MORE_WORK, // When changing this please make a PR with ONLY this value changed, we will do a review of your analysis to find out of it is complete enough.
   changelog: CHANGELOG,
+  specDiscussionUrl: 'https://github.com/WoWAnalyzer/WoWAnalyzer/issues/386',
   parser: CombatLogParser,
   path: __dirname, // used for generating a GitHub link directly to your spec
 };
