c310f3b9d7f0df54f10566efb05ec0b67b0c8790
Laust <lafa16ac@student.cbs.dk>
2017-10-02 17:21:18 +0200

    added spec info to config and updated changelog

--- src/Parser/MarksmanshipHunter/CHANGELOG.js
+++ src/Parser/MarksmanshipHunter/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+02-10-2017 - Added spec info aswell as spec discussion URL + some basic rotational information while the modules aren't completed. 
 26-09-2017 - Fixed Cooldown to not include windburst movement speed buff
 05-09-2017 - added base files for new MM Spec
 `;
--- src/Parser/MarksmanshipHunter/CONFIG.js
+++ src/Parser/MarksmanshipHunter/CONFIG.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SPECS from 'common/SPECS';
+import SPELLS from 'common/spells';
+import SpellLink from 'common/SpellLink';
 import SPEC_ANALYSIS_COMPLETENESS from 'common/SPEC_ANALYSIS_COMPLETENESS';
 
 import CombatLogParser from './CombatLogParser';
@@ -8,15 +10,28 @@ import CHANGELOG from './CHANGELOG';
 export default {
   spec: SPECS.MARKSMANSHIP_HUNTER,
   maintainer: '@JLassie82',
-  completeness: SPEC_ANALYSIS_COMPLETENESS.NEEDS_MORE_WORK, // When changing this please make a PR with ONLY this value changed, we will do a review of your analysis to find out of it is complete enough.
-  changelog: CHANGELOG,
-  parser: CombatLogParser,
-  path: __dirname, // used for generating a GitHub link directly to your spec
-  footer: (
-    <div className="panel fade-in" style={{ margin: '15px auto 30px', maxWidth: 400, textAlign: 'center' }}>
-      <div className="panel-body text-muted">
-        Questions about Marksmanship? Visit <a href="https://discord.gg/yqer4BX ">Trueshot Lodge</a> Discord.<br />
-      </div>
+
+  description: (
+    <div>
+      Hey, we're working on making this analyzer as good as we can. We hope that the suggestions that will be implemented in the future, will be helpful in improving your overall performance.
+
+      You might notice that currently there aren't a lot of suggestions, or a lot of stuff generally implemented. This is the reason the spec is still listed as "Needs more work", but we hope to be able to remedy this in the nearest future.
+
+      The modules we plan on implementing will include a focus tracker, a patient sniper tracker, a t202p tracker and more. There is a link below to the issue on GitHub for more information.
+
+      As a general rule of thumb, you want to never be capping on focus, you always want to be casting something, and you want to utilize t202p by casting aimed shots in pairs. This results in a rotation where you cast <SpellLink id={SPELLS.MARKED_SHOT}/> followed by <SpellLink id={SPELLS.ARCANE_SHOT}/> into 2x <SpellLink id={SPELLS.AIMED_SHOT}/> - these windows can be started at around 65 focus. You want to use <SpellLink id={SPELLS.WINDBURST}/> as often as possible, without interrupting your current <SpellLink id={SPELLS.VULNERABLE}/> window, and these can be started at almost any focus level. You want to be casting <SpellLink id={SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED}/> as often as possible, however generally never during <SpellLink id={SPELLS.TRUESHOT}/>. You should always be starting a <SpellLink id={SPELLS.TRUESHOT}/> window with high focus, and after a <SpellLink id={SPELLS.MARKED_SHOT}/> or <SpellLink id={SPELLS.WINDBURST}/> to gain the most out of the only cooldown we have. For more in-depth rotational stuff, please check the guide linked beneath.
+
+      If you want to learn more about Marksmanship Hunters, join the Hunter community on the Trueshot Lodge discord: <a href="https://discordapp.com/invite/trueshot" target="_blank" rel="noopener noreferrer">https://discordapp.com/invite/trueshot</a>. The <kbd>#Marksmanship</kbd> channel has a lot of helpful people, and if you post your logs in <kbd>#log-reviews</kbd>, you can expect to get some good pointers for improvement from the community. The best guide available currently is the guide on <a href="https://www.icy-veins.com/wow/marksmanship-hunter-pve-dps-guide">icy-veins</a>. It is maintained by Azortharion, one of the best hunters in the world, and it is constantly fact-checked by community-members, and improved upon on an almost weekly basis.
     </div>
   ),
+  // When changing this please make a PR with ONLY this value changed, we will do a review of your analysis to find out of it is complete enough.
+  completeness: SPEC_ANALYSIS_COMPLETENESS.NEEDS_MORE_WORK,
+  //Link to GitHub issue with
+  specDiscussionUrl: 'https://github.com/WoWAnalyzer/WoWAnalyzer/issues/441',
+
+  // Don't change these:
+  changelog: CHANGELOG,
+  parser: CombatLogParser,
+  // used for generating a GitHub link directly to your spec:
+  path: __dirname,
 };
