8f267404b178cd1312a16f219fbda3d6735de09c
Martijn Hols <martijnhols@hotmail.com>
2017-10-03 18:39:42 +0200

    Organize AVAILABLE_CONFIGS by spec order used on frontpage so it looks less like a crazy person's

--- src/Parser/AVAILABLE_CONFIGS.js
+++ src/Parser/AVAILABLE_CONFIGS.js
@@ -1,79 +1,75 @@
-import HolyPaladin from './HolyPaladin/CONFIG';
-import RetributionPaladin from './RetributionPaladin/CONFIG';
+import BloodDeathKnight from './BloodDeathKnight/CONFIG';
 
-import RestoDruid from './RestoDruid/CONFIG';
-import GuardianDruid from './GuardianDruid/CONFIG';
+import HavocDemonHunter from './HavocDemonHunter/CONFIG';
+import VengeanceDemonHunter from './VengeanceDemonHunter/CONFIG';
+
 import BalanceDruid from './BalanceDruid/CONFIG';
 import FeralDruid from './FeralDruid/CONFIG';
+import GuardianDruid from './GuardianDruid/CONFIG';
+import RestoDruid from './RestoDruid/CONFIG';
+
+import MarksmanshipHunter from './MarksmanshipHunter/CONFIG';
+
+// Mage
+
+import BrewmasterMonk from './BrewmasterMonk/CONFIG';
+import WindwalkerMonk from './WindwalkerMonk/CONFIG';
+import MistweaverMonk from './MistweaverMonk/CONFIG';
+
+import HolyPaladin from './HolyPaladin/CONFIG';
+import RetributionPaladin from './RetributionPaladin/CONFIG';
 
 import DisciplinePriest from './DisciplinePriest/CONFIG';
 import HolyPriest from './HolyPriest/CONFIG';
 import ShadowPriest from './ShadowPriest/CONFIG';
 
+import SubtletyRogue from './SubtletyRogue/CONFIG';
+
 import ElementalShaman from './ElementalShaman/CONFIG';
 import EnhancementShaman from './EnhancementShaman/CONFIG';
 import RestorationShaman from './RestorationShaman/CONFIG';
 
-import MistweaverMonk from './MistweaverMonk/CONFIG';
-import WindwalkerMonk from './WindwalkerMonk/CONFIG';
-import BrewmasterMonk from './BrewmasterMonk/CONFIG';
-
-import SubtletyRogue from './SubtletyRogue/CONFIG';
-
-import VengeanceDemonHunter from './VengeanceDemonHunter/CONFIG';
-
 import AfflictionWarlock from './AfflictionWarlock/CONFIG';
-
-import BloodDeathKnight from './BloodDeathKnight/CONFIG';
-
-import DestructionWarlock from './DestructionWarlock/CONFIG';
-
-import MarksmanshipHunter from './MarksmanshipHunter/CONFIG';
-
-import HavocDemonHunter from './HavocDemonHunter/CONFIG';
-
 import DemonologyWarlock from './DemonologyWarlock/CONFIG';
+import DestructionWarlock from './DestructionWarlock/CONFIG';
 
 import ProtectionWarrior from './ProtectionWarrior/CONFIG';
 
-// Order of this should be the order in which specs were added to production
 export default [
-  HolyPaladin,
+  BloodDeathKnight,
 
-  RestoDruid,
-  GuardianDruid,
+  HavocDemonHunter,
+  VengeanceDemonHunter,
+
   BalanceDruid,
   FeralDruid,
+  GuardianDruid,
+  RestoDruid,
+
+  MarksmanshipHunter,
+
+  // Mage
+
+  BrewmasterMonk,
+  WindwalkerMonk,
+  MistweaverMonk,
+
+  HolyPaladin,
+  RetributionPaladin,
 
   DisciplinePriest,
   HolyPriest,
   ShadowPriest,
 
-  MistweaverMonk,
-  WindwalkerMonk,
-  BrewmasterMonk,
+  SubtletyRogue,
 
   ElementalShaman,
   EnhancementShaman,
   RestorationShaman,
 
-  SubtletyRogue,
-
-  VengeanceDemonHunter,
-
   AfflictionWarlock,
-
-  BloodDeathKnight,
-
-  DestructionWarlock,
-
-  MarksmanshipHunter,
-
-  HavocDemonHunter,
-
-  RetributionPaladin,
-
   DemonologyWarlock,
+  DestructionWarlock,
 
   ProtectionWarrior,
 ];
