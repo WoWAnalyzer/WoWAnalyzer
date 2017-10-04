ce291f1d61c47c0a4cca39bf505a8527bea9be8c
Unknown <eric.schneider@rutgers.edu>
2017-10-04 01:00:47 -0400

    Added Focus Chart

--- src/Parser/MarksmanshipHunter/CHANGELOG.js
+++ src/Parser/MarksmanshipHunter/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+04-10-2018 - Added Focus Generation/Waste Tracker (Chart tab)
 26-09-2017 - Fixed Cooldown to not include windburst movement speed buff
 05-09-2017 - added base files for new MM Spec
 `;
