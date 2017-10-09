3a9599d4ac082c26c786e91f0f041605257ab97a
Laust <lafa16ac@student.cbs.dk>
2017-10-09 16:19:35 +0200

    little amount of progress on patientsniper module

--- src/Parser/MarksmanshipHunter/Modules/Talents/PatientSniper.js
+++ src/Parser/MarksmanshipHunter/Modules/Talents/PatientSniper.js
@@ -6,17 +6,14 @@ import Enemies from 'Parser/Core/Modules/Enemies';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import SpellLink from 'common/SpellLink';
+import Combatants from "../../../Core/Modules/Combatants";
 
 class PatientSniper extends Module {
+
   static dependencies = {
     enemies: Enemies,
-  };
-  oneSecondIntoVulnerable = 0;
-  twoSecondsIntoVulnerable = 0;
-  threeSecondsIntoVulnerable = 0;
-  fourSecondsIntoVulnerable = 0;
-  fiveSecondsIntoVulnerable = 0;
-  sixSecondsIntoVulnerable = 0;
+    combatants: Combatants,
+  }
 
   /*
   A module to track the effectiveness of the Patient Sniper talent
@@ -29,18 +26,28 @@ class PatientSniper extends Module {
   > 5-5.99seconds passed = 30%
   > 6-6.99seconds passed = 36%
   > Vulnerable fades from the target
-   */
+  */
+
+  //The various levels of patient sniper
+  zeroSecondsIntoVulnerable = 0;
+  oneSecondIntoVulnerable = 0;
+  twoSecondsIntoVulnerable = 0;
+  threeSecondsIntoVulnerable = 0;
+  fourSecondsIntoVulnerable = 0;
+  fiveSecondsIntoVulnerable = 0;
+  sixSecondsIntoVulnerable = 0;
+
   on_initialized() {
     this.active = this.combatants.selected.hasTalent(SPELLS.PATIENT_SNIPER_TALENT.id);
   }
 
-  on_byPlayer_cast(event) {
+  on_byPlayer_damage(event) {
+    const spellId = event.ability.guid;
+    const eventTimestamp = event.timestamp;
     const enemy = this.enemies.getEntity(event);
-    if(!enemy.hasBuff(SPELLS.VULNERABLE.id)) {
-      return;
-    }
-    if(enemy.refreshBuff(SPELLS.VULNERABLE.id)) {
 
+    if (spellId !== SPELLS.WINDBURST.id || spellId !== SPELLS.MARKED_SHOT.id) {
+      return;
     }
 
   }
