5942f123f4c9cd4ac8c030e753f93b700c5faa72
Martijn Hols <martijnhols@hotmail.com>
2017-10-10 21:47:51 +0200

    Actually fix buffs in combatantinfo for other players

--- src/Parser/Core/Modules/Helpers/ApplyBuffFixer.js
+++ src/Parser/Core/Modules/Helpers/ApplyBuffFixer.js
@@ -16,6 +16,19 @@ class ApplyBuffFixer extends Module {
     throw new Error('Fight doesn\'t have a first event, something must have gone wrong.');
   }
 
+  _initialized = false;
+  on_initialized() {
+    this._initialized = true;
+  }
+  // We need to track `combatantinfo` events this way since they are included in the `events` passed to `reorderEvents` due to technical reasons (it's a different API call). We still need `combatantinfo` for all players, so cache it manually.
+  _combatantInfoEvents = [];
+  on_combatantinfo(event) {
+    if (!this._initialized) {
+      // Stop after initialization to avoid parsing the player twice, and this fixes a crash since the selected player's `combatantinfo` is received after `reorderEvents()`
+      this._combatantInfoEvents.push(event);
+    }
+  }
+
   _buffsAppliedByPlayerId = {};
 
   /**
@@ -36,16 +49,18 @@ class ApplyBuffFixer extends Module {
       this._buffsAppliedByPlayerId[targetId] = this._buffsAppliedByPlayerId[targetId] || [];
 
       if (event.type === 'applybuff') {
-        this._buffsAppliedByPlayerId[targetId].push(event.ability.guid);
+        const spellId = event.ability.guid;
+        this._buffsAppliedByPlayerId[targetId].push(spellId);
       }
       if (['removebuff', 'applybuffstack', 'removebuffstack', 'refreshbuff'].includes(event.type)) {
-        if (this._buffsAppliedByPlayerId[targetId].includes(event.ability.guid)) {
+        const spellId = event.ability.guid;
+        if (this._buffsAppliedByPlayerId[targetId].includes(spellId)) {
           // This buff has an `applybuff` event and so isn't broken :D
           continue;
         }
 
         const fightDuration = formatDuration((event.timestamp - this.owner.fight.start_time) / 1000);
-        debug && console.warn(fightDuration, 'Found a buff on', ((playersById[targetId] && playersById[targetId].name) || '???'), 'that was applied before the pull:', event.ability.name, event.ability.guid, '! Fabricating an `applybuff` event so you don\'t have to do anything special to take this into account.');
+        debug && console.warn(fightDuration, 'Found a buff on', ((playersById[targetId] && playersById[targetId].name) || '???'), 'that was applied before the pull:', event.ability.name, spellId, '! Fabricating an `applybuff` event so you don\'t have to do anything special to take this into account.');
         const applybuff = {
           // These are all the properties a normal `applybuff` event would have.
           timestamp: events[firstEventIndex].timestamp,
@@ -61,49 +76,50 @@ class ApplyBuffFixer extends Module {
         };
         events.splice(firstEventIndex, 0, applybuff);
         // It shouldn't happen twice, but better be safe than sorry.
-        this._buffsAppliedByPlayerId[targetId].push(event.ability.guid);
+        this._buffsAppliedByPlayerId[targetId].push(spellId);
       }
     }
     // endregion
 
     // region `combatantinfo` based detection
     // This catches buffs that never drop, such as Flasks and more importantly.
+    this._combatantInfoEvents.forEach(event => {
+      const targetId = event.sourceID;
+      event.auras.forEach(aura => {
+        const spellId = aura.ability;
 
-    for (let i = 0; i < events.length; i += 1) {
-      const event = events[i];
-      if (event.type === 'combatantinfo') {
-        event.auras.forEach(aura => {
-          const targetId = event.sourceID;
+        this._buffsAppliedByPlayerId[targetId] = this._buffsAppliedByPlayerId[targetId] || [];
 
-          this._buffsAppliedByPlayerId[targetId] = this._buffsAppliedByPlayerId[targetId] || [];
+        if (this._buffsAppliedByPlayerId[targetId].includes(spellId)) {
+          // This buff has an `applybuff` event and so isn't broken :D
+          return;
+        }
 
-          if (this._buffsAppliedByPlayerId[targetId].includes(aura.ability)) {
-            // This buff has an `applybuff` event and so isn't broken :D
-            return;
-          }
-
-          debug && console.warn('Found a buff on', ((playersById[targetId] && playersById[targetId].name) || '???'), 'in the combatantinfo that was applied before the pull and never dropped:', (SPELLS[aura.ability] && SPELLS[aura.ability].name) || '???', aura.ability, '! Fabricating an `applybuff` event so you don\'t have to do anything special to take this into account.');
-          const applybuff = {
-            // These are all the properties a normal `applybuff` event would have.
-            timestamp: events[firstEventIndex].timestamp,
-            type: 'applybuff',
-            ability: {
-              guid: aura.ability,
-              name: 'Unknown',
-              abilityIcon: aura.icon,
-            },
-            sourceID: aura.source,
-            sourceIsFriendly: true,
-            targetID: targetId,
-            targetIsFriendly: true,
-            // Custom properties:
-            prepull: true,
-            __fabricated: true,
-          };
-          events.splice(firstEventIndex, 0, applybuff);
-        });
-      }
-    }
+        debug && console.warn('Found a buff on', ((playersById[targetId] && playersById[targetId].name) || '???'), 'in the combatantinfo that was applied before the pull and never dropped:', (SPELLS[spellId] && SPELLS[spellId].name) || '???', spellId, '! Fabricating an `applybuff` event so you don\'t have to do anything special to take this into account.');
+        const applybuff = {
+          // These are all the properties a normal `applybuff` event would have.
+          timestamp: events[firstEventIndex].timestamp,
+          type: 'applybuff',
+          ability: {
+            guid: spellId,
+            name: 'Unknown',
+            abilityIcon: aura.icon,
+          },
+          sourceID: aura.source,
+          sourceIsFriendly: true,
+          targetID: targetId,
+          targetIsFriendly: true,
+          // Custom properties:
+          prepull: true,
+          __fabricated: true,
+        };
+        events.splice(firstEventIndex, 0, applybuff);
+        // It shouldn't happen twice, but better be safe than sorry.
+        this._buffsAppliedByPlayerId[targetId].push(spellId);
+      });
+    });
+    // We don't need it anymore, this might introduce a crash if this method is ever called twice, but we're under the presumption that never happens so if it does crash we might have to verify if everything works properly with this behavior.
+    this._combatantInfoEvents = undefined;
     // endregion
 
     return events;
