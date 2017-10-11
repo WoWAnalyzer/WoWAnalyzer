9a5f59b7df876480b0ae2cf7ffeae27720530b06
Martijn Hols <martijnhols@hotmail.com>
2017-10-11 23:03:33 +0200

    Added a suggestion for casting Light of the Martyr while Holy Shock is available

--- src/Parser/HolyPaladin/Modules/Items/MaraadsDyingBreath.js
+++ src/Parser/HolyPaladin/Modules/Items/MaraadsDyingBreath.js
@@ -7,7 +7,6 @@ import Module from 'Parser/Core/Module';
 import calculateEffectiveHealing from 'Parser/Core/calculateEffectiveHealing';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
-const MARAADS_HEALING_BUFF_ID = 234862;
 const MARAADS_HEALING_INCREASE_PER_STACK = 0.1;
 
 class MaraadsDyingBreath extends Module {
@@ -30,7 +29,7 @@ class MaraadsDyingBreath extends Module {
     if (spellId !== SPELLS.LIGHT_OF_THE_MARTYR.id) {
       return;
     }
-    if (!this.combatants.selected.hasBuff(MARAADS_HEALING_BUFF_ID, event.timestamp)) {
+    if (!this.combatants.selected.hasBuff(SPELLS.MARAADS_DYING_BREATH_BUFF.id, event.timestamp)) {
       return;
     }
 
@@ -38,7 +37,7 @@ class MaraadsDyingBreath extends Module {
   }
   on_toPlayer_removebuff(event) {
     const buffId = event.ability.guid;
-    if (buffId !== MARAADS_HEALING_BUFF_ID) {
+    if (buffId !== SPELLS.MARAADS_DYING_BREATH_BUFF.id) {
       return;
     }
     if (!this._lastHeal) {
@@ -47,7 +46,7 @@ class MaraadsDyingBreath extends Module {
 
     // In the case of Maraad's Dying Breath each LotM consumes the stacks of the buff remaining. So this event is only called once per buffed LotM. When the buff is removed it first calls a `removebuffstack` that removes all additional stacks from the buff before it calls a `removebuff`, `removebuffstack` is the only way we can find the amount of stacks it had.
     const heal = this._lastHeal;
-    const buff = this.combatants.selected.getBuff(MARAADS_HEALING_BUFF_ID, heal.timestamp);
+    const buff = this.combatants.selected.getBuff(SPELLS.MARAADS_DYING_BREATH_BUFF.id, heal.timestamp);
     const stacks = buff && buff.stacks ? (buff.stacks + 1) : 1;
 
     const amount = heal.amount;
@@ -80,7 +79,7 @@ class MaraadsDyingBreath extends Module {
     this.totalHealing += healing;
     this.healingGainOverLotm += healing;
 
-    const buff = this.combatants.selected.getBuff(MARAADS_HEALING_BUFF_ID, matchedHeal.timestamp);
+    const buff = this.combatants.selected.getBuff(SPELLS.MARAADS_DYING_BREATH_BUFF.id, matchedHeal.timestamp);
     const stacks = buff && buff.stacks ? (buff.stacks + 1) : 1;
 
     // Since FoL beacon transfers, the only gain from Maraad's over a FoL would be the increase from Maraad's to beacon transfer
@@ -92,7 +91,7 @@ class MaraadsDyingBreath extends Module {
     if (spellId !== SPELLS.LIGHT_OF_THE_MARTYR_DAMAGE_TAKEN.id) {
       return;
     }
-    if (!this.combatants.selected.hasBuff(MARAADS_HEALING_BUFF_ID, event.timestamp)) {
+    if (!this.combatants.selected.hasBuff(SPELLS.MARAADS_DYING_BREATH_BUFF.id, event.timestamp)) {
       return;
     }
 
