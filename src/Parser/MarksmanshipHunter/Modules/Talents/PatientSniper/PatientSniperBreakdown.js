545e87d5d4454a35771f9ddfd6337821569429b7
Laust <lafa16ac@student.cbs.dk>
2017-10-12 13:51:58 +0200

    attempt1 at creating tab for PatientSniper

---
+++ src/Parser/MarksmanshipHunter/Modules/Talents/PatientSniper/PatientSniperBreakdown.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import SpellIcon from 'common/SpellIcon';
+import SpellLink from 'common/SpellLink';
+import { formatPercentage } from 'common/format';
+
+
+import PatientSniperTracker from './PatientSniperTracker';
+
+import SPELLS from 'common/SPELLS';
+
+class PatientSniperBreakdown extends React.Component {
+  static dependencies = {
+    patientSniperTracker: PatientSniperTracker,
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>Time into Vulnerable:</th>
+              <th colSpan="2">Amount</th>
+              <th colSpan="2">% of total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td style={{ width: '30%' }}>
+                <SpellIcon id={SPELLS.AIMED_SHOT.id} />{'  '}
+                <SpellLink id={SPELLS.AIMED_SHOT.id} />s outside of <SpellLink id={SPELLS.VULNERABLE.id} />
+              </td>
+              <td style={{ width: '30%' }}>
+                <dfn>
+                  HEJ
+                </dfn>
+              </td>
+              <td style={{ width: '30%' }}>
+                <dfn>
+                  {formatPercentage(nonVulnerableAimedShots)};
+                </dfn>
+              </td>
+
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default PatientSniperBreakdown;
