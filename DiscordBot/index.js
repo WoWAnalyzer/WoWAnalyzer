ba94fd4b1758969a256af82083c76aaad8c2ade9
Martijn Hols <martijnhols@hotmail.com>
2017-10-12 23:46:59 +0200

    Set bot's game name to WoWAnalyzer.com

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -5,6 +5,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    game: {
+      name: 'WoWAnalyzer.com',
+      type: 0,
+    },
+  });
 });
 
 client.on('message', msg => {
