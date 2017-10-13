330ae02641f7a2c6f1becc546da7185a87c144bc
Collin Cromwell <collcrom@gmail.com>
2017-10-13 12:08:12 -0500

    first pass at BotA uptime

--- src/Parser/RetributionPaladin/Modules/PaladinCore/BlessingOfTheAshbringer.js
+++ src/Parser/RetributionPaladin/Modules/PaladinCore/BlessingOfTheAshbringer.js
@@ -19,18 +19,26 @@ class BlessingOfTheAshbringer extends Module {
 	//Since Blessing of the Ashbringer is not always in the WCL buffs
 	//This should always return the correct uptime
 	get botaUptime() {
+		const blessingOfTheAshbringerUptime = this.combatants.selected.getBuffUptime(SPELLS.BLESSING_OF_THE_ASHBRINGER.id) / this.owner.fightDuration;
 		const blessingOfKingsUptime = this.combatants.selected.getBuffUptime(SPELLS.GREATER_BLESSING_OF_KINGS.id) / this.owner.fightDuration;
 		const blessingOfWisdomUptime = this.combatants.selected.getBuffUptime(SPELLS.GREATER_BLESSING_OF_WISDOM.id) / this.owner.fightDuration;
 		//returns the lower of the two uptimes since thats what your Blessing of the Ashbringer uptime was
-		return blessingOfWisdomUptime > blessingOfKingsUptime ? blessingOfKingsUptime : blessingOfWisdomUptime;
+		if(blessingOfTheAshbringerUptime){
+			return blessingOfWisdomUptime;
+		}
+		return (blessingOfWisdomUptime > blessingOfKingsUptime) ? blessingOfKingsUptime : blessingOfWisdomUptime
 	}
 
 	suggestions(when) {
 		const blessingOfTheAshbringerUptime = this.botaUptime;
-		when(blessingOfWisdomUptime).isLessThan(0.95)
+		when(blessingOfTheAshbringerUptime).isLessThan(0.95)
 			.addSuggestion((suggest, actual, reccomended) => {
-				return suggest('Your ')
-			})
+				return suggest(<span>Your <SpellLink id={SPELLS.BLESSING_OF_THE_ASHBRINGER.id}/> of ${formatPercentage(blessingOfTheAshbringerUptime)} is less than 95%, make sure to apply <SpellLink id={SPELLS.GREATER_BLESSING_OF_WISDOM.id} /> and <SpellLink id={SPELLS.GREATER_BLESSING_OF_KINGS.id} /></span>)
+					.icon(SPELLS.BLESSING_OF_THE_ASHBRINGER.id)
+					.actual(`${formatPercentage(actual)}% uptime`)
+					.reccomended(`${formatPercentage(reccomended)}% is reccomended`)
+					.regular(reccomended).major(reccomended - 0.05);
+			});
 	}
 }
 
