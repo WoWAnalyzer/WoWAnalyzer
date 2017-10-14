840ac431f29f46b7b7a7fc05d794d055a8ac0411
Unknown <eric.schneider@rutgers.edu>
2017-10-14 18:35:38 -0400

    Style fixes

--- src/Parser/MarksmanshipHunter/Modules/Features/TimeFocusCapped.js
+++ src/Parser/MarksmanshipHunter/Modules/Features/TimeFocusCapped.js
@@ -10,43 +10,39 @@ class TimeFocusCapped extends Module {
     focusTracker: FocusTracker,
   };
 
-  getTotalWaste(){
+  get getTotalWaste(){
     let waste = this.focusTracker.secondsCapped * this.focusTracker.focusGen;
     if (this.focusTracker.generatorCasts) {
-      Object.keys(this.focusTracker.activeFocusWasted).forEach((generator) => {
-        waste += this.focusTracker.activeFocusWasted[generator];
-
-      });
+      waste += Object.values(this.focusTracker.activeFocusWasted).reduce((sum, waste) => sum + waste, 0);
     }
     return Math.round(waste);
   }
 
   statistic() {
-    const totalFocusWaste = this.getTotalWaste();
-    const tooltipData = "You wasted <b>" + totalFocusWaste + " </b> focus. <br /> That's <b>" + Math.round(totalFocusWaste/(this.owner.fightDuration/1000 * this.focusTracker.focusGen) * 10000)/100 + "% </b> of your total focus generated. <br /> For more details, see the Focus Chart tab.";
+    const totalFocusWaste = this.getTotalWaste;
     const percentCapped = Math.round(this.focusTracker.secondsCapped/(this.owner.fightDuration/1000) * 10000)/100;
     return (
       <StatisticBox
         icon = {<Icon icon='ability_hunter_focusfire' alt = 'Focus Wasted' />}
-        label = {'Time Focus Capped'}
-        tooltip= {tooltipData}
-        value =  {percentCapped + " %"}
+        label = 'Time Focus Capped'
+        tooltip= {`You wasted <b> ${totalFocusWaste}  </b> focus. <br /> 
+        That's <b>  ${formatPercentage(totalFocusWaste/(this.owner.fightDuration/1000 * this.focusTracker.focusGen) * 10000)/100}% </b> of your total focus generated. 
+        <br /> For more details, see the Focus Chart tab.`}
+        value =  {`${percentCapped} %`}
       //Time not Focus-Capped: {Math.round((this.owner.fightDuration / 1000 - this.focusTracker.secondsCapped) * 100) / 100}s / {Math.floor(this.owner.fightDuration / 1000)}
       footer={(
         <div className="statistic-bar">
         <div
           className="stat-health-bg"
           style={{ width: `${(100-percentCapped)}%` }}
-          data-tip={`You spent <b>${100-percentCapped}%</b> of your time, or <b>${Math.round(Math.floor(this.owner.fightDuration/1000) - this.focusTracker.secondsCapped)}s</b> under the focucs cap.`}
+          data-tip={`You spent <b>${100-percentCapped}%</b> of your time, or <b>${Math.round(Math.floor(this.owner.fightDuration/1000) - this.focusTracker.secondsCapped)}s</b> under the focus cap.`}
         >
-          <img src="/img/sword.png" alt="Time not focus capped" />
         </div>
           <div
             className="stat-overhealing-bg"
             style={{ width: `${percentCapped}%` }}
-            data-tip={`You spent <b>${percentCapped}%</b>, or <b>${Math.round(this.focusTracker.secondsCapped)}s</b> of your time focus Capped.`}
+            data-tip={`You spent <b>${percentCapped}%</b>, or <b>${Math.round(this.focusTracker.secondsCapped)}s</b> of your time focus capped.`}
           >
-            <img src="/img/afk.png" alt="Time focus capped" />
           </div>
         </div>
       )}
