148400aac681c702aed955beff630b9bd138dd45
Aveline <hello@aveline.be>
2017-10-14 12:38:51 +0200

    added and regrouped Rogue legendaries

--- src/common/ITEMS.js
+++ src/common/ITEMS.js
@@ -862,12 +862,17 @@ const ITEMS = {
     icon: 'inv_pants_cloth_02',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
-
-  // Subtlety Rogue Legendaries
-  SHADOW_SATYRS_WALK: {
-    id: 137032,
-    name: 'Shadow Satyr\'s Walk',
-    icon: 'inv_boots_mail_dungeonmail_c_04',
+  THE_DREADLORDS_DECEIT: {
+    id: 137021,
+    name: 'The Dreadlord\'s Deceit',
+    icon: 'inv_cape_pandaria_d_03',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  // Assassination Rogue
+  DUSKWALKERS_FOOTPADS: {
+    id: 137030,
+    name: 'Duskwalker\'s Footpads',
+    icon: 'inv_boots_leather_8',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
   ZOLDYCK_FAMILY_TRAINING_SHACKLES: {
@@ -876,16 +881,54 @@ const ITEMS = {
     icon: 'inv_bracer_leather_raiddruid_i_01',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
+  THE_EMPTY_CROWN: {
+    id: 151815,
+    name: 'The Empty Crown',
+    icon: 'inv_crown_02',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  // Outlaw Rogue Legendaries
+  THRAXIS_TRICKSY_TREADS: {
+    id: 137031,
+    name: 'Thraxi\'s Tricksy Treads',
+    icon: 'inv_boots_leather_03a',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  GREENSKINS_WATERLOGGED_WRISTCUFFS: {
+    id: 137099,
+    name: 'Greenskin\'s Waterlogged Wristcuffs',
+    icon: 'inv_bracer_leather_raidrogue_k_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  SHIVARRAN_SYMMETRY: {
+    id: 141321,
+    name: 'Shivarran Symmetry',
+    icon: 'inv_gauntlets_83',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  THE_CURSE_OF_RESTLESSNESS: {
+    id: 151817,
+    name: 'The Curse of Restlessness',
+    icon: 'inv_qiraj_draperegal',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  // Subtlety Rogue Legendaries
+  SHADOW_SATYRS_WALK: {
+    id: 137032,
+    name: 'Shadow Satyr\'s Walk',
+    icon: 'inv_boots_mail_dungeonmail_c_04',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  DENIAL_OF_THE_HALF_GIANTS: {
+    id: 137100,
+    name: 'Denial of the Half-Giants',
+    icon: 'inv_bracer_leather_panda_b_02_crimson',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
   THE_FIRST_OF_THE_DEAD: {
     id: 151818,
     name: 'The First of the Dead',
     icon: 'inv_glove_cloth_raidwarlockmythic_q_01',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  THE_DREADLORDS_DECEIT: {
-    id: 137021,
-    name: 'The Dreadlord\'s Deceit',
-    icon: 'inv_cape_pandaria_d_03',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
 
