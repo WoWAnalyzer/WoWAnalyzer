a68a5add057b0c6b83814e73041bc70380403e05
Aveline <hello@aveline.be>
2017-10-14 12:51:08 +0200

    added and regrouped Shaman legendaries

--- src/common/ITEMS.js
+++ src/common/ITEMS.js
@@ -188,43 +188,6 @@ const ITEMS = {
     icon: 'inv_shoulder_126',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
-  // Resto Shammy
-  ROOTS_OF_SHALADRASSIL: {
-    id: 132466,
-    name: 'Roots of Shaladrassil',
-    icon: 'inv_robe_pants_pvpwarlock_c_02',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  PRAETORIANS_TIDECALLERS: {
-    id: 137058,
-    name: 'Praetorian\'s Tidecallers',
-    icon: 'inv_gauntlets_plate_raidpaladin_i_01',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  FOCUSER_OF_JONAT: {
-    id: 137051,
-    name: 'Focuser of Jonat, the Elder',
-    icon: 'inv_jewelry_ring_96',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  INTACT_NAZJATAR_MOLTING: {
-    id: 137085,
-    name: 'Intact Nazjatar Molting',
-    icon: 'inv_leather_raiddruid_m_01belt',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  NOBUNDOS_REDEMPTION: {
-    id: 137104,
-    name: 'Nobundo\'s Redemption',
-    icon: 'inv_bracer_leather_cataclysm_b_01',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  UNCERTAIN_REMINDER: {
-    id: 143732,
-    name: 'Uncertain Reminder',
-    icon: 'inv_helm_mail_korkronshaman_d_01',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
 
   // Shared legenadries
   KILJAEDENS_BURNING_WISH: {
@@ -233,12 +196,25 @@ const ITEMS = {
     icon: 'sha_spell_fire_bluepyroblast_nightmare',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
-  // ENHANCEMENT SHAMAN LEGENDARIES
-  AKAINUS_ABSOLUTE_JUSTICE: {
-	  id: 137084,
-	  name: 'Akainu\'s Absolute Justice',
-	  icon: 'inv_bracer_mail_pvphunter_c_02',
-	  quality: ITEM_QUALITIES.LEGENDARY,
+  ROOTS_OF_SHALADRASSIL: {
+    id: 132466,
+    name: 'Roots of Shaladrassil',
+    icon: 'inv_robe_pants_pvpwarlock_c_02',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
+  // Shared Shaman
+  UNCERTAIN_REMINDER: {
+    id: 143732,
+    name: 'Uncertain Reminder',
+    icon: 'inv_helm_mail_korkronshaman_d_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  SOUL_OF_THE_FARSEER: {
+    id: 151647,
+    name: 'Soul of the Farseer',
+    icon: 'inv_70_quest_ring2b',
+    quality: ITEM_QUALITIES.LEGENDARY,
   },
   EYE_OF_THE_TWISTING_NETHER: {
     id: 137050,
@@ -252,16 +228,42 @@ const ITEMS = {
     icon: 'inv_gauntlets_85',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
+  // Elemental Shaman
+  THE_DECEIVERS_BLOOD_PACT: {
+    id: 137035,
+    name: 'The Deceiver\'s Blood Pact',
+    icon: 'inv_boots_chain_07',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ECHOES_OF_THE_GREAT_SUNDERING: {
+    id: 137074,
+    name: 'Echoes of the Great Sundering',
+    icon: 'inv_shoulder_plate_raidwarrior_j_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  PRISTINE_PROTO_SCALE_GIRDLE: {
+    id: 137083,
+    name: 'Pristine Proto-Scale Girdle',
+    icon: 'inv_belt_mail_panda_b_01white',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ALAKIRS_ACRIMONY: {
+    id: 137102,
+    name: 'Al\'Akir\'s Acrimony',
+    icon: 'inv_bracer_plate_pvpdeathknight_e_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  // ENHANCEMENT SHAMAN LEGENDARIES
+  AKAINUS_ABSOLUTE_JUSTICE: {
+	  id: 137084,
+	  name: 'Akainu\'s Absolute Justice',
+	  icon: 'inv_bracer_mail_pvphunter_c_02',
+	  quality: ITEM_QUALITIES.LEGENDARY,
+  },
   EMALONS_CHARGED_CORE: {
     id: 137616,
     name: 'Emalon\'s Charged Core',
     icon: 'inv_chest_mail_dungeonmail_c_04',
-    quality: ITEM_QUALITIES.LEGENDARY,
-  },
-  SOUL_OF_THE_FARSEER: {
-    id: 151647,
-    name: 'Soul of the Farseer',
-    icon: 'inv_70_quest_ring2b',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
   STORM_TEMPESTS: {
@@ -274,6 +276,43 @@ const ITEMS = {
     id: 138117,
     name: 'Spiritual Journey',
     icon: 'inv_boot_mail_raidhunter_m_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  // Resto Shaman
+  FOCUSER_OF_JONAT: {
+    id: 137051,
+    name: 'Focuser of Jonat, the Elder',
+    icon: 'inv_jewelry_ring_96',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  INTACT_NAZJATAR_MOLTING: {
+    id: 137085,
+    name: 'Intact Nazjatar Molting',
+    icon: 'inv_leather_raiddruid_m_01belt',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ELEMENTAL_REBALANCERS: {
+    id: 137036,
+    name: 'Elemental Rebalancers',
+    icon: 'inv_boots_mail_raidshaman_j_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  PRAETORIANS_TIDECALLERS: {
+    id: 137058,
+    name: 'Praetorian\'s Tidecallers',
+    icon: 'inv_gauntlets_plate_raidpaladin_i_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },  
+  NOBUNDOS_REDEMPTION: {
+    id: 137104,
+    name: 'Nobundo\'s Redemption',
+    icon: 'inv_bracer_leather_cataclysm_b_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  FIRE_IN_THE_DEEP: {
+    id: 151785,
+    name: 'Fire in the Deep',
+    icon: 'inv_chest_mail_raidshaman_m_01',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
 
