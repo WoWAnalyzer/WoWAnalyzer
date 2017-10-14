e4126508ddf1dfee70c6e22624b6965bcab6b192
Collin Cromwell <collcrom@gmail.com>
2017-10-14 13:03:21 -0500

    made uptime a getter and made sure that the buff was applied from the selected player

--- src/Parser/RetributionPaladin/Modules/PaladinCore/BlessingOfTheAshbringer.js
+++ src/Parser/RetributionPaladin/Modules/PaladinCore/BlessingOfTheAshbringer.js
@@ -13,11 +13,8 @@ class BlessingOfTheAshbringer extends Module {
 		combatants: Combatants,
 	};
 
-
-	//Blessing of the Ashbringer = BotA
-	botaUptime = 0;
-	blessingOfWisdomUptime = 0;
 	blessingOfKingsUptime = 0;
+	blessingOfWisdomUptime = 0;
 
 	on_initialized() {
 		this.active = this.combatants.selected.traitsBySpellId[SPELLS.BLESSING_OF_THE_ASHBRINGER.id] > 0;
@@ -27,31 +24,36 @@ class BlessingOfTheAshbringer extends Module {
 	on_byPlayer_applybuff(event) {
 		const spellId = event.ability.guid;
 		const targetId = event.targetID;
-		this.botaUptime = this.combatants.selected.getBuffUptime(SPELLS.BLESSING_OF_THE_ASHBRINGER_BUFF.id) / this.owner.fightDuration;
+		const playerId = this.owner.playerId;
 		
-		if(spellId === SPELLS.GREATER_BLESSING_OF_WISDOM.id){
+		if(spellId === SPELLS.GREATER_BLESSING_OF_WISDOM.id && event.sourceID === playerId){
 			this.blessingOfWisdomUptime = this.combatants.players[targetId].getBuffUptime(spellId) / this.owner.fightDuration;
 			debug && console.log(this.blessingOfWisdomUptime, 'wisdom uptime');
+			
 		}
-		if(spellId === SPELLS.GREATER_BLESSING_OF_KINGS.id){
+		if(spellId === SPELLS.GREATER_BLESSING_OF_KINGS.id && event.sourceID === playerId){
 			this.blessingOfKingsUptime = this.combatants.players[targetId].getBuffUptime(spellId) / this.owner.fightDuration;
 			debug && console.log(this.blessingOfKingsUptime, 'kings uptime');
-		}
+		}	
+	}
 
+	get uptime() {
+		let uptime = this.combatants.selected.getBuffUptime(SPELLS.BLESSING_OF_THE_ASHBRINGER_BUFF.id) / this.owner.fightDuration;
 		//If Blessing of the Ashbringer is undef/NaN set it to be 
 		//the lower of the uptimes between Kings and Wisdom
-		if(!this.botaUptime){
-			this.botaUptime = this.blessingOfWisdomUptime > this.blessingOfKingsUptime ? this.blessingOfKingsUptime : this.blessingOfKingsUptime;
-			debug && console.log(this.botaUptime, 'Blessing of the Ashbringer');
+		if(!uptime){
+			uptime = this.blessingOfWisdomUptime > this.blessingOfKingsUptime ? this.blessingOfKingsUptime : this.blessingOfWisdomUptime;
+			debug && console.log(uptime, 'Blessing of the Ashbringer');
 		}
+		return uptime;
 	}
 
 	suggestions(when) {
-		when(this.botaUptime).isLessThan(0.95)
+		when(this.uptime).isLessThan(0.95)
 			.addSuggestion((suggest, actual, recommended) => {
 				return suggest(<span>Your <SpellLink id={SPELLS.BLESSING_OF_THE_ASHBRINGER.id} /> uptime is low. Make sure to apply <SpellLink id={SPELLS.GREATER_BLESSING_OF_WISDOM.id} /> and <SpellLink id={SPELLS.GREATER_BLESSING_OF_KINGS.id} /> before the fight starts.</span>)
 					.icon(SPELLS.BLESSING_OF_THE_ASHBRINGER.icon)
-					.actual(`${formatPercentage(this.botaUptime)}%`)
+					.actual(`${formatPercentage(this.uptime)}%`)
 					.recommended(`${formatPercentage(recommended)}% is recommended`)
 					.major(recommended);
 			});
