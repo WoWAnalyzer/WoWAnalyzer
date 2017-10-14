fadb62e48bafd53dccefaa6e904e385c5c31dc9a
Aveline <hello@aveline.be>
2017-10-14 02:39:10 +0200

    DH legendaries

--- src/common/ITEMS.js
+++ src/common/ITEMS.js
@@ -981,6 +981,14 @@ const ITEMS = {
     quality: ITEM_QUALITIES.LEGENDARY,
   },
 
+  // Shared Demon Hunter legendaries
+  SOUL_OF_THE_SLAYER: {
+    id: 151639,
+    name: 'Soul of the Slayer',
+    icon: 'inv_70_dungeon_ring6a',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
   // Vengeance Demon Hunter legendaries
   KIREL_NARAK: {
     id: 138949,
@@ -1004,6 +1012,68 @@ const ITEMS = {
     id: 137091,
     name: 'The Defiler\'s Lost Vambraces',
     icon: 'inv_bracer_plate_pvpdeathknight_c_02',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  RUNEMASTERS_PAULDRONS: {
+    id: 137071,
+    name: 'Runemaster\'s Pauldrons',
+    icon: 'inv_shoulder_105',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  CLOAK_OF_FEL_FLAMES: {
+    id: 137066,
+    name: 'Cloak of Fel Flames',
+    icon: 'inv_misc_cape_09',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  SPIRIT_OF_THE_DARKNESS_FLAME: {
+    id: 144292,
+    name: 'Spirit of the Darkness Flame',
+    icon: 'inv_glove_leather_legionendgame_c_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
+  // Havoc Demon Hunter Legendaries
+  MOARG_BIONIC_STABILIZERS: {
+    id: 137090,
+    name: 'Mo\'arg Bionic Stabilizers',
+    icon: 'inv_plate_raiddeathknightmythic_o_01bracer',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  RADDONS_CASCADING_EYES: {
+    id: 137061,
+    name: 'Raddon\'s Cascading Eyes',
+    icon: 'inv_misc_bandana_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ACHOR_THE_ETERNAL_HUNGER: {
+    id: 137014,
+    name: 'Achor, the Eternal Hunger',
+    icon: 'inv_chest_mail_raidhuntermythic_p_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  LORAMUS_THALIPEDES_SACRIFICE: {
+    id: 137022,
+    name: 'Loramus Thalipedes\' Sacrifice',
+    icon: 'inv_boot_leather_raiddruid_k_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ANGER_OF_THE_HALF_GIANTS: {
+    id: 137038,
+    name: 'Anger of the Half-Giants',
+    icon: 'inv_60pvp_ring1a',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  DELUSIONS_OF_GRANDEUR: {
+    id: 144279,
+    name: 'Delusions of Grandeur',
+    icon: 'inv_shoulder_51',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  CHAOS_THEORY: {
+    id: 151798,
+    name: 'Chaos Theory',
+    icon: 'inv_cape_draenorraid_d_01plate_dk',
     quality: ITEM_QUALITIES.LEGENDARY,
   },
 
