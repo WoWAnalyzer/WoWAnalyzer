44f9f4801d98ce72aec9f3ebecf7d60d7c07f88f
Juko8 <ulrichenevoldsen@hotmail.com>
2017-10-15 14:17:07 +0200

    Initialized a Katusos chi saved tracking

---
+++ src/Parser/WindwalkerMonk/Modules/Items/Katsuos.js
@@ -0,0 +1,26 @@
+import ITEMS from 'common/ITEMS';
+import SPELLS from 'common/SPELLS';
+import Module from 'Parser/Core/Module';
+
+import Combatants from 'Parser/Core/Modules/Combatants';
+
+const debug = false;
+
+class Katsuos extends Module {
+  static dependencies = {
+    combatatants: Combatants,
+  };
+  chiSaved = 0;
+
+  on_initialized() {
+    this.active = this.combatatants.selected.hasFeet(ITEMS.KATSUOS_ECLIPSE.id);
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+
+    if (spellId === SPELLS.FISTS_OF_FURY_CAST.id! & this.combatatants.selected.hasBuff(SPELLS.SERENITY_TALENT.id)) {
+      chiSaved += 1;
+    }
+  }
+}
