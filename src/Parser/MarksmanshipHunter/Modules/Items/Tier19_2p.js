ce24e3c46e5488db5e054ce2857d9a3ebafcc307
Putro <lafa16ac@student.cbs.dk>
2017-10-16 21:45:52 +0200

    fix to meet requests

--- src/Parser/MarksmanshipHunter/Modules/Items/Tier19_2p.js
+++ src/Parser/MarksmanshipHunter/Modules/Items/Tier19_2p.js
@@ -7,11 +7,24 @@ import RESOURCE_TYPES from 'common/RESOURCE_TYPES';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import SpellLink from 'common/SpellLink';
-import { formatNumber } from "../../../../common/format";
+import { formatNumber } from "common/format";
 
 const debug = false;
 
 const CDR_PER_FOCUS = 22.22222;
+
+const AFFECTED_ABILITIES = [
+  SPELLS.AIMED_SHOT.id,
+  SPELLS.PIERCING_SHOT_TALENT.id,
+  SPELLS.BURSTING_SHOT.id,
+  SPELLS.MARKED_SHOT.id,
+  SPELLS.WINDBURST.id,
+  SPELLS.BARRAGE_TALENT.id,
+  SPELLS.VOLLEY_TALENT.id,
+  SPELLS.EXPLOSIVE_SHOT_TALENT.id,
+  SPELLS.BLACK_ARROW_TALENT.id,
+
+];
 
 class Tier19_2p extends Module {
   static dependencies = {
@@ -29,7 +42,7 @@ class Tier19_2p extends Module {
   }
   on_byPlayer_cast(event) {
     const spellId = event.ability.guid;
-    if (spellId !== SPELLS.AIMED_SHOT.id && spellId !== SPELLS.PIERCING_SHOT_TALENT.id && spellId !== SPELLS.BURSTING_SHOT.id && spellId !== SPELLS.MARKED_SHOT.id && spellId !== SPELLS.WINDBURST.id) {
+    if (AFFECTED_ABILITIES.every(id => spellId !== id)) {
       return;
     }
     if (event.classResources) {
@@ -60,7 +73,7 @@ class Tier19_2p extends Module {
       title: <SpellLink id={SPELLS.HUNTER_MM_T19_2P_BONUS.id} />,
       result: (
         <dfn data-tip={`You wasted ${formatNumber(this.wastedTrueshotReductionMs / 1000)} seconds of CDR.<br/> `}>
-          You reduced <SpellLink id={SPELLS.TRUESHOT.id} /> by {formatNumber(this.effectiveTrueshotReductionMs / 1000)} seconds in total over the course of the fight.
+           <SpellLink id={SPELLS.TRUESHOT.id} /> CD reduced by {formatNumber(this.effectiveTrueshotReductionMs / 1000)} seconds in total.
         </dfn>
       ),
     };
