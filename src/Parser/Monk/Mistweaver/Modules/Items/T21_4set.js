a4d287fa584c9ec091c3aad63602e7ae604dc769
anomoly <jkippley@gmail.com>
2017-10-17 22:09:53 -0500

    CHANGELOG Updates - Add trigger count to 4set

--- src/Parser/Monk/Mistweaver/Modules/Items/T21_4set.js
+++ src/Parser/Monk/Mistweaver/Modules/Items/T21_4set.js
@@ -15,8 +15,23 @@ class T21_4set extends Module {
     abilityTracker: AbilityTracker,
   };
 
+  boltCount = 0;
+  averageBoltsPerCast = 0;
+
   on_initialized() {
     this.active = this.combatants.selected.hasBuff(SPELLS.CHIJIS_BATTLEGEAR_4_PIECE_BUFF.id);
+  }
+
+  on_byPlayer_heal(event) {
+    const spellId = event.ability.guid;
+    const renewingMist = this.abilityTracker.getAbility(SPELLS.RENEWING_MIST.id);
+
+    if(spellId !== SPELLS.CHI_BOLT.id) {
+      return;
+    }
+
+    this.boltCount += 1;
+    this.averageBoltsPerCast = this.boltCount / renewingMist.casts;
   }
 
   item() {
@@ -27,9 +42,9 @@ class T21_4set extends Module {
       icon: <SpellIcon id={SPELLS.CHIJIS_BATTLEGEAR_4_PIECE_BUFF.id} />,
       title: <SpellLink id={SPELLS.CHIJIS_BATTLEGEAR_4_PIECE_BUFF.id} />,
       result: (
-        <span>
+        <dfn data-tip={`Average bolts per trigger: ${this.averageBoltsPerCast.toFixed(2)}`}>
           {this.owner.formatItemHealingDone(healing)}
-        </span>
+        </dfn>
       ),
     };
   }
