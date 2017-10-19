bb087207c6bffeb181352c35396bcf6710017ffb
Martijn Hols <martijnhols@hotmail.com>
2017-10-19 21:44:47 +0200

    Remove unnecessary comments

--- src/Parser/AVAILABLE_CONFIGS.js
+++ src/Parser/AVAILABLE_CONFIGS.js
@@ -1,101 +1,77 @@
-// Deathknight
 import BloodDeathKnight from './DeathKnight/Blood/CONFIG';
 
-// Demon Hunter
 import HavocDemonHunter from './DemonHunter/Havoc/CONFIG';
 import VengeanceDemonHunter from './DemonHunter/Vengeance/CONFIG';
 
-// Druid
 import BalanceDruid from './Druid/Balance/CONFIG';
 import FeralDruid from './Druid/Feral/CONFIG';
 import GuardianDruid from './Druid/Guardian/CONFIG';
 import RestoDruid from './RestoDruid/CONFIG';
 
-// Hunt
-import Marksmanship from './Hunter/Marksmanship/CONFIG';
+import MarksmanshipHunter from './Hunter/Marksmanship/CONFIG';
 
-// Mage
 import FrostMage from './Mage/Frost/CONFIG';
 import FireMage from './Mage/Fire/CONFIG';
 
-// Monk
 import BrewmasterMonk from './Monk/Brewmaster/CONFIG';
 import WindwalkerMonk from './Monk/Windwalker/CONFIG';
 import MistweaverMonk from './Monk/Mistweaver/CONFIG';
 
-// Paladin
 import HolyPaladin from './Paladin/Holy/CONFIG';
 import RetributionPaladin from './RetributionPaladin/CONFIG';
 
-// Priest
 import DisciplinePriest from './Priest/Discipline/CONFIG';
 import HolyPriest from './Priest/Holy/CONFIG';
 import ShadowPriest from './Priest/Shadow/CONFIG';
 
-// Rogue
 import SubtletyRogue from './Rogue/Subtlety/CONFIG';
 
-// Shaman
 import ElementalShaman from './Shaman/Elemental/CONFIG';
 import EnhancementShaman from './Shaman/Enhancement/CONFIG';
 import RestorationShaman from './Shaman/Restoration/CONFIG';
 
-// Warlock
 import AfflictionWarlock from './Warlock/Affliction/CONFIG';
 import DemonologyWarlock from './Warlock/Demonology/CONFIG';
 import DestructionWarlock from './Warlock/Destruction/CONFIG';
 
-// Warrior
 import ProtectionWarrior from './Warrior/Protection/CONFIG';
 
 export default [
-  // Deathknight
   BloodDeathKnight,
 
-  // Demon Hunter
   HavocDemonHunter,
   VengeanceDemonHunter,
 
-  // Druid
   BalanceDruid,
   FeralDruid,
   GuardianDruid,
   RestoDruid,
 
-  // Hunter
-  Marksmanship,
+  MarksmanshipHunter,
 
-  // Mage
   FrostMage,
   FireMage,
 
-  // Monk
   BrewmasterMonk,
   WindwalkerMonk,
   MistweaverMonk,
 
-  // Paladin
   HolyPaladin,
   RetributionPaladin,
 
-  // Priest
   DisciplinePriest,
   HolyPriest,
   ShadowPriest,
 
-  // Rogue
   SubtletyRogue,
 
-  // Shaman
   ElementalShaman,
   EnhancementShaman,
   RestorationShaman,
 
-  // Warlock
   AfflictionWarlock,
   DemonologyWarlock,
   DestructionWarlock,
 
-  // Warrior
   ProtectionWarrior,
 ];
