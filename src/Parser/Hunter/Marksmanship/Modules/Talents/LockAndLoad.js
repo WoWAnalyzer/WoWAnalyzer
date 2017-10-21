3423ebb93e1df1f1c6fecc64cbb24c7c84370f84
Putro <lafa16ac@student.cbs.dk>
2017-10-21 14:57:19 +0200

    updated for PR

--- src/Parser/Hunter/Marksmanship/Modules/Talents/LockAndLoad.js
+++ src/Parser/Hunter/Marksmanship/Modules/Talents/LockAndLoad.js
@@ -20,8 +20,9 @@ class LockAndLoad extends Module {
   halfLNLProcs = 0;
   noGainLNLProcs = 0;
   totalProcs = 0;
-  autoshots = 0;
+  autoShots = 0;
   wastedInstants = 0;
+  _curruntStacks = 0;
 
   on_initialized() {
     this.active = this.combatants.selected.hasTalent(SPELLS.LOCK_AND_LOAD_TALENT.id) || this.combatants.selected.hasFinger(ITEMS.SOUL_OF_THE_HUNTMASTER.id);
@@ -34,32 +35,39 @@ class LockAndLoad extends Module {
     }
     this.fullLNLProcs += 1;
     this.totalProcs += 1;
+    this._curruntStacks = 2;
     debug && console.log('full lnl proc, this is number ', this.fullLNLProcs);
 
   }
 
-  //haven't found a log where this happens yet, but this should theoretically work.
-  on_byPlayer_applybuffstack(event) {
-    const buffId = event.ability.guid;
-    if (buffId !== SPELLS.LOCK_AND_LOAD_BUFF.id) {
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (!this.combatants.selected.hasBuff(SPELLS.LOCK_AND_LOAD_BUFF.id, event.timestamp)){
       return;
     }
-    this.halfLNLProcs += 1;
-    this.totalProcs += 1;
-    this.wastedInstants += 1;
-    debug && console.log('at 1 stack proc, this is number ', this.halfLNLProcs);
-
+    if (spellId !== SPELLS.AIMED_SHOT.id) {
+      return;
+    }
+    this._curruntStacks -= 1;
   }
   on_byPlayer_refreshbuff(event) {
     const buffId = event.ability.guid;
     if (buffId !== SPELLS.LOCK_AND_LOAD_BUFF.id) {
       return;
     }
-    this.noGainLNLProcs += 1;
-    this.totalProcs += 1;
-    this.wastedInstants += 2;
-    debug && console.log('at 2 stacks already proc, this is number ', this.noGainLNLProcs);
+    if (this._curruntStacks === 1) {
+      this.halfLNLProcs += 1;
+      this.wastedInstants += 1;
+      debug && console.log('at 1 stacks already proc, this is number ', this.halfLNLProcs);
 
+    }
+    if (this._curruntStacks === 2) {
+      this.noGainLNLProcs += 1;
+      this.wastedInstants += 2;
+      debug && console.log('at 2 stacks already proc, this is number ', this.noGainLNLProcs);
+    }
+    this.totalProcs += 1;
+    this._curruntStacks = 2;
   }
 
   on_byPlayer_damage(event) {
@@ -67,17 +75,17 @@ class LockAndLoad extends Module {
     if (spellID !== SPELLS.AUTO_SHOT.id) {
       return;
     }
-    this.autoshots += 1;
+    this.autoShots += 1;
   }
 
   statistic() {
-    const expectedProcs = Math.round(this.autoshots * 0.08);
+    const expectedProcs = Math.round(this.autoShots * 0.08);
     return (
       <StatisticBox
         icon={<SpellIcon id={SPELLS.LOCK_AND_LOAD_TALENT.id} />}
-        value={`${formatPercentage(this.totalProcs / expectedProcs, 1)}%`}
-        label={`of expected procs`}
-        tooltip={`You had a total of ${this.totalProcs} procs, and your expected amount of procs was ${expectedProcs}. <br /> You had ${this.noGainLNLProcs} with 2 lnl stacks remaining and ${this.halfLNLProcs} while you had 1 stack remaining. This resulted in you losing out on ${this.wastedInstants} instant casts of Aimed Shot due to overlapping.`}
+        value={`${this.wastedInstants} (${formatPercentage(this.wastedInstants/(this.totalProcs*2))}%)`}
+        label={`lost instant casts`}
+        tooltip={`You had ${formatPercentage(this.totalProcs / expectedProcs, 1)}% procs of what you could expect to get over the encounter. <br /> You had a total of ${this.totalProcs} procs, and your expected amount of procs was ${expectedProcs}. <br /> You had ${this.noGainLNLProcs} procs with 2 lnl stacks remaining and ${this.halfLNLProcs} while you had 1 stack remaining.`}
       />
     );
   }
--- src/Parser/Hunter/Marksmanship/Modules/Talents/TrueAim.js
+++ src/Parser/Hunter/Marksmanship/Modules/Talents/TrueAim.js
@@ -9,6 +9,8 @@ import SpellIcon from 'common/SpellIcon';
 import { formatPercentage } from 'common/format';
 import StatisticBox from 'Main/StatisticBox';
 
+const MAX_STACKS = 10;
+
 class TrueAim extends Module {
   static dependencies = {
     combatants: Combatants,
@@ -21,6 +23,7 @@ class TrueAim extends Module {
 
   //starts -1 because the stacks drop at end of combat, but that shouldn't count as a time where it dropped
   timesDropped = -1;
+  totalTimesDropped = -1;
 
   on_initialized() {
     this.active = this.combatants.selected.hasTalent(SPELLS.TRUE_AIM_TALENT.id);
@@ -31,13 +34,13 @@ class TrueAim extends Module {
     if (debuffId !== SPELLS.TRUE_AIM_DEBUFF.id) {
       return;
     }
-    //I can't use on_byPlayer_applydebuff as it won't work in conjunction with on_byplayer_applydebuffstack
+    //I can't use on_byPlayer_applydebuff as it won't work in conjunction with on_byplayer_removedebuff
     //so we do it this way, and just make a check if _currentStacks is 0, in which case we set it to 1 to indicate it already has been applied.
     if (this._currentStacks === 0) {
       this._currentStacks = 1;
     }
     this._currentStacks += 1;
-    if (this._currentStacks === 10) {
+    if (this._currentStacks === MAX_STACKS) {
       this.startOfMaxStacks = event.timestamp;
     }
   }
@@ -46,26 +49,29 @@ class TrueAim extends Module {
     if (debuffId !== SPELLS.TRUE_AIM_DEBUFF.id) {
       return;
     }
-    if (this._currentStacks === 10) {
+    if (this._currentStacks === MAX_STACKS) {
       this.timeAtMaxStacks += event.timestamp - this.startOfMaxStacks;
     }
     //ensures Trickshot cleaving doesn't count as multiple resets of stacks
-    if (this._currentStacks > 2) {
+    if (this._currentStacks >= 3) {
       this.timesDropped += 1;
     }
+    this.totalTimesDropped += 1;
     this._currentStacks = 0;
   }
 
   statistic() {
     const percentTimeAtMaxTAStacks = formatPercentage(this.timeAtMaxStacks / this.owner.fightDuration);
-    return (
-      <StatisticBox
-        icon={<SpellIcon id={SPELLS.TRUE_AIM_DEBUFF.id} />}
-        value={`${percentTimeAtMaxTAStacks} %`}
-        label="10 stack uptime"
-        tooltip={`You reset your stacks ${this.timesDropped} times over the course of the encounter`}
-      />
-    );
+    if (this.timesDropped > 0) {
+      return (
+        <StatisticBox
+          icon={<SpellIcon id={SPELLS.TRUE_AIM_DEBUFF.id} />}
+          value={`${percentTimeAtMaxTAStacks} %`}
+          label="10 stack uptime"
+          tooltip={`You reset your stacks (when they were at 3 stacks or higher) ${this.timesDropped} times over the course of the encounter. <br />Your total amount of resets (including with trickshot cleaving) was: ${this.totalTimesDropped}`}
+        />
+      );
+    }
   }
 
 }
