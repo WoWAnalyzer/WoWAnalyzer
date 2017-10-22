c50555e4391a08a00deda3972b0f7279e11ecb2e
Martijn Hols <martijnhols@hotmail.com>
2017-10-22 21:01:03 +0200

    Specific handling for 521 http code

--- src/common/fetchWcl.js
+++ src/common/fetchWcl.js
@@ -2,6 +2,12 @@ import makeWclUrl from './makeWclUrl';
 
 export default function fetchWcl(endpoint, queryParams) {
   return fetch(makeWclUrl(endpoint, queryParams))
+    .then(response => {
+      if (response.status === 521) {
+        throw new Error('The API is currently down. This is usually for maintenance which should only take about 10 seconds. Please try again in a moment.');
+      }
+      return response;
+    })
     .then(response => response.json())
     .then(json => {
       if (json.status === 400 || json.status === 401) {
