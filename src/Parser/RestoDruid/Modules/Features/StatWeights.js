68b31e2a8d98005a23f4907e7828b0bc097d534c
Kelton Finch <keltonfinch@gmail.com>
2017-10-23 18:40:56 -0400

    [resto druid] StatWeight code cleanup

--- src/Parser/RestoDruid/Modules/Features/StatWeights.js
+++ src/Parser/RestoDruid/Modules/Features/StatWeights.js
@@ -9,7 +9,7 @@ import CritEffectBonus from 'Parser/Core/Modules/Helpers/CritEffectBonus';
 
 import { getSpellInfo } from '../../SpellInfo';
 
-const DEBUG = true;
+const DEBUG = false;
 
 const ARMOR_INT_MULTIPLIER = 1.05; // 5% int bonus from wearing all leather means each new point of int worth 1.05 vs character sheet int
 
@@ -27,7 +27,6 @@ class StatWeights extends Module {
   totalOneInt = 0;
   totalOneCrit = 0;
   totalOneHasteHpm = 0;
-//  totalOneHasteHpct = 0;
   totalOneMastery = 0;
   totalOneVers = 0;
   totalOneLeech = 0;
@@ -55,93 +54,81 @@ class StatWeights extends Module {
     const amount = healVal.effective;
     const spellInfo = getSpellInfo(event.ability.guid);
 
-    // LEECH //
+    // region LEECH
     // We have to calculate leech weight differently depending on if we already have any leech rating.
     // Leech is marked as a 'multplier' heal, so we have to check it before we do the early return below
     const hasLeech = this.combatants.selected.leechRating > 0; // TODO replace when dynamic stats
-    let oneLeech = 0;
     if(hasLeech && spellInfo.isLeech && !healVal.overheal) {
-      oneLeech = amount / this.combatants.selected.leechRating; // TODO replace when dynamic stats
+      this.totalOneLeech += amount / this.combatants.selected.leechRating; // TODO replace when dynamic stats
     } else if(!hasLeech) {
       // TODO this will be a pain to implement
     }
-    this.totalOneLeech += oneLeech;
+    // endregion
 
-    if(spellInfo.multiplier) {
-      return; // multiplier heals don't count for weights and they're adjusted out for total healing
-    }
-    this.totalAdjustedHealing += healVal.effective;
-    if(spellInfo.ignored) {
-      return; // ignored heals don't count for weights but they are part of adjusted total healing
+    if(!spellInfo.multiplier) {
+      this.totalAdjustedHealing += healVal.effective;
     }
 
-    if(healVal.overheal) {
-      return; // if a spell overheals, we know it couldn't have healed for more
+    if(spellInfo.multiplier || spellInfo.ignored || healVal.overheal) {
+      return;
     }
 
-    const hotCount = target.activeBuffs()
-        .map(buffObj => buffObj.ability.guid)
-        .filter(buffId => getSpellInfo(buffId).masteryStack)
-        .length;
-
-    // INT //
-    let oneInt = 0;
+    // region INT
     if(spellInfo.int) {
       const currInt = this._getCurrInt(); // TODO replace when dynamic stats
       const bonusFromOneInt = (1 / currInt) * ARMOR_INT_MULTIPLIER;
-      oneInt = amount * bonusFromOneInt;
+      this.totalOneInt += amount * bonusFromOneInt;
     }
-    this.totalOneInt += oneInt;
+    // endregion
 
-    // CRIT //
-    let oneCrit = 0;
+    // region CRIT
     if(spellInfo.crit) {
       const currCritPerc = this.combatants.selected.critPercentage; // TODO replace when dynamic stats
       const bonusFromOneCrit = 1 / 40000; // TODO replace when stat constants exist
       if(spellInfo.isLivingSeed) {
         // Living Seed doesn't crit, but it procs from crits only. This calculation approximates increased LS frequency due to more crits.
         const additionalLivingSeedChance = bonusFromOneCrit / currCritPerc;
-        oneCrit = additionalLivingSeedChance * amount;
+        this.totalOneCrit += additionalLivingSeedChance * amount;
       } else {
         const critMult = this.critEffectBonus.getBonus(event);
         const noCritHealing = event.hitType === HIT_TYPES.CRIT ? amount / critMult : amount;
-        oneCrit = noCritHealing * bonusFromOneCrit * (critMult - 1);
+        this.totalOneCrit += noCritHealing * bonusFromOneCrit * (critMult - 1);
       }
     }
-    this.totalOneCrit += oneCrit;
+    // endregion
 
-    // HASTE //
-    const currHastePerc = this.combatants.selected.hastePercentage; // TODO replace when dynamic stats
-    const bonusFromOneHaste = 1 / 37500; // TODO replace when stat constants exist
-    const noHasteHealing = amount / (1 + currHastePerc);
-
-    let oneHasteHpm = 0;
+    // region HASTE
     if(spellInfo.hasteHpm) {
-      oneHasteHpm = bonusFromOneHaste * noHasteHealing;
+      const currHastePerc = this.combatants.selected.hastePercentage; // TODO replace when dynamic stats
+      const bonusFromOneHaste = 1 / 37500; // TODO replace when stat constants exist
+      const noHasteHealing = amount / (1 + currHastePerc);
+      this.totalOneHasteHpm += bonusFromOneHaste * noHasteHealing;
     }
-    this.totalOneHasteHpm += oneHasteHpm;
+    // endregion
 
     // FIXME Previous hasteHpct calculation was unsatisfactory. Excluding entirely for now.
 
-    // MASTERY //
-    let oneMastery = 0;
+    // region MASTERY
     if(spellInfo.mastery) {
       const currMasteryPerc = this._getCurrMasteryPerc(); // TODO replace when dynamic stats
       const bonusFromOneMastery = 1 / 66666; // TODO replace when stat constants exist
+      const hotCount = target.activeBuffs()
+          .map(buffObj => buffObj.ability.guid)
+          .filter(buffId => getSpellInfo(buffId).masteryStack)
+          .length;
       const noMasteryHealing = amount / (1 + (currMasteryPerc * hotCount));
-      oneMastery = noMasteryHealing * bonusFromOneMastery * hotCount;
+      this.totalOneMastery += noMasteryHealing * bonusFromOneMastery * hotCount;
     }
-    this.totalOneMastery += oneMastery;
+    // endregion
 
-    // VERS //
-    let oneVers = 0;
+    // region VERS
     if(spellInfo.vers) {
       const currVersPerc = this.combatants.selected.versatilityPercentage; // TODO replace when dynamic stats
       const bonusFromOneVers = 1 / 47500; // TODO replace when stat constants exist
       const noVersHealing = amount / (1 + currVersPerc);
-      oneVers = noVersHealing * bonusFromOneVers;
+      this.totalOneVers += noVersHealing * bonusFromOneVers;
     }
-    this.totalOneVers += oneVers;
+    // endregion
   }
 
   // FIXME temporary hacky handler for Concordance until stat tracker is implemented
@@ -167,7 +154,6 @@ class StatWeights extends Module {
       console.log(`Int - ${formatNumber(this.totalOneInt)}`);
       console.log(`Crit - ${formatNumber(this.totalOneCrit)}`);
       console.log(`Haste HPM - ${formatNumber(this.totalOneHasteHpm)}`);
-  //    console.log(`Haste HPCT - ${formatNumber(this.totalOneHasteHpct)}`);
       console.log(`Mastery - ${formatNumber(this.totalOneMastery)}`);
       console.log(`Vers - ${formatNumber(this.totalOneVers)}`);
       console.log(`Leech - ${formatNumber(this.totalOneLeech)}`);
@@ -185,7 +171,6 @@ class StatWeights extends Module {
     const intWeight = this.totalOneInt / this.totalOneInt;
     const critWeight = this.totalOneCrit / this.totalOneInt;
     const hasteHpmWeight = this.totalOneHasteHpm / this.totalOneInt;
-  //  const hasteHpctWeight = this.totalOneHasteHpct / this.totalOneInt;
     const masteryWeight = this.totalOneMastery / this.totalOneInt;
     const versWeight = this.totalOneVers / this.totalOneInt;
     let leechWeight = this.totalOneLeech / this.totalOneInt;
@@ -196,7 +181,6 @@ class StatWeights extends Module {
     const intForOnePercent = this._ratingPerOnePercent(this.totalOneInt);
     const critForOnePercent = this._ratingPerOnePercent(this.totalOneCrit);
     const hasteHpmForOnePercent = this._ratingPerOnePercent(this.totalOneHasteHpm);
-  //  const hasteHpctForOnePercent = this._ratingPerOnePercent(this.totalOneHasteHpct);
     const masteryForOnePercent = this._ratingPerOnePercent(this.totalOneMastery);
     const versForOnePercent = this._ratingPerOnePercent(this.totalOneVers);
     let leechForOnePercent = this._ratingPerOnePercent(this.totalOneLeech);
@@ -205,14 +189,12 @@ class StatWeights extends Module {
     }
 
     const hasteHpmTooltip = "HPM stands for 'Healing per Mana'. In valuing Haste, it considers only the faster HoT ticking and not the reduced cast times. Effectively it models haste's bonus to mana efficiency. This is typically the better calculation to use for raid encounters where mana is an issue.";
-//    const hasteHpctTooltip = "HPCT stands for 'Healing per Cast Time'. In valuing Haste, it considers both the faster HoT ticking and the ability to cast more spells in the same amount of time. This can be good for modeling a burst of healing over several seconds, but remember that over the course of a fight casting more spells means running out of mana faster.";
     const leechTooltip = "Leech weight can currently only be calculated when player already has some Leech rating";
 
     return [
       { stat:'Intellect', weight:intWeight, ratingForOne:intForOnePercent },
       { stat:'Crit', weight:critWeight, ratingForOne:critForOnePercent },
       { stat:'Haste (HPM)', weight:hasteHpmWeight, ratingForOne:hasteHpmForOnePercent, tooltip:hasteHpmTooltip },
-  //    { stat:'Haste (HPCT)', weight:hasteHpctWeight, ratingForOne:hasteHpctForOnePercent, tooltip:hasteHpctTooltip },
       { stat:'Mastery', weight:masteryWeight, ratingForOne:masteryForOnePercent },
       { stat:'Versatility', weight:versWeight, ratingForOne:versForOnePercent },
       { stat: 'Leech', weight:leechWeight, ratingForOne:leechForOnePercent, tooltip:leechTooltip },
