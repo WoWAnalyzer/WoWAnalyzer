55dd0724e11670b020a6790bf0390229b97262b2
jjs451 <jjs451@msstate.edu>
2017-10-22 23:58:57 -0500

    Fixed warnings from eslint

--- src/Parser/DeathKnight/Unholy/Modules/Features/CooldownTracker.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/CooldownTracker.js
@@ -1,6 +1,4 @@
-import SPELLS from 'common/SPELLS';
-
-import CoreCooldownTracker, { BUILT_IN_SUMMARY_TYPES } from 'Parser/Core/Modules/CooldownTracker';
+import CoreCooldownTracker from 'Parser/Core/Modules/CooldownTracker';
 
 class CooldownTracker extends CoreCooldownTracker {
   static cooldownSpells = [
