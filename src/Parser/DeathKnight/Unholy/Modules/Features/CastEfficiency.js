9c23c337c3e98903a3b8b4ec04fdead0fb076b88
Maxweii <Maxweii@users.noreply.github.com>
2017-10-25 15:55:59 -0400

    Update CastEfficiency.js
    Grammer fixes and rewordings

--- src/Parser/DeathKnight/Unholy/Modules/Features/CastEfficiency.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/CastEfficiency.js
@@ -18,7 +18,7 @@ class CastEfficiency extends CoreCastEfficiency {
       category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
       getCooldown: haste => 90,
       recommendedCastEfficiency: 0.95,
-      extraSuggestion: <span>This is one of your main DPS cooldown.  It is okay to not use it immediately if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/>, with <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/> and <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> having less than 10 seconds left on their CDs. However <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/> takes priority for being empowered by <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/>. </span>,
+      extraSuggestion: <span>Making sure to use <SpellLink id={SPELLS.APOCALYPSE.id}/> immediately after it's cooldown is up is important, try to plan for it's use as it is coming off cooldown. If you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/>, empowering <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/> with <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> takes priority, so do not worry if an <SpellLink id={SPELLS.APOCALYPSE.id}/> cast is not empowered. </span>,
     },
 
     {
@@ -26,7 +26,7 @@ class CastEfficiency extends CoreCastEfficiency {
       category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
       getCooldown: haste => 60, // TODO: add support for shadow infusion - adding suggestion note to account for difference between Infected Claws and Shadow Infusion as well
       recommendedCastEfficiency: 0.90,
-      extraSuggestion: <span>You should normally be using this off CD, but if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> it is okay to hold if <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/>'s CD has less than 30 seconds remaining.</span>,
+      extraSuggestion: <span>Normally you should be using this off CD, but if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> it is okay to hold if <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/>'s CD has less than 30 seconds remaining.</span>,
     },
 
     {
@@ -35,7 +35,7 @@ class CastEfficiency extends CoreCastEfficiency {
       getCooldown: haste => 120, // TODO: needs to account for CoF
       recommendedCastEfficiency: 0.90,
       isActive: combatant => combatant.hasTalent(SPELLS.DARK_ARBITER_TALENT.id),
-      extraSuggestion: 'This is your main DPS cooldown. Cast this off cooldown, and if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> make sure <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> can be cast immediatly after.',
+      extraSuggestion: 'This is your main DPS cooldown. Try to cast this off cooldown, however if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> make sure <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> can be cast immediatly after.',
     },
 
     {
@@ -44,7 +44,7 @@ class CastEfficiency extends CoreCastEfficiency {
       getCooldown: haste => 180, // TODO: needs to account for CoF
       recommendedCastEfficiency: 0.90,
       isActive: combatant => combatant.hasTalent(SPELLS.DEFILE_TALENT.id) || combatant.hasTalent(SPELLS.SOUL_REAPER_TALENT.id),
-      extraSuggestion: 'This is your main DPS cooldown.  Cast this off cooldown, if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> make sure <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> can be cast immediatly after.',
+      extraSuggestion: 'This is your main DPS cooldown.  Try to cast this off cooldown, however if you are wearing <ItemLink id={ITEMS.TAKTHERITRIXS_SHOULDERPADS.id}/> make sure <SpellLink id={SPELLS.DARK_TRANSFORMATION.id}/> can be cast immediatly after.',
     },
   ];
 }
