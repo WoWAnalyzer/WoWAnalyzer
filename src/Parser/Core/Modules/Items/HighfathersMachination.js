97533217ee24d8e94876561f481bc20ef647d14f
Dyspho <33057289+Dyspho@users.noreply.github.com>
2017-10-25 15:33:00 +0200

    Update HighfathersMachination.js

--- src/Parser/Core/Modules/Items/HighfathersMachination.js
+++ src/Parser/Core/Modules/Items/HighfathersMachination.js
@@ -1,7 +1,6 @@
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
 import React from 'react';
-import { formatNumber } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
@@ -21,7 +20,7 @@ class HighfathersMachination extends Analyzer {
     const spellId = event.ability.guid;
 
     if (spellId === SPELLS.AMANTHULS_PRESENCEHEAL.id) {
-      this.procUsed++;
+      this.procUsed += 1;
       this.healing += (event.amount || 0) + (event.absorbed || 0);
     }
   }
@@ -29,22 +28,23 @@ class HighfathersMachination extends Analyzer {
   on_byPlayer_applybuff(event) {
       const spellId = event.ability.guid;
       if (spellId === SPELLS.AMANTHULS_PRESENCEBUFF.id) {
-          this.totalProc++;
+          this.totalProc += 1;
       }
   }
 
   on_byPlayer_applybuffstack(event) {
       const spellId = event.ability.guid;
       if (spellId === SPELLS.AMANTHULS_PRESENCEBUFF.id) {
-          this.totalProc++;
+          this.totalProc += 1;
       }
   }
 
   item() {
+    const procWasted = this.totalProc - this.procUsed;
     return {
       item: ITEMS.HIGHFATHERS_MACHINATION,
       result: (
-        <dfn data-tip={`The trinket applied a total of ${this.totalProc} healing stacks.<br>${this.procUsed} stacks were used to heal a total amount of ${formatNumber(this.healing)}.<br>${this.totalProc - this.procUsed} stacks wasted.`}>
+        <dfn data-tip={`The trinket applied a total of ${this.totalProc} healing stacks.<br>${procWasted} stacks were wasted and didn\'t contribute to healing.`}>
         {this.owner.formatItemHealingDone(this.healing)}
         </dfn>),
     };
