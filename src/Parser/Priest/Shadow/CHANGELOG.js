58b5ba43932d74d5f1b9d499c2a6454311bd152c
Martijn Hols <martijnhols@hotmail.com>
2017-10-25 22:38:44 +0200

    Update Shadow Priest completion to "not actively maintained" since it hasn't been updated in a long time

--- src/Parser/Priest/Shadow/CHANGELOG.js
+++ src/Parser/Priest/Shadow/CHANGELOG.js
@@ -2,4 +2,3 @@ export default `
 29-08-2017 - Shadow Priest: Added Insanity Resource on the Voidform graphs. Fixed pet damage. (by hassebewlen)
 26-08-2017 - Shadow Priest: Initial implementation. (by hassebewlen)
 `;
-
