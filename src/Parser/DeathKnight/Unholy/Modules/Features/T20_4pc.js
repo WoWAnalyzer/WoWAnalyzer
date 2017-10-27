eb46bd818b3fba9a204601161fa2e6e24b54f4cc
Maxweii <Maxweii@users.noreply.github.com>
2017-10-26 20:21:47 -0400

    Death Knight - Unholy - Create T20_4pc.js
    Creating tracker for tier 20 four piece buff uptime

---
+++ src/Parser/DeathKnight/Unholy/Modules/Features/T20_4pc.js
@@ -0,0 +1,31 @@
+++import React from 'react';
+ + +
+ + +import Analyzer from 'Parser/Core/Analyzer';
+ + +import Combatants from 'Parser/Core/Modules/Combatants';
+ + +
+ + +import SPELLS from 'common/SPELLS';
+ + +import SpellLink from 'common/SpellLink';
+ + +import SpellIcon from 'common/SpellIcon';
+ + +import { formatPercentage } from 'common/format';
+ + +
+ + +class Tier20_4set extends Analyzer {
+ + +  static dependencies = {
+ + +    combatants: Combatants,
+ + +  };
+ + +
+ + +  on_initialized() {
+ + +    this.active = this.combatants.selected.hasBuff(SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id);
+ + +  }
+ + +
+ + +  item() {
+ + +    const uptime = this.combatants.selected.getBuffUptime(SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id) / this.owner.fightDuration;
+ + +    return {
+ + +      id: `spell-${SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id}`,
+ + +      icon: <SpellIcon id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id} />,
+ + +      title: <SpellLink id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id} />,
+ + +      result: <span>{formatPercentage(uptime)} % uptime on <SpellLink id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id}/>.</span>,
+ + +    };
+ + +  }
+ + +}
+ + +
+ + +export default Tier20_4pc;
