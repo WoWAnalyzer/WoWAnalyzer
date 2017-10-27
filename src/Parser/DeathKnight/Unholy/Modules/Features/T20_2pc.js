061f2e23da5c175e580b9ff3610d8d57009e1522
Maxweii <Maxweii@users.noreply.github.com>
2017-10-26 21:36:39 -0400

    Update T20_2pc.js

--- src/Parser/DeathKnight/Unholy/Modules/Features/T20_2pc.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/T20_2pc.js
@@ -14,16 +14,16 @@
  + +  };
  + +
  + +  on_initialized() {
- + +    this.active = this.combatants.selected.hasBuff(SPELLS.DEATH_KNIGHT_UNHOLY_T20_2P_BONUS.id);
+ + +    this.active = this.combatants.selected.hasBuff(SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id);
  + +  }
  + +
  + +  item() {
  + +    const uptime = this.combatants.selected.getBuffUptime(SPELLS.DEATH_KNIGHT_UNHOLY_T20_2P_BONUS.id) / this.owner.fightDuration;
  + +    return {
- + +      id: `spell-${SPELLS.DEATH_KNIGHT_UNHOLY_T20_4P_BONUS.id}`,
- + +      icon: <SpellIcon id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_2P_BONUS.id} />,
- + +      title: <SpellLink id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_2P_BONUS.id} />,
- + +      result: <span>{formatPercentage(uptime)} % uptime on <SpellLink id={SPELLS.DEATH_KNIGHT_UNHOLY_T20_2P_BONUS.id}/>.</span>,
+ + +      id: `spell-${SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id}`,
+ + +      icon: <SpellIcon id={SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id} />,
+ + +      title: <SpellLink id={SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id} />,
+ + +      result: <span>{formatPercentage(uptime)} % uptime on <SpellLink id={SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id}/>.</span>,
  + +    };
  + +  }
  + +}
