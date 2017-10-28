204ab42c689f657e5c60f8513a25fec66e7f3da5
Martijn Hols <martijnhols@hotmail.com>
2017-10-28 15:13:03 +0200

    Update explanation comment

--- src/Parser/Paladin/Holy/Modules/Features/StatWeights.js
+++ src/Parser/Paladin/Holy/Modules/Features/StatWeights.js
@@ -21,17 +21,17 @@ const INFUSION_OF_LIGHT_BUFF_EXPIRATION_BUFFER = 150; // the buff expiration can
 const INFUSION_OF_LIGHT_BUFF_MINIMAL_ACTIVE_TIME = 200; // if someone heals with FoL and then immediately casts a HS race conditions may occur. This prevents that (although the buff is probably not applied before the FoL).
 const INFUSION_OF_LIGHT_FOL_HEALING_INCREASE = 0.5;
 
-/*
+/**
  * Holy Paladin Stat Weights Methodology
  *
  * Approach -
- * This module generates the players stat weights using the actual logged events. We keep a listing of all the player's healing spells
+ * This module generates the players stat weights using the actual logged events. We made a list of all healing spells
  * along with which stats those spells scales with, and for each stat a heal scales with we do some math to find out
- * how much the last point of that stat contributed in healing. We compare the total healing increases
- * caused by seperately increasing each stat by one in order to generate weights.
+ * how much the last point of that stat contributed in healing. We compare the total healing contributions
+ * caused by each stat in order to generate weights.
  *
  * Overheal -
- * The toughest problem to face when generating healing stat weights is what to do about overhealing. Unfortunately, there is
+ * A problem to face when generating healing stat weights is what to do about overhealing. Unfortunately, there is
  * no one correct answer. A main issue is what I call The "Would've" Problem. We can say "an extra bit of healing here doesn't
  * matter because it *would've* just caused the next heal to overheal", but how far can we take this logic? My approach is that
  * it doesn't only matter how much is healed, but also how quickly. We'll count all healing that doesn't overheal even if we
@@ -45,22 +45,51 @@ const INFUSION_OF_LIGHT_FOL_HEALING_INCREASE = 0.5;
  * I think an advantage of disregarding all partial overheals is that it gives "top off" heals an effective lower weight than
  * "life saver" heals. Still, this is a decision I will revisit.
  *
+ * This applies for Intellect, Mastery, Versatility and Leech as they are stats that only increase the power (health) a heal restores.
+ * The other stats are special and behave differently as explained below.
+ *
  * Stat Tracking -
- * These calculations work best when they use the players actual stats at the moment of a heal. As such, accuracy will be
- * improved when a StatTracker module is implemented, rather than being forced to use the player's stats at the moment of pull.
- * For now, I'm handling the most common big Paladin stat buffs manually.
+ * These calculations work best when they use the players actual stats at the moment of a heal. This is being taken care of by the
+ * StatTracker module which adjusts a player's stats based on applied buffs. If all buffs are implemented properly, this has a very
+ * high accuracy.
  *
  * Intellect -
- * Math here is straightforward, as spells that scale with int scale directly and linearly with total int. Due to the 'all leather'
- * bonus, each point of int gained is multiplied by 1.05, which has to be taken into account. The increase in power from
- * one int is simply 1.05 / totalInt.
+ * Math here is straightforward, as spells that scale with int scale directly and linearly with total int. Due to the 'all plate'
+ * bonus, each point of int gained is increased by 5%, which has to be taken into account. Effectively the increase in power from
+ * one int is simply `1.05 / totalInt`.
+ *
+ * Because heals with overhealing are ignored, if every single heal had overhealed the value of Int would be 0. This makes sense as
+ * adding any health on top of the heal would only be adding to overhealing.
  *
  * Crit -
- * The general formula is critChanceFromOneRating * healAmount, but we have to make sure to account for anything that makes a
- * crit stronger than 2x (e.g. Drape of Shame), and we also have to account for if healAmount is already a crit. If the heal
- * is already a crit then we double count the weight, so we first have to normalize it to the amount it would have done
- * had it not crit. Special handling is needed for Holy Shock obviously. We assume that higher crit will result in proportionally
- * more Infusion of Lights, and so get the bonus to Infusion of Light as NYI.
+ * 1 more crit does not directly increase the power of a heal, instead it just increases the chance that heals that didn't crit,
+ * crit. Predicting the increased amount of crits from 1 crit rating would be impossible and averaging it our likely inaccurate.
+ * But this isn't a problem for our approach since we are calculating stats **not** based on *gaining 1 more rating* but based on the
+ * value of the *last 1 rating* of a stat. This means we don't need to predict anything and we only need to base our calculations on
+ * heals that actually crit during this log.
+ *
+ * If heals with overhealing were ignored, if every single heal had overhealed the value of Crit would be 0. This wouldn't make sense
+ * as crit gives a boost of 100% (+Drape of Shame and other bonuses) and if only 10% of that boost overhealed it still gave us 90%
+ * healing. This means we need to take into account heals that overheal when calculating the value of crit.
+ * The only time crit should be worth 0 is if we are crit **hard** capped. This means exceeding 100% crit chance.
+ *
+ * With this in mind imagine when you average out a fight this is the result with 33% crit chance (10,000 rating) total:
+ * ```
+ * type effective overheal   raw
+ * hit      1,000        0 1,000
+ * hit      1,000        0 1,000
+ * crit     1,500      500 2,000
+ * ```
+ * This approach would ignore the two hits completely and focus on the crit.
+ * The crit's base healing was `1,000`, the raw crit part `1,000` and `500` of the crit part was effective crit healing. `1` crit
+ * rating would be worth `500 / crit rating` if we didn't get a `8%` base crit chance. Doing `500 * (1 - (8% / 33%))` gives us the
+ * averaged out value of the crit **rating** alone; `378,79`, dividing this by the crit rating gets us the value per 1 rating;
+ * `378,79 / 10,000 = 0,0379` healing per rating. This gives us the healing value of the last 1 rating in this log.
+ *
+ * If we had 9% crit, the 500 effective healing gained from the rating would be just 55 HP after adjusting for base crit chance, which makes sense as only a fraction of the crit is gained from the rating.
+ *
+ * If at the heal-event the critical strike chance was more than 100% for that spell, it is completely ignored and not included in
+ * the weight of this spell. We take into account Holy Shock's double crit chance as well as crit buffs such as Avenging Wrath.
  *
  * Haste -
  * NYI
@@ -235,17 +264,6 @@ class StatWeights extends Analyzer {
     return { baseCritChance, ratingCritChance };
   }
   _criticalStrikeEffectiveHealing(event, healVal) {
-    // Imagine when you average out a fight this is the result with 33% crit chance (10,000 rating) total:
-    // ```
-    // type effective overheal   raw
-    // hit      1,000        0 1,000
-    // hit      1,000        0 1,000
-    // crit     1,500      500 2,000
-    // ```
-    // This approach would ignore the two hits completely and focus on the crit.
-    // The crit's base healing was `1,000`, the raw crit part `1,000` and `500` of the crit part was effective crit healing. `1` crit rating would be worth `500 / crit rating` if we didn't get a `8%` base crit chance. Doing `500 * (1 - (8% / 33%))` gives us the averaged out value of the crit **rating** alone; `378,79`, dividing this by the crit rating gets us the value per one stat; `378,79 / 10,000 = 0,0379` healing per rating.
-    // If we had 9% crit, the 500 effective healing gained from the rating would be just 55 HP after adjusting for base crit chance, which makes sense as only a fraction of the crit is gained from the rating.
-
     if (event.hitType === HIT_TYPES.CRIT) {
       // This collects the total effective healing contributed by the last 1 point of critical strike rating.
       // We don't make any predictions on normal hits based on crit chance since this would be guess work and we are a log analysis system so we prefer to only work with facts. Actual crit heals are undeniable facts, unlike speculating the chance a normal hit might have crit (and accounting for the potential overhealing of that).
