9b88241c017f0d06dc4e5c009d80087b79eb57d1
Martijn Hols <martijnhols@hotmail.com>
2017-11-01 21:21:06 +0100

    Fixed eslint warning

--- src/Main/ReportSelecter.js
+++ src/Main/ReportSelecter.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import PatreonButton from './PatreonButton';
-import GithubButton from './GithubButton';
 import DiscordButton from './DiscordButton';
 
 class ReportSelecter extends Component {
