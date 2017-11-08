8582e01ee11829144633062caf9fe04b3eda1668
Chris Grasty <chris.grasty@gmail.com>
2017-11-07 21:38:18 -0600

    Fixed links

--- src/Parser/Mage/Frost/CONFIG.js
+++ src/Parser/Mage/Frost/CONFIG.js
@@ -17,13 +17,10 @@ export default {
   description: (
     <div>
       Hello Everyone! We are always looking to improve the Frost Mage Analyzers and Modules; so if you find any issues or if there is something missing that you would like to see added, please open an Issue on GitHub or send a message to us on Discord (Sharrq#7530 or Sref#3865) <br /> <br />
-	    Additionally, if you need further assistance in improving your gameplay as a Frost Mage, you can refer to the following resources:<br /> <br />
-      Discord <br />
-      https://discord.gg/0gLMHikX2aZ23VdA <br /> <br />
-      Altered Time (Mage Forums/Guides) <br />
-      https://www.altered-time.com/forum/ <br /> <br />
-      Icy Veins (Class Guides) <br />
-      https://www.icy-veins.com/wow/frost-mage-pve-dps-guide <br />
+	    Additionally, if you need further assistance in improving your gameplay as a Frost Mage, you can refer to the following resources:<br />
+      <a href="https://discord.gg/0gLMHikX2aZ23VdA" target="_blank" rel="noopener noreferrer">Mage Class Discord</a> <br />
+      <a href="https://www.altered-time.com/forum/" target="_blank" rel="noopener noreferrer">Altered Time (Mage Forums/Guides)</a> <br />
+      <a href="https://www.icy-veins.com/wow/frost-mage-pve-dps-guide" target="_blank" rel="noopener noreferrer">Icy Veins (Frost Mage Guide)</a> <br/>
     </div>
   ),
   specDiscussionUrl: 'https://github.com/WoWAnalyzer/WoWAnalyzer/milestone/3',
