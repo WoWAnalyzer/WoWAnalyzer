bb5b4105d28db3ddef1d4853dd055fecd2372e9a
Putro <lafa16ac@student.cbs.dk>
2017-11-09 22:44:32 +0100

    initial support for T192p - DF bit broken

--- src/Parser/Hunter/BeastMastery/CombatLogParser.js
+++ src/Parser/Hunter/BeastMastery/CombatLogParser.js
@@ -7,9 +7,11 @@ import CooldownThroughputTracker from './Modules/Features/CooldownThroughputTrac
 import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
 
 //Items
+import Tier19_2p from './Modules/Items/Tier19_2p.js';
 import SoulOfTheHuntmaster from '../Shared/Modules/Items/SoulOfTheHuntmaster';
 import QaplaEredunWarOrder from "./Modules/Items/QaplaEredunWarOrder";
 import Tier20_2p from "./Modules/Items/Tier20_2p";
+import Tier20_4p from "./Modules/Items/Tier20_4p";
 
 //Spells
 import DireBeast from "./Modules/Spells/DireBeast/DireBeast";
@@ -23,7 +25,6 @@ import KillerCobra from "./Modules/Talents/KillerCobra";
 
 //Traits
 import TitansThunder from "./Modules/Traits/TitansThunder";
-import Tier20_4p from "./Modules/Items/Tier20_4p";
 
 class CombatLogParser extends CoreCombatLogParser {
   static specModules = {
@@ -44,6 +45,7 @@ class CombatLogParser extends CoreCombatLogParser {
     //Items
     soulOfTheHuntmaster: SoulOfTheHuntmaster,
     qaplaEredunWarOrder: QaplaEredunWarOrder,
+    tier19_2p: Tier19_2p,
     tier20_2p: Tier20_2p,
     tier20_4p: Tier20_4p,
 
