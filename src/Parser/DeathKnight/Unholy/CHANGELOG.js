44394b218da6a02fb1c1626fb9d6ddf0d1c71376
jjs451 <jjs451@msstate.edu>
2017-11-09 13:51:20 -0600

    fixed bugs

--- src/Parser/DeathKnight/Unholy/CHANGELOG.js
+++ src/Parser/DeathKnight/Unholy/CHANGELOG.js
@@ -1,22 +1,22 @@
 export default  [
   {
-    date: '11-04-2017',
+    date: new Date('11-04-2017'),
     changes: 'Scourge of Worlds, Cold Heart, and Unholy Frenzy trackers added',
-    contributors: 'Khazak',
+    contributors: ['Khazak'],
   },
   {
-    date: '11-02-2017',
+    date: new Date('11-02-2017'),
     changes: ' cast efficiency fixes',
-    contributors: 'Khazak',
+    contributors: ['Khazak'],
   },
   {
-    date: '11-01-2017',
+    date: new Date('11-01-2017'),
     changes: 'Added Dark Arbiter Module',
-    contributors: 'Khazak',
+    contributors: ['Khazak'],
   },
   {
-    date: '10-22-2017',
+    date: new Date('10-22-2017'),
     changes: 'Added initial Unholy support by @Khazak',
-    contributors: 'Khazak',
+    contributors: ['Khazak'],
   },
 ];
