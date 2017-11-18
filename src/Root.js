a4af5f3328e70cbf90f9c3f677b57b20868cdc01
Martijn Hols <martijnhols@hotmail.com>
2017-11-18 17:50:43 +0100

    Fix test

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
@@ -17,10 +18,8 @@ const middleware = routerMiddleware(history);
 
 const store = createStore(
   reducers,
-  compose(
-    applyMiddleware(thunk),
-    applyMiddleware(middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(thunk, middleware)
   )
 );
 
