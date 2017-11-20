0477f7f404474feef4d93a33845f24d4cb08548d
Putro <lafa16ac@student.cbs.dk>
2017-11-20 20:45:27 +0100

    made prepull events and added an uptime suggestion

--- src/Parser/Hunter/Marksmanship/Modules/Spells/Trueshot.js
+++ src/Parser/Hunter/Marksmanship/Modules/Spells/Trueshot.js
@@ -25,13 +25,28 @@ class Trueshot extends Analyzer {
   totalCastsPrTS = 0;
   executeTrueshots = 0;
   startFocusForCombatant = 0;
+  prepullTrueshots = 0;
+
+  on_byPlayer_applybuff(event) {
+    const buffId = event.ability.guid;
+    if (buffId !== SPELLS.TRUESHOT.id && !event.prepull) {
+      return;
+    }
+    //ensures we only get 1 of these events
+    if (this.trueshotCasts === 0) {
+      //adds 1 to trueshotCasts to properly show that it was cast prepull
+      this.trueshotCasts += 1;
+      this.prepullTrueshots += 1;
+    }
+  }
 
   on_byPlayer_cast(event) {
-    //checks for the combatants start focus and assigns it once - useful if the combatant has precast trueshot
-    if (this.startFocusForCombatant === 0) {
+//checks if we had a prepull trueshot, in which case the firstCast done symbolises our starting focus of that one trueshot
+    if (this.prepullTrueshots > 0 && this.startFocusForCombatant === 0) {
       event.classResources.forEach(classResource => {
         if (classResource.type === RESOURCE_TYPES.FOCUS && classResource['amount'] > this.startFocusForCombatant) {
           this.startFocusForCombatant += classResource['amount'];
+          this.accumulatedFocusAtTSCast += this.startFocusForCombatant;
         }
       });
     }
@@ -56,7 +71,6 @@ class Trueshot extends Analyzer {
     if (!this.combatants.selected.hasBuff(SPELLS.TRUESHOT.id, event.timestamp)) {
       return;
     }
-    //ensures that if the player started
     this.totalCastsPrTS += 1;
     if (isCrit) {
       this.totalCritsInTS += 1;
@@ -76,6 +90,7 @@ class Trueshot extends Analyzer {
     const averageFocusAtTS = formatNumber(this.accumulatedFocusAtTSCast / this.trueshotCasts);
     const percentAimedCrits = formatPercentage(this.aimedCritsInTS / this.aimedShotsPrTS);
     const percentCastCrits = formatPercentage(this.totalCritsInTS / this.totalCastsPrTS);
+    const uptimePrCast = ((this.combatants.getBuffUptime(SPELLS.TRUESHOT.id) / this.trueshotCasts) / 1000).toFixed(2);
 
     return (
       <StatisticBox icon={<SpellIcon id={SPELLS.TRUESHOT.id} />}
@@ -90,7 +105,7 @@ class Trueshot extends Analyzer {
               }}
             />
             {'  '}
-            {averageFocusAtTS > 0 ? averageFocusAtTS : this.startFocusForCombatant}{' '}
+            {averageFocusAtTS}{' '}
             <Icon
               icon='ability_hunter_focusfire'
               alt='Average Focus At Trueshot Cast'
@@ -102,13 +117,14 @@ class Trueshot extends Analyzer {
           </span>
         )}
         label={'Trueshot info'}
-        tooltip={`Information regarding your average Trueshot window: <ul> <li>You started your Trueshot windows with an average of ${averageFocusAtTS > 0 ? averageFocusAtTS : this.startFocusForCombatant} focus.</li> <li> You hit an average of ${averageAimedCasts} Aimed Shots inside each Trueshot window. </li> <li> Your Trueshot Aimed Shots had a crit rate of ${percentAimedCrits}%. </li> <li>Your overall crit rate during Trueshot was ${percentCastCrits}%. </li></ul>`} />
+        tooltip={`Information regarding your average Trueshot window: <ul> <li>You started your Trueshot windows with an average of ${averageFocusAtTS} focus.</li> <li> You hit an average of ${averageAimedCasts} Aimed Shots inside each Trueshot window. </li> <li> Your Trueshot Aimed Shots had a crit rate of ${percentAimedCrits}%. </li> <li>Your overall crit rate during Trueshot was ${percentCastCrits}%. </li> <li>You spent an average of ${uptimePrCast} seconds in trueshot pr cast of Trueshot.</li></ul>`} />
     );
   }
 
   suggestions(when) {
     const averageAimedCasts = formatNumber(this.aimedShotsPrTS / this.trueshotCasts);
     const averageFocusAtTS = formatNumber(this.accumulatedFocusAtTSCast / this.trueshotCasts);
+    const uptimePrCast = ((this.combatants.getBuffUptime(SPELLS.TRUESHOT.id) / this.trueshotCasts) / 1000).toFixed(2);
     when(averageAimedCasts).isLessThan(8)
       .addSuggestion((suggest, actual, recommended) => {
         return suggest(<span>You only cast {averageAimedCasts} <SpellLink id={SPELLS.AIMED_SHOT.id} />s inside your average <SpellLink id={SPELLS.TRUESHOT.id} /> window. This is your only DPS cooldown, and it's important to maximize it to it's fullest potential by getting as many Aimed Shot squeezed in as possible, while still making sure that they are all within <SpellLink id={SPELLS.VULNERABLE.id} />. <br /> This can be done by making sure to use <SpellLink id={SPELLS.WINDBURST.id} /> to open <SpellLink id={SPELLS.VULNERABLE.id} /> windows, not using <SpellLink id={TALENTS.A_MURDER_OF_CROWS_TALENT_SHARED.id} /> while in <SpellLink id={SPELLS.TRUESHOT.id} /> or starting <SpellLink id={SPELLS.TRUESHOT.id} /> at higher focus. </span>)
@@ -119,7 +135,7 @@ class Trueshot extends Analyzer {
           .regular(recommended - 0.9)
           .major(recommended - 1.9);
       });
-    when(averageFocusAtTS > 0 ? averageFocusAtTS : this.startFocusForCombatant).isLessThan(90)
+    when(averageFocusAtTS).isLessThan(90)
       .addSuggestion((suggest, actual, recommended) => {
         return suggest(<span>You started your average <SpellLink id={SPELLS.TRUESHOT.id} /> at {averageFocusAtTS} focus, try and pool a bit more before casting <SpellLink id={SPELLS.TRUESHOT.id} />. This can be done through casting an additional <SpellLink id={SPELLS.ARCANE_SHOT.id} /> or by monitoring the cooldown of <SpellLink id={SPELLS.TRUESHOT.id} /> and adjusting play to ensure your focus won't be depleted when it comes off cooldown.</span>)
           .icon(SPELLS.TRUESHOT.icon)
@@ -135,6 +151,15 @@ class Trueshot extends Analyzer {
           .actual(`You had ${this.executeTrueshots} trueshot casts during Bullseye`)
           .recommended(`casting atleast 1 trueshot in execute is recommended`)
           .major(recommended);
+      });
+    when(uptimePrCast).isLessThan(15)
+      .addSuggestion((suggest, actual, recommended) => {
+      return suggest(<span>You should make sure to utilise every possible second of <SpellLink id={SPELLS.TRUESHOT.id}/> uptime as you can. Remember to cast it atleast 15 seconds before the boss dies, so you don't lose out on valuable time, aswell as remember to not cast it until the boss has been engaged.</span>)
+        .icon(SPELLS.TRUESHOT.icon)
+        .actual(`You had an average of ${uptimePrCast} seconds of trueshot uptime per cast`)
+        .recommended(`${recommended} seconds uptime per cast is recommended`)
+        .regular(recommended-0.1)
+        .major(recommended-0.5);
       });
   }
   statisticOrder = STATISTIC_ORDER.CORE(8);
