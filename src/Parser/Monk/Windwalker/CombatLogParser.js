fac6623084b30550e577a60c9e64f5654bf2b4e9
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2017-11-22 10:32:07 +0100

    [Windwalker] Imported to parser and updated changelog

--- src/Parser/Monk/Windwalker/CombatLogParser.js
+++ src/Parser/Monk/Windwalker/CombatLogParser.js
@@ -16,11 +16,12 @@ import FistsofFury from './Modules/Spells/FistsofFury';
 import HitCombo from './Modules/Talents/HitCombo';
 import EnergizingElixir from './Modules/Talents/EnergizingElixir';
 
-// Legendaries
+// Legendaries / Items
 import KatsuosEclipse from './Modules/Items/KatsuosEclipse';
 import CenedrilReflectorOfHatred from './Modules/Items/CenedrilReflectorOfHatred';
 import SoulOfTheGrandmaster from './Modules/Items/SoulOfTheGrandmaster';
 import TheEmperorsCapacitor from './Modules/Items/TheEmperorsCapacitor';
+import T21_2set from './Modules/Items/T21_2set';
 
 class CombatLogParser extends CoreCombatLogParser {
   static specModules = {
@@ -45,6 +46,7 @@ class CombatLogParser extends CoreCombatLogParser {
     cenedrilReflectorOfHatred: CenedrilReflectorOfHatred,
     soulOfTheGrandmaster: SoulOfTheGrandmaster,
     theEmperorsCapacitor: TheEmperorsCapacitor,
+    T21_2set: T21_2set,
   };
 }
 
