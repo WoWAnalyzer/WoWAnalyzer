711469ae3c6ffc2de940cd63e48d4deed22a9d4c
Hans Ripa <hans.ripa@gmail.com>
2017-11-25 13:01:30 +0100

    With changelog.

--- src/Parser/Priest/Shadow/CHANGELOG.js
+++ src/Parser/Priest/Shadow/CHANGELOG.js
@@ -1,4 +1,5 @@
 export default `
+25-11-2017 - Fixed damage increase modifier of Heart of the Void & cooldown on Shadow crash due to class changes.
 15-11-2017 - Added The Twins Painful Touch (Legendary), Anunds Seared Shackles (Legendary), Heart of the Void (Legendary) & Zenkaram Iridis (Legendary).
 04-11-2017 - Fixed ABC downtime.
 29-08-2017 - Added Insanity Resource on the Voidform graphs. Fixed pet damage. (by hassebewlen)
