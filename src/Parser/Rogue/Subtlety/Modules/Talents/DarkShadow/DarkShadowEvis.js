2378493622a03c24f9927efca80d376b2a21832f
Nikita Snitnikov <snitnik@gmail.com>
2017-11-26 13:52:44 +0100

    DarkShadowEvis suggestion

--- src/Parser/Rogue/Subtlety/Modules/Talents/DarkShadow/DarkShadowEvis.js
+++ src/Parser/Rogue/Subtlety/Modules/Talents/DarkShadow/DarkShadowEvis.js
@@ -3,22 +3,39 @@ import React from 'react';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
-import DarkShadow from './DarkShadow';
+import SpellLink from 'common/SpellLink'; 
+import Wrapper from 'common/Wrapper';
 
+import DarkShadow from './DarkShadow';
 
 class DarkShadowEvis extends DarkShadow {
 
+  
+  suggestions(when) {
+    const totalEviscerateHitsInShadowDance  = this.danceDamageTracker.getAbility(SPELLS.EVISCERATE.id).damageHits;
+    const danceEvis = totalEviscerateHitsInShadowDance / this.totalShadowDanceCast;
+    when(danceEvis).isLessThan(1.75)
+    .addSuggestion((suggest, actual, recommended) => {
+      return suggest(<Wrapper>Try to cast more <SpellLink id={SPELLS.EVISCERATE.id} /> during <SpellLink id={SPELLS.SHADOW_DANCE.id} /> when you are using <SpellLink id={SPELLS.DARK_SHADOW_TALENT.id} />. </Wrapper>)
+        .icon(SPELLS.EVISCERATE.icon)
+        .actual(`You cast an average of ${actual.toFixed(2)} Eviscerates during Shadow Dance. This number includes Eviscerates cast from Death from Above.`)
+        .recommended(`>${recommended} is recommended`)
+        .regular(1.5)
+        .major(1.25);
+    });
+  }
+
   statistic() {    
   const totalEviscerateHitsInShadowDance  = this.danceDamageTracker.getAbility(SPELLS.EVISCERATE.id).damageHits;
-    const danceEvis = totalEviscerateHitsInShadowDance / this.totalShadowDanceCast;
-    return (
-        <StatisticBox
-          icon={<SpellIcon id={SPELLS.EVISCERATE.id} />}
-          value={`${danceEvis.toFixed(2)}`}
-          label="Average Eviscerates in Shadow Dance"
-          tooltip={`Your average Eviscerate casts per Shadow Dance. Your cast ${totalEviscerateHitsInShadowDance} Eviscerates in ${this.totalShadowDanceCast} Shadow Dances. This number includes Eviscerates cast from Death from Above. Subtlety rogue should cast as many as possible (usually 2 times) Eviscerates in a Shadow Dance to get benefit from 30% damage increasing of Dark Shadow talent.`}
-          />
-    );
+  const danceEvis = totalEviscerateHitsInShadowDance / this.totalShadowDanceCast;
+  return (
+      <StatisticBox
+        icon={<SpellIcon id={SPELLS.EVISCERATE.id} />}
+        value={`${danceEvis.toFixed(2)}`}
+        label="Average Eviscerates in Shadow Dance"
+        tooltip={`Your average Eviscerate casts per Shadow Dance. You cast ${totalEviscerateHitsInShadowDance} Eviscerates in ${this.totalShadowDanceCast} Shadow Dances. This number includes Eviscerates cast from Death from Above.`}
+        />
+  );
   }
 
   statisticOrder = STATISTIC_ORDER.OPTIONAL(1);
