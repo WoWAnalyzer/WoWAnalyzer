950de4ed8678919404ace76609ef8a479cd2b7ef
Snitnikov Nikita <snitnik@gmail.com>
2017-11-28 23:55:57 +0100

    Dfa combo - start work

---
+++ src/Parser/Rogue/Subtlety/Modules/Talents/DFA/DeathFormAbove.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Analyzer from 'Parser/Core/Analyzer';
+import SPELLS from 'common/SPELLS';
+import Combatants from "Parser/Core/Modules/Combatants";
+import Wrapper from 'common/Wrapper';
+import SpellLink from 'common/SpellLink'; 
+
+//DfA needs to be used with both Symbols of Death and Shadow Dance (Dark Shadow).
+class DeathFormAbove extends Analyzer {
+  static dependencies = {
+    combatants: Combatants,
+  };
+
+  hasDarkShadow = false;
+  dfaTimestamp = 0;
+  dfaMaxWindow = 3000;
+
+  failedCombos = 0;
+  
+  on_initialized() {
+    this.active = this.owner.modules.combatants.selected.hasTalent(SPELLS.DEATH_FROM_ABOVE_TALENT.id);
+    this.hasDarkShadow = this.active = this.owner.modules.combatants.selected.hasTalent(SPELLS.DARK_SHADOW_TALENT.id);
+  }
+  
+  
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+
+    //Find DfA, and save the timestamp. 
+    if (spellId === SPELLS.DEATH_FROM_ABOVE_TALENT.id) {
+        this.dfaTimestamp = event.timestamp;
+    }
+  }
+
+  on_byPlayer_damage(event) {
+    //Skip non dfa events
+    if(event.timestamp > this.dfaTimestamp + this.dfaMaxWindow) return;
+
+    //Find Eviscerate inside DFA
+    const spellId = event.ability.guid;
+    if(spellId !== SPELLS.EVISCERATE.id) return;
+
+    //If the Dfa Eviscerate hits without Symbols or Dark Shadow DFA - we have a problem!
+    if (!this.combatants.selected.hasBuff(SPELLS.SYMBOLS_OF_DEATH.id) 
+    || ( this.hasDarkShadow && !this.combatants.selected.hasBuff(SPELLS.SYMBOLS_OF_DEATH.id) )) {
+      this.failedCombos += 1;    
+    }   
+  }
+
+  
+  suggestions(when) {
+    when(this.failedCombos).isGreaterThan(0)
+      .addSuggestion((suggest, actual, recommended) => {
+        return suggest(<Wrapper>          
+          <SpellLink id={SPELLS.DEATH_FROM_ABOVE_TALENT.id} />. <br /> 
+           while in <SpellLink id={SPELLS.DEATH_FROM_ABOVE_TALENT.id} /> 
+           or starting <SpellLink id={SPELLS.DEATH_FROM_ABOVE_TALENT.id} />  </Wrapper>)
+          .icon(SPELLS.DEATH_FROM_ABOVE_TALENT.icon)
+          .actual(`${actual} Death from Above combos failed.`)
+          .recommended(`${recommended} is recommended`)
+          //Core rotation, should never happen.
+          .major(0.1);
+      });
+    }
+}
+
+export default DeathFormAbove;
