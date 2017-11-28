11ffa83b54bea6ddddc7a4711030ce072e73a438
Martijn Hols <martijnhols@hotmail.com>
2017-11-28 21:18:34 +0100

    Remove obsolete message

--- src/Main/PlayerSelecter.js
+++ src/Main/PlayerSelecter.js
@@ -7,9 +7,6 @@ import ReactTooltip from 'react-tooltip';
 import { getReport } from 'selectors/report';
 import { getCombatants } from 'selectors/combatants';
 
-import DiscordButton from './DiscordButton';
-import PatreonButton from './PatreonButton';
-import GithubButton from './GithubButton';
 import PlayerSelectionList from './PlayerSelectionList';
 
 class PlayerSelecter extends Component {
@@ -60,21 +57,6 @@ class PlayerSelecter extends Component {
           </div>
           <div className="panel-body" style={{ padding: 0 }}>
             <PlayerSelectionList />
-          </div>
-        </div>
-
-        <div className="panel">
-          <div className="panel-body">
-            <div className="flex">
-              <div className="flex-main" style={{ paddingRight: 10 }}>
-                If you're not in the list your spec may not be supported yet. Specs are added by enthusiastic players of the spec themselves. Adding specs is easy if you're familiar with JavaScript, find out more on <a href="https://github.com/WoWAnalyzer/WoWAnalyzer/blob/master/CONTRIBUTING.md">GitHub</a> or <a href="https://discord.gg/AxphPxU" target="_blank" rel="noopener noreferrer">join the WoW Analyzer Discord</a> for additional help.
-              </div>
-              <div className="flex-sub hidden-xs">
-                <DiscordButton style={{ marginLeft: 20 }} />
-                <PatreonButton style={{ marginLeft: 20 }} />
-                <GithubButton style={{ marginLeft: 20 }} text="Add your spec" href="https://github.com/WoWAnalyzer/WoWAnalyzer/blob/master/CONTRIBUTING.md" />
-              </div>
-            </div>
           </div>
         </div>
       </div>
