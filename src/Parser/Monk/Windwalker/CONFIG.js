039607d8655fbf0498ac41aaf107cdeec27fe7ae
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2017-11-28 08:41:25 +0100

    another quick edit

--- src/Parser/Monk/Windwalker/CONFIG.js
+++ src/Parser/Monk/Windwalker/CONFIG.js
@@ -12,7 +12,7 @@ export default {
   <div>
       Hello! We have been working hard to make the Windwalker analyzer good, but there is still things left to improve.  We hope that the suggestions and statistics will be helpful in improving your overall performance. Try and focus on improving only a few things at a time, until those become ingrained in your muscle memory so as to not be concentrating on many different things. <br /> <br/>
   
-    If you have any questions about the analyzer or Windwalker monks in general, join us in the <a href="https://discord.gg/0dkfBMAxzTkWj21F" target="_blank" rel="noopener noreferrer">Peak of Serenity</a> discord server for any questions or feedback. You can reach me there as Juko8. Make sure to also check out our resources on Peakofserenity.com as well, it has pretty much everything you need to know. 
+    If you have any questions about the analyzer or Windwalker monks in general, join us in the <a href="https://discord.gg/0dkfBMAxzTkWj21F" target="_blank" rel="noopener noreferrer">Peak of Serenity</a> discord server and talk to us. You can reach me there as Juko8. Make sure to also check out our resources on Peakofserenity.com as well, it has pretty much everything you need to know. 
   </div>
   ),
   completeness: SPEC_ANALYSIS_COMPLETENESS.GOOD, // When changing this please make a PR with ONLY this value changed, we will do a review of your analysis to find out of it is complete enough.
