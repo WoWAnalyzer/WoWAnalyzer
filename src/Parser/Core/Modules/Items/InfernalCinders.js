9a12caf74bb491fc7139e5dd502c23b8a5fce983
Kelton Finch <keltonfinch@gmail.com>
2017-12-03 18:29:44 -0500

    Removed superfluous tooltip from InfernalCinders

--- src/Parser/Core/Modules/Items/InfernalCinders.js
+++ src/Parser/Core/Modules/Items/InfernalCinders.js
@@ -6,11 +6,16 @@ import { formatNumber } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
+/*
+ * Infernal Cinders -
+ * Equip: Your melee attacks have a chance to deal an additional 82910 Fire damage. The critical strike chance of this damage is increased by 10% for each ally within 10 yds who bears this item.
+ */
 class InfernalCinders extends Analyzer {
   static dependencies = {
     combatants: Combatants,
   };
-  damageIncreased = 0;
+
+  damage = 0;
 
   on_initialized() {
     this.active = this.combatants.selected.hasTrinket(ITEMS.INFERNAL_CINDERS.id);
@@ -20,22 +25,14 @@ class InfernalCinders extends Analyzer {
     const spellId = event.ability.guid;
 
     if (spellId === SPELLS.INFERNAL_CINDERS.id) {
-      this.damageIncreased += event.amount;
+      this.damage += event.amount;
     }
   }
 
   item() {
     return {
       item: ITEMS.INFERNAL_CINDERS,
-      result: (
-        <dfn
-          data-tip={`The effective damage contributed by Infernal Cinders.<br/>
-            Damage: ${this.owner.formatItemDamageDone(this.damageIncreased)}<br/>
-            Total Damage: ${formatNumber(this.damageIncreased)}`}
-        >
-          {this.owner.formatItemDamageDone(this.damageIncreased)}
-        </dfn>
-      ),
+      result: this.owner.formatItemDamageDone(this.damage),
     };
   }
 }
