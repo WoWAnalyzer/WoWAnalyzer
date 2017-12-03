d36bb033f96a2adf4c3564e3b719a6be5e281761
Kelton Finch <keltonfinch@gmail.com>
2017-12-02 21:08:07 -0500

    [core] Completly reworked DGD. Added formatTimestamp utility

--- src/Parser/Core/Modules/Items/DeceiversGrandDesign.js
+++ src/Parser/Core/Modules/Items/DeceiversGrandDesign.js
@@ -4,17 +4,23 @@ import Wrapper from 'common/Wrapper';
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
 import ItemLink from 'common/ItemLink';
-import { formatDuration } from 'common/format';
+import { formatDuration, formatPercentage } from 'common/format';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
-const debug = false;
+const debug = true;
+
 // time before and after shield proc to show user
 const PROC_EVENT_START_BUFFER = 5000;
 const PROC_EVENT_END_BUFFER = 1000;
 
 const HOT_DURATION = 120000;
+
+// percentage of total duration for suggestion
+const MINOR = 0.80;
+const AVERAGE = 0.60;
+const MAJOR = 0.30;
 
 class DecieversGrandDesign extends Analyzer {
   static dependencies = {
@@ -24,13 +30,7 @@ class DecieversGrandDesign extends Analyzer {
   healingHot = 0;
   healingAbsorb = 0;
 
-  targetOne = null;
-  targetOneCastTimestamp = null;
-
-  targetTwo = null;
-  targetTwoCastTimestamp = null;
-
-  procs = [];
+  casts = [];
 
   on_initialized() {
     this.active = this.combatants.selected.hasTrinket(ITEMS.DECEIVERS_GRAND_DESIGN.id);
@@ -38,125 +38,100 @@ class DecieversGrandDesign extends Analyzer {
 
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
-
     if (spellId === SPELLS.GUIDING_HAND.id) {
       this.healingHot += (event.amount || 0) + (event.absorbed || 0);
-
-      // Account for precasting
-      if (this.targetOne === event.targetID || this.targetTwo === event.targetID) {
-
-      } else if (!this.targetOne) {
-        this.targetOne = event.targetID;
-      } else {
-        this.targetTwo = event.targetID;
-      }
     }
   }
 
   on_byPlayer_absorbed(event) {
     const spellId = event.ability.guid;
-
     if (spellId === SPELLS.FRUITFUL_MACHINATIONS.id) {
       this.healingAbsorb += (event.amount || 0) + (event.absorbed || 0);
     }
   }
 
-  on_byPlayer_cast(event) {
+  on_byPlayer_applybuff(event) {
     const spellId = event.ability.guid;
-
     if (spellId === SPELLS.GUIDING_HAND.id) {
-      if (this.targetOne === null) {
-        this.targetOne = event.targetID;
-        this.targetOneCastTimestamp = event.timestamp;
-        debug && console.log(`Target One: ${this.targetOne}`);
-      } else if (this.targetTwo === null) {
-        this.targetTwo = event.targetID;
-        this.targetTwoCastTimestamp = event.timestamp;
-        debug && console.log(`Target Two: ${this.targetTwo}`);
+      debug && console.log("Guiding Hand applied to " + event.targetID +" @ " + this.owner.formatTimestamp(event.timestamp));
+
+      const target = event.targetID;
+      let targetName = '';
+      if (!this.combatants.players[target]) {
+        targetName = 'Pet';
       } else {
-        debug && console.log('Logic Error?!');
+        targetName = this.combatants.players[target].name;
       }
+      this.casts.push({
+        target,
+        targetName,
+        applied: event.timestamp,
+        active: true,
+        shieldProc: false,
+      });
+
+    } else if (spellId === SPELLS.FRUITFUL_MACHINATIONS.id) {
+      debug && console.log("Fruitful Machinations proc on " + event.targetID +" @ " + this.owner.formatTimestamp(event.timestamp));
+      const lastCastOnTarget = this.casts.filter(cast => cast.target === event.targetID).pop();
+      if (!lastCastOnTarget) {
+        console.warn("No Guiding Hand application logged for targetID " + event.targetID);
+        return;
+      }
+      lastCastOnTarget.shieldProc = true;
     }
   }
 
-  on_byPlayer_applybuff(event) {
+  on_byPlayer_removebuff(event) {
     const spellId = event.ability.guid;
-    if (spellId !== SPELLS.FRUITFUL_MACHINATIONS.id) {
+    if (spellId !== SPELLS.GUIDING_HAND.id) {
       return;
     }
 
-    const targetId = event.targetID;
-    const startTime = event.timestamp - PROC_EVENT_START_BUFFER;
-    const endTime = event.timestamp + PROC_EVENT_END_BUFFER;
-    const timeSinceStart = event.timestamp - this.owner.fight.start_time;
-    let timeSinceCast;
-    if (targetId === this.targetOne) {
-      this.targetOne = null;
-      timeSinceCast = event.timestamp - this.targetOneCastTimestamp;
-      debug && console.log('Proc on Target one:', targetId, ' @ timestamp: ', event.timestamp);
-    } else if (targetId === this.targetTwo) {
-      this.targetTwo = null;
-      timeSinceCast = event.timestamp - this.targetTwoCastTimestamp;
-      debug && console.log('Proc on Target two:', targetId, ' @ timestamp: ', event.timestamp);
-    }
-
-    if (debug) {
-      this.procs.push({
-        name: 'Test User',
-        report: this.owner.report.code,
-        fight: this.owner.fight.id,
-        target: targetId,
-        timeSinceStart,
-        timeSinceCast,
-        start: startTime,
-        end: endTime,
-      });
-    }
-
-    // store info about shield procs for later display
-    let name = '';
-    if (!this.combatants.players[targetId]) {
-      name = 'Pet';
-    } else {
-      name = this.combatants.players[targetId].name;
-    }
-
-    this.procs.push({
-      name,
-      report: this.owner.report.code,
-      fight: this.owner.fight.id,
-      target: targetId,
-      timeSinceStart,
-      timeSinceCast,
-      start: startTime,
-      end: endTime,
-    });
-    debug && console.log(this.procs);
-    debug && console.log(`https://www.warcraftlogs.com/reports/${this.owner.report.code}/#fight=${this.owner.fight.id}&source=${this.procs[0].target}&type=summary&start=${this.procs[0].start}&end=${this.procs[0].end}&view=events`);
-  }
-
-  on_finished() {
-    if (debug) {
-      console.log('Proc Checks: ', this.procs);
-      console.log(`Healing HoT: ${this.healingHot}`);
-      console.log(`Absorbed: ${this.healingAbsorb}`);
-      console.log(`Report Code: ${this.owner.report.code}`);
-    }
+    debug && console.log("Guiding Hand removed from " + event.targetID +" @ " + this.owner.formatTimestamp(event.timestamp));
+    const cast = this.casts.find(cast => cast.active && cast.target === event.targetID);
+    cast.active = false;
+    cast.removed = event.timestamp;
+    cast.duration = cast.removed - cast.applied;
   }
 
   get totalHealing() {
     return this.healingHot + this.healingAbsorb;
   }
 
+  get averageDuration() { // average doesn't include HoTs still active on fight end, null if there are no completed HoTs
+    const totalDuration = this.casts.filter(cast => !cast.active).reduce((acc, cast) => acc + cast.duration, 0);
+    const totalCompleted = this.casts.filter(cast => !cast.active).length;
+    return !totalCompleted ? null : totalDuration / totalCompleted;
+  }
+
   item() {
+    const avgDurationDisplay = !this.averageDuration ? `N/A` : `${(this.averageDuration / 1000).toFixed(0)}s`;
     return {
       item: ITEMS.DECEIVERS_GRAND_DESIGN,
       result: (
         <dfn data-tip={`Healing breakdown:
-          <ul>
-            <li>HoT: <b>${this.owner.formatItemHealingDone(this.healingHot)}</b></li>
-            <li>Shield Proc: <b>${this.owner.formatItemHealingDone(this.healingAbsorb)}</b></li>
-          </ul>
+        <ul>
+          <li>HoT: <b>${this.owner.formatItemHealingDone(this.healingHot)}</b></li>
+          <li>Shield Proc: <b>${this.owner.formatItemHealingDone(this.healingAbsorb)}</b></li>
+        </ul>
+        Casts: <b>${this.casts.length}</b><br>
+        Average Duration: <b>${avgDurationDisplay}</b><br>
+        Cast Detail:
+        <ul>
+          ${this.casts.reduce((arr, cast) => {
+            let castResult = ` -> `;
+            if (!cast.active) {
+              if (cast.shieldProc) {
+                castResult += `procced after ${(cast.duration / 1000).toFixed(0)}s`;
+              } else {
+                castResult += `expired naturally`;
+              }
+            } else {
+              castResult += `fight ended`;
+            }
+            return arr + `<li>on ${cast.targetName} @${this.owner.formatTimestamp(cast.applied)}${castResult}</li>`
+          }, '')}
+        </ul>
         `}>
           {this.owner.formatItemHealingDone(this.totalHealing)}
         </dfn>
@@ -165,24 +140,33 @@ class DecieversGrandDesign extends Analyzer {
   }
 
   suggestions(when) {
-    when(this.procs.length).isGreaterThan(0)
-      .addSuggestion((suggest) => {
+    const report = this.owner.report.code;
+    const fight = this.owner.fight.id;
+    const averagePercent = this.averageDuration / HOT_DURATION;
+    when(averagePercent).isLessThan(MINOR)
+      .addSuggestion((suggest, actual, recommended) => {
         return suggest(
           <Wrapper>
-            Your <ItemLink id={ITEMS.DECEIVERS_GRAND_DESIGN.id} /> procced earlier than expected. Try to cast it on players without spiky health pools. The following events procced the effect:<br />
-            {this.procs.map((proc, index) => {
-              const url = `https://www.warcraftlogs.com/reports/${proc.report}/#fight=${proc.fight}&source=${proc.target}&type=summary&start=${proc.start}&end=${proc.end}&view=events`;
-              const secondsLeftOnHot = ((HOT_DURATION - proc.timeSinceCast) / 1000).toFixed(0);
+            Your <ItemLink id={ITEMS.DECEIVERS_GRAND_DESIGN.id} /> was proccing early. Try to cast it on players without spiky health pools. The following events procced the shield:<br />
+            {this.casts.map((cast, index) => {
+              if (!cast.shieldProc) {
+                return;
+              }
+              const reportWindowStart = cast.removed - PROC_EVENT_START_BUFFER;
+              const reportWindowEnd = cast.removed + PROC_EVENT_END_BUFFER;
+              const url = `https://www.warcraftlogs.com/reports/${report}/#fight=${fight}&source=${cast.target}&type=summary&start=${reportWindowStart}&end=${reportWindowEnd}&view=events`;
               return (
                 <div key={index}>
-                  Proc {index + 1}: <a href={url} target="_blank" rel="noopener noreferrer">{proc.name} @{formatDuration(proc.timeSinceStart / 1000)} with {secondsLeftOnHot}s remaining on HoT</a>
+                  Cast {index + 1} procced on <a href={url} target="_blank" rel="noopener noreferrer">{cast.targetName} @ {this.owner.formatTimestamp(cast.removed)}</a> after {(cast.duration / 1000).toFixed(0)}s
                 </div>
               );
             })}
           </Wrapper>
         )
         .icon(ITEMS.DECEIVERS_GRAND_DESIGN.icon)
-        .regular(2).major(4);
+        .actual(`HoT averaged ${formatPercentage(averagePercent, 0)}% of max duration.`)
+        .recommended(`${formatPercentage(recommended, 0)}% is recommended`)
+        .regular(AVERAGE).major(MAJOR);
       });
   }
 }
