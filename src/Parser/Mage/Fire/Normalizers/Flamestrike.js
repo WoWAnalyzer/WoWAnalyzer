4eb0a15ec8be810f34ead6eac56706b97a5f376b
Chris Grasty <chris.grasty@gmail.com>
2017-12-02 21:14:18 -0600

    removed divine purpose comments

--- src/Parser/Mage/Fire/Normalizers/Flamestrike.js
+++ src/Parser/Mage/Fire/Normalizers/Flamestrike.js
@@ -4,7 +4,6 @@ import EventsNormalizer from 'Parser/Core/EventsNormalizer';
 
 class Flamestrike extends EventsNormalizer {
   /**
-   * Divine Purpose procs sometimes are logged before the related cast. This makes dealing with it harder, so reorder it.
    * @param {Array} events
    * @returns {Array}
    */
@@ -16,10 +15,9 @@ class Flamestrike extends EventsNormalizer {
       if (event.type === 'cast' && event.ability.guid === SPELLS.FLAMESTRIKE.id) {
         const castTimestamp = event.timestamp;
 
-        // Loop through the event history in reverse to detect if there was a `applybuff` event on the same player that was the result of this cast and thus misordered
         for (let previousEventIndex = eventIndex; previousEventIndex >= 0; previousEventIndex -= 1) {
           const previousEvent = fixedEvents[previousEventIndex];
-          if ((castTimestamp - previousEvent.timestamp) > 50) { // the max delay between the heal and cast events never looks to be more than this.
+          if ((castTimestamp - previousEvent.timestamp) > 50) {
             break;
           }
           if (previousEvent.type === 'removebuff' && previousEvent.ability.guid === SPELLS.HOT_STREAK.id && previousEvent.sourceID === event.sourceID) {
