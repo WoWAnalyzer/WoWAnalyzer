8888af67de5afdd6bcba3cb6899c0df7da9d5ba0
Nikita Snitnikov <snitnik@gmail.com>
2017-12-03 19:29:28 +0100

    relax requirements

--- src/Parser/Rogue/Subtlety/Modules/Talents/DarkShadow/DarkShadowSpecterOfBetrayal.js
+++ src/Parser/Rogue/Subtlety/Modules/Talents/DarkShadow/DarkShadowSpecterOfBetrayal.js
@@ -15,14 +15,14 @@ class DarkShadowSpecterOfBetrayal extends DarkShadow {
     const totalSpecterCastsInShadowDance  = this.danceDamageTracker.getAbility(SPELLS.SUMMON_DREAD_REFLECTION.id).casts;
     const totalSpecterCast  = this.damageTracker.getAbility(SPELLS.SUMMON_DREAD_REFLECTION.id).casts;
     const castsInDanceShare = totalSpecterCastsInShadowDance / totalSpecterCast;
-    when(castsInDanceShare).isLessThan(0.95)
+    when(castsInDanceShare).isLessThan(0.90)
     .addSuggestion((suggest, actual, recommended) => {
       return suggest(<Wrapper>Use <ItemLink id={ITEMS.SPECTER_OF_BETRAYAL.id} /> during <SpellLink id={SPELLS.SHADOW_DANCE.id} /> when you are using <SpellLink id={SPELLS.DARK_SHADOW_TALENT.id} />. </Wrapper>)
         .icon(ITEMS.SPECTER_OF_BETRAYAL.icon)
         .actual(`You used Specter of Betrayal in Shadow Dance ${formatPercentage(actual)}% of the time`)
         .recommended(`>${formatPercentage(recommended)}% is recommended`)
-        .regular(0.9)
-        .major(0.85);
+        .regular(0.80);
+        //Its around 1% dps loss when pressing it on cooldown, so never Major.
     });
   }
 
