44c88ffdd509722a297c74fa35bff7acfde238d3
Putro <lafa16ac@student.cbs.dk>
2017-12-03 12:56:48 +0100

    initial work

---
+++ src/Parser/Hunter/BeastMastery/Modules/Talents/AMurderOfCrows.js
@@ -0,0 +1,37 @@
+import Combatants from 'Parser/Core/Modules/Combatants';
+import Analyzer from 'Parser/Core/Analyzer';
+import SPELLS from 'common/SPELLS';
+import SpellUsable from 'Parser/Core/Modules/SpellUsable';
+
+const CROWS_USE_REGARDLESS_THRESHHOLD = 30;
+
+
+class AMurderOfCrows extends Analyzer {
+
+  static dependencies = {
+    combatants: Combatants,
+    spellUsable: SpellUsable,
+  };
+
+  on_initialized() {
+    this.active = this.combatants.selected.hasTalent(SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id);
+  }
+
+  on_byPlayer_damage(event) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.A_MURDER_OF_CROWS_SPELL.id) {
+      return;
+    }
+    this.damage += event.amount;
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.A_MURDER_OF_CROWS_SPELL.id) {
+      return;
+    }
+  }
+
+}
+
+export default AMurderOfCrows;
