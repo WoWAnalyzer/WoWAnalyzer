be12c57002601a61debc5df36e46eaf0bc9f9973
Putro <lafa16ac@student.cbs.dk>
2017-12-04 23:21:02 +0100

    remove unused imports

--- src/Parser/Hunter/BeastMastery/Modules/Traits/CobraCommander.js
+++ src/Parser/Hunter/BeastMastery/Modules/Traits/CobraCommander.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import SPELLS from 'common/SPELLS';
-import StatisticBox from "Main/StatisticBox";
 import SpellIcon from "common/SpellIcon";
-import { formatNumber } from 'common/format';
 import SpellLink from 'common/SpellLink';
 
 class CobraCommander extends Analyzer {
