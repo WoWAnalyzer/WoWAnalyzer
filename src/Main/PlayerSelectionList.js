d9e4672393163666f0dcabccb1e5ccb952d6e031
Martijn Hols <martijnhols@hotmail.com>
2017-12-07 01:42:32 +0100

    Fix an issue where demon hunters were colored like mere hunters Fix an issue where PlayerSelectionList crashed when still loading combatants (this happened when opening the popover) Refactored PlayerSelectionList a bit

--- src/Main/PlayerSelectionList.js
+++ src/Main/PlayerSelectionList.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -13,84 +13,53 @@ import ROLES from 'common/ROLES';
 
 import makeAnalyzerUrl from './makeAnalyzerUrl';
 
-class PlayerSelectionList extends React.PureComponent {
+const UNKNOWN_ROLE = 'UNKNOWN_ROLE';
+
+export class PlayerSelectionList extends React.PureComponent {
   static propTypes = {
     report: PropTypes.shape({
-      code: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
       friendlies: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number.isRequired,
-        type: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
       })),
     }).isRequired,
     fightId: PropTypes.number.isRequired,
     combatants: PropTypes.arrayOf(PropTypes.shape({
       sourceID: PropTypes.number.isRequired,
-    })).isRequired,
+      specID: PropTypes.number.isRequired,
+    })),
   };
 
   componentWillUnmount() {
     ReactTooltip.hide();
   }
 
-  renderRoleGroups(report, fightId, friendlies) {
-    const roleGroups = {};
-    roleGroups[ROLES.TANK] = [];
-    roleGroups[ROLES.HEALER] = [];
-    roleGroups[ROLES.DPS.MELEE] = [];
-    roleGroups[ROLES.DPS.RANGED] = [];
-    roleGroups[null] = []; // Spec might not be found if the combatantinfo errored, this happens extremely rarely. Example report: CJBdLf3c2zQXkPtg/13-Heroic+Kil'jaeden+-+Kill+(7:40)
-
-    friendlies.forEach(({ friendly, combatant }) => {
-      const spec = SPECS[combatant.specID];
-      roleGroups[spec ? spec.role : null].push([friendly, combatant]);
-    });
-
-    const rows = [];
-    Object.keys(roleGroups).forEach(( roleID ) => {
-      if ( roleID !== "null" || roleGroups[roleID].length > 0 ) {
-        rows.push(this.renderRoleGroup(report, fightId, parseInt(roleID, 10), roleGroups[roleID]));
-      }
-    });
-    return(rows);
+  groupByRole(friendlies) {
+    return friendlies.reduce((obj, friendly) => {
+      const spec = SPECS[friendly.combatant.specID];
+      const role = spec && spec.role !== null ? spec.role : UNKNOWN_ROLE;
+      obj[role] = obj[role] || [];
+      obj[role] = [...obj[role], friendly];
+      return obj;
+    }, {});
   }
 
-  renderRoleGroup(report, fightId, roleID, friendlies) {
-    let role;
-    switch (roleID) {
-      case ROLES.TANK: role = 'tanks'; break;
-      case ROLES.HEALER: role = 'healers'; break;
-      case ROLES.DPS.MELEE: role = 'melee-dps'; break;
-      case ROLES.DPS.RANGED: role = 'ranged-dps'; break;
-      default: role = 'role-unknown'; break;
-    }
-
-    return(
-      <div key={role} className="card">
-        { this.renderRoleHeader(roleID, friendlies.length) }
-        <ul className="list selection players">
-          {
-            friendlies.map(( arr ) => {
-              return this.renderFriendly(report, fightId, arr[0], arr[1]);
-            })
-          }
-        </ul>
-      </div>
-    );
-  }
-
-  renderRoleHeader(roleID, countFriendlies) {
+  renderRoleHeader(roleID, numFriendlies) {
     let icon;
     let header;
-    switch (roleID) {
+    let styles = {
+      borderRadius: '50%',
+      marginLeft: 10,
+      marginRight: 10,
+    };
+    switch (Number(roleID)) {
       case ROLES.TANK:
         icon = 'tank';
-        header = countFriendlies === 1 ? 'Tank' : 'Tanks';
+        header = numFriendlies === 1 ? 'Tank' : 'Tanks';
         break;
       case ROLES.HEALER:
         icon = 'healer';
-        header = countFriendlies === 1 ? 'Healer' : 'Healers';
+        header = numFriendlies === 1 ? 'Healer' : 'Healers';
         break;
       case ROLES.DPS.MELEE:
         icon = 'dps';
@@ -102,25 +71,27 @@ class PlayerSelectionList extends React.PureComponent {
         break;
       default: // Use a non-visible image for correct spacing
         icon = 'tank';
-        header = 'Unknown';
+        header = 'Unparsable due to corrupt combatlog event.';
+        styles = {
+          ...styles,
+          visibility: 'hidden',
+        };
         break;
     }
 
-    const styles = isNaN(roleID) ? { marginLeft: 10, marginRight: 10, visibility: 'hidden' } : { borderRadius: '50%', marginLeft: 10, marginRight: 10 };
-
-    return(
+    return (
       <h4 className="card-title">
         <img src={`/roles/${icon}.jpg`} alt="Role Icon" style={styles} />{header}
       </h4>
     );
   }
 
-  renderFriendly(report, fightId, friendly, combatant) {
-    const spec = SPECS[combatant.specID];
+  renderFriendly(report, fightId, friendly) {
+    const spec = SPECS[friendly.combatant.specID];
 
     if (!spec) {
       // Spec might not be found if the combatantinfo errored, this happens extremely rarely. Example report: CJBdLf3c2zQXkPtg/13-Heroic+Kil'jaeden+-+Kill+(7:40)
-      return(
+      return (
         <li key={friendly.id} className="item selectable">
           <Link
             to={makeAnalyzerUrl(report, fightId, friendly.name)}
@@ -135,9 +106,9 @@ class PlayerSelectionList extends React.PureComponent {
         </li>
       );
     } else {
-      return(
+      return (
         <li key={friendly.id} className="item selectable">
-          <Link to={makeAnalyzerUrl(report, fightId, friendly.name)} className={spec.className} style={{ marginLeft: 47 }}>
+          <Link to={makeAnalyzerUrl(report, fightId, friendly.name)} className={spec.className.replace(' ', '')} style={{ marginLeft: 47 }}>
             {this.renderSpecIcon(spec)} {friendly.name} ({spec.specName})
           </Link>
         </li>
@@ -154,32 +125,55 @@ class PlayerSelectionList extends React.PureComponent {
   render() {
     const { report, fightId, combatants } = this.props;
 
-    if ( combatants.length === 0 ) {
-      return(
+    if (!combatants) {
+      return (
+        <div className="container">
+          <div>
+            <h1>Fetching players...</h1>
+
+            <div className="spinner" />
+          </div>
+        </div>
+      );
+    }
+    if (combatants.length === 0) {
+      return (
         <li className="text-danger" style={{ padding: '15px 22px' }}>
           Could not find any players in this report. Make sure the log is recorded with Advanced Combat Logging enabled. You can enable this in-game in the network settings.
         </li>
       );
-    } else {
-      return(
-        this.renderRoleGroups(report, fightId,
-          report.friendlies
-            .map(friendly => ({
-              friendly,
-              combatant: combatants.find(combatant => combatant.sourceID === friendly.id),
-            }))
-            .filter(player => !!player.combatant)
-            .sort((a, b) => {
-              if (a.friendly.name > b.friendly.name) {
-                return 1;
-              } else if (a.friendly.name < b.friendly.name) {
-                return -1;
-              }
-              return 0;
-            })
-        )
-      );
     }
+
+    const roleGroups = this.groupByRole(
+      report.friendlies
+        .map(friendly => ({
+          ...friendly,
+          combatant: combatants.find(combatant => combatant.sourceID === friendly.id),
+        }))
+        .filter(player => !!player.combatant)
+    );
+
+    return Object.keys(roleGroups)
+      .map(roleID => {
+        const friendlies = roleGroups[roleID];
+        return (
+          <div key={roleID === null ? -1 : roleID} className="card">
+            {this.renderRoleHeader(roleID, friendlies.length)}
+            <ul className="list selection players">
+              {friendlies
+                .sort((a, b) => {
+                  if (a.name > b.name) {
+                    return 1;
+                  } else if (a.name < b.name) {
+                    return -1;
+                  }
+                  return 0;
+                })
+                .map(friendly => this.renderFriendly(report, fightId, friendly))}
+            </ul>
+          </div>
+        );
+      });
   }
 }
 
@@ -188,7 +182,6 @@ const mapStateToProps = state => {
 
   return ({
     fightId,
-
     report: getReport(state),
     fight: getFightById(state, fightId),
     combatants: getCombatants(state),
