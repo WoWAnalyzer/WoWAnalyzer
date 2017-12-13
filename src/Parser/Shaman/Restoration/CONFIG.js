f3fbf4219d9a140a7f0f4b63614a9f7c02297858
Travis Harris <travistylerharris@outlook.com>
2017-12-13 12:20:37 -0800

    Adding resto shaman spec description and discord chat

--- src/Parser/Shaman/Restoration/CONFIG.js
+++ src/Parser/Shaman/Restoration/CONFIG.js
@@ -1,16 +1,23 @@
-import { Hartra344 } from 'MAINTAINERS';
+import React from 'react';
+import { Hartra344, Versaya } from 'MAINTAINERS';
 import SPECS from 'common/SPECS';
 import SPEC_ANALYSIS_COMPLETENESS from 'common/SPEC_ANALYSIS_COMPLETENESS';
-
 import CombatLogParser from './CombatLogParser';
 import CHANGELOG from './CHANGELOG';
 
 export default {
   spec: SPECS.RESTORATION_SHAMAN,
-  maintainers: [Hartra344],
+  maintainers: [Hartra344, Versaya],
   completeness: SPEC_ANALYSIS_COMPLETENESS.NEEDS_MORE_WORK,
-  specDiscussionUrl: 'https://github.com/WoWAnalyzer/WoWAnalyzer/issues/601',
-  
+  description: (
+    <div>
+      Welcome to the Resto Shaman analyzer! We hope you find these suggestions and statistics useful.
+      <br /><br />
+
+      If you want to learn more about Resto Shaman, join the Druid community at the <a href="https://discord.gg/AcTek6e" target="_blank" rel="noopener noreferrer">Ancestral Guidance channel</a>.
+    </div>
+  ),
+  specDiscussionUrl: 'https://github.com/WoWAnalyzer/WoWAnalyzer/issues/56',
   // Shouldn't have to change these:
   changelog: CHANGELOG,
   parser: CombatLogParser,
