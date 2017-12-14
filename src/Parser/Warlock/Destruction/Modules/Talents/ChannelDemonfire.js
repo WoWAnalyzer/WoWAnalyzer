e16cdee6b3dca2b8fee22912920b638fd7019662
Will Swartz <superderpicus@gmail.com>
2017-12-14 04:34:04 -0700

    remove combatants
    dont need it

--- src/Parser/Warlock/Destruction/Modules/Talents/ChannelDemonfire.js
+++ src/Parser/Warlock/Destruction/Modules/Talents/ChannelDemonfire.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Analyzer from 'Parser/Core/Analyzer';
-import Combatants from 'Parser/Core/Modules/Combatants';
 import Enemies from 'Parser/Core/Modules/Enemies';
 
 import SPELLS from 'common/SPELLS';
@@ -11,7 +10,6 @@ import { formatNumber, formatPercentage } from 'common/format';
 
 class ChannelDemonfire extends Analyzer {
   static dependencies = {
-    combatants: Combatants,
     enemies: Enemies,
   };
 
