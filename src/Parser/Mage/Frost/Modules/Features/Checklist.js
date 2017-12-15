04488fe635cef60e7ad1c6d910c9bedc658cbb75
Kelton Finch <keltonfinch@gmail.com>
2017-12-15 15:50:59 -0500

    [frost mage] First few rules of checklist...

---
+++ src/Parser/Mage/Frost/Modules/Features/Checklist.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import SPELLS from 'common/SPELLS';
+import ITEMS from 'common/ITEMS';
+
+import SpellLink from 'common/SpellLink';
+import Wrapper from 'common/Wrapper';
+import ItemLink from 'common/ItemLink';
+
+import CoreChecklist, { Rule, Requirement, GenericCastEfficiencyRequirement } from 'Parser/Core/Modules/Features/Checklist';
+import CastEfficiency from 'Parser/Core/Modules/CastEfficiency';
+import Combatants from 'Parser/Core/Modules/Combatants';
+
+import CancelledCasts from '../../../Shared/Modules/Features/CancelledCasts';
+import AlwaysBeCasting from './AlwaysBeCasting';
+
+class Checklist extends CoreChecklist {
+  static dependencies = {
+    castEfficiency: CastEfficiency,
+    combatants: Combatants,
+    alwaysBeCasting: AlwaysBeCasting,
+    cancelledCasts: CancelledCasts,
+  };
+
+  rules = [
+    new Rule({
+      name: 'Always Be Casting',
+      description: <Wrapper><em><b>Continuously chaining casts throughout an encounter is the single most important thing for achieving good DPS as a caster</b></em>. There shoule be no delay at all between your spell casts, it's better to start casting the wrong spell than to think for a few seconds and then cast the right spell. You should be able to handle a fight's mechanics with the minimum possible interruption to your casting.</Wrapper>,
+      requirements: () => {
+        return [
+          new Requirement({
+            name: 'Downtime',
+            check: () => this.alwaysBeCasting.downtimeSuggestionThresholds,
+          }),
+          new Requirement({
+            name: 'Cancelled Casts',
+            check: () => this.cancelledCasts.cancelledCastSuggestionThresholds,
+          }),
+        ];
+      },
+    }),
+    new Rule({
+      name: 'Use Your Cooldowns',
+      description: <Wrapper>Your cooldowns are an important contributor to your DPS, and should be used as frequently as possible throughout a fight. A cooldown should be held on to only if a priority DPS phase is coming <em>soon</em>. Holding cooldowns too long will hurt your DPS.</Wrapper>,
+      requirements: () => {
+        const combatant = this.combatants.selected;
+        return [
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.FROZEN_ORB,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.EBONBOLT,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.ICY_VEINS,
+          }),
+          // TODO add more talented cooldowns (which?)
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.COMET_STORM_TALENT,
+            when: combatant.hasTalent(SPELLS.COMET_STORM_TALENT.id),
+          }),
+        ];
+      },
+    }),
+  ];
+}
+
+export default Checklist;
