bd394f333d6c4cfc19a3e0e1d55d32536b1e24f3
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2017-12-16 10:50:06 +0100

    Create ClawingShadowsEfficiency.js
    Added Clawing Shadows for people that use the talent

---
+++ src/Parser/DeathKnight/Unholy/Modules/Features/ClawingShadowsEfficiency.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import SPELLS from 'common/SPELLS';
+import SpellIcon from 'common/SpellIcon';
+import SpellLink from 'common/SpellLink';
+import { formatPercentage } from 'common/format';
+
+import Analyzer from 'Parser/Core/Analyzer';
+import Enemies from 'Parser/Core/Modules/Enemies';
+import Combatants from 'Parser/Core/Modules/Combatants';
+import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+
+class ClawingShadowsEfficiency extends Analyzer {
+  static dependencies = {
+    enemies: Enemies,
+    combatants: Combatants,
+  };
+  on_initialized() {
+    this.active = this.combatants.selected.hasTalent(SPELLS.CLAWING_SHADOWS_TALENT.id);
+  }
+  // used to track how many stacks a target has
+  targets = {};
+
+  totalClawingShadowsCasts = 0;
+  clawingShadowCastsZeroWounds = 0;
+
+  on_byPlayer_applydebuffstack(event){
+    const spellId = event.ability.guid;
+    if(spellId === SPELLS.FESTERING_WOUND.id){
+      this.targets[event.targetInstance] = event.stack;
+	  
+    }
+  }
+
+  on_byPlayer_removedebuffstack(event){
+    const spellId = event.ability.guid;
+    if(spellId === SPELLS.FESTERING_WOUND.id){
+      this.targets[event.targetInstance] = event.stack;
+    }
+  }
+
+  on_byPlayer_removedebuff(event){
+    const spellId = event.ability.guid;
+    if(spellId === SPELLS.FESTERING_WOUND.id){
+      this.targets[event.targetInstance] = 0;
+    }
+  }
+
+  on_byPlayer_cast(event){
+    const spellId = event.ability.guid;
+    if(spellId === SPELLS.CLAWING_SHADOWS_TALENT.id){
+      this.totalClawingShadowsCasts++;
+      if(this.targets.hasOwnProperty(event.targetInstance)){
+        const currentTargetWounds = this.targets[event.targetInstance];
+        if(currentTargetWounds < 1){
+			this.clawingShadowCastsZeroWounds++;
+        }
+      }
+    }
+  }
+
+  suggestions(when) {
+    const percentCastZeroWounds = this.clawingShadowCastsZeroWounds/this.totalClawingShadowsCasts;
+    const strikeEfficiency = 1 - percentCastZeroWounds;
+    when(strikeEfficiency).isLessThan(0.60)
+        .addSuggestion((suggest, actual, recommended) => {
+          return suggest(<span>You are casting <SpellLink id={SPELLS.CLAWING_SHADOWS_TALENT.id}/> too often.  When spending runes remember to cast <SpellLink id={SPELLS.FESTERING_STRIKE.id}/> instead on targets with no stacks of <SpellLink id={SPELLS.FESTERING_WOUND.id}/></span>)
+            .icon(SPELLS.CLAWING_SHADOWS_TALENT.icon)
+            .actual(`${formatPercentage(actual)}% of Clawing Shadows were used with Wounds on the target`)
+            .recommended(`>${formatPercentage(recommended)}% is recommended`)
+            .regular(recommended - 0.30).major(recommended - 0.40);
+        });
+  }
+
+  statistic() {
+    const percentCastZeroWounds = this.clawingShadowCastsZeroWounds/this.totalClawingShadowsCasts;
+    const strikeEfficiency = 1 - percentCastZeroWounds;
+    return (
+      <StatisticBox
+        icon={<SpellIcon id={SPELLS.CLAWING_SHADOWS_TALENT.id} />}
+        value={`${formatPercentage(strikeEfficiency)} %`}
+        label={'Clawing Shadows Efficiency'}
+        tooltip={`${this.clawingShadowCastsZeroWounds} out of ${this.totalClawingShadowsCasts} Clawing Shadows were used with no Festering Wounds on the target.`}
+      />
+    );
+  }
+
+  statisticOrder = STATISTIC_ORDER.CORE(3);
+}
+
+export default ClawingShadowsEfficiency;
