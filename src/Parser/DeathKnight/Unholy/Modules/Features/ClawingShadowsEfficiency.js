af2c723e79f8315f256b8d4e86dc3c6a91b92073
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2017-12-16 18:09:53 +0100

    Update ClawingShadowsEfficiency.js
    Same as the changes made to scourge strike

--- src/Parser/DeathKnight/Unholy/Modules/Features/ClawingShadowsEfficiency.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/ClawingShadowsEfficiency.js
@@ -9,6 +9,7 @@ import Analyzer from 'Parser/Core/Analyzer';
 import Enemies from 'Parser/Core/Modules/Enemies';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+import { encodeTargetString } from 'Parser/Core/Modules/EnemyInstances';
 
 class ClawingShadowsEfficiency extends Analyzer {
   static dependencies = {
@@ -27,48 +28,48 @@ class ClawingShadowsEfficiency extends Analyzer {
   on_byPlayer_applydebuffstack(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.FESTERING_WOUND.id){
-      this.targets[event.targetInstance] = event.stack;
-	  
-    }
+		this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.stack;
+	}
   }
 
   on_byPlayer_removedebuffstack(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.FESTERING_WOUND.id){
-      this.targets[event.targetInstance] = event.stack;
+		this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.stack;
     }
   }
 
   on_byPlayer_removedebuff(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.FESTERING_WOUND.id){
-      this.targets[event.targetInstance] = 0;
-    }
+		this.targets[encodeTargetString(event.targetID, event.targetInstance)] = 0;
+	}
   }
 
   on_byPlayer_cast(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.CLAWING_SHADOWS_TALENT.id){
-      this.totalClawingShadowsCasts++;
-      if(this.targets.hasOwnProperty(event.targetInstance)){
-        const currentTargetWounds = this.targets[event.targetInstance];
-        if(currentTargetWounds < 1){
+		this.totalClawingShadowsCasts++;
+		if(this.targets.hasOwnProperty(encodeTargetString(event.targetID, event.targetInstance))){
+			const currentTargetWounds = this.targets[encodeTargetString(event.targetID, event.targetInstance)];
+			if(currentTargetWounds < 1){
+				this.clawingShadowCastsZeroWounds++;
+			}
+		} else {
 			this.clawingShadowCastsZeroWounds++;
-        }
-      }
     }
   }
 
   suggestions(when) {
     const percentCastZeroWounds = this.clawingShadowCastsZeroWounds/this.totalClawingShadowsCasts;
     const strikeEfficiency = 1 - percentCastZeroWounds;
-    when(strikeEfficiency).isLessThan(0.60)
+    when(strikeEfficiency).isLessThan(0.80)
         .addSuggestion((suggest, actual, recommended) => {
           return suggest(<span>You are casting <SpellLink id={SPELLS.CLAWING_SHADOWS_TALENT.id}/> too often.  When spending runes remember to cast <SpellLink id={SPELLS.FESTERING_STRIKE.id}/> instead on targets with no stacks of <SpellLink id={SPELLS.FESTERING_WOUND.id}/></span>)
             .icon(SPELLS.CLAWING_SHADOWS_TALENT.icon)
             .actual(`${formatPercentage(actual)}% of Clawing Shadows were used with Wounds on the target`)
             .recommended(`>${formatPercentage(recommended)}% is recommended`)
-            .regular(recommended - 0.30).major(recommended - 0.40);
+            .regular(recommended - 0.20).major(recommended - 0.40);
         });
   }
 
