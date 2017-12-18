74c9d9d57ee617cd93812d3f693eaa9036d601db
Martijn Hols <martijnhols@hotmail.com>
2017-12-18 16:02:40 +0100

    Fix eslint warning

--- src/Parser/Monk/Brewmaster/Modules/Features/Checklist.js
+++ src/Parser/Monk/Brewmaster/Modules/Features/Checklist.js
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-import Wrapper from 'common/Wrapper';
-import SPELLS from 'common/SPELLS';
-import ITEMS from 'common/ITEMS';
-import SpellLink from 'common/SpellLink';
-import ItemLink from 'common/ItemLink';
+// import Wrapper from 'common/Wrapper';
+// import SPELLS from 'common/SPELLS';
+// import ITEMS from 'common/ITEMS';
+// import SpellLink from 'common/SpellLink';
+// import ItemLink from 'common/ItemLink';
 
-import CoreChecklist, { Rule, Requirement, GenericCastEfficiencyRequirement } from 'Parser/Core/Modules/Features/Checklist';
+import CoreChecklist, { /*Rule, Requirement, GenericCastEfficiencyRequirement*/ } from 'Parser/Core/Modules/Features/Checklist';
 
 class Checklist extends CoreChecklist {
   static dependencies = {
