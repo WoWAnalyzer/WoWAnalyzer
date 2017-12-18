12100f718f9aa5f20a684a470ebef68e7d6d1c1d
Martijn Hols <martijnhols@hotmail.com>
2017-12-18 14:23:17 +0100

    Reformat Brewmaster CombatLogParser

--- src/Parser/Monk/Brewmaster/CombatLogParser.js
+++ src/Parser/Monk/Brewmaster/CombatLogParser.js
@@ -1,21 +1,16 @@
 import CoreCombatLogParser from 'Parser/Core/CombatLogParser';
 import DamageDone from 'Parser/Core/Modules/DamageDone';
-
 // Core
 import HealingDone from './Modules/Core/HealingDone';
 import DamageTaken from './Modules/Core/DamageTaken';
 import HealingReceived from './Modules/Core/HealingReceived';
 import Stagger from './Modules/Core/Stagger';
-
 // Spells
 import IronSkinBrew from './Modules/Spells/IronSkinBrew';
 import BlackoutCombo from './Modules/Spells/BlackoutCombo';
-
-
 // Features
 import Abilities from './Modules/Features/Abilities';
 import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
-
 // Items
 import T20_2pc from './Modules/Items/T20_2pc';
 import T20_4pc from './Modules/Items/T20_4pc';
