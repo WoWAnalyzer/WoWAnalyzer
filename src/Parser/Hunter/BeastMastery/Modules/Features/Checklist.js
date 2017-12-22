312299bf6d46dddaf18dac27769980ad2edb5dc6
Putro <lafa16ac@student.cbs.dk>
2017-12-22 02:30:51 +0100

    more work on bm

--- src/Parser/Hunter/BeastMastery/Modules/Features/Checklist.js
+++ src/Parser/Hunter/BeastMastery/Modules/Features/Checklist.js
@@ -31,11 +31,79 @@ class Checklist extends CoreChecklist {
     //talents
     aMurderOfCrows: AMurderOfCrows,
 
+    //Bestial Wrath
+
 
   };
 
   rules = [
+    new Rule({
+      name: 'Use core spells as often as possible',
+      description: <Wrapper><a href="https://www.icy-veins.com/wow/beast-mastery-hunter-pve-dps-rotation-cooldowns-abilities" target="_blank" rel="noopener noreferrer">More info.</a></Wrapper>,
+      requirements: () => {
+        const combatant = this.combatants.selected;
+        return [
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.DIRE_BEAST,
+            when: !combatant.hasTalent(SPELLS.DIRE_FRENZY_TALENT.id),
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.DIRE_FRENZY_TALENT,
+            when: combatant.hasTalent(SPELLS.DIRE_FRENZY_TALENT.id),
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.TITANS_THUNDER,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.BESTIAL_WRATH,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.KILL_COMMAND,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED,
+            when: combatant.hasTalent(SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id),
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.ASPECT_OF_THE_WILD,
+          }),
+          new GenericCastEfficiencyRequirement({
+            spell: SPELLS.BARRAGE_TALENT,
+            when: combatant.hasTalent(SPELLS.BARRAGE_TALENT.id),
+          }),
+        ];
+      },
+    }),
+    new Rule({
+      name: 'Dire Beast Efficiency',
+      description: <Wrapper>USE DireBeast properly</Wrapper>,
+      requirements: () => {
+        return [
+        new Requirement({
 
+        })
+        ];
+      },
+    }),
+    new Rule({
+      name: 'Downtime',
+      description: <Wrapper> Try to reduce the delay between casting spells. If everything is on cooldown, try and use <SpellLink id={SPELLS.COBRA_SHOT.id} /> to stay off the focus cap and do some damage.</Wrapper>,
+      requirements: () => {
+        return [
+          new Requirement({
+            name: <Wrapper><Icon
+              icon='spell_mage_altertime'
+              alt='Casting downtime'
+              style={{
+                height: '1.3em',
+                marginTop: '-.1em',
+              }}
+            /> Downtime</Wrapper>,
+            check: () => this.alwaysBeCasting.suggestionThresholds,
+          }),
+        ];
+      },
+    }),
     new Rule({
       name: 'Be well prepared',
       description: 'Being prepared is important if you want to perform to your highest potential',
