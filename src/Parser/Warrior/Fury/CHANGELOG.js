04079fabc3ac1c8fd34556a8dcbdcece54082102
Alexander Qvist-Hellum <Alexander@svejbaekgaard.dk>
2017-12-25 05:23:53 +0100

    Updated changelog

--- src/Parser/Warrior/Fury/CHANGELOG.js
+++ src/Parser/Warrior/Fury/CHANGELOG.js
@@ -2,6 +2,11 @@ import { Maldark } from 'MAINTAINERS';
 
 export default [
   {
+    date: new Date('2017-12-24'),
+    changes: 'Added Rampage analysis for Frothing Berserker talent.',
+    contributors: [Maldark],
+  },
+  {
     date: new Date('2017-12-14'),
     changes: 'Added initial support.',
     contributors: [Maldark],
