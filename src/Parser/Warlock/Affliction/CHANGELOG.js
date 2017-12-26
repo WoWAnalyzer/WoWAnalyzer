5645f75494a7eae9ef4c1f3a4094c428a227266a
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-12-26 18:20:12 +0100

    Rewrote Affliction changelog

--- src/Parser/Warlock/Affliction/CHANGELOG.js
+++ src/Parser/Warlock/Affliction/CHANGELOG.js
@@ -1,8 +1,36 @@
-export default `
-16-09-2017 - Added Doomguard/Infernal/Grimoire of Service to Cooldowns tab. (by Chizu)
-09-09-2017 - Added Empowered Life Tap module. (by Chizu)
-27-08-2017 - Reworked the DS/UA sniping module to provide more relevant info. (by Chizu)
-24-08-2017 - Added Drain Soul / Unstable Affliction sniping module. (by Chizu)
-21-08-2017 - Added rest of the legendaries (apart from head), some ToS trinkets and T20 set bonuses. (by Chizu)
-18-08-2017 - Added <span class="Warlock">Affliction Warlock</span> support by @Chizu.
-`;
+import React from 'react';
+
+import { Chizu } from 'MAINTAINERS';
+import Wrapper from 'common/Wrapper';
+import ItemLink from 'common/ItemLink';
+import ITEMS from 'common/ITEMS';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+
+export default [
+  {
+    date: new Date('2017-09-16'),
+    changes: <Wrapper>Added <SpellLink id={SPELLS.SUMMON_DOOMGUARD_UNTALENTED.id} icon/>/<SpellLink id={SPELLS.SUMMON_INFERNAL_UNTALENTED.id} icon/>/<SpellLink id={SPELLS.GRIMOIRE_OF_SERVICE_TALENT.id} icon/> to Cooldowns tab.</Wrapper>,
+    contributors: [Chizu],
+  },
+  {
+    date: new Date('2017-09-09'),
+    changes: <Wrapper>Added <SpellLink id={SPELLS.EMPOWERED_LIFE_TAP_TALENT.id} icon/> module.</Wrapper>,
+    contributors: [Chizu],
+  },
+  {
+    date: new Date('2017-08-27'),
+    changes: <Wrapper>Reworked the <SpellLink id={SPELLS.DRAIN_SOUL.id} icon/>/<SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id} icon/> sniping module to provide more relevant info.</Wrapper>,
+    contributors: [Chizu],
+  },
+  {
+    date: new Date('2017-08-24'),
+    changes: <Wrapper>Added <SpellLink id={SPELLS.DRAIN_SOUL.id} icon/>/<SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id} icon/> sniping module.</Wrapper>,
+    contributors: [Chizu],
+  },
+  {
+    date: new Date('2017-08-21'),
+    changes: <Wrapper>Added rest of the legendaries (apart from <ItemLink id={ITEMS.HOOD_OF_ETERNAL_DISDAIN.id} icon/>), some ToS trinkets and T20 set bonuses.</Wrapper>,
+    contributors: [Chizu],
+  },
+];
