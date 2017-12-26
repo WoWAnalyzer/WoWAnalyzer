58b397e151e0417a768854798e9422720a0ac991
Alexander Qvist-Hellum <Alexander@svejbaekgaard.dk>
2017-12-26 02:33:30 +0100

    Updated changelog

--- src/Parser/Warrior/Fury/CHANGELOG.js
+++ src/Parser/Warrior/Fury/CHANGELOG.js
@@ -2,6 +2,11 @@ import { Maldark } from 'MAINTAINERS';
 
 export default [
   {
+    date: new Date('2017-12-25'),
+    changes: 'Added T21 analysis as well as Rampage cancellation analysis and Mastery support.',
+    contributors: [Maldark],
+  },
+  {
     date: new Date('2017-12-24'),
     changes: 'Added Rampage analysis for Frothing Berserker talent.',
     contributors: [Maldark],
