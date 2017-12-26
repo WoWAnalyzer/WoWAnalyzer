0b91d6a6b58e91abde1e7f2b13ace28e117b9b55
Putro <lafa16ac@student.cbs.dk>
2017-12-26 16:34:13 +0100

    double quotes

--- src/Parser/Hunter/Shared/Modules/Features/FocusUsage.js
+++ src/Parser/Hunter/Shared/Modules/Features/FocusUsage.js
@@ -260,7 +260,7 @@ class FocusUsage extends Analyzer {
       <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
         <div className="row">
           <StatisticsListBox
-            title={<span> Focus usage</span>}
+            title="Focus usage"
             containerProps={{ className: 'col-xs-12' }}
           >
             {this.focusUsageChart()}
