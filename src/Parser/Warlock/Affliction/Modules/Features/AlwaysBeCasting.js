0f3d14d6d5f823d92181dc21dd69c3d94ec3db80
Lukas Stuchlik <stuchlik1@seznam.cz>
2017-12-26 23:01:35 +0100

    Rewrote ABC for all specs.

--- src/Parser/Warlock/Affliction/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Warlock/Affliction/Modules/Features/AlwaysBeCasting.js
@@ -6,19 +6,28 @@ import SPELLS from 'common/SPELLS';
 import { formatPercentage } from 'common/format';
 import { STATISTIC_ORDER } from 'Main/StatisticBox';
 import SpellLink from 'common/SpellLink';
+import Wrapper from 'common/Wrapper';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
+  get suggestionThresholds() {
+    return {
+      actual: this.totalTimeWasted / this.owner.fightDuration,
+      isGreaterThan: {
+        minor: 0.2,
+        average: 0.35,
+        major: 0.4,
+      },
+      style: 'percentage',
+    };
+  }
 
   suggestions(when) {
-    const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
-
-    when(deadTimePercentage).isGreaterThan(0.2)
+    when(this.suggestionThresholds)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. Even if you have to move, try casting something instant - maybe refresh your dots or replenish your mana with <SpellLink id={SPELLS.LIFE_TAP.id} /></span>)
+        return suggest(<Wrapper>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. Even if you have to move, try casting something instant - maybe refresh your dots or replenish your mana with <SpellLink id={SPELLS.LIFE_TAP.id} />. Make good use of your <SpellLink id={SPELLS.DEMONIC_CIRCLE_TALENT_TELEPORT.id} /> when you can.</Wrapper>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended + 0.15).major(recommended + 0.2);
+          .recommended(`<${formatPercentage(recommended)}% is recommended`);
       });
   }
 
--- src/Parser/Warlock/Destruction/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Warlock/Destruction/Modules/Features/AlwaysBeCasting.js
@@ -6,18 +6,28 @@ import SPELLS from 'common/SPELLS';
 import { formatPercentage } from 'common/format';
 import { STATISTIC_ORDER } from 'Main/StatisticBox';
 import SpellLink from 'common/SpellLink';
+import Wrapper from 'common/Wrapper';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
-  suggestions(when) {
-    const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
+  get suggestionThresholds() {
+    return {
+      actual: this.totalTimeWasted / this.owner.fightDuration,
+      isGreaterThan: {
+        minor: 0.2,
+        average: 0.35,
+        major: 0.4,
+      },
+      style: 'percentage',
+    };
+  }
 
-    when(deadTimePercentage).isGreaterThan(0.2)
+  suggestions(when) {
+    when(this.suggestionThresholds)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. When you know you'll be moving, try to save <SpellLink id={SPELLS.DIMENSIONAL_RIFT_CAST.id} /> or <SpellLink id={SPELLS.CONFLAGRATE.id} /> charges or replenish your mana with <SpellLink id={SPELLS.LIFE_TAP.id} />. Make good use of your <SpellLink id={SPELLS.DEMONIC_CIRCLE_TALENT_TELEPORT.id} /> when you can. </span>)
+        return suggest(<Wrapper>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. When you know you'll be moving, try to save <SpellLink id={SPELLS.DIMENSIONAL_RIFT_CAST.id} /> or <SpellLink id={SPELLS.CONFLAGRATE.id} /> charges or replenish your mana with <SpellLink id={SPELLS.LIFE_TAP.id} />. Make good use of your <SpellLink id={SPELLS.DEMONIC_CIRCLE_TALENT_TELEPORT.id} /> when you can.</Wrapper>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended + 0.15).major(recommended + 0.2);
+          .recommended(`<${formatPercentage(recommended)}% is recommended`);
       });
   }
 
