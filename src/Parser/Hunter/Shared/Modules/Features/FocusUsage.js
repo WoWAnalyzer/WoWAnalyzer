ac98d162001971e4e7c54b22dd91c8a37215ecd9
Putro <lafa16ac@student.cbs.dk>
2017-12-26 16:33:05 +0100

    made it way less spec specific throughout

--- src/Parser/Hunter/Shared/Modules/Features/FocusUsage.js
+++ src/Parser/Hunter/Shared/Modules/Features/FocusUsage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Doughnut as DoughnutChart } from 'react-chartjs-2';
 
 import SPELLS from 'common/SPELLS';
-import SPECS from 'common/SPECS';
 
 import SpellLink from 'common/SpellLink';
 import { formatPercentage } from 'common/format';
@@ -11,145 +10,120 @@ import Analyzer from 'Parser/Core/Analyzer';
 import StatisticsListBox from 'Main/StatisticsListBox';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import STATISTIC_ORDER from 'Main/STATISTIC_ORDER';
+import ITEMS from 'common/ITEMS';
 
 const CHART_SIZE = 100;
 
+//to ensure we don't count multiple volley hits as the same cast
 const BUFFER_MS = 100;
 
+//the cost per auto attack with volley up
 const VOLLEY_FOCUS_COST = 3;
 
-const LIST_OF_BM_FOCUS_SPENDERS = [
+//bm legendary that reduces focus cost - only needed because of volley, else it's baked into the cost
+const ROAR_OF_THE_SEVEN_LIONS_FOCUS_REDUCTION = 0.15;
+
+const LIST_OF_FOCUS_SPENDERS = [
+  //bm specific
   SPELLS.COBRA_SHOT.id,
   SPELLS.MULTISHOT.id,
   SPELLS.KILL_COMMAND.id,
-  SPELLS.REVIVE_PET_AND_MEND_PET.id,
-  SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id,
-  SPELLS.BARRAGE_TALENT.id,
-  SPELLS.VOLLEY_ACTIVATED.id,
-];
-
-const LIST_OF_MM_FOCUS_SPENDERS = [
+  //mm specific
   SPELLS.AIMED_SHOT.id,
   SPELLS.MARKED_SHOT.id,
-  SPELLS.REVIVE_PET_AND_MEND_PET.id,
-  SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id,
-  SPELLS.BARRAGE_TALENT.id,
-  SPELLS.VOLLEY_ACTIVATED.id,
   SPELLS.WINDBURST.id,
   SPELLS.PIERCING_SHOT_TALENT.id,
   SPELLS.BLACK_ARROW_TALENT.id,
   SPELLS.EXPLOSIVE_SHOT_TALENT.id,
+  //shared
+  SPELLS.REVIVE_PET_AND_MEND_PET.id,
+  SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id,
+  SPELLS.BARRAGE_TALENT.id,
+  SPELLS.VOLLEY_ACTIVATED.id,
 ];
-
-//code grabbed from Parser/Paladin/Holy/Modules/PaladinCore/CastBehavior.js
 
 class FocusUsage extends Analyzer {
   static dependencies = {
     combatants: Combatants,
   };
-  focusSpenderCastsBM = {
+
+  focusSpenderCasts = {
     [SPELLS.COBRA_SHOT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Cobra Shot",
+      name: SPELLS.COBRA_SHOT.name,
       color: '#ecd1b6',
     },
     [SPELLS.MULTISHOT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Multishot",
+      name: SPELLS.MULTISHOT.name,
       color: '#c1ec9c',
     },
     [SPELLS.KILL_COMMAND.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Kill Command",
+      name: SPELLS.KILL_COMMAND.name,
       color: '#abff3d',
     },
     [SPELLS.REVIVE_PET_AND_MEND_PET.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Revive/Mend pet",
+      name: SPELLS.REVIVE_PET_AND_MEND_PET.name,
       color: '#ec0003',
     },
     [SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "A Murder of Crows",
+      name: SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.name,
       color: '#8b8dec',
     },
     [SPELLS.BARRAGE_TALENT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Barrage",
+      name: SPELLS.BARRAGE_TALENT.name,
       color: '#ec5c58',
     },
     [SPELLS.VOLLEY_ACTIVATED.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Volley",
+      name: SPELLS.VOLLEY_ACTIVATED.name,
       color: '#66ecd8',
     },
-  };
-  focusSpenderCastsMM = {
     [SPELLS.AIMED_SHOT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Aimed Shot",
+      name: SPELLS.AIMED_SHOT.name,
       color: '#84ec81',
     },
     [SPELLS.MARKED_SHOT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Marked Shot",
+      name: SPELLS.MARKED_SHOT.name,
       color: '#ff7d0a',
-    },
-    [SPELLS.REVIVE_PET_AND_MEND_PET.id]: {
-      casts: 0,
-      focusUsed: 0,
-      name: "Revive/Mend Pet",
-      color: '#ec0003',
-    },
-    [SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id]: {
-      casts: 0,
-      focusUsed: 0,
-      name: "A Murder of Crows",
-      color: '#8b8dec',
-    },
-    [SPELLS.BARRAGE_TALENT.id]: {
-      casts: 0,
-      focusUsed: 0,
-      name: "Barrage",
-      color: '#ec5c58',
-    },
-    [SPELLS.VOLLEY_ACTIVATED.id]: {
-      casts: 0,
-      focusUsed: 0,
-      name: "Volley",
-      color: '#66ecd8',
     },
     [SPELLS.WINDBURST.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Windburst",
+      name: SPELLS.WINDBURST.name,
       color: '#ecd1b6',
     },
     [SPELLS.PIERCING_SHOT_TALENT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Piercing Shot",
+      name: SPELLS.PIERCING_SHOT_TALENT.name,
       color: '#d440ec',
     },
     [SPELLS.BLACK_ARROW_TALENT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Black Arrow",
+      name: SPELLS.BLACK_ARROW_TALENT.name,
       color: '#2a2a2a',
     },
     [SPELLS.EXPLOSIVE_SHOT_TALENT.id]: {
       casts: 0,
       focusUsed: 0,
-      name: "Explosive Shot",
+      name: SPELLS.EXPLOSIVE_SHOT_TALENT.name,
       color: '#ecda4c',
     },
   };
@@ -228,34 +202,26 @@ class FocusUsage extends Analyzer {
 
   on_byPlayer_cast(event) {
     const spellId = event.ability.guid;
-    if (this.combatants.selected.spec === SPECS.MARKSMANSHIP_HUNTER) {
-      if (LIST_OF_MM_FOCUS_SPENDERS.every(id => spellId !== id)) {
-        return;
-      }
-      this.focusSpenderCastsMM[spellId].casts += 1;
-      this.focusSpenderCastsMM[spellId].focusUsed += event.classResources[0]['cost'] || 0;
-    } else if (this.combatants.selected.spec === SPECS.BEAST_MASTERY_HUNTER) {
-      if (LIST_OF_BM_FOCUS_SPENDERS.every(id => spellId !== id)) {
-        return;
-      }
-      this.focusSpenderCastsBM[spellId].casts += 1;
-      this.focusSpenderCastsBM[spellId].focusUsed += event.classResources[0]['cost'] || 0;
+    if (LIST_OF_FOCUS_SPENDERS.every(id => spellId !== id)) {
+      return;
     }
+    this.focusSpenderCasts[spellId].casts += 1;
+    this.focusSpenderCasts[spellId].focusUsed += event.classResources[0]['cost'] || 0;
   }
+
   on_byPlayer_damage(event) {
     const spellId = event.ability.guid;
     if (spellId !== SPELLS.VOLLEY_ACTIVATED.id) {
       return;
     }
     if (event.timestamp > (this.lastVolleyHit + BUFFER_MS)) {
-      if (this.combatants.selected.spec === SPECS.MARKSMANSHIP_HUNTER) {
-        this.focusSpenderCastsMM[spellId].casts += 1;
-        this.focusSpenderCastsMM[spellId].focusUsed += VOLLEY_FOCUS_COST;
+      this.focusSpenderCasts[spellId].casts += 1;
+      if (this.combatants.selected.hasBuff(SPELLS.BESTIAL_WRATH.id) && this.combatants.selected.hasWaist(ITEMS.ROAR_OF_THE_SEVEN_LIONS.id)) {
+        this.focusSpenderCasts[spellId].focusUsed += VOLLEY_FOCUS_COST - (VOLLEY_FOCUS_COST * ROAR_OF_THE_SEVEN_LIONS_FOCUS_REDUCTION);
 
       }
-      if (this.combatants.selected.spec === SPECS.BEAST_MASTERY_HUNTER) {
-        this.focusSpenderCastsBM[spellId].casts += 1;
-        this.focusSpenderCastsBM[spellId].focusUsed += VOLLEY_FOCUS_COST;
+      else {
+        this.focusSpenderCasts[spellId].focusUsed += VOLLEY_FOCUS_COST;
       }
       this.lastVolleyHit = event.timestamp;
     }
@@ -264,45 +230,31 @@ class FocusUsage extends Analyzer {
   focusUsageChart() {
     let totalFocusUsed = 0;
     const items = [];
-    const bmFocusSpenders = [[SPELLS.COBRA_SHOT.id], [SPELLS.MULTISHOT.id], [SPELLS.KILL_COMMAND.id], [SPELLS.REVIVE_PET_AND_MEND_PET.id], [SPELLS.A_MURDER_OF_CROWS_TALENT_SHARED.id], [SPELLS.BARRAGE_TALENT.id], [SPELLS.VOLLEY_ACTIVATED.id]];
-    const mmFocusSpenders = [[SPELLS.AIMED_SHOT.id], [SPELLS.MARKED_SHOT.id], [SPELLS.REVIVE_PET_AND_MEND_PET.id], [SPELLS.BARRAGE_TALENT.id], [SPELLS.VOLLEY_ACTIVATED.id], [SPELLS.WINDBURST.id], [SPELLS.PIERCING_SHOT_TALENT.id], [SPELLS.BLACK_ARROW_TALENT.id], [SPELLS.EXPLOSIVE_SHOT_TALENT.id]];
-    if (this.combatants.selected.spec === SPECS.MARKSMANSHIP_HUNTER) {
-      mmFocusSpenders.forEach(focusSpender => {
-        if (this.focusSpenderCastsMM[focusSpender].casts > 0) {
-          items.push({
-            color: this.focusSpenderCastsMM[focusSpender].color,
-            label: this.focusSpenderCastsMM[focusSpender].name,
-            spellId: focusSpender,
-            value: this.focusSpenderCastsMM[focusSpender].focusUsed,
-          });
-        }
-      });
-      mmFocusSpenders.map(ability => totalFocusUsed += this.focusSpenderCastsMM[ability].focusUsed);
-    } else if (this.combatants.selected.spec === SPECS.BEAST_MASTERY_HUNTER) {
-      bmFocusSpenders.forEach(focusSpender => {
-        if (this.focusSpenderCastsBM[focusSpender].casts > 0) {
-          items.push({
-            color: this.focusSpenderCastsBM[focusSpender].color,
-            label: this.focusSpenderCastsBM[focusSpender].name,
-            spellId: focusSpender,
-            value: this.focusSpenderCastsBM[focusSpender].focusUsed,
-          });
-        }
-      });
-      bmFocusSpenders.map(ability => totalFocusUsed += this.focusSpenderCastsBM[ability].focusUsed);
 
-    }
+    LIST_OF_FOCUS_SPENDERS.forEach(id => {
+      if (this.focusSpenderCasts[id].casts > 0 && this.focusSpenderCasts[id].focusUsed > 0) {
+        items.push({
+          color: this.focusSpenderCasts[id].color,
+          label: this.focusSpenderCasts[id].name,
+          spellId: id,
+          value: Math.round(this.focusSpenderCasts[id].focusUsed),
+        });
+        totalFocusUsed += this.focusSpenderCasts[id].focusUsed;
+      }
+    });
     return (
       <div className="flex">
         <div className="flex-sub" style={{ paddingRight: 12 }}>
           {this.chart(items)}
         </div>
         <div className="flex-main" style={{ fontSize: '80%', paddingTop: 3 }}>
-          {this.legend(items, totalFocusUsed)}
+          {this.legend(items, totalFocusUsed.toFixed(1))}
         </div>
       </div>
-    );
+    )
+      ;
   }
+
   statistic() {
     return (
       <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
