29a2ee77a23019dd089d969cbee215e99df06539
J. David Smith <emallson@atlanis.net>
2017-12-29 15:33:43 -0500

    Changed RJW module to use `getBuffUptime`

--- src/Parser/Monk/Brewmaster/Modules/Spells/RushingJadeWind.js
+++ src/Parser/Monk/Brewmaster/Modules/Spells/RushingJadeWind.js
@@ -4,8 +4,7 @@ import SpellLink from 'common/SpellLink';
 import { formatPercentage } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
-
-const debug = false;
+import Wrapper from 'common/Wrapper';
 
 // the buff events all use this spell
 export const RUSHING_JADE_WIND_BUFF = SPELLS.RUSHING_JADE_WIND_TALENT; 
@@ -15,51 +14,17 @@ class RushingJadeWind extends Analyzer {
     combatants: Combatants,
   };
 
-  inactiveDuration = 0;
-  _lastDropped = 0;
-  _active = false;
-
   on_initialized() {
-    this._lastDropped = this.owner.fight.start_time;
     this.active = this.combatants.selected.hasTalent(SPELLS.RUSHING_JADE_WIND_TALENT.id);
-  }
-
-  on_byPlayer_applybuff(event) {
-    if(event.ability.guid === RUSHING_JADE_WIND_BUFF.id) {
-      this.inactiveDuration += event.timestamp - this._lastDropped;
-      this._active = true;
-      debug && console.log("RJW re-applied", event.timestamp, this.inactiveDuration);
-    }
-  }
-
-  on_byPlayer_removebuff(event) {
-    if(event.ability.guid === RUSHING_JADE_WIND_BUFF.id) {
-      this._lastDropped = event.timestamp;
-      this._active = false;
-      debug && console.log("RJW dropped", event.timestamp);
-    }
-  }
-
-  on_finished() {
-    if(!this._active) {
-      this.inactiveDuration += this.owner.fight.end_time - this._lastDropped;
-    }
-  }
-
-  get uptime() {
-    if(this.owner.fightDuration === 0) {
-      return 0;
-    }
-    const downtime = this.inactiveDuration / this.owner.fightDuration;
-    return 1.0 - downtime;
   }
 
   // using a suggestion rather than a checklist item for this as RJW is
   // purely offensive
   suggestions(when) {
-    when(this.uptime).isLessThan(0.95)
+    const uptime = this.combatants.selected.getBuffUptime(RUSHING_JADE_WIND_BUFF.id) / this.owner.fightDuration;
+    when(uptime).isLessThan(0.95)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span>You had low uptime on <SpellLink id={SPELLS.RUSHING_JADE_WIND.id} />. Try to maintain 100% uptime by refreshing the buff before it drops.</span>)
+        return suggest(<Wrapper>You had low uptime on <SpellLink id={SPELLS.RUSHING_JADE_WIND.id} />. Try to maintain 100% uptime by refreshing the buff before it drops.</Wrapper>)
           .icon(SPELLS.RUSHING_JADE_WIND.icon)
           .actual(`${formatPercentage(actual)}% uptime`)
           .recommended(`${Math.round(formatPercentage(recommended))}% is recommended`)
--- src/Parser/Monk/Brewmaster/Modules/Spells/RushingJadeWind.test.js
+++ src/Parser/Monk/Brewmaster/Modules/Spells/RushingJadeWind.test.js
@@ -9,22 +9,6 @@ const talented_combatant = {
   hasTalent: (id) => id === SPELLS.RUSHING_JADE_WIND_TALENT.id,
 };
 
-const BUFF = {...RUSHING_JADE_WIND_BUFF, guid: RUSHING_JADE_WIND_BUFF.id};
-
-const player = 1;
-
-// should have 50% uptime, sequences of applybuff/applybuff or
-// removebuff/removebuff are assumed to never happen
-const halfUptime = [
-  {sourceID: player, targetID: player, timestamp: 1000, type: "applybuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 2000, type: "removebuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 3000, type: "applybuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 4000, type: "removebuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 5000, type: "applybuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 6000, type: "removebuff", ability: BUFF},
-  {sourceID: player, targetID: player, timestamp: 8000, type: "applybuff", ability: BUFF},
-];
-
 describe("Rushing Jade Wind", () => {
   let rjw;
   beforeEach(() => {
@@ -33,14 +17,8 @@ describe("Rushing Jade Wind", () => {
       toPlayer: () => true,
       byPlayerPet: () => false,
       toPlayerPet: () => false,
-      fight: {
-        start_time: 0,
-        end_time: 10000,
-      },
-      fightDuration: 10000,
     });
-    rjw.combatants = {selected: talented_combatant};
-    rjw.triggerEvent("initialized");
+    rjw.combatants = {};
   });
 
   it("should be inactive for a user without the talent", () => {
@@ -50,24 +28,8 @@ describe("Rushing Jade Wind", () => {
   });
 
   it("should be active for a user with the talent", () => {
-    // the default is for rjw to use the talent
+    rjw.combatants.selected = talented_combatant;
     rjw.triggerEvent("initialized");
     expect(rjw.active).toBe(true);
-  });
-
-  it("should show 0% uptime if no events have been processed", () => {
-    rjw.owner.fightDuration = 0;
-    expect(rjw.uptime).toBe(0);
-  });
-
-  it("should show 0% uptime if the fight ends without it ever activating", () => {
-    rjw.triggerEvent("finished");
-    expect(rjw.uptime).toBe(0);
-  });
-
-  it("should track uptime via the buff", () => {
-    halfUptime.forEach((event) => rjw.triggerEvent("byPlayer_" + event.type, event));
-    rjw.triggerEvent("finished");
-    expect(rjw.uptime).toBe(0.5);
   });
 });
