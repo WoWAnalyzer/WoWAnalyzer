0742e74cd8dcd889798d2131607a5a7a4fa5deda
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2018-01-01 09:58:31 +0100

    Update RpPoolingDA.js
    Fixed all requests

--- src/Parser/DeathKnight/Unholy/Modules/Features/RpPoolingDA.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/RpPoolingDA.js
@@ -3,6 +3,8 @@ import React from 'react';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import SpellLink from 'common/SpellLink';
+import RESOURCE_TYPES from 'common/RESOURCE_TYPES';
+import Wrapper from 'common/Wrapper';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
@@ -16,43 +18,70 @@ class RpPoolingDA extends Analyzer {
   on_initialized() {
     this.active = this.combatants.selected.hasTalent(SPELLS.DARK_ARBITER_TALENT.id);
   }
+  // used to track how many stacks a target has
+  targets = {};
 
   totalRunicPowerPooled = 0;
   totalDarkArbiterCasts = 0;
+  averageRpPooled = 0;
   currentRP = 0;
   maxRP = 0;
   
-   on_toPlayer_energize(event){
-	   this.currentRP = event.classResources[0]['amount']/10 || 0;
-	   this.maxRP = event.classResources[0]['max']/10;
-   }
+  
+  on_byPlayer_energize(event) {
+    const runicPowerResource = event.classResources.find(resource => resource.type === RESOURCE_TYPES.RUNIC_POWER);
+    if (runicPowerResource) {
+      this.currentRP = (runicPowerResource.amount || 0)/10;
+      this.maxRP = (runicPowerResource.max || 0)/10;
+    }
+  }
+  
+   on_byPlayer_cast(event) {
+    const runicPowerResource = event.classResources.find(resource => resource.type === RESOURCE_TYPES.RUNIC_POWER);
+    if (runicPowerResource) {
+      this.currentRP = (runicPowerResource.amount || 0)/10;
+      this.maxRP = (runicPowerResource.max || 0)/10;
+    }
+  }
    
   on_byPlayer_cast(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.DARK_ARBITER_TALENT.id){
 		this.totalDarkArbiterCasts++;
 		this.totalRunicPowerPooled += this.currentRP;
+		this.averageRpPooled = (this.totalRunicPowerPooled / this.totalDarkArbiterCasts).toFixed(0);
 		}
 	}
+	
+   get suggestionThresholds() {
+    return {
+      actual: (this.totalRunicPowerPooled/this.totalDarkArbiterCasts).toFixed(1),
+      isLessThan: {
+        minor: (this.maxRP-20),
+        average: (this.maxRP-30),
+        major: (this.maxRP-40),
+      },
+      style: 'number',
+      suffix: 'Runic Power',
+    };
+  }
 
   suggestions(when) {
-    const averageRpPooled = (this.totalRunicPowerPooled/this.totalDarkArbiterCasts).toFixed(1);
-    when(averageRpPooled).isLessThan(this.maxRP-20)
+    when(this.suggestionThresholds.actual).isLessThan(this.suggestionThresholds.isLessThan.minor)
  		  .addSuggestion((suggest, actual, recommended) => {
-			return suggest(<span>You are casting <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/> without enough runic power. Make sure to pool some runic power before you cast <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/>.</span>)
+			return suggest(<Wrapper> You are casting <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/> without enough runic power. Make sure to pool some runic power before you cast <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id}/>.</Wrapper>)
 				.icon(SPELLS.DARK_ARBITER_TALENT.icon)
-				.actual(`${averageRpPooled} of runic power were pooled on average`)
+				.actual(`${this.averageRpPooled} of runic power were pooled on average`)
 				.recommended(`>${(recommended)} is recommended`)
 				.regular(recommended - 20).major(recommended - 40);
         });
   }
 
   statistic() {
-    const averageRpPooled = (this.totalRunicPowerPooled/this.totalDarkArbiterCasts).toFixed(0);
     return (
       <StatisticBox
         icon={<SpellIcon id={SPELLS.DARK_ARBITER_TALENT.id} />}
-        value={`${averageRpPooled}/${this.maxRP}`}
+        value={`${this.averageRpPooled}/${this.maxRP}`}
         label={'Average Runic Power Pooled before Dark Arbiter'}
 		tooltip={`A total amount of ${this.totalRunicPowerPooled} runic power was pooled for ${this.totalDarkArbiterCasts} casts of Dark Arbiter`}
       />
