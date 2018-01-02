4af0ba7544cab2a4d6460c663691f9470505726d
J. David Smith <emallson@atlanis.net>
2018-01-02 18:14:10 -0500

    Fixed BoC+KS CDR interaction
    After additional testing, turns out it only applies to the first hit after all.

--- src/Parser/Monk/Brewmaster/Modules/Spells/KegSmash.js
+++ src/Parser/Monk/Brewmaster/Modules/Spells/KegSmash.js
@@ -26,7 +26,6 @@ class KegSmash extends Analyzer {
   _next_target = 0;
 
   _boc_buff_active = false;
-  _boc_apply_to_ks = false;
   
   on_byPlayer_applybuff(event) {
     const spellId = event.ability.guid;
@@ -53,11 +52,26 @@ class KegSmash extends Analyzer {
     const spellId = event.ability.guid;
     if(SPELLS.KEG_SMASH.id === spellId) {
       this.totalCasts += 1;
-      this._boc_apply_to_ks = this._boc_buff_active;
       this._next_target = event.targetID;
+
+      if(this._boc_buff_active) {
+        this.bocHits += 1; // assuming (not a big assumption) that we get ≥ 1 hit per cast
+
+        // possible for minor loss of correctness due to potential gap
+        // between cast and hit, but even on long fights this won't
+        // amount to much
+        const actualBocReduction = this.isb.reduceCooldown(BOC_KEG_SMASH_REDUCTION);
+        this.bocCDR += actualBocReduction;
+        this.wastedBocCDR += BOC_KEG_SMASH_REDUCTION - actualBocReduction;
+      }
     }
   }
 
+  // The only complex part of CDR tracking with Keg Smash is that it has
+  // (effectively) multi-strike with the 'Stave Off' trait -- and that
+  // each additional hit *also* reduces brew cooldowns. However, only
+  // the first hit gets the BoC reduction (so that is handled in _cast). Since KS also hits in an AoE,
+  // we need to be careful not to double- or triple-count its CDR.
   on_byPlayer_damage(event) {
     const spellId = event.ability.guid;
     if(SPELLS.KEG_SMASH.id !== spellId || this._next_target !== event.targetID) {
@@ -68,14 +82,6 @@ class KegSmash extends Analyzer {
     const actualReduction = this.isb.reduceCooldown(KEG_SMASH_REDUCTION);
     this.cdr += actualReduction;
     this.wastedCDR += KEG_SMASH_REDUCTION - actualReduction;
-
-    if(this._boc_apply_to_ks) {
-      this.bocHits += 1;
-
-      const actualBocReduction = this.isb.reduceCooldown(BOC_KEG_SMASH_REDUCTION);
-      this.bocCDR += actualBocReduction;
-      this.wastedBocCDR += BOC_KEG_SMASH_REDUCTION - actualBocReduction;
-    }
   }
 }
 
