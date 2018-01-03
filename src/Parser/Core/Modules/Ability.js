040e171335fc6287c3ca787df5580d062231e36c
Martijn Hols <martijnhols@hotmail.com>
2018-01-03 23:10:41 +0100

    Improve Ability comments

--- src/Parser/Core/Modules/Ability.js
+++ src/Parser/Core/Modules/Ability.js
@@ -1,26 +1,10 @@
 import PropTypes from 'prop-types';
 
-/**
- * Available properties:
- *
- * required spell {object or array of objects} The spell definition with { id, name and icon }, or an array of spell definitions with the same format. If an array of spell definitions is provided, the first element in the array will be what shows in suggestions / cast timeline. Multiple spell definitions in the same ability can be used to tie multiple cast / buff IDs together as the same ability (with a shared cooldown)
- * optional name {string} the name to use if it is different from the name provided by the `spell` object.
- * required category {string} The name of the category to place this spell in, you should usually use the SPELL_CATEGORIES enum for these values.
- * optional cooldown {func} A function to calculate the cooldown of a spell. Parameters provided: `hastePercentage`, `selectedCombatant`
- * optional enabled {func} Whether the spell is enabled (available to the player) and should be displayed. This should only be used for hiding spells that are unavailable, for example due to talents. If you have a spell behaving differently with a legendary for example, you can also add that spell twice and use this property to toggle the one applicable. Parameters provided: `selectedCombatant`
- * optional charges {number} The amount of charges the spell has by default.
- * optional recommendedEfficiency {number} The custom recommended cast efficiency. Default is 80% (0.8).
- * optional suggestion {bool} If this is set to `true`, this spell will trigger a suggestion.
- * optional extraSuggestion {string} Provide additional information in the suggestion.
- *
- * Rarely necessary:
- * optional isUndetectable {bool} A boolean to indicate it can not be detected whether the player his this spells. This makes it so the spell is hidden when there are 0 casts in the fight. This should only be used for spells that can't be detected if a player has access to them, like racials.
- * optional casts {func} A function to get the amount of casts done of a spell. Parameters provided: `castCount`, `parser`
- * optional maxCasts {func} A function to get the max amount of casts for a spell. Parameters provided: `cooldown`, `fightDuration`, `getAbility`, `parser`
- * optional importance {string} If set, this suggestion will get this static importance value. Use this ISSUE_IMPORTANCE enum for this.
- */
 class Ability {
   static propTypes = {
+    /**
+     * REQUIRED The spell definition. If an array of spell definitions is provided, the first element in the array will be what shows in suggestions / cast timeline. Multiple spell definitions in the same ability can be used to tie multiple cast / buff IDs together as the same ability (with a shared cooldown)
+     */
     spell: PropTypes.oneOfType([
       PropTypes.shape({
         id: PropTypes.number.isRequired,
@@ -33,29 +17,75 @@ class Ability {
         icon: PropTypes.string.isRequired,
       })),
     ]),
+    /**
+     * The name to use if it is different from the name provided by the `spell` object.
+     */
     name: PropTypes.string,
+    /**
+     * REQUIRED The name of the category to place this spell in, you should usually use the SPELL_CATEGORIES enum for these values.
+     */
     category: PropTypes.string.isRequired,
+    /**
+     * The cooldown of a spell at the time of the call, this can be a function for more complicated calls or even to check for buffs. Parameters provided: `hastePercentage`, `selectedCombatant`
+     */
     cooldown: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.number,
     ]),
+    /**
+     * NYI, do not use
+     */
     channel: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.number,
     ]),
+    /**
+     * The amount of charges the spell has by default. Reminder: only the first used charge will recharge, so a spell having
+     */
     charges: PropTypes.number,
+    /**
+     * Whether the spell is on the GCD.
+     * If this spell overlaps in the Spell Timeline it likes is incorrectly marked as on the GCD and should be removed.
+     */
     isOnGCD: PropTypes.bool,
+    // TODO: Add properties `staticGCD` and `baseGcd` since the baseGcd can be different per spell (e.g. Brewmaster's Effuse has a 1.5sec base GCD and most other spells are 1sec)
     castEfficiency: PropTypes.shape({
+      /**
+       * If this is set to `true`, this spell will trigger a Cast Efficiency suggestion when the efficiency is below the set threshold (with one of the props below, or the default 80%).
+       */
       suggestion: PropTypes.bool,
+      /**
+       * The custom recommended cast efficiency. Default is 80% (0.8).
+       */
       recommendedEfficiency: PropTypes.number,
       averageIssueEfficiency: PropTypes.number,
       majorIssueEfficiency: PropTypes.number,
+      /**
+       * Extra suggestion text for after the Cast Efficiency suggestion. Use this for example to give the user a reason why.
+       */
       extraSuggestion: PropTypes.node,
+      /**
+       * A function to get the amount of casts done of a spell. Parameters provided: `castCount`, `parser`
+       * DEPRECATED: Usage should be avoided. This may be removed in the future.
+       */
       casts: PropTypes.func,
+      /**
+       * A function to get the max amount of casts for a spell. Parameters provided: `cooldown`, `fightDuration`, `getAbility`, `parser`
+       * DEPRECATED: Usage should be avoided. This may be removed in the future.
+       */
       maxCasts: PropTypes.func,
+      /**
+       * If set, this suggestion will get this static importance value. Use this ISSUE_IMPORTANCE enum for this.
+       */
       importance: PropTypes.string,
     }),
+    /**
+     * Whether the spell is enabled (available to the player) and should be displayed. This should only be used for hiding spells that are unavailable, for example due to talents.
+     */
     enabled: PropTypes.bool,
+    /**
+     * A boolean to indicate it can not be detected whether the player his this spells. This makes it so the spell is hidden when there are 0 casts in the fight. This should only be used for spells that can't be detected if a player has access to them, like racials.
+     */
     isUndetectable: PropTypes.bool,
   };
 
