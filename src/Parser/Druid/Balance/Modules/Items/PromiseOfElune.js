88f8bbd25f8419910e825300f4d71ee3de17999d
Gebuz <johanln@hotmail.dk>
2018-01-05 19:15:58 +0100

    Removed redudant dependencies

--- src/Parser/Druid/Balance/Modules/Items/PromiseOfElune.js
+++ src/Parser/Druid/Balance/Modules/Items/PromiseOfElune.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
-import SpellLink from 'common/SpellLink';
-import { formatPercentage } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
-import Wrapper from 'common/Wrapper';
 import ItemDamageDone from 'Main/ItemDamageDone';
 
 const DAMAGE_MODIFIER = 0.08;
