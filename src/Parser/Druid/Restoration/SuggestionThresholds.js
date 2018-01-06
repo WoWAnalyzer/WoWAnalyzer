256045383d855685a16afa20097baff42a2df383
Kelton Finch <keltonfinch@gmail.com>
2018-01-06 16:55:21 -0500

    [resto druid] Started work on suggestions update

--- src/Parser/Druid/Restoration/SuggestionThresholds.js
+++ src/Parser/Druid/Restoration/SuggestionThresholds.js
@@ -1,16 +1,6 @@
 
 export default {
 
-  ABC_NOT_HEALING: {
-    minor: 0.30,
-    regular: 0.40,
-    major: 1.00,
-  },
-  ABC_NOT_CASTING: {
-    minor: 0.20,
-    regular: 0.35,
-    major: 1.00,
-  },
   LIFEBLOOM_UPTIME: {
     minor: 0.85,
     regular: 0.70,
