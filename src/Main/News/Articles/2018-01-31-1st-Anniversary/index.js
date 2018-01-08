3fa47908d2a84c426e24aeda3fd03a4eab0da40a
Martijn Hols <martijnhols@hotmail.com>
2018-01-08 18:07:52 +0100

    Revert "Remove recap progress for a clean PR"
    This reverts commit 30f5fdd0903cbf6f826ed33cbfd339513efa4770.

---
+++ src/Main/News/Articles/2018-01-31-1st-Anniversary/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import RegularArticle from 'Main/News/RegularArticle';
+
+export default (
+  <RegularArticle title="WoWAnalyzer's first anniversary" published="2017-12-24">
+    NYI
+  </RegularArticle>
+);
