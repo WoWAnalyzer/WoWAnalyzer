30f5fdd0903cbf6f826ed33cbfd339513efa4770
Martijn Hols <martijnhols@hotmail.com>
2018-01-08 17:58:38 +0100

    Remove recap progress for a clean PR

--- src/Main/News/Articles/2018-01-31-1st-Anniversary/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import RegularArticle from 'Main/News/RegularArticle';
-
-export default (
-  <RegularArticle title="WoWAnalyzer's first anniversary" published="2017-12-24">
-    NYI
-  </RegularArticle>
-);
--- src/Main/News/Articles/index.js
+++ src/Main/News/Articles/index.js
@@ -1,5 +1,4 @@
 export default [
-  // require('./2018-01-31-1st-Anniversary').default,
   require('./2017-12-24-Version-2-0').default,
   require('./2017-11-26-Updated-for-Antorus').default,
   require('./2017-10-21-DiscordBot').default,
