543449c58f62de214421690189b929774bab9bfe
Collin Cromwell <collcrom@gmail.com>
2018-01-11 15:30:03 -0600

    added defiled augment rune to casts that arent casts, changed the suggestions in ABC for ret

--- src/Parser/Paladin/Retribution/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Paladin/Retribution/Modules/Features/AlwaysBeCasting.js
@@ -61,23 +61,14 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
   }
 
   suggestions(when) {
-    const {
-      actual,
-      isGreaterThan: {
-        minor,
-        average,
-        major,
-      },
-    } = this.suggestionThresholds;
     const boss = this.owner.boss;
 
     if (!boss || !boss.fight.disableDowntimeSuggestion) {
-      when(actual).isGreaterThan(minor).addSuggestion((suggest, actual, recommended) => {
+      when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
         return suggest(<Wrapper>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. Even if you have to move, try casting something instant with range like <SpellLink id={SPELLS.JUDGMENT_CAST.id} /> or <SpellLink id={SPELLS.DIVINE_STORM.id} /></Wrapper>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(average).major(major);
+          .recommended(`<${formatPercentage(recommended)}% is recommended`);
       });
     }
   }
