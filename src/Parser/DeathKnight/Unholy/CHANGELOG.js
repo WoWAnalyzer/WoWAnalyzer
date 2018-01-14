d4643e2ec6d6d24bcf9675cc01432a37c265721e
Aveline Estié <hello@aveline.be>
2018-01-14 23:21:26 +0000

    DK Changelogs

--- src/Parser/DeathKnight/Unholy/CHANGELOG.js
+++ src/Parser/DeathKnight/Unholy/CHANGELOG.js
@@ -1,4 +1,11 @@
-import {Khazak, Bicepspump} from 'MAINTAINERS';
+import React from 'react';
+
+import { Khazak } from 'MAINTAINERS';
+import Wrapper from 'common/Wrapper';
+import ITEMS from 'common/ITEMS';
+import ItemLink from 'common/ItemLink';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
 
 export default  [
   {
@@ -18,37 +25,37 @@ export default  [
   },
   {
     date: new Date('2017-12-15'),
-    changes: 'Removed Scourge of Worlds tracker after feedback',
+    changes: <Wrapper>Removed <SpellLink id={SPELLS.SCOURGE_OF_WORLDS.id} icon /> tracker after feedback</Wrapper>,
     contributors: [Khazak],
   },
   {
     date: new Date('2017-12-14'),
-    changes: 'Added T21 set bonus',
+    changes: <Wrapper>Added <SpellLink id={SPELLS.UNHOLY_DEATH_KNIGHT_T21_2SET_BONUS.id} icon /> and <SpellLink id={SPELLS.UNHOLY_DEATH_KNIGHT_T21_4SET_BONUS.id} icon /></Wrapper>,
     contributors: [Khazak],
   },
   {
     date: new Date('2017-11-14'),
-    changes: 'Added T20 2 piece bonus tracking',
+    changes: <Wrapper>Added <SpellLink id={SPELLS.UNHOLY_DEATH_KNIGHT_T20_2SET_BONUS.id} icon /> tracking</Wrapper>,
     contributors: [Khazak],
   },
   {
     date: new Date('2017-11-04'),
-    changes: 'Scourge of Worlds, Cold Heart, and Unholy Frenzy trackers added',
+    changes: <Wrapper><SpellLink id={SPELLS.SCOURGE_OF_WORLDS.id} icon />, <ItemLink id={ITEMS.COLD_HEART.id} icon />, and <SpellLink id={SPELLS.UNHOLY_FRENZY_BUFF.id} icon /> trackers added</Wrapper>,
     contributors: [Khazak],
   },
   {
     date: new Date('2017-11-02'),
-    changes: ' cast efficiency fixes',
+    changes: 'Cast efficiency fixes',
     contributors: [Khazak],
   },
   {
     date: new Date('2017-11-01'),
-    changes: 'Added Dark Arbiter Module',
+    changes: <Wrapper>Added <SpellLink id={SPELLS.DARK_ARBITER_TALENT.id} icon /> Module</Wrapper>,
     contributors: [Khazak],
   },
   {
     date: new Date('2017-10-22'),
-    changes: 'Added initial Unholy support by @Khazak',
+    changes: 'Added initial Unholy support',
     contributors: [Khazak],
   },
 ];
