1ae819ac97a60f8bf664199e890657fdacdcca07
kpey <kpey7826@uni.sydney.edu.au>
2018-01-15 02:55:50 +1100

    Added support for Smoldering Heart uptime, including a rough expected uptime based on Maelstrom spent.

--- src/Parser/Shaman/Elemental/CombatLogParser.js
+++ src/Parser/Shaman/Elemental/CombatLogParser.js
@@ -21,6 +21,7 @@ import Tier21_4Set from './Modules/Items/Tier21_4set.js';
 
 import TheDeceiversBloodPact from './Modules/Items/TheDeceiversBloodPact';
 import EchoesOfTheGreatSundering from './Modules/Items/EchoesOfTheGreatSundering';
+import SmolderingHeart from './Modules/Items/SmolderingHeart';
 
 import './Modules/Main/main.css';
 
@@ -50,6 +51,7 @@ class CombatLogParser extends CoreCombatLogParser {
     // Legendaries:
     theDeceiversBloodPact: TheDeceiversBloodPact,
     echoesOfTheGreatSundering: EchoesOfTheGreatSundering,
+    smolderingHeart: SmolderingHeart,
 
     //Setboni
     tier21_2p: Tier21_2Set,
