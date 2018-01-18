d5d2e910c111821b45e2d0e3cac96d4438418c63
Putro <lafa16ac@student.cbs.dk>
2018-01-18 01:22:52 +0100

    udpate channeling helper

--- src/Parser/Hunter/Survival/Modules/Helper/Channeling.js
+++ src/Parser/Hunter/Survival/Modules/Helper/Channeling.js
@@ -2,14 +2,6 @@ import SPELLS from 'common/SPELLS';
 import CoreChanneling from 'Parser/Core/Modules/Channeling';
 import { formatMilliseconds } from 'common/format';
 
-/**
- * Crackling Jade Lightning don't reveal in the combatlog when channeling begins and ends, this fabricates the required events so that ABC can handle it properly.
- * Combatlog event order is messy, it often looks like:
- * 1. applydebuff Crackling Jade Lightning
- * 2. begincast/cast new spell
- * 3. removedebuff Crackling Jade Lightning
- * To avoid Crackling Jade Lightning as being marked "canceled" when we start a new spell we mark it as ended instead on the begincast/cast.
- */
 class Channeling extends CoreChanneling {
   on_byPlayer_cast(event) {
     if (event.ability.guid === SPELLS.FURY_OF_THE_EAGLE_TRAIT.id) {
