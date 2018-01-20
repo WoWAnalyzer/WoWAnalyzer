b45ac557516bcf1b8df6f174526b98f04e3d2423
Snitnikov Nikita <snitnik@gmail.com>
2018-01-20 19:44:17 +0100

    Assasiantion basics

--- src/Parser/Rogue/Assassination/CombatLogParser.js
+++ src/Parser/Rogue/Assassination/CombatLogParser.js
@@ -3,6 +3,13 @@ import CoreCombatLogParser from 'Parser/Core/CombatLogParser';
 import DamageDone from 'Parser/Core/Modules/DamageDone';
 import Abilities from './Modules/Features/Abilities';
 
+import ComboPointDetails from '../Common/Resources/ComboPointDetails';
+import ComboPointTracker from '../Common/Resources/ComboPointTracker';
+import ComboPoints from './Modules/RogueCore/ComboPoints';
+import EnergyDetails from '../Common/Resources/EnergyDetails';
+import EnergyTracker from '../Common/Resources/EnergyTracker';
+import Energy from './Modules/RogueCore/Energy';
+
 
 import MantleOfTheMasterAssassin from '../Common/Legendaries/MantleOfTheMasterAssassin';
 import SoulOfTheShadowblade from '../Common/Legendaries/SoulOfTheShadowblade';
@@ -13,16 +20,17 @@ class CombatLogParser extends CoreCombatLogParser {
   static specModules = {
     //Feature
     damageDone: [DamageDone, { showStatistic: true }],
+    abilities: Abilities,
 
     //Resource
-    // comboPointTracker: ComboPointTracker,
-    // comboPointDetails: ComboPointDetails,
-    // energyTracker: EnergyTracker,
-    // energyDetails: EnergyDetails,
+    comboPointTracker: ComboPointTracker,
+    comboPointDetails: ComboPointDetails,
+    comboPoints: ComboPoints,
+    energyTracker: EnergyTracker,
+    energyDetails: EnergyDetails,
+    energy: Energy,
 
     //Trackers
-    // symbolsDamageTracker: SymbolsDamageTracker,
-    // danceDamageTracker: DanceDamageTracker,
     // mantleDamageTracker: MantleDamageTracker,
 
     //Core
