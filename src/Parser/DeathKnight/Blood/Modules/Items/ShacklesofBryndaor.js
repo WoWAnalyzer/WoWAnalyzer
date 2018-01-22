8b20ad1d2c41668496ba63e9febdabb8c27b1fbf
yajinni <yajinni@gmail.com>
2018-01-22 18:07:40 -0500

    Added possible death strike gained from extra RP

--- src/Parser/DeathKnight/Blood/Modules/Items/ShacklesofBryndaor.js
+++ src/Parser/DeathKnight/Blood/Modules/Items/ShacklesofBryndaor.js
@@ -17,10 +17,18 @@ class ShacklesofBryndaor extends Analyzer {
 
   on_initialized() {
     this.active = this.combatants.selected.hasWrists(ITEMS.SHACKLES_OF_BRYNDAOR.id);
+    if (this.combatants.selected.hasTalent(SPELLS.OSSUARY_TALENT.id)) {
+      this.dsCost -= 5;
+    }
+    if (this.combatants.selected.hasBuff(SPELLS.BLOOD_DEATH_KNIGHT_T20_2SET_BONUS_BUFF.id)) {
+      this.dsCost -= 5;
+    }
   }
 
   rpGained=0;
   rpPercent=0;
+  dsCost = 45;
+  extraDS=0;
 
   on_toPlayer_energize(event) {
     if (event.ability.guid !== SPELLS.SHACKLES_OF_BRYNDAOR_BUFF.id) {
@@ -30,14 +38,18 @@ class ShacklesofBryndaor extends Analyzer {
   }
 
 
+
+
   item() {
     this.rpPercent=this.rpGained / this.runicPowerTracker.totalRPGained;
+    this.extraDS=this.rpGained /this.dsCost;
     return {
       item: ITEMS.SHACKLES_OF_BRYNDAOR,
       result:(
         <Wrapper>
           Runic Power Refunded : {this.rpGained}<br />
           {formatPercentage(this.rpPercent)} % of total RP <br />
+          Extra Death Strikes possible: {Math.trunc(this.extraDS)}
 
         </Wrapper>
       ),
