68c2d5b780a69d79974b0e4fd3fa872e3a0b5c67
yajinni <yajinni@gmail.com>
2018-01-22 12:50:29 -0500

    Further optimizations
    Most importantly reworked the reseting of stacks to be more accurate.

--- src/Parser/DeathKnight/Blood/Modules/Items/SkullflowersHaemostasis.js
+++ src/Parser/DeathKnight/Blood/Modules/Items/SkullflowersHaemostasis.js
@@ -6,6 +6,8 @@ import ITEMS from 'common/ITEMS';
 import ItemDamageDone from 'Main/ItemDamageDone';
 import ItemHealingDone from 'Main/ItemHealingDone';
 import Wrapper from 'common/Wrapper';
+import calculateEffectiveDamage from 'Parser/Core/calculateEffectiveDamage';
+import calculateEffectiveHealing from 'Parser/Core/calculateEffectiveHealing';
 
 class SkullflowersHaemostasis extends Analyzer {
 
@@ -18,18 +20,19 @@ class SkullflowersHaemostasis extends Analyzer {
   }
 
   buffStack = 0;
-  savedBuffStack=0;
   wastedBuff = 0;
   maxBuffStacks = 5;
   damage=0;
   heal=0;
-  percentBuff=1/6;
+  percentBuff=0.2;
+  deathStrikeDamageIsDone = false;
+  deathStrikeHealingIsDone = false;
+
 
 
   on_byPlayer_applybuff(event) {
     if (event.ability.guid === SPELLS.HAEMOSTASIS_BUFF.id) {
       this.buffStack += 1;
-      this.savedBuffStack=this.buffStack;
     }
   }
 
@@ -38,18 +41,10 @@ class SkullflowersHaemostasis extends Analyzer {
       if (this.buffstack >= this.maxBuffStacks) {
         this.wastedBuff += 1;
         this.buffStack = this.maxBuffStacks;
-        this.savedBuffStack=this.buffStack;
       }
       else {
         this.buffStack += 1;
-        this.savedBuffStack=this.buffStack;
       }
-    }
-  }
-
-  on_byPlayer_removebuff(event) {
-    if (event.ability.guid === SPELLS.HAEMOSTASIS_BUFF.id) {
-      this.buffStack = 0;
     }
   }
 
@@ -57,14 +52,26 @@ class SkullflowersHaemostasis extends Analyzer {
     if (event.ability.guid !== SPELLS.DEATH_STRIKE_HEAL.id) {
       return;
     }
-    this.heal += (event.amount + (event.absorbed || 0)) * this.percentBuff * this.savedBuffStack;
+    this.heal += calculateEffectiveHealing(event,this.percentBuff * this.buffStack);
+    this.deathStrikeHealingIsDone = true;
+    if (this.deathStrikeHealingIsDone && this.deathStrikeDamageIsDone === true) {
+      this.buffStack = 0;
+      this.deathStrikeDamageIsDone = false;
+      this.deathStrikeHealingIsDone = false;
+    }
   }
 
   on_byPlayer_damage(event) {
     if (event.ability.guid !== SPELLS.DEATH_STRIKE.id) {
       return;
     }
-    this.damage += (event.amount + (event.absorbed || 0)) * this.percentBuff * this.savedBuffStack;
+    this.damage += calculateEffectiveDamage(event,this.percentBuff * this.buffStack);
+    this.deathStrikeDamageIsDone = true;
+    if (this.deathStrikeHealingIsDone && this.deathStrikeDamageIsDone === true) {
+      this.buffStack = 0;
+      this.deathStrikeDamageIsDone = false;
+      this.deathStrikeHealingIsDone = false;
+    }
   }
 
 
