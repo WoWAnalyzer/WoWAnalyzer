4716c71d73a3a67bb1348882c33f262dc1d40548
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2018-01-24 18:39:29 +0100

    [Windwalker] Adding statistic showing actual casts vs expected casts during SEF or Serenity

--- src/Parser/Monk/Windwalker/Modules/Spells/StormEarthAndFire.js
+++ src/Parser/Monk/Windwalker/Modules/Spells/StormEarthAndFire.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+import SpellIcon from 'common/SpellIcon';
+
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import SPELLS from 'common/SPELLS';
@@ -6,6 +10,12 @@ class StormEarthAndFire extends Analyzer{
   static dependencies = {
     combatants: Combatants,
   };
+
+  risingSunKicks = 0;
+  fistsOfFuries = 0;
+  strikeOfTheWindlords = 0;
+  whirlingDragonPunches = 0;
+  sefCount = 0;
 
   get traitsCDReduction() {
     let traitsCDReduction = 0;
@@ -36,6 +46,57 @@ class StormEarthAndFire extends Analyzer{
     const reducedCooldownWithTraits = 90 - this.traitsCDReduction;
     return reducedCooldownWithTraits;
   }
+
+  on_toPlayer_applybuff(event) {
+    const spellId = event.ability.guid;
+    if (SPELLS.STORM_EARTH_AND_FIRE_CAST.id === spellId || SPELLS.SERENITY_TALENT.id === spellId) {
+      this.sefCount += 1;
+    }
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+
+    if (!this.combatants.selected.hasBuff(SPELLS.STORM_EARTH_AND_FIRE_CAST.id) && !this.combatants.selected.hasBuff(SPELLS.SERENITY_TALENT.id)) {
+      return;
+    }
+    if (SPELLS.RISING_SUN_KICK.id === spellId) {
+      this.risingSunKicks += 1;
+    }
+    if (SPELLS.FISTS_OF_FURY_CAST.id === spellId) {
+      this.fistsOfFuries += 1;
+    }
+    if (SPELLS.STRIKE_OF_THE_WINDLORD.id === spellId) {
+      this.strikeOfTheWindlords += 1;
+    }
+    if (SPELLS.WHIRLING_DRAGON_PUNCH_TALENT.id === spellId) {
+      this.whirlingDragonPunches += 1;
+    }
+  }
+
+
+  statistic() {
+   const icon = this.combatants.selected.hasTalent(SPELLS.SERENITY_TALENT.id) ? SPELLS.SERENITY_TALENT : SPELLS.STORM_EARTH_AND_FIRE_CAST;
+   return (
+     <StatisticBox
+       icon={<SpellIcon id={icon.id} />}
+       label={
+          <div>
+          During your {this.sefCount} {icon.name}s you cast: 
+          <li>{this.risingSunKicks}/{this.sefCount*2+1} Rising Sun kicks</li> 
+          <li>{this.fistsOfFuries}/{this.sefCount} Fists of Furies</li>
+          <li>{this.strikeOfTheWindlords}/{this.sefCount} Strikes of the Windlord</li>
+              {this.whirlingDragonPunches > 0
+              ? <li>{this.whirlingDragonPunches}/{this.sefCount} Whirling Dragon Punches</li> : ''
+              }
+        </div>
+       }
+     />
+   );
+  }
+statisticOrder = STATISTIC_ORDER.CORE(14);
+
+
 }
 
 export default StormEarthAndFire;
