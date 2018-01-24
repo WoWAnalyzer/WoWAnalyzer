8262e9c1f262989b0e0511ea7f1923da96f76d1d
Collin Cromwell <collcrom@gmail.com>
2018-01-24 13:43:39 -0600

    removed DoG suggestion

--- src/Parser/DemonHunter/Havoc/Modules/Items/DelusionsOfGrandeur.js
+++ src/Parser/DemonHunter/Havoc/Modules/Items/DelusionsOfGrandeur.js
@@ -31,27 +31,6 @@ class DelusionsOfGrandeur extends Analyzer {
 		return this.furyTracker.cooldownReductionWasted / this.furyTracker.cooldownReduction;
 	}
 
-	get suggestionThresholds() {
-    return {
-      actual: this.percentCDRWasted,
-      isGreaterThan: {
-        minor: 0.05,
-        average: 0.15,
-        major: 0.25,
-      },
-      style: 'percentage',
-    };
-  }
-
-  suggestions(when) {
-  	when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
-  		return suggest(<Wrapper>You wasted {formatPercentage(actual)}% of the cooldown reduction from <ItemLink id={ITEMS.DELUSIONS_OF_GRANDEUR.id} icon/>. Make sure to be using <SpellLink id={SPELLS.METAMORPHOSIS_HAVOC.id} icon/> on cooldown with this item.</Wrapper>)
-  			.icon(ITEMS.DELUSIONS_OF_GRANDEUR.icon)
-  			.actual(`${formatNumber(this.furyTracker.cooldownReductionWasted)} seconds wasted`)
-  			.recommended(`Wasting less than ${formatPercentage(recommended)}% is recommended.`);
-  	});
-  }
-
 	item() {
 		return {
 			item: ITEMS.DELUSIONS_OF_GRANDEUR,
