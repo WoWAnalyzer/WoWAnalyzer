87b284138307087843322236a51c0017985f8ec9
Anatta336 <sdriver@gmx.co.uk>
2018-01-26 21:39:29 +0000

    update new-style changelog

--- src/Parser/Druid/Feral/CHANGELOG.js
+++ src/Parser/Druid/Feral/CHANGELOG.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import { Thieseract, Anatta336 } from 'MAINTAINERS';
@@ -7,6 +6,11 @@ import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 
 export default [
+  { 
+    date: new Date('2018-01-26'),
+    changes: <Wrapper>Added low energy <SpellLink id={SPELLS.FEROCIOUS_BITE.id} /> tracking</Wrapper>,
+    contributors: [Anatta336],
+  },
   { 
     date: new Date('2018-01-24'),
     changes: <Wrapper>Added <SpellLink id={SPELLS.ASHAMANES_RIP.id} /> uptime tracking</Wrapper>,
@@ -63,19 +67,3 @@ export default [
     contributors: [Thieseract],
   },
 ];
-=======
-export default `
-26-01-2018 - Feral Druid: Added low energy Ferocious Bite tracking (by Anatta336).
-24-01-2018 - Feral Druid: Added Ashamane's Rip uptime tracking (by Anatta336).
-22-09-2019 - Feral Druid: Added 5 combo point finisher tracking (by Thieseract).
-11-09-2019 - Feral Druid: Refactored combo point tracking properties (by Thieseract).
-11-09-2019 - Feral Druid: Added Savage Roar damage contribution (by Thieseract).
-10-09-2019 - Feral Druid: Changed exceptions to conditional assignment on combo point tracker (by Thieseract).
-06-09-2017 - Feral Druid: Updated combo point tracking to handle non standard generator and spenders (by Thieseract).
-04-09-2017 - Feral Druid: Cleaned up this.owner modules where possible (by Thieseract).
-04-09-2017 - Feral Druid: Added combo point tracking (by Thieseract).
-04-09-2017 - Feral Druid: Added bleed uptime tracking (by Thieseract).
-04-09-2017 - Feral Druid: CooldownTracker configured (by Thieseract).
-03-09-2017 - Feral Druid: Initial commit. Damage done, ABC, and Abilities added (by Thieseract).
-`;
->>>>>>> add tracking of low energy ferocious bite use
