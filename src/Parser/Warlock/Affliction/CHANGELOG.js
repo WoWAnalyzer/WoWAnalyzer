ec499a0a8c5564dad530c73e5796a0564b5bcea6
Lukas Stuchlik <stuchlik1@seznam.cz>
2018-01-28 23:10:33 +0100

    Upgraded the spec completeness to Good.

--- src/Parser/Warlock/Affliction/CHANGELOG.js
+++ src/Parser/Warlock/Affliction/CHANGELOG.js
@@ -9,6 +9,11 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
+    date: new Date('2018-01-28'),
+    changes: <Wrapper>Upgrade the spec completeness to Good!</Wrapper>,
+    contributors: [Chizu],
+  },
+  {
     date: new Date('2018-01-27'),
     changes: <Wrapper>Updated the Checklist to show a Rule about buffing <SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id} icon/>, low mana Rule and also refined some texts.</Wrapper>,
     contributors: [Chizu],
--- src/Parser/Warlock/Affliction/CONFIG.js
+++ src/Parser/Warlock/Affliction/CONFIG.js
@@ -12,14 +12,17 @@ import CHANGELOG from './CHANGELOG';
 export default {
   spec: SPECS.AFFLICTION_WARLOCK,
   maintainers: [Chizu],
-  completeness: SPEC_ANALYSIS_COMPLETENESS.NEEDS_MORE_WORK, // good = it matches most common manual reviews in class discords, great = it support all important class features
+  completeness: SPEC_ANALYSIS_COMPLETENESS.GOOD, // good = it matches most common manual reviews in class discords, great = it support all important class features
   description: (
     <div>
-      Hello fellow Netherlords! While I gotta admit this tool feels more like a statistic than something that really helps you (just yet!), I hope it still is useful to you. Any suggestions as to what could be useful to see are welcome and I'll try to implement them in order for this tool to be more than just a glorified WCL log. <br /> <br />
+      Hello fellow Netherlords! With some help from a theorycrafter from Warlock Discord, we've put together this fight to help you improve your gameplay. It should be fine for you generally, but it will be even more useful in an expert's hands. <br /> <br />
 
-      As for a general rule of thumb - don't overcap your shards, try to get them wherever you can, snipe shards from adds when they're present. Always try to buff your <SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id}/>s as much as possible, with <SpellLink id={SPELLS.DRAIN_SOUL.id}/>, <SpellLink id={SPELLS.REAP_SOULS.id}/> or <SpellLink id={SPELLS.HAUNT_TALENT.id}/>. Don't let your dots fall off and try to move as least as you can (but don't ignore encounter mechanics... too much, hehe). <br /> <br />
+      As for a general rule of thumb - don't overcap your shards, try to get them wherever you can, snipe shards from adds when they're present. Always try to buff your
+      <SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id} icon/>s as much as possible, with <SpellLink id={SPELLS.DRAIN_SOUL.id} icon/>, <SpellLink id={SPELLS.REAP_SOULS.id} icon/> or
+      <SpellLink id={SPELLS.HAUNT_TALENT.id} icon/>. Don't let your dots fall off and try to move as least as you can (but don't ignore encounter mechanics... too much, hehe). <br /> <br />
 
-      If you have any questions about Warlocks, feel free to pay a visit to <a href="https://goo.gl/7PH6Bn" target="_blank" rel="noopener noreferrer">Council of the Black Harvest Discord</a>, if you'd like to discuss anything about this analyzer, leave a message on the GitHub issue or message me @Chizu on WoWAnalyzer Discord.
+      If you have any questions about Warlocks, feel free to pay a visit to <a href="https://goo.gl/7PH6Bn" target="_blank" rel="noopener noreferrer">Council of the Black Harvest Discord</a>
+      or <a href="http://lockonestopshop.com" target="_blank" rel="noopener noreferrer">Lock One Stop Shop</a>, if you'd like to discuss anything about this analyzer, leave a message on the GitHub issue or message me @Chizu on WoWAnalyzer Discord.
     </div>
   ),
   changelog: CHANGELOG,
