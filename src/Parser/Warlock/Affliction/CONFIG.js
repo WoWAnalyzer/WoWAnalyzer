d5613912617ac9d3ddf5ee74bfe324e4cf849c74
Lukáš Stuchlík <sMteX@users.noreply.github.com>
2018-01-29 04:15:29 +0100

    Typo

--- src/Parser/Warlock/Affliction/CONFIG.js
+++ src/Parser/Warlock/Affliction/CONFIG.js
@@ -15,14 +15,14 @@ export default {
   completeness: SPEC_ANALYSIS_COMPLETENESS.GREAT, // good = it matches most common manual reviews in class discords, great = it support all important class features
   description: (
     <div>
-      Hello fellow Netherlords! With some help from a theorycrafter from Warlock Discord, we've put together this fight to help you improve your gameplay. It should be fine for you generally, but it will be even more useful in an expert's hands. <br /> <br />
+      Hello fellow Netherlords! With some help from a theorycrafter from Warlock Discord, we've put together this tool to help you improve your gameplay. It should be fine for you generally, but it will be even more useful in an expert's hands. <br /> <br />
 
       As for a general rule of thumb - don't overcap your shards, try to get them wherever you can, snipe shards from adds when they're present. Always try to buff your
       <SpellLink id={SPELLS.UNSTABLE_AFFLICTION_CAST.id} icon/>s as much as possible, with <SpellLink id={SPELLS.DRAIN_SOUL.id} icon/>, <SpellLink id={SPELLS.REAP_SOULS.id} icon/> or
       <SpellLink id={SPELLS.HAUNT_TALENT.id} icon/>. Don't let your dots fall off and try to move as least as you can (but don't ignore encounter mechanics... too much, hehe). <br /> <br />
 
       If you have any questions about Warlocks, feel free to pay a visit to <a href="https://goo.gl/7PH6Bn" target="_blank" rel="noopener noreferrer">Council of the Black Harvest Discord</a>
-      or <a href="http://lockonestopshop.com" target="_blank" rel="noopener noreferrer">Lock One Stop Shop</a>, if you'd like to discuss anything about this analyzer, leave a message on the GitHub issue or message me @Chizu on WoWAnalyzer Discord.
+      or <a href="http://lockonestopshop.com" target="_blank" rel="noopener noreferrer">Lock One Stop Shop</a>, if you'd like to discuss anything about this analyzer, message me @Chizu on WoWAnalyzer Discord.
     </div>
   ),
   changelog: CHANGELOG,
