aa6f6e7f13d2044c6745984c07026df27a534439
J. David Smith <emallson@atlanis.net>
2018-01-29 01:42:23 -0500

    Rewrote StaggerPoolGraph with Chart.js
    This involved adding a dependency (chartjs-plugin-annotation) to handle
    the annotations I'd been using with Chartify

--- src/Parser/Monk/Brewmaster/Modules/Features/StaggerPoolGraph.js
+++ src/Parser/Monk/Brewmaster/Modules/Features/StaggerPoolGraph.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import ChartistGraph from 'react-chartist';
-import Chartist from 'chartist';
-import 'chartist-plugin-legend';
+import { Line as LineChart } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
 import Analyzer from 'Parser/Core/Analyzer';
 import Tab from 'Main/Tab';
 
-import specialEventIndicators from 'Main/Chartist/specialEventIndicators';
 import { formatDuration } from 'Main/ManaLevelGraph';
 import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS';
-
-import './StaggerPoolGraph.css';
 
 import StaggerFabricator from '../Core/StaggerFabricator';
 
@@ -22,6 +17,7 @@ class StaggerPoolGraph extends Analyzer {
   
   _staggerEvents = [];
   _purifyTimestamps = [];
+  _deathTimestamps = [];
 
   on_addstagger(event) {
     this._staggerEvents.push(event);
@@ -35,6 +31,10 @@ class StaggerPoolGraph extends Analyzer {
     }
   }
 
+  on_toPlayer_death(event) {
+    this._deathTimestamps.push(event.timestamp);
+  }
+
   plot() {
     const labels = Array.from({length: Math.ceil(this.owner.fightDuration / 1000)}, (x, i) => i);
     const poolBySeconds = labels.reduce((obj, sec) => {
@@ -42,84 +42,120 @@ class StaggerPoolGraph extends Analyzer {
       return obj;
     }, {});
 
-    const purifiesBySeconds = labels.reduce((obj, sec) => {
-      obj[sec] = undefined;
-      return obj;
-    }, {});
+    const purifies = this._purifyTimestamps.map(timestamp => Math.floor((timestamp - this.owner.fight.start_time) / 1000));
+    const deaths = this._deathTimestamps.map(timestamp => Math.floor((timestamp - this.owner.fight.start_time) / 1000));
 
     this._staggerEvents.forEach(({ timestamp, newPooledDamage }) => {
       const seconds = Math.floor((timestamp - this.owner.fight.start_time) / 1000);
       poolBySeconds[seconds] = newPooledDamage;
-      purifiesBySeconds[seconds] = undefined; // intentionally set to undefined for `specialEventIndicators`
-    });
-    this._purifyTimestamps.forEach(timestamp => {
-      const seconds = Math.floor((timestamp - this.owner.fight.start_time) / 1000);
-      purifiesBySeconds[seconds] = true; // note that we can't have two purifies in a second due to brew gcd
     });
 
+    // fix stagger after deaths
     let lastPoolContents = 0;
     for(const label in labels) {
       if(poolBySeconds[label] === undefined) {
         poolBySeconds[label] = lastPoolContents;
-        continue;
+      } else {
+        lastPoolContents = poolBySeconds[label];
       }
-
-      lastPoolContents = poolBySeconds[label];
+      if(deaths.includes(Number(label))) {
+        lastPoolContents = 0;
+      }
     }
 
     const chartData = {
       labels,
-      series: [ 
+      datasets: [ 
         {
-          className: 'stagger-pool',
-          name: 'Stagger Pool Contents',
+          label: 'Stagger Pool Contents',
           data: Object.values(poolBySeconds),
+          backgroundColor: 'rgba(240, 234, 214, 0.2)',
+          borderColor: 'rgb(240, 234, 214)',
+          borderWidth: 2,
+        },
+        // add labels to legend
+        {
+          label: 'Purifying Brew Cast',
+          backgroundColor: '#00ff96',
+          data: [],
+          fillOpacity: 0.2,
         },
         {
-          className: 'purifies',
-          name: 'Purifying Brew Cast',
-          data: Object.values(purifiesBySeconds),
+          label: 'Player Deaths',
+          backgroundColor: 'rgb(183, 76, 75)',
+          data: [],
+          fillOpacity: 0.2,
         },
       ],
     };
 
+    function annotations(values, color) {
+      return values.map(index => { 
+        return {
+          type: 'line',
+          mode: 'vertical',
+          borderColor: color,
+          borderWidth: 2,
+          value: index,
+          scaleID: 'x-axis-0',
+        };
+      });
+    }
+
     return (
       <div className="graph-container">
-        <ChartistGraph
-          type="Line"
+        <LineChart
           data={chartData}
+          height={100}
+          width={300}
           options={{
-            showArea: true,
-            showPoint: false,
-            fullWidth: true,
-            height: '350px',
-            lineSmooth: Chartist.Interpolation.simple({
-              fillHoles: true,
-            }),
-            axisX: {
-              labelInterpolationFnc: function skipLabels(seconds) {
-                if (seconds % 30 === 0) {
-                  return formatDuration(seconds);
-                }
-                return null;
-              },
-              offset: 15,
-            },
-            axisY: {
-              onlyInteger: false,
-              offset: 75,
-              labelInterpolationFnc: function skipLabels(dmg) {
-                return formatNumber(dmg);
+            tooltips: {
+              callbacks: {
+                label: function(tooltipItem, data) {
+                  return `Damage in Stagger Pool: ${formatNumber(tooltipItem.yLabel)}`;
+                },
               },
             },
-            plugins: [ 
-              Chartist.plugins.legend({
-                classNames: [ 'stagger-pool', 'purifies' ],
-              }),
-              specialEventIndicators({ series: [ 'purifies' ]}),
-            ],
+            legend: {
+              labels: {
+                fontColor: '#ccc',
+              },
+            },
+            animation: {
+              duration: 0,
+            },
+            elements: {
+              line: {
+                tension: 0,
+              },
+              point: {
+                radius: 0,
+              },
+            },
+            scales: {
+              xAxes: [{
+                labelString: 'Time',
+                ticks: {
+                  fontColor: '#ccc',
+                  callback: formatDuration,
+                },
+              }],
+              yAxes: [{
+                labelString: 'Damage in Stagger Pool',
+                ticks: {
+                  fontColor: '#ccc',
+                  callback: formatNumber,
+                },
+              }],
+            },
+            annotation: {
+              annotations: [
+                ...annotations(purifies, 'rgba(0, 255, 150, 0.5)'),
+                ...annotations(deaths, 'rgba(183, 76, 75, 0.8)'),
+              ],
+            },
           }}
-          />
+        />
       </div>
     );
   }
