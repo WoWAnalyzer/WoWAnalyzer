62c25e8734a16c0ef6f39c16755a8c7d848eae4c
Martijn Hols <martijnhols@hotmail.com>
2018-02-02 00:01:13 +0100

    Function components actually aren't considered pure???

--- src/Main/Results/Header.js
+++ src/Main/Results/Header.js
@@ -6,33 +6,40 @@ import getBossName from 'common/getBossName';
 
 import SkullRaidMarker from './Images/skull-raidmarker.png';
 
-const Headers = ({ spec, playerName, boss, fight }) => (
-  <header>
-    <div className={`player ${spec.className.replace(' ', '')}`}>
-      <img src={`/specs/${spec.className.replace(' ', '')}-${spec.specName.replace(' ', '')}.jpg`} alt="Player avatar" />{' '}
-      <Textfit mode="single" max={80}>
-        {playerName}
-      </Textfit>
-    </div>
-    <div className="versus">versus</div>
-    <div className="boss">
-      <img src={boss ? boss.headshot : SkullRaidMarker} alt="Boss avatar" />
-      <Textfit mode="single" max={80}>
-        {getBossName(fight)}
-      </Textfit>
-    </div>
-  </header>
-);
-Headers.propTypes = {
-  spec: PropTypes.shape({
-    className: PropTypes.string.isRequired,
-    specName: PropTypes.string.isRequired,
-  }).isRequired,
-  playerName: PropTypes.string.isRequired,
-  boss: PropTypes.shape({
-    headshot: PropTypes.string.isRequired,
-  }),
-  fight: PropTypes.object.isRequired,
-};
+class Headers extends React.PureComponent {
+  static propTypes = {
+    spec: PropTypes.shape({
+      className: PropTypes.string.isRequired,
+      specName: PropTypes.string.isRequired,
+    }).isRequired,
+    playerName: PropTypes.string.isRequired,
+    boss: PropTypes.shape({
+      headshot: PropTypes.string.isRequired,
+    }),
+    fight: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { spec, playerName, boss, fight } = this.props;
+
+    return (
+      <header>
+        <div className={`player ${spec.className.replace(' ', '')}`}>
+          <img src={`/specs/${spec.className.replace(' ', '')}-${spec.specName.replace(' ', '')}.jpg`} alt="Player avatar" />{' '}
+          <Textfit mode="single" max={80}>
+            {playerName}
+          </Textfit>
+        </div>
+        <div className="versus">versus</div>
+        <div className="boss">
+          <img src={boss ? boss.headshot : SkullRaidMarker} alt="Boss avatar" />
+          <Textfit mode="single" max={80}>
+            {getBossName(fight)}
+          </Textfit>
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Headers;
