7ecbf99f4be98a716a652ffe558fc0a4de44ebb1
Putro <lafa16ac@student.cbs.dk>
2018-02-02 15:47:36 +0100

    changelog and tooltip

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -9,6 +9,11 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
+    date: new Date('2018-02-02'),
+    changes: <Wrapper>Updated <ItemLink id={ITEMS.SEPHUZS_SECRET.id} icon /> to also showcase the multiplicative effect that is added as part of the baseline and the proc on top of the flat bonuses</Wrapper>,
+    contributors: [Putro],
+  },
+  {
     date: new Date('2018-01-28'),
     changes: <Wrapper>Added <i>How It's Made</i>, <i>How To Change</i>, <i>Spec Maintainer</i> and <i>State Of The Spec</i> boxes to the results page.</Wrapper>,
     contributors: [Zerotorescue],
