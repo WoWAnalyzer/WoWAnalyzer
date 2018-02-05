7d74680db699d29dd1424cce0a84bc80474cd37c
Putro <lafa16ac@student.cbs.dk>
2018-02-05 17:29:24 +0100

    change constants around

--- src/Parser/Hunter/Shared/Modules/Items/CallOfTheWild.js
+++ src/Parser/Hunter/Shared/Modules/Items/CallOfTheWild.js
@@ -20,15 +20,16 @@ const EMBRACE_CDR = 0.2;
 const PATHFINDER_CDR = 1 / 3;
 const COTW_CDR = 0.35;
 
-let BASE_EAGLE_COOLDOWN = 120000;
-let BASE_CHEETAH_COOLDOWN = 180000;
-let BASE_TURTLE_COOLDOWN = 180000;
-const BASE_WILD_COOLDOWN = 120000;
-
 class CallOfTheWild extends Analyzer {
   static dependencies = {
     combatants: Combatants,
   };
+
+  //the base values that we can change as needed
+  baseEagleCooldown = 120000;
+  baseCheetahCooldown = 180000;
+  baseTurtleCooldown = 180000;
+  baseWildCooldown = 120000;
 
   //used later when I set their CDs with the bracers equipped
   eagleCooldownWithBracers = 0;
@@ -52,18 +53,18 @@ class CallOfTheWild extends Analyzer {
     this.active = this.combatants.selected.hasWrists(ITEMS.CALL_OF_THE_WILD.id);
     //handles preemptively lowered cooldown if the SV player has this trait
     if (this.combatants.selected.traitsBySpellId[SPELLS.EMBRACE_OF_THE_ASPECTS.id]) {
-      BASE_EAGLE_COOLDOWN *= (1 - EMBRACE_CDR);
-      BASE_CHEETAH_COOLDOWN *= (1 - EMBRACE_CDR);
-      BASE_TURTLE_COOLDOWN *= (1 - EMBRACE_CDR);
+      this.baseEagleCooldown *= (1 - EMBRACE_CDR);
+      this.baseCheetahCooldown *= (1 - EMBRACE_CDR);
+      this.baseTurtleCooldown *= (1 - EMBRACE_CDR);
     }
     //handles preemptively lowered cooldown if the BM player has this trait
     if (this.combatants.selected.traitsBySpellId[SPELLS.PATHFINDER_TRAIT.id]) {
-      BASE_CHEETAH_COOLDOWN *= (1 - PATHFINDER_CDR);
+      this.baseCheetahCooldown *= (1 - PATHFINDER_CDR);
     }
-    this.eagleCooldownWithBracers = BASE_EAGLE_COOLDOWN * (1 - COTW_CDR);
-    this.cheetahCooldownWithBracers = BASE_CHEETAH_COOLDOWN * (1 - COTW_CDR);
-    this.turtleCooldownWithBracers = BASE_TURTLE_COOLDOWN * (1 - COTW_CDR);
-    this.wildCooldownWithBracers = BASE_WILD_COOLDOWN * (1 - COTW_CDR);
+    this.eagleCooldownWithBracers = this.baseEagleCooldown * (1 - COTW_CDR);
+    this.cheetahCooldownWithBracers = this.baseCheetahCooldown * (1 - COTW_CDR);
+    this.turtleCooldownWithBracers = this.baseTurtleCooldown * (1 - COTW_CDR);
+    this.wildCooldownWithBracers = this.baseWildCooldown * (1 - COTW_CDR);
   }
 
   on_byPlayer_cast(event) {
@@ -72,27 +73,27 @@ class CallOfTheWild extends Analyzer {
       return;
     }
     if (spellId === SPELLS.ASPECT_OF_THE_EAGLE.id) {
-      if (this.lastEagleCastTime && BASE_EAGLE_COOLDOWN > (event.timestamp - this.lastEagleCastTime)) {
-        this.eagleCDR += Math.max((BASE_EAGLE_COOLDOWN - (event.timestamp - this.lastEagleCastTime)), 0);
+      if (this.lastEagleCastTime && this.baseEagleCooldown > (event.timestamp - this.lastEagleCastTime)) {
+        this.eagleCDR += Math.max((this.baseEagleCooldown - (event.timestamp - this.lastEagleCastTime)), 0);
       }
       this.lastEagleCastTime = event.timestamp;
     }
     if (spellId === SPELLS.ASPECT_OF_THE_CHEETAH.id) {
-      if (this.lastCheetahCastTime && BASE_CHEETAH_COOLDOWN > (event.timestamp - this.lastCheetahCastTime)) {
-        this.cheetahCDR += Math.max((BASE_CHEETAH_COOLDOWN - (event.timestamp - this.lastCheetahCastTime)), 0);
+      if (this.lastCheetahCastTime && this.baseCheetahCooldown > (event.timestamp - this.lastCheetahCastTime)) {
+        this.cheetahCDR += Math.max((this.baseCheetahCooldown - (event.timestamp - this.lastCheetahCastTime)), 0);
 
       }
       this.lastCheetahCastTime = event.timestamp;
     }
     if (spellId === SPELLS.ASPECT_OF_THE_TURTLE.id) {
-      if (this.lastTurtleCastTime && BASE_TURTLE_COOLDOWN > (event.timestamp - this.lastTurtleCastTime)) {
-        this.turtleCDR += Math.max((BASE_TURTLE_COOLDOWN - (event.timestamp - this.lastTurtleCastTime)), 0);
+      if (this.lastTurtleCastTime && this.baseTurtleCooldown > (event.timestamp - this.lastTurtleCastTime)) {
+        this.turtleCDR += Math.max((this.baseTurtleCooldown - (event.timestamp - this.lastTurtleCastTime)), 0);
       }
       this.lastTurtleCastTime = event.timestamp;
     }
     if (spellId === SPELLS.ASPECT_OF_THE_WILD.id) {
-      if (this.lastWildCastTime && BASE_WILD_COOLDOWN > (event.timestamp - this.lastWildCastTime)) {
-        this.wildCDR += Math.max((BASE_WILD_COOLDOWN - (event.timestamp - this.lastWildCastTime)), 0);
+      if (this.lastWildCastTime && this.baseWildCooldown > (event.timestamp - this.lastWildCastTime)) {
+        this.wildCDR += Math.max((this.baseWildCooldown - (event.timestamp - this.lastWildCastTime)), 0);
       }
       this.lastWildCastTime = event.timestamp;
     }
@@ -100,10 +101,10 @@ class CallOfTheWild extends Analyzer {
 
   item() {
     let tooltipText = `This shows a breakdown of the cooldown reduction provided by Call of the Wild: <br/> We only count CDR that was effective, meaning we are only showing the time between your casts that happened too soon after your last cast than would have been possible without Call of the Wild equipped.<ul>`;
-    tooltipText += (this.eagleCDR > 0) ? `<li>Aspect of the Eagle</li><ul><li>Total CDR: ${(this.eagleCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.eagleCDR / BASE_EAGLE_COOLDOWN).toFixed(1)}</li></ul>` : ``;
-    tooltipText += (this.cheetahCDR > 0) ? `<li>Aspect of the Cheetah</li><ul><li>Total CDR: ${(this.cheetahCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.cheetahCDR / BASE_CHEETAH_COOLDOWN).toFixed(1)}</li></ul>` : ``;
-    tooltipText += (this.turtleCDR > 0) ? `<li>Aspect of the Turtle</li><ul><li>Total CDR: ${(this.turtleCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.turtleCDR / BASE_TURTLE_COOLDOWN).toFixed(1)}</li></ul>` : ``;
-    tooltipText += (this.wildCDR > 0) ? `<li>Aspect of the Wild</li><ul><li>Total CDR: ${(this.wildCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.wildCDR / BASE_WILD_COOLDOWN).toFixed(1)}</li></ul>` : ``;
+    tooltipText += (this.eagleCDR > 0) ? `<li>Aspect of the Eagle</li><ul><li>Total CDR: ${(this.eagleCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.eagleCDR / this.baseEagleCooldown).toFixed(1)}</li></ul>` : ``;
+    tooltipText += (this.cheetahCDR > 0) ? `<li>Aspect of the Cheetah</li><ul><li>Total CDR: ${(this.cheetahCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.cheetahCDR / this.baseCheetahCooldown).toFixed(1)}</li></ul>` : ``;
+    tooltipText += (this.turtleCDR > 0) ? `<li>Aspect of the Turtle</li><ul><li>Total CDR: ${(this.turtleCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.turtleCDR / this.baseTurtleCooldown).toFixed(1)}</li></ul>` : ``;
+    tooltipText += (this.wildCDR > 0) ? `<li>Aspect of the Wild</li><ul><li>Total CDR: ${(this.wildCDR / 1000).toFixed(1)} seconds</li><li>Gained casts: ${(this.wildCDR / this.baseWildCooldown).toFixed(1)}</li></ul>` : ``;
     tooltipText += `</ul>`;
     return {
       item: ITEMS.CALL_OF_THE_WILD,
