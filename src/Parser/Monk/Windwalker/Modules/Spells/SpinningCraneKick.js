da1c8f2ea2395d19792794cb56f6b221dbfd4062
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2018-02-07 11:17:39 +0100

    [Windwalker] Updating SCK module and removing unnecessary MotCTarget class

--- src/Parser/Monk/Windwalker/Modules/Spells/SpinningCraneKick.js
+++ src/Parser/Monk/Windwalker/Modules/Spells/SpinningCraneKick.js
@@ -14,7 +14,9 @@ class SpinningCraneKick extends Analyzer {
   badCasts = 0;
   markoftheCraneTargets = [];
   lastSpinningCraneKickTick = 0;
+  spinningCraneKickHits = 0;
   totalMarksDuringHits = 0;
+  markoftheCraneStacks = 0;
 
   on_byPlayer_applydebuff(event) {
     const spellId = event.ability.guid;
@@ -29,7 +31,7 @@ class SpinningCraneKick extends Analyzer {
     //console.log(event.targetID, targetInstance, event.timestamp);
     const markoftheCraneTarget = {targetID: event.targetID, targetInstance: targetInstance, timestamp: event.timestamp };
     this.markoftheCraneTargets.push(markoftheCraneTarget);
-    console.log(markoftheCraneTarget, "added");
+    //console.log(markoftheCraneTarget, "added");
   }
 
   on_byPlayer_refreshdebuff(event) {
@@ -43,36 +45,54 @@ class SpinningCraneKick extends Analyzer {
     }
     const markoftheCraneTarget = {targetID: event.targetID, targetInstance: targetInstance, timestamp: event.timestamp };
     let i = 0;
+    let found = null;
     while (i <= this.markoftheCraneTargets.length - 1) {
       if (this.markoftheCraneTargets[i].targetID === markoftheCraneTarget.targetID && this.markoftheCraneTargets[i].targetInstance === markoftheCraneTarget.targetInstance) {
         this.markoftheCraneTargets[i].timestamp = markoftheCraneTarget.timestamp;
+        found = true;
+      }
+      i++;
+    }
+    // on_byPlayer_cast occasionally removes the wrong target, which doesn't hurt the immediate following calculation, but causes wrong calculations on the next cast.
+    // This re- adds any wrongfully removed marks 
+    if (found === null) {
+      this.markoftheCraneTargets.push(markoftheCraneTarget);
+    }
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.SPINNING_CRANE_KICK.id) {
+      return;
+    }
+    this.markoftheCraneStacks = 0;
+    let i = 0;
+    while (i < this.markoftheCraneTargets.length) {
+      // removing expired targets to avoid looking through huge arrays in logs with a lot of targets
+      if (event.timestamp - this.markoftheCraneTargets[i].timestamp > 15000) {
+        console.log(this.markoftheCraneTargets[i], "Mark of the Crane expired and to be removed");
+        console.log(this.markoftheCraneTargets.splice(this.markoftheCraneTargets[i], 1), "Mark Removed");
+      }
+      else {
+        this.markoftheCraneStacks++;
+        //console.log(this.markoftheCraneTargets[i], "Mark of the Crane found and counted");       
       }
       i++;
     }
   }
 
   on_byPlayer_damage(event) {
-    const spellId = event.ability.guid;
-    let markoftheCraneStacks = 0;
+    const spellId = event.ability.guid; 
     if (spellId !== SPELLS.SPINNING_CRANE_KICK_DAMAGE.id) {
       return;
     }
     this.spinningCraneKickHits++;
-
-    let i = 0;
-    while (i <= this.markoftheCraneTargets.length - 1) {
-      if (event.timestamp - this.markoftheCraneTargets[i].timestamp > 15000) {
-        console.log(this.markoftheCraneTargets[i], "Mark of the Crane expired and removed");
-        this.markoftheCraneTargets.splice(this.markoftheCraneTargets[i], 1);
-       }
-      else {
-        markoftheCraneStacks++;
-        console.log(this.markoftheCraneTargets[i], "Mark of the Crane found and counted");
-      }
-      i++;
+    console.log("mark of the crane stacks", this.markoftheCraneStacks, event.timestamp);
+    if (event.timestamp - this.lastSpinningCraneKickTick > 100) {
+      this.totalMarksDuringHits += this.markoftheCraneStacks;
+      this.lastSpinningCraneKickTick = event.timestamp;
     }
-    console.log("mark of the crane stacks", markoftheCraneStacks);
-    this.totalMarksDuringHits += markoftheCraneStacks;
+    //console.log("Total marks during hits", this.totalMarksDuringHits, "Total hits", this.spinningCraneKickHits, event.timestamp);
   }
 
   statistic() {
