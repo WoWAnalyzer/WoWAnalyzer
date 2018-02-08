7ec91450c609915e283cc8d0f016792f012d5adf
hawkcorrigan <corriganhawk@gmail.com>
2018-02-08 18:11:23 +0100

    First working version I think it should give mostly correct numbers but yet to test properly

--- src/Parser/Shaman/Elemental/Modules/Items/EchoesOfTheGreatSundering.js
+++ src/Parser/Shaman/Elemental/Modules/Items/EchoesOfTheGreatSundering.js
@@ -6,7 +6,7 @@ import ITEMS from 'common/ITEMS';
 import SPELLS from 'common/SPELLS';
 import ItemIcon from 'common/ItemIcon';
 import ItemLink from 'common/ItemLink';
-import { formatPercentage } from 'common/format';
+//import { formatPercentage } from 'common/format';
 import ItemDamageDone from 'Main/ItemDamageDone';
 
 class EchoesOfTheGreatSundering extends Analyzer {
@@ -17,22 +17,25 @@ class EchoesOfTheGreatSundering extends Analyzer {
     buffedEarthquakeCasts = 0;
     echoesProcsCounter = 0;
     unbuffedEarthquakeDamage = 0;
+  buffedEarthquakeDamage = 0;
     totalEarthquakeCasts = 0;
 
     state=0;
+  endtime = 0;
 
     on_initialized() {
         this.active = this.combatants.selected.hasShoulder(ITEMS.ECHOES_OF_THE_GREAT_SUNDERING.id);
     }
 
     on_byPlayer_cast(event) {
-        if (event.spellId !== SPELLS.EARTHQUAKE.id)
+      const spellId = event.ability.guid;
+      if (spellId !== SPELLS.EARTHQUAKE.id)
           return;
-
-        if(this.player.hasBuff(SPELLS.ECHOES_OF_THE_GREAT_SUNDERING_BUFF))
-          this.endtime=this.timstamp+6000;
-        else
-          if(this.timestamp < this.buffEndtime)
+      if (this.combatants.selected.hasBuff(SPELLS.ECHOES_OF_THE_GREAT_SUNDERING_BUFF.id, event.timestamp)) {
+        this.endtime = event.timestamp + 6100;
+        this.state = 1;
+      }
+        else if (this.timestamp < this.endtime)
             this.state=2;
     }
 
@@ -44,18 +47,24 @@ class EchoesOfTheGreatSundering extends Analyzer {
         return;
 
       if(this.state===0) {
-        this.unbuffedEarthquakeDamage += event.amount;
         this.unbuffedNormalizedEarthquakeDamage += event.amount/event.hitType;
       }
-      if(this.state===1)
+      if (this.state === 1) {
         this.buffedEarthquakeDamage += event.amount;
-    }
-
-    on_byPlayer_applybuff(event) {
-        const spellId = event.ability.guid;
-        if (spellId === SPELLS.ECHOES_OF_THE_GREAT_SUNDERING_BUFF.id) {
-            this.echoesProcsCounter += 1;
+        this.buffedNormalizedEarthquakeDamage += event.amount / event.hitType;
+      }
+      if (this.state === 2) {
+        if (this.buffedNormalizedEarthquakeDamage !== 0) {
+          if (Math.abs((event.amount / event.hitType) / this.buffedNormalizedEarthquakeDamage) < 0.2) {
+            this.buffedEarthquakeDamage += event.amount;
+          }
         }
+        else if (this.unbuffedNormalizedEarthquakeDamage !== 0) {
+          if (Math.abs((event.amount / event.hitType) / this.unbuffedNormalizedEarthquakeDamage) > 0.2) {
+            this.buffedEarthquakeDamage += event.amount;
+          }
+        }
+      }
     }
 
     item() {
