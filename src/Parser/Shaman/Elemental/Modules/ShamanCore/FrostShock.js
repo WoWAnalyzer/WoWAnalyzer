b421bbff7c18f1f1fe73b796797d4317c81ed9a9
HawkCorrigan <dgroelle@siemens.md.st.schule.de>
2018-02-09 18:51:53 +0100

    remove leftover debug stuff

--- src/Parser/Shaman/Elemental/Modules/ShamanCore/FrostShock.js
+++ src/Parser/Shaman/Elemental/Modules/ShamanCore/FrostShock.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS';
+import TALENTS from 'common/SPELLS/TALENTS/SHAMAN';
 import SpellLink from 'common/SpellLink';
 import { formatPercentage } from 'common/format';
 import RESOURCE_TYPES from 'common/RESOURCE_TYPES';
@@ -14,21 +15,21 @@ class FrostShock extends Analyzer {
   };
 
   badFrostShockCount = 0;
-  mehFrostShockCount = 0;
+  suboptimalFrostShockCount = 0;  //not used yet, wanna talk to tc-people for thresholds
 
   on_byPlayer_cast(event) {
     if (event.ability.guid === SPELLS.FROST_SHOCK.id) {
       this.active = true;
-      if (!this.combatants.selected.hasBuff(210714, event.timestamp))
+      if (!this.combatants.selected.hasBuff(TALENTS.ICEFURY_TALENT.id, event.timestamp))
         this.badFrostShockCount++;
 
       const resource = event.classResources[0];
       if (resource.type === RESOURCE_TYPES.MAELSTROM.id)
         if (resource.amount < 20)
           this.badFrostShockCount++;
-      
+
       if (!this.combatants.selected.hasBuff(SPELLS.ELEMENTAL_FOCUS.id, event.timestamp))
-        this.mehFrostShockCount++;
+        this.suboptimalFrostShockCount++;
     }
   }
   suggestions(when) {
@@ -36,7 +37,7 @@ class FrostShock extends Analyzer {
       .addSuggestion((suggest, actual, recommended) => {
         return suggest(<span>Your <SpellLink id={SPELLS.FROST_SHOCK.id} /> can be improved.</span>)
           .icon(SPELLS.FROST_SHOCK.icon)
-          .actual(`${this.badFrostShockCount} bad Casts`)
+          .actual(`${this.badFrostShockCount} bad Casts. Only cast Frost Shock when you have Ice Fury-Stacks left and at least 20 Maelstrom`)
           .recommended(`>${formatPercentage(recommended)}% is recommended`)
           .regular(1).major(3);
       });
