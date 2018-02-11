cd3ba51698f4e16b9f10001101cdf31cf4371970
niseko <rentz@hotmail.de>
2018-02-11 19:37:11 +0100

    Fix Totems not counting towards mastery since 7.3.5 patch
    HTT / HST changed to be pets instead of totems

--- src/Parser/Shaman/Restoration/Modules/Features/MasteryEffectiveness.js
+++ src/Parser/Shaman/Restoration/Modules/Features/MasteryEffectiveness.js
@@ -22,25 +22,37 @@ class MasteryEffectiveness extends Analyzer {
   totalMasteryHealing = 0;
   totalMaxPotentialMasteryHealing = 0;
 
-  on_byPlayer_heal(event) {
-    const isAbilityAffectedByMastery = ABILITIES_AFFECTED_BY_MASTERY.indexOf(event.ability.guid) !== -1;
+  on_byPlayer_summon(event) {
+    if (event.ability.guid === SPELLS.HEALING_TIDE_TOTEM_CAST.id) {
+      this.httID = event.targetID;
+    } else if (event.ability.guid === SPELLS.HEALING_STREAM_TOTEM_CAST.id) {
+      this.hstID = event.targetID;
+    }
+  }
 
-    const healingDone = event.amount + (event.absorb || 0) + (event.overheal || 0);
+  on_heal(event) {
+    if (this.owner.byPlayer(event) || event.sourceID === this.httID  || event.sourceID === this.hstID) {
+      const isAbilityAffectedByMastery = ABILITIES_AFFECTED_BY_MASTERY.indexOf(event.ability.guid) !== -1;
 
-    if (isAbilityAffectedByMastery) {
-      const healthBeforeHeal = event.hitPoints - event.amount;
-      const masteryEffectiveness = 1 - healthBeforeHeal / event.maxHitPoints;
+      const healingDone = event.amount + (event.absorb || 0) + (event.overheal || 0);
 
-      // The base healing of the spell (excluding any healing added by mastery)
-      const masteryPercent = this.statTracker.currentMasteryPercentage;
-      const baseHealingDone = healingDone / (1 + masteryPercent * masteryEffectiveness);
-      const masteryHealingDone = healingDone - baseHealingDone;
-      // The max potential mastery healing if we had a mastery effectiveness of 100% on this spell. This does NOT include the base healing
-      // Example: a heal that did 1,324 healing with 32.4% mastery with 100% mastery effectiveness will have a max potential mastery healing of 324.
-      const maxPotentialMasteryHealing = baseHealingDone * masteryPercent; // * 100% mastery effectiveness
+      if (isAbilityAffectedByMastery) {
+        const healthBeforeHeal = event.hitPoints - event.amount;
+        const masteryEffectiveness = 1 - healthBeforeHeal / event.maxHitPoints;
 
-      this.totalMasteryHealing += Math.max(0, masteryHealingDone - (event.overheal || 0));
-      this.totalMaxPotentialMasteryHealing += Math.max(0, maxPotentialMasteryHealing - (event.overheal || 0));
+        // The base healing of the spell (excluding any healing added by mastery)
+        const masteryPercent = this.statTracker.currentMasteryPercentage;
+        const baseHealingDone = healingDone / (1 + masteryPercent * masteryEffectiveness);
+        const masteryHealingDone = healingDone - baseHealingDone;
+        // The max potential mastery healing if we had a mastery effectiveness of 100% on this spell. This does NOT include the base healing
+        // Example: a heal that did 1,324 healing with 32.4% mastery with 100% mastery effectiveness will have a max potential mastery healing of 324.
+        const maxPotentialMasteryHealing = baseHealingDone * masteryPercent; // * 100% mastery effectiveness
+
+        this.totalMasteryHealing += Math.max(0, masteryHealingDone - (event.overheal || 0));
+        this.totalMaxPotentialMasteryHealing += Math.max(0, maxPotentialMasteryHealing - (event.overheal || 0));
+
+        event.masteryEffectiveness = masteryEffectiveness;
+      }
     }
   }
 
