0a737c25390fd5142ff06450e45db68d4973c7b8
Lucas Liberacki <coolcodingguy@gmail.com>
2018-02-11 13:34:42 -0500

    Fixed an issue with the getPlayerName returning the hyphen in the result.

--- src/selectors/url/report/getPlayerName.js
+++ src/selectors/url/report/getPlayerName.js
@@ -4,7 +4,7 @@ export default state => {
   const match = getMatch(state);
   if(match && match.params.player){
     const index = match.params.player.indexOf('-');
-    return index !== -1 ? match.params.player.substr(index) : match.params.player;
+    return index !== -1 ? match.params.player.substr(index + 1) : match.params.player;
   }
   return null;
 };
