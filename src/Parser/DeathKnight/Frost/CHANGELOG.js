240bbd510050c250c9b7b8e6de85f62c0d2ae3cb
jjs451 <jjs451@msstate.edu>
2018-02-11 22:18:55 -0600

    update changelog

--- src/Parser/DeathKnight/Frost/CHANGELOG.js
+++ src/Parser/DeathKnight/Frost/CHANGELOG.js
@@ -9,6 +9,11 @@ import SpellLink from 'common/SpellLink';
 
 export default [
     {
+      date: new Date('2018-02-11'),
+      changes: <Wrapper>Added stat box for <SpellLink id={SPELLS.GATHERING_STORM_TALENT.id} icon /></Wrapper>,
+      contributors: [Khazak],
+    },
+    {
       date: new Date('2018-01-30'),
       changes: <Wrapper>Added stats for <SpellLink id={SPELLS.FROST_DEATH_KNIGHT_T21_2SET_BONUS.id} icon /> and <SpellLink id={SPELLS.FROST_DEATH_KNIGHT_T21_4SET_BONUS.id} icon /> </Wrapper>,
       contributors: [Khazak],
