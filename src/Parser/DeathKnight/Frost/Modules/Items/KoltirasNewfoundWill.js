a9c64702f999aa26fc70c6dad21bfb3dfbca63d4
jjs451 <jjs451@msstate.edu>
2018-02-12 14:50:16 -0600

    added rune refunds

--- src/Parser/DeathKnight/Frost/Modules/Items/KoltirasNewfoundWill.js
+++ src/Parser/DeathKnight/Frost/Modules/Items/KoltirasNewfoundWill.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-import Wrapper from 'common/Wrapper';
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
-import SpellLink from 'common/SpellLink';
-import { formatPercentage } from 'common/format';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import calculateEffectiveDamage from 'Parser/Core/calculateEffectiveDamage';
 
 import ItemDamageDone from 'Main/ItemDamageDone';
+
+import RuneTracker from '../../../Shared/RuneTracker';
 
 /**
  * Equip: Obliterate deals 10% more damage and has a 15% chance to refund 1 runes.
@@ -21,6 +20,7 @@ const DAMAGE_MODIFIER = .1;
 class KoltirasNewfoundWill extends Analyzer{
     static dependencies = {
         combatants: Combatants,
+        runeTracker: RuneTracker,
     }
 
     bonusDamage = 0;
@@ -33,15 +33,20 @@ class KoltirasNewfoundWill extends Analyzer{
         const spellId = event.ability.guid;
         if(spellId === SPELLS.OBLITERATE_MAIN_HAND_DAMAGE.id || spellId === SPELLS.OBLITERATE_OFF_HAND_DAMAGE.id){
             this.bonusDamage += calculateEffectiveDamage(event, DAMAGE_MODIFIER);
-        }
-        
+        }        
+    }
+
+    get runesGenerated(){
+        // this is a little ugly to prevent the page from crashing when it accesses runeTracker before runeTracker is done processing
+        return this.runeTracker.buildersObj.hasOwnProperty(SPELLS.KOLTIRAS_NEWFOUND_WILL.id) ? this.runeTracker.buildersObj[SPELLS.KOLTIRAS_NEWFOUND_WILL.id].generated: 0;
     }
 
     item() {
-        console.log(this.bonusDamage)
         return {
           item: ITEMS.KOLTIRAS_NEWFOUND_WILL,
-          result: <ItemDamageDone amount={this.bonusDamage} />,
+          result: <dfn data-tip={`Runes refunded: ${this.runesGenerated}`} >
+          <ItemDamageDone amount={this.bonusDamage} />
+          </dfn>,
         };
     }
 }
