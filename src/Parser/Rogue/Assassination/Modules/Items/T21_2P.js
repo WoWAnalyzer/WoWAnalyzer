5533ba70cb308f7933b37cbb317e191dd0124bc2
Adil Asif <asifadilanis@gmail.com>
2018-02-15 10:07:01 -0800

    Edited tooltip to provide more useful information.

--- src/Parser/Rogue/Assassination/Modules/Items/T21_2P.js
+++ src/Parser/Rogue/Assassination/Modules/Items/T21_2P.js
@@ -8,6 +8,10 @@ import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import { formatPercentage } from 'common/format';
 
+/**
+* Equip: When you use Envenom, your Deadly and Wound poisons have 35% increased chance to critically strike for 6 sec.
+*/
+
 class T21_2P extends Analyzer {
   static dependencies = {
     combatants: Combatants,
@@ -23,7 +27,7 @@ class T21_2P extends Analyzer {
       icon: <SpellIcon id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS.id} />,
       title: <SpellLink id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS.id} />,
       result: (
-        <dfn data-tip={`It's worth keeping this in mind and trying to maximize your uptime, but not at the expense of your primary rotation. This should, most of the time, naturally happen in the process of clipping your Envenom debuff.`}>
+        <dfn data-tip={`Ideally you should be aiming for somewhere around 80% uptime. This should, most of the time, naturally happen in the process of clipping your Envenom debuff; do not chase higher uptime on this at the expense of your regular rotation.`}>
           {formatPercentage(this.percentUptime)}% <SpellLink id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS_BUFF.id} icon /> uptime
         </dfn>
       ),
