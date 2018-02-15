edce0f897ba61181dee37a1eef62bd4551b940ef
HawkCorrigan <dgroelle@siemens.md.st.schule.de>
2018-02-15 19:03:43 +0100

    Added requested changes

--- src/Parser/Shaman/Elemental/Modules/ShamanCore/FrostShock.js
+++ src/Parser/Shaman/Elemental/Modules/ShamanCore/FrostShock.js
@@ -16,25 +16,36 @@ class FrostShock extends Analyzer {
   badFrostShockCount = 0;
   suboptimalFrostShockCount = 0;  //not used yet, wanna talk to tc-people for thresholds
 
+  on_initialized() {
+    this.active = false;
+  }
+
   on_byPlayer_cast(event) {
-    if (event.ability.guid === SPELLS.FROST_SHOCK.id) {
-      this.active = true;
-      if (!this.combatants.selected.hasBuff(TALENTS.ICEFURY_TALENT.id, event.timestamp))
-        this.badFrostShockCount++;
+    if (event.ability.guid !== SPELLS.FROST_SHOCK.id) {
+      return;
+    }
+    this.active = true;
+    if (!this.combatants.selected.hasBuff(TALENTS.ICEFURY_TALENT.id, event.timestamp))
+      this.badFrostShockCount++;
 
-      const resource = event.classResources[0];
-      if (resource.type === RESOURCE_TYPES.MAELSTROM.id)
-        if (resource.amount < 20)
-          this.badFrostShockCount++;
+    const resource = event.classResources[0];
+    if (resource.type !== RESOURCE_TYPES.MAELSTROM.id) {
+      return;
+    }
+    
+    if (resource.amount < 20) {
+      this.badFrostShockCount++;
+    }
 
-      if (!this.combatants.selected.hasBuff(SPELLS.ELEMENTAL_FOCUS.id, event.timestamp))
-        this.suboptimalFrostShockCount++;
+    if (!this.combatants.selected.hasBuff(SPELLS.ELEMENTAL_FOCUS.id, event.timestamp)) {
+      this.suboptimalFrostShockCount++;
     }
   }
+
   suggestions(when) {
     when(this.badFrostShockCount).isGreaterThan(0)
       .addSuggestion((suggest) => {
-        return suggest(<span>Your <SpellLink id={SPELLS.FROST_SHOCK.id} /> can be improved.</span>)
+        return suggest(<span>Your <SpellLink id={SPELLS.FROST_SHOCK.id} /> usage can be improved.</span>)
           .icon(SPELLS.FROST_SHOCK.icon)
           .actual(`${this.badFrostShockCount} bad Casts. Only cast Frost Shock when you have Ice Fury-Stacks left and at least 20 Maelstrom`)
           .recommended(`0 is recommended`)
