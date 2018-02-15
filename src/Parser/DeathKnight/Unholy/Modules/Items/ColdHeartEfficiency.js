355d36f169a39badabd3cf51c8f8713a0d50bc2e
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2018-02-15 14:04:10 +0000

    Update ColdHeartEfficiency.js
    I misunderstood what you meant with the movement of the constants, I hope it's fine now.

--- src/Parser/DeathKnight/Unholy/Modules/Items/ColdHeartEfficiency.js
+++ src/Parser/DeathKnight/Unholy/Modules/Items/ColdHeartEfficiency.js
@@ -9,6 +9,16 @@ import { formatPercentage } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
+  
+const unholyStrengthDuration = 15000;
+const concordanceDuration = 10000;
+const khazgorothDuration = 15000;
+  
+const coldHeartMaxStack = 20;
+const coldHeartOptimalMinStack = 14;
+	
+const remainingDurationAllowed = 4000;
+const maxDurationAtMaxStacksAllowed = 4000;
 
 class ColdHeartEfficiency extends Analyzer {
   static dependencies = {
@@ -20,7 +30,7 @@ class ColdHeartEfficiency extends Analyzer {
 
   totalColdHeartCasts = 0;
   correctColdHeartCasts = 0;
-  buffColdHeart = 20; //Stacks start at 20 at start of fight
+  buffColdHeart = coldHeartMaxStack; //Stacks start at 20 at start of fight
   
   unholyStrengthStart = 0;
   unholyStrengthRemaining = 0;
@@ -33,16 +43,6 @@ class ColdHeartEfficiency extends Analyzer {
   
   timeAtMaxStacksStart = this.owner.fight.start_time;
   timeAtMaxStacks = 0;
-  
-  unholyStrengthDuration = 15000;
-  concordanceDuration = 10000;
-  khazgorothDuration = 15000;
-  
-  coldHeartMaxStack = 20;
-  coldHeartOptimalMinStack = 14;
-	
-  remainingDurationAllowed = 4000;
-  maxDurationAtMaxStacksAllowed = 4000;
   
   castsTooEarly = 0;
   castsTooLate = 0;
@@ -91,7 +91,7 @@ class ColdHeartEfficiency extends Analyzer {
     const spellID = event.ability.guid;
     if (spellID === SPELLS.COLD_HEART_BUFF.id) {
       this.buffColdHeart = event.stack;
-	  if (this.buffColdHeart == 20) {
+	  if (this.buffColdHeart === coldHeartMaxStack) {
 		  this.timeAtMaxStacksStart = event.timestamp;
 	  }
     }
@@ -104,22 +104,21 @@ class ColdHeartEfficiency extends Analyzer {
 		this.totalColdHeartCasts++;
 	  
 		this.timeAtMaxStacks = event.timestamp - this.timeAtMaxStacksStart;
-	
-		this.unholyStrengthRemaining = this.unholyStrengthDuration - (event.timestamp - this.unholyStrengthStart);
-		this.concordanceRemaining = this.concordanceDuration - (event.timestamp - this.concordanceStart);
-		this.khazgorothRemaining = this.khazgorothDuration - (event.timestamp - this.khazgorothStart);
+
+		this.unholyStrengthRemaining = unholyStrengthDuration - (event.timestamp - this.unholyStrengthStart);
+		this.concordanceRemaining = concordanceDuration - (event.timestamp - this.concordanceStart);
+		this.khazgorothRemaining = khazgorothDuration - (event.timestamp - this.khazgorothStart);
 	  
 		//This checks wether or not any of the three buffs are about to fall off within the next 4 seconds.
-		if ((this.unholyStrengthRemaining > 0 && this.unholyStrengthRemaining < this.remainingDurationAllowed) || (this.concordanceRemaining > 0 && this.concordanceRemaining < this.remainingDurationAllowed) || (this.khazgorothRemaining > 0 && this.khazgorothRemaining < this.remainingDurationAllowed)){
-			if (this.buffColdHeart < this.coldHeartMaxStack && this.buffColdHeart >= this.coldHeartOptimalMinStack) {
+		if ((this.unholyStrengthRemaining > 0 && this.unholyStrengthRemaining < remainingDurationAllowed) || (this.concordanceRemaining > 0 && this.concordanceRemaining < remainingDurationAllowed) || (this.khazgorothRemaining > 0 && this.khazgorothRemaining < remainingDurationAllowed)){
+			if (this.buffColdHeart < coldHeartMaxStack && this.buffColdHeart >= coldHeartOptimalMinStack) {
 				this.correctColdHeartCasts++;
-			}else if (this.buffColdHeart < 20) {
-				this.castsTooEarly++;
 			}
-		}			
-		if (this.buffColdHeart === 20 && this.timeAtMaxStacks <= this.maxDurationAtMaxStacksAllowed) {
+		}else if (this.buffColdHeart < coldHeartMaxStack) {
+			this.castsTooEarly++;
+		}else if (this.buffColdHeart === coldHeartMaxStack && this.timeAtMaxStacks <= maxDurationAtMaxStacksAllowed) {
 			this.correctColdHeartCasts++;
-		}else if(this.buffColdHeart === 20){
+		}else if(this.buffColdHeart === coldHeartMaxStack){
 			this.castsTooLate++;
 		}
 		//This is only here for double casting Cold Heart. If Cold Heart is casted again before it reaches 2 stacks, the event won't update.
@@ -130,7 +129,7 @@ class ColdHeartEfficiency extends Analyzer {
     const castEfficiency = this.correctColdHeartCasts / this.totalColdHeartCasts;
     when(castEfficiency).isLessThan(0.8)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span> You are casting <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at non optimal times. {this.castsTooLate} cast(s) were made too late and {this.castsTooEarly} cast(s) were made too early. You either want to cast <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> when at 20 stacks of <SpellLink id={SPELLS.COLD_HEART_BUFF.id} /> ASAP, or when you are above 13 stacks and any of your buffs <SpellLink id={SPELLS.UNHOLY_STRENGTH_BUFF.id}/> or <SpellLink id={SPELLS.CONCORDANCE_OF_THE_LEGIONFALL_STRENGTH.id} />  or <SpellLink id={SPELLS.KHAZGOROTHS_SHAPING.id} /> are about to run out. You also don't want to hold <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at 20 stacks for too long.</span>)
+        return suggest(<wrapper> You are casting <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at non optimal times. {this.castsTooLate} cast(s) were made too late and {this.castsTooEarly} cast(s) were made too early. You either want to cast <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> when at 20 stacks of <SpellLink id={SPELLS.COLD_HEART_BUFF.id} /> ASAP, or when you are above 13 stacks and any of your buffs <SpellLink id={SPELLS.UNHOLY_STRENGTH_BUFF.id}/> or <SpellLink id={SPELLS.CONCORDANCE_OF_THE_LEGIONFALL_STRENGTH.id} />  or <SpellLink id={SPELLS.KHAZGOROTHS_SHAPING.id} /> are about to run out. You also don't want to hold <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at 20 stacks for too long.</wrapper>)
           .icon(SPELLS.CHAINS_OF_ICE.icon)
           .actual(`${formatPercentage(actual)}% of Chains of Ice were cast correctly.`)
           .recommended(`>${formatPercentage(recommended)}% is recommended`)
