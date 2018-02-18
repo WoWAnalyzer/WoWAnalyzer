bcd7bc88227baf1176f3245218af042f75e975a7
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2018-02-18 10:36:19 +0000

    Update ColdHeartEfficiency.js
    I moved all the constants except ColdHeartMaxStacks since they were only used in that specific cast event.

--- src/Parser/DeathKnight/Unholy/Modules/Items/ColdHeartEfficiency.js
+++ src/Parser/DeathKnight/Unholy/Modules/Items/ColdHeartEfficiency.js
@@ -5,20 +5,13 @@ import SpellIcon from 'common/SpellIcon';
 import SpellLink from 'common/SpellLink';
 import ITEMS from 'common/ITEMS';
 import { formatPercentage } from 'common/format';
+import Wrapper from 'common/Wrapper';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
   
-const unholyStrengthDuration = 15000;
-const concordanceDuration = 10000;
-const khazgorothDuration = 15000;
-  
 const coldHeartMaxStack = 20;
-const coldHeartOptimalMinStack = 14;
-	
-const remainingDurationAllowed = 4000;
-const maxDurationAtMaxStacksAllowed = 4000;
 
 class ColdHeartEfficiency extends Analyzer {
   static dependencies = {
@@ -102,6 +95,15 @@ class ColdHeartEfficiency extends Analyzer {
 	
     if (spellId === SPELLS.CHAINS_OF_ICE.id) {
 		this.totalColdHeartCasts++;
+      
+    const unholyStrengthDuration = 15000;
+    const concordanceDuration = 10000;
+    const khazgorothDuration = 15000;
+      
+    const coldHeartOptimalMinStack = 14;
+	
+    const remainingDurationAllowed = 4000;
+    const maxDurationAtMaxStacksAllowed = 4000;
 	  
 		this.timeAtMaxStacks = event.timestamp - this.timeAtMaxStacksStart;
 
@@ -129,7 +131,7 @@ class ColdHeartEfficiency extends Analyzer {
     const castEfficiency = this.correctColdHeartCasts / this.totalColdHeartCasts;
     when(castEfficiency).isLessThan(0.8)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<wrapper> You are casting <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at non optimal times. {this.castsTooLate} cast(s) were made too late and {this.castsTooEarly} cast(s) were made too early. You either want to cast <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> when at 20 stacks of <SpellLink id={SPELLS.COLD_HEART_BUFF.id} /> ASAP, or when you are above 13 stacks and any of your buffs <SpellLink id={SPELLS.UNHOLY_STRENGTH_BUFF.id}/> or <SpellLink id={SPELLS.CONCORDANCE_OF_THE_LEGIONFALL_STRENGTH.id} />  or <SpellLink id={SPELLS.KHAZGOROTHS_SHAPING.id} /> are about to run out. You also don't want to hold <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at 20 stacks for too long.</wrapper>)
+        return suggest(<Wrapper> You are casting <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at non optimal times. {this.castsTooLate} cast(s) were made too late and {this.castsTooEarly} cast(s) were made too early. You either want to cast <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> when at 20 stacks of <SpellLink id={SPELLS.COLD_HEART_BUFF.id} /> ASAP, or when you are above 13 stacks and any of your buffs <SpellLink id={SPELLS.UNHOLY_STRENGTH_BUFF.id}/> or <SpellLink id={SPELLS.CONCORDANCE_OF_THE_LEGIONFALL_STRENGTH.id} />  or <SpellLink id={SPELLS.KHAZGOROTHS_SHAPING.id} /> are about to run out. You also don't want to hold <SpellLink id={SPELLS.CHAINS_OF_ICE.id} /> at 20 stacks for too long.</Wrapper>)
           .icon(SPELLS.CHAINS_OF_ICE.icon)
           .actual(`${formatPercentage(actual)}% of Chains of Ice were cast correctly.`)
           .recommended(`>${formatPercentage(recommended)}% is recommended`)
