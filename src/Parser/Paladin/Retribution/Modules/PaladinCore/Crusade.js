cad765d7de7b47864c5b6d87e790ca4e45e730f5
collcrom <collcrom@gmail.com>
2018-02-21 22:55:13 -0600

    got crusade suggestion and updated cast efficency for Blade of Justice

--- src/Parser/Paladin/Retribution/Modules/PaladinCore/Crusade.js
+++ src/Parser/Paladin/Retribution/Modules/PaladinCore/Crusade.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import Wrapper from 'common/Wrapper';
 import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import { formatNumber } from 'common/format';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import Analyzer from 'Parser/Core/Analyzer';
-import {formatMilliseconds} from 'common/format';
+import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
 
-const CAST_BUFFER = 1000;
+const CAST_BUFFER = 500;
 
 class Crusade extends Analyzer {
 	static dependencies = {
 		combatants: Combatants,
+		abilityTracker: AbilityTracker,
 	};
 
 	on_initialized() {
@@ -17,7 +21,7 @@ class Crusade extends Analyzer {
 	}
 
 	crusadeCastTimestamp = 0;
-	badFirstGlobal = [];
+	badFirstGlobal = 0;
 
 	on_byPlayer_cast(event) {
 		const spellId = event.ability.guid;
@@ -32,13 +36,35 @@ class Crusade extends Analyzer {
 		if (spellId !== SPELLS.CRUSADE_TALENT.id) {
 			return;
 		}
-		if(this.crusadeCastTimestamp && event.timestamp < this.crusadeCastTimestamp + CAST_BUFFER) {
-			console.log(event.timestamp - this.crusadeCastTimestamp)
-			console.log(formatMilliseconds(event.timestamp - this.owner.fight.start_time))
-			this.badFirstGlobal.push(event.timestamp);
-			this.crusadeCastTimestamp = null;
+		if(this.crusadeCastTimestamp && event.timestamp > this.crusadeCastTimestamp + CAST_BUFFER) {
+			this.badFirstGlobal++;
 		}
-		console.log(this.badFirstGlobal)
+		this.crusadeCastTimestamp = null;
+	}
+
+	get badGlobalPercent() {
+		return this.badFirstGlobal / this.abilityTracker.getAbility(SPELLS.CRUSADE_TALENT.id).casts;
+	}
+
+	get suggestionThresholds() {
+		return {
+			actual: this.badGlobalPercent,
+			isGreaterThan: {
+				minor: 0,
+				average: 0.25,
+				major: 0.5,
+			},
+			style: 'percentage',
+		};
+	}
+
+	suggestions(when) {
+		when(this.suggestionThresholds).addSuggestion((suggest, actual) => {
+			return suggest(<Wrapper>You want to build stacks of <SpellLink id={SPELLS.CRUSADE_TALENT.id} icon/> as quickly as possible. Make sure you are using <SpellLink id={SPELLS.TEMPLARS_VERDICT.id} icon/> or <SpellLink id={SPELLS.DIVINE_STORM.id} icon/> almost instantly after casting <SpellLink id={SPELLS.CRUSADE_TALENT.id} icon/>.</Wrapper>)
+				.icon(SPELLS.CRUSADE_TALENT.icon)
+				.actual(`${formatNumber(this.badFirstGlobal)} bad first global(s)`)
+				.recommended(`0 is recommended`);
+		});
 	}
 }
 
