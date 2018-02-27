8d9c983b1ae9172716043965c2ccf01037c59ef6
collcrom <collcrom@gmail.com>
2018-02-27 15:31:51 -0600

    added in bonus damage from chaos blades

--- src/Parser/DemonHunter/Havoc/Modules/Items/ChaosTheory.js
+++ src/Parser/DemonHunter/Havoc/Modules/Items/ChaosTheory.js
@@ -3,7 +3,7 @@ import React from 'react';
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
 import { formatNumber } from 'common/format';
-
+import calculateEffectiveDamage from 'Parser/Core/calculateEffectiveDamage';
 import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
@@ -13,7 +13,23 @@ import ItemDamageDone from 'Main/ItemDamageDone';
 * Equip: Blade Dance has a 10% chance to grant you Chaos Blades for 6 sec.
 */
 
-const CHAOS_THEORY = {PROC_CHANCE: .1, DURATION: 6000};
+/*
+* Chaos Blades:
+* Increases all damage done by 30% for 18 sec.
+*
+* While active, your auto attack deals 150% increased damage, and causes Chaos damage.
+*/
+
+const CHAOS_THEORY = {
+	PROC_CHANCE: .1, 
+	DURATION: 6000, 
+};
+
+const CHAOS_BLADES = {
+	AUTO_MODIFIER: 1.5,
+	DAMAGE_MODIFIER: .3,
+};
+
 const MS_BUFFER = 100;
 
 class ChaosTheory extends Analyzer {
@@ -21,7 +37,7 @@ class ChaosTheory extends Analyzer {
 		combatants: Combatants,
 		abilityTracker: AbilityTracker,
 	};
-	hits = 0;
+	damage = 0;
 	procs = 0;
 	bonusDamage = 0;
 	lastCastTimestamp = 0;
@@ -52,13 +68,27 @@ class ChaosTheory extends Analyzer {
 
 	on_byPlayer_damage(event) {
 		const spellId = event.ability.guid;
+		if(event.targetIsFriendly) {
+      return;
+    }
 		if(event.timestamp > this.lastApplybuffTimestamp + CHAOS_THEORY.DURATION) {
 			return;
 		}
-		if (spellId === SPELLS.CHAOS_BLADES_DAMAGE_MH.id || spellId === SPELLS.CHAOS_BLADES_DAMAGE_OH.id) {
-			this.bonusDamage += event.amount + (event.absorbed || 0);
+		if(!this.combatants.selected.hasBuff(SPELLS.CHAOS_BLADES_TALENT.id, event.timestamp)) {
+			return;
 		}
+		//Extra Melee Damage
+		if (spellId === SPELLS.CHAOS_BLADES_DAMAGE_MH.id || spellId === SPELLS.CHAOS_BLADES_DAMAGE_OH.id) {
+			this.bonusDamage += calculateEffectiveDamage(event, CHAOS_BLADES.AUTO_MODIFIER);
+		}
+		//All Other Damage
+		else {
+			this.bonusDamage += calculateEffectiveDamage(event, CHAOS_BLADES.DAMAGE_MODIFIER);
+		}
+		
 	}
+
+	
 
 	get expectedProcs() {
 		return (this.abilityTracker.getAbility(SPELLS.DEATH_SWEEP.id).casts + this.abilityTracker.getAbility(SPELLS.BLADE_DANCE.id).casts) * CHAOS_THEORY.PROC_CHANCE;
@@ -68,7 +98,7 @@ class ChaosTheory extends Analyzer {
 		return {
 			item: ITEMS.CHAOS_THEORY,
 			result: (
-				<dfn data-tip={`You had <b>${this.procs}</b> procs (<b>${formatNumber(this.expectedProcs)}</b> expected procs)</br> Total Damage: ${formatNumber(this.bonusDamage)}`}>
+				<dfn data-tip={`You had <b>${this.procs}</b> procs (<b>${formatNumber(this.expectedProcs)}</b> expected procs)</br> Total Damage: <b>${formatNumber(this.bonusDamage)}</b>`}>
 					<ItemDamageDone amount={this.bonusDamage}/>
 				</dfn>
 			),
