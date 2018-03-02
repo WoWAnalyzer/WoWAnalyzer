a4f342ceb18d48b1261c4ba637e6aada59efb55a
collcrom <collcrom@gmail.com>
2018-03-01 18:21:43 -0600

    updated the t21 2pc icon to make more sense

--- src/Parser/Rogue/Assassination/Modules/Items/T21_2P.js
+++ src/Parser/Rogue/Assassination/Modules/Items/T21_2P.js
@@ -24,8 +24,8 @@ class T21_2P extends Analyzer {
   item() {
     return {
       id: SPELLS.ASSA_ROGUE_T21_2SET_BONUS_BUFF.id,
-      icon: <SpellIcon id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS.id} />,
-      title: <SpellLink id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS.id} />,
+      icon: <SpellIcon id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS_BUFF.id} />,
+      title: <SpellLink id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS_BUFF.id} />,
       result: (
         <dfn data-tip={`Ideally you should be aiming for somewhere around 80% uptime. This should, most of the time, naturally happen in the process of clipping your Envenom debuff; do not chase higher uptime on this at the expense of your regular rotation.`}>
           {formatPercentage(this.percentUptime)}% <SpellLink id={SPELLS.ASSA_ROGUE_T21_2SET_BONUS_BUFF.id} icon /> uptime
