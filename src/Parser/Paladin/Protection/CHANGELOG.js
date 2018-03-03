c946b3940a43e4eee34a397d3cc52bb6f408244b
collcrom <collcrom@gmail.com>
2018-03-03 16:48:08 -0600

    update changelog

---
+++ src/Parser/Paladin/Protection/CHANGELOG.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Wrapper from 'common/Wrapper';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import ITEMS from 'common/ITEMS';
+import ItemLink from 'common/ItemLink';
+import { Hewhosmites } from 'MAINTAINERS';
+
+export default [
+	{
+		date: new Date('2018-03-03'),
+		changes: <Wrapper>Added <ItemLink id={ITEMS.PILLARS_OF_INMOST_LIGHT.id} icon/></Wrapper>,
+		contributors: [Hewhosmites],
+	},
+];
