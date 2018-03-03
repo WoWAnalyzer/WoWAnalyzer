3e974165bf155f31ac117b2ed1f86ee7c24eeb6b
Bicepspump <34515306+Bicepspump@users.noreply.github.com>
2018-03-03 13:01:32 +0000

    Update VirulentPlagueEfficiency.js
    I think I took care of all the suggestions here. I'm not too sure why the indentation acts weird, it looks fine when I put it together in Notepad++

--- src/Parser/DeathKnight/Unholy/Modules/Features/VirulentPlagueEfficiency.js
+++ src/Parser/DeathKnight/Unholy/Modules/Features/VirulentPlagueEfficiency.js
@@ -20,54 +20,57 @@ class VirulentPlagueEfficiency extends Analyzer {
   targets = {};
   
   totalOutBreakCasts = 0;
-  totalVirulentDuration = 0;
   totalTimeWasted = 0;
   
   get VirulentDuration(){
-	if (this.combatants.selected.hasTalent(SPELLS.EBON_FEVER_TALENT.id)) {
-		return(19.65);
-	} else{
-		return(33.3);
-	}
+	  if (this.combatants.selected.hasTalent(SPELLS.EBON_FEVER_TALENT.id)) {
+		  return(13.65);
+	  } else{
+		  return(27.3);
+	  }
   }
   
   on_byPlayer_refreshdebuff(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.VIRULENT_PLAGUE.id){
-		this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.timestamp;
-	}
+		  this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.timestamp + 1000*this.VirulentDuration;
+	  }
   }
   
    on_byPlayer_applydebuff(event){
     const spellId = event.ability.guid;
     if(spellId === SPELLS.VIRULENT_PLAGUE.id){
-		this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.timestamp + 1000*0.3*this.VirulentDuration;
-		//Adding 3.15 seconds when buff is only applied. This is for cases when the target does not benefit from the epidemic effect (Dots spreading to adds not staying by target for instance.)
-	}
+		  this.targets[encodeTargetString(event.targetID, event.targetInstance)] = event.timestamp + 1000*this.VirulentDuration - 1000*0.3*this.VirulentDuration;
+		  //Removing 3.15 seconds when buff is only applied. This is for cases when the target does not benefit from the epidemic effect (Dots spreading to adds not staying by target for instance.)
+	  }
   }
   
 
   
   on_byPlayer_cast(event) {
-	const spellID = event.ability.guid;
-	if(spellID === SPELLS.OUTBREAK.id){	
-		this.totalOutBreakCasts += 1;
-		if (this.targets[encodeTargetString(event.targetID, event.targetInstance)]) {
-			//We subtract 6 seconds from the total duration since this is the time left after Outbreak finishes.
-			if (((this.VirulentDuration - 6) - (event.timestamp -  this.targets[encodeTargetString(event.targetID, event.targetInstance)]) / 1000) >= 0) {
-				this.totalTimeWasted += (this.VirulentDuration - 6) - (event.timestamp -  this.targets[encodeTargetString(event.targetID, event.targetInstance)]) / 1000;
-			}	
-		}
-	}  
+	  const spellID = event.ability.guid;
+	  if(spellID === SPELLS.OUTBREAK.id){	
+		  this.totalOutBreakCasts += 1;
+		  if (this.targets[encodeTargetString(event.targetID, event.targetInstance)]) {
+		  	//We subtract 6 seconds from the total duration since this is the time left after Outbreak finishes.
+			  if (((event.timestamp -  this.targets[encodeTargetString(event.targetID, event.targetInstance)]) / 1000) >= 0) {
+			  	this.totalTimeWasted += (event.timestamp -  this.targets[encodeTargetString(event.targetID, event.targetInstance)]) / 1000;
+			  }	
+		  }
+	  }  
   }
  
-    get suggestionThresholds() {
+  get averageTimeWasted(){
+    return(this.totalTimeWasted/this.totalOutBreakCasts);
+  }
+  
+  get suggestionThresholds() {
     return {
-      actual: 5-(this.totalTimeWasted/(this.totalOutBreakCasts)).toFixed(1),
-      isLessThan: {
-        minor: (4),
-        average: (2),
-        major: (0),
+      actual: this.averageTimeWasted,
+      isGreaterThan: {
+        minor: (1),
+        average: (3),
+        major: (5),
       },
       style: 'number',
       suffix: 'Average',
@@ -80,8 +83,8 @@ class VirulentPlagueEfficiency extends Analyzer {
  		  .addSuggestion((suggest, actual, recommended) => {
 			return suggest(<Wrapper> You are casting <SpellLink id={SPELLS.VIRULENT_PLAGUE.id}/> too often. Try to cast <SpellLink id={SPELLS.VIRULENT_PLAGUE.id}/> as close to it falling off as possible.</Wrapper>)
 				.icon(SPELLS.VIRULENT_PLAGUE.icon)
-				.actual(`${(this.totalTimeWasted/this.totalOutBreakCasts).toFixed(1)} seconds of Virulent Plague uptime was wasted on average for each cast of Outbreak`)
-				.recommended(`<${6-(recommended)} is recommended`);
+				.actual(`${(this.averageTimeWasted).toFixed(1)} seconds of Virulent Plague uptime was wasted on average for each cast of Outbreak`)
+				.recommended(`<${recommended} is recommended`);
         });
   }
 
@@ -89,9 +92,9 @@ class VirulentPlagueEfficiency extends Analyzer {
     return (
       <StatisticBox
         icon={<SpellIcon id={SPELLS.VIRULENT_PLAGUE.id} />}
-        value={`${(this.totalTimeWasted/this.totalOutBreakCasts).toFixed(1)} seconds`}
+        value={`${(this.averageTimeWasted).toFixed(1)} seconds`}
         label={'Average Virulent Plague Duration Waste'}
-		tooltip={`A total amount of ${this.totalTimeWasted.toFixed(1)} seconds of Virulent Plague uptime was wasted with an average amount of ${(this.totalTimeWasted/this.totalOutBreakCasts).toFixed(1)} per cast`}
+		tooltip={`A total amount of ${this.totalTimeWasted.toFixed(1)} seconds of Virulent Plague uptime was wasted with an average amount of ${(this.averageTimeWasted).toFixed(1)} seconds per cast`}
       />
     );
   }
