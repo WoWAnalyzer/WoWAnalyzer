6bbc493bb6a4bfb790a3557455b411349bdc3a22
collcrom <collcrom@gmail.com>
2018-03-03 15:27:00 -0600

    removed health class variables

--- src/Parser/Rogue/Assassination/Modules/Items/ZoldyckFamilyTrainingShackles.js
+++ src/Parser/Rogue/Assassination/Modules/Items/ZoldyckFamilyTrainingShackles.js
@@ -14,62 +14,60 @@ import ItemDamageDone from 'Main/ItemDamageDone';
 */
 
 const POISONS_AND_BLEEDS = [
-	SPELLS.DEADLY_POISON_PROC.id,
-	SPELLS.DEADLY_POISON_DOT.id,
-	SPELLS.FROM_THE_SHADOWS.id,
-	SPELLS.POISON_BOMB.id,
-	SPELLS.POISON_KNIVES.id,
-	SPELLS.KINGSBANE.id,
-	SPELLS.KINGSBANE_MAINHAND.id,
-	SPELLS.KINGSBANE_OFFHAND.id,
-	SPELLS.ENVENOM.id,
-	SPELLS.POISONED_KNIFE.id,
-	SPELLS.TOXIC_BLADE_TALENT.id,
-	SPELLS.GARROTE.id,
-	SPELLS.RUPTURE.id,
+  SPELLS.DEADLY_POISON_PROC.id,
+  SPELLS.DEADLY_POISON_DOT.id,
+  SPELLS.FROM_THE_SHADOWS.id,
+  SPELLS.POISON_BOMB.id,
+  SPELLS.POISON_KNIVES.id,
+  SPELLS.KINGSBANE.id,
+  SPELLS.KINGSBANE_MAINHAND.id,
+  SPELLS.KINGSBANE_OFFHAND.id,
+  SPELLS.ENVENOM.id,
+  SPELLS.POISONED_KNIFE.id,
+  SPELLS.TOXIC_BLADE_TALENT.id,
+  SPELLS.GARROTE.id,
+  SPELLS.RUPTURE.id,
 ];
 
 const ZOLDYCK_FAMILY_TRAINING_SHACKLES = {
-	INCREASE: .3,
-	HEALTH_PERCENT: .3,
+  INCREASE: .3,
+  HEALTH_PERCENT: .3,
 };
 
 class ZoldyckFamilyTrainingShackles extends Analyzer {
-	static dependencies = {
-		combatants: Combatants,
-	};
+  static dependencies = {
+    combatants: Combatants,
+  };
 
-	bonusDamage = 0;
+  bonusDamage = 0;
 
-	on_initialized() {
+  on_initialized() {
     this.active = this.combatants.selected.hasWrists(ITEMS.ZOLDYCK_FAMILY_TRAINING_SHACKLES.id);
   }
 
   on_byPlayer_damage(event) {
-  	const spellId = event.ability.guid;
-  	if(!POISONS_AND_BLEEDS.includes(spellId)) {
-  		return;
-  	}
-  	this.currentHealth = event.hitPoints;
-  	this.maxHealth = event.maxHitPoints;
-  	if(this.healthPercent < ZOLDYCK_FAMILY_TRAINING_SHACKLES.HEALTH_PERCENT) {
-  		this.bonusDamage += calculateEffectiveDamage(event, ZOLDYCK_FAMILY_TRAINING_SHACKLES.INCREASE);
-  	}
+    const spellId = event.ability.guid;
+    if(!POISONS_AND_BLEEDS.includes(spellId)) {
+      return;
+    }
+    if(this.healthPercent(event.hitPoints, event.maxHitPoints) < ZOLDYCK_FAMILY_TRAINING_SHACKLES.HEALTH_PERCENT) {
+      this.bonusDamage += calculateEffectiveDamage(event, ZOLDYCK_FAMILY_TRAINING_SHACKLES.INCREASE);
+    }
   }
 
-  get healthPercent() {
-  	return this.currentHealth / this.maxHealth;
+  get healthPercent(currentHealth, maxHealth) {
+    return currentHealth / maxHealth;
   }
 
   item() {
-  	return {
-  		item: ITEMS.ZOLDYCK_FAMILY_TRAINING_SHACKLES,
-  		result: (
-  			<dfn data-tip={`Total Damage Contributed: <b>${formatNumber(this.bonusDamage)}</b>`}>
-  				<ItemDamageDone amount={this.bonusDamage}/>
-  			</dfn>
-  		),
-  	};
+    return {
+      item: ITEMS.ZOLDYCK_FAMILY_TRAINING_SHACKLES,
+      result: (
+        <dfn data-tip={`Total Damage Contributed: <b>${formatNumber(this.bonusDamage)}</b>`}>
+          <ItemDamageDone amount={this.bonusDamage}/>
+        </dfn>
+      ),
+    };
   }
 }
 
