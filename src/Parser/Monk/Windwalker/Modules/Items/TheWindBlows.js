82062836cdb1dae4ed684f304afa82b32ae527d3
collcrom <collcrom@gmail.com>
2018-03-03 20:11:07 -0600

    added the wind blows legendary

---
+++ src/Parser/Monk/Windwalker/Modules/Items/TheWindBlows.js
@@ -0,0 +1,35 @@
+import ITEMS from 'common/ITEMS';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import Analyzer from 'Parser/Core/Analyzer';
+import Wrapper from  'common/Wrapper';
+import Combatants from 'Parser/Core/Modules/Combatants';
+
+class TheWindBlows extends Analyzer {
+  static dependencies = {
+    combatatants: Combatants,
+  };
+  freeBlackoutKicks = 0; 
+  
+
+  on_initialized() {
+    this.active = this.combatatants.selected.hasHead(ITEMS.THE_WIND_BLOWS.id);
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.STRIKE_OF_THE_WINDLORD.id) {
+      return;
+    }
+    this.freeBlackoutKicks++;
+  }
+
+  item() {
+    return {
+      item: ITEMS.THE_WIND_BLOWS,
+      result: <Wrapper>{this.freeBlackoutKicks} free Blackout Kicks</Wrapper>,
+    };
+  }
+}
+
+export default TheWindBlows;
