8d98d3d8a5dc45cea0480ec7f39c1e95a0325218
Juko8 <ulrichenevoldsen@hotmail.com>
2018-03-05 20:38:03 +0100

    A missing bracket

--- src/Parser/Monk/Windwalker/Modules/Spells/FistsofFury.js
+++ src/Parser/Monk/Windwalker/Modules/Spells/FistsofFury.js
@@ -52,7 +52,7 @@ class FistsofFury extends Analyzer {
       when(this.averageTicks).isLessThan(averageTicksRecommended).addSuggestion((suggest, actual, recommended) => {
             return suggest(<span> You are cancelling your <SpellLink id={SPELLS.FISTS_OF_FURY_CAST.id} /> casts early and losing ticks </span>)
                 .icon(SPELLS.FISTS_OF_FURY_CAST.icon).actual(`${this.averageTicks.toFixed(2)} average ticks on each Fists of Fury cast`)
-                .recommended(`Aim to get 5 ticks with each Fists of Fury cast${tier20Text)
+                .recommended(`Aim to get 5 ticks with each Fists of Fury cast${tier20Text}`)
                 .regular(recommended - 0.2).major(recommended - 0.5);
         });
     }
