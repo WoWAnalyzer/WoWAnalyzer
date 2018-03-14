f4064456fa144d943b619536e35ed33083c21552
collcrom <collcrom@gmail.com>
2018-03-14 18:27:13 -0500

    updated changelog

--- src/Parser/Paladin/Protection/CHANGELOG.js
+++ src/Parser/Paladin/Protection/CHANGELOG.js
@@ -6,6 +6,11 @@ import ITEMS from 'common/ITEMS';
 import ItemLink from 'common/ItemLink';
 
 export default [
+	{
+		date: new Date('2018-03-14'),
+		changes: 'Updated all the abilites',
+		contributors: [Hewhosmites],
+	},
   {
     date: new Date('2018-03-03'),
     changes: <Wrapper>Added <ItemLink id={ITEMS.PILLARS_OF_INMOST_LIGHT.id} icon /></Wrapper>,
