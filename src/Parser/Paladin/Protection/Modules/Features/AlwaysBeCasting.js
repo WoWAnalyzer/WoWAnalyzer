b6eecdb456ca0c1e4d1764b8d7a9511ec6992c6b
collcrom <collcrom@gmail.com>
2018-03-14 18:08:20 -0500

    updated the alwaysBeCasting.js

--- src/Parser/Paladin/Protection/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Paladin/Protection/Modules/Features/AlwaysBeCasting.js
@@ -1,39 +1,39 @@
 import React from 'react';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import Wrapper from 'common/Wrapper';
+
+import { formatPercentage } from 'common/format';
 
 import CoreAlwaysBeCasting from 'Parser/Core/Modules/AlwaysBeCasting';
 
-import SPELLS from 'common/SPELLS';
-import { formatPercentage } from 'common/format';
 import { STATISTIC_ORDER } from 'Main/StatisticBox';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
-  static ABILITIES_ON_GCD = [
-    SPELLS.AVENGERS_SHIELD.id,
-    SPELLS.CONSECRATION_CAST.id,
-    SPELLS.DIVINE_STEED.id,
-    SPELLS.FLASH_OF_LIGHT.id,
-    SPELLS.HAMMER_OF_JUSTICE.id,
-    SPELLS.JUDGMENT_CAST.id,
-    SPELLS.EYE_OF_TYR.id,
-    //Talents
-    SPELLS.AEGIS_OF_LIGHT_TALENT.id,
-    SPELLS.BASTION_OF_LIGHT_TALENT.id,
-    SPELLS.BLESSED_HAMMER_TALENT.id,
-    SPELLS.BLESSING_OF_SPELLWARDING_TALENT.id,
-    SPELLS.SERAPHIM_TALENT.id,
-  ];
 
-  suggestions(when) {
-    const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
+  get suggestionThresholds() {
+    return {
+      actual: this.downtimePercentage,
+      isGreaterThan: {
+        minor: 0.2,
+        average: 0.35,
+        major: 0.4,
+      },
+      style: 'percentage',
+    };
+  }
 
-    when(deadTimePercentage).isGreaterThan(0.2)
-      .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC).</span>)
+   suggestions(when) {
+    const boss = this.owner.boss;
+
+    if (!boss || !boss.fight.disableDowntimeSuggestion) {
+      when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
+        return suggest(<Wrapper>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. Even if you have to move, try casting something instant with range like <SpellLink id={SPELLS.JUDGMENT_CAST.id} icon/> or <SpellLink id={SPELLS.AVENGERS_SHIELD.id} icon/></Wrapper>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended + 0.15).major(recommended + 0.2);
+          .recommended(`<${formatPercentage(recommended)}% is recommended`);
       });
+    }
   }
 
   statisticOrder = STATISTIC_ORDER.CORE(1);
