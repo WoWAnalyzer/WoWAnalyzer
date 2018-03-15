3ac55d16782c049eaea4439bf5a268ae81d3bae7
Martijn Hols <martijnhols@hotmail.com>
2018-03-15 22:23:39 +0100

    Assume SotR CDR is also 4 sec on blocked crits

--- src/Parser/Paladin/Protection/Modules/Spells/Judgment.js
+++ src/Parser/Paladin/Protection/Modules/Spells/Judgment.js
@@ -26,7 +26,8 @@ class Judgment extends Analyzer {
     }
 
     if (this.spellUsable.isOnCooldown(SPELLS.SHIELD_OF_THE_RIGHTEOUS.id)) {
-      const isCrit = event.hitType === HIT_TYPES.CRIT;
+      // Nope, I did not verify if blocked crits count as crits for this trait, I just assumed it. Please do test if you can and report back or fix this comment.
+      const isCrit = event.hitType === HIT_TYPES.CRIT || event.hitType === HIT_TYPES.BLOCKED_CRIT;
       const reduction = isCrit ? REDUCTION_TIME_CRIT : REDUCTION_TIME_REGULAR;
       this.spellUsable.reduceCooldown(SPELLS.SHIELD_OF_THE_RIGHTEOUS.id, reduction);
     }
