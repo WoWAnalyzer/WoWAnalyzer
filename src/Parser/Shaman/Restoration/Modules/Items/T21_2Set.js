14d88a6aa68d3bd4b316f2f487415325cf9fbb5d
niseko <rentz@hotmail.de>
2018-03-16 12:40:38 +0100

    added feeding to existing item modules

--- src/Parser/Shaman/Restoration/Modules/Items/T21_2Set.js
+++ src/Parser/Shaman/Restoration/Modules/Items/T21_2Set.js
@@ -8,10 +8,13 @@ import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import ItemHealingDone from 'Main/ItemHealingDone';
 
+import CooldownThroughputTracker from '../Features/CooldownThroughputTracker';
+
 class Restoration_Shaman_T21_2Set extends Analyzer {
   static dependencies = {
     abilityTracker: AbilityTracker,
     combatants: Combatants,
+    cooldownThroughputTracker: CooldownThroughputTracker,
   };
 
   on_initialized() {
@@ -20,12 +23,28 @@ class Restoration_Shaman_T21_2Set extends Analyzer {
 
   item() {
     const healing = this.abilityTracker.getAbility(SPELLS.RAINFALL.id).healingEffective;
+    const feeding = this.cooldownThroughputTracker.getIndirectHealing(SPELLS.RAINFALL.id);
 
     return {
       id: `spell-${SPELLS.RESTORATION_SHAMAN_T21_2SET_BONUS_BUFF.id}`,
       icon: <SpellIcon id={SPELLS.RAINFALL.id} />,
       title: <SpellLink id={SPELLS.RESTORATION_SHAMAN_T21_2SET_BONUS_BUFF.id} />,
-      result: <ItemHealingDone amount={healing} />,
+      result: (
+        <dfn
+          data-tip={`
+            Healing
+            <ul>
+              <li>${this.owner.formatItemHealingDone(healing)}</li>
+            </ul>
+            Feeding
+            <ul>
+              <li>${this.owner.formatItemHealingDone(feeding)}</li>
+            </ul>
+          `}
+        >
+          <ItemHealingDone amount={healing+feeding} />
+        </dfn>
+      ),
     };
   }
 }
