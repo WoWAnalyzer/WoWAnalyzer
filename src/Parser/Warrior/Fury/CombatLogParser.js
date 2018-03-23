d8eba427d270e93135c1423d5ab93f90cd5ccc72
Martijn Hols <martijnhols@hotmail.com>
2018-03-23 12:00:26 +0100

    Fury Warrior: Handle Convergence of Fates

--- src/Parser/Warrior/Fury/CombatLogParser.js
+++ src/Parser/Warrior/Fury/CombatLogParser.js
@@ -4,6 +4,7 @@ import DamageDone from 'Parser/Core/Modules/DamageDone';
 import Abilities from './Modules/Abilities';
 import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
 import CooldownThroughputTracker from './Modules/Features/CooldownThroughputTracker';
+import SpellUsable from './Modules/Features/SpellUsable';
 
 import EnrageUptime from './Modules/BuffDebuff/EnrageUptime';
 import FrothingBerserkerUptime from './Modules/BuffDebuff/FrothingBerserkerUptime';
@@ -24,6 +25,7 @@ class CombatLogParser extends CoreCombatLogParser {
     abilities: Abilities,
     alwaysBeCasting: AlwaysBeCasting,
     cooldownThroughputTracker: CooldownThroughputTracker,
+    spellUsable: SpellUsable,
 
     enrageUptime: EnrageUptime,
     frothingBerserkerUptime: FrothingBerserkerUptime,
