72746f1da4095bf4dea1423acbab6d781a1480b7
Juko8 <ulrichenevoldsen@hotmail.com>
2018-03-25 00:03:15 +0100

    [Monk] Allowing referral from links to peakofserenity.com to be tracked

--- src/Parser/Monk/Mistweaver/CONFIG.js
+++ src/Parser/Monk/Mistweaver/CONFIG.js
@@ -18,7 +18,7 @@ export default {
     <Wrapper>
       Hello all! Thanks so much for taking the time use this tool as a way to improve your play. The goal is to provide targeted suggestions to improve your overall Mistweaver Monk play. The suggestions are based on the current theorycrafting and practical knowledge from some of the best Mistweavers playing this game. (And even some former mistweavers who still like to help us dreamers out.) <br /> <br />
 
-      The tool is not perfect so I am always looking to improve it. If you have any suggestions or comments, don't hesitated to swing by the GitHub Issue linked below, or the <a href="https://discord.gg/0dkfBMAxzTkWj21F" target="_blank" rel="noopener noreferrer">Peak of Serenity</a> discord server. You can also contact me directly on either BattleNet (Anomoly#1464) or Discord (Anomoly#6931). Thanks and I hope you continue to enjoy the tool!
+      The tool is not perfect so I am always looking to improve it. If you have any suggestions or comments, don't hesitated to swing by the GitHub Issue linked below, or the <a href="https://discord.gg/0dkfBMAxzTkWj21F">Peak of Serenity</a> discord server. You can also contact me directly on either BattleNet (Anomoly#1464) or Discord (Anomoly#6931). Thanks and I hope you continue to enjoy the tool!
     </Wrapper>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
