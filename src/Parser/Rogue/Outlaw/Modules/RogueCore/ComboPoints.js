796f08c40c2ae91db155d240369d78bf27eb8c74
Sadurn <37560941+Sadurn@users.noreply.github.com>
2018-03-28 11:33:43 -0700

    Update ComboPoints.js
    Added note about unavoidable waste, added support for other combo point generators.

--- src/Parser/Rogue/Outlaw/Modules/RogueCore/ComboPoints.js
+++ src/Parser/Rogue/Outlaw/Modules/RogueCore/ComboPoints.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import SPELLS from 'common/SPELLS';
+import TALENTS from 'common/SPELLS/TALENTS';
 import SpellLink from 'common/SpellLink';
 import Wrapper from 'common/Wrapper';
 import resourceSuggest from 'Parser/Core/Modules/ResourceTracker/ResourceSuggest';
@@ -16,7 +17,7 @@ class ComboPoints extends Analyzer {
   
 
   makeExtraSuggestion(spell) {
-    return <Wrapper>Avoid wasting combo points when casting <SpellLink id={spell.id}  />. </Wrapper>;
+    return <Wrapper>Avoid wasting combo points when casting <SpellLink id={spell.id}  />. Note that some combo-point wastage is unavoidable due to second saber slash procs during the duration of <SpellLink id={193356}  />.  </Wrapper>;
   }
 
   suggestions(when) {    
@@ -34,6 +35,27 @@ class ComboPoints extends Analyzer {
       avg: 0.1, 
       major: 0.2,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.AMBUSH),
+    });
+  	resourceSuggest(when,  this.comboPointTracker, {
+      spell: SPELLS.PISTOL_SHOT,
+      minor: 0,
+      avg: 0.1, 
+      major: 0.2,
+      extraSuggestion: this.makeExtraSuggestion(SPELLS.PISTOL_SHOT),
+    });
+  	resourceSuggest(when,  this.comboPointTracker, {
+      spell: SPELLS.BLUNDERBUSS,
+      minor: 0,
+      avg: 0.1, 
+      major: 0.2,
+      extraSuggestion: this.makeExtraSuggestion(SPELLS.BLUNDERBUSS),
+    });
+    resourceSuggest(when,  this.comboPointTracker, {
+      spell: TALENTS.GHOSTLY_STRIKE_TALENT,
+      minor: 0,
+      avg: 0.1, 
+      major: 0.2,
+      extraSuggestion: this.makeExtraSuggestion(TALENTS.GHOSTLY_STRIKE_TALENT),
     });
     }
 }
