eb48c8985e85cf44cf89048e6297236c8d282e38
niseko <rentz@hotmail.de>
2018-03-28 04:37:15 +0200

    removed more deprecated stuff

--- src/Parser/Shaman/Restoration/Modules/ShamanCore/ShamanAbilityTracker.js
+++ src/Parser/Shaman/Restoration/Modules/ShamanCore/ShamanAbilityTracker.js
@@ -1,12 +1,16 @@
 import SPELLS from 'common/SPELLS';
 
 import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
+import Combatants from 'Parser/Core/Modules/Combatants';
 
 const TIDAL_WAVES_SPELL_ID = 53390;
 const TIDAL_WAVES_BUFF_EXPIRATION_BUFFER = 50; // the buff expiration can occur several MS before the heal event is logged, this is the buffer time that an IoL charge may have dropped during which it will still be considered active.
 const TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME = 100; // Minimal duration for which you must have tidal waves. Prevents it from counting a HS/HW as buffed when you cast a riptide at the end.
 
 class ShamanAbilityTracker extends AbilityTracker {
+  static dependencies = {
+    combatants: Combatants,
+  };
   on_byPlayer_cast(event) {
     if (super.on_byPlayer_cast) {
       super.on_byPlayer_cast(event);
@@ -17,7 +21,7 @@ class ShamanAbilityTracker extends AbilityTracker {
       return;
     }
 
-    if (!this.owner.modules.combatants.selected.hasBuff(SPELLS.RESTORATION_SHAMAN_T20_4SET_BONUS_BUFF.id, event.timestamp)) {
+    if (!this.combatants.selected.hasBuff(SPELLS.RESTORATION_SHAMAN_T20_4SET_BONUS_BUFF.id, event.timestamp)) {
       return;
     }
 
@@ -33,7 +37,7 @@ class ShamanAbilityTracker extends AbilityTracker {
     const cast = this.getAbility(spellId, event.ability);
 
     if (spellId === SPELLS.HEALING_WAVE.id || spellId === SPELLS.HEALING_SURGE_RESTORATION.id) {
-      const hasTw = this.owner.modules.combatants.selected.hasBuff(TIDAL_WAVES_SPELL_ID, event.timestamp, TIDAL_WAVES_BUFF_EXPIRATION_BUFFER, TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME);
+      const hasTw = this.combatants.selected.hasBuff(TIDAL_WAVES_SPELL_ID, event.timestamp, TIDAL_WAVES_BUFF_EXPIRATION_BUFFER, TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME);
 
       if (hasTw) {
         cast.healingTwHits = (cast.healingTwHits || 0) + 1;
