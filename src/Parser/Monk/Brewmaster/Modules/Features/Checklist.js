d4126c45a8905a26ab99046ad56a224dbeac537e
J. David Smith <emallson@atlanis.net>
2018-04-08 12:17:24 -0400

    Removed explicit icons from BrM checklist

--- src/Parser/Monk/Brewmaster/Modules/Features/Checklist.js
+++ src/Parser/Monk/Brewmaster/Modules/Features/Checklist.js
@@ -35,12 +35,12 @@ class Checklist extends CoreChecklist {
     new Rule({
       name: (
         <Wrapper>
-          Mitigate damage with <SpellLink id={SPELLS.IRONSKIN_BREW.id} icon />.
+          Mitigate damage with <SpellLink id={SPELLS.IRONSKIN_BREW.id} />.
         </Wrapper>
       ),
       description: (
         <Wrapper>
-          <SpellLink id={SPELLS.STAGGER.id} icon /> is our main damage mitigation tool. <SpellLink id={SPELLS.IRONSKIN_BREW.id} icon /> increases the amount of damage that we can mitigate with Stagger while active. It is possible to maintain 100% uptime without reaching any particular haste threshold due to the cooldown reduction applied by <SpellLink id={SPELLS.KEG_SMASH.id} icon /> and <SpellLink id={SPELLS.TIGER_PALM.id} icon />. If you are having difficulty maintaining your buff you may need to improve your cast efficiency or reduce the amount of purification you are doing.
+          <SpellLink id={SPELLS.STAGGER.id} /> is our main damage mitigation tool. <SpellLink id={SPELLS.IRONSKIN_BREW.id} /> increases the amount of damage that we can mitigate with Stagger while active. It is possible to maintain 100% uptime without reaching any particular haste threshold due to the cooldown reduction applied by <SpellLink id={SPELLS.KEG_SMASH.id} /> and <SpellLink id={SPELLS.TIGER_PALM.id} />. If you are having difficulty maintaining your buff you may need to improve your cast efficiency or reduce the amount of purification you are doing.
         </Wrapper>
       ),
       requirements: () => {
@@ -55,12 +55,12 @@ class Checklist extends CoreChecklist {
     new Rule({
       name: (
         <Wrapper>
-          Mitigate damage with <SpellLink id={SPELLS.BREATH_OF_FIRE.id} icon />.
+          Mitigate damage with <SpellLink id={SPELLS.BREATH_OF_FIRE.id} />.
         </Wrapper>
       ),
       description: (
         <Wrapper>
-          <SpellLink id={SPELLS.BREATH_OF_FIRE.id} icon /> provides a 4-7% damage reduction through the <SpellLink id={SPELLS.HOT_BLOODED.id} icon /> trait. It is possible to maintain 100% uptime on this debuff both with and without <ItemLink id={ITEMS.SALSALABIMS_LOST_TUNIC.id} icon  />.
+          <SpellLink id={SPELLS.BREATH_OF_FIRE.id} /> provides a 4-7% damage reduction through the <SpellLink id={SPELLS.HOT_BLOODED.id} /> trait. It is possible to maintain 100% uptime on this debuff both with and without <ItemLink id={ITEMS.SALSALABIMS_LOST_TUNIC.id} />.
         </Wrapper>
       ),
       requirements: () => {
@@ -95,7 +95,7 @@ class Checklist extends CoreChecklist {
       name: 'Generate enough brews through your rotation.',
       description: (
         <Wrapper>
-          The cooldown of all brews is reduced by your key rotational abilities: <SpellLink id={SPELLS.KEG_SMASH.id} icon /> and <SpellLink id={SPELLS.TIGER_PALM.id} icon />. Maintaining a proper rotation will help ensure you have enough brews available to maintain <SpellLink id={SPELLS.IRONSKIN_BREW.id} icon />.
+          The cooldown of all brews is reduced by your key rotational abilities: <SpellLink id={SPELLS.KEG_SMASH.id} /> and <SpellLink id={SPELLS.TIGER_PALM.id} />. Maintaining a proper rotation will help ensure you have enough brews available to maintain <SpellLink id={SPELLS.IRONSKIN_BREW.id} />.
         </Wrapper>
       ),
       requirements: () => {
@@ -145,18 +145,18 @@ class Checklist extends CoreChecklist {
 
         const boc = this.tp.bocEmpoweredThreshold;
         reqs.push(new Requirement({
-          name: <Wrapper><SpellLink id={SPELLS.BLACKOUT_COMBO_TALENT.id} icon />-empowered <SpellLink id={SPELLS.TIGER_PALM.id} icon>Tiger Palms</SpellLink></Wrapper>,
+          name: <Wrapper><SpellLink id={SPELLS.BLACKOUT_COMBO_TALENT.id} />-empowered <SpellLink id={SPELLS.TIGER_PALM.id} >Tiger Palms</SpellLink></Wrapper>,
           check: () => boc,
           when: () => !!boc,
         }));
         reqs.push(new Requirement({
-          name: <dfn data-tip={"For the purposes of DPS, any Blackout Combo buff that goes unused or is used on a non-Tiger Palm ability is considered Wasted. Specific fight mechanics may call for using Blackout Combo on other abilities."}>Wasted <SpellLink id={SPELLS.BLACKOUT_COMBO_TALENT.id} icon /> empowerments</dfn>,
+          name: <dfn data-tip={"For the purposes of DPS, any Blackout Combo buff that goes unused or is used on a non-Tiger Palm ability is considered Wasted. Specific fight mechanics may call for using Blackout Combo on other abilities."}>Wasted <SpellLink id={SPELLS.BLACKOUT_COMBO_TALENT.id} /> empowerments</dfn>,
           check: () => this.boc.dpsWasteThreshold,
           when: () => !!boc,
         }));
 
         reqs.push(new Requirement({
-          name: <Wrapper><SpellLink id={SPELLS.RUSHING_JADE_WIND.id} icon /> uptime</Wrapper>,
+          name: <Wrapper><SpellLink id={SPELLS.RUSHING_JADE_WIND.id} /> uptime</Wrapper>,
           check: () => this.rjw.uptimeThreshold,
           when: () => !!this.rjw.uptimeThreshold,
         }));
