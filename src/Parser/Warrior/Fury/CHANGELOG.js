16bc7baa03df1729df5032048994b52579d87bf2
Aelexe <Aelexe@gmail.com>
2018-04-09 11:48:31 +1200

    Fix Charge cooldown/charge modifications for Double Time

--- src/Parser/Warrior/Fury/CHANGELOG.js
+++ src/Parser/Warrior/Fury/CHANGELOG.js
@@ -1,6 +1,16 @@
-import { Maldark } from 'CONTRIBUTORS';
+import React from 'react';
+
+import { Maldark, Aelexe} from 'CONTRIBUTORS';
+import Wrapper from 'common/Wrapper';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
 
 export default [
+  {
+    date: new Date('2018-04-09'),
+    changes: <Wrapper>Add <SpellLink id={SPELLS.CHARGE.id} icon /> cooldown and charge modifications for <SpellLink id={SPELLS.DOUBLE_TIME_TALENT.id} icon />.</Wrapper>,
+    contributors: [Aelexe],
+  },
   {
     date: new Date('2017-12-25'),
     changes: 'Added T21 analysis as well as Rampage cancellation analysis and Mastery support.',
