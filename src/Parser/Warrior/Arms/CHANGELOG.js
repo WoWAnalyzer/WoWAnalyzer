33fa254c0c5e2c7ca26a7cad3239f41baca30e43
Aelexe <Aelexe@gmail.com>
2018-04-09 21:20:16 +1200

    Create Battle Cry analyzer with Shattered Defense analysis
    Add change log entry of Battle Cry analyzer

--- src/Parser/Warrior/Arms/CHANGELOG.js
+++ src/Parser/Warrior/Arms/CHANGELOG.js
@@ -8,6 +8,11 @@ import SpellLink from 'common/SpellLink';
 export default [
   {
     date: new Date('2018-04-09'),
+    changes: <Wrapper>Added a suggestion for preparing <SpellLink id={SPELLS.SHATTERED_DEFENSES.id} icon /> for <SpellLink id={SPELLS.BATTLE_CRY.id} icon />.</Wrapper>,
+    contributors: [Aelexe],
+  },
+  {
+    date: new Date('2018-04-09'),
     changes: <Wrapper>Add <SpellLink id={SPELLS.CHARGE.id} icon /> cooldown and charge modifications for <SpellLink id={SPELLS.DOUBLE_TIME_TALENT.id} icon />.</Wrapper>,
     contributors: [Aelexe],
   },
--- src/Parser/Warrior/Arms/CombatLogParser.js
+++ src/Parser/Warrior/Arms/CombatLogParser.js
@@ -3,6 +3,7 @@ import DamageDone from 'Parser/Core/Modules/DamageDone';
 
 import Abilities from './Modules/Abilities';
 import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
+import BattleCry from './Modules/Features/BattleCry';
 import CooldownThroughputTracker from './Modules/Features/CooldownThroughputTracker';
 import ColossusSmashUptime from './Modules/BuffDebuff/ColossusSmashUptime';
 import TacticianProc from './Modules/BuffDebuff/TacticianProc';
@@ -20,6 +21,7 @@ class CombatLogParser extends CoreCombatLogParser {
 
     // Features
     alwaysBeCasting: AlwaysBeCasting,
+    battleCry: BattleCry,
     cooldownThroughputTracker: CooldownThroughputTracker,
     colossusSmashUptime: ColossusSmashUptime,
     tacticianProc: TacticianProc,
