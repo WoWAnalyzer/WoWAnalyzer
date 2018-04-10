89ca98a0596a759db10523f2d2409b093af91c15
niseko <rentz@hotmail.de>
2018-04-10 08:55:28 +0200

    updated contributors

--- src/CONTRIBUTORS.js
+++ src/CONTRIBUTORS.js
@@ -328,7 +328,21 @@ export const joshinator = {
 export const niseko = {
   nickname: 'niseko',
   github: 'niseko',
+  discord: 'nisekoi#4130',
   avatar: require('./Images/niseko-avatar.jpg'),
+  mains: [{
+    name: "Niseko",
+    spec: SPECS.RESTORATION_SHAMAN,
+    link: "https://worldofwarcraft.com/en-gb/character/stormscale/niseko",
+  },
+  {
+    name: "Nisefy",
+    spec: SPECS.MISTWEAVER_MONK,
+    link: "https://worldofwarcraft.com/en-gb/character/stormscale/nisefy",
+  }],
+  links: {
+    "Ancestral Guidance": "https://ancestralguidance.com/",
+  },
 };
 export const Aelexe = {
   nickname: 'Aelexe',
