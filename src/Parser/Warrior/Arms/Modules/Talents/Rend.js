19950f6228d19e826c09518849f8d59fe117a9ca
Aelexe <Aelexe@gmail.com>
2018-04-11 12:45:37 +1200

    Fix code review suggestions

--- src/Parser/Warrior/Arms/Modules/Talents/Rend.js
+++ src/Parser/Warrior/Arms/Modules/Talents/Rend.js
@@ -5,6 +5,7 @@ import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import Enemies from 'Parser/Core/Modules/Enemies';
+import { encodeTargetString } from 'Parser/Core/Modules/EnemyInstances';
 import Analyzer from 'Parser/Core/Analyzer';
 
 const EXECUTE_RANGE = 0.2;
@@ -26,28 +27,30 @@ class RendAnalyzer extends Analyzer {
   on_byPlayer_cast(event) {
     if(SPELLS.REND_TALENT.id === event.ability.guid) {
       // If Rend is used flag the target to check their health on the next damage event.
-      this.targets.push(event.targetID);
+      this.targets.push(encodeTargetString(event.targetID, event.targetInstance));
     }
   }
 
   on_byPlayer_damage(event) {
-    const targetId = event.targetID;
+    const targetString = encodeTargetString(event.targetID, event.targetInstance);
 
-    if(SPELLS.REND_TALENT.id === event.ability.guid && this.targets.indexOf(targetId) !== -1) {
+    if(SPELLS.REND_TALENT.id === event.ability.guid && this.targets.indexOf(targetString) !== -1) {
+      // Unflag the target for checking.
+      this.targets.splice(this.targets.indexOf(targetString), 1);
+
       // Check if the target's health was within execute range.
-      if(event.hitPoints / event.maxHitPoints < EXECUTE_RANGE) {
-        if(this.failedTargets.indexOf(targetId) === -1) {
-          // If this is the first time Rend was used on this target in execute range we flag that target.
-          // This is to prevent false positives from Rend being applied just as the target enters execute range.
-          this.failedTargets.push(targetId);
-        } else {
-          // Increment the counter
-          this.rendsInExecuteRange += 1;
-        }
+      if(event.hitPoints / event.maxHitPoints >= EXECUTE_RANGE) {
+        return;
       }
 
-      // Unflag the target for checking.
-      this.targets.splice(this.targets.indexOf(targetId), 1);
+      if(this.failedTargets.indexOf(targetString) === -1) {
+        // If this is the first time Rend was used on this target in execute range we flag that target.
+        // This is to prevent false positives from Rend being applied just as the target enters execute range.
+        this.failedTargets.push(targetString);
+      } else {
+        // Increment the counter
+        this.rendsInExecuteRange += 1;
+      }
     }
   }
 
@@ -64,7 +67,7 @@ class RendAnalyzer extends Analyzer {
         return suggest(<Wrapper>You should avoid using <SpellLink id={SPELLS.REND_TALENT.id} icon/> on a target in <SpellLink id={SPELLS.EXECUTE.id} icon/> range.</Wrapper>)
           .icon(SPELLS.REND_TALENT.icon)
           .actual(`Rend was used ${actual} times on a target in execute range`)
-          .recommended('0 is recommended.');
+          .recommended(`${recommended} is recommended.`);
       });
   }
 }
