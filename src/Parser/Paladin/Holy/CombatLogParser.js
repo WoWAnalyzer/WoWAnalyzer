bcbf46b4b4e348bd09f01ef269d22cf8bd249941
Martijn Hols <martijnhols@hotmail.com>
2018-04-14 15:46:32 +0200

    Holy Paladin: remove artifact traits/abilities

--- src/Parser/Paladin/Holy/CombatLogParser.js
+++ src/Parser/Paladin/Holy/CombatLogParser.js
@@ -26,7 +26,6 @@ import Abilities from './Modules/Abilities';
 import Checklist from './Modules/Features/Checklist';
 import MasteryEffectiveness from './Modules/Features/MasteryEffectiveness';
 import AlwaysBeCasting from './Modules/Features/AlwaysBeCasting';
-import TyrsDeliverance from './Modules/Features/TyrsDeliverance';
 import CooldownThroughputTracker from './Modules/Features/CooldownThroughputTracker';
 import StatValues from './Modules/Features/StatValues';
 
@@ -38,15 +37,6 @@ import HolyAvenger from './Modules/Talents/HolyAvenger';
 import DivinePurpose from './Modules/Talents/DivinePurpose';
 import CrusadersMight from './Modules/Talents/CrusadersMight';
 import JudgmentOfLight from './Modules/Talents/JudgmentOfLight';
-
-import RelicTraits from './Modules/Traits/RelicTraits';
-import DeliverTheLight from './Modules/Traits/DeliverTheLight';
-import ExpelTheDarkness from './Modules/Traits/ExpelTheDarkness';
-import SacredDawn from './Modules/Traits/SacredDawn';
-import SecondSunrise from './Modules/Traits/SecondSunrise';
-import ShockTreatment from './Modules/Traits/ShockTreatment';
-import TyrsMunificence from './Modules/Traits/TyrsMunificence';
-import JusticeThroughSacrifice from './Modules/Traits/JusticeThroughSacrifice';
 
 import DrapeOfShame from './Modules/Items/DrapeOfShame';
 import Ilterendi from './Modules/Items/Ilterendi';
@@ -91,7 +81,6 @@ class CombatLogParser extends CoreCombatLogParser {
     abilities: Abilities,
     masteryEffectiveness: MasteryEffectiveness,
     alwaysBeCasting: AlwaysBeCasting,
-    tyrsDeliverance: TyrsDeliverance,
     cooldownThroughputTracker: CooldownThroughputTracker,
     statValues: StatValues,
 
@@ -104,16 +93,6 @@ class CombatLogParser extends CoreCombatLogParser {
     divinePurpose: DivinePurpose,
     crusadersMight: CrusadersMight,
     judgmentOfLight: JudgmentOfLight,
-
-    // Traits
-    relicTraits: RelicTraits,
-    deliverTheLight: DeliverTheLight,
-    expelTheDarkness: ExpelTheDarkness,
-    sacredDawn: SacredDawn,
-    secondSunrise: SecondSunrise,
-    shockTreatment: ShockTreatment,
-    tyrsMunificence: TyrsMunificence,
-    justiceThroughSacrifice: JusticeThroughSacrifice,
 
     // Items:
     drapeOfShame: DrapeOfShame,
