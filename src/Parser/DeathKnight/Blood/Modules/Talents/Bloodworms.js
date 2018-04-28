137a6e0493fa3ee9891379a917fae63ffa2974f3
yajinni <yajinni@gmail.com>
2018-04-27 21:25:29 -0400

    Final Code for bloodworms

--- src/Parser/DeathKnight/Blood/Modules/Talents/Bloodworms.js
+++ src/Parser/DeathKnight/Blood/Modules/Talents/Bloodworms.js
@@ -7,6 +7,8 @@ import Combatants from 'Parser/Core/Modules/Combatants';
 import SpellIcon from 'common/SpellIcon';
 import { formatThousands } from 'common/format';
 
+//Worms last 15 sec. But sometimes lag and such makes them expire a little bit early.
+const WORMLIFESPAN = 14900;
 class Bloodworms extends Analyzer {
   static dependencies = {
     abilityTracker: AbilityTracker,
@@ -16,8 +18,8 @@ class Bloodworms extends Analyzer {
   totalSummons=0;
   totalHealing=0;
   totalDamage=0;
-  sourceInstance=0;
   poppedEarly=0;
+  wormID=0;
 
   bloodworm = [];
 
@@ -27,33 +29,33 @@ class Bloodworms extends Analyzer {
   }
 
   poppedWorms(bloodworm) {
-    console.log(bloodworm);
-    const popped = bloodworm.filter(e => {
-      console.log(e.killedTime - e.summonedTime);
-      return e.killedTime - e.summonedTime <= 15000
-    });
-    return popped.length;
+    return bloodworm.filter(e =>  e.killedTime - e.summonedTime <= WORMLIFESPAN).length;
   }
 
   on_byPlayer_summon(event) {
-    if (event.ability.guid === SPELLS.BLOODWORM.id) {
-      this.bloodworm.push({
-        uniqueID: event.targetInstance,
-        summonedTime: event.timestamp,
-      });
-      this.totalSummons+= 1;
+    if (event.ability.guid !== SPELLS.BLOODWORM.id) {
+      return;
     }
+    this.bloodworm.push({
+      uniqueID: event.targetInstance,
+      summonedTime: event.timestamp,
+    });
+    this.totalSummons+= 1;
+    this.wormID = event.targetID;
   }
 
   on_byPlayerPet_damage(event) {
-    if (event.sourceID === this.wormID) {
-      this.totalDamage += event.amount + (event.absorbed || 0);
-      console.log("Worm Damage ", this.sourceInstance);
+    if (event.sourceID !== this.wormID) {
+      return;
     }
-  }
+      this.totalDamage += event.amount + (event.absorbed || 0);
+    }
+
 
   on_byPlayerPet_instakill(event) {
-    if (event.ability.guid === SPELLS.BLOODWORM_DEATH.id) {
+    if (event.ability.guid !== SPELLS.BLOODWORM_DEATH.id) {
+      return;
+    }
       let index = -1;
       this.bloodworm.forEach((e, i) => {
         if (e.uniqueID === event.targetInstance) {
@@ -62,19 +64,16 @@ class Bloodworms extends Analyzer {
       });
 
       if (index === -1) {
-        console.log("Death of unknown Bloodworm");
         return;
       }
-      console.log("index    ", index);
       this.bloodworm[index].killedTime = event.timestamp;
-    }
   }
 
   on_toPlayer_heal(event) {
-    if (event.ability.guid === SPELLS.BLOODWORM_DEATH.id) {
-      console.log("Worm Heal ");
-      this.totalHealing+= (event.amount || 0) + (event.absorbed || 0);
+    if (event.ability.guid !== SPELLS.BLOODWORM_DEATH.id) {
+      return;
     }
+      this.totalHealing+= (event.amount || 0) + (event.absorbed || 0);
   }
 
   statistic() {
@@ -86,7 +85,7 @@ class Bloodworms extends Analyzer {
         label="Bloodworm Stats"
         tooltip={`<strong>Damage:</strong> ${formatThousands(this.totalDamage)} / ${this.owner.formatItemDamageDone(this.totalDamage)}<br>
         <strong>Number of worms summoned:</strong> ${this.totalSummons}<br>
-        <strong># of worms popped early:</strong> ${this.poppedWorms(this.bloodworms)}
+        <strong># of worms popped early:</strong> ${this.poppedWorms(this.bloodworm)}
         `}
       />
 
