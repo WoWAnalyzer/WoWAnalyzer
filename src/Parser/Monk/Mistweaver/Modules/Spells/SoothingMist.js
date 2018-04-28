8680edeac23d7dcd976faa93505cd618ed861c5a
anomoly <jkippley@gmail.com>
2018-04-28 09:02:21 -0400

    SooM Threshold Update

--- src/Parser/Monk/Mistweaver/Modules/Spells/SoothingMist.js
+++ src/Parser/Monk/Mistweaver/Modules/Spells/SoothingMist.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
+import SUGGESTION_IMPORTANCE from 'Parser/Core/ISSUE_IMPORTANCE';
 
 import Combatants from 'Parser/Core/Modules/Combatants';
 
@@ -34,9 +35,7 @@ class SoothingMist extends Analyzer {
 
   get soomTicksPerDuration() {
     const soomTicks = (this.soomTicks * 2 / this.owner.fightDuration * 1000) || 0;
-    if (soomTicks >= 1.5) {
-      return true;
-    }
+    return soomTicks >= 1.5;
   }
 
   get suggestionThresholds() {
@@ -48,15 +47,14 @@ class SoothingMist extends Analyzer {
   }
 
   suggestions(when) {
-    when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
+    when(this.suggestionThresholds).addSuggestion((suggest) => {
         return suggest(
           <React.Fragment>
             You are allowing <SpellLink id={SPELLS.SOOTHING_MIST.id} /> to channel for an extended period of time. <SpellLink id={SPELLS.SOOTHING_MIST.id} /> does little healing, so your time is better spent DPS'ing throug the use of <SpellLink id={SPELLS.TIGER_PALM.id} /> and <SpellLink id={SPELLS.BLACKOUT_KICK.id} />.
           </React.Fragment>
         )
           .icon(SPELLS.SOOTHING_MIST.icon)
-          .actual(`${this.soomTicksPerDuration.toFixed(2)} ticks per second`)
-          .recommended(`<${recommended} ticks per second is recommended`);
+          .staticImportance(SUGGESTION_IMPORTANCE.MAJOR);
       });
   }
 }
