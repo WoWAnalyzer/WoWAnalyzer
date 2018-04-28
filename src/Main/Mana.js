9fbed50c3da7d440893b4c3badc09d1d34765acd
YogeshSharma0201 <yogesh02011998@gmail.com>
2018-04-28 20:08:30 +0530

    Reimplement ManaUsage graph using chart.js

--- src/Main/Mana.js
+++ src/Main/Mana.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ManaLevelGraph from 'Main/ManaLevelGraph';
+import ManaUsageGraph from 'Main/ManaUsageGraph';
+
 
 const Mana = ({ parser }) => (
   <div>
@@ -13,6 +15,15 @@ const Mana = ({ parser }) => (
       end={parser.fight.end_time}
       manaUpdates={parser.modules.manaValues.manaUpdates}
       currentTimestamp={parser.currentTimestamp}
+    /> {/* the currentTimestamp makes sure the Mana tab re-renders after parsing events */}
+
+    <h1>Mana usage</h1>
+    <ManaUsageGraph
+      start={parser.fight.start_time}
+      end={parser.fight.end_time}
+      healingBySecond={parser.modules.healingDone.bySecond}
+      manaUpdates={parser.modules.manaValues.manaUpdates}
+      timestamp={parser.currentTimestamp}
     /> {/* the currentTimestamp makes sure the Mana tab re-renders after parsing events */}
 
   </div>
