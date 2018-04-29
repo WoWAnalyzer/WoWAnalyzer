c3274ad2e6e02a508137e80af1490ca8dfe6309b
Martijn Hols <martijnhols@hotmail.com>
2018-04-29 12:46:29 +0200

    Fix eslint warning

--- src/Main/Layout/NavigationBar.js
+++ src/Main/Layout/NavigationBar.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import PremiumIcon from 'Icons/Premium';
 import GitHubIcon from 'Icons/GitHubMarkSmall';
 
 import { getFightId, getPlayerName } from 'selectors/url/report';
