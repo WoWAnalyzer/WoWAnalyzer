c1af6cb941764af1513550fbc5b550887b6db5a4
anomoly <jkippley@gmail.com>
2018-05-04 10:13:41 -0400

    Initial Uldir Boss Setup

---
+++ src/Raids/Uldir/Taloc.js
@@ -0,0 +1,16 @@
+import Background from './Images/Backgrounds/Aggramar.jpg';
+import Headshot from './Images/Headshots/Aggramar.png';
+
+export default {
+  id: 2144,
+  name: 'Taloc',
+  background: Background,
+  headshot: Headshot,
+  icon: '',
+  fight: {
+    //vantusRuneBuffId: 250144,
+    // TODO: Add fight specific props
+    // e.g. baseDowntime (seconds, percentage, based on (de)buff, etc)
+    // e.g. ads
+  },
+};
