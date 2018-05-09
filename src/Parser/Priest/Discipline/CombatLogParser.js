d4192ff462f0754259da94d4482c0263d9b5ffaf
karlpralow <karlpralow@gmail.com>
2018-05-09 13:55:33 -0400

    Lenience Update

--- src/Parser/Priest/Discipline/CombatLogParser.js
+++ src/Parser/Priest/Discipline/CombatLogParser.js
@@ -26,7 +26,7 @@ import AtonementApplicationSource from './Modules/Features/AtonementApplicationS
 import AtonementDamageSource from './Modules/Features/AtonementDamageSource';
 import AtonementHealingDone from './Modules/Features/AtonementHealingDone';
 import PowerWordBarrier from './Modules/Features/PowerWordBarrier';
-// import LeniencesReward from './Modules/Features/LeniencesReward';
+import Lenience from './Modules/Spells/Lenience';
 import PurgeTheWicked from './Modules/Features/PurgeTheWicked';
 
 import Tier19_2set from './Modules/Items/Tier19_2set';
@@ -84,7 +84,7 @@ class CombatLogParser extends CoreCombatLogParser {
     atonementDamageSource: AtonementDamageSource,
     atonementHealingDone: AtonementHealingDone,
     powerWordBarrier: PowerWordBarrier,
-    // leniencesReward: LeniencesReward,
+    lenience: Lenience,
     purgeTheWicked: PurgeTheWicked,
     rapture: Rapture,
 
