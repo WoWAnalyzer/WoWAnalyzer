e06e78193487994a78bbb5b862b3247eb9216a53
Ryan Park <ryan.park@unosquare.com>
2018-05-13 23:03:35 +0100

    Reverence effectiveness

--- src/Parser/Priest/Discipline/Modules/Spells/Reverence.js
+++ src/Parser/Priest/Discipline/Modules/Spells/Reverence.js
@@ -24,22 +24,10 @@ class Reverence extends Analyzer {
     statTracker: StatTracker,
   };
 
-  healing = 0;
+  reverenceHealing = 0;
+  baseHealing = 0; // Healing that mastery stemmed from
 
-  on_byPlayer_heal(event) {
-    // Check if the heal is whitelisted
-    if (!PRIEST_WHITELIST.includes(event.ability.guid)) {
-      return;
-    }
-
-    // Get the target
-    const target = this.combatants.getEntity(event);
-
-    // Mastery only buffs players benefitting from Atonement
-    if (!target.hasBuff(SPELLS.ATONEMENT_BUFF.id)) {
-      return;
-    }
-
+  getReverenceHealing(event) {
     // Get our mastery level at the moment, this is required for temporary buffs
     const currentMastery = this.statTracker.currentMasteryPercentage;
     const masteryContribution = calculateEffectiveHealing(
@@ -47,7 +35,31 @@ class Reverence extends Analyzer {
       currentMastery
     );
 
-    this.healing += masteryContribution;
+    return masteryContribution;
+  }
+
+  on_byPlayer_heal(event) {
+    // Check if the heal is whitelisted
+    if (!PRIEST_WHITELIST.includes(event.ability.guid)) {
+      return 0;
+    }
+
+    // Get the target
+    const target = this.combatants.getEntity(event);
+
+    // Mastery only buffs players benefitting from Atonement
+    if (!target.hasBuff(SPELLS.ATONEMENT_BUFF.id)) {
+      return 0;
+    }
+
+    // Calculate healing
+    const reverenceHealing = this.getReverenceHealing(event);
+
+    // Account for base healing without mastery (??? JD ???)
+    const baseHealing = event.amount - reverenceHealing;
+
+    this.baseHealing += baseHealing;
+    this.reverenceHealing += reverenceHealing;
   }
 
   statistic() {
@@ -55,13 +67,19 @@ class Reverence extends Analyzer {
       <StatisticBox
         icon={<SpellIcon id={SPELLS.REVERENCE.id} />}
         value={`${formatNumber(
-          this.healing / this.owner.fightDuration * 1000
+          this.reverenceHealing / this.owner.fightDuration * 1000
         )} HPS`}
         label={
           <dfn
-            data-tip={`Reverence contributed towards ${formatPercentage(
-              this.owner.getPercentageOfTotalHealingDone(this.healing)
-            )}% of your healing.`}
+            data-tip={`
+            Reverence contributed towards ${formatPercentage(
+              this.owner.getPercentageOfTotalHealingDone(this.reverenceHealing)
+            )}% of your healing.
+
+            ${formatPercentage(
+              this.owner.getPercentageOfTotalHealingDone(this.baseHealing)
+            )}% of your healing benefitted from Reverence.
+            `}
           >
             Mastery Healing
           </dfn>
