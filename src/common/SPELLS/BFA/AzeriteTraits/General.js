de4c3f9dcaa6562de85528500dbc79bdc114931a
Martijn Hols <martijnhols@hotmail.com>
2018-05-16 18:55:55 +0200

    Implement a few more general traits

--- src/common/SPELLS/BFA/AzeriteTraits/General.js
+++ src/common/SPELLS/BFA/AzeriteTraits/General.js
@@ -1,17 +1,27 @@
 export default {
-  BLIGHTBORNE_INFUSION: {
+  BLIGHTBORNE_INFUSION: { // buff
     id: 273826,
     name: 'Blightborne Infusion',
     icon: 'ability_creature_disease_05',
   },
-  SECRETS_OF_THE_DEEP_SURGING_DROPLET: {
+  SECRETS_OF_THE_DEEP_SURGING_DROPLET: { // buff
     id: 273842,
     name: 'Secrets of the Deep',
     icon: 'inv_misc_enchantedpearlf',
   },
-  SECRETS_OF_THE_DEEP_VOID_DROPLET: {
+  SECRETS_OF_THE_DEEP_VOID_DROPLET: { // buff
     id: 273843,
     name: 'Secrets of the Deep',
     icon: 'inv_misc_enchantedpearlf',
+  },
+  CHAMPION_OF_AZEROTH: { // buff
+    id: 270586,
+    name: 'Champion of Azeroth',
+    icon: 'spell_holy_championsgrace',
+  },
+  VAMPIRIC_SPEED: { // buff (speed)
+    id: 269239,
+    name: 'Vampiric Speed',
+    icon: 'inv_misc_monsterfang_02',
   },
 };
