306c9d34aaf01930becb11aa2f7546af46f31b14
joshinat0r <joshuahahn1996@googlemail.com>
2018-05-17 15:16:46 +0200

    percentage

--- src/Parser/DeathKnight/Blood/Modules/Core/DeathsCaress.js
+++ src/Parser/DeathKnight/Blood/Modules/Core/DeathsCaress.js
@@ -22,13 +22,13 @@ class DeathsCaress extends Analyzer {
 
   get averageCastSuggestionThresholds() {
     return {
-      actual: this.dcCasts,
-      isGreaterThan: {
+      actual: 1 - (this.dcCasts / (this.tracker.runesMaxCasts - this.tracker.runesWasted)),
+      isLessThan: {
         minor: 1,
-        average: 3,
-        major: 5,
+        average: .95,
+        major: .9,
       },
-      style: 'number',
+      style: 'percentage',
     };
   }
 
@@ -37,7 +37,7 @@ class DeathsCaress extends Analyzer {
         .addSuggestion((suggest, actual, recommended) => {
           return suggest(<React.Fragment>Avoid casting <SpellLink id={SPELLS.DEATHS_CARESS.id} /> unless you're out of melee range and about to cap your runes while <SpellLink id={SPELLS.DEATH_AND_DECAY.id} /> and <SpellLink id={SPELLS.BLOODDRINKER_TALENT.id} /> are on cooldown. Dump runes primarily with <SpellLink id={SPELLS.HEART_STRIKE.id} />.</React.Fragment>)
             .icon(SPELLS.DEATHS_CARESS.icon)
-            .actual(`${formatPercentage(actual / (this.tracker.runesMaxCasts - this.tracker.runesWasted))}% runes spend with Death's Caress`)
+            .actual(`${formatPercentage(this.dcCasts / (this.tracker.runesMaxCasts - this.tracker.runesWasted))}% runes spend with Death's Caress`)
             .recommended(`0% are recommended`);
         });
   }
