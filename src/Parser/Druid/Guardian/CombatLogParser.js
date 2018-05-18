68952cb0d102c31bacd99ae1fd4416fa597612c4
yajinni <yajinni@gmail.com>
2018-05-18 18:59:09 -0400

    Fix travis error

--- src/Parser/Druid/Guardian/CombatLogParser.js
+++ src/Parser/Druid/Guardian/CombatLogParser.js
@@ -3,6 +3,7 @@ import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
 import HealingDone from 'Parser/Core/Modules/HealingDone';
 import DamageDone from 'Parser/Core/Modules/DamageDone';
 import DamageTaken from 'Parser/Core/Modules/DamageTaken';
+import DeathRecapTracker from 'Main/DeathRecapTracker';
 
 import Abilities from './Modules/Abilities';
 import ActiveTargets from './Modules/Features/ActiveTargets';
@@ -13,7 +14,6 @@ import IronFurGoEProcs from './Modules/Features/IronFurGoEProcs';
 import FrenziedRegenGoEProcs from './Modules/Features/FrenziedRegenGoEProcs';
 import RageWasted from './Modules/Features/RageWasted';
 import AntiFillerSpam from './Modules/Features/AntiFillerSpam';
-import DeathRecapTracker from 'Main/DeathRecapTracker';
 
 import IronFur from './Modules/Spells/IronFur';
 import Thrash from './Modules/Spells/Thrash';
