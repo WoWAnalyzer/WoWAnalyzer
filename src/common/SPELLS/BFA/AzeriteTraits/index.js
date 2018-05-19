21a4a01b9c32b5a7866ce1b291b01906a81152d9
Martijn Hols <martijnhols@hotmail.com>
2018-05-19 23:20:39 +0200

    Include Hunter traits in spells

--- src/common/SPELLS/BFA/AzeriteTraits/index.js
+++ src/common/SPELLS/BFA/AzeriteTraits/index.js
@@ -1,5 +1,6 @@
 import safeMerge from 'common/safeMerge';
 import General from './General';
 import Paladin from './Paladin';
+import Hunter from './Hunter';
 
-export default safeMerge(General, Paladin);
+export default safeMerge(General, Paladin, Hunter);
