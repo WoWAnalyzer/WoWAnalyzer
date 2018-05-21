6d81c88ea464d338fdcfcbc6918053ccb5235402
Aveline Estié <hello@aveline.be>
2018-05-21 14:42:30 +0200

    Add new file for Darkmoon trinkets

---
+++ src/common/ITEMS/BFA/Others.js
@@ -0,0 +1,22 @@
+export default {
+  DARKMOON_DECK_BLOCKADES: {
+    id: 159128,
+    name: 'Darkmoon Deck: Blockades',
+    icon: '70_inscription_deck_immortality',
+  },
+  DARKMOON_DECK_TIDES: {
+    id: 159127,
+    name: 'Darkmoon Deck: Tides',
+    icon: '70_inscription_deck_promises',
+  },
+  DARKMOON_DECK_SQUALLS: {
+    id: 159126,
+    name: 'Darkmoon Deck: Squalls',
+    icon: '70_inscription_deck_hellfire',
+  },
+  DARKMOON_DECK_FATHOMS: {
+    id: 159125,
+    name: 'Darkmoon Deck: Fathoms',
+    icon: '70_inscription_deck_dominion',
+  },
+};
--- src/common/ITEMS/BFA/index.js
+++ src/common/ITEMS/BFA/index.js
@@ -2,5 +2,6 @@ import safeMerge from 'common/safeMerge';
 import Dungeons from './Dungeons';
 import Raids from './Raids';
 import Potions from './Potions';
+import Others from './Others';
 
-export default safeMerge(Dungeons, Raids, Potions);
+export default safeMerge(Dungeons, Raids, Potions, Others);
