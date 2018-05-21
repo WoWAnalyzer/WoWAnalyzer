4a83c5bfc125a3ed29ab32245aa1310d38d00f1d
Aveline Estié <hello@aveline.be>
2018-05-21 14:51:32 +0200

    Add Underrot / Tol Dagor trinkets

--- src/common/ITEMS/BFA/Dungeons/index.js
+++ src/common/ITEMS/BFA/Dungeons/index.js
@@ -4,6 +4,33 @@ import ITEM_QUALITIES from 'common/ITEM_QUALITIES';
 
 export default {
   // Group by instance (with comments)
+  // The Underrot
+  ROTCRUSTED_VOODOO_DOLL: {
+    id: 159624,
+    name: 'Rotcrusted Voodoo Doll',
+    icon: 'inv_misc_primitive_toy05',
+    quality: ITEM_QUALITIES.BLUE,
+  },
+  VIAL_OF_ANIMATED_BLOOD: {
+    id: 159625,
+    name: 'Vial of Animated Blood',
+    icon: 'inv_misc_food_legion_leyblood',
+    quality: ITEM_QUALITIES.BLUE,
+  },
+
+  // Tol Dagor
+  IGNITION_MAGES_FUSE: {
+    id: 159615,
+    name: 'Ignition Mage\'s Fuse',
+    icon: 'inv_misc_rope_01',
+    quality: ITEM_QUALITIES.BLUE,
+  },
+  JES_HOWLER: {
+    id: 159627,
+    name: 'Jes\' Howler',
+    icon: 'ability_shaman_freedomwolf',
+    quality: ITEM_QUALITIES.BLUE,
+  },
 
   // Quests.............. upscaled in beta
   ENGRANGED_DIEMETRADON_FIN: {
