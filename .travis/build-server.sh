f38f3da8670ea3efd98ad24ce1ae5d640eb844b2
Martijn Hols <martijnhols@hotmail.com>
2018-05-23 20:53:49 +0200

    Fix server building

--- .travis/build-server.sh
+++
@@ -1,5 +0,0 @@
-cd server;
-npm run build;
-echo "The server has already been built so we no longer need the devDependencies (reduces the final bundle size)";
-npm prune --production;
-cd ..;
--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,16 @@
-./build-server.sh
+echo "# Build server";
+cd server;
+npm run build;
+echo "The server has already been built so we no longer need the devDependencies (reduces the final bundle size)";
+npm prune --production;
+cd ..;
+
+echo "# Build app";
 npm run build
+
+echo "# Build Docker image";
 export REPO=martijnhols/wowanalyzer;
+export TAG="build-$TRAVIS_BUILD_NUMBER"
 #export BRANCH=$(
 #	if [ "$TRAVIS_BRANCH" == "master" ]; then
 #		echo "latest";
@@ -8,9 +18,7 @@ export REPO=martijnhols/wowanalyzer;
 #		echo $TRAVIS_BRANCH;
 #	fi | sed -r 's/\//-/g'
 #);
-echo "#docker build";
-docker build --tag $REPO:$TRAVIS_BUILD_NUMBER --file Dockerfile.package .;
-echo "#docker login";
+docker build --tag $REPO:$TAG --file Dockerfile.package .;
+echo "# Push Docker image";
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
-echo "#docker push";
-docker push $REPO:$TRAVIS_BUILD_NUMBER;
+docker push $REPO:$TAG;
