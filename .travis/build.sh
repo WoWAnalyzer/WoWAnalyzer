5ed3bcb2031e189177a52d7f2b4f9e55f2344932
Martijn Hols <martijnhols@hotmail.com>
2018-05-23 22:15:54 +0200

    Update .travis/build.sh: don't generate a sourcemap for pull requests for speed

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,20 +5,22 @@ echo "The server has already been built so we no longer need the devDependencies
 npm prune --production;
 cd ..;
 
+if [ "$TRAVIS_PULL_REQUEST" ~= "false" ]; then
+	# Save some time
+	export GENERATE_SOURCEMAP=false;
+fi
+
 echo "# Build app";
 npm run build
 
-echo "# Build Docker image";
-export REPO=martijnhols/wowanalyzer;
-export TAG="build-$TRAVIS_BUILD_NUMBER";
-#export BRANCH=$(
-#	if [ "$TRAVIS_BRANCH" == "master" ]; then
-#		echo "latest";
-#	else
-#		echo $TRAVIS_BRANCH;
-#	fi | sed -r 's/\//-/g'
-#);
-docker build --tag $REPO:$TAG --file Dockerfile.package .;
-echo "# Push Docker image";
-docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
-docker push $REPO:$TAG;
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	# Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.
+
+	echo "# Build Docker image";
+	export REPO=martijnhols/wowanalyzer;
+	export TAG="build-$TRAVIS_BUILD_NUMBER";
+	docker build --tag $REPO:$TAG --file Dockerfile.package .;
+	echo "# Push Docker image";
+	docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
+	docker push $REPO:$TAG;
+fi
