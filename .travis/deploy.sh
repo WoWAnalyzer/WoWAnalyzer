dc01be7a06cf66a978b64502487eb22a32456dfd
Martijn Hols <martijnhols@hotmail.com>
2018-05-25 00:12:52 +0200

    Update .travis.yml

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,7 +10,6 @@ export DEPLOY_TAG=$(
 
 echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
 
-export REPO=martijnhols/wowanalyzer;
 docker build --tag $REPO:$BUILD_TAG --file Dockerfile.package .;
 echo "> Push Docker image";
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
