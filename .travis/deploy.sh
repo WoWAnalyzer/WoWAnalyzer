e01dcca0cd2cc4b12dfcea74847d0ea2601d2c10
Martijn Hols <martijnhols@hotmail.com>
2018-05-24 23:15:42 +0200

    Update .travis: move build commands back for improved logging

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 export REPO=martijnhols/wowanalyzer;
-export OLD_TAG="build-$TRAVIS_BUILD_NUMBER";
-export NEW_TAG=$(
+export BUILD_TAG="build-$TRAVIS_BUILD_NUMBER";
+export DEPLOY_TAG=$(
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "latest";
   else
@@ -8,7 +8,12 @@ export NEW_TAG=$(
   fi | sed -r 's/\//-/g'
 );
 
+echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
+
+export REPO=martijnhols/wowanalyzer;
+docker build --tag $REPO:$BUILD_TAG --file Dockerfile.package .;
+echo "> Push Docker image";
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
-docker pull $REPO:$OLD_TAG;
-docker tag $REPO:$OLD_TAG $REPO:$NEW_TAG;
-docker push $REPO:$NEW_TAG;
+docker push $REPO:$BUILD_TAG;
+docker tag $REPO:$BUILD_TAG $REPO:$DEPLOY_TAG;
+docker push $REPO:$DEPLOY_TAG;
