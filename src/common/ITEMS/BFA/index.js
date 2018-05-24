e6a3f2ad18b2db21e7260a8414b197b9fc8bcb7c
Aveline Estié <hello@aveline.be>
2018-05-24 15:58:13 +0200

    Change file name from Others to Crafted

--- src/common/ITEMS/BFA/index.js
+++ src/common/ITEMS/BFA/index.js
@@ -2,6 +2,6 @@ import safeMerge from 'common/safeMerge';
 import Dungeons from './Dungeons';
 import Raids from './Raids';
 import Potions from './Potions';
-import Others from './Others';
+import Crafted from './Crafted';
 
-export default safeMerge(Dungeons, Raids, Potions, Others);
+export default safeMerge(Dungeons, Raids, Potions, Crafted);
