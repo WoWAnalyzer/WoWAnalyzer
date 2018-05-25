32e73568b968ba76a53371b192b252336cd86167
Martijn Hols <martijnhols@hotmail.com>
2018-05-25 23:49:25 +0200

    Load news async to reduce initial bundle size

--- src/Main/News/View.js
+++ src/Main/News/View.js
@@ -3,25 +3,40 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import articles from './Articles';
+import ArticleLoader from './ArticleLoader';
 
-const View = ({ articleId }) => {
-  const article = articles.find(article => article.props.title === articleId);
-  return (
-    <div className="container">
-      <Link to="/">
-        Home
-      </Link> &gt;{' '}
-      <Link to="/#Announcements">
-        Announcements
-      </Link> &gt;{' '}
-      {article && article.props.title}<br /><br />
+class View extends React.PureComponent {
+  static propTypes = {
+    articleId: PropTypes.string.isRequired,
+  };
 
-      {article}
-    </div>
-  );
-};
-View.propTypes = {
-  articleId: PropTypes.string.isRequired,
-};
+  render() {
+    const fileName = articles[this.props.articleId];
+
+    return (
+      <div className="container">
+        <Link to="/">
+          Home
+        </Link> &gt;{' '}
+        <Link to="/#Announcements">
+          Announcements
+        </Link>
+        <ArticleLoader
+          key={fileName}
+          fileName={fileName}
+        >
+          {({ children, showLoader }) => showLoader ? <div className="spinner" style={{ fontSize: 5 }} /> : (
+            <React.Fragment>
+              {' '}&gt;{' '}
+              {children && children.props.title}<br /><br />
+
+              {children}
+            </React.Fragment>
+          )}
+        </ArticleLoader>
+      </div>
+    );
+  }
+}
 
 export default View;
