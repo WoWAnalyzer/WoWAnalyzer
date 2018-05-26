d32de17e02c6d76be976d739091b46e9f61f4f22
Martijn Hols <martijnhols@hotmail.com>
2018-05-26 22:53:44 +0200

    Refactor Results component: reduce size

--- src/Main/Report/Results/index.js
+++ src/Main/Report/Results/index.js
@@ -10,9 +10,6 @@ import SuggestionIcon from 'Icons/Suggestion';
 import ArmorIcon from 'Icons/Armor';
 import StatisticsIcon from 'Icons/Statistics';
 
-import ReadableList from 'common/ReadableList';
-import parseVersionString from 'common/parseVersionString';
-import Warning from 'common/Alert/Warning';
 import ItemLink from 'common/ItemLink';
 import ItemIcon from 'common/ItemIcon';
 import lazyLoadComponent from 'common/lazyLoadComponent';
@@ -21,12 +18,13 @@ import SuggestionsTab from 'Main/SuggestionsTab';
 import ActivityIndicator from 'Main/ActivityIndicator';
 import WarcraftLogsLogo from 'Main/Images/WarcraftLogs-logo.png';
 import WipefestLogo from 'Main/Images/Wipefest-logo.png';
-import Contributor from 'Main/Contributor';
 import ItemStatisticBox from 'Main/ItemStatisticBox';
 
 import ResultsWarning from './ResultsWarning';
 import Header from './Header';
 import DetailsTab from './DetailsTab';
+import About from './About';
+import Divider from './Divider';
 import Odyn from './Images/odyn.jpg';
 
 import './Results.css';
@@ -34,7 +32,6 @@ import './Results.css';
 const DevelopmentTab = lazyLoadComponent(() => import(/* webpackChunkName: 'DevelopmentTab' */ 'Main/DevelopmentTab').then(exports => exports.default));
 const EventsTab = lazyLoadComponent(() => import(/* webpackChunkName: 'EventsTab' */ 'Main/EventsTab').then(exports => exports.default));
 
-const CURRENT_GAME_PATCH = '7.3.5';
 
 const MAIN_TAB = {
   CHECKLIST: 'Checklist',
@@ -170,53 +167,13 @@ class Results extends React.PureComponent {
     return null;
   }
 
-  renderAbout() {
-    const { spec, description, contributors, patchCompatibility } = this.context.config;
-    const specPatchCompatibility = parseVersionString(patchCompatibility);
-    const latestPatch = parseVersionString(CURRENT_GAME_PATCH);
-    const isOutdated = specPatchCompatibility.major < latestPatch.major || specPatchCompatibility.minor < latestPatch.minor || specPatchCompatibility.patch < latestPatch.patch;
-
-    return (
-      <div className="panel">
-        <div className="panel-heading">
-          <h2>About {spec.specName} {spec.className}</h2>
-        </div>
-        <div className="panel-body">
-          {description}
-
-          <div className="row" style={{ marginTop: '1em' }}>
-            <div className="col-lg-4" style={{ fontWeight: 'bold', paddingRight: 0 }}>
-              Contributor{contributors.length > 1 && 's'}
-            </div>
-            <div className="col-lg-8">
-              <ReadableList>
-                {contributors.map(contributor => <Contributor key={contributor.nickname} {...contributor} />)}
-              </ReadableList>
-            </div>
-          </div>
-          <div className="row" style={{ marginTop: '0.5em' }}>
-            <div className="col-lg-4" style={{ fontWeight: 'bold', paddingRight: 0 }}>
-              Updated for patch
-            </div>
-            <div className="col-lg-8">
-              {patchCompatibility}
-            </div>
-          </div>
-          {isOutdated && (
-            <Warning style={{ marginTop: '1em' }}>
-              The analysis for this spec is outdated. It may be inaccurate for spells that were changed since patch {patchCompatibility}.
-            </Warning>
-          )}
-        </div>
-      </div>
-    );
-  }
   renderContent() {
     const { parser, selectedDetailsTab, makeTabUrl } = this.props;
     const report = parser.report;
     const fight = parser.fight;
     const modules = parser._modules;
     const selectedCombatant = modules.combatants.selected;
+    const config = this.context.config;
 
     const results = parser.generateResults();
 
@@ -248,7 +205,7 @@ class Results extends React.PureComponent {
       <React.Fragment>
         <div className="row">
           <div className="col-md-4">
-            {this.renderAbout()}
+            <About config={config} />
 
             <div>
               <a
@@ -314,15 +271,11 @@ class Results extends React.PureComponent {
           </div>
         </div>
 
-        <div className="divider" />
+        <Divider />
 
-        <div className="row">
-          <div className="col-md-12">
-            {this.renderStatistics(results.statistics, results.items, selectedCombatant)}
-          </div>
-        </div>
+        {this.renderStatistics(results.statistics, results.items, selectedCombatant)}
 
-        <div className="divider" />
+        <Divider />
 
         <DetailsTab tabs={results.tabs} selected={selectedDetailsTab} makeTabUrl={makeTabUrl} />
       </React.Fragment>
