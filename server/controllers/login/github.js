2eb7b002bda200df5a1690e538e8637efa921df1
Martijn Hols <martijnhols@hotmail.com>
2018-05-27 14:09:38 +0200

    server: Premium lasts for 1 month after last commit (to master)

--- server/controllers/login/github.js
+++ server/controllers/login/github.js
@@ -1,5 +1,6 @@
 import Express from 'express';
 import Passport from 'passport';
+import request from 'request-promise-native';
 import { Strategy as GitHubStrategy } from 'passport-github';
 
 const router = Express.Router();
@@ -8,19 +9,36 @@ Passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.GITHUB_CALLBACK_URL,
-    scope: 'public_repo',
   },
-  function(accessToken, refreshToken, profile, done) {
+  async function(accessToken, refreshToken, profile, done) {
     // passport-patreon removes data we need from `profile`, so re-extract the raw data received
     const fullProfile = profile._json;
 
-    console.log(profile);
+    // This will only get commits to master, but this should generally be sufficient.
+    const url = `https://api.github.com/repos/WoWAnalyzer/WoWAnalyzer/commits?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&author=${fullProfile.login}`;
+    const lastCommitDate = await request.get({
+      url: url,
+      headers: {
+        'User-Agent': 'WoWAnalyzer.com API',
+      },
+      gzip: true, // using gzip is 80% quicker
+    })
+      .then(jsonString => JSON.parse(jsonString))
+      .then(json => {
+        if (!json || json.length === 0) {
+          return null;
+        }
+        // We only need the most recent commit
+        return json[0];
+      })
+      .then(commit => commit ? commit.commit.committer.date : null);
 
     done(null, {
       name: fullProfile.name,
       avatar: fullProfile.avatar_url,
       github: {
         id: fullProfile.id,
+        lastCommitDate,
         accessToken,
         refreshToken,
       },
--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,10 +7,22 @@ const router = Express.Router();
 router.get('/', requireAuthenticated, function(req, res) {
   const user = req.user;
 
+  let premium = false;
+  if (user.patreon && user.patreon.pledgeAmount >= 100) {
+    premium = true;
+  }
+  if (user.github && user.github.lastCommitDate) {
+    const lastCommitDate = Date.parse(user.github.lastCommitDate);
+    const now = Date.now();
+    const secondsSinceLastCommit = (now - lastCommitDate) / 1000;
+    const secondsPerMonth = 3600 * 24 * 30;
+    premium = secondsSinceLastCommit < secondsPerMonth;
+  }
+
   res.json({
     name: user.name,
     avatar: user.avatar,
-    premium: (user.patreon && user.patreon.pledgeAmount >= 100/*TODO: || user.github*/) || false,
+    premium: premium,
   });
 });
 
