e6d0cd18e240124f416733b1796242e3816c2b49
Martijn Hols <martijnhols@hotmail.com>
2018-05-27 13:08:32 +0200

    server: Add GitHub login (no contribution checking yet)

---
+++ server/controllers/login/github.js
@@ -0,0 +1,44 @@
+import Express from 'express';
+import Passport from 'passport';
+import { Strategy as GitHubStrategy } from 'passport-github';
+
+const router = Express.Router();
+
+Passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
+    scope: 'public_repo',
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // passport-patreon removes data we need from `profile`, so re-extract the raw data received
+    const fullProfile = profile._json;
+
+    console.log(profile);
+
+    done(null, {
+      name: fullProfile.name,
+      avatar: fullProfile.avatar_url,
+      github: {
+        id: fullProfile.id,
+        accessToken,
+        refreshToken,
+      },
+    });
+  }
+));
+
+router.get('/', Passport.authenticate('github'));
+router.get(
+  '/callback',
+  Passport.authenticate('github', {
+    successRedirect: '/premium',
+    failureRedirect: '/premium',
+  }),
+  function (req, res) {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.send(JSON.stringify(req.user));
+  }
+);
+
+export default router;
