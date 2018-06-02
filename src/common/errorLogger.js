67b083646b5adc21faf3ae1813576583b7e4f310
Martijn Hols <martijnhols@hotmail.com>
2018-06-02 13:24:39 +0200

    Warn when Raven DSN is missing

--- src/common/errorLogger.js
+++ src/common/errorLogger.js
@@ -2,7 +2,11 @@ import Raven from 'raven-js';
 
 export function install() {
   if (process.env.NODE_ENV === 'production') {
-    Raven.config(process.env.REACT_APP_RAVEN_DSN).install();
+    if (process.env.REACT_APP_RAVEN_DSN) {
+      Raven.config(process.env.REACT_APP_RAVEN_DSN).install();
+    } else {
+      console.warn('Unable to install Raven, missing DSN.');
+    }
   }
 }
 
