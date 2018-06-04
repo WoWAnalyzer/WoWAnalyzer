5daaffee67324d4ba46a40d6165742573c29249c
niseko <rentz@hotmail.de>
2018-06-04 18:30:37 +0200

    add healing potion

--- src/Parser/Core/Modules/Features/SpellInfo.js
+++ src/Parser/Core/Modules/Features/SpellInfo.js
@@ -86,6 +86,13 @@ export default {
     mastery: false,
     vers: true, // confirmed
   },
+  [SPELLS.COASTAL_HEALING_POTION.id]: {
+    int: false,
+    crit: false,
+    hasteHpct: false,
+    mastery: false,
+    vers: true,
+  },
   [SPELLS.MARK_OF_THE_ANCIENT_PRIESTESS.id]: {
     int: false,
     crit: true,
--- src/common/ITEMS/BFA/Potions.js
+++ src/common/ITEMS/BFA/Potions.js
@@ -1,4 +1,9 @@
 export default {
+  COASTAL_HEALING_POTION: {
+    id: 152494,
+    name: 'Coastal Healing Potion',
+    icon: 'inv_alchemy_80_potion01red',
+  },
   COASTAL_MANA_POTION: {
     id: 152495,
     name: 'Coastal Mana Potion',
