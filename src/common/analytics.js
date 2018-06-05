94b8094e40ded46f742e00292ddc6cf3d353e110
Martijn Hols <martijnhols@hotmail.com>
2018-06-05 23:30:32 +0200

    Track in-app navigation

--- src/common/analytics.js
+++ src/common/analytics.js
@@ -1,21 +1,42 @@
+function init() {
+  window._paq = window._paq || [];
+}
+function paq() {
+  return window._paq;
+}
+function installed() {
+  return !!window._paq;
+}
+function push(...args) {
+  return paq().push(...args);
+}
+
 export function install() {
   if (process.env.NODE_ENV === 'production') {
-    window._paq = [];
+    init();
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    window._paq.push(['trackPageView']);
-    window._paq.push(['enableLinkTracking']);
-    (function () {
-      const u = 'https://matomo.wowanalyzer.com/';
-      window._paq.push(['setTrackerUrl', u + 'js/']);
-      window._paq.push(['setSiteId', '1']);
-      const d = document;
-      const g = d.createElement('script');
-      const s = d.getElementsByTagName('script')[0];
-      g.type = 'text/javascript';
-      g.async = true;
-      g.defer = true;
-      g.src = u + 'js/';
-      s.parentNode.insertBefore(g, s);
-    })();
+    push(['trackPageView']);
+    push(['enableLinkTracking']);
+
+    const u = 'https://matomo.wowanalyzer.com/';
+    push(['setTrackerUrl', u + 'js/']);
+    push(['setSiteId', '1']);
+    const d = document;
+    const g = d.createElement('script');
+    const s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'js/';
+    s.parentNode.insertBefore(g, s);
+  }
+}
+
+export function track(oldLocation, newLocation) {
+  if (installed()) {
+    push(['setReferrerUrl', oldLocation]);
+    push(['setDocumentTitle', document.title]);
+    push(['setCustomUrl', newLocation]);
+    push(['trackPageView']);
   }
 }
