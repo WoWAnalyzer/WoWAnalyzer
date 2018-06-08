3d9fb83cd69ca90027adcb1e99751faa7a7c92fe
HighTech_LowLife <hlowlife1@gmail.com>
2018-06-08 15:04:49 -0400

    First Mate's Spyglass Trinket Changes

--- src/Parser/Core/Modules/Items/BFA/FirstMatesSpyglass.js
+++ src/Parser/Core/Modules/Items/BFA/FirstMatesSpyglass.js
@@ -5,6 +5,7 @@ import ITEMS from 'common/ITEMS';
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import HIT_TYPES from 'Parser/Core/HIT_TYPES';
+import Abilities from 'Parser/Core/Modules/Abilities';
 import { formatPercentage } from 'common/format';
 
 /**
@@ -14,16 +15,29 @@ import { formatPercentage } from 'common/format';
 class FirstMatesSpyglass extends Analyzer {
   static dependencies = {
     combatants: Combatants,
+    abilities: Abilities,
   };
   
   casts = 0;
-  timesDamageCrit = 0;
-  timesHealCrit = 0;
+  timesHit = 0;
+  timesCrit = 0;
     
   on_initialized() {
     this.active = this.combatants.selected.hasTrinket(
       ITEMS.FIRST_MATES_SPYGLASS.id
     );
+    if (this.active) {
+      this.abilities.add({
+        spell: SPELLS.SPYGLASS_SIGHT,
+        name: ITEMS.FIRST_MATES_SPYGLASS.name,
+        category: Abilities.SPELL_CATEGORIES.ITEMS,
+        charges: 1,
+        cooldown: 120,
+        castEfficiency: {
+          suggestion: true,
+        },
+      });
+    }
   }
   
   on_byPlayer_cast(event) {
@@ -38,9 +52,10 @@ class FirstMatesSpyglass extends Analyzer {
       return;
     }
     if (event.hitType !== HIT_TYPES.CRIT) {
-      return;
+      this.timesHit += 1;
     }
-    this.timesDamageCrit += 1;
+    this.timesHit += 1;
+    this.timesCrit += 1;
   }
   
   on_byPlayer_heal(event) {
@@ -48,9 +63,10 @@ class FirstMatesSpyglass extends Analyzer {
       return;
     }      
     if (event.hitType !== HIT_TYPES.CRIT) {
-      return;
+      this.timesHit += 1;
       }
-      this.timesHealCrit += 1;
+      this.timesHit += 1;
+      this.timesCrit += 1;
   }
   
   get totalBuffUptime() {
@@ -61,8 +77,7 @@ class FirstMatesSpyglass extends Analyzer {
     return {
       item: ITEMS.FIRST_MATES_SPYGLASS,
       result: (
-        <dfn data-tip={`You critically damaged ${this.timesDamageCrit} times with this buff up<br/>
-                        You critically healed ${this.timesHealCrit} times with this buff up`}>
+        <dfn data-tip={`You critically hit ${formatPercentage(this.timesCrit / this.timesHit)}% of the time with this buff up`}>
           Used {this.casts} times / {formatPercentage(this.totalBuffUptime)}% uptime
         </dfn>
       ),
