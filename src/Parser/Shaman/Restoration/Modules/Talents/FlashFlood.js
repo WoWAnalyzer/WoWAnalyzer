04ae7ad8378ea6fbf09e675cd778acb15a333f9d
niseko <rentz@hotmail.de>
2018-06-09 05:07:55 +0200

    add soul and healing rain

--- src/Parser/Shaman/Restoration/Modules/Talents/FlashFlood.js
+++ src/Parser/Shaman/Restoration/Modules/Talents/FlashFlood.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import SpellLink from 'common/SpellLink';
 import SPELLS from 'common/SPELLS';
+import ITEMS from 'common/ITEMS';
 import { formatPercentage } from 'common/format';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
 import StatTracker from 'Parser/Core/Modules/StatTracker';
 import calculateEffectiveHealing from 'Parser/Core/calculateEffectiveHealing';
+
+import Abilities from '../Abilities';
 
 const BUFFER = 750;
 const flashFloodHaste = 0.2;
@@ -18,23 +21,33 @@ const spellsConsumingFlashFlood = [
   SPELLS.HEALING_WAVE.id,
   SPELLS.HEALING_SURGE_RESTORATION.id,
   SPELLS.CHAIN_HEAL.id,
-  SPELLS.HEALING_RAIN_HEAL.id, // should a HoT be here
-  // SPELLS.WELLSPRING_HEAL.id, -- bugged for now, to be fixed soon
+  // SPELLS.HEALING_RAIN_HEAL.id, -- handled differently since its a HoT
+  SPELLS.WELLSPRING_HEAL.id,
 ];
 
 class FlashFlood extends Analyzer {
   static dependencies = {
     combatants: Combatants,
     statTracker: StatTracker,
+    abilities: Abilities,
   };
   healing = 0;
+  healingRainTimestamp = 0;
+  healingRainHastePercent = 0;
 
   on_initialized() {
-    this.active = this.combatants.selected.hasTalent(SPELLS.FLASH_FLOOD_TALENT.id);
+    this.active = this.combatants.selected.hasTalent(SPELLS.FLASH_FLOOD_TALENT.id) || this.combatants.selected.hasFinger(ITEMS.SOUL_OF_THE_FARSEER.id);
   }
 
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
+
+    if(this.healingRainTimestamp && spellId === SPELLS.HEALING_RAIN_HEAL.id && 
+      this.healingRainTimestamp + this.abilities.getExpectedCooldownDuration(SPELLS.HEALING_RAIN_CAST.id) <= event.timestamp) {
+      const flashFloodContribution = flashFloodHaste / (flashFloodHaste + this.healingRainHastePercent);
+      this.healing += calculateEffectiveHealing(event, flashFloodContribution);
+      return;
+    }
 
     if (!spellsConsumingFlashFlood.includes(spellId)) {
       return;
@@ -54,8 +67,25 @@ class FlashFlood extends Analyzer {
       }
     }
 
-    const crashingWavesContribution = flashFloodHaste / (flashFloodHaste + currentHastePercent);
-    this.healing += calculateEffectiveHealing(event, crashingWavesContribution);
+    const flashFloodContribution = flashFloodHaste / (flashFloodHaste + currentHastePercent);
+    this.healing += calculateEffectiveHealing(event, flashFloodContribution);
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+
+    if (spellId !== SPELLS.HEALING_RAIN_CAST.id) {
+      return;
+    }
+
+    const hasFlashFlood = this.combatants.selected.hasBuff(SPELLS.FLASH_FLOOD_BUFF.id, event.timestamp, BUFFER, BUFFER);
+    if(!hasFlashFlood) {
+      this.healingRainTimestamp = 0;
+      return;
+    }
+
+    this.healingRainTimestamp = event.timestamp;
+    this.healingRainHastePercent = this.statTracker.currentHastePercentage;
   }
 
   subStatistic() {
