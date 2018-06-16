f06afe5ee33c44a19abb03edf36370c6920978e5
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-06-16 13:12:00 +0200

    Removed old talents, added new Affliction ones

--- src/common/SPELLS/TALENTS/WARLOCK.js
+++ src/common/SPELLS/TALENTS/WARLOCK.js
@@ -2,49 +2,37 @@
 
 export default {
   // Shared
-  EMPOWERED_LIFE_TAP_TALENT: { id: 235157, name: "Empowered Life Tap", icon: "spell_shadow_manafeed" },
-  DEMONIC_CIRCLE_TALENT: { id: 48018, name: "Demonic Circle", icon: "spell_shadow_demoniccirclesummon", manaCost: 55000 },
-  MORTAL_COIL_TALENT: { id: 6789, name: "Mortal Coil", icon: "ability_warlock_mortalcoil", manaCost: 66000 },
-  SHADOWFURY_TALENT: { id: 30283, name: "Shadowfury", icon: "ability_warlock_shadowfurytga" },
-  PHANTOM_SINGULARITY_TALENT: { id: 205179, name: "Phantom Singularity", icon: "inv_enchant_voidsphere" },
-  SOUL_HARVEST_TALENT: { id: 196098, name: "Soul Harvest", icon: "spell_warlock_demonsoul" },
   DEMON_SKIN_TALENT: { id: 219272, name: "Demon Skin", icon: "spell_shadow_felarmour" },
   BURNING_RUSH_TALENT: { id: 111400, name: "Burning Rush", icon: "ability_deathwing_sealarmorbreachtga" },
   DARK_PACT_TALENT: { id: 108416, name: "Dark Pact", icon: "warlock_sacrificial_pact" },
-  GRIMOIRE_OF_SUPREMACY_TALENT: { id: 152107, name: "Grimoire of Supremacy", icon: "warlock_grimoireofcommand" },
-  GRIMOIRE_OF_SERVICE_TALENT: { id: 108501, name: "Grimoire of Service", icon: "warlock_grimoireofservice" },
+  DARKFURY_TALENT: { id: 264874, name: "Darkfury", icon: "ability_warlock_shadowfurytga" },
+  MORTAL_COIL_TALENT: { id: 6789, name: "Mortal Coil", icon: "ability_warlock_mortalcoil", manaCost: 400 },
+  DEMONIC_CIRCLE_TALENT: { id: 268358, name: "Demonic Circle", icon: "spell_shadow_demoniccirclesummon", manaCost: 400 },
   GRIMOIRE_OF_SACRIFICE_TALENT: { id: 108503, name: "Grimoire of Sacrifice", icon: "warlock_grimoireofsacrifice" },
-  SIPHON_LIFE_TALENT: { id: 63106, name: "Siphon Life", icon: "spell_shadow_requiem" },
   SOUL_CONDUIT_TALENT: { id: 215941, name: "Soul Conduit", icon: "spell_shadow_soulleech_2" },
   // Destruction
-  BACKDRAFT_TALENT: { id: 196406, name: "Backdraft", icon: "ability_warlock_backdraft" },
-  ROARING_BLAZE_TALENT: { id: 205184, name: "Roaring Blaze", icon: "ability_warlock_inferno" },
-  SHADOWBURN_TALENT: { id: 17877, name: "Shadowburn", icon: "spell_shadow_scourgebuild", manaCost: 22000 },
-  REVERSE_ENTROPY_TALENT: { id: 205148, name: "Reverse Entropy", icon: "ability_warlock_backdraftgreen" },
-  ERADICATION_TALENT: { id: 196412, name: "Eradication", icon: "ability_warlock_eradication" },
-  CATACLYSM_TALENT: { id: 152108, name: "Cataclysm", icon: "achievement_zone_cataclysm" },
-  FIRE_AND_BRIMSTONE_TALENT: { id: 196408, name: "Fire and Brimstone", icon: "ability_warlock_fireandbrimstone" },
-  WREAK_HAVOC_TALENT: { id: 196410, name: "Wreak Havoc", icon: "ability_warlock_baneofhavoc" },
-  CHANNEL_DEMONFIRE_TALENT: { id: 196447, name: "Channel Demonfire", icon: "spell_fire_ragnaros_lavaboltgreen", manaCost: 52800 },
+  GRIMOIRE_OF_SUPREMACY_TALENT: { id: 152107, name: "Grimoire of Supremacy", icon: "warlock_grimoireofcommand" }, // jine ID: 266086, jiny efekt, mozna jina ikona?
+  ROARING_BLAZE_TALENT: { id: 205184, name: "Roaring Blaze", icon: "ability_warlock_inferno" }, // sedi
+  SHADOWBURN_TALENT: { id: 17877, name: "Shadowburn", icon: "spell_shadow_scourgebuild", manaCost: 22000 }, // sedi, mozna manaCost jiny
+  REVERSE_ENTROPY_TALENT: { id: 205148, name: "Reverse Entropy", icon: "ability_warlock_backdraftgreen" }, // sedi
+  ERADICATION_TALENT: { id: 196412, name: "Eradication", icon: "ability_warlock_eradication" }, // sedi
+  CATACLYSM_TALENT: { id: 152108, name: "Cataclysm", icon: "achievement_zone_cataclysm" }, // sedi
+  FIRE_AND_BRIMSTONE_TALENT: { id: 196408, name: "Fire and Brimstone", icon: "ability_warlock_fireandbrimstone" }, // sedi
+  CHANNEL_DEMONFIRE_TALENT: { id: 196447, name: "Channel Demonfire", icon: "spell_fire_ragnaros_lavaboltgreen", manaCost: 52800 }, // sedi, mozna manaCost jiny
   // Demonology
-  SHADOWY_INSPIRATION_TALENT: { id: 196269, name: "Shadowy Inspiration", icon: "warlock_curse_shadow" },
-  SHADOWFLAME_TALENT: { id: 205181, name: "Shadowflame", icon: "ability_warlock_shadowflame" },
-  DEMONIC_CALLING_TALENT: { id: 205145, name: "Demonic Calling", icon: "ability_warlock_impoweredimp" },
-  IMPENDING_DOOM_TALENT: { id: 196270, name: "Impending Doom", icon: "spell_shadow_impphaseshift" },
-  IMPROVED_DREADSTALKERS_TALENT: { id: 196272, name: "Improved Dreadstalkers", icon: "ability_warlock_empoweredimp" },
-  IMPLOSION_TALENT: { id: 196277, name: "Implosion", icon: "spell_shadow_shadowandflame", manaCost: 66000 },
-  HAND_OF_DOOM_TALENT: { id: 196283, name: "Hand of Doom", icon: "ability_warlock_handofguldan" },
-  POWER_TRIP_TALENT: { id: 196605, name: "Power Trip", icon: "spell_shadow_demonictactics" },
-  GRIMOIRE_OF_SYNERGY_TALENT: { id: 171975, name: "Grimoire of Synergy", icon: "warlock_grimoireofsacrifice" },
-  SUMMON_DARKGLARE_TALENT: { id: 205180, name: "Summon Darkglare", icon: "achievement_boss_durumu" },
-  DEMONBOLT_TALENT: { id: 157695, name: "Demonbolt", icon: "spell_warlock_demonbolt", manaCost: 52800 },
+  DEMONIC_CALLING_TALENT: { id: 205145, name: "Demonic Calling", icon: "ability_warlock_impoweredimp" }, // sedi
   // Affliction
-  HAUNT_TALENT: { id: 48181, name: "Haunt", icon: "ability_warlock_haunt", manaCost: 55000 },
+  NIGHTFALL_TALENT: { id: 108558, name: "Nightfall", icon: "spell_shadow_twilight" },
+  DRAIN_SOUL_TALENT: { id: 198590, name: "Drain Soul", icon: "spell_shadow_haunting" },
+  DEATHBOLT_TALENT: { id: 264106, name: "Deathbolt", icon: "spell_fire_twilightflamebolt", manaCost: 400 },
   WRITHE_IN_AGONY_TALENT: { id: 196102, name: "Writhe in Agony", icon: "spell_shadow_curseofsargeras" },
-  MALEFIC_GRASP_TALENT: { id: 235155, name: "Malefic Grasp", icon: "ability_warlock_everlastingaffliction" },
-  CONTAGION_TALENT: { id: 196105, name: "Contagion", icon: "ability_creature_disease_03" },
   ABSOLUTE_CORRUPTION_TALENT: { id: 196103, name: "Absolute Corruption", icon: "ability_bossmannoroth_empoweredmannorothsgaze" },
-  HOWL_OF_TERROR_TALENT: { id: 5484, name: "Howl of Terror", icon: "ability_warlock_howlofterror" },
+  SIPHON_LIFE_TALENT: { id: 63106, name: "Siphon Life", icon: "spell_shadow_requiem" },
   SOW_THE_SEEDS_TALENT: { id: 196226, name: "Sow the Seeds", icon: "spell_shadow_seedofdestruction" },
-  DEATHS_EMBRACE_TALENT: { id: 234876, name: "Death's Embrace", icon: "spell_shadow_deathsembrace" },
+  PHANTOM_SINGULARITY_TALENT: { id: 205179, name: "Phantom Singularity", icon: "inv_enchant_voidsphere" },
+  VILE_TAINT_TALENT: { id: 278350, name: "Vile Taint", icon: "sha_spell_shadow_shadesofdarkness_nightborne" },
+  SHADOW_EMBRACE_TALENT: { id: 32388, name: "Shadow Embrace", icon: "spell_shadow_shadowembrace" },
+  HAUNT_TALENT: { id: 48181, name: "Haunt", icon: "ability_warlock_haunt", manaCost: 400 },
+  CREEPING_DEATH_TALENT: { id: 264000, name: "Creeping Death", icon: "ability_creature_cursed_03" },
+  DARK_SOUL_MISERY_TALENT: { id: 113860, name: "Dark Soul: Misery", icon: "spell_warlock_soulburn" },
 };
