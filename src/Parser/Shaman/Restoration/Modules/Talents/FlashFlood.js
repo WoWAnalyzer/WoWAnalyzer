3dd63027627daade2fc13efbfa8abf60379cdd4a
niseko <rentz@hotmail.de>
2018-06-17 08:55:28 +0200

    rework FlashFlood to return time saved

--- src/Parser/Shaman/Restoration/Modules/Talents/FlashFlood.js
+++ src/Parser/Shaman/Restoration/Modules/Talents/FlashFlood.js
@@ -3,89 +3,68 @@ import React from 'react';
 import SpellLink from 'common/SpellLink';
 import SPELLS from 'common/SPELLS';
 import ITEMS from 'common/ITEMS';
-import { formatPercentage } from 'common/format';
 
 import Analyzer from 'Parser/Core/Analyzer';
 import Combatants from 'Parser/Core/Modules/Combatants';
-import StatTracker from 'Parser/Core/Modules/StatTracker';
-import calculateEffectiveHealing from 'Parser/Core/calculateEffectiveHealing';
+import GlobalCooldown from 'Parser/Core/Modules/GlobalCooldown';
 
-import Abilities from '../Abilities';
-
-const BUFFER = 750;
-const flashFloodHaste = 0.2;
-const tidalWavesHaste = 0.3;
+const FLASH_FLOOD_HASTE = 0.2;
 
 // All heal spells with cast time...
-const spellsConsumingFlashFlood = [
+const SPELLS_CONSUMING_FLASH_FLOOD = [
   SPELLS.HEALING_WAVE.id,
-  SPELLS.HEALING_SURGE_RESTORATION.id,
   SPELLS.CHAIN_HEAL.id,
-  // SPELLS.HEALING_RAIN_HEAL.id, -- handled differently since its a HoT
   SPELLS.WELLSPRING_HEAL.id,
+  SPELLS.HEALING_SURGE_RESTORATION.id, //-- always below GCD
+  SPELLS.HEALING_RAIN_HEAL.id,
 ];
 
 class FlashFlood extends Analyzer {
   static dependencies = {
     combatants: Combatants,
-    statTracker: StatTracker,
-    abilities: Abilities,
+    globalCooldown: GlobalCooldown,
   };
-  healing = 0;
-  healingRainTimestamp = 0;
-  healingRainHastePercent = 0;
+
+  beginCastTimestamp = 0;
+  timeSaved = 0;
+  beginCastGlobalCooldown = 0;
 
   on_initialized() {
     this.active = this.combatants.selected.hasTalent(SPELLS.FLASH_FLOOD_TALENT.id) || this.combatants.selected.hasFinger(ITEMS.SOUL_OF_THE_FARSEER.id);
   }
 
-  on_byPlayer_heal(event) {
+  on_byPlayer_begincast(event) {
     const spellId = event.ability.guid;
-
-    if(this.healingRainTimestamp && spellId === SPELLS.HEALING_RAIN_HEAL.id && 
-      this.healingRainTimestamp + this.abilities.getExpectedCooldownDuration(SPELLS.HEALING_RAIN_CAST.id) <= event.timestamp) {
-      const flashFloodContribution = flashFloodHaste / (flashFloodHaste + this.healingRainHastePercent);
-      this.healing += calculateEffectiveHealing(event, flashFloodContribution);
+    if (!SPELLS_CONSUMING_FLASH_FLOOD.includes(spellId)) {
       return;
     }
 
-    if (!spellsConsumingFlashFlood.includes(spellId)) {
+    if(event.isCancelled) {
       return;
     }
 
-    const hasFlashFlood = this.combatants.selected.hasBuff(SPELLS.FLASH_FLOOD_BUFF.id, event.timestamp, BUFFER, BUFFER);
+    const hasFlashFlood = this.combatants.selected.hasBuff(SPELLS.FLASH_FLOOD_BUFF.id, event.timestamp);
     if(!hasFlashFlood) {
       return;
     }
 
-    let currentHastePercent = this.statTracker.currentHastePercentage;
-
-    if(spellId === SPELLS.HEALING_WAVE.id) {
-      const hasTidalWave = this.combatants.selected.hasBuff(SPELLS.TIDAL_WAVES_BUFF.id, event.timestamp, BUFFER, BUFFER);
-      if(hasTidalWave) {
-        currentHastePercent += tidalWavesHaste;
-      }
-    }
-
-    const flashFloodContribution = flashFloodHaste / (flashFloodHaste + currentHastePercent);
-    this.healing += calculateEffectiveHealing(event, flashFloodContribution);
+    this.beginCastTimestamp = event.timestamp;
+    this.beginCastGlobalCooldown = this.globalCooldown.getCurrentGlobalCooldown(spellId);
   }
 
   on_byPlayer_cast(event) {
-    const spellId = event.ability.guid;
-
-    if (spellId !== SPELLS.HEALING_RAIN_CAST.id) {
+    if(!this.beginCastTimestamp) {
+      return;
+    }
+    
+    const castTime = event.timestamp - this.beginCastTimestamp;
+    if (castTime <= this.beginCastGlobalCooldown) {
+      this.beginCastTimestamp = 0;
       return;
     }
 
-    const hasFlashFlood = this.combatants.selected.hasBuff(SPELLS.FLASH_FLOOD_BUFF.id, event.timestamp, BUFFER, BUFFER);
-    if(!hasFlashFlood) {
-      this.healingRainTimestamp = 0;
-      return;
-    }
-
-    this.healingRainTimestamp = event.timestamp;
-    this.healingRainHastePercent = this.statTracker.currentHastePercentage;
+    this.timeSaved += castTime / (1-FLASH_FLOOD_HASTE) * FLASH_FLOOD_HASTE;
+    this.beginCastTimestamp = 0;
   }
 
   subStatistic() {
@@ -95,7 +74,9 @@ class FlashFlood extends Analyzer {
           <SpellLink id={SPELLS.FLASH_FLOOD_TALENT.id} />
         </div>
         <div className="flex-sub text-right">
-          {formatPercentage(this.owner.getPercentageOfTotalHealingDone(this.healing))} %
+          <dfn data-tip={`Cast time saved by Flash Flood.`}>
+            {(this.timeSaved / 1000).toFixed(2)} seconds
+          </dfn>
         </div>
       </div>
     );
