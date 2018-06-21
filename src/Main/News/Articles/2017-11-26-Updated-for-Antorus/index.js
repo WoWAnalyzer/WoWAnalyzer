9d0ae994bfa42c5e0a13e144c40275d9d848925b
Martijn Hols <git@martijnhols.nl>
2018-06-21 22:09:13 +0200

    Premium announcement article

--- src/Main/News/Articles/2017-11-26-Updated-for-Antorus/index.js
+++ src/Main/News/Articles/2017-11-26-Updated-for-Antorus/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import { Zerotorescue } from 'CONTRIBUTORS';
 import AntorusImage from 'Raids/AntorusTheBurningThrone/Images/antorus.jpg';
 import ImageArticle from 'Main/News/ImageArticle';
 
 export default (
-  <ImageArticle title="Updated for Antorus" published="2017-12-24" image={AntorusImage} style={{ paddingTop: 350 }}>
+  <ImageArticle title="Updated for Antorus" publishedAt="2017-12-24" publishedBy={Zerotorescue} image={AntorusImage} style={{ paddingTop: 350 }}>
     We've been working hard to implement all the new trinkets and tier bonuses available in the new raid <i>Antorus, the Burning Throne</i>. Let us know on <a href="https://discord.gg/AxphPxU">Discord</a> or <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> if you're still missing anything.
   </ImageArticle>
 );
