67d9cdc507192e14a636d9b5c32a9968c502945c
niseko <rentz@hotmail.de>
2018-06-21 19:20:30 +0200

    return early

--- src/Parser/Shaman/Restoration/Modules/Items/Tidecallers.js
+++ src/Parser/Shaman/Restoration/Modules/Items/Tidecallers.js
@@ -22,11 +22,7 @@ class Tidecallers extends Analyzer {
     this.active = this.owner.modules.combatants.selected.hasHands(ITEMS.PRAETORIANS_TIDECALLERS.id);
   }
 
-  on_heal(event) {
-    if (!this.owner.byPlayer(event) && !this.owner.byPlayerPet(event)) {
-      return;
-    }
-
+  on_byPlayerPet_heal(event) {
     const spellId = event.ability.guid;
     const healingDone = event.amount + (event.absorbed || 0);
 
--- src/Parser/Shaman/Restoration/Modules/ShamanCore/ShamanAbilityTracker.js
+++ src/Parser/Shaman/Restoration/Modules/ShamanCore/ShamanAbilityTracker.js
@@ -3,7 +3,6 @@ import SPELLS from 'common/SPELLS';
 import AbilityTracker from 'Parser/Core/Modules/AbilityTracker';
 import Combatants from 'Parser/Core/Modules/Combatants';
 
-const TIDAL_WAVES_SPELL_ID = 53390;
 const TIDAL_WAVES_BUFF_EXPIRATION_BUFFER = 50; // the buff expiration can occur several MS before the heal event is logged, this is the buffer time that an IoL charge may have dropped during which it will still be considered active.
 const TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME = 100; // Minimal duration for which you must have tidal waves. Prevents it from counting a HS/HW as buffed when you cast a riptide at the end.
 
@@ -17,17 +16,19 @@ class ShamanAbilityTracker extends AbilityTracker {
       super.on_byPlayer_heal(event);
     }
     const spellId = event.ability.guid;
+
+    if (spellId !== SPELLS.HEALING_WAVE.id && spellId !== SPELLS.HEALING_SURGE_RESTORATION.id) {
+      return;
+    }
+
     const cast = this.getAbility(spellId, event.ability);
+    const hasTw = this.combatants.selected.hasBuff(SPELLS.TIDAL_WAVES_BUFF.id, event.timestamp, TIDAL_WAVES_BUFF_EXPIRATION_BUFFER, TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME);
 
-    if (spellId === SPELLS.HEALING_WAVE.id || spellId === SPELLS.HEALING_SURGE_RESTORATION.id) {
-      const hasTw = this.combatants.selected.hasBuff(TIDAL_WAVES_SPELL_ID, event.timestamp, TIDAL_WAVES_BUFF_EXPIRATION_BUFFER, TIDAL_WAVES_BUFF_MINIMAL_ACTIVE_TIME);
-
-      if (hasTw) {
-        cast.healingTwHits = (cast.healingTwHits || 0) + 1;
-        cast.healingTwHealing = (cast.healingTwHealing || 0) + (event.amount || 0);
-        cast.healingTwAbsorbed = (cast.healingTwAbsorbed || 0) + (event.absorbed || 0);
-        cast.healingTwOverheal = (cast.healingTwOverheal || 0) + (event.overheal || 0);
-      }
+    if (hasTw) {
+      cast.healingTwHits = (cast.healingTwHits || 0) + 1;
+      cast.healingTwHealing = (cast.healingTwHealing || 0) + (event.amount || 0);
+      cast.healingTwAbsorbed = (cast.healingTwAbsorbed || 0) + (event.absorbed || 0);
+      cast.healingTwOverheal = (cast.healingTwOverheal || 0) + (event.overheal || 0);
     }
   }
 }
