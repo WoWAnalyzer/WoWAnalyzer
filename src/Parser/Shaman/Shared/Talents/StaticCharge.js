698a5161c53e09e6b911f4f774608a0b76da9798
niseko <rentz@hotmail.de>
2018-06-21 20:23:11 +0200

    rename leftover Lightning Surge Totems

--- src/Parser/Shaman/Shared/Talents/StaticCharge.js
+++ src/Parser/Shaman/Shared/Talents/StaticCharge.js
@@ -23,12 +23,12 @@ class StaticCharge extends Analyzer {
       return;
     }
 
-    const cooldownRemaining = this.spellUsable.cooldownRemaining(SPELLS.LIGHTNING_SURGE_TOTEM.id);
+    const cooldownRemaining = this.spellUsable.cooldownRemaining(SPELLS.CAPACITOR_TOTEM.id);
     if (cooldownRemaining <= minimumCooldown) {
       return;
     }
 
-    this.spellUsable.reduceCooldown(SPELLS.LIGHTNING_SURGE_TOTEM.id, stunReduction);
+    this.spellUsable.reduceCooldown(SPELLS.CAPACITOR_TOTEM.id, stunReduction);
   }
 }
 
