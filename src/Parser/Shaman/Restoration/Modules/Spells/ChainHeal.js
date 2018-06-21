422dbc903e3c0c62e36616ce2a4e49e29fcbfc03
niseko <rentz@hotmail.de>
2018-06-21 16:53:45 +0200

    reduce code duplication

--- src/Parser/Shaman/Restoration/Modules/Spells/ChainHeal.js
+++ src/Parser/Shaman/Restoration/Modules/Spells/ChainHeal.js
@@ -19,15 +19,20 @@ class ChainHeal extends Analyzer {
     combatants: Combatants,
   };
 
+  on_initialized() {
+    this.maxTargets = this.combatants.selected.hasTalent(SPELLS.HIGH_TIDE_TALENT.id) ? 5 : 4;
+    this.suggestedTargets = this.maxTargets * CHAIN_HEAL_TARGET_EFFICIENCY;
+  }
+
   suggestions(when) {
     const suggestedThreshold = this.suggestionThreshold;
     if (isNaN(suggestedThreshold.actual)) {
       return;
     }
-    const maxTargets = this.combatants.selected.hasTalent(SPELLS.HIGH_TIDE_TALENT.id) ? 5 : 4;
+
     when(suggestedThreshold.actual).isLessThan(suggestedThreshold.isLessThan.minor)
       .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<span>Try to always cast <SpellLink id={SPELLS.CHAIN_HEAL.id} /> on groups of people, so that it heals all {maxTargets} potential targets.</span>)
+        return suggest(<span>Try to always cast <SpellLink id={SPELLS.CHAIN_HEAL.id} /> on groups of people, so that it heals all {this.maxTargets} potential targets.</span>)
           .icon(SPELLS.CHAIN_HEAL.icon)
           .actual(`${suggestedThreshold.actual.toFixed(2)} average targets healed`)
           .recommended(`${suggestedThreshold.isLessThan.minor} average targets healed`)
@@ -35,46 +40,37 @@ class ChainHeal extends Analyzer {
       });
   }
 
-  get suggestionThreshold() {
+  get avgHits() {
     const chainHeal = this.abilityTracker.getAbility(SPELLS.CHAIN_HEAL.id);
-
     const casts = chainHeal.casts || 0;
     const hits = chainHeal.healingHits || 0;
-    const avgHits = hits / casts;
+    return hits / casts || 0;
+  }
 
-    const maxTargets = this.combatants.selected.hasTalent(SPELLS.HIGH_TIDE_TALENT.id) ? 5 : 4;
-    const suggestedTargets = maxTargets * CHAIN_HEAL_TARGET_EFFICIENCY;
+  get casts() {
+    const chainHeal = this.abilityTracker.getAbility(SPELLS.CHAIN_HEAL.id);
+    return chainHeal.casts || 0;
+  }
 
+  get suggestionThreshold() {
     return {
-      actual: avgHits,
+      actual: this.avgHits,
       isLessThan: {
-        minor: suggestedTargets,//Missed 1 target
-        average: suggestedTargets - 1,//Missed 2-3 targets
-        major: suggestedTargets - 2,//Missed more than 3 targets
+        minor: this.suggestedTargets,//Missed 1 target
+        average: this.suggestedTargets - 1,//Missed 2-3 targets
+        major: this.suggestedTargets - 2,//Missed more than 3 targets
       },
       style: 'number',
     };
-
   }
+
   statistic() {
-    const chainHeal = this.abilityTracker.getAbility(SPELLS.CHAIN_HEAL.id);
-
-    const casts = chainHeal.casts || 0;
-    const hits = chainHeal.healingHits || 0;
-    const avgHits = hits / casts;
-
-    const maxTargets = this.combatants.selected.hasTalent(SPELLS.HIGH_TIDE_TALENT.id) ? 5 : 4;
-
-    if (isNaN(avgHits)) {
-      return null;
-    }
-
     return (
       <StatisticBox
         icon={<SpellIcon id={SPELLS.CHAIN_HEAL.id} />}
-        value={avgHits.toFixed(2)}
+        value={this.avgHits.toFixed(2)}
         label={(
-          <dfn data-tip={`The average number of targets healed by Chain Heal out of the maximum amount of targets. You cast a total of ${casts} Chain Heals, which healed an average of ${avgHits.toFixed(2)} out of ${maxTargets} targets.`}>
+          <dfn data-tip={`The average number of targets healed by Chain Heal out of the maximum amount of targets. You cast a total of ${this.casts} Chain Heals, which healed an average of ${this.avgHits.toFixed(2)} out of ${this.maxTargets} targets.`}>
             Average Chain Heal targets
           </dfn>
         )}
@@ -85,4 +81,3 @@ class ChainHeal extends Analyzer {
 }
 
 export default ChainHeal;
-
