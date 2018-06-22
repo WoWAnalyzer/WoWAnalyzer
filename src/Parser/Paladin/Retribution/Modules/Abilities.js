70c002f1c9446c476180975233d4588d9d5a876a
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2018-06-22 14:30:20 +0200

    Quick fix

--- src/Parser/Paladin/Retribution/Modules/Abilities.js
+++ src/Parser/Paladin/Retribution/Modules/Abilities.js
@@ -64,7 +64,7 @@ class Abilities extends CoreAbilities {
         spell: SPELLS.CRUSADER_STRIKE,
         category: Abilities.SPELL_CATEGORIES.ROTATIONAL,
         charges: 2,
-        cooldown: cooldown: haste => (combatant.hasTalent(SPELLS.THE_FIRES_OF_JUSTICE_TALENT.id) ? 5.1 : 6) / (1 + haste),
+        cooldown: haste => (combatant.hasTalent(SPELLS.THE_FIRES_OF_JUSTICE_TALENT.id) ? 5.1 : 6) / (1 + haste),
         isOnGCD: true,
         castEfficiency: {
           suggestion: true,
