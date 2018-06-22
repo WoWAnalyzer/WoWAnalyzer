723f7feee69314b487ea93bf247cc479eb94a7b1
niseko <rentz@hotmail.de>
2018-06-22 02:49:39 +0200

    cleaned up constants & ABC

--- src/Parser/Priest/Holy/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Priest/Holy/Modules/Features/AlwaysBeCasting.js
@@ -13,31 +13,17 @@ const HEALING_ABILITIES_ON_GCD = [
   SPELLS.RENEW.id,
   SPELLS.HOLY_WORD_SERENITY.id,
   SPELLS.HOLY_WORD_SANCTIFY.id,
-  SPELLS.LIGHT_OF_TUURE_TRAIT.id,
   SPELLS.BINDING_HEAL_TALENT.id,
   SPELLS.CIRCLE_OF_HEALING_TALENT.id,
   SPELLS.HALO_TALENT.id,
   SPELLS.DIVINE_STAR_TALENT.id,
+  SPELLS.APOTHEOSIS_TALENT.id,
+  SPELLS.DIVINE_HYMN_CAST.id,
+  SPELLS.HOLY_WORD_SALVATION_TALENT.id,
 ];
 
 class AlwaysBeCasting extends CoreAlwaysBeCastingHealing {
   static HEALING_ABILITIES_ON_GCD = HEALING_ABILITIES_ON_GCD;
-  static ABILITIES_ON_GCD = [
-    ...HEALING_ABILITIES_ON_GCD,
-    SPELLS.FADE.id,
-    SPELLS.ANGELIC_FEATHER_TALENT.id,
-    SPELLS.BODY_AND_MIND_TALENT.id,
-    SPELLS.SMITE.id,
-    SPELLS.GUARDIAN_SPIRIT.id,
-    SPELLS.DISPEL_MAGIC.id,
-    SPELLS.LEAP_OF_FAITH.id,
-    SPELLS.PURIFY.id,
-
-    // DPS abilities that likely have no reason to be stored in SPELLS
-    14915, // Holy Fire
-    88625, // Holy Word: Chastise
-    132157, // Holy Nova
-  ];
 
   suggestions(when) {
     const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
