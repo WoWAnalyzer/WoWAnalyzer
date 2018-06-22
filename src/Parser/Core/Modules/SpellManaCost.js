7d43f5e152baf47173a891a556f2f4215734dfd9
niseko <rentz@hotmail.de>
2018-06-22 02:47:18 +0200

    Symbol of Hope doesn't nullify mana costs anymore

--- src/Parser/Core/Modules/SpellManaCost.js
+++ src/Parser/Core/Modules/SpellManaCost.js
@@ -12,7 +12,7 @@ class SpellManaCost extends Analyzer {
     // Manipulate the event to include mana information so that we don't have to copy paste this anywhere we want to know mana. This can't be done through static functions as some mana costs require state (through class properties) to work properly. E.g. Penance triggers up to 4 cast events but only the first costs mana.
     event.manaCost = this.getManaCost(event);
     event.rawManaCost = this.getRawManaCost(event);
-    event.isManaCostNullified = this.combatants.selected.hasBuff(SPELLS.INNERVATE.id, event.timestamp) || this.combatants.selected.hasBuff(SPELLS.SYMBOL_OF_HOPE_TALENT.id, event.timestamp);
+    event.isManaCostNullified = this.combatants.selected.hasBuff(SPELLS.INNERVATE.id, event.timestamp);
   }
 
   getHardcodedManaCost(event) {
