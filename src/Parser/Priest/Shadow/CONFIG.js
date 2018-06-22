4c5ea1b0e6a6dd1da391cb92fe58e4f967cfd409
Martijn Hols <git@martijnhols.nl>
2018-06-23 00:37:01 +0200

    Shadow Priest: Update patch status and message

--- src/Parser/Priest/Shadow/CONFIG.js
+++ src/Parser/Priest/Shadow/CONFIG.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { hassebewlen } from 'CONTRIBUTORS';
+import { hassebewlen, Zerotorescue } from 'CONTRIBUTORS';
 import SPECS from 'common/SPECS';
 import Warning from 'common/Alert/Warning';
 
@@ -8,17 +8,17 @@ import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list.
-  contributors: [hassebewlen],
+  contributors: [hassebewlen, Zerotorescue],
   // The WoW client patch this spec was last updated to be fully compatible with.
-  patchCompatibility: '7.3',
+  patchCompatibility: '8.0',
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
     <React.Fragment>
-      Hi! Welcome to the Shadow Priest analyzer.<br /><br />
-
       <Warning>
-        Hi <kbd>@Zerotorescue</kbd> here. I updated this spec to be compatible with BFA, but I'm no Shadow expert. If there's anything wrong please @poke me on Discord and I'll fix it if I can.
+        Hi <kbd>@Zerotorescue</kbd> here. I updated this spec to be compatible with BFA, but I'm no Shadow expert. If there's anything wrong please @poke me on Discord and I'll fix it if I can.<br /><br />
+
+        This spec needs a focused maintainer. If you want to give it a try, check <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> for more information.
       </Warning>
     </React.Fragment>
   ),
