34662d236c6909b2e882944c226906ef4a25fac4
Martijn Hols <git@martijnhols.nl>
2018-06-24 16:30:33 +0200

    Update downtime suggestion

--- src/Parser/Rogue/Outlaw/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Rogue/Outlaw/Modules/Features/AlwaysBeCasting.js
@@ -6,9 +6,9 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
     return {
       actual: this.downtimePercentage,
       isGreaterThan: {
-        minor: 0.1,
-        average: 0.15,
-        major: 0.2,
+        minor: 0.2,
+        average: 0.25,
+        major: 0.3,
       },
       style: 'percentage',
     };
