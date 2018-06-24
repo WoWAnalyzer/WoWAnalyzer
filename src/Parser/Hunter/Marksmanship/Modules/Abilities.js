cb424c87c1cee2c2cf4658d9823183ee9aa1f960
Putro <lafa16ac@student.cbs.dk>
2018-06-24 21:23:08 +0200

    fix aimed shot cd

--- src/Parser/Hunter/Marksmanship/Modules/Abilities.js
+++ src/Parser/Hunter/Marksmanship/Modules/Abilities.js
@@ -12,6 +12,8 @@ class Abilities extends CoreAbilities {
       {
         spell: SPELLS.AIMED_SHOT,
         category: Abilities.SPELL_CATEGORIES.ROTATIONAL,
+        cooldown: haste => 12 / (1 + haste),
+        charges: 2,
         gcd: {
           base: 1500,
         },
