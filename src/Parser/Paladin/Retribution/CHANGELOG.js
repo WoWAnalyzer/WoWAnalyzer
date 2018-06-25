414317960f79594e164017d750daedff7ea368b1
Juko\Ulrich <ulrichenevoldsen@hotmail.com>
2018-06-25 16:10:42 +0200

    Updated Changelog

--- src/Parser/Paladin/Retribution/CHANGELOG.js
+++ src/Parser/Paladin/Retribution/CHANGELOG.js
@@ -5,6 +5,11 @@ import { Hewhosmites, Zerotorescue, Juko8} from 'CONTRIBUTORS';
 
 export default [
   {
+    date: new Date('2018-06-25'),
+    changes: <React.Fragment>Added modules for <SpellLink id={SPELLS.RIGHTEOUS_VERDICT_TALENT.id} icon /> and <SpellLink id={SPELLS.INQUISITION_TALENT.id} icon /> </React.Fragment>,
+    contributors: [Juko8],
+  },
+  {
     date: new Date('2018-06-24'),
     changes: <React.Fragment>Updated modules for <SpellLink id={SPELLS.ART_OF_WAR.id} icon /> and <SpellLink id={SPELLS.JUDGMENT_CAST.id} icon /></React.Fragment>,
     contributors: [Juko8],
