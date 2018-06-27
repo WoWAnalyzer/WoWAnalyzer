7fbd3724022468a8eff57f18810ba252485ed2db
Martijn Hols <git@martijnhols.nl>
2018-06-27 20:48:03 +0200

    Subtlety Rogue: Update Combo Points resource tracker

--- src/Parser/Rogue/Subtlety/Modules/RogueCore/ComboPoints.js
+++ src/Parser/Rogue/Subtlety/Modules/RogueCore/ComboPoints.js
@@ -18,32 +18,46 @@ class ComboPoints extends Analyzer {
 
   suggestions(when) {
     resourceSuggest(when, this.comboPointTracker, {
-      spell: SPELLS.BACKSTAB,
+      spell: SPELLS.MARKED_FOR_DEATH_TALENT, // 5 CP
+      minor: 0,
+      avg: 0.05,
+      major: 0.1,
+      extraSuggestion: this.makeExtraSuggestion(SPELLS.MARKED_FOR_DEATH_TALENT),
+    });
+    resourceSuggest(when, this.comboPointTracker, {
+      spell: SPELLS.BACKSTAB, // 1 CP
       minor: 0.05,
       avg: 0.10,
       major: 0.15,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.BACKSTAB),
     });
     resourceSuggest(when, this.comboPointTracker, {
-      spell: SPELLS.GLOOMBLADE_TALENT,
+      spell: SPELLS.GLOOMBLADE_TALENT, // 1 CP
       minor: 0.05,
       avg: 0.10,
       major: 0.15,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.GLOOMBLADE_TALENT),
     });
     resourceSuggest(when, this.comboPointTracker, {
-      spell: SPELLS.SHADOWSTRIKE,
+      spell: SPELLS.SHADOWSTRIKE, // 2 CP
       minor: 0.05,
       avg: 0.10,
       major: 0.15,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.SHADOWSTRIKE),
     });
     resourceSuggest(when, this.comboPointTracker, {
-      spell: SPELLS.SHURIKEN_STORM,
+      spell: SPELLS.SHURIKEN_STORM, // 1 CP per target hit
       minor: 0.1,
       avg: 0.2,
       major: 0.3,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.SHURIKEN_STORM),
+    });
+    resourceSuggest(when, this.comboPointTracker, {
+      spell: SPELLS.SHURIKEN_TOSS, // 1 CP
+      minor: 0.05,
+      avg: 0.10,
+      major: 0.15,
+      extraSuggestion: this.makeExtraSuggestion(SPELLS.SHURIKEN_TOSS),
     });
     resourceSuggest(when, this.comboPointTracker, {
       spell: SPELLS.SHADOW_TECHNIQUES,
@@ -51,6 +65,13 @@ class ComboPoints extends Analyzer {
       avg: 0.2,
       major: 0.3,
       extraSuggestion: <span> Use a weak Aura to track <SpellLink id={SPELLS.SHADOW_TECHNIQUES.id} />. This is an advanced suggestion and should not be addressed first. </span>,
+    });
+    resourceSuggest(when, this.comboPointTracker, {
+      spell: SPELLS.CHEAP_SHOT, // 2 CP
+      minor: 0,
+      avg: 0.1,
+      major: 0.2,
+      extraSuggestion: this.makeExtraSuggestion(SPELLS.CHEAP_SHOT),
     });
   }
 }
