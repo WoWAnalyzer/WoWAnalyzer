2a9f43a2d12c724cbea328fc7d6fd8d8b19ee191
Putro <lafa16ac@student.cbs.dk>
2018-06-28 23:24:07 +0200

    LethalShots work and Channeling

--- src/Parser/Hunter/Marksmanship/Modules/Talents/PiercingShot.js
+++ src/Parser/Hunter/Marksmanship/Modules/Talents/PiercingShot.js
@@ -7,8 +7,7 @@ import SpellLink from "common/SpellLink";
 import ItemDamageDone from 'Main/ItemDamageDone';
 
 /**
- * A powerful shot which deals up to (2 * 775%) Physical damage to the target and up to 775% Physical damage to all enemies between you and the target.
- * Damage increased against targets with Vulnerable.
+ * A powerful shot which deals (112.5% of Attack power)% Physical damage to the target and up to [(112.5% of Attack power)% / (3)] Physical damage to all enemies between you and the target.
  */
 class PiercingShot extends Analyzer {
 
