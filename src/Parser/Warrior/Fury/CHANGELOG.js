714a9d64f1bfbcff8fa7b91066bb8e8e1dfaf186
Martijn Hols <git@martijnhols.nl>
2018-06-30 21:17:37 +0200

    Fury Warrior: Update config and changelog

--- src/Parser/Warrior/Fury/CHANGELOG.js
+++ src/Parser/Warrior/Fury/CHANGELOG.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import { Maldark, Aelexe} from 'CONTRIBUTORS';
+import { Maldark, Aelexe, Zerotorescue } from 'CONTRIBUTORS';
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 
 export default [
+  {
+    date: new Date('2018-06-30'),
+    changes: 'Update all abilities to new BFA values.',
+    contributors: [Zerotorescue],
+  },
   {
     date: new Date('2018-04-09'),
     changes: <React.Fragment>Add <SpellLink id={SPELLS.CHARGE.id} icon /> cooldown and charge modifications for <SpellLink id={SPELLS.DOUBLE_TIME_TALENT.id} icon />.</React.Fragment>,
