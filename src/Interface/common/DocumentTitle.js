42098056d3d4c9a2859c31cc4878e3cfff79540a
Martijn Hols <git@martijnhols.nl>
2018-07-01 23:35:27 +0200

    Comments

--- src/Interface/common/DocumentTitle.js
+++ src/Interface/common/DocumentTitle.js
@@ -12,6 +12,7 @@ class DocumentTitle extends React.PureComponent {
     const { title } = this.props;
 
     document.title = title ? `${title} - ${siteName}` : siteName;
+    // console.log('New document title:', document.title);
 
     return null;
   }
