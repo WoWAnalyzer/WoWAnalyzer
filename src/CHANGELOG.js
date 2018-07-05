b19a67cbe1e41b25d6c9aa8119bfb4aea78bbb04
Martijn Hols <git@martijnhols.nl>
2018-07-05 20:41:47 +0200

    Update changelog

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -8,6 +8,11 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
+    date: new Date('2018-07-05'),
+    changes: 'Added a toggle to the results page to adjust statistics for fight downtime. This is an experimental feature.',
+    contributors: [Zerotorescue],
+  },
+  {
     date: new Date('2018-06-28'),
     changes: 'Allow Warcaftlogs- & BattleNet-character-links in the report selecter.',
     contributors: [joshinator],
