1e55fd293cf0cf3e6d0952c475aa8215f95602f5
Nikita Snitnikov <snitnik@gmail.com>
2018-07-08 21:08:59 +0200

    Outlaw spell updates

--- src/Parser/Rogue/Outlaw/CONFIG.js
+++ src/Parser/Rogue/Outlaw/CONFIG.js
@@ -19,10 +19,6 @@ export default {
         Hey there! A good basis has been implemented for this spec, but it needs to be fleshed out more to provide all the feedback possible.<br /><br />
         This spec needs a focused maintainer. If you want to give it a try, check <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> for more information.
       </Warning>
-      <br />
-      <Warning>
-        Hi <kbd>@Zerotorescue</kbd> here. I updated this spec to be compatible with BFA, but I'm no Outlaw expert. If there's anything wrong please @poke me on Discord and I'll fix it if I can.
-      </Warning>
     </React.Fragment>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
