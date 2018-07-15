33b6a2af1d7c726966bf0809a19f86ce51727e1b
Martijn Hols <git@martijnhols.nl>
2018-07-15 20:21:48 +0200

    Revert "Add a note redirecting users to the BFA version"
    This reverts commit 156caaa315c45c2a4a869574b350baf4501b0af6.

--- src/Main/Header.js
+++ src/Main/Header.js
@@ -43,11 +43,10 @@ class Header extends React.PureComponent {
         <div className="container image-overlay">
           <div className="row">
             <div className="col-lg-6 col-md-10">
-              <h1><Trans>WoW&shy;Analyzer for Legion</Trans></h1>
+              <h1><Trans>WoW&shy;Analyzer</Trans></h1>
               <div className="description">
                 <Trans>Analyze your raid logs to get personal suggestions and metrics to improve your performance. Just enter a Warcraft Logs report:</Trans>
               </div>
-
               {showReportSelecter && (
                 <div>
                   <div className="parse-tabs">
@@ -71,10 +70,6 @@ class Header extends React.PureComponent {
                   )}
                 </div>
               )}
-
-              <div style={{ fontSize: '1.2em', fontWeight: 600, marginTop: 5 }} className={`alert alert-info`}>
-                Welcome to the WoWAnalyzer for Legion. <a href="https://bfa.wowanalyzer.com">Click here</a> to switch to the BFA version.
-              </div>
                 
               {/*{process.env.NODE_ENV !== 'test' && <ServiceStatus style={{ marginBottom: 5 }} />}*/}
               <div className="about">
