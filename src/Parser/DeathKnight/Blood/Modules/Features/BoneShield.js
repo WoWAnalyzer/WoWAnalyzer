0e184f392986b9cd2709377b1735e61c27bbeacd
yajinni <yajinni@gmail.com>
2018-07-18 16:34:47 -0400

    Boneshield DR Removal
    The talents/traits dont exist anymore.
    And bonshields DR behaved very differently now, only applies to physical damage and is based on armor calculations. So removing everything in relation to that till we get a standardized module for these calcs since it affects all tanks.

--- src/Parser/DeathKnight/Blood/Modules/Features/BoneShield.js
+++ src/Parser/DeathKnight/Blood/Modules/Features/BoneShield.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Analyzer from 'Parser/Core/Analyzer';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
-import { formatDuration, formatNumber, formatPercentage } from 'common/format';
+import { formatDuration, formatPercentage } from 'common/format';
 import { STATISTIC_ORDER } from 'Main/StatisticBox';
 import ExpandableStatisticBox from 'Main/ExpandableStatisticBox';
 import StatTracker from 'Parser/Core/Modules/StatTracker';
 import BoneShieldTimesByStacks from '../Features/BoneShieldTimesByStacks';
 
-const SKELETAL_SHATTERING_DR = 0.08;
-const BONE_SHIELD_DR = 0.16;
 
 class BoneShield extends Analyzer {
 
@@ -18,50 +16,11 @@ class BoneShield extends Analyzer {
     boneShieldTimesByStacks: BoneShieldTimesByStacks,
   };
 
-  hasSS = false;
-  hasSD = false;
-
-  boneShieldMitigated = 0;
-  skeletalShatteringMitigated = 0;
-
-  constructor(...args) {
-    super(...args);
-    this.hasSS = this.selectedCombatant.traitsBySpellId[SPELLS.SKELETAL_SHATTERING_TRAIT.id];
-    this.hasSD = this.selectedCombatant.hasTalent(SPELLS.SPECTRAL_DEFLECTION_TALENT.id);
-  }
-
-  get boneShieldAbsorbTooltip() {
-    return formatNumber(this.boneShieldMitigated + this.skeletalShatteringMitigated) + ' Bone Shield Absorb<br>';
-  }
 
   get boneShieldTimesByStack() {
     return this.boneShieldTimesByStacks.boneShieldTimesByStacks;
   }
 
-  get skeletalShatteringTooltip() {
-    if (this.hasSS) {
-      return 'On average, Skeletal Shattering would have contributed ' +
-        formatPercentage(this.skeletalShatteringMitigated / (this.boneShieldMitigated + this.skeletalShatteringMitigated)) +
-        '% (' + formatNumber(this.skeletalShatteringMitigated) + ') to this.<br>';
-    } else {
-      return '';
-    }
-  }
-
-  on_toPlayer_damage(event) {
-
-    if (!this.selectedCombatant.hasBuff(SPELLS.BONE_SHIELD.id)) {
-      return;
-    }
-
-    const preMitigatedBoneShield = (event.amount + event.absorbed) / (1 - BONE_SHIELD_DR);
-    this.boneShieldMitigated += preMitigatedBoneShield * BONE_SHIELD_DR;
-
-    if (this.hasSS) {
-      const preMitigatedSkeletalShattering = (event.amount + event.absorbed) / (1 - BONE_SHIELD_DR - SKELETAL_SHATTERING_DR);
-      this.skeletalShatteringMitigated += preMitigatedSkeletalShattering * SKELETAL_SHATTERING_DR * this.statTracker.currentCritPercentage;
-    }
-  }
 
   get uptime() {
     return this.selectedCombatant.getBuffUptime(SPELLS.BONE_SHIELD.id) / this.owner.fightDuration;
@@ -91,13 +50,11 @@ class BoneShield extends Analyzer {
 
   statistic() {
 
-    if (this.hasSD) {
-
       return (
         <ExpandableStatisticBox
           icon={<SpellIcon id={SPELLS.BONE_SHIELD.id} />}
           value={`${formatPercentage(this.uptime)} %`}
-          label="Bone Shield Uptime"
+          label="Bone Shield uptime"
         >
           <table className="table table-condensed">
             <thead>
@@ -120,39 +77,6 @@ class BoneShield extends Analyzer {
         </ExpandableStatisticBox>
 
       );
-
-    } else {
-
-      return (
-        <ExpandableStatisticBox
-          icon={<SpellIcon id={SPELLS.BONE_SHIELD.id} />}
-          value={`${formatPercentage(this.uptime)} %`}
-          label="Bone Shield Uptime"
-          tooltip={`~${this.boneShieldAbsorbTooltip}
-          ${this.skeletalShatteringTooltip}
-          <b>Those numbers are estimates as it's not possible to track the actual Bone Shield absorb.</b>`}
-        >
-          <table className="table table-condensed">
-            <thead>
-              <tr>
-                <th>Stacks</th>
-                <th>Time (s)</th>
-                <th>Time (%)</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.boneShieldTimesByStack.map((e, i) => (
-                <tr key={i}>
-                  <th>{i}</th>
-                  <td>{formatDuration(e.reduce((a, b) => a + b, 0) / 1000)}</td>
-                  <td>{formatPercentage(e.reduce((a, b) => a + b, 0) / this.owner.fightDuration)}%</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </ExpandableStatisticBox>
-      );
-    }
   }
   statisticOrder = STATISTIC_ORDER.CORE(5);
 }
