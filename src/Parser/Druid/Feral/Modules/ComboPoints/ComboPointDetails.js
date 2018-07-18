060a2e8f7a45090e7a995fbbd99ce3c68f6afc48
Anatta336 <sdriver@gmx.co.uk>
2018-07-18 02:52:45 +0100

    expose thresholds for ComboPointDetails

--- src/Parser/Druid/Feral/Modules/ComboPoints/ComboPointDetails.js
+++ src/Parser/Druid/Feral/Modules/ComboPoints/ComboPointDetails.js
@@ -3,6 +3,8 @@ import Analyzer from 'Parser/Core/Analyzer';
 import Tab from 'Main/Tab';
 import StatisticBox, { STATISTIC_ORDER } from 'Main/StatisticBox';
 import { formatPercentage } from 'common/format';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
 
 import ResourceBreakdown from './ComboPointBreakdown';
 import WastedPointsIcon from '../Images/feralComboPointIcon.png';
@@ -39,40 +41,55 @@ class ComboPointDetails extends Analyzer {
   }
   statisticOrder = STATISTIC_ORDER.CORE(6);
 
-  get suggestionThresholds() {
+  get wastingSuggestionThresholds() {
     return {
       actual: this.pointsWastedPerMinute,
       isGreaterThan: {
-        minor: 5,
-        average: 10,
-        major: 15,
+        minor: 0,
+        average: 5,
+        major: 10,
       },
       style: 'number',
     };
   }
 
-  suggestions(when) {
-    when(this.suggestionThresholds)
-      .addSuggestion((suggest, actual, recommended) => {
-        return suggest('You are wasting Combo Points. Try to use them and not let them cap and go to waste unless you\'re preparing for bursting adds etc.')
-          .icon('creatureportrait_bubble')
-          .actual(`${this.pointsWasted} Combo Points wasted (${this.pointsWastedPerMinute.toFixed(2)} per minute)`)
-          .recommended(`< ${recommended.toFixed(2)} Combo Points per minute wasted are recommended`);
-      });
-
-
+  get finishersBelowMaxSuggestionThresholds() {
     const maxComboPointCasts = Object.values(this.comboPointTracker.spendersObj).reduce((acc, spell) => acc + spell.spentByCast.filter(cps => cps === 5).length, 0);
     const totalCasts = this.comboPointTracker.spendersCasts;
     const maxComboPointPercent = maxComboPointCasts / totalCasts;
+    return {
+      actual: maxComboPointPercent,
+      isLessThan: {
+        minor: 0.95,
+        average: 0.90,
+        major: 0.85,
+      },
+      style: 'percentage',
+    };
+  }
 
-    when(maxComboPointPercent).isLessThan(0.95)
-      .addSuggestion((suggest, actual, recommended) => {
-        return suggest(<React.Fragment>You are casting too many finishers with less that 5 combo points. Try to make sure that you are generating 5 combo points before casting a finisher unless it is an emergency.</React.Fragment>)
-          .icon('creatureportrait_bubble')
-          .actual(`${formatPercentage(actual)}% of finishers were cast with 5 combo points`)
-          .recommended(`>${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended - 0.05).major(recommended - 0.10);
-      });
+  suggestions(when) {
+    when(this.wastingSuggestionThresholds).addSuggestion((suggest, actual, recommended) => {
+      return suggest(
+        <React.Fragment>
+          You are wasting combo points. Avoid using generators once you reach the maximum.
+        </React.Fragment>
+      )
+        .icon('creatureportrait_bubble')
+        .actual(`${actual.toFixed(1)} combo points wasted per minute`)
+        .recommended('zero waste is recommended');
+    });
+
+    when(this.finishersBelowMaxSuggestionThresholds).addSuggestion((suggest, actual, recommended) => {
+      return suggest(
+        <React.Fragment>
+          You are casting too many finishers with less that 5 combo points. Apart from <SpellLink id={SPELLS.SAVAGE_ROAR_TALENT.id} /> during the opening of a fight you should always use finishers with a full 5 combo points.
+        </React.Fragment>
+      )
+        .icon('creatureportrait_bubble')
+        .actual(`${formatPercentage(actual)}% of finishers were cast with 5 combo points`)
+        .recommended(`>${formatPercentage(recommended)}% is recommended`);
+    });
   }
 
   tab() {
