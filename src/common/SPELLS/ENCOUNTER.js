e1ddb352f60174d977f379242831da5e4a08533d
hpabst <hpabst@ualberta.ca>
2018-07-19 12:23:41 -0600

    Add encounter spells and damage for garothi worldbreaker and felhounds.

---
+++ src/common/SPELLS/ENCOUNTER.js
@@ -0,0 +1,220 @@
+
+
+export default {
+  // Antorus the Burning Throne
+
+  // Garothi Worldbreaker
+  //Casts:
+  GAROTHI_ANNIHILATION: {
+    id: 244294,
+    name: 'Annihilation',
+    icon: 'trade_engineering',
+  },
+  GAROTHI_DECIMATION: {
+    id: 244399,
+    name: 'Decimation',
+    icon: 'ability_mage_firestarter',
+  },
+  GAROTHI_EMPOWERED: {
+    id: 245237,
+    name: 'Empowered',
+    icon: 'ability_demonhunter_empowerwards',
+  },
+  GAROTHI_ERADICATION: {
+    id: 244969,
+    name: 'Eradication',
+    icon: 'ability_warrior_collossussmash',
+  },
+  GAROTHI_HAYWIRE_0: {
+    id: 246965,
+    name: 'Haywire',
+    icon: 'spell_shaman_staticshock',
+  },
+  GAROTHI_HAYWIRE_1: {
+    id: 246897,
+    name: 'Haywire',
+    icon: 'spell_shaman_staticshock',
+  },
+  GAROTHI_HAYWIRE_DECIMATION: {
+    id: 246919,
+    name: 'Haywire Decimation',
+    icon: 'ability_mage_firestarter',
+  },
+  GAROTHI_LURING_DESTRUCTION: {
+    id: 246848,
+    name: 'Luring Destruction',
+    icon: 'achievement_boss_hellfire_felreaver',
+  },
+  GAROTHI_SHRAPNEL: {
+    id: 247044,
+    name: "Shrapnel",
+    icon: "ability_deathwing_shrapnel",
+  },
+  //Damage:
+  GAROTHI_APOCALYPSE_DRIVE: {
+    id: 253300,
+    name: "Apocalypse Drive",
+    icon: 'ability_bossfelmagnaron_handempowered',
+  },
+  GAROTHI_FEL_BOMBARDMENT: {
+    id: 244532,
+    name: 'Fel Bombardment',
+    icon: 'ability_hunter_snipershot',
+  },
+  GAROTHI_LURING_DESTRUCTION_DAMAGE: {
+    id: 247159,
+    name: 'Luring Destruction',
+    icon: 'achievement_boss_hellfire_felreaver',
+  },
+  GAROTHI_MELEE: { //Garothi's special melee ability to deal with range issues on tanks.
+    id: 248230,
+    name: 'Melee',
+    icon: 'spell_fire_felimmolation',
+  },
+
+  //Felhounds:
+  FELHOUNDS_DECAY_DEBUFF: {
+    id: 245098,
+    name: "Decay",
+    icon: "spell_shadow_burningspirit",
+  },
+  FELHOUNDS_DESOLATE_PATH: {
+    id: 244825,
+    name: "Desolate Path",
+    icon: "ability_rhyolith_magmaflow_wave",
+  },
+  FELHOUNDS_ENFLAME_CORRUPTION: {
+    id: 244057,
+    name: "Enflame Corruption",
+    icon: "spell_fire_moltenblood",
+  },
+  FELHOUNDS_MOLTEN_TOUCH: {
+    id: 244072,
+    name: "Molten Touch",
+    icon: "spell_fire_fireball",
+  },
+  FELHOUNDS_SHADOW_BURST: {
+    id: 245868,
+    name: "Shadow Burst",
+    icon: "ability_priest_shadowyapparition",
+  },
+  FELHOUNDS_SHADOW_STORM: {
+    id: 245888,
+    name: "Shadow Storm",
+    icon: "spell_shadow_blackplague",
+  },
+  FELHOUNDS_SIPHON_CORRUPTION: {
+    id: 244056,
+    name: "Siphon Corruption",
+    icon: "ability_deathknight_deathsiphon2",
+  },
+  FELHOUNDS_SMOULDERING: {
+    id: 251445,
+    name: "Smouldering",
+    icon: "spell_fire_flameshock",
+  },
+  FELHOUNDS_BURNING_FLASH:{
+    id: 244681,
+    name: "Burning Flash",
+    icon: "inv_misc_orb_05",
+  },
+  FELHOUNDS_BURNING_MAW: {
+    id: 254747,
+    name: "Burning Maw",
+    icon: "spell_fire_flameshock",
+  },
+  FELHOUNDS_BURNING_REMNANT: {
+    id: 245022,
+    name: "Burning Remnant",
+    icon: "spell_burningbladeshaman_blazing_radiance",
+  },
+  FELHOUNDS_CONSUMING_DETONATION:{
+    id: 244130,
+    name: "Consuming Detonation",
+    icon: "spell_priest_void-blast", // - is intentional, not a typo
+  },
+  FELHOUNDS_CONSUMING_OBLITERATION: {
+    id: 249293,
+    name: "Consuming Obliteration",
+    icon: "inv_enchant_voidsphere",
+  },
+  FELHOUNDS_CONSUMING_SPHERE: {
+    id: 254403,
+    name: "Consuming Sphere",
+    icon: "inv_enchant_essencecosmicgreater",
+  },
+  FELHOUNDS_CORRUPTING_MAW: {
+    id: 254760,
+    name: "Corrupting Maw",
+    icon: "spell_shadow_burningspirit",
+  },
+  FELHOUNDS_DECAY: {
+    id: 254383,
+    name: "Decay",
+    icon: "spell_shadow_burningspirit",
+  },
+  FELHOUNDS_DESOLATE_PATH_DAMAGE: {
+    id: 244767,
+    name: "Desolate Path",
+    icon: "ability_rhyolith_magmaflow_wave",
+  },
+  FELHOUNDS_ENFLAMED: {
+    id: 244473,
+    name: "Enflamed",
+    icon: "spell_burningsoul",
+  },
+  FELHOUNDS_ENFLAMED_BUFF: {
+    id: 248815,
+    name: "Enflamed",
+    icon: "spell_burningsoul",
+  },
+  FELHOUNDS_MOLTEN_FLARE: {
+    id: 244163,
+    name: "Molten Flare",
+    icon: "spell_fire_fireball02",
+  },
+  FELHOUNDS_SHADOWSCAR: {
+    id: 245100,
+    name: "Shadowscar",
+    icon: "spell_shadow_shadowfury",
+  },
+  FELHOUNDS_SINGED: {
+    id: 245097,
+    name: "Singed",
+    icon: "spell_fire_incinerate",
+  },
+  FELHOUNDS_SINGED_BUFF: {
+    id: 244091,
+    name: "Singed",
+    icon: "spell_fire_incinerate",
+  },
+  FELHOUNDS_SIPHONED: {
+    id: 244583,
+    name: "Siphoned",
+    icon: "warlock_curse_shadow_aura",
+  },
+  FELHOUNDS_SIPHONED_BUFF: {
+    id: 248819,
+    name: "Siphoned",
+    icon: "warlock_curse_shadow_aura",
+  },
+  FELHOUNDS_SMOULDERING_DAMAGE: {
+    id: 254384,
+    name: "Smouldering",
+    icon: "spell_fire_flameshock",
+  },
+  FELHOUNDS_SMOULDERING_BUFF: {
+    id: 251445,
+    name: "Smouldering",
+    icon: "spell_fire_flameshock",
+  },
+  
+
+
+
+
+
+
+
+
+};
