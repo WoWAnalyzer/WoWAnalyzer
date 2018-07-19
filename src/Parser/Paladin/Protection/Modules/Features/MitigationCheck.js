98b8d07c35b0d8cca4010b4e4009757712eddf3d
hpabst <hpabst@ualberta.ca>
2018-07-18 22:33:47 -0600

    Change to get boss info via findByBossId rather than having to add each boss to MitigationCheck manually.

--- src/Parser/Paladin/Protection/Modules/Features/MitigationCheck.js
+++ src/Parser/Paladin/Protection/Modules/Features/MitigationCheck.js
@@ -7,54 +7,37 @@ import { formatPercentage } from 'common/format';
 
 import StatisticBox from 'Main/StatisticBox';
 
-import Garothi from 'Raids/AntorusTheBurningThrone/GarothiWorldbreaker';
-import Felhounds from 'Raids/AntorusTheBurningThrone/FelhoundsOfSargeras';
-import Council from 'Raids/AntorusTheBurningThrone/AntoranHighCommand';
-import Eonar from 'Raids/AntorusTheBurningThrone/EonarLifebinder';
-import PKeeper from 'Raids/AntorusTheBurningThrone/PortalKeeperHasabel';
-import Imonar from 'Raids/AntorusTheBurningThrone/ImonarTheSoulhunter';
-import KG from 'Raids/AntorusTheBurningThrone/Kingaroth';
-import Vari from 'Raids/AntorusTheBurningThrone/Varimathras';
-import Coven from 'Raids/AntorusTheBurningThrone/TheCovenOfShivarra';
-import Agg from 'Raids/AntorusTheBurningThrone/Aggramar';
-import Argus from 'Raids/AntorusTheBurningThrone/ArgusTheUnmaker';
+import {findByBossId} from 'Raids';
 
 const mitigationBuffs = {'Paladin' : [SPELLS.SHIELD_OF_THE_RIGHTEOUS_BUFF.id,
                                       SPELLS.ARDENT_DEFENDER.id,
                                       SPELLS.GUARDIAN_OF_ANCIENT_KINGS.id]};
 
-const mitigationChecks = new Map([
-  [Garothi.id, Object.values(Garothi.fight.softMitigationChecks)],
-  [Felhounds.id, Object.values(Felhounds.fight.softMitigationChecks)],
-  [Council.id, Object.values(Council.fight.softMitigationChecks)],
-  [Eonar.id, Object.values(Eonar.fight.softMitigationChecks)],
-  [PKeeper.id, Object.values(PKeeper.fight.softMitigationChecks)],
-  [Imonar.id, Object.values(Imonar.fight.softMitigationChecks)],
-  [KG.id, Object.values(KG.fight.softMitigationChecks)],
-  [Vari.id, Object.values(Vari.fight.softMitigationChecks)],
-  [Coven.id, Object.values(Coven.fight.softMitigationChecks)],
-  [Agg.id, Object.values(Agg.fight.softMitigationChecks)],
-  [Argus.id, Object.values(Argus.fight.softMitigationChecks)],
-]);
-
 
 class MitigationCheck extends Analyzer{
+
   static checks;
   static buffCheck;
-  static checksPassed = 0;
-  static checksFailed = 0;
+  static checksPassed;
+  static checksFailed;
 
 
   constructor(...args) {
     super(...args);
-    const boss = this.owner.boss;
-    this.checks = mitigationChecks.get(boss.id);
-    if(typeof this.checks === "undefined"){
-      this.checks = [];
-    }
-    this.buffCheck = mitigationBuffs[this.owner.player.type];
     this.checksPassed = 0;
     this.checksFailed = 0;
+    const boss = findByBossId(this.owner.boss.id);
+    if(boss !== null && boss.fight.hasOwnProperty('softMitigationChecks')){
+      this.checks = Object.values(boss.fight.softMitigationChecks);
+      if(typeof this.checks === "undefined"){
+        this.checks = [];
+      }
+      this.buffCheck = mitigationBuffs[this.owner.player.type];
+    }else{
+      this.checks = [];
+      this.buffCheck = [];
+    }
+
   }
 
 
