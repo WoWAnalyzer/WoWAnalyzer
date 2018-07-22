ef32ac09b7f41838cbdb70c10fee977c64f42627
Martijn Hols <git@martijnhols.nl>
2018-07-22 09:40:01 +0200

    Fix eslint

--- src/Parser/Druid/Feral/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Druid/Feral/Modules/Features/AlwaysBeCasting.js
@@ -1,11 +1,10 @@
-import { formatPercentage } from 'common/format';
 import CoreAlwaysBeCasting from 'Parser/Core/Modules/AlwaysBeCasting';
 import { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
   suggestions(when) {
     // override the suggestions from CoreAlwaysBeCasting so there's never any generated, but we still get the statistic.
-    return undefined;
+    return null;
   }
   statisticOrder = STATISTIC_ORDER.CORE(1);
 }
