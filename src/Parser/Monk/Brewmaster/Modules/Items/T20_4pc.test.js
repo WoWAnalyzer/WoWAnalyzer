e515db5b4e01a48d988bf707622baf860fa17e76
J. David Smith <emallson@atlanis.net>
2018-07-22 17:12:54 -0400

    internal updates to how gift of the ox is handled

--- src/Parser/Monk/Brewmaster/Modules/Items/T20_4pc.test.js
+++ src/Parser/Monk/Brewmaster/Modules/Items/T20_4pc.test.js
@@ -3,26 +3,29 @@ import { SimpleFight } from 'tests/Parser/Brewmaster/Fixtures/SimpleFight';
 import TestCombatLogParser from 'tests/TestCombatLogParser';
 
 import StaggerFabricator from '../Core/StaggerFabricator';
+import GiftOfTheOx from '../Normalizers/GiftOfTheOx';
 import T20_4pc from './T20_4pc';
 
 describe('Brewmaster.T20_4pc', () => {
   let parser;
   let item;
   let fab;
+  let gotox;
   beforeEach(() => {
     parser = new TestCombatLogParser();
     parser.selectedCombatant.traitsBySpellId = { [SPELLS.STAGGERING_AROUND.id]: 0 };
     fab = new StaggerFabricator(parser);
+    gotox = new GiftOfTheOx(parser);
     item = new T20_4pc(parser);
     item.fab = fab;
     fab._hasTier20_4pc = true;
   });
   it('how many gift of the ox orbs were absorbed as a heal', () => {
-    parser.processEvents(SimpleFight);
+    parser.processEvents(gotox.normalize(SimpleFight));
     expect(item.orbsEaten).toBe(1);
   });
   it('how much stagger was reduced by absorbing the gift of the ox orbs', () => {
-    parser.processEvents(SimpleFight);
+    parser.processEvents(gotox.normalize(SimpleFight));
     expect(item.staggerSaved).toBe(23.450000000000003);
   });
 });
