b72cfde710208f541c0d617e1c42ece98b96e4a5
Chris Grasty <chris.grasty@gmail.com>
2018-07-25 23:17:44 -0500

    [Arcane Mage] Mana Chart
    This adds a mana chart for Arcane. I decided against using the Mana Usage Chart as i didnt feel like the DPS Throughput was all that helpful, so i went with the Mana Level Chart instead as part of playing Arcane well is ending the fight with as little mana as possible (since you should burn your mana at the end of the fight regardless of cooldown state.

--- src/Parser/Mage/Arcane/CombatLogParser.js
+++ src/Parser/Mage/Arcane/CombatLogParser.js
@@ -6,6 +6,9 @@ import Abilities from './Modules/Features/Abilities';
 import CooldownThroughputTracker from './Modules/Features/CooldownThroughputTracker';
 import Channeling from './Modules/Features/Channeling';
 
+import Mana from './Modules/ManaChart/Mana';
+import ManaValues from './Modules/ManaChart/ManaValues';
+
 import ArcaneMissiles from './Modules/Features/ArcaneMissiles';
 
 import ArcaneFamiliar from './Modules/Features/ArcaneFamiliar';
@@ -25,6 +28,8 @@ class CombatLogParser extends CoreCombatLogParser {
     cooldownThroughputTracker: CooldownThroughputTracker,
     damageDone: [DamageDone, { showStatistic: true }],
     channeling: Channeling,
+    mana: Mana,
+    manaValues: ManaValues,
     cancelledCasts: CancelledCasts,
     arcaneMissiles: ArcaneMissiles,
 
