235d2222a7c187abca14924b38294e1eef9d83a4
yajinni <yajinni@gmail.com>
2018-07-31 21:08:06 -0400

    Added notes as to why some sugestions are setup the way they are.

--- src/Parser/DemonHunter/Vengeance/Modules/Spells/SoulCleaveSoulsConsumed.js
+++ src/Parser/DemonHunter/Vengeance/Modules/Spells/SoulCleaveSoulsConsumed.js
@@ -11,7 +11,12 @@ class SoulCleaveSoulsConsumed extends Analyzer {
   static dependencies = {
     soulFragmentsConsume: SoulFragmentsConsume,
   };
-
+  /* Feed The Demon talent is taken in defensive builds. In those cases you want to generate and consume souls as quickly
+   as possible. So how you consume your souls down matter. If you dont take that talent your taking a more balanced
+   build meaning you want to consume souls in a way that boosts your dps. That means feeding the souls into spirit
+   bomb as efficiently as possible (cast at 4+ souls) for a dps boost and have soul cleave absorb souls as little as
+   possible since it provides no extra dps.
+  */
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.SPIRIT_BOMB_TALENT.id) && !this.selectedCombatant.hasTalent(SPELLS.FEED_THE_DEMON_TALENT.id);
