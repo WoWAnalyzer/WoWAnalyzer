240b0d156179d62c8f3a005fdc2f8ccf2468af7d
yajinni <yajinni@gmail.com>
2018-08-01 18:49:02 -0400

    new line fix

--- src/Parser/Warrior/Fury/Modules/Talents/FuriousSlashTimesByStacks.js
+++ src/Parser/Warrior/Fury/Modules/Talents/FuriousSlashTimesByStacks.js
@@ -2,28 +2,28 @@ import Analyzer from 'Parser/Core/Analyzer';
 import SPELLS from 'common/SPELLS';
 import HIT_TYPES from 'Parser/Core/HIT_TYPES';
 
-/* 
+/*
   furiousSlashTimesByStacks() returns an array with the durations of each FS buff stack
 */
 
 const MAX_FURIOUS_SLASH_STACKS = 3;
 
 class FuriousSlashTimesByStacks extends Analyzer {
-	
+
 	furiousSlashStacks = [];
 	lastFuriousSlashStack = 0;
 	lastFuriousSlashUpdate = this.owner.fight.start_time;
-	
+
 	constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.FURIOUS_SLASH_TALENT.id);
 	this.furiousSlashStacks = Array.from({length: MAX_FURIOUS_SLASH_STACKS + 1}, x => []);
     }
-  
+
   	get furiousSlashTimesByStacks() {
 		return this.furiousSlashStacks;
 	}
-	
+
 	on_byPlayer_damage(event) {
 		const spellId = event.ability.guid;
 		if(spellId !== SPELLS.FURIOUS_SLASH_TALENT.id || event.hit_type === HIT_TYPES.DODGE || event.hit_type === HIT_TYPES.DODGE)
@@ -53,7 +53,7 @@ class FuriousSlashTimesByStacks extends Analyzer {
 		if(spellId !== SPELLS.FURIOUS_SLASH_TALENT_BUFF.id){
 			return;
 		}
-		
+
 		this.furiousSlashStacks[this.lastFuriousSlashStack].push(event.timestamp - this.lastFuriousSlashUpdate);
 		this.lastFuriousSlashUpdate = event.timestamp;
 		this.lastFuriousSlashStack = 0;
