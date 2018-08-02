4fc3e56138bdb6a11e46bbc694c73ea8030e0627
Martijn Hols <git@martijnhols.nl>
2018-08-02 23:41:50 +0200

    Refactor Gemhide

--- src/Parser/Core/Modules/Spells/BFA/AzeriteTraits/Gemhide.js
+++ src/Parser/Core/Modules/Spells/BFA/AzeriteTraits/Gemhide.js
@@ -1,53 +1,54 @@
 import React from 'react';
-import SpellIcon from 'common/SpellIcon';
-import { formatNumber, formatPercentage } from 'common/format';
-import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
-import Analyzer from 'Parser/Core/Analyzer';
-import { calculateAzeriteEffects } from 'common/stats';
-import SPELLS from 'common/SPELLS';
 
-export function gemhideStats(combatant) {
-  if (!combatant.hasTrait(SPELLS.GEMHIDE.id)) {
-    return null;
-  }
-  let armor = 0;
-  let avoidance = 0;
-  for (const rank of combatant.traitsBySpellId[SPELLS.GEMHIDE.id]) {
-    const [av, ar] = calculateAzeriteEffects(SPELLS.GEMHIDE.id, rank);
-    armor += ar;
-    avoidance += av;
-  }
-  return { armor, avoidance };
-}
+import SpellIcon from 'common/SpellIcon';
+import SPELLS from 'common/SPELLS';
+import { formatNumber, formatPercentage } from 'common/format';
+import { calculateAzeriteEffects } from 'common/stats';
+import Analyzer from 'Parser/Core/Analyzer';
+import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
+
+const gemhideStats = traits => Object.values(traits).reduce((obj, rank) => {
+  const [avoidance, armor] = calculateAzeriteEffects(SPELLS.GEMHIDE.id, rank);
+  obj.avoidance += avoidance;
+  obj.armor += armor;
+  return obj;
+}, {
+  avoidance: 0,
+  armor: 0,
+});
 
 export const STAT_TRACKER = {
-  armor: (combatant) => gemhideStats(combatant).armor,
-  avoidance: (combatant) => gemhideStats(combatant).avoidance,
+  armor: combatant => gemhideStats(combatant.traitsBySpellId[SPELLS.GEMHIDE.id]).armor,
+  avoidance: combatant => gemhideStats(combatant.traitsBySpellId[SPELLS.GEMHIDE.id]).avoidance,
 };
 
+/**
+ * Gemhide
+ * When dealt damage greater than 10% of your maximum health, gain 95 Avoidance and 475 Armor for 10 sec.
+ *
+ * Example report: https://www.warcraftlogs.com/reports/vyfNxYzcHr8mLXZ6#fight=12&type=summary&source=18
+ */
 class Gemhide extends Analyzer {
   armor = 0;
   avoidance = 0;
   constructor(...args) {
     super(...args);
-    const resp = gemhideStats(this.selectedCombatant);
-    if (resp === null) {
-      this.active = false;
+    this.active = this.selectedCombatant.hasTrait(SPELLS.GEMHIDE.id);
+    if (!this.active) {
       return;
     }
-    const { armor, avoidance } = resp;
-    this.armor += armor;
-    this.avoidance += avoidance;
+
+    const { armor, avoidance } = gemhideStats(this.selectedCombatant.traitsBySpellId[SPELLS.GEMHIDE.id]);
+    this.armor = armor;
+    this.avoidance = avoidance;
   }
 
   get uptime() {
     return this.selectedCombatant.getBuffUptime(SPELLS.GEMHIDE_BUFF.id) / this.owner.fightDuration;
   }
-
   get avgArmor() {
     return this.uptime * this.armor;
   }
-
   get avgAvoidance() {
     return this.uptime * this.avoidance;
   }
@@ -62,6 +63,7 @@ class Gemhide extends Analyzer {
     if (event.sourceIsFriendly) {
       return;
     }
+
     this._totalHits += 1;
     this._hitsMitigated += this.selectedCombatant.hasBuff(SPELLS.GEMHIDE_BUFF.id) ? 1 : 0;
   }
@@ -77,7 +79,7 @@ class Gemhide extends Analyzer {
             {formatNumber(this.avgAvoidance)} Avoidance
           </React.Fragment>
         )}
-        label={'Avg. Stats from Gemhide'}
+        label="Avg. Stats from Gemhide"
         tooltip={`Gemhide grants <b>${this.armor} Armor</b> and <b>${this.avoidance} Avoidance</b> while active.<br/>It was active for <b>${formatPercentage(this.uptime)}%</b> of the fight, mitigating <b>${formatPercentage(this.pctHitsMitigated)}%</b> of incoming hits.`}
       />
     );
