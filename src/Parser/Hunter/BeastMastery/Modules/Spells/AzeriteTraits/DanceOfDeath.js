ecb0b31263ad75935a82dda29064acc94ea4dbd6
hpabst <hpabst@ualberta.ca>
2018-08-03 07:33:08 -0700

    Use TraitStatisticBox rather than StatisticBox. Show uptime rather than number of procs on statistic box.

--- src/Parser/Hunter/BeastMastery/Modules/Spells/AzeriteTraits/DanceOfDeath.js
+++ src/Parser/Hunter/BeastMastery/Modules/Spells/AzeriteTraits/DanceOfDeath.js
@@ -1,45 +1,41 @@
 import React from 'react';
-import SpellIcon from 'common/SpellIcon';
 import Analyzer from 'Parser/Core/Analyzer';
 import {formatNumber, formatPercentage} from 'common/format';
 import {calculateAzeriteEffects} from 'common/stats';
-import StatisticBox from 'Interface/Others/StatisticBox';
+import TraitStatisticBox, { STATISTIC_ORDER } from 'Interface/Others/TraitStatisticBox';
 import SPELLS from 'common/SPELLS/index';
 
 
-export function danceOfDeathStats(combatant){
-  if(!combatant.hasTrait(SPELLS.DANCE_OF_DEATH.id)){
-    return null;
-  }
-  let agility = 0;
-  for(const rank of combatant.traitsBySpellId[SPELLS.DANCE_OF_DEATH.id]){
-    const [agi] = calculateAzeriteEffects(SPELLS.DANCE_OF_DEATH.id, rank);
-    agility += agi;
-  }
-  return {agility};
-}
+const danceOfDeathStats = traits => Object.values(traits).reduce((obj, rank) => {
+  const [agility] = calculateAzeriteEffects(SPELLS.DANCE_OF_DEATH.id, rank);
+  obj.agility += agility;
+  return obj;
+}, {
+  agility: 0,
+});
 
 export const STAT_TRACKER = {
-  agility: (combatant) => danceOfDeathStats(combatant).agility,
+  agility: combatant => danceOfDeathStats(combatant.traitsBySpelld[SPELLS.DANCE_OF_DEATH.id]).agility,
 };
 
 /**
  * Barbed Shot has a chance equal to your critical strike chance to grant you 314 agility
  * for 8 sec.
+ *
+ * Example report: https://www.warcraftlogs.com/reports/Nt9KGvDncPmVyjpd#boss=-2&difficulty=0&type=summary&source=9
  */
 class DanceOfDeath extends Analyzer{
   agility = 0;
 
   constructor(...args) {
     super(...args);
-    const response = danceOfDeathStats(this.selectedCombatant);
-    if (response === null) {
-      this.active = false;
+    this.active = this.selectedCombatant.hasTrait(SPELLS.DANCE_OF_DEATH.id);
+    if(!this.active){
       return;
     }
-    this.agility += response.agility;
+    const {agility} = danceOfDeathStats(this.selectedCombatant.traitsBySpellId[SPELLS.DANCE_OF_DEATH.id]);
+    this.agility = agility;
   }
-
 
   get uptime(){
     return this.selectedCombatant.getBuffUptime(SPELLS.DANCE_OF_DEATH_BUFF.id) / this.owner.fightDuration;
@@ -55,12 +51,13 @@ class DanceOfDeath extends Analyzer{
 
   statistic(){
     return (
-      <StatisticBox
-        icon={<SpellIcon id={SPELLS.DANCE_OF_DEATH.id} />}
+      <TraitStatisticBox
+        position={STATISTIC_ORDER.OPTIONAL()}
+        trait={SPELLS.DANCE_OF_DEATH.id}
         value={(
           <React.Fragment>
-            {formatNumber(this.avgAgility)} Agility <br />
-            {formatNumber(this.numProcs)} Procs
+            {formatNumber(this.avgAgility)} Average Agility <br />
+            {formatPercentage(this.uptime)}% Uptime
           </React.Fragment>
         )}
         label={"Dance of Death"}
