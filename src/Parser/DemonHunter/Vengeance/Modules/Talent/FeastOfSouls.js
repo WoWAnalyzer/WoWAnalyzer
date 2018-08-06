c8b44391433ad92e0b3f1dac062762e612b8db3a
yajinni <yajinni@gmail.com>
2018-08-06 15:44:51 -0400

    Feast of souls talent

---
+++ src/Parser/DemonHunter/Vengeance/Modules/Talent/FeastOfSouls.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Analyzer from 'Parser/Core/Analyzer';
+import SPELLS from 'common/SPELLS/index';
+import SpellIcon from 'common/SpellIcon';
+import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
+import { formatNumber } from 'common/format';
+
+class FeastOfSouls extends Analyzer {
+
+  heal = 0;
+
+  constructor(...args) {
+    super(...args);
+    this.active = this.selectedCombatant.hasTalent(SPELLS.FEAST_OF_SOULS_TALENT.id);
+  }
+
+  on_byPlayer_heal(event) {
+    const spellID = event.ability.guid;
+    if (spellID !== SPELLS.FEAST_OF_SOULS_HEAL.id) {
+      return;
+    }
+    this.heal += event.amount;
+  }
+
+  statistic() {
+    return (
+      <StatisticBox
+        icon={<SpellIcon id={SPELLS.FEAST_OF_SOULS_TALENT.id} />}
+        value={`${this.owner.formatItemHealingDone(this.heal)}`}
+        label="Feast of Souls"
+        tooltip={`This shows the extra hps that the talent provides.<br/>
+                  <b>Total extra healing:</b> ${formatNumber(this.heal)}`}
+      />
+    );
+  }
+  statisticOrder = STATISTIC_ORDER.CORE(7);
+}
+
+export default FeastOfSouls;
