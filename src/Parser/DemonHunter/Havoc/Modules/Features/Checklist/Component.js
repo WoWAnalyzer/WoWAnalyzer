75b6c8c75ef1fcac08a1cd0e838b4989f7159c0b
Anatta336 <sdriver@gmx.co.uk>
2018-08-11 01:43:02 +0100

    fix checklist crash related to Immolation Aura

--- src/Parser/DemonHunter/Havoc/Modules/Features/Checklist/Component.js
+++ src/Parser/DemonHunter/Havoc/Modules/Features/Checklist/Component.js
@@ -41,7 +41,7 @@ class HavocDemonHunterChecklist extends React.PureComponent {
         >
           <AbilityRequirement spell={SPELLS.FEL_RUSH.id} />
           {combatant.hasTalent(SPELLS.FELBLADE_TALENT.id) && <AbilityRequirement spell={SPELLS.FELBLADE_TALENT.id} />}
-          {combatant.hasTalent(SPELLS.IMMOLATION_AURA_TALENT.id) && <AbilityRequirement spell={SPELLS.IMMOLATION_AURA.id} />}
+          {combatant.hasTalent(SPELLS.IMMOLATION_AURA_TALENT.id) && <AbilityRequirement spell={SPELLS.IMMOLATION_AURA_TALENT.id} />}
         </Rule>
 
         <Rule
