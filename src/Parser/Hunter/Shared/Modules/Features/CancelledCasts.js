cd163b974001e095982d9d40e31b5b24a0ff72e5
Laust <lafa16ac@student.cbs.dk>
2018-08-13 17:00:08 +0200

    cancelledCast update

--- src/Parser/Hunter/Shared/Modules/Features/CancelledCasts.js
+++ src/Parser/Hunter/Shared/Modules/Features/CancelledCasts.js
@@ -17,9 +17,6 @@ class CancelledCasts extends CoreCancelledCasts {
   static IGNORED_ABILITIES = [
     //Include the spells that you do not want to be tracked and spells that are castable while casting
     SPELLS.EXPLOSIVE_SHOT_DAMAGE.id,
-    SPELLS.WINDBURST_MOVEMENT_SPEED.id,
-    SPELLS.CYCLONIC_BURST_IMPACT_TRAIT.id,
-    SPELLS.CYCLONIC_BURST_TRAIT.id,
     SPELLS.GOLGANNETHS_VITALITY_RAVAGING_STORM.id,
   ];
 
