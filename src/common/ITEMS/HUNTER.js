79ce09b39d39227d2be20262f512adba745286fb
Laust <lafa16ac@student.cbs.dk>
2018-08-13 16:00:02 +0200

    repopulate items file in hope of fixing eslint error

--- src/common/ITEMS/HUNTER.js
+++ src/common/ITEMS/HUNTER.js
@@ -1,3 +1,126 @@
+import ITEM_QUALITIES from '../ITEM_QUALITIES';
+
 //TODO - Repopulate with BFA items or just delete file?
 export default {
+  // Shared legendaries
+  THE_SHADOW_HUNTERS_VOODOO_MASK: {
+    id: 137064,
+    name: 'The Shadow Hunter\'s Voodoo Mask',
+    icon: 'inv_helmet_116',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  SOUL_OF_THE_HUNTMASTER: {
+    id: 151641,
+    name: 'Soul of the Huntmaster',
+    icon: 'inv_70_quest_ring8a',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  CALL_OF_THE_WILD: {
+    id: 137101,
+    name: 'Call of the Wild',
+    icon: 'inv_bracer_36b',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
+  // Beast Mastery legendaries
+  ROAR_OF_THE_SEVEN_LIONS: {
+    id: 137080,
+    name: 'Roar of the Seven Lions',
+    icon: 'inv_belt_plate_pvpdeathknight_c_02',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  QAPLA_EREDUN_WAR_ORDER: {
+    id: 137227,
+    name: 'Qa\'pla, Eredun War Order',
+    icon: 'inv_boot_mail_raidhunter_n_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  THE_APEX_PREDATORS_CLAW: {
+    id: 137382,
+    name: 'The Apex Predator\'s Claw',
+    icon: 'inv_jewelry_ring_100',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  THE_MANTLE_OF_COMMAND: {
+    id: 144326,
+    name: 'The Mantle of Command',
+    icon: 'inv_shoulder_mail_raidshaman_m_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  PARSELS_TONGUE: {
+    id: 151805,
+    name: 'Parsel\'s Tongue',
+    icon: 'inv_chest_mail_dungeonmail_c_03',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
+  // Marksmanship legendaries
+  MAGNETIZED_BLASTING_CAP_LAUNCHER: {
+    id: 141353,
+    name: 'Magnetized Blasting Cap Launcher',
+    icon: 'inv_bracer_mail_raidhunter_j_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ULLRS_FEATHER_SNOWSHOES: {
+    id: 137033,
+    name: 'Ullr\'s Feather Snowshoes',
+    icon: 'inv_boots_mail_08',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  ZEVRIMS_HUNGER: {
+    id: 137055,
+    name: 'Zevrim\'s Hunger',
+    icon: 'inv_jewelry_ring_36',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  WAR_BELT_OF_THE_SENTINEL_ARMY: {
+    id: 137081,
+    name: 'War Belt of the Sentinel Army',
+    icon: 'inv_belt_66green',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  MKII_GYROSCOPIC_STABILIZER: {
+    id: 144303,
+    name: 'MKII Gyroscopic Stabilizer',
+    icon: 'inv_glove_mail_raidshamanmythic_o_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  CELERITY_OF_THE_WINDRUNNERS: {
+    id: 151803,
+    name: 'Celerity of the Windrunners',
+    icon: 'inv_cape_430dungeon_c_04',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+
+  // Survival legendaries
+  NESINGWARYS_TRAPPING_TREADS: {
+    id: 137034,
+    name: 'Nesingwary\'s Trapping Treads',
+    icon: 'inv_boots_mail_panda_b_02',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  FRIZZOS_FINGERTRAP: {
+    id: 137043,
+    name: 'Frizzo\'s Fingertrap',
+    icon: 'inv_archaeology_ogres_sorcerer_king_toe_ring',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  HELBRINE_ROPE_OF_THE_MIST_MARAUDER: {
+    id: 137082,
+    name: 'Helbrine, Rope of the Mist Marauder',
+    icon: 'inv_belt_mail_draenordungeon_c_01',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  BUTCHERS_BONE_APRON: {
+    id: 144361,
+    name: 'Butcher\'s Bone Apron',
+    icon: 'inv_chest_chain_15',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
+  UNSEEN_PREDATORS_CLOAK: {
+    id: 151807,
+    name: 'Unseen Predator\'s Cloak',
+    icon: 'inv_cape_leather_raiddemonhunter_r_01_long',
+    quality: ITEM_QUALITIES.LEGENDARY,
+  },
 };
