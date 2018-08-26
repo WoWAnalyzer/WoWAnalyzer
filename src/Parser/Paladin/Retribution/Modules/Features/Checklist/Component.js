8e4803c49624fa3b2bc512c29debbcc0674a8598
Juko8 <ulrichenevoldsen@hotmail.com>
2018-08-26 23:12:32 +0200

    [Ret] Initial work on updating retribution checklist

---
+++ src/Parser/Paladin/Retribution/Modules/Features/Checklist/Component.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import Checklist from 'Parser/Core/Modules/Features/Checklist2';
+import Rule from 'Parser/Core/Modules/Features/Checklist2/Rule';
+import Requirement from 'Parser/Core/Modules/Features/Checklist2/Requirement';
+import PreparationRule from 'Parser/Core/Modules/Features/Checklist2/PreparationRule';
+import GenericCastEfficiencyRequirement from 'Parser/Core/Modules/Features/Checklist2/GenericCastEfficiencyRequirement';
+
+class RetributionPaladinChecklist extends React.PureComponent {
+  static propTypes = {
+    castEfficiency: PropTypes.object.isRequired,
+    combatant: PropTypes.shape({
+      hasTalent: PropTypes.func.isRequired,
+      hasTrinket: PropTypes.func.isRequired,
+    }).isRequired,
+    thresholds: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { combatant, castEfficiency, thresholds } = this.props;
+
+    const AbilityRequirement = props => (
+      <GenericCastEfficiencyRequirement
+        castEfficiency={castEfficiency.getCastEfficiencyForSpellId(props.spell)}
+        {...props}
+      />
+    );
+  }
+}
+
+export default RetributionPaladinChecklist;
