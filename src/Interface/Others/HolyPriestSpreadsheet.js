ef6fdd2f17d875ff927638beadba392fdde4fb90
James Moats <tjmoats@demark.com>
2018-09-07 08:19:17 -0700

    stubbing out spreadsheet

--- src/Interface/Others/HolyPriestSpreadsheet.js
+++ src/Interface/Others/HolyPriestSpreadsheet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SPELLS from 'common/SPELLS';
 
 class MonkSpreadsheet extends React.Component {
   static propTypes = {
@@ -47,6 +48,22 @@ class MonkSpreadsheet extends React.Component {
                 <td>Versatility Rating</td>
                 <td>{parser.selectedCombatant._combatantInfo.versatilityHealingDone}</td>
               </tr>
+              <tr>
+                <td>Fight Length</td>
+                <td>{Math.floor(parser.fightDuration / 1000)}</td>
+              </tr>
+              <tr>
+                <td>Total Healing from Azerite Traits</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Renews refreshed by Enduring Renewal</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Renews from benediction</td>
+                <td></td>
+              </tr>
             </tbody>
           </table>
           <table style={styles.table}>
@@ -55,6 +72,121 @@ class MonkSpreadsheet extends React.Component {
                 <td>Spell Name</td>
                 <td>Overheal %</td>
                 <td>CPM</td>
+                <td>Casting Efficiency %</td>
+                <td>Average Targets / Cast</td>
+              </tr>
+              <tr>
+                <td>Heal</td>
+                <td>{overhealingSpell(SPELLS.GREATER_HEAL.id)}</td>
+                <td>{cpm(SPELLS.GREATER_HEAL.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Flash Heal</td>
+                <td>{overhealingSpell(SPELLS.FLASH_HEAL.id)}</td>
+                <td>{cpm(SPELLS.FLASH_HEAL.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Renew</td>
+                <td>{overhealingSpell(SPELLS.RENEW.id)}</td>
+                <td>{cpm(SPELLS.RENEW.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Prayer of Healing</td>
+                <td>{overhealingSpell(SPELLS.PRAYER_OF_HEALING.id)}</td>
+                <td>{cpm(SPELLS.PRAYER_OF_HEALING.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Prayer of Mending</td>
+                <td>{overhealingSpell(SPELLS.PRAYER_OF_MENDING_HEAL.id)}</td>
+                <td>{cpm(SPELLS.PRAYER_OF_MENDING_CAST.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Holy Word: Serenity</td>
+                <td>{overhealingSpell(SPELLS.HOLY_WORD_SERENITY.id)}</td>
+                <td>{cpm(SPELLS.HOLY_WORD_SERENITY.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Holy Word: Sanctify</td>
+                <td>{overhealingSpell(SPELLS.HOLY_WORD_SANCTIFY.id)}</td>
+                <td>{(getAbility(SPELLS.HOLY_WORD_SANCTIFY.id).healingHits / Math.floor(parser.fightDuration / 1000 / 60) || 0).toFixed(2)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Guardian Spirit</td>
+                <td>{overhealingSpell(SPELLS.GUARDIAN_SPIRIT.id)}</td>
+                <td>{cpm(SPELLS.GUARDIAN_SPIRIT.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Divine Hymn</td>
+                <td>{overhealingSpell(SPELLS.DIVINE_HYMN_HEAL.id)}</td>
+                <td>{cpm(SPELLS.DIVINE_HYMN_CAST.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Desperate Prayer</td>
+                <td>{overhealingSpell(SPELLS.DESPERATE_PRAYER.id)}</td>
+                <td>{cpm(SPELLS.DESPERATE_PRAYER.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td>Cosmic Ripple</td>
+                <td>{overhealingSpell(SPELLS.CHI_WAVE_TALENT.id)}</td>
+                <td>N/A</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Binding Heal</td>
+                <td>{overhealingSpell(SPELLS.BINDING_HEAL_TALENT.id)}</td>
+                <td>{cpm(SPELLS.BINDING_HEAL_TALENT.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Circle of Healing</td>
+                <td>{overhealingSpell(SPELLS.CIRCLE_OF_HEALING_TALENT.id)}</td>
+                <td>{cpm(SPELLS.CIRCLE_OF_HEALING_TALENT.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Divine Star</td>
+                <td>{overhealingSpell(SPELLS.DIVINE_STAR_TALENT.id)}</td>
+                <td>{cpm(SPELLS.DIVINE_STAR_TALENT.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Halo</td>
+                <td>{overhealingSpell(SPELLS.HALO_TALENT.id)}</td>
+                <td>{cpm(SPELLS.HALO_TALENT.id)}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Holy Word: Salvation</td>
+                <td>{overhealingSpell(SPELLS.HOLY_WORD_SALVATION_TALENT.id)}</td>
+                <td>{cpm(SPELLS.HOLY_WORD_SALVATION_TALENT.id)}</td>
+                <td></td>
+                <td></td>
               </tr>
             </tbody>
           </table>
