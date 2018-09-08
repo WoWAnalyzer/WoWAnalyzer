d7c9ae0cebaa7e029a7441f3867b6a29a4c994ce
Laust <lafa16ac@student.cbs.dk>
2018-09-08 10:17:45 +0200

    remove quickNav

--- src/Parser/Core/CombatLogParser.js
+++ src/Parser/Core/CombatLogParser.js
@@ -135,8 +135,6 @@ import FangsOfIntertwinedEssence from './Modules/Items/BFA/Dungeons/FangsOfInter
 import BalefireBranch from './Modules/Items/BFA/Dungeons/BalefireBranch';
 import ConchofDarkWhispers from './Modules/Items/BFA/Dungeons/ConchofDarkWhispers';
 import GalecallersBoon from './Modules/Items/BFA/Dungeons/GalecallersBoon';
-//Enchants
-import QuickNavigation from './Modules/Items/BFA/Enchants/QuickNavigation';
 // Crafted
 import DarkmoonDeckTides from './Modules/Items/BFA/Crafted/DarkmoonDeckTides';
 // Azerite Traits
@@ -290,8 +288,6 @@ class CombatLogParser {
     galecallersBoon: GalecallersBoon,
     // Crafted
     darkmoonDeckTides: DarkmoonDeckTides,
-    // Enchants
-    quickNavigation: QuickNavigation,
     // Azerite Traits
     gemhide: Gemhide,
     // Uldir
