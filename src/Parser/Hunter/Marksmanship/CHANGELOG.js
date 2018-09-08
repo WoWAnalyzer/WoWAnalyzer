961206ad6a8356a7a36acaaaf4f746135dd79836
Laust <lafa16ac@student.cbs.dk>
2018-09-08 10:46:30 +0200

    steady focus gcd work

--- src/Parser/Hunter/Marksmanship/CHANGELOG.js
+++ src/Parser/Hunter/Marksmanship/CHANGELOG.js
@@ -8,13 +8,18 @@ import { Blazballs, JLassie82, Putro } from 'CONTRIBUTORS';
 
 export default [
   {
+    date: new Date('2018-08-06'),
+    changes: <React.Fragment>Adds initial tracking for <SpellLink id={SPELLS.STEADY_FOCUS_TALENT.id} /> to ensure the GCD is accurate in the analyzer.</React.Fragment>,
+    contributors: [Putro],
+  },
+  {
     date: new Date('2018-08-12'),
     changes: 'Removed all legendaries and tier gear in preparation for Battle for Azeroth launch',
     contributors: [Putro],
   },
   {
     date: new Date('2018-08-06'),
-    changes: <React.Fragment>Created a <SpellLink id={SPELLS.CAREFUL_AIM_TALENT.id} /> module, adds buff indicators to relevant spells in the timeline, adjusted placement of statistic boxes and added example logs to everything BFA related.</React.Fragment>,
+    changes: <React.Fragment>Adds buff indicators to relevant spells in the timeline, adjusted placement of statistic boxes and added example logs to everything BFA related.</React.Fragment>,
     contributors: [Putro],
   },
   {
