b63aede4ea7fd208f5e2ff087118a0560440d9ae
domainzero <domain@mythicpl.us>
2018-09-10 15:49:54 -0600

    Added context to prepotion checker
    Prepotion checker will now suggest the proper potions based on class/spec.

--- src/Parser/Core/Modules/Items/PrePotion.js
+++ src/Parser/Core/Modules/Items/PrePotion.js
@@ -19,6 +19,43 @@ const HEALER_SPECS = [
   SPECS.MISTWEAVER_MONK.id,
   SPECS.RESTORATION_SHAMAN.id,
 ];
+// these suggestions are all based on Icy Veins guide recommendations, i.e. which potion to use in which situation.
+// most guides recommend to use Battle Potion of Primary Stat, but I have broken out the class/spec combos whose guides
+// recommend to use Rising Death or Bursting Blood in certain situations.
+const AGI_SPECS = [
+  SPECS.GUARDIAN_DRUID.id,
+  SPECS.FERAL_DRUID.id,
+  SPECS.BEAST_MASTERY_HUNTER.id,
+  SPECS.MARKSMANSHIP_HUNTER.id,
+  SPECS.ASSASSINATION_ROGUE.id,
+  SPECS.OUTLAW_ROGUE.id,
+  SPECS.SUBTLETY_ROGUE.id,
+  SPECS.ENHANCEMENT_SHAMAN.id,
+  SPECS.BREWMASTER_MONK.id,
+  SPECS.WINDWALKER_MONK.id,
+  SPECS.VENGEANCE_DEMON_HUNTER.id,
+  SPECS.HAVOC_DEMON_HUNTER.id,
+];
+
+const STR_SPECS = [
+  SPECS.PROTECTION_PALADIN.id,
+  SPECS.PROTECTION_WARRIOR.id,
+  SPECS.BLOOD_DEATH_KNIGHT.id,
+  SPECS.RETRIBUTION_PALADIN.id,
+];
+
+const BURSTING_BLOOD = [
+  SPECS.SURVIVAL_HUNTER.id,
+  SPECS.ARMS_WARRIOR.id,
+  SPECS.FURY_WARRIOR.id,
+  SPECS.FROST_DEATH_KNIGHT.id,
+  SPECS.UNHOLY_DEATH_KNIGHT.id,
+];
+
+const RISING_DEATH = [
+  SPECS.SHADOW_PRIEST.id,
+  SPECS.FROST_MAGE.id,
+];
 
 const PRE_POTIONS = [
   SPELLS.BATTLE_POTION_OF_INTELLECT.id,
@@ -94,37 +131,90 @@ class PrePotion extends Analyzer {
       style: 'boolean',
     };
   }
-
   suggestions(when) {
     when(this.prePotionSuggestionThresholds)
       .addSuggestion((suggest) => {
-        return suggest(<React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={ITEMS.BATTLE_POTION_OF_INTELLECT.id} /> can be very effective (even for healers), especially during shorter encounters.</React.Fragment>)
-          .icon(ITEMS.BATTLE_POTION_OF_INTELLECT.icon)
+        let potionId;
+        let potionIcon;
+        let suggestionText;
+        const agiSpecs = AGI_SPECS.includes(this.selectedCombatant.specId);
+        const strSpecs = STR_SPECS.includes(this.selectedCombatant.specId);
+        const risingDeath = RISING_DEATH.includes(this.selectedCombatant.specId);
+        const burstingBlood = BURSTING_BLOOD.includes(this.selectedCombatant.specId);
+        if (agiSpecs) {
+          potionId = ITEMS.BATTLE_POTION_OF_AGILITY.id;
+          potionIcon = ITEMS.BATTLE_POTION_OF_AGILITY.icon;
+          suggestionText = <React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={potionId} /> can be very effective, especially during shorter encounters.</React.Fragment>;
+        } else if (strSpecs) {
+            potionId = ITEMS.BATTLE_POTION_OF_STRENGTH.id;
+            potionIcon = ITEMS.BATTLE_POTION_OF_STRENGTH.icon;
+            suggestionText = <React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={potionId} /> can be very effective, especially during shorter encounters.</React.Fragment>;
+        } else if (risingDeath) {
+            potionId = ITEMS.POTION_OF_RISING_DEATH.id;
+            potionIcon = ITEMS.POTION_OF_RISING_DEATH.icon;
+            suggestionText = <React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={potionId} /> can be very effective in a pure single target situation, especially during shorter encounters. In a multi-target encounter, a potion such as <ItemLink id={ITEMS.BATTLE_POTION_OF_STRENGTH.id} /> could be very effective.</React.Fragment>;
+        } else if (burstingBlood) {
+            potionId = ITEMS.POTION_OF_BURSTING_BLOOD.id;
+            potionIcon = ITEMS.POTION_OF_BURSTING_BLOOD.icon;
+            suggestionText = <React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={potionId} /> can be very effective in a pure single target situation, especially during shorter encounters. In a multi-target encounter, a potion such as <ItemLink id={ITEMS.BATTLE_POTION_OF_STRENGTH.id} /> could be very effective.</React.Fragment>;
+        }
+        else {
+             potionId = ITEMS.BATTLE_POTION_OF_INTELLECT.id;
+             potionIcon = ITEMS.BATTLE_POTION_OF_INTELLECT.icon;
+             suggestionText = <React.Fragment>You did not use a potion before combat. Using a potion before combat allows you the benefit of two potions in a single fight. A potion such as <ItemLink id={potionId} /> can be very effective, especially during shorter encounters.</React.Fragment>;
+        }
+        return suggest(suggestionText)
+          .icon(potionIcon)
           .staticImportance(SUGGESTION_IMPORTANCE.MINOR);
       });
+
+
     when(this.secondPotionSuggestionThresholds)
       .addSuggestion((suggest) => {
         let suggestionText;
         let importance;
+        let potionIcon;
+        const agiSpecs = AGI_SPECS.includes(this.selectedCombatant.specId);
+        const strSpecs = STR_SPECS.includes(this.selectedCombatant.specId);
+        const risingDeath = RISING_DEATH.includes(this.selectedCombatant.specId);
+        const burstingBlood = BURSTING_BLOOD.includes(this.selectedCombatant.specId);
         // Only healer specs would use a mana potion all other specs either don't use mana as a primary resource (such as bears)
         // or have another method to regen mana, this fixes an issue with Guardian where they shift out of bear form and cast a
         // spell but mana is not their primary resource and should not use a mana potion.
         const healerSpec = HEALER_SPECS.includes(this.selectedCombatant.specId);
-        if (!healerSpec) {
-          suggestionText = <React.Fragment>You forgot to use a potion during combat. By using a potion during combat such as <ItemLink id={ITEMS.BATTLE_POTION_OF_INTELLECT.id} /> you can increase your DPS (especially if lined up with damage cooldowns) and/or suvivability during a fight.</React.Fragment>;
+        if (agiSpecs) {
+          suggestionText = <React.Fragment>You forgot to use a potion during combat. By using a potion during combat such as <ItemLink id={ITEMS.BATTLE_POTION_OF_AGILITY.id} /> you can increase your DPS (especially if lined up with damage cooldowns) and/or suvivability during a fight.</React.Fragment>;
+          // Change the icon to fit the description
+          potionIcon = ITEMS.BATTLE_POTION_OF_AGILITY.icon;
+          importance = SUGGESTION_IMPORTANCE.MINOR;
+        } else if (strSpecs) {
+          suggestionText = <React.Fragment>You forgot to use a potion during combat. By using a potion during combat such as <ItemLink id={ITEMS.BATTLE_POTION_OF_STRENGTH.id} /> you can increase your DPS (especially if lined up with damage cooldowns) and/or suvivability during a fight.</React.Fragment>;
+          // Change the icon to fit the description
+          potionIcon = ITEMS.BATTLE_POTION_OF_STRENGTH.icon;
+          importance = SUGGESTION_IMPORTANCE.MINOR;
+        } else if (risingDeath) {
+          suggestionText = <React.Fragment>You forgot to use a potion during combat. By using a potion during combat such as <ItemLink id={ITEMS.POTION_OF_RISING_DEATH.id} /> you can increase your DPS (especially if lined up with damage cooldowns). If the encounter has become multi-target at the time you would use a potion, it may be beneficial to use <ItemLink id={ITEMS.BATTLE_POTION_OF_INTELLECT.id} /> instead.</React.Fragment>;
+          potionIcon = ITEMS.POTION_OF_RISING_DEATH.icon;
+          importance = SUGGESTION_IMPORTANCE.MINOR;
+        } else if (burstingBlood) {
+          suggestionText = <React.Fragment>You forgot to use a potion during combat. By using a potion during combat such as <ItemLink id={ITEMS.POTION_OF_BURSTING_BLOOD.id} /> you can increase your DPS (especially if lined up with damage cooldowns). If the encounter has become multi-target at the time you would use a potion, it may be beneficial to use <ItemLink id={ITEMS.BATTLE_POTION_OF_STRENGTH.id} /> instead.</React.Fragment>;
+          potionIcon = ITEMS.POTION_OF_BURSTING_BLOOD.icon;
           importance = SUGGESTION_IMPORTANCE.MINOR;
         } else if (!this.neededManaSecondPotion) {
           suggestionText = <React.Fragment>You forgot to use a potion during combat. Using a potion during combat allows you the benefit of either increasing output through <ItemLink id={ITEMS.BATTLE_POTION_OF_INTELLECT.id} /> or allowing you to gain mana using <ItemLink id={ITEMS.COASTAL_MANA_POTION.id} />, for example.</React.Fragment>;
+          potionIcon = ITEMS.BATTLE_POTION_OF_INTELLECT.icon;
           importance = SUGGESTION_IMPORTANCE.MINOR;
         } else {
           suggestionText = <React.Fragment>You ran out of mana (OOM) during the encounter without using a second potion. Use a second potion such as <ItemLink id={ITEMS.COASTAL_MANA_POTION.id} />or if the fight allows <ItemLink id={ITEMS.POTION_OF_REPLENISHMENT.id} /> to regenerate some mana.</React.Fragment>;
+          potionIcon = ITEMS.POTION_OF_REPLENISHMENT.icon;
           importance = SUGGESTION_IMPORTANCE.REGULAR;
         }
         return suggest(suggestionText)
-          .icon(ITEMS.LEYTORRENT_POTION.icon)
+          .icon(potionIcon)
           .staticImportance(importance);
       });
   }
 }
 
 export default PrePotion;
+
