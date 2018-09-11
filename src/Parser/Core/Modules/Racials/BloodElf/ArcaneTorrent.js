f8f93687311fb74cfc5654987716f1c4cefb9a14
Laust <lafa16ac@student.cbs.dk>
2018-09-11 15:21:47 +0200

    update AT

--- src/Parser/Core/Modules/Racials/BloodElf/ArcaneTorrent.js
+++ src/Parser/Core/Modules/Racials/BloodElf/ArcaneTorrent.js
@@ -13,6 +13,8 @@ class ArcaneTorrent extends Analyzer {
     abilities: Abilities,
   };
 
+  castEfficiency = 0;
+
   constructor(...args) {
     super(...args);
     const spec = this.selectedCombatant.spec;
@@ -21,33 +23,24 @@ class ArcaneTorrent extends Analyzer {
       return;
     }
     if (SPEC_WITH_LOWER_CAST_EFFICIENCY.includes(spec)) {
-      this.abilities.add({
-        spell: [SPELLS.ARCANE_TORRENT_MANA1, SPELLS.ARCANE_TORRENT_MANA2, SPELLS.ARCANE_TORRENT_MANA3, SPELLS.ARCANE_TORRENT_RAGE, SPELLS.ARCANE_TORRENT_ENERGY, SPELLS.ARCANE_TORRENT_RUNIC_POWER, SPELLS.ARCANE_TORRENT_MONK, SPELLS.ARCANE_TORRENT_FOCUS, SPELLS.ARCANE_TORRENT_FURY],
-        category: Abilities.SPELL_CATEGORIES.COOLDOWNS,
-        cooldown: 90,
-        gcd: {
-          base: 1500,
-        },
-        timelineSortIndex: 35,
-        castEfficiency: {
-          suggestion: true,
-          recommendedEfficiency: 0.5,
-        },
-      });
+      this.castEfficiency = 0.5;
     } else {
-      this.abilities.add({
-        spell: [SPELLS.ARCANE_TORRENT_MANA1, SPELLS.ARCANE_TORRENT_MANA2, SPELLS.ARCANE_TORRENT_MANA3, SPELLS.ARCANE_TORRENT_RAGE, SPELLS.ARCANE_TORRENT_ENERGY, SPELLS.ARCANE_TORRENT_RUNIC_POWER, SPELLS.ARCANE_TORRENT_MONK, SPELLS.ARCANE_TORRENT_FOCUS, SPELLS.ARCANE_TORRENT_FURY],
-        category: Abilities.SPELL_CATEGORIES.COOLDOWNS,
-        cooldown: 90,
-        gcd: {
-          base: 1500,
-        },
-        timelineSortIndex: 35,
-        castEfficiency: {
-          suggestion: true,
-        },
-      });
+      this.castEfficiency = 0.8;
     }
+
+    this.abilities.add({
+      spell: [SPELLS.ARCANE_TORRENT_MANA1, SPELLS.ARCANE_TORRENT_MANA2, SPELLS.ARCANE_TORRENT_MANA3, SPELLS.ARCANE_TORRENT_RAGE, SPELLS.ARCANE_TORRENT_ENERGY, SPELLS.ARCANE_TORRENT_RUNIC_POWER, SPELLS.ARCANE_TORRENT_MONK, SPELLS.ARCANE_TORRENT_FOCUS, SPELLS.ARCANE_TORRENT_FURY],
+      category: Abilities.SPELL_CATEGORIES.COOLDOWNS,
+      cooldown: 90,
+      gcd: {
+        base: 1500,
+      },
+      timelineSortIndex: 35,
+      castEfficiency: {
+        suggestion: true,
+        recommendedEfficiency: this.castEfficiency,
+      },
+    });
   }
 }
 
