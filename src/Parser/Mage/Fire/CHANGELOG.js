24d1b4f784bafa5467f7ab3d171b42ea52092d52
Chris Grasty <chris.grasty@gmail.com>
2018-09-14 16:56:40 -0500

    [Fire Mage] Updates & Fixes
    Updated the following for Fire Mage
    
    - Removed Legendaries & Tier Sets
    - Updated CombustionMarqueeBindings to look for Pyroclasm instead
    - Updated Heating Up to look for Searing Touch instead of Koralons
    - Updated Checklist to new format
    - Updated StatBoxes with the position setting

--- src/Parser/Mage/Fire/CHANGELOG.js
+++ src/Parser/Mage/Fire/CHANGELOG.js
@@ -4,6 +4,11 @@ import { Sharrq } from 'CONTRIBUTORS';
 
 export default [
   {
+    date: new Date('2018-9-14'),
+    changes: <React.Fragment>Updated Checklist</React.Fragment>,
+    contributors: [Sharrq],
+  },
+  {
     date: new Date('2018-6-28'),
     changes: <React.Fragment>Updated for 8.0 BFA Prepatch.</React.Fragment>,
     contributors: [Sharrq],
