9f999fb23b5ad43f3b9b4d56cff462ece2fdf3a3
Cyniikal <lyle.brown@okstate.edu>
2018-09-17 17:00:33 -0500

    Rewrote DefensiveStance.js to not make another Wcl request

--- src/Parser/Warrior/Arms/Modules/Talents/DefensiveStance.js
+++ src/Parser/Warrior/Arms/Modules/Talents/DefensiveStance.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS/TALENTS/WARRIOR';
-import fetchWcl from 'common/fetchWclApi';
 import SpellIcon from 'common/SpellIcon';
 import { formatThousands, formatNumber } from 'common/format';
 import Analyzer from 'Parser/Core/Analyzer';
-import LazyLoadStatisticBox, { STATISTIC_ORDER } from 'Interface/Others/LazyLoadStatisticBox';
-import makeWclUrl from 'common/makeWclUrl';
-
-const DEFENSIVE_STANCE_DAMAGE_REDUCED = 0.25;
+import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
 
 class DefensiveStance extends Analyzer {
   perSecond(amount) {
@@ -20,12 +16,6 @@ class DefensiveStance extends Analyzer {
     return this.perSecond(this.totalDamageMitigated);
   }
 
-  get filter() {
-    const playerName = this.owner.player.name;
-    // Include any damage while selected player has Defensive Stance active.
-    return `type='applybuff' AND ability.id=${SPELLS.DEFENSIVE_STANCE_TALENT.id} TO target.name='${playerName}' AND type='removebuff' AND ability.id=${SPELLS.DEFENSIVE_STANCE_TALENT.id} END`;
-  }
-
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.DEFENSIVE_STANCE_TALENT.id);
@@ -33,38 +23,21 @@ class DefensiveStance extends Analyzer {
       return;
     }
     this.addEventListener('damage', this.handleDamageTaken, {
-      toPlayer: true,
+      toPlayer: true
     });
   }
 
   handleDamageTaken(event) {
-    this.totalDamageMitigated += (event.mitigated || 0);
-  }
-
-  loaded = false;
-  load() {
-    return fetchWcl(`report/tables/damage-taken/${this.owner.report.code}`, {
-      start: this.owner.fight.start_time,
-      end: this.owner.fight.end_time,
-      filter: this.filter,
-    })
-      .then(json => {
-        console.log('Received damage taken while Defensive Stance is up', json);
-        const actualDamageTaken = json.entries
-          .filter(entry => entry.id === this.owner.playerId)
-          .reduce((damageTaken, entry) => damageTaken + entry.total, 0);
-        const damageMitigated = actualDamageTaken * DEFENSIVE_STANCE_DAMAGE_REDUCED;
-        this.totalDamageMitigated += damageMitigated;
-        this.loaded = true;
-      });
+    if(this.selectedCombatant.hasBuff(SPELLS.DEFENSIVE_STANCE_TALENT.id)) {
+      this.totalDamageMitigated += (event.mitigated || 0)
+    }
   }
 
   statistic() {
-    const tooltip = this.loaded ? `
+    const tooltip = `
       <b>Total:</b><br />
-      Effective damage reduction: ${formatThousands(this.totalDamageMitigated)} damage (${formatThousands(this.perSecond(this.totalDamageMitigated))} DRPS)<br /><br />
-    ` : 'Click to load the required data.';
-    const footer = this.loaded && (
+      Effective damage reduction: ${formatThousands(this.totalDamageMitigated)} damage (${formatThousands(this.perSecond(this.totalDamageMitigated))} DRPS)<br /><br />`
+    const footer = (
       <div className="statistic-bar">
         <div
           className="stat-health-bg"
@@ -76,21 +49,14 @@ class DefensiveStance extends Analyzer {
     );
 
     return (
-      <LazyLoadStatisticBox
+      <StatisticBox
         position={STATISTIC_ORDER.OPTIONAL(60)}
-        loader={this.load.bind(this)}
         icon={<SpellIcon id={SPELLS.DEFENSIVE_STANCE_TALENT.id} />}
         value={`≈${formatNumber(this.drps)} DRPS`}
         label="Damage reduced"
         tooltip={tooltip}
         footer={footer}
         footerStyle={{ overflow: 'hidden' }}
-        warcraftLogs={makeWclUrl(this.owner.report.code, {
-          fight: this.owner.fightId,
-          type: 'damage-taken',
-          pins: `2$Off$#244F4B$expression$${this.filter}`,
-          view: 'events',
-        })}
       />
     );
   }
