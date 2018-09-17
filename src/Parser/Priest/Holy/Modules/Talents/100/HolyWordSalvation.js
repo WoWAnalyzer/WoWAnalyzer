cd780c9576bd1d6758644afe5dcc4aaf9d11de66
James Moats <tjmoats@demark.com>
2018-09-17 14:32:34 -0700

    salv!

--- src/Parser/Priest/Holy/Modules/Talents/100/HolyWordSalvation.js
+++ src/Parser/Priest/Holy/Modules/Talents/100/HolyWordSalvation.js
@@ -1,25 +1,76 @@
 import Analyzer from 'Parser/Core/Analyzer';
 import SPELLS from 'common/SPELLS';
-import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
+import TraitStatisticBox, { STATISTIC_ORDER } from 'Interface/Others/TraitStatisticBox';
 import STATISTIC_CATEGORY from 'Interface/Others/STATISTIC_CATEGORY';
 import SpellIcon from 'common/SpellIcon';
 import React from 'react';
+import Renew from 'Parser/Priest/Holy/Modules/Spells/Renew';
+import PrayerOfMending from 'Parser/Priest/Holy/Modules/Spells/PrayerOfMending';
+import ItemHealingDone from 'Interface/Others/ItemHealingDone';
+import { formatThousands } from 'common/format';
 
 class HolyWordSalvation extends Analyzer {
+  salvTicks = 0;
+  healingFromSalv = 0;
+  overhealingFromSalv = 0;
+
+  static dependencies = {
+    renew: Renew,
+    prayerOfMending: PrayerOfMending,
+  };
+
+  get renewCount() {
+    return this.renew.renewsFromSalv;
+  }
+
+  get healingFromRenew() {
+    return this.renew.healingFromRenew(this.renewCount);
+  }
+
+  get pomCount() {
+    return this.prayerOfMending.pomTicksFromSalv;
+  }
+
+  get healingFromPom() {
+    return this.prayerOfMending.averagePomTickHeal * this.pomCount;
+  }
+
+  get totalHealing() {
+    return this.healingFromSalv + this.healingFromRenew + this.healingFromPom;
+  }
+
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.HOLY_WORD_SALVATION_TALENT.id);
   }
 
+  on_byPlayer_heal(event) {
+    const spellId = event.ability.guid;
+
+    if (spellId === SPELLS.HOLY_WORD_SALVATION_TALENT.id) {
+      this.healingFromSalv += event.amount | 0;
+      this.overhealingFromSalv += event.overhealing | 0;
+      this.salvTicks++;
+    }
+  }
+
   statistic() {
     return (
 
-      <StatisticBox
+      <TraitStatisticBox
         category={STATISTIC_CATEGORY.TALENTS}
         icon={<SpellIcon id={SPELLS.HOLY_WORD_SALVATION_TALENT.id} />}
-        value={"Value"}
+        value={(
+          <React.Fragment>
+            <ItemHealingDone amount={this.totalHealing} />
+          </React.Fragment>
+        )}
         label="Holy Word: Salvation"
-        tooltip={``}
+        tooltip={`
+          Healing from (~${this.salvTicks}) Salv: ${formatThousands(this.healingFromSalv)}<br />
+          Healing from (~${this.renewCount}) Renews: ${formatThousands(this.healingFromRenew)}<br />
+          Healing from (~${this.pomCount}) PoMs: ${formatThousands(this.healingFromPom)}
+        `}
       />
 
     );
