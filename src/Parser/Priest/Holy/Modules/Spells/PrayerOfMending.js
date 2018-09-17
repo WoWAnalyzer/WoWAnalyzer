23d091b6e03a60d4c22e7ed1c59100b888c89f30
James Moats <tjmoats@demark.com>
2018-09-17 14:32:29 -0700

    building out pom module

--- src/Parser/Priest/Holy/Modules/Spells/PrayerOfMending.js
+++ src/Parser/Priest/Holy/Modules/Spells/PrayerOfMending.js
@@ -4,18 +4,39 @@ import Analyzer from 'Parser/Core/Analyzer';
 class PrayerOfMending extends Analyzer {
   totalPoMHealing = 0;
   totalPoMOverhealing = 0;
-
   pomCasts = 0;
+  salvCasts = 0;
   pomHealTicks = 0;
   pomBuffCount = 0;
-
+  pomRemovalCount = 0;
+  prepullPomBuffs = 0;
   lastSalvCastTime = 0;
-  pomsFromSalv = 0;
 
-  stackTracker = {};
+  get pomTicksFromSalv() {
+    if (this.salvCasts === 0) {
+      return 0;
+    }
+
+    console.log(this.pomCasts, this.pomHealTicks, this.pomBuffCount, this.pomRemovalCount);
+    const estSalvPoms = this.pomBuffCount - this.pomTicksFromCast - (this.prepullPomBuffs * 10);
+
+    return estSalvPoms;
+  }
+
+  get pomTicksFromCast() {
+    return this.pomCasts * 5;
+  }
 
   get wastedPomStacks() {
     return this.pomBuffCount - this.pomHealTicks;
+  }
+
+  get averagePomTickHeal() {
+    return this.totalPoMHealing / this.pomHealTicks;
+  }
+
+  get averagePomTickOverheal() {
+    return this.totalPoMOverhealing / this.pomHealTicks;
   }
 
   on_byPlayer_cast(event) {
@@ -24,8 +45,9 @@ class PrayerOfMending extends Analyzer {
     if (spellId === SPELLS.PRAYER_OF_MENDING_CAST.id) {
       this.pomCasts++;
     }
-    if (spellId === SPELLS.HOLY_WORD_SALVATION_TALENT) {
+    if (spellId === SPELLS.HOLY_WORD_SALVATION_TALENT.id) {
       this.lastSalvCastTime = event.timestamp;
+      this.salvCasts++;
     }
   }
 
@@ -40,53 +62,24 @@ class PrayerOfMending extends Analyzer {
   }
 
   on_byPlayer_applybuff(event) {
-
-  }
-
-  on_byPlayer_removebuff(event) {
     const spellId = event.ability.guid;
-
     if (spellId === SPELLS.PRAYER_OF_MENDING_BUFF.id) {
-      console.log("Remove buff");
-      this.handleStack(event.targetID, 0);
-    }
-  }
-
-  on_byPlayer_applybuffstack(event) {
-    const spellId = event.ability.guid;
-
-    if (spellId === SPELLS.PRAYER_OF_MENDING_BUFF.id) {
-      console.log("Apply Buff Stack " + event.stack);
-      this.handleStack(event.targetID, event.stack);
+      if (event.prepull) {
+        this.prepullPomBuffs++;
+        this.pomCasts++;
+      }
     }
   }
 
   on_byPlayer_changebuffstack(event) {
     const spellId = event.ability.guid;
-
     if (spellId === SPELLS.PRAYER_OF_MENDING_BUFF.id) {
-      console.log("Change Buff Stack " + event.newStacks);
-      console.log(event);
-      this.handleStack(event.targetID, event.stack, event.oldStacks);
+      if (event.stacksGained > 0) {
+        this.pomBuffCount++;
+      } else {
+        this.pomRemovalCount++;
+      }
     }
-  }
-
-  on_byPlayer_removebuffstack(event) {
-    const spellId = event.ability.guid;
-
-    if (spellId === SPELLS.PRAYER_OF_MENDING_BUFF.id) {
-      console.log("Remove Buff Stack " + event.stack);
-      this.handleStack(event.targetID, 0);
-    }
-  }
-
-  handleStack(targetId, newStack, eventOldStacks) {
-    const oldStack = this.stackTracker[targetId] || 0;
-    if (eventOldStacks &&oldStack !== eventOldStacks) {
-
-    }
-    console.log(`${targetId} stacks changed from ${oldStack} to ${newStack}`);
-    this.stackTracker[targetId] = newStack;
   }
 }
 
