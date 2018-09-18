c8899c52a8834d74d03dc9e3e28d99559135ed8e
James Moats <tjmoats@demark.com>
2018-09-18 14:47:21 -0700

    stubbing more

--- src/Parser/Priest/Holy/Modules/Spells/HolyWords/HolyWordBase.js
+++ src/Parser/Priest/Holy/Modules/Spells/HolyWords/HolyWordBase.js
@@ -2,24 +2,23 @@ import React from 'react';
 import SPELLS from 'common/SPELLS/index';
 import Analyzer from 'Parser/Core/Analyzer';
 
-const BASE_COOLDOWN = 60000;
-
 class HolyWordBase extends Analyzer {
-  serendipityProccers = {
-    [SPELLS.GREATER_HEAL.id]: 1.0,
-    [SPELLS.FLASH_HEAL.id]: 1.0,
-    [SPELLS.BINDING_HEAL_TALENT.id]: 0.5,
-  };
+  baseCooldown = 60000;
+  remainingCooldown = 0;
+  serendipityProccers = {};
 
   healing = 0;
   overhealing = 0;
   totalCasts = 0;
-  baseCooldown = 60000;
   reductionBySpell = {};
   apotheosisReductionBySpell = {};
   wastedCooldown = 0;
   spellId = 0;
-  spotheosisMultiplier = 3;
+
+  apotheosisCasts = 0;
+  apotheosisActive = false;
+  apotheosisMultiplier = 3;
+
   get totalCooldownReduction() {
     let totalCDR = 0;
     Object.keys(this.reductionBySpell).map(function (key, index) {
@@ -42,6 +41,43 @@ class HolyWordBase extends Analyzer {
     // Set up proper serendipity reduction values
     if (this.selectedCombatant.hasTalent(SPELLS.LIGHT_OF_THE_NAARU_TALENT.id)) {
       this.serendipityReduction += 2000;
+    }
+  }
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (spellId === this.spellId) {
+      this.totalCasts++;
+      this.remainingCooldown = 60000;
+    } else if (this.serendipityProccers[spellId] != null) {
+      if (this.apotheosisActive) {
+
+      } else {
+
+      }
+    }
+  }
+
+  on_byPlayer_heal(event) {
+    const spellId = event.ability.guid;
+    if (spellId === this.spellId) {
+      this.healing += event.amount || 0;
+      this.overhealing += event.overheal || 0;
+    }
+  }
+
+  on_byPlayer_applybuff(event) {
+    const spellId = event.ability.guid;
+    if (spellId === SPELLS.APOTHEOSIS_TALENT.id) {
+      this.apotheosisCasts++;
+      this.apotheosisActive = true;
+    }
+  }
+
+  on_byPlayer_removebuff(event) {
+    const spellId = event.ability.guid;
+    if (spellId === SPELLS.APOTHEOSIS_TALENT.id) {
+      this.apotheosisActive = false;
     }
   }
 }
