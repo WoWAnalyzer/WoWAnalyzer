6253c9ceebbd5ef77da54d5b80bd976869ec262a
James Moats <tjmoats@demark.com>
2018-09-18 13:00:39 -0700

    Revert "removing unbuilt talents for now"
    This reverts commit 90b3273d09cd387eeacac22df4a6d0a22d310d1a.

--- src/Parser/Priest/Holy/Modules/Talents/100/index.js
+++ src/Parser/Priest/Holy/Modules/Talents/100/index.js
@@ -1,5 +1,9 @@
+import Apotheosis from './Apotheosis';
 import HolyWordSalvation from './HolyWordSalvation';
+import LightOfTheNaru from './LightOfTheNaru';
 
 export {
+  Apotheosis,
   HolyWordSalvation,
+  LightOfTheNaru,
 };
