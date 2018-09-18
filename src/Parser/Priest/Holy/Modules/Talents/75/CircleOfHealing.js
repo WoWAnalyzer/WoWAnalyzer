79dd122025717e46d1c65b0f0f35dda072100f6a
James Moats <tjmoats@demark.com>
2018-09-18 09:22:37 -0700

    CoH

--- src/Parser/Priest/Holy/Modules/Talents/75/CircleOfHealing.js
+++ src/Parser/Priest/Holy/Modules/Talents/75/CircleOfHealing.js
@@ -1,26 +1,69 @@
 import Analyzer from 'Parser/Core/Analyzer';
 import SPELLS from 'common/SPELLS';
-import StatisticBox, { STATISTIC_ORDER } from 'Interface/Others/StatisticBox';
+import TraitStatisticBox, { STATISTIC_ORDER } from 'Interface/Others/TraitStatisticBox';
 import STATISTIC_CATEGORY from 'Interface/Others/STATISTIC_CATEGORY';
 import SpellIcon from 'common/SpellIcon';
 import React from 'react';
+import ItemHealingDone from 'Interface/Others/ItemHealingDone';
+import { formatPercentage, formatThousands } from 'common/format';
 
 // Example Log: http://localhost:3000/report/aBxvzDZJQP7431Nt/21-Normal+G'huun+-+Kill+(7:11)/15-Liarine
 class CircleOfHealing extends Analyzer {
+  circleOfHealingCasts = 0;
+  circleOfHealingHealing = 0;
+  circleOfHealingOverhealing = 0;
+  circleOfHealingTargetsHit = 0;
+
+  get overHealPercent() {
+    return this.circleOfHealingOverhealing / this.rawHealing;
+  }
+
+  get rawHealing() {
+    return this.circleOfHealingHealing + this.circleOfHealingOverhealing;
+  }
+
+  get averageTargetsHit() {
+    return this.circleOfHealingTargetsHit / this.circleOfHealingCasts;
+  }
+
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.CIRCLE_OF_HEALING_TALENT.id);
   }
 
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    if (spellId === SPELLS.CIRCLE_OF_HEALING_TALENT.id) {
+      this.circleOfHealingCasts++;
+    }
+  }
+
+  on_byPlayer_heal(event) {
+    const spellId = event.ability.guid;
+    if (spellId === SPELLS.CIRCLE_OF_HEALING_TALENT.id) {
+      this.circleOfHealingTargetsHit++;
+      this.circleOfHealingHealing += event.amount || 0;
+      this.circleOfHealingOverhealing += event.overheal || 0;
+    }
+  }
+
   statistic() {
     return (
 
-      <StatisticBox
+      <TraitStatisticBox
         category={STATISTIC_CATEGORY.TALENTS}
         icon={<SpellIcon id={SPELLS.CIRCLE_OF_HEALING_TALENT.id} />}
-        value={"Value"}
+        value={(
+          <React.Fragment>
+            <ItemHealingDone amount={this.circleOfHealingHealing} />
+          </React.Fragment>
+        )}
         label="Circle of Healing"
-        tooltip={``}
+        tooltip={`
+          Coh Casts: ${this.circleOfHealingCasts}<br />
+          Total Healing: ${formatThousands(this.circleOfHealingHealing)} (${formatPercentage(this.overHealPercent)}% OH)<br />
+          Average Targets Hit: ${this.averageTargetsHit.toFixed(2)}
+        `}
       />
 
     );
