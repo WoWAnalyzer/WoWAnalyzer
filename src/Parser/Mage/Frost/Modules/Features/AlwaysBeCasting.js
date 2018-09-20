a43b6c511285de3a8c7d898d317b7faf839ddef9
Dambroda <43309639+Dambroda@users.noreply.github.com>
2018-09-20 13:11:44 -0700

    Fix ABC suggestion grammar; suggest casting FB during boss immunity

--- src/Parser/Mage/Frost/Modules/Features/AlwaysBeCasting.js
+++ src/Parser/Mage/Frost/Modules/Features/AlwaysBeCasting.js
@@ -29,7 +29,7 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
     if (!boss || !boss.fight.disableDowntimeSuggestion) {
       when(this.downtimeSuggestionThresholds)
         .addSuggestion((suggest, actual, recommended) => {
-          return suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. Even if you have to move, try casting instants, even unbuffed <SpellLink id={SPELLS.ICE_LANCE.id} /> spam is better than nothing.</span>)
+          return suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC) and try to reduce the delay between casting spells. If you have to move, try casting instants; even unbuffed <SpellLink id={SPELLS.ICE_LANCE.id} /> spam is better than nothing. Additionally, if you are standing still while waiting for a boss damage reduction or immunity phase to end, you should still be casting <SpellLink id={SPELLS.FROSTBOLT.id} /> to generate procs or build <SpellLink id={SPELLS.ICICLES_BUFF.id}/> to use when the immunity ends.</span>)
             .icon('spell_mage_altertime')
             .actual(`${formatPercentage(actual)}% downtime`)
             .recommended(`<${formatPercentage(recommended)}% is recommended`);
