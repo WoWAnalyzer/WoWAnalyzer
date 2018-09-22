f103031f7051853a9a3041dd9a345e3d6467056a
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-09-22 05:59:14 +0200

    Refactor - Destruction Warlock

--- src/Parser/Warlock/Destruction/Modules/Talents/SoulConduit.js
+++ src/Parser/Warlock/Destruction/Modules/Talents/SoulConduit.js
@@ -4,13 +4,12 @@ import Analyzer from 'Parser/Core/Analyzer';
 
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
-
 import { formatPercentage } from 'common/format';
+
+import StatisticListBoxItem from 'Interface/Others/StatisticListBoxItem';
 
 import SoulShardTracker from '../SoulShards/SoulShardTracker';
 
-
-// credit to feretory of souls module
 class SoulConduit extends Analyzer {
   static dependencies = {
     soulShardTracker: SoulShardTracker,
@@ -23,20 +22,13 @@ class SoulConduit extends Analyzer {
 
   subStatistic() {
     const spent = this.soulShardTracker.spent / 10;
-    const shardsGained = this.soulShardTracker.getGeneratedBySpell(SPELLS.SOUL_CONDUIT_TALENT.id) / 10;
+    const shardsGained = this.soulShardTracker.getGeneratedBySpell(SPELLS.SOUL_CONDUIT_SHARD_GEN.id) / 10;
     return (
-      <div className="flex">
-        <div className="flex-main">
-          <SpellLink id={SPELLS.SOUL_CONDUIT_TALENT.id}>
-            Soul Shards Gained
-          </SpellLink>
-        </div>
-        <div className="flex-sub text-right">
-        <dfn data-tip={`Your Soul Conduit refunded ${formatPercentage(shardsGained/spent)} % of soul shards spent`}>
-            {shardsGained}
-          </dfn>
-        </div>
-      </div>
+      <StatisticListBoxItem
+        title={<React.Fragment>Shards generated with <SpellLink id={SPELLS.SOUL_CONDUIT_TALENT.id} /></React.Fragment>}
+        value={shardsGained}
+        valueTooltip={`Your Soul Conduit refunded ${formatPercentage(shardsGained/spent)} % of soul shards spent`}
+      />
     );
   }
 }
