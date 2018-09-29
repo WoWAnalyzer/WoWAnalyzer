3409c8ef1fcd591761c7a4dd149e5f840c818fd4
Martijn Hols <git@martijnhols.nl>
2018-09-29 12:54:03 +0200

    Mark gear as epic when item level is appropriate

--- src/Parser/Core/Modules/Features/CharacterTab/Component/Gear.js
+++ src/Parser/Core/Modules/Features/CharacterTab/Component/Gear.js
@@ -9,6 +9,7 @@ class Gear extends React.PureComponent {
   static propTypes = {
     gear: PropTypes.array.isRequired,
   };
+
   render() {
     const gear = this.props.gear;
 
@@ -28,20 +29,28 @@ class Gear extends React.PureComponent {
           <div className="col-md-12 hpadding-lg-30">{/* some bonus padding so it looks to be aligned with the icon for stats */}
             {[...gear, ...relics]
               .filter(item => item.id !== 0)
-              .map(item => (
-                <div key={item.id} style={{ display: 'inline-block', textAlign: 'center' }}>
-                  {item.itemLevel}
-                  <ItemLink
-                    id={item.id}
-                    quality={item.quality || ITEM_QUALITIES.EPIC}// relics don't have a quality, but they're always epic
-                    details={item}
-                    style={{ margin: '5px', display: 'block', fontSize: '46px', lineHeight: 1 }}
-                    icon={false}
-                  >
-                    <Icon icon={item.icon} style={{ border: '3px solid currentColor' }} />
-                  </ItemLink>
-                </div>
-              ))}
+              .map(item => {
+                // Items seem to turn epic from 340 item level, but WCL doesn't show this properly
+                let quality = item.itemLevel >= 340 ? ITEM_QUALITIES.EPIC : item.quality;
+                if (!quality) {
+                  quality = ITEM_QUALITIES.EPIC; // relics don't have a quality, but they're always epic
+                }
+
+                return (
+                  <div key={item.id} style={{ display: 'inline-block', textAlign: 'center' }}>
+                    {item.itemLevel}
+                    <ItemLink
+                      id={item.id}
+                      quality={quality}
+                      details={item}
+                      style={{ margin: '5px', display: 'block', fontSize: '46px', lineHeight: 1 }}
+                      icon={false}
+                    >
+                      <Icon icon={item.icon} style={{ border: '3px solid currentColor' }} />
+                    </ItemLink>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </React.Fragment>
