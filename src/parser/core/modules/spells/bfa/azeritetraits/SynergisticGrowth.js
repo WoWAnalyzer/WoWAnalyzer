bb9faa5dbe2b85faed4c41d9a249d3024b78e40f
James Moats <tjmoats@demark.com>
2018-10-02 08:40:11 -0700

    Adding examples for more specs

--- src/parser/core/modules/spells/bfa/azeritetraits/SynergisticGrowth.js
+++ src/parser/core/modules/spells/bfa/azeritetraits/SynergisticGrowth.js
@@ -10,7 +10,9 @@ export const STAT_TRACKER = {
  * Synergistic Growth
  * Gain x mastery while active
  *
- * Example report: /report/4qXRxW1fYLPaJgMA/7-Mythic+Zul+-+Wipe+1+(1:25)/8-Monkacarl
+ * Example report Monk:   /report/4qXRxW1fYLPaJgMA/7-Mythic+Zul+-+Wipe+1+(1:25)/8-Monkacarl
+ * Example report Druid:  /report/8CVY9f42cBRxZPMT/46-Mythic+Fetid+Devourer+-+Kill+(5:03)/29-Dunao
+ * Example report Priest: /report/DfJ2TbtmA8BHNcWK/20-Mythic+Zek'voz+-+Kill+(9:48)/18-Fearrful
  */
 class SynergisticGrowth extends GenericStatProc {
   constructor(...args) {
