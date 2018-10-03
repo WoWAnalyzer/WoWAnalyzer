1bbd05ef1ba587cd8c23143cc75b2d18cef04a0b
James Moats <tjmoats@demark.com>
2018-10-03 09:23:46 -0700

    updating renew module

--- src/parser/priest/holy/modules/spells/Renew.js
+++ src/parser/priest/holy/modules/spells/Renew.js
@@ -2,34 +2,56 @@ import SPELLS from 'common/SPELLS';
 import Analyzer from 'parser/core/Analyzer';
 
 import { ABILITIES_THAT_TRIGGER_ENDURING_RENEWAL } from '../../constants';
+
 const MS_BUFFER = 100;
 
 class Renew extends Analyzer {
   totalRenewHealing = 0;
   totalRenewOverhealing = 0;
+  totalRenewAbsorbs = 0;
 
   renewsCast = 0;
   totalRenewApplications = 0;
 
-  renewsFromBenedictionAndRenew = 0;
-  renewsFromEnduringRenewal = 0;
-  renewsFromSalv = 0;
-
+  salvationEnabled = false;
   lastSalvationCast = 0;
-  lastErSpellCast = 0;
+  renewsFromSalvation = 0;
+
+  enduringRenewalActive = false;
+  lastEnduringRenewalSpellCast = 0;
+  renewsFromEnduringRenewal = 0;
+
+  benedictionActive = false;
+  renewsFromBenedictionAndRenew = 0;
+
+  constructor(...args) {
+    super(...args);
+
+    if (this.selectedCombatant.hasTalent(SPELLS.ENDURING_RENEWAL_TALENT.id)) {
+      this.enduringRenewalEnabled = true;
+    }
+    if (this.selectedCombatant.hasTalent(SPELLS.BENEDICTION_TALENT.id)) {
+      this.benedictionActive = true;
+    }
+  }
 
   get renewsFromBenediction() {
     return this.renewsFromBenedictionAndRenew - this.renewsCast;
   }
 
-  healingFromRenew(renewCount) {
+  healingFromRenew(applicationCount) {
     const averageHealingPerRenewApplication = this.totalRenewHealing / this.totalRenewApplications;
-    return averageHealingPerRenewApplication * renewCount;
+    return averageHealingPerRenewApplication * applicationCount;
   }
 
-  overhealingFromRenew(renewCount) {
+  overhealingFromRenew(applicationCount) {
     const averageOverHealingPerRenewApplication = this.totalRenewOverhealing / this.totalRenewApplications;
-    return averageOverHealingPerRenewApplication * renewCount;
+    return averageOverHealingPerRenewApplication * applicationCount;
+  }
+
+  absorptionFromRenew(applicationCount) {
+    const averageAbsorptionPerRenewApplication = this.totalRenewOverhealing / this.totalRenewApplications;
+    return averageAbsorptionPerRenewApplication * applicationCount;
   }
 
   on_byPlayer_heal(event) {
@@ -38,6 +60,7 @@ class Renew extends Analyzer {
     if (spellId === SPELLS.RENEW.id) {
       this.totalRenewHealing += event.amount || 0;
       this.totalRenewOverhealing += event.overheal || 0;
+      this.totalRenewAbsorbs += event.absorbed || 0;
     }
   }
 
@@ -51,24 +74,35 @@ class Renew extends Analyzer {
       this.lastSalvationCast = event.timestamp;
     }
     else if (ABILITIES_THAT_TRIGGER_ENDURING_RENEWAL.includes(spellId)) {
-      this.lastErSpellCast = event.timestamp;
+      this.lastEnduringRenewalSpellCast = event.timestamp;
     }
   }
 
   on_byPlayer_applybuff(event) {
+    this.handleRenewApplication(event);
+  }
+
+  on_byPlayer_refreshbuff(event) {
+    this.handleRenewApplication(event);
+  }
+
+  handleRenewApplication(event) {
     const spellId = event.ability.guid;
 
-    if (spellId === SPELLS.RENEW.id) {
-      this.totalRenewApplications++;
+    if (spellId !== SPELLS.RENEW.id) {
+      return;
+    }
 
-      if (event.timestamp - this.lastSalvationCast < MS_BUFFER) {
-        this.renewsFromSalv++;
-      }
-      else if (event.timestamp - this.lastErSpellCast < MS_BUFFER) {
-        this.renewsFromEnduringRenewal++;
-      } else {
-        this.renewsFromBenedictionAndRenew++;
-      }
+    this.totalRenewApplications++;
+
+    if (this.salvationEnabled && event.timestamp - this.lastSalvationCast < MS_BUFFER) {
+      this.renewsFromSalvation++;
+    }
+    else if (this.enduringRenewalActive && event.timestamp - this.lastEnduringRenewalSpellCast < MS_BUFFER) {
+      this.renewsFromEnduringRenewal++;
+    }
+    else {
+      this.renewsFromBenedictionAndRenew++;
     }
   }
 }
