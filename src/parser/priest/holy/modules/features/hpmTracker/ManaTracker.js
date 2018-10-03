3bd0bf3965960429cf08c8e50f7fdf0d7c9e8062
James Moats <tjmoats@demark.com>
2018-10-03 09:43:46 -0700

    adding mana tracker

---
+++ src/parser/priest/holy/modules/features/hpmTracker/ManaTracker.js
@@ -0,0 +1,13 @@
+import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
+import ResourceTracker from 'parser/core/modules/resourcetracker/ResourceTracker';
+
+class ManaTracker extends ResourceTracker {
+
+  constructor(...args) {
+    super(...args);
+    this.resource = RESOURCE_TYPES.MANA;
+    this.maxResource = 100000;
+  }
+}
+
+export default ManaTracker;
