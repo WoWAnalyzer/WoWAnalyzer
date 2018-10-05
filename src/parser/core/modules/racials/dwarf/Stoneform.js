e9d927cd09c087397f9ecaa66879296f9212923d
kujan <fredrik.k91@gmail.com>
2018-10-05 23:03:43 +0200

    tooltip update, comments and log example

--- src/parser/core/modules/racials/dwarf/Stoneform.js
+++ src/parser/core/modules/racials/dwarf/Stoneform.js
@@ -3,10 +3,16 @@ import Analyzer from 'parser/core/Analyzer';
 
 import Combatants from 'parser/core/modules/Combatants';
 import { formatNumber } from 'common/format';
-import SPELLS from 'common/SPELLS';
-import SpellIcon from 'common/SpellIcon';
 import RACES from 'game/RACES';
+import SpellIcon from 'common/SpellIcon';
+import SPELLS from 'common/SPELLS';
 import StatisticBox from 'interface/others/StatisticBox';
+
+/**
+ * Removes all poison, disease, curse, magic, and bleed effects and reduces all physical damage taken by 10% for 8 sec.
+ *
+ * Example log: https://www.warcraftlogs.com/reports/gYCR2kZaJHVbjfxP/#fight=52&source=16&type=damage-taken
+ */
 
 const STONEFORM_DAMAGE_REDUCTION = 0.1;
 const FALLING_DAMAGE_ABILITY_ID = 3;
@@ -19,6 +25,7 @@ class Stoneform extends Analyzer {
   
   damageReduced = 0;
   physicalDamageTaken = 0;
+
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.race && this.selectedCombatant.race === RACES.Dwarf;
@@ -26,7 +33,7 @@ class Stoneform extends Analyzer {
 
   on_toPlayer_damage(event) {
     const spellId = event.ability.guid;
-    if (spellId === FALLING_DAMAGE_ABILITY_ID) {
+    if (spellId === FALLING_DAMAGE_ABILITY_ID) { // Falling damage is the same type as physical but ignores DRs.
       return;
     }
     
@@ -43,7 +50,7 @@ class Stoneform extends Analyzer {
     return(
       <StatisticBox
         icon={<SpellIcon id={SPELLS.STONEFORM_BUFF.id} />}
-        value={`${formatNumber(this.damageReduced)}`}
+        value={`≈${formatNumber(this.damageReduced)}`}
         label="Stoneform Damage Reduced"
         tooltip={`Over the course of the encounter you took ${formatNumber(this.physicalDamageTaken)} physical damage while Stoneform was active`}
       />
