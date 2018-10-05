33d0ad2092b1db74be41956c55a71ce812b66f79
Anatta336 <sdriver@gmx.co.uk>
2018-10-05 14:22:23 +0100

    remove legion legendaries and tier sets

--- src/parser/druid/feral/modules/combopoints/ComboPointTracker.js
+++ src/parser/druid/feral/modules/combopoints/ComboPointTracker.js
@@ -22,18 +22,6 @@ class ComboPointTracker extends ResourceTracker {
     this.maxResource = 5;
   }
 
-  on_byPlayer_cast(event) {
-    const spellId = event.ability.guid;
-
-    // T21 4pc set bonus can proc "free Ferocious Bite that counts as if it spent the full 5 CPs"
-    // the energy cost is properly missing from the cast, but the CP cost shows very strangely. (amount: x, max: 5, cost: 5) where x is the amount of CPs you had on cast.
-    // Returning before this CP cost can be processed to avoid messing up the results
-    if ((spellId === SPELLS.FEROCIOUS_BITE.id) && event.classResources && !event.classResources.find(resource => resource.type === RESOURCE_TYPES.ENERGY.id)) {
-      return;
-    }
-    super.on_byPlayer_cast(event);
-  }
-
   _applyBuilder(spellId, resource, gain, waste) {
     const isMaxBefore = (this.current === this.maxResource);
     super._applyBuilder(spellId, resource, gain, waste);
