a74904e962750ead58fa4bcd469bc2574d14cf22
Martijn Hols <git@martijnhols.nl>
2018-10-06 13:36:05 +0200

    Fix more crashes by lingui

--- src/parser/paladin/holy/modules/talents/RuleOfLaw.js
+++ src/parser/paladin/holy/modules/talents/RuleOfLaw.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { t } from '@lingui/macro';
 
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import SpellLink from 'common/SpellLink';
 import { formatPercentage } from 'common/format';
-
 import Analyzer from 'parser/core/Analyzer';
-
 import StatisticBox, { STATISTIC_ORDER } from 'interface/others/StatisticBox';
+import { i18n } from 'interface/RootLocalizationProvider';
 
 class RuleOfLaw extends Analyzer {
   constructor(...args) {
@@ -42,13 +42,13 @@ class RuleOfLaw extends Analyzer {
         .recommended(`>${formatPercentage(recommended)}% is recommended`);
     });
   }
-  statistic({ i18n }) {
+  statistic() {
     return (
       <StatisticBox
         position={STATISTIC_ORDER.CORE(31)}
         icon={<SpellIcon id={SPELLS.RULE_OF_LAW_TALENT.id} />}
         value={`${formatPercentage(this.uptime)} %`}
-        label={i18n.t`${SPELLS.RULE_OF_LAW_TALENT.name} uptime`}
+        label={i18n._(t`${SPELLS.RULE_OF_LAW_TALENT.name} uptime`)}
       />
     );
   }
