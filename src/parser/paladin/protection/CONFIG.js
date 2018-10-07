924d8cde887d3233feea8a1219af0b90c1a0965f
Martijn Hols <git@martijnhols.nl>
2018-10-07 22:25:30 +0200

    Separate Paladin chunks

--- src/parser/paladin/protection/CONFIG.js
+++ src/parser/paladin/protection/CONFIG.js
@@ -37,7 +37,7 @@ export default {
   // The contents of your changelog.
   changelog: CHANGELOG,
   // The CombatLogParser class for your spec.
-  parser: () => import('./CombatLogParser' /* webpackChunkName: "Paladin" */).then(exports => exports.default),
+  parser: () => import('./CombatLogParser' /* webpackChunkName: "ProtectionPaladin" */).then(exports => exports.default),
   // The path to the current directory (relative form project root). This is used for generating a GitHub link directly to your spec's code.
   path: __dirname,
 };
