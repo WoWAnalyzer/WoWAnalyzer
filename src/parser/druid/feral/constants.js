0a850761ed29cb8063ce76da6c12f19692d6fd3c
Anatta336 <sdriver@gmx.co.uk>
2018-10-10 19:39:04 +0100

    add Wild Fleshrending trait for Feral

--- src/parser/druid/feral/constants.js
+++ src/parser/druid/feral/constants.js
@@ -8,3 +8,8 @@ export const BITE_EXECUTE_RANGE = 0.25;
 export const SAVAGE_ROAR_DAMAGE_BONUS = 0.15;
 export const JAGGED_WOUNDS_MODIFIER = 0.80;
 export const PANDEMIC_FRACTION = 0.3;
+
+export const SHRED_COEFFICIENT = 0.380562;
+export const SWIPE_CAT_COEFFICIENT = 0.25;
+export const SWIPE_BEAR_COEFFICIENT = 0.30;
+export const BRUTAL_SLASH_COEFFICIENT = 0.60;
