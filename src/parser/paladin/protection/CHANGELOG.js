df6bcbfdebaca1bd7169c3e700e3180cf2d84348
J. David Smith <emallson@atlanis.net>
2018-10-10 20:32:36 -0400

    added checklist support for SotR casts and updated Protadin config

--- src/parser/paladin/protection/CHANGELOG.js
+++ src/parser/paladin/protection/CHANGELOG.js
@@ -5,8 +5,13 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
+    date: new Date('10 October 2018'),
+    contributors: [emallson],
+    changes: <>Added tracking for which <SpellLink id={SPELLS.SHIELD_OF_THE_RIGHTEOUS.id} /> casts are effective.</>,
+  },
+  {
     date: new Date('6 October 2018'),
     contributors: [emallson],
-    changes: <React.Fragment>Added support for <SpellLink id={SPELLS.INSPIRING_VANGUARD.id} />, including the ability to exactly detect <SpellLink id={SPELLS.GRAND_CRUSADER.id} /> resets when this trait is taken.</React.Fragment>,
+    changes: <>Added support for <SpellLink id={SPELLS.INSPIRING_VANGUARD.id} />, including the ability to exactly detect <SpellLink id={SPELLS.GRAND_CRUSADER.id} /> resets when this trait is taken.</>,
   },
 ];
