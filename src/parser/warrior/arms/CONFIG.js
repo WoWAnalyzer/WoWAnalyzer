2cac46650c43076997ae3b2a4d49987231b086a0
Chris Grasty <chris.grasty@gmail.com>
2018-10-12 13:51:07 -0500

    [Arms Warrior] A Little Bit of Cleanup
    - Fixed some spell IDs that were not correct (according to the console alerts about missing spells from the spellbook
    - Updated Config/Contributors
    - Fixed some Ability information

--- src/parser/warrior/arms/CONFIG.js
+++ src/parser/warrior/arms/CONFIG.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import { Aelexe } from 'CONTRIBUTORS';
+import { Sharrq } from 'CONTRIBUTORS';
 import SPECS from 'game/SPECS';
 import Warning from 'interface/common/Alert/Warning';
 
@@ -8,7 +8,7 @@ import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
-  contributors: [],
+  contributors: [Sharrq],
   // The WoW client patch this spec was last updated to be fully compatible with.
   patchCompatibility: '7.3.5',
   // If set to  false`, the spec will show up as unsupported.
@@ -17,17 +17,18 @@ export default {
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
     <>
-      Hey I've been hard at work making this analyzer for you. I hope the suggestions give you useful pointers to improve your performance. Remember: focus on improving only one or two important things at a time. Improving isn't easy and will need your full focus until it becomes second nature to you.<br /><br />
+      <>
+      Hello Everyone! We are always looking to improve the Arms Warrior Analyzers and Modules; so if you find any issues or if there is something missing that you would like to see added, please open an Issue on GitHub or send a message to Sharrq on Discord (Sharrq#7530) <br /> <br />
+	    Additionally, if you need further assistance in improving your gameplay as an Arms Warrior, you can refer to the following resources:<br />
+      <a href="https://discord.gg/0pYY7932lTH4FHW6" target="_blank" rel="noopener noreferrer">Warrior Class Discord</a> <br />
+      <a href="https://www.icy-veins.com/wow/arms-warrior-pve-dps-guide" target="_blank" rel="noopener noreferrer">Icy Veins (Arms Warrior Guide)</a> <br /><br />
+    </>
 
       <Warning>
-        Hey there! A good basis has been implemented for this spec, but it needs to be fleshed out more to provide all the feedback possible.<br /><br />
+        Hello everyone! My name is <kbd>@Sharrq</kbd> and I am currently working on adding support for this spec with the help of the experts and theorycrafters in the Warrior Discord. While I do not play warrior (and never have), the experts and theorycrafters in the Warrior Class Discord were nice enough to assist with provide the suggestions and feedback that I will be adding to the spec. <br /><br />
 
-        This spec needs a focused maintainer. If you want to give it a try, check <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> for more information.
+        If you main an Arms Warrior and would like to contribute to this spec on a more permanent basis, check our <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> for more information.
       </Warning><br />
-
-      <Warning>
-        Hi <kbd>@Zerotorescue</kbd> here. I updated this spec to be compatible with BFA, but I'm no Arms expert. If there's anything wrong please @poke me on Discord and I'll fix it if I can.
-      </Warning>
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
