d5f129f3d381df2219120fed9f18ad736a11842e
Martijn Hols <git@martijnhols.nl>
2018-10-13 15:20:24 +0200

    Update header to indicate this is the prepatch version

--- src/Interface/Others/Header.js
+++ src/Interface/Others/Header.js
@@ -43,7 +43,7 @@ class Header extends React.PureComponent {
         <div className="container image-overlay">
           <div className="row">
             <div className="col-lg-6 col-md-10">
-              <h1><Trans>WoW&shy;Analyzer</Trans></h1>
+              <h1><Trans>Prepatch WoW&shy;Analyzer</Trans></h1>
               <div className="description">
                 <Trans>Analyze your raid logs to get personal suggestions and metrics to improve your performance. Just enter a Warcraft Logs report:</Trans>
               </div>
@@ -70,6 +70,10 @@ class Header extends React.PureComponent {
                   )}
                 </div>
               )}
+
+              <div style={{ fontSize: '1.2em', fontWeight: 600, marginTop: 5 }} className={`alert alert-info`}>
+                Welcome to the WoWAnalyzer for the Battle for Azeroth Prepatch. <a href="https://wowanalyzer.com">Click here</a> to switch to today's version.
+              </div>
                 
               {/*{process.env.NODE_ENV !== 'test' && <ServiceStatus style={{ marginBottom: 5 }} />}*/}
               <div className="about">
