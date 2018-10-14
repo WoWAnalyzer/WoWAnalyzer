91ed4da313d0117fb76994b91df7750c30a97e5c
Martijn Hols <git@martijnhols.nl>
2018-10-14 12:54:54 +0200

    Use default Docker username and password

--- .docker/production/deploy.sh
+++ .docker/production/deploy.sh
@@ -10,7 +10,7 @@ export DEPLOY_TAG=$(
 
 echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
 
-echo "$DOCKER_PASSWORD_2" | docker login -u "$DOCKER_USERNAME_2" --password-stdin;
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 docker tag wowanalyzer $REPO:$BUILD_TAG;
 docker push $REPO:$BUILD_TAG;
 docker tag $REPO:$BUILD_TAG $REPO:$DEPLOY_TAG;
