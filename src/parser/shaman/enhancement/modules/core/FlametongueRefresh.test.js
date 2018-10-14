2caf06ea41f7f4d41702dd30ff903212ae1f6889
Blanton, Taylor <matthewtblanton@gmail.com>
2018-10-14 09:53:01 -0500

    Added test for en/disabling FlametongueRefresh based on Searing Assault

---
+++ src/parser/shaman/enhancement/modules/core/FlametongueRefresh.test.js
@@ -0,0 +1,30 @@
+import FlametongueRefresh from './FlametongueRefresh';
+import SPELLS from 'common/SPELLS';
+
+describe('Flametongue Refresh module', () => {
+    it('should be disabled when Searing assault is taken', () => {
+        const ownerWithSearingAssault = {
+            selectedCombatant: {
+                hasTalent: jest.fn(() => true),
+            },
+            addEventListener: jest.fn(),
+        };
+
+        const module = new FlametongueRefresh({owner: ownerWithSearingAssault});
+        expect(module.active).toBe(false);
+        expect(ownerWithSearingAssault.selectedCombatant.hasTalent).toHaveBeenCalledWith(SPELLS.SEARING_ASSAULT_TALENT.id);
+    });
+
+    it('should be enabled when Searing assault is not taken', () => {
+        const ownerWithSearingAssault = {
+            selectedCombatant: {
+                hasTalent: jest.fn(() => false),
+            },
+            addEventListener: jest.fn(),
+        };
+
+        const module = new FlametongueRefresh({owner: ownerWithSearingAssault});
+        expect(module.active).toBe(true);
+        expect(ownerWithSearingAssault.selectedCombatant.hasTalent).toHaveBeenCalledWith(SPELLS.SEARING_ASSAULT_TALENT.id);
+    });
+});
