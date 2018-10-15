e670cc7bcd5e7fd155971c76ec9203b2c201baca
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-10-15 08:10:06 +0200

    [WIP] Stacked uptime, shows unreal numbers (250% uptime etc.) yet

--- src/parser/warlock/affliction/modules/talents/ShadowEmbrace.js
+++ src/parser/warlock/affliction/modules/talents/ShadowEmbrace.js
@@ -8,6 +8,7 @@ import getDamageBonus from 'parser/monk/brewmaster/modules/core/GetDamageBonus';
 import StatisticListBoxItem from 'interface/others/StatisticListBoxItem';
 import SpellLink from 'common/SpellLink';
 import { formatThousands } from 'common/format';
+import { encodeTargetString } from 'parser/shared/modules/EnemyInstances';
 
 const BONUS_PER_STACK = 0.03;
 
@@ -17,6 +18,17 @@ class ShadowEmbrace extends Analyzer {
   };
 
   damage = 0;
+
+  debuffs = {
+    /*
+    [target string]: {
+      [stack]: {
+        start: timestamp,
+        uptime: time
+      }
+    }
+     */
+  };
 
   constructor(...args) {
     super(...args);
@@ -35,7 +47,69 @@ class ShadowEmbrace extends Analyzer {
     this.damage += getDamageBonus(event, shadowEmbrace.stacks * BONUS_PER_STACK);
   }
 
+  get defaultObj() {
+    return {
+      0: {
+        // ignored, see comment in changedebuffstack handler
+        start: null,
+        uptime: 0,
+      },
+      1: {
+        start: null,
+        uptime: 0,
+      },
+      2: {
+        start: null,
+        uptime: 0,
+      },
+      3: {
+        start: null,
+        uptime: 0,
+      },
+    };
+  }
+
+  on_byPlayer_changedebuffstack(event) {
+    if (event.ability.guid !== SPELLS.SHADOW_EMBRACE_DEBUFF.id) {
+      return;
+    }
+    if (event.targetIsFriendly) {
+      return;
+    }
+    const target = encodeTargetString(event.targetID, event.targetInstance);
+    this.debuffs[target] = this.debuffs[target] || this.defaultObj;
+    // add uptime on oldStacks, start counting uptime on newStacks
+    this.debuffs[target][event.oldStacks].uptime += event.timestamp - this.debuffs[target][event.oldStacks].start;
+    this.debuffs[target][event.oldStacks].start = null;
+    this.debuffs[target][event.newStacks].start = event.timestamp;
+  }
+
+  get stackedUptime() {
+    const duration = this.owner.fightDuration;
+    const uptimes = {
+      1: 0,
+      2: 0,
+      3: 0,
+    };
+    Object.values(this.debuffs).forEach((target) => {
+      // I got confused trying to iterate through the objects, and this is easier, albeit verbose
+      uptimes[1] += target[1].uptime;
+      uptimes[2] += target[2].uptime;
+      uptimes[3] += target[3].uptime;
+    });
+    Object.keys(uptimes).forEach(key => {
+      uptimes[key] /= duration;
+    });
+    // we ignore the 0-stack part from this.debuffs:
+    // 1) it's unknown when to initialize (encounter start? add spawn?)
+    // 2) uptime without stacks is easier to calculate as 1 - "anyStackUptimePercentage"
+    const totalUptimePercentage = this.enemies.getBuffUptime(SPELLS.SHADOW_EMBRACE_DEBUFF.id) / duration;
+    uptimes[0] = 1 - totalUptimePercentage;
+    return uptimes;
+  }
+
   subStatistic() {
+    console.log('Stacked uptime', this.stackedUptime);
     return (
       <StatisticListBoxItem
         title={<><SpellLink id={SPELLS.SHADOW_EMBRACE_TALENT.id} /> bonus damage</>}
