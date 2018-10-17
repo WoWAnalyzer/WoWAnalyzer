5001a1d22d83be525ec6d62b7f86986f8c13aa63
niseko <rentz@hotmail.de>
2018-10-17 16:02:47 +0200

    change wording on the tooltip

--- src/interface/others/DeathRecap.js
+++ src/interface/others/DeathRecap.js
@@ -144,7 +144,7 @@ class DeathRecap extends React.PureComponent {
                           `You healed yourself for ${formatNumber(event.amount)}` :
                           `${sourceName} healed you for ${formatNumber(event.amount)}`
                           }
-                          ${event.absorbed > 0 ? ` and ${formatNumber(event.absorbed)} of that healing was absorbed<br/>` : ''}
+                          ${event.absorbed > 0 ? `, ${formatNumber(event.absorbed)} of that healing was absorbed` : ''}
                           ${event.overheal > 0 ? ` and overhealed for ${formatNumber(event.overheal)}<br/>` : ''}
                         `} style={(event.amount === 0 && event.absorbed > 0) ? {color: 'orange'} : {color: 'green'}}>
                           +{formatNumber(event.amount)} {event.absorbed > 0 ? `(A: ${formatNumber(event.absorbed)} )` : ''} {event.overheal > 0 ? `(O: ${formatNumber(event.overheal)} )` : ''}
