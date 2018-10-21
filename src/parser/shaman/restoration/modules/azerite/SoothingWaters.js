67d8b4db85bd519d252319ac58719795892c104f
niseko <rentz@hotmail.de>
2018-10-21 09:58:05 +0200

    made soothing waters less complicated

--- src/parser/shaman/restoration/modules/azerite/SoothingWaters.js
+++ src/parser/shaman/restoration/modules/azerite/SoothingWaters.js
@@ -1,10 +1,14 @@
-import HIT_TYPES from 'game/HIT_TYPES';
 import SPELLS from 'common/SPELLS';
 import StatTracker from 'parser/shared/modules/StatTracker';
 import CritEffectBonus from 'parser/shared/modules/helpers/CritEffectBonus';
 import BaseHealerAzerite from './BaseHealerAzerite';
 
-const HEAL_WINDOW_MS = 150;
+const HEAL_WINDOW_MS = 100;
+
+/**
+ * Soothing Waters:
+ * Your Chain Heal heals its primary target for an additional 756 healing.
+ */
 
 class SoothingWaters extends BaseHealerAzerite {
   static dependencies = {
@@ -14,13 +18,14 @@ class SoothingWaters extends BaseHealerAzerite {
   static TRAIT = SPELLS.SOOTHING_WATERS_TRAIT.id;
   static HEAL = SPELLS.SOOTHING_WATERS_TRAIT.id;
 
-  buffer = [];
   traitRawHealing = 0;
-  intellectOnHeal = 0;
+
+  chainHealEvent;
+  chainHealTarget;
 
   constructor(...args) {
     super(...args);
-    this.active = this.selectedCombatant.hasTrait(this.constructor.TRAIT);
+    this.active = this.hasTrait;
     if (!this.active) {
       return;
     }
@@ -28,9 +33,28 @@ class SoothingWaters extends BaseHealerAzerite {
     this.traitRawHealing = this.azerite.reduce((total, trait) => total + trait.rawHealing, 0);
   }
 
-  // Similar to the High Tide module, we can't use the actual chain heal events as they're in the wrong order.
-  // We're buffering the events, calculate the base healing and find out which is the initial one by finding the biggest healing event.
-  // more information on the "why" here: https://github.com/WoWAnalyzer/WoWAnalyzer/blob/master/src/parser/shaman/restoration/modules/talents/HighTide.js#L55
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+
+    if (spellId !== SPELLS.CHAIN_HEAL.id) {
+      return;
+    }
+
+    // filtering out healing events if they're not from the current cast
+    if (this.chainHealEvent && event.timestamp - this.chainHealEvent.timestamp >= HEAL_WINDOW_MS) {
+      this.chainHealEvent = undefined;
+    }
+
+    // sometimes you have heal events before the cast happens, so check here as well
+    // comparing the target ID guarantees no false positives as each cast can only heal the same target once
+    if (this.chainHealEvent && this.chainHealEvent.targetID === event.targetID) {
+      this.processTrait(this.chainHealEvent);
+      this.chainHealEvent = undefined;
+    } else {
+      this.chainHealTarget = event.targetID;
+    }
+  }
+
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
 
@@ -38,44 +62,23 @@ class SoothingWaters extends BaseHealerAzerite {
       return;
     }
 
-    // Detects new chain heal casts, can't use the cast event for this as its often somewhere in between its healing events
-    if (!this.chainHealTimestamp || event.timestamp - this.chainHealTimestamp > HEAL_WINDOW_MS) {
-      this.processBuffer();
-      this.chainHealTimestamp = event.timestamp;
+    // we only want the initial heal - as that is the heal on the person you target on the cast,
+    // you only have to compare the heal and target ID.
+    if (this.chainHealTarget && this.chainHealTarget === event.targetID) {
+      this.processTrait(event);
+      this.chainHealTarget = undefined;
+    } else {
+      this.chainHealEvent = event;
     }
-
-    let heal = event.amount + (event.absorb || 0) + (event.overheal || 0);
-    if (event.hitType === HIT_TYPES.CRIT) {
-      const critMult = this.critEffectBonus.getBonus(event);
-      heal /= critMult;
-    }
-    const currentMastery = this.statTracker.currentMasteryPercentage;
-    const masteryEffectiveness = Math.max(0, 1 - (event.hitPoints - event.amount) / event.maxHitPoints);
-    const baseHealingDone = heal / (1 + currentMastery * masteryEffectiveness);
-    this.intellectOnHeal = this.statTracker.currentIntellectRating;
-
-    this.buffer.push({
-      baseHealingDone: baseHealingDone,
-      ...event,
-    });
   }
 
-  processBuffer() {
-    if (!this.buffer[0]) {
-      return;
-    }
-    // we only need the event with the highest healing done, which is the initial
-    const initialHitEvent = this.buffer.reduce((prev, curr) => prev.baseHealingDone > curr.baseHealingDone ? prev : curr);
-
-    const initialHitHealing = SPELLS.CHAIN_HEAL.coefficient * this.intellectOnHeal;
+  processTrait(initialHitEvent) {
+    this.healingtotal += initialHitEvent.amount + (initialHitEvent.absorbed || 0);
+    const currentIntellect = this.statTracker.currentIntellectRating;
+    const initialHitHealing = SPELLS.CHAIN_HEAL.coefficient * currentIntellect;
     const traitComponent = this.traitRawHealing / (initialHitHealing + this.traitRawHealing);
 
     this.processHealing(initialHitEvent, traitComponent);
-    this.buffer = [];
-  }
-
-  on_finished() {
-    this.processBuffer();
   }
 }
 
