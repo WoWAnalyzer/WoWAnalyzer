93b37569861e071325fd147708ad2db1b99c1d9a
TJ Moats <tj@moats.io>
2018-10-26 09:34:46 -0700

    cleaning up trait

--- src/parser/shared/modules/spells/bfa/azeritetraits/SynergisticGrowth.js
+++ src/parser/shared/modules/spells/bfa/azeritetraits/SynergisticGrowth.js
@@ -4,18 +4,14 @@ import {formatPercentage, formatNumber} from 'common/format';
 import { calculateAzeriteEffects } from 'common/stats';
 import SPELLS from 'common/SPELLS/index';
 import TraitStatisticBox, { STATISTIC_ORDER } from 'interface/others/TraitStatisticBox';
-import StatWeights from '../../../../../druid/restoration/modules/features/StatWeights';
 
 /**
  Casting Wild Growth grants you 165 Mastery for 10 sec sec. This cannot occur more than once every 30 sec.
  Example Log: /report/nfVcdAMXFGBgwvzp/2-Heroic+Taloc+-+Kill+(3:55)/21-Palamx
  */
 class SynergisticGrowth extends Analyzer{
-  static dependencies = {
-    statWeights: StatWeights,
-  };
-
   masteryBuff = 0;
+  procs = 0;
 
   constructor(...args){
     super(...args);
@@ -30,6 +26,10 @@ class SynergisticGrowth extends Analyzer{
     return this.selectedCombatant.getBuffUptime(SPELLS.SYNERGISTIC_GROWTH_BUFF.id) / this.owner.fightDuration;
   }
 
+  get totalBuffProcs() {
+    return this.selectedCombatant.getBuffTriggerCount(SPELLS.SYNERGISTIC_GROWTH_BUFF.id);
+  }
+
   get averageStatGain(){
     const averageStacks = this.selectedCombatant.getStackWeightedBuffUptime(SPELLS.SYNERGISTIC_GROWTH_BUFF.id) / this.owner.fightDuration;
     return averageStacks * this.masteryBuff;
@@ -40,12 +40,11 @@ class SynergisticGrowth extends Analyzer{
       <TraitStatisticBox
         position={STATISTIC_ORDER.OPTIONAL()}
         trait={SPELLS.SYNERGISTIC_GROWTH.id}
-        value={(
-          <>
-            {formatPercentage(this.totalBuffUptime)}% uptime<br />
-            {formatNumber(this.averageStatGain)} average mastery gained.
-          </>
-        )}
+        value={`${formatNumber(this.averageStatGain)} average Mastery`}
+        tooltip={`
+          ${formatPercentage(this.totalBuffUptime)}% Uptime<br />
+          ${this.totalBuffProcs} Total Procs
+        `}
       />
     );
   }
