0c72dda0830dc2cced0e3fd1c17bc3e9777525ee
Aveline Estié <hello@aveline.be>
2018-10-28 10:37:37 +0100

    Add proper stat tracking based on Blood Rite / Overwhelming Power traits

--- src/parser/shared/modules/spells/bfa/azeritetraits/Tradewinds.js
+++ src/parser/shared/modules/spells/bfa/azeritetraits/Tradewinds.js
@@ -1,50 +1,68 @@
 import React from 'react';
 import Analyzer from 'parser/core/Analyzer';
-import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS/index';
+import { formatPercentage } from 'common/format';
+import { calculateAzeriteEffects } from 'common/stats';
 import TraitStatisticBox, { STATISTIC_ORDER } from 'interface/others/TraitStatisticBox';
+
+const tradewindsStats = traits => Object.values(traits).reduce((obj, rank) => {
+  const [mastery] = calculateAzeriteEffects(SPELLS.TRADEWINDS.id, rank);
+  obj.mastery += mastery;
+  return obj;
+}, {
+  mastery: 0,
+});
+
+export const STAT_TRACKER = {
+  mastery: combatant => {
+    return tradewindsStats(combatant.traitsBySpellId[SPELLS.TRADEWINDS.id]).mastery;
+  },
+};
 
 /**
  * Your spells and abilities have a chance to grant you 583 Mastery for 15 sec. 
  * When this effect expires it jumps once to a nearby ally, granting them 115 Mastery for 8 sec.
  */
 class Tradewinds extends Analyzer {
-  proccedBuffs = 0;
-  refreshedBuffs = 0;
+  mastery = 0;
+  procs = 0;
 
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTrait(SPELLS.TRADEWINDS.id);
+
+    if (!this.active) {
+      return;
+    }
+
+    const { mastery } = tradewindsStats(this.selectedCombatant.traitsBySpellId[SPELLS.TRADEWINDS.id]);
+    this.mastery = mastery;
   }
 
   on_byPlayer_applybuff(event) {
-    const spellId = event.ability.guid;
-
-    if (spellId !== SPELLS.TRADEWINDS.id) {
-      return;
-    }
-
-    this.proccedBuffs++;
+    this.handleBuff(event);
   }
 
   on_byPlayer_refreshbuff(event) {
-    const spellId = event.ability.guid;
-    if (spellId !== SPELLS.TRADEWINDS.id) {
+    this.handleBuff(event);
+  }
+
+  get uptime() {
+    return this.selectedCombatant.getBuffUptime(SPELLS.TRADEWINDS.id) / this.owner.fightDuration;
+  }
+
+  get averageMastery() {
+    return (this.mastery * this.uptime).toFixed(0);
+  }
+
+  handleBuff(event) {
+    if (event.ability.guid !== SPELLS.TRADEWINDS.id) {
       return;
     }
 
-    this.refreshedBuffs++;
-  }
-  get totalBuffs() {
-    return this.proccedBuffs + this.refreshedBuffs;
-  }
-
-  get procsPerMinute() {
-    return this.totalBuffs / (this.owner.fightDuration / 60000);
-  }
-
-  get uptimePct() {
-    return (this.selectedCombatant.getBuffUptime(SPELLS.TRADEWINDS.id) / this.owner.fightDuration) * 100;
+    if (event.type === "applybuff" || event.type === "refreshbuff") {
+      this.procs++;
+    }
   }
 
   statistic() {
@@ -52,14 +70,11 @@ class Tradewinds extends Analyzer {
       <TraitStatisticBox
         position={STATISTIC_ORDER.OPTIONAL()}
         trait={SPELLS.TRADEWINDS.id}
-        value={(
-          <>
-          {this.uptimePct.toFixed(0)}% uptime<br />
-          {this.procsPerMinute.toFixed(2)} procs per minute
-          </>)}
-        tooltip={`Procced <b>${formatNumber(this.totalBuffs)}</b> times,
-                  <b>${formatNumber(this.refreshedBuffs)}</b> were refreshes.
-                `}
+        value={`${this.averageMastery} average Mastery`}
+        tooltip={`
+        ${SPELLS.TRADEWINDS.name} grants <b>${this.mastery} mastery</b> while active.<br/>
+        You had <b>${this.procs} ${SPELLS.TRADEWINDS.name} procs</b> resulting in ${formatPercentage(this.uptime)}% uptime.
+        `}
       />
     );
   }
