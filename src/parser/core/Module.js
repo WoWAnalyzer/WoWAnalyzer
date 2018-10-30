3eb000ebe6fc431bae28efd5f10b2e1cd979cc26
Martijn Hols <git@martijnhols.nl>
2018-10-30 21:06:53 +0100

    Throw an error when missing required properties in a Module

--- src/parser/core/Module.js
+++ src/parser/core/Module.js
@@ -20,6 +20,10 @@ class Module {
   constructor({ owner, priority, ...others }) {
     if (this.constructor.__dangerousInvalidUsage) {
       throw new TypeError('The class Module can not be used directly, you probably want to use Analyzer instead.');
+    } else if (!owner) {
+      throw new Error('Missing option: owner');
+    } else if (priority === undefined) {
+      throw new Error('Missing option: priority');
     }
 
     this.owner = owner;
