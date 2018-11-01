272040108e5de2d058975144f94f5c917ef4ec91
Robby Groot <robby@vetomedia.nl>
2018-11-01 09:13:28 +0100

    [Beast Mastery] Implement the suggestions made by @Pewtro regarding the Killer Instinct module

--- src/parser/hunter/beastmastery/modules/talents/KillerInstinct.js
+++ src/parser/hunter/beastmastery/modules/talents/KillerInstinct.js
@@ -4,16 +4,21 @@ import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
 import TalentStatisticBox from 'interface/others/TalentStatisticBox';
 import ItemDamageDone from 'interface/others/ItemDamageDone';
 import StatisticListBoxItem from 'interface/others/StatisticListBoxItem';
+import calculateEffectiveDamage from 'parser/core/calculateEffectiveDamage';
 import SpellIcon from 'common/SpellIcon';
 
 import SPELLS from 'common/SPELLS';
 import Analyzer from 'parser/core/Analyzer';
 import { formatNumber } from 'common/format';
 
-
 const KILLER_INSTINCT_TRESHOLD = 0.35;
 const KILLER_INSTINCT_CONTRIBUTION = 0.5;
 
+/**
+ * Kill Command deals 50% increased damage against enemies below 35% health.
+ *
+ * Example log: https://www.warcraftlogs.com/reports/CznQpdmRFBJkjg4w/#fight=40&source=16&type=damage-done
+ */
 class KillerInstinct extends Analyzer {
   casts = 0;
   castsWithExecute = 0;
@@ -35,10 +40,7 @@ class KillerInstinct extends Analyzer {
     if (enemyHealthPercent <= KILLER_INSTINCT_TRESHOLD) {
       this.castsWithExecute++;
 
-      const totalDamage = (event.amount + event.absorbed);
-      const baseDamage = totalDamage / (1 + KILLER_INSTINCT_CONTRIBUTION);
-      const traitDamage = totalDamage - baseDamage;
-
+      const traitDamage = calculateEffectiveDamage(event, KILLER_INSTINCT_CONTRIBUTION);
       this.damage += traitDamage;
     }
   }
@@ -50,8 +52,8 @@ class KillerInstinct extends Analyzer {
         icon={<SpellIcon id={SPELLS.KILLER_INSTINCT_TALENT.id} />}
         value={<>{formatNumber(this.castsWithExecute)} casts at &lt;35% health</>}
         label="Killer Instinct"
-        tooltip={`You've casted a total of ${this.casts} Kill Commands, of which ${this.castsWithExecute} were on enemies with less than 35% of their health remaining.
-                  These ${this.castsWithExecute} casts have provided you a total of ${formatNumber(this.damage)} extra damage throughout the fight.`}
+        tooltip={`You cast a total of ${this.casts} Kill Commands, of which ${this.castsWithExecute} were on enemies with less than 35% of their health remaining.
+                  These ${this.castsWithExecute} casts provided you a total of ${formatNumber(this.damage)} extra damage throughout the fight.`}
       />
     );
   }
