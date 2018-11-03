a6c7e7263aae85451b7aaa878e4f61f1425b0968
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-03 21:07:38 +0100

    Implemented Checklist for Destruction Warlocks.

--- src/parser/warlock/destruction/CHANGELOG.js
+++ src/parser/warlock/destruction/CHANGELOG.js
@@ -7,6 +7,11 @@ import SPELLS from 'common/SPELLS';
 
 export default [
   {
+    date: new Date('2018-11-03'),
+    changes: 'Implemented Checklist for Destruction Warlocks.',
+    contributors: [Chizu],
+  },
+  {
     date: new Date('2018-11-02'),
     changes: <><SpellLink id={SPELLS.FIRE_AND_BRIMSTONE_TALENT.id} /> should now correctly track bonus fragments and cleaved damage again.</>,
     contributors: [Chizu],
