da28c95b1471cef1034372fdd7da855a232a9d81
mathieu <mathieu.carlier@viacesi.fr>
2018-11-04 20:27:11 +0100

    Updated CONFIG

--- src/parser/warrior/arms/CONFIG.js
+++ src/parser/warrior/arms/CONFIG.js
@@ -31,7 +31,7 @@ export default {
   // The contents of your changelog.
   changelog: CHANGELOG,
   // The CombatLogParser class for your spec.
-  parser: () => import('./CombatLogParser' /* webpackChunkName: "Warrior" */).then(exports => exports.default),
+  parser: () => import('./CombatLogParser' /* webpackChunkName: "ArmsWarrior" */).then(exports => exports.default),
   // The path to the current directory (relative form project root). This is used for generating a GitHub link directly to your spec's code.
   path: __dirname,
 };
