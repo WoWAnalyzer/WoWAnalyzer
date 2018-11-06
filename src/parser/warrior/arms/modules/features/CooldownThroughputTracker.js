c3de0156c5ca5903ff6a9a2fb8b380f727be254f
mathieu <mathieu.carlier@viacesi.fr>
2018-11-06 14:32:40 +0100

    Reworked Colossus Smash & Warbreaker, added Avatar & Deadly Calm

--- src/parser/warrior/arms/modules/features/CooldownThroughputTracker.js
+++ src/parser/warrior/arms/modules/features/CooldownThroughputTracker.js
@@ -5,19 +5,67 @@ class CooldownThroughputTracker extends CoreCooldownThroughputTracker {
   static cooldownSpells = [
     ...CooldownThroughputTracker.cooldownSpells,
     {
-      // Debuff applied by Colossus Smash and Warbreaker
-      spell: SPELLS.COLOSSUS_SMASH_DEBUFF,
-      summary: [
-        BUILT_IN_SUMMARY_TYPES.DAMAGE,
-      ],
-    },
-    {
       spell: SPELLS.SWEEPING_STRIKES,
       summary: [
         BUILT_IN_SUMMARY_TYPES.DAMAGE,
       ],
     },
+    {
+      spell: SPELLS.AVATAR_TALENT,
+      summary: [
+        BUILT_IN_SUMMARY_TYPES.DAMAGE,
+      ],
+    },
+    {
+      spell: SPELLS.DEADLY_CALM_TALENT,
+      summary: [
+        BUILT_IN_SUMMARY_TYPES.DAMAGE,
+      ],
+    },
   ];
+
+  static castCooldowns = [
+    {
+      spell: SPELLS.WARBREAKER_TALENT,
+      duration: 10,
+      summary: [
+        BUILT_IN_SUMMARY_TYPES.DAMAGE,
+      ],
+    },
+    {
+      spell: SPELLS.COLOSSUS_SMASH,
+      duration: 10,
+      summary: [
+        BUILT_IN_SUMMARY_TYPES.DAMAGE,
+      ],
+    },
+  ];
+
+  on_byPlayer_cast(event) {
+    const spellId = event.ability.guid;
+    const cooldownSpell = this.constructor.castCooldowns.find(cooldownSpell => cooldownSpell.spell.id === spellId);
+    if (cooldownSpell) {
+      const cooldown = this._addFixedCooldown(cooldownSpell, event.timestamp);
+      this.activeCooldowns.push(cooldown);
+    }
+    super.on_byPlayer_cast && super.on_byPlayer_cast(event);
+  }
+
+  _addFixedCooldown(cooldownSpell, timestamp) {
+    const cooldown = {
+      ...cooldownSpell,
+      start: timestamp,
+      end: timestamp + cooldownSpell.duration * 1000,
+      events: [],
+    };
+    this.pastCooldowns.push(cooldown);
+    return cooldown;
+  }
+
+  trackEvent(event) {
+    this.activeCooldowns = this.activeCooldowns.filter(cooldown => !cooldown.end || event.timestamp < cooldown.end);
+    super.trackEvent(event);
+  }
 }
 
 export default CooldownThroughputTracker;
