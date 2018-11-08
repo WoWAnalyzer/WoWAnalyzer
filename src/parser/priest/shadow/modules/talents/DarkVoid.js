b8f37402b4b0d9ef8c9f29009eef6d80c58a2fb6
James Moats <tjmoats@demark.com>
2018-11-08 13:12:16 -0700

    Adding recomendation

--- src/parser/priest/shadow/modules/talents/DarkVoid.js
+++ src/parser/priest/shadow/modules/talents/DarkVoid.js
@@ -10,6 +10,7 @@ import ItemDamageDone from 'interface/others/ItemDamageDone';
 import { formatNumber } from 'common/format';
 import AbilityTracker from 'parser/priest/shadow/modules/core/AbilityTracker';
 import ShadowWordPain from 'parser/priest/shadow/modules/spells/ShadowWordPain';
+import SpellLink from 'common/SpellLink';
 
 // Example Log: /report/ZdaVqwLnvKHmcG7P/34-Heroic+Zul+-+Kill+(4:06)/16-매기사제
 class DarkVoid extends Analyzer {
@@ -69,6 +70,27 @@ class DarkVoid extends Analyzer {
         position={STATISTIC_ORDER.CORE(3)}
       />
     );
+  }
+
+  get uptimeSuggestionThresholds() {
+    return {
+      actual: this.averageTargetsHit,
+      isLessThan: {
+        minor: 3,
+        major: 2,
+      },
+      style: 'number',
+    };
+  }
+
+  suggestions(when) {
+    when(this.uptimeSuggestionThresholds)
+      .addSuggestion((suggest, actual, recommended) => {
+        return suggest(<>Try and cast <SpellLink id={SPELLS.DARK_VOID_TALENT.id} /> when there are more targets to hit.</>)
+          .icon(SPELLS.DARK_VOID_TALENT.icon)
+          .actual(`You hit ${actual} target on average per cast of ${SPELLS.DARK_VOID_TALENT.name}.`)
+          .recommended(`You should try and hit ${recommended} or more targets per cast. Try saving the cooldown for add heavy phases.`);
+      });
   }
 }
 
