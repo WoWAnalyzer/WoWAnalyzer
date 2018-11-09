926e8b26921a807649df642391ab413fd6e2583a
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-10 00:23:43 +0100

    Changelog

--- src/parser/warlock/demonology/CHANGELOG.js
+++ src/parser/warlock/demonology/CHANGELOG.js
@@ -6,6 +6,11 @@ import SPELLS from 'common/SPELLS';
 
 export default [
   {
+    date: new Date('2018-11-10'),
+    changes: <>Added a Pet Timeline tab, allowing you to see your demons' lifespans and highlighting important casts, such as <SpellLink id={SPELLS.NETHER_PORTAL_TALENT.id} />, <SpellLink id={SPELLS.SUMMON_DEMONIC_TYRANT.id} /> and <SpellLink id={SPELLS.IMPLOSION_CAST.id} />.</>,
+    contributors: [Chizu],
+  },
+  {
     date: new Date('2018-11-08'),
     changes: <>Reworked pet tracking system, fixed <SpellLink id={SPELLS.GRIMOIRE_FELGUARD_TALENT.id} /> talent module.</>,
     contributors: [Chizu],
