68ae33afe463b65d73d1c97863732c50638f17ed
Martijn Hols <git@martijnhols.nl>
2018-11-09 23:43:15 +0100

    Track Blizzard API calls in Prometheus

--- server/helpers/retryingRequest.js
+++ server/helpers/retryingRequest.js
@@ -7,14 +7,28 @@ const defaultOptions = {
   maxAttempts: 3,
   reattemptDelay: 300,
   shouldRetry: err => err instanceof RequestError,
+  onBeforeAttempt: null,
+  onSuccess: null,
+  onFailure: null,
 };
 
 async function retryingRequest(options, attempt = 1) {
   try {
     // console.debug('REQUEST', options.url);
-    return await request.get(options);
+    if (options.onBeforeAttempt) {
+      options.onBeforeAttempt(attempt);
+    }
+    const result = await request.get(options);
+    if (options.onSuccess) {
+      options.onSuccess(result, attempt);
+    }
+
+    return result;
   } catch (err) {
     // console.debug('REQUEST', 'ERROR', err.message);
+    if (options.onFailure) {
+      options.onFailure(err, attempt);
+    }
     const shouldRetry = options.shouldRetry || defaultOptions.shouldRetry;
     if (shouldRetry(err)) {
       const maxAttempts = options.maxAttempts || defaultOptions.maxAttempts;
