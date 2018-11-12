7181cdfed42be87bec66e9fb9ac9f2b4f97ad8af
Aelexe <Aelexe@gmail.com>
2018-11-12 14:35:44 +1300

    Add Azerokk's Resonating Heart module

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -9,6 +9,11 @@ import Contributor from 'interface/contributor/Button';
 
 export default [
   {
+    date: new Date('2018-11-12'),
+    changes: <>Added <ItemLink id={ITEMS.AZEROKKS_RESONATING_HEART.id} /> module.</>,
+    contributors: [Aelexe],
+  },
+  {
     date: new Date('2018-11-11'),
     changes: <>Added <ItemLink id={ITEMS.DISC_OF_SYSTEMATIC_REGRESSION.id} /> module.</>,
     contributors: [Matardarix],
