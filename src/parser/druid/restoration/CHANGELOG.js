77f58ce9c6ea1bf454d36b8a2e838bc6681c8027
Michael Bui <micb@redpill-linpro.com>
2018-11-12 09:27:29 +0100

    Updated changelog.js

--- src/parser/druid/restoration/CHANGELOG.js
+++ src/parser/druid/restoration/CHANGELOG.js
@@ -8,6 +8,7 @@ export default [
     changes: 'Added checklist and suggestions for WG with high initial overhealing.',
     contributors: [blazyb],
   },
+  {
     date: new Date('2018-11-06'),
     changes: 'Fixed a minor bug with Flourish module and ordering of event being skewed.',
     contributors: [blazyb],
