d102cc33cdbd07d98b477b9ebf2535d0a72b3114
Michael Bui <micb@redpill-linpro.com>
2018-11-14 13:43:48 +0100

    Resto druid - Added azerite trait rampant growth

--- src/parser/druid/restoration/CHANGELOG.js
+++ src/parser/druid/restoration/CHANGELOG.js
@@ -4,6 +4,11 @@ import { Yajinni, blazyb, fel1ne, Qbz } from 'CONTRIBUTORS';
 
 export default [
   {
+    date: new Date('2018-11-14'),
+    changes: 'Added support for Azerite trait Rampant Growth',
+    contributors: [blazyb],
+  },
+  {
     date: new Date('2018-11-07'),
     changes: 'Added support for random LB blooms in Photosynthesis.',
     contributors: [blazyb],
