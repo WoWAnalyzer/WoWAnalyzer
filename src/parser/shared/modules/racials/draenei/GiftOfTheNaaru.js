8647e4dae6313ab9661f334a179792d837c1f9a5
Dambroda <43309639+Dambroda@users.noreply.github.com>
2018-11-14 12:33:17 -0800

    remove switch statement, replace with spell array

--- src/parser/shared/modules/racials/draenei/GiftOfTheNaaru.js
+++ src/parser/shared/modules/racials/draenei/GiftOfTheNaaru.js
@@ -1,6 +1,5 @@
 import SPELLS from 'common/SPELLS/index';
 import RACES from 'game/RACES';
-import SPECS from 'game/SPECS';
 import Analyzer from 'parser/core/Analyzer';
 import Abilities from 'parser/shared/modules/Abilities';
 
@@ -19,55 +18,9 @@ class GiftOfTheNaaru extends Analyzer {
       return;
     }
 
-    let spell;
-    switch (this.selectedCombatant.spec) {
-      case SPECS.FROST_DEATH_KNIGHT:
-      case SPECS.BLOOD_DEATH_KNIGHT:
-      case SPECS.UNHOLY_DEATH_KNIGHT:
-        spell = SPELLS.GIFT_OF_THE_NAARU_DK;
-        break;
-      case SPECS.SURVIVAL_HUNTER:
-      case SPECS.MARKSMANSHIP_HUNTER:
-      case SPECS.BEAST_MASTERY_HUNTER:
-        spell = SPELLS.GIFT_OF_THE_NAARU_HUNTER;
-        break;
-      case SPECS.WINDWALKER_MONK:
-      case SPECS.MISTWEAVER_MONK:
-      case SPECS.BREWMASTER_MONK:
-        spell = SPELLS.GIFT_OF_THE_NAARU_MONK;
-        break;
-      case SPECS.ARCANE_MAGE:
-      case SPECS.FIRE_MAGE:
-      case SPECS.FROST_MAGE:
-        spell = SPELLS.GIFT_OF_THE_NAARU_MAGE;
-        break;
-      case SPECS.HOLY_PRIEST:
-      case SPECS.SHADOW_PRIEST:
-      case SPECS.DISCIPLINE_PRIEST:
-        spell = SPELLS.GIFT_OF_THE_NAARU_PRIEST;
-        break;
-      case SPECS.HOLY_PALADIN:
-      case SPECS.RETRIBUTION_PALADIN:
-      case SPECS.PROTECTION_PALADIN:
-        spell = SPELLS.GIFT_OF_THE_NAARU_PALADIN;
-        break;
-      case SPECS.RESTORATION_SHAMAN:
-      case SPECS.ELEMENTAL_SHAMAN:
-      case SPECS.ENHANCEMENT_SHAMAN:
-        spell = SPELLS.GIFT_OF_THE_NAARU_SHAMAN;
-        break;
-      case SPECS.FURY_WARRIOR:
-      case SPECS.ARMS_WARRIOR:
-      case SPECS.PROTECTION_WARRIOR:
-        spell = SPELLS.GIFT_OF_THE_NAARU_WARRIOR;
-        break;
-      default:
-        this.error("Invalid spec found for Draenei character: ", this.selectedCombatant.spec);
-        return;
-    }
-
     this.abilities.add({
-      spell: spell,
+      spell: [SPELLS.GIFT_OF_THE_NAARU_DK, SPELLS.GIFT_OF_THE_NAARU_HUNTER, SPELLS.GIFT_OF_THE_NAARU_MONK, SPELLS.GIFT_OF_THE_NAARU_MAGE,
+        SPELLS.GIFT_OF_THE_NAARU_PRIEST, SPELLS.GIFT_OF_THE_NAARU_PALADIN, SPELLS.GIFT_OF_THE_NAARU_SHAMAN, SPELLS.GIFT_OF_THE_NAARU_WARRIOR],
       category: Abilities.SPELL_CATEGORIES.DEFENSIVE,
       cooldown: 180,
       gcd: null,
