0fef0fd58daca362229e147cdedf27dde38d7c04
Juko8 <ulrichenevoldsen@hotmail.com>
2018-11-15 20:42:17 +0100

    Revert "[Ret] Fix judgments being shown consumed on all ticks from a divine storm"
    This reverts commit c6a3f392ad6cb7bd002610f0d1b1228d32645a3e.

--- src/parser/paladin/retribution/modules/core/Judgment.js
+++ src/parser/paladin/retribution/modules/core/Judgment.js
@@ -9,10 +9,6 @@ import SpellLink from 'common/SpellLink';
 import { formatPercentage } from 'common/format';
 import StatisticBox, { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 
-const JUDGMENT_DURATION = 15000;
-
-const HOLY_POWER_ATTACKS = [SPELLS.TEMPLARS_VERDICT_DAMAGE.id, SPELLS.DIVINE_STORM_DAMAGE.id, SPELLS.JUSTICARS_VENGEANCE_TALENT.id];
-
 class Judgment extends Analyzer {
   static dependencies = {
     enemies: Enemies,
@@ -21,73 +17,38 @@ class Judgment extends Analyzer {
   divineStormConsumptions = 0;
   justicarsVengeanceConsumptions = 0;
   judgmentsApplied = 0;
-  judgmentsOverwritten = 0;
-  judgmentTargets = [];
 
   on_byPlayer_applydebuff(event) {
     const spellId = event.ability.guid;
     if (spellId !== SPELLS.JUDGMENT_DEBUFF.id) {
       return;
     }
-    // The event doesn't specify instance on the first target of that type
-    let targetInstance = event.targetInstance;
-    if (targetInstance === undefined) {
-      targetInstance = 1;
-    }
     this.judgmentsApplied += 1;
-    const judgmentTarget = { targetID: event.targetID, targetInstance: targetInstance, timestamp: event.timestamp };
-    this.judgmentTargets.push(judgmentTarget);
-  }
-
-  on_byPlayer_refreshdebuff(event) {
-    const spellId = event.ability.guid;
-    if (spellId !== SPELLS.JUDGMENT_DEBUFF.id) {
-      return;
-    }
-    let targetInstance = event.targetInstance;
-    if (targetInstance === undefined) {
-      targetInstance = 1;
-    }
-    this.judgmentsOverwritten += 1;
-    this.judgmentTargets.forEach((judgmentTarget, index) => {
-      if (judgmentTarget.targetID === event.targetID && judgmentTarget.targetInstance === targetInstance) {
-        judgmentTarget.timestamp = event.timestamp;
-      }
-    });
   }
 
   on_byPlayer_damage(event) {
+    const enemy = this.enemies.getEntity(event);
     const spellId = event.ability.guid;
-    if (!HOLY_POWER_ATTACKS.includes(spellId)) {
+
+    if (!enemy) {
       return;
     }
-    let targetInstance = event.targetInstance;
-    if (targetInstance === undefined) {
-      targetInstance = 1;
+    if (!enemy.hasBuff(SPELLS.JUDGMENT_DEBUFF.id, null, 250)) {
+      return;
     }
-    this.judgmentTargets.forEach((judgmentTarget, index) => {
-      // remove expired judgments
-      const expirationTimestamp = judgmentTarget.timestamp + JUDGMENT_DURATION;
-      if (expirationTimestamp < event.timestamp) {
-        this.judgmentTargets.splice(index, 1);
-      }
-      if (judgmentTarget.targetID === event.targetID && judgmentTarget.targetInstance === targetInstance) {
-        switch (spellId) {
-          case SPELLS.TEMPLARS_VERDICT_DAMAGE.id:
-            this.templarsVerdictConsumptions += 1;
-            break;
-          case SPELLS.DIVINE_STORM_DAMAGE.id:
-            this.divineStormConsumptions += 1;
-            break;
-          case SPELLS.JUSTICARS_VENGEANCE_TALENT.id:
-            this.justicarsVengeanceConsumptions += 1;
-            break;
-          default:
-            break;
-        }
-        this.judgmentTargets.splice(index, 1);
-      }
-    });
+    switch (spellId) {
+      case SPELLS.TEMPLARS_VERDICT_DAMAGE.id:
+        this.templarsVerdictConsumptions += 1;
+        break;
+      case SPELLS.DIVINE_STORM_DAMAGE.id:
+        this.divineStormConsumptions += 1;
+        break;
+      case SPELLS.JUSTICARS_VENGEANCE_TALENT.id:
+        this.justicarsVengeanceConsumptions += 1;
+        break;
+      default:
+        break;
+    }
   }
 
   get judgmentsConsumed() {
@@ -129,10 +90,9 @@ class Judgment extends Analyzer {
         label="Judgments Consumed"
         tooltip={`
           Judgments Applied: ${this.judgmentsApplied}<br>
-          Templars Verdicts Consumptions: ${this.templarsVerdictConsumptions}<br>
-          Divine Storm Consumptions: ${this.divineStormConsumptions}
-          ${justicarsVengeanceText}<br>
-          Judgments Overwritten: ${this.judgmentsOverwritten}`}
+          Templars Verdicts consumptions: ${this.templarsVerdictConsumptions}<br>
+          Divine Storm consumptions: ${this.divineStormConsumptions}
+          ${justicarsVengeanceText}`}
       />
     );
   }
