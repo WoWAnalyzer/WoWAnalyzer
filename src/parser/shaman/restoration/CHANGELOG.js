66bcd0b940ac02c39d18fb333e294bc95582f71b
niseko <rentz@hotmail.de>
2018-11-15 21:36:21 +0100

    update date

--- src/parser/shaman/restoration/CHANGELOG.js
+++ src/parser/shaman/restoration/CHANGELOG.js
@@ -6,7 +6,7 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
-    date: new Date('2018-11-12'),
+    date: new Date('2018-11-15'),
     changes: <>The <SpellLink id={SPELLS.EARTHEN_WALL_TOTEM_TALENT.id} /> module now provides efficiency details on individual casts, and filters out healing on pets.</>,
     contributors: [niseko],
   },
