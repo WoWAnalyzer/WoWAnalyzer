76a5625a52b6dbb5f7d8781dddf9d20c4aa18744
Martijn Hols <github@martijnhols.nl>
2018-11-16 20:22:50 +0100

    Revert "Revert "Move usages of `triggerEvent` and `fabricateEvent` to `EventEmitter` module""

--- src/parser/monk/windwalker/modules/features/checklist/Module.js
+++ src/parser/monk/windwalker/modules/features/checklist/Module.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Analyzer from 'parser/core/Analyzer';
+import BaseChecklist from 'parser/shared/modules/features/Checklist2/Module';
 import CastEfficiency from 'parser/shared/modules/CastEfficiency';
 import Combatants from 'parser/shared/modules/Combatants';
 import PreparationRuleAnalyzer from 'parser/shared/modules/features/Checklist2/PreparationRuleAnalyzer';
@@ -18,7 +18,7 @@ import ChiDetails from '../../chi/ChiDetails';
 
 import Component from './Component';
 
-class Checklist extends Analyzer {
+class Checklist extends BaseChecklist {
   static dependencies = {
     combatants: Combatants,
     castEfficiency: CastEfficiency,
