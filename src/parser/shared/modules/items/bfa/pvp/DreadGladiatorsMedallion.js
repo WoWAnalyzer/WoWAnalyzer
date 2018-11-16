5bb87448e99a6a327cf35eb95ece553995a12301
niseko <rentz@hotmail.de>
2018-11-16 14:44:30 +0100

    add missing medallion ability

--- src/parser/shared/modules/items/bfa/pvp/DreadGladiatorsMedallion.js
+++ src/parser/shared/modules/items/bfa/pvp/DreadGladiatorsMedallion.js
@@ -5,12 +5,16 @@ import ITEMS from 'common/ITEMS/index';
 import Analyzer from 'parser/core/Analyzer';
 import { formatPercentage, formatNumber } from 'common/format';
 import { calculateSecondaryStatDefault } from 'common/stats';
+import Abilities from 'parser/shared/modules/Abilities';
 
 /**
  * Dread Gladiator's Medallion -
  * Use: Increases Versatility by 429 for 20 sec. (2 Min Cooldown)
  */
 class DreadGladiatorsMedallion extends Analyzer {
+  static dependencies = {
+    abilities: Abilities,
+  };
   statBuff = 0;
 
   constructor(...args) {
@@ -19,6 +23,16 @@ class DreadGladiatorsMedallion extends Analyzer {
 
     if(this.active) {
       this.statBuff = calculateSecondaryStatDefault(300, 576, this.selectedCombatant.getItem(ITEMS.DREAD_GLADIATORS_MEDALLION.id).itemLevel);
+      this.abilities.add({
+        spell: SPELLS.RAPID_ADAPTATION,
+        buffSpellId: SPELLS.RAPID_ADAPTATION.id,
+        name: ITEMS.DREAD_GLADIATORS_MEDALLION.name,
+        category: Abilities.SPELL_CATEGORIES.ITEMS,
+        cooldown: 120,
+        castEfficiency: {
+          suggestion: true,
+        },
+      });
     }
   }
 
