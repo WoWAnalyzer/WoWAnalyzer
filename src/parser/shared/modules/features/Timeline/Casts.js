47f8f40863d21edcacef12a6503713bc8ba61d55
Martijn Hols <git@martijnhols.nl>
2018-11-18 23:35:06 +0100

    Stack hoisted spells if they would overlap

--- src/parser/shared/modules/features/Timeline/Casts.js
+++ src/parser/shared/modules/features/Timeline/Casts.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { formatDuration } from 'common/format';
 import Icon from 'common/Icon';
 import SpellLink from 'common/SpellLink';
-import PropTypes from 'prop-types';
+
+const ICON_WIDTH = 22;
 
 class Casts extends React.PureComponent {
   static propTypes = {
@@ -35,6 +37,8 @@ class Casts extends React.PureComponent {
         return null;
     }
   }
+  _lastHoisted = null;
+  _level = 0;
   renderCast(event) {
     const left = this.getOffsetLeft(event.timestamp);
     if (event.channel) {
@@ -42,14 +46,28 @@ class Casts extends React.PureComponent {
     }
 
     const hoist = !event.globalCooldown && event.type !== 'begincast';
+    let level = 0;
+    if (hoist) {
+      const margin = left - this._lastHoisted;
+      if (margin < ICON_WIDTH) {
+        this._level += 1;
+        level = this._level;
+      } else {
+        this._level = 0;
+      }
+      this._lastHoisted = left;
+    }
 
     return (
       <SpellLink
         key={`cast-${event.ability.guid}-${left}`}
         id={event.ability.guid}
         icon={false}
-        className={`cast ${hoist ? 'off-gcd' : ''} ${event.isCancelled ? 'cancelled' : ''}`}
-        style={{ left }}
+        className={`cast ${hoist ? 'hoist' : ''} ${event.isCancelled ? 'cancelled' : ''}`}
+        style={{
+          left,
+          '--level': level > 0 ? level : undefined,
+        }}
       >
         {hoist && (
           <div className="time-indicator" />
