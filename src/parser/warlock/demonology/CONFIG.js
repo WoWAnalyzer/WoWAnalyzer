43fb40078ccf599e681579a204afad5babd6f4c1
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-19 23:25:13 +0100

    Removed the Warning from Demo config

--- src/parser/warlock/demonology/CONFIG.js
+++ src/parser/warlock/demonology/CONFIG.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Chizu } from 'CONTRIBUTORS';
 import retryingPromise from 'common/retryingPromise';
 import SPECS from 'game/SPECS';
-import Warning from 'interface/common/Alert/Warning';
 
 import CHANGELOG from './CHANGELOG';
 
@@ -21,11 +20,6 @@ export default {
       Hello fellow Netherlords! Currently this spec is still in development by me with help of <strong>Motoko</strong> from Warlock Discord. Stay tuned, features will be added over time.<br /> <br />
 
       If you have any questions about Warlocks, feel free to pay a visit to <a href="https://discord.gg/BlackHarvest" target="_blank" rel="noopener noreferrer">Council of the Black Harvest Discord</a> or <a href="https://lockonestopshop.com" target="_blank" rel="noopener noreferrer">Lock One Stop Shop</a>, if you'd like to discuss anything about this analyzer, message me at @Chizu#2873 on WoWAnalyzer Discord.<br /><br />
-
-      <Warning>
-        The Demonology Warlock analysis isn't complete yet. What we do show should be good to use, but it does not show the complete picture.<br />
-        If there is something missing, incorrect, or inaccurate, please report it on <a href="https://github.com/WoWAnalyzer/WoWAnalyzer/issues/new">GitHub</a> or contact us on <a href="https://discord.gg/AxphPxU">Discord</a>.
-      </Warning>
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
