31787c09d49a25079b6c8ecac6dc9a627e46eb31
Martijn Hols <git@martijnhols.nl>
2018-11-20 23:06:32 +0100

    Fix AbilitiesMissing module

--- src/parser/shared/modules/AbilitiesMissing.js
+++ src/parser/shared/modules/AbilitiesMissing.js
@@ -1,15 +1,20 @@
 import EventSubscriber from 'parser/core/EventSubscriber';
+import Abilities from 'parser/core/modules/Abilities';
 import CASTS_THAT_ARENT_CASTS from 'parser/core/CASTS_THAT_ARENT_CASTS';
 
 /**
  * Validate that all spells castable by the player is in the spellbook
  */
 class AbilitiesMissing extends EventSubscriber {
+  static dependencies = {
+    abilities: Abilities,
+  };
+
   on_byPlayer_cast(event) {
     if (!event.ability) {
       return;
     }
-    const ability = this.getAbility(event.ability.guid);
+    const ability = this.abilities.getAbility(event.ability.guid);
     if (!ability && !CASTS_THAT_ARENT_CASTS.includes(event.ability.guid)) {
       console.warn('Ability missing from spellbook:', event.ability);
     }
