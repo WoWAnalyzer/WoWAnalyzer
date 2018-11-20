cdaf2ca70a0ac745fb4e075d9484b0d471157319
Dambroda <43309639+Dambroda@users.noreply.github.com>
2018-11-19 19:58:01 -0800

    fix retpally gcd dep override

--- src/parser/paladin/retribution/modules/core/GlobalCooldown.js
+++ src/parser/paladin/retribution/modules/core/GlobalCooldown.js
@@ -1,20 +1,12 @@
 import CoreGlobalCooldown from 'parser/shared/modules/GlobalCooldown';
 import SPELLS from 'common/SPELLS';
-import Haste from './Haste';
-import Abilities from '../Abilities';
 
-const MIN_GCD= 750;
+const MIN_GCD = 750;
 
 /**
  * The Global Cooldown from Wake of Ashes is currently double dipping haste
- */ 
-
+ */
 class GlobalCooldown extends CoreGlobalCooldown {
-  static dependencies = {
-    abilities: Abilities,
-    haste: Haste,
-  };
-
   getGlobalCooldownDuration(spellId) {
     const gcd = super.getGlobalCooldownDuration(spellId);
     if (!gcd) {
