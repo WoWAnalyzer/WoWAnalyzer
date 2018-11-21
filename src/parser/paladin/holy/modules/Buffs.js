d0c3969e25ade7698da430a0fb832fdf362abbda
Martijn Hols <git@martijnhols.nl>
2018-11-21 20:37:25 +0100

    Holy Paladin: Add all available buffs

--- src/parser/paladin/holy/modules/Buffs.js
+++ src/parser/paladin/holy/modules/Buffs.js
@@ -3,8 +3,7 @@ import CoreBuffs from 'parser/core/modules/Buffs';
 class Buffs extends CoreBuffs {
   buffs() {
     return [
-      // Hehe you probably expected there to be anything here
-      // BAZINGA
+      // Hehe did I get you again
     ];
   }
 }
