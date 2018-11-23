b40409e44539b02f79672fcc5871098f10953af1
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-23 13:37:53 +0100

    Change formula

--- src/parser/core/calculateBonusAzeriteDamage.js
+++ src/parser/core/calculateBonusAzeriteDamage.js
@@ -6,11 +6,12 @@
  * @returns {number} Bonus damage contributed by the trait
  */
 export default function calculateBonusAzeriteDamage(event, traitBonus, ...coefficients) {
-  // In order to be general and not weird to pass attack power into "intellect" field, it's in the coefficients array (which all get multiplied together to form the base damage)
-  const baseDamage = coefficients.reduce((total, current) => total * current, 1);
-  // gets what percentage trait bonus is in the modified base damage
-  const traitPortion = traitBonus / (baseDamage + traitBonus);
   const actualDamage = event.amount + (event.absorbed || 0);
-  // scales down the actual damage according to the percentage
-  return actualDamage * traitPortion;
+  // In order to generalize, instead of hardcoded fields for intellect, attack power, spell coefficients
+  // there's just one array that gets multiplied together to form the base damage
+  const baseDamage = coefficients.reduce((total, current) => total * current, 1);
+  // Get modifier from `actualDamage = (baseDamage + traitBonus) * modifier`
+  const modifier = actualDamage / (baseDamage + traitBonus);
+  // Scale the trait bonus by the same modifier
+  return traitBonus * modifier;
 }
