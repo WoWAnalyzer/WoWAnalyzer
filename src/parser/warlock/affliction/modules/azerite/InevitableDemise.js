3d267076f9f9e06d249348dcd24be77f96a31752
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-23 18:17:59 +0100

    Added DR and simplified ID buff handling - untested

--- src/parser/warlock/affliction/modules/azerite/InevitableDemise.js
+++ src/parser/warlock/affliction/modules/azerite/InevitableDemise.js
@@ -14,7 +14,7 @@ import TraitStatisticBox from 'interface/others/TraitStatisticBox';
 import ItemDamageDone from 'interface/others/ItemDamageDone';
 
 const DRAIN_LIFE_SP_COEFFICIENT = 0.12; // taken from Simcraft SpellDataDump
-const debug = false;
+const debug = true;
 
 class InevitableDemise extends Analyzer {
   static dependencies = {
@@ -31,11 +31,19 @@ class InevitableDemise extends Analyzer {
     if (!this.active) {
       return;
     }
-    this.damagePerStack = this.selectedCombatant.traitsBySpellId[SPELLS.INEVITABLE_DEMISE.id].reduce((total, rank) => {
+    const damageBeforeDR = this.selectedCombatant.traitsBySpellId[SPELLS.INEVITABLE_DEMISE.id].reduce((total, rank) => {
       const [ damage ] = calculateAzeriteEffects(SPELLS.INEVITABLE_DEMISE.id, rank);
       return total + damage;
     }, 0);
-    debug && this.log('Total damage per ID stack', this.damagePerStack);
+    const traitCount = this.selectedCombatant.traitsBySpellId[SPELLS.INEVITABLE_DEMISE.id].length;
+    // Inevitable Demise suffers from diminishing returns for multiple traits
+    // First trait is worth 100%, all others are 75%
+    // 1 trait (100%) = damageBeforeDR * 1
+    // 2 traits (87,5%) = damageBeforeDR * (1 + 0.75)/2 = 0.875 * damageBeforeDR
+    // 3 traits (83,333%) = damageBeforeDR * (1 + 0.75 + 0.75)/3 = 0.833333 * damageBeforeDR
+    this.damagePerStack = damageBeforeDR * ((1 + 0.75 * (traitCount - 1)) / traitCount); // traitCount should never be 0, if player doesn't have it, we return early
+
+    debug && this.log(`Total damage per tick - before DR: ${damageBeforeDR}, trait count: ${traitCount}, after DR: ${this.damagePerStack}`);
 
     this.addEventListener(Entities.changebuffstack.by(SELECTED_PLAYER).spell(SPELLS.INEVITABLE_DEMISE_BUFF), this.onIDChangeBuffStack);
     this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.DRAIN_LIFE), this.onDrainLifeDamage);
@@ -46,6 +54,9 @@ class InevitableDemise extends Analyzer {
   }
 
   onDrainLifeDamage(event) {
+    const buff = this.selectedCombatant.getBuff(SPELLS.INEVITABLE_DEMISE_BUFF);
+    const currentStacks = buff ? buff.stacks : 0;
+    debug && this.log(`Current stacks - getBuff: ${currentStacks}, manually: ${this._currentStacks}`);
     const totalDamage = event.amount + (event.absorbed || 0);
     const bonusDamage = calculateBonusAzeriteDamage(event, this._currentStacks * this.damagePerStack, DRAIN_LIFE_SP_COEFFICIENT, this.statTracker.currentIntellectRating);
     debug && this.log(`Drain Life damage, current stacks: ${this._currentStacks}, current bonus base damage: ${this._currentStacks * this.damagePerStack}, total damage: ${totalDamage}, Drain damage: ${totalDamage - bonusDamage}, bonus damage: ${bonusDamage}`);
