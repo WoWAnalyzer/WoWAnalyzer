ba1b24aad12c37360e23092903750d601756df0a
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-23 15:28:25 +0100

    Fixed math

--- src/parser/warlock/affliction/modules/azerite/InevitableDemise.js
+++ src/parser/warlock/affliction/modules/azerite/InevitableDemise.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Entities from 'parser/shared/modules/Entities';
 import Events from 'parser/core/Events';
-
-import HIT_TYPES from 'game/HIT_TYPES';
+import calculateBonusAzeriteDamage from 'parser/core/calculateBonusAzeriteDamage';
+import StatTracker from 'parser/shared/modules/StatTracker';
 
 import SPELLS from 'common/SPELLS';
 import { calculateAzeriteEffects } from 'common/stats';
@@ -13,9 +13,14 @@ import { formatThousands } from 'common/format';
 import TraitStatisticBox from 'interface/others/TraitStatisticBox';
 import ItemDamageDone from 'interface/others/ItemDamageDone';
 
+const DRAIN_LIFE_SP_COEFFICIENT = 0.12; // taken from Simcraft SpellDataDump
 const debug = false;
 
 class InevitableDemise extends Analyzer {
+  static dependencies = {
+    statTracker: StatTracker,
+  };
+
   _currentStacks = 0;
   damagePerStack = 0;
   damage = 0;
@@ -42,8 +47,8 @@ class InevitableDemise extends Analyzer {
 
   onDrainLifeDamage(event) {
     const totalDamage = event.amount + (event.absorbed || 0);
-    const bonusDamage = this._currentStacks * this.damagePerStack * (event.hitType === HIT_TYPES.CRIT ? 2 : 1);
-    debug && this.log(`Drain Life damage, current stacks: ${this._currentStacks}, total damage: ${totalDamage}, base Drain damage: ${totalDamage - bonusDamage}, bonus damage: ${bonusDamage}`);
+    const bonusDamage = calculateBonusAzeriteDamage(event, this._currentStacks * this.damagePerStack, DRAIN_LIFE_SP_COEFFICIENT, this.statTracker.currentIntellectRating);
+    debug && this.log(`Drain Life damage, current stacks: ${this._currentStacks}, current bonus base damage: ${this._currentStacks * this.damagePerStack}, total damage: ${totalDamage}, Drain damage: ${totalDamage - bonusDamage}, bonus damage: ${bonusDamage}`);
     this.damage += bonusDamage;
   }
 
@@ -51,8 +56,9 @@ class InevitableDemise extends Analyzer {
     return (
       <TraitStatisticBox
         trait={SPELLS.INEVITABLE_DEMISE.id}
-        value={<ItemDamageDone amount={this.damage} />}
-        tooltip={`Bonus Drain Life damage: ${formatThousands(this.damage)}`}
+        value={<ItemDamageDone amount={this.damage} approximate />}
+        tooltip={`Estimated bonus Drain Life damage: ${formatThousands(this.damage)}<br /><br />
+                The damage is an approximation using current Intellect values at given time. Note that because we might miss some Intellect buffs (e.g. trinkets, traits), the value of current Intellect might be also little incorrect`}
       />
     );
   }
