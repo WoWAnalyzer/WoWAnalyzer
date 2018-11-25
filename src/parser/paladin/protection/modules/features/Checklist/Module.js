73e64cc9cc6184e411b52c0ec53746c6ed80749c
J. David Smith <emallson@atlanis.net>
2018-11-25 01:38:28 -0500

    changed hotr from cast efficiency to filler casts (downtime should catch *not* casting it at all)

--- src/parser/paladin/protection/modules/features/Checklist/Module.js
+++ src/parser/paladin/protection/modules/features/Checklist/Module.js
@@ -8,6 +8,7 @@ import PreparationRuleAnalyzer from 'parser/shared/modules/features/Checklist2/P
 
 import ShieldOfTheRighteous from '../ShieldOfTheRighteous';
 import Consecration from '../../spells/Consecration';
+import HammerOfTheRighteous from '../../spells/HammerOfTheRighteous';
 import LightOfTheProtector from '../../spells/LightOfTheProtector';
 
 import Component from './Component';
@@ -21,6 +22,7 @@ class Checklist extends BaseChecklist {
     shieldOfTheRighteous: ShieldOfTheRighteous,
     consecration: Consecration,
     lotp: LightOfTheProtector,
+    hotr: HammerOfTheRighteous,
   };
 
   render(){
@@ -30,6 +32,7 @@ class Checklist extends BaseChecklist {
         castEfficiency={this.castEfficiency}
         extras={{
           lotpAbility: this.lotp._activeSpell,
+          hotrAbility: this.hotr._activeSpell,
         }}
         thresholds={{
           ...this.preparationRuleAnalyzer.thresholds,
@@ -37,6 +40,7 @@ class Checklist extends BaseChecklist {
           shieldOfTheRighteous: this.shieldOfTheRighteous.suggestionThresholds,
           lotpDelay: this.lotp.delaySuggestion,
           lotpOverheal: this.lotp.overhealSuggestion,
+          hotrBadCasts: this.hotr.badCastThreshold,
         }}
       />
     );
