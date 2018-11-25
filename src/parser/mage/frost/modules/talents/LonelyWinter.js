045cf47cbbad76faf0f46b6d218d2a756618d3d2
Dambroda <43309639+Dambroda@users.noreply.github.com>
2018-11-24 17:08:32 -0800

    Changelog entry and upgraded tooltip for LW

--- src/parser/mage/frost/modules/talents/LonelyWinter.js
+++ src/parser/mage/frost/modules/talents/LonelyWinter.js
@@ -15,26 +15,37 @@ const AFFECTED_SPELLS = [
   SPELLS.FLURRY_DAMAGE,
 ];
 
+// You can no longer summon your Water Elemental, but Frostbolt, Ice Lance, and Flurry deal 25% increased damage.
 class LonelyWinter extends Analyzer {
 
-  totalDamage = 0;
+  bonusDamage = {};
 
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTalent(SPELLS.LONELY_WINTER_TALENT.id);
     this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(AFFECTED_SPELLS), this.onAffectedDamage);
+    AFFECTED_SPELLS.forEach(spell => { this.bonusDamage[spell.id] = 0; });
   }
 
   onAffectedDamage(event) {
-    this.totalDamage += calculateEffectiveDamage(event, BONUS);
+    this.bonusDamage[event.ability.guid] += calculateEffectiveDamage(event, BONUS);
   }
 
   statistic() {
+    let totalDamage = 0;
+    let tooltip = "When analyzing this talent, take into account any DPS you lost by not having a Water Elemental.";
+    tooltip += Object.keys(this.bonusDamage).reduce((acc, spellId) => {
+      const spellBonus = this.bonusDamage[spellId];
+      totalDamage += spellBonus;
+      return acc + `<li>Bonus <b>${SPELLS[spellId].name}</b> damage: ${formatNumber(spellBonus)}</li>`;
+    }, '<ul>');
+    tooltip += `</ul>Total damage increase: ${formatNumber(totalDamage)}`;
+
     return (
       <TalentStatisticBox
         talent={SPELLS.LONELY_WINTER_TALENT.id}
-        value={this.owner.formatItemDamageDone(this.totalDamage)}
-        tooltip={`Total damage increase: ${formatNumber(this.totalDamage)}`}
+        value={this.owner.formatItemDamageDone(totalDamage)}
+        tooltip={tooltip}
       />
     );
   }
