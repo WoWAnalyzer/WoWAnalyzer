60c58631e2d99ed6ca560957740c574417f3c339
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-11-25 15:01:35 +0100

    Removed new event filters

--- src/parser/shared/modules/Entities.js
+++ src/parser/shared/modules/Entities.js
@@ -1,6 +1,5 @@
 import Analyzer from 'parser/core/Analyzer';
 import EventEmitter from 'parser/core/modules/EventEmitter';
-import EventFilter from 'parser/core/EventFilter';
 
 const debug = false;
 
@@ -11,14 +10,6 @@ class Entities extends Analyzer {
   static dependencies = {
     eventEmitter: EventEmitter,
   };
-
-  static get changedebuffstack() {
-    return new EventFilter('changedebuffstack');
-  }
-
-  static get changebuffstack() {
-    return new EventFilter('changebuffstack');
-  }
 
   getEntities() {
     throw new Error('Not implemented');
