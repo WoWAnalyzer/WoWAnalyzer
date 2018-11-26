3db23e2fb400cd2ed0c9b15f98c59006a64b72f7
sMteX <stuchlik1@seznam.cz>
2018-11-26 12:39:43 +0100

    Refactor

--- src/parser/warlock/destruction/modules/azerite/BurstingFlare.js
+++ src/parser/warlock/destruction/modules/azerite/BurstingFlare.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import EventFilter from 'parser/core/EventFilter';
+import Analyzer from 'parser/core/Analyzer';
 
 import SPELLS from 'common/SPELLS';
 import { calculateAzeriteEffects } from 'common/stats';
@@ -38,31 +37,6 @@ class BurstingFlare extends Analyzer {
     }
     this.mastery = burstingFlareStats(this.selectedCombatant.traitsBySpellId[SPELLS.BURSTING_FLARE.id]);
     debug && this.log(`Total bonus from BF: ${this.mastery}`);
-
-    // by no chance I was able to get it working with CombatLogParser.finished and possibly Entities.changebuffstack
-    // not a clue why not but it just doesn't work (supposedly circular dependencies but the same code (at least with finished) works elsewhere, not here), this does
-    this.addEventListener(new EventFilter('changebuffstack').by(SELECTED_PLAYER).spell(SPELLS.BURSTING_FLARE_BUFF), this.onBurstingFlareChangeBuffStack);
-    this.addEventListener(new EventFilter('finished'), this.onFinished);
-  }
-
-  onBurstingFlareChangeBuffStack(event) {
-    debug && this.log(`BF change buffstack, last timestamp ${this._lastChangeTimestamp}, old stacks ${event.oldStacks}, new stacks ${event.newStacks}`);
-    if (this._lastChangeTimestamp && event.oldStacks !== 0) {
-      const uptimeOnStack = event.timestamp - this._lastChangeTimestamp;
-      debug && this.log(`Uptime on old stack: ${uptimeOnStack}`);
-      this.totalMastery += event.oldStacks * this.mastery * uptimeOnStack;
-      debug && this.log(`Total mastery increased to ${this.totalMastery}`);
-    }
-    this._currentStacks = event.newStacks;
-    this._lastChangeTimestamp = event.timestamp;
-  }
-
-  onFinished(event) {
-    if (this._currentStacks !== 0) {
-      const uptimeOnStack = event.timestamp - this._lastChangeTimestamp;
-      debug && this.log(`Fight ended, adding rest of BF uptime on ${this._currentStacks} stacks: ${uptimeOnStack}`);
-      this.totalMastery += this._currentStacks * this.mastery * uptimeOnStack;
-    }
   }
 
   get uptime() {
@@ -70,7 +44,8 @@ class BurstingFlare extends Analyzer {
   }
 
   get averageMastery() {
-    return (this.totalMastery / this.owner.fightDuration).toFixed(0);
+    const averageStacks = this.selectedCombatant.getStackWeightedBuffUptime(SPELLS.BURSTING_FLARE_BUFF.id) / this.owner.fightDuration;
+    return (averageStacks * this.mastery).toFixed(0);
   }
 
   statistic() {
