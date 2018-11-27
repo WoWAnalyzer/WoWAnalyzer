2385c93f54bde5c69ceebddd0520d717917d2157
Juko8 <ulrichenevoldsen@hotmail.com>
2018-11-27 17:32:14 +0100

    Use new event listeners Retribution module
    Updated this too since it's dependent on the constants that were changed

--- src/parser/paladin/retribution/modules/core/Retribution.js
+++ src/parser/paladin/retribution/modules/core/Retribution.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import Analyzer from 'parser/core/Analyzer';
-
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events from 'parser/core/Events';
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
 import { formatNumber, formatPercentage } from 'common/format';
 import StatisticBox, { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 
 import calculateEffectiveDamage from 'parser/core/calculateEffectiveDamage';
+import CombatLogParser from 'parser/core/CombatLogParser';
 import { ABILITIES_AFFECTED_BY_DAMAGE_INCREASES } from '../../constants';
+
 
 const RETRIBUTION_DAMAGE_BONUS = 0.2;
 
@@ -18,18 +20,20 @@ class Retribution extends Analyzer {
 
   constructor(...args) {
     super(...args);
-    this.abilitiesAffectedByRetribution.push(SPELLS.MELEE.id);
+    this.abilitiesAffectedByRetribution.push(SPELLS.MELEE);
+
+    // event listeners
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(ABILITIES_AFFECTED_BY_DAMAGE_INCREASES), this.onAffectedDamage);
+    this.addEventListener(CombatLogParser.finished, this.onFinished);
   }
 
-  on_byPlayer_damage(event) {
-    const spellId = event.ability.guid;
-    if (!this.selectedCombatant.hasBuff(SPELLS.RETRIBUTION_BUFF.id) || !this.abilitiesAffectedByRetribution.includes(spellId)) {
-      return;
+  onAffectedDamage(event) {
+    if (this.selectedCombatant.hasBuff(SPELLS.RETRIBUTION_BUFF.id)) {
+      this.bonusDmg += calculateEffectiveDamage(event, RETRIBUTION_DAMAGE_BONUS);
     }
-    this.bonusDmg += calculateEffectiveDamage(event, RETRIBUTION_DAMAGE_BONUS);
   }
 
-  on_finished() {
+  onFinished() {
     this.active = this.bonusDmg > 0;
   }
 
