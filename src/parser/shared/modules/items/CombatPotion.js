33df178d50b9bda183ef70d1017378a1e7b7d69f
Alastair Scott <alastair.scott91@gmail.com>
2018-11-27 19:12:38 +0000

    #2742 - Updated CombatPotion.js
    Fix for #2742
    
    Updated the CombatPotion.js file to include Potion of Replenishment to show it on the timeline.

--- src/parser/shared/modules/items/CombatPotion.js
+++ src/parser/shared/modules/items/CombatPotion.js
@@ -16,6 +16,7 @@ class CombatPotion extends Potion {
     SPELLS.STEELSKIN_POTION,
     SPELLS.COASTAL_MANA_POTION,
     SPELLS.COASTAL_REJUVENATION_POTION,
+    SPELLS.POTION_OF_REPLENISHMENT,
   ];
   static recommendedEfficiency = 0;
   static extraAbilityInfo = {
@@ -27,7 +28,8 @@ class CombatPotion extends Potion {
       SPELLS.POTION_OF_RISING_DEATH.id,
       SPELLS.POTION_OF_BURSTING_BLOOD.id,
       SPELLS.STEELSKIN_POTION.id,
-      SPELLS.COASTAL_REJUVENATION_POTION.id],
+      SPELLS.COASTAL_REJUVENATION_POTION.id,
+      SPELLS.POTION_OF_REPLENISHMENT.id],
   }
 }
 
