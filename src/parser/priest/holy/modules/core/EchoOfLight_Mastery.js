3de146219cfd8f2560d260f654c140b3ac3d6611
James Moats <tjmoats@demark.com>
2018-11-28 14:20:48 -0700

    Adding debug variable

--- src/parser/priest/holy/modules/core/EchoOfLight_Mastery.js
+++ src/parser/priest/holy/modules/core/EchoOfLight_Mastery.js
@@ -9,6 +9,8 @@ import ItemHealingDone from 'interface/others/ItemHealingDone';
 import { formatNumber, formatPercentage } from 'common/format';
 import { ABILITIES_THAT_TRIGGER_MASTERY } from '../../constants';
 
+const DEBUG = false;
+
 class EchoOfLight_Mastery extends Analyzer {
   static dependencies = {
     abilityTracker: AbilityTracker,
@@ -93,7 +95,9 @@ class EchoOfLight_Mastery extends Analyzer {
 
     // As far as I can tell, this happens when the combat log is out of order. You shouldn't receive a tick of EoL without a target having a buff apply event.
     if (!this.targetMasteryPool[targetId] || this.targetMasteryPool[targetId].remainingTicks < 1) {
-      console.warn(`There was a mastery tick for ${event.amount + (event.absorbed || 0)} (${event.overheal || 0} OH) for a target that doesn't have a mastery pool!`);
+      if (DEBUG){
+        console.warn(`There was a mastery tick for ${event.amount + (event.absorbed || 0)} (${event.overheal || 0} OH) for a target that doesn't have a mastery pool!`);
+      }
       return;
     }
 
