fdfdac4f17fcbb6b05ae189dae53a70290590ab0
willowsnow <willow.snow14@gmail.com>
2018-11-29 02:09:05 -0600

    Convert Endless Rage to 2 spaces

--- src/parser/warrior/fury/modules/talents/EndlessRage.js
+++ src/parser/warrior/fury/modules/talents/EndlessRage.js
@@ -6,31 +6,31 @@ import Events from 'parser/core/Events';
 import { SELECTED_PLAYER } from 'parser/core/EventFilter';
 
 class EndlessRage extends Analyzer {
-    rageGen = 0;
+  rageGen = 0;
     
-    constructor(...args) {
-        super(...args);
+  constructor(...args) {
+    super(...args);
 
-        this.active = this.selectedCombatant.hasTalent(SPELLS.ENDLESS_RAGE_TALENT.id);
+    this.active = this.selectedCombatant.hasTalent(SPELLS.ENDLESS_RAGE_TALENT.id);
 
-        this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).to(SELECTED_PLAYER), this.onPlayerBuff);
+    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).to(SELECTED_PLAYER), this.onPlayerBuff);
+  }
+
+  onPlayerBuff(event) {
+    if (event.ability.guid === SPELLS.ENRAGE.id) {
+      this.rageGen += 6;
     }
+  }
 
-    onPlayerBuff(event) {
-        if (event.ability.guid === SPELLS.ENRAGE.id) {
-            this.rageGen += 6;
-        }
-    }
-
-    statistic() {
-        return (
-            <TalentStatisticBox
-              talent={SPELLS.ENDLESS_RAGE_TALENT.id}
-              value={`${this.rageGen} rage generated`}
-              label="Endless Rage"
-            />
-        );
-    }
+  statistic() {
+    return (
+      <TalentStatisticBox
+        talent={SPELLS.ENDLESS_RAGE_TALENT.id}
+        value={`${this.rageGen} rage generated`}
+        label="Endless Rage"
+      />
+      );
+  }
 }
 
 export default EndlessRage;
