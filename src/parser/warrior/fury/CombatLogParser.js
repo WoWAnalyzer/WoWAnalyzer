7709c806cb8905b4afa8e04d9b2df9c4cc107821
Alastair Scott <alastair.scott91@gmail.com>
2018-12-02 07:08:03 +0000

    [Fury Warrior] Siegebreaker implementation
    Picked up previous PR and re implemented Siegebreaker along with a fix to the Enrage buff.
    
    Log for testing:
    http://localhost:3000/report/BT1vYNRD2WZaj83t/3-Mythic+Taloc+-+Kill+(4:55)/14-Shirius

--- src/parser/warrior/fury/CombatLogParser.js
+++ src/parser/warrior/fury/CombatLogParser.js
@@ -1,41 +1,43 @@
 import CoreCombatLogParser from 'parser/core/CombatLogParser';
 import DamageDone from 'parser/shared/modules/DamageDone';
 
-import Checklist from './modules/features/checklist/Module';
 import Abilities from './modules/Abilities';
 import AlwaysBeCasting from './modules/features/AlwaysBeCasting';
+import Checklist from './modules/features/checklist/Module';
 import CooldownThroughputTracker from './modules/features/CooldownThroughputTracker';
 import SpellUsable from './modules/features/SpellUsable';
 
 import Enrage from './modules/buffdebuff/Enrage';
 
-import FrothingBerserker from './modules/talents/FrothingBerserker';
-
 import MissedRampage from './modules/spells/MissedRampage';
 import RampageCancelled from './modules/features/RampageCancelled';
+
 import AngerManagement from './modules/talents/AngerManagement';
+import FrothingBerserker from './modules/talents/FrothingBerserker';
 import FuriousSlashTimesByStacks from './modules/talents/FuriousSlashTimesByStacks';
 import FuriousSlashUptime from './modules/talents/FuriousSlashUptime';
+import Siegebreaker from './modules/talents/Siegebreaker';
 
 class CombatLogParser extends CoreCombatLogParser {
   static specModules = {
     damageDone: [DamageDone, {showStatistic: true}],
 
-    checklist: Checklist,
     abilities: Abilities,
     alwaysBeCasting: AlwaysBeCasting,
+    checklist: Checklist,
     cooldownThroughputTracker: CooldownThroughputTracker,
     spellUsable: SpellUsable,
 
     enrageUptime: Enrage,
 
-    frothingBerserker: FrothingBerserker,
-    
     missedRampage: MissedRampage,
     rampageCancelled: RampageCancelled,
+
     angerManagement: AngerManagement,
+    frothingBerserker: FrothingBerserker,    
     furiousSlashTimesByStacks: FuriousSlashTimesByStacks,
     furiousSlashUptime: FuriousSlashUptime,
+    siegebreaker: Siegebreaker,
 
     // Overrides default PrePotion
     //prePotion: PrePotion, TODO: Update this to BFA and ensure it works properly with parser/core/Modules/Features/Checklist2/PreparationRuleAnalyzer.js
