d1bc78245a8c334d86d48460116a4065e480a58e
Travis Harris <travisharris@traviss-mbp.lan>
2018-12-07 22:42:45 -0800

    fixing some styling issues and removing some debug logs

--- server/controllers/api/spell.js
+++ server/controllers/api/spell.js
@@ -1,43 +1,40 @@
-import Express from "express";
-import Sequelize from "sequelize";
-import Raven from "raven";
+import Express from 'express';
+import Sequelize from 'sequelize';
+import Raven from 'raven';
 
-import { fetchSpell as fetchSpellfromBattleNet } from "helpers/wowCommunityApi";
+import { fetchSpell as fetchSpellfromBattleNet } from 'helpers/wowCommunityApi';
 
-import models from "../../models";
+import models from '../../models';
 
 const Spell = models.Spell;
 
 /**
- * Fetches Spell info(name and icon) from the battle net API. 
+ * Fetches Spell info(name and icon) from the battle net API.
  * After fetching from API it'll store in MySQL DB in order to reduce the number of calls to the battle net API
  * and reduce latency on subsequent calls
  */
 
 function sendJson(res, json) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.send(json);
 }
 async function proxySpellApi(res, region, spellId) {
   try {
-    console.log("Fetching Spell from Battle.net");
+    console.log('Fetching Spell from Battle.net');
     const start = Date.now();
     const response = await fetchSpellfromBattleNet(region, spellId);
     const responseTime = Date.now() - start;
-    console.log("Battle.net response time:", responseTime, "ms");
+    console.log('Battle.net response time:', responseTime, 'ms');
     const json = JSON.parse(response);
     sendJson(res, json);
     return json;
   } catch (error) {
     const { statusCode, message, response } = error;
-    console.log("Error fetching Spell", statusCode, message);
+    console.log('Error fetching Spell', statusCode, message);
     const body = response ? response.body : null;
     // Ignore 404 - Spell not found errors. We check for the text so this doesn't silently break when the API endpoint changes.
-    const isCharacterNotFoundError =
-      statusCode === 404 &&
-      body &&
-      body.includes("Unable to get spell information.");
+    const isCharacterNotFoundError = statusCode === 404 && body && body.includes('Unable to get spell information.');
     if (!isCharacterNotFoundError) {
       Raven.installed && Raven.captureException(error);
     }
@@ -51,18 +48,18 @@ async function storeSpell({ id, name, icon }) {
     id,
     name,
     icon,
-    lastSeenAt: Sequelize.fn("NOW"),
+    lastSeenAt: Sequelize.fn('NOW'),
   });
 }
 
 const router = Express.Router();
-router.get("/:region([A-Z]{2})/:id([0-9]+)", async (req, res) => {
+router.get('/:region([A-Z]{2})/:id([0-9]+)', async (req, res) => {
   const { region, id } = req.params;
   let spell = await Spell.findById(id);
   if (spell) {
     res.send(spell);
     spell.update({
-      lastSeenAt: Sequelize.fn("NOW"),
+      lastSeenAt: Sequelize.fn('NOW'),
     });
   } else {
     spell = await proxySpellApi(res, region, id);
--- server/helpers/wowCommunityApi.js
+++ server/helpers/wowCommunityApi.js
@@ -1,17 +1,15 @@
-import {
-  blizzardApiResponseLatencyHistogram,
-} from "helpers/metrics";
-import RequestTimeoutError from "helpers/request/RequestTimeoutError";
-import RequestSocketTimeoutError from "helpers/request/RequestSocketTimeoutError";
-import RequestConnectionResetError from "helpers/request/RequestConnectionResetError";
-import RequestUnknownError from "helpers/request/RequestUnknownError";
-import retryingRequest from "./retryingRequest";
+import { blizzardApiResponseLatencyHistogram } from 'helpers/metrics';
+import RequestTimeoutError from 'helpers/request/RequestTimeoutError';
+import RequestSocketTimeoutError from 'helpers/request/RequestSocketTimeoutError';
+import RequestConnectionResetError from 'helpers/request/RequestConnectionResetError';
+import RequestUnknownError from 'helpers/request/RequestUnknownError';
+import retryingRequest from './retryingRequest';
 
 const availableRegions = {
-  eu: "ru_RU",
-  us: "en_US",
-  tw: "zh_TW",
-  kr: "ko_KR",
+  eu: 'ru_RU',
+  us: 'en_US',
+  tw: 'zh_TW',
+  kr: 'ko_KR',
 };
 
 const USER_AGENT = process.env.USER_AGENT;
@@ -22,20 +20,16 @@ const get = (url, metricLabels) => {
   return retryingRequest({
     url,
     headers: {
-      "User-Agent": USER_AGENT,
+      'User-Agent': USER_AGENT,
     },
     gzip: true,
     // we'll be making several requests, so pool connections
     forever: true,
     timeout: 4000, // ms after which to abort the request, when a character is uncached it's not uncommon to take ~2sec
     shouldRetry: error => {
-      const {
-        statusCode,
-        response,
-      } = error;
+      const { statusCode, response } = error;
       const body = response ? response.body : null;
-      const isCharacterNotFoundError =
-        statusCode === 404 && body && body.includes("Character not found.");
+      const isCharacterNotFoundError = statusCode === 404 && body && body.includes('Character not found.');
       // Previously `shouldRetry` checked for just `err instanceof RequestError || statusCode === 503` - but the Blizzard API is so buggy with random errors, we're better off just retrying for anything unexpected. 503 errors happen regularly which probably means the character wasn't cached, try again once. The API also sometimes randomly throws 404 errors (without "Character not found." in the body) for no reason, we should retry those too.
       const shouldRetry = !isCharacterNotFoundError;
       return shouldRetry;
@@ -46,19 +40,19 @@ const get = (url, metricLabels) => {
     onFailure: err => {
       if (err instanceof RequestTimeoutError) {
         end({
-          statusCode: "timeout",
+          statusCode: 'timeout',
         });
       } else if (err instanceof RequestSocketTimeoutError) {
         end({
-          statusCode: "socket timeout",
+          statusCode: 'socket timeout',
         });
       } else if (err instanceof RequestConnectionResetError) {
         end({
-          statusCode: "connection reset",
+          statusCode: 'connection reset',
         });
       } else if (err instanceof RequestUnknownError) {
         end({
-          statusCode: "unknown",
+          statusCode: 'unknown',
         });
       } else {
         end({
@@ -81,12 +75,14 @@ const getAccessToken = async region => {
     return clientToken[region].accessToken;
   }
 
-  const url = `https://${region}.battle.net/oauth/token?grant_type=client_credentials&client_id=${process.env.BATTLE_NET_API_CLIENT_ID}&client_secret=${process.env.BATTLE_NET_API_CLIENT_SECRET}`;
-  
+  const url = `https://${region}.battle.net/oauth/token?grant_type=client_credentials&client_id=${process.env.BATTLE_NET_API_CLIENT_ID}&client_secret=${
+    process.env.BATTLE_NET_API_CLIENT_SECRET
+  }`;
+
   const tokenRequst = await retryingRequest({
     url,
     headers: {
-      "User-Agent": USER_AGENT,
+      'User-Agent': USER_AGENT,
     },
     gzip: true,
     // we'll be making several requests, so pool connections
@@ -97,26 +93,26 @@ const getAccessToken = async region => {
     },
     onBeforeAttempt: () => {
       end = blizzardApiResponseLatencyHistogram.startTimer({
-        category: "access_token",
+        category: 'access_token',
         region,
       });
     },
     onFailure: err => {
       if (err instanceof RequestTimeoutError) {
         end({
-          statusCode: "timeout",
+          statusCode: 'timeout',
         });
       } else if (err instanceof RequestSocketTimeoutError) {
         end({
-          statusCode: "socket timeout",
+          statusCode: 'socket timeout',
         });
       } else if (err instanceof RequestConnectionResetError) {
         end({
-          statusCode: "connection reset",
+          statusCode: 'connection reset',
         });
       } else if (err instanceof RequestUnknownError) {
         end({
-          statusCode: "unknown",
+          statusCode: 'unknown',
         });
       } else {
         end({
@@ -133,9 +129,7 @@ const getAccessToken = async region => {
 
   const tokenData = JSON.parse(tokenRequst);
   let expireDate = new Date();
-  expireDate = expireDate.setSeconds(
-    expireDate.getSeconds() + tokenData.expires_in
-  );
+  expireDate = expireDate.setSeconds(expireDate.getSeconds() + tokenData.expires_in);
 
   clientToken[region] = {
     accessToken: tokenData.access_token,
@@ -144,32 +138,32 @@ const getAccessToken = async region => {
   return clientToken[region].accessToken;
 };
 
-export async function fetchCharacter(region, realm, name, fields = "") {
+export async function fetchCharacter(region, realm, name, fields = '') {
   region = region.toLowerCase();
   if (!availableRegions[region]) {
-    throw new Error("Region not recognized.");
+    throw new Error('Region not recognized.');
   }
   const accessToken = await getAccessToken(region);
-  const url = `${makeBaseUrl(region)}/wow/character/${encodeURIComponent(realm)}/${encodeURIComponent(name)}?locale=${ availableRegions[region]}&fields=${fields}&access_token=${accessToken}`;
+  const url = `${makeBaseUrl(region)}/wow/character/${encodeURIComponent(realm)}/${encodeURIComponent(name)}?locale=${
+    availableRegions[region]
+  }&fields=${fields}&access_token=${accessToken}`;
 
   return get(url, {
-    category: "character",
+    category: 'character',
     region,
   });
 }
 
 export async function fetchItem(region, itemId) {
-  console.log(region);
-  console.log(itemId);
   region = region.toLowerCase();
   if (!availableRegions[region]) {
-    throw new Error("Region not recognized.");
+    throw new Error('Region not recognized.');
   }
   const accessToken = await getAccessToken(region);
   const url = `${makeBaseUrl(region)}/wow/item/${encodeURIComponent(itemId)}?locale=${availableRegions[region]}&access_token=${accessToken}`;
-  
+
   return get(url, {
-    category: "item",
+    category: 'item',
     region,
   });
 }
@@ -177,13 +171,13 @@ export async function fetchItem(region, itemId) {
 export async function fetchSpell(region, spellId) {
   region = region.toLowerCase();
   if (!availableRegions[region]) {
-    throw new Error("Region not recognized.");
+    throw new Error('Region not recognized.');
   }
   const accessToken = await getAccessToken(region);
   const url = `${makeBaseUrl(region)}/wow/spell/${encodeURIComponent(spellId)}?locale=${availableRegions[region]}&access_token=${accessToken}`;
-  
+
   return get(url, {
-    category: "item",
+    category: 'item',
     region,
   });
 }
