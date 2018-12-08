8f8a22e58f4c60ca9a362d2496313297d9315cfe
Travis Harris <travih@microsoft.com>
2018-12-07 16:09:21 -0800

    Update character fetching API on server to use new blizzard API

--- server/helpers/wowCommunityApi.js
+++ server/helpers/wowCommunityApi.js
@@ -1,25 +1,26 @@
-import { blizzardApiResponseLatencyHistogram } from 'helpers/metrics';
-import RequestTimeoutError from 'helpers/request/RequestTimeoutError';
-import RequestSocketTimeoutError from 'helpers/request/RequestSocketTimeoutError';
-import RequestConnectionResetError from 'helpers/request/RequestConnectionResetError';
-import RequestUnknownError from 'helpers/request/RequestUnknownError';
-import retryingRequest from './retryingRequest';
+import { blizzardApiResponseLatencyHistogram } from "helpers/metrics";
+import RequestTimeoutError from "helpers/request/RequestTimeoutError";
+import RequestSocketTimeoutError from "helpers/request/RequestSocketTimeoutError";
+import RequestConnectionResetError from "helpers/request/RequestConnectionResetError";
+import RequestUnknownError from "helpers/request/RequestUnknownError";
+import retryingRequest from "./retryingRequest";
 
 const availableRegions = {
-  'eu': 'ru_RU',
-  'us': 'en_US',
-  'tw': 'zh_TW',
-  'kr': 'ko_KR',
+  eu: "ru_RU",
+  us: "en_US",
+  tw: "zh_TW",
+  kr: "ko_KR"
 };
 
 const USER_AGENT = process.env.USER_AGENT;
+let clientToken = {};
 
 const get = (url, metricLabels) => {
   let end;
   return retryingRequest({
     url,
     headers: {
-      'User-Agent': USER_AGENT,
+      "User-Agent": USER_AGENT
     },
     gzip: true,
     // we'll be making several requests, so pool connections
@@ -28,7 +29,8 @@ const get = (url, metricLabels) => {
     shouldRetry: error => {
       const { statusCode, response } = error;
       const body = response ? response.body : null;
-      const isCharacterNotFoundError = statusCode === 404 && body && body.includes('Character not found.');
+      const isCharacterNotFoundError =
+        statusCode === 404 && body && body.includes("Character not found.");
       // Previously `shouldRetry` checked for just `err instanceof RequestError || statusCode === 503` - but the Blizzard API is so buggy with random errors, we're better off just retrying for anything unexpected. 503 errors happen regularly which probably means the character wasn't cached, try again once. The API also sometimes randomly throws 404 errors (without "Character not found." in the body) for no reason, we should retry those too.
       const shouldRetry = !isCharacterNotFoundError;
       return shouldRetry;
@@ -38,33 +40,91 @@ const get = (url, metricLabels) => {
     },
     onFailure: err => {
       if (err instanceof RequestTimeoutError) {
-        end({ statusCode: 'timeout' });
+        end({ statusCode: "timeout" });
       } else if (err instanceof RequestSocketTimeoutError) {
-        end({ statusCode: 'socket timeout' });
+        end({ statusCode: "socket timeout" });
       } else if (err instanceof RequestConnectionResetError) {
-        end({ statusCode: 'connection reset' });
+        end({ statusCode: "connection reset" });
       } else if (err instanceof RequestUnknownError) {
-        end({ statusCode: 'unknown' });
+        end({ statusCode: "unknown" });
       } else {
         end({ statusCode: err.statusCode });
       }
     },
     onSuccess: () => {
       end({ statusCode: 200 });
-    },
+    }
   });
 };
-const makeBaseUrl = region => `https://${region}.api.battle.net/wow`;
+const makeBaseUrl = region => `https://${region}.api.blizzard.com`;
 
-export async function fetchCharacter(region, realm, name, fields = '') {
+const getAccessToken = async region => {
+  let end;
+  if (!!clientToken[region] && !!clientToken[region].accessToken && clientToken[region].expires > new Date()) {
+    return clientToken[region].accessToken;
+  }
+  const url = `https://${region}.battle.net/oauth/token?grant_type=client_credentials&client_id=${process.env.BATTLE_NET_API_CLIENT_ID}&client_secret=${process.env.BATTLE_NET_API_CLIENT_SECRET}`;
+  const tokenRequst = await retryingRequest({
+    url,
+    headers: {
+      "User-Agent": USER_AGENT
+    },
+    gzip: true,
+    // we'll be making several requests, so pool connections
+    forever: true,
+    timeout: 4000, 
+    shouldRetry: error => {
+      return true;
+    },
+    onBeforeAttempt: () => {
+      end = blizzardApiResponseLatencyHistogram.startTimer({
+        category: "access_token",
+        region
+      });
+    },
+    onFailure: err => {
+      if (err instanceof RequestTimeoutError) {
+        end({ statusCode: "timeout" });
+      } else if (err instanceof RequestSocketTimeoutError) {
+        end({ statusCode: "socket timeout" });
+      } else if (err instanceof RequestConnectionResetError) {
+        end({ statusCode: "connection reset" });
+      } else if (err instanceof RequestUnknownError) {
+        end({ statusCode: "unknown" });
+      } else {
+        end({ statusCode: err.statusCode });
+      }
+    },
+    onSuccess: () => {
+      end({ statusCode: 200 });
+    }
+  });
+  const tokenData = JSON.parse(tokenRequst);
+  let expireDate = new Date();
+  expireDate = expireDate.setSeconds(
+    expireDate.getSeconds() + tokenData.expires_in
+  );
+  clientToken[region] = {
+    accessToken: tokenData.access_token,
+    expires: expireDate
+  };
+  return clientToken[region].accessToken;
+};
+
+export async function fetchCharacter(region, realm, name, fields = "") {
   region = region.toLowerCase();
   if (!availableRegions[region]) {
-    throw new Error('Region not recognized.');
+    throw new Error("Region not recognized.");
   }
+  const accessToken = await getAccessToken(region);
+  const url = `${makeBaseUrl(region)}/wow/character/${encodeURIComponent(
+    realm
+  )}/${encodeURIComponent(name)}?locale=${
+    availableRegions[region]
+  }&fields=${fields}&access_token=${accessToken}`;
 
-  const url = `${makeBaseUrl(region)}/character/${encodeURIComponent(realm)}/${encodeURIComponent(name)}?locale=${availableRegions[region]}&fields=${fields}&apikey=${process.env.BATTLE_NET_API_KEY}`;
   return get(url, {
-    category: 'character',
-    region,
+    category: "character",
+    region
   });
 }
