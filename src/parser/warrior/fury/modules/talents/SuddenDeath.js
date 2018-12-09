66485d11d61fee31285f240127c593df78767c63
Alastair Scott <alastair.scott91@gmail.com>
2018-12-09 10:41:16 +0000

    Updated Sudden Death
    Here's an updated version based on feedback.

--- src/parser/warrior/fury/modules/talents/SuddenDeath.js
+++ src/parser/warrior/fury/modules/talents/SuddenDeath.js
@@ -5,6 +5,7 @@ import TalentStatisticBox from 'interface/others/TalentStatisticBox';
 import Analyzer from 'parser/core/Analyzer';
 import Events from 'parser/core/Events';
 import { SELECTED_PLAYER } from 'parser/core/EventFilter';
+import SpellUsable from '../features/SpellUsable';
 
 /**
  * Your attacks have a chance to reset the cooldown of Execute
@@ -12,17 +13,23 @@ import { SELECTED_PLAYER } from 'parser/core/EventFilter';
  */
 
 class SuddenDeath extends Analyzer {
+  static dependencies = {
+    spellUsable: SpellUsable,
+  };
+
   suddenDeathProcs = 0;
   suddenDeathProcsUsed = 0;
-  suddenDeathDamage = 0;
+  executeDamageEvents = [];
 
   lastExecuteCast = 0;
   lastSuddenDeathExecuteCast = 0;
+  lastSuddenDeathTargetID = 0;
 
   constructor(...args) {
     super(...args);
 
     this.active = this.selectedCombatant.hasTalent(SPELLS.SUDDEN_DEATH_TALENT_FURY.id);
+    this.executeThreshold = this.selectedCombatant.hasTalent(SPELLS.MASSACRE_TALENT_FURY.id) ? 0.35 : 0.2;
 
     if (!this.active) {
       return;
@@ -38,13 +45,23 @@ class SuddenDeath extends Analyzer {
     if (this.selectedCombatant.hasBuff(SPELLS.SUDDEN_DEATH_TALENT_FURY_BUFF.id)) {
       this.lastSuddenDeathExecuteCast = event.timestamp;
       this.suddenDeathProcsUsed += 1;
+      this.lastSuddenDeathTargetID = event.targetID;
     }
     this.lastExecuteCast = event.timestamp;
   }
 
   onExecuteDamage(event) {
-    if (this.lastExecuteCast === this.lastSuddenDeathExecuteCast) {
-      this.suddenDeathDamage += event.amount + (event.absorbed || 0);
+    if (!this.executeDamageEvents[this.lastExecuteCast]) {
+      this.executeDamageEvents[this.lastExecuteCast] = {
+        damageDone: 0,
+        isMainTargetAboveThreshold: false,
+        isSuddenDeath: this.lastExecuteCast === this.lastSuddenDeathExecuteCast,
+      };        
+    }
+    this.executeDamageEvents[this.lastExecuteCast].damageDone += event.amount + (event.absorbed || 0);
+
+    if (event.targetID === this.lastSuddenDeathTargetID && this.isExecuteAboveThreshold(event)) {
+      this.executeDamageEvents[this.lastExecuteCast].isMainTargetAboveThreshold = true;
     }
   }
 
@@ -52,8 +69,28 @@ class SuddenDeath extends Analyzer {
     this.suddenDeathProcs += 1;
   }
 
+  isExecuteAboveThreshold(event) {
+    return event.hitPoints / event.maxHitPoints > this.executeThreshold;
+  }
+
+  get damageAboveThreshold() {
+    return this.executeDamageEvents.reduce((total, event) => {
+      return event.isMainTargetAboveThreshold ? total + event.damageDone : total;
+    }, 0);
+  }
+
   get damagePercent() {
-    return this.owner.getPercentageOfTotalDamageDone(this.suddenDeathDamage);
+    return this.owner.getPercentageOfTotalDamageDone(this.damageAboveThreshold);
+  }
+
+  get executeCastsAboveThreshold() {
+    return this.executeDamageEvents.filter(e => e.isMainTargetAboveThreshold && e.isSuddenDeath).length;
+  }
+
+  get effectiveExecuteCDR() {
+    return this.spellUsable.executeCdrEvents.reduce((total, cdrValue, index) => {
+      return this.executeDamageEvents[index] && !this.executeDamageEvents[index].isMainTargetAboveThreshold ? total + cdrValue : total;
+    }, 0);
   }
 
   statistic() {
@@ -62,7 +99,11 @@ class SuddenDeath extends Analyzer {
         talent={SPELLS.SUDDEN_DEATH_TALENT_FURY.id}
         value={`${this.suddenDeathProcsUsed} / ${this.suddenDeathProcs} procs used`}
         label="Sudden Death"
-        tooltip={`Sudden Death contributed to <b>${formatThousands(this.suddenDeathDamage)} (${formatPercentage(this.damagePercent)}%)</b> of your overall damage.`}
+        tooltip={`Sudden Death usage above ${formatPercentage(this.executeThreshold)}%<br />
+        Damage done: <b>${formatThousands(this.damageAboveThreshold)} (${formatPercentage(this.damagePercent)}%)</b><br />
+        Execute casts: <b>${formatThousands(this.executeCastsAboveThreshold)}</b><br /><br />
+        Sudden Death usage below ${formatPercentage(this.executeThreshold)}%</u><br />
+        Effective CDR: <b>${(this.effectiveExecuteCDR / 1000).toFixed(2)}s</b>`}
       />
     );
   }
