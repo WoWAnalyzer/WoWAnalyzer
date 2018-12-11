7a7f29aa95d56c65d96237cc141228af2b09e158
J David Smith <emallson@archlinux.us>
2018-12-10 23:39:37 -0500

    change restosham spreadsheet to put a 0 for E&F

--- src/interface/others/RestorationShamanSpreadsheet.js
+++ src/interface/others/RestorationShamanSpreadsheet.js
@@ -59,6 +59,7 @@ class RestorationShamanSpreadsheet extends React.Component {
               <tr><td>{parser.getModule(SurgingTides).surgingTideProcsPerMinute}</td></tr>
               <tr><td>{((parser.getModule(SpoutingSpirits).spoutingSpiritsHits || getAbility(SPELLS.SPOUTING_SPIRITS_HEAL.id).healingHits) / casts(SPELLS.SPIRIT_LINK_TOTEM.id) || 0).toFixed(2)}</td></tr>
               <tr><td>{((parser.getModule(OverflowingShores).overflowingShoresHits || getAbility(SPELLS.OVERFLOWING_SHORES_HEAL.id).healingHits) / casts(SPELLS.HEALING_RAIN_CAST.id) || 0).toFixed(2)}</td></tr>
+              <tr><td>0</td></tr>
               <tr><td>{parser.getModule(Combatants).playerCount}</td></tr>
               <tr><td>{cpm(SPELLS.ASTRAL_SHIFT.id)}</td></tr>
               <tr><td>{(parser.selectedCombatant.getBuffUptime(SPELLS.GHOST_WOLF.id) / 1000).toFixed(2)}</td></tr>
