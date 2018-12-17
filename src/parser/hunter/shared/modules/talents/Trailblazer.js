3874bba8883069bcd74bd08aa3ec9353da3be364
Dambroda <43309639+Dambroda@users.noreply.github.com>
2018-12-17 11:16:23 +0100

    Update src/parser/hunter/shared/modules/talents/Trailblazer.js
    Co-Authored-By: Pewtro <isondkwamm@gmail.com>

--- src/parser/hunter/shared/modules/talents/Trailblazer.js
+++ src/parser/hunter/shared/modules/talents/Trailblazer.js
@@ -25,7 +25,7 @@ class Trailblazer extends Analyzer {
     return (
       <TalentStatisticBox
         talent={SPELLS.TRAILBLAZER_TALENT.id}
-        value={`${formatPercentage(this.percentUptime)}%`}
+         value={`${formatPercentage(this.percentUptime)}% uptime`}
       />
     );
   }
