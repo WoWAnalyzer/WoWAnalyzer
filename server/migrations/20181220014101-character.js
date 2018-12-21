b94afaaa735d9028681fe6e267352aba6007eca0
Travis Harris <travisharris@Traviss-MacBook-Pro.local>
2018-12-20 16:12:51 -0800

    Fixes for problems happening in production

--- server/migrations/20181220014101-character.js
+++ server/migrations/20181220014101-character.js
@@ -27,13 +27,27 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.removeColumn('Character', 'class'),
-      queryInterface.removeColumn('Character', 'race'),
-      queryInterface.removeColumn('Character', 'thumbnail'),
-      queryInterface.removeColumn('Character', 'faction'),
-      queryInterface.removeColumn('Character', 'role'),
-      queryInterface.removeColumn('Character', 'spec'),
-      queryInterface.removeColumn('Character', 'talents'),
+      queryInterface.changeColumn('Character', 'class', Sequelize.INTEGER, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'race', Sequelize.INTEGER, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'gender', Sequelize.INTEGER, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'achievementPoints', Sequelize.INTEGER, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'thumbnail', Sequelize.STRING, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'faction', Sequelize.INTEGER, {
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('Character', 'battlegroup', Sequelize.STRING, {
+        allowNull: false,
+      }),
     ]);
   },
 };
