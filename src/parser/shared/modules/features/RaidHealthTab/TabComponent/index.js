78550fc8a994e61023de9c479b871d460a523cfe
Martijn Hols <git@martijnhols.nl>
2018-12-25 00:39:03 +0100

    Fix raid health layout

--- src/parser/shared/modules/features/RaidHealthTab/TabComponent/index.js
+++ src/parser/shared/modules/features/RaidHealthTab/TabComponent/index.js
@@ -6,15 +6,16 @@ import Panel from 'interface/others/Panel';
 import Graph from './Graph';
 
 const Mana = ({ parser }) => (
-  <Panel style={{ padding: '15px 22px' }}>
-    <h1>Raid health</h1>
+  <Panel
+    title="Raid health"
+    explanation="The stacked health of your raid to give an idea of the healing intensity. Most progression raid fights the raid will rarely be topped."
+  >
     <Graph
       reportCode={parser.report.code}
       actorId={parser.playerId}
       start={parser.fight.start_time}
       end={parser.fight.end_time}
-      currentTimestamp={parser.currentTimestamp}
-    /> {/* the currentTimestamp makes sure the Mana tab re-renders after parsing events */}
+    />
   </Panel>
 );
 Mana.propTypes = {
