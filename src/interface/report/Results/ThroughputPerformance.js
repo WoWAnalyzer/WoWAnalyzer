e15371f52163fe4c1b326f7b29ee7379100b74a5
Martijn Hols <git@martijnhols.nl>
2018-12-26 22:57:07 +0100

    Less magic numbers

--- src/interface/report/Results/ThroughputPerformance.js
+++ src/interface/report/Results/ThroughputPerformance.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import SPECS from 'game/SPECS';
 import fetchWcl from 'common/fetchWclApi';
 import CombatLogParser from 'parser/core/CombatLogParser';
+
+const DAYS_PER_WEEK = 7;
+const SECONDS_PER_DAY = 86400;
+const TOTAL_SPECS = SPECS.count;
 
 class ThroughputPerformance extends React.PureComponent {
   static propTypes = {
@@ -41,13 +46,13 @@ class ThroughputPerformance extends React.PureComponent {
     // We want to stagger the requests so not all specs are refreshed at the same time.
     // We achieve this by adding a static amount of time to `now` based on the spec index (0-35).
     const specIndex = parser.selectedCombatant.spec.index;
-    const secondsOffset = (7 * 86400 * specIndex / 35);
+    const secondsOffset = (DAYS_PER_WEEK * SECONDS_PER_DAY * specIndex / TOTAL_SPECS);
     // We mutate now so that if there's a year crossover it will properly go to week 1 instead of 53/54
     const now = new Date((new Date()).getTime() + (secondsOffset * 1000));
     // We need this to calculate the amount of weeks difference
     const onejan = new Date(now.getFullYear(), 0, 1);
     // This calculates the difference in weeks
-    const currentWeek = Math.ceil((((now - onejan) / 86400 / 1000) + onejan.getDay() + 1) / 7); // current calendar-week
+    const currentWeek = Math.ceil((((now - onejan) / SECONDS_PER_DAY / 1000) + onejan.getDay() + 1) / DAYS_PER_WEEK); // current calendar-week
 
     return fetchWcl(`rankings/encounter/${parser.fight.boss}`, {
       class: parser.selectedCombatant.spec.ranking.class,
