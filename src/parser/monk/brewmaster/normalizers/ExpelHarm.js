1ba580c4c4ab62055baf60df7756108246e59f02
J. David Smith <emallson@atlanis.net>
2018-12-25 23:04:42 -0500

    added 50ms buffer window to expel harm normalizer

--- src/parser/monk/brewmaster/normalizers/ExpelHarm.js
+++ src/parser/monk/brewmaster/normalizers/ExpelHarm.js
@@ -6,6 +6,12 @@ function isGotOxHeal(event) {
   return event.type === 'heal' && GIFT_OF_THE_OX_SPELL_IDS.includes(event.ability.guid);
 }
 
+function isCFHeal(event) {
+  return event.type === 'heal' && event.ability.guid === SPELLS.CELESTIAL_FORTUNE_HEAL.id;
+}
+
+const BUFFER_WINDOW = 50; // don't allow any larger gaps than this
+
 /**
  * Expel Harm comes after all the orbs are consumed, and honestly its a
  * huge pain in the ass. This normalizer puts it first instead.
@@ -26,8 +32,10 @@ export default class ExpelHarm extends EventsNormalizer {
       }
       // find the start of run of orbs consumed
       // safe --- game won't let you cast EH with 0 orbs
+      //
+      // Celestial Fortune heals can occur between orb heals.
       let i = idx;
-      while(events[i-1].timestamp === event.timestamp) {
+      while(events[i-1].timestamp > event.timestamp - BUFFER_WINDOW && (isGotOxHeal(events[i-1]) || isCFHeal(events[i-1]))) {
         i -= 1;
       }
       const target = i;
