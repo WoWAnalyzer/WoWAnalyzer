9263332f343bb8472a065661f7c0f5caa88868c5
Martijn Hols <git@martijnhols.nl>
2018-12-30 16:19:30 +0100

    use PlayerHits component as a background for the LoD statistic

--- src/interface/statistics/components/BoringSpellValue/index.js
+++ src/interface/statistics/components/BoringSpellValue/index.js
@@ -5,14 +5,15 @@ import SpellIcon from '../../../../common/SpellIcon';
 
 import './BoringSpellValue.scss';
 
-const BoringSpellValue = ({ spell, value, label }) => (
-  <div className="flex boring-spell-value">
+const BoringSpellValue = ({ spell, value, label, extra, className }) => (
+  <div className={`flex boring-spell-value ${className || ''}`}>
     <div className="flex-sub icon">
       <SpellIcon id={spell.id} />
     </div>
     <div className="flex-main value">
       <div>{value}</div>
       <small>{label}</small>
+      {extra}
     </div>
   </div>
 );
@@ -22,6 +23,8 @@ BoringSpellValue.propTypes = {
   }).isRequired,
   value: PropTypes.node.isRequired,
   label: PropTypes.node.isRequired,
+  extra: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default BoringSpellValue;
--- src/parser/paladin/holy/modules/core/LightOfDawn.js
+++ src/parser/paladin/holy/modules/core/LightOfDawn.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS';
-import SpellIcon from 'common/SpellIcon';
-import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events from 'parser/core/Events';
 import { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 import Statistic from 'interface/statistics/Statistic';
-import char1 from 'interface/statistics/components/AverageHitsPerCast/char1.png';
-import char1Active from 'interface/statistics/components/AverageHitsPerCast/char1-active.png';
-import char2 from 'interface/statistics/components/AverageHitsPerCast/char2.png';
-import char2Active from 'interface/statistics/components/AverageHitsPerCast/char2-active.png';
-import char3 from 'interface/statistics/components/AverageHitsPerCast/char3.png';
-import char3Active from 'interface/statistics/components/AverageHitsPerCast/char3-active.png';
-import char4 from 'interface/statistics/components/AverageHitsPerCast/char4.png';
-import char4Active from 'interface/statistics/components/AverageHitsPerCast/char4-active.png';
-import char5 from 'interface/statistics/components/AverageHitsPerCast/char5.png';
-import char5Active from 'interface/statistics/components/AverageHitsPerCast/char5-active.png';
-import './CharacterHitPerCast.scss';
+import BoringSpellValue from 'interface/statistics/components/BoringSpellValue';
+import PlayerHits from 'interface/statistics/components/PlayerHits';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events from 'parser/core/Events';
+
+import './LightOfDawn.scss';
 
 class LightOfDawn extends Analyzer {
   _casts = 0;
@@ -41,54 +33,15 @@ class LightOfDawn extends Analyzer {
     return (
       <Statistic
         position={STATISTIC_ORDER.CORE(60)}
+        size="small"
       >
-        <div className="pad">
-          <label><SpellIcon id={SPELLS.LIGHT_OF_DAWN_CAST.id} /> Average hits per cast</label>
-
-          <div className="value">{playersHitPerCast.toFixed(2)} players</div>
-
-          <div className="characters-hit-per-cast">
-            <div className="backdrop">
-              <div style={{ backgroundImage: `url(${char1})` }} />
-              <div style={{ backgroundImage: `url(${char2})` }} />
-              <div style={{ backgroundImage: `url(${char3})` }} />
-              <div style={{ backgroundImage: `url(${char4})` }} />
-              <div style={{ backgroundImage: `url(${char5})` }} />
-            </div>
-            <div className="active">
-              <div
-                style={{
-                  backgroundImage: `url(${char1Active})`,
-                  '--p': Math.max(0, Math.min(1, (performance - 0.0) / 0.2)),
-                }}
-              />
-              <div
-                style={{
-                  backgroundImage: `url(${char2Active})`,
-                  '--p': Math.max(0, Math.min(1, (performance - 0.2) / 0.2)),
-                }}
-              />
-              <div
-                style={{
-                  backgroundImage: `url(${char3Active})`,
-                  '--p': Math.max(0, Math.min(1, (performance - 0.4) / 0.2)),
-                }}
-              />
-              <div
-                style={{
-                  backgroundImage: `url(${char4Active})`,
-                  '--p': Math.max(0, Math.min(1, (performance - 0.6) / 0.2)),
-                }}
-              />
-              <div
-                style={{
-                  backgroundImage: `url(${char5Active})`,
-                  '--p': Math.max(0, Math.min(1, (performance - 0.8) / 0.2)),
-                }}
-              />
-            </div>
-          </div>
-        </div>
+        <BoringSpellValue
+          spell={SPELLS.LIGHT_OF_DAWN_CAST}
+          value={playersHitPerCast.toFixed(2)}
+          label="Average targets hit per cast"
+          className="light-of-dawn-hits-per-cast"
+          extra={<PlayerHits performance={performance} />}
+        />
       </Statistic>
     );
   }
