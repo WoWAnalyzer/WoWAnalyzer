e2674786640c0f710e0ddd923abdf31910ae6f69
David Grölle <david.groelle@protonmail.com>
2018-12-30 15:55:13 +0100

    Make the assessment for QuickDraw fairer

--- src/parser/rogue/outlaw/modules/talents/QuickDraw.js
+++ src/parser/rogue/outlaw/modules/talents/QuickDraw.js
@@ -6,7 +6,7 @@ import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events from 'parser/core/Events';
 import calculateEffectiveDamage from 'parser/core/calculateEffectiveDamage';
 import TalentStatisticBox from 'interface/others/TalentStatisticBox';
-import ItemDamageDone from 'interface/others/ItemDamageDone';
+import { formatNumber } from 'common/format';
 
 const BASE_CP_GENERATED = 1;
 const DAMAGE_INCREASE = 0.5;
@@ -18,19 +18,30 @@ class QuickDraw extends Analyzer {
     if (!this.active) {
       return;
     }
-    this.addEventListener(Events.energize.by(SELECTED_PLAYER).spell(SPELLS.PISTOL_SHOT), this._onPistolShot);
-    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.PISTOL_SHOT), this._onPistolShot);
+    this.addEventListener(Events.energize.by(SELECTED_PLAYER).spell(SPELLS.PISTOL_SHOT), this._onPistolShotEnergize);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.PISTOL_SHOT), this._onPistolShotDamage);
   }
 
   damageGained = 0;
-  comboPointsGained = 0;
+  procs = 0;
+  comboPointsWasted = 0;
 
-  _onPistolShot(event) {
+  _onPistolShotEnergize(event) {
     if (!this.selectedCombatant.hasBuff(SPELLS.OPPORTUNITY.id)) {
       return;
     }
-    this.comboPointsGained +=
-      (event.resourceChange > 0 ? event.resourceChange - BASE_CP_GENERATED : 0) || 0;
+    const cpGeneratedByBroadside = this.selectedCombatant.hasBuff(SPELLS.BROADSIDE.id) ? 1 : 0;
+    const cpForEvent = (event.resourceChange || 0) + (event.waste || 0) - cpGeneratedByBroadside - BASE_CP_GENERATED;
+    this.procs += cpForEvent > 0 ? 1 : 0;
+    if(cpForEvent>0 && event.waste>0){
+        this.comboPointsWasted+=1;
+    }
+    if(cpForEvent>0){
+      console.log(event);
+    }
+  }
+
+  _onPistolShotDamage(event) {
     this.damageGained += calculateEffectiveDamage(event, DAMAGE_INCREASE) || 0;
   }
 
@@ -38,7 +49,9 @@ class QuickDraw extends Analyzer {
     return (
       <TalentStatisticBox
         talent={SPELLS.QUICK_DRAW_TALENT.id}
-        value={<ItemDamageDone amount={this.damageGained} />} />
+        value={<>${this.procs} procs</>}
+        label={<>Quick Draw procs did ${formatNumber(this.damageGained)} extra damage and generated ${this.procs} Combopoints.</>}
+      />
     );
   }
 }
