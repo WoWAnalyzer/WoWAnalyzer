1cc0ba6a5515ccfb34fe7a593e3e087f3a223935
Alastair Scott <alastair.scott91@gmail.com>
2019-01-05 23:48:43 +0000

    Updated PatchChecker to provide link to 8.0 version of the site

---
+++ src/common/PATCHES.js
@@ -0,0 +1,16 @@
+const PATCHES = [
+  {
+    name: '8.0',
+    timestamp: 1534197600000,
+    urlPrefix: '80',
+    isCurrent: false,
+  },
+  {
+    name: '8.1',
+    timestamp: 1537221600000,
+    urlPrefix: '',
+    isCurrent: true,
+  },
+];
+
+export default PATCHES;
