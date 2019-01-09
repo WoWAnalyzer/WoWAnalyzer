657e569f745f4d861c623924ed7d8b91cc02c393
Martijn Hols <git@martijnhols.nl>
2019-01-09 23:20:40 +0100

    Update react-tooltip-lite to 3.0

--- src/common/parseVersionString.test.js
+++ src/common/parseVersionString.test.js
@@ -1,6 +1,6 @@
 import parseVersionString from './parseVersionString';
 
-describe('ReadableList', () => {
+describe('parseVersionString', () => {
   it('recognizes a full version string', () => {
     expect(parseVersionString('7.3.5')).toEqual({
       major: 7,
