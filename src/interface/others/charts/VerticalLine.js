96ccaeb64a00a8c12d83b898a853d88caf09f5b7
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-01-14 02:44:52 +0100

    Added VerticalLine component, rewrote SelfHealTimingGraph. Fixed legend item align

---
+++ src/interface/others/charts/VerticalLine.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './VerticalLine.css';
+
+class VerticalLine extends React.Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    style: PropTypes.shape({
+      wrapper: PropTypes.object,
+      line: PropTypes.object,
+    }),
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    style: {},
+    className: '',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTooltip: false,
+    };
+  }
+
+  render() {
+    const {
+      value,
+      style,
+      className,
+      children,
+      // ton of properties get injected from XYPlot
+      xRange,
+      xDomain,
+      marginLeft,
+      marginTop,
+      innerHeight,
+      innerWidth,
+    } = this.props;
+    // xDomain = [minX, maxX] - e.g. [8022559, 8222902]
+    // xRange = [0, maxXpixels] - e.g. [0, 1160]
+    // interpolate value using those two
+    const left = marginLeft + xRange[1] * value / xDomain[1];
+    const top = marginTop;
+    const orientation = left > innerWidth / 2 ? 'left' : 'right';
+    return (
+      <div
+        className={`rv-vertical-line ${className}`}
+        style={{
+          left: `${left}px`,
+          top: `${top}px`,
+          ...style.wrapper,
+        }}
+      >
+        <div
+          className="rv-vertical-line__line"
+          style={{
+            height: `${innerHeight}px`,
+            ...style.line,
+          }}
+          onMouseEnter={() => this.setState({ showTooltip: true })}
+          onMouseLeave={() => this.setState({ showTooltip: false })}
+        />
+
+        {children && this.state.showTooltip && (
+          <div className={`rv-vertical-line__tooltip rv-vertical-line__tooltip--${orientation} react-tooltip-lite`}>
+            {children}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default VerticalLine;
