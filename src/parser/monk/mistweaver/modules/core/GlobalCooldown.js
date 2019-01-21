292da0801b055f0c06ea070f12541085250b443f
Martijn Hols <git@martijnhols.nl>
2019-01-21 21:42:28 +0100

    Mistweaver Monk: Same for Essence Font

--- src/parser/monk/mistweaver/modules/core/GlobalCooldown.js
+++ src/parser/monk/mistweaver/modules/core/GlobalCooldown.js
@@ -6,7 +6,7 @@ import CoreGlobalCooldown from 'parser/shared/modules/GlobalCooldown';
  */
 class GlobalCooldown extends CoreGlobalCooldown {
   on_byPlayer_cast(event) {
-    if (event.ability.guid === SPELLS.CRACKLING_JADE_LIGHTNING.id || event.ability.guid === SPELLS.SOOTHING_MIST.id) {
+    if (event.ability.guid === SPELLS.CRACKLING_JADE_LIGHTNING.id || event.ability.guid === SPELLS.SOOTHING_MIST.id || event.ability.guid === SPELLS.ESSENCE_FONT.id) {
       // Channeling fabricates fake `beginchannel` events for these abilities, that already takes care of the GCD.
       return;
     }
