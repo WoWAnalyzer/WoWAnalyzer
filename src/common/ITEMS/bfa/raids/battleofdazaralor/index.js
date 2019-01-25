b149cee8bbc8a1df92b3b408b4b4f9efc7fb32ce
Dylan Dirlam <dadirlam@hotmail.com>
2019-01-25 14:44:41 -0500

    Add Battle of Dazaralor item files

---
+++ src/common/ITEMS/bfa/raids/battleofdazaralor/index.js
@@ -0,0 +1,21 @@
+// Items only found in this instance
+import ITEM_QUALITIES from 'game/ITEM_QUALITIES';
+
+export default {
+  // Group by boss (with comments)
+  // Champion of the Light
+  WARD_OF_ENVELOPMENT: {
+    id: 165569,
+    name: "Ward of Envelopment",
+    icon: "misc_legionfall_paladin",
+    quality: ITEM_QUALITIES.PURPLE,
+  },
+  // Grong
+  // Jadefire Masters
+  // Opulence
+  // Conclave of the Chosen
+  // King Rastahkhan
+  // Higher Tinker Mekkatorque
+  // Stormwall Blockade
+  // Lady Jaina Proudmoore
+};
