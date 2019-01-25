cce04e01d897e1445ad7a7dccaf5347d71daea7f
joshinat0r <joshuahahn1996@googlemail.com>
2019-01-25 11:43:19 +0100

    update zones for the characters parses

--- src/game/ZONES.js
+++ src/game/ZONES.js
@@ -7,6 +7,56 @@
 
 const ZONES = [
   {
+    "id": 21,
+    "name": "Battle of Dazar'alor",
+    "encounters": [
+      {
+        "id": 2265,
+        "name": "Champion of the Light",
+        "npcID": 144680
+      },
+      {
+        "id": 2266,
+        "name": "Jadefire Masters",
+        "npcID": 144690
+      },
+      {
+        "id": 2263,
+        "name": "Grong",
+        "npcID": 144637
+      },
+      {
+        "id": 2271,
+        "name": "Opulence",
+        "npcID": 145261
+      },
+      {
+        "id": 2268,
+        "name": "Conclave of the Chosen",
+        "npcID": 144747
+      },
+      {
+        "id": 2272,
+        "name": "King Rastakhan",
+        "npcID": 145616
+      },
+      {
+        "id": 2276,
+        "name": "Mekkatorque",
+        "npcID": 144796
+      },
+      {
+        "id": 2280,
+        "name": "Stormwall Blockade",
+        "npcID": 146256
+      },
+      {
+        "id": 2281,
+        "name": "Lady Jaina Proudmoore",
+        "npcID": 146416
+      }
+    ],
+   }, {
     "id": 19,
     "name": "Uldir",
     "encounters": [
