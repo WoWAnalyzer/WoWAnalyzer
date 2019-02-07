86f32fff974d78f333573d9261352715cb4bf00f
Michael Bui <micb@redpill-linpro.com>
2019-02-08 00:13:32 +0100

    Resto druid - include mastery healing in Grove tending

--- src/parser/druid/restoration/modules/items/azeritetraits/GroveTending.js
+++ src/parser/druid/restoration/modules/items/azeritetraits/GroveTending.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Analyzer from 'parser/core/Analyzer';
-import {formatNumber} from 'common/format';
+import Mastery from 'parser/druid/restoration/modules/core/Mastery';
+import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import TraitStatisticBox, { STATISTIC_ORDER } from 'interface/others/TraitStatisticBox';
 import StatWeights from '../../features/StatWeights';
@@ -12,6 +13,7 @@ import {getPrimaryStatForItemLevel, findItemLevelByPrimaryStat} from './/common'
 class GroveTending extends Analyzer{
   static dependencies = {
     statWeights: StatWeights,
+    mastery: Mastery,
   };
 
   healing = 0;
@@ -37,16 +39,21 @@ class GroveTending extends Analyzer{
 
   statistic(){
     const throughputPercent = this.owner.getPercentageOfTotalHealingDone(this.healing);
+    const masteryHealing = this.mastery.getMasteryHealing(SPELLS.GROVE_TENDING.id);
+    const masteryPercent = this.owner.getPercentageOfTotalHealingDone(masteryHealing);
+
     const onePercentThroughputInInt = this.statWeights._ratingPerOnePercent(this.statWeights.totalOneInt);
-    const intGain = onePercentThroughputInInt * throughputPercent * 100;
+    const intGain = onePercentThroughputInInt * (throughputPercent + masteryPercent) * 100;
     const ilvlGain = findItemLevelByPrimaryStat(getPrimaryStatForItemLevel(this.avgItemLevel) + intGain) - this.avgItemLevel;
 
     return(
       <TraitStatisticBox
         position={STATISTIC_ORDER.OPTIONAL()}
         trait={SPELLS.GROVE_TENDING_TRAIT.id}
-        value={`${this.owner.formatItemHealingDone(this.healing)}`}
-        tooltip={`Grove Tending gave you equivalent to <b>${formatNumber(intGain)}</b> (${formatNumber(intGain/this.traitLevel)}
+        value={`${this.owner.formatItemHealingDone(this.healing + masteryHealing)}`}
+        tooltip={`Healing from HoT: ${this.owner.formatItemHealingDone(this.healing)} <br/>
+                  Healing from extra mastery stack: ${this.owner.formatItemHealingDone(masteryHealing)} <br/>
+                  Grove Tending gave you equivalent to <b>${formatNumber(intGain)}</b> (${formatNumber(intGain/this.traitLevel)}
             per level) int. This is worth roughly <b>${formatNumber(ilvlGain)}</b> (${formatNumber(ilvlGain/this.traitLevel)}
             per level) item levels.`}
       />
