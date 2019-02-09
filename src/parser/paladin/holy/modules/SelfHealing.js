4293284b6efacf9e51d57bfcf194a04a43b79086
Tim Gardiner <tgar1989@gmail.com>
2019-02-08 19:11:52 -0500

    fixed warnings

--- src/parser/paladin/holy/modules/SelfHealing.js
+++ src/parser/paladin/holy/modules/SelfHealing.js
@@ -5,7 +5,7 @@ import SpellIcon from 'common/SpellIcon';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import StatisticBox, { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 import Events from 'parser/core/Events';
-import { formatThousands, formatNumber, formatPercentage } from 'common/format';
+import { formatThousands, formatPercentage } from 'common/format';
 import HealingDone from 'parser/shared/modules/HealingDone';
 
 import { DIRECT_SELF_HEALING_ABILITIES } from '../constants';
@@ -16,13 +16,13 @@ import { DIRECT_SELF_HEALING_ABILITIES } from '../constants';
 
 class SelfHealing extends Analyzer {
     static dependencies = {
-        healingDone: HealingDone
+        healingDone: HealingDone,
     };
-
 
     _healing = 0;
     _overhealing = 0;
     _selfHealPercent = 0;
+
     constructor(...args) {
         super(...args);
         this.addEventListener(Events.heal.by(SELECTED_PLAYER), this._onCast);
@@ -32,23 +32,24 @@ class SelfHealing extends Analyzer {
         if (DIRECT_SELF_HEALING_ABILITIES.includes(event.ability.guid)) {
             if (this.owner.player.id === event.targetID) {
                 this._healing += event.amount;
-                this._overhealing += event.overheal;
+                this._overhealing += event.overheal || 0;
             }
-        };
+        }
     }
 
     getSelfHealingPercent() {
         return this._healing / this.healingDone.total._regular;
     }
+
     statistic() {
         return (
             <StatisticBox
-                position={STATISTIC_ORDER.CORE(61)}
-                icon={<SpellIcon id={SPELLS.LIGHT_OF_DAWN_CAST.id} />}
-                value={`${formatPercentage(this.getSelfHealingPercent())} %`}
-                tooltip={`self-healing: <b>${formatThousands(this._healing)}</b>
+              position={STATISTIC_ORDER.CORE(61)}
+              icon={<SpellIcon id={SPELLS.LIGHT_OF_DAWN_CAST.id} />}
+              value={`${formatPercentage(this.getSelfHealingPercent())} %`}
+              tooltip={`self-healing: <b>${formatThousands(this._healing)}</b>
                         self-overhealing:<b> ${formatThousands(this._overhealing)}</b>`}
-                label="Self Healing"
+              label="Self Healing"
             />
         );
     }
