52f711ff88008e7b645ed9055ef2d04ef195f875
Martijn Hols <git@martijnhols.nl>
2019-02-17 20:37:36 +0100

    Fix invalid prop check

--- src/parser/shared/modules/Entities.js
+++ src/parser/shared/modules/Entities.js
@@ -75,7 +75,7 @@ class Entities extends Analyzer {
 
     this._triggerChangeBuffStack(buff, event.timestamp, 0, 1);
 
-    if (event.prepull && entity.__fromCombatantinfo) {
+    if (event.prepull && event.__fromCombatantinfo) {
       // Prepull buffs were already applied in the Combatant constructor
       return;
     }
