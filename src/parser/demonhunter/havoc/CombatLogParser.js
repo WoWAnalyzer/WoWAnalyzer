d34fd5ef43411740a2e2b19d2e3615ad3d00b6ee
yajinni <yajinni@gmail.com>
2019-02-20 20:39:31 -0500

    initial spells

--- src/parser/demonhunter/havoc/CombatLogParser.js
+++ src/parser/demonhunter/havoc/CombatLogParser.js
@@ -28,6 +28,9 @@ import TrailofRuin from './modules/talents/TrailofRuin';
 import FelBarrage from './modules/talents/FelBarrage';
 import FelMastery from './modules/talents/FelMastery';
 import Netherwalk from './modules/talents/Netherwalk';
+import DarkSlash from './modules/talents/DarkSlash';
+import CycleOfHatred from './modules/talents/CycleOfHatred';
+
 //Resources
 import FuryDetails from './modules/resourcetracker/FuryDetails';
 import FuryTracker from './modules/resourcetracker/FuryTracker';
@@ -67,6 +70,8 @@ class CombatLogParser extends CoreCombatLogParser {
     momentum: Momentum,
     nemesis: Nemesis,
     netherwalk: Netherwalk,
+    darkSlash: DarkSlash,
+    cycleOfHatred: CycleOfHatred,
 
 
     //Resources
