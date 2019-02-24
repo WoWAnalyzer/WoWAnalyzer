37f5a7c291bbc7993c333c5d98ac42049d917f6f
Florian Schut <mail@florianschut.nl>
2019-02-24 23:45:32 +0100

    [EDIT]: Added myself to contributors to note changed made in the changelist

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Yajinni, Mamtooth, Zerotorescue, Putro, joshinator, Gebuz, ackwell, emallson, blazyb, Dambroda, Nalhan, Satyric, niseko, Khadaj, Fyruna, Matardarix, jos3p, Aelexe, Chizu, Hartra344, Hordehobbs } from 'CONTRIBUTORS';
+import { Yajinni, Mamtooth, Zerotorescue, Putro, joshinator, Gebuz, ackwell, emallson, blazyb, Dambroda, Nalhan, Satyric, niseko, Khadaj, Fyruna, Matardarix, jos3p, Aelexe, Chizu, Hartra344, Hordehobbs, Dorixius } from 'CONTRIBUTORS';
 import ItemLink from 'common/ItemLink';
 import ITEMS from 'common/ITEMS';
 import SPELLS from 'common/SPELLS';
@@ -7,6 +7,11 @@ import SpellLink from 'common/SpellLink';
 import Contributor from 'interface/contributor/Button';
 
 export default [
+  {
+    date: new Date('2019-02-24'),
+    changes: <>Added role count to raid composition screen</>,
+    contributors: [Dorixius],
+  },
   {
     date: new Date('2019-02-15'),
     changes: <>If Warcraft Logs sends a corrupt JSON message, try to automatically decorrupt it.</>,
