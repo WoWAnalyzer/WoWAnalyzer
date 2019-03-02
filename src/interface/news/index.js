14703665239146c80453fbad482ad0f881baebaf
Martijn Hols <git@martijnhols.nl>
2019-03-02 16:11:41 +0100

    Add Twitter link

--- src/interface/news/index.js
+++ src/interface/news/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import DocumentTitle from 'interface/common/DocumentTitle';
+import TwitterIcon from 'interface/icons/Twitter';
 
 import NewsList from './List';
 
@@ -10,7 +11,17 @@ class News extends React.PureComponent {
       <>
         <DocumentTitle /> {/* prettiest is if the Home page has no title at all */}
 
-        <h1 id="news-top">News</h1>
+        <div className="flex">
+          <div className="flex-main">
+            <h1 id="news-top">News</h1>
+          </div>
+          <div className="flex-sub" style={{ paddingTop: 30 }}>
+            <small>More news?</small>
+            <span style={{ fontSize: 18, marginLeft: 10 }}>
+              <TwitterIcon colored /> <a href="https://twitter.com/WoWAnalyzer">Follow @WoWAnalyzer</a>
+            </span>
+          </div>
+        </div>
         <NewsList topAnchor="news-top" />
       </>
     );
