73e2a2ca8445a5ddbeb9c61086f92d3f4b8fb926
Martijn Hols <git@martijnhols.nl>
2019-03-06 19:08:05 +0100

    Add config for M+ dungeons

--- src/raids/dungeons/index.js
+++ src/raids/dungeons/index.js
@@ -3,5 +3,14 @@ export default {
   background: undefined, // TODO: Set up
   bosses: {
     AtalDazar: require('./AtalDazar').default,
+    Freehold: require('./Freehold').default,
+    KingsRest: require('./KingsRest').default,
+    ShrineOfTheStorm: require('./ShrineOfTheStorm').default,
+    SiegeOfBoralus: require('./SiegeOfBoralus').default,
+    TempleOfSethraliss: require('./TempleOfSethraliss').default,
+    TheMotherlode: require('./TheMotherlode').default,
+    TheUnderrot: require('./TheUnderrot').default,
+    TolDagor: require('./TolDagor').default,
+    WaycrestManor: require('./WaycrestManor').default,
   },
 };
