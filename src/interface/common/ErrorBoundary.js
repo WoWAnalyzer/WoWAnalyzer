3945d38d9c3c5b35ee471e38b21e306cb253786a
Martijn Hols <git@martijnhols.nl>
2019-03-07 19:38:24 +0100

    Improve error boundary rendering

--- src/interface/common/ErrorBoundary.js
+++ src/interface/common/ErrorBoundary.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { captureException } from 'common/errorLogger';
-import FullscreenError from 'interface/common/FullscreenError';
-
-import ApiDownBackground from './images/api-down-background.gif';
 
 class ErrorBoundary extends React.Component {
   static propTypes = {
@@ -37,30 +34,27 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.error) {
       return (
-        <FullscreenError
-          error="A rendering error occured."
-          details="An error occured while trying to render (a part of) this page. Please try again."
-          background={ApiDownBackground}
-          errorDetails={(
-            <>
-              <p>{this.state.error.message}</p>
-              {this.state.error.stack && (
-                <pre style={{ color: 'red', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
-                  {this.state.error.stack}
-                </pre>
-              )}
-              {this.state.errorDetails && (
-                <pre style={{ color: 'red' }}>
-                  {this.state.errorDetails}
-                </pre>
-              )}
-            </>
+        <div className="alert alert-danger">
+          <h1 style={{ marginTop: 0 }}>An error occured while trying to render this part of the page.</h1>
+          <p className="text-muted">
+            This is usually caused by a bug in our code. If you're handy with computers please consider sending us a Pull Request with a fix on <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a>. Otherwise please let us know in an issue on <a href="https://github.com/WoWAnalyzer/WoWAnalyzer">GitHub</a> or leave us a message on <a href="https://wowanalyzer.com/discord">Discord</a> so we can fix it for you.
+          </p>
+
+          <h1>The error</h1>
+          <p className="text-muted">Technical information to help you fix it. Or us if not you. Technical information to help whoever is inclined to fix the issue.</p>
+          <p>{this.state.error.message}</p>
+          {this.state.error.stack && (
+            <pre style={{ color: 'red', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
+              {this.state.error.stack.trim()}
+            </pre>
           )}
-        >
-          <div className="text-muted">
-            This is usually caused by a bug, please let us know about the issue on GitHub or Discord so we can fix it.
-          </div>
-        </FullscreenError>
+          {this.state.errorDetails && (
+            <pre style={{ color: 'red' }}>
+              The above error occurred in the component:
+              {this.state.errorDetails}
+            </pre>
+          )}
+        </div>
       );
     }
     return this.props.children;
