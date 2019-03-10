a16735173fb63d20d51c913583ffde4c67fe8f1c
Peter Schmidt <pschmidt@buffalo.edu>
2019-03-09 20:54:59 -0500

    Converted to event listeners, beacon transfer still not working.

--- src/parser/shared/modules/spells/bfa/azeritetraits/GraceOfTheJusticar.js
+++ src/parser/shared/modules/spells/bfa/azeritetraits/GraceOfTheJusticar.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Analyzer from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS/index';
 import TraitStatisticBox, { STATISTIC_ORDER } from 'interface/others/TraitStatisticBox';
 import ItemHealingDone from 'interface/others/ItemHealingDone';
+import Events from 'parser/core/Events';
+
+
 
 /**
  * Grace of the Justicar
@@ -11,44 +14,44 @@ import ItemHealingDone from 'interface/others/ItemHealingDone';
  * Example Log: https://www.warcraftlogs.com/reports/kMbVanmJwCg7WrAz#fight=last&type=summary&source=2
  */
 class GraceOfTheJusticar extends Analyzer {
-  graceHealing = 0;
+  healing = 0;
   targetsHit = 0;
   beaconTransfer = 0;
   casts = 0;
-  justJudged = false;
 
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTrait(SPELLS.GRACE_OF_THE_JUSTICAR_TRAIT.id);
-  }
-
-  on_byPlayer_cast(event) {
-    if (event.ability.guid === SPELLS.JUDGMENT_CAST_ALT.id) {
-      this.justJudged = true;
-      this.casts += 1;
-    }
-    else {
-      this.justJudged = false;
-    }
-  }
-
-  on_byPlayer_heal(event) {
-    const spellId = event.ability.guid;
-    if (spellId === SPELLS.GRACE_OF_THE_JUSTICAR.id) {
-      this.graceHealing += event.amount + (event.absorbed || 0);
-      this.targetsHit += 1;
+    if (!this.active) {
       return;
     }
-    if (spellId === SPELLS.BEACON_OF_LIGHT_HEAL.id && this.justJudged) {
-      this.beaconTransfer += event.amount + (event.absorbed || 0);
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.JUDGMENT_CAST_ALT), this.onCast);
+    this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.GRACE_OF_THE_JUSTICAR), this.onHeal);
+    //this.addEventListener(Events.beacontransfer.by(SELECTED_PLAYER).originalHeal(SPELLS.GRACE_OF_THE_JUSTICAR), this.onBeaconTransfer);
+  }
+
+  onBeaconTransfer(event) {
+    const spellId = event.originalHeal.ability.guid;
+    if (spellId !== SPELLS.GRACE_OF_THE_JUSTICAR.id) {
+      return;
     }
+    this.beaconTransfer += event.amount + (event.absorbed || 0);
+  }
+
+  onCast(event) {
+    this.casts += 1;
+  }
+
+  onHeal(event) {
+    this.healing += event.amount + (event.absorbed || 0);
+    this.targetsHit += 1;
   }
 
   get playersHitPerCast() {
     return (this.targetsHit / this.casts) || 0;
   }
   get totalHealing() {
-    return this.graceHealing + this.beaconTransfer;
+    return this.healing + this.beaconTransfer;
   }
 
   statistic() {
@@ -59,6 +62,7 @@ class GraceOfTheJusticar extends Analyzer {
         value={<ItemHealingDone amount={this.totalHealing} />}
         tooltip={`
           Total healing done: ${formatNumber(this.totalHealing)}<br />
+          Becon Transfer: ${formatNumber(this.beaconTransfer)}<br />
           Players hit per cast: ${this.playersHitPerCast.toFixed(2)}
         `}
       />
