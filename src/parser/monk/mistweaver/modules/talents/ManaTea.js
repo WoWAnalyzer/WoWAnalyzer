7637cf8370eeb181d9502fdbb0346b5e12f98cea
Abel Melchiore <abelgm@charter.net>
2019-03-11 03:46:57 -0400

    Mana tea update
    -added tracker for overhealing in mana tea window
    -added suggestion for it too
    -tbh don't know why package-log.json updated

--- src/parser/monk/mistweaver/modules/talents/ManaTea.js
+++ src/parser/monk/mistweaver/modules/talents/ManaTea.js
@@ -1,7 +1,8 @@
 // Modified from Original Code by Blazyb and his Innervate module.
 import React from 'react';
 
-import SPELLS from 'common/SPELLS';
+import SPELLS from 'common/SPELLS'; //Do we really need all spells when we just care about monk spells (but it is nice since it gets talents too)
+import MONKSPELLS from 'common/SPELLS/monk';
 import SpellLink from 'common/SpellLink';
 import { formatNumber, formatThousands } from 'common/format';
 import TalentStatisticBox from 'interface/others/TalentStatisticBox';
@@ -24,7 +25,6 @@ class ManaTea extends Analyzer {
   manaSavedMT = 0;
   manateaCount = 0;
 
-  effCasts = 0;
   enmCasts = 0;
   efCasts = 0;
   lcCasts = 0;
@@ -32,12 +32,18 @@ class ManaTea extends Analyzer {
   revCasts = 0;
   vivCasts = 0;
   rjwCasts = 0;
+  soomTicks = 0;
 
   nonManaCasts = 0;
   castsUnderManaTea = 0;
 
   hasLifeCycles = false;
   casted = false;
+
+  castedStackFrame = [MONKSPELLS.GUSTS_OF_MISTS.id];//alwasy have gusts since this will only proc when a spell is hard cast
+  effectivehealing = 0;
+  overhealing = 0;
+  overhealingPercent = 0.0;
 
   constructor(...args) {
     super(...args);
@@ -50,8 +56,28 @@ class ManaTea extends Analyzer {
   on_toPlayer_applybuff(event) {
     const spellId = event.ability.guid;
     if (SPELLS.MANA_TEA_TALENT.id === spellId) {
-      this.manateaCount += 1;
-      debug && console.log(`Mana Tea Cast +1. Total:${this.manateaCount}`);
+      this.manateaCount += 1;//count the number of mana teas to make an average over teas
+    }
+  }
+
+  on_toPlayer_removebuff(event){
+    const spellId = event.ability.guid;
+    if(SPELLS.MANA_TEA_TALENT.id === spellId){
+      console.log("we lost mana tea");
+      this.castedStackFrame = [MONKSPELLS.GUSTS_OF_MISTS.id];//alwasy have gusts since this will only proc when a spell is hard cast
+    }
+  }
+
+  on_heal(event){
+    if (this.selectedCombatant.hasBuff(SPELLS.MANA_TEA_TALENT.id)) {//if this is in a mana tea window
+      const spellId = event.ability.guid;
+      const that = this;
+      Object.keys(MONKSPELLS).forEach(function(k){//is the heal a monk spell (this goes through all monk spells even brew/ww)
+        if (spellId === MONKSPELLS[k].id && that.castedStackFrame.includes(event.ability.guid)) {//also check if this is casted (this allows us to ignore hots that were already casted)
+          that.effectivehealing += (event.amount || 0) + (event.absorbed || 0);
+          that.overhealing +=(event.overheal || 0);
+        }
+      });
     }
   }
 
@@ -59,6 +85,18 @@ class ManaTea extends Analyzer {
     const spellId = event.ability.guid;
 
     if (this.selectedCombatant.hasBuff(SPELLS.MANA_TEA_TALENT.id)) {
+      this.castedStackFrame.push(spellId);
+      if(SPELLS.SOOTHING_MIST.id === spellId){//added soothing mist since it costs mana now
+        this.addToManaSaved(SPELLS.SOOTHING_MIST.manaCost, spellId);
+        this.castsUnderManaTea += 1;
+        this.soomTicks += 1;
+        this.casted = true;
+        this.castedStackFrame.push(SPELLS.SOOTHING_MIST_STATUE.id);//if statue is talented check for its healing too
+      }
+      if(SPELLS.INVOKE_CHIJI_THE_RED_CRANE_TALENT.id === spellId){//added this for overhealing tracking
+        this.castedStackFrame.push(SPELLS.CRANE_HEAL.id);//if chi-ji is talented check for its healing too
+      }
+
       if (SPELLS.ENVELOPING_MIST.id === spellId) {
         this.addToManaSaved(SPELLS.ENVELOPING_MIST.manaCost, spellId);
         this.castsUnderManaTea += 1;
@@ -66,6 +104,7 @@ class ManaTea extends Analyzer {
         this.casted = true;
       }
       if (SPELLS.ESSENCE_FONT.id === spellId) {
+        this.castedStackFrame.push(SPELLS.ESSENCE_FONT_BUFF.id);
         this.addToManaSaved(SPELLS.ESSENCE_FONT.manaCost, spellId);
         this.castsUnderManaTea += 1;
         this.efCasts += 1;
@@ -78,6 +117,7 @@ class ManaTea extends Analyzer {
         this.casted = true;
       }
       if (SPELLS.RENEWING_MIST.id === spellId) {
+        this.castedStackFrame.push(SPELLS.RENEWING_MIST_HEAL.id);
         this.addToManaSaved(SPELLS.RENEWING_MIST.manaCost, spellId);
         this.castsUnderManaTea += 1;
         this.remCasts += 1;
@@ -115,29 +155,22 @@ class ManaTea extends Analyzer {
       this.nonManaCasts += 1;
       return;
     }
-    // Lifecycles reduces the mana cost of both Vivify and Enveloping Mists.  We must take that into account when calculating mana saved.
-    if (this.hasLifeCycles) {
-      if (this.selectedCombatant.hasBuff(SPELLS.LIFECYCLES_VIVIFY_BUFF.id) && spellId === SPELLS.VIVIFY.id) {
-        this.manaSavedMT += ((spellBaseMana * (1 - (SPELLS.LIFECYCLES_VIVIFY_BUFF.manaPercRed))) * (1 - manaTeaReduction));
-        debug && console.log('LC Viv Cast');
-      } else if ((this.selectedCombatant.hasBuff(SPELLS.LIFECYCLES_ENVELOPING_MIST_BUFF.id) && spellId === SPELLS.ENVELOPING_MIST.id)) {
-        this.manaSavedMT += ((spellBaseMana * (1 - (SPELLS.LIFECYCLES_ENVELOPING_MIST_BUFF.manaPercRed))) * (1 - manaTeaReduction));
-      } else {
-        this.manaSavedMT += (spellBaseMana * (1 - manaTeaReduction));
-      }
-    } else {
-      this.manaSavedMT += (spellBaseMana * (1 - manaTeaReduction));
-    }
+    this.manaSavedMT += (spellBaseMana * (1 - manaTeaReduction));//removed lifecycles as the combo isn't possible anymore
   }
   on_fightend() {
-    if (debug) {
-      console.log(`Mana Tea Casted: ${this.manateaCount}`);
-      console.log(`Mana saved: ${this.manaSavedMT}`);
-      console.log(`Avg. Mana saved: ${this.manaSavedMT / this.manateaCount}`);
-      console.log(`Total Casts under Mana Tea: ${this.castsUnderManaTea}`);
-      console.log(`Avg Casts under Mana Tea: ${this.castsUnderManaTea / this.manateaCount}`);
-      console.log(`Free spells cast: ${this.nonManaCasts}`);
-    }
+    this.overHealingPercent = this.overhealing/(this.overhealing+this.effectivehealing);
+    this.overHealingPercent = Math.round(this.overHealingPercent * 1000)/1000;
+    if(debug){
+    console.log(`Mana Tea Casted: ${this.manateaCount}`);
+    console.log(`Mana saved: ${this.manaSavedMT}`);
+    console.log(`Avg. Mana saved: ${this.manaSavedMT / this.manateaCount}`);
+    console.log(`Total Casts under Mana Tea: ${this.castsUnderManaTea}`);
+    console.log(`Avg Casts under Mana Tea: ${this.castsUnderManaTea / this.manateaCount}`);
+    console.log(`Free spells cast: ${this.nonManaCasts}`);
+    console.log(`Effective healing: ${this.effectivehealing}`);
+    console.log(`Overhealing healing: ${this.overhealing}`);
+    console.log(`OverHealing percentage: ${this.overHealingPercent}`);
+  }
   }
 
   get avgMtSaves() {
@@ -162,16 +195,44 @@ class ManaTea extends Analyzer {
     };
   }
 
+  get avgOverHealing(){
+    let overHealingPercent = this.overhealing/(this.overhealing+this.effectivehealing);
+    overHealingPercent = Math.round(overHealingPercent * 1000)/1000;
+    return overHealingPercent;
+  }
+
+  get suggestionThresholdsMana(){
+    return {
+      actual: this.avgOverHealing,
+      isGreaterThan: {
+        minor: .20,
+        average: .30,
+        major: .40,
+      },
+      style: 'percentage',
+    };
+  }
+
   suggestions(when) {
     when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
       return suggest(
         <>
-          Your mana spent during <SpellLink id={SPELLS.MANA_TEA_TALENT.id} /> can be improved. Always aim to cast your highest mana spells such as <SpellLink id={SPELLS.ESSENCE_FONT.id} /> or <SpellLink id={SPELLS.VIVIFY.id} />.
+          Your mana spent during <SpellLink id={SPELLS.MANA_TEA_TALENT.id} /> can be improved. Aim to only cast <SpellLink id={SPELLS.VIVIFY.id} /> untill the last second then cast <SpellLink id={SPELLS.ESSENCE_FONT.id} />.
         </>
       )
         .icon(SPELLS.MANA_TEA_TALENT.icon)
         .actual(`${formatNumber(this.avgMtSaves)} average mana saved per Mana Tea cast`)
         .recommended(`${(recommended / 1000).toFixed(0)}k average mana saved is recommended`);
+    });
+    when(this.suggestionThresholdsMana).addSuggestion((suggest, actual, recommended) => {
+      return suggest(
+        <>
+          On average during your <SpellLink id={SPELLS.MANA_TEA_TALENT.id} /> you overhealed a decent amount. This can be improved. Always aim to cast into targets with lower health.
+        </>
+      )
+        .icon(SPELLS.MANA_TEA_TALENT.icon)
+        .actual(`${this.overHealingPercent*100} % average overhealing per Mana Tea cast`)
+        .recommended(`under 20% over healing is recommended`);
     });
   }
 
@@ -189,7 +250,7 @@ class ManaTea extends Analyzer {
               {this.efCasts > 0 && <li>{(this.efCasts)} Essence Font casts</li>}
               {this.efCasts > 0 && <li>{(this.vivCasts)} Vivfy casts</li>}
               {this.efCasts > 0 && <li>{(this.enmCasts)} Enveloping Mists casts</li>}
-              <li>{(this.rjwCasts + this.revCasts + this.remCasts + this.lcCasts + this.effCasts)} other spells casted.</li>
+              <li>{(this.rjwCasts + this.revCasts + this.remCasts + this.lcCasts)} other spells casted.</li>
               <li>{(this.nonManaCasts)} non-mana casts during Mana Tea</li>
             </ul>
           </>
