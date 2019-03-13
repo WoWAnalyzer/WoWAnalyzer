2c7b2fca51d833c9a0fa214ed7ff117138c2f8fc
Martijn Hols <git@martijnhols.nl>
2019-03-14 00:22:17 +0100

    Change in-feed ad styling

--- src/interface/report/Results/Statistics.js
+++ src/interface/report/Results/Statistics.js
@@ -105,8 +105,7 @@ class Statistics extends React.PureComponent {
                   <div className="col-md-6 hidden-xs">
                     <Ad
                       data-ad-format="fluid"
-                      data-ad-layout-key="-f1+5n+54-df+7k"
-                      data-ad-client="ca-pub-8048055232081854"
+                      data-ad-layout-key="-ei+3w+e8-ee-en"
                       data-ad-slot="2181760614"
                     />
                   </div>
