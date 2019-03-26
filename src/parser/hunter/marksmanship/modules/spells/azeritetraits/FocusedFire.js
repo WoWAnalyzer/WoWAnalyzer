ba2121ad8838462f563f632dbf8fe68b3162b37c
Putro <isondkwamm@gmail.com>
2019-03-26 20:59:09 +0100

    [Hunter] Remove a console log in DireBeast and add Focused Fire Azerite Trait to Marksmanship

---
+++ src/parser/hunter/marksmanship/modules/spells/azeritetraits/FocusedFire.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Analyzer from 'parser/core/Analyzer';
+import SPELLS from 'common/SPELLS';
+import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
+import AzeritePowerStatistic from 'interface/statistics/AzeritePowerStatistic';
+import { calculateAzeriteEffects } from 'common/stats';
+import StatTracker from 'parser/shared/modules/StatTracker';
+import { formatNumber } from 'common/format';
+import SpellLink from 'common/SpellLink';
+
+const focusedFireStats = traits => Object.values(traits).reduce((obj, rank) => {
+  const [damage] = calculateAzeriteEffects(SPELLS.FOCUSED_FIRE.id, rank);
+  obj.damage += damage;
+  return obj;
+}, {
+  damage: 0,
+
+});
+
+class FocusedFire extends Analyzer {
+  static dependencies = {
+    statTracker: StatTracker,
+  };
+  focusGained = 0;
+  focusWasted = 0;
+  damagePotential = 0;
+
+  constructor(...args) {
+    super(...args);
+    this.active = this.selectedCombatant.hasTrait(SPELLS.FOCUSED_FIRE.id);
+    if (!this.active) {
+      return;
+    }
+    const { damage } = focusedFireStats(this.selectedCombatant.traitsBySpellId[SPELLS.FOCUSED_FIRE.id]);
+    this.damagePotential = damage * 10;
+  }
+
+  on_byPlayer_energize(event) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.FOCUSED_FIRE_FOCUS.id) {
+      return;
+    }
+    this.focusGained += event.resourceChange;
+    this.focusWasted += event.waste;
+  }
+
+  statistic() {
+    return (
+      <AzeritePowerStatistic
+        size="flexible"
+      >
+        <BoringSpellValueText spell={SPELLS.FOCUSED_FIRE}>
+          {this.focusGained}/{this.focusWasted + this.focusGained}
+          <small> gained Focus</small>
+          <br />
+          <small>Up to</small>
+          {formatNumber(this.damagePotential)}
+          <small> damage per <SpellLink id={SPELLS.RAPID_FIRE.id} /></small>
+        </BoringSpellValueText>
+      </AzeritePowerStatistic>
+    );
+  }
+
+}
+
+export default FocusedFire;
