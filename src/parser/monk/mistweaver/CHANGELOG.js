7de1930ca6cf3811ec8896367e9a7a67a6cce47b
Abel Melchiore <abelgm@charter.net>
2019-03-28 23:41:20 -0400

    ChangeLog
    Changed from ' -> <> so the spellLink would work

--- src/parser/monk/mistweaver/CHANGELOG.js
+++ src/parser/monk/mistweaver/CHANGELOG.js
@@ -12,7 +12,7 @@ export default [
   },
   {
     date: new Date('2019-03-21'),
-    changes: 'Updated mastery tracking to more accurately reflect the spell that triggered the <SpellLink id={SPELLS.GUST_OF_MIST.id} />. This includes updates to the statistic and healing efficiency sections.',
+    changes: <>Updated mastery tracking to more accurately reflect the spell that triggered the <SpellLink id={SPELLS.GUSTS_OF_MISTS.id} />. This includes updates to the statistic and healing efficiency sections.</>,
     contributors: [Abelito75],
   },
   {
