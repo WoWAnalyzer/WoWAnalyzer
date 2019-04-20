ef93785a81b9335824e190e02d9391fc79f0ec9d
Michael Bush <budawolf333@gmail.com>
2019-04-20 17:48:12 -0400

    Handling CP gains from Sinister Strike procs
    There is a bug I noticed with WoW combat logs where the CPs gained from SS procs do not appear in an energize event.
    The handler here will look for damage events from that SS proc and use the processInvisibleEnergize from ResourceTracker to account for the the CP gains.
    As far as I've been able to tell, wasted CP from a SS that procs are what gets shown in the energize event, so I believe the ResourceTracker should handle this gain accurately

--- src/parser/rogue/outlaw/modules/core/ComboPoints.js
+++ src/parser/rogue/outlaw/modules/core/ComboPoints.js
@@ -26,9 +26,9 @@ class ComboPoints extends Analyzer {
     });
     resourceSuggest(when, this.comboPointTracker, {
       spell: SPELLS.SINISTER_STRIKE, // 1 CP + 35% chance for another
-      minor: 0,
-      avg: 0.05,
-      major: 0.1,
+      minor: 0.05, // Due to the 35% chance to double hit, especially with the Broadside RTB buff, you are bound to burn some CP
+      avg: 0.1,
+      major: 0.15,
       extraSuggestion: this.makeExtraSuggestion(SPELLS.SINISTER_STRIKE),
     });
     resourceSuggest(when, this.comboPointTracker, {
@@ -43,8 +43,6 @@ class ComboPoints extends Analyzer {
       minor: 0,
       avg: 0.05,
       major: 0.1,
-      // TODO: Beta logs incorrectly add bonus CP from an extra Sinister Strike to Pistol shot. 
-      // Want to see if its fixed in PrePatch or can be fixed on WCL side before creating workaround.
       extraSuggestion: this.makeExtraSuggestion(SPELLS.PISTOL_SHOT),
     });
     resourceSuggest(when, this.comboPointTracker, {
