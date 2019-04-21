5cbfe072a460bdfa2a45c2101d8acc8713d271b1
Michael Bush <budawolf333@gmail.com>
2019-04-21 16:09:41 -0400

    Added a chart to show the distribution of buffs gained from RTB casts

---
+++ src/parser/rogue/outlaw/modules/core/RollTheBonesCounter.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
+import Analyzer from 'parser/core/Analyzer';
+import Statistic from 'interface/statistics/Statistic';
+import DonutChart from 'interface/statistics/components/DonutChart';
+
+import RollTheBonesCastTracker from './RollTheBonesCastTracker';
+
+class RollTheBonesCounter extends Analyzer {
+  static dependencies = {
+    rollTheBonesCastTracker: RollTheBonesCastTracker,
+  };
+
+  constructor(...args) {
+    super(...args);
+    this.active = !this.selectedCombatant.hasTalent(SPELLS.SLICE_AND_DICE_TALENT.id);
+  }
+
+  rolltheBonesBuffDistributionChart() {
+    const castTracker = this.rollTheBonesCastTracker;
+    
+    const distributionObj = castTracker.rolltheBonesCastEvents.reduce((buffCount, cast) => {
+      buffCount[cast.appliedBuffEvents.length] = (buffCount[cast.appliedBuffEvents.length] || 0) + 1;
+      return buffCount;
+    }, { });
+
+    const items = [
+      {
+        color: '#00b159',
+        label: <>1 Buff</>,
+        value: distributionObj[1] || 0,
+      },
+      {
+        color: '#db00db',
+        label: <>2 Buffs</>,
+        value: distributionObj[2] || 0,
+      },
+      {
+        color: '#f37735',
+        label: <>5 Buffs</>,
+        value: distributionObj[5] || 0,
+      },
+    ];
+
+    return (
+      <DonutChart
+        items={items}
+      />
+    );
+  }
+
+  statistic() {
+    return (
+      <Statistic
+        tooltip="Simulated averages are approximately 80% chance for 1 buff, 19% chance for 2 buffs, 1% chance for 5 buffs"
+      >
+        <div className="pad">
+          <label><SpellLink id={SPELLS.ROLL_THE_BONES.id} /> distribution</label>
+
+          {this.rolltheBonesBuffDistributionChart()}
+        </div>
+      </Statistic>
+    );
+  }
+}
+
+export default RollTheBonesCounter;
