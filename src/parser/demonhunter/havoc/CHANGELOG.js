1a0bf5252d77141f2c1effd66c060110a847d11e
yajinni <yajinni@gmail.com>
2019-04-21 02:19:26 -0400

    Update CHANGELOG.js

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -8,6 +8,11 @@ import SpellLink from 'common/SpellLink';
 
 export default [
   {
+    date: new Date('2019-04-21'),
+    changes: <>Cleaned up the Fury usage code and added a condition for <SpellLink id={SPELLS.BLIND_FURY_TALENT.id} /> that tracks the Fury it generates and only considers Fury above 50 at the time of cast to be wasted.</>,
+    contributors: [Yajinni],
+  },
+  {
     date: new Date('2019-03-12'),
     changes: <>Fixed an error in the <SpellLink id={SPELLS.BLIND_FURY_TALENT.id} /> analyzer.</>,
     contributors: [Zerotorescue],
