4cb2dae81b488376fd525d2267b71141e101e5e6
Scotsoo <jackscotson@gmail.com>
2019-05-10 20:18:27 +0100

    Adding average HOA lvl in RaidCompositionDetails
    Implements #2962

--- src/interface/report/RaidCompositionDetails.js
+++ src/interface/report/RaidCompositionDetails.js
@@ -10,21 +10,29 @@ class RaidCompositionDetails extends React.PureComponent {
     dps: PropTypes.number.isRequired,
     ranged: PropTypes.number.isRequired,
     ilvl: PropTypes.number.isRequired,
+    heartLvl: PropTypes.number.isRequired,
   };
 
   render() {
-    const { tanks, healers, dps, ranged, ilvl } = this.props;
+    const { tanks, healers, dps, ranged, ilvl, heartLvl } = this.props;
 
     return (
       <div className="raid-composition">
         <div className="bar">
           <div className="panel-body" style={{ padding: 0 }}>
             <div className="flex">
-              <div className="flex-sub icon">
+            <div className="flex-sub icon">
                 <Icon icon="inv_helmet_03" />
                 <div className="role-count" style={{ fontSize: '0.78em' }}>
                   {/*Ilvl is gonna be a larger number than the composition so it's shrunk slightly to avoid different sized tiles */}
                   {Math.round(ilvl)}
+                </div>
+              </div>
+              <div className="flex-sub icon">
+                <Icon icon="inv_heartofazeroth" />
+                <div className="role-count" style={{ fontSize: '0.78em' }}>
+                  {/*heartLvl is gonna be a larger number than the composition so it's shrunk slightly to avoid different sized tiles */}
+                  {Math.round(heartLvl)}
                 </div>
               </div>
               <div className="flex-sub icon">
