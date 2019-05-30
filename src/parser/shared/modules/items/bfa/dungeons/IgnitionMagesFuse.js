cd51603653c1e4bdf62246ffdaac2dcd424b36fd
5chmidt <pschmidt@buffalo.edu>
2019-05-29 21:50:59 -0400

    Added skeleton for testing.

--- src/parser/shared/modules/items/bfa/dungeons/IgnitionMagesFuse.js
+++ src/parser/shared/modules/items/bfa/dungeons/IgnitionMagesFuse.js
@@ -9,32 +9,46 @@ import BoringItemValueText from 'interface/statistics/components/BoringItemValue
 import Analyzer from 'parser/core/Analyzer';
 import { formatPercentage, formatNumber } from 'common/format';
 import { calculateSecondaryStatDefault } from 'common/stats';
-
+import StatTracker from 'parser/shared/modules/StatTracker';
 /**
- * Ignition Mage's Fuse -
- * Use: Ignite the fuse, gaining 312 Haste every 4 sec. Haste is removed after 20 sec.
+  Ignition Mage's Fuse
+  Item Level 340
+  Binds when picked up
+  Unique-Equipped
+  Trinket	
+  +205 Intellect
+  Use: Ignite the fuse, gaining 168 Haste every 4 sec. Haste is removed after 20 sec. (2 Min Cooldown)
  */
+
 class IgnitionMagesFuse extends Analyzer {
+  static dependencies = {
+    abilities: Abilities,
+    statTracker: StatTracker,
+  };
+
   statBuff = 0;
-  statBudget = 0;
+  casts = 0;
 
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasTrinket(ITEMS.IGNITION_MAGES_FUSE.id);
 
     if(this.active) {
-      this.statBuff = calculateSecondaryStatDefault(300, 84, this.selectedCombatant.getItem(ITEMS.IgnitionMagesFuse.id).itemLevel);
-      this.statBudget = calculateSecondaryStatDefault(340, 200, this.selectedCombatant.getItem(ITEMS.IgnitionMagesFuse.id).itemLevel);
+      this.statBuff = calculateSecondaryStatDefault(340, 164, this.selectedCombatant.getItem(ITEMS.IGNITION_MAGES_FUSE.id).itemLevel);
+      this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.IGNITION_MAGES_FUSE.id), this.onCast);
     }
+  }
 
+  onCast(event) {
+    this.casts += 1;
   }
 
   get totalBuffUptime() {
-    return this.selectedCombatant.getBuffUptime(SPELLS.IgnitionMagesFuse_BUFF.id) / this.owner.fightDuration;
+    return this.selectedCombatant.getBuffUptime(SPELLS.IGNITION_MAGES_FUSE.id) / this.owner.fightDuration;
   }
 
   get averageStatGain(){
-    const averageStacks = this.selectedCombatant.getStackWeightedBuffUptime(SPELLS.IgnitionMagesFuse_BUFF.id) / this.owner.fightDuration;
+    const averageStacks = this.selectedCombatant.getStackWeightedBuffUptime(SPELLS.IGNITION_MAGES_FUSE.id) / this.owner.fightDuration;
     return averageStacks * this.statBuff;
   }
 
@@ -42,15 +56,14 @@ class IgnitionMagesFuse extends Analyzer {
     return (
       <ItemStatistic
         size="flexible"
-        tooltip={`The stat budget for a non-proc main hand would yield ${formatNumber(this.statBudget)} secondary stats`}
       >
-        <BoringItemValueText item={ITEMS.IgnitionMagesFuse}>
-          <UptimeIcon /> {formatPercentage(this.totalBuffUptime)}% <small>uptime</small> <br />
-          <HasteIcon /> {formatNumber(this.averageStatGain)} <small>average Haste gained</small>
+        <BoringItemValueText item={ITEMS.IGNITION_MAGES_FUSE}>
+          <HasteIcon /> {formatNumber(this.averageStatGain)} <small>average Haste gained</small> <br />
         </BoringItemValueText>
       </ItemStatistic>
     );
   }
 }
+
 
 export default IgnitionMagesFuse;
