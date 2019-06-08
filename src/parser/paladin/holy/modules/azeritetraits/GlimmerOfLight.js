dfbc411835d5aca3b6e8e91e7502b5de98354a07
5chmidt <pschmidt@buffalo.edu>
2019-06-08 10:23:12 -0400

    Skeleton module for testing.

---
+++ src/parser/paladin/holy/modules/azeritetraits/GlimmerOfLight.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import { formatNumber } from 'common/format';
+import SPELLS from 'common/SPELLS/index';
+import TraitStatisticBox, { STATISTIC_ORDER } from 'interface/others/TraitStatisticBox';
+import ItemHealingDone from 'interface/others/ItemHealingDone';
+import Events from 'parser/core/Events';
+
+import BeaconHealSource from '../beacons/BeaconHealSource.js';
+
+
+/**
+ * Glimmer of Light,	Azerite Power
+ * Requires Paladin (Holy, Holy)
+ * Holy Shock leaves a Glimmer of Light on the target for 30 sec.  
+ * When you Holy Shock, all targets with Glimmer of Light are damaged for 1076 or healed for 1587. (at ilvl 400)
+ * Example Log: https://www.warcraftlogs.com/reports/TX4nzPy8WwrfLv97#fight=19&type=auras&source=5&ability=287280
+ */
+class GlimmerOfLight extends Analyzer {
+  static dependencies = {
+    beaconHealSource: BeaconHealSource,
+  };
+
+  healing = 0;
+  glimmerHeals = 0;
+  healingTransfered = 0;
+  casts = 0;
+
+  constructor(...args) {
+    super(...args);
+    this.active = this.selectedCombatant.hasTrait(SPELLS.GLIMMER_OF_LIGHT.id);
+    if (!this.active) {
+      return;
+    }
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.HOLY_SHOCK_HEAL), this.onCast);
+    this.addEventListener(Events.onHeal.by(SELECTED_PLAYER).spell(SPELLS.GLIMMER_OF_LIGHT), this.onHeal)
+    this.addEventListener(this.beaconHealSource.beacontransfer.by(SELECTED_PLAYER), this.onBeaconTransfer);
+  }
+
+  onBeaconTransfer(event) {
+    const spellId = event.originalHeal.ability.guid;
+    if (spellId !== SPELLS.GLIMMER_OF_LIGHT.id) {
+      return;
+    }
+    this.healingTransfered += event.amount + (event.absorbed || 0);
+  }
+
+  onCast(event) {
+    this.casts += 1;
+  }
+
+  onHeal(event) {
+    this.healing += event.amount + (event.absorbed || 0);
+    this.glimmerHeals += 1;
+  }
+
+  get healsPerCast(){
+    return this.glimmerHeals / this.casts; 
+    }
+
+  get totalHealing() {
+    return this.healing + this.healingTransfered;
+  }
+
+  statistic() {
+    return (
+      <TraitStatisticBox
+        position={STATISTIC_ORDER.OPTIONAL()}
+        trait={SPELLS.GLIMMER_OF_LIGHT.id}
+        value={(
+          <>
+            <ItemHealingDone amount={this.totalHealing} /><br />
+            {this.healsPerCast.toFixed(1)} Heals/Cast
+          </>
+        )}
+        tooltip={(
+          <>
+            Total healing done: <b>{formatNumber(this.totalHealing)}</b><br />
+            Beacon healing transfered: <b>{formatNumber(this.healingTransfered)}</b><br />
+            Buffed players per Holy Shock: <b>this.healsPerCast.toFixed(1)</b><br />
+          </>
+        )}
+      />
+    );
+  }
+}
+
+export default GlimmerOfLight;
