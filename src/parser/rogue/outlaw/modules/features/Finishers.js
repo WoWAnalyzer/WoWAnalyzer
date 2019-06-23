ee47f867bdf04f90e207af762d6066372aa949f3
Michael Bush <budawolf333@gmail.com>
2019-06-23 13:55:18 -0400

    Moved the bulk of the finisher module logic into a shared Rogue module
    Switched the event being used to the spendresource event per tsabo's suggestion, which greatly simplified the module. This allowed removing the dependency on abilityTracker as well.

--- src/parser/rogue/outlaw/modules/features/Finishers.js
+++ src/parser/rogue/outlaw/modules/features/Finishers.js
@@ -1,28 +1,14 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS';
-import { formatPercentage } from 'common/format';
 import SpellLink from 'common/SpellLink';
-import RESOURCE_TYPES, { getResource } from 'game/RESOURCE_TYPES';
-import AbilityTracker from 'parser/shared/modules/AbilityTracker';
-import Analyzer from 'parser/core/Analyzer';
 
-import ComboPointTracker from '../../../shared/resources/ComboPointTracker';
+import FinisherTracker from '../../../shared/resources/FinisherTracker';
 
-class Finishers extends Analyzer {
-  static dependencies = {
-    abilityTracker: AbilityTracker,
-    comboPointTracker: ComboPointTracker,
-  };
+class Finishers extends FinisherTracker {
 
-  inefficientFinishers = [];
-
-  get maximumComboPoints(){
-    return this.comboPointTracker.maxResource;
-  }
-
-  get recommendedFinisherPoints(){
-    let points = this.maximumComboPoints;
+  recommendedFinisherPoints(){
+    let points = super.recommendedFinisherPoints();
 
     if(this.selectedCombatant.hasBuff(SPELLS.BROADSIDE.id)){
       points -= 1;
@@ -34,51 +20,6 @@ class Finishers extends Analyzer {
     return points;
   }
 
-  get finisherInefficiency(){
-    const dispatchCasts = this.getCasts(SPELLS.DISPATCH.id);
-    const rolltheBonesCasts = this.getCasts(SPELLS.ROLL_THE_BONES.id);
-    const betweentheEyesCasts = this.getCasts(SPELLS.BETWEEN_THE_EYES.id);
-
-    return this.inefficientFinishers.length / (dispatchCasts + rolltheBonesCasts + betweentheEyesCasts);
-  }
-
-  get suggestionThresholds() {
-    return {
-      actual: this.finisherInefficiency,
-      isGreaterThan: {
-        minor: 0.0,
-        average: .05,
-        major: 0.1,
-      },
-      style: 'percentage',
-    };
-  }
-
-  getCasts(spellId){
-    const ability = this.abilityTracker.getAbility(spellId);
-
-    if(ability){
-      return ability.casts;
-    }
-
-    return 0;
-  }
-
-  on_byPlayer_cast(event){
-    if(!event || !event.classResources){
-      return;
-    }
-
-    const cpResource = getResource(event.classResources, RESOURCE_TYPES.COMBO_POINTS.id);
-    if(!cpResource){
-      return;
-    }
-
-    if(cpResource.cost < this.recommendedFinisherPoints){
-      this.inefficientFinishers.push(event);
-    }
-  }
-
   get quickDrawSuggestionText(){
     if(this.selectedCombatant.hasTalent(SPELLS.QUICK_DRAW_TALENT.id)){
       return <>, or you have an <SpellLink id={SPELLS.OPPORTUNITY.id} /> proc,</>;
@@ -86,13 +27,12 @@ class Finishers extends Analyzer {
     return '';
   }
 
-  suggestions(when) {
-    when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
-      return suggest(<>Try to use your finishers at {this.maximumComboPoints} combo points. However if you have <SpellLink id={SPELLS.BROADSIDE.id} /> active{this.quickDrawSuggestionText} use your finisher at {this.maximumComboPoints-1} combo points instead.</>)
-        .icon(SPELLS.DISPATCH.icon)
-        .actual(`${formatPercentage(actual)}% inefficient casts`)
-        .recommended(`${formatPercentage(recommended)}% is recommended`);
-    });
+  extraSuggestion(){ 
+    return <>If you have <SpellLink id={SPELLS.BROADSIDE.id} /> active{this.quickDrawSuggestionText} use your finisher at {this.maximumComboPoints-1} combo points instead.</>;
+  }
+
+  suggestionIcon(){
+    return SPELLS.DISPATCH.icon;
   }
 }
 
---
+++ src/parser/rogue/shared/resources/FinisherTracker.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import SPELLS from 'common/SPELLS';
+import { formatPercentage } from 'common/format';
+import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
+import Analyzer from 'parser/core/Analyzer';
+
+import ComboPointTracker from './ComboPointTracker';
+
+const MINOR_THRESHOLD = 0.05;
+const AVERAGE_THRESHOLD = 0.1;
+const MAJOR_THRESHOLD = 0.2;
+
+class FinisherTracker extends Analyzer {
+  static dependencies = {
+    comboPointTracker: ComboPointTracker,
+  };
+
+  inefficientFinisherCount = 0;
+
+  /**
+   * IMPLEMENTME
+   * This is where logic is implemented for deciding how many combo points should be used for finishers.
+   * @returns {number}  The minimum number of combo points for finishers; CP spends less than this number are considered inefficient.
+   */
+  recommendedFinisherPoints(){
+    const points = this.maximumComboPoints;
+    return points;
+  }
+
+  /**
+   * IMPLEMENTME
+   * Return spec specific suggestion text or JSX node here.
+   */
+  extraSuggestion(){
+    return '';
+  }
+
+  /**
+   * IMPLEMENTME
+   * This can be overridden to change the suggestion icon to be something relevant to the spec.
+   */
+  suggestionIcon(){
+    return SPELLS.EVISCERATE.icon;
+  }
+
+  on_byPlayer_spendresource(event) {
+    const spent = event.resourceChange;
+    if (event.resourceChangeType !== RESOURCE_TYPES.COMBO_POINTS.id) {
+      return;
+    }
+
+    if(spent < this.recommendedFinisherPoints()){
+      this.inefficientFinisherCount += 1;
+    }
+  }
+
+  get maximumComboPoints(){
+    return this.comboPointTracker.maxResource;
+  }
+
+  get totalFinisherCount(){
+    return this.comboPointTracker.spendersCasts;
+  }
+
+  get finisherInefficiency(){
+    return this.inefficientFinisherCount / this.totalFinisherCount;
+  }
+
+  get suggestionThresholds() {
+    return {
+      actual: this.finisherInefficiency,
+      isGreaterThan: {
+        minor: MINOR_THRESHOLD,
+        average: AVERAGE_THRESHOLD,
+        major: MAJOR_THRESHOLD,
+      },
+      style: 'percentage',
+    };
+  }
+
+  suggestions(when) {
+    when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => {
+      return suggest(<React.Fragment>Try to use your finishers at {this.maximumComboPoints} combo points. {this.extraSuggestion()}</React.Fragment>)
+        .icon(this.suggestionIcon())
+        .actual(`${formatPercentage(actual)}% (${this.inefficientFinisherCount} out of ${this.totalFinisherCount}) inefficient casts`)
+        .recommended(`<${formatPercentage(recommended)}% is recommended`);
+    });
+  }
+}
+
+export default FinisherTracker;
