501550623de16b47ec165ae01bf4a01769112fc0
Zeboot <ctbuschmann@gmail.com>
2019-06-25 07:03:17 +0200

    fix cooldowns

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Yajinni, Mamtooth, Zerotorescue, Putro, joshinator, Gebuz, ackwell, emallson, blazyb, Dambroda, Nalhan, Satyric, niseko, Khadaj, Fyruna, Matardarix, jos3p, Aelexe, Chizu, Hartra344, Hordehobbs, Dorixius, Sharrq, Scotsoo, HolySchmidt, Zeboot } from 'CONTRIBUTORS';
+import { Yajinni, Mamtooth, Zerotorescue, Putro, joshinator, Gebuz, ackwell, emallson, blazyb, Dambroda, Nalhan, Satyric, niseko, Khadaj, Fyruna, Matardarix, jos3p, Aelexe, Chizu, Hartra344, Hordehobbs, Dorixius, Sharrq, Scotsoo, HolySchmidt, Zeboot, Abelito75 } from 'CONTRIBUTORS';
 import ItemLink from 'common/ItemLink';
 import ITEMS from 'common/ITEMS';
 import SPELLS from 'common/SPELLS';
@@ -11,6 +11,11 @@ export default [
     date: new Date('2019-06-19'),
     changes: 'Added Eternal Palace raid information to zones.',
     contributors: [Yajinni],
+  },
+  {
+    date: new Date('2019-06-16'),
+    changes: <>Fixed issue where Cooldown's Even more button would cause the website to crash. </>,
+    contributors: [Abelito75, Zeboot],
   },
   {
     date: new Date('2019-06-16'),
