75769cc2aaba39f0f4475cb58fc50809f96afed4
niseko <rentz@hotmail.de>
2019-06-25 21:12:05 +0200

    add the ever rising tide

---
+++ src/common/SPELLS/bfa/essences/essences.js
@@ -0,0 +1,38 @@
+/**
+ * All azerite essences go in here.
+ * You need to do this manually, usually an easy way to do this is by opening a WCL report and clicking the icons of spells to open the relevant Wowhead pages, here you can get the icon name by clicking the icon, copy the name of the spell and the ID is in the URL.
+ * You can access these entries like other entries in the spells files by importing `common/SPELLS` and using the assigned property on the SPELLS object. Please try to avoid abbreviating properties.
+ */
+
+export default {
+  EVER_RISING_TIDE_MAJOR: {
+    id: 299876,
+    name: 'Overcharge Mana',
+    icon: 'spell_azerite_essence09',
+  },
+  EVER_RISING_TIDE_MINOR: {
+    id: 299879,
+    name: 'The Ever-Rising Tide',
+    icon: 'inv_elemental_mote_mana',
+  },
+  EVER_RISING_TIDE_HEALING_BUFF: {
+    id: 299624,
+    name: 'Overcharge Mana',
+    icon: 'spell_azerite_essence09',
+  },
+  EVER_RISING_TIDE_CHARGING_BUFF: {
+    id: 296072,
+    name: 'Overcharge Mana',
+    icon: 'spell_azerite_essence09',
+  },
+  EVER_RISING_TIDE_STAT_BUFF: {
+    id: 296059,
+    name: 'The Ever-Rising Tide',
+    icon: 'inv_elemental_mote_mana',
+  },
+  EVER_RISING_TIDE_ENERGIZE: {
+    id: 296065,
+    name: 'The Ever-Rising Tide',
+    icon: 'inv_elemental_mote_mana',
+  },
+};
---
+++ src/parser/shared/modules/spells/bfa/essences/TheEverRisingTide.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import SPELLS from 'common/SPELLS/index';
+import { formatNumber } from 'common/format';
+import { calculatePrimaryStat } from 'common/stats';
+import SpellLink from 'common/SpellLink';
+import PrimaryStatIcon from 'interface/icons/PrimaryStat';
+import ItemHealingDone from 'interface/others/ItemHealingDone';
+import ItemManaGained from 'interface/others/ItemManaGained';
+import AzeritePowerStatistic from 'interface/statistics/AzeritePowerStatistic';
+import calculateEffectiveHealing from 'parser/core/calculateEffectiveHealing';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events from 'parser/core/Events';
+import Abilities from 'parser/core/modules/Abilities';
+import StatTracker from 'parser/shared/modules/StatTracker';
+
+const OVERCHARGE_MANA_HEALING_INCREASE_PER_STACK = 0.04;
+const MANA_REGEN_PER_SECOND = 800;
+
+class TheEverRisingTide extends Analyzer {
+  static dependencies = {
+    abilities: Abilities,
+    statTracker: StatTracker,
+  };
+  healing = 0;
+  buffActive = false;
+  stacks = 0;
+  manaGained = 0;
+  stat = 0;
+
+  constructor(...args) {
+    super(...args);
+    this.hasMajor = this.selectedCombatant.hasEssence(SPELLS.EVER_RISING_TIDE_MAJOR.id);
+    this.active = this.hasMajor | this.selectedCombatant.hasEssence(SPELLS.EVER_RISING_TIDE_MINOR.id);
+    if (!this.active) {
+      return;
+    }
+    this.stat = calculatePrimaryStat(420, 1569, this.selectedCombatant.neck.itemLevel);
+    if (this.selectedCombatant.essenceRank(SPELLS.EVER_RISING_TIDE_MINOR.id) < 2) {
+      this.stat /= 0.8; // rank 2 grants 20% more stats
+    }
+    this.abilities.add({
+      spell: SPELLS.EVER_RISING_TIDE_CHARGING_BUFF,
+      name: SPELLS.EVER_RISING_TIDE_CHARGING_BUFF.name,
+      category: Abilities.SPELL_CATEGORIES.ITEMS,
+      cooldown: 30,
+    });
+
+    this.statTracker.add(SPELLS.EVER_RISING_TIDE_STAT_BUFF.id, {
+      intellect: this.stat,
+    });
+    console.log(this.stat);
+
+    this.addEventListener(Events.heal.by(SELECTED_PLAYER), this._onHeal);
+    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.EVER_RISING_TIDE_HEALING_BUFF), this._applyBuff);
+    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.EVER_RISING_TIDE_HEALING_BUFF), this._removebuff);
+    this.addEventListener(Events.applybuffstack.by(SELECTED_PLAYER).spell(SPELLS.EVER_RISING_TIDE_HEALING_BUFF), this._applyBuff);
+    this.addEventListener(Events.energize.by(SELECTED_PLAYER).spell(SPELLS.EVER_RISING_TIDE_ENERGIZE), this._energize);
+  }
+
+  _onHeal(event) {
+    if (!this.buffActive) {
+      return;
+    }
+    this.healing += calculateEffectiveHealing(event, OVERCHARGE_MANA_HEALING_INCREASE_PER_STACK * this.stacks);
+  }
+
+  _applyBuff(event) {
+    this.buffActive = true;
+    this.stacks = event.stack || 1;
+  }
+
+  _removebuff(event) {
+    this.buffActive = false;
+  }
+
+  _energize(event) {
+    this.manaGained += event.resourceChange;
+  }
+
+  get manaLost() {
+    return this.selectedCombatant.getBuffUptime(SPELLS.EVER_RISING_TIDE_CHARGING_BUFF.id) / 1000 * MANA_REGEN_PER_SECOND;
+  }
+
+  get minorBuffUptime() {
+    return this.selectedCombatant.getBuffUptime(SPELLS.EVER_RISING_TIDE_STAT_BUFF.id) / this.owner.fightDuration;
+  }
+
+  statistic() {
+    return (
+      <AzeritePowerStatistic size="flexible">
+        {this.hasMajor && (
+          <div className="pad">
+            <label><SpellLink id={SPELLS.EVER_RISING_TIDE_MAJOR.id} /> - Major Rank {this.selectedCombatant.essenceRank(SPELLS.EVER_RISING_TIDE_MAJOR.id)}</label>
+            <div className="value">
+              <ItemHealingDone amount={this.healing} /><br />
+              <ItemManaGained amount={this.manaLost * -1} />
+            </div>
+          </div>
+        )}
+        <div className="pad">
+          <label><SpellLink id={SPELLS.EVER_RISING_TIDE_MINOR.id} /> - Minor Rank {this.selectedCombatant.essenceRank(SPELLS.EVER_RISING_TIDE_MINOR.id)}</label>
+          <div className="value">
+            <PrimaryStatIcon stat={this.selectedCombatant.spec.primaryStat} /> {formatNumber(this.minorBuffUptime * this.stat)} <small>average {this.selectedCombatant.spec.primaryStat} gained</small><br />
+            <ItemManaGained amount={this.manaGained} />
+          </div>
+        </div>
+      </AzeritePowerStatistic>
+    );
+  }
+}
+
+export default TheEverRisingTide;
