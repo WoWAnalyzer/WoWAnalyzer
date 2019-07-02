f3e807a2027f036fb52f766c41e80b473dbd0757
Qbz <conorlavelleqbz@gmail.com>
2019-07-01 22:00:08 -0700

    Added overhealing absorb tracking. Added more testing logs. Removed usage table

--- src/parser/shared/modules/spells/bfa/essences/TheWellOfExistence.js
+++ src/parser/shared/modules/spells/bfa/essences/TheWellOfExistence.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS/index';
-import { formatNumber, formatDuration } from 'common/format';
+import { formatNumber, formatPercentage } from 'common/format';
 import SpellLink from 'common/SpellLink';
 
 import ItemHealingDone from 'interface/others/ItemHealingDone';
@@ -13,15 +13,36 @@ import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events from 'parser/core/Events';
 import Abilities from 'parser/core/modules/Abilities';
 
-//https://www.warcraftlogs.com/reports/wg7GpmZxhat6TLjV#fight=41&source=44
+const debug = true;
+
+// Can these values be queried? 
+const OVERHEAL_ABSORB_RATE = 0.15;
+const RANK_TWO_HEALING_BOOST = 1.5;
+// https://www.wowhead.com/spell=299936/the-well-of-existence
+const RANK_THREE_DOUBLE_CAP = 10982;
+
+// Live multiple Rank 1 fights/uses 
+//   https://www.warcraftlogs.com/reports/ADH6NnKYPGxtf318#boss=-2&difficulty=0&type=healing&ability=296197
+//   Grong 4 uses 
+//   https://www.warcraftlogs.com/reports/ADH6NnKYPGxtf318#type=healing&fight=9&source=14
+//   Conclave 5 uses 
+//   https://www.warcraftlogs.com/reports/ADH6NnKYPGxtf318#type=healing&fight=22&source=14
+// Live rank 1 only used twice 
+// https://www.warcraftlogs.com/reports/wg7GpmZxhat6TLjV/#fight=41&source=3
+// PTR rank 3
+// https://www.warcraftlogs.com/reports/HKnChBYqyXk61g3V/#fight=8&type=healing&source=23
 class TheWellOfExistence extends Analyzer {
   static dependencies = {
     abilities: Abilities,
   };
-  
   majorHealing = 0;
   minorHealing = 0;
-
+  totalAbsorbedOverhealing = 0;
+  currentAbsorbedOverhealing = 0;
+  rankThreeDoubledOverhealing = 0;
+  // for debug 
+  totalOverhealing = 0;
+    
   constructor(...args) {
     super(...args);
     this.active = this.selectedCombatant.hasEssence(SPELLS.WELL_OF_EXISTENCE.traitId);
@@ -29,6 +50,7 @@ class TheWellOfExistence extends Analyzer {
       return;
     }
     this.hasMajor = this.selectedCombatant.hasMajor(SPELLS.WELL_OF_EXISTENCE.traitId);
+    this.rankThreeOrAbove = this.selectedCombatant.essenceRank(SPELLS.WELL_OF_EXISTENCE.traitId) >= 3;
 
     if(this.hasMajor){
       this.abilities.add({
@@ -39,28 +61,80 @@ class TheWellOfExistence extends Analyzer {
     }
     
     this.addEventListener(Events.heal.by(SELECTED_PLAYER), this._onHeal);
+    debug && this.addEventListener(Events.fightend, this._fightend);
   }
 
-  numMajorCasts = 0;
-  majorCasts = {};
   _onHeal(event) {
+    // Essence passive heal 
     if(SPELLS.WELL_OF_EXISTENCE_HEAL.id === event.ability.guid)
     {
       this.minorHealing += event.amount;
+      this.currentAbsorbedOverhealing -= event.amount;
     }
-    else if(SPELLS.WELL_OF_EXISTENCE_MAJOR_ABILITY.id === event.ability.guid) {
+    // Essence active(major) heal 
+    else if(SPELLS.WELL_OF_EXISTENCE_MAJOR_ABILITY.id === event.ability.guid) 
+    {
       this.majorHealing += event.amount;
-      this.numMajorCasts += 1;
-      this.majorCasts[this.numMajorCasts] = {
-        timestamp: event.timestamp,
-        healing: event.amount,
-      }; 
-    } 
+
+      if(this.rankThreeOrAbove)
+      {
+        // Rank 3 doesnt dump the well, only as much as needed (don't need to consider overheal)
+        // Healing is amplified, so you lost (heal divided by amplification) from the well
+        this.currentAbsorbedOverhealing -= event.amount / RANK_TWO_HEALING_BOOST;
+      }
+      else
+      {
+        // Under rank 3 you lose all stored healing on active 
+        this.currentAbsorbedOverhealing = 0
+      }
+    }
+    // anything else that overhealed
+    else if(event.overheal)
+    {
+      if(debug){
+        this.totalOverhealing += event.overheal;
+      }
+      
+      const absorbableOverhealing = event.overheal * OVERHEAL_ABSORB_RATE;
+      // If overheal can be doubled and below threshold to do it
+      if(this.rankThreeOrAbove && this.currentAbsorbedOverhealing < RANK_THREE_DOUBLE_CAP)
+      {
+        const doubledOverhealing = 2 * absorbableOverhealing;
+        // If all overheal is below the threshold
+        if(this.currentAbsorbedOverhealing + doubledOverhealing < RANK_THREE_DOUBLE_CAP)
+        {
+          this.rankThreeDoubledOverhealing += absorbableOverhealing;
+          this.currentAbsorbedOverhealing += doubledOverhealing;
+        }
+        // If only part of overheal is below the threshold
+        else
+        {
+          const remainingDoubleableOverhealing = RANK_THREE_DOUBLE_CAP - this.currentAbsorbedOverhealing;
+          this.rankThreeDoubledOverhealing += remainingDoubleableOverhealing;
+          const totalAbsorbed = (2 * remainingDoubleableOverhealing) + (absorbableOverhealing - remainingDoubleableOverhealing);
+          this.currentAbsorbedOverhealing += totalAbsorbed;
+        }
+      }
+      else
+      {
+        this.currentAbsorbedOverhealing += absorbableOverhealing;
+      }
+      
+      this.totalAbsorbedOverhealing += absorbableOverhealing;
+    }
+  }
+  
+  // This function is only connected in debug
+  _fightend() {
+    console.log('Total Overhealing: ' + this.totalOverhealing);
+    const expectedOverhealing = this.totalOverhealing * OVERHEAL_ABSORB_RATE
+    console.log('Total Absorbed Overhealing Expected : ' + expectedOverhealing + '. Reported: ' + this.totalAbsorbedOverhealing);
   }
 
   statistic() {
-    const nth = (number) => number + (["st", "nd", "rd"][((number + 90) % 100 - 10) % 10 - 1] || "th");
     const rank = this.selectedCombatant.essenceRank(SPELLS.WELL_OF_EXISTENCE.traitId);
+    console.log(this.totalAbsorbedOverhealing)
+    console.log(this.rankThreeDoubledOverhealing)
     return (
       <StatisticGroup category={STATISTIC_CATEGORY.ITEMS}>
         <ItemStatistic 
@@ -70,37 +144,15 @@ class TheWellOfExistence extends Analyzer {
             <label><SpellLink id={SPELLS.WELL_OF_EXISTENCE.id} /> - Minor Rank {rank}</label>
             <div className="value">
               <ItemHealingDone amount={this.minorHealing} /><br />
+              {formatNumber(this.totalAbsorbedOverhealing)} <small>Overheal absorbed</small><br />
+              {this.rankThreeOrAbove && (<>{formatPercentage(this.rankThreeDoubledOverhealing / this.totalAbsorbedOverhealing)}% <small>overheal doubled</small><br /></>)}
             </div>
           </div>
         </ItemStatistic>
         {this.hasMajor && (
           <ItemStatistic
             ultrawide
-            size="flexible"
-            dropdown={(
-              <table className="table table-condensed">
-                <thead>
-                  <tr>
-                    <th>Cast</th>
-                    <th>Time</th>
-                    <th>Healing</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    Object.values(this.majorCasts).map((cast, index) => {
-                      return (
-                        <tr key={index}>
-                          <th>{nth(index + 1)}</th>
-                          <td>{formatDuration((cast.timestamp - this.owner.fight.start_time) / 1000) || 0}</td>
-                          <td>{formatNumber(cast.healing)}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            )}>
+            size="flexible">
             <div className="pad">
               <label><SpellLink id={SPELLS.WELL_OF_EXISTENCE_MAJOR.id} /> - Major Rank {rank}</label>
               <div className="value">
