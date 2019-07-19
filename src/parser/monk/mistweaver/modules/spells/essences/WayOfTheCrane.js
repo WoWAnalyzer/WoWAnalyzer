1607d99a2f1b3ef8217ba6e07c99612779cdfc1e
Abel Melchiore <abelgm@charter.net>
2019-07-18 22:50:32 -0400

    Update WayOfTheCrane.js

--- src/parser/monk/mistweaver/modules/spells/essences/WayOfTheCrane.js
+++ src/parser/monk/mistweaver/modules/spells/essences/WayOfTheCrane.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import SPELLS from 'common/SPELLS';
 import Analyzer from 'parser/core/Analyzer';
-import StatisticBox from 'interface/others/StatisticBox';
 import ItemHealingDone from 'interface/others/ItemHealingDone';
 import SpellIcon from 'common/SpellIcon';
 import { formatNumber } from 'common/format';
+import ItemStatisticBox from 'interface/others/ItemStatisticBox';
 
 
 
@@ -15,11 +15,11 @@ import { formatNumber } from 'common/format';
 
 class WayOfTheCrane extends Analyzer {
 
-  customMap = null;
-  damageSpell = "";
-  lastTimeStamp = 0;
-  inWotc = false;
-  wotcTime = 0;
+  _customMap = null;
+  _damageSpell = "";
+  _lastTimeStamp = 0;
+  _inWotc = false;
+  _wotcTime = 0;
 
   constructor(...args) {
     super(...args);
@@ -38,26 +38,25 @@ class WayOfTheCrane extends Analyzer {
     if(!this.selectedCombatant.hasBuff(SPELLS.WAY_OF_THE_CRANE.id)){
       return;
     }
-    this.damageSpell = event.ability.name;
+    this._damageSpell = event.ability.name;
   }
 
   on_byPlayer_heal(event) {
     const spellId = event.ability.guid;
     if(spellId === SPELLS.WAY_OF_THE_CRANE_HEAL.id || spellId === SPELLS.WAY_OF_THE_CRANE_HEAL_HONOR.id){
-      if(this.customMap.get(this.damageSpell)===undefined){
-        this.customMap.set(this.damageSpell, [1, event.amount || 0, event.overheal || 0]);
+      if(this._customMap.get(this._damageSpell)===undefined){
+        let healingEvents = {
+          casts: 1,
+          healing: event.amount || 0,
+          overheal:  event.overheal || 0,
+        };
+        this._customMap.set(this._damageSpell, healingEvents);
       }else{
-        let array = this.customMap.get(this.damageSpell);
-        let heal = array[1];
-        let overheal = array[2];
-        if(event.amount){
-          heal += event.amount;
-        }
-        if(event.overheal){
-          overheal += event.overheal;
-        }
-        array = [array[0] + 1, heal, overheal];
-        this.customMap.set(this.damageSpell, array);
+        let healingEvents = this._customMap.get(this._damageSpell);
+        healingEvents.casts +=1;
+        healingEvents.healing += event.amount;
+        healingEvents.overheal += event.overheal||0;
+        this._customMap.set(this._damageSpell, healingEvents);
       }
     }
   }
@@ -66,47 +65,49 @@ class WayOfTheCrane extends Analyzer {
     if(event.ability.guid !== SPELLS.WAY_OF_THE_CRANE.id){
         return;
     }
-    this.inWotc = true;
-    this.lastTimeStamp = event.lastTimeStamp;
+    this._inWotc = true;
+    this._lastTimeStamp = event.lastTimeStamp;
   }
 
   on_byPlayer_removebuff(event){
     if(event.ability.guid !== SPELLS.WAY_OF_THE_CRANE.id){
       return;
     }
-    this.wotcTime += 15;
-    this.inWotc = false;
+    this._wotcTime += 15;
+    this._inWotc = false;
   }
 
   on_fightend(){
-    if(this.inWotc === true){
-      this.wotcTime += (this.selectedCombatant.fightDuration - this.lastTimeStamp)/1000;
+    if(this._inWotc === true){
+      this._wotcTime += (this.selectedCombatant.fightDuration - this._lastTimeStamp)/1000;
     }
   }
 
   statistic() {
     let totalHeal = 0;
-    this.customMap.forEach(function(value, key) {
-      totalHeal += value[1]; 
+    this._customMap.forEach(function(value, key) {
+      totalHeal += value.healing; 
     });
-    console.log(totalHeal);
-    const arrayOfDamageSpells = Array.from(this.customMap.keys());
+    const arrayOfDamageSpells = Array.from(this._customMap.keys());
     return (
-      <StatisticBox
+      <ItemStatisticBox
         label="Way Of The Crane"
         icon={<SpellIcon id={SPELLS.WAY_OF_THE_CRANE.id} />}
         value={<ItemHealingDone amount={totalHeal} />}
         tooltip={(
           arrayOfDamageSpells.map(spell => (
-            <div>{spell} did {this.customMap.get(spell)[1]} healing, {this.customMap.get(spell)[2]} overhealing in {this.customMap.get(spell)[0]/3} casts.</div>
+            <div>{spell} did {this._customMap.get(spell)[1]} healing, {this._customMap.get(spell)[2]} overhealing in {this._customMap.get(spell)[0]/3} casts.</div>
           ))
         )}
       >
+      <div>
+        The healing done by your damaging abilities <b>during Way of the Crane</b>. That is: the HPS column doesn't count time outside of WotC.
+      </div>
       <table className="table table-condensed">
         <thead>
           <tr>
             <th>Spell</th>
-            <th>During Wotc HPS</th>
+            <th>HPS</th>
             <th>HPCT</th>
           </tr>
         </thead>
@@ -115,14 +116,14 @@ class WayOfTheCrane extends Analyzer {
             arrayOfDamageSpells.map(spell => (
               <tr>
               <td>{spell}</td>
-              <td>{formatNumber(this.customMap.get(spell)[1]/this.wotcTime)}</td>
-              <td>{formatNumber(this.customMap.get(spell)[1]/(this.customMap.get(spell)[0]/3*1.5))}</td>
+              <td>{formatNumber(this._customMap.get(spell).healing/this._wotcTime)}</td>
+              <td>{formatNumber(this._customMap.get(spell).healing/(this._customMap.get(spell).casts/3*1.5))}</td>
               </tr>
             ))
           }
           </tbody>
       </table>
-      </StatisticBox>
+      </ItemStatisticBox>
 
     );
   }
