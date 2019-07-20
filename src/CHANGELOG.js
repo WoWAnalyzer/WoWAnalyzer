11b58c19639514a82661982e8fc5ed3085f36852
Martijn Hols <git@martijnhols.nl>
2019-07-20 22:44:09 +0200

    Show changelog entries on the frontpage

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 import Contributor from 'interface/contributor/Button';
 
 export default [
+  change(date(2019, 7, 20), 'Show changelog entries along with news on the frontpage.', [Zerotorescue]),
   change(date(2019, 7, 20), <>Removed ads by Google and most ad spots as they were not worth the degraded experience for you guys. Instead, please consider a monthly donation on <a href="https://www.patreon.com/wowanalyzer">Patreon</a> to support the project and unlock Premium, or placing bounties on tickets for people to solve on <a href="https://www.bountysource.com/teams/wowanalyzer">BountySource</a> (currently active bounties: $1,550).</>, [Zerotorescue]),
   change(date(2019, 7, 20), <>Added <SpellLink id={SPELLS.WORLDVEIN_RESONANCE.id} /> essence.</>, [Anatta336]),
   change(date(2019,7,20), 'Updated the build process to ensure every new pull request has a changelog entry.', Zerotorescue),
