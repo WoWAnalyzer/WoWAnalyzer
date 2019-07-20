e43f5b1368c68f04ecbd727796d770b0e1639838
Martijn Hols <git@martijnhols.nl>
2019-07-20 12:37:12 +0200

    Fix git command

--- scripts/check-for-changelog.js
+++ scripts/check-for-changelog.js
@@ -1,9 +1,12 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function getChangedFiles() {
+function getTargetBranch() {
+  return process.env.TRAVIS_BRANCH;
+}
+async function getChangedFiles(targetBranch) {
   // eslint-disable-next-line no-unused-vars
-  const { stdout, stderr } = await exec(`git diff --name-only HEAD...$TRAVIS_BRANCH`);
+  const { stdout, stderr } = await exec(`git diff --name-only ${targetBranch}...HEAD`);
   // TODO: How do I properly handle stderr?
   return stdout.trim().split('\n');
 }
@@ -13,21 +16,22 @@ function hasChangelog(changedFiles) {
 }
 
 async function main() {
-  const changedFiles = await getChangedFiles();
-  console.log('Changed files:');
-  changedFiles.forEach(console.log);
-  let exitCode;
+  const targetBranch = getTargetBranch();
+  const changedFiles = await getChangedFiles(targetBranch);
+  console.log(`Changed files since ${targetBranch}:`);
+  changedFiles.forEach(path => console.log(path));
+  console.log();
+
   if (hasChangelog(changedFiles)) {
     console.log('Found a changelog entry. Thanks!');
-    exitCode = 0;
+    process.exit(0);
   } else {
     console.error('Error: Changelog entry missing.\n' +
       'A changelog entry is required. Please explain your change in a relevant CHANGELOG file.\n' +
       'Use the spec specific CHANGELOG file if it was a spec/class specific change. If it\n' +
       'affects many specs you can use the CHANGELOG file in the root src folder.');
-    exitCode = 1;
+    process.exit(1);
   }
-  process.exit(exitCode);
 }
 
 main();
