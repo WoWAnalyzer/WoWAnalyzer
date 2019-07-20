99b5a670125ecbb2473e48469872b6db44bece93
Martijn Hols <git@martijnhols.nl>
2019-07-20 12:03:30 +0200

    Add build step to check for a changelog entry

---
+++ scripts/check-for-changelog.js
@@ -0,0 +1,33 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function getChangedFiles() {
+  const { stdout, stderr } = await exec(`git diff --name-only HEAD...$TRAVIS_BRANCH`);
+  // TODO: How do I properly handle stderr?
+  return stdout.trim().split('\n');
+}
+
+async function hasChangelog(changedFiles) {
+  return changedFiles.some(path => path.includes('/CHANGELOG.js'));
+}
+
+async function checkForChangelog() {
+  const changedFiles = await getChangedFiles();
+  return hasChangelog(changedFiles);
+}
+
+async function main() {
+  const hasChangelog = await checkForChangelog();
+  let exitCode;
+  if (hasChangelog) {
+    console.log('Found a changelog entry. Thanks!');
+    exitCode = 0;
+  } else {
+    console.error('Error: Changelog entry missing.\n' +
+      'A changelog entry is required. Please explain your change in a relevant CHANGELOG file. Use the spec specific CHANGELOG file if it was a spec/class specific change. If it affects many specs, use the CHANGELOG file in the root src folder.');
+    exitCode = 1;
+  }
+  process.exit(exitCode);
+}
+
+main();
