b3cd6a8d5026cb4f6a57b7ddbd7cca5cc1883177
Abel Melchiore <abelgm@charter.net>
2019-07-31 12:49:40 -0400

    Port warrior spell update
    Potentially not a full spell update but a good chunk

--- src/parser/warrior/protection/CHANGELOG.js
+++ src/parser/warrior/protection/CHANGELOG.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-import { Zerotorescue } from 'CONTRIBUTORS';
+import { Zerotorescue, Abelito75 } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2019, 7, 31),<>We have a supporter</>,Abelito75),
   change(date(2019, 7, 20), 'Sorry, this spec has no contributors.', Zerotorescue),
 ];
