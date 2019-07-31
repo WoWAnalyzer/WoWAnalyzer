b33a52017265b4b9cade0d79a11c17f433fbb593
Zeboot <ctbuschmann@gmail.com>
2019-07-31 22:00:45 +0200

    changelog entry

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 import Contributor from 'interface/contributor/Button';
 
 export default [
+  change(date(2019, 7, 31), 'Added \'degraded experience\' toaster in case of disabled modules.', Zeboot),
   change(date(2019, 7, 27), 'Added Unbridled Fury to the list of strong pre-potions.', emallson),
   change(date(2019, 7, 27), <>Added <SpellLink id={SPELLS.NULL_DYNAMO.id} /> essence.</>, emallson),
   change(date(2019, 7, 25), 'Fixed a crash in the Ever-Rising Tide Module', HawkCorrigan),
