175263626182f6ccd1433a7c3cc119bdf76118d2
Zeboot <ctbuschmann@gmail.com>
2019-08-02 20:20:27 +0200

    lint

--- src/interface/report/Results/index.js
+++ src/interface/report/Results/index.js
@@ -27,7 +27,6 @@ import ChangelogTab from 'interface/others/ChangelogTab';
 import ErrorBoundary from 'interface/common/ErrorBoundary';
 import Checklist from 'parser/shared/modules/features/Checklist/Module';
 import StatTracker from 'parser/shared/modules/StatTracker';
-import MODULE_ERROR from 'parser/core/MODULE_ERROR';
 
 import './Results.scss';
 import Header from './Header';
