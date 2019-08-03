c36b36e068b2ab50476fb81c7f148519237c1dec
Martijn Hols <git@martijnhols.nl>
2019-08-03 14:51:49 +0200

    Fix console warning about history import

--- src/Root.js
+++ src/Root.js
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { Provider as ReduxProvider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 
 import reducers from 'interface/reducers';
@@ -12,8 +12,7 @@ import App from 'interface/App';
 import RootLocalizationProvider from 'interface/RootLocalizationProvider';
 import { Provider } from 'interface/LocationContext';
 
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
+const history = createBrowserHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
 const middleware = routerMiddleware(history);
