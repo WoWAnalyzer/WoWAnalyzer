1c960001a0616a40895e731ba2b815df2e1ad6e0
Martijn Hols <git@martijnhols.nl>
2019-08-06 22:35:05 +0200

    Deduplicate Travis code

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,3 @@
-export REPO=wowanalyzer/wowanalyzer;
-export BUILD_TAG="build-$TRAVIS_BUILD_NUMBER";
 export DEPLOY_TAG=$(
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "latest";
@@ -10,6 +8,6 @@ export DEPLOY_TAG=$(
 
 echo "> Tagging the build tag as the latest of its branch.";
 # Travis doesn't allow sharing build artifacts between build steps, so we need to fetch a new copy.
-docker pull $REPO:$BUILD_TAG;
-docker tag $REPO:$BUILD_TAG $REPO:$DEPLOY_TAG;
-docker push $REPO:$DEPLOY_TAG;
+docker pull $DOCKER_REPO:$DOCKER_IMAGE_BUILD_TAG;
+docker tag $DOCKER_REPO:$DOCKER_IMAGE_BUILD_TAG $DOCKER_REPO:$DEPLOY_TAG;
+docker push $DOCKER_REPO:$DEPLOY_TAG;
--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,13 +1,3 @@
-export REPO=wowanalyzer/wowanalyzer;
-export BUILD_TAG="build-$TRAVIS_BUILD_NUMBER";
-export DEPLOY_TAG=$(
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo "latest";
-  else
-    echo $TRAVIS_BRANCH;
-  fi | sed -r 's/\//-/g'
-);
-
 echo "> Uploading the Docker image for this specific build. This allows us to go back to a particular build at any time but won't cause an auomated deploy since each build tag is unique.";
-docker tag wowanalyzer $REPO:$BUILD_TAG;
-docker push $REPO:$BUILD_TAG;
+docker tag wowanalyzer $DOCKER_REPO:$DOCKER_IMAGE_BUILD_TAG;
+docker push $DOCKER_REPO:$DOCKER_IMAGE_BUILD_TAG;
