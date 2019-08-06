54cbb47ef06a633d4c0f0613a955e03dd1eb7c75
Joshua Grant <grantjoshua1995@gmail.com>
2019-08-06 18:29:02 +0100

    Further improvements for mobile. Reduced font size as applicable.

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -8,7 +8,7 @@ import { change, date } from 'common/changelog';
 import Contributor from 'interface/contributor/Button';
 
 export default [
-  change(date(2019, 8, 4), 'Updated mobile view for the news header so it no longer causes horizontal scrolling.', Amrux),
+  change(date(2019, 8, 4), 'General responsive improvements for better mobile experience.', Amrux),
   change(date(2019, 8, 3), 'Keep track of disabled modules names during production.', Zeboot),
   change(date(2019, 8, 3), 'Made the error handler more resilient to errors in browser extensions.', Zerotorescue),
   change(date(2019, 8, 3), 'Changed polyfilling so we might accidentally support more old and/or shitty browsers.', Zerotorescue),
