0d41a987c856691a096a3263148022f8b6319a38
Viridis <viridis101@gmail.Com>
2019-08-09 18:10:59 +0200

    Add bad Furious Gaze statistics

--- src/parser/demonhunter/havoc/modules/spells/azeritetraits/FuriousGaze.js
+++ src/parser/demonhunter/havoc/modules/spells/azeritetraits/FuriousGaze.js
@@ -9,7 +9,8 @@ import HasteIcon from 'interface/icons/Haste';
 import StatTracker from 'parser/shared/modules/StatTracker';
 import Events from 'parser/core/Events';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-
+import SpellLink from 'common/SpellLink';
+import AbilityTracker from 'parser/shared/modules/AbilityTracker';
 
 const furiousGazeStats = traits => Object.values(traits).reduce((obj, rank) => {
   const [haste] = calculateAzeriteEffects(SPELLS.FURIOUS_GAZE.id, rank);
@@ -18,6 +19,8 @@ const furiousGazeStats = traits => Object.values(traits).reduce((obj, rank) => {
 }, {
   haste: 0,
 });
+
+const FURIOUS_GAZE_DURATION = 12000;
 
 /**
  * Furious Gaze
@@ -28,11 +31,13 @@ const furiousGazeStats = traits => Object.values(traits).reduce((obj, rank) => {
 class FuriousGaze extends Analyzer{
 
   static dependencies = {
+    abilityTracker: AbilityTracker,
     statTracker: StatTracker,
   };
 
   haste = 0;
   furiousGazeProcsCounter = 0;
+  furiousGazeStartTime = undefined;
 
   constructor(...args) {
     super(...args);
@@ -47,11 +52,21 @@ class FuriousGaze extends Analyzer{
     this.statTracker.add(SPELLS.FURIOUS_GAZE_BUFF.id, {
       haste,
     });
-    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.FURIOUS_GAZE_BUFF), this.onBuffEvent);
+    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.FURIOUS_GAZE_BUFF), this.onFuriousGazeApply);
+    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.FURIOUS_GAZE_BUFF), this.onFuriousGazeRemove);
   }
 
-  onBuffEvent(event) {
-    this.furiousGazeProcsCounter += 1;
+  onFuriousGazeApply(event) {
+    if (this.furiousGazeStartTime === undefined) {
+      this.furiousGazeStartTime = event.timestamp;
+    }
+  }
+
+  onFuriousGazeRemove(event) {
+    if (this.furiousGazeStartTime !== undefined) {
+      this.furiousGazeProcsCounter += Math.round((event.timestamp - this.furiousGazeStartTime) / FURIOUS_GAZE_DURATION);
+      this.furiousGazeStartTime = undefined;
+    }
   }
 
   get averageHaste() {
@@ -62,19 +77,29 @@ class FuriousGaze extends Analyzer{
     return this.selectedCombatant.getBuffUptime(SPELLS.FURIOUS_GAZE_BUFF.id) / this.owner.fightDuration;
   }
 
+  get eyeBeamCasts() {
+    return this.abilityTracker.getAbility(SPELLS.EYE_BEAM.id).casts || 0;
+  }
+
   statistic() {
     return (
       <ItemStatistic
         size="flexible"
         tooltip={(
           <>
+            <SpellLink id={SPELLS.FURIOUS_GAZE.id} /> buff is only gained when you succesfully<br />
+            finish an <SpellLink id={SPELLS.EYE_BEAM.id} /> channel. Cancelling the channel <br />
+            prematurely will result in not receiving the buff and will <br />
+            translate in a considerable DPS loss.<br /><br />
+
             {formatPercentage(this.buffUptime)}% uptime<br />
-            {this.furiousGazeProcsCounter} Procs
+            {this.furiousGazeProcsCounter} out of {this.eyeBeamCasts} possible procs<br />
           </>
         )}
       >
         <BoringSpellValueText spell={SPELLS.FURIOUS_GAZE}>
-          <HasteIcon /> {formatNumber(this.averageHaste)} <small>average Haste</small>
+          <HasteIcon /> {formatNumber(this.averageHaste)} <small>average Haste</small><br />
+          {this.eyeBeamCasts - this.furiousGazeProcsCounter} <small><SpellLink id={SPELLS.FURIOUS_GAZE.id} /> buffs lost</small><br />
         </BoringSpellValueText>
       </ItemStatistic>
     );
