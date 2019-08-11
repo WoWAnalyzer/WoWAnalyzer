686f309f6e528dfe5e9df5c7523bbec047cb63f0
Zeboot <ctbuschmann@gmail.com>
2019-08-11 02:51:00 +0200

    changelog

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 import Contributor from 'interface/contributor/Button';
 
 export default [
+  change(date(2019, 8, 11), 'Added more phase trigger types to improve our phase detection.', Zeboot),
   change(date(2019, 8, 7), <>Updated <SpellLink id={SPELLS.CONCENTRATED_FLAME.id} /> healing calculation.</>, Yajinni),
   change(date(2019, 8, 6), <>Added <ItemLink id={ITEMS.POTION_OF_WILD_MENDING.id} />.</>, niseko),
   change(date(2019, 8, 6), <>Added <SpellLink id={SPELLS.WELL_OF_EXISTENCE_MAJOR.id} /></>, Qbz),
