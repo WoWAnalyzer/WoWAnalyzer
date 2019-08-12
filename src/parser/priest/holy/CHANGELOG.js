435c6c453944f495ea09ab1ee9e805abbb6d9083
Martijn Hols <github@martijnhols.nl>
2019-08-12 21:11:49 +0200

    Update CHANGELOG.js

--- src/parser/priest/holy/CHANGELOG.js
+++ src/parser/priest/holy/CHANGELOG.js
@@ -6,7 +6,7 @@ import SPELLS from 'common/SPELLS';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2019, 8, 6), 'Added essence Lucid Dreams.', [blazyb]),
+  change(date(2019, 8, 12), 'Added essence Lucid Dreams.', [blazyb]),
   change(date(2019, 4, 9), <>Adding Holy Nova card and updating spreadsheet</>, [Khadaj]),
   change(date(2019, 3, 12), <>Fixed an error in the <SpellLink id={SPELLS.PRAYERFUL_LITANY.id} /> analyzer.</>, [Zerotorescue]),
   change(date(2018, 12, 18), <>Adding <SpellLink id={SPELLS.PROMISE_OF_DELIVERANCE.id} /> and <SpellLink id={SPELLS.DEATH_DENIED.id} />.</>, [Khadaj]),
