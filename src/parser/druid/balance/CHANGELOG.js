0715c099914f884f9d9630066addbd2a001eb900
Sean Anderson <wing5wong@hotmail.com>
2019-08-13 08:07:28 +1200

    use correct CL

--- src/parser/druid/balance/CHANGELOG.js
+++ src/parser/druid/balance/CHANGELOG.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Gebuz, Abelito75 } from 'CONTRIBUTORS';
+import { Gebuz, Abelito75, Wing5wong } from 'CONTRIBUTORS';
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2019, 8, 12), 'Fixed an incorrect spell name on the results page for Balance Druids when using Stellar Flare.', Wing5wong),
   change(date(2019, 4, 30), 'Added High Noon and Power of the Moon azerite pieces to the statistics tab.', [Abelito75]),
   change(date(2019, 4, 27), 'Added DawningSun azerite piece to the statistics tab.', [Abelito75]),
   change(date(2018, 8, 26), 'Updated the empowerment tracker to use the new log format for the buff.', [Gebuz]),
