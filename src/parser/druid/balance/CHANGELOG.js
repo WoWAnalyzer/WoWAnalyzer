9003be6866f3c21fb9eebaff94680ae39a1e9e50
Sean Anderson <wing5wong@hotmail.com>
2019-08-13 08:09:26 +1200

    suggestions not results

--- src/parser/druid/balance/CHANGELOG.js
+++ src/parser/druid/balance/CHANGELOG.js
@@ -6,7 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2019, 8, 12), 'Fixed an incorrect spell name on the results page for Balance Druids when using Stellar Flare.', Wing5wong),
+  change(date(2019, 8, 12), 'Fixed an incorrect spell name on the suggestions for Balance Druids when using Stellar Flare.', Wing5wong),
   change(date(2019, 4, 30), 'Added High Noon and Power of the Moon azerite pieces to the statistics tab.', [Abelito75]),
   change(date(2019, 4, 27), 'Added DawningSun azerite piece to the statistics tab.', [Abelito75]),
   change(date(2018, 8, 26), 'Updated the empowerment tracker to use the new log format for the buff.', [Gebuz]),
