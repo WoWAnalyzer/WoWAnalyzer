66e92baaf878997d77fb2b731618bc3461d5d8ea
Abel Melchiore <abelgm@charter.net>
2019-08-15 02:06:38 -0400

    changelog

--- src/parser/warrior/protection/CHANGELOG.js
+++ src/parser/warrior/protection/CHANGELOG.js
@@ -5,6 +5,7 @@ import { Zerotorescue, Abelito75 } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2019, 8, 15),<>Shield Slam module created for assuming possible casts.</>,Abelito75),
   change(date(2019, 8, 9),<>Big Overhaul to Checklist. We also have a working checklist again</>,Abelito75),
   change(date(2019, 7, 31),<>We have a supporter</>,Abelito75),
   change(date(2019, 7, 20), 'Sorry, this spec has no contributors.', Zerotorescue),
