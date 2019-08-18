5fb928ce6614e8f1c6771301fada3c0c5916681a
Ulrich Enevoldsen <ulrichenevoldsen@hotmail.com>
2019-08-18 14:01:55 +0800

    Fists of Fury label edit

--- src/parser/monk/windwalker/modules/spells/FistsofFury.js
+++ src/parser/monk/windwalker/modules/spells/FistsofFury.js
@@ -68,7 +68,7 @@ class FistsofFury extends Analyzer {
         tooltip="Fists of Fury ticks 5 times over the duration of the channel"
       >
       <BoringSpellValueText spell={SPELLS.FISTS_OF_FURY_CAST}>
-        {this.averageTicks.toFixed(2)} <small>Average Fists of Fury Ticks</small>
+        {this.averageTicks.toFixed(2)} <small>Average ticks per cast</small>
       </BoringSpellValueText>
       </Statistic>
     );
