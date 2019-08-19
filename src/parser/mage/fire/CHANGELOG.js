4ba21da8a26336a520e21d0565170fd0e5b996b9
Chris Grasty <chris.grasty@oracle.com>
2019-08-19 13:18:21 -0500

    [Fire Mage] Hyperthread Wristwraps Support
    Adds support for Hyperthread Wristwraps from Mechagon
    
    Also added in a fix for Meteor where the class was named wrong

--- src/parser/mage/fire/CHANGELOG.js
+++ src/parser/mage/fire/CHANGELOG.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { Sharrq } from 'CONTRIBUTORS';
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
+import ITEMS from 'common/ITEMS';
+import ItemLink from 'common/ItemLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2019, 8, 19), <>Added support for <ItemLink id={ITEMS.HYPERTHREAD_WRISTWRAPS.id} />.</>, [Sharrq]),
   change(date(2019, 8, 16), <>Modified <SpellLink id={SPELLS.COMBUSTION.id} /> during <SpellLink id={SPELLS.FIRESTARTER_TALENT.id} /> to only check the first cast during Combustion and not every cast.</>, [Sharrq]),
   change(date(2019, 8, 16), <>Fixed a bug that was causing the <SpellLink id={SPELLS.PYROCLASM_TALENT.id} /> checklist item to show up without Pyroclasm talented.</>, [Sharrq]),
   change(date(2019, 8, 6), <>Added statistics and suggestions for <SpellLink id={SPELLS.METEOR_TALENT.id} /></>, [Sharrq]),
