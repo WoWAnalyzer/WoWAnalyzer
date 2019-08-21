1192fd1eb40f6275318865d2ff9a73917785b8b2
Michael Cook <mcook4728@gmail.com>
2019-08-21 09:30:25 -0700

    remove commented import

--- src/parser/shaman/elemental/modules/checklist/Module.js
+++ src/parser/shaman/elemental/modules/checklist/Module.js
@@ -7,9 +7,6 @@ import CastEfficiency from 'parser/shared/modules/CastEfficiency';
 import Combatants from 'parser/shared/modules/Combatants';
 import PreparationRuleAnalyzer from 'parser/shared/modules/features/Checklist/PreparationRuleAnalyzer';
 
-
-// import CancelledCasts from '../features/CancelledCasts';
-
 import Component from './Component';
 
 class Checklist extends BaseChecklist {
