84945d30d33562c38b39c554c701dc0276333ed9
Martijn Hols <git@martijnhols.nl>
2019-10-24 20:33:32 +0200

    Fix activity time icons when not using certain legacy statistic boxes

--- src/interface/others/StatisticBox.js
+++ src/interface/others/StatisticBox.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Statistic from 'interface/statistics/Statistic';
 import STATISTIC_CATEGORY from './STATISTIC_CATEGORY';
+import './StatisticBox.css';
 
 export { default as STATISTIC_ORDER } from './STATISTIC_ORDER';
 export { default as STATISTIC_CATEGORY } from './STATISTIC_CATEGORY';
