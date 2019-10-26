4aa45d03a1ff54dc9a5d6e1019c186cb833d5f8a
Zeboot <ctbuschmann@gmail.com>
2019-10-26 14:53:39 +0200

    rename standard

--- src/parser/DEFAULT_BUILD.js
+++ src/parser/DEFAULT_BUILD.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ArmoryIcon from 'interface/icons/Armory';
 export default {
-    name: "Standard Rotation",
+    name: "Standard Build",
     url: "standard",
     icon: <ArmoryIcon />,
 };
