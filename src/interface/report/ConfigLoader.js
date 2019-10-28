1448b1cbc525b99509c337a90f21a0d731ae2978
Zeboot <ctbuschmann@gmail.com>
2019-10-28 19:20:59 +0100

    wording

--- src/interface/report/ConfigLoader.js
+++ src/interface/report/ConfigLoader.js
@@ -19,9 +19,9 @@ class ConfigLoader extends React.PureComponent {
   }
   static getConfig(specId) {
     const config = AVAILABLE_CONFIGS.find(config => config.spec.id === specId);
-    //find active configs, if any exist
+    //find supported builds, if any exist
     const activeBuilds = config.builds && Object.keys(config.builds).filter(b => config.builds[b].supported);
-    //remove all inactive configs
+    //remove all inactive builds
     config.builds = (activeBuilds && activeBuilds.length > 0 && activeBuilds.reduce((obj, key) => {
         obj[key] = config.builds[key];
         return obj;
