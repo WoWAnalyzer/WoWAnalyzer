d785dd3daca225a5512898853ca0c9b21b14026e
Joe Sommer <joe.j.sommer@gmail.com>
2019-11-11 18:00:32 -0600

    Update example report to a log from EP

--- src/parser/deathknight/frost/CONFIG.js
+++ src/parser/deathknight/frost/CONFIG.js
@@ -26,7 +26,7 @@ export default {
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport: '/report/TGzmk4bXDZJndpj7/6-Heroic+Opulence+-+Kill+(8:12)/7-Lichie',
+  exampleReport: '/report/TcCmAWp2RGzvkK69/1-Heroic+Abyssal+Commander+Sivara+-+Kill+(2:22)/Khazakk',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
