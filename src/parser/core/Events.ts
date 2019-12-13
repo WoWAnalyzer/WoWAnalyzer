172bd7af3143af04540a8440bc73756329d505e1
Martijn Hols <git@martijnhols.nl>
2019-12-13 22:57:06 +0100

    Add a bunch of Events

--- src/parser/core/Events.ts
+++ src/parser/core/Events.ts
@@ -33,16 +33,97 @@ enum EventType {
   Interrupt = 'interrupt',
   Death = 'death',
   Resurrect = 'resurrect',
+
+  // Fabricated:
+  GlobalCooldown = 'globalcooldown',
+  BeginChannel = 'beginchannel',
 }
+
+interface Ability {
+  name: string;
+  guid: number;
+  type: number;
+  abilityIcon: string;
+}
+interface ClassResources {
+  amount: number;
+  max: number;
+  type: number;
+}
+
 export interface Event {
   type: EventType;
   timestamp: number;
 }
+export interface BeginCastEvent extends Event {
+  type: EventType.BeginCast;
+
+  ability: Ability;
+  castEvent: CastEvent;
+  channel: {
+    type: 'beginchannel';
+    timestamp: 858735;
+    ability: Ability;
+    sourceID: number;
+    isCancelled: boolean;
+  };
+  isCancelled: boolean;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  target: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  targetIsFriendly: boolean;
+}
+export interface BeginChannelEvent extends Event {
+  type: EventType.BeginChannel;
+  ability: Ability;
+  sourceID: number;
+  isCancelled: boolean;
+}
 export interface CastEvent extends Event {
   type: EventType.Cast;
-  ability: {
-    guid: number;
+  ability: Ability;
+  absorb?: number;
+  armor?: number;
+  attackPower?: number;
+  classResources?: Array<
+    ClassResources & {
+      cost: number;
+    }
+  >;
+  facing?: number;
+  hitPoints?: number;
+  itemLevel?: number;
+  mapID?: number;
+  maxHitPoints?: number;
+  resourceActor?: number;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  spellPower?: number;
+  target?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  targetID?: number;
+  targetIsFriendly: boolean;
+  x?: number;
+  y?: number;
+
+  // Added by the SpellResourceCost module if active
+  rawResourceCost?: {
+    [resourceType: number]: number;
   };
+  resourceCost?: {
+    [resourceType: number]: number;
+  };
+  // Added by the GlobalCooldown module
+  globalCooldown: {
+    ability: Ability;
+    duration: number;
+    sourceID: number;
+    targetID: number;
+    timestamp: number;
+    trigger: CastEvent;
+    type: EventType.GlobalCooldown;
+    __fabricated: true;
+  };
+  // Added by any module, used in the timeline
   meta?: {
     isInefficientCast?: boolean;
     inefficientCastReason?: React.ReactNode;
@@ -50,34 +131,271 @@ export interface CastEvent extends Event {
 }
 export interface HealEvent extends Event {
   type: EventType.Heal;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetInstance?: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  hitType: number;
   amount: number;
-  absorbed: number;
+  overheal?: number;
+  tick?: boolean;
+  resourceActor: number;
+  classResources: ClassResources[];
+  hitPoints: number;
+  maxHitPoints: number;
+  attackPower: number;
+  spellPower: number;
+  armor: number;
+  absorb: number;
+  x: number;
+  y: number;
+  facing: number;
+  mapID: number;
+  itemLevel: number;
+}
+export interface AbsorbedEvent extends Event {
+  type: EventType.Absorbed;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  attacker?: {
+    name: 'Environment';
+    id: -1;
+    guid: 0;
+    type: 'NPC';
+    icon: 'NPC';
+  };
+  attackerID?: number;
+  attackerIsFriendly: boolean;
+  amount: number;
+  extraAbility: Ability;
 }
 export interface DamageEvent extends Event {
   type: EventType.Damage;
+
+  source?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceID?: number;
+  sourceIsFriendly: true;
+  targetID: number;
+  targetIsFriendly: false;
+  ability: Ability;
+  hitType: number;
   amount: number;
   absorbed: number;
+  resourceActor?: number;
+  classResources?: ClassResources[];
+  hitPoints?: number;
+  maxHitPoints?: number;
+  attackPower?: number;
+  spellPower: number;
+  armor?: number;
+  absorb?: number;
+  x?: number;
+  y?: number;
+  facing?: number;
+  mapID?: number;
+  itemLevel?: number;
+  mitigated?: number;
+  unmitigatedAmount?: number;
+  tick?: boolean;
+  overkill?: number;
+}
+export interface BuffEvent {}
+export interface ApplyBuffEvent extends BuffEvent {
+  type: EventType.ApplyBuff;
+  ability: Ability;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  targetInstance?: number;
+  absorb?: number;
+}
+export interface ApplyDebuffEvent extends BuffEvent {
+  type: EventType.ApplyDebuff;
+  ability: Ability;
+  source?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceID?: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  targetInstance?: number;
+  absorb?: number;
+}
+export interface RemoveBuffEvent extends BuffEvent {
+  type: EventType.RemoveBuff;
+  ability: Ability;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  targetInstance?: number;
+  absorb?: number;
+}
+export interface RemoveDebuffEvent extends BuffEvent {
+  type: EventType.RemoveDebuff;
+  ability: Ability;
+  source?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceID?: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  absorb?: number;
+}
+export interface ApplyBuffStackEvent extends Event {
+  type: EventType.ApplyBuffStack;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  stack: number;
+}
+export interface ApplyDebuffStackEvent extends Event {
+  type: EventType.ApplyDebuffStack;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  stack: number;
+}
+export interface RemoveBuffStack extends Event {
+  type: EventType.RemoveBuffStack;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  stack: number;
+}
+export interface RemoveDebuffStack extends Event {
+  type: EventType.RemoveBuffStack;
+
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+  stack: number;
+}
+export interface RefreshBuffEvent extends Event {
+  type: EventType.RefreshBuff;
+
+  source?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceID?: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+}
+export interface RefreshDebuffEvent extends Event {
+  type: EventType.RefreshBuff;
+
+  source?: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceID?: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+}
+export interface EnergizeEvent extends Event {
+  type: EventType.Energize;
+  ability: Ability;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  resourceChange: number;
+  resourceChangeType: number;
+  otherResourceChange: number;
+  waste: number;
+  resourceActor: number;
+  classResources: ClassResources[];
+  hitPoints: number;
+  maxHitPoints: number;
+  attackPower: number;
+  spellPower: number;
+  armor: number;
+  x: number;
+  y: number;
+  facing: number;
+  mapID: number;
+  itemLevel: number;
+}
+export interface DeathEvent extends Event {
+  type: EventType.Death;
+  source: { name: 'Environment'; id: -1; guid: 0; type: 'NPC'; icon: 'NPC' };
+  sourceIsFriendly: boolean;
+  targetID: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+}
+export interface SummonEvent extends Event {
+  type: EventType.Summon;
+  sourceID: number;
+  sourceIsFriendly: boolean;
+  target: {
+    name: string;
+    id: number;
+    guid: number;
+    type: string;
+    petOwner: number;
+    icon: string;
+  };
+  targetInstance: number;
+  targetIsFriendly: boolean;
+  ability: Ability;
+}
+export interface GlobalCooldownEvent extends Event {
+  type: EventType.GlobalCooldown;
+  ability: Ability;
+  duration: number;
+  sourceID: number;
+  targetID: number;
+  timestamp: number;
+  trigger: CastEvent;
+  __fabricated: true;
 }
 
 const Events = {
   /**
-   * BEWARE: These events/properties are NOT COMPLETE. See the Event log for a full list of available props and events.
+   * BEWARE: These events/properties are NOT COMPLETE. See the Event log for a
+   * full list of available props and events.
    *
    * Generic props:
    * - timestamp: the timestamp of the event, relative to log start
-   * - type: the event type, you should generally not use this and properly separate event listeners.
+   * - type: the event type, you should generally not use this and properly
+   * separate event listeners.
    * - sourceID: who initiated the event
    * - sourceIsFriendly: whether the source was friendly to the selected player
    * - targetID: who was affected by the event
-   * - targetIsFriendly: whether the target was friendly to the selected player. BEWARE: @Any dps classes: make sure if you do a damage statistic that you do NOT include friendly fire to other players (such as from Aura of Sacrifice). This does not gain any damage bonuses.
-   * - ability: object of the ability/spell involved. Shape: { name, guid, type: I believe this is the magic school type, abilityIcon }
+   * - targetIsFriendly: whether the target was friendly to the selected
+   * player. BEWARE: @Any dps classes: make sure if you do a damage statistic
+   * that you do NOT include friendly fire to other players (such as from Aura
+   * of Sacrifice). This does not gain any damage bonuses.
+   * - ability: object of the ability/spell involved. Shape: { name, guid,
+   * type: I believe this is the magic school type, abilityIcon }
    * - resourceActor:
    * - classResources: array of resources (mana, energy, etc)
-   * - hitPoints: for healing these are the hitpoints AFTER the event's modifications are applied, for other events it might be before? you should check to make sure for damage, energize and such events
+   * - hitPoints: for healing these are the hitpoints AFTER the event's
+   * modifications are applied, for other events it might be before? you should
+   * check to make sure for damage, energize and such events
    * - maxHitPoints: this max amount of hitpoints of the target
    * - attackPower:
    * - spellPower:
-   * - x: x location on the map. See paladin/holy/modules/features/MasteryEffectiveness for an example module that uses this data.
+   * - x: x location on the map. See
+   * paladin/holy/modules/features/MasteryEffectiveness for an example module
+   * that uses this data.
    * - y: y location on the map
    * - facing: the direction the player is facing
    * - sourceMarker:
@@ -87,27 +405,33 @@ const Events = {
    */
 
   /**
-   * This event is called for events where the player, a player pet or a target of the player/player pet dealt or took damage.
-   * Event specific props:
+   * This event is called for events where the player, a player pet or a target
+   * of the player/player pet dealt or took damage. Event specific props:
    * - amount: effective damage
-   * - absorbed: damage absorbed by a buff on the target (e.g. https://www.wowhead.com/spell=269279/resounding-protection). This should generally be considered effective damage.
-   * - overkill: if the target died, this is the amount of damage that exceeded their remaining health
+   * - absorbed: damage absorbed by a buff on the target (e.g.
+   * https://www.wowhead.com/spell=269279/resounding-protection). This should
+   * generally be considered effective damage.
+   * - overkill: if the target died, this is the amount of damage that exceeded
+   * their remaining health
    * - hitType:
    * - mitigated:
    * - unmitigatedAmount:
    * - tick:
    *
-   * NOTE: Do not use this event to track absorb-healing (e.g. by a spell such as Resounding Protection). Use the `absorbed` event instead.
+   * NOTE: Do not use this event to track absorb-healing (e.g. by a spell such
+   * as Resounding Protection). Use the `absorbed` event instead.
    * @returns {EventFilter}
    */
   get damage() {
     return new EventFilter(EventType.Damage);
   },
   /**
-   * This event is called for events where the player, a player pet or a target of the player/player pet was healed.
-   * Event specific props:
+   * This event is called for events where the player, a player pet or a target
+   * of the player/player pet was healed. Event specific props:
    * - amount: effective healing
-   * - absorbed: healing absorbed by a debuff on the target (e.g. https://www.wowhead.com/spell=233263/embrace-of-the-eclipse). This should generally be considered effective healing.
+   * - absorbed: healing absorbed by a debuff on the target (e.g.
+   * https://www.wowhead.com/spell=233263/embrace-of-the-eclipse). This should
+   * generally be considered effective healing.
    * - overheal: overhealing
    * @returns {EventFilter}
    */
@@ -115,16 +439,19 @@ const Events = {
     return new EventFilter(EventType.Heal);
   },
   /**
-   * Triggered in addition to the regular heal event whenever a heal is absorbed. Can be used to determine what buff or debuff was absorbing the healing.
-   * NOTE: This should only be used if you need to know **which ability soaked the healing**. If you want to track the amount of absorbed healing by a spell, use the `absorb` prop of the `heal` event.
+   * Triggered in addition to the regular heal event whenever a heal is
+   * absorbed. Can be used to determine what buff or debuff was absorbing the
+   * healing. NOTE: This should only be used if you need to know **which
+   * ability soaked the healing**. If you want to track the amount of absorbed
+   * healing by a spell, use the `absorb` prop of the `heal` event.
    * @returns {EventFilter}
    */
   get healabsorbed() {
     return new EventFilter(EventType.HealAbsorbed);
   },
   /**
-   * This event is called for events where the player, a player pet or a target of the player/player pet was healed.
-   * Event specific props:
+   * This event is called for events where the player, a player pet or a target
+   * of the player/player pet was healed. Event specific props:
    * - ability: The ability responsible for absorbed damage (i.e. the shield)
    * - amount: effective damage absorbed
    * - attackerID:
@@ -136,7 +463,9 @@ const Events = {
     return new EventFilter(EventType.Absorbed);
   },
   /**
-   * This event is called when the player begins casting an ability that has a cast time. This is also called for some channeled abilities, but not everyone. This is NOT cast for most instant abilities.
+   * This event is called when the player begins casting an ability that has a
+   * cast time. This is also called for some channeled abilities, but not
+   * everyone. This is NOT cast for most instant abilities.
    * @returns {EventFilter}
    */
   get begincast() {
@@ -144,7 +473,9 @@ const Events = {
   },
   /**
    * This event is called when the player successfully cast an ability.
-   * BEWARE: Blizzard also sometimes uses this event type for mechanics and spell ticks or bolts. This can even occur in between a begincast and cast finish!
+   * BEWARE: Blizzard also sometimes uses this event type for mechanics and
+   * spell ticks or bolts. This can even occur in between a begincast and cast
+   * finish!
    * @returns {EventFilter}
    */
   get cast() {
@@ -160,7 +491,8 @@ const Events = {
   },
   /**
    * Event specific props:
-   * - absorb: If the buff can absorb healing (maybe there are debuffs that absorb damage too?), this reflects the size of the (healing) absorb.
+   * - absorb: If the buff can absorb healing (maybe there are debuffs that
+   * absorb damage too?), this reflects the size of the (healing) absorb.
    * @returns {EventFilter}
    */
   get applydebuff() {
@@ -196,7 +528,8 @@ const Events = {
   },
   /**
    * Event specific props:
-   * - absorb: If the buff could absorb damage, the size of the shield remaining. This is UNUSED/WASTED damage absorb.
+   * - absorb: If the buff could absorb damage, the size of the shield
+   * remaining. This is UNUSED/WASTED damage absorb.
    * @returns {EventFilter}
    */
   get removebuff() {
@@ -204,7 +537,8 @@ const Events = {
   },
   /**
    * Event specific props:
-   * - absorb: If the buff could absorb healing (maybe there are debuffs that absorb damage too?), this reflects the size of the (healing) absorb.
+   * - absorb: If the buff could absorb healing (maybe there are debuffs that
+   * absorb damage too?), this reflects the size of the (healing) absorb.
    * @returns {EventFilter}
    */
   get removedebuff() {
