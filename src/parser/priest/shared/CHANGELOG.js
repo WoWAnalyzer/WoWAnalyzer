d8b449b4f7e85b33c18f9ba7b0bbb6e681ebd3c4
David Grölle <david.groelle@protonmail.com>
2019-12-19 17:52:02 +0100

    Appease the changelog gods

---
+++ src/parser/priest/shared/CHANGELOG.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+  HawkCorrigan
+} from 'CONTRIBUTORS';
+import { change, date } from 'common/changelog';
+
+export default [
+  change(date(2019, 12, 19), <>Fixed some ordering issues in the DP module.</>, [HawkCorrigan]),
+];
