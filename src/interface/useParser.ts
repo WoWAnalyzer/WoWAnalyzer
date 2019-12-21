038a181c6484a00b5db5ef3cd13a755ee53e96cb
Martijn Hols <git@martijnhols.nl>
2019-12-21 22:08:47 +0100

    Add comment

--- src/interface/useParser.ts
+++ src/interface/useParser.ts
@@ -8,6 +8,10 @@ const useParser = (config: Config) => {
   const [parserClass, setParserClass] = useState<typeof CombatLogParser>();
   const load = async (parser: () => Promise<typeof CombatLogParser>) => {
     const parserClass = await retryingPromise(parser);
+    // A state setting can also be a function, since parserClass is considered
+    // a function during runtime as well, we need to always set the value with
+    // the function method to avoid the setter from trying to execute the
+    // parser.
     setParserClass(() => parserClass);
   };
 
