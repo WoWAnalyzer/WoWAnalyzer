c8d72f8ef71829325430f4e1890dc679a15300ee
Sharrq <cjgrasty86@gmail.com>
2019-12-22 19:41:15 -0600

    Revert "items"
    This reverts commit cb56f1e5c8c8a46fa47e854c99980dd889d7dffe.

--- src/parser/mage/fire/modules/items/HyperthreadWristwraps.js
+++ src/parser/mage/fire/modules/items/HyperthreadWristwraps.js
@@ -5,7 +5,7 @@ import SpellLink from 'common/SpellLink';
 import ItemLink from 'common/ItemLink';
 import { formatPercentage } from 'common/format';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { CastEvent } from 'parser/core/Events';
+import Events from 'parser/core/Events';
 import AbilityTracker from 'parser/shared/modules/AbilityTracker';
 import EnemyInstances from 'parser/shared/modules/EnemyInstances';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
@@ -18,34 +18,30 @@ class HyperthreadWristwraps extends Analyzer {
     spellUsable: SpellUsable,
     enemies: EnemyInstances,
   };
-  protected abilityTracker!: AbilityTracker;
-  protected spellUsable!: SpellUsable;
-  protected enemies!: EnemyInstances;
 
+  lastThreeSpells = [];
   fireBlastReductions = 0;
   badWristUse = 0;
 
-  constructor(options: any) {
-    super(options);
+  constructor(...args) {
+    super(...args);
     this.active = this.selectedCombatant.hasWrists(ITEMS.HYPERTHREAD_WRISTWRAPS.id);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.onSpellCast);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.HYPERTHREAD_WRISTWRAPS_CAST), this.onWristUse);
   }
 
-  protected lastThreeSpells: number[] = [];
-  onSpellCast(event: CastEvent) {
-    const spellId = event.ability.guid;
-    if (spellId === SPELLS.HYPERTHREAD_WRISTWRAPS_CAST.id) {
+  onSpellCast(event) {
+    if (event.ability.guid === SPELLS.HYPERTHREAD_WRISTWRAPS_CAST.id) {
       return;
     }
 
     if (this.lastThreeSpells.length === 3) {
       this.lastThreeSpells.shift();
     }
-    this.lastThreeSpells.push(spellId);
+    this.lastThreeSpells.push(event.ability.guid);
   }
 
-  onWristUse(event: CastEvent) {
+  onWristUse(event) {
     const bracerReduction = this.selectedCombatant.hasBuff(SPELLS.LUCID_DREAMS_MAJOR.id) ? 10000 : 5000;
     debug && console.log(this.lastThreeSpells);
     this.lastThreeSpells.forEach(spell => {
@@ -79,9 +75,9 @@ class HyperthreadWristwraps extends Analyzer {
     };
   }
 
-  suggestions(when: any) {
+  suggestions(when) {
 		when(this.suggestionThresholds)
-			.addSuggestion((suggest: any, actual: any, recommended: any) => {
+			.addSuggestion((suggest, actual, recommended) => {
 				return suggest(<>You used your <ItemLink id={ITEMS.HYPERTHREAD_WRISTWRAPS.id} /> improperly {this.badWristUse} times. In order to get the most out of the item, you should ensure that you are reducing <SpellLink id={SPELLS.FIRE_BLAST.id} /> twice per use. To accomplish this, you should cast <SpellLink id={SPELLS.FIRE_BLAST.id} /> > <SpellLink id={SPELLS.PYROBLAST.id} /> > <SpellLink id={SPELLS.FIRE_BLAST.id} /> > <ItemLink id={ITEMS.HYPERTHREAD_WRISTWRAPS.id} />. That way <SpellLink id={SPELLS.FIRE_BLAST.id} /> gets reduced twice.</>)
 					.icon(ITEMS.HYPERTHREAD_WRISTWRAPS.icon)
 					.actual(`${formatPercentage(this.wristEfficiency)}% Utilization`)
