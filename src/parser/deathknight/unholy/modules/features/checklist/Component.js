dfe5e7e1456edbcf921b2e0c534b49a7dc3e914c
Martijn Hols <git@martijnhols.nl>
2020-01-01 23:16:26 +0100

    Fix AbilityRequirement propTypes

--- src/parser/deathknight/unholy/modules/features/checklist/Component.js
+++ src/parser/deathknight/unholy/modules/features/checklist/Component.js
@@ -17,7 +17,7 @@ const UnholyDeathKnightChecklist = ({ combatant, castEfficiency, thresholds }) =
     />
   );
   AbilityRequirement.propTypes = {
-    spell: PropTypes.object.isRequired,
+    spell: PropTypes.number.isRequired,
   };
 
   return (
--- src/parser/monk/windwalker/modules/features/checklist/Component.js
+++ src/parser/monk/windwalker/modules/features/checklist/Component.js
@@ -17,7 +17,7 @@ const WindwalkerMonkChecklist = ({ combatant, castEfficiency, thresholds }) => {
     />
   );
   AbilityRequirement.propTypes = {
-    spell: PropTypes.object.isRequired,
+    spell: PropTypes.number.isRequired,
   };
 
   return (
