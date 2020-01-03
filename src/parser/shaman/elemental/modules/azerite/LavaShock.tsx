d30c262b7c3318ec02e3d2283b97c4ef03d723e5
Martijn Hols <git@martijnhols.nl>
2020-01-03 23:13:15 +0100

    Reformat elemental TS modules

--- src/parser/shaman/elemental/modules/azerite/LavaShock.tsx
+++ src/parser/shaman/elemental/modules/azerite/LavaShock.tsx
@@ -12,7 +12,7 @@ import { DamageEvent } from 'parser/core/Events';
 import Statistic from 'interface/statistics/Statistic';
 import { Trans } from '@lingui/macro';
 
-const ES_SP_COEFFICIENT = 2.1;// taken from Simcraft SpellDataDump (250% -> 210% in 8.1)
+const ES_SP_COEFFICIENT = 2.1; // taken from Simcraft SpellDataDump (250% -> 210% in 8.1)
 
 class LavaShock extends Analyzer {
   static dependencies = {
@@ -30,35 +30,42 @@ class LavaShock extends Analyzer {
     if (!this.active) {
       return;
     }
-    this.traitBonus = this.selectedCombatant.traitsBySpellId[SPELLS.LAVA_SHOCK.id]
-      .reduce((sum: number, rank: number) => sum + calculateAzeriteEffects(SPELLS.LAVA_SHOCK.id, rank)[0], 0);
+    this.traitBonus = this.selectedCombatant.traitsBySpellId[
+      SPELLS.LAVA_SHOCK.id
+    ].reduce(
+      (sum: number, rank: number) =>
+        sum + calculateAzeriteEffects(SPELLS.LAVA_SHOCK.id, rank)[0],
+      0,
+    );
   }
 
   on_byPlayer_damage(event: DamageEvent) {
     if (event.ability.guid !== SPELLS.EARTH_SHOCK.id) {
       return;
     }
-    const buff:any = this.selectedCombatant.getBuff(SPELLS.LAVA_SHOCK_BUFF.id);
+    const buff: any = this.selectedCombatant.getBuff(SPELLS.LAVA_SHOCK_BUFF.id);
 
     if (buff === undefined) {
       return;
     }
-    const [bonusDamage] = calculateBonusAzeriteDamage(event, [(buff.stacks || 0) * this.traitBonus], ES_SP_COEFFICIENT, this.statTracker.currentIntellectRating);
+    const [bonusDamage] = calculateBonusAzeriteDamage(
+      event,
+      [(buff.stacks || 0) * this.traitBonus],
+      ES_SP_COEFFICIENT,
+      this.statTracker.currentIntellectRating,
+    );
     this.damageGained += bonusDamage;
     this.procs += 1;
   }
 
   statistic() {
     return (
-      <Statistic
-        position={STATISTIC_ORDER.OPTIONAL()}
-        size="small"
-        >
+      <Statistic position={STATISTIC_ORDER.OPTIONAL()} size="small">
         <BoringSpellValue
           spell={SPELLS.LAVA_SHOCK}
           value={<Trans>{formatNumber(this.damageGained)} Damage</Trans>}
           label={<Trans>Damage done</Trans>}
-      />
+        />
       </Statistic>
     );
   }
