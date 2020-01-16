310958d0373373146b4d0859cacfeed56204fcf7
Sharrq <cjgrasty86@gmail.com>
2020-01-15 18:29:40 -0600

    [Arcane Mage] Updated Compatibility to 8.3
    No changes for Arcane Mage in 8.3. Updating spec compatibility to 8.3

--- src/parser/mage/arcane/CHANGELOG.js
+++ src/parser/mage/arcane/CHANGELOG.js
@@ -6,6 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 1, 15), 'Updated Spec Compatibility to 8.3',[Sharrq]),
   change(date(2019, 10, 1), <>Removed the check to see if <SpellLink id={SPELLS.ARCANE_POWER.id} /> was available when the fight ended.</>,[Sharrq]),
   change(date(2019, 9, 30), 'Updated Spec Compatibility to 8.2.5.',[Sharrq]),
   change(date(2019, 9, 16), 'Fix a bug where arcane mage mana graph would not show time labels correctly.', fluffels),
