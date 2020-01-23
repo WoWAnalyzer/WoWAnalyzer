bd03d6cdd0417c50a5eac258082dfa72b597c97e
Joscha Rentz <rentz@hotmail.de>
2020-01-23 16:00:54 +0100

    filter everything but class abilities

--- src/common/SPELLS/bfa/corruption/corruption.js
+++ src/common/SPELLS/bfa/corruption/corruption.js
@@ -1,14 +1,20 @@
+// Spells such as on use casts or buffs triggered by corruption
+// most of them are probably in common/corruptionIdMap
 
 export default {
-  INEFFABLE_TRUTH: { 
+  INEFFABLE_TRUTH_BUFF: {
     id: 316801,
     name: 'Ineffable Truth',
     icon: 'inv_wand_1h_nzothraid_d_01',
   },
-  INEFFABLE_TRUTH: { 
-    id: 316801,
+  INEFFABLE_TRUTH_30: {
+    id: 318303,
+    name: 'Ineffable Truth',
+    icon: 'inv_wand_1h_nzothraid_d_01',
+  },
+  INEFFABLE_TRUTH_50: {
+    id: 318484,
     name: 'Ineffable Truth',
     icon: 'inv_wand_1h_nzothraid_d_01',
   },
 };
-//318303
