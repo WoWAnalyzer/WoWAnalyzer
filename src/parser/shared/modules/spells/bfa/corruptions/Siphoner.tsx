d160869e31f07b7810ded849087e94c251de4949
Joscha Rentz <rentz@hotmail.de>
2020-02-05 16:18:55 +0100

    typescript

--- src/parser/shared/modules/spells/bfa/corruptions/Siphoner.tsx
+++ src/parser/shared/modules/spells/bfa/corruptions/Siphoner.tsx
@@ -1,46 +1,48 @@
 import React from 'react';
 
-import Events from 'parser/core/Events';
+import Events, { HealEvent } from 'parser/core/Events';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import calculateEffectiveHealing from 'parser/core/calculateEffectiveHealing';
 import SPELLS from 'common/SPELLS/index';
-import ItemStatistic from 'interface/statistics/ItemStatistic';
+import Statistic from 'interface/statistics/Statistic';
+import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
 import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
 import StatTracker from 'parser/shared/modules/StatTracker';
 import ItemHealingDone from 'interface/ItemHealingDone';
 
-const T1_SIPHON = 0.03;
-const T2_SIPHON = 0.05;
-const T3_SIPHON = 0.08;
+const T1_SIPHON: number = 0.03;
+const T2_SIPHON: number = 0.05;
+const T3_SIPHON: number = 0.08;
 
 class Siphoner extends Analyzer {
   static dependencies = {
     statTracker: StatTracker,
   };
+  protected statTracker!: StatTracker;
 
-  leechRatingFromSiphoner = 0;
-  healing = 0;
+  leechRatingFromSiphoner: number = 0;
+  healing: number = 0;
 
-  constructor(...args) {
-    super(...args);
+  constructor(options: any) {
+    super(options);
     this.active = this.selectedCombatant.hasCorruptionByName("Siphoner");
     if (!this.active) {
       return;
     }
 
-    let leechPercent = 0;
+    let leechPercent: number = 0;
     leechPercent += this.selectedCombatant.getCorruptionCount(SPELLS.SIPHONER_T1.id) * T1_SIPHON;
     leechPercent += this.selectedCombatant.getCorruptionCount(SPELLS.SIPHONER_T2.id) * T2_SIPHON;
     leechPercent += this.selectedCombatant.getCorruptionCount(SPELLS.SIPHONER_T3.id) * T3_SIPHON;
 
-    this.leechRatingFromSiphoner = leechPercent * this.statTracker.leechRatingPerPercent;
+    this.leechRatingFromSiphoner = leechPercent * options.statTracker.leechRatingPerPercent;
 
-    this.statTracker.forceChangeStats({ leech: this.leechRatingFromSiphoner }, "Siphoner");
+    options.statTracker.forceChangeStats({ leech: this.leechRatingFromSiphoner }, "Siphoner");
 
     this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.LEECH), this.leech);
   }
 
-  leech(event) {
+  leech(event: HealEvent) {
     const siph = this.leechRatingFromSiphoner / this.statTracker.currentLeechRating;
     const effectiveHealing = calculateEffectiveHealing(event, siph);
     this.healing += effectiveHealing;
@@ -48,11 +50,11 @@ class Siphoner extends Analyzer {
 
   statistic() {
     return (
-      <ItemStatistic size="flexible">
+      <Statistic size="flexible" category={STATISTIC_CATEGORY.ITEMS}>
         <BoringSpellValueText spell={SPELLS.SIPHONER_T3}>
         <ItemHealingDone amount={this.healing} />
         </BoringSpellValueText>
-      </ItemStatistic>
+      </Statistic>
     );
   }
 }
