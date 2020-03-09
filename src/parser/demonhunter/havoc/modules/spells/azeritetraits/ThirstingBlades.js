142571d14f6760ee2b85ff718af9942f8e93a6e6
Brad Duvall <bradford.duvall@gmail.com>
2020-03-09 17:40:19 -0400

    first fix on thirsting blades

---
+++ src/parser/demonhunter/havoc/modules/spells/azeritetraits/ThirstingBlades.js
@@ -0,0 +1,105 @@
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import { calculateAzeriteEffects } from 'common/stats';
+import SPELLS from 'common/SPELLS';
+import Events from 'parser/core/Events';
+import AzeritePowerStatistic from 'interface/statistics/AzeritePowerStatistic';
+import SpellLink from 'common/SpellLink';
+import { formatNumber, formatPercentage } from 'common/format';
+import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
+import React from 'react';
+
+// grabbed from RevolvingBlades.js
+const azeriteTraitStats = traits => Object.values(traits).reduce((obj, rank) => {
+  const [damage] = calculateAzeriteEffects(SPELLS.THIRSTING_BLADES.id, rank);
+  obj.damage += damage;
+  return obj;
+}, {
+  damage: 0,
+});
+
+/**
+ * Thirsting Blades Azerite Power
+ * Requires Demon Hunter (Havoc)
+ * 
+ * Every 1.5 sec, increase the damage of Chaos Strike by 10 and reduce its cost by 1 Fury. This effect stacks
+ * 
+ * Heavily borrowed from RevolvingBlades.js to make this module
+ */
+class ThirstingBlades extends Analyzer {
+
+  damagePerThirstingBlades = 0; // calculate damage per stack for azerite trait
+  stacksGained = 0; // total over entire fight used (wont count stacks that dont get used like at end of fight)
+  castedCount = 0; // how many chaos strikes or annhilation casts (buff is removed/used)
+  currentStacks = 0; // tracks the stack amount everytime the buff is removed/used
+
+  constructor(...args) {
+    super(...args);
+
+    this.addEventListener(Events.applybuffstack.by(SELECTED_PLAYER), this.onApplyBuffStack);
+    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER), this.onApplyBuff);
+    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER), this.onRemoveThirstingBlades);
+
+    const { damage } = azeriteTraitStats(this.selectedCombatant.traitsBySpellId[SPELLS.THIRSTING_BLADES.id]);
+    this.damagePerThirstingBlades = damage;
+
+  }
+
+  onApplyBuff(event) {
+    if(event.ability.name === "Thirsting Blades") {
+      this.currentStacks = 1;
+    }
+  }
+
+  onApplyBuffStack(event) {
+    if(event.ability.name === "Thirsting Blades") {
+      this.currentStacks += 1;
+    }
+  }
+
+  onRemoveThirstingBlades(event) {
+    if(event.ability.name === "Thirsting Blades") {
+      this.stacksGained += this.currentStacks;
+      this.castedCount += 1;
+      this.currentStacks = 0;
+    }
+  }
+
+  statistic() {
+
+    const totalDamage = this.stacksGained * this.damagePerThirstingBlades;
+    const damageThroughputPercent = this.owner.getPercentageOfTotalDamageDone(totalDamage);
+    const dps = totalDamage / this.owner.fightDuration * 1000;
+
+    return (
+      <AzeritePowerStatistic
+        size="flexible"
+        tooltip={(
+          <>
+            <SpellLink id={SPELLS.THIRSTING_BLADES.id} /> stacks every 1.5 seconds, increasing <SpellLink id={SPELLS.ANNIHILATION.id} /><br />
+            and <SpellLink id={SPELLS.CHAOS_STRIKE.id} /> by {formatNumber(this.damagePerThirstingBlades)} damage per <SpellLink id={SPELLS.THIRSTING_BLADES.id} /> stack.<br />
+            <br />
+            Reduces the Fury cost for <SpellLink id={SPELLS.ANNIHILATION.id} /><br />
+            and <SpellLink id={SPELLS.CHAOS_STRIKE.id} /> by 1 per stack of <SpellLink id={SPELLS.THIRSTING_BLADES.id} /> <br />
+          </>
+        )}
+      >
+        <BoringSpellValueText spell={SPELLS.THIRSTING_BLADES}>
+          <img
+            src="/img/sword.png"
+            alt="Damage"
+            className="icon"
+          /> 
+            {formatNumber(dps)} DPS 
+            <small>
+              {formatPercentage(damageThroughputPercent)} % of total
+            </small>
+            <br />
+            {this.stacksGained}  
+            <small> Fury saved</small>
+        </BoringSpellValueText>
+      </AzeritePowerStatistic>
+    );
+  }
+}
+
+export default ThirstingBlades;
