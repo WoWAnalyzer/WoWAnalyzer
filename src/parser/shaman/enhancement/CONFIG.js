f46507b7c9daea6984712ebdc85b1c157ac5b47d
Danny Janse <d.janse@live.com>
2020-04-12 03:49:11 +0200

    Added Vetyst to contributors and changed compatibility to 8.3 for Enhancement Shaman.

--- src/parser/shaman/enhancement/CONFIG.js
+++ src/parser/shaman/enhancement/CONFIG.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HawkCorrigan } from 'CONTRIBUTORS';
+import { HawkCorrigan, Vetyst } from 'CONTRIBUTORS';
 import SPECS from 'game/SPECS';
 import Warning from 'interface/Alert/Warning';
 
@@ -8,9 +8,9 @@ import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list.
-  contributors: [HawkCorrigan],
+  contributors: [HawkCorrigan, Vetyst],
   // The WoW client patch this spec was last updated to be fully compatible with.
-  patchCompatibility: '8.0.1',
+  patchCompatibility: '8.3',
   // If set to  false`, the spec will show up as unsupported.
   isSupported: false,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
@@ -23,7 +23,7 @@ export default {
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport: '/report/67LHQfJjCFzgyXBr/8-Normal+Stormwall+Blockade+-+Kill+(7:17)/118-Scryd',
+  exampleReport: '/report/XCr9vJdmcKQtTWLz/4-Heroic+Maut+-+Kill+(4:04)/Part%C3%ADcle/standard',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
