cb52b5fb744aa774386020fe6c934c1f9fd962b4
Danny Janse <d.janse@live.com>
2020-04-13 08:01:31 +0200

    Added statistics for Arcanic Pulsar azerite trait

--- src/parser/druid/balance/CHANGELOG.js
+++ src/parser/druid/balance/CHANGELOG.js
@@ -6,6 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 4, 13), <>Added statistics for <SpellLink id={SPELLS.ARCANIC_PULSAR_TRAIT.id} /> azerite trait.</>, []),
   change(date(2019, 8, 15), <>Fixed damage calculations for <SpellLink id={SPELLS.STREAKING_STARS.id} />.</>, [Viridis]),
   change(date(2019, 8, 13), <>Added tracking for <SpellLink id={SPELLS.STREAKING_STARS.id} />.</>, [Viridis]),
   change(date(2019, 8, 12), 'Fixed an incorrect spell name on the suggestions for Balance Druids when using Stellar Flare.', Wing5wong),
---
+++ src/parser/druid/balance/modules/talents/azeritetraits/ArcanicPulsar.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import SPELLS from 'common/SPELLS/index';
+
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+
+import Statistic from 'interface/statistics/Statistic';
+import Events from 'parser/core/Events';
+import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
+import BoringSpellValueText
+  from 'interface/statistics/components/BoringSpellValueText';
+import ItemDamageDone from 'interface/ItemDamageDone';
+import { calculateAzeriteEffects } from 'common/stats';
+
+class ArcanicPulsar extends Analyzer {
+  /**
+   * Starsurge's damage is increased by 2181. Every 9 Starsurges, gain
+   * Celestial Alignment for 6 sec.
+   *
+   * Example Log:
+   *
+   */
+
+  protected damageGained = 0;
+  protected bonusDamage = 0;
+  protected firstStarsurgeCast = true;
+  protected celestialAlignmentCount = 0;
+
+  constructor(options: any) {
+    super(options);
+    this.active
+      = this.selectedCombatant.hasTrait(SPELLS.ARCANIC_PULSAR_TRAIT.id);
+
+    if (!this.active) {
+      return;
+    }
+
+    this.bonusDamage
+      = this.selectedCombatant.traitsBySpellId[SPELLS.ARCANIC_PULSAR_TRAIT.id]
+      .reduce((total, rank) => {
+        const [damage] = calculateAzeriteEffects(
+          SPELLS.ARCANIC_PULSAR_TRAIT.id,
+          rank,
+        );
+        return total + damage;
+      }, 0);
+
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER)
+        .spell(SPELLS.STARSURGE_MOONKIN),
+      this.onStarsurgeCast,
+    );
+
+    this.addEventListener(
+      Events.damage.by(SELECTED_PLAYER)
+        .spell(SPELLS.STARSURGE_MOONKIN),
+      this.onStarsurgeDamage,
+    );
+  }
+
+  onStarsurgeCast() {
+    if (this.firstStarsurgeCast) {
+      this.firstStarsurgeCast = false;
+      return;
+    }
+
+    const buff = this.selectedCombatant.getBuff(SPELLS.ARCANIC_PULSAR_BUFF.id);
+    if (!buff) {
+      this.celestialAlignmentCount++;
+    }
+  }
+
+  onStarsurgeDamage() {
+    this.damageGained += this.bonusDamage;
+  }
+
+  statistic() {
+    return (
+      <Statistic
+        position={STATISTIC_ORDER.OPTIONAL()}
+        size="flexible"
+        category={'AZERITE_POWERS'}
+        tooltip={`Celestial Alignment was triggered ${this.celestialAlignmentCount} times.`}
+      >
+        <BoringSpellValueText spell={SPELLS.ARCANIC_PULSAR_TRAIT}>
+          <ItemDamageDone amount={this.damageGained} />
+        </BoringSpellValueText>
+      </Statistic>
+    );
+  }
+}
+
+export default ArcanicPulsar;
