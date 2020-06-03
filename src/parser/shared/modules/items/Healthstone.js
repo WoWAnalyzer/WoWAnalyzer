baf29da0eb989ee97554933788ce664d4659716c
Danny Janse <d.janse@live.com>
2020-06-03 14:56:40 +0200

    disable healthstone module if no warlock was found.

--- src/parser/shared/modules/items/Healthstone.js
+++ src/parser/shared/modules/items/Healthstone.js
@@ -1,11 +1,25 @@
 import SPELLS from 'common/SPELLS/index';
 
+import Combatants from 'parser/shared/modules/Combatants';
+import Abilities from 'parser/core/modules/Abilities';
+import Buffs from 'parser/core/modules/Buffs';
+import SpellUsable from 'parser/shared/modules/SpellUsable';
+import AbilityTracker from 'parser/shared/modules/AbilityTracker';
+import { Class } from 'parser/core/Events';
 import Potion from './Potion';
-
 /**
  * Tracks Healthstone cooldown.
  */
 class Healthstone extends Potion {
+  // Potion does not use Combatants, other dependencies are inherit from Potion.
+  static dependencies = {
+    abilities: Abilities,
+    buffs: Buffs,
+    spellUsable: SpellUsable,
+    abilityTracker: AbilityTracker,
+    combatants: Combatants,
+  };
+
   static spells = [
     SPELLS.HEALTHSTONE,
   ];
@@ -13,6 +27,11 @@ class Healthstone extends Potion {
   static extraAbilityInfo = {
     isDefensive: true,
   };
+
+  get isAvailable() {
+    const players = Object.values(this.combatants.players);
+    return players.find(combatant => (combatant.spec.className === Class.Warlock));
+  }
 }
 
 export default Healthstone;
