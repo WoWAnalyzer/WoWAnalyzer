063518f2584acda21b8cfe71d75c4d54f2a58d59
Danny Janse <d.janse@live.com>
2020-06-04 11:29:19 +0200

    Changes based on Dambroda's review

--- src/parser/shared/modules/items/Healthstone.js
+++ src/parser/shared/modules/items/Healthstone.js
@@ -1,22 +1,14 @@
 import SPELLS from 'common/SPELLS/index';
 
 import Combatants from 'parser/shared/modules/Combatants';
-import Abilities from 'parser/core/modules/Abilities';
-import Buffs from 'parser/core/modules/Buffs';
-import SpellUsable from 'parser/shared/modules/SpellUsable';
-import AbilityTracker from 'parser/shared/modules/AbilityTracker';
 import { Class } from 'parser/core/Events';
 import Potion from './Potion';
 /**
  * Tracks Healthstone cooldown.
  */
 class Healthstone extends Potion {
-  // Potion does not use Combatants, other dependencies are inherit from Potion.
   static dependencies = {
-    abilities: Abilities,
-    buffs: Buffs,
-    spellUsable: SpellUsable,
-    abilityTracker: AbilityTracker,
+    ...Potion.dependencies,
     combatants: Combatants,
   };
 
@@ -30,7 +22,7 @@ class Healthstone extends Potion {
 
   get isAvailable() {
     const players = Object.values(this.combatants.players);
-    return players.find(combatant => (combatant.spec.className === Class.Warlock));
+    return players.some(combatant => (combatant.spec.className === Class.Warlock));
   }
 }
 
