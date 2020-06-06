fbf7f0fb4d13b8afdea33e73f130c8e36c993961
Pewtro <laustsf@gmail.com>
2020-06-07 01:54:46 +0200

    Move Lock and Load and Volley binomial calculations from their own to the shared calculation

--- src/parser/hunter/marksmanship/modules/talents/LockAndLoad.tsx
+++ src/parser/hunter/marksmanship/modules/talents/LockAndLoad.tsx
@@ -11,6 +11,7 @@ import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
 import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
 import { ApplyBuffEvent, CastEvent, DamageEvent, RefreshBuffEvent } from 'parser/core/Events';
+import { singleProbabilityBinomialCalculation } from 'parser/shared/modules/helpers/Probabilities';
 
 /**
  * Your ranged auto attacks have a 5% chance to trigger Lock and Load, causing your next Aimed Shot to cost no Focus and be instant.
@@ -50,9 +51,9 @@ class LockAndLoad extends Analyzer {
     this.hasLnLBuff = true;
     if (this.spellUsable.isOnCooldown(SPELLS.AIMED_SHOT.id)) {
       const expectedCooldownDuration = this.abilities.getExpectedCooldownDuration(SPELLS.AIMED_SHOT.id, this.spellUsable.cooldownTriggerEvent(SPELLS.AIMED_SHOT.id));
-      if(expectedCooldownDuration) {
-      const newChargeCDR = expectedCooldownDuration - this.spellUsable.cooldownRemaining(SPELLS.AIMED_SHOT.id);
-      this.spellUsable.endCooldown(SPELLS.AIMED_SHOT.id, false, event.timestamp, newChargeCDR);
+      if (expectedCooldownDuration) {
+        const newChargeCDR = expectedCooldownDuration - this.spellUsable.cooldownRemaining(SPELLS.AIMED_SHOT.id);
+        this.spellUsable.endCooldown(SPELLS.AIMED_SHOT.id, false, event.timestamp, newChargeCDR);
       }
     }
   }
@@ -77,7 +78,7 @@ class LockAndLoad extends Analyzer {
     this.totalProcs += 1;
   }
 
-  on_byPlayer_damage(event:DamageEvent) {
+  on_byPlayer_damage(event: DamageEvent) {
     const spellID = event.ability.guid;
     if (spellID !== SPELLS.AUTO_SHOT.id) {
       return;
@@ -89,58 +90,8 @@ class LockAndLoad extends Analyzer {
     return this.autoShots * PROC_CHANCE;
   }
 
-  GetZPercent(z: number) {
-    // If z is greater than 6.5 standard deviations from the mean the number of significant digits will be outside of a reasonable range.
-    if (z < -6.5) {
-      return 0.0;
-    }
-
-    if (z > 6.5) {
-      return 1.0;
-    }
-
-    let factK = 1;
-    let sum = 0;
-    let term = 1;
-    let k = 0;
-    const loopStop = Math.exp(-23);
-
-    while (Math.abs(term) > loopStop) {
-      term = 0.3989422804 * Math.pow(-1, k) * Math.pow(z, k) / (2 * k + 1) /
-        Math.pow(2, k) * Math.pow(z, k + 1) / factK;
-      sum += term;
-      k += 1;
-      factK *= k;
-    }
-    sum += 0.5;
-
-    return sum;
-  }
-
-  binomialCalculation(procs: number, tries: number, procChance: number) {
-    //Correcting for continuity we add 0.5 to procs, because we're looking for the probability of getting at most the amount of procs we received if P(X <= a), then P(X<a+0.5)
-    const correctedProcs = procs + 0.5;
-    const nonProcChance = 1 - procChance;
-
-    const stddev = Math.sqrt(procChance * nonProcChance * tries);
-    //zScore is calculated by saying (X - M) / stddev
-    const zScore = (correctedProcs - this.pn) / stddev;
-
-    return this.GetZPercent(zScore);
-  }
-
-  //pn is the mean value of procs
-  get pn() {
-    return PROC_CHANCE * this.autoShots;
-  }
-
-  //qn is the mean value of non-procs
-  get qn() {
-    return (1 - PROC_CHANCE) * this.autoShots;
-  }
-
   statistic() {
-    const binomCalc = this.binomialCalculation(this.totalProcs, this.autoShots, PROC_CHANCE);
+    const binomCalc = singleProbabilityBinomialCalculation(this.totalProcs, this.autoShots, PROC_CHANCE);
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(10)}
@@ -153,14 +104,6 @@ class LockAndLoad extends Analyzer {
             You had a total of {this.totalProcs} procs, and your expected amount of procs was {formatNumber(this.expectedProcs)}. <br />
             <ul>
               <li>You have a ~{formatPercentage(binomCalc)}% chance of getting this amount of procs or fewer in the future with this amount of autoattacks.</li>
-              {/*these two first tooltipText additions will probably NEVER happen, but it'd be fun if they ever did*/}
-              {binomCalc === 0 && <li>You had so few procs that the chance of you getting fewer procs than what you had on this attempt is going to be de facto 0%. Consider yourself the unluckiest man alive.</li>}
-              {binomCalc === 1 && <li>You had so many procs that the chance of you getting fewer procs than what you had on this attempt is going to be de facto 100%. Consider yourself the luckiest man alive.</li>}
-              {(binomCalc > 0 && binomCalc < 1) && (
-                (this.pn > 10 || this.qn > 10) ?
-                  <li>Due to normal approximation these results are within 2% margin of error.</li> :
-                  <li>Because you had under {10 / PROC_CHANCE} auto attacks and due to normal approximation these results have a margin of error of over 2%.</li>
-              )}
             </ul>
           </>
         )}
