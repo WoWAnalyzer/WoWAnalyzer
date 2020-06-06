e94996000ae06f24be7d045eaea674e7f7e3e211
Pewtro <laustsf@gmail.com>
2020-06-06 23:48:11 +0200

    Move old helper out of warlock

--- src/parser/warlock/demonology/modules/pets/helpers.js
+++ src/parser/warlock/demonology/modules/pets/helpers.js
@@ -1,6 +1,6 @@
+import { isPermanentPet } from 'parser/shared/modules/pets/helpers';
 import PETS from './PETS';
 
-export const isPermanentPet = guid => guid.toString().length > 6;
 export const isWildImp = guid => guid === PETS.WILD_IMP_HOG.guid || guid === PETS.WILD_IMP_INNER_DEMONS.guid;
 export const isRandomPet = guid => isWarlockPet(guid) && !isPermanentPet(guid) && PETS[guid].isRandom;
 export const isWarlockPet = guid => isPermanentPet(guid) || !!PETS[guid];
