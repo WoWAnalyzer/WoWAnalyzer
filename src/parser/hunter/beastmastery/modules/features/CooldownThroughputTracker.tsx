24b9730e7f55cf1c1de64546d433e48c80172e25
Pewtro <laustsf@gmail.com>
2020-06-09 01:40:08 +0200

    Add uptime to Beast Cleave and Aspect of the Wild. Add debuff uptime to Blood of the Enemy

--- src/parser/hunter/beastmastery/modules/features/CooldownThroughputTracker.tsx
+++ src/parser/hunter/beastmastery/modules/features/CooldownThroughputTracker.tsx
@@ -1,5 +1,6 @@
 import SPELLS from 'common/SPELLS';
 import CoreCooldownThroughputTracker, { BUILT_IN_SUMMARY_TYPES } from 'parser/shared/modules/CooldownThroughputTracker';
+import { ApplyDebuffEvent, CastEvent } from 'parser/core/Events';
 
 class CooldownThroughputTracker extends CoreCooldownThroughputTracker {
   static cooldownSpells = [
@@ -16,7 +17,28 @@ class CooldownThroughputTracker extends CoreCooldownThroughputTracker {
         BUILT_IN_SUMMARY_TYPES.DAMAGE,
       ],
     },
+    {
+      spell: SPELLS.BLOOD_OF_THE_ENEMY,
+      summary: [
+        BUILT_IN_SUMMARY_TYPES.DAMAGE,
+      ],
+    },
   ];
+
+  on_byPlayer_applydebuff(event: ApplyDebuffEvent) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.BLOOD_OF_THE_ENEMY.id) {
+      this.startCooldown(event);
+    }
+  }
+
+  on_byPlayer_cast(event: CastEvent) {
+    const spellId = event.ability.guid;
+    this.trackEvent(event);
+    if (spellId === SPELLS.BLOOD_OF_THE_ENEMY.id) {
+      this.startCooldown(event);
+    }
+  }
 }
 
 export default CooldownThroughputTracker;
