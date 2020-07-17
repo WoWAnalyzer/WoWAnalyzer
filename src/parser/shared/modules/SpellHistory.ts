7752e41c97cb97d06fc0e85fe928098b6c106165
Dambroda <43309639+Dambroda@users.noreply.github.com>
2020-07-17 13:26:14 -0700

    fix an import on SpellHistory

--- src/parser/shared/modules/SpellHistory.ts
+++ src/parser/shared/modules/SpellHistory.ts
@@ -1,4 +1,5 @@
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Abilities from 'parser/core/modules/Abilities';
 import Channeling from 'parser/shared/modules/Channeling';
 import Events, {
   ApplyBuffEvent,
@@ -11,7 +12,6 @@ import Events, {
 } from 'parser/core/Events';
 
 import SpellUsable from './SpellUsable';
-import Abilities from '../../core/modules/Abilities';
 
 class SpellHistory extends Analyzer {
   static dependencies = {
