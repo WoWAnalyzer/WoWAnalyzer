d75a5b2937988558102625f3d79b7691ab054494
Laust <laustsf@gmail.com>
2020-07-25 17:42:01 +0200

    [Hunter] Add Kill Shot for all specs for SL

---
+++ src/parser/hunter/shared/modules/spells/KillShot.tsx
@@ -0,0 +1,8 @@
+import Analyzer from 'parser/core/Analyzer';
+
+//TODO Add something akin to ExecuteRange.js in arms/modules/core/Execute to track proper ABC of Kill Shot
+class KillShot extends Analyzer {
+
+}
+
+export default KillShot;
