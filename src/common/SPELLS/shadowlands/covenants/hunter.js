1cc75dee18dd2afa7f415bb015746b0620734b7a
Pewtro <laustsf@gmail.com>
2020-08-02 19:42:55 +0200

    [Hunter] Clean up SPELLS folders

--- src/common/SPELLS/shadowlands/covenants/hunter.js
+++ src/common/SPELLS/shadowlands/covenants/hunter.js
@@ -1,7 +1,27 @@
 export default {
+
+  //region Venthyr
   FLAYERS_MARK: {
     id: 324156,
     name: 'Flayer\'s Mark',
     icon: 'ability_hunter_assassinate2',
   },
+  //endregion
+
+  //region Kyrian
+  RESONATING_ARROW: {
+    id: 308491,
+    name: 'Resonating Arrow',
+    icon: 'ability_bastion_hunter',
+  },
+  //endregion
+
+  //region Necrolord
+
+  //endregion
+
+  //region Night Fae
+
+  //endregion
+
 };
