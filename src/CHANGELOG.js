d3214f3b377cf3b2d5f85ffc9de41e22cce67ad7
Pewtro <laustsf@gmail.com>
2020-08-08 17:58:55 +0200

    [Core] Update changelog, since I actually, actually fixed it now

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -10,7 +10,7 @@ import Contributor from 'interface/ContributorButton';
 
 // prettier-ignore
 export default [
-  change(date(2020, 8, 5), 'Fix an issue where changelogs wouldn\'t count in pull requests.', Putro),
+  change(date(2020, 8, 8), 'Fixed an issue where changelogs wouldn\'t count in pull requests.', Putro),
   change(date(2020, 8, 4), 'Fixed a bug causing the total fight duration field to be improperly calculated, leading to confusing downtime/death percentages', Dambroda),
   change(date(2020, 8, 2), 'Remove Corruption in preparation for Shadowlands', Putro),
   change(date(2020, 7, 27), 'Updated contributor details to TypeScript and fixed contributor description not appearing when viewing those details', Dambroda),
