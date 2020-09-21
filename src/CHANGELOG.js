49a74f2529f06efe7eaea4ad0c35a2c5e8cf6cee
Sharrq <cjgrasty86@gmail.com>
2020-09-20 20:03:49 -0500

    changelog

--- src/CHANGELOG.js
+++ src/CHANGELOG.js
@@ -10,6 +10,7 @@ import Contributor from 'interface/ContributorButton';
 
 // prettier-ignore
 export default [
+  change(date(2020, 9, 20), 'Update checks for Enchants, Potions, Flasks, and Food for Shadowlands', Sharrq),
   change(date(2020, 9, 15), 'Add the possibility to fake player information in development if it doesn\'t exist in combatantinfo', Putro),
   change(date(2020, 8, 14), 'Added a boss name abbreviation function', Putro),
   change(date(2020, 8, 13), 'Added a generic Execute module that should help in standardizing and maintaining execute modules across specs', Putro),
