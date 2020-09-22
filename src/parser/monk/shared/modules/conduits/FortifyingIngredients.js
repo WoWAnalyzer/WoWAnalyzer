a4e194c816ef6a484aa8722aec6317aecb7a8caa
Abel Melchiore <abelgm@charter.net>
2020-09-21 20:15:59 -0400

    yes juko you're right

--- src/parser/monk/shared/modules/conduits/FortifyingIngredients.js
+++ src/parser/monk/shared/modules/conduits/FortifyingIngredients.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import SPELLS from 'common/SPELLS';
-import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events from 'parser/core/Events';
+import Analyzer from 'parser/core/Analyzer';
 
 import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
+import HealingDone from 'parser/shared/modules/throughput/HealingDone';
 import Statistic from 'interface/statistics/Statistic';
 import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
 import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
 import ItemHealingDone from 'interface/ItemHealingDone';
 
 class FortifyingIngredients extends Analyzer {
-
-  shield = 0;
+	static dependencies = {
+    healingDone: HealingDone,
+  };
 
   /**
    * Whenever you cast fort brew you gain a shield for x% of your hp
@@ -24,14 +25,11 @@ class FortifyingIngredients extends Analyzer {
     if (!this.active) {
       return;
     }
-    this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.FORTIFYING_INGREDIENTS_SHIELD), this.addShield);
   }
 
-  addShield(event) {
-    this.shield += (event.absorb || 0);
-  }
 
   statistic() {
+    const shield = this.healingDone.byAbility(SPELLS.FORTIFYING_INGREDIENTS.id).effective;
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(13)}
@@ -39,7 +37,7 @@ class FortifyingIngredients extends Analyzer {
         category={STATISTIC_CATEGORY.COVENANTS}
       >
         <BoringSpellValueText spell={SPELLS.FORTIFYING_INGREDIENTS}>
-          <ItemHealingDone amount={this.shield} />
+          <ItemHealingDone amount={shield} />
         </BoringSpellValueText>
       </Statistic>
     );
