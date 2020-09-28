cfe4be679b4b2f40164216199200d8f953888dd5
Dambroda <43309639+Dambroda@users.noreply.github.com>
2020-09-28 10:56:39 -0700

    Convert SpellUsable to TS

--- src/parser/warrior/protection/modules/features/SpellUsable.ts
+++ src/parser/warrior/protection/modules/features/SpellUsable.ts
@@ -1,20 +1,25 @@
 import SPELLS from 'common/SPELLS';
 import CoreSpellUsable from 'parser/shared/modules/SpellUsable';
 import GlobalCooldown from 'parser/shared/modules/GlobalCooldown';
+import { Event, CastEvent } from 'parser/core/Events';
 
 class SpellUsable extends CoreSpellUsable {
   static dependencies = {
     ...CoreSpellUsable.dependencies,
     globalCooldown: GlobalCooldown,
   };
+  protected globalCooldown!: GlobalCooldown;
 
-  constructor(...args) {
-    super(...args);
+  hasDevastator: boolean;
+
+  constructor(options: any) {
+    super(options);
     this.hasDevastator = this.selectedCombatant.hasTalent(SPELLS.DEVASTATOR_TALENT.id);
   }
 
-  lastPotentialTriggerForShieldSlam = null;
-  on_byPlayer_cast(event) {
+  lastPotentialTriggerForShieldSlam: CastEvent | null = null;
+
+  on_byPlayer_cast(event: CastEvent) {
     if (super.on_byPlayer_cast) {
       super.on_byPlayer_cast(event);
     }
@@ -31,7 +36,7 @@ class SpellUsable extends CoreSpellUsable {
     }
   }
 
-  beginCooldown(spellId, cooldownTriggerEvent) {
+  beginCooldown(spellId: number, cooldownTriggerEvent: Event<any>) {
     if (spellId === SPELLS.SHIELD_SLAM.id) {
       if (this.isOnCooldown(spellId)) {
         this.endCooldown(spellId, undefined, this.lastPotentialTriggerForShieldSlam ? this.lastPotentialTriggerForShieldSlam.timestamp : undefined);
