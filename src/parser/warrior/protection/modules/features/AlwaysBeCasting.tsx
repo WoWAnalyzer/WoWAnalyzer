e79e546795caab2ed96a942cd445e5fa0e390657
Dambroda <43309639+Dambroda@users.noreply.github.com>
2020-09-28 10:58:51 -0700

    Convert ABC to TS

--- src/parser/warrior/protection/modules/features/AlwaysBeCasting.tsx
+++ src/parser/warrior/protection/modules/features/AlwaysBeCasting.tsx
@@ -3,15 +3,15 @@ import { formatPercentage } from 'common/format';
 import CoreAlwaysBeCasting from 'parser/shared/modules/AlwaysBeCasting';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
-  suggestions(when) {
+  suggestions(when: any) {
     const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
 
     when(deadTimePercentage).isGreaterThan(0.2)
-      .addSuggestion((suggest, actual, recommended) => {
+      .addSuggestion((suggest: any, actual: number, recommended: number) => {
         return suggest(<span> Your downtime can be improved. Try to Always Be Casting (ABC)..</span>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`${Math.round(formatPercentage(recommended))}% is recommended`)
+          .recommended(`${Math.round(Number(formatPercentage(recommended)))}% is recommended`)
           .regular(recommended + 0.05).major(recommended + 0.15);
       });
   }
