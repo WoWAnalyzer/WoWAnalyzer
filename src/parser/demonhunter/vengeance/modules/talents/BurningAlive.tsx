8858de50b9ee35328bad42fe3a76a90bb8181463
Dambroda <43309639+Dambroda@users.noreply.github.com>
2020-09-29 20:54:32 -0700

    Convert BurningAlive to tS

--- src/parser/demonhunter/vengeance/modules/talents/BurningAlive.tsx
+++ src/parser/demonhunter/vengeance/modules/talents/BurningAlive.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import Analyzer from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import SPELLS from 'common/SPELLS/index';
 import { formatNumber } from 'common/format';
 import TalentStatisticBox from 'interface/others/TalentStatisticBox';
 import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
+import Events, { DamageEvent } from 'parser/core/Events';
 
 //WCL: https://www.warcraftlogs.com/reports/JxyY7HCDcjqMA9tf/#fight=1&source=15
 class BurningAlive extends Analyzer {
 
   damage = 0;
 
-  constructor(...args) {
-    super(...args);
+  constructor(options: any) {
+    super(options);
     this.active = this.selectedCombatant.hasTalent(SPELLS.BURNING_ALIVE_TALENT.id);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.FIERY_BRAND_DOT), this.onSpreadDamage);
   }
 
-  on_byPlayer_damage(event) {
-    const spellID = event.ability.guid;
-    if (spellID !== SPELLS.FIERY_BRAND_DOT.id) {
-      return;
-    }
+  onSpreadDamage(event: DamageEvent) {
     this.damage += event.amount;
   }
-
-
 
   statistic() {
     return (
       <TalentStatisticBox
+        icon={undefined} // TODO deprecated; replace with Statistic
+        label={undefined}
         talent={SPELLS.BURNING_ALIVE_TALENT.id}
         position={STATISTIC_ORDER.CORE(10)}
         value={this.owner.formatItemDamageDone(this.damage)}
