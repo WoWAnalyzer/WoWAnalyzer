1474bfb50c35ce18f1d2360c1f90078deba611f3
alliepet <72270749+alliepet@users.noreply.github.com>
2020-10-02 12:12:16 -0500

    Update CHANGELOG.js

--- src/parser/monk/mistweaver/CHANGELOG.js
+++ src/parser/monk/mistweaver/CHANGELOG.js
@@ -6,7 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2020, 10, 1), <>Corrected Nourshing Chi to track all hots. </>, Moonrabbit),
+  change(date(2020, 10, 2), <>Corrected Nourshing Chi to track all hots. </>, Moonrabbit),
   change(date(2020, 10, 1), <>Updated spell data so stat weights will be accurate. </>, Abelito75),
   change(date(2020, 10, 1), <>Added Statistics for Tear of Morning and Ancient Teachings of the Monastery. </>, Abelito75),
   change(date(2020, 9, 30), <>Integration Tests have been added for Mistweaver. </>, Anomoly),
