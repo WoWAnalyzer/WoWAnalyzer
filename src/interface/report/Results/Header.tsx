59b6e42cb4c78da7ed32549117e4fe04bcb365bc
Martijn Hols <git@martijnhols.nl>
2020-10-04 23:06:50 +0200

    Refactor Header

---
+++ src/interface/report/Results/Header.tsx
@@ -0,0 +1,139 @@
+import React, { ComponentType, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Phase } from 'raids';
+import Fight from 'parser/core/Fight';
+import DEFAULT_BUILD from 'parser/DEFAULT_BUILD';
+import Config, { Build } from 'parser/Config';
+import { TooltipElement } from 'common/Tooltip';
+import { getLabel as getDifficultyLabel } from 'game/DIFFICULTIES';
+import getBossName from 'common/getBossName';
+
+import PhaseSelector from './PhaseSelector';
+import TimeFilter from './TimeFilter';
+import HeaderBackground from './HeaderBackground';
+import NavigationBar from './NavigationBar';
+
+import './Header.scss';
+
+interface Props {
+  config: Config;
+  name: string;
+  characterProfile: {
+    region: string;
+    thumbnail: string;
+  };
+  boss: {
+    name: string;
+    headshot: string;
+    background: string;
+    backgroundPosition: string;
+  };
+  handlePhaseSelection: () => void;
+  applyFilter: () => void;
+  phases?: {
+    [key: string]: Phase;
+  };
+  makeBuildUrl: (selectedTab: string, buildName: string) => string;
+  build: string;
+  selectedPhase: string;
+  selectedInstance: number;
+  isLoading: boolean;
+  fight: Fight;
+  makeTabUrl: (url: string) => string;
+  selectedTab: string;
+  tabs: Array<{
+    title: ReactNode;
+    icon?: ComponentType;
+    url: string;
+    order?: number;
+  }>;
+}
+
+const Header = ({
+  config: { spec, builds },
+  build,
+  name,
+  fight,
+  boss,
+  handlePhaseSelection,
+  selectedPhase,
+  selectedInstance,
+  phases,
+  isLoading,
+  applyFilter,
+  characterProfile,
+  makeTabUrl,
+  tabs,
+  selectedTab,
+  makeBuildUrl,
+}: Props) => {
+  let playerThumbnail;
+  if (characterProfile?.thumbnail) {
+    playerThumbnail = `https://render-${characterProfile.region}.worldofwarcraft.com/character/${characterProfile.thumbnail}`;
+  } else {
+    playerThumbnail = `/specs/${spec.className}-${spec.specName}.jpg`.replace(/ /, '');
+  }
+
+  const renderBuild = (build: Build, active: boolean) => {
+    return (
+      <Link to={makeBuildUrl(selectedTab, build.url)}>
+        <span className={'build ' + (active ? 'active' : '')}>
+          <TooltipElement content={build.name}>{build.icon}</TooltipElement>
+        </span>
+      </Link>
+    );
+  };
+
+  return (
+    <header>
+      <HeaderBackground boss={boss} />
+
+      <div className="info container">
+        <div className="boss">
+          <h2>{getDifficultyLabel(fight.difficulty)}</h2>
+          <h1>{boss ? boss.name : getBossName(fight, false)}</h1>
+          <h2>
+            {phases && (
+              <PhaseSelector
+                fight={fight}
+                phases={phases}
+                handlePhaseSelection={handlePhaseSelection}
+                selectedPhase={selectedPhase}
+                selectedInstance={selectedInstance}
+                isLoading={isLoading}
+              />
+            )}
+          </h2>
+          <div className="timefilter">
+            <TimeFilter fight={fight} isLoading={isLoading} applyFilter={applyFilter} />
+          </div>
+        </div>
+        <div className="player">
+          <div className="avatar">
+            <img src={playerThumbnail} alt="" />
+          </div>
+          <div className="details">
+            <h2 className="builds">
+              {builds && (
+                <>
+                  Build:
+                  {Object.keys(builds).map(b => renderBuild(builds[b], build === builds[b].url))}
+                  {renderBuild(DEFAULT_BUILD, !build)}
+                </>
+              )}
+            </h2>
+            <h2>
+              {spec.specName} {spec.className}
+            </h2>
+            <h1 className="name">{name}</h1>
+          </div>
+        </div>
+      </div>
+
+      <NavigationBar makeTabUrl={makeTabUrl} tabs={tabs} selectedTab={selectedTab} />
+    </header>
+  );
+};
+
+export default Header;
--- src/parser/DEFAULT_BUILD.tsx
+++ src/parser/DEFAULT_BUILD.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ArmoryIcon from 'interface/icons/Armory';
+import { Build } from 'parser/Config';
 
 export default {
   name: 'Standard Build',
   url: 'standard',
   icon: <ArmoryIcon />,
-};
+} as Build;
