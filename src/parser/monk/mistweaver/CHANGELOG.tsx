936df9f7df7fd15f40cf893ae7fe4ce05bc2cbf9
anomoly <jkippley@gmail.com>
2020-10-06 19:39:04 -0400

    EF and SPK Updates to use event listeners

--- src/parser/monk/mistweaver/CHANGELOG.tsx
+++ src/parser/monk/mistweaver/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import { change, date } from 'common/changelog';
 
 
 export default [
+  change(date(2020, 10, 6), <>Updated <SpellLink id={SPELLS.SPINNING_CRANE_KICK.id} /> and <SpellLink id={SPELLS.ESSENCE_FONT.id} /> modules to use Event Listeners.</>, [Anomoly]),
   change(date(2020, 10, 6), <>Updated <SpellLink id={SPELLS.RENEWING_MIST.id} /> to use Event Listeners.</>, [Anomoly]),
   change(date(2020, 10, 6), <>Updated core spec module files to TypeScript.</>, [Anomoly]),
   change(date(2020, 10, 6), <>Moved <SpellLink id={SPELLS.INVOKE_CHIJI_THE_RED_CRANE_TALENT.id} /> statistic box to the correct location.</>, [Moonrabbit]),
