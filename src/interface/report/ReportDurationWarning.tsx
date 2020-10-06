63f78b470d0d8cdacc856f1d56f613eef948a03b
William Schutte <williamdschutte@gmail.com>
2020-10-05 20:21:41 -0400

    refactor: ReportDurationWarning class component refactored to functional component

--- src/interface/report/ReportDurationWarning.tsx
+++ src/interface/report/ReportDurationWarning.tsx
@@ -11,26 +11,24 @@ interface Props {
 const DAYS_IN_MS = 86400000;
 export const MAX_REPORT_DURATION = DAYS_IN_MS * 7;
 
-class ReportDurationWarning extends React.PureComponent<Props> {
+const ReportDurationWarning = ({ duration }: Props) => {
 
-  get durationInDays() {
-    return this.props.duration / DAYS_IN_MS;
-  }
+  const durationInDays = () => {
+    return duration / DAYS_IN_MS;
+  };
 
-  render() {
-    return (
+  return (
       <div className="container">
         <Warning style={{ marginBottom: 30 }}>
           <h2><Trans>Report exceeds supported duration</Trans></h2>
           <Trans>
-            The current report contains data collected over <strong>{formatNumber(this.durationInDays)} {this.durationInDays > 1 ? 'days' : 'day'}</strong>.
+            The current report contains data collected over <strong>{formatNumber(durationInDays())} {durationInDays() > 1 ? 'days' : 'day'}</strong>.
             This could lead to parsing issues with fights later in the report.
             We recommend that you split your logs before uploading them to warcraftlogs.com.
           </Trans>
         </Warning>
       </div>
-    );
-  }
-}
+  );
+};
 
 export default ReportDurationWarning;
