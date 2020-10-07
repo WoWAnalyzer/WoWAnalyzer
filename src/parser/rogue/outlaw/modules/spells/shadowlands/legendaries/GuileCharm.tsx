46e1894e3eac23268ad6286fdda5d854b0f7f927
darthelit <dvincent25@gmail.com>
2020-10-07 09:12:24 -0500

    Update layout for Guile Charm stat

--- src/parser/rogue/outlaw/modules/spells/shadowlands/legendaries/GuileCharm.tsx
+++ src/parser/rogue/outlaw/modules/spells/shadowlands/legendaries/GuileCharm.tsx
@@ -4,8 +4,8 @@ import Abilities from 'parser/core/modules/Abilities';
 import SPELLS from 'common/SPELLS';
 import Statistic from 'interface/statistics/Statistic';
 import { formatPercentage } from 'common/format';
-import BoringSpellValue from 'interface/statistics/components/BoringSpellValue';
-import SpellLink from 'common/SpellLink';
+import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
+import SpellIcon from 'common/SpellIcon';
 
 class GuileCharm extends Analyzer {
   static dependencies = {
@@ -37,12 +37,11 @@ class GuileCharm extends Analyzer {
     return (
       <>
         <Statistic size="flexible" tooltip="This measures how long each buff from this legendary is active.">
-          <div className="pad">
-            <label><SpellLink id={SPELLS.GUILE_CHARM.id} /> Insight Uptime</label>
-          </div>
-          <BoringSpellValue spell={SPELLS.SHALLOW_INSIGHT_BUFF} label={''} value={`${formatPercentage(this.percentUptime.shallowInsight)}%`} />
-          <BoringSpellValue spell={SPELLS.MODERATE_INSIGHT_BUFF} label={''} value={`${formatPercentage(this.percentUptime.moderateInsight)}%`} />
-          <BoringSpellValue spell={SPELLS.DEEP_INSIGHT_BUFF} label={''} value={`${formatPercentage(this.percentUptime.deepInsight)}%`} />
+          <BoringSpellValueText spell={SPELLS.GUILE_CHARM}>
+            <SpellIcon id={SPELLS.SHALLOW_INSIGHT_BUFF.id} /> {formatPercentage(this.percentUptime.shallowInsight)}% <small>Shallow Insight uptime</small><br />
+            <SpellIcon id={SPELLS.MODERATE_INSIGHT_BUFF.id} /> {formatPercentage(this.percentUptime.moderateInsight)}% <small>Moderate Insight uptime</small><br />
+            <SpellIcon id={SPELLS.DEEP_INSIGHT_BUFF.id} /> {formatPercentage(this.percentUptime.deepInsight)}% <small>Deep Insight uptime</small>
+          </BoringSpellValueText>
         </Statistic>
       </>
     );
