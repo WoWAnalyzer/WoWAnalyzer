e2ed77913f4dac7d1a0a5699e028c079ea7a6cdf
jafowler <jacobtfowler@gmail.com>
2020-10-07 12:41:14 -0400

    Revert "adding contributor information" as its not needed in class specific stuff
    This reverts commit db61b48c5f596c899ca96fe0975b1472da5072f6.

--- src/parser/deathknight/frost/CHANGELOG.tsx
+++ src/parser/deathknight/frost/CHANGELOG.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { Khazak, JacobFowler } from 'CONTRIBUTORS';
+import { Khazak } from 'CONTRIBUTORS';
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
-    change(date(2020, 10, 7), <>Updating localization for UI strings</>, [JacobFowler]),
     change(date(2020, 8, 11), 'Converted Frost Death Knight modules to Typescript', [Khazak]),
     change(date(2020, 5, 11), <>Fixed issue where <SpellLink id={SPELLS.ICECAP_TALENT.id} /> was not properly counting the internal cooldown</>, [Khazak]),
     change(date(2020, 4, 28), <>Added <SpellLink id={SPELLS.CHILL_STREAK_TALENT.id} /> to Abilities</>, [Khazak]),
