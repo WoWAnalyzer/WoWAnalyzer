1e650efb6af6311e1cdc5ffae6b3b96f1f1bbe69
Sharrq <cjgrasty86@gmail.com>
2020-10-07 21:39:01 -0500

    changelogs

--- src/parser/deathknight/unholy/CHANGELOG.js
+++ src/parser/deathknight/unholy/CHANGELOG.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-import { Khazak } from 'CONTRIBUTORS';
+import { Khazak, Sharrq } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 10, 13), 'Removed BFA Azerite Traits.', [Sharrq]),
   change(date(2020, 8, 4), 'Behind the scenes clean up to prep for better support in Shadowlands', [Khazak]),
   change(date(2018, 8, 30), 'Removed Legion modules and updated basic analysis for BFA', [Khazak]),
 ];
