130926f8fa2daf9906cddde09786d0a1fb145a9f
vohrr <matthew.d.pingel@gmail.com>
2020-10-08 23:43:00 -0400

    refactor based on code review
    updated to use eventlisteners for damage events for stack management. added eventlisteners to account for essence font during kyrian chiji

--- src/parser/monk/mistweaver/modules/talents/InvokeChiJi.js
+++ src/parser/monk/mistweaver/modules/talents/InvokeChiJi.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SPELLS from 'common/SPELLS';
 import {Trans} from '@lingui/macro';
-import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER, SELECTED_PLAYER_PET } from 'parser/core/Analyzer';
 import { formatNumber } from 'common/format';
 import Events from 'parser/core/Events';
 import Statistic from 'interface/statistics/Statistic';
@@ -9,6 +9,7 @@ import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
 import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
 import ItemHealingDone from 'interface/ItemHealingDone';
 import SpellLink from 'common/SpellLink';
+import StatTracker from 'parser/shared/modules/StatTracker';
 
 /** 
  * Blackout Kick, Totm BoKs, Rising Sun Kick and Spinning Crane Kick generate stacks of Invoke Chi-Ji, the Red Crane, which reduce the cast time and mana 
@@ -17,6 +18,7 @@ import SpellLink from 'common/SpellLink';
  * Casting Enveloping Mist while Chiji is active applies Enveloping Breath on up to 6 nearby allies within 10 yards.
 */
 class InvokeChiJi extends Analyzer {
+  static dependencies = {statTracker: StatTracker};
   chijiActive = false;
   //healing breakdown vars
   gustHealing = 0;
@@ -32,6 +34,10 @@ class InvokeChiJi extends Analyzer {
   chijiGlobals = 0;
   chijiUses = 0;
   lastGlobal = 0;
+  efChannelStart = 0;
+  efChannelEnd = 0;
+  
+  envCount = 0;
   
   constructor(...args) {
     super(...args);
@@ -39,11 +45,15 @@ class InvokeChiJi extends Analyzer {
     if (!this.active) {return;}
     this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.GUST_OF_MISTS_CHIJI), this.handleGust);
     this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.ENVELOPING_BREATH), this.handleEnvelopingBreath);
-    this.addEventListener(Events.applybuffstack.by(SELECTED_PLAYER).spell(SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF), this.handleApplyChijiStack);
-    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF), this.handleApplyChijiStack);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.ENVELOPING_MIST), this.handleEnvelopCast);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.INVOKE_CHIJI_THE_RED_CRANE_TALENT), this.handleChijiStart);
+    this.addEventListener(Events.death.to(SELECTED_PLAYER_PET), this.handleChijiDeath);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell([SPELLS.BLACKOUT_KICK, SPELLS.RISING_SUN_KICK_SECOND, SPELLS.BLACKOUT_KICK_TOTM]), this.handleOvercapStacks)
+    //need a different eventlistener beacause chiji currently only applies 1 stack per cast of sck, not on each dmg event
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.SPINNING_CRANE_KICK), this.handleOvercapStacks);
     this.addEventListener(Events.GlobalCooldown.by(SELECTED_PLAYER), this.handleGlobal);
+    this.addEventListener(Events.BeginChannel.by(SELECTED_PLAYER).spell(SPELLS.ESSENCE_FONT), this.handleEssenceFontStart);
+    this.addEventListener(Events.EndChannel.by(SELECTED_PLAYER).spell(SPELLS.ESSENCE_FONT), this.handleEssenceFontEnd);
   }
 
   //missed gcd mangement
@@ -53,23 +63,43 @@ class InvokeChiJi extends Analyzer {
     this.chijiGlobals += 1;
     this.chijiUses +=1;
   }
+  
+  handleChijiDeath(event) {
+    this.chijiActive = false;
+  }
 
   handleGlobal(event){
-    //cant get eventlistener to find chiji's death so we will check against duration here
-    if(event.timestamp - this.chijiStart > SPELLS.INVOKE_CHIJI_THE_RED_CRANE_TALENT.duration) {
-      this.chijiActive = false;
-    }
     if(this.chijiActive) {
       this.chijiGlobals += 1;
       //if timebetween globals is longer than the gcd add the difference to the missed gcd tally
-      //we dont care about accounting for channels/casts longer than the gcd since you shouldnt be using them during chiji anyway
-      if((event.timestamp - this.lastGlobal) > event.duration) {
+      //we only care about accounting for channels of essence font during WoO, other than that it should be the gcd during chiji
+      if(event.ability.guid === SPELLS.ESSENCE_FONT.id && this.selectedCombatant.hasBuff(SPELLS.WEAPONS_OF_ORDER_BUFF_AND_HEAL.id)) {
+        return;
+      } else if((event.timestamp - this.lastGlobal) > event.duration) {
         this.missedGlobals += (event.timestamp - this.lastGlobal - event.duration)/event.duration;
       }
       this.lastGlobal = event.timestamp;
     }
   }
 
+  handleEssenceFontStart(event) { 
+    if(this.selectedCombatant.hasBuff(SPELLS.WEAPONS_OF_ORDER_BUFF_AND_HEAL.id)) { 
+       this.efChannelStart = this.lastGlobal = event.timestamp;
+    }
+  }
+
+  handleEssenceFontEnd(event) {
+    const gcd = 1500 / (1 + this.statTracker.hastePercentage(this.statTracker.currentHasteRating));
+    if(this.selectedCombatant.hasBuff(SPELLS.WEAPONS_OF_ORDER_BUFF_AND_HEAL.id)) { 
+      this.efChannelEnd = event.timestamp;
+      let duration = this.efChannelEnd - this.efChannelStart;
+      if(duration > gcd) { 
+        this.lastGlobal =  this.efChannelEnd - gcd;
+      }
+   }
+  }
+
+  //healing management
   handleGust(event) {
     this.gustHealing += (event.amount || 0) + (event.absorbed || 0);
   }
@@ -79,15 +109,17 @@ class InvokeChiJi extends Analyzer {
   }
   
   //stackbreakown management
-  handleApplyChijiStack() {
-      if (this.chijiStackCount === SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF.maxStacks) {
-          this.wastedStacks += 1;
+  handleOvercapStacks(event) {
+    if(this.chijiActive) {
+      if(this.chijiStackCount === SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF.maxStacks) {
+        this.wastedStacks += 1;
       } else {
-          this.chijiStackCount += 1;
+        this.chijiStackCount += 1;
+      }
     }
   }
 
-  handleEnvelopCast(event) {
+  handleEnvelopCast() {
     //in some cases the last envelop is cast after chiji has expired but the buff can still be consumed
       if(this.chijiActive || this.selectedCombatant.hasBuff(SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF.id)) {
         if (this.chijiStackCount === SPELLS.INVOKE_CHIJI_THE_RED_CRANE_BUFF.maxStacks) {
