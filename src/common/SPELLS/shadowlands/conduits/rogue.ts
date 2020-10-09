91f6ccdf62fe150bedc2a0538025cad71cbbb44d
darthelit <dvincent25@gmail.com>
2020-10-08 20:26:56 -0500

    readd conduits and covenants

--- src/common/SPELLS/shadowlands/conduits/rogue.ts
+++ src/common/SPELLS/shadowlands/conduits/rogue.ts
@@ -2,7 +2,11 @@ import { SpellList } from "common/SPELLS/Spell";
 
 const conduits: SpellList = {
   //region Kyrian
-
+  BRONS_CALL_TO_ACTION: {
+    id: 333961,
+    name: 'Bron\'s Call to Action',
+    icon: 'ui_sigil_kyrian',
+  },
   //endregion
 
   //region Necrolord
@@ -15,6 +19,19 @@ const conduits: SpellList = {
 
   //region Venthyr
 
+  //endregion
+
+  //region shared
+  DEEPER_DAGGERS: {
+    id: 341549,
+    name: 'Deeper Daggers',
+    icon: 'ability_rogue_focusedattacks',
+  },
+  DEEPER_DAGGERS_BUFF: {
+    id: 341550,
+    name: 'Deeper Daggers',
+    icon: 'ability_rogue_focusedattacks',
+  },
   //endregion
 };
 export default conduits;
