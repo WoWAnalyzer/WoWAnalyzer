0682e6b1251ec97577dda9afc5b12401d14e9bf4
David Grölle <david.groelle@protonmail.com>
2020-10-11 21:35:50 +0200

    Add changelog

--- src/parser/shaman/elemental/CHANGELOG.tsx
+++ src/parser/shaman/elemental/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 10, 11), <>Convert to Typescript</>, HawkCorrigan),
   change(date(2020, 8, 28), <>First go at removing obsolete Spells and Azerite.</>, [HawkCorrigan]),
   change(date(2020, 3, 17), <>Adjust wording of downtime suggestion. Cancelled casts don't consume a gcd.</>, [Draenal]),
   change(date(2020, 2, 28), <>Added a statistic for <SpellLink id={SPELLS.ANCESTRAL_RESONANCE.id} />.</>, [niseko]),
