ae3629c60f3726a55a166a86d0ca33629f551abe
Abel Melchiore <abelgm@charter.net>
2020-10-12 03:46:35 -0400

    Niseko taught me something today

--- src/parser/monk/mistweaver/modules/features/MistweaverHealingEfficiencyTracker.tsx
+++ src/parser/monk/mistweaver/modules/features/MistweaverHealingEfficiencyTracker.tsx
@@ -54,7 +54,7 @@ class MistweaverHealingEfficiencyTracker extends HealingEfficiencyTracker {
   protected refreshingJadeWind!: RefreshingJadeWind;
   protected expelHarm!: ExpelHarm;
 
-  getCustomSpellStats(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}, spellId: number) {
+  getCustomSpellStats(spellInfo: SpellInfoDetails, spellId: number) {
     if (spellId === SPELLS.ESSENCE_FONT.id) {
       spellInfo = this.getEssenceFontDetails(spellInfo);
     } else if (spellId === SPELLS.ENVELOPING_MIST.id) { //maybe consider adding tft buffed version's spell id too
@@ -80,14 +80,14 @@ class MistweaverHealingEfficiencyTracker extends HealingEfficiencyTracker {
     return spellInfo;
   }
 
-  getSoothingMistDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getSoothingMistDetails(spellInfo: SpellInfoDetails) {
     // the default tracker gets the healing of the soothing mists, but only the mana for the first cast. Every tick costs mana.
     spellInfo.manaSpent = this.soothingMist.soomTicks * SPELLS.SOOTHING_MIST.manaCost;
     spellInfo.healingDone = spellInfo.healingDone + this.soothingMist.gustsHealing;
     return spellInfo;
   }
 
-  getEnvelopingMistsDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getEnvelopingMistsDetails(spellInfo: SpellInfoDetails) {
     spellInfo.healingDone = spellInfo.healingDone + this.envelopingMists.gustsHealing + this.envelopingMists.healingIncrease;
     // Enveloping breath part
     spellInfo.healingDone += this.healingDone.byAbility(SPELLS.ENVELOPING_BREATH.id).effective;
@@ -95,14 +95,14 @@ class MistweaverHealingEfficiencyTracker extends HealingEfficiencyTracker {
     return spellInfo;
   }
 
-  getEssenceFontDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getEssenceFontDetails(spellInfo: SpellInfoDetails) {
     spellInfo.healingDone = this.essenceFont.totalHealing + this.essenceFontMastery.healing + this.essenceFont.totalAbsorbs;
     spellInfo.overhealingDone = this.essenceFont.totalOverhealing;
     spellInfo.healingHits = this.essenceFont.targetsEF;
     return spellInfo;
   }
 
-  getRenewingMistDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getRenewingMistDetails(spellInfo: SpellInfoDetails) {
     // Vivify splashes due to ReM should be attributed to ReM's value, because without casting ReM, you wouldn't get the splash.
     spellInfo.healingDone = this.renewingMist.totalHealing + this.vivify.remVivifyHealing + this.renewingMist.gustsHealing + this.renewingMist.totalAbsorbs;
     spellInfo.overhealingDone = this.renewingMist.totalOverhealing;
@@ -110,38 +110,38 @@ class MistweaverHealingEfficiencyTracker extends HealingEfficiencyTracker {
     return spellInfo;
   }
 
-  getVivifyDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getVivifyDetails(spellInfo: SpellInfoDetails) {
     // As described in the ReM section, the ReM Vivify splashes need to be removed from the healing done.
     spellInfo.healingDone = spellInfo.healingDone + this.vivify.gustsHealing - this.vivify.remVivifyHealing;
     return spellInfo;
   }
 
-  getRefreshingJadeWindDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getRefreshingJadeWindDetails(spellInfo: SpellInfoDetails) {
     spellInfo.healingDone = this.refreshingJadeWind.healingRJW;
     spellInfo.overhealingDone = this.refreshingJadeWind.overhealingRJW;
     return spellInfo;
   }
 
-  getRisingSunKickDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getRisingSunKickDetails(spellInfo: SpellInfoDetails) {
     // Since I don't want messy code right now it will only give the rising mist healing not any of the other fun stuff it gives indirectly
     spellInfo.healingDone = this.healingDone.byAbility(SPELLS.RISING_MIST_HEAL.id).effective;
     spellInfo.overhealingDone = this.healingDone.byAbility(SPELLS.RISING_MIST_HEAL.id).overheal;
     return spellInfo;
   }
 
-  getYulonDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getYulonDetails(spellInfo: SpellInfoDetails) {
     // Get all soothing breath and enveloping breath healing since its all bc of yu'lon
     spellInfo.healingDone = this.healingDone.byAbility(SPELLS.SOOTHING_BREATH.id).effective;
     spellInfo.overhealingDone = this.healingDone.byAbility(SPELLS.SOOTHING_BREATH.id).overheal;
     return spellInfo;
   }
 
-  getChijiDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getChijiDetails(spellInfo: SpellInfoDetails) {
     spellInfo.healingDone = this.healingDone.byAbility(SPELLS.GUST_OF_MISTS_CHIJI.id).effective;
     spellInfo.overhealingDone = this.healingDone.byAbility(SPELLS.GUST_OF_MISTS_CHIJI.id).overheal;
     return spellInfo;
   }
-  getExpelHarmDetails(spellInfo: {spell: Spell; casts: number; healingHits: number; healingDone: number; overhealingDone: number; damageHits: number; damageDone: number; damageAbsorbed: number; manaSpent: number; manaGained: ManaTracker;}) {
+  getExpelHarmDetails(spellInfo: SpellInfoDetails) {
     spellInfo.healingDone = spellInfo.healingDone + this.expelHarm.gustsHealing + this.expelHarm.selfHealing + this.expelHarm.targetHealing;
     spellInfo.overhealingDone = spellInfo.overhealingDone + this.expelHarm.selfOverheal + this.expelHarm.targetOverheal;
     return spellInfo;
@@ -150,3 +150,16 @@ class MistweaverHealingEfficiencyTracker extends HealingEfficiencyTracker {
 }
 
 export default MistweaverHealingEfficiencyTracker;
+
+interface SpellInfoDetails {
+  spell: Spell;
+  casts: number;
+  healingHits: number;
+  healingDone: number;
+  overhealingDone: number;
+  damageHits: number;
+  damageDone: number;
+  damageAbsorbed: number;
+  manaSpent: number;
+  manaGained: ManaTracker;
+}
