6471374da3bd68c6a271fa613c6d4b2e28545b38
Sharrq <cjgrasty86@gmail.com>
2020-10-16 17:47:09 -0500

    [Core] Warning for Classic Logs
    Added a warning if you are loading a classic log.

--- src/interface/report/FightSelectionPanel.js
+++ src/interface/report/FightSelectionPanel.js
@@ -8,6 +8,8 @@ import FightSelectionPanelList from './FightSelectionPanelList';
 
 const FightSelectionPanel = props => {
   const { report, killsOnly } = props;
+  console.log(report)
+  console.log(report.gameVersion)
 
   return (
     <>
