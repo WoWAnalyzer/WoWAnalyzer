0297289a12c08fbdec44fa9d75faa408d715cdad
Abel Melchiore <abelgm@charter.net>
2020-10-16 20:44:09 -0400

    oops one icon broke a bit ago

--- src/parser/monk/mistweaver/CHANGELOG.tsx
+++ src/parser/monk/mistweaver/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import { change, date } from 'common/changelog';
 
 
 export default [
+  change(date(2020, 10, 14), <>Corrected upwelling's icon. </>, Abelito75),
   change(date(2020, 10, 14), <>Updating Yu'lon's tooltip! </>, Abelito75),
   change(date(2020, 10, 14), <>We're updated for 9.0.1! </>, Abelito75),
   change(date(2020, 10, 12), <>Updated <SpellLink id={SPELLS.ENVELOPING_MIST.id} /> and <SpellLink id={SPELLS.SOOTHING_MIST.id} /> modules to use Event Listeners.</>, [Anomoly]),
