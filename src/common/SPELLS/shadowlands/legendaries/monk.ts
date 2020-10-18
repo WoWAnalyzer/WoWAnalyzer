5c74175131cabe565524bd9d4db40c64078252f1
Juko8 <ulrichenevoldsen@hotmail.com>
2020-10-18 15:07:59 +0200

    Add Last Emperor's Capacitor

--- src/common/SPELLS/shadowlands/legendaries/monk.ts
+++ src/common/SPELLS/shadowlands/legendaries/monk.ts
@@ -1,6 +1,6 @@
-import { SpellList } from "common/SPELLS/Spell";
+import { SpellList, LegendarySpell } from "common/SPELLS/Spell";
 
-const legendaries: SpellList = {
+const legendaries: SpellList<LegendarySpell> = {
   //region Brewmaster
 
   //endregion
@@ -31,10 +31,55 @@ const legendaries: SpellList = {
   //endregion
 
   //region Windwalker
+  XUENS_BATTLEGEAR: {
+    id: 337481,
+    name: 'Xuen\'s Battlegear',
+    icon: 'monk_stance_whitetiger',
+    bonusID: 7070,
+  },
+  KEEFERS_SKYREACH: {
+    id: 337334,
+    name: 'Keefer\'s Skyreach',
+    icon: 'inv__fistofthewhitetiger',
+    bonusID: 7068,
+  },
+  LAST_EMPERORS_CAPACITOR: {
+    id: 337292,
+    name: 'Last Emperor\'s Capacitor',
+    icon: 'ability_warrior_unrelentingassault',
+    bonusID: 7069,
+  },
+  LAST_EMPERORS_CAPACITOR_BUFF: {
+    id: 337291,
+    name: 'The Emperor\'s Capcitor',
+    icon: 'ability_monk_cracklingjadelightning',
+  },
+  JADE_IGNITION: {
+    id: 337483,
+    name: 'Jade Ignition',
+    icon: 'ability_monk_chiexplosion',
+    bonusID: 7071,
+  },
+  JADE_IGNITION_BUFF: {
+    id: 337571,
+    name: 'Chi Energy',
+    icon: 'ability_monk_chiexplosion',
+  },
+  JADE_IGNITION_DAMAGE: {
+    id: 337342,
+    name: 'Chi Explosion',
+    icon: 'ability_monk_chiexplosion',
+  },
 
   //endregion
 
   //region Shared
+  FATAL_TOUCH: {
+    id: 337296,
+    name: 'Fatal Touch',
+    icon: 'ability_monk_touchofdeath',
+    bonusID: 7081,
+  },
 
   //endregion
 };
