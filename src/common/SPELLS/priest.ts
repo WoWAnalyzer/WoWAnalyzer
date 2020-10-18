9c02230c90a3102b434e9b2ff59aaa3a062c65d6
Ryan Park <admin@ryanpark.co.uk>
2020-10-18 19:40:59 +0100

    Delete Light's Wrath sadge

--- src/common/SPELLS/priest.ts
+++ src/common/SPELLS/priest.ts
@@ -137,11 +137,6 @@ const spells: SpellList<PriestSpell> = {
     icon: 'spell_holy_layonhands',
     manaCost: 450,
   },
-  LIGHTS_WRATH: {
-    id: 207946,
-    name: "Light's Wrath",
-    icon: 'inv_staff_2h_artifacttome_d_01',
-  },
   MIND_CONTROL: {
     id: 605,
     name: 'Mind Control',
