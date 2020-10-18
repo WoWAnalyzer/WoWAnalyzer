68e02661aa1a66a30f9c0c8d077a5557d38887c4
Ryan Park <admin@ryanpark.co.uk>
2020-10-18 19:39:21 +0100

    Convert donut chart to tsx

--- src/interface/statistics/components/DonutChart/index.tsx
+++ src/interface/statistics/components/DonutChart/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import BaseChart from 'interface/others/BaseChart';
 
@@ -7,46 +7,62 @@ import SpellLink from 'common/SpellLink';
 import { TooltipElement } from 'common/Tooltip';
 
 import './style.scss';
+import { VisualizationSpec } from 'react-vega';
 
-class DonutChart extends React.PureComponent {
+export type DonutChartItem = {
+  label: string | ReactNode;
+  spellId?: number;
+  value: number;
+  color: string;
+  valueTooltip?: ReactNode;
+  tooltip?: ReactNode;
+};
+
+type DonutChartProps = {
+  items: DonutChartItem[];
+  /**
+   * Strongly recommend not changing this to maintain consistency
+   */
+  chartSize?: number;
+  innerRadiusFactor?: number;
+};
+
+class DonutChart extends React.PureComponent<DonutChartProps> {
   static propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
-      value: PropTypes.number.isRequired,
-      label: PropTypes.node.isRequired,
-      color: PropTypes.string.isRequired,
-      tooltip: PropTypes.node,
-      spellId: PropTypes.number,
-      valueTooltip: PropTypes.node,
-    })).isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.number.isRequired,
+        label: PropTypes.node.isRequired,
+        color: PropTypes.string.isRequired,
+        tooltip: PropTypes.node,
+        spellId: PropTypes.number,
+        valueTooltip: PropTypes.node,
+      }),
+    ).isRequired,
     // While you could change the chart size, I strongly recommend you do not for consistency and to avoid breaking whenever this component is modified. Do you really need to adjust the size?
     chartSize: PropTypes.number,
     innerRadiusFactor: PropTypes.number,
   };
+
   static defaultProps = {
     chartSize: 90,
     innerRadiusFactor: 0.28,
   };
 
-  renderLegend(items) {
+  renderLegend(items: DonutChartItem[]) {
     const total = items.reduce((sum, item) => sum + item.value, 0);
 
     return (
       <div className="legend">
         {items.map(({ color, label, tooltip, value, spellId, valueTooltip }, index) => {
-          label = tooltip ? (
-            <TooltipElement content={tooltip}>{label}</TooltipElement>
-          ) : label;
-          label = spellId ? (
-            <SpellLink id={spellId}>{label}</SpellLink>
-          ) : label;
+          label = tooltip ? <TooltipElement content={tooltip}>{label}</TooltipElement> : label;
+          label = spellId ? <SpellLink id={spellId}>{label}</SpellLink> : label;
           return (
             <div key={index} className="flex">
               <div className="flex-sub">
                 <div className="circle" style={{ background: color }} />
               </div>
-              <div className="flex-main">
-                {label}
-              </div>
+              <div className="flex-main">{label}</div>
               <div className="flex-sub">
                 <TooltipElement content={valueTooltip ? valueTooltip : value}>
                   {formatPercentage(value / total, 0)}%
@@ -58,13 +74,13 @@ class DonutChart extends React.PureComponent {
       </div>
     );
   }
-  renderChart(items, chartSize, innerRadiusFactor) {
+  renderChart(items: DonutChartItem[], chartSize: number, innerRadiusFactor: number) {
     const innerRadius = chartSize * innerRadiusFactor;
 
     const data = {
       items,
     };
-    const spec = {
+    const spec: VisualizationSpec = {
       data: {
         name: 'items',
       },
@@ -82,8 +98,8 @@ class DonutChart extends React.PureComponent {
           type: 'nominal',
           legend: null,
           scale: {
-            domain: items.map(({label}) => label),
-            range: items.map(({color}) => color),
+            domain: items.map(({ label }: any) => label),
+            range: items.map(({ color }) => color),
           },
         },
       },
@@ -93,17 +109,12 @@ class DonutChart extends React.PureComponent {
     };
     return (
       <div className="chart">
-        <BaseChart
-          width={chartSize}
-          height={chartSize}
-          spec={spec}
-          data={data}
-        />
+        <BaseChart width={chartSize} height={chartSize} spec={spec} data={data} />
       </div>
     );
   }
   render() {
-    const { items, chartSize, innerRadiusFactor } = this.props;
+    const { items, chartSize, innerRadiusFactor }: any = this.props;
 
     return (
       <div className="donut-chart">
