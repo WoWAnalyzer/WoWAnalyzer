aaf4e56540d5e85337525cab3d3dcf88890e1a6b
vohrr <matthew.d.pingel@gmail.com>
2020-10-19 02:01:05 -0400

    updated changelog and snapshots

--- src/parser/monk/mistweaver/CHANGELOG.tsx
+++ src/parser/monk/mistweaver/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import { change, date } from 'common/changelog';
 
 
 export default [
+  change(date(2020, 10, 19), <>added enveloping breath to effective healing stat box. </>, Vohrr),
   change(date(2020, 10, 14), <>Converted some more files to typescript as well as fixing internalization. </>, Abelito75),
   change(date(2020, 10, 14), <>Corrected upwelling's icon. </>, Abelito75),
   change(date(2020, 10, 14), <>Updating Yu'lon's tooltip! </>, Abelito75),
