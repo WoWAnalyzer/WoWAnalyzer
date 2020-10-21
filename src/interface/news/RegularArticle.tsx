168e6802f8cd9233f30260453057ae756acdd25e
ssabbar <64788101+ssabbar@users.noreply.github.com>
2020-10-21 15:30:01 +0200

    Update RegularArticle

--- src/interface/news/RegularArticle.tsx
+++ src/interface/news/RegularArticle.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Contributor from 'interface/ContributorButton';
 import makeNewsUrl from 'interface/news/makeUrl';
 import Panel from 'interface/others/Panel';
+import { ContributorType } from 'interface/ContributorButton/ContributorButton';
 
-const RegularArticle = props => {
+interface Props {
+  title: ReactNode;
+  bodyStyle: object;
+  children: ReactNode,
+  publishedAt: string,
+  publishedBy: ContributorType
+}
+
+const RegularArticle = (props: Props) => {
   const { title, children, bodyStyle, publishedAt, publishedBy } = props;
 
   return (
@@ -23,16 +32,6 @@ const RegularArticle = props => {
       </Panel>
     </article>
   );
-};
-
-RegularArticle.propTypes = {
-  title: PropTypes.node.isRequired,
-  children: PropTypes.node.isRequired,
-  bodyStyle: PropTypes.object,
-  publishedAt: PropTypes.string.isRequired,
-  publishedBy: PropTypes.shape({
-    nickname: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default RegularArticle;
