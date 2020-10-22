6a8e04c75c496c3ce31267060b2ca089a60c6693
Leo Zhekov <zhekov21@gmail.com>
2020-10-22 18:33:55 +0300

    3801 - forgot to add changelog entry.

--- src/parser/deathknight/frost/CHANGELOG.tsx
+++ src/parser/deathknight/frost/CHANGELOG.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Khazak } from 'CONTRIBUTORS';
+import { Khazak, LeoZhekov } from 'CONTRIBUTORS';
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+    change(date(2020, 10, 22), 'Replaced deprecated StatisticBoxes with Statistic for frost DK', [LeoZhekov]),
     change(date(2020, 10, 16), 'Updated Abilities with pre-patch spells', [Khazak]),
     change(date(2020, 8, 11), 'Converted Frost Death Knight modules to Typescript', [Khazak]),
     change(date(2020, 5, 11), <>Fixed issue where <SpellLink id={SPELLS.ICECAP_TALENT.id} /> was not properly counting the internal cooldown</>, [Khazak]),
