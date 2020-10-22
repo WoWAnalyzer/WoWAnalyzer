c69c6fa2714bdf84531a5e02dfa965ca9769b38f
Leo Zhekov <zhekov21@gmail.com>
2020-10-22 18:37:24 +0300

    3801 - fixed merge conflicts

--- src/parser/deathknight/frost/CHANGELOG.tsx
+++ src/parser/deathknight/frost/CHANGELOG.tsx
@@ -6,8 +6,8 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
-    change(date(2020, 10, 22), 'Tweaked spells for prepatch, fixed bug where abilities that only wasted Runic Power and generated none were not showing up in the resource tab', [Khazak]),
     change(date(2020, 10, 22), 'Replaced deprecated StatisticBoxes with Statistic for frost DK', [LeoZhekov]),
+    change(date(2020, 10, 22), 'Tweaked spells for prepatch, fixed bug where abilities that only wasted Runic Power and generated none were not showing up in the resource tab', [Khazak]),
     change(date(2020, 10, 16), 'Updated Abilities with pre-patch spells', [Khazak]),
     change(date(2020, 8, 11), 'Converted Frost Death Knight modules to Typescript', [Khazak]),
     change(date(2020, 5, 11), <>Fixed issue where <SpellLink id={SPELLS.ICECAP_TALENT.id} /> was not properly counting the internal cooldown</>, [Khazak]),
