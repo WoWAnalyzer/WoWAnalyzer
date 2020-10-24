2c1c701c9f459350fb74197eb7f9025377e974f1
guy biton <guy.biton@booking.com>
2020-10-24 19:20:46 +0200

    Migrage PlayerSection to TS, Update dps range roles to num 3 to sort dps by melle/range

--- src/interface/report/PlayerSelection/index.tsx
+++ src/interface/report/PlayerSelection/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import SPECS from 'game/SPECS';
 import ROLES from 'game/ROLES';
+import { CombatantInfoEvent } from 'parser/core/Events';
 
 import PlayerTile from './PlayerTile';
 import './PlayerSelection.scss';
 
-const ROLE_SORT_KEY = {
+const ROLE_SORT_KEY: {[key: string]: number} = {
   [ROLES.TANK]: 0,
   [ROLES.HEALER]: 1,
   [ROLES.DPS.MELEE]: 2,
-  [ROLES.DPS.RANGED]: 2,
+  //I'm not sure if this is a bug or intended behaviour, But if this is 2 then there is no sort based or range/melle only on dps.
+  [ROLES.DPS.RANGED]: 3,
 };
-function sortPlayers(a, b) {
+
+function sortPlayers(a: Player, b: Player) {  
   const aSpec = SPECS[a.combatant.specID];
   const bSpec = SPECS[b.combatant.specID];
-
   const aRoleSortKey = aSpec ? ROLE_SORT_KEY[aSpec.role] : -1;
   const bRoleSortKey = bSpec ? ROLE_SORT_KEY[bSpec.role] : -1;
 
@@ -33,17 +34,33 @@ function sortPlayers(a, b) {
   return a.name.localeCompare(b.name);
 }
 
-const PlayerSelection = ({ players, makeUrl }) => (
+interface Fight {
+  id: number;
+}
+
+interface Player { 
+  combatant: CombatantInfoEvent;
+  fights: Fight[];
+  guid: number;
+  icon: string;
+  id: number;
+  name: string;
+  region: string;
+  server: string;
+  type: string;
+}
+
+interface Props {
+  players: Player[];
+  makeUrl: (playerId: string) => void;
+}
+
+const PlayerSelection = ({ players, makeUrl }: Props) => (
   <div className="player-selection">
     {players.sort(sortPlayers).map(player => (
       <PlayerTile key={player.guid} player={player} makeUrl={makeUrl} />
     ))}
   </div>
 );
-
-PlayerSelection.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.object).isRequired,
-  makeUrl: PropTypes.func.isRequired,
-};
 
 export default PlayerSelection;
