e53c910734106271ddf14bcddd19ca7ed4515c2a
Henry Pabst <hpabst@ualberta.ca>
2020-10-25 13:25:15 -0700

    Add missing changelog entry

--- src/parser/paladin/protection/CHANGELOG.tsx
+++ src/parser/paladin/protection/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 10, 25), <>Create analyzers for <SpellLink id={SPELLS.REDOUBT_TALENT.id} /> and <SpellLink id={SPELLS.BLESSED_HAMMER_TALENT.id}/>.</>, Hordehobbs),
   change(date(2020, 10, 23), <>Aggregate Prot and Ret <SpellLink id={SPELLS.JUDGMENT_CAST.id} /> analyzers into single analyzer.</>, Hordehobbs),
   change(date(2020, 10, 21), 'Add Holy Shield spell blocks analyzer', Hordehobbs),
   change(date(2020, 10, 18), 'Converted legacy listeners to new event filters', Zeboot),
