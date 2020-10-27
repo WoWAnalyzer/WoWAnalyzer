538e30e7e87439acc079459383a3fabfa8ea8bfe
khazakdk <joe.j.sommer@gmail.com>
2020-10-26 20:15:57 -0500

    [Core] Convert ResourceTracker to TS

--- src/parser/shared/modules/resources/resourcetracker/ResourceTracker.ts
+++ src/parser/shared/modules/resources/resourcetracker/ResourceTracker.ts
@@ -1,6 +1,27 @@
-import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER, Options } from 'parser/core/Analyzer';
 import EventEmitter from 'parser/core/modules/EventEmitter';
-import Events, { EventType } from 'parser/core/Events';
+import Events, { EventType, ClassResources, EnergizeEvent, CastEvent, HealEvent, SpendResourceEvent } from 'parser/core/Events';
+import { Resource } from 'game/RESOURCE_TYPES';
+
+type BuilderObj = {
+  generated: number,
+  wasted: number,
+  casts: number,
+}
+
+type SpenderObj = {
+  spent: number,
+  spentByCast: number[],
+  casts: number,
+}
+
+type ResourceUpdate = {
+  timestamp: number | undefined,
+  current: number,
+  waste: number,
+  generated: number,
+  used: number,
+}
 
 /**
  * This is an 'abstract' implementation of a framework for tracking resource generating/spending.
@@ -13,20 +34,22 @@ class ResourceTracker extends Analyzer {
     // spellResourceCost: SpellResourceCost,
   };
 
+  protected eventEmitter!: EventEmitter;
+
   current = 0;
-  resourceUpdates = [];
+  resourceUpdates: ResourceUpdate[] = [];
 
   // stores resource gained/spent/wasted by ability ID
-  buildersObj = {};
-  spendersObj = {};
+  buildersObj: {[index: number]: BuilderObj} = {};
+  spendersObj: {[index: number]: SpenderObj} = {};
 
   // TODO set this to the resource you wish to track constructor.. see the appropriate objects in game/RESOURCE_TYPES
-  resource;
+  resource!: Resource;
 
   // TODO a classes 'main' resource passes the max along with events, but for other resources this may need to be defined
-  maxResource;
+  maxResource!: number;
 
-  constructor(options){
+  constructor(options: Options){
     super(options);
     this.addEventListener(Events.energize.to(SELECTED_PLAYER), this.onEnergize);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.onCast);
@@ -39,15 +62,15 @@ class ResourceTracker extends Analyzer {
   // isRegenHasted; // TODO iff true, regeneration rate will be scaled with haste
 
   // TODO if you wish an ability to show in results even if it wasn't used, add it using these functions constructor
-  initBuilderAbility(spellId) {
+  initBuilderAbility(spellId: number) {
     this.buildersObj[spellId] = { generated: 0, wasted: 0, casts: 0 };
   }
-  initSpenderAbility(spellId) {
+  initSpenderAbility(spellId: number) {
     this.spendersObj[spellId] = { spent: 0, spentByCast: [], casts: 0 };
   }
 
   // BUILDERS - Handled on energize, using the 'resourceChange' field
-  onEnergize(event) {
+  onEnergize(event: EnergizeEvent) {
     const spellId = event.ability.guid;
 
     if(event.resourceChangeType !== this.resource.id) {
@@ -56,7 +79,7 @@ class ResourceTracker extends Analyzer {
 
     const waste = event.waste;
     const gain = event.resourceChange - waste;
-    this._applyBuilder(spellId, this.getResource(event), gain, waste, event.timestamp);
+    this._applyBuilder(spellId, gain, waste, this.getResource(event), event.timestamp);
   }
 
   // FIXME Track resource drains too, so that the 'current' value can be more accurate
@@ -68,14 +91,14 @@ class ResourceTracker extends Analyzer {
    * @param {number} spellId - The spellId to attribute the resource gain to
    * @param {number} amount - The raw amount of resources to gain
    */
-  processInvisibleEnergize(spellId, amount) {
+  processInvisibleEnergize(spellId: number, amount: number) {
     const maxGain = this.maxResource !== undefined ? this.maxResource - this.current : amount;
     const gain = Math.min(amount, maxGain);
     const waste = Math.max(amount - maxGain, 0);
-    this._applyBuilder(spellId, null, gain, waste);
+    this._applyBuilder(spellId, gain, waste);
   }
 
-  _applyBuilder(spellId, resource, gain, waste, timestamp) {
+  _applyBuilder(spellId: number, gain: number, waste: number,  resource?: ClassResources, timestamp?: number) {
     if (!this.buildersObj[spellId]) {
         this.initBuilderAbility(spellId);
     }
@@ -104,13 +127,18 @@ class ResourceTracker extends Analyzer {
   }
 
   // SPENDERS - Handled on cast, using the 'classResources' field
-  onCast(event) {
+  onCast(event: CastEvent) {
     const spellId = event.ability.guid;
 
     if(!this.shouldProcessCastEvent(event)) {
         return;
     }
     const eventResource = this.getResource(event);
+
+    if(!eventResource){
+      return;
+    }
+
     if (eventResource && eventResource.max) {
       this.maxResource = eventResource.max; // track changes in max resource, which can happen due to procs / casts
     }
@@ -145,23 +173,24 @@ class ResourceTracker extends Analyzer {
   }
 
   // TODO if your spec has an ability cost reduction that doesn't show in events, handle it manually by overriding here. Or extend SpellResourceCost and apply the discount there.
-  getReducedCost(event) {
+  getReducedCost(event: CastEvent) {
     if (event.resourceCost && event.resourceCost[this.resource.id] !== undefined) {
       return event.resourceCost[this.resource.id];
     }
-    return this.getResource(event).cost;
+    return this.getResource(event)?.cost;
   }
 
-  getResource(event) {
+  getResource(event: CastEvent | HealEvent | EnergizeEvent ) {
     if(!event.classResources) {
-      return null;
+      return undefined;
     } else {
-      return event.classResources.find(r=>r.type === this.resource.id);
+      return event.classResources.find(r => r.type === this.resource.id);
     }
-  }
+  }  
 
-  triggerSpendEvent(spent, event) {
-    this.eventEmitter.fabricateEvent({
+  triggerSpendEvent(spent: number, event: CastEvent) {
+
+    const fabricatedEvent: SpendResourceEvent = {
       type: EventType.SpendResource,
       timestamp: event.timestamp,
       sourceID: event.sourceID,
@@ -169,22 +198,25 @@ class ResourceTracker extends Analyzer {
       resourceChange: spent,
       resourceChangeType: this.resource.id,
       ability: event.ability,
-    }, event);
+      __fabricated: true,
+    };
+
+    this.eventEmitter.fabricateEvent(fabricatedEvent, event);
   }
 
-  shouldProcessCastEvent(event) {
+  shouldProcessCastEvent(event: CastEvent) {
     return Boolean(this.getResource(event));
   }
 
-  getGeneratedBySpell(spellId) {
+  getGeneratedBySpell(spellId: number) {
     return (this.buildersObj[spellId] && this.buildersObj[spellId].generated) || 0;
   }
 
-  getWastedBySpell(spellId) {
+  getWastedBySpell(spellId: number) {
     return (this.buildersObj[spellId] && this.buildersObj[spellId].wasted) || 0;
   }
 
-  getBuilderCastsBySpell(spellId) {
+  getBuilderCastsBySpell(spellId: number) {
     return (this.buildersObj[spellId] && this.buildersObj[spellId].casts) || 0;
   }
 
