95e663a29eed08d13d15ded040aa1940e74b2263
Joshua Hahn <joshuahahn1996@googlemail.com>
2020-10-31 14:50:58 +0100

    move runeforge spells to deathknight-spell files

--- src/common/SPELLS/deathknight.ts
+++ src/common/SPELLS/deathknight.ts
@@ -4,10 +4,10 @@
  * You can access these entries like other entries in the spells files by importing `common/SPELLS` and using the assigned property on the SPELLS object. Please try to avoid abbreviating properties.
  */
 
-import runeforges from 'parser/deathknight/shared/runeforges'
 import safeMerge from 'common/safeMerge';
 
 import { SpellList } from "./Spell";
+import { Enchant } from 'common/ITEMS/Item';
 
 const spells: SpellList = {
   // Blood:
@@ -592,6 +592,64 @@ const spells: SpellList = {
     id: 189096,
     name: 'Runic Power',
     icon: 'inv_sword_62',
+  },
+};
+
+const runeforges: SpellList<Enchant> = {
+  RUNE_OF_THE_FALLEN_CRUSADER: {
+    id: 53344,
+    name: 'Rune of the Fallen Crusader',
+    icon: 'spell_holy_retributionaura',
+    effectId: 3368,
+  },
+
+  RUNE_OF_RAZORICE: {
+    id: 53343,
+    name: ' Rune of Razorice',
+    icon: 'spell_frost_frostarmor',
+    effectId: 3370,
+  },
+
+  RUNE_OF_THE_STONESKIN_GARGOYLE: {
+    id: 62158,
+    name: 'Rune of the Stoneskin Gargoyle',
+    icon: 'inv_sword_130',
+    effectId: 3847,
+  },
+
+  RUNE_OF_HYSTERIA: {
+    id: 326911,
+    name: 'Rune of Hysteria',
+    icon: 'ability_deathknight_runicimpowerment',
+    effectId: 6243,
+  },
+
+  RUNE_OF_SANGUINATION: {
+    id: 326805,
+    name: 'Rune of Sanguination',
+    icon: 'ability_argus_deathfog',
+    effectId: 6241,
+  },
+
+  RUNE_OF_APOCALYPSE: {
+    id: 327082,
+    name: 'Rune of Apocalypse',
+    icon: 'spell_deathknight_thrash_ghoul',
+    effectId: 6245,
+  },
+
+  RUNE_OF_UNENDING_THIRST: {
+    id: 326977,
+    name: 'Rune of Unending Thirst',
+    icon: 'spell_nzinsanity_bloodthirst',
+    effectId: 6244,
+  },
+
+  RUNE_OF_SPELLWARDING: {
+    id: 326855,
+    name: 'Rune of Spellwarding',
+    icon: 'spell_fire_twilightfireward',
+    effectId: 6242,
   },
 };
 
