13390f34a80f7177950f4dcc6826ae414f8c0bd9
jos3p <3185600+jos3p@users.noreply.github.com>
2020-11-15 07:10:49 +0100

    Update changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -9,6 +9,7 @@ import Contributor from 'interface/ContributorButton';
 
 // prettier-ignore
 export default [
+  change(date(2020, 11, 15), 'Updated i18n ids', jos3p),
   change(date(2020, 11, 7), 'Adjust our Soulbind, Covenant, Conduit scripts per latest raid testing', Putro),
   change(date(2020, 11, 4), 'Migrate Castle Nathria to TS', Guyius),
   change(date(2020, 11, 2), 'Converted the following components to TypeScript: ItemLink, ResourceLink, Rule and Expandable', Mae),
