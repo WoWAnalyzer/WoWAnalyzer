28462fcc8ca524e69fc2bd9dde4a11c64a63d880
Sharrq <cjgrasty86@gmail.com>
2020-11-28 00:12:29 -0600

    [Core] Delete BFA & Add New Dungeons
    - Deleted all of the BFA Modules (Left the ITEM/SPELL IDs for now until things like the Character Tab can be updated)
    - Added configs and backgrounds for the Shadowlands Dungeons

---
+++ src/raids/dungeons/SpiresOfAscension.js
@@ -0,0 +1,13 @@
+import Background from './backgrounds/SpiresOfAscension.jpg';
+import Headshot from './headshots/SpiresOfAscension.jpg';
+
+export default {
+  id: 12285,
+  name: 'Spires of Ascension',
+  background: Background,
+  headshot: Headshot,
+  icon: 'achievement_dungeon_spiresofascension',
+  fight: {
+    // TODO: Add vantusRuneBuffId: 250144,
+  },
+};
