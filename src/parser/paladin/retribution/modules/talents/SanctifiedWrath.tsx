9a7c89491c61049789956c91e868f0a635c237f3
Skeletor <lordskeletoresq@gmail.com>
2020-12-01 02:42:28 -0500

    SpacesRBad
    Don't use spaces like a dummy.

--- src/parser/paladin/retribution/modules/talents/SanctifiedWrath.tsx
+++ src/parser/paladin/retribution/modules/talents/SanctifiedWrath.tsx
@@ -20,9 +20,6 @@ class SanctifiedWrath extends Analyzer {
 
   protected abilityTracker!: AbilityTracker;
 
-  baseDuration = AW_BASE_DURATION;
-  buffId: null | number = null;
-
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.hasTalent(SPELLS.SANCTIFIED_WRATH_TALENT_RETRIBUTION.id);
