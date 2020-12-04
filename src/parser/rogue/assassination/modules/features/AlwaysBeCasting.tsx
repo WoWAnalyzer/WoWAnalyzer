b7b91f0268d513c3044d9b4f809026c0fcb2e9e3
Daniel Vincent <dvincent@amuniversal.com>
2020-12-04 09:11:09 -0600

    Fix PR comments

--- src/parser/rogue/assassination/modules/features/AlwaysBeCasting.tsx
+++ src/parser/rogue/assassination/modules/features/AlwaysBeCasting.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { When } from 'parser/core/ParseResults';
+import { ThresholdStyle, When } from 'parser/core/ParseResults';
 // import SPELLS from 'common/SPELLS';
 import CoreAlwaysBeCasting from 'parser/shared/modules/AlwaysBeCasting';
 import { formatPercentage } from 'common/format';
-import { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 // import SpellLink from 'common/SpellLink';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
-//   get downtimeSuggestionThresholds() {
-//     return {
-//       actual: this.downtimePercentage,
-//       isGreaterThan: {
-//         minor: 0.4,
-//         average: 0.45,
-//         major: 0.5,
-//       },
-//       style: ThresholdStyle.PERCENTAGE,
-//     };
-//   }
-// }
+  get suggestionThresholds() {
+    return {
+      actual: this.downtimePercentage,
+      isGreaterThan: {
+        minor: 0.4,
+        average: 0.45,
+        major: 0.5,
+      },
+      style: ThresholdStyle.PERCENTAGE,
+    };
+  }
 
 // TODO: Change the following to be Ass Rogue specific.
 // If a fight requires movement, try saving <SpellLink id={SPELLS.PRESENCE_OF_MIND.id} /> to make your next 2 <SpellLink id={SPELLS.ARCANE_BLAST.id} /> casts instant.
@@ -28,7 +26,7 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
     const boss = this.owner.boss;
 
     if(!boss || !boss.fight.disableDeathSuggestion) {
-      when(deadTimePercentage).isGreaterThan(0.4)
+      when(deadTimePercentage).isGreaterThan(this.suggestionThresholds.isGreaterThan.minor)
         .addSuggestion((suggest, actual, recommended) => suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells.</span>)
           .icon('spell_mage_altertime')
           .actual(`${formatPercentage(actual)}% downtime`)
@@ -36,8 +34,6 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
           .regular(recommended + 0.05).major(recommended + 0.1));
     }
   }
-
-  statiticOrder: any = STATISTIC_ORDER.CORE(1)
 }
 
 export default AlwaysBeCasting;
