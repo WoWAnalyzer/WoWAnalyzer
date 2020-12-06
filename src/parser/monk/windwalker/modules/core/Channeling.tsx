cf411ed632886b08d3ae1412d262c4ed16c750b3
Juko8 <ulrichenevoldsen@hotmail.com>
2020-12-06 19:40:30 +0100

    update ww to typescript 6

--- src/parser/monk/windwalker/modules/core/Channeling.tsx
+++ src/parser/monk/windwalker/modules/core/Channeling.tsx
@@ -1,7 +1,8 @@
 import SPELLS from 'common/SPELLS';
 import CoreChanneling from 'parser/shared/modules/Channeling';
-import Events from 'parser/core/Events';
-import { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, { ApplyBuffEvent, ApplyDebuffEvent, CastEvent, RemoveBuffEvent, RemoveDebuffEvent } from 'parser/core/Events';
+import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Ability from 'parser/core/modules/Ability';
 
 /**
  * Crackling Jade Lightning don't reveal in the combatlog when channeling begins and ends, this fabricates the required events so that ABC can handle it properly.
@@ -12,14 +13,15 @@ import { SELECTED_PLAYER } from 'parser/core/Analyzer';
  * To avoid Crackling Jade Lightning as being marked "canceled" when we start a new spell we mark it as ended instead on the begincast/cast.
  */
 class Channeling extends CoreChanneling {
-  constructor(options){
+  constructor(options: Options){
     super(options);
     this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.FISTS_OF_FURY_CAST), this.onApplyBuff);
     this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.FISTS_OF_FURY_CAST), this.onRemoveBuff);
     this.addEventListener(Events.applydebuff.by(SELECTED_PLAYER).spell(SPELLS.CRACKLING_JADE_LIGHTNING), this.onApplyDebuff);
     this.addEventListener(Events.removedebuff.by(SELECTED_PLAYER).spell(SPELLS.CRACKLING_JADE_LIGHTNING), this.onRemoveDebuff);
   }
-  onCast(event) {
+
+  onCast(event: CastEvent) {
     if (event.ability.guid === SPELLS.CRACKLING_JADE_LIGHTNING.id || event.ability.guid === SPELLS.FISTS_OF_FURY_CAST.id) {
       // We track Crackling Jade Lightning and Fists of Fury differently
       return;
@@ -27,7 +29,7 @@ class Channeling extends CoreChanneling {
     super.onCast(event);
   }
 
-  cancelChannel(event, ability) {
+  cancelChannel(event: CastEvent, ability: Ability) {
     if (this.isChannelingSpell(SPELLS.CRACKLING_JADE_LIGHTNING.id) || this.isChannelingSpell(SPELLS.FISTS_OF_FURY_CAST.id)) {
       // If a channeling spell is "canceled" it was actually just ended, so if it looks canceled then instead just mark it as ended
       this.log('Marking', this._currentChannel.ability.name, 'as ended since we started casting something else');
@@ -37,24 +39,24 @@ class Channeling extends CoreChanneling {
     }
   }
 
-  onApplyBuff(event) {
+  onApplyBuff(event: ApplyBuffEvent) {
     this.beginChannel(event);
   }
 
-  onApplyDebuff(event) {
+  onApplyDebuff(event: ApplyDebuffEvent) {
     this.beginChannel(event);
   }
 
   // Looking at `removebuff` will includes progress towards a tick that never happened. This progress could be considered downtime as it accounts for nothing.
   // If it's ever decided to consider the time between last tick and channel ending as downtime, just change the endchannel trigger.
-  onRemoveDebuff(event) {
+  onRemoveDebuff(event: RemoveDebuffEvent) {
     if (!this.isChannelingSpell(SPELLS.CRACKLING_JADE_LIGHTNING.id)) {
       // This may be true if we did the event-order fix in begincast/cast and it was already ended there.
       return;
     }
     this.endChannel(event);
   }
-  onRemoveBuff(event) {
+  onRemoveBuff(event: RemoveBuffEvent) {
     if (!this.isChannelingSpell(SPELLS.FISTS_OF_FURY_CAST.id)) {
       // This may be true if we did the event-order fix in begincast/cast and it was already ended there.
       return;
--- src/parser/monk/windwalker/modules/features/AlwaysBeCasting.tsx
+++ src/parser/monk/windwalker/modules/features/AlwaysBeCasting.tsx
@@ -6,6 +6,7 @@ import { formatPercentage } from 'common/format';
 import { STATISTIC_ORDER } from 'interface/others/StatisticBox';
 import { i18n } from '@lingui/core';
 import { t } from '@lingui/macro';
+import { ThresholdStyle, When } from 'parser/core/ParseResults';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
   get downtimePercentage() {
@@ -20,11 +21,11 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
         average: 0.3,
         major: 0.4,
       },
-      style: 'percentage',
+      style: ThresholdStyle.PERCENTAGE,
     };
   }
 
-  suggestions(when) {
+  suggestions(when: When) {
     when(this.suggestionThresholds).addSuggestion((suggest, actual, recommended) => suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells.</span>)
           .icon('spell_mage_altertime')
           .actual(i18n._(t('monk.windwalker.alwaysBeCasting.downtime')`${formatPercentage(actual)}% downtime`))
