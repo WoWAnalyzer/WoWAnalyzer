6bb3754eb69dfc268ffac9529ae008ff85009e9b
Putro <laustsf@gmail.com>
2020-12-08 21:44:40 +0100

    [Shaman] Goodbye BFA

--- src/common/SPELLS/bfa/azeritetraits/index.js
+++ src/common/SPELLS/bfa/azeritetraits/index.js
@@ -1,8 +1,7 @@
 import safeMerge from 'common/safeMerge';
 
 import General from './general';
-import Shaman from './shaman';
 import Warlock from './warlock';
 import Warrior from './warrior';
 
-export default safeMerge(General, Shaman, Warlock, Warrior);
+export default safeMerge(General, Warlock, Warrior);
