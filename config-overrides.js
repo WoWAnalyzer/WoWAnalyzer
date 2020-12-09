c88090b746ae596f4aba5315a84a224f6983b183
Putro <laustsf@gmail.com>
2020-12-10 00:53:51 +0100

    Revert "Quicker build workflow"

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,10 @@
-const { override, disableEsLint } = require('customize-cra');
-// eslint-disable-next-line import/no-extraneous-dependencies
-// const EslintPlugin = require('eslint-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
-
-// Enable when we upgrade to CRA v4:
-// // customize-cra's disableEsLint disables the rules, but disabling the entire
-// // plugin seem to give us more performance.
-// const disableEsLint = () => (config) => ({
-//   ...config,
-//   plugins: config.plugins.filter((plugin) => !(plugin instanceof EslintPlugin)),
-// });
-const disableTypeChecking = () => (config) => ({
-  ...config,
-  plugins: config.plugins.filter((plugin) => !(plugin instanceof ForkTsCheckerWebpackPlugin)),
-});
+const {
+  override,
+  useEslintRc,
+  disableEsLint,
+} = require('customize-cra');
 
 module.exports = override(
-  process.env.DISABLE_AUTOMATIC_ESLINT && disableEsLint(),
-  process.env.DISABLE_AUTOMATIC_ESLINT && disableTypeChecking(),
+  process.env.DISABLE_AUTOMATIC_ESLINT ? disableEsLint() : undefined,
   // addBabelPlugin('babel-plugin-transform-typescript-metadata'),
 );
