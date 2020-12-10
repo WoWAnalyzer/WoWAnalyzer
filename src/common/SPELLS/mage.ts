d2ae931cf3e248339d12ea548e2cca97f1a799e7
Sharrq <cjgrasty86@gmail.com>
2020-12-10 11:34:45 -0600

    [Arcane Mage] Allow Missiles during TOTM if Echo is talented

--- src/common/SPELLS/mage.ts
+++ src/common/SPELLS/mage.ts
@@ -454,6 +454,11 @@ const spells = {
     icon: 'spell_nature_wispsplode',
   },
   TOUCH_OF_THE_MAGI: {
+    id: 321507,
+    name: 'Touch of the Magi',
+    icon: 'spell_mage_icenova',
+  },
+  TOUCH_OF_THE_MAGI_DEBUFF: {
     id: 210725,
     name: 'Touch of the Magi',
     icon: 'spell_mage_icenova',
