bef850f20f5d2af6b288a282ae37cfd8338cb804
Sharrq <cjgrasty86@gmail.com>
2020-12-11 20:41:28 -0600

    Sun Kings Salvation

--- src/raids/castlenathria/SunKingsSalvation.ts
+++ src/raids/castlenathria/SunKingsSalvation.ts
@@ -1,4 +1,6 @@
 import { Boss } from "raids/index";
+import DIFFICULTIES from 'game/DIFFICULTIES';
+import { EventType } from 'parser/core/Events';
 
 import Background from './images/backgrounds/SunKingsSalvation.jpg';
 import Headshot from './images/headshots/SunKingsSalvation.jpg';
@@ -15,6 +17,50 @@ const SunKingsSalvation: Boss = {
     softMitigationChecks: {
       physical: [],
       magical: [],
+    },
+    phases: {
+      P1: {
+        name: 'Stage 1: Ministers of Vice 1',
+        multiple: false,
+        key: "P1",
+        difficulties: [DIFFICULTIES.NORMAL_RAID, DIFFICULTIES.HEROIC_RAID, DIFFICULTIES.MYTHIC_RAID],
+      },
+      P2: {
+        key: "P2",
+        name: 'Stage 2: Reflection of Guilt 1',
+        multiple: false,
+        difficulties: [DIFFICULTIES.NORMAL_RAID, DIFFICULTIES.HEROIC_RAID, DIFFICULTIES.MYTHIC_RAID],
+        filter: {
+          type: EventType.Health,
+          guid: 165805,
+          health: 55.0,
+          eventInstance: 0,
+        },
+      },
+      P1_2: {
+        name: 'Stage 1: Ministers of Vice 2',
+        multiple: false,
+        key: "P1_2",
+        difficulties: [DIFFICULTIES.NORMAL_RAID, DIFFICULTIES.HEROIC_RAID, DIFFICULTIES.MYTHIC_RAID],
+        filter: {
+          type: EventType.Health,
+          guid: 165805,
+          health: 45.0,
+          eventInstance: 0,
+        },
+      },
+      P2_2: {
+        key: "P2_2",
+        name: 'Stage 2: Reflection of Guilt 2',
+        multiple: false,
+        difficulties: [DIFFICULTIES.NORMAL_RAID, DIFFICULTIES.HEROIC_RAID, DIFFICULTIES.MYTHIC_RAID],
+        filter: {
+          type: EventType.Health,
+          guid: 165805,
+          health: 10.0,
+          eventInstance: 0,
+        },
+      },
     },
   },
 };
