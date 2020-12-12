5f6883ed404a16fd0681af8415e12d5fc719c74f
Martijn Hols <git@martijnhols.nl>
2020-12-12 12:35:59 +0100

    Update changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import { change, date } from 'common/changelog';
 
 // prettier-ignore
 export default [
+  change(date(2020, 12, 12), 'Fix a few translation issues.', Zerotorescue),
   change(date(2020, 12, 11), 'Updated Healing Healing Efficiency to show two decimal places', Abelito75),
   change(date(2020, 12, 11), 'Converted some root files from JS to TS', Keraldi),
   change(date(2020, 12, 10), 'Removed Heart of Azeroth from player selection screen as well as added Anti-magic zone to the tracked cooldowns', Abelito75),
