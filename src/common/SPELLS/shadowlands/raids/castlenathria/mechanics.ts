dc786c8add8a1dbc100c062583a1cac040252ad2
Sharrq <cjgrasty86@gmail.com>
2020-12-11 19:10:58 -0600

    castle nathria raid spell files

---
+++ src/common/SPELLS/shadowlands/raids/castlenathria/mechanics.ts
@@ -0,0 +1,29 @@
+export default {
+  // Group by boss (with comments)
+
+  //Shriekwing
+  BLOOD_SHROUD: {
+    id: 328921,
+    name: 'Blood Shroud',
+    icon: '',
+  },
+
+  //Huntsman Altimor
+
+  //Hungering Destroyer
+
+  //Lady Inerva Darkvein
+
+  //Artificer Xymox
+
+  //Sun King
+
+  //Council of Blood
+
+  //Sludgefist
+
+  //Stoneborne Generals
+
+  //Sire Denathrius
+
+};
