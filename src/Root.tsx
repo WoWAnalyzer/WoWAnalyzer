20cc655e7d0e1c9907337b2d2bb94bd93fbfe27b
Martijn Hols <git@martijnhols.nl>
2020-12-12 12:33:57 +0100

    Update snapshots

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider as ReduxProvider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -13,14 +13,16 @@ import I18nProvider from './localization/I18nProvider';
 
 const store = createStore(createReducers(), composeWithDevTools(applyMiddleware(thunk)));
 
-const Root = () => (
+interface Props {
+  children?: ReactNode;
+}
+
+const Root = ({ children }: Props) => (
   <ReduxProvider store={store}>
     <I18nProvider>
       {/* We need to place the error boundary inside all providers since it uses i18n for localized messages. */}
       <RootErrorBoundary>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <BrowserRouter>{children || <App />}</BrowserRouter>
       </RootErrorBoundary>
     </I18nProvider>
   </ReduxProvider>
