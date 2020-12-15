9f9920cdcdd2bd1de64fbafa9d3e1047049707b6
Daniel Vincent <dvincent@amuniversal.com>
2020-12-15 13:08:36 -0600

    Fix suggestions per PR comments

--- src/parser/rogue/subtlety/modules/features/AlwaysBeCasting.tsx
+++ src/parser/rogue/subtlety/modules/features/AlwaysBeCasting.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
-// import SPELLS from 'common/SPELLS';
+import SPELLS from 'common/SPELLS';
 import CoreAlwaysBeCasting from 'parser/shared/modules/AlwaysBeCasting';
 import { formatPercentage } from 'common/format';
-// import SpellLink from 'common/SpellLink';
+import SpellLink from 'common/SpellLink';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
   get suggestionThresholds() {
@@ -17,20 +17,15 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
       style: ThresholdStyle.PERCENTAGE,
     };
   }
-// TODO: Change the following to be Ass Rogue specific.
-// If a fight requires movement, try saving <SpellLink id={SPELLS.PRESENCE_OF_MIND.id} /> to make your next 2 <SpellLink id={SPELLS.ARCANE_BLAST.id} /> casts instant.
 
   suggestions(when: When) {
-    const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
     const boss = this.owner.boss;
 
     if(!boss || !boss.fight.disableDeathSuggestion) {
-      when(deadTimePercentage).isGreaterThan(this.suggestionThresholds.isGreaterThan.minor)
-        .addSuggestion((suggest, actual, recommended) => suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells.</span>)
+      when(this.suggestionThresholds)
+        .addSuggestion((suggest, actual, recommended) => suggest(<span>Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay between casting spells. If everything is on cooldown, try to use <SpellLink id={SPELLS.BACKSTAB.id} /> or <SpellLink id={SPELLS.SHADOWSTRIKE.id} /> to stay off the energy cap and do some damage.</span>)
           .icon('spell_mage_altertime')
-          .actual(`${formatPercentage(actual)}% downtime`)
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended + 0.05).major(recommended + 0.1));
+          .actual(`${formatPercentage(actual)}% downtime`));
     }
   }
 }
