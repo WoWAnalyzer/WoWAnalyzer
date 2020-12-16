d7fb4e7ec06c9ac1f54079b4fba46c78f0ea2524
Martijn Hols <git@martijnhols.nl>
2020-12-16 14:14:19 +0100

    Update changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 
 // prettier-ignore
 export default [
+  change(date(2020, 12, 16), 'Fix character API realm slugs for non-English realms.', Zerotorescue),
   change(date(2020, 12, 15), <> Add support for the Engineering scopes, <ItemLink id={ITEMS.ENCHANT_WEAPON_OPTICAL_TARGET_EMBIGGENER.id} /> and <ItemLink id={ITEMS.ENCHANT_WEAPON_INFRA_GREEN_REFLEX_SIGHT.id} />, to the enchant checker. </>, Putro),
   change(date(2020, 12, 15), 'Add support for Aura Broken events in our Events tab', Putro),
   change(date(2020, 12, 13), 'Using the browser refresh on fight selection now correctly reloads the report from WCL.', Zerotorescue),
