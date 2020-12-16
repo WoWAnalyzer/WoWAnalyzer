17af1bf08ec562e793f81e8efd6e8572d404b479
Martijn Hols <git@martijnhols.nl>
2020-12-16 14:13:31 +0100

    Fix character API realm slugs

--- src/common/makeApiUrl.ts
+++ src/common/makeApiUrl.ts
@@ -1,24 +1,36 @@
 import makeUrl from './makeUrl';
+import REALMS from './REALMS';
 
 export interface QueryParams {
   [key: string]: string | number | boolean | undefined;
 }
 
 export default function makeApiUrl(endpoint: string, queryParams: QueryParams = {}) {
-  return makeUrl(`${process.env.REACT_APP_SERVER_BASE}${process.env.REACT_APP_API_BASE}${endpoint}`, queryParams);
+  return makeUrl(
+    `${process.env.REACT_APP_SERVER_BASE}${process.env.REACT_APP_API_BASE}${endpoint}`,
+    queryParams,
+  );
 }
-export function makeCharacterApiUrl(characterId?: string, region?: string, realm?: string, name?: string) {
+export function makeCharacterApiUrl(
+  characterId?: string,
+  region?: string,
+  realm?: string,
+  name?: string,
+) {
   const parts = ['character'];
   if (characterId) {
     parts.push(characterId);
   }
   if (region && realm && name) {
+    const realmSlug = REALMS[region as 'EU' | 'US' | 'KR' | 'TW']?.find(
+      (item) => item.name === realm,
+    )?.slug;
     parts.push(region);
-    parts.push(realm);
+    parts.push(realmSlug || realm);
     parts.push(name);
   }
 
-  return makeApiUrl(parts.map(part => encodeURIComponent(part)).join('/'));
+  return makeApiUrl(parts.map((part) => encodeURIComponent(part)).join('/'));
 }
 
 export function makeGuildApiUrl(region?: string, realm?: string, name?: string) {
@@ -29,15 +41,15 @@ export function makeGuildApiUrl(region?: string, realm?: string, name?: string) 
     parts.push(name);
   }
 
-  return makeApiUrl(parts.map(part => encodeURIComponent(part)).join('/'));
+  return makeApiUrl(parts.map((part) => encodeURIComponent(part)).join('/'));
 }
 
 export function makeItemApiUrl(itemId: string) {
   const parts = ['item', itemId];
-  return makeApiUrl(parts.map(part => encodeURIComponent(part)).join('/'));
+  return makeApiUrl(parts.map((part) => encodeURIComponent(part)).join('/'));
 }
 
 export function makeSpellApiUrl(spellId: number) {
   const parts = ['spell', spellId];
-  return makeApiUrl(parts.map(part => encodeURIComponent(part)).join('/'));
+  return makeApiUrl(parts.map((part) => encodeURIComponent(part)).join('/'));
 }
