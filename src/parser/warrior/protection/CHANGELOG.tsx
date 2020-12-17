9599d1dae25f4d81ebcec8e3598b98f62bd7c3e5
Abelito75 <abelgm@charter.net>
2020-12-17 12:20:43 -0500

    abilities broken

--- src/parser/warrior/protection/CHANGELOG.tsx
+++ src/parser/warrior/protection/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { Abelito75, Putro, Zeboot } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 12, 17), 'Fixed undefined spell error.', Abelito75),
   change(date(2020, 12, 17), 'Added some shadowlandisms.', Abelito75),
   change(date(2020, 12, 17), 'Removed all BFAisms.', Abelito75),
   change(date(2020, 12, 15), 'Bumped level of support to 9.0.2', Putro),
