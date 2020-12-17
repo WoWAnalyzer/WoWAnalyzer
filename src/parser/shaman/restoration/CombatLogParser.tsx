9cc0024fabb5c4baf517b8de6cebf74bba922e60
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-12-17 04:42:37 +0100

    Update CombatLogParser.tsx
    Maybe?

--- src/parser/shaman/restoration/CombatLogParser.tsx
+++ src/parser/shaman/restoration/CombatLogParser.tsx
@@ -79,7 +79,7 @@ import RiptideNormalizer from './normalizers/RiptideNormalizer';
 import { ABILITIES_AFFECTED_BY_HEALING_INCREASES } from './constants';
 
 class CombatLogParser extends CoreCombatLogParser {
-  static abilitiesAffectedByHealingIncreases = ABILITIES_AFFECTED_BY_HEALING_INCREASES;
+  static abilitiesAffectedByHealingIncreases = ABILITIES_AFFECTED_BY_HEALING_INCREASES.map(val => val.id); // I know this is deprecated, and i have no idea about its use. This is a guess from my side
 
   static specModules = {
     lowHealthHealing: LowHealthHealing,
