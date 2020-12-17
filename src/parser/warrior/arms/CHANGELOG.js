af49c1605a709ed4523480df316dfa1a022834a4
Abelito75 <abelgm@charter.net>
2020-12-17 06:37:24 -0500

    changelogs

--- src/parser/warrior/arms/CHANGELOG.js
+++ src/parser/warrior/arms/CHANGELOG.js
@@ -1,1 +1,6 @@
-export default [];
+import { Abelito75 } from 'CONTRIBUTORS';
+import { change, date } from 'common/changelog';
+
+export default [
+  change(date(2020, 12, 17), 'Removed all BFAisms.', Abelito75),
+];
--- src/parser/warrior/protection/CHANGELOG.tsx
+++ src/parser/warrior/protection/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { Abelito75, Zeboot } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 12, 17), 'Removed all BFAisms.', Abelito75),
   change(date(2020, 10, 18), 'Updated Talents', Abelito75),
   change(date(2020, 10, 18), 'Converted legacy listeners to new event filters', Zeboot),
   change(date(2020, 10, 14), <>Fixed checklist and updated spellbook to prevent crashes.</>, Abelito75),
