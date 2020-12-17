87f72f4ecd0980156026f4255064df31c31f1ae0
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-12-17 04:38:54 +0100

    Update indexById.ts

--- src/common/indexById.ts
+++ src/common/indexById.ts
@@ -1,5 +1,29 @@
 // For ease of use we may want to be able to both access items by code names for completion/compile time checking (e.g. `ABILITIES.AURA_OF_MERCY_TALENT`) and by spell id for runtime evaluation (e.g. `ABILITIES[183415]`)
-const indexById = obj => {
+
+const indexById = <T extends { [key: string]: any; id: number }>(list: { [key: string]: T; [key: number] : T }) => {
+  for (const key in list) {
+    const ability = list[key];
+
+    if (process.env.NODE_ENV === 'development') {
+      if (list[key].hasOwnProperty("id") && list[ability.id] && !list[ability.id].__ignoreDuplication && !ability.__ignoreDuplication) {
+        throw new Error('A spell with this ID already exists: ' + ability.id + ',' + key);
+      }
+    }
+    
+    if (list[key].hasOwnProperty("id")) {
+      list[list[key].id] = ability; 
+    }
+      
+  }
+  return list;
+}
+
+export default indexById;
+
+
+/* OLD CODE
+
+const indexById = obj {} => {
   // Hackety hack
   obj.count = Object.keys(obj).length;
   Object.keys(obj).forEach((key) => {
@@ -16,3 +40,4 @@ const indexById = obj => {
 };
 
 export default indexById;
+*/
