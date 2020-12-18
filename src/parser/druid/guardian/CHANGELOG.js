ea92b953656f286e031ea98a859deeb09a774b22
Matthew Abbott <abbottmg@me.com>
2020-12-18 16:23:29 -0600

    Add contributor info and a changelog entry

--- src/parser/druid/guardian/CHANGELOG.js
+++ src/parser/druid/guardian/CHANGELOG.js
@@ -1,6 +1,7 @@
-import { Zeboot } from 'CONTRIBUTORS';
+import { Kettlepaw, Zeboot } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 12, 18), 'Fixed compiler errors affecing FrenziedRegen and AntiFillerSpam', Kettlepaw),
   change(date(2020, 10, 18), 'Converted legacy listeners to new event filters', Zeboot),
 ];
