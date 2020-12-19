5b7a13c6b0074b9aed4d87dba3576ee63cbba0e0
Putro <laustsf@gmail.com>
2020-12-19 17:48:01 +0100

    [Core] Give our conduit and soulbindTrait fields more indicative names than 'artifact' and 'heartOfAzeroth'

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 
 // prettier-ignore
 export default [
+  change(date(2020, 12, 19), 'Removed the last remnants of Azerite across the codebase', Putro),
   change(date(2020, 12, 19), <> Fix issues with <ItemLink id={ITEMS.INSCRUTABLE_QUANTUM_DEVICE.id} /> not accounting for the correct stat given. </>, Putro),
   change(date(2020, 12, 19), 'Adjust our weak Potion suggestion to only show if a weak potion was actually used', Putro),
   change(date(2020, 12, 17), 'Added phase information for Castle Nathria bosses', Sharrq),
