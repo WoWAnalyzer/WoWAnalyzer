a110eedaf0631b04db28fe76dbd039938c82dd33
Matthew Abbott <abbottmg@users.noreply.github.com>
2020-12-19 14:23:43 -0600

    Correct a typo in the new changelog entry

--- src/parser/druid/guardian/CHANGELOG.js
+++ src/parser/druid/guardian/CHANGELOG.js
@@ -2,6 +2,6 @@ import { Kettlepaw, Zeboot } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2020, 12, 18), 'Fixed compiler errors affecing FrenziedRegen and AntiFillerSpam', Kettlepaw),
+  change(date(2020, 12, 18), 'Fixed compiler errors affecting FrenziedRegen and AntiFillerSpam', Kettlepaw),
   change(date(2020, 10, 18), 'Converted legacy listeners to new event filters', Zeboot),
 ];
