58ae50cd94154da66472aa08ff46e8d157508d63
Joshua Hahn <joshuahahn1996@googlemail.com>
2020-12-20 19:37:00 +0100

    windfury whitelist

--- src/parser/shared/modules/spells/Windfury.tsx
+++ src/parser/shared/modules/spells/Windfury.tsx
@@ -9,9 +9,40 @@ import BoringSpellValueText from 'interface/statistics/components/BoringSpellVal
 import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
 import Events, { DamageEvent } from 'parser/core/Events';
 import ItemDamageDone from 'interface/ItemDamageDone';
-import ROLES from 'game/ROLES';
 import HIT_TYPES from 'game/HIT_TYPES';
 import { formatPercentage } from 'common/format';
+import SPECS from 'game/SPECS';
+
+/*
+  Checks if the player had windfury uptime, if so this module will look at the time between melee swings
+  A swing within 200ms of the previous one is counted as windfury proc
+
+  Parrying a hit reduces the next swing timer by 40%, thats why we ignore them
+*/
+
+// ToDo: look how dualwielding timers and hunters work
+
+const WINDFURY_TRESHOLD = 200 // every melee 200ms after its previous one are windfury procs
+const WHITELIST =[
+  SPECS.HOLY_PALADIN,
+  SPECS.PROTECTION_PALADIN,
+  SPECS.RETRIBUTION_PALADIN,
+  SPECS.ARMS_WARRIOR,
+  // fury
+  SPECS.PROTECTION_WARRIOR,
+  SPECS.FERAL_DRUID, // feral & guardian are singlehanded
+  SPECS.GUARDIAN_DRUID,
+  SPECS.BLOOD_DEATH_KNIGHT,
+  SPECS.UNHOLY_DEATH_KNIGHT,
+  SPECS.FROST_DEATH_KNIGHT, // 2h only
+  // range hunters
+  SPECS.SURVIVAL_HUNTER,
+  // rogues
+  // enh shaman
+  SPECS.BREWMASTER_MONK,
+  // windwalker
+  SPECS.MISTWEAVER_MONK
+]
 
 class Windfury extends Analyzer {
   swingResets: number = 0;
@@ -23,9 +54,9 @@ class Windfury extends Analyzer {
   constructor(options: Options) {
     super(options);
 
-    const active = (this.selectedCombatant.spec.role === ROLES.DPS.MELEE || this.selectedCombatant.spec.role === ROLES.TANK) 
-      && Boolean(this.selectedCombatant.mainHand.id)
-      && !this.selectedCombatant.offHand.id;
+    const isWhitelisted = Boolean(WHITELIST.find((spec: { id: number; }) => spec.id === this.selectedCombatant.specId)?.id)
+    const isUsing2h = Boolean(this.selectedCombatant.mainHand.id) && !this.selectedCombatant.offHand.id
+    const active = isWhitelisted && isUsing2h;
     this.active = active;
     if (!active) {
       return;
@@ -48,14 +79,14 @@ class Windfury extends Analyzer {
       return;
     }
 
-    // parries reduce the new swing timer by 40%, ignore melees after parries
+    // parries reduce the new swing timer by 40%, ignore melees after parries 
     if (this.lastParry) {
       this.lastParry = false
       return;
     }
 
     const diff = event.timestamp - this.lastSwing;
-    if (diff <= 200) {
+    if (diff <= WINDFURY_TRESHOLD) {
       this.swingResets += 1;
       this.swingResetDamage += event.amount + (event.absorb || 0);
     }
@@ -67,7 +98,7 @@ class Windfury extends Analyzer {
   }
 
   statistic() {
-    // we dont know beforehand if the player was in the shamans group
+    // show this stat for all specs whitelisted specs that had uptime
     if (this.uptime === 0) {
       return
     }
@@ -78,8 +109,8 @@ class Windfury extends Analyzer {
         size="flexible"
         tooltip={(
           <>
-            <strong>{this.swingResets}</strong> windfury procs (totem uptime was {formatPercentage(this.uptime)}%).<br/>
-            Tracking windfury at 100% accuracy is impossible as they just appear as additional melee hits, but tracking the time between melee swings and ignoring parry-hasted ones makes it fairly reliable.<br/>
+            <strong>{this.swingResets}</strong> windfury procs (totem uptime was {formatPercentage(this.uptime)}%).<br/><br/>
+            Tracking windfury at 100% accuracy is impossible as they just appear as additional melee hits, but tracking the time between melee swings and ignoring parry-hasted ones makes it a fairly reliable method.<br/>
           </>
         )}
       >
