9f4cdc6aec06c900e5878d2917287cd6fb27c2e5
Sharrq <cjgrasty86@gmail.com>
2020-12-19 23:19:49 -0600

    [Core] More Warning Messages
    Added warning messages for when Advanced Combat Logging is not enabled and if there are no dungeon/boss encounters in the log.

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -8,6 +8,7 @@ import { change, date } from 'common/changelog';
 
 // prettier-ignore
 export default [
+  change(date(2020, 12, 19), 'Add warnings for logs with Advanced Combat Logging disabled and when no encounters are found.', Sharrq),
   change(date(2020, 12, 19), <> Fix issues with <ItemLink id={ITEMS.INSCRUTABLE_QUANTUM_DEVICE.id} /> not accounting for the correct stat given. </>, Putro),
   change(date(2020, 12, 19), 'Adjust our weak Potion suggestion to only show if a weak potion was actually used', Putro),
   change(date(2020, 12, 17), 'Added phase information for Castle Nathria bosses', Sharrq),
