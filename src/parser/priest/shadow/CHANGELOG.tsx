fa9dbde02e45c9621fbae4de1dbe930b4f183dc1
Dylan Dirlam <dadirlam@hotmail.com>
2020-12-21 14:38:37 -0500

    Update CHANGELOG.tsx

--- src/parser/priest/shadow/CHANGELOG.tsx
+++ src/parser/priest/shadow/CHANGELOG.tsx
@@ -4,7 +4,8 @@ import { Abelito75, Adoraci, Sharrq, Zeboot } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2020, 12, 10), <>Corrected Power Infusion spell Id.</>, [Abelito75]),
+  change(date(2020, 12, 21), <>Corrected spell cooldowns and ID changes from launch.</>, [Adoraci]),
+  change(date(2020, 12, 10), <>Corrected Power Infusion spell ID.</>, [Abelito75]),
   change(date(2020, 10, 23), <>Update example log to more recent one.</>, [Adoraci]),
   change(date(2020, 10, 18), <>Converted legacy listeners to new event filters</>, Zeboot),
   change(date(2020, 10, 17), <>Updated for Shadowlands Pre-Patch.</>, [Adoraci]),
