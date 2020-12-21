f0e643c1f098ff801080d4c4f849188c3e39860d
Matthew Abbott <abbottmg@me.com>
2020-12-21 17:23:23 -0600

    Update EW event listener to find absorb events

--- src/parser/druid/guardian/modules/talents/Earthwarden.js
+++ src/parser/druid/guardian/modules/talents/Earthwarden.js
@@ -54,7 +54,7 @@ class Earthwarden extends Analyzer {
     super(...args);
     this.active = this.selectedCombatant.lv45Talent === SPELLS.EARTHWARDEN_TALENT.id;
     this.addEventListener(Events.damage.to(SELECTED_PLAYER).spell(ABILITIES_THAT_CONSUME_EW), this.onDamage);
-    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.EARTHWARDEN_BUFF), this.onAbsorbed);
+    this.addEventListener(Events.absorbed.by(SELECTED_PLAYER).spell(SPELLS.EARTHWARDEN_BUFF), this.onAbsorbed);
   }
 
   onDamage(event) {
