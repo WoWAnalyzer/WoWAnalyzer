4a6d6104c90a5428beac573591f7cd3d54b307a3
flurreN <Philipsabri@gmail.com>
2020-12-23 23:53:56 +0100

    Added myself as contributer and updated CHANGELOG
    Signed-off-by: flurreN <Philipsabri@gmail.com>

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -1,6 +1,7 @@
-import { LeoZhekov } from 'CONTRIBUTORS';
+import { LeoZhekov, flurreN } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
   change(date(2020, 10, 30), 'Updated the deprecated StatisticBox elements with the new Statistic ones.', [LeoZhekov]),
+  change(date(2020, 12, 23), 'Updated spells and talents for SL', [flurreN]),
 ];
