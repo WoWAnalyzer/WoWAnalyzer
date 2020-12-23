f903558223a50e8085265de3fb053bbe66fdee9c
flurreN <philipsabri@gmail.com>
2020-12-23 03:30:26 +0100

    Updating Glaive Tempest parse for DH havoc
    Signed-off-by: flurreN <philipsabri@gmail.com>

--- src/parser/demonhunter/havoc/modules/talents/GlaiveTempest.js
+++ src/parser/demonhunter/havoc/modules/talents/GlaiveTempest.js
@@ -1,7 +1,50 @@
+import React from 'react';
+import SPELLS from 'common/SPELLS';
+import TalentStatisticBox from 'interface/others/TalentStatisticBox';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
+import { formatThousands, formatPercentage } from 'common/format';
+import Events from 'parser/core/Events';
+
 
 class GlaiveTempest extends Analyzer {
 
+    damage = 0;
+
+    constructor(...args) {
+        super(...args);
+        this.active = this.selectedCombatant.hasTalent(SPELLS.GLAIVE_TEMPEST_TALENT.id);
+        if (!this.active) {
+          return;
+        }
+        this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.GLAIVE_TEMPEST_DAMAGE), this.onDamageEvent);
+      }
+    
+
+    onDamageEvent(event) {
+        this.damage += event.amount;
+      }
+
+    statistic() {
+        return (
+          <TalentStatisticBox
+            talent={SPELLS.GLAIVE_TEMPEST_TALENT.id}
+            position={STATISTIC_ORDER.OPTIONAL(6)}
+            value={(
+                <>
+                  {this.owner.formatItemDamageDone(this.damage)}
+                </>
+            )}
+            tooltip={(
+                <>
+                  {formatThousands(this.damage)} Total damage<br />
+                </>
+            )}
+          />
+        );
+    }    
 }
+
+
 
 export default GlaiveTempest;
