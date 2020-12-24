d09647793e30d0724613dda254776a26f440a4fd
flurreN <Philipsabri@gmail.com>
2020-12-24 08:33:38 +0100

    Changelog

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -2,6 +2,7 @@ import { LeoZhekov, flurreN } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 12, 24), 'Updated spells and talents for SL', [flurreN]),
   change(date(2020, 12, 23), 'Updated spells and talents for SL', [flurreN]),
   change(date(2020, 10, 30), 'Updated the deprecated StatisticBox elements with the new Statistic ones.', [LeoZhekov]),
 ];
