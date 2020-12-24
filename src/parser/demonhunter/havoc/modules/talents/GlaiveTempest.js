db7aeaf8ca4f6f6f464c69516d075d93c2808c8d
flurreN <Philipsabri@gmail.com>
2020-12-24 22:17:57 +0100

    Switched out depricated code to the new Statistic code, and cleaned up some spacing

--- src/parser/demonhunter/havoc/modules/talents/GlaiveTempest.js
+++ src/parser/demonhunter/havoc/modules/talents/GlaiveTempest.js
@@ -1,46 +1,49 @@
 import React from 'react';
 import SPELLS from 'common/SPELLS';
-import TalentStatisticBox from 'interface/others/TalentStatisticBox';
+//import TalentStatisticBox from 'interface/others/TalentStatisticBox';
+import Statistic from 'interface/statistics/Statistic';
+import STATISTIC_CATEGORY from 'interface/others/STATISTIC_CATEGORY';
+import BoringSpellValueText from 'interface/statistics/components/BoringSpellValueText';
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import STATISTIC_ORDER from 'interface/others/STATISTIC_ORDER';
 import { formatThousands } from 'common/format';
 import Events from 'parser/core/Events';
 
 class GlaiveTempest extends Analyzer {
 
-    damage = 0;
+  damage = 0;
 
-    constructor(...args) {
-        super(...args);
-        this.active = this.selectedCombatant.hasTalent(SPELLS.GLAIVE_TEMPEST_TALENT.id);
-        if (!this.active) {
-          return;
-        }
-        this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.GLAIVE_TEMPEST_DAMAGE), this.onDamageEvent);
-      }
+  constructor(...args) {
+    super(...args);
+    this.active = this.selectedCombatant.hasTalent(SPELLS.GLAIVE_TEMPEST_TALENT.id);
+    if (!this.active) {
+      return;
+    }
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.GLAIVE_TEMPEST_DAMAGE), this.onDamageEvent);
+  }
 
-    onDamageEvent(event) {
-      this.damage += event.amount + (event.absorb || 0);
-      }
+  onDamageEvent(event) {
+    this.damage += event.amount + (event.absorb || 0);
+  }
 
-    statistic() {
-        return (
-          <TalentStatisticBox
-            talent={SPELLS.GLAIVE_TEMPEST_TALENT.id}
-            position={STATISTIC_ORDER.OPTIONAL(6)}
-            value={(
-                <>
-                  {this.owner.formatItemDamageDone(this.damage)}
-                </>
-            )}
-            tooltip={(
-                <>
-                  {formatThousands(this.damage)} Total damage<br />
-                </>
-            )}
-          />
-        );
-    }    
+  statistic() {
+    return (
+      <Statistic
+        size="flexible"
+        category={STATISTIC_CATEGORY.TALENTS}
+        tooltip={(
+          <>
+            {formatThousands(this.damage)} Total damage<br />
+          </>
+      )}
+      >
+        <BoringSpellValueText spell={SPELLS.GLAIVE_TEMPEST_TALENT}>
+          <>
+          {this.owner.formatItemDamageDone(this.damage)}
+          </>
+        </BoringSpellValueText>
+      </Statistic>
+    );
+  }
 }
 
 export default GlaiveTempest;
