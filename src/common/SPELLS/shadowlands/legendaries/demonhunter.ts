6dca8b7dec3255552825592a9a7ba607345aae9b
flurreN <Philipsabri@gmail.com>
2020-12-26 22:01:57 +0100

    Added BonusID for general and havoc specific legendary spells

--- src/common/SPELLS/shadowlands/legendaries/demonhunter.ts
+++ src/common/SPELLS/shadowlands/legendaries/demonhunter.ts
@@ -1,28 +1,30 @@
 import { LegendarySpell, SpellList } from "common/SPELLS/Spell";
 
-//BonusID:?
-
 const legendaries: SpellList<LegendarySpell> = {
   //region Havoc
   CHAOS_THEORY: {
     id: 337551,
     name: 'Chaos Theory',
     icon: 'inv_glaive_1h_artifactaldrochi_d_03dual',
+    bonusID: 7050,
   },
   BURNING_WOUND: {
     id: 346279,
     name: 'Burning Wound',
     icon: 'spell_fire_felhellfire',
+    bonusID: 7219,
   }, 
   DARKER_NATURE: {
     id: 346264,
     name: 'Darker Nature',
     icon: 'ability_demonhunter_eyebeam',
+    bonusID: 7218,
   }, 
   ERRATIC_FEL_CORE: {
     id: 337685,
     name: 'Erratic Fel Core',
     icon: 'inv_archaeology_70_crystallineeyeofundravius',
+    bonusID: 7051,
   }, 
   //endregion
 
@@ -31,21 +33,25 @@ const legendaries: SpellList<LegendarySpell> = {
     id: 337545,
     name: 'Fel Flame Fortification',
     icon: 'spell_fire_felfire',
+    bonusID: 7047,
   },
   FIERY_SOUL: {
     id: 337547,
     name: 'Fiery Soul',
     icon: 'inv__felbarrage',
+    bonusID: 7048,
   }, 
   RAZELIKHS_DEFILEMENT: {
     id: 337544,
     name: 'Razelikh\'s Defilement',
     icon: 'ability_demonhunter_concentratedsigils',
+    bonusID: 7046,
   }, 
   SPIRIT_OF_THE_DARKNESS_FLAME: {
     id: 337541,
     name: 'Spirit of the Darkness Flame',
     icon: 'inv_misc_head_dragon_blue_nightmare',
+    bonusID: 7045,
   }, 
   //endregion
 
@@ -54,21 +60,25 @@ const legendaries: SpellList<LegendarySpell> = {
     id: 337504,
     name: 'Collective Anguish',
     icon: 'artifactability_havocdemonhunter_anguishofthedeceiver',
+    bonusID: 7041,
   },
   DARKEST_HOUR: {
     id: 337539,
     name: 'Darkest Hour',
     icon: 'ability_demonhunter_darkness',
+    bonusID: 7044,
   }, 
   DARKGLARE_BOON: {
     id: 337534,
     name: 'Darkglare Boon',
     icon: 'inv_jewelry_necklace_53',
+    bonusID: 7043,
   }, 
   FEL_BOMBARDMENT: {
     id: 337775,
     name: 'Fel Bombardment',
     icon: 'inv_misc_head_dragon_blue_nightmare',
+    bonusID: 7052,
   }, 
   //endregion
 } as const;
