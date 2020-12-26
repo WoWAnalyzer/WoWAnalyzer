a2ab6b153950a21c2d9d4c4c665224ef01a81927
flurreN <Philipsabri@gmail.com>
2020-12-26 22:08:18 +0100

    lint

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -5,7 +5,7 @@ import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 
 export default [
-  change(date(2020, 12, 26, 'Legendary, Covenant and Conduits added for Havoc', [flurreN]),
+  change(date(2020, 12, 26), 'Legendary, Covenant and Conduits added for Havoc', [flurreN]),
   change(date(2020, 12, 26), <><SpellLink id={SPELLS.NETHERWALK_TALENT.id} /> and <SpellLink id={SPELLS.DARKNESS.id} /> now have a correct gcd.</>, flurreN),
   change(date(2020, 12, 24), 'Updated CDs and baselines for SL', [flurreN]),
   change(date(2020, 12, 23), 'Updated spells and talents for SL', [flurreN]),
