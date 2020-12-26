c0490a43f2275836c9e6c33e476248cedc1a4e61
flurreN <Philipsabri@gmail.com>
2020-12-26 04:17:28 +0100

    Removed code that made it look wrong in timeline

--- src/parser/demonhunter/havoc/modules/core/Channeling.tsx
+++ src/parser/demonhunter/havoc/modules/core/Channeling.tsx
@@ -1,45 +1,16 @@
 import SPELLS from 'common/SPELLS';
 import CoreChanneling from 'parser/shared/modules/Channeling';
-import Events, {RemoveBuffEvent, ApplyBuffEvent, CastEvent} from 'parser/core/Events';
+import Events, {RemoveBuffEvent, CastEvent} from 'parser/core/Events';
 import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-
 
 class Channeling extends CoreChanneling {
 
   constructor(options: Options) {
     super(options);
-    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER), this.onApplyBuff);
     this.addEventListener(Events.removebuff.by(SELECTED_PLAYER), this.onRemoveBuff);
   }
 
-  cancelChannel(event: any, ability: any) {
-    if (this.isChannelingSpell(SPELLS.EYE_BEAM.id)) {
-      this.endChannel(event);
-    } else {
-      super.cancelChannel(event, ability);
-    }
-  }
-
-  //Eye beam with the Meta buff doesn't get caught by applybuff for some reason so we also include the on cast
-
-  //Eye Beam w/o the meta buff
-  onApplyBuff(event: ApplyBuffEvent) {
-    if (!this.selectedCombatant.hasBuff(SPELLS.METAMORPHOSIS_HAVOC_BUFF.id)) {
-      return;
-    }
-    const spellId = event.ability.guid;
-    if (spellId === SPELLS.EYE_BEAM.id) {
-      this.beginChannel(event);
-      return;
-    }
-    super.onCast(event);
-  }
-
-  //Eye beam with the meta buff
   onCast(event: CastEvent) {
-    if (this.selectedCombatant.hasBuff(SPELLS.METAMORPHOSIS_HAVOC_BUFF.id)) {
-      return;
-    }
     const spellId = event.ability.guid;
     if (spellId === SPELLS.EYE_BEAM.id) {
       this.beginChannel(event);
@@ -58,6 +29,14 @@ class Channeling extends CoreChanneling {
       return;
     }
     this.endChannel(event);
+  }
+
+  cancelChannel(event: any, ability: any) {
+    if (this.isChannelingSpell(SPELLS.EYE_BEAM.id)) {
+      this.endChannel(event);
+    } else {
+      super.cancelChannel(event, ability);
+    }
   }
 }
 
