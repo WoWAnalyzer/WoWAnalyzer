2984a443841613c0c0f4d9433b044db256592b3b
Taylor Wilkinson <57736014+tjw87912@users.noreply.github.com>
2020-12-27 13:18:32 -0500

    add changelog entry

--- src/parser/demonhunter/vengeance/CHANGELOG.js
+++ src/parser/demonhunter/vengeance/CHANGELOG.js
@@ -2,6 +2,7 @@ import { Zeboot, LeoZhekov, TurianSniper } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 12, 27), 'Initial SL update for talent changes and covenant abilities', TurianSniper),
   change(date(2020, 12, 25), 'Fix variable name for Immolation Aura', TurianSniper),
   change(date(2020, 10, 30), 'Replaced the deprecated StatisticBox with the new Statistic', LeoZhekov),
   change(date(2020, 10, 18), 'Converted legacy listeners to new event filters', Zeboot),
