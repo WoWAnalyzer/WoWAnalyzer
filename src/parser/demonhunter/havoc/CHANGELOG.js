90018d41df6bb75145c739464ded19d5bb371bf1
flurreN <Philipsabri@gmail.com>
2020-12-28 01:58:25 +0100

    Changelog

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import { LeoZhekov, flurreN } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'common/SpellLink';
 
 export default [
-  change(date(2020, 12, 24), 'Glaive Tempest added to the parser and are shown in Talent statistics', [flurreN]),
+  change(date(2020, 12, 28), <>Added support for <SpellLink id={SPELLS.GLAIVE_TEMPEST_TALENT.id} /> </>, flurreN),
   change(date(2020, 12, 23), 'Updated spells and talents for SL', [flurreN]),
   change(date(2020, 10, 30), 'Updated the deprecated StatisticBox elements with the new Statistic ones.', [LeoZhekov]),
 ];
