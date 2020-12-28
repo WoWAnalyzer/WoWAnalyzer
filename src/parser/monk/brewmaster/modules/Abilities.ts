37726538b3f1c2e5c9eb707ee39043faadacff5f
Joey Bolduc-Gilbert <JoeyBG@gmail.com>
2020-12-28 16:17:58 -0500

    Fix black ox brew crash

--- src/parser/monk/brewmaster/modules/Abilities.ts
+++ src/parser/monk/brewmaster/modules/Abilities.ts
@@ -10,7 +10,7 @@ class Abilities extends CoreAbilities {
       {
         spell: SPELLS.KEG_SMASH,
         category: Abilities.SPELL_CATEGORIES.ROTATIONAL,
-        cooldown: haste => 8 / (1 + haste),
+        cooldown: (haste) => 8 / (1 + haste),
         charges: 1,
         castEfficiency: {
           suggestion: true,
@@ -50,7 +50,7 @@ class Abilities extends CoreAbilities {
       {
         spell: SPELLS.RUSHING_JADE_WIND_TALENT_BREWMASTER,
         category: Abilities.SPELL_CATEGORIES.ROTATIONAL,
-        cooldown: haste => 6 / (1 + haste),
+        cooldown: (haste) => 6 / (1 + haste),
         enabled: combatant.hasTalent(SPELLS.RUSHING_JADE_WIND_TALENT_BREWMASTER.id),
         gcd: {
           static: 1000,
@@ -83,7 +83,8 @@ class Abilities extends CoreAbilities {
       {
         spell: SPELLS.PURIFYING_BREW,
         category: Abilities.SPELL_CATEGORIES.DEFENSIVE,
-        cooldown: haste => (combatant.hasTalent(SPELLS.LIGHT_BREWING_TALENT) ? 20 : 16) / (1 + haste),
+        cooldown: (haste) =>
+          (combatant.hasTalent(SPELLS.LIGHT_BREWING_TALENT) ? 16 : 20) / (1 + haste),
         charges: 2,
         gcd: null,
         castEfficiency: {
@@ -94,7 +95,7 @@ class Abilities extends CoreAbilities {
       {
         spell: SPELLS.CELESTIAL_BREW,
         category: Abilities.SPELL_CATEGORIES.DEFENSIVE,
-        cooldown: combatant.hasTalent(SPELLS.LIGHT_BREWING_TALENT) ? 60 : 48,
+        cooldown: combatant.hasTalent(SPELLS.LIGHT_BREWING_TALENT) ? 48 : 60,
         gcd: {
           static: 1000,
         },
