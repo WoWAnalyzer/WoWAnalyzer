fe86ec221203d6ae3b1601c2308f6ca4b25a6827
Ryan D <radean0909@gmail.com>
2020-12-27 17:01:11 -0700

    updated Abilities requirements for Sigil of Flame based on covenant and legendary

--- src/parser/demonhunter/vengeance/CHANGELOG.js
+++ src/parser/demonhunter/vengeance/CHANGELOG.js
@@ -2,8 +2,7 @@ import { Zeboot, LeoZhekov, TurianSniper, Geeii } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2020, 12, 27), 'Updated Soul Cleave tracking', Geeii),
-  change(date(2020, 12, 27), 'Updated to use Fury resource, instead of outdated Pain', Geeii),
+  change(date(2020, 12, 27), 'Updated to use Fury resource, instead of outdated Pain. Updated Soul Cleave reporting, updated ability tracking for Sigil of Flame for some cases', Geeii),
   change(date(2020, 12, 27), 'Initial SL update for talent changes and covenant abilities', TurianSniper),
   change(date(2020, 12, 25), 'Fix variable name for Immolation Aura', TurianSniper),
   change(date(2020, 10, 30), 'Replaced the deprecated StatisticBox with the new Statistic', LeoZhekov),
