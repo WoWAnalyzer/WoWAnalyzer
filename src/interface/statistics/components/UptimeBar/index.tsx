432eccee4c7b29aaa68b49c8d60509c0f7b75dc9
Joey Bolduc-Gilbert <JoeyBG@gmail.com>
2020-12-30 16:35:33 -0500

    Convert StatisticBars & UptimeBar to TS

---
+++ src/interface/statistics/components/UptimeBar/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './UptimeBar.scss';
+
+type Uptime = {
+  start: number;
+  end: number;
+};
+
+type Props = {
+  uptimeHistory: Uptime[];
+  start: number;
+  end: number;
+};
+
+const UptimeBar = ({ uptimeHistory, start: fightStart, end: fightEnd }: Props) => {
+  const fightDuration = fightEnd - fightStart;
+
+  return (
+    <div className="uptime-bar">
+      {uptimeHistory.map((buff) => {
+        const start = buff.start;
+        const end = buff.end !== null ? buff.end : fightEnd;
+
+        return (
+          <div
+            key={`${start}-${end}`}
+            style={{
+              left: `${((start - fightStart) / fightDuration) * 100}%`,
+              width: `${((end - start) / fightDuration) * 100}%`,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default UptimeBar;
