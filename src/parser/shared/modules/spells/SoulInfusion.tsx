a37664aefc52048469d0766e39600b8e23bdfc65
flurreN <Philipsabri@gmail.com>
2020-12-31 04:51:55 +0100

    Removed updated test and rewrote channeld code

--- src/parser/shared/modules/spells/SoulInfusion.tsx
+++ src/parser/shared/modules/spells/SoulInfusion.tsx
@@ -1,42 +1,23 @@
 import SPELLS from 'common/SPELLS';
 import CoreChanneling from 'parser/shared/modules/Channeling';
-import Events, {RemoveBuffEvent, CastEvent} from 'parser/core/Events';
+import Events, {RemoveBuffEvent, CastEvent, BeginChannelEvent, EndChannelEvent} from 'parser/core/Events';
 import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 
 class SoulInfusion extends CoreChanneling {
 
   constructor(options: Options) {
     super(options);
-    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER), this.onRemoveBuff);
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SPELLS.SOUL_INFUSION), this.onBeginChannel);
+    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.SOUL_INFUSION), this.onRemoveBuff);
   }
 
-  onCast(event: CastEvent) {
-    const spellId = event.ability.guid;
-    if (spellId === SPELLS.SOUL_INFUSION.id) {
-      this.beginChannel(event);
-      return;
-    }
-    super.onCast(event);
+  onBeginChannel(event: CastEvent) {
+    this.beginChannel(event);
   }
 
   onRemoveBuff(event: RemoveBuffEvent) {
-    const spellId = event.ability.guid;
-    if (spellId !== SPELLS.SOUL_INFUSION.id) {
-      return;
-    }
-    if (!this.isChannelingSpell(SPELLS.SOUL_INFUSION.id)) {
-      // This may be true if we did the event-order fix in begincast/cast and it was already ended there.
-      return;
-    }
+    //Buff will be removed if channeld is canceled 
     this.endChannel(event);
-  }
-
-  cancelChannel(event: any, ability: any) {
-    if (this.isChannelingSpell(SPELLS.SOUL_INFUSION.id)) {
-      this.endChannel(event);
-    } else {
-      super.cancelChannel(event, ability);
-    }
   }
 }
 
