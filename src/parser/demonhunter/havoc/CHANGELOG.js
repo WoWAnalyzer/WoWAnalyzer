15215dcfe6dfc80ac21520ce3dcf2305c07d3c62
flurreN <Philipsabri@gmail.com>
2021-01-09 12:52:48 +0100

    Something broke changelog - fixed

--- src/parser/demonhunter/havoc/CHANGELOG.js
+++ src/parser/demonhunter/havoc/CHANGELOG.js
@@ -5,10 +5,10 @@ import SPELLS from 'common/SPELLS';
 import SpellLink from 'common/SpellLink';
 
 export default [
-  change(date(2021, 1, 9), <>Suggestions for <SpellLink id={SPELLS.DEMONIC_TALENT_HAVOC.id} />, <SpellLink id={SPELLS.BLADE_DANCE.id} /> and <SpellLink id={SPELLS.DEATH_SWEEP.id} /> have been updated</>, [flurreN]),
-  change(date(2021, 1, 6), <>Added <SpellLink id={SPELLS.ELYSIAN_DECREE.id} /> to Statistics</>, [flurreN]),
-  change(date(2020, 12, 30), <>Added <SpellLink id={SPELLS.SINFUL_BRAND.id} /> and <SpellLink id={SPELLS.THE_HUNT.id}/> to Statistics</>, [flurreN]),
-  change(date(2020, 12, 29), <>Fixed bug where <SpellLink id={SPELLS.DEMONIC_TALENT_HAVOC.id} /> were causing errors</>, [flurreN]),
+  change(date(2021, 1, 9), <>Suggestions for <SpellLink id={SPELLS.DEMONIC_TALENT_HAVOC.id} />, <SpellLink id={SPELLS.BLADE_DANCE.id} /> and <SpellLink id={SPELLS.DEATH_SWEEP.id} /> have been updated</>, flurreN),
+  change(date(2021, 1, 6), <>Added <SpellLink id={SPELLS.ELYSIAN_DECREE.id} /> to Statistics</>, flurreN),
+  change(date(2020, 12, 30), <>Added <SpellLink id={SPELLS.SINFUL_BRAND.id} /> and <SpellLink id={SPELLS.THE_HUNT.id}/> to Statistics</>, flurreN),
+  change(date(2020, 12, 29), <>Fixed bug where <SpellLink id={SPELLS.DEMONIC_TALENT_HAVOC.id} /> were causing errors</>, flurreN),
   change(date(2020, 12, 28), <><SpellLink id={SPELLS.COLLECTIVE_ANGUISH.id} /> and <SpellLink id={SPELLS.CHAOS_THEORY.id} /> is added to Statistics</>, flurreN),
   change(date(2020, 12, 28), <><SpellLink id={SPELLS.FELBLADE_TALENT.id} /> is now shown correctly in timeline and more information added to Statistic box</>, flurreN),
   change(date(2020, 12, 28), 'Legendary, Covenant and Conduits added for Havoc', flurreN),
