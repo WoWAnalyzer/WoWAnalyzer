de3a5a06762fc39d2695ddc01172c343c6b3ccf8
Putro <laustsf@gmail.com>
2021-01-16 12:41:11 +0100

    Revert "[Core] Update the warning displayed when we don't find any logs"
    This reverts commit b55a7cacd8e797d867c316c3a731a9e889e7b47e.

--- src/interface/report/EncountersNotFoundWarning.tsx
+++ src/interface/report/EncountersNotFoundWarning.tsx
@@ -8,7 +8,7 @@ const EncountersNotFoundWarning = () => (
       <Warning style={{ marginBottom: 30 }}>
         <h2><Trans id="interface.report.encountersNotFoundWarning.noEncountersFound">No Encounters Found</Trans></h2>
         <Trans id="interface.report.encountersNotFoundWarning.noEncountersFoundDetails">
-          We were unable to find any encounters in this report. If Warcraft Logs shows a dungeon or boss encounter on this report, then try refreshing this page by pressing the refresh button in the top right of the page to re-pull the report from Warcraft Logs. Additionally, please note that due to the way combat logs work, we are unable to evaluate Target Dummy logs.
+          We were unable to find any encounters in this report. If Warcraft Logs shows a dungeon or boss encounter on this report, then try refreshing this page (Press F5) to re-pull the report from Warcraft Logs. Additionally, please note that due to the way combat logs work, we are unable to evaluate Target Dummy logs.
         </Trans>
       </Warning>
     </div>
