1c14c5278ff89467d40d3d356958b722059d90ec
tiborhercz <tibor@hercz.nl>
2021-01-16 21:31:05 +0100

    update changelog message

--- src/parser/druid/feral/CHANGELOG.js
+++ src/parser/druid/feral/CHANGELOG.js
@@ -4,7 +4,7 @@ import { Abelito75, Zeboot, LeoZhekov, Tora, Xcessiv, Tiboonn } from 'CONTRIBUTO
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2021, 1, 16), <>Add class conduits</>, Tiboonn),
+  change(date(2021, 1, 16), <>Added spell information for conduits</>, Tiboonn),
   change(date(2020, 12, 15), <>Update Bloodtalons for Shadowlands</>, Tora),
   change(date(2020, 12, 14), <>Corrected tracking of Berserk cooldown usage.</>, Xcessiv),
   change(date(2020, 11, 6), <>Replaced the deprecated StatisticBoxes with the new Statistic and reworked SavageRoar module.</>, LeoZhekov),
