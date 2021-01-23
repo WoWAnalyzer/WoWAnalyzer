8bf14c25f8fe89835588e617eec6fe78950430f6
Martijn Hols <git@martijnhols.nl>
2021-01-23 15:28:28 +0100

    Fix conflict

--- src/parser/warrior/arms/modules/core/Execute/EarlyDotRefresh.js
+++ src/parser/warrior/arms/modules/core/Execute/EarlyDotRefresh.js
@@ -1,6 +1,6 @@
 import SPELLS from 'common/SPELLS';
 import React from 'react';
-import SpellLink from 'common/SpellLink';
+import { SpellLink } from 'interface';
 import { t } from '@lingui/macro';
 import { formatPercentage } from 'common/format';
 
