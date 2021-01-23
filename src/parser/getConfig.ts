c5dd1347cdb397ca7e97a5c8e618033867edd1c0
Martijn Hols <git@martijnhols.nl>
2021-01-23 14:47:33 +0100

    Move getConfig to parser

---
+++ src/parser/getConfig.ts
@@ -0,0 +1,19 @@
+import AVAILABLE_CONFIGS from 'parser';
+
+export default function getConfig(specId: number) {
+  const config = AVAILABLE_CONFIGS.find((config) => config.spec.id === specId);
+  //find visible builds, if any exist
+  const activeBuilds =
+    config.builds && Object.keys(config.builds).filter((b) => config.builds[b].visible);
+  //remove all inactive builds
+  config.builds =
+    (activeBuilds &&
+      activeBuilds.length > 0 &&
+      activeBuilds.reduce((obj, key) => {
+        obj[key] = config.builds[key];
+        return obj;
+      }, {})) ||
+    undefined;
+
+  return config;
+}
