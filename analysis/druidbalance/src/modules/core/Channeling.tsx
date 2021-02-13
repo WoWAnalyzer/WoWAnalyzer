5bde0c232887396ffbb8d79d7aa509d90356a4db
Kartarn <deraluf@googlemail.com>
2021-02-13 23:24:34 +0100

    Fixed ESLint errors

--- analysis/druidbalance/src/modules/core/Channeling.tsx
+++ analysis/druidbalance/src/modules/core/Channeling.tsx
@@ -1,14 +1,8 @@
 import SPELLS from 'common/SPELLS';
 import CoreChanneling from 'parser/shared/modules/Channeling';
-import Events, { CastEvent, RemoveBuffEvent } from 'parser/core/Events';
-import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import { CastEvent } from 'parser/core/Events';
 
 class Channeling extends CoreChanneling {
-
-  constructor(options: Options){
-    super(options);
-  }
-
   onCast(event: CastEvent) {
     const spellId = event.ability.guid;
     if (spellId === SPELLS.CONVOKE_SPIRITS.id) {
@@ -25,7 +19,6 @@ class Channeling extends CoreChanneling {
     }
     super.cancelChannel(event, ability);
   }
-
 }
 
 export default Channeling;
--- analysis/druidbalance/src/modules/features/Buffs.tsx
+++ analysis/druidbalance/src/modules/features/Buffs.tsx
@@ -4,39 +4,37 @@ import CoreBuffs from 'parser/core/modules/Buffs';
 
 class Buffs extends CoreBuffs {
   buffs() {
-    const combatant = this.selectedCombatant;
-
     // This should include ALL buffs that can be applied by your spec.
     // This data can be used by various kinds of modules to improve their results, and modules added in the future may rely on buffs that aren't used today.
     return [
-        {
-            spellId: SPELLS.CELESTIAL_ALIGNMENT.id,
-            timelineHighlight: true,
-        },
-        {
-            spellId: SPELLS.WILD_CHARGE_TALENT.id,
-            timelineHighlight: false,
-        },
-        {
-            spellId: SPELLS.BALANCE_OF_ALL_THINGS.id,
-            timelineHighlight: true,
-        },
-        {
-            spellId: SPELLS.OWLKIN_FRENZY.id,
-            timelineHighlight: true,
-        },
-        {
-            spellId: SPELLS.BARKSKIN.id,
-            timelineHighlight: false,
-        },
-        {
-            spellId: SPELLS.STARLORD.id,
-            timelineHighlight: false,
-        },
-        {
-            spellId: Object.keys(BLOODLUST_BUFFS).map(item => Number(item)),
-            timelineHighlight: true,
-        },
+      {
+        spellId: SPELLS.CELESTIAL_ALIGNMENT.id,
+        timelineHighlight: true,
+      },
+      {
+        spellId: SPELLS.WILD_CHARGE_TALENT.id,
+        timelineHighlight: false,
+      },
+      {
+        spellId: SPELLS.BALANCE_OF_ALL_THINGS.id,
+        timelineHighlight: true,
+      },
+      {
+        spellId: SPELLS.OWLKIN_FRENZY.id,
+        timelineHighlight: true,
+      },
+      {
+        spellId: SPELLS.BARKSKIN.id,
+        timelineHighlight: false,
+      },
+      {
+        spellId: SPELLS.STARLORD.id,
+        timelineHighlight: false,
+      },
+      {
+        spellId: Object.keys(BLOODLUST_BUFFS).map((item) => Number(item)),
+        timelineHighlight: true,
+      },
     ];
   }
 }
