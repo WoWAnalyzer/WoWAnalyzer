f46e77a24760f1a8944a5e96367b187cfc7bb55d
Brian Powell <brian.powell@rocketmail.com>
2021-02-18 16:57:41 -0500

    Added Conduits, Soulbinds and legendary spells for guardian druids

--- src/common/SPELLS/shadowlands/soulbinds/generaldraven.ts
+++ src/common/SPELLS/shadowlands/soulbinds/generaldraven.ts
@@ -1,4 +1,44 @@
 const soulbindPowers = {
+    SERVICE_IN_STONE: {
+        id: 340159,
+        name: 'Service In Stone',
+        icon: 'inv_misc_reforgedarchstone_01',
+    },
+    MOVE_AS_ONE: {
+        id: 319982,
+        name: 'Move As One',
+        icon: 'spell_holy_prayerofhealing',
+    },
+    ENDURING_GLOOM: {
+        id: 319976,
+        name: 'Enduring Gloom',
+        icon: 'ability_garrosh_touch_of_yshaarj',
+    },
+    UNBREAKABLE_BODY: {
+        id: 332755,
+        name: 'Unbreakable Body',
+        icon: 'inv_chest_mail_revendreth_d_01',
+    },
+    EXPEDITION_LEADER: {
+        id: 332756,
+        name: 'Expedition Leader',
+        icon: 'spell_misc_warsongfocus',
+    },
+    HOLD_YOUR_GROUND: {
+        id: 332754,
+        name: 'Hold Your Ground',
+        icon: 'achievement_bg_killxenemies_generalsroom',
+    },
+    SUPERIOR_TACTICS: {
+        id: 332753,
+        name: 'Superior Tactics',
+        icon: 'inv_inscription_80_warscroll_battleshout',
+    },
+    BUILT_FOR_WAR: {
+        id: 319973,
+        name: 'Built For War',
+        icon: 'ability_warrior_intensifyrage',
+    },
 
 } as const;
 export default soulbindPowers;
