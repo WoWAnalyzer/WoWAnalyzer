b30797fa625597a31521715ec02f41bc6981b7b3
Brian Powell <brian.powell@rocketmail.com>
2021-02-19 22:52:04 -0500

    runtime error
    Fixed runtime error with Combat Meditation

--- src/common/SPELLS/shadowlands/soulbinds/pelagos.ts
+++ src/common/SPELLS/shadowlands/soulbinds/pelagos.ts
@@ -1,4 +1,14 @@
 const soulbindPowers = {
+  COMBAT_MEDITATION_TRIGGER: {
+    id: 328917,
+    name: 'Combat Meditation',
+    icon: 'spell_holy_spellwarding',
+  },
+  COMBAT_MEDITATION_PICK_UP: {
+    id: 328913,
+    name: 'Combat Meditation',
+    icon: 'spell_holy_spellwarding',
+  },
   COMBAT_MEDITATION: {
     id: 328908,
     name: 'Combat Meditation',
