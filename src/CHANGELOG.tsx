2b19361ecc21e6a65d0c235dbb5f8160731bf8c3
alex <acornellier@drw.com>
2021-02-25 18:12:43 -0500

    changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -9,6 +9,7 @@ import { change, date } from 'common/changelog';
 
 // prettier-ignore
 export default [
+  change(date(2021, 2, 25), 'Converted various components to functional components in TypeScript.', acornellier),
   change(date(2021, 2, 24), <>Fixed a crash triggered by <SpellLink id={SPELLS.CONVOKE_SPIRITS.id} /> casting 0 spells</>, acornellier),
   change(date(2021, 2, 14), <>Update <ItemLink id={ITEMS.VANTUS_RUNE_CASTLE_NATHRIA.id} /> versatility value.</>, Adoraci),
   change(date(2021, 1, 20), 'Rework spec support: automatically mark specs as unsupported when patch does not match the game and added a toggle to mark a spec with partial support.', Zerotorescue),
