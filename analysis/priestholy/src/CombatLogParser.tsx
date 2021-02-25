d8322a8206a655363171d77810650aecb555bb35
alex <acornellier@drw.com>
2021-02-25 15:13:07 -0500

    [hpriest] remove spreadsheet tab

--- analysis/priestholy/src/CombatLogParser.tsx
+++ analysis/priestholy/src/CombatLogParser.tsx
@@ -63,8 +63,6 @@ import DivineImage from './modules/shadowlands/items/DivineImage';
 // Conduits
 import ResonantWords from './modules/shadowlands/conduits/ResonantWords';
 
-import HolyPriestSpreadsheet from './HolyPriestSpreadsheet';
-
 class CombatLogParser extends CoreCombatLogParser {
   static abilitiesAffectedByHealingIncreases = ABILITIES_AFFECTED_BY_HEALING_INCREASES;
 
@@ -156,24 +154,6 @@ class CombatLogParser extends CoreCombatLogParser {
     boonOfTheAscended: BoonOfTheAscended,
     faeGuardians: FaeGuardians,
   };
-
-  generateResults(adjustForDowntime: boolean) {
-    const results = super.generateResults(adjustForDowntime);
-
-    results.tabs = [
-      ...results.tabs,
-      {
-        title: 'Player Log Data',
-        url: 'player-log-data',
-        render: () => (
-          <Panel style={{ padding: '15px 22px 15px 15px' }}>
-            <HolyPriestSpreadsheet parser={this} />
-          </Panel>
-        ),
-      },
-    ];
-    return results;
-  }
 }
 
 export default CombatLogParser;
