d5e8f5913922b0e7aa96fa1926f73af87777e15d
Martijn Hols <git@martijnhols.nl>
2021-03-06 12:17:36 +0100

    Update @emico/eslint-config to v4 and configure import/order to group @wowanalyzer imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,30 @@
+const CHECK_CODESTYLE = process.env.CODE_STYLE === 'true';
+
 module.exports = {
   extends: '@emico/eslint-config',
+  rules: {
+    'import/order': [
+      // This is an annoying code style rule that can be fixed automatically.
+      // Only check it during the precommit fix script, and in CI.
+      CHECK_CODESTYLE ? 'warn' : 'off',
+      {
+        groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
+        pathGroups: [
+          {
+            pattern: '@wowanalyzer/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
   overrides: [
     // Disable some rules for .js files to not have to update all old files in one go
     {
