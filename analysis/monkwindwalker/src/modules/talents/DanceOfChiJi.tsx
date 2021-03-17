a307903fa78e1a7dd3a036f4ad7cf44ba9f48bdc
Talby <33384684+Talby223@users.noreply.github.com>
2021-03-17 13:10:53 -0500

    Update for Dance of Chi'ji
    Trying a solution inspired by the Death Knight module

--- analysis/monkwindwalker/src/modules/talents/DanceOfChiJi.tsx
+++ analysis/monkwindwalker/src/modules/talents/DanceOfChiJi.tsx
@@ -6,7 +6,13 @@ import UptimeIcon from 'interface/icons/Uptime';
 import Analyzer, { Options } from 'parser/core/Analyzer';
 import calculateEffectiveDamage from 'parser/core/calculateEffectiveDamage';
 import { SELECTED_PLAYER, SELECTED_PLAYER_PET } from 'parser/core/EventFilter';
-import Events, { DamageEvent } from 'parser/core/Events';
+import Events, {
+  ApplyBuffEvent,
+  ApplyBuffStackEvent,
+  CastEvent,
+  DamageEvent,
+  RemoveBuffEvent,
+} from 'parser/core/Events';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import Statistic from 'parser/ui/Statistic';
@@ -15,28 +21,39 @@ import React from 'react';
 
 import { ABILITIES_AFFECTED_BY_DAMAGE_INCREASES } from '../../constants';
 
-const DAMAGE_MULTIPLIER = 2
+const DAMAGE_BOOST = 0.2
 
 class DANCE_OF_CHI_JI extends Analyzer {
+currentStacks: number = 0;
+
 constructor( options: Options) {
     super(options);
     this.addEventListener(
+      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.DANCE_OF_CHI_JI_BUFF),
+      this.onApplyBuff,
+    );
+    this.addEventListener(
         Events.damage.by(SELECTED_PLAYER).spell(SPELLS.SPINNING_CRANE_KICK),
-        this.SpinningCraneKickDamage,
+        this.onDamage,
       );
 }
-    totalDamage = 0;
+    bonusDamage = 0;
 
-    SpinningCraneKickDamage(even: DamageEvent) {
-      const buffInfo = this.selectedCombatant.getBuff(SPELLS.DANCE_OF_CHI_JI_BUFF.id);
-      if(!buffInfo) {
-        return;
-      }
-      this.totalDamage += calculateEffectiveDamage(event, DAMAGE_MULTIPLIER);
-    }
+  onApplyBuff(event: ApplyBuffEvent) {
+    this.currentStacks = 1;
+  }
+
+  onDamage(event: DamageEvent) {
+    const boostedDamage = calculateEffectiveDamage(event, DAMAGE_BOOST * this.currentStacks);
+    this.bonusDamage += boostedDamage;
+  }
+
+  onRemoveBuff(event: RemoveBuffEvent) {
+    this.currentStacks = 0;
+  }
 
   get dps() {
-    return (this.totalDamage / this.owner.fightDuration) * 1000;
+    return (this.bonusDamage / this.owner.fightDuration) * 1000;
   }
 
  
@@ -47,14 +64,14 @@ constructor( options: Options) {
         size="flexible"
         tooltip={
           <>
-            Total damage increase: {formatNumber(this.totalDamage)}
+            Total damage increase: {formatNumber(this.bonusDamage)}
           </>
         }
       >
         <BoringSpellValueText spell={SPELLS.DANCE_OF_CHI_JI_TALENT}>
           <img src="/img/sword.png" alt="Damage" className="icon" /> {formatNumber(this.dps)} DPS{' '}
           <small>
-            {formatPercentage(this.owner.getPercentageOfTotalDamageDone(this.totalDamage))} % of
+            {formatPercentage(this.owner.getPercentageOfTotalDamageDone(this.bonusDamage))} % of
             total
           </small>
         </BoringSpellValueText>
