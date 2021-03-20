58bb8456d1d3f187ea320de8a9d049901c91b96b
Kartarn <deraluf@googlemail.com>
2021-03-20 12:01:35 +0100

    Astral Power efficiency now considers BOAT legendary

--- analysis/druidbalance/src/modules/resourcetracker/AstralPowerDetails.tsx
+++ analysis/druidbalance/src/modules/resourcetracker/AstralPowerDetails.tsx
@@ -1,5 +1,6 @@
 import { t } from '@lingui/macro';
 import { formatPercentage } from 'common/format';
+import SPELLS from 'common/SPELLS';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 import { Panel } from 'interface';
 import Analyzer from 'parser/core/Analyzer';
@@ -15,6 +16,11 @@ import AstralPowerTracker from './AstralPowerTracker';
 const MINOR_THRESHOLD = 0;
 const AVERAGE_THRESHOLD = 0.02;
 const MAJOR_THRESHOLD = 0.05;
+
+// When using the Balance Of All Things (BOAT) Legendary, it is expected to overcap a bit of Astral Power to have 90 AP available on entering an Eclipse
+const MINOR_THRESHOLD_BOAT = 0.05;
+const AVERAGE_THRESHOLD_BOAT = 0.07;
+const MAJOR_THRESHOLD_BOAT = 0.1;
 
 class AstralPowerDetails extends Analyzer {
   get wasted() {
@@ -34,12 +40,16 @@ class AstralPowerDetails extends Analyzer {
   }
 
   get suggestionThresholdsWasted() {
+    const usingBoat = this.selectedCombatant.hasLegendaryByBonusID(
+      SPELLS.BALANCE_OF_ALL_THINGS_SOLAR.bonusID,
+    );
+
     return {
       actual: this.wastedPercent,
       isGreaterThan: {
-        minor: MINOR_THRESHOLD,
-        average: AVERAGE_THRESHOLD,
-        major: MAJOR_THRESHOLD,
+        minor: usingBoat ? MINOR_THRESHOLD_BOAT : MINOR_THRESHOLD,
+        average: usingBoat ? AVERAGE_THRESHOLD_BOAT : AVERAGE_THRESHOLD,
+        major: usingBoat ? MAJOR_THRESHOLD_BOAT : MAJOR_THRESHOLD,
       },
       style: ThresholdStyle.PERCENTAGE,
     };
