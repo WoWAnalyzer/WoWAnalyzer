5ef56514681598fb64bc842c7a34c45415ef2642
Dylan Dirlam <dadirlam@hotmail.com>
2021-04-03 18:24:58 -0400

    Add Dambroda to contributor of other mage specs

--- analysis/magefire/src/CONFIG.tsx
+++ analysis/magefire/src/CONFIG.tsx
@@ -1,4 +1,4 @@
-import { Adoraci } from 'CONTRIBUTORS';
+import { Adoraci, Dambroda } from 'CONTRIBUTORS';
 import SPECS from 'game/SPECS';
 import React from 'react';
 
@@ -6,7 +6,7 @@ import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
-  contributors: [Adoraci],
+  contributors: [Adoraci, Dambroda],
   // The WoW client patch this spec was last updated.
   patchCompatibility: '9.0.5',
   isPartial: false,
