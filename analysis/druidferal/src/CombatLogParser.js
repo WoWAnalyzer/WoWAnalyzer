7d861d7015768fc49402ee594a9e98cac0a2c975
Kelton Finch <keltonfinch@gmail.com>
2021-04-21 10:34:45 -0400

    Fixed RakeBleed import

--- analysis/druidferal/src/CombatLogParser.js
+++ analysis/druidferal/src/CombatLogParser.js
@@ -1,4 +1,3 @@
-import RakeBleed from 'analysis/druidferal/src/normalizers/RakeBleed';
 import CoreCombatLogParser from 'parser/core/CombatLogParser';
 
 import Abilities from './modules/Abilities';
@@ -32,6 +31,7 @@ import Predator from './modules/talents/Predator';
 import SavageRoar from './modules/talents/SavageRoar';
 import BleedDebuffEvents from './normalizers/BleedDebuffEvents';
 import ComboPointsFromAoE from './normalizers/ComboPointsFromAoE';
+import RakeBleed from './normalizers/RakeBleed';
 
 class CombatLogParser extends CoreCombatLogParser {
   static specModules = {
