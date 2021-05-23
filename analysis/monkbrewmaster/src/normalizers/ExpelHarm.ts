af4399b53369c699f72ed02f68cfd97bac30b7d1
Maxime Burlat <maximeburlat@gmail.com>
2021-05-23 16:16:20 -0500

    rebuilt ExpelHarm Normalizer from core EventOrderNormalizer

---
+++ analysis/monkbrewmaster/src/normalizers/ExpelHarm.ts
@@ -0,0 +1,45 @@
+import SPELLS from 'common/SPELLS';
+import EventOrderNormalizer from 'parser/core/EventOrderNormalizer';
+import { EventOrder } from 'parser/core/EventOrderNormalizer';
+import { EventType } from 'parser/core/Events';
+import { Options } from 'parser/core/Module';
+
+import { GIFT_OF_THE_OX_SPELL_IDS } from '../constants';
+
+const CelestialFortuneHealOrder: EventOrder = {
+  beforeEventId: SPELLS.EXPEL_HARM.id,
+  beforeEventType: EventType.Cast,
+  afterEventId: SPELLS.CELESTIAL_FORTUNE_HEAL.id,
+  afterEventType: EventType.Heal,
+  bufferMs: 50,
+  anyTarget: true,
+  updateTimestamp: true,
+};
+
+const ExpelHarmHealOrder: EventOrder = {
+  beforeEventId: SPELLS.EXPEL_HARM.id,
+  beforeEventType: EventType.Cast,
+  afterEventId: SPELLS.EXPEL_HARM.id,
+  afterEventType: EventType.Heal,
+  bufferMs: 50,
+  anyTarget: true,
+  updateTimestamp: true,
+};
+
+export default class ExpelHarm extends EventOrderNormalizer {
+  constructor(options: Options) {
+    super(options, [
+      CelestialFortuneHealOrder,
+      ExpelHarmHealOrder,
+      ...GIFT_OF_THE_OX_SPELL_IDS.map((id) => ({
+        beforeEventId: SPELLS.EXPEL_HARM.id,
+        beforeEventType: EventType.Cast,
+        afterEventId: id,
+        afterEventType: EventType.Heal,
+        bufferMs: 50,
+        anyTarget: true,
+        updateTimestamp: true,
+      })),
+    ]);
+  }
+}
