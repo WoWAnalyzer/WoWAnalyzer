fcb5ed4bea472146ac76ea4a5cb3f17447b74c84
kedearian <kedearian@gmail.com>
2021-06-14 17:06:55 -0500

    Fix for linting
    removed '.' per ESlint test.

--- src/common/SPELLS/shaman.ts
+++ src/common/SPELLS/shaman.ts
@@ -103,7 +103,7 @@ const spells = {
     manaCost: 150,
   },
   LIGHTNING_SHIELD_ELEMENTAL: {
-    id: 344174, // Appears to be the spellID used when lightning shield does damage as Elemental Spec.
+    id: 344174, // Appears to be the spellID used when lightning shield does damage as Elemental Spec
     name: 'Lightning Shield',
     icon: 'spell_nature_lightningshield',
     manaCost: 150,
