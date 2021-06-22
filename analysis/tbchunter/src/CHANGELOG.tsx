0c0cb383f66e863dcbfe0acd3cc8155fd4b7e0b8
Martijn Hols <git@martijnhols.nl>
2021-06-22 10:53:33 +0200

    Update changelogs

--- analysis/tbchunter/src/CHANGELOG.tsx
+++ analysis/tbchunter/src/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { Zerotorescue } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2021, 6, 22), 'Configure abilities and buffs for the timeline.', Zerotorescue),
   change(date(2021, 6, 7), 'Initial setup', Zerotorescue),
 ];
