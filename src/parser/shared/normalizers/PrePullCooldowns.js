b621597ba15157925d3d26effe1cfca58840e855
Kelton Finch <keltonfinch@gmail.com>
2021-07-04 23:23:08 -0400

    Fixed a bug where erroneous costs could be assigned to pre-cast spells

--- src/parser/shared/normalizers/PrePullCooldowns.js
+++ src/parser/shared/normalizers/PrePullCooldowns.js
@@ -140,8 +140,13 @@ class PrePullCooldowns extends EventsNormalizer {
          * information more accurately.
          */
         if (precastClassResources === null && event.classResources) {
-          debug && console.debug('Setting prepull class resources to:', event.classResources);
+          debug && console.debug('Setting prepull class resources (but stripping costs) to:', event.classResources);
           precastClassResources = event.classResources;
+          precastClassResources.map(classResource => {
+            const noCostClassResource = Object.assign({}, classResource);
+            noCostClassResource.cost = undefined;
+            return noCostClassResource;
+          });
         }
 
         // If a cast is found for a damage spell, remove it from the search
