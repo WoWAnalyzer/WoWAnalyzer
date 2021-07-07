c22816c8ffc33168eed927bb7ee097cfc2b9a975
Martijn Hols <git@martijnhols.nl>
2021-07-07 11:20:51 +0200

    Fix metric memoization does not consider all args

--- src/parser/core/metric.ts
+++ src/parser/core/metric.ts
@@ -9,16 +9,32 @@ export interface Info {
 }
 export type Metric<Value = any> = (events: AnyEvent[], info: Info, ...args: any[]) => Value;
 
+// Shallow compare all args including the events array
+function isEqual(args: any[], lastArgs: any[]) {
+  if (args.length !== lastArgs.length) {
+    return false;
+  }
+
+  return args.every((arg, index) => arg === lastArgs[index]);
+}
+
+/**
+ * Wrap this around any metrics doing work. This will memoize results so there's
+ * no performance impact when the metric is used multiple times (and this may
+ * do more in the future.)
+ */
 const metric = <T extends any[], U>(fn: (events: AnyEvent[], ...args: T) => U) => {
   // We store the last events in the CombatLogParser anyway, so leaving
   // instances in stats should not be significant.
-  let lastEvents: AnyEvent[] | undefined = undefined;
+  let lastArgs: any[] | undefined = undefined;
   let lastValue: any | undefined = undefined;
+  // TODO: Memoize multiple
 
   return (events: AnyEvent[], ...args: T): U => {
-    if (events !== lastEvents) {
+    const allArgs = [events, ...args];
+    if (!lastArgs || !isEqual(allArgs, lastArgs)) {
       lastValue = fn(events, ...args);
-      lastEvents = events;
+      lastArgs = allArgs;
     }
 
     return lastValue;
