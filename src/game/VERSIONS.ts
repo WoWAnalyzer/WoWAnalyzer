cbbaeb60e64393199104c187c0ea2332aedad406
Kelton Finch <keltonfinch@gmail.com>
2021-07-28 11:37:35 -0400

    Reverted unintended change to version

--- src/game/VERSIONS.ts
+++ src/game/VERSIONS.ts
@@ -3,7 +3,7 @@ import Expansion from './Expansion';
 // The current version of the game. Used to check spec patch compatibility and as a caching key.
 const VERSIONS: Partial<{ [expansion in Expansion]: string }> = {
   [Expansion.TheBurningCrusade]: '2.5.1',
-  [Expansion.Shadowlands]: '9.0.5',
+  [Expansion.Shadowlands]: '9.0.1',
 };
 
 export default VERSIONS;
