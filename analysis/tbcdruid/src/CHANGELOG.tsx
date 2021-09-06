a90dfca9eae2366732fac9a89e2c5fa0d50329e9
TJ Moats <tj.moats@smartrent.com>
2021-09-06 14:08:35 -0500

    lint

--- analysis/tbcdruid/src/CHANGELOG.tsx
+++ analysis/tbcdruid/src/CHANGELOG.tsx
@@ -1,5 +1,4 @@
 import { change, date } from 'common/changelog';
 import { Khadaj } from 'CONTRIBUTORS';
-import React from 'react';
 
 export default [change(date(2021, 9, 6), 'Stubbing out Resto Druid Analyzer', Khadaj)];
--- analysis/tbcdruid/src/modules/Abilities.tsx
+++ analysis/tbcdruid/src/modules/Abilities.tsx
@@ -1,9 +1,6 @@
 import CoreAbilities from 'parser/core/modules/Abilities';
 import { SpellbookAbility } from 'parser/core/modules/Ability';
 
-import lowRankSpells from '../lowRankSpells';
-import * as SPELLS from '../SPELLS';
-
 class Abilities extends CoreAbilities {
   spellbook(): SpellbookAbility[] {
     const baseSpells: SpellbookAbility[] = [];
