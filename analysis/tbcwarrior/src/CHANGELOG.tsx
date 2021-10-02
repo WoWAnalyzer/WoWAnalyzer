dacd99703b26aaef49350fe6a552f33944787d03
Matthew Lam <matt@mryanlam.com>
2021-10-01 17:00:36 -0700

    copy mage

---
+++ analysis/tbcwarrior/src/CHANGELOG.tsx
@@ -0,0 +1,6 @@
+import { change, date } from 'common/changelog';
+import { Charurun } from 'CONTRIBUTORS';
+
+export default [
+  change(date(2021, 8, 14), 'Initial setup, only supports Prot.', Charurun),
+];
---
+++ analysis/tbcwarrior/src/SPELLS.ts
@@ -0,0 +1,46 @@
+// Arcane
+export const AMPLIFY_MAGIC = 33946;
+export const ARCANE_BLAST = 30451;
+export const ARCANE_BLAST_DEBUFF = 36032;
+export const ARCANE_EXPLOSION = 27082;
+export const ARCANE_MISSILES = 38699;
+export const ARCANE_POWER = 12042;
+export const BLINK = 1953;
+export const DAMPEN_MAGIC = 33944;
+export const COUNTERSPELL = 2139;
+export const EVOCATION = 12051;
+export const MAGE_ARMOR = 27125;
+export const MANA_SHIELD = 27131;
+export const PRESENCE_OF_MIND = 12043;
+export const SPELLSTEAL = 30449;
+export const REMOVE_CURSE = 475;
+
+// Frost
+export const BLIZZARD = 27085;
+export const COLD_SNAP = 11958;
+export const CONE_OF_COLD = 27087;
+export const FROSTBOLT = 27072;
+export const FROST_ARMOR = 7301;
+export const FROST_NOVA = 27088;
+export const ICE_ARMOR = 27124;
+export const ICE_BLOCK = 45438;
+export const ICE_LANCE = 30455;
+export const ICY_VEINS = 12472;
+export const ICE_BARRIER = 33405;
+
+// Fire
+export const FIREBALL = 27070;
+export const FIRE_BLAST = 27079;
+export const FLAMESTRIKE = 27086;
+export const MOLTEN_ARMOR = 30482;
+export const SCORCH = 27074;
+
+// Wand / Autoattack
+export const AUTO_ATTACK = 5019;
+
+// CONJURED ITEMS
+export const MANA_EMERALD = 27103;
+
+// TRINKETS
+
+export const BADGE_ICON = 35163;
