d3fa9f7fc0a78fe93e49896f1adc5af3d4ced42e
Matthew Lam <matt@mryanlam.com>
2021-10-01 17:18:52 -0700

    initial spells and low rank spells

--- analysis/tbcwarrior/src/SPELLS.ts
+++ analysis/tbcwarrior/src/SPELLS.ts
@@ -1,46 +1,27 @@
-// Arcane
-export const AMPLIFY_MAGIC = 33946;
-export const ARCANE_BLAST = 30451;
-export const ARCANE_BLAST_DEBUFF = 36032;
-export const ARCANE_EXPLOSION = 27082;
-export const ARCANE_MISSILES = 38699;
-export const ARCANE_POWER = 12042;
-export const BLINK = 1953;
-export const DAMPEN_MAGIC = 33944;
-export const COUNTERSPELL = 2139;
-export const EVOCATION = 12051;
-export const MAGE_ARMOR = 27125;
-export const MANA_SHIELD = 27131;
-export const PRESENCE_OF_MIND = 12043;
-export const SPELLSTEAL = 30449;
-export const REMOVE_CURSE = 475;
+// Protection
+export const SHIELD_SLAM = 30356;
+export const REVENGE = 30357;
+export const SHIELD_BLOCK = 2565;
+export const DEVASTATE = 30022;
+export const SHIELD_WALL = 871;
+export const LAST_STAND = 12975;
 
-// Frost
-export const BLIZZARD = 27085;
-export const COLD_SNAP = 11958;
-export const CONE_OF_COLD = 27087;
-export const FROSTBOLT = 27072;
-export const FROST_ARMOR = 7301;
-export const FROST_NOVA = 27088;
-export const ICE_ARMOR = 27124;
-export const ICE_BLOCK = 45438;
-export const ICE_LANCE = 30455;
-export const ICY_VEINS = 12472;
-export const ICE_BARRIER = 33405;
+// Arms
 
-// Fire
-export const FIREBALL = 27070;
-export const FIRE_BLAST = 27079;
-export const FLAMESTRIKE = 27086;
-export const MOLTEN_ARMOR = 30482;
-export const SCORCH = 27074;
 
-// Wand / Autoattack
-export const AUTO_ATTACK = 5019;
+// Fury
 
-// CONJURED ITEMS
-export const MANA_EMERALD = 27103;
+
+// All
+export const AUTO_ATTACK = 1;
+export const HEROIC_STRIKE = 29707;
+export const THUNDER_CLAP = 25264;
+export const SPELL_REFLECT = 23920;
+export const BLOOD_RAGE = 2687;
+export const CLEAVE = 25231;
+export const TAUNT = 355;
+export const COMMANDING_SHOUT = 469;
+export const DEMORALIZING_SHOUT = 25203;
+export const BATTLE_SHOUT = 2048;
 
 // TRINKETS
-
-export const BADGE_ICON = 35163;
