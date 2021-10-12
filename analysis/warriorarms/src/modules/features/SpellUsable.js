8ac14a58a3b6bd77ccfb587c487bd68f0c3e336a
elasticspoon <quesadillaman@gmail.com>
2021-10-11 21:48:54 -0400

    swapped to and statements

--- analysis/warriorarms/src/modules/features/SpellUsable.js
+++ analysis/warriorarms/src/modules/features/SpellUsable.js
@@ -12,24 +12,25 @@ class SpellUsable extends CoreSpellUsable {
     super.onEvent(event);
     // Tactician passive: You have a 1.40% chance per Rage spent on damaging abilities to reset the remaining cooldown on Overpower.
     // normally charges dont count down simultaneously. these do
-    if (event.type === EventType.ApplyBuff || event.type === EventType.RefreshBuff) {
-      if (event.ability.guid === SPELLS.TACTICIAN.id) {
-        if (this.isOnCooldown(SPELLS.OVERPOWER.id)) {
-          const remainingCD =
-            this.abilities.getAbility(SPELLS.OVERPOWER.id).cooldown * 1000 -
-            this.cooldownRemaining(SPELLS.OVERPOWER.id);
-          this.endCooldown(SPELLS.OVERPOWER.id, false, this.owner.currentTimestamp, remainingCD);
-        }
-      }
+    if (
+      (event.type === EventType.ApplyBuff || event.type === EventType.RefreshBuff) &&
+      event.ability.guid === SPELLS.TACTICIAN.id &&
+      this.isOnCooldown(SPELLS.OVERPOWER.id)
+    ) {
+      const remainingCD =
+        this.abilities.getAbility(SPELLS.OVERPOWER.id).cooldown * 1000 -
+        this.cooldownRemaining(SPELLS.OVERPOWER.id);
+
+      this.endCooldown(SPELLS.OVERPOWER.id, false, this.owner.currentTimestamp, remainingCD);
     }
 
     // Battlelord legendary: overpower has a chance to reset mortal strike
-    if (event.type === EventType.ApplyBuff || event.type === EventType.RefreshBuff) {
-      if (event.ability.guid === SPELLS.BATTLELORD_ENERGIZE.id) {
-        if (this.isOnCooldown(SPELLS.MORTAL_STRIKE.id)) {
-          this.endCooldown(SPELLS.MORTAL_STRIKE.id);
-        }
-      }
+    if (
+      (event.type === EventType.ApplyBuff || event.type === EventType.RefreshBuff) &&
+      event.ability.guid === SPELLS.BATTLELORD_ENERGIZE.id &&
+      this.isOnCooldown(SPELLS.MORTAL_STRIKE.id)
+    ) {
+      this.endCooldown(SPELLS.MORTAL_STRIKE.id);
     }
   }
 }
