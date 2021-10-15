1779317fedade3e8db41d087a7e2cc9f5c3e62ff
Matthew Lam <matt@mryanlam.com>
2021-10-14 23:18:32 -0700

    changelog

--- analysis/tbcwarrior/src/CHANGELOG.tsx
+++ analysis/tbcwarrior/src/CHANGELOG.tsx
@@ -2,6 +2,7 @@ import { change, date } from 'common/changelog';
 import { Charurun, Khadaj } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2021, 10, 14), 'Initial setup for Arms', Charurun),
   change(date(2021, 10, 2), 'Initial setup, only supports Prot.', Charurun),
   change(date(2021, 10, 1), 'Stubbing out Warrior Analyzer', Khadaj),
 ];
