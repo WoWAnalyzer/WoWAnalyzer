9f6a7610542f4b9e069b51aa8334bd63ac09e7f3
Josh Yaxley <joshyaxley93@gmail.com>
2021-10-15 10:06:49 +0100

    Add Fiery Soul statistic

--- analysis/demonhuntervengeance/src/modules/shadowlands/legendaries/FierySoul.tsx
+++ analysis/demonhuntervengeance/src/modules/shadowlands/legendaries/FierySoul.tsx
@@ -2,6 +2,10 @@ import SPELLS from 'common/SPELLS';
 import Analyzer, { Options } from 'parser/core/Analyzer';
 import { EventType } from 'parser/core/Events';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
+import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
+import Statistic from 'parser/ui/Statistic';
+import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import React from 'react';
 
 import { ConsumeSoulFragmentsEvent } from '../../statistics/SoulFragmentsConsume';
 
@@ -16,6 +20,8 @@ class FierySoul extends Analyzer {
     spellUsable: SpellUsable,
   };
   protected spellUsable!: SpellUsable;
+
+  totalCooldownReduction = 0;
 
   constructor(options: Options) {
     super(options);
@@ -33,11 +39,24 @@ class FierySoul extends Analyzer {
     }
 
     if (this.spellUsable.isOnCooldown(SPELLS.FIERY_BRAND.id)) {
-      this.spellUsable.reduceCooldown(
+      this.totalCooldownReduction += this.spellUsable.reduceCooldown(
         SPELLS.FIERY_BRAND.id,
         event.numberofSoulFragmentsConsumed * COOLDOWN_REDUCTION,
       );
     }
+  }
+
+  statistic() {
+    return (
+      <Statistic size="flexible" category={STATISTIC_CATEGORY.ITEMS}>
+        <BoringSpellValueText spellId={SPELLS.FIERY_SOUL.id}>
+          <>
+            {Math.floor(this.totalCooldownReduction / 1000)}s{' '}
+            <small>of Fiery Brand cooldown reduction</small>
+          </>
+        </BoringSpellValueText>
+      </Statistic>
+    );
   }
 }
 
