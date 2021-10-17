f49e06360ced832b433b8c84c84b7823842c5785
J David Smith <emallson@emallson.net>
2021-10-17 13:08:25 -0400

    remove paywall on timeline

--- src/interface/report/Results/TimelineTab.tsx
+++ src/interface/report/Results/TimelineTab.tsx
@@ -1,11 +1,8 @@
 import { getAlertComponent } from 'interface/Alert';
-import usePremium from 'interface/usePremium';
 import CombatLogParser from 'parser/core/CombatLogParser';
 import Abilities from 'parser/core/modules/Abilities';
 import Buffs from 'parser/core/modules/Buffs';
 import DistanceMoved from 'parser/shared/modules/DistanceMoved';
-// import SpellUsable from 'parser/shared/modules/SpellUsable';
-// import GlobalCooldown from 'parser/shared/modules/GlobalCooldown';
 import React, { ReactNode } from 'react';
 
 import { useConfig } from '../ConfigContext';
@@ -16,7 +13,6 @@ interface Props {
 }
 
 const TimelineTab = ({ parser }: Props) => {
-  const premium = usePremium();
   const config = useConfig();
 
   let alert: ReactNode = null;
@@ -52,10 +48,7 @@ const TimelineTab = ({ parser }: Props) => {
         abilities={parser.getModule(Abilities)}
         buffs={parser.getModule(Buffs)}
         movement={parser.getModule(DistanceMoved).instances}
-        premium={premium}
         config={parser.config.timeline}
-        // isAbilityCooldownsAccurate={parser.getModule(SpellUsable).isAccurate}
-        // isGlobalCooldownAccurate={parser.getModule(GlobalCooldown).isAccurate}
       />
     </>
   );
