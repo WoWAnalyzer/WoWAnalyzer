c33d7b64b6153fd5266593cba0807657dda9946d
elasticspoon <quesadillaman@gmail.com>
2021-10-23 22:32:28 -0400

    buffStacks conditional added

--- src/parser/shared/metrics/apl/conditions/buffStacks.tsx
+++ src/parser/shared/metrics/apl/conditions/buffStacks.tsx
@@ -4,62 +4,54 @@ import { EventType } from 'parser/core/Events';
 import React from 'react';
 
 import { Condition, tenseAlt } from '../index';
+import { Range } from './index';
 
-export function moreThanBuffStacks(spell: Spell, stack: number): Condition<boolean> {
-  return {
-    key: `buffPresent-${spell.id}`,
-    init: () => false,
-    update: (state, event) => {
-      switch (event.type) {
-        case EventType.ApplyBuffStack:
-          if (event.ability.guid === spell.id && event.stack > stack) {
-            return true;
-          }
-          break;
-        case EventType.RemoveBuffStack:
-          if (event.ability.guid === spell.id && event.stack <= stack) {
-            return false;
-          }
-          break;
-      }
-
-      return state;
-    },
-    validate: (state, _event) => state,
-    describe: (tense) => (
-      <>
-        more than {stack} stacks of <SpellLink id={spell.id} /> {tenseAlt(tense, 'is', 'was')}{' '}
-        present
-      </>
-    ),
-  };
+function formatRange(range: Range): string {
+  if (range.atLeast !== undefined && range.atMost !== undefined) {
+    return `${range.atLeast}-${range.atMost}`;
+  } else if (range.atLeast !== undefined) {
+    return `at least ${range.atLeast}`;
+  } else if (range.atMost !== undefined) {
+    return `at most ${range.atMost}`;
+  } else {
+    return '';
+  }
 }
 
-export function lessThanBuffStacks(spell: Spell, stack: number): Condition<boolean> {
+export default function buffStacks(spell: Spell, range: Range): Condition<number> {
   return {
     key: `buffPresent-${spell.id}`,
-    init: () => false,
+    init: () => 0,
     update: (state, event) => {
       switch (event.type) {
         case EventType.ApplyBuffStack:
-          if (event.ability.guid === spell.id && event.stack < stack) {
-            return true;
+          if (event.ability.guid === spell.id) {
+            return event.stack;
+          }
+          break;
+        case EventType.ApplyBuff:
+          if (event.ability.guid === spell.id) {
+            return 1;
           }
           break;
         case EventType.RemoveBuffStack:
-          if (event.ability.guid === spell.id && event.stack >= stack) {
-            return false;
+          if (event.ability.guid === spell.id) {
+            return event.stack;
+          }
+          break;
+        case EventType.RemoveBuff:
+          if (event.ability.guid === spell.id) {
+            return 0;
           }
           break;
       }
-
       return state;
     },
-    validate: (state, _event) => state,
+    validate: (state, _event) =>
+      state >= (range.atLeast || 0) && (range.atMost === undefined || state <= range.atMost),
     describe: (tense) => (
       <>
-        less than {stack} stacks of <SpellLink id={spell.id} /> {tenseAlt(tense, 'is', 'was')}{' '}
-        present
+        you {tenseAlt(tense, 'have', 'had')} {formatRange(range)} <SpellLink id={spell.id} icon />
       </>
     ),
   };
