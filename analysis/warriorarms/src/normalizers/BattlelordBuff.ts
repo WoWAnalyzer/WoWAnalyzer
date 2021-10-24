b7bb285ea8f19699d0cff0e86ca6a865349fb9e9
elasticspoon <quesadillaman@gmail.com>
2021-10-23 22:32:51 -0400

    battlelord normalizer added

---
+++ analysis/warriorarms/src/normalizers/BattlelordBuff.ts
@@ -0,0 +1,26 @@
+import SPELLS from 'common/SPELLS';
+import EventOrderNormalizer, { EventOrder } from 'parser/core/EventOrderNormalizer';
+import { EventType } from 'parser/core/Events';
+import { Options } from 'parser/core/Module';
+
+const EVENT_ORDERS: EventOrder[] = [
+  {
+    beforeEventId: SPELLS.MORTAL_STRIKE.id,
+    beforeEventType: EventType.Cast,
+    afterEventId: SPELLS.BATTLELORD.id,
+    afterEventType: EventType.RemoveBuff,
+    bufferMs: 50,
+    anyTarget: true,
+  },
+];
+
+/**
+ * Ensures that the Battlelord Buff is removed after mortal strike is cast.
+ */
+class BattlelordNormalizer extends EventOrderNormalizer {
+  constructor(options: Options) {
+    super(options, EVENT_ORDERS);
+  }
+}
+
+export default BattlelordNormalizer;
