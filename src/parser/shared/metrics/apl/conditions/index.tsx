3d6bcdeb3101aa78b939235c93737d778d094227
elasticspoon <quesadillaman@gmail.com>
2021-10-23 22:33:21 -0400

    added new conditionals to index

--- src/parser/shared/metrics/apl/conditions/index.tsx
+++ src/parser/shared/metrics/apl/conditions/index.tsx
@@ -1,13 +1,16 @@
 export { default as hasConduit } from './hasConduit';
-export { default as hasLegendary } from './hasLegendary';
+export { hasLegendary, hasNoLegendary } from './hasLegendary';
 export { default as optional } from './optional';
 export { buffPresent, buffMissing } from './buffPresent';
+export { hasTalent, hasNoTalent } from './hasTalent';
 export { default as spellAvailable } from './spellAvailable';
 export { default as hasResource } from './hasResource';
 export { default as inExecute } from './inExecute';
 export { default as or } from './or';
 export { default as always } from './always';
 export { default as and } from './and';
+export { default as spellCharges } from './spellCharges';
+export { default as buffStacks } from './buffStacks';
 
 export interface Range {
   atLeast?: number;
