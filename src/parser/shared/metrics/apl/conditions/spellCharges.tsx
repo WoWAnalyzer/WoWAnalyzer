853a5c0cb6b71e4c54d02c6280307c1ae3787f25
elasticspoon <quesadillaman@gmail.com>
2021-10-23 22:31:07 -0400

    spellCharges conditional added

--- src/parser/shared/metrics/apl/conditions/spellCharges.tsx
+++ src/parser/shared/metrics/apl/conditions/spellCharges.tsx
@@ -1,31 +1,40 @@
 import type Spell from 'common/SPELLS/Spell';
 import { SpellLink } from 'interface';
-import { UpdateSpellUsableEvent, EventType } from 'parser/core/Events';
+import { EventType } from 'parser/core/Events';
 import React from 'react';
 
-import { Condition, cooldownEnd, tenseAlt } from '../index';
+import { Condition, tenseAlt } from '../index';
+import { Range } from './index';
 
-export default function spellCharges(
-  spell: Spell,
-  charges: number,
-): Condition<UpdateSpellUsableEvent | null> {
+function formatRange(range: Range): string {
+  if (range.atLeast !== undefined && range.atMost !== undefined) {
+    return `${range.atLeast}-${range.atMost}`;
+  } else if (range.atLeast !== undefined) {
+    return `at least ${range.atLeast}`;
+  } else if (range.atMost !== undefined) {
+    return `at most ${range.atMost}`;
+  } else {
+    return '';
+  }
+}
+
+export default function spellCharges(spell: Spell, range: Range): Condition<number> {
   return {
     key: `spellCharges-${spell.id}`,
-    init: () => null,
+    init: () => 0,
     update: (state, event) => {
       if (event.type === EventType.UpdateSpellUsable && event.ability.guid === spell.id) {
-        return event;
+        return event.chargesAvailable;
       } else {
         return state;
       }
     },
     validate: (state, event) =>
-      state === null ||
-      state.chargesAvailable === charges ||
-      (state.chargesAvailable === charges - 1 && cooldownEnd(state) <= event.timestamp + 100),
+      state >= (range.atLeast || 0) && (range.atMost === undefined || state <= range.atMost),
     describe: (tense) => (
       <>
-        <SpellLink id={spell.id} /> {tenseAlt(tense, 'has', 'had')} {charges} charges
+        you {tenseAlt(tense, 'have', 'had')} {formatRange(range)} charges of{' '}
+        <SpellLink id={spell.id} icon />
       </>
     ),
   };
