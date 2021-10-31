70bec96731a8f71cb5b32b9b5d981667c348f65d
J David Smith <emallson@emallson.net>
2021-10-31 13:02:29 -0400

    changelog entry

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -52,6 +52,7 @@ import React from 'react';
 
 // prettier-ignore
 export default [
+  change(date(2021, 10, 31), 'Prepare for "energize" event rename.', emallson),
   change(date(2021, 10, 27), 'Clean up character page for TBC characters.', emallson),
   change(date(2021, 10, 21), 'Remove paywall on timeline.', emallson),
   change(date(2021, 10, 20), 'Fix a crashing issue with Sanctum of Domination Vantus Runes.', emallson),
