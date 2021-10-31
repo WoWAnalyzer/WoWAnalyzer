00daa02e6ef5ccd5354cf4e9b630bd06ed255724
Putro <laustsf@gmail.com>
2021-10-31 20:33:24 +0100

    Changelog entries where relevant

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -52,6 +52,7 @@ import React from 'react';
 
 // prettier-ignore
 export default [
+  change(date(2021, 10, 31), 'Fix some bugs with the execute helper module and allow .by and .to to filter by an array.', Putro),
   change(date(2021, 10, 27), 'Clean up character page for TBC characters.', emallson),
   change(date(2021, 10, 21), 'Remove paywall on timeline.', emallson),
   change(date(2021, 10, 20), 'Fix a crashing issue with Sanctum of Domination Vantus Runes.', emallson),
