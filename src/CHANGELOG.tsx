ba20f870b50e4f17f15f0c8090c228283dbe1d5a
Jeff Anderson <jander99@gmail.com>
2021-12-07 00:04:58 -0500

    Update dev dependencies

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -54,6 +54,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2021, 12, 6), 'Updated some internal dependencies', Jeff),
   change(date(2021, 12, 4), 'Fixed an issue with stat tracking in Mythic+ dungeons. Mythic+ still not officially supported, but at least it shouldn\'t crash', emallson),
   change(date(2021, 11, 30), 'Fixed Spiritual Mana Potion not being tracked issue.', Jeff),
   change(date(2021, 11, 27), 'Internal dependency updates.', emallson),
