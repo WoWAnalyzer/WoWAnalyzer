e1ab068f65b1f5b253cba7bda4a655541063cf34
Putro <laustsf@gmail.com>
2021-12-12 13:37:07 +0100

    [EncounterStats] The similiar kill times should link directly to the persons log you click on and not just to the report, and added a filter by covenant toggle

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -54,6 +54,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2021, 12, 13), 'Added a filter by covenant toggle to the Similiar Kill Times module under the Character tab.', Putro),
   change(date(2021, 12, 4), 'Fixed an issue with stat tracking in Mythic+ dungeons. Mythic+ still not officially supported, but at least it shouldn\'t crash', emallson),
   change(date(2021, 11, 30), 'Fixed Spiritual Mana Potion not being tracked issue.', Jeff),
   change(date(2021, 11, 27), 'Internal dependency updates.', emallson),
