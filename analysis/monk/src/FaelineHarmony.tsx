aa99a74e0bb14bca976d971053c7a3c78aa3bf9a
Hurstilol <85832226+Hurstilol@users.noreply.github.com>
2021-12-12 02:57:11 +0100

    Update FaelineHarmony.tsx

--- analysis/monk/src/FaelineHarmony.tsx
+++ analysis/monk/src/FaelineHarmony.tsx
@@ -36,7 +36,7 @@ class FaelineHarmony extends Analyzer {
         this.onAffectedDamage,
       );
       this.addEventListener(
-      Events.heal.by(SELECTED_PLAYER | SELECTED_PLAYER_PET),
+        Events.heal.by(SELECTED_PLAYER | SELECTED_PLAYER_PET),
         this.onAffectedHealing,
       );
     }
@@ -69,15 +69,15 @@ class FaelineHarmony extends Analyzer {
         tooltip={
           <>
             This shows the HPS and DPS provided by the {formatPercentage(amp_exposure)}% increase
-           from  Faeline Harmony.
+            from Faeline Harmony.
           </>
         }
       >
         <BoringSpellValueText spellId={SPELLS.FAELINE_HARMONY_DEBUFF.id}>
-          <ItemDamageDone amount={this.totalDamage}/> 
+          <ItemDamageDone amount={this.totalDamage} />
         </BoringSpellValueText>
         <BoringSpellValueText spellId={SPELLS.FAELINE_HARMONY_BUFF.id}>
-          <ItemHealingDone amount={this.totalHealing}/> 
+          <ItemHealingDone amount={this.totalHealing} />
         </BoringSpellValueText>
       </Statistic>
     );
