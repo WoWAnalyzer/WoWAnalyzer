04aee9a7ee157e45bee33f6e4c6e29db214a79ed
Hurstilol <85832226+Hurstilol@users.noreply.github.com>
2021-12-13 23:34:30 +0100

    Update CombatLogParser.ts

--- analysis/monkwindwalker/src/CombatLogParser.ts
+++ analysis/monkwindwalker/src/CombatLogParser.ts
@@ -15,6 +15,7 @@ import Buffs from './modules/Buffs';
 import Channeling from './modules/core/Channeling';
 import GlobalCooldown from './modules/core/GlobalCooldown';
 import WeaponsOfOrderWindwalker from './modules/covenants/WeaponsOfOrder';
+import XuensBond from './modules/covenants/XuensBond';
 import AlwaysBeCasting from './modules/features/AlwaysBeCasting';
 import Checklist from './modules/features/checklist/Module';
 import CooldownThroughputTracker from './modules/features/CooldownThroughputTracker';
@@ -81,6 +82,7 @@ class CombatLogParser extends CoreCombatLogParser {
     faelineHarmony: FaelineHarmony,
     sinisterTeachings: SinisterTeachings,
     imbuedReflections: ImbuedReflections,
+    xuensBond: XuensBond,
   };
 }
 
