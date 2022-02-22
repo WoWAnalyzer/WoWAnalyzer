50414194cafee50a85864bd151398aad56ed6297
khazakdk <joe.j.sommer@gmail.com>
2022-02-22 09:02:54 -0600

    Change CDR for convo to flat per rank

--- analysis/deathknightunholy/src/modules/spells/conduits/ConvocationOfTheDead.tsx
+++ analysis/deathknightunholy/src/modules/spells/conduits/ConvocationOfTheDead.tsx
@@ -1,35 +1,19 @@
 import SPELLS from 'common/SPELLS';
 import CooldownIcon from 'interface/icons/Cooldown';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { ResourceChangeEvent } from 'parser/core/Events';
+import Events, { EnergizeEvent } from 'parser/core/Events';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
 import ConduitSpellText from 'parser/ui/ConduitSpellText';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import { STATISTIC_ORDER } from 'parser/ui/StatisticBox';
+import React from 'react';
 
 /**
- * When a Festering Wound bursts the cooldown of Apocalypse is reduced by 0.5 sec.
+ * When a Festering Wound bursts the cooldown of Apocalypse is reduced by 1.0 sec.
  */
 
-const CONVOCATION_OF_THE_DEAD_EFFECT_BY_RANK = [
-  0,
-  5,
-  5.5,
-  6,
-  6.5,
-  7,
-  7.5,
-  8,
-  8.5,
-  9,
-  9.5,
-  10,
-  10.5,
-  11,
-  11.5,
-  12,
-];
+const CONVOCATION_OF_THE_DEAD_CDR = 1.0;
 
 class ConvocationOfTheDead extends Analyzer {
   static dependencies = {
@@ -46,26 +30,22 @@ class ConvocationOfTheDead extends Analyzer {
     this.conduitRank = this.selectedCombatant.conduitRankBySpellID(
       SPELLS.CONVOCATION_OF_THE_DEAD.id,
     );
-    if (!this.conduitRank) {
-      this.active = false;
-      return;
-    }
     this.addEventListener(
-      Events.resourcechange.by(SELECTED_PLAYER).spell(SPELLS.FESTERING_WOUND_BURST),
+      Events.energize.by(SELECTED_PLAYER).spell(SPELLS.FESTERING_WOUND_BURST),
       this.onWoundBurst,
     );
   }
 
-  onWoundBurst(event: ResourceChangeEvent) {
+  onWoundBurst(event: EnergizeEvent) {
     if (this.spellUsable.isOnCooldown(SPELLS.APOCALYPSE.id)) {
-      this.cooldownReduction += CONVOCATION_OF_THE_DEAD_EFFECT_BY_RANK[this.conduitRank] / 10;
+      this.cooldownReduction += CONVOCATION_OF_THE_DEAD_CDR;
       this.spellUsable.reduceCooldown(
         SPELLS.APOCALYPSE.id,
-        CONVOCATION_OF_THE_DEAD_EFFECT_BY_RANK[this.conduitRank] / 10,
+        CONVOCATION_OF_THE_DEAD_CDR,
         event.timestamp,
       );
     } else {
-      this.wastedCooldownReduction += CONVOCATION_OF_THE_DEAD_EFFECT_BY_RANK[this.conduitRank] / 10;
+      this.wastedCooldownReduction += CONVOCATION_OF_THE_DEAD_CDR;
     }
   }
 
@@ -76,7 +56,7 @@ class ConvocationOfTheDead extends Analyzer {
         size="flexible"
         category={STATISTIC_CATEGORY.COVENANTS}
       >
-        <ConduitSpellText spellId={SPELLS.CONVOCATION_OF_THE_DEAD.id} rank={this.conduitRank}>
+        <ConduitSpellText spell={SPELLS.CONVOCATION_OF_THE_DEAD} rank={this.conduitRank}>
           <CooldownIcon /> {this.cooldownReduction.toFixed(1)}s <small> of Apocalypse CDR</small>
           <br />
           <CooldownIcon /> {this.wastedCooldownReduction.toFixed(1)}s{' '}
