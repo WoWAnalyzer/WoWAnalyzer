b58e07dbe65457fcaa23c222e3ca5ba2c69c4edb
khazakdk <joe.j.sommer@gmail.com>
2022-02-22 09:20:41 -0600

    Fix vscode trying to be too smart and change things for me

--- analysis/deathknightunholy/src/modules/spells/conduits/ConvocationOfTheDead.tsx
+++ analysis/deathknightunholy/src/modules/spells/conduits/ConvocationOfTheDead.tsx
@@ -1,7 +1,7 @@
 import SPELLS from 'common/SPELLS';
 import CooldownIcon from 'interface/icons/Cooldown';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { EnergizeEvent } from 'parser/core/Events';
+import Events, { ResourceChangeEvent } from 'parser/core/Events';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
 import ConduitSpellText from 'parser/ui/ConduitSpellText';
 import Statistic from 'parser/ui/Statistic';
@@ -31,12 +31,12 @@ class ConvocationOfTheDead extends Analyzer {
       SPELLS.CONVOCATION_OF_THE_DEAD.id,
     );
     this.addEventListener(
-      Events.energize.by(SELECTED_PLAYER).spell(SPELLS.FESTERING_WOUND_BURST),
+      Events.resourceChange.by(SELECTED_PLAYER).spell(SPELLS.FESTERING_WOUND_BURST),
       this.onWoundBurst,
     );
   }
 
-  onWoundBurst(event: EnergizeEvent) {
+  onWoundBurst(event: ResourceChangeEvent) {
     if (this.spellUsable.isOnCooldown(SPELLS.APOCALYPSE.id)) {
       this.cooldownReduction += CONVOCATION_OF_THE_DEAD_CDR;
       this.spellUsable.reduceCooldown(
