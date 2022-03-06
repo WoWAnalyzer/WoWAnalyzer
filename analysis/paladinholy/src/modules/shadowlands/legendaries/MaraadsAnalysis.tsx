6abe186da25a5c42d6f7768b62a1d242a4ade255
xizbow <93213703+xizbow1@users.noreply.github.com>
2022-03-06 14:38:15 -0800

    Attempt to quell lint errors in maraadsanalysis.tsx

--- analysis/paladinholy/src/modules/shadowlands/legendaries/MaraadsAnalysis.tsx
+++ analysis/paladinholy/src/modules/shadowlands/legendaries/MaraadsAnalysis.tsx
@@ -3,7 +3,7 @@ import SPELLS from 'common/SPELLS';
 import HIT_TYPES from 'game/HIT_TYPES';
 import { SpellLink } from 'interface';
 import Analyzer, { SELECTED_PLAYER, Options } from 'parser/core/Analyzer';
-import Events, { BuffEvent, CastEvent, DamageEvent, HealEvent } from 'parser/core/Events';
+import Events, { CastEvent, HealEvent } from 'parser/core/Events';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
 
 const OVERHEAL_THRESHOLD = 0.5;
@@ -16,36 +16,32 @@ class MaraadsAnalysis extends Analyzer {
 
   constructor(options: Options) {
     super(options);
-    this.active = this.selectedCombatant.hasLegendaryByBonusID(
-      SPELLS.MARAADS_DYING_BREATH.bonusID,
-    );
+    this.active = this.selectedCombatant.hasLegendaryByBonusID(SPELLS.MARAADS_DYING_BREATH.bonusID);
     if (!this.active) {
       return;
     }
     this.addEventListener(
-      Events.cast.spell(SPELLS.LIGHT_OF_THE_MARTYR).by(SELECTED_PLAYER), 
-      this.cast
+      Events.cast.spell(SPELLS.LIGHT_OF_THE_MARTYR).by(SELECTED_PLAYER)
+      ,this.cast,
     );
     this.addEventListener(
-      Events.heal.spell(SPELLS.LIGHT_OF_THE_MARTYR).by(SELECTED_PLAYER), 
-      this.heal
+      Events.heal.spell(SPELLS.LIGHT_OF_THE_MARTYR).by(SELECTED_PLAYER)
+      ,this.heal,
     );
     this.addEventListener(
-      Events.cast.spell(SPELLS.LIGHT_OF_DAWN_CAST).by(SELECTED_PLAYER), 
-      this.LODcast
+      Events.cast.spell(SPELLS.LIGHT_OF_DAWN_CAST).by(SELECTED_PLAYER)
+      ,this.LODcast,
     );
   }
 
-
-  
-  cast(event: CastEvent){
+  cast(event: CastEvent) {
     this.totalCasts += 1;
   }
 
-  LODcast(event: CastEvent){
+  LODcast(event: CastEvent) {
     this.LODcasts += 1;
   }
-  
+
   private heal(event: HealEvent) {
     const totalHeal = event.amount + (event.overheal || 0) + (event.absorbed || 0);
     const effectiveHeal = event.amount + (event.absorbed || 0);
@@ -56,9 +52,9 @@ class MaraadsAnalysis extends Analyzer {
 
   get unbuffedCastsSuggestion() {
     return {
-      actual: 1 - (this.LODcasts/this.totalCasts),
+      actual: 1 - this.LODcasts / this.totalCasts,
       isGreaterThan: {
-        minor: .1,
+        minor: 0.1,
         average: 0.2,
         major: 0.3,
       },
@@ -67,10 +63,10 @@ class MaraadsAnalysis extends Analyzer {
   }
 
   get notEnoughCastsSuggestion() {
-    return{
-      actual: 1 - (this.totalCasts/this.LODcasts),
-      isGreaterThan:{
-        minor: .05,
+    return {
+      actual: 1 - this.totalCasts / this.LODcasts,
+      isGreaterThan: {
+        minor: 0.05,
         average: 0.1,
         major: 0.15,
       },
@@ -83,7 +79,7 @@ class MaraadsAnalysis extends Analyzer {
       actual: this.castsOverhealed / this.totalCasts,
       isGreaterThan: {
         minor: 0.1,
-        average: 0.20,
+        average: 0.2,
         major: 0.35,
       },
       style: ThresholdStyle.PERCENTAGE,
@@ -93,8 +89,10 @@ class MaraadsAnalysis extends Analyzer {
     when(this.overhealSuggestion).addSuggestion((suggest, actual, recommended) =>
       suggest(
         <>
-          Try to avoid casting <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id}/> buffed <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} /> when a large portion of it
-          would overheal. If you are finding that this is happening very frequently, consider using a different legendary.
+          Try to avoid casting <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id} /> buffed{'.'}
+          <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} /> when a large portion of it would
+          overheal. If you are finding that this is happening very frequently, consider using a
+          different legendary.
         </>,
       )
         .icon(SPELLS.MARAADS_DYING_BREATH.icon)
@@ -104,8 +102,10 @@ class MaraadsAnalysis extends Analyzer {
     when(this.unbuffedCastsSuggestion).addSuggestion((suggest, actual, recommended) =>
       suggest(
         <>
-          Try to avoid casting unbuffed <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} />, as it is a very inefficient
-          spell when it isn't buffed by <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id} />.
+          Try to avoid casting unbuffed <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} />, as it is a
+          very inefficient spell when it isn't buffed by{' '}
+
+          <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id} />.
         </>,
       )
         .icon(SPELLS.LIGHT_OF_THE_MARTYR.icon)
@@ -115,13 +115,20 @@ class MaraadsAnalysis extends Analyzer {
     when(this.notEnoughCastsSuggestion).addSuggestion((suggest, actual, recommended) =>
       suggest(
         <>
-          Try to keep your casts of <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} /> at a 1:1 ratio with your casts of <SpellLink id={SPELLS.LIGHT_OF_DAWN_CAST.id} />, as you
-          are wasting a large amount of healing by overwriting the <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id} /> proc. If you are frequently unable to 
-          find a suitable target for your buffed <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} />, consider using a different legendary.
+          Try to keep your casts of <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} /> at a 1:1 ratio
+          with your casts of <SpellLink id={SPELLS.LIGHT_OF_DAWN_CAST.id} />, as you are wasting a
+          large amount of healing by overwriting the{' '}
+          <SpellLink id={SPELLS.MARAADS_DYING_BREATH.id} /> proc. If you are frequently unable to
+          find a suitable target for your buffed <SpellLink id={SPELLS.LIGHT_OF_THE_MARTYR.id} />,
+          consider using a different legendary.
         </>,
       )
         .icon(SPELLS.LIGHT_OF_THE_MARTYR.icon)
-        .actual(`You had ${formatPercentage(actual)}% more casts of Light of Dawn than Light of the Martyr`)
+        .actual(
+          `You had ${formatPercentage(
+            actual,
+          )}% more casts of Light of Dawn than Light of the Martyr`,
+        )
         .recommended(`< ${formatPercentage(recommended)}% is recommended`),
     );
   }
