b4e8ba709d9f2625fbdde29482c9945d23dd6499
Jeff Anderson <jander99@gmail.com>
2022-04-02 22:58:25 -0400

    Dependabot Updates

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -54,6 +54,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2022, 4, 2), 'Dependabot Updates', Jeff),
   change(date(2022, 4, 2), 'Fix issue with Donut Chart failing to render.', emallson),
   change(date(2022, 3, 25), 'Handle Unity legendary.', emallson),
   change(date(2022, 3, 23), 'Fixed degraded experience for Venthyr Mistweaver', Trevor),
