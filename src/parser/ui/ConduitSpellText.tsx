34f71c0e8cb2be1af1d52508128f090e95d27d81
Oscar Busk <oscar.busk@gmail.com>
2022-04-15 15:31:50 +0200

    Use context.parser to figure out rank and itemlevel.
    With this information we can show the "rank XX" text as well as link to the correct level of the conduit.

--- src/parser/ui/ConduitSpellText.tsx
+++ src/parser/ui/ConduitSpellText.tsx
@@ -1,21 +1,35 @@
 import { SpellIcon } from 'interface';
 import { SpellLink } from 'interface';
+import CombatLogParser from 'parser/core/CombatLogParser';
+import PropTypes from 'prop-types';
 import { ReactNode } from 'react';
 
 interface Props {
   spellId: number;
-  rank: number;
+  /** @deprecated We can figure out the rank with just the id, so no need to pass in */
+  rank?: number;
   children: ReactNode;
   className?: string;
 }
 
-const ConduitSpellText = ({ spellId, rank, children, className }: Props) => (
-  <div className={`pad boring-text ${className || ''}`}>
-    <label>
-      <SpellIcon id={spellId} /> <SpellLink id={spellId} icon={false} /> - Rank {rank}
-    </label>
-    <div className="value">{children}</div>
-  </div>
-);
+interface Context {
+  parser: CombatLogParser;
+}
+
+const ConduitSpellText = ({ spellId, children, className }: Props, { parser }: Context) => {
+  const { rank, itemLevel } = parser.selectedCombatant.conduitsByConduitID[spellId];
+  return (
+    <div className={`pad boring-text ${className || ''}`}>
+      <label>
+        <SpellIcon id={spellId} /> <SpellLink id={spellId} icon={false} ilvl={itemLevel} /> - Rank{' '}
+        {rank}
+      </label>
+      <div className="value">{children}</div>
+    </div>
+  );
+};
+ConduitSpellText.contextTypes = {
+  parser: PropTypes.object.isRequired,
+};
 
 export default ConduitSpellText;
