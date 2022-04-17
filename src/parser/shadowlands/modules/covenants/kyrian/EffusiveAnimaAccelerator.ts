509df8953eea795151df877503dd7fbad00046fa
J David Smith <emallson@emallson.net>
2022-04-17 00:53:08 -0400

    fix effusive anima accelerator handling for monk/shaman

--- src/parser/shadowlands/modules/covenants/kyrian/EffusiveAnimaAccelerator.ts
+++ src/parser/shadowlands/modules/covenants/kyrian/EffusiveAnimaAccelerator.ts
@@ -11,17 +11,17 @@ interface SpellInfo {
   name: string;
 }
 
-const SPELLS_BY_CLASS: { [key: number]: SpellInfo } = {
+const SPELLS_BY_CLASS: { [key: number]: SpellInfo | undefined } = {
   [CLASSES.DEATH_KNIGHT]: SPELLS.SHACKLE_THE_UNWORTHY,
   [CLASSES.DEMON_HUNTER]: SPELLS.ELYSIAN_DECREE,
   [CLASSES.DRUID]: SPELLS.KINDRED_SPIRITS,
   [CLASSES.HUNTER]: SPELLS.RESONATING_ARROW,
   [CLASSES.MAGE]: SPELLS.RADIANT_SPARK,
-  [CLASSES.MONK]: SPELLS.WEAPONS_OF_ORDER,
+  [CLASSES.MONK]: SPELLS.WEAPONS_OF_ORDER_CAST,
   [CLASSES.PALADIN]: SPELLS.DIVINE_TOLL,
   [CLASSES.PRIEST]: SPELLS.BOON_OF_THE_ASCENDED,
   [CLASSES.ROGUE]: SPELLS.ECHOING_REPRIMAND,
-  [CLASSES.SHAMAN]: SPELLS.VESPER_TOTEM,
+  [CLASSES.SHAMAN]: undefined,
   [CLASSES.WARLOCK]: SPELLS.SCOURING_TITHE,
   [CLASSES.WARRIOR]: SPELLS.SPEAR_OF_BASTION,
 };
@@ -45,13 +45,19 @@ class EffusiveAnimaAccelerator extends Analyzer {
 
   // Array of triggering cast and number of debuff applications
   casts: Array<[CastEvent, number]> = [];
-  classAbility: SpellInfo;
+  classAbility?: SpellInfo;
 
   constructor(options: Options) {
     super(options);
 
     const classId = getClassBySpecId(this.selectedCombatant.specId);
     this.classAbility = SPELLS_BY_CLASS[classId];
+
+    if (this.classAbility === undefined) {
+      console.warn('No Kyrian ability defined for class.', classId);
+      this.active = false;
+      return;
+    }
 
     const active = this.selectedCombatant.hasSoulbind(SOULBINDS.FORGELITE_PRIME_MIKANIKOS.id);
 
@@ -74,7 +80,7 @@ class EffusiveAnimaAccelerator extends Analyzer {
   onDebuff(event: ApplyDebuffEvent) {
     if (this.casts.length === 0) {
       debug &&
-        this.warn(event.ability.name, 'applied before first', this.classAbility.name, 'cast');
+        this.warn(event.ability.name, 'applied before first', this.classAbility?.name, 'cast');
       return;
     }
     const [lastCast, applications] = this.casts[this.casts.length - 1];
