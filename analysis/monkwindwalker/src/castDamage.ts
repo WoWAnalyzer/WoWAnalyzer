8f93379726bc4f442de7cacff6cca72cede6ec40
Oscar Busk <oscar.busk@gmail.com>
2022-04-18 22:06:01 +0200

    1 to many, many to 1 map

--- analysis/monkwindwalker/src/castDamage.ts
+++ analysis/monkwindwalker/src/castDamage.ts
@@ -9,28 +9,26 @@ import Spell from 'common/SPELLS/Spell';
  *
  * This map only makes sense with 1-to-1 connections.
  */
-const castDamageMap: ReadonlyMap<Spell, Spell> = new Map<Spell, Spell>([
-  [SPELLS.FISTS_OF_FURY_CAST, SPELLS.FISTS_OF_FURY_DAMAGE],
-  [SPELLS.WHIRLING_DRAGON_PUNCH_TALENT, SPELLS.WHIRLING_DRAGON_PUNCH_TALENT_TICK],
-  [SPELLS.SPINNING_CRANE_KICK, SPELLS.SPINNING_CRANE_KICK_DAMAGE],
-  [SPELLS.CHI_BURST_TALENT, SPELLS.CHI_BURST_TALENT_DAMAGE],
-  [SPELLS.CHI_WAVE_TALENT, SPELLS.CHI_WAVE_TALENT_DAMAGE],
-  [SPELLS.RISING_SUN_KICK, SPELLS.RISING_SUN_KICK_SECOND],
+const castDamageMap: ReadonlyMap<Spell, Spell[]> = new Map([
+  [SPELLS.FISTS_OF_FURY_CAST, [SPELLS.FISTS_OF_FURY_DAMAGE]],
+  [SPELLS.WHIRLING_DRAGON_PUNCH_TALENT, [SPELLS.WHIRLING_DRAGON_PUNCH_TALENT_TICK]],
+  [SPELLS.SPINNING_CRANE_KICK, [SPELLS.SPINNING_CRANE_KICK_DAMAGE]],
+  [SPELLS.CHI_BURST_TALENT, [SPELLS.CHI_BURST_TALENT_DAMAGE]],
+  [SPELLS.CHI_WAVE_TALENT, [SPELLS.CHI_WAVE_TALENT_DAMAGE]],
+  [SPELLS.RISING_SUN_KICK, [SPELLS.RISING_SUN_KICK_SECOND]],
+  [
+    SPELLS.FAELINE_STOMP_CAST,
+    [SPELLS.FAELINE_STOMP_PULSE_DAMAGE, SPELLS.FAELINE_STOMP_DAMAGE_AND_HEAL],
+  ],
 ]);
 
 const entries = Array.from(castDamageMap.entries());
 
 /** Provide id of cast ability and get damage spell back */
-export const castToDamage: { [id: string]: Spell | Spell[] } = {
-  ...Object.fromEntries(entries.map(([cast, damage]) => [cast.id, damage])),
-  [SPELLS.FAELINE_STOMP_CAST.id]: [
-    SPELLS.FAELINE_STOMP_PULSE_DAMAGE,
-    SPELLS.FAELINE_STOMP_DAMAGE_AND_HEAL,
-  ],
-};
+export const castToDamage: { [id: string]: Spell[] } = Object.fromEntries(
+  entries.map(([cast, damage]) => [cast.id, damage]),
+);
 /** Provide id of damage ability and get cast ability back */
-export const damageToCast: { [id: string]: Spell } = {
-  ...Object.fromEntries(entries.map(([cast, damage]) => [damage.id, cast])),
-  [SPELLS.FAELINE_STOMP_PULSE_DAMAGE.id]: SPELLS.FAELINE_STOMP_CAST,
-  [SPELLS.FAELINE_STOMP_DAMAGE_AND_HEAL.id]: SPELLS.FAELINE_STOMP_CAST,
-};
+export const damageToCast: { [id: string]: Spell } = Object.fromEntries(
+  entries.map(([cast, dSpells]) => dSpells.map((d) => [d, cast])).flat(),
+);
