2b3cc3bb1b29ab17ec5f05e25010e4a292b775a5
Oscar Busk <oscar.busk@gmail.com>
2022-04-18 14:13:56 +0200

    Rebuild EventsLoader into useEvents hook

--- src/interface/report/EventsLoader.tsx
+++ src/interface/report/EventsLoader.tsx
@@ -1,4 +1,4 @@
-import { fetchEvents } from 'common/fetchWclApi';
+import useEvents from 'interface/useEvents';
 import { AnyEvent } from 'parser/core/Events';
 import { WCLFight } from 'parser/core/Fight';
 import { PlayerInfo } from 'parser/core/Player';
@@ -12,51 +12,12 @@ interface Props {
   children: (isLoading: boolean, events: AnyEvent[] | null) => React.ReactNode;
 }
 
-interface State {
-  isLoading: boolean;
-  events: AnyEvent[] | null;
-}
+// TODO: Refactor Report to a functional component so this component can be
+//  removed in favor of using the hook
+const EventsLoader = ({ children, report, fight, player }: Props) => {
+  const events = useEvents({ report, fight, player });
 
-class EventsLoader extends React.PureComponent<Props, State> {
-  state = {
-    isLoading: true,
-    events: null,
-  };
+  return children(!events, events);
+};
 
-  componentDidMount() {
-    this.load();
-  }
-
-  componentDidUpdate(prevProps: Props, prevState: State) {
-    if (
-      prevProps.report !== this.props.report ||
-      prevProps.fight !== this.props.fight ||
-      prevProps.player !== this.props.player
-    ) {
-      this.setState({
-        isLoading: true,
-        events: null,
-      });
-      this.load();
-    }
-  }
-
-  async load() {
-    const events = await this.loadEvents();
-    this.setState({
-      isLoading: false,
-      events,
-    });
-  }
-  loadEvents() {
-    const { report, fight, player } = this.props;
-
-    return fetchEvents(report.code, fight.start_time, fight.end_time, player.id);
-  }
-
-  render() {
-    return this.props.children(this.state.isLoading, this.state.events);
-  }
-}
-
-export default EventsLoader;
+export default EventsLoader as React.ComponentType<Props>;
