e7bb88291ef6f1ae28f991520899b9d0a83dd14d
Oscar Busk <oscar.busk@gmail.com>
2022-04-22 11:11:25 +0200

    Track damage of all abilties PLUS their associated spells

--- analysis/monkwindwalker/src/constants.ts
+++ analysis/monkwindwalker/src/constants.ts
@@ -127,8 +127,13 @@ export const ABILITIES_AFFECTED_BY_PRIMORDIAL_POWER: Spell[] = [
  * Seems to match DAMAGE_AFFECTED_BY_MASTERY but excludes
  * Expel Harm
  */
-export const DAMAGE_AFFECTED_BY_PRIMORDIAL_POWER: Spell[] = ABILITIES_AFFECTED_BY_PRIMORDIAL_POWER.map(
-  (ability) => castToDamage[ability.id] || ability,
-).flat();
+export const DAMAGE_AFFECTED_BY_PRIMORDIAL_POWER: Spell[] = [
+  // Include both all original cast events
+  ...ABILITIES_AFFECTED_BY_PRIMORDIAL_POWER,
+  // And all known damage spells
+  ...ABILITIES_AFFECTED_BY_PRIMORDIAL_POWER.map(
+    (ability) => castToDamage[ability.id] || ability,
+  ).flat(),
+];
 
 export const BLACKOUT_KICK_COOLDOWN_REDUCTION_MS = 1000;
