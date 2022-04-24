7cef55a117d8fbc3503177c223db2a250b811f0c
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-04-24 15:03:34 +0200

    added damage + direct healing

--- analysis/priestdiscipline/src/modules/shadowlands/conduits/SwiftPenitence.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/conduits/SwiftPenitence.tsx
@@ -1,30 +1,41 @@
 import SPELLS from 'common/SPELLS';
-import Analyzer from 'parser/core/Analyzer';
-import { DamageEvent } from 'parser/core/Events';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, { DamageEvent, HealEvent } from 'parser/core/Events';
 import { Options } from 'parser/core/Module';
 import ConduitSpellText from 'parser/ui/ConduitSpellText';
+import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 
 import { SWIFT_PENITENCE_INCREASE } from '@wowanalyzer/priest-discipline/src/constants';
+import {
+  IsPenanceHealEvent,
+  IsPenanceDamageEvent,
+} from '@wowanalyzer/priest-discipline/src/modules/spells/Helper';
 
 import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
+import Penance from '../../spells/Penance';
 
-// is this needed?
 interface DirtyDamageEvent extends DamageEvent {
   penanceBoltNumber?: number;
 }
-// interface DirtyHealEvent extends HealEvent {
-//   penanceBoltNumber?: number;
-// }
+
+interface DirtyHealEvent extends HealEvent {
+  penanceBoltNumber?: number;
+}
 
 class SwiftPenitence extends Analyzer {
+  static dependencies = {
+    penance: Penance,
+  };
+
   conduitRank: number = 0;
   bonusSwiftPenitenceAtoneHealing: number = 0;
   bonusSwiftPenitenceDirectHealing: number = 0;
-  conduitIncrease: number = 0; // what is this?
+  bonusSwiftPenitenceDamage: number = 0;
+  conduitIncrease: number = 0;
 
   constructor(options: Options) {
     super(options);
@@ -37,7 +48,8 @@ class SwiftPenitence extends Analyzer {
     this.conduitIncrease = SWIFT_PENITENCE_INCREASE[this.conduitRank];
 
     this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.onAtone);
-    // this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.PENANCE_HEAL), this.onHeal);
+    this.addEventListener(Events.heal.by(SELECTED_PLAYER), this.onHeal);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER), this.onDamage);
   }
 
   onAtone(event: AtonementAnalyzerEvent) {
@@ -51,11 +63,38 @@ class SwiftPenitence extends Analyzer {
         totalHealing / (1 + this.conduitIncrease);
       if (adjustedHealing >= 0) {
         this.bonusSwiftPenitenceAtoneHealing += adjustedHealing;
-        console.log(event.healEvent);
       }
     }
     if (typeof penanceBoltNumber !== 'number') {
       return;
+    }
+  }
+
+  onHeal(event: HealEvent) {
+    if (!IsPenanceHealEvent(event)) {
+      return;
+    }
+    const { penanceBoltNumber } = event as DirtyHealEvent;
+    if (typeof penanceBoltNumber !== 'number') {
+      return;
+    }
+    if (penanceBoltNumber === 0) {
+      const totalHealing = event.amount + (event.overheal || 0) + (event.absorbed || 0);
+      const adjustedHealing =
+        event.amount + (event.absorbed || 0) - totalHealing / (1 + this.conduitIncrease);
+      if (adjustedHealing >= 0) {
+        this.bonusSwiftPenitenceDirectHealing += adjustedHealing;
+      }
+    }
+  }
+
+  onDamage(event: DamageEvent) {
+    if (!IsPenanceDamageEvent(event)) {
+      return;
+    }
+
+    if (event.penanceBoltNumber === 0) {
+      this.bonusSwiftPenitenceDamage += event.amount / (1 + this.conduitIncrease);
     }
   }
 
@@ -68,7 +107,9 @@ class SwiftPenitence extends Analyzer {
       >
         <>
           <ConduitSpellText spellId={SPELLS.SWIFT_PENITENCE.id} rank={this.conduitRank}>
-            <ItemHealingDone amount={this.bonusSwiftPenitenceAtoneHealing} />
+            <ItemHealingDone amount={this.bonusSwiftPenitenceAtoneHealing} /> <br />
+            <ItemHealingDone amount={this.bonusSwiftPenitenceDirectHealing} /> <br />
+            <ItemDamageDone amount={this.bonusSwiftPenitenceDamage} />
           </ConduitSpellText>
         </>
       </Statistic>
