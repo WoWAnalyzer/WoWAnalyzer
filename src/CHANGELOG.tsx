c3bb2890e875c04799650bcd650b78ac28dd86ca
Oscar Busk <oscar.busk@gmail.com>
2022-04-24 22:38:07 +0200

    [iqd] Add changelog entry

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -59,6 +59,7 @@ import SpellLink from 'interface/SpellLink';
 // prettier-ignore
 export default [
   change(date(2022, 4, 27), 'Updated stat multiplier table for Shadowlands values', Putro),
+  change(date(2022, 4, 26), <>Add statistics for effects of <ItemLink id={ITEMS.INSCRUTABLE_QUANTUM_DEVICE.id} />.</>, nullDozzer),
   change(date(2022, 4, 20), 'Preformance increase for talent identification.', Abelito75),
   change(date(2022, 4, 19), 'Prevent invalid spells from passing internal tests and getting deployed.', emallson),
   change(date(2022, 4, 17), 'Fix conduits not being parsed correctly in some cases', nullDozzer),
