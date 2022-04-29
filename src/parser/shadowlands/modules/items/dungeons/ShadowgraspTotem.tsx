4f4964f178187fbc969a38eb84d4cacf7ec8d87a
Oscar Busk <oscar.busk@gmail.com>
2022-04-29 22:49:29 +0200

    [sgt] Use existing systems to track spells and buffs

--- src/parser/shadowlands/modules/items/dungeons/ShadowgraspTotem.tsx
+++ src/parser/shadowlands/modules/items/dungeons/ShadowgraspTotem.tsx
@@ -1,16 +1,23 @@
 import { formatNumber, formatPercentage } from 'common/format';
 import ITEMS from 'common/ITEMS';
 import SPELLS from 'common/SPELLS';
+import Spell from 'common/SPELLS/Spell';
 import { ItemIcon, ItemLink, TooltipElement } from 'interface';
 import Analyzer, { Options, SELECTED_PLAYER, SELECTED_PLAYER_PET } from 'parser/core/Analyzer';
-import calculateMaxCasts from 'parser/core/calculateMaxCasts';
 import Events, { CastEvent, DamageEvent, HealEvent, Item } from 'parser/core/Events';
+import Abilities from 'parser/core/modules/Abilities';
+import CastEfficiency from 'parser/shared/modules/CastEfficiency';
+import SpellUsable from 'parser/shared/modules/SpellUsable';
 import BoringItemValueText from 'parser/ui/BoringItemValueText';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 
+const ITEM: Item = ITEMS.SHADOWGRASP_TOTEM;
+const SUMMON_SPELL: Spell = SPELLS.SHADOWGRASP_TOTEM_SUMMON;
+const DAMAGE_SPELL: Spell = SPELLS.SHADOWGRASP_TOTEM_DAMAGE;
+const HEAL_SPELL: Spell = SPELLS.SHADOWGRASP_TOTEM_HEAL;
 const COOLDOWN_REDUCTION_SECONDS = 15;
 const DEFAULT_COOLDOWN_SECONDS = 60 * 2;
 const DEFAULT_TICK_RATE_MS = 1_000;
@@ -28,41 +35,60 @@ function calculateHaste(baseTickRate: number, currentTickRate: number) {
  * - https://www.warcraftlogs.com/reports/twQhDxWCZN2jPbnp#fight=6&type=damage-done&source=13
  */
 class ShadowgraspTotem extends Analyzer {
+  static dependencies = {
+    abilities: Abilities,
+    castEfficiency: CastEfficiency,
+    spellUsable: SpellUsable,
+  };
+  protected abilities!: Abilities;
+  protected castEfficiency!: CastEfficiency;
+  protected spellUsable!: SpellUsable;
+
   private item: Item;
 
   /** Timestamps of damage events since the last cast */
   private currentDot?: number[];
   /** All timestamps of all casts */
   private dots: number[][] = [];
-  private numberCasts = 0;
   private totalDamage = 0;
   private healing: number[] = [];
 
-  constructor(options: Options) {
+  constructor(
+    options: Options & {
+      abilities: Abilities;
+      castEfficiency: CastEfficiency;
+      spellUsable: SpellUsable;
+    },
+  ) {
     super(options);
 
-    this.item = this.selectedCombatant.getItem(ITEMS.SHADOWGRASP_TOTEM.id)!;
+    this.item = this.selectedCombatant.getItem(ITEM.id)!;
     if (this.item == null) {
       this.active = false;
       return;
     }
 
+    // Add cast as an ability to show effective usage and cooldown in timeline
+    options.abilities.add({
+      spell: SUMMON_SPELL.id,
+      category: Abilities.SPELL_CATEGORIES.ITEMS,
+      cooldown: DEFAULT_COOLDOWN_SECONDS,
+      gcd: null,
+      castEfficiency: {
+        suggestion: true,
+        recommendedEfficiency: 0.8,
+      },
+    });
+
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(SUMMON_SPELL), this.onCast);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER_PET).spell(DAMAGE_SPELL), this.onDamage);
     this.addEventListener(
-      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.SHADOWGRASP_TOTEM_SUMMON),
-      this.onCast,
-    );
-    this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER_PET).spell(SPELLS.SHADOWGRASP_TOTEM_DAMAGE),
-      this.onDamage,
-    );
-    this.addEventListener(
-      Events.heal.by(SELECTED_PLAYER).to(SELECTED_PLAYER).spell(SPELLS.SHADOWGRASP_TOTEM_HEAL),
+      Events.heal.by(SELECTED_PLAYER).to(SELECTED_PLAYER).spell(HEAL_SPELL),
       this.onHeal,
     );
   }
 
   onCast(event: CastEvent) {
-    this.numberCasts += 1;
     this.dots.push((this.currentDot = []));
   }
 
@@ -73,6 +99,15 @@ class ShadowgraspTotem extends Analyzer {
 
   onHeal(event: HealEvent) {
     this.healing.push((event.amount || 0) + (event.absorbed || 0));
+
+    // Inform other modules that cooldown has been reduced
+    if (this.spellUsable.isOnCooldown(SUMMON_SPELL.id)) {
+      this.spellUsable.reduceCooldown(SUMMON_SPELL.id, COOLDOWN_REDUCTION_SECONDS * 1000);
+    }
+  }
+
+  private getCastEfficiancy() {
+    return this.castEfficiency.getCastEfficiencyForSpellId(SUMMON_SPELL.id);
   }
 
   private get numberHeals() {
@@ -108,18 +143,9 @@ class ShadowgraspTotem extends Analyzer {
     // The heal is triggered at the same point as the cooldown reduction
     return this.numberHeals * COOLDOWN_REDUCTION_SECONDS;
   }
-
-  private get maxCasts() {
-    // It should still be on cooldown when the fight is over hopefully
-    return Math.ceil(
-      calculateMaxCasts(
-        DEFAULT_COOLDOWN_SECONDS,
-        this.owner.fightDuration + this.cooldownReductionSeconds * 1000,
-      ),
-    );
-  }
-
   statistic() {
+    const { casts = 0, maxCasts = 0 } = this.getCastEfficiancy() ?? {};
+
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(1)}
@@ -128,7 +154,7 @@ class ShadowgraspTotem extends Analyzer {
         tooltip={
           <>
             <ItemLink id={this.item.id} details={this.item} /> did {formatNumber(this.totalDamage)}{' '}
-            damage in the {formatNumber(this.numberCasts)} times it was used.
+            damage in the {formatNumber(casts)} times it was used.
             <br />
             Targets died {this.numberHeals} times, which healed a total of{' '}
             {formatNumber(this.totalHealing)} (average {formatNumber(this.averageHealing)} HP) and
@@ -137,7 +163,7 @@ class ShadowgraspTotem extends Analyzer {
         }
       >
         <BoringItemValueText item={this.item}>
-          {this.numberCasts} Uses <small>{this.maxCasts} possible</small>
+          {casts} Uses <small>{maxCasts} possible</small>
           <div className="value" style={{ paddingBottom: '0.5em' }}>
             <ItemDamageDone amount={this.totalDamage} />
             <small style={{ display: 'block' }}>
