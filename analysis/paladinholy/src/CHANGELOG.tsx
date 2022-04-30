e7717ad773a31fea69f3deb9a7bad303f8b02229
5chmidt <pschmidt@buffalo.edu>
2022-04-30 10:15:03 -0400

    Added changelog entry.

--- analysis/paladinholy/src/CHANGELOG.tsx
+++ analysis/paladinholy/src/CHANGELOG.tsx
@@ -1,10 +1,11 @@
 
 import { change, date } from 'common/changelog';
 import SPELLS from 'common/SPELLS'
-import { Abelito75, acornellier, Putro, Sref, xizbow, Zeboot } from 'CONTRIBUTORS';
+import { Abelito75, acornellier, Putro, Sref, xizbow, Zeboot, HolySchmidt } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2022, 4, 22), <>Added tracking for <SpellLink id={SPELLS.VANQUISHERS_HAMMER.id}/>. </>, HolySchmidt),
   change(date(2022, 4, 22), <>Update example log. </>, Abelito75),
   change(date(2022, 4, 15), <>Fixed an issue when trying to load logs with Beacon of Virtue. </>, Abelito75),
   change(date(2022, 4, 15), <>Added Holy Power Generated Per Minute statistic </>, xizbow),
