add763cf3e0bf8c29db3cb81026af83b7d7962eb
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-02 09:44:48 +0200

    storing changes

--- analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
@@ -2,20 +2,21 @@
 import SPELLS from 'common/SPELLS';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 // import ApplyBuff from 'parser/shared/normalizers/ApplyBuff';
-import Events, { ApplyBuffEvent } from 'parser/core/Events';
+import Events, { ApplyBuffEvent, DamageEvent, Ability } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
-// DamageEvent, HealEvent, Ability, CastEvent,
-// import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
+
+import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
 
 // const CLARITY_EXTENSION_DURATION = 6000;
 
 class ClarityOfMindEvang extends Analyzer {
   private atonementHealing: number = 0;
   private healingMap: Map<number, number> = new Map();
+  private abilityMap: Map<number, Ability> = new Map();
   raptureBuffActive = false;
   raptureShields = [] as any;
 
@@ -31,6 +32,7 @@ class ClarityOfMindEvang extends Analyzer {
       return;
     }
 
+    this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.handleAtone);
     this.addEventListener(Events.applybuff.by(SELECTED_PLAYER), this.checkRapture);
     // this.addEventListener(ApplyBuff, this.raptureTest)
   }
@@ -56,7 +58,32 @@ class ClarityOfMindEvang extends Analyzer {
     }
   }
 
+  private handleAtone(event: AtonementAnalyzerEvent) {
+    console.log(event);
+    if (event) {
+      event;
+    }
+    console.log(this.owner.formatTimestamp(event.timestamp));
+    this.attributeToMap(event.healEvent.amount, event.damageEvent);
+    this.atonementHealing += event.healEvent.amount;
+  }
+
+  private attributeToMap(amount: number, sourceEvent?: DamageEvent) {
+    if (!sourceEvent) {
+      return;
+    }
+    const { ability } = sourceEvent;
+
+    // Set ability in map
+    this.abilityMap.set(ability.guid, ability);
+
+    // Attribute healing
+    const currentValue = this.healingMap.get(ability.guid) || 0;
+    this.healingMap.set(ability.guid, currentValue + amount);
+  }
+
   statistic() {
+    // console.log(this.healingMap);
     // console.log(this.raptureShields);
     return (
       <Statistic
