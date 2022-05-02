5c490617453c873d4b96b824631c5f6e4512532a
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-02 23:31:12 +0200

    initial module. working when combining rapture + evang

--- analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
@@ -1,26 +1,24 @@
-// import { formatThousands } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-// import ApplyBuff from 'parser/shared/normalizers/ApplyBuff';
-import Events, { ApplyBuffEvent, DamageEvent, Ability } from 'parser/core/Events';
+import Events, { CastEvent } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 
+import { POWER_WORD_SHIELD_ATONEMENT_DUR } from '@wowanalyzer/priest-discipline/src/constants';
+
 import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
 
-// const CLARITY_EXTENSION_DURATION = 6000;
+const CLARITY_EXTENSION_DURATION = 6000;
+const EVANG_EXTENSION_DURATION = 6000;
 
 class ClarityOfMindEvang extends Analyzer {
   private atonementHealing: number = 0;
-  private healingMap: Map<number, number> = new Map();
-  private abilityMap: Map<number, Ability> = new Map();
-  raptureBuffActive = false;
+  raptureBuffActive = true;
   raptureShields = [] as any;
 
-  // private abilityMap: Map<number, Ability> = new Map();
   constructor(options: Options) {
     super(options);
 
@@ -33,58 +31,60 @@ class ClarityOfMindEvang extends Analyzer {
     }
 
     this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.handleAtone);
-    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER), this.checkRapture);
-    // this.addEventListener(ApplyBuff, this.raptureTest)
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.checkRapture);
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.applyRapture);
+    this.addEventListener(
+      Events.removebuff.spell(SPELLS.RAPTURE).by(SELECTED_PLAYER),
+      this.raptureRemoved,
+    );
   }
 
-  private checkRapture(event: ApplyBuffEvent) {
+  private checkRapture(event: CastEvent) {
     const spellId = event.ability.guid;
-    if (spellId !== SPELLS.ATONEMENT_BUFF.id || SPELLS.RAPTURE.id) {
+    if (spellId !== SPELLS.POWER_WORD_SHIELD.id && spellId !== SPELLS.RAPTURE.id) {
       return;
     }
-    console.log(event);
-    this.selectedCombatant.buffs.forEach((buff) => {
-      if (buff.ability.name === 'Rapture') {
-        console.log(this.owner.formatTimestamp(buff.timestamp));
-        // console.log(buff) ;
-      }
-    });
-    // console.log(this.selectedCombatant.has2Piece());
-    if (SELECTED_PLAYER) {
+    if (this.raptureBuffActive) {
       this.raptureShields.push({
         applyBuff: event,
-        atonementEvents: [],
       });
     }
   }
 
-  private handleAtone(event: AtonementAnalyzerEvent) {
-    console.log(event);
-    if (event) {
-      event;
-    }
-    console.log(this.owner.formatTimestamp(event.timestamp));
-    this.attributeToMap(event.healEvent.amount, event.damageEvent);
-    this.atonementHealing += event.healEvent.amount;
-  }
-
-  private attributeToMap(amount: number, sourceEvent?: DamageEvent) {
-    if (!sourceEvent) {
+  applyRapture(event: CastEvent) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.RAPTURE.id) {
       return;
     }
-    const { ability } = sourceEvent;
+    // return this.raptureBuffActive = true;
+  }
 
-    // Set ability in map
-    this.abilityMap.set(ability.guid, ability);
+  raptureRemoved() {
+    return (this.raptureBuffActive = false);
+  }
 
-    // Attribute healing
-    const currentValue = this.healingMap.get(ability.guid) || 0;
-    this.healingMap.set(ability.guid, currentValue + amount);
+  private handleAtone(event: AtonementAnalyzerEvent) {
+    this.raptureShields.forEach((rapture: any) => {
+      const end =
+        rapture.applyBuff.timestamp +
+        EVANG_EXTENSION_DURATION +
+        POWER_WORD_SHIELD_ATONEMENT_DUR +
+        CLARITY_EXTENSION_DURATION;
+      const start =
+        rapture.applyBuff.timestamp + EVANG_EXTENSION_DURATION + POWER_WORD_SHIELD_ATONEMENT_DUR;
+
+      if (
+        event.targetID === rapture.applyBuff.targetID &&
+        event.timestamp > start &&
+        event.timestamp < end
+      ) {
+        this.atonementHealing += event.healEvent.amount;
+      }
+    });
   }
 
   statistic() {
-    // console.log(this.healingMap);
-    // console.log(this.raptureShields);
+    console.log(`${this.atonementHealing} - testing to check`);
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(13)}
@@ -101,7 +101,7 @@ class ClarityOfMindEvang extends Analyzer {
       >
         <>
           <BoringSpellValueText spellId={SPELLS.CLARITY_OF_MIND.id}>
-            <ItemHealingDone amount={0} />
+            <ItemHealingDone amount={this.atonementHealing} />
           </BoringSpellValueText>
         </>
       </Statistic>
