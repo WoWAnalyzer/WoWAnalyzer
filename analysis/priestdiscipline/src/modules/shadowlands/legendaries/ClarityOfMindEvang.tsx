b197814ddf8e161c3b130bca4c13c4e90449b3f3
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-03 01:34:45 +0200

    added to the tooltip

--- analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/legendaries/ClarityOfMindEvang.tsx
@@ -1,8 +1,10 @@
 import SPELLS from 'common/SPELLS';
+import { SpellLink } from 'interface';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { CastEvent } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
+import ItemManaGained from 'parser/ui/ItemManaGained';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
@@ -13,10 +15,11 @@ import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementA
 
 const CLARITY_EXTENSION_DURATION = 6000;
 const EVANG_EXTENSION_DURATION = 6000;
+const POWER_WORD_SHIELD_MANA_COST = 1550;
 
 class ClarityOfMindEvang extends Analyzer {
   private atonementHealing: number = 0;
-  raptureBuffActive = true;
+  raptureBuffActive = false;
   raptureShields = [] as any;
 
   constructor(options: Options) {
@@ -32,6 +35,7 @@ class ClarityOfMindEvang extends Analyzer {
 
     this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.handleAtone);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.checkRapture);
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.checkEvang);
     this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.applyRapture);
     this.addEventListener(
       Events.removebuff.spell(SPELLS.RAPTURE).by(SELECTED_PLAYER),
@@ -56,7 +60,23 @@ class ClarityOfMindEvang extends Analyzer {
     if (spellId !== SPELLS.RAPTURE.id) {
       return;
     }
-    // return this.raptureBuffActive = true;
+    this.raptureShields.push({ applyBuff: event });
+    this.raptureBuffActive = true;
+  }
+
+  checkEvang(event: CastEvent) {
+    const spellId = event.ability.guid;
+    if (spellId !== SPELLS.EVANGELISM_TALENT.id) {
+      return;
+    }
+    this.raptureShields.forEach((shield: any, index: number) => {
+      if (
+        event.timestamp > shield.applyBuff.timestamp &&
+        event.timestamp < shield.applyBuff.timestamp + POWER_WORD_SHIELD_ATONEMENT_DUR
+      ) {
+        this.raptureShields[index].extendedByEvang = true;
+      }
+    });
   }
 
   raptureRemoved() {
@@ -67,11 +87,13 @@ class ClarityOfMindEvang extends Analyzer {
     this.raptureShields.forEach((rapture: any) => {
       const end =
         rapture.applyBuff.timestamp +
-        EVANG_EXTENSION_DURATION +
+        (rapture.extendedByEvang ? EVANG_EXTENSION_DURATION : 0) +
         POWER_WORD_SHIELD_ATONEMENT_DUR +
         CLARITY_EXTENSION_DURATION;
       const start =
-        rapture.applyBuff.timestamp + EVANG_EXTENSION_DURATION + POWER_WORD_SHIELD_ATONEMENT_DUR;
+        rapture.applyBuff.timestamp +
+        (rapture.extendedByEvang ? EVANG_EXTENSION_DURATION : 0) +
+        POWER_WORD_SHIELD_ATONEMENT_DUR;
 
       if (
         event.targetID === rapture.applyBuff.targetID &&
@@ -84,7 +106,8 @@ class ClarityOfMindEvang extends Analyzer {
   }
 
   statistic() {
-    console.log(`${this.atonementHealing} - testing to check`);
+    const manaSaved = 0.2 * POWER_WORD_SHIELD_MANA_COST * this.raptureShields.length;
+    console.log(manaSaved);
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(13)}
@@ -92,16 +115,21 @@ class ClarityOfMindEvang extends Analyzer {
         category={STATISTIC_CATEGORY.ITEMS}
         tooltip={
           <>
-            <strong>Bonus Atonement Healing:</strong>
-            <br />
-            <strong>Direct Healing:</strong>
-            <br />
+            This value is calculated from the healing contributed from the last 6 seconds of the
+            applied <SpellLink id={SPELLS.ATONEMENT_BUFF.id} />. If you cast{' '}
+            <SpellLink id={SPELLS.POWER_WORD_SHIELD.id} /> during{' '}
+            <SpellLink id={SPELLS.RAPTURE.id} /> and the atonement was only extended by Evangelism
+            due to <SpellLink id={SPELLS.CLARITY_OF_MIND.id} />
+            's atonement exenstion, it is not counted by the module as with proper play, it is not
+            reccomended to begin a ramp over 15 seconds before pressing{' '}
+            <SpellLink id={SPELLS.EVANGELISM_TALENT.id} />.
           </>
         }
       >
         <>
           <BoringSpellValueText spellId={SPELLS.CLARITY_OF_MIND.id}>
-            <ItemHealingDone amount={this.atonementHealing} />
+            <ItemHealingDone amount={this.atonementHealing} /> <br />
+            <ItemManaGained amount={manaSaved} />
           </BoringSpellValueText>
         </>
       </Statistic>
