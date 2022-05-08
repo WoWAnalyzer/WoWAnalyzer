fd87743f5454dfd4747674d32dd18bf7ad4f99fc
Oscar Busk <oscar.busk@gmail.com>
2022-05-09 01:01:33 +0200

    Add example of WOO chi reduction

--- analysis/monkwindwalker/src/modules/resources/SpellChiCost.ts
+++ analysis/monkwindwalker/src/modules/resources/SpellChiCost.ts
@@ -20,6 +20,8 @@ class SpellChiCost extends SpellResourceCost {
   protected getResourceCost(event: CastEvent) {
     let cost = super.getResourceCost(event);
 
+    // Example
+    // https://www.warcraftlogs.com/reports/GDqkTZ9JVr7AjH4X/#fight=33&source=4&type=damage-done
     if (this.selectedCombatant.hasBuff(SPELLS.WEAPONS_OF_ORDER_CHI_DISCOUNT.id)) {
       cost -= 1;
     }
