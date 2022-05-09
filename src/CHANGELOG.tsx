1019614ae5d9000066cb8eae68f7fa4eeade1451
Oscar Busk <oscar.busk@gmail.com>
2022-05-09 02:27:36 +0200

    Add changelog entry

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -58,6 +58,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2022, 5, 9), <>Handle "extraattacks" events for things like <SpellLink id={SPELLS.WINDFURY_TOTEM.id}/></>, nullDozzer),
   change(date(2022, 5, 6), 'Condensed helpful functions into one file.', Abelito75),
   change(date(2022, 5, 5), 'Add support for Ascended Vigor', xepheris),
   change(date(2022, 5, 5), 'Add support for Blood-Spattered Scale and Codex of the First Technique trinkets.', xepheris),
