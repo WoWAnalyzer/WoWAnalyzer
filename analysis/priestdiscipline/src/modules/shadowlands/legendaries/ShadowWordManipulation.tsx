493665553bee6a61307dde5547fc664d44ba6179
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-11 19:20:47 +0200

    initial commit to make file

---
+++ analysis/priestdiscipline/src/modules/shadowlands/legendaries/ShadowWordManipulation.tsx
@@ -0,0 +1,22 @@
+// import { formatThousands } from 'common/format';
+// import SPELLS from 'common/SPELLS';
+// import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+// import Events, { DamageEvent, HealEvent } from 'parser/core/Events';
+// import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
+// import ItemHealingDone from 'parser/ui/ItemHealingDone';
+// import ItemManaGained from 'parser/ui/ItemManaGained';
+// import Statistic from 'parser/ui/Statistic';
+// import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+// import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+
+// import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
+
+// class ShadowWordManipulation extends Analyzer{
+
+//   constructor(options: Options) {
+//     super(options);
+
+//     // this.addEventListener()
+//   }
+
+// }
