532801d32a79785e50d03ace9b23df7aac29c554
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-12 01:31:10 +0200

    initial commit

--- analysis/priestdiscipline/src/modules/shadowlands/legendaries/ShadowWordManipulation.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/legendaries/ShadowWordManipulation.tsx
@@ -1,22 +1,95 @@
-// import { formatThousands } from 'common/format';
-// import SPELLS from 'common/SPELLS';
-// import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-// import Events, { DamageEvent, HealEvent } from 'parser/core/Events';
-// import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
-// import ItemHealingDone from 'parser/ui/ItemHealingDone';
-// import ItemManaGained from 'parser/ui/ItemManaGained';
-// import Statistic from 'parser/ui/Statistic';
-// import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
-// import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+import SPELLS from 'common/SPELLS';
+import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, { ApplyBuffStackEvent, RemoveBuffEvent } from 'parser/core/Events';
+import StatTracker from 'parser/shared/modules/StatTracker';
+import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
+import ItemHealingDone from 'parser/ui/ItemHealingDone';
+import Statistic from 'parser/ui/Statistic';
+import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 
-// import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
+import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../../core/AtonementAnalyzer';
 
-// class ShadowWordManipulation extends Analyzer{
+class ShadowWordManipulation extends Analyzer {
+  protected statTracker!: StatTracker;
 
-//   constructor(options: Options) {
-//     super(options);
+  static dependencies = {
+    statTracker: StatTracker,
+  };
 
-//     // this.addEventListener()
-//   }
+  healing = 0;
+  swmBuffActive = false;
+  stacks = 0;
+  totalCritPercentage = 0;
+  constructor(options: Options) {
+    super(options);
 
-// }
+    this.active = this.selectedCombatant.hasLegendary(SPELLS.SHADOW_WORD_MANIPULATION);
+    if (!this.active) {
+      return;
+    }
+
+    this.addEventListener(
+      Events.applybuffstack.spell(SPELLS.SHADOW_WORD_MANIPULATION_BUFF).by(SELECTED_PLAYER),
+      this.swmBuffed,
+    );
+
+    this.addEventListener(
+      Events.removebuff.spell(SPELLS.SHADOW_WORD_MANIPULATION_BUFF).by(SELECTED_PLAYER),
+      this.swmRemoved,
+    );
+
+    this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.handleAtone);
+  }
+  swmBuffed(event: ApplyBuffStackEvent) {
+    this.swmBuffActive = true;
+    this.stacks = event.stack;
+  }
+
+  swmRemoved(event: RemoveBuffEvent) {
+    this.swmBuffActive = false;
+    this.stacks = 0;
+  }
+
+  handleAtone(event: AtonementAnalyzerEvent) {
+    if (!('absorb' in event.healEvent) || !this.swmBuffActive) {
+      // ignoring spirit shell
+      // console.log(event);
+      return;
+    }
+    console.log(event);
+    const bonusCrit = this.stacks * 0.05;
+    const effectiveHealing = event.healEvent.amount + (event.healEvent.absorbed || 0);
+    const totalHealing = effectiveHealing + (event.healEvent.overheal || 0); // raw heal
+    const swmHeal =
+      totalHealing -
+      (totalHealing / (1 + this.statTracker.currentCritPercentage + bonusCrit)) *
+        (this.statTracker.currentCritPercentage + 1);
+    const effectiveSwmHeal = swmHeal - (event.healEvent.overheal || 0);
+    console.log(effectiveSwmHeal);
+    if (effectiveSwmHeal > 0) {
+      this.healing += effectiveSwmHeal;
+    }
+    // this.statTracker.currentCritPercentage
+  }
+
+  statistic() {
+    return (
+      <Statistic
+        position={STATISTIC_ORDER.OPTIONAL(13)}
+        size="flexible"
+        category={STATISTIC_CATEGORY.ITEMS}
+        tooltip={<></>}
+      >
+        <>
+          <BoringSpellValueText spellId={SPELLS.SHADOW_WORD_MANIPULATION.id}>
+            <ItemHealingDone amount={this.healing} /> <br />
+            {/* <ItemManaGained amount={manaSaved} /> */}
+          </BoringSpellValueText>
+        </>
+      </Statistic>
+    );
+  }
+}
+
+export default ShadowWordManipulation;
