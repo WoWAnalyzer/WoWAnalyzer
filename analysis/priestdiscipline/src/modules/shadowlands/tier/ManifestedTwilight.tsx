b6338cb7d74fdff18a28565fe1497df196891f9f
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-05-16 18:56:27 +0200

    added some first steps to cehcking when the buff is active

--- analysis/priestdiscipline/src/modules/shadowlands/tier/ManifestedTwilight.tsx
+++ analysis/priestdiscipline/src/modules/shadowlands/tier/ManifestedTwilight.tsx
@@ -1,12 +1,27 @@
 import SPELLS from 'common/SPELLS';
 import SpellLink from 'interface/SpellLink';
-import Analyzer, { Options } from 'parser/core/Analyzer';
+import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, { CastEvent } from 'parser/core/Events';
 import BoringValueText from 'parser/ui/BoringValueText';
+import ItemManaGained from 'parser/ui/ItemManaGained';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+
+import { SHADOW_MEND_ATONEMENT_DUR } from '../../../constants';
 // import SpellIcon from 'interface/SpellIcon';
+const MANIFESTED_TWILIGHT_BONUS_MS = 2000;
+
+type SmendInfo = {
+  cast: CastEvent;
+  extendedByEvangIn2P?: boolean;
+  extendedByEvangPre2P?: boolean;
+};
+
 class ManifestedTwilight extends Analyzer {
+  twoPieceShadowMends = 0;
+  shadowMendCasts: SmendInfo[] = [];
+  manaSaved = 0;
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.has2Piece();
@@ -14,9 +29,49 @@ class ManifestedTwilight extends Analyzer {
     if (!this.active) {
       return;
     }
+
+    this.addEventListener(
+      Events.cast.spell(SPELLS.SHADOW_MEND).by(SELECTED_PLAYER),
+      this.onShadowMend,
+    );
+
+    this.addEventListener(
+      Events.cast.spell(SPELLS.EVANGELISM_TALENT).by(SELECTED_PLAYER),
+      this.onEvang,
+    );
+  }
+
+  onShadowMend(event: CastEvent) {
+    if (this.selectedCombatant.hasBuff(SPELLS.MANIFESTED_TWILIGHT_BUFF_2P.id)) {
+      this.twoPieceShadowMends += 1;
+      this.shadowMendCasts.push({ cast: event });
+      this.manaSaved += SPELLS.SHADOW_MEND.manaCost;
+    }
+  }
+
+  onEvang(event: CastEvent) {
+    this.shadowMendCasts.forEach((shadowMend, index: number) => {
+      if (
+        event.timestamp > shadowMend.cast.timestamp &&
+        event.timestamp < shadowMend.cast.timestamp + SHADOW_MEND_ATONEMENT_DUR
+      ) {
+        this.shadowMendCasts[index].extendedByEvangPre2P = true;
+      }
+
+      if (
+        event.timestamp > shadowMend.cast.timestamp + SPELLS.SHADOW_MEND.atonementDuration &&
+        event.timestamp <
+          shadowMend.cast.timestamp +
+            SPELLS.SHADOW_MEND.atonementDuration +
+            MANIFESTED_TWILIGHT_BONUS_MS
+      ) {
+        this.shadowMendCasts[index].extendedByEvangIn2P = true;
+      }
+    });
   }
 
   statistic() {
+    console.log(this.shadowMendCasts);
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(13)}
@@ -30,7 +85,11 @@ class ManifestedTwilight extends Analyzer {
               <SpellLink id={SPELLS.MANIFESTED_TWILIGHT_BUFF_2P.id} />
             </>
           }
-        ></BoringValueText>
+        >
+          <>
+            <ItemManaGained amount={this.manaSaved} />
+          </>
+        </BoringValueText>
       </Statistic>
     );
   }
