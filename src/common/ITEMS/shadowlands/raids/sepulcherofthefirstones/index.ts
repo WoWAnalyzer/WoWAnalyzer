e03db7d43fad6119aa17c7592759a99d1a0ebf70
Oscar Busk <oscar.busk@gmail.com>
2022-05-18 23:44:51 +0200

    Add files for Sepulcher raid items and spells

---
+++ src/common/ITEMS/shadowlands/raids/sepulcherofthefirstones/index.ts
@@ -0,0 +1,35 @@
+/**
+ * NAME: {
+ *   id: number,
+ *   name: string,
+ *   icon: string,
+ *   quality: number,
+ * },
+ */
+import { ItemList } from 'common/ITEMS/Item';
+
+const items: ItemList = {
+  //Vigilant Guardian
+  //endregion
+  //Skolex, the Insatiable Ravener
+  //endregion
+  //Artificier Xy'mox
+  //endregion
+  //Halondrus the Reclaimer
+  //endregion
+  //Dausegne, the Fallen Oracle
+  //endregion
+  //Prototype Pantheon
+  //endregion
+  //Lihuvim, Principle Architect
+  //endregion
+  //Anduin Wrynn
+  //endregion
+  //Lords of Dread
+  //endregion
+  //Rygelon
+  //endregion
+  //The Jailer
+  //endregion
+};
+export default items;
