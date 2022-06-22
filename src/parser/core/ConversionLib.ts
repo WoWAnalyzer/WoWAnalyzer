4e7f3d73c005e26b7498f6b4cbe13a71976bcff7
Abelito75 <abelgm@charter.net>
2022-06-21 22:15:06 -0400

    Comment

--- src/parser/core/ConversionLib.ts
+++ src/parser/core/ConversionLib.ts
@@ -1,5 +1,11 @@
 import Spell from 'common/SPELLS/Spell';
 
+/**
+ * A simple function to convert spell objects or numbers to spell ID
+ *
+ * @param toID The Spell you want to become an ID
+ * @returns the id as a number
+ */
 export function convertToSpellID(toID: number | Spell): number {
   let spellId = toID;
   const spellObj = toID as Spell;
