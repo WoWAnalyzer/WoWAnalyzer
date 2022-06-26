e30bcc66e6516a95a245e38a34a2fb31a73cae3d
J David Smith <emallson@emallson.net>
2022-06-26 16:56:02 -0400

    hide guide prototype by default

--- src/interface/report/Results/Overview.tsx
+++ src/interface/report/Results/Overview.tsx
@@ -14,8 +14,23 @@ interface Props {
   guide?: React.FC;
 }
 
+const PrototypeSwitcher = ({
+  guideMode,
+  setGuideMode,
+}: {
+  guideMode: boolean;
+  setGuideMode: (value: boolean) => void;
+}) => (
+  <a style={{ justifySelf: 'end' }} href="#prototype" onClick={() => setGuideMode(!guideMode)}>
+    {guideMode ? 'Return to Normal View' : 'View Prototype'}
+  </a>
+);
+
 const Overview = ({ guide: GuideComponent, checklist, issues }: Props) => {
   const config = useConfig();
+  const [guideMode, setGuideMode] = React.useState(
+    window.sessionStorage?.getItem('guideMode') === 'true',
+  );
 
   let alert: ReactNode = null;
   if (config.pages?.overview?.text) {
@@ -32,12 +47,19 @@ const Overview = ({ guide: GuideComponent, checklist, issues }: Props) => {
     );
   }
 
-  return GuideComponent ? (
-    <div className="container">
+  const setMode = React.useCallback((value: boolean) => {
+    window.sessionStorage.setItem('guideMode', value.toString());
+    setGuideMode(value);
+  }, []);
+
+  return guideMode && GuideComponent ? (
+    <div className="container" style={{ display: 'grid' }}>
+      <PrototypeSwitcher guideMode={guideMode} setGuideMode={setMode} />
       <GuideComponent />
     </div>
   ) : (
-    <div className="container">
+    <div className="container" style={{ display: 'grid' }}>
+      {GuideComponent && <PrototypeSwitcher guideMode={guideMode} setGuideMode={setMode} />}
       {alert}
 
       {config.pages?.overview?.hideChecklist !== true && <Checklist>{checklist}</Checklist>}
