4ae3732036ce2f8cf3e050686a260b8e5a138558
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-06-30 20:20:32 +0200

    another small bit added to the guide

--- analysis/priestdiscipline/src/Guide.tsx
+++ analysis/priestdiscipline/src/Guide.tsx
@@ -1,6 +1,6 @@
 import SPELLS from 'common/SPELLS';
 import { SpellLink } from 'interface';
-import { Section, GuideProps } from 'interface/guide';
+import { Section, GuideProps, SubSection } from 'interface/guide';
 
 import type CombatLogParser from './CombatLogParser';
 
@@ -9,16 +9,20 @@ export default function Guide({
   events,
   info,
 }: GuideProps<typeof CombatLogParser>): JSX.Element {
-  const result = 'Test';
   return (
     <>
       <Section title="Main Ramp">
-        {result}
         When using effects which extend <SpellLink id={SPELLS.ATONEMENT_BUFF.id} /> such as{' '}
         <SpellLink id={SPELLS.EVANGELISM_TALENT.id} /> or{' '}
         <SpellLink id={SPELLS.SPIRIT_SHELL_TALENT.id} />, it is critical to your success as a
-        Discipline priest to correctly apply atonemnets, extend them and follow the appropriate DPS
+        Discipline priest to correctly apply atonements, extend them and follow the appropriate DPS
         rotation.
+        <SubSection title="Applicators">
+          The first step of an <SpellLink id={SPELLS.EVANGELISM_TALENT.id} /> ramp is to apply{' '}
+          <strong> ARBITRARY ATONEMENT NUMBER </strong> of atonements using{' '}
+          <SpellLink id={SPELLS.POWER_WORD_SHIELD.id} /> or <SpellLink id={SPELLS.SHADOW_MEND.id} />
+          .
+        </SubSection>
       </Section>
     </>
   );
