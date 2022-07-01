9c58981b9509de62bcd56eb4c03eab1d479f3890
J David Smith <emallson@emallson.net>
2022-07-01 13:39:46 -0400

    "simplify" buff count graph

--- src/parser/shared/modules/BuffCountGraph.tsx
+++ src/parser/shared/modules/BuffCountGraph.tsx
@@ -12,6 +12,7 @@ import BaseChart, { formatTime } from 'parser/ui/BaseChart';
 import { VisualizationSpec } from 'react-vega';
 import { AutoSizer } from 'react-virtualized';
 import { Field, PositionDef, StringFieldDef } from 'vega-lite/build/src/channeldef';
+import { UnitSpec } from 'vega-lite/build/src/spec';
 
 /**
  * A customizable graph that displays the number of a chosen buff that are active at any given time.
@@ -25,8 +26,6 @@ abstract class BuffCountGraph extends Analyzer {
   buffTrackerLookup: { [key: number]: GraphedSpellInternalTracker[] };
   /** Cast trackers indexed by spellId */
   castTrackerLookup: { [key: number]: GraphedSpellInternalTracker[] };
-  /** History of buff counts used to draw graph line */
-  buffCountHistory: BuffDataObj[];
 
   /**
    * Creates a BuffCountGraph showing the specified buffs and casts.
@@ -49,7 +48,6 @@ abstract class BuffCountGraph extends Analyzer {
     this.castTrackers = castSpecs.map((spec) => this._convertToInternalTracker(spec));
     this.buffTrackerLookup = this._generateLookup(this.buffTrackers);
     this.castTrackerLookup = this._generateLookup(this.castTrackers);
-    this.buffCountHistory = [];
 
     // setup listeners - spell list needs to filtered through a Set to remove duplicates
     const buffSpellList = [...new Set(this.buffTrackers.flatMap((spec) => spec.spells))];
@@ -124,27 +122,14 @@ abstract class BuffCountGraph extends Analyzer {
       return;
     }
 
-    // in order to generate a stepped graph, we need to add a data point showing
-    // the before counts and the after counts on timestamp when change occurs
-
-    // add 'before' data
-    const beforeData: BuffDataObj = { timestamp: event.timestamp };
-    this.buffTrackers.forEach((tracker) => {
-      beforeData[tracker.fieldId] = tracker.currCount;
-    });
-    this.buffCountHistory.push(beforeData);
-
     // register the change
     applicableTrackers.forEach((tracker) => {
       tracker.currCount += change;
+      tracker.changes.push({
+        timestamp: event.timestamp,
+        count: tracker.currCount,
+      });
     });
-
-    // add 'after' data
-    const afterData: BuffDataObj = { timestamp: event.timestamp };
-    this.buffTrackers.forEach((tracker) => {
-      afterData[tracker.fieldId] = tracker.currCount;
-    });
-    this.buffCountHistory.push(afterData);
   }
 
   onCast(event: CastEvent) {
@@ -166,29 +151,63 @@ abstract class BuffCountGraph extends Analyzer {
 
   /** Generates a tooltip mapping (used by vega) */
   generateTooltips(): Array<StringFieldDef<Field>> {
-    return this.buffTrackers.map((tracker) => ({
-      field: tracker.fieldId,
-      type: 'quantitative' as const,
-      title: tracker.tooltipName,
-      format: '.3~s',
-    }));
+    // if we want to go back to "Spell: count" as the tooltip format, it needs to be an extra layer afaik
+    return [
+      {
+        field: 'tooltipName',
+        type: 'nominal',
+        title: 'Spell',
+      },
+      {
+        field: 'count',
+        type: 'quantitative',
+        title: 'Count',
+        format: '.3~s',
+      },
+    ];
   }
 
   /** Generates filled in mouseover areas (used by vega) */
-  generateAreas() {
-    return this.buffTrackers.map((tracker) => ({
+  generateAreas(): UnitSpec<Field> {
+    return {
+      data: {
+        values: this.buffTrackers,
+      },
       mark: {
         type: 'area' as const,
         line: {
-          interpolate: 'linear' as const,
-          color: 'rgb(' + tracker.color + ')',
           strokeWidth: 1,
         },
-        color: 'rgba(' + tracker.color + ', 0.15)',
+        interpolate: 'step',
       },
+      transform: [
+        {
+          flatten: ['changes'],
+          as: ['change'],
+        },
+        {
+          calculate: `datum.change.timestamp - ${this.owner.fight.start_time}`,
+          as: 'timestamp_shifted',
+        },
+        // this window op works around the fact that way may get multiple buff
+        // changes in a single timestamp
+        {
+          window: [
+            {
+              op: 'max',
+              field: 'change.count',
+              as: 'count',
+            },
+          ],
+          // this frame says "only include entries with the same exact timestamp"
+          frame: [0, 0],
+          groupby: ['tooltipName'],
+          sort: [{ field: 'timestamp_shifted', order: 'ascending' }],
+        },
+      ],
       encoding: {
         y: {
-          field: tracker.fieldId,
+          field: 'count',
           type: 'quantitative' as const,
           title: null,
           axis: {
@@ -196,8 +215,18 @@ abstract class BuffCountGraph extends Analyzer {
             format: '~s',
           },
         },
+        color: {
+          field: 'tooltipName',
+          type: 'nominal',
+          title: 'Spell',
+          scale: {
+            domain: this.buffTrackers.map(({ tooltipName }) => tooltipName),
+            range: this.buffTrackers.map(({ color }) => `rgb(${color})`),
+          },
+        },
+        tooltip: this.generateTooltips(),
       },
-    }));
+    };
   }
 
   /** Generates a cast spec mapping */
@@ -278,9 +307,8 @@ abstract class BuffCountGraph extends Analyzer {
       ],
       encoding: {
         x: xAxis,
-        tooltip: this.generateTooltips(),
       },
-      layer: [...this.generateAreas(), ...this.generateCasts(xAxis)],
+      layer: [this.generateAreas(), ...this.generateCasts(xAxis)],
     };
 
     return (
@@ -296,7 +324,6 @@ abstract class BuffCountGraph extends Analyzer {
             <BaseChart
               spec={spec}
               data={{
-                buffHistory: this.buffCountHistory,
                 ...this._generateCastData(),
               }}
               width={width}
@@ -331,12 +358,6 @@ type GraphedSpellInternalTracker = {
   changes: SpellChange[];
   /** The current number of buffs out (for buffs, ignored for casts) */
   currCount: number;
-};
-
-/** Data format expected by vega for buff data points */
-type BuffDataObj = {
-  timestamp: number;
-  [key: string]: number;
 };
 
 /**
