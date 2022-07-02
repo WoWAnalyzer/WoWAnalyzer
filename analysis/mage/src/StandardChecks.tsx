f7eeda8bcfa64a5eb35f0b1a7f27058ccf8f4d50
Sharrq <cjgrasty86@gmail.com>
2022-07-01 22:02:47 -0400

    progress

--- analysis/mage/src/StandardChecks.tsx
+++ analysis/mage/src/StandardChecks.tsx
@@ -1,14 +1,23 @@
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import CASTS_THAT_ARENT_CASTS from 'parser/core/CASTS_THAT_ARENT_CASTS';
 import { SpellInfo } from 'parser/core/EventFilter';
-import { AnyEvent } from 'parser/core/Events';
+import {
+  EventType,
+  AnyEvent,
+  CastEvent,
+  BeginChannelEvent,
+  BeginCastEvent,
+} from 'parser/core/Events';
 import Events from 'parser/core/Events';
+import AbilityTracker from 'parser/shared/modules/AbilityTracker';
 import EventHistory from 'parser/shared/modules/EventHistory';
 
 class StandardChecks extends Analyzer {
   static dependencies = {
+    abilityTracker: AbilityTracker,
     eventHistory: EventHistory,
   };
+  protected abilityTracker!: AbilityTracker;
   protected eventHistory!: EventHistory;
 
   castBreakdownDuringBuff(buff: SpellInfo) {
@@ -16,50 +25,39 @@ class StandardChecks extends Analyzer {
      * @param buff the spell object for the buff
      * @returns an array containing each unique spell cast and the number of times it was cast
      */
-    const buffRemovals: AnyEvent[] = this.eventHistory.last(
-      undefined,
-      undefined,
-      Events.removebuff.by(SELECTED_PLAYER).spell(buff),
-    );
-    if (this.selectedCombatant.hasBuff(buff.id, this.owner.fight.end_time - 1)) {
-      buffRemovals.push(this.eventHistory.last(1, undefined, Events.fightend)[0]);
-    }
+    const castEvents: CastEvent[] = this.getEventsDuringBuff(buff, EventType.Cast);
     const castArray: number[][] = [];
-    buffRemovals &&
-      buffRemovals.forEach((e) => {
-        const buffApply = this.eventHistory.last(
-          1,
-          undefined,
-          Events.applybuff.by(SELECTED_PLAYER).spell(buff),
-          e.timestamp,
-        )[0];
-        const buffDuration = e.timestamp - buffApply.timestamp - 1;
-        const castEvents = this.eventHistory.last(
-          undefined,
-          buffDuration,
-          Events.cast.by(SELECTED_PLAYER),
-          e.timestamp,
-        );
-        castEvents &&
-          castEvents.forEach((c) => {
-            if (CASTS_THAT_ARENT_CASTS.includes(c.ability.guid)) {
-              return;
-            }
-            const index = castArray.findIndex((arr) => arr.includes(c.ability.guid));
-            if (index !== -1) {
-              castArray[index][1] += 1;
-            } else {
-              castArray.push([c.ability.guid, 1]);
-            }
-          });
+    castEvents &&
+      castEvents.forEach((c) => {
+        const index = castArray.findIndex((arr) => arr.includes(c.ability.guid));
+        if (index !== -1) {
+          castArray[index][1] += 1;
+        } else {
+          castArray.push([c.ability.guid, 1]);
+        }
       });
     return castArray;
   }
 
-  countCastsDuringBuff(buff: SpellInfo, cast?: SpellInfo | undefined) {
+  countTotalCasts(spell: SpellInfo) {
+    return this.abilityTracker.getAbility(spell.id).casts;
+  }
+
+  countEventsDuringBuff(buff: SpellInfo, eventType: string, cast?: SpellInfo | undefined) {
     /**
      * @param buff the spell object for the buff
+     * @param eventType the type of event that you want to search for. i.e. "cast", "begincast", EventType.Cast, EventType.BeginCast, etc.
      * @param cast an optional cast spell object to count. Omit or leave undefined to count all casts
+     */
+    const events = this.getEventsDuringBuff(buff, eventType, cast);
+    return events.length;
+  }
+
+  getEventsDuringBuff(buff: SpellInfo, eventType: string, spell?: SpellInfo | undefined) {
+    /**
+     * @param buff the spell object for the buff
+     * @param eventType the type of event that you want to search for. i.e. "cast", "begincast", EventType.Cast, EventType.BeginCast, etc.
+     * @param spell an optional spell object to search. Omit or leave undefined to count all events
      */
     const buffRemovals: AnyEvent[] = this.eventHistory.last(
       undefined,
@@ -69,7 +67,8 @@ class StandardChecks extends Analyzer {
     if (this.selectedCombatant.hasBuff(buff.id, this.owner.fight.end_time - 1)) {
       buffRemovals.push(this.eventHistory.last(1, undefined, Events.fightend)[0]);
     }
-    let totalCasts = 0;
+    let events: any;
+    const filteredEvents: any = [];
     buffRemovals &&
       buffRemovals.forEach((e) => {
         const buffApply = this.eventHistory.last(
@@ -79,22 +78,83 @@ class StandardChecks extends Analyzer {
           e.timestamp,
         )[0];
         const buffDuration = e.timestamp - buffApply.timestamp - 1;
-        const castEvents = this.eventHistory.last(
-          undefined,
-          buffDuration,
-          cast ? Events.cast.by(SELECTED_PLAYER).spell(cast) : Events.cast.by(SELECTED_PLAYER),
-          e.timestamp,
-        );
-        castEvents &&
-          castEvents.forEach((c) => {
-            if (CASTS_THAT_ARENT_CASTS.includes(c.ability.guid)) {
-              return;
-            } else {
-              totalCasts += 1;
-            }
+        switch (eventType) {
+          case EventType.Cast:
+            events = this.getCastEvents(undefined, e.timestamp, buffDuration, spell);
+            break;
+          case EventType.BeginCast:
+            events = this.getBeginCastEvents(undefined, e.timestamp, buffDuration, spell);
+            break;
+        }
+        events &&
+          events.forEach((event: AnyEvent) => {
+            filteredEvents.push(event);
           });
       });
-    return totalCasts;
+    return filteredEvents;
+  }
+
+  getCastEvents(
+    count?: number | undefined,
+    startTimestamp?: number | undefined,
+    duration?: number | undefined,
+    spell?: SpellInfo | undefined,
+  ) {
+    const events = this.eventHistory.last(
+      count,
+      duration,
+      spell ? Events.cast.by(SELECTED_PLAYER).spell(spell) : Events.cast.by(SELECTED_PLAYER),
+      startTimestamp || this.owner.fight.end_time,
+    );
+    const filteredCasts: CastEvent[] = [];
+    events &&
+      events.forEach((cast) => {
+        if (CASTS_THAT_ARENT_CASTS.includes(cast.ability.guid)) {
+          return;
+        } else {
+          filteredCasts.push(cast);
+        }
+      });
+    return filteredCasts;
+  }
+
+  getBeginCastEvents(
+    count?: number | undefined,
+    startTimestamp?: number | undefined,
+    duration?: number | undefined,
+    spell?: SpellInfo | undefined,
+  ) {
+    const events = this.eventHistory.last(
+      count,
+      duration,
+      spell
+        ? Events.begincast.by(SELECTED_PLAYER).spell(spell)
+        : Events.begincast.by(SELECTED_PLAYER),
+      startTimestamp || this.owner.fight.end_time,
+    );
+    const filteredCasts: BeginCastEvent[] = [];
+    events &&
+      events.forEach((cast) => {
+        filteredCasts.push(cast);
+      });
+    return filteredCasts;
+  }
+
+  highlightTimeline(
+    event: CastEvent | BeginChannelEvent | CastEvent[] | BeginChannelEvent[],
+    tooltip: string,
+  ) {
+    if (Array.isArray(event)) {
+      event.forEach((e) => {
+        e.meta = e.meta || {};
+        e.meta.isInefficientCast = true;
+        e.meta.inefficientCastReason = tooltip;
+      });
+    } else {
+      event.meta = event.meta || {};
+      event.meta.isInefficientCast = true;
+      event.meta.inefficientCastReason = tooltip;
+    }
   }
 }
 
--- analysis/magefire/src/modules/features/CombustionSpellUsage.tsx
+++ analysis/magefire/src/modules/features/CombustionSpellUsage.tsx
@@ -2,74 +2,41 @@ import { Trans } from '@lingui/macro';
 import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import { SpellLink } from 'interface';
-import Analyzer, { SELECTED_PLAYER, Options } from 'parser/core/Analyzer';
-import Events, { BeginCastEvent, CastEvent, EventType } from 'parser/core/Events';
+import Analyzer from 'parser/core/Analyzer';
+import { EventType } from 'parser/core/Events';
 import { When, ThresholdStyle } from 'parser/core/ParseResults';
-import AbilityTracker from 'parser/shared/modules/AbilityTracker';
-import EventHistory from 'parser/shared/modules/EventHistory';
-import SpellUsable from 'parser/shared/modules/SpellUsable';
+
+import StandardChecks from '@wowanalyzer/mage/src/StandardChecks';
 
 class CombustionSpellUsage extends Analyzer {
   static dependencies = {
-    spellUsable: SpellUsable,
-    abilityTracker: AbilityTracker,
-    eventHistory: EventHistory,
+    standardChecks: StandardChecks,
   };
-  protected spellUsable!: SpellUsable;
-  protected abilityTracker!: AbilityTracker;
-  protected eventHistory!: EventHistory;
+  protected standardChecks!: StandardChecks;
 
-  scorchCastsStarted = 0;
-  scorchCastsCompleted = 0;
-  fireballCastsStarted = 0;
-  fireballCastsCompleted = 0;
-
-  constructor(options: Options) {
-    super(options);
-    this.addEventListener(
-      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.FIREBALL),
-      this.fireballCasts,
+  get fireballCastsDuringCombustion() {
+    const fireballCasts = this.standardChecks.getEventsDuringBuff(
+      SPELLS.COMBUSTION,
+      EventType.Cast,
+      SPELLS.FIREBALL,
     );
-    this.addEventListener(
-      Events.begincast.by(SELECTED_PLAYER).spell(SPELLS.FIREBALL),
-      this.fireballCasts,
-    );
+    const tooltip = `This Fireball was cast during Combustion. Since Combustion has a short duration, you are better off using your instant abilities to get as many instant/free Pyroblasts as possible. If you run out of instant abilities, cast Scorch instead since it has a shorter cast time.`;
+    fireballCasts && this.standardChecks.highlightTimeline(fireballCasts, tooltip);
+    return fireballCasts.length;
   }
 
-  //Because Fireball has a longer cast time than Scorch, the player should never cast Fireball during Combustion.
-  fireballCasts(event: CastEvent | BeginCastEvent) {
-    const hasCombustion = this.selectedCombatant.hasBuff(SPELLS.COMBUSTION.id);
-
-    if (!hasCombustion) {
-      return;
-    }
-
-    const combustionApplied = this.eventHistory.last(
-      1,
-      undefined,
-      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.COMBUSTION),
-    )[0].timestamp;
-    if (
-      event.type === EventType.Cast &&
-      (!event.channel?.beginChannel || event.channel.beginChannel.timestamp >= combustionApplied)
-    ) {
-      this.fireballCastsCompleted += 1;
-      event.meta = event.meta || {};
-      event.meta.isInefficientCast = true;
-      event.meta.inefficientCastReason = `This Fireball was cast during Combustion. Since Combustion has a short duration, you are better off using your instant abilities to get as many instant/free Pyroblasts as possible. If you run out of instant abilities, cast Scorch instead since it has a shorter cast time.`;
-    }
-
-    if (event.type === EventType.BeginCast) {
-      this.fireballCastsStarted += 1;
-    }
+  get fireballBeginCasts() {
+    return this.standardChecks.countEventsDuringBuff(
+      SPELLS.COMBUSTION,
+      EventType.BeginCast,
+      SPELLS.FIREBALL,
+    );
   }
 
   get fireballCastsPerCombustion() {
-    return this.fireballCastsStarted / this.combustionCasts;
-  }
-
-  get combustionCasts() {
-    return this.abilityTracker.getAbility(SPELLS.COMBUSTION.id).casts;
+    return (
+      this.fireballCastsDuringCombustion / this.standardChecks.countTotalCasts(SPELLS.COMBUSTION)
+    );
   }
 
   get fireballDuringCombustionThresholds() {
@@ -88,10 +55,10 @@ class CombustionSpellUsage extends Analyzer {
     when(this.fireballDuringCombustionThresholds).addSuggestion((suggest, actual, recommended) =>
       suggest(
         <>
-          You started to cast <SpellLink id={SPELLS.FIREBALL.id} /> {this.fireballCastsStarted}{' '}
-          times ({this.fireballCastsPerCombustion.toFixed(2)} per Combustion), and completed{' '}
-          {this.fireballCastsCompleted} casts, during <SpellLink id={SPELLS.COMBUSTION.id} />.
-          Combustion has a short duration, so you are better off using instant abilities like{' '}
+          You started to cast <SpellLink id={SPELLS.FIREBALL.id} /> {this.fireballBeginCasts} times
+          ({this.fireballCastsPerCombustion.toFixed(2)} per Combustion), and completed{' '}
+          {this.fireballCastsDuringCombustion} casts, during <SpellLink id={SPELLS.COMBUSTION.id} />
+          . Combustion has a short duration, so you are better off using instant abilities like{' '}
           <SpellLink id={SPELLS.FIRE_BLAST.id} /> or <SpellLink id={SPELLS.PHOENIX_FLAMES.id} />. If
           you run out of instant cast abilities, use <SpellLink id={SPELLS.SCORCH.id} /> instead of
           Fireball since it has a shorter cast time.
