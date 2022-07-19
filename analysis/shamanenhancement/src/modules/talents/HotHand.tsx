1f9107162dba7f7e8518e996b743dcfb10762e8f
Danny Janse <d.janse@live.com>
2022-07-19 18:58:40 +0200

    Reduce the CD of Lava Lash when the Hot Hand buff is active.

--- analysis/shamanenhancement/src/modules/talents/HotHand.tsx
+++ analysis/shamanenhancement/src/modules/talents/HotHand.tsx
@@ -2,6 +2,8 @@ import SPELLS from 'common/SPELLS';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import { calculateEffectiveDamage } from 'parser/core/EventCalculateLib';
 import Events, { DamageEvent } from 'parser/core/Events';
+import Haste from 'parser/shared/modules/Haste';
+import SpellUsable from 'parser/shared/modules/SpellUsable';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import Statistic from 'parser/ui/Statistic';
@@ -21,6 +23,13 @@ const HOT_HAND = {
  *
  */
 class HotHand extends Analyzer {
+  static dependencies = {
+    spellUsable: SpellUsable,
+    haste: Haste,
+  };
+  protected spellUsable!: SpellUsable;
+  protected haste!: Haste;
+
   protected buffedLavaLashDamage: number = 0;
 
   constructor(options: Options) {
@@ -29,9 +38,46 @@ class HotHand extends Analyzer {
     this.active = this.selectedCombatant.hasTalent(SPELLS.HOT_HAND_TALENT.id);
 
     this.addEventListener(
+      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.HOT_HAND_BUFF),
+      this.reduceLavaLushCDOnBuff,
+    );
+
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.LAVA_LASH),
+      this.reduceLavaLushCDOnCast,
+    );
+
+    this.addEventListener(
+      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.HOT_HAND_BUFF),
+      this.restoreLavaLushCDRemoveBuff,
+    );
+
+    this.addEventListener(
       Events.damage.by(SELECTED_PLAYER).spell(SPELLS.LAVA_LASH),
       this.onLavaLashDamage,
     );
+  }
+
+  calcLavaLushCd75pct() {
+    return (9 / (1 + this.haste.current)) * 1000;
+  }
+
+  reduceLavaLushCDOnBuff() {
+    if (this.spellUsable.isOnCooldown(SPELLS.LAVA_LASH.id)) {
+      this.spellUsable.reduceCooldown(SPELLS.LAVA_LASH.id, this.calcLavaLushCd75pct());
+    }
+  }
+
+  reduceLavaLushCDOnCast() {
+    if (this.selectedCombatant.hasBuff(SPELLS.HOT_HAND_BUFF.id)) {
+      this.reduceLavaLushCDOnBuff();
+    }
+  }
+
+  restoreLavaLushCDRemoveBuff() {
+    if (this.spellUsable.isOnCooldown(SPELLS.LAVA_LASH.id)) {
+      this.spellUsable.cooldownRemaining(SPELLS.LAVA_LASH.id);
+    }
   }
 
   onLavaLashDamage(event: DamageEvent) {
