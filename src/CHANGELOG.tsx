fbb8eb2f1c4645afef4e890f090b173235c74ec8
Danny Janse <d.janse@live.com>
2022-07-21 01:39:53 +0200

    restore unintentionally removed changelog..

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -61,6 +61,7 @@ import SpellLink from 'interface/SpellLink';
 // prettier-ignore
 export default [
   change(date(2022, 7, 21), 'Updated enchants on character overview for SL and TBC.', Vetyst),
+  change(date(2022, 7, 19), 'CN_translations: About page, help wanted page and some warlock specs translated.', Lucky0604),
   change(date(2022, 7, 10), 'Bump patch compatibility for all 9.2 specs to 9.2.5.', emallson),
   change(date(2022, 7, 9), 'Fix issue where time filtering could cause the report to hang.', emallson),
   change(date(2022, 6, 29), 'CN translations: add some parts of the report about tbc warlock trans', Lucky0604),
