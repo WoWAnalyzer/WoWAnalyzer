09e83ff94b8ffa80e48032c783e0734bafcfc4fc
Sharrq <cjgrasty86@gmail.com>
2022-07-25 18:11:42 -0400

    Hot Streak Pre Casts

--- src/analysis/retail/mage/fire/modules/features/HotStreak.tsx
+++ src/analysis/retail/mage/fire/modules/features/HotStreak.tsx
@@ -89,7 +89,7 @@ class HotStreak extends Analyzer {
               <li>Used procs - {this.totalHotStreakProcs - this.expiredProcs()}</li>
               <li>Expired procs - {this.expiredProcs()}</li>
               <li>
-                Procs used without a Fireball - {this.hotStreakPreCasts.noCastBeforeHotStreak}
+                Procs used without a Fireball - {this.hotStreakPreCasts.missingHotStreakPreCast()}
               </li>
             </ul>
           </>
