07a6b03a73db62f4168abc49d4ff597ce073519b
Kelton Finch <keltonfinch@gmail.com>
2022-08-07 16:09:54 -0400

    Updated handling of Swiftmend text conditionals to be more readable. Fixed usage of paragraph tags.

--- analysis/druidrestoration/src/Guide.tsx
+++ analysis/druidrestoration/src/Guide.tsx
@@ -60,16 +60,17 @@ function RejuvenationSubsection({ modules, events, info }: GuideProps<typeof Com
   };
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.REJUVENATION.id} />
-      </b>{' '}
-      is your primary filler spell and will almost always be your most cast spell. It can be used on
-      injured raiders or even pre-cast on full health raiders if you know big damage is coming soon.
-      Do not spam it unmotivated - you'll run yourself out of mana. You also shouldn't cast it on
-      targets that already have a high duration Rejuvenation, as you will clip duration. Note that
-      some high-overheal Rejuvs are unavoidable due to heal sniping, but if a large proportion of
-      them are, you might be casting too much.
-      <p />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.REJUVENATION.id} />
+        </b>{' '}
+        is your primary filler spell and will almost always be your most cast spell. It can be used
+        on injured raiders or even pre-cast on full health raiders if you know big damage is coming
+        soon. Do not spam it unmotivated - you'll run yourself out of mana. You also shouldn't cast
+        it on targets that already have a high duration Rejuvenation, as you will clip duration.
+        Note that some high-overheal Rejuvs are unavoidable due to heal sniping, but if a large
+        proportion of them are, you might be casting too much.
+      </p>
       <strong>Rejuvenation cast breakdown</strong>
       <small>
         {' '}
@@ -85,14 +86,15 @@ function RejuvenationSubsection({ modules, events, info }: GuideProps<typeof Com
 function WildGrowthSubsection({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.WILD_GROWTH.id} />
-      </b>{' '}
-      is your best healing spell when multiple raiders are injured. It quickly heals a lot of
-      damage, but has a high mana cost. Use Wild Growth over Rejuvenation if there are at least 3
-      injured targets. Remember that only allies within 30 yds of the primary target can be hit -
-      don't cast this on an isolated player!
-      <p />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.WILD_GROWTH.id} />
+        </b>{' '}
+        is your best healing spell when multiple raiders are injured. It quickly heals a lot of
+        damage, but has a high mana cost. Use Wild Growth over Rejuvenation if there are at least 3
+        injured targets. Remember that only allies within 30 yds of the primary target can be hit -
+        don't cast this on an isolated player!
+      </p>
       <strong>Wild Growth casts</strong>
       <small>
         {' '}
@@ -109,23 +111,24 @@ function RegrowthSubsection({ modules, events, info }: GuideProps<typeof CombatL
   const hasAbundance = info.combatant.hasTalent(SPELLS.ABUNDANCE_TALENT);
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.REGROWTH.id} />
-      </b>{' '}
-      is for urgent spot healing. The HoT it applies is very weak, meaning Regrowth is only
-      efficient when its direct portion is effective. Exceptions are when Regrowth is free due to{' '}
-      <SpellLink id={SPELLS.CLEARCASTING_BUFF.id} /> /{' '}
-      <SpellLink id={SPELLS.NATURES_SWIFTNESS.id} />{' '}
-      {hasAbundance && (
-        <>
-          or cheap due to <SpellLink id={SPELLS.ABUNDANCE_TALENT.id} />.<p />
-          Even with <SpellLink id={SPELLS.ABUNDANCE_TALENT.id} /> you still shouldn't cast Regrowth
-          during your ramp. Wait until after you <SpellLink id={SPELLS.CONVOKE_SPIRITS.id} /> or{' '}
-          <SpellLink id={SPELLS.FLOURISH_TALENT.id} />, then you can fill with high-stack Regrowth
-          casts.
-        </>
-      )}
-      <p />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.REGROWTH.id} />
+        </b>{' '}
+        is for urgent spot healing. The HoT it applies is very weak, meaning Regrowth is only
+        efficient when its direct portion is effective. Exceptions are when Regrowth is free due to{' '}
+        <SpellLink id={SPELLS.CLEARCASTING_BUFF.id} /> /{' '}
+        <SpellLink id={SPELLS.NATURES_SWIFTNESS.id} />{' '}
+        {hasAbundance && (
+          <>
+            or cheap due to <SpellLink id={SPELLS.ABUNDANCE_TALENT.id} />. Even with{' '}
+            <SpellLink id={SPELLS.ABUNDANCE_TALENT.id} /> you still shouldn't cast Regrowth during
+            your ramp. Wait until after you <SpellLink id={SPELLS.CONVOKE_SPIRITS.id} /> or{' '}
+            <SpellLink id={SPELLS.FLOURISH_TALENT.id} />, then you can fill with high-stack Regrowth
+            casts.
+          </>
+        )}
+      </p>
       <strong>Regrowth casts</strong>
       <small>
         {' '}
@@ -140,16 +143,17 @@ function RegrowthSubsection({ modules, events, info }: GuideProps<typeof CombatL
 function LifebloomSubsection({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.LIFEBLOOM_HOT_HEAL.id} />
-      </b>{' '}
-      can only be active on one target at time and provides similar throughput to Rejuvenation.
-      However, it causes <SpellLink id={SPELLS.CLEARCASTING_BUFF.id} /> procs and so is a big
-      benefit to your mana efficiency . It should always be active on a target - the tank is usually
-      a safe bet.
-      <p />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.LIFEBLOOM_HOT_HEAL.id} />
+        </b>{' '}
+        can only be active on one target at time and provides similar throughput to Rejuvenation.
+        However, it causes <SpellLink id={SPELLS.CLEARCASTING_BUFF.id} /> procs and so is a big
+        benefit to your mana efficiency . It should always be active on a target - the tank is
+        usually a safe bet.
+      </p>
       {info.combatant.hasTalent(SPELLS.PHOTOSYNTHESIS_TALENT) && (
-        <>
+        <p>
           Because you took{' '}
           <strong>
             <SpellLink id={SPELLS.PHOTOSYNTHESIS_TALENT.id} />
@@ -168,8 +172,7 @@ function LifebloomSubsection({ modules, events, info }: GuideProps<typeof Combat
             {formatPercentage(modules.photosynthesis.othersLifebloomUptime / info.fightDuration, 1)}
             %
           </strong>
-          <p />
-        </>
+        </p>
       )}
       {modules.lifebloom.subStatistic()}
     </SubSection>
@@ -179,13 +182,14 @@ function LifebloomSubsection({ modules, events, info }: GuideProps<typeof Combat
 function EfflorescenceSubsection({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.EFFLORESCENCE_CAST.id} />
-      </b>{' '}
-      is extremely mana efficient if you're good about placing it where raiders are standing. Under
-      the boss is usually a safe bet. While it's acceptable to let it drop during heavy movement,
-      you should otherwise aim to keep it active at all times.
-      <p />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.EFFLORESCENCE_CAST.id} />
+        </b>{' '}
+        is extremely mana efficient if you're good about placing it where raiders are standing.
+        Under the boss is usually a safe bet. While it's acceptable to let it drop during heavy
+        movement, you should otherwise aim to keep it active at all times.
+      </p>
       {modules.efflorescence.subStatistic()}
     </SubSection>
   );
@@ -196,69 +200,87 @@ function SwiftmendSubsection({ modules, events, info }: GuideProps<typeof Combat
   const hasVi = info.combatant.hasLegendary(SPELLS.VERDANT_INFUSION);
   const has4p = info.combatant.has4Piece();
   const procCount = (hasSotf ? 1 : 0) + (hasVi ? 1 : 0) + (has4p ? 1 : 0);
-  const chartDescription = ` - ${
-    hasVi ? 'Blue is great (extended high value HoTs), ' : ''
-  }Green is a fine cast, ${procCount > 0 ? 'Yellow' : 'Red'} is a non-triage (>50% health) cast${
-    hasVi ? '' : ' that removes a WG or Rejuv'
-  }${procCount ? ' (still acceptable for generating procs)' : ''}.  Mouseover for more details.`;
+
+  let chartDescription = ' - ';
+  if (hasVi) {
+    // has proc(s) including VI
+    chartDescription +=
+      'Blue is great (extended high value HoTs), Green is a fine cast, Yellow is a non-triage (>50% health) cast (still acceptable for generating procs).';
+  } else if (procCount > 0) {
+    // has proc(s), but VI isn't one of them
+    chartDescription +=
+      'Green is a fine cast, Yellow is a non-triage (>50% health) cast that removes a WG or Rejuv (still acceptable for generating procs).';
+  } else {
+    // no procs
+    chartDescription +=
+      'Green is a fine cast, Red is a non-triage (>50% health) cast that removes a WG or Rejuv.';
+  }
+  chartDescription += ' Mouseover for more details.';
+
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.SWIFTMEND.id} />
-      </b>{' '}
-      is our emergency heal but it removes a HoT on its target, hurting overall throughput. Normally
-      it should only be used on targets who need urgent healing.
-      <br />
-      {procCount === 1 && `However, you have a proc that is generated by casting Swiftmend: `}
-      {procCount > 1 && `However, you have procs that are generated by casting Swiftmend: `}
-      {hasSotf && (
-        <>
-          <SpellLink id={SPELLS.SOUL_OF_THE_FOREST_TALENT_RESTORATION.id} />
-          &nbsp;
-        </>
-      )}
-      {hasVi && (
-        <>
-          <SpellLink id={SPELLS.VERDANT_INFUSION.id} />
-          &nbsp;
-        </>
-      )}
-      {has4p && (
-        <>
-          <SpellLink id={SPELLS.RESTO_DRUID_TIER_28_4P_SET_BONUS.id} /> (the Tier 4pc)&nbsp;
-        </>
-      )}
-      {procCount > 0 && (
-        <>
-          {procCount === 1 ? `This ability is ` : `These abilities are `}very powerful, so you
-          should cast Swiftmend frequently in order to generate procs - even on targets who don't
-          need urgent healing.
-        </>
-      )}
-      <p />
-      <strong>Swiftmend casts</strong>
-      <small>{chartDescription}</small>
-      {modules.swiftmend.guideTimeline}
+      <p>
+        <b>
+          <SpellLink id={SPELLS.SWIFTMEND.id} />
+        </b>{' '}
+        is our emergency heal but it removes a HoT on its target, hurting overall throughput.
+        Normally it should only be used on targets who need urgent healing.
+        <br />
+        {procCount === 1 && `However, you have a proc that is generated by casting Swiftmend: `}
+        {procCount > 1 && `However, you have procs that are generated by casting Swiftmend: `}
+        {hasSotf && (
+          <>
+            <SpellLink id={SPELLS.SOUL_OF_THE_FOREST_TALENT_RESTORATION.id} />
+            &nbsp;
+          </>
+        )}
+        {hasVi && (
+          <>
+            <SpellLink id={SPELLS.VERDANT_INFUSION.id} />
+            &nbsp;
+          </>
+        )}
+        {has4p && (
+          <>
+            <SpellLink id={SPELLS.RESTO_DRUID_TIER_28_4P_SET_BONUS.id} /> (the Tier 4pc)&nbsp;
+          </>
+        )}
+        {procCount > 0 && (
+          <>
+            {procCount === 1 ? `This ability is ` : `These abilities are `}very powerful, so you
+            should cast Swiftmend frequently in order to generate procs - even on targets who don't
+            need urgent healing.
+          </>
+        )}
+      </p>
+      <p>
+        <strong>Swiftmend casts</strong>
+        <small>{chartDescription}</small>
+        {modules.swiftmend.guideTimeline}
+      </p>
       {info.combatant.hasTalent(SPELLS.SOUL_OF_THE_FOREST_TALENT_RESTORATION) && (
         <>
-          <p />
-          <strong>
-            <SpellLink id={SPELLS.SOUL_OF_THE_FOREST_TALENT_RESTORATION.id} />
-          </strong>{' '}
-          procs are generated by casting Swiftmend. Consuming the proc with{' '}
-          <SpellLink id={SPELLS.WILD_GROWTH.id} /> is by far the highest value, but if only a single
-          target needs big healing it's acceptable to use <SpellLink id={SPELLS.REJUVENATION.id} />{' '}
-          or <SpellLink id={SPELLS.REGROWTH.id} />. <SpellLink id={SPELLS.CONVOKE_SPIRITS.id} /> can
-          both generate and consume procs - you should always use your proc before casting Convoke
-          to avoid overwriting or consuming on a bad target. Never let a proc expire.
-          <p />
-          <strong>Soul of the Forest usage</strong>
-          <small>
-            {' '}
-            - Green is a Wild Growth use, Yellow is a Rejuvenation or Regrowth use, and Red is an
-            expired or overwritten proc. Mouseover for more details.
-          </small>
-          {modules.soulOfTheForest.guideTimeline}
+          <p>
+            <strong>
+              <SpellLink id={SPELLS.SOUL_OF_THE_FOREST_TALENT_RESTORATION.id} />
+            </strong>{' '}
+            procs are generated by casting Swiftmend. Consuming the proc with{' '}
+            <SpellLink id={SPELLS.WILD_GROWTH.id} /> is by far the highest value, but if only a
+            single target needs big healing it's acceptable to use{' '}
+            <SpellLink id={SPELLS.REJUVENATION.id} /> or <SpellLink id={SPELLS.REGROWTH.id} />.{' '}
+            <SpellLink id={SPELLS.CONVOKE_SPIRITS.id} /> can both generate and consume procs - you
+            should always use your proc before casting Convoke to avoid overwriting or consuming on
+            a bad target. Never let a proc expire.
+          </p>
+          <p>
+            <strong>Soul of the Forest usage</strong>
+            <small>
+              {' '}
+              - Green is a Wild Growth use, Yellow is a Rejuvenation or Regrowth use, and Red is an
+              expired or overwritten proc. Mouseover for more details.
+            </small>
+            {modules.soulOfTheForest.guideTimeline}
+          </p>
         </>
       )}
     </SubSection>
@@ -268,13 +290,13 @@ function SwiftmendSubsection({ modules, events, info }: GuideProps<typeof Combat
 function CenarionWardSubsection({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <SubSection title="">
-      <b>
-        <SpellLink id={SPELLS.CENARION_WARD_TALENT.id} />
-      </b>{' '}
-      is a talented HoT on a short cooldown. It is extremely powerful and efficient and should be
-      cast virtually on cooldown. A tank is usually the best target.
-      <br />
-      <br />
+      <p>
+        <b>
+          <SpellLink id={SPELLS.CENARION_WARD_TALENT.id} />
+        </b>{' '}
+        is a talented HoT on a short cooldown. It is extremely powerful and efficient and should be
+        cast virtually on cooldown. A tank is usually the best target.
+      </p>
       <strong>Cenarion Ward usage and cooldown</strong>
       <div className="flex-main chart" style={{ padding: 5 }}>
         <CooldownBar
