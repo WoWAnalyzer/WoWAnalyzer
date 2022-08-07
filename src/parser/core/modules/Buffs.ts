394cd836977cd3278d712d4cbf4dd4af3f7e3e26
Richard Harrah <topplethenunnery@gmail.com>
2022-08-07 01:20:40 -0400

    convert Buff and Buffs to TypeScript
    convert Buff and Buffs to TypeScript to make the
    future PR for supporting Fated Raids easier.
    
    changes to getOptionalModule and getModule are necessary
    to enable getting modules with dependencies in their
    constructor, as TypeScript doesn't allow accessing the
    derived properties from the constructor.
    see https://github.com/Microsoft/TypeScript/issues/6110.

--- src/parser/core/modules/Buffs.ts
+++ src/parser/core/modules/Buffs.ts
@@ -1,10 +1,22 @@
 import SPELLS from 'common/SPELLS';
 import BLOODLUST_BUFFS from 'game/BLOODLUST_BUFFS';
-import Module from 'parser/core/Module';
+import Module, { Options } from 'parser/core/Module';
+import Ability from 'parser/core/modules/Ability';
 import Haste from 'parser/shared/modules/Haste';
 
 import Abilities from './Abilities';
-import Buff from './Buff';
+import Buff, { SpellbookBuff } from './Buff';
+
+function isAbilityWithBuffSpellId(
+  ability: Ability,
+): ability is Ability & { buffSpellId: number | number[] } {
+  return ability.buffSpellId !== null && ability.buffSpellId !== 0;
+}
+
+type BuffsOptions = Options & {
+  abilities: Abilities;
+  haste: Haste;
+};
 
 // TODO: Make a separate but similar Debuffs module
 /**
@@ -13,29 +25,38 @@ import Buff from './Buff';
  */
 class Buffs extends Module {
   static dependencies = {
-    haste: Haste,
     abilities: Abilities,
+    haste: Haste,
   };
   static BUFF_CLASS = Buff;
+
+  protected abilities!: Abilities;
+  protected haste!: Haste;
+
+  activeBuffs: Buff[] = [];
+  constructor(options: BuffsOptions) {
+    super(options);
+    // passing options is necessary due to how module dependencies work
+    // see https://github.com/Microsoft/TypeScript/issues/6110 for more info
+    this.loadBuffs(this.buffs(options));
+  }
 
   /**
    * This will be called *once* during initialization. This isn't nearly as well worked out as the Abilities modules and was in fact extremely rushed. So I have no clue if you should include all buffs here, or just important ones. We'll figure it out later.
    * @returns {object[]}
    */
-  buffs() {
+  buffs(options: BuffsOptions): SpellbookBuff[] {
     // This list will NOT be recomputed during the fight. If a cooldown changes based on something like Haste or a Buff you need to put it in a function.
     // While you can put checks for talents/traits outside of the cooldown prop, you generally should aim to keep everything about a single spell together. In general only move a prop up if you're regularly checking for the same talent/trait in multiple spells.
     // I think anyway, this might all change lul.
     return [
       // Convert the legacy buffSpellId prop
-      ...this.abilities.activeAbilities
-        .filter((ability) => Boolean(ability.buffSpellId))
-        .map((ability) => ({
-          spellId: ability.buffSpellId,
-          triggeredBySpellId:
-            ability.spell !== ability.buffSpellId ? ability.primarySpell : undefined,
-          timelineHighlight: true,
-        })),
+      ...options.abilities.activeAbilities.filter(isAbilityWithBuffSpellId).map((ability) => ({
+        spellId: ability.buffSpellId,
+        triggeredBySpellId:
+          ability.spell !== ability.buffSpellId ? ability.primarySpell : undefined,
+        timelineHighlight: true,
+      })),
       {
         spellId: Object.keys(BLOODLUST_BUFFS).map((item) => Number(item)),
         timelineHighlight: true,
@@ -47,14 +68,9 @@ class Buffs extends Module {
     ];
   }
 
-  activeBuffs = [];
-  constructor(options) {
-    super(options);
-    this.loadBuffs(this.buffs());
-  }
-  loadBuffs(buffs) {
+  loadBuffs(buffs: SpellbookBuff[]) {
     this.activeBuffs = buffs
-      .map((options) => new this.constructor.BUFF_CLASS(options))
+      .map((options) => new Buffs.BUFF_CLASS(options))
       .filter((ability) => ability.enabled);
   }
 
@@ -62,15 +78,15 @@ class Buffs extends Module {
    * Add a buff to the list of active buffs.
    * @param {object} options An object with all the properties and their values that gets passed to the Buff class.
    */
-  add(options) {
-    const buff = new this.constructor.BUFF_CLASS(options);
+  add(options: SpellbookBuff) {
+    const buff = new Buffs.BUFF_CLASS(options);
     this.activeBuffs.push(buff);
   }
 
   /**
    * Returns the first ACTIVE buff with the given spellId (or undefined if there is no such buff)
    */
-  getBuff(spellId) {
+  getBuff(spellId: number) {
     return this.activeBuffs.find((buff) => {
       if (buff.spellId instanceof Array) {
         return buff.spellId.includes(spellId);
