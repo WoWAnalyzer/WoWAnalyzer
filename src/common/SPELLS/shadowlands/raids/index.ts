a9958d9a72219ec7b4c5648542eb95e3e8444e34
Richard Harrah <topplethenunnery@gmail.com>
2022-08-10 21:25:12 -0400

    remove Fated Infusions to reduce complexity of PR

--- src/common/SPELLS/shadowlands/raids/index.ts
+++ src/common/SPELLS/shadowlands/raids/index.ts
@@ -1,8 +1,7 @@
 import safeMerge from 'common/safeMerge';
 
 import CastleNathria from './castlenathria';
-import Fated from './fated';
 import SanctumOfDomination from './sanctumofdomination';
 import SepulcherOfTheFirstOnes from './sepulcherofthefirstones';
 
-export default safeMerge(CastleNathria, SanctumOfDomination, SepulcherOfTheFirstOnes, Fated);
+export default safeMerge(CastleNathria, SanctumOfDomination, SepulcherOfTheFirstOnes);
