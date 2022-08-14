542215adb204d3c50e73e6a70a5a675a6d739151
Lukáš Stuchlík <stuchlik1@seznam.cz>
2022-08-15 01:45:36 +0200

    Add local eslint rule to require spreading parent dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ const CI = Boolean(process.env.CI);
 
 module.exports = {
   extends: ['@martijnhols/eslint-config', 'plugin:react/jsx-runtime'],
+  plugins: ['local'],
   rules: {
+    'local/module-spread-parent-dependencies': 'error',
     'no-use-before-define': 'off',
     'import/order': [
       // This is an annoying code style rule that can be fixed automatically.
--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -54,13 +54,15 @@ import {
   Tialyss,
   Lucky0604,
   ToppleTheNun,
-  Vetyst
+  Vetyst,
+  Chizu
 } from 'CONTRIBUTORS';
 import { ItemLink } from 'interface';
 import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2022, 8, 15), 'Added eslint rule that requires spreading parent module dependencies', Chizu),
   change(date(2022, 8, 14), 'Improve boss phase typing.', ToppleTheNun),
   change(date(2022, 8, 14), 'Add spell and item data for missing Sanctum of Domination items.', ToppleTheNun),
   change(date(2022, 8, 12), 'Update patch information in order to warn that a report is from an earlier patch.', Vetyst),
