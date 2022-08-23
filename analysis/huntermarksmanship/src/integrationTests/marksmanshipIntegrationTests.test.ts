2c2e3a7aa84764d2844df97637bc4a260562724f
Kelton Finch <keltonfinch@gmail.com>
2022-08-23 10:21:53 -0400

    Deleting the correct files this time...

---
+++ analysis/huntermarksmanship/src/integrationTests/marksmanshipIntegrationTests.test.ts
@@ -0,0 +1,14 @@
+import integrationTest from 'parser/core/tests/integrationTest';
+import path from 'path';
+
+import CombatLogParser from '../CombatLogParser';
+
+describe(
+  'Marksmanship Hunter integration test: Single Target',
+  integrationTest(CombatLogParser, path.resolve(__dirname, 'marksmanship-single-target.zip')),
+);
+
+describe(
+  'Marksmanship Hunter integration test: Multi Target',
+  integrationTest(CombatLogParser, path.resolve(__dirname, 'marksmanship-multi-target.zip')),
+);
