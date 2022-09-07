709f0266cb91e4a34b583238c4d8605f5d3d8a9f
J David Smith <emallson@emallson.net>
2022-09-07 12:33:52 -0400

    remove dead file

--- src/common/SPELLS/talents/evoker.ts
+++
@@ -1,9 +0,0 @@
-// Generated file, changes will be overwritten!
-import { SpellList } from '../Spell';
-
-const talents: SpellList = {
-  //Shared
-  //Devastation
-  //Preservation
-};
-export default talents;
