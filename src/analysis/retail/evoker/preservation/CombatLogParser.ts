c9d0cf8edaa776e024bc83d7671e185d2ff1dcc6
J David Smith <emallson@emallson.net>
2022-09-07 11:19:43 -0400

    add abilities file to Evoke CLPs

--- src/analysis/retail/evoker/preservation/CombatLogParser.ts
+++ src/analysis/retail/evoker/preservation/CombatLogParser.ts
@@ -1,7 +1,11 @@
 import MainCombatLogParser from 'parser/core/CombatLogParser';
 
+import Abilities from '../shared/modules/features/Abilities';
+
 class CombatLogParser extends MainCombatLogParser {
-  static specModules = {};
+  static specModules = {
+    abilities: Abilities,
+  };
 }
 
 export default CombatLogParser;
