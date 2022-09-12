075bcea5d656ac66d5b7098265b595fe013f3250
Daniel Vincent <dvincent25@gmail.com>
2022-09-12 18:52:46 -0500

    Update dev and pres abilities

--- src/analysis/retail/evoker/shared/modules/features/Abilities.tsx
+++ src/analysis/retail/evoker/shared/modules/features/Abilities.tsx
@@ -25,69 +25,6 @@ class Abilities extends CoreAbilities {
           base: 1500,
         },
       },
-      {
-        spell: SPELLS.DISINTEGRATE.id,
-        category: SPELL_CATEGORY.ROTATIONAL_AOE,
-        cooldown: 0,
-        gcd: {
-          base: 1500,
-        },
-      },
-      {
-        spell: TALENTS.REVERSION_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL,
-        cooldown: 9,
-        charges: 1,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.REVERSION_PRESERVATION_TALENT.id),
-      },
-      {
-        spell: TALENTS.DREAM_BREATH_PRESERVATION_TALENT.id,
-        enabled: combatant.hasTalent(TALENTS.DREAM_BREATH_PRESERVATION_TALENT.id),
-        category: SPELL_CATEGORY.ROTATIONAL_AOE,
-        cooldown: 25,
-        gcd: {
-          base: 1500,
-        },
-      },
-      {
-        spell: TALENTS.ECHO_PRESERVATION_TALENT.id,
-        enabled: combatant.hasTalent(TALENTS.ECHO_PRESERVATION_TALENT.id),
-        category: SPELL_CATEGORY.ROTATIONAL,
-        cooldown: 0,
-        gcd: {
-          base: 1500,
-        },
-      },
-      {
-        spell: TALENTS.TIME_DILATION_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL,
-        cooldown: 60,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.TIME_DILATION_PRESERVATION_TALENT.id),
-      },
-      {
-        spell: TALENTS.SPIRITBLOOM_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL_AOE,
-        cooldown: 30,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.SPIRITBLOOM_PRESERVATION_TALENT.id),
-      },
-      {
-        spell: TALENTS.TEMPORAL_ANOMALY_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL_AOE,
-        cooldown: 6,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.TEMPORAL_ANOMALY_PRESERVATION_TALENT.id),
-      },
       //endregion
       //region Damage Spells
       {
@@ -100,7 +37,7 @@ class Abilities extends CoreAbilities {
       },
       {
         spell: SPELLS.AZURE_STRIKE.id,
-        category: SPELL_CATEGORY.HEALER_DAMAGING_SPELL,
+        category: SPELL_CATEGORY.ROTATIONAL,
         cooldown: 0,
         gcd: {
           base: 1500,
@@ -115,33 +52,6 @@ class Abilities extends CoreAbilities {
         gcd: {
           base: 1500,
         },
-      },
-      {
-        spell: TALENTS.REWIND_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.COOLDOWNS,
-        cooldown: 240,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.REWIND_PRESERVATION_TALENT.id),
-      },
-      {
-        spell: TALENTS.EMERALD_COMMUNION_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.COOLDOWNS,
-        cooldown: 180,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.EMERALD_COMMUNION_PRESERVATION_TALENT.id),
-      },
-      {
-        spell: TALENTS.DREAM_FLIGHT_PRESERVATION_TALENT.id,
-        category: SPELL_CATEGORY.COOLDOWNS,
-        cooldown: 60,
-        gcd: {
-          base: 1500,
-        },
-        enabled: combatant.hasTalent(TALENTS.DREAM_FLIGHT_PRESERVATION_TALENT.id),
       },
       {
         spell: TALENTS.LANDSLIDE_TALENT.id,
