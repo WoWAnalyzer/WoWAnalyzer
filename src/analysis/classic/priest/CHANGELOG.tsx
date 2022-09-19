74154dcd0c2803f2683f84ac5741eef86e1d0e8c
TJ Moats <tj@moats.io>
2022-09-19 07:25:58 -0500

    changelog

--- src/analysis/classic/priest/CHANGELOG.tsx
+++ src/analysis/classic/priest/CHANGELOG.tsx
@@ -2,5 +2,5 @@ import { change, date } from 'common/changelog';
 import { Khadaj } from 'CONTRIBUTORS';
 
 export default [
-  change(date(2022, 5, 25), 'WOTLK initial priest cleanup', Khadaj),
+  change(date(2022, 9, 19), 'WOTLK initial priest cleanup', Khadaj),
 ];
