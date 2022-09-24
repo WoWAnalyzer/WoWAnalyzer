992fc27f7dcc0d66600ec667a6f0db0b5c77b7a8
Richard Harrah <topplethenunnery@gmail.com>
2022-09-23 22:11:01 -0400

    add jsdoc comment regarding use of isSupportedRegion

--- src/common/regions.ts
+++ src/common/regions.ts
@@ -11,6 +11,11 @@ export function isRegion(region: string | undefined): region is Region {
   return ALL_REGIONS.includes(region as Region);
 }
 
+/**
+ * Not every region is supported by the Blizzard API (and, by extension, the bits of the
+ * WoWA API that use the Blizzard API). This is used to check that a region is supported before
+ * sending a request.
+ */
 export function isSupportedRegion(region: string | undefined): region is SupportedRegion {
   // the 'as' below is just to allow the use of .includes
   return SUPPORTED_REGIONS.includes(region as SupportedRegion);
