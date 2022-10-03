23e9ef22a328e544a225c3e16243aa8e3b52282d
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-03 18:35:29 +0200

    added indemnity talent

--- src/analysis/retail/priest/discipline/CHANGELOG.tsx
+++ src/analysis/retail/priest/discipline/CHANGELOG.tsx
@@ -1,7 +1,10 @@
 import { change, date } from 'common/changelog';
+import { TALENTS_PRIEST } from 'common/TALENTS';
 import { Hana } from 'CONTRIBUTORS';
+import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2022, 10, 3), <>Added <SpellLink id={TALENTS_PRIEST.INDEMNITY_DISCIPLINE_TALENT.id}/>.</>, Hana),
   change(date(2022, 10, 3), <>Dragonflight Clean up.</>, Hana),
 ];
