4278328f4ca2d0cbe3538f6c423a6e9cf576ec59
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-04 00:38:19 +0200

    added damage

--- src/analysis/retail/priest/discipline/modules/spells/Expiation.tsx
+++ src/analysis/retail/priest/discipline/modules/spells/Expiation.tsx
@@ -1,15 +1,17 @@
 import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import { TALENTS_PRIEST } from 'common/TALENTS';
-import Analyzer from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import { Options } from 'parser/core/Module';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import { SpellLink } from 'interface';
-import AtonementAnalyzer from '../core/AtonementAnalyzer';
-import { calculateEffectiveHealing } from 'parser/core/EventCalculateLib';
+import AtonementAnalyzer, { AtonementAnalyzerEvent } from '../core/AtonementAnalyzer';
+import { calculateEffectiveDamage, calculateEffectiveHealing } from 'parser/core/EventCalculateLib';
+import Events, { DamageEvent } from 'parser/core/Events';
+import ItemDamageDone from 'parser/ui/ItemDamageDone';
 
 const EXPIATION_RANK_INCREASE = 0.1;
 class Expiation extends Analyzer {
@@ -18,31 +20,34 @@ class Expiation extends Analyzer {
   mindBlastHealing = 0;
   talentRank = 0;
   expiationIncrease = 0;
+  expiationDamage = 0;
+  bonusDamage = 0;
 
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.hasTalent(TALENTS_PRIEST.EXPIATION_DISCIPLINE_TALENT.id);
     this.addEventListener(AtonementAnalyzer.atonementEventFilter, this.onAtonement);
 
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER), this.onDamage);
     this.talentRank = this.selectedCombatant.getTalentRank(
       TALENTS_PRIEST.EXPIATION_DISCIPLINE_TALENT.id,
     );
     this.expiationIncrease = this.talentRank * EXPIATION_RANK_INCREASE;
   }
 
-  onAtonement(event: any) {
+  onAtonement(event: AtonementAnalyzerEvent) {
     const { healEvent, damageEvent } = event;
 
     if (
-      damageEvent.ability.guid !== SPELLS.EXPIATION_DAMAGE.id &&
-      damageEvent.ability.guid !== TALENTS_PRIEST.SHADOW_WORD_DEATH_TALENT.id &&
-      damageEvent.ability.guid !== SPELLS.MIND_BLAST.id
+      damageEvent?.ability.guid !== SPELLS.EXPIATION_DAMAGE.id &&
+      damageEvent?.ability.guid !== TALENTS_PRIEST.SHADOW_WORD_DEATH_TALENT.id &&
+      damageEvent?.ability.guid !== SPELLS.MIND_BLAST.id
     ) {
       return;
     }
 
     if (damageEvent.ability.guid === SPELLS.EXPIATION_DAMAGE.id) {
-      this.expiationHealing += healEvent.amount + (event.absorbed || 0);
+      this.expiationHealing += healEvent.amount;
     }
 
     if (damageEvent.ability.guid === SPELLS.MIND_BLAST.id) {
@@ -51,6 +56,22 @@ class Expiation extends Analyzer {
 
     if (damageEvent.ability.guid === TALENTS_PRIEST.SHADOW_WORD_DEATH_TALENT.id) {
       this.deathHealing += calculateEffectiveHealing(healEvent, this.expiationIncrease);
+    }
+  }
+
+  onDamage(event: DamageEvent) {
+    if (
+      event.ability.guid !== SPELLS.EXPIATION_DAMAGE.id &&
+      event.ability.guid !== TALENTS_PRIEST.SHADOW_WORD_DEATH_TALENT.id &&
+      event.ability.guid !== SPELLS.MIND_BLAST.id
+    ) {
+      return;
+    }
+
+    if (event.ability.guid === SPELLS.EXPIATION_DAMAGE.id) {
+      this.expiationDamage += event.amount;
+    } else {
+      this.bonusDamage += calculateEffectiveDamage(event, this.expiationIncrease);
     }
   }
 
@@ -75,13 +96,17 @@ class Expiation extends Analyzer {
                 <SpellLink id={SPELLS.MIND_BLAST.id} />: {formatNumber(this.mindBlastHealing)}{' '}
               </li>
             </ul>
+            The bonus damage to <SpellLink id={SPELLS.MIND_BLAST.id} /> and{' '}
+            <SpellLink id={TALENTS_PRIEST.SHADOW_WORD_DEATH_TALENT.id} /> was:{' '}
+            {formatNumber(this.bonusDamage)}.
           </>
         }
         category={STATISTIC_CATEGORY.COVENANTS}
       >
         <BoringSpellValueText spellId={TALENTS_PRIEST.EXPIATION_DISCIPLINE_TALENT.id}>
           <>
-            <ItemHealingDone amount={totalHealing} />
+            <ItemHealingDone amount={totalHealing} /> <br />
+            <ItemDamageDone amount={this.bonusDamage + this.expiationDamage} />
           </>
         </BoringSpellValueText>
       </Statistic>
