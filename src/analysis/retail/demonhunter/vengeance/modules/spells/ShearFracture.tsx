85ab92a7157cf26adabd43d995b3cc4438117cfa
Richard Harrah <topplethenunnery@gmail.com>
2022-10-05 10:22:04 -0400

    convert ShearFracture to use EventLinkNormalizer

--- src/analysis/retail/demonhunter/vengeance/modules/spells/ShearFracture.tsx
+++ src/analysis/retail/demonhunter/vengeance/modules/spells/ShearFracture.tsx
@@ -8,36 +8,28 @@ import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { CastEvent, RemoveBuffStackEvent } from 'parser/core/Events';
 import { NumberThreshold, ThresholdStyle, When } from 'parser/core/ParseResults';
 
+import {
+  getGeneratingCast,
+  getWastedSoulFragment,
+} from '../../normalizers/ShearFractureNormalizer';
+
 /*WCL: https://www.warcraftlogs.com/reports/Y7BWyCx3mHVZzPrk#fight=last&type=summary&view=events&pins=2%24Off%24%23244F4B%24casts%7Cauras%24-1%240.0.0.Any%240.0.0.Any%24true%240.0.0.Any%24true%24258920%7C204255%7C203981%7C263642
 Default spell is Shear (generates 1 soul). Fracture (generates 2 souls) talent replaces it.
 Vengence can have a maxium of 5 souls. Log has 4 fracture casts. Meaning 5 gained souls and 3 wasted. 2 of the 4 casts are bad.
-Souls are generated on a slight delay. Souls should be casted with in 1.3 seconds.
-If a soul is casted and then a buff is removed within 50 ms that means it was a wasted soul generation.
+Souls are generated on a slight delay. Souls should be cast within 1.3 seconds.
+If a soul is cast and then a buff is removed within 100 ms that means it was a wasted soul generation.
 */
 
-const WASTED_SOUL_MS_BUFFER = 50;
-const FRACTURE_SOUL_GENERATE_BUFFER = 1300;
-
-class ShearFracture extends Analyzer {
-  castTimeStamp = 0;
+export default class ShearFracture extends Analyzer {
   badCasts = 0;
-  onSoulFragmentCastTimeStamp = 0;
-  wastedSoulsPerCast = 0;
-  soulFragmentBuffFadeTimeStamp = 0;
-  lastCastEvent?: CastEvent;
   cast: Spell | Talent = SPELLS.SHEAR;
+  lastCast: CastEvent | undefined;
 
   constructor(options: Options) {
     super(options);
-
     if (this.selectedCombatant.hasTalent(TALENTS_DEMON_HUNTER.FRACTURE_TALENT.id)) {
       this.cast = TALENTS_DEMON_HUNTER.FRACTURE_TALENT;
     }
-    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(this.cast), this.onCast);
-    this.addEventListener(
-      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.SOUL_FRAGMENT),
-      this.onSoulFragmentCast,
-    );
     this.addEventListener(
       Events.removebuffstack.by(SELECTED_PLAYER).spell(SPELLS.SOUL_FRAGMENT_STACK),
       this.onSoulFragmentBuffFade,
@@ -56,48 +48,33 @@ class ShearFracture extends Analyzer {
     };
   }
 
-  onCast(event: CastEvent) {
-    this.castTimeStamp = event.timestamp;
-    this.lastCastEvent = event;
-  }
-
-  onSoulFragmentCast(event: CastEvent) {
-    this.onSoulFragmentCastTimeStamp = event.timestamp;
-  }
-
   onSoulFragmentBuffFade(event: RemoveBuffStackEvent) {
-    if (!this.lastCastEvent) {
+    const wastedSoulFragment = getWastedSoulFragment(event);
+    if (!wastedSoulFragment) {
       return;
     }
-    this.soulFragmentBuffFadeTimeStamp = event.timestamp;
-    if (this.soulFragmentBuffFadeTimeStamp - this.castTimeStamp > FRACTURE_SOUL_GENERATE_BUFFER) {
+    const generatingCast = getGeneratingCast(wastedSoulFragment);
+    if (!generatingCast) {
       return;
     }
 
-    if (
-      this.soulFragmentBuffFadeTimeStamp - this.onSoulFragmentCastTimeStamp <
-      WASTED_SOUL_MS_BUFFER
-    ) {
-      this.wastedSoulsPerCast += 1;
-
-      //Exit early if the wasted soul is from the same fracture cast
-      if (this.wastedSoulsPerCast > 1) {
-        this.wastedSoulsPerCast = 0;
-        return;
-      }
-
-      this.badCasts += 1;
-      this.lastCastEvent.meta = this.lastCastEvent.meta || {};
-      this.lastCastEvent.meta.isInefficientCast = true;
-      this.lastCastEvent.meta.inefficientCastReason = 'Fracture cast that over capped souls';
+    // Exit early if the wasted soul is from the same fracture cast
+    if (this.lastCast?.timestamp === generatingCast.timestamp) {
+      return;
     }
+
+    this.lastCast = generatingCast;
+    this.badCasts += 1;
+    this.lastCast.meta = this.lastCast.meta || {};
+    this.lastCast.meta.isInefficientCast = true;
+    this.lastCast.meta.inefficientCastReason = 'Fracture cast that over capped souls';
   }
 
   suggestions(when: When) {
     when(this.wastedCasts).addSuggestion((suggest, actual, recommended) =>
       suggest(
         <>
-          You cast of <SpellLink id={this.cast.id} /> generated souls beyond the cap of 5.
+          Your cast of <SpellLink id={this.cast.id} /> generated souls beyond the cap of 5.
         </>,
       )
         .icon(this.cast.icon)
@@ -111,5 +88,3 @@ class ShearFracture extends Analyzer {
     );
   }
 }
-
-export default ShearFracture;
