908966d426e55837ba005410b277e9fec2bd56ba
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-08 10:32:48 +0200

    changelog

--- src/analysis/retail/priest/discipline/CHANGELOG.tsx
+++ src/analysis/retail/priest/discipline/CHANGELOG.tsx
@@ -5,6 +5,8 @@ import { SpellLink } from 'interface';
 
 
 export default [
-  change(date(2022, 10, 3), <>Added <SpellLink id={TALENTS_PRIEST.INDEMNITY_DISCIPLINE_TALENT.id}/>.</>, Hana),
+  change(date(2022, 10, 8), <>Added <SpellLink id={TALENTS_PRIEST.AEGIS_OF_WRATH_TALENT.id}/>.</>, Hana),
+  change(date(2022, 10, 3), <>Added <SpellLink id={TALENTS_PRIEST.EXPIATION_TALENT.id}/>.</>, Hana),
+  change(date(2022, 10, 3), <>Added <SpellLink id={TALENTS_PRIEST.INDEMNITY_TALENT.id}/>.</>, Hana),
   change(date(2022, 10, 3), <>Dragonflight Clean up.</>, Hana),
 ];
