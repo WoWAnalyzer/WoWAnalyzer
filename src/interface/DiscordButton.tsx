3c4a65ef230ee1a2f7982ecccdd9c47040bb4cd4
Richard Harrah <topplethenunnery@gmail.com>
2022-10-11 22:36:06 -0400

    convert Discord button to TypeScript

--- src/interface/DiscordButton.tsx
+++ src/interface/DiscordButton.tsx
@@ -1,15 +1,15 @@
+import { CSSProperties } from 'react';
 import DiscordLogo from 'interface/icons/Discord';
-import PropTypes from 'prop-types';
 
 import './ThirdPartyButtons.css';
 
-const DiscordButton = ({ style }) => (
+interface DiscordButtonProps {
+  style?: CSSProperties;
+}
+const DiscordButton = ({ style }: DiscordButtonProps) => (
   <a className="btn discord" role="button" href="https://discord.gg/AxphPxU" style={style}>
     <DiscordLogo />
   </a>
 );
-DiscordButton.propTypes = {
-  style: PropTypes.object,
-};
 
 export default DiscordButton;
