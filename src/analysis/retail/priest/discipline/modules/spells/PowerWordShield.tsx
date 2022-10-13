75b240a78779db61157776754a909e8b01ac8a97
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-13 17:36:48 +0200

    updated to use map

--- src/analysis/retail/priest/discipline/modules/spells/PowerWordShield.tsx
+++ src/analysis/retail/priest/discipline/modules/spells/PowerWordShield.tsx
@@ -34,7 +34,7 @@ class PowerWordShield extends Analyzer {
 
   hasAegis = false;
   decayedShields = 0;
-  shieldApplications: ShieldInfo[] = [];
+  private shieldApplications: Map<number, ShieldInfo> = new Map();
   shieldOfAbsolutionValue = 0;
   critCount = 0;
   pwsValue = 0;
@@ -80,7 +80,7 @@ class PowerWordShield extends Analyzer {
   }
 
   onShieldApplication(event: ApplyBuffEvent) {
-    this.shieldApplications.push({
+    this.shieldApplications.set(event.targetID, {
       event: event,
       shieldOfAbsolutionValue: this.shieldOfAbsolutionValue,
       healing: 0,
@@ -104,48 +104,49 @@ class PowerWordShield extends Analyzer {
   }
 
   onShieldExpiry(event: RemoveBuffEvent) {
-    this.shieldApplications.forEach((shieldApplication) => {
-      if (
-        event.targetID === shieldApplication.event.targetID &&
-        event.timestamp > shieldApplication.event.timestamp &&
-        shieldApplication.event.timestamp + POWER_WORD_SHIELD_DURATION_MS > event.timestamp
-      ) {
-        const shieldAmount = shieldApplication.event.absorb || 0; // the initial amount, from the ApplyBuffEvent
-        const shieldOfAbsolutionBonus = shieldApplication.crit
-          ? shieldApplication.shieldOfAbsolutionValue * 2
-          : shieldApplication.shieldOfAbsolutionValue; // the amount the shield was increased by the 4p
-        const basePowerWordShieldAmount = (shieldAmount - shieldOfAbsolutionBonus) / 1.5;
-        let totalShielded = shieldApplication.healing; // this is the amount of healing the shield did
+    const info = this.shieldApplications.get(event.targetID);
+    if (
+      !info ||
+      info.event.timestamp > event.timestamp || // not sure how this happens? fabrication stuff?
+      info.event.timestamp + POWER_WORD_SHIELD_DURATION_MS < event.timestamp
+    ) {
+      return;
+    }
 
-        this.pwsValue += basePowerWordShieldAmount;
-        totalShielded -= basePowerWordShieldAmount;
-        this.t23pValue +=
-          totalShielded >= shieldOfAbsolutionBonus ? shieldOfAbsolutionBonus : totalShielded;
+    const shieldAmount = info.event.absorb || 0; // the initial amount, from the ApplyBuffEvent
+    const shieldOfAbsolutionBonus = info.crit
+      ? info.shieldOfAbsolutionValue * 2
+      : info.shieldOfAbsolutionValue; // the amount the shield was increased by the 4p
+    const basePowerWordShieldAmount = (shieldAmount - shieldOfAbsolutionBonus) / 1.5;
+    let totalShielded = info.healing; // this is the amount of healing the shield did
 
-        if (!this.hasAegis) {
-          return;
-        }
-        if (totalShielded < shieldOfAbsolutionBonus) {
-          this.aegisValue += totalShielded - shieldOfAbsolutionBonus;
-          return;
-        }
-        totalShielded -=
-          totalShielded >= shieldOfAbsolutionBonus ? shieldOfAbsolutionBonus : totalShielded;
-        this.aegisValue += totalShielded;
-      }
-    });
+    this.pwsValue += basePowerWordShieldAmount;
+    totalShielded -= basePowerWordShieldAmount;
+    this.t23pValue +=
+      totalShielded >= shieldOfAbsolutionBonus ? shieldOfAbsolutionBonus : totalShielded;
+
+    if (!this.hasAegis) {
+      return;
+    }
+    if (totalShielded < shieldOfAbsolutionBonus) {
+      this.aegisValue += totalShielded - shieldOfAbsolutionBonus;
+      return;
+    }
+    totalShielded -=
+      totalShielded >= shieldOfAbsolutionBonus ? shieldOfAbsolutionBonus : totalShielded;
+    this.aegisValue += totalShielded;
   }
 
   onPWSAbsorb(event: AbsorbedEvent) {
-    this.shieldApplications.forEach((shieldApplication) => {
-      if (
-        event.targetID === shieldApplication.event.targetID &&
-        event.timestamp > shieldApplication.event.timestamp &&
-        shieldApplication.event.timestamp + POWER_WORD_SHIELD_DURATION_MS > event.timestamp
-      ) {
-        shieldApplication.healing += event.amount;
-      }
-    });
+    const info = this.shieldApplications.get(event.targetID);
+    if (
+      !info ||
+      info.event.timestamp > event.timestamp || // not sure how this happens? fabrication stuff?
+      info.event.timestamp + POWER_WORD_SHIELD_DURATION_MS < event.timestamp
+    ) {
+      return;
+    }
+    info.healing += event.amount;
   }
 
   onPenanceHeal(event: HealEvent) {
@@ -161,7 +162,6 @@ class PowerWordShield extends Analyzer {
   }
 
   statistic() {
-    console.log(this.shieldApplications);
     return (
       <Statistic
         position={STATISTIC_ORDER.OPTIONAL(13)}
