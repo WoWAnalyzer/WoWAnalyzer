080c1dc20e92f1c954f5769318a93c9b9932cde7
jharrell <4829245+jharrell@users.noreply.github.com>
2022-10-15 11:02:08 -0500

    js to ts

--- src/analysis/retail/warlock/affliction/modules/core/SoulShardTracker.tsx
+++ src/analysis/retail/warlock/affliction/modules/core/SoulShardTracker.tsx
@@ -1,18 +1,19 @@
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
+import { Options } from 'parser/core/Analyzer';
+import { CastEvent } from 'parser/core/Events';
 import ResourceTracker from 'parser/shared/modules/resources/resourcetracker/ResourceTracker';
 
 class SoulShardTracker extends ResourceTracker {
-  constructor(...args) {
-    super(...args);
+  constructor(options: Options) {
+    super(options);
     this.resource = RESOURCE_TYPES.SOUL_SHARDS;
   }
 
-  onCast(event) {
+  onCast(event: CastEvent) {
     const classResources = this.getResource(event);
     if (classResources) {
-      // event resource amounts in cast are 10x what they should be for some reason (range 0-50)
       classResources.amount /= 10;
-      classResources.cost /= 10;
+      classResources.cost = classResources.cost ? classResources.cost / 10 : 0;
       classResources.max /= 10;
       super.onCast(event);
     }
--- src/analysis/retail/warlock/affliction/modules/features/Checklist/Component.tsx
+++ src/analysis/retail/warlock/affliction/modules/features/Checklist/Component.tsx
@@ -1,15 +1,21 @@
 import SPELLS from 'common/SPELLS';
+import shadowlandsSpells from 'common/SPELLS/shadowlands/covenants'
+import talents from 'common/TALENTS/warlock';
 import COVENANTS from 'game/shadowlands/COVENANTS';
 import { SpellLink } from 'interface';
 import PreparationRule from 'parser/retail/modules/features/Checklist/PreparationRule';
 import Checklist from 'parser/shared/modules/features/Checklist';
+import {
+  AbilityRequirementProps,
+  ChecklistProps,
+  DotUptimeProps,
+} from 'parser/shared/modules/features/Checklist/ChecklistTypes';
 import GenericCastEfficiencyRequirement from 'parser/shared/modules/features/Checklist/GenericCastEfficiencyRequirement';
 import Requirement from 'parser/shared/modules/features/Checklist/Requirement';
 import Rule from 'parser/shared/modules/features/Checklist/Rule';
-import PropTypes from 'prop-types';
 
-const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, shardTracker }) => {
-  const DotUptime = (props) => (
+const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds }: ChecklistProps) => {
+  const DotUptime = (props: DotUptimeProps) => (
     <Requirement
       name={
         <>
@@ -19,72 +25,66 @@ const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, sha
       thresholds={props.thresholds}
     />
   );
-  DotUptime.propTypes = {
-    id: PropTypes.number.isRequired,
-  };
 
-  const AbilityRequirement = (props) => (
+  const AbilityRequirement = (props: AbilityRequirementProps) => (
     <GenericCastEfficiencyRequirement
       castEfficiency={castEfficiency.getCastEfficiencyForSpellId(props.spell)}
       {...props}
     />
   );
-  AbilityRequirement.propTypes = {
-    spell: PropTypes.number.isRequired,
-  };
 
   return (
     <Checklist>
       <Rule
         name="Maintain your DoTs and debuffs on the boss"
-        description="Affliction Warlocks rely on DoTs and debuffs as means of dealing damage to the target. You should try to keep as highest uptime on them as possible."
+        description="Affliction Warlocks rely heavily on DoTs in order to deal damage to the target. You should try and have as high of an uptime as possible."
       >
         <DotUptime id={SPELLS.AGONY.id} thresholds={thresholds.agony} />
         <DotUptime id={SPELLS.CORRUPTION_CAST.id} thresholds={thresholds.corruption} />
         <DotUptime id={SPELLS.UNSTABLE_AFFLICTION.id} thresholds={thresholds.unstableAffliction} />
-        <DotUptime id={SPELLS.SHADOW_EMBRACE_TALENT.id} thresholds={thresholds.shadowEmbrace} />
-        {combatant.hasTalent(SPELLS.SIPHON_LIFE_TALENT.id) && (
-          <DotUptime id={SPELLS.SIPHON_LIFE_TALENT.id} thresholds={thresholds.siphonLife} />
+        {combatant.hasTalent(talents.SHADOW_EMBRACE_TALENT.id) && (
+          <DotUptime id={talents.SHADOW_EMBRACE_TALENT.id} thresholds={thresholds.shadowEmbrace} />
         )}
-        {combatant.hasTalent(SPELLS.HAUNT_TALENT.id) && (
-          <DotUptime id={SPELLS.HAUNT_TALENT.id} thresholds={thresholds.haunt} />
+        <DotUptime id={talents.SHADOW_EMBRACE_TALENT.id} thresholds={thresholds.shadowEmbrace} />
+        {combatant.hasTalent(talents.SIPHON_LIFE_TALENT.id) && (
+          <DotUptime id={talents.SIPHON_LIFE_TALENT.id} thresholds={thresholds.siphonLife} />
         )}
-        {combatant.hasCovenant(COVENANTS.KYRIAN.id) && (
-          <DotUptime id={SPELLS.SCOURING_TITHE.id} thresholds={thresholds.scouringTithe} />
+        {combatant.hasTalent(talents.HAUNT_TALENT.id) && (
+          <DotUptime id={talents.HAUNT_TALENT.id} thresholds={thresholds.haunt} />
         )}
       </Rule>
       <Rule
         name="Don't cap your Soul Shards"
         description="Soul Shards are your main and most important resource and since their generation is random as Affliction, it's very important not to let them cap."
       >
-        <Requirement
-          name="Wasted shards per minute"
-          thresholds={thresholds.soulShards}
-          valueTooltip={`You wasted ${shardTracker.wasted} shards.`}
+        <Requirement name="Wasted shards per minute" thresholds={thresholds.soulShardDetails}
         />
       </Rule>
       <Rule
         name="Use your cooldowns"
         description="Be mindful of your cooldowns if you are specced into them and use them when it's appropriate. It's okay to hold a cooldown for a little bit when the encounter requires it (burn phases), but generally speaking you should use them as much as you can."
       >
-        <AbilityRequirement spell={SPELLS.SUMMON_DARKGLARE.id} />
-        {combatant.hasTalent(SPELLS.DARK_SOUL_MISERY_TALENT.id) && (
-          <AbilityRequirement spell={SPELLS.DARK_SOUL_MISERY_TALENT.id} />
+        {combatant.hasTalent(talents.SUMMON_DARKGLARE_TALENT.id) && (
+          <AbilityRequirement spell={SPELLS.SUMMON_DARKGLARE.id} />
         )}
-        {combatant.hasTalent(SPELLS.VILE_TAINT_TALENT.id) && (
-          <AbilityRequirement spell={SPELLS.VILE_TAINT_TALENT.id} />
+        {combatant.hasTalent(talents.VILE_TAINT_TALENT.id) && (
+          <AbilityRequirement spell={talents.VILE_TAINT_TALENT.id} />
         )}
-        {combatant.hasTalent(SPELLS.PHANTOM_SINGULARITY_TALENT.id) && (
-          <AbilityRequirement spell={SPELLS.PHANTOM_SINGULARITY_TALENT.id} />
+        {combatant.hasTalent(talents.PHANTOM_SINGULARITY_TALENT.id) && (
+          <AbilityRequirement spell={talents.PHANTOM_SINGULARITY_TALENT.id} />
         )}
+        {combatant.hasTalent(talents.SOUL_ROT_TALENT.id) && (
+          <AbilityRequirement spell={talents.SOUL_ROT_TALENT.id} />
+        )}
+        {/* covenant spells below. Here be dragons. */}
         {combatant.hasCovenant(COVENANTS.NIGHT_FAE.id) && (
-          <AbilityRequirement spell={SPELLS.SOUL_ROT.id} />
+          <AbilityRequirement spell={shadowlandsSpells.SOUL_ROT.id} />
         )}
         {combatant.hasCovenant(COVENANTS.NECROLORD.id) && (
-          <AbilityRequirement spell={SPELLS.DECIMATING_BOLT.id} />
+          <AbilityRequirement spell={shadowlandsSpells.DECIMATING_BOLT.id} />
         )}
         {combatant.hasCovenant(COVENANTS.VENTHYR.id) && (
-          <AbilityRequirement spell={SPELLS.IMPENDING_CATASTROPHE_CAST.id} />
+          <AbilityRequirement spell={shadowlandsSpells.IMPENDING_CATASTROPHE_CAST.id} />
         )}
       </Rule>
       <Rule
@@ -94,9 +94,9 @@ const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, sha
             Use other spells in your toolkit to your advantage. For example, you can try to minimize
             necessary movement by using <SpellLink id={SPELLS.DEMONIC_GATEWAY_CAST.id} icon />,{' '}
             <SpellLink id={SPELLS.DEMONIC_CIRCLE.id} icon />,{' '}
-            <SpellLink id={SPELLS.BURNING_RUSH_TALENT.id} icon /> or mitigate incoming damage with{' '}
+            <SpellLink id={talents.BURNING_RUSH_TALENT.id} icon /> or mitigate incoming damage with{' '}
             <SpellLink id={SPELLS.UNENDING_RESOLVE.id} icon />/
-            <SpellLink id={SPELLS.DARK_PACT_TALENT.id} icon />.<br />
+            <SpellLink id={talents.DARK_PACT_TALENT.id} icon />.<br />
             While you shouldn't cast these defensives on cooldown, be aware of them and use them
             whenever effective. Not using them at all indicates you might not be aware of them or
             not using them optimally.
@@ -104,12 +104,12 @@ const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, sha
         }
       >
         <AbilityRequirement spell={SPELLS.DEMONIC_CIRCLE_TELEPORT.id} />
-        {combatant.hasTalent(SPELLS.DARK_PACT_TALENT.id) && (
-          <AbilityRequirement spell={SPELLS.DARK_PACT_TALENT.id} />
+        {combatant.hasTalent(talents.DARK_PACT_TALENT.id) && (
+          <AbilityRequirement spell={talents.DARK_PACT_TALENT.id} />
         )}
         <AbilityRequirement spell={SPELLS.UNENDING_RESOLVE.id} />
         {combatant.hasCovenant(COVENANTS.NIGHT_FAE.id) && (
-          <AbilityRequirement spell={SPELLS.SOULSHAPE.id} />
+          <AbilityRequirement spell={shadowlandsSpells.SOULSHAPE.id} />
         )}
       </Rule>
       <Rule
@@ -118,7 +118,7 @@ const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, sha
           <>
             You should try to avoid doing nothing during the fight. When you're out of Soul Shards,
             cast <SpellLink id={SPELLS.SHADOW_BOLT_AFFLI.id} icon />/
-            <SpellLink id={SPELLS.DRAIN_SOUL_TALENT.id} icon />, refresh your DoTs etc. When you
+            <SpellLink id={talents.DRAIN_SOUL_TALENT.id} icon />, refresh your DoTs etc. When you
             have to move, use your instant abilities or try to utilize{' '}
             <SpellLink id={SPELLS.DEMONIC_CIRCLE.id} icon>
               Teleport
@@ -131,21 +131,11 @@ const AfflictionWarlockChecklist = ({ combatant, castEfficiency, thresholds, sha
           </>
         }
       >
-        <Requirement name="Downtime" thresholds={thresholds.downtime} />
+        <Requirement name="Downtime" thresholds={thresholds.downtimeSuggestionThresholds} />
       </Rule>
       <PreparationRule thresholds={thresholds} />
     </Checklist>
   );
-};
-
-AfflictionWarlockChecklist.propTypes = {
-  castEfficiency: PropTypes.object.isRequired,
-  combatant: PropTypes.shape({
-    hasTalent: PropTypes.func.isRequired,
-    hasCovenant: PropTypes.func.isRequired,
-  }).isRequired,
-  thresholds: PropTypes.object.isRequired,
-  shardTracker: PropTypes.object.isRequired,
 };
 
 export default AfflictionWarlockChecklist;
