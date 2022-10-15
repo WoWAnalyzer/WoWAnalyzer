6bf7d63b39f802e94e1ba22af0bf390cb52444ba
jharrell <4829245+jharrell@users.noreply.github.com>
2022-10-15 18:55:46 -0500

    Add Statistic back to ABC

--- src/analysis/retail/warlock/affliction/modules/features/AlwaysBeCasting.tsx
+++ src/analysis/retail/warlock/affliction/modules/features/AlwaysBeCasting.tsx
@@ -1,14 +1,15 @@
-import { t } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 import { formatPercentage } from 'common/format';
 import SPELLS from 'common/SPELLS';
-import talents from 'common/TALENTS/warlock'
+import TALENTS from 'common/TALENTS/warlock'
 import { SpellLink } from 'interface';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
 import CoreAlwaysBeCasting from 'parser/shared/modules/AlwaysBeCasting';
+import Gauge from 'parser/ui/Gauge';
+import Statistic from 'parser/ui/Statistic';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 
 class AlwaysBeCasting extends CoreAlwaysBeCasting {
-  position = STATISTIC_ORDER.CORE(1);
 
   get downtimeSuggestionThresholds() {
     return {
@@ -29,7 +30,7 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
           Your downtime can be improved. Try to Always Be Casting (ABC), try to reduce the delay
           between casting spells. Even if you have to move, try casting something instant - maybe
           refresh your dots. Make good use of your <SpellLink id={SPELLS.DEMONIC_CIRCLE.id} /> or
-          <SpellLink id={talents.BURNING_RUSH_TALENT.id} /> when you can.
+          <SpellLink id={TALENTS.BURNING_RUSH_TALENT.id} /> when you can.
         </>,
       )
         .icon('spell_mage_altertime')
@@ -42,6 +43,39 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
         .recommended(`<${formatPercentage(recommended)}% is recommended`)
         .regular(recommended + 0.15)
         .major(recommended + 0.2),
+    );
+  }
+
+  statistic() {
+    return (
+      <Statistic
+        position={STATISTIC_ORDER.CORE(10)}
+        tooltip={
+          <Trans id="warlock.affliction.alwaysBeCasting.statistic.tooltip">
+            Downtime is available time not used to cast anything (including not having your GCD
+            rolling). This can be caused by delays between casting spells, latency, cast
+            interrupting or just simply not casting anything (e.g. due to movement/stunned).
+            <br />
+            <ul>
+              <li>
+                You spent <strong>{formatPercentage(this.activeTimePercentage)}%</strong> of your
+                time casting something.
+              </li>
+              <li>
+                You spent <strong>{formatPercentage(this.downtimePercentage)}%</strong> of your time
+                casting nothing at all.
+              </li>
+            </ul>
+          </Trans>
+        }
+      >
+        <div className="pad">
+          <label>
+            <Trans id="warlock.affliction.alwaysBeCasting.statistic.label">Active time</Trans>
+          </label>
+          <Gauge value={this.activeTimePercentage} />
+        </div>
+      </Statistic>
     );
   }
 }
