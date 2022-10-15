6fe6752021a88f76ba974ec7bfc83366b6c826ee
Lukáš Stuchlík <stuchlik1@seznam.cz>
2022-10-15 23:21:19 +0200

    changelog

--- src/analysis/retail/rogue/subtlety/CHANGELOG.js
+++ src/analysis/retail/rogue/subtlety/CHANGELOG.js
@@ -1,10 +1,11 @@
 import { change, date } from 'common/changelog';
 import SPELLS from 'common/SPELLS';
-import { Tyndi, Zeboot, Putro, Hordehobbs, Akai } from 'CONTRIBUTORS';
+import { Tyndi, Zeboot, Putro, Hordehobbs, Akai, Chizu } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2022, 10, 15), 'Initial support for Dragonflight - cleanup of old effects', Chizu),
   change(date(2022, 3, 6), <>Bump to partial support for 9.2</>, Tyndi),
   change(date(2021, 8, 12), <>Added <SpellLink id={SPELLS.FLAGELLATION.id} /> suggestion in overview section. Fixed Flagellation damage in statistics section.</>, Akai),
   change(date(2021, 5, 2), <>Fix bug in Sepsis analyzer in mythic plus analysis.</>, Hordehobbs),
