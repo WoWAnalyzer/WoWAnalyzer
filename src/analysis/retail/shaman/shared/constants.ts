00e72af344613d3040eeeabaa604c1d67164a2eb
Danny Janse <d.janse@live.com>
2022-10-17 04:11:18 +0200

    Update enhancement shaman spellbook (#5185)

--- src/analysis/retail/shaman/shared/constants.ts
+++ src/analysis/retail/shaman/shared/constants.ts
@@ -9,3 +9,11 @@ export const ELEMENTAL_BLAST_BUFFS = [
   SPELLS.ELEMENTAL_BLAST_MASTERY,
   SPELLS.ELEMENTAL_BLAST_HASTE,
 ];
+
+export const TOTEMIC_SURGE_SCALING = [0, 2, 4];
+
+// Go with the flow - Spirit Walk
+export const GWTF_SW_SCALING = [0, 7.5, 15];
+
+// Go with the flow - Gust of Wind
+export const GWTF_GOW_SCALING = [0, 5, 10];
