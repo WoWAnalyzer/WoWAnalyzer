b5217b2d0843ea075f13bcb22cbd2aa5a0b358de
trevorm4 <trevorm4@illinois.edu>
2022-10-16 20:18:22 -0700

    add viv functions for mol

--- src/analysis/retail/monk/mistweaver/modules/spells/MistsOfLife.tsx
+++ src/analysis/retail/monk/mistweaver/modules/spells/MistsOfLife.tsx
@@ -28,12 +28,17 @@ class MistsOfLife extends Analyzer {
   extraEnvbHealing: number = 0;
   extraEnvbOverHealing: number = 0;
   extraEnvbHits: number = 0;
+  extraEnvmApplications: number = 0;
   extraEnvmHealing: number = 0;
   extraEnvmOverHealing: number = 0;
   extraEnvmHits: number = 0;
+  extraRemApplications: number = 0;
   extraRemHealing: number = 0;
   extraRemOverHealing: number = 0;
   extraRemHits: number = 0;
+  extraVivCleaves: number = 0;
+  extraVivHealing: number = 0;
+  extraVivOverhealing: number = 0;
   envmHealingIncrease: number = 0;
   extraEnvBonusHealing: number = 0;
 
@@ -61,6 +66,15 @@ class MistsOfLife extends Analyzer {
     this.addEventListener(
       Events.heal.by(SELECTED_PLAYER).spell(SPELLS.ENVELOPING_BREATH_HEAL),
       this.handleEnvbHeal,
+    );
+    this.addEventListener(Events.heal.by(SELECTED_PLAYER).spell(SPELLS.VIVIFY), this.handleVivify);
+    this.addEventListener(
+      Events.heal.by(SELECTED_PLAYER).spell(SPELLS.RENEWING_MIST_HEAL),
+      this.handleRemHeal,
+    );
+    this.addEventListener(
+      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.RENEWING_MIST_HEAL),
+      this.handleRemApply,
     );
     this.addEventListener(Events.heal.by(SELECTED_PLAYER), this.handleHeal);
   }
@@ -137,6 +151,58 @@ class MistsOfLife extends Analyzer {
     this.extraEnvBonusHealing += calculateEffectiveHealing(event, this.envmHealingIncrease);
   }
 
+  handleRemApply(event: ApplyBuffEvent | RefreshBuffEvent) {
+    const targetId = event.targetID;
+    const spellId = event.ability.guid;
+    if (
+      UNAFFECTED_SPELLS.includes(spellId) ||
+      !this.hotTracker.hots[targetId] ||
+      !this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id]
+    ) {
+      return;
+    }
+    const hot = this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id];
+    if (this.hotTracker.fromMistsOfLife(hot)) {
+      this.extraRemApplications += 1;
+    }
+  }
+
+  handleRemHeal(event: HealEvent) {
+    const targetId = event.targetID;
+    const spellId = event.ability.guid;
+    if (
+      UNAFFECTED_SPELLS.includes(spellId) ||
+      !this.hotTracker.hots[targetId] ||
+      !this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id]
+    ) {
+      return;
+    }
+    const hot = this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id];
+    if (this.hotTracker.fromMistsOfLife(hot)) {
+      this.extraRemHealing += event.amount || 0;
+      this.extraRemOverHealing += event.overheal || 0;
+      this.extraRemHits += 1;
+    }
+  }
+
+  handleVivify(event: HealEvent) {
+    const targetId = event.targetID;
+    const spellId = event.ability.guid;
+    if (
+      UNAFFECTED_SPELLS.includes(spellId) ||
+      !this.hotTracker.hots[targetId] ||
+      !this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id]
+    ) {
+      return;
+    }
+    const hot = this.hotTracker.hots[targetId][SPELLS.RENEWING_MIST_HEAL.id];
+    if (this.hotTracker.fromMistsOfLife(hot)) {
+      this.extraVivCleaves += 1;
+      this.extraVivHealing += event.amount || 0;
+      this.extraVivOverhealing += event.overheal || 0;
+    }
+  }
+
   statistic() {
     return (
       <Statistic
@@ -165,12 +231,32 @@ class MistsOfLife extends Analyzer {
               Extra <SpellLink id={TALENTS_MONK.ENVELOPING_BREATH_TALENT.id} /> direct healing:{' '}
               {formatNumber(this.extraEnvbHealing)}
             </li>
+            <li>
+              <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id} /> extra hits:{' '}
+              {this.extraRemHits}
+            </li>
+            <li>
+              Extra <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id} /> direct healing:{' '}
+              {formatNumber(this.extraRemHealing)}
+            </li>
+            <li>
+              Extra <SpellLink id={SPELLS.VIVIFY.id} /> cleaves: {this.extraVivCleaves}
+            </li>
+            <li>
+              Extra <SpellLink id={SPELLS.VIVIFY.id} /> direct healing:{' '}
+              {formatNumber(this.extraVivHealing)}
+            </li>
           </ul>
         }
       >
         <TalentSpellText talent={TALENTS_MONK.MISTS_OF_LIFE_TALENT}>
           <ItemHealingDone
-            amount={this.extraEnvmHealing + this.extraEnvbHealing + this.extraEnvBonusHealing}
+            amount={
+              this.extraEnvmHealing +
+              this.extraEnvbHealing +
+              this.extraVivHealing +
+              this.extraEnvBonusHealing
+            }
           />
         </TalentSpellText>
       </Statistic>
