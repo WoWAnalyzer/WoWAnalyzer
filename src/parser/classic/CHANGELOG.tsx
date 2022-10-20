573d39cd2f57f9d311302d5d6de93b9a87dd0cb0
TJ Moats <tj@moats.io>
2022-10-20 08:23:04 -0500

    Adding Classic Changelog

---
+++ src/parser/classic/CHANGELOG.tsx
@@ -0,0 +1,7 @@
+import { change, date } from 'common/changelog';
+import { Khadaj } from 'CONTRIBUTORS';
+
+// prettier-ignore
+export default [
+  change(date(2022, 10, 20), 'Adding Classic Specific Changelog.', Khadaj),
+];
