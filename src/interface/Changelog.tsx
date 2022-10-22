18285b7c8f63ab6ed4b8991b3e1262cb1a4bab96
TJ Moats <tj@moats.io>
2022-10-22 16:39:25 -0500

    removing changelog bits

--- src/interface/Changelog.tsx
+++ src/interface/Changelog.tsx
@@ -1,6 +1,4 @@
 import CORE_CHANGELOG from 'CHANGELOG';
-import CLASSIC_CHANGELOG from 'parser/classic/CHANGELOG';
-
 import { ChangelogEntry } from 'common/changelog';
 import Contributor from 'interface/ContributorButton';
 import ReadableListing from 'interface/ReadableListing';
@@ -13,7 +11,7 @@ interface Props {
 
 const Changelog = ({ changelog, limit, includeCore = true }: Props) => {
   const mergedChangelog: ChangelogEntry[] = includeCore
-    ? [...CORE_CHANGELOG, ...CLASSIC_CHANGELOG, ...changelog].sort(
+    ? [...CORE_CHANGELOG, ...changelog].sort(
         (a: ChangelogEntry, b: ChangelogEntry) => b.date.getTime() - a.date.getTime(),
       )
     : changelog;
--- src/parser/classic/CHANGELOG.tsx
+++
@@ -1,8 +0,0 @@
-import { change, date } from 'common/changelog';
-import { Khadaj } from 'CONTRIBUTORS';
-
-// prettier-ignore
-export default [
-  change(date(2022, 10, 20), 'Updating WOTLK Flask list.', Khadaj),
-  change(date(2022, 10, 20), 'Adding Classic Specific Changelog.', Khadaj),
-];
