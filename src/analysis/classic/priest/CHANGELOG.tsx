42f00d2d4c3c4bb86372fd091a278c9221675061
TJ Moats <tj@moats.io>
2022-10-26 10:44:11 -0500

    changelog

--- src/analysis/classic/priest/CHANGELOG.tsx
+++ src/analysis/classic/priest/CHANGELOG.tsx
@@ -2,6 +2,7 @@ import { change, date } from 'common/changelog';
 import { Khadaj } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2022, 10, 26), 'Finishing up priest analysis', Khadaj),
   change(date(2022, 10, 25), 'Defining more priest spells', Khadaj),
   change(date(2022, 10, 13), 'Adding DOT tracking for Shadow', Khadaj),
   change(date(2022, 10, 13), 'Updating Priest Spell IDS', Khadaj),
