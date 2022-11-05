4ffccd642068f98fa6b3efdc141a71a30b9b97e1
Putro <laustsf@gmail.com>
2022-11-05 16:54:50 +0100

    Adjust Dire Beast summon spell id and mark it as a non-cast

--- src/parser/core/CASTS_THAT_ARENT_CASTS.ts
+++ src/parser/core/CASTS_THAT_ARENT_CASTS.ts
@@ -47,6 +47,7 @@ const spells: number[] = [
 
   //region Hunter
   SPELLS.BARBED_SHOT_PET_BUFF.id, //The buff applied to BM Hunter pet when casting Barbed Shot
+  SPELLS.DIRE_BEAST_SUMMON.id, //Additional cast event associated with summoning a Dire Beast
   //endregion
 
   //region Mage
