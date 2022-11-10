20b69ed783f9d12c0f1ac5533913586dee40c390
Aveline Estié <aa.estie@gmail.com>
2022-11-10 15:34:56 +0100

    [Warlock] Add missing entries to Spellbook (#5337)

--- src/analysis/retail/warlock/affliction/CHANGELOG.tsx
+++ src/analysis/retail/warlock/affliction/CHANGELOG.tsx
@@ -1,11 +1,12 @@
 import { change, date } from 'common/changelog';
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/warlock';
-import { Dambroda, Sharrq, Maleficien, Akhtal, Pirrang, ToppleTheNun, Jonfanz } from 'CONTRIBUTORS';
+import { Dambroda, Sharrq, Maleficien, Akhtal, Pirrang, ToppleTheNun, Jonfanz, Arlie } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2022, 11, 10), <>Fix <SpellLink id={TALENTS.DRAIN_SOUL_TALENT.id} /> not showing as intended and add <SpellLink id={SPELLS.IMP_SINGE_MAGIC} />.</>, Arlie),
   change(date(2022, 11, 9), 'Remove Shadowlands covenant abilities from checklist.', ToppleTheNun),
   change(date(2022, 10, 20), <>Fix <SpellLink id={TALENTS.DRAIN_SOUL_TALENT.id} /> damage calculations and add initial support for <SpellLink id={TALENTS.DREAD_TOUCH_TALENT.id} />.</>, Jonfanz),
   change(date(2022, 10, 14), 'Begin working on support for Dragonflight.', Jonfanz),
