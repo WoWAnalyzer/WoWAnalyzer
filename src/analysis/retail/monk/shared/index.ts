42d204a46df2039ca46b8a4c723df922057a13eb
trevorm4 <trevorm4@illinois.edu>
2022-11-12 15:10:15 -0800

    Fix build failure

--- src/analysis/retail/monk/shared/index.ts
+++ src/analysis/retail/monk/shared/index.ts
@@ -4,4 +4,5 @@ export { default as FaelineStomp } from './FaelineStomp';
 export { default as TouchOfDeath } from './TouchOfDeath';
 export { default as MysticTouch } from './MysticTouch';
 export { default as DampenHarm } from './DampenHarm';
+export { default as SaveThemAll } from './SaveThemAll';
 export * from './constants';
