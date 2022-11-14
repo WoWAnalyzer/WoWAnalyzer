92348f5945905336ed5e5fa1f12b1900f3bb52fa
Sharrq <cjgrasty86@gmail.com>
2022-11-13 20:17:05 -0500

    Update CHANGELOG.tsx

--- src/analysis/retail/mage/fire/CHANGELOG.tsx
+++ src/analysis/retail/mage/fire/CHANGELOG.tsx
@@ -3,6 +3,7 @@ import { Sharrq } from 'CONTRIBUTORS';
 
 // prettier-ignore
 export default [
+  change(date(2022, 11, 13), `Initial pass on Dragonflight Compatability.`, Sharrq),
   change(date(2022, 10, 30), `Update Dragonflight SPELLS, Abilities, and Buffs`, Sharrq),
   change(date(2022, 10, 9), 'Initial Dragonflight support', Sharrq),
 ];
