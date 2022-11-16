e2b20da9f3bb6473894b195ca714b12bd5e88107
vohrr <matthew.d.pingel@gmail.com>
2022-11-16 17:37:49 -0500

    Changelog Entry

--- src/analysis/retail/evoker/preservation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/preservation/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { Tyndi, Vohrr } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2022, 11, 16), <>Updated removed shields from Mastery Effectiveness for a rework and relabeled and added a tooltip indicating such</>, Vohrr),
   change(date(2022, 11, 14), <>Added preliminary hot tracking, cast attributions, & cast normalizers for Echo handling. Updated <SpellLink id={TALENTS_EVOKER.SPIRITBLOOM_TALENT.id}/> and <SpellLink id={TALENTS_EVOKER.DREAM_BREATH_TALENT.id}/> modules</>, Vohrr),
   change(date(2022, 10, 25), 'Updated abilities list to include all available abilities.', Tyndi),
   change(date(2022, 10, 19), 'Added module tracking healing effected by mastery.', Tyndi),
