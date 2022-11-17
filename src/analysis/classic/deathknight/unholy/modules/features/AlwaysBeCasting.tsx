c1d93b5ba98da608219a6092d3d39b60a94967fc
Jazminite <jazminitehs@gmail.com>
2022-11-17 18:05:05 -0500

    adjust downtime thresholds

--- src/analysis/classic/deathknight/unholy/modules/features/AlwaysBeCasting.tsx
+++ src/analysis/classic/deathknight/unholy/modules/features/AlwaysBeCasting.tsx
@@ -10,9 +10,9 @@ class AlwaysBeCasting extends CoreAlwaysBeCasting {
     return {
       actual: this.downtimePercentage,
       isGreaterThan: {
-        minor: 0.1,
-        average: 0.15,
-        major: 0.2,
+        minor: 0.25,
+        average: 0.3,
+        major: 0.35,
       },
       style: ThresholdStyle.PERCENTAGE,
     };
