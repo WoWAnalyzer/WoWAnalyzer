78a1c0cafe11597091662370d2437b4471378a8d
trevorm4 <trevorm4@illinois.edu>
2022-11-17 18:55:27 -0800

    fix rd and remove lc statistic

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2022, 11, 17),<> Fix some typos and remove <SpellLink id={TALENTS_MONK.LIFE_COCOON_TALENT.id}/> statistic</> ,Trevor),
   change(date(2022, 11, 14), <>Updated average <SpellLink id={SPELLS.RENEWING_MIST_HEAL.id}/> per <SpellLink id={SPELLS.VIVIFY.id}/> suggestion threshold to be accurate for specific talent selections</>, Vohrr),
   change(date(2022, 11, 13), <>Updates to Hot Tracker to better handle <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id}/> tracking. Added <SpellLink id={TALENTS_MONK.RAPID_DIFFUSION_TALENT.id}/> module.</>, Vohrr),
   change(date(2022, 11, 13), <>Add <SpellLink id={TALENTS_MONK.MISTS_OF_LIFE_TALENT.id}/> module</>, Trevor),
--- src/analysis/retail/monk/mistweaver/modules/spells/LifeCocoon.tsx
+++ src/analysis/retail/monk/mistweaver/modules/spells/LifeCocoon.tsx
@@ -2,14 +2,8 @@ import Analyzer, { Options } from 'parser/core/Analyzer';
 import { calculateEffectiveHealing } from 'parser/core/EventCalculateLib';
 import Events, { HealEvent } from 'parser/core/Events';
 import Combatants from 'parser/shared/modules/Combatants';
-import ItemHealingDone from 'parser/ui/ItemHealingDone';
-import Statistic from 'parser/ui/Statistic';
-import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
-import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 import { TALENTS_MONK } from 'common/TALENTS';
-
 import { LIFE_COCOON_HEALING_BOOST } from '../../constants';
-import TalentSpellText from 'parser/ui/TalentSpellText';
 
 class LifeCocoon extends Analyzer {
   static dependencies = {
@@ -24,7 +18,7 @@ class LifeCocoon extends Analyzer {
   }
 
   cocoonBuff(event: HealEvent) {
-    //Life Cocoon works on any HoT that has this flag checked even if they don't come from the mistweaver themselves
+    // Life Cocoon works on any HoT that has this flag checked even if they don't come from the mistweaver themselves
     if (!event.tick) {
       return;
     }
@@ -38,22 +32,6 @@ class LifeCocoon extends Analyzer {
     if (target.hasBuff(TALENTS_MONK.LIFE_COCOON_TALENT.id, event.timestamp, 0, 0)) {
       this.healing += calculateEffectiveHealing(event, LIFE_COCOON_HEALING_BOOST);
     }
-  }
-
-  statistic() {
-    return (
-      <Statistic
-        position={STATISTIC_ORDER.OPTIONAL(70)}
-        category={STATISTIC_CATEGORY.TALENTS}
-        size="flexible"
-        tooltip={<>Life Cocoon boosts HoTs from other players as wells as your own.</>}
-      >
-        <TalentSpellText talent={TALENTS_MONK.LIFE_COCOON_TALENT}>
-          <ItemHealingDone amount={this.healing} />
-          <br />
-        </TalentSpellText>
-      </Statistic>
-    );
   }
 }
 
