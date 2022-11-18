5da61adbbafc93eecf9c6050da902204b25aac14
trevorm4 <trevorm4@illinois.edu>
2022-11-17 19:27:48 -0800

    Addressed lc comment and change bdb condition in revival module

--- src/analysis/retail/monk/mistweaver/modules/spells/LifeCocoon.tsx
+++ src/analysis/retail/monk/mistweaver/modules/spells/LifeCocoon.tsx
@@ -2,8 +2,14 @@ import Analyzer, { Options } from 'parser/core/Analyzer';
 import { calculateEffectiveHealing } from 'parser/core/EventCalculateLib';
 import Events, { HealEvent } from 'parser/core/Events';
 import Combatants from 'parser/shared/modules/Combatants';
+import ItemHealingDone from 'parser/ui/ItemHealingDone';
+import Statistic from 'parser/ui/Statistic';
+import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 import { TALENTS_MONK } from 'common/TALENTS';
+
 import { LIFE_COCOON_HEALING_BOOST } from '../../constants';
+import TalentSpellText from 'parser/ui/TalentSpellText';
 
 class LifeCocoon extends Analyzer {
   static dependencies = {
@@ -32,6 +38,22 @@ class LifeCocoon extends Analyzer {
     if (target.hasBuff(TALENTS_MONK.LIFE_COCOON_TALENT.id, event.timestamp, 0, 0)) {
       this.healing += calculateEffectiveHealing(event, LIFE_COCOON_HEALING_BOOST);
     }
+  }
+
+  statistic() {
+    return (
+      <Statistic
+        position={STATISTIC_ORDER.OPTIONAL(70)}
+        category={STATISTIC_CATEGORY.TALENTS}
+        size="flexible"
+        tooltip={<>Life Cocoon boosts HoTs from other players as wells as your own.</>}
+      >
+        <TalentSpellText talent={TALENTS_MONK.LIFE_COCOON_TALENT}>
+          <ItemHealingDone amount={this.healing} />
+          <br />
+        </TalentSpellText>
+      </Statistic>
+    );
   }
 }
 
