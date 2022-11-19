41cea8276e7e585fd62bc5684e4f887ea9753109
Matthew P <matthew.d.pingel@gmail.com>
2022-11-18 19:44:53 -0500

    [Preservation] Added Reversion Module (#5377)

--- src/analysis/retail/evoker/preservation/constants.ts
+++ src/analysis/retail/evoker/preservation/constants.ts
@@ -1,3 +1,10 @@
-export const EMPATH_REGEN_FACTOR = 1;
+//hots
+export const ECHO_BASE_DURATION = 15000;
+export const REVERSION_BASE_DURATION = 12000;
+export const DREAM_BREATH_MIN_DURATION = 4000;
+export const DREAM_BREATH_MAX_DURATION = 16000;
 
+//talents
+export const TIMELESS_MAGIC = 0.15;
+export const EMPATH_REGEN_FACTOR = 1;
 export const FLOW_STATE_FACTOR = 0.1;
