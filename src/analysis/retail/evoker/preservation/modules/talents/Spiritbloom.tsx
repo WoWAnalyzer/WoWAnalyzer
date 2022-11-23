4f2727ac8fe8f9ac51ad7532f3b4baffcaba03fe
trevorm4 <trevorm4@illinois.edu>
2022-11-22 21:42:53 -0800

    Remove useless stats from evoker and cleanup cast link file

--- src/analysis/retail/evoker/preservation/modules/talents/Spiritbloom.tsx
+++ src/analysis/retail/evoker/preservation/modules/talents/Spiritbloom.tsx
@@ -2,13 +2,7 @@ import SPELLS from 'common/SPELLS';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import AbilityTracker from 'parser/shared/modules/AbilityTracker';
 import Events, { HealEvent } from 'parser/core/Events';
-import Statistic from 'parser/ui/Statistic';
-import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
-import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import { TALENTS_EVOKER } from 'common/TALENTS';
-import TalentSpellText from 'parser/ui/TalentSpellText';
-import ItemHealingDone from 'parser/ui/ItemHealingDone';
-import { formatNumber } from 'common/format';
 
 class DreamBreath extends Analyzer {
   static dependencies = {
@@ -41,28 +35,6 @@ class DreamBreath extends Analyzer {
 
   onSplitHeal(event: HealEvent) {
     this.spiritbloomFirstSplitHealingDone += event.amount + (event.absorbed || 0);
-  }
-
-  statistic() {
-    return (
-      <Statistic
-        size="flexible"
-        position={STATISTIC_ORDER.CORE(1)}
-        category={STATISTIC_CATEGORY.TALENTS}
-        tooltip={
-          <ul>
-            <li>Primary Target Healing: {formatNumber(this.spiritbloomInitialHealingDone)}</li>
-            <li>Cleave Target Healing: {formatNumber(this.spiritbloomFirstSplitHealingDone)}</li>
-          </ul>
-        }
-      >
-        <TalentSpellText talent={TALENTS_EVOKER.SPIRITBLOOM_TALENT}>
-          <ItemHealingDone
-            amount={this.spiritbloomInitialHealingDone + this.spiritbloomFirstSplitHealingDone}
-          />
-        </TalentSpellText>
-      </Statistic>
-    );
   }
 }
 
