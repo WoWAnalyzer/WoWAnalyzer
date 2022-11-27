28c648d2bac86913ca3fde54627f1a48f1533f66
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-11-26 21:05:44 -0500

    add dragonflight gems (#5436)
    Co-authored-by: Kelton Finch <keltonfinch@gmail.com>

--- src/common/ITEMS/dragonflight/index.ts
+++ src/common/ITEMS/dragonflight/index.ts
@@ -1,7 +1,8 @@
 import Enchants from './enchants';
+import Gems from './gems';
 import safeMerge from 'common/safeMerge';
 
-const items = safeMerge(Enchants);
+const items = safeMerge(Enchants, Gems);
 
 export default items;
 export * from './tier';
