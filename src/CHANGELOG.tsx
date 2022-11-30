b11aa23d6d3af9b30b3377ce57455ecb62be63e1
Richard Harrah <topplethenunnery@gmail.com>
2022-11-29 23:31:47 -0500

    remove analysis for SL-specific items

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -67,6 +67,7 @@ import SpellLink from 'interface/SpellLink';
 import { TALENTS_SHAMAN } from 'common/TALENTS';
 
 export default [
+  change(date(2022, 11, 29), 'Remove analysis for Shadowlands dungeon/raid/crafted items.', ToppleTheNun),
   change(date(2022, 11, 28), 'Add chevron to cooldown expandables.', ToppleTheNun),
   change(date(2022, 11, 26), 'Add potions for Dragonflight.', ToppleTheNun),
   change(date(2022, 11, 26), 'Add gems for Dragonflight.', ToppleTheNun),
