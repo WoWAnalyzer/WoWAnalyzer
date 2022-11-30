d70ee67652af2b4e4f67b1ad8c3168b8e6c11cb8
Richard Harrah <topplethenunnery@gmail.com>
2022-11-30 14:00:22 -0500

    add Refreshing Healing Potion support

---
+++ src/common/SPELLS/dragonflight/potions.ts
@@ -0,0 +1,11 @@
+import { spellIndexableList } from '../Spell';
+
+const spells = spellIndexableList({
+  REFRESHING_HEALING_POTION: {
+    id: 370511,
+    name: 'Refreshing Healing Potion',
+    icon: 'inv_potion_49',
+  },
+});
+
+export default spells;
--- src/parser/retail/modules/items/HealthPotion.ts
+++ src/parser/retail/modules/items/HealthPotion.ts
@@ -6,7 +6,7 @@ import Potion from './Potion';
  * Tracks health potion cooldown.
  */
 class HealthPotion extends Potion {
-  static spells = [SPELLS.SPIRITUAL_HEALING_POTION.id, SPELLS.COSMIC_HEALING_POTION.id];
+  static spells = [SPELLS.REFRESHING_HEALING_POTION.id];
   static recommendedEfficiency = 0;
   static extraAbilityInfo = {
     isDefensive: true,
