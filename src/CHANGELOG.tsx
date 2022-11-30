149d3aa02841c75e970de8f99927525cdb8dd3e5
Richard Harrah <topplethenunnery@gmail.com>
2022-11-29 21:37:03 -0500

    deprecate access to SL Combatant info

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -67,6 +67,7 @@ import SpellLink from 'interface/SpellLink';
 import { TALENTS_SHAMAN } from 'common/TALENTS';
 
 export default [
+  change(date(2022, 11, 29), 'Deprecate/disable access to Shadowlands covenants, soulbinds, conduits, and legendaries.', ToppleTheNun),
   change(date(2022, 11, 28), 'Add chevron to cooldown expandables.', ToppleTheNun),
   change(date(2022, 11, 26), 'Add potions for Dragonflight.', ToppleTheNun),
   change(date(2022, 11, 26), 'Add gems for Dragonflight.', ToppleTheNun),
