cd343de806987ab5cd23bc524c5b1c27db68fdac
Trevor <trevorm4@illinois.edu>
2022-12-06 22:19:53 -0500

    Mark Preservation as fully supported (#5483)

--- src/analysis/retail/evoker/preservation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/preservation/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { Trevor, Tyndi, Vohrr } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2022, 12, 4), <>Mark Preservation Evoker as fully supported</>, Trevor),
   change(date(2022, 12, 4), <>Added <SpellLink id={TALENTS_EVOKER.CYCLE_OF_LIFE_TALENT.id}/> module</>, Trevor),
   change(date(2022, 12, 4), <>Added <SpellLink id={TALENTS_EVOKER.TIME_LORD_TALENT.id}/> module</>, Trevor),
   change(date(2022, 12, 4), <>Add <SpellLink id={TALENTS_EVOKER.DREAM_FLIGHT_TALENT.id}/> module and update checklist</>, Trevor),
