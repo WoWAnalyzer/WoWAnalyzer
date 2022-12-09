6b4954625f77c6fb634bd2bd358b7cd1c9feb2e9
emallson <emallson@emallson.net>
2022-12-08 21:01:59 -0500

    split & reorganize major defensive guide section

---
+++ src/analysis/retail/monk/brewmaster/modules/core/MajorDefensives/config.ts
@@ -0,0 +1,13 @@
+import { DampenHarm } from 'analysis/retail/monk/shared';
+import CelestialBrew from '../../spells/CelestialBrew';
+import { DiffuseMagic } from './DiffuseMagic';
+import { FortifyingBrew } from './FortifyingBrew';
+import { ZenMeditation } from './ZenMeditation';
+
+export const MAJOR_ANALYZERS = [
+  CelestialBrew,
+  FortifyingBrew,
+  DampenHarm,
+  DiffuseMagic,
+  ZenMeditation,
+];
