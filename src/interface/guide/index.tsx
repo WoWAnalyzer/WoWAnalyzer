b5d8f16e65dd14c42acbcdc5bbbc3e73e7916de1
emallson <emallson@emallson.net>
2022-12-12 17:13:26 -0500

    clean up the SubSection prop type

--- src/interface/guide/index.tsx
+++ src/interface/guide/index.tsx
@@ -296,13 +296,7 @@ export const GuideContainer = ({ children }: { children: React.ReactNode }) => (
  * A section within a section. This can be nested (so you'd have a
  * sub-sub-section). Don't go too crazy with that.
  */
-export const SubSection = ({
-  children,
-  title,
-  // eslint-disable-next-line react/prop-types
-  id,
-  ...props
-}: React.PropsWithChildren<{ title?: string } & React.ComponentProps<'div'>>) => (
+export const SubSection = ({ children, title, id, ...props }: React.ComponentProps<'div'>) => (
   <section className="subsection" id={id}>
     <header>{title || ''}</header>
     <div {...props}>{children}</div>
