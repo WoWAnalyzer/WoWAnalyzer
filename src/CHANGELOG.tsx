2a274a643376daa3d1f2e5aab7f750e38134c372
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-12-13 14:38:01 -0500

    mark T28 tier set references as deprecated (#5505)

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -16,6 +16,7 @@ import { ItemLink } from 'interface';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2022, 12, 13), 'Mark T28 tier set references as deprecated.', ToppleTheNun),
   change(date(2022, 12, 13), 'Add stat enhancements for foods except for feasts.', ToppleTheNun),
   change(date(2022, 12, 8), 'Updated rating per % values in StatTracker to 70 levels, and updated base mana pools in preparation for next talent regeneration', Putro),
   change(date(2022, 12, 8), 'Add support for empowered cast log events.', ToppleTheNun),
