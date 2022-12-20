7b2d52b8cb66ed703be77b4d5e58d5f6cd248d7c
vohrr <matthew.d.pingel@gmail.com>
2022-12-20 01:51:22 -0500

    Updating outdated JSS and SooM suggestions

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2022, 12, 20), <>Fix suggestion for <SpellLink id={TALENTS_MONK.SUMMON_JADE_SERPENT_STATUE_TALENT.id}/></>, Vohrr),
   change(date(2022, 12, 18), <>Fix suggestion for <SpellLink id={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT.id}/> usage based on talent selection</>, Trevor),
   change(date(2022, 12, 18), <>Add suggestion for <SpellLink id={TALENTS_MONK.ANCIENT_TEACHINGS_TALENT.id}/> buff uptime</>, Trevor),
   change(date(2022, 12, 14), <>Remove <SpellLink id={TALENTS_MONK.FAELINE_STOMP_TALENT.id}/> cast efficiency suggestion for Mistweaver</>, Trevor),
