ab0f1f51113a5d420c320f7b0c9971477b23f771
Jazminite <jazminitehs@gmail.com>
2022-12-20 23:23:39 -0500

    Classic Unholy DK updates

--- src/analysis/classic/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/classic/deathknight/unholy/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { jazminite } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2022, 12, 20), 'Update info box and set partial to false.', jazminite),
   change(date(2022, 11, 17), 'Initial guide for Classic Unholy Death Knight.', jazminite),
 ];
