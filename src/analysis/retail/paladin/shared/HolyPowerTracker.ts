bac58157f0855a28bbe28ef21b6952cfaab57eca
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-12-21 19:22:05 -0500

    re-order Beacon handling on fight end (#5557)

--- src/analysis/retail/paladin/shared/HolyPowerTracker.ts
+++ src/analysis/retail/paladin/shared/HolyPowerTracker.ts
@@ -1,18 +1,20 @@
 import SPELLS from 'common/SPELLS';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 import ResourceTracker from 'parser/shared/modules/resources/resourcetracker/ResourceTracker';
+import { Options } from 'parser/core/Analyzer';
+import { CastEvent } from 'parser/core/Events';
 
 class HolyPowerTracker extends ResourceTracker {
-  constructor(...args) {
-    super(...args);
+  constructor(options: Options) {
+    super(options);
     this.resource = RESOURCE_TYPES.HOLY_POWER;
   }
 
-  getAdjustedCost(event) {
-    if (!this.getResource(event).cost) {
+  getAdjustedCost(event: CastEvent) {
+    let cost = this.getResource(event)?.cost ?? 0;
+    if (!cost) {
       return 0;
     }
-    let cost = this.getResource(event).cost;
     if (this.selectedCombatant.hasBuff(SPELLS.FIRES_OF_JUSTICE_BUFF.id)) {
       cost = cost - 1;
     }
