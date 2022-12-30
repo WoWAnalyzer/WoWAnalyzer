f278ab20366d14ea566870ec346eaec0684848c3
Jazminite <65823478+jazminite@users.noreply.github.com>
2022-12-30 16:03:58 -0500

    Update Buffs.tsx

--- src/analysis/classic/_templates/healer/modules/features/Buffs.tsx
+++ src/analysis/classic/_templates/healer/modules/features/Buffs.tsx
@@ -1,7 +1,7 @@
 import CoreAuras from 'parser/core/modules/Auras';
 import SPELLS from 'common/SPELLS/classic';
 import BLOODLUST_BUFFS from 'game/BLOODLUST_BUFFS';
-import ITEMS from 'game/classic/ITEM_BUFFS';
+import ITEM_BUFFS from 'game/classic/ITEM_BUFFS';
 
 class Buffs extends CoreAuras {
   // A list of Buffs (on the current player) to highlight on the Timeline
@@ -18,7 +18,7 @@ class Buffs extends CoreAuras {
         timelineHighlight: true,
       },
       {
-        spellId: Object.keys(ITEMS).map((item) => Number(item)),
+        spellId: Object.keys(ITEM_BUFFS).map((item) => Number(item)),
         timelineHighlight: true,
       },
     ];
