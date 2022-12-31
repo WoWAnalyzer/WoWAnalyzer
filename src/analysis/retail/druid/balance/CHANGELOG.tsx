e998e21ac2769807985ff2846f19be326b2eeafd
Travis Harris <travisharris@microsoft.com>
2022-12-31 14:18:34 -0600

    Update my contributor info and add changelog entry

--- src/analysis/retail/druid/balance/CHANGELOG.tsx
+++ src/analysis/retail/druid/balance/CHANGELOG.tsx
@@ -1,9 +1,10 @@
 import { change, date } from 'common/changelog';
 import { TALENTS_DRUID } from 'common/TALENTS';
-import { Sref, ToppleTheNun } from 'CONTRIBUTORS';
+import { Hartra344, Sref, ToppleTheNun } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2022, 12, 31), <>Add support for <SpellLink id={TALENTS_DRUID.WANING_TWILIGHT_TALENT} /> uptime and DPS contribution statistic</>, Hartra344 ),
   change(date(2022, 10, 17), <>Only check <SpellLink id={TALENTS_DRUID.INNERVATE_TALENT} /> efficiency if talented into it.</>, ToppleTheNun),
   change(date(2022, 10, 17), <>Add <SpellLink id={TALENTS_DRUID.CONVOKE_THE_SPIRITS_TALENT} /> to Abilities list.</>, ToppleTheNun),
   change(date(2022, 9, 16), <>Initial updates for Dragonflight Talent system.</>, Sref),
