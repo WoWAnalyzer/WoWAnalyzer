4745b1e416cd2fd7d52f211e050a2f6d9c8fec6e
Jazminite <jazminitehs@gmail.com>
2022-12-30 19:35:58 -0500

    Classic P1 Trinkets

--- src/common/ITEMS/classic/trinkets.ts
+++ src/common/ITEMS/classic/trinkets.ts
@@ -1,29 +1,143 @@
 import { itemIndexableList } from 'common/ITEMS/Item';
 
 const items = itemIndexableList({
-  // Dying Curse - https://www.wowhead.com/wotlk/item=40255
-  DYING_CURSE_BUFF: {
-    id: 60494,
+  // --------
+  // PHASE 1
+  // --------
+  DARKMOON_CARD_BERSERKER: {
+    id: 42989,
+    name: 'Darkmoon Card: Berserker!',
+    icon: 'inv_inscription_tarotberserker',
+    buffId: 60196,
+    buffName: 'Berserker!',
+  },
+  DARKMOON_CARD_DEATH: {
+    id: 42990,
+    name: 'Darkmoon Card: Death',
+    icon: 'inv_inscription_tarotdeath',
+    buffId: 60203,
+    buffName: 'Darkmoon Card: Death',
+  },
+  DARKMOON_CARD_GREATNESS_AGILITY: {
+    id: 44253,
+    name: 'Darkmoon Card: Greatness',
+    icon: 'inv_inscription_tarotgreatness',
+    buffId: 60233,
+    buffName: 'Greatness',
+  },
+  DARKMOON_CARD_GREATNESS_INTELLECT: {
+    id: 44255,
+    name: 'Darkmoon Card: Greatness',
+    icon: 'inv_inscription_tarotgreatness',
+    buffId: 60234,
+    buffName: 'Greatness',
+  },
+  DARKMOON_CARD_GREATNESS_SPIRIT: {
+    id: 44254,
+    name: 'Darkmoon Card: Greatness',
+    icon: 'inv_inscription_tarotgreatness',
+    buffId: 60235,
+    buffName: 'Greatness',
+  },
+  DARKMOON_CARD_GREATNESS_STRENGTH: {
+    id: 42987,
+    name: 'Darkmoon Card: Greatness',
+    icon: 'inv_inscription_tarotgreatness',
+    buffId: 60229,
+    buffName: 'Greatness',
+  },
+  DARKMOON_CARD_ILLUSION: {
+    id: 42988,
+    name: 'Darkmoon Card: Illusion',
+    icon: 'inv_inscription_tarotillusion',
+    buffId: 57350,
+    buffName: 'Illusionary Barrier',
+    spellId: 57350,
+  },
+  DYING_CURSE: {
+    id: 40255,
     name: 'Dying Curse',
-    icon: 'spell_holy_mindvision',
+    icon: 'inv_trinket_naxxramas03',
+    buffId: 60494,
+    buffName: 'Dying Curse',
   },
-  // Embrace of the Spider - https://www.wowhead.com/wotlk/item=39229
-  EMBRACE_SPIDER_BUFF: {
-    id: 60492,
+  EMBRACE_OF_THE_SPIDER: {
+    id: 39229,
     name: 'Embrace of the Spider',
-    icon: 'spell_holy_searinglight',
+    icon: 'inv_trinket_naxxramas04',
+    buffId: 60492,
+    buffName: 'Embrace of the Spider',
   },
-  // Illustration of the Dragon Soul - https://www.wowhead.com/wotlk/item=40432
-  ILLUSTRATION_DRAGON_SOUL_BUFF: {
-    id: 60486,
+  FORGE_EMBER: {
+    id: 37660,
+    name: 'Forge Ember',
+    icon: 'spell_fire_fire',
+    buffId: 60479,
+    buffName: 'Forge Ember',
+  },
+  GRIM_TOLL: {
+    id: 40256,
+    name: 'Grim Toll',
+    icon: 'inv_trinket_naxxramas04',
+    buffId: 60437,
+    buffName: 'Grim Toll',
+  },
+  ILLUSTRATION_OF_THE_DRAGON_SOUL: {
+    id: 40432,
     name: 'Illustration of the Dragon Soul',
     icon: 'inv_offhand_hyjal_d_01',
+    buffId: 60486,
+    buffName: 'Illustration of the Dragon Soul',
   },
-  // Sundial of the Exiled - https://www.wowhead.com/wotlk/item=40682
-  SUNDIAL_BUFF: {
-    id: 60064,
-    name: 'Now is the time!',
+  JETZES_BELL: {
+    id: 37835,
+    name: "Je'Tze's Bell",
+    icon: 'inv_misc_bell_01',
+    buffId: 49623,
+    buffName: 'Effervescence',
+  },
+  LOATHEBS_SHADOW: {
+    id: 39257,
+    name: "Loatheb's Shadow",
+    icon: 'inv_trinket_naxxramas03',
+    buffId: 60439,
+    buffName: "Loatheb's Shadow",
+    spellId: 60439,
+  },
+  MARK_OF_NORGANNON: {
+    id: 40531,
+    name: 'Mark of Norgannon',
     icon: 'ability_hunter_readiness',
+    buffId: 60319,
+    buffName: 'Mark of Norgannon',
+  },
+  MARK_OF_THE_WAR_PRISONER: {
+    id: 37873,
+    name: 'Mark of the War Prisoner',
+    icon: 'inv_jewelry_talisman_13',
+    buffId: 60480,
+    buffName: 'Mark of the War Prisoner',
+  },
+  METEORITE_WHETSTONE: {
+    id: 37390,
+    name: 'Meteorite Whetstone',
+    icon: 'inv_misc_stonetablet_02',
+    buffId: 60302,
+    buffName: 'Meteorite Whetstone',
+  },
+  MIRROR_OF_TRUTH: {
+    id: 40684,
+    name: 'Mirror of Truth',
+    icon: 'inv_jewelry_talisman_08',
+    buffId: 60065,
+    buffName: 'Reflection of Torment',
+  },
+  SUNDIAL_OF_THE_EXILED: {
+    id: 40682,
+    name: 'Sundial of the Exiled',
+    icon: 'ability_hunter_readiness',
+    buffId: 60064,
+    buffName: 'Now is the Time!',
   },
 });
 
