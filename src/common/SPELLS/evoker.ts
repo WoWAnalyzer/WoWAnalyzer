b61a43c5f3575a76ca13643bca17c3e14ce28ecd
trevorm4 <trevorm4@illinois.edu>
2023-01-04 16:59:06 -0800

    Fix bug with font of magic

--- src/common/SPELLS/evoker.ts
+++ src/common/SPELLS/evoker.ts
@@ -33,6 +33,11 @@ const spells = spellIndexableList({
     name: 'Dream Breath',
     icon: 'ability_evoker_dreambreath',
   },
+  DREAM_BREATH_FONT: {
+    id: 382614,
+    name: 'Dream Breath',
+    icon: 'ability_evoker_dreambreath',
+  },
   TWIN_GUARDIAN_SHIELD: {
     id: 370889,
     name: 'Twin Guardian',
@@ -50,6 +55,11 @@ const spells = spellIndexableList({
   },
   SPIRITBLOOM: {
     id: 367230,
+    name: 'Spiritbloom',
+    icon: 'ability_evoker_spiritbloom2',
+  },
+  SPIRITBLOOM_FONT: {
+    id: 382731,
     name: 'Spiritbloom',
     icon: 'ability_evoker_spiritbloom2',
   },
