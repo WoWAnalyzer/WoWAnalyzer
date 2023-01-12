431e31d28f9d5d5f89729576824ff6831a980a53
Richard Harrah <topplethenunnery@gmail.com>
2023-01-12 11:33:27 -0500

    fix changelog/config TS issues

--- src/analysis/retail/rogue/assassination/CONFIG.tsx
+++ src/analysis/retail/rogue/assassination/CONFIG.tsx
@@ -2,7 +2,7 @@ import Expansion from 'game/Expansion';
 import SPECS from 'game/SPECS';
 import { AlertWarning } from 'interface';
 
-// import CHANGELOG from './CHANGELOG';
+import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
@@ -38,7 +38,7 @@ export default {
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
   spec: SPECS.ASSASSINATION_ROGUE,
   // The contents of your changelog.
-  changelog: [],
+  changelog: CHANGELOG,
   // The CombatLogParser class for your spec.
   // parser: () =>
   //   import('./CombatLogParser' /* webpackChunkName: "AssassinationRogue" */).then(
