7dd63d0ac078df9518117d8ad7d13689ec1d9613
Toreole <toreole.official@gmail.com>
2023-01-17 22:43:49 +0100

    Fix RageTracker for Arms Warrior erroring on some logs
    The "Rage wasted" part of the analysis is still wrong as of now, but im working on it.

--- src/analysis/retail/warrior/arms/modules/features/RageTracker.tsx
+++ src/analysis/retail/warrior/arms/modules/features/RageTracker.tsx
@@ -1,8 +1,15 @@
 import SPELLS from 'common/SPELLS';
 import HIT_TYPES from 'game/HIT_TYPES';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
-import { SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events from 'parser/core/Events';
+import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, {
+  BeginChannelEvent,
+  CastEvent,
+  DamageEvent,
+  DrainEvent,
+  HealEvent,
+  ResourceChangeEvent,
+} from 'parser/core/Events';
 import ResourceTracker from 'parser/shared/modules/resources/resourcetracker/ResourceTracker';
 
 const RAGE_PER_MELEE_HIT = 25;
@@ -10,21 +17,26 @@ const RAGE_PER_MELEE_HIT = 25;
 class RageUsage extends ResourceTracker {
   lastMeleeTaken = 0;
 
-  constructor(...args) {
-    super(...args);
+  constructor(options: Options) {
+    super(options);
     this.resource = RESOURCE_TYPES.RAGE;
     this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MELEE), this.onDamage);
   }
 
-  getAdjustedCost(event) {
-    if (!this.getResource(event).cost) {
-      return 0;
-    }
-    const cost = this.getResource(event).cost / 10;
+  getAdjustedCost(
+    event:
+      | BeginChannelEvent
+      | CastEvent
+      | HealEvent
+      | DamageEvent
+      | ResourceChangeEvent
+      | DrainEvent,
+  ) {
+    const cost = (this.getResource(event)?.cost || 0) / 10;
     return cost;
   }
 
-  onDamage(event) {
+  onDamage(event: DamageEvent) {
     if (event.hitType === HIT_TYPES.CRIT) {
       this.processInvisibleEnergize(
         SPELLS.RAGE_AUTO_ATTACKS.id,
