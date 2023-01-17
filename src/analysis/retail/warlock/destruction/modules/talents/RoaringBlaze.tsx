b18d2fed34485ac73c7a52f4ddb76b285f7f26f6
Darya Talanina <dtalanina@gmail.com>
2023-01-17 21:26:34 +0100

    Add Roaring Blaze debuff uptime

--- src/analysis/retail/warlock/destruction/modules/talents/RoaringBlaze.tsx
+++ src/analysis/retail/warlock/destruction/modules/talents/RoaringBlaze.tsx
@@ -3,18 +3,22 @@ import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/warlock';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { DamageEvent } from 'parser/core/Events';
+import Enemies from 'parser/shared/modules/Enemies';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import UptimeIcon from 'interface/icons/Uptime';
 
 /*
   Roaring Blaze (Tier 90 Destruction talent):
     Conflagrate burns the target for an additional (48% of Spell power) Fire damage over 6 sec.
  */
 class RoaringBlaze extends Analyzer {
-  get dps() {
-    return (this.damage / this.owner.fightDuration) * 1000;
-  }
+  static dependencies = {
+    enemies: Enemies,
+  };
+
+  protected enemies!: Enemies;
 
   damage = 0;
 
@@ -31,11 +35,18 @@ class RoaringBlaze extends Analyzer {
     this.damage += (event.amount || 0) + (event.absorbed || 0);
   }
 
+  get dps() {
+    return (this.damage / this.owner.fightDuration) * 1000;
+  }
+
+  get uptime() {
+    return this.enemies.getBuffUptime(SPELLS.ROARING_BLAZE_DAMAGE.id) / this.owner.fightDuration;
+  }
+
   statistic() {
     return (
       <Statistic
         category={STATISTIC_CATEGORY.TALENTS}
-        size="small"
         tooltip={`${formatThousands(this.damage)} damage`}
       >
         <BoringSpellValueText spellId={TALENTS.ROARING_BLAZE_TALENT.id}>
@@ -43,6 +54,9 @@ class RoaringBlaze extends Analyzer {
           <small>
             {formatPercentage(this.owner.getPercentageOfTotalDamageDone(this.damage))} % of total
           </small>
+          <br />
+          <UptimeIcon />
+          {formatPercentage(this.uptime, 0)}%<small> uptime</small>
         </BoringSpellValueText>
       </Statistic>
     );
