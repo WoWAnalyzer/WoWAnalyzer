4b611daecf8016e6f200ebfefd8206c7dcb2e4c4
Darya Talanina <dtalanina@gmail.com>
2023-01-17 18:48:21 +0100

    Display % of buffed chaos bolts

--- src/analysis/retail/warlock/destruction/modules/talents/MadnessOfAzhaqir.tsx
+++ src/analysis/retail/warlock/destruction/modules/talents/MadnessOfAzhaqir.tsx
@@ -1,8 +1,10 @@
 import { formatPercentage } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/warlock';
+import { TooltipElement } from 'interface';
 import UptimeIcon from 'interface/icons/Uptime';
-import Analyzer, { Options } from 'parser/core/Analyzer';
+import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events from 'parser/core/Events';
 import Statistic from 'parser/ui/Statistic';
 import { STATISTIC_ORDER } from 'parser/ui/StatisticBox';
 import TalentSpellText from 'parser/ui/TalentSpellText';
@@ -11,14 +13,42 @@ class MadnessOfAzhaqir extends Analyzer {
   static talent = TALENTS.MADNESS_OF_THE_AZJAQIR_TALENT;
   static buffId = SPELLS.MADNESS_OF_AZHAQIR_BUFF.id;
 
+  buffedCasts: number = 0;
+  totalCasts: number = 0;
+
   constructor(options: Options) {
     super(options);
 
     this.active = this.selectedCombatant.hasTalent(MadnessOfAzhaqir.talent);
+
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.CHAOS_BOLT),
+      this.onChaosBoltCast,
+    );
+  }
+
+  onChaosBoltCast() {
+    this.totalCasts += 1;
+
+    if (this.selectedCombatant.hasBuff(MadnessOfAzhaqir.buffId)) {
+      this.buffedCasts += 1;
+    }
   }
 
   get uptime() {
     return this.selectedCombatant.getBuffUptime(MadnessOfAzhaqir.buffId) / this.owner.fightDuration;
+  }
+
+  get buffedChaosBoltCasts() {
+    return this.buffedCasts;
+  }
+
+  get totalChaosBoltCasts() {
+    return this.totalCasts;
+  }
+
+  get buffedCastsPercentage() {
+    return this.buffedCasts / this.totalCasts;
   }
 
   statistic() {
@@ -26,6 +56,11 @@ class MadnessOfAzhaqir extends Analyzer {
       <Statistic position={STATISTIC_ORDER.CORE()}>
         <TalentSpellText talent={MadnessOfAzhaqir.talent}>
           <UptimeIcon /> {formatPercentage(this.uptime)}% <small>uptime</small>
+          <br />
+          {formatPercentage(this.buffedCastsPercentage, 0)}%{' '}
+          <TooltipElement content={`${this.buffedCasts} / ${this.totalCasts}`}>
+            <small>buffed Chaos Bolt casts</small>
+          </TooltipElement>
         </TalentSpellText>
       </Statistic>
     );
