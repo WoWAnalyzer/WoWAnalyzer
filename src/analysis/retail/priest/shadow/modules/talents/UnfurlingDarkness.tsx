a85af093784a8255f08bdb559cf5a04ea89e5e13
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2023-01-18 17:09:14 -0500

    Fix wrong spelllink in Unfurling Darkness

--- src/analysis/retail/priest/shadow/modules/talents/UnfurlingDarkness.tsx
+++ src/analysis/retail/priest/shadow/modules/talents/UnfurlingDarkness.tsx
@@ -114,7 +114,7 @@ class UnfurlingDarkness extends Analyzer {
     const explanation = (
       <p>
         <b>
-          <SpellLink id={TALENTS.SHADOWY_INSIGHT_TALENT.id} />
+          <SpellLink id={TALENTS.UNFURLING_DARKNESS_TALENT.id} />
         </b>{' '}
         is gained by casting <SpellLink id={SPELLS.VAMPIRIC_TOUCH.id} />.<br />
         Cast <SpellLink id={SPELLS.VAMPIRIC_TOUCH.id} /> while the buff is active to avoid wasting
