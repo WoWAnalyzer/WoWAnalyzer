9b034069281f2084c06f55961b7a3bedc68d65ff
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2023-01-18 17:07:51 -0500

    Fix tracking of Mind Blast spell usable

--- src/analysis/retail/priest/shadow/modules/features/ShadowyInsight.tsx
+++ src/analysis/retail/priest/shadow/modules/features/ShadowyInsight.tsx
@@ -50,6 +50,7 @@ class ShadowyInsight extends Analyzer {
 
   onBuffApplied(event: ApplyBuffEvent) {
     this.abilities.increaseMaxCharges(event, SPELLS.MIND_BLAST.id, 1);
+    this.spellUsable.endCooldown(SPELLS.MIND_BLAST.id, event.timestamp, false, true);
 
     this.procsGained += 1; // Add a proc to the counter
   }
