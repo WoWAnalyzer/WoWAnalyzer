10ddfbb70ba2323ecd20eca8b631a8904d30fdaa
Kelton Finch <keltonfinch@gmail.com>
2023-01-21 14:24:48 -0500

    Druid Resto - T29 4pc (#5689)
    * First draft of resto t29 4pc module
    
    * Finishing touches to Druid Resto 4pc statistic

--- src/analysis/retail/druid/restoration/CHANGELOG.tsx
+++ src/analysis/retail/druid/restoration/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { TALENTS_DRUID } from 'common/TALENTS';
 import SPELLS from 'common/SPELLS';
 
 export default [
+  change(date(2023, 1, 21), <>Added statistic for Vault of the Incarnates 4 Piece. </>, Sref),
   change(date(2022, 12, 30), <>Improve accuracy of 2 piece module </>, Trevor),
   change(date(2022, 12, 29), <>Added statistic for Vault of the Incarnates 2 Piece. </>, Phased),
   change(date(2022, 12, 14), <>Added Preparation Section to Guide.</>, Sref),
