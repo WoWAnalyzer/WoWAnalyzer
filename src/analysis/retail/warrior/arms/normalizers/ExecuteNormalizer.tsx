3d91a606a323d8861c511f40a44e6006ebecdd4e
Toreole <toreole.official@gmail.com>
2023-01-26 22:54:22 +0100

    Added missing changelog entry for normalizer

--- src/analysis/retail/warrior/arms/normalizers/ExecuteNormalizer.tsx
+++ src/analysis/retail/warrior/arms/normalizers/ExecuteNormalizer.tsx
@@ -4,17 +4,13 @@ import { CastEvent, DamageEvent, EventType } from 'parser/core/Events';
 import { Options } from 'parser/core/Analyzer';
 
 const relation = 'execute_dmg';
+const executeCastDmgDelayBuffer = 500;
+
 const EXECUTE_LINK: EventLink = {
   linkRelation: relation,
   linkingEventId: [SPELLS.EXECUTE_GLYPHED.id, SPELLS.EXECUTE.id],
   linkingEventType: EventType.Cast,
-  forwardBufferMs: 500,
-  //seems quite large buffer, but its a long animation, and the combatlog is unreliable.
-  //a few examples for the timestamps:
-  //Cast3585726:Damage3586071 (~350ms)
-  //Cast3889203:Damage3889540 (~340ms)
-  //Cast3900203:Damage3900536 (~330ms)
-  //assuming it varies with haste and server lag at the time.
+  forwardBufferMs: executeCastDmgDelayBuffer,
 
   referencedEventId: [SPELLS.EXECUTE_DAMAGE.id, SPELLS.EXECUTE_GLYPHED.id],
   referencedEventType: EventType.Damage,
