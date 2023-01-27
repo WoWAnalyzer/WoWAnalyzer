afb4ed7fd89ce042c2c4f79cb2b0536af41bf3fb
Richard Harrah <topplethenunnery@gmail.com>
2023-01-26 19:09:40 -0500

    add Animacharged finisher tracking

--- src/analysis/retail/rogue/assassination/CHANGELOG.tsx
+++ src/analysis/retail/rogue/assassination/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { ToppleTheNun } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2023, 1, 26), 'Add support for Animacharged CPs and low CP finishers in opener.', ToppleTheNun),
   change(date(2023, 1, 24), 'Update for Dragonflight.', ToppleTheNun),
 ];
--- src/analysis/retail/rogue/assassination/modules/core/FinisherUse.tsx
+++ src/analysis/retail/rogue/assassination/modules/core/FinisherUse.tsx
@@ -1,17 +1,25 @@
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { CastEvent } from 'parser/core/Events';
-import { BadColor, GoodColor } from 'interface/guide';
-import { ResourceLink } from 'interface';
+import { BadColor, GoodColor, OkColor } from 'interface/guide';
+import { ResourceLink, SpellLink } from 'interface';
 import DonutChart from 'parser/ui/DonutChart';
 import Statistic from 'parser/ui/Statistic';
 import { STATISTIC_ORDER } from 'parser/ui/StatisticBox';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 import getResourceSpent from 'parser/core/getResourceSpent';
+import SPELLS from 'common/SPELLS/rogue';
+import TALENTS from 'common/TALENTS/rogue';
+
 import { FINISHERS, getMaxComboPoints } from '../../constants';
+import { formatDurationMillisMinSec } from 'common/format';
+
+const OPENER_MAX_DURATION_MS = 15000;
 
 export default class FinisherUse extends Analyzer {
   totalFinisherCasts = 0;
+  animachargedCasts = 0;
   lowCpFinisherCasts = 0;
+  openerLowCpFinisherCasts = 0;
 
   constructor(options: Options) {
     super(options);
@@ -19,7 +27,12 @@ export default class FinisherUse extends Analyzer {
   }
 
   get maxCpFinishers() {
-    return this.totalFinisherCasts - this.lowCpFinisherCasts;
+    return (
+      this.totalFinisherCasts -
+      this.animachargedCasts -
+      this.lowCpFinisherCasts -
+      this.openerLowCpFinisherCasts
+    );
   }
 
   get chart() {
@@ -33,11 +46,36 @@ export default class FinisherUse extends Analyzer {
         ),
       },
       {
+        color: OkColor,
+        label: 'Low CP Opener Finishers',
+        value: this.openerLowCpFinisherCasts,
+        tooltip: (
+          <>
+            This includes low CP finisher casts in the first{' '}
+            {formatDurationMillisMinSec(OPENER_MAX_DURATION_MS)} of an encounter.
+          </>
+        ),
+      },
+      {
         color: BadColor,
         label: 'Low CP Finishers',
         value: this.lowCpFinisherCasts,
       },
     ];
+
+    if (this.selectedCombatant.hasTalent(TALENTS.ECHOING_REPRIMAND_TALENT)) {
+      items.push({
+        color: '#40DDF9',
+        label: 'Animacharged Finishers',
+        value: this.animachargedCasts,
+        tooltip: (
+          <>
+            This includes finishers cast using an Animacharged CP from{' '}
+            <SpellLink id={TALENTS.ECHOING_REPRIMAND_TALENT} />.
+          </>
+        ),
+      });
+    }
 
     return <DonutChart items={items} />;
   }
@@ -60,9 +98,35 @@ export default class FinisherUse extends Analyzer {
     if (cpsSpent === 0) {
       return;
     }
+
+    const hasAnimacharged2CP = this.selectedCombatant.hasBuff(
+      SPELLS.ANIMACHARGED_CP2.id,
+      event.timestamp,
+    );
+    const hasAnimacharged3CP = this.selectedCombatant.hasBuff(
+      SPELLS.ANIMACHARGED_CP3.id,
+      event.timestamp,
+    );
+    const hasAnimacharged4CP = this.selectedCombatant.hasBuff(
+      SPELLS.ANIMACHARGED_CP4.id,
+      event.timestamp,
+    );
+    const timeIntoEncounter = event.timestamp - this.owner.fight.start_time;
+    const isInOpener = timeIntoEncounter <= OPENER_MAX_DURATION_MS;
+
     this.totalFinisherCasts += 1;
-    if (cpsSpent < getMaxComboPoints(this.selectedCombatant) - 1) {
-      this.lowCpFinisherCasts += 1;
+    if (hasAnimacharged2CP && cpsSpent === 2) {
+      this.animachargedCasts += 1;
+    } else if (hasAnimacharged3CP && cpsSpent === 3) {
+      this.animachargedCasts += 1;
+    } else if (hasAnimacharged4CP && cpsSpent === 4) {
+      this.animachargedCasts += 1;
+    } else if (cpsSpent < getMaxComboPoints(this.selectedCombatant) - 1) {
+      if (isInOpener) {
+        this.openerLowCpFinisherCasts += 1;
+      } else {
+        this.lowCpFinisherCasts += 1;
+      }
     }
   }
 }
