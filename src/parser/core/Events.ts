2a3753143fa9de74c8f4e10dd970461443ddddf0
Putro <laustsf@gmail.com>
2023-01-30 15:39:18 +0100

    Use optional chaining in EventLinkNormalizer related functions

--- src/parser/core/Events.ts
+++ src/parser/core/Events.ts
@@ -186,9 +186,8 @@ type MappedEventTypes = {
   [EventType.FilterBuffInfo]: FilterBuffInfoEvent;
 };
 
-export type AnyEvent<
-  ET extends keyof MappedEventTypes = keyof MappedEventTypes
-> = MappedEventTypes[ET];
+export type AnyEvent<ET extends keyof MappedEventTypes = keyof MappedEventTypes> =
+  MappedEventTypes[ET];
 
 export interface Ability {
   /** The ability's name */
@@ -287,24 +286,19 @@ export function HasLocation<T extends EventType>(event: Event<T>): event is Loca
 /** Gets the events related to the given event with the given relation (key). Events will not
  *  by default have any relations, you must add them with an {@link EventLinkNormalizer}. */
 export function GetRelatedEvents(event: AnyEvent, relation: string): AnyEvent[] {
-  return event._linkedEvents === undefined
-    ? []
-    : event._linkedEvents.filter((le) => le.relation === relation).map((le) => le.event);
+  return event?._linkedEvents?.filter((le) => le.relation === relation).map((le) => le.event) ?? [];
 }
 
 /** Returns true iff the given event has a relation with the given relation (key). Events will not
  *  by default have any relations, you must add them with an {@link EventLinkNormalizer}. */
 export function HasRelatedEvent(event: AnyEvent, relation: string): boolean {
-  return (
-    event._linkedEvents !== undefined &&
-    event._linkedEvents.find((le) => le.relation === relation) !== undefined
-  );
+  return event?._linkedEvents?.find((le) => le.relation === relation) !== undefined;
 }
 
 /** Adds a relation between events using the `_linkedEvents` field.
  *  This should not be done manually, use {@link EventLinkNormalizer} */
 export function AddRelatedEvent(event: AnyEvent, relation: string, relatedEvent: AnyEvent): void {
-  if (event._linkedEvents === undefined) {
+  if (!event?._linkedEvents) {
     event._linkedEvents = [];
   }
   event._linkedEvents.push({ relation, event: relatedEvent });
