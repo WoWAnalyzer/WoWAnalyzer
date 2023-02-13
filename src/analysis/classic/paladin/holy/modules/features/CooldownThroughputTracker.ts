02fba3edff05957b3c7fade59b5797af58973f4b
Jazminite <jazminitehs@gmail.com>
2023-02-13 13:18:38 -0500

    small updates

--- src/analysis/classic/paladin/holy/modules/features/CooldownThroughputTracker.ts
+++ src/analysis/classic/paladin/holy/modules/features/CooldownThroughputTracker.ts
@@ -6,7 +6,6 @@ import CoreCooldownThroughputTracker, {
 class CooldownThroughputTracker extends CoreCooldownThroughputTracker {
   static castCooldowns = [
     ...CoreCooldownThroughputTracker.castCooldowns,
-    // Add Cooldown Spells specific to Spec
     {
       spell: SPELLS.INNERVATE.id,
       summary: [BUILT_IN_SUMMARY_TYPES.MANA],
