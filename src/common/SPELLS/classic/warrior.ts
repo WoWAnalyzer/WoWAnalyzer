bd226f4239bd942794f27cc1690f222c74d54246
AcidBomb <47693252+Ac1dBomb@users.noreply.github.com>
2023-02-15 04:23:36 -0500

    Add files via upload
    Added all spells

--- src/common/SPELLS/classic/warrior.ts
+++ src/common/SPELLS/classic/warrior.ts
@@ -1,10 +1,264 @@
 import { spellIndexableList } from '../Spell';
 
 const spells = spellIndexableList({
-  VIGILANCE: {
+  // --------
+  // SHARED
+  // --------
+ Battle_Shout: {
+    id: 47436,
+    name: 'Battle Shout (Rank 9)',
+    icon: 'ability_warrior_battleshout', 
+   },
+Battle_Stance: {
+    id: 2457,
+    name: 'Battle Stance',
+    icon: 'ability_warrior_offensivestance', 
+   },
+Berserker_Rage: {
+    id: 18499,
+    name: 'Berserker Rage',
+    icon: 'spell_nature_ancestralguardian', 
+   },
+Berserker_Stance: {
+    id: 2458,
+    name: 'Berserker Stance',
+    icon: 'ability_racial_avatar', 
+   },
+Blood_rage: {
+    id: 2687,
+    name: 'Blood Rage',
+    icon: 'ability_racial_bloodrage', 
+   },
+Challenging_Shout: {
+    id: 1161,
+    name: 'Challenging Shout',
+    icon: 'ability_bullrush', 
+   },
+Charge: {
+    id: 11578,
+    name: 'Charge',
+    icon: 'ability_warrior_charge', 
+   },
+Cleave: {
+    id: 47520,
+    name: 'Cleave',
+    icon: 'ability_warrior_cleave', 
+   },
+Commanding_Shout: {
+    id: 47440,
+    name: 'Commanding Shout',
+    icon: 'ability_warrior_rallyingcry', 
+   },
+Defensive_Stance: {
+    id: 71,
+    name: 'Defensive Stance',
+    icon: 'ability_warrior_defensivestance', 
+   },
+Demoralizing_Shout: {
+    id: 47437,
+    name: 'Demoralizing Shout',
+    icon: 'ability_warrior_warcry', 
+   },
+Disarm: {
+    id: 676,
+    name: 'Disarm',
+    icon: 'ability_warrior_disarm', 
+   },
+Enraged_Regeneration: {
+    id: 55694,
+    name: 'Enraged Regeneration',
+    icon: 'ability_warrior_focusedrage', 
+   },
+Execute: {
+    id: 47471,
+    name: 'Execute',
+    icon: 'inv_sword_48', 
+   },
+Hamstring: {
+    id: 1715,
+    name: 'Hamstring',
+    icon: 'ability_shockwave', 
+   },
+Heroic_Strike: {
+    id: 47450,
+    name: 'Heroic Strike',
+    icon: 'ability_rogue_ambush', 
+   },
+Heroic_Throw: {
+    id: 57550,
+    name: 'Heroic Throw',
+    icon: 'inv_axe_66', 
+   },
+Intercept: {
+    id: 20252,
+    name: 'Intercept',
+    icon: 'ability_rogue_sprint', 
+   },
+Intervene: {
+    id: 3411,
+    name: 'Intervene',
+    icon: 'ability_warrior_victoryrush', 
+   },
+Intimidating_Shout: {
+    id: 5246,
+    name: 'Intimidating Shout',
+    icon: 'ability_golemthunderclap', 
+   },
+Mocking_Blow: {
+    id: 694,
+    name: 'Mocking Blow',
+    icon: 'ability_warrior_punishingblow', 
+   },
+Overpower: {
+    id: 7384,
+    name: 'Overpower',
+    icon: 'ability_meleedamage', 
+   },
+Pummel: {
+    id: 6552,
+    name: 'Pummel',
+    icon: 'inv_gauntlets_04', 
+   },
+Recklessness: {
+    id: 1719,
+    name: 'Recklessness',
+    icon: 'ability_criticalstrike', 
+   }, 
+Rend: {
+    id: 47465,
+    name: 'Rend',
+    icon: 'ability_gouge', 
+   },
+Retaliation: {
+    id: 20230,
+    name: 'Retaliation',
+    icon: 'ability_warrior_challange', 
+   },
+Revenge: {
+    id: 57823,
+    name: 'Revenge',
+    icon: 'ability_warrior_revenge', 
+   },
+Shattering_Throw: {
+    id: 64382,
+    name: 'Shattering Throw',
+    icon: 'ability_warrior_shatteringthrow', 
+   },
+Shield_Bash: {
+    id: 72,
+    name: 'Shield Bash',
+    icon: 'ability_warrior_shieldbash', 
+   },
+Shield_Block: {
+    id: 2565,
+    name: 'Shield Block',
+    icon: 'ability_defend', 
+   },
+Shield_Slam: {
+    id: 47488,
+    name: 'Shield Slam',
+    icon: 'inv_shield_05', 
+   },
+Shield_Wall: {
+    id: 871,
+    name: 'Shield Wall',
+    icon: 'ability_warrior_shieldwall',
+   },
+Slam: {
+    id: 47475,
+    name: 'Slam',
+    icon: 'ability_warrior_decisivestrike', 
+   },
+Spell_Reflection: {
+    id: 23920,
+    name: 'Spell Reflection',
+    icon: 'ability_warrior_shieldreflection', 
+   },
+Sunder_Armor: {
+    id: 7386,
+    name: 'Sunder Armor',
+    icon: 'ability_warrior_sunder', 
+   },
+Taunt: {
+    id: 355,
+    name: 'Taunt',
+    icon: 'spell_nature_reincarnation', 
+   },
+Thunder_Clap: {
+    id: 47502,
+    name: 'Thunder Clap',
+    icon: 'spell_nature_thunderclap', 
+   },
+Victory_Rush: {
+    id: 34428,
+    name: 'Victory Rush',
+    icon: 'ability_warrior_devastate', 
+   },
+Whirlwind: {
+    id: 1680,
+    name: 'Whirlwind',
+    icon: 'ability_whirlwind', 
+  },
+  //Talents  
+SHOCKWAVE: {
+    id: 46968,
+    name: 'Shockwave',
+    icon: 'ability_warrior_shockwave',
+  }, 
+LAST_STAND: {
+    id: 12975,
+    name: 'Last Stand',
+    icon: 'spell_holy_ashestoashes',
+  },
+VIGILANCE: {
     id: 50720,
     name: 'Vigilance',
     icon: 'ability_warrior_vigilance',
+  },
+BLADESTORM: {
+    id: 46924,
+    name: 'Bladestorm',
+    icon: 'ability_warrior_bladestorm',
+  },
+MORTAL_STRIKE: {
+    id: 47486,
+    name: 'Mortal Strike',
+    icon: 'ability_warrior_savageblow',
+  },
+BLOODTHIRST: {
+    id: 23881,
+    name: 'Bloodthirst',
+    icon: 'spell_nature_bloodlust',
+  },
+DEATHWISH: {
+    id: 12292,
+    name: 'Deathwish',
+    icon: 'spell_shadow_deathpact',
+  },
+SWEEPING_STRIKES: {
+    id: 12328,
+    name: 'Sweeping Strikes',
+    icon: 'ability_rogue_slicedice',
+  },
+DEVASTATE: {
+    id: 47498,
+    name: 'Devastate',
+    icon: 'inv_sword_11',
+  },
+PIERCING_HOWL: {
+    id: 12323,
+    name: 'Piercing Howl',
+    icon: 'spell_shadow_deathscream',
+  },
+HEROIC_FURY: {
+    id: 60970,
+    name: 'Heroic Fury',
+    icon: 'ability_heroicleap',
+  },
+CONCUSSION_BLOW: {
+    id: 12809,
+    name: 'Concussion Blow',
+    icon: 'ability_thunderbolt',
   },
 });
 
