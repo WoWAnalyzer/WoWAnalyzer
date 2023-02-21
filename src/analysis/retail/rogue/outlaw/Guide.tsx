4d7d57b0ca80d54888700be1dd6431089fd0d940
Zac <zak.darmoise1@gmail.com>
2023-02-21 07:48:53 +0100

    [OUTLAW] Guide add resources energy section

--- src/analysis/retail/rogue/outlaw/Guide.tsx
+++ src/analysis/retail/rogue/outlaw/Guide.tsx
@@ -1,21 +1,63 @@
-import { GuideProps } from 'interface/guide';
+import { GuideProps, Section, SubSection } from 'interface/guide';
 import PreparationSection from 'interface/guide/components/Preparation/PreparationSection';
-// import { t, Trans } from '@lingui/macro';
-// import EnergyCapWaste from 'analysis/retail/rogue/shared/guide/EnergyCapWaste';
-// import TALENTS from 'common/TALENTS/rogue';
-// import HideExplanationsToggle from 'interface/guide/components/HideExplanationsToggle';
-// import { ResourceLink, SpellLink } from 'interface';
-// import SPELLS from 'common/SPELLS';
-// import { RoundedPanel, SideBySidePanels } from 'interface/guide/components/GuideDivs';
-// import CooldownUsage from 'parser/core/MajorCooldowns/CooldownUsage';
-// import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
+import { t, Trans } from '@lingui/macro';
+import EnergyCapWaste from 'analysis/retail/rogue/shared/guide/EnergyCapWaste';
+//import TALENTS from 'common/TALENTS/rogue';
+//import HideExplanationsToggle from 'interface/guide/components/HideExplanationsToggle';
+import { ResourceLink } from 'interface';
+//import SPELLS from 'common/SPELLS';
+//import { RoundedPanel, SideBySidePanels } from 'interface/guide/components/GuideDivs';
+//import CooldownUsage from 'parser/core/MajorCooldowns/CooldownUsage';
+import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 
 import CombatLogParser from './CombatLogParser';
 
 export default function Guide({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <>
+      <ResourceUsageSection modules={modules} events={events} info={info} />
       <PreparationSection />
     </>
+  );
+}
+
+function ResourceUsageSection({ modules }: GuideProps<typeof CombatLogParser>) {
+  const percentAtCap = modules.energyTracker.percentAtCap;
+  const energyWasted = modules.energyTracker.wasted;
+
+  return (
+    <Section
+      title={t({
+        id: 'guide.rogue.outlaw.sections.resources.title',
+        message: 'Resource Use',
+      })}
+    >
+      <SubSection
+        title={t({
+          id: 'guide.rogue.outlaw.sections.resources.energy.title',
+          message: 'Energy',
+        })}
+      >
+        <p>
+          <Trans id="guide.rogue.outlaw.sections.resources.energy.summary">
+            Your primary resource is <ResourceLink id={RESOURCE_TYPES.ENERGY.id} />. Typically,
+            ability use will be limited by <ResourceLink id={RESOURCE_TYPES.ENERGY.id} />, not time.
+            Avoid capping <ResourceLink id={RESOURCE_TYPES.ENERGY.id} /> - lost{' '}
+            <ResourceLink id={RESOURCE_TYPES.ENERGY.id} /> regeneration is lost DPS. It will
+            occasionally be impossible to avoid capping{' '}
+            <ResourceLink id={RESOURCE_TYPES.ENERGY.id} /> - like while handling mechanics or during
+            intermission phases.
+          </Trans>
+        </p>
+        <EnergyCapWaste
+          percentAtCap={percentAtCap}
+          perfectTimeAtCap={0.05}
+          goodTimeAtCap={0.1}
+          okTimeAtCap={0.15}
+          wasted={energyWasted}
+        />
+        {modules.energyGraph.plot}
+      </SubSection>
+    </Section>
   );
 }
