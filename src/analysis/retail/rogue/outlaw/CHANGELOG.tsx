33a8c0cb27c78c6000cd0f7fcacfc39243d07c9f
zac <zak.darmoise1@gmail.com>
2023-02-22 15:47:28 +0100

    Update changelog

--- src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
+++ src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
@@ -2,8 +2,10 @@ import { change, date } from 'common/changelog';
 import { Anty, zac } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 import TALENTS from 'common/TALENTS/rogue';
+import SPELLS from 'common/SPELLS';
 
 export default [
+  change(date(2023, 2, 22), <>Add Fan the hammer normalizer to ignore subsequents <SpellLink id={SPELLS.PISTOL_SHOT}/> fake casts events.</>, zac),
   change(date(2023, 2, 20), <>First implementation of <SpellLink id={TALENTS.AUDACITY_TALENT} />.</>, zac),
   change(date(2022,11, 3), <>Enabling Spec for Dragonflight.</>, Anty),
 ];
