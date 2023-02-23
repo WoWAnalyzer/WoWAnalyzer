08ae2320aac1b22b6f084a598aae5a235dc0dd04
Arbixal <arbixal@hotmail.com>
2023-02-23 11:54:54 +1100

    Forgot the changelog entry.

--- src/analysis/classic/druid/restoration/CHANGELOG.tsx
+++ src/analysis/classic/druid/restoration/CHANGELOG.tsx
@@ -2,6 +2,7 @@ import { change, date } from 'common/changelog';
 import { jazminite, Arbixal } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2023, 2, 23), 'Added better guidance around the use of Swiftmend.', Arbixal),
   change(date(2023, 2, 6), 'Add clearcasting analyzer, relaxed Wild Growth categorization, cleaned up spell definitions', Arbixal),
   change(date(2023, 1, 25), 'Add Revitalise, Rejuvenation, Wild Growth, Lifebloom, and Swiftmend Analyzers', Arbixal),
   change(date(2023, 1, 10), 'Add Classic Restoration Druid stub.', jazminite),
