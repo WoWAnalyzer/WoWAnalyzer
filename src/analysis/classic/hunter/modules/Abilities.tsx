416f0a00100b80a03b9da19c2e469d5a5c21a803
bdfreeman1421 <bfreeman@optonline.net>
2023-02-23 06:01:27 -0500

    Initial Classic Survial Hunter spells

--- src/analysis/classic/hunter/modules/Abilities.tsx
+++ src/analysis/classic/hunter/modules/Abilities.tsx
@@ -1,4 +1,5 @@
 import SPELLS from 'common/SPELLS/classic/hunter';
+import RACIALS from 'common/SPELLS/classic/racials';
 import CoreAbilities from 'parser/core/modules/Abilities';
 import SPELL_CATEGORY from 'parser/core/SPELL_CATEGORY';
 
@@ -11,9 +12,75 @@ class Abilities extends CoreAbilities {
         gcd: null,
       },
       {
-        spell: [SPELLS.ARCANE_SHOT.id, ...SPELLS.ARCANE_SHOT.lowRanks],
+        spell: [SPELLS.SERPENT_STING.id, ...SPELLS.SERPENT_STING.lowRanks],
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: { base: 1500 },
+      },
+      // Survival
+      {
+        spell: [SPELLS.EXPLOSIVE_SHOT.lowRanks[0]],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+      },
+      {
+        spell: [SPELLS.EXPLOSIVE_SHOT.id, ...SPELLS.EXPLOSIVE_SHOT.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+      },
+      {
+        spell: [SPELLS.BLACK_ARROW.id, ...SPELLS.BLACK_ARROW.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+        cooldown: 30,
+      },
+      {
+        spell: [SPELLS.EXPLOSIVE_TRAP.id, ...SPELLS.EXPLOSIVE_TRAP.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+      },
+      // TODO only applies in Execute Phase 20%
+      {
+        spell: [SPELLS.KILL_SHOT.id, ...SPELLS.KILL_SHOT.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+        cooldown: 15,
+      },
+      {
+        spell: [SPELLS.AIMED_SHOT.id, ...SPELLS.AIMED_SHOT.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+        cooldown: 10,
+      },
+      {
+        spell: [SPELLS.MULTI_SHOT.id, ...SPELLS.MULTI_SHOT.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+        cooldown: 10,
+      },
+      {
+        spell: [SPELLS.STEADY_SHOT.id, ...SPELLS.STEADY_SHOT.lowRanks],
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: { base: 1500 },
+      },
+      {
+        spell: [SPELLS.RAPID_FIRE.id],
+        category: SPELL_CATEGORY.COOLDOWNS,
+        gcd: { base: 1500 },
+        cooldown: 300,
+      },
+      {
+        spell: RACIALS.BERSERKING.id,
+        buffSpellId: RACIALS.BERSERKING.id,
+        category: SPELL_CATEGORY.COOLDOWNS,
+        cooldown: 180,
+        isUndetectable: true,
+        gcd: null,
+        castEfficiency: {
+          suggestion: true,
+          majorIssueEfficiency: 0.2,
+          averageIssueEfficiency: 0.5,
+          recommendedEfficiency: 0.7,
+        },
       },
     ];
   }
--- src/analysis/classic/hunter/modules/AlwaysBeCasting.ts
+++ src/analysis/classic/hunter/modules/AlwaysBeCasting.ts
@@ -6,6 +6,7 @@ import SPELLS from 'common/SPELLS/classic/hunter';
 class AlwaysBeCasting extends BaseAlwaysBeCasting {
   onEndChannel(event: EndChannelEvent) {
     if (event.ability.guid === SPELLS.AUTO_SHOT.id) {
+      // TODO WOTLK auto shot does not clip
       // Auto Shot cast time can overlap with the GCD (this is ideal play), when this happens we can't include both in the active time.
       // Just the GCD is active time.
       // Just the Auto Shot cast time is active time (you can't cast a SS during it).
