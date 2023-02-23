9976a24feee25e1931b1612417d2fb2e7dd445a7
zac <zak.darmoise1@gmail.com>
2023-02-24 00:41:45 +0100

    [OUTLAW] Update changelog

--- src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
+++ src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import SpellLink from 'interface/SpellLink';
 import TALENTS from 'common/TALENTS/rogue';
 
 export default [
+  change(date(2023, 2, 24), <>Various fixes.</>, zac),
   change(date(2023, 2, 20), <>First implementation of <SpellLink id={TALENTS.AUDACITY_TALENT} />.</>, zac),
   change(date(2022,11, 3), <>Enabling Spec for Dragonflight.</>, Anty),
 ];
