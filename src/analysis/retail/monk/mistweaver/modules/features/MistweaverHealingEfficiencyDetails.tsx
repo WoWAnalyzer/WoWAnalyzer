5f6110790d5bf6344549a2942c29308ef3241024
vohrr <matthew.d.pingel@gmail.com>
2023-03-01 18:15:47 -0500

    talent wrapping & shaohaos lessons line

--- src/analysis/retail/monk/mistweaver/modules/features/MistweaverHealingEfficiencyDetails.tsx
+++ src/analysis/retail/monk/mistweaver/modules/features/MistweaverHealingEfficiencyDetails.tsx
@@ -25,19 +25,37 @@ class MistweaverHealingEfficiencyDetails extends HealingEfficiencyDetails {
             given the healing from duplicated gusts, since without{' '}
             <SpellLink id={TALENTS_MONK.ESSENCE_FONT_TALENT.id} /> the second gust would not have
             happened. <br />
-            <SpellLink id={TALENTS_MONK.ESSENCE_FONT_TALENT.id} /> is given the healing of{' '}
-            <SpellLink id={TALENTS_MONK.ANCIENT_TEACHINGS_TALENT.id} />. <br />
+            {this.selectedCombatant.hasTalent(TALENTS_MONK.ANCIENT_TEACHINGS_TALENT) && (
+              <>
+                <SpellLink id={TALENTS_MONK.ESSENCE_FONT_TALENT.id} /> is given the healing of{' '}
+                <SpellLink id={TALENTS_MONK.ANCIENT_TEACHINGS_TALENT.id} />. <br />
+              </>
+            )}
             <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id} /> healing from{' '}
-            <SpellLink id={TALENTS_MONK.RAPID_DIFFUSION_TALENT.id} /> is given to the spell that
-            procced it. <br />
+            {this.selectedCombatant.hasTalent(TALENTS_MONK.RAPID_DIFFUSION_TALENT) && (
+              <>
+                <SpellLink id={TALENTS_MONK.RAPID_DIFFUSION_TALENT.id} /> is given to the spell that
+                procced it. <br />
+              </>
+            )}
             <SpellLink id={TALENTS_MONK.ENVELOPING_MIST_TALENT.id} /> is given the healing of{' '}
             <SpellLink id={TALENTS_MONK.ENVELOPING_BREATH_TALENT.id} /> since without casting{' '}
             <SpellLink id={TALENTS_MONK.ENVELOPING_MIST_TALENT.id} />,{' '}
             <SpellLink id={TALENTS_MONK.ENVELOPING_BREATH_TALENT.id} /> healing would not occur.{' '}
             <br />
-            <SpellLink id={TALENTS_MONK.MISTY_PEAKS_TALENT.id} /> healing is attributed to the
-            source cast of the <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id} /> that procced
-            it.
+            {this.selectedCombatant.hasTalent(TALENTS_MONK.MISTY_PEAKS_TALENT) && (
+              <>
+                <SpellLink id={TALENTS_MONK.MISTY_PEAKS_TALENT.id} /> healing is attributed to the
+                source cast of the <SpellLink id={TALENTS_MONK.RENEWING_MIST_TALENT.id} /> that
+                procced it.
+              </>
+            )}
+            {this.selectedCombatant.hasTalent(TALENTS_MONK.SHAOHAOS_LESSONS_TALENT) && (
+              <>
+                <SpellLink id={TALENTS_MONK.SHAOHAOS_LESSONS_TALENT.id} /> healing is attributed to{' '}
+                <SpellLink id={TALENTS_MONK.SHEILUNS_GIFT_TALENT.id} />.
+              </>
+            )}
           </>
         }
         pad={false}
