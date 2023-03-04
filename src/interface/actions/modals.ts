a6ef3425dbc5866475bfed657b923b0b1e896920
emallson <emallson@emallson.net>
2023-03-04 14:19:45 -0500

    clean up how stacked modals appear

--- src/interface/actions/modals.ts
+++ src/interface/actions/modals.ts
@@ -1,12 +1,14 @@
 export const OPEN_MODAL = 'OPEN_MODAL';
-export function openModal() {
+export function openModal(key: string) {
   return {
     type: OPEN_MODAL,
+    key,
   };
 }
 export const CLOSE_MODAL = 'CLOSE_MODAL';
-export function closeModal() {
+export function closeModal(key: string) {
   return {
     type: CLOSE_MODAL,
+    key,
   };
 }
