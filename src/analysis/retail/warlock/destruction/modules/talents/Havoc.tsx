c77c4c75ad0e00a66f00891dd69f8369e7e44e97
dodse <dodsonn@gmail.com>
2023-03-05 13:06:51 -0500

    Havoc module updates:  1. also activate when mayhem is talented, show uptime too when that is the case  2. try to make damage calc a little more accurate by filtering to destro abilities that are duplicated by it

--- src/analysis/retail/warlock/destruction/modules/talents/Havoc.tsx
+++ src/analysis/retail/warlock/destruction/modules/talents/Havoc.tsx
@@ -1,11 +1,14 @@
 import { formatNumber, formatPercentage, formatThousands } from 'common/format';
 import SPELLS from 'common/SPELLS';
+import TALENTS from 'common/TALENTS/warlock';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { DamageEvent } from 'parser/core/Events';
 import Enemies from 'parser/shared/modules/Enemies';
-import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
+import UptimeIcon from 'interface/icons/Uptime';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import TalentSpellText from 'parser/ui/TalentSpellText';
+import { Talent } from 'common/TALENTS/types';
 
 class Havoc extends Analyzer {
   get dps() {
@@ -19,19 +22,46 @@ class Havoc extends Analyzer {
   protected enemies!: Enemies;
 
   damage = 0;
+  talent: Talent = TALENTS.HAVOC_TALENT;
+
+  HAVOCABLE_ABILITY_IDS = [
+    SPELLS.CHAOS_BOLT,
+    SPELLS.INCINERATE,
+    SPELLS.CONFLAGRATE,
+    TALENTS.SOUL_FIRE_TALENT,
+    TALENTS.SOULBURN_TALENT,
+    SPELLS.IMMOLATE,
+  ].map(s => s.id)
 
   constructor(options: Options) {
     super(options);
-    this.addEventListener(Events.damage.by(SELECTED_PLAYER), this.onDamage);
+    if (this.selectedCombatant.hasTalent(TALENTS.MAYHEM_TALENT)) {
+      this.talent = TALENTS.MAYHEM_TALENT;
+    }
+    this.active = this.selectedCombatant.hasTalent(TALENTS.HAVOC_TALENT) || this.selectedCombatant.hasTalent(TALENTS.MAYHEM_TALENT);
+
+    if (this.active) {
+      this.addEventListener(Events.damage.by(SELECTED_PLAYER), this.onDamage);
+    }
   }
 
   onDamage(event: DamageEvent) {
     const enemy = this.enemies.getEntity(event);
-    if (!enemy || !enemy.hasBuff(SPELLS.HAVOC.id, event.timestamp)) {
+    // filtering to just hits that can be duplicated will make this a little more accurate,
+    // but it'll still also count spells cast directly on a target with havoc
+    if (!enemy || !enemy.hasBuff(SPELLS.HAVOC.id, event.timestamp) || !this.HAVOCABLE_ABILITY_IDS.includes(event.ability.guid)) {
       return;
     }
 
     this.damage += event.amount + (event.absorbed || 0);
+  }
+
+  get uptime() {
+    return this.enemies.getBuffUptime(SPELLS.HAVOC.id) / this.owner.fightDuration;
+  }
+
+  shouldShowUptime() {
+    return this.selectedCombatant.hasTalent(TALENTS.MAYHEM_TALENT);
   }
 
   // TODO: this could perhaps be reworked somehow to be more accurate but not sure how yet. Take it as a Havoc v1.0
@@ -43,7 +73,7 @@ class Havoc extends Analyzer {
     return (
       <Statistic
         category={STATISTIC_CATEGORY.TALENTS}
-        size="small"
+        size="flexible"
         tooltip={
           <>
             You cleaved {formatThousands(this.damage)} damage to targets afflicted by your Havoc.
@@ -54,12 +84,19 @@ class Havoc extends Analyzer {
           </>
         }
       >
-        <BoringSpellValueText spellId={SPELLS.HAVOC.id}>
+        <TalentSpellText talent={this.talent}>
           {formatNumber(this.dps)} DPS{' '}
           <small>
             {formatPercentage(this.owner.getPercentageOfTotalDamageDone(this.damage))} % of total
           </small>
-        </BoringSpellValueText>
+          {this.shouldShowUptime() && (
+            <>
+              <br />
+              <UptimeIcon />
+              {formatPercentage(this.uptime, 0)}%<small> uptime</small>
+            </>
+          )}
+        </TalentSpellText>
       </Statistic>
     );
   }
