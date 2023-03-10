32c265685c11dd5bcd0722469cd1c097089c039c
dodse <dodsonn@gmail.com>
2023-03-09 19:34:46 -0500

    update Nightfall to take into account that it stacks to 2

--- src/analysis/retail/warlock/affliction/modules/spells/Nightfall.tsx
+++ src/analysis/retail/warlock/affliction/modules/spells/Nightfall.tsx
@@ -1,16 +1,23 @@
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/warlock';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { ApplyBuffEvent, RefreshBuffEvent, RemoveBuffEvent } from 'parser/core/Events';
+import Events, {
+  ApplyBuffEvent,
+  ApplyBuffStackEvent,
+  RefreshBuffEvent,
+  RemoveBuffEvent,
+} from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 
 const BUFF_DURATION = 12000;
 const BUFFER = 100;
+const MAX_STACKS = 2;
 
 class Nightfall extends Analyzer {
   wastedProcs = 0;
+  stacks = 0;
   private buffApplyTimestamp: number | null = null;
 
   constructor(options: Options) {
@@ -28,13 +35,32 @@ class Nightfall extends Analyzer {
       Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.NIGHTFALL_BUFF),
       this.onNightfallRemove,
     );
+    this.addEventListener(
+      Events.removebuffstack.by(SELECTED_PLAYER).spell(SPELLS.NIGHTFALL_BUFF),
+      this.onNightfallRemoveStack,
+    );
+    this.addEventListener(
+      Events.applybuffstack.by(SELECTED_PLAYER).spell(SPELLS.NIGHTFALL_BUFF),
+      this.onNightfallApplyStack,
+    );
   }
 
   onNightfallApplyRefresh(event: ApplyBuffEvent | RefreshBuffEvent) {
-    if (this.buffApplyTimestamp !== null) {
+    if (this.stacks === MAX_STACKS) {
       this.wastedProcs += 1;
+    } else {
+      this.stacks += 1;
     }
     this.buffApplyTimestamp = event.timestamp;
+  }
+
+  onNightfallApplyStack(event: ApplyBuffStackEvent) {
+    this.stacks += 1;
+    this.buffApplyTimestamp = event.timestamp;
+  }
+
+  onNightfallRemoveStack() {
+    this.stacks -= 1;
   }
 
   onNightfallRemove(event: RemoveBuffEvent) {
@@ -43,6 +69,7 @@ class Nightfall extends Analyzer {
       // buff fell off naturally
       this.wastedProcs += 1;
     }
+    this.stacks = 0;
     this.buffApplyTimestamp = null;
   }
 
