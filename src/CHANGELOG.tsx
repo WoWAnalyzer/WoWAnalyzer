4a4860c02a493529cd80c7bc59d212b146ec0526
emallson <emallson@emallson.net>
2023-03-15 04:08:57 -0400

    Re-apply "[CI] Setup Prettier to run a check on the CI"

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -23,6 +23,7 @@ import { ItemLink } from 'interface';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2023, 3, 15), "Add prettier to the CI", Putro),
   change(date(2023, 3, 8), 'Add Classic WotLK P2 trinkets.', jazminite),
   change(date(2023, 3, 6), 'Fix combatants not being dispatched to the Redux store.', ToppleTheNun),
   change(date(2023, 3, 4), 'Add Playwright tests for validating application behavior.', ToppleTheNun),
