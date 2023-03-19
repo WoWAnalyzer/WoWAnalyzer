9b76c238c8973ec4b63c613dc554009aecce36fe
Cameron Stubber <subaru@amnet.net.au>
2023-03-19 14:41:30 +0800

    Add myself to Spec Contribs

--- src/analysis/retail/mage/arcane/CONFIG.tsx
+++ src/analysis/retail/mage/arcane/CONFIG.tsx
@@ -1,4 +1,4 @@
-import { Sharrq, Dambroda } from 'CONTRIBUTORS';
+import { Sharrq, Dambroda, SyncSubaru } from 'CONTRIBUTORS';
 import Expansion from 'game/Expansion';
 import SPECS from 'game/SPECS';
 
@@ -6,7 +6,7 @@ import CHANGELOG from './CHANGELOG';
 
 export default {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
-  contributors: [Sharrq, Dambroda],
+  contributors: [Sharrq, Dambroda, SyncSubaru],
   expansion: Expansion.Dragonflight,
   // The WoW client patch this spec was last updated.
   patchCompatibility: '10.0.0',
