3dae083d232413fde70d8f5b5a40ba8541048a8a
Cameron Stubber <subaru@amnet.net.au>
2023-03-19 14:34:51 +0800

    Basic Radiant Spark Checker

--- src/analysis/retail/mage/arcane/CHANGELOG.tsx
+++ src/analysis/retail/mage/arcane/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { Sharrq, emallson, SyncSubaru } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2023, 3, 19), <>Created anaylser for <SpellLink id={talents.RADIANT_SPARK_TALENT} /> ramp phases.</>, SyncSubaru),
   change(date(2023, 3, 13), <>Updated bonus damage multiplier of <SpellLink id={talents.ARCANE_HARMONY_TALENT} />.</>, SyncSubaru),
   change(date(2023, 1, 17), <>Fixed outdated reference to the Shadowlands version of <SpellLink id={talents.RADIANT_SPARK_TALENT} />.</>, emallson),
   change(date(2022, 10, 30), `Update Dragonflight SPELLS, Abilities, and Buffs`, Sharrq),
