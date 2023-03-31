b109c77530995ea86d0f2518e346dd3b3573d97f
Richard Harrah <topplethenunnery@gmail.com>
2023-03-31 17:29:58 -0400

    mark id prop as deprecated on SpellLink

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -23,6 +23,7 @@ import { ItemLink } from 'interface';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2023, 3, 31), 'Refactor SpellLink props to use "spell" instead of "id".', ToppleTheNun),
   change(date(2023, 3, 31), 'Add Dragonflight season 1 M+ dungeon images.', ToppleTheNun),
   change(date(2023, 3, 30), 'Add ability to use PTR tooltips based on report zone.', ToppleTheNun),
   change(date(2023, 3, 29), 'Mark 10.0.5 logs as not current.', ToppleTheNun),
