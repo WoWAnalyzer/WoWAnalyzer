05328891c5636558adac61364c6d03726d53eab9
Richard Harrah <topplethenunnery@gmail.com>
2023-03-31 16:00:43 -0400

    add M+ Season 1 dungeons

---
+++ src/game/raids/mythicplusseasonone/TempleOfTheJadeSerpent.ts
@@ -0,0 +1,15 @@
+import { Dungeon } from 'game/raids';
+
+import Background from './backgrounds/TempleOfTheJadeSerpent.jpg';
+import Headshot from './headshots/TempleOfTheJadeSerpent.jpg';
+
+const TempleOfTheJadeSerpent: Dungeon = {
+  id: 10960,
+  name: 'Temple of the Jade Serpent',
+  background: Background,
+  headshot: Headshot,
+  icon: 'achievement_jadeserpent',
+  fight: {},
+};
+
+export default TempleOfTheJadeSerpent;
---
+++ src/game/raids/mythicplusseasonone/index.ts
@@ -0,0 +1,23 @@
+import AlgetharAcademy from 'game/raids/mythicplusseasonone/AlgetharAcademy';
+import AzureVault from 'game/raids/mythicplusseasonone/AzureVault';
+import CourtOfStars from 'game/raids/mythicplusseasonone/CourtOfStars';
+import HallsOfValor from 'game/raids/mythicplusseasonone/HallsOfValor';
+import NokhudOffensive from 'game/raids/mythicplusseasonone/NokhudOffensive';
+import RubyLifePools from 'game/raids/mythicplusseasonone/RubyLifePools';
+import ShadowmoonBurialGrounds from 'game/raids/mythicplusseasonone/ShadowmoonBurialGrounds';
+import TempleOfTheJadeSerpent from 'game/raids/mythicplusseasonone/TempleOfTheJadeSerpent';
+
+export default {
+  name: 'Mythic+ Season 1',
+  background: undefined, // TODO: Set up
+  bosses: {
+    AlgetharAcademy,
+    AzureVault,
+    CourtOfStars,
+    HallsOfValor,
+    NokhudOffensive,
+    RubyLifePools,
+    ShadowmoonBurialGrounds,
+    TempleOfTheJadeSerpent,
+  },
+};
