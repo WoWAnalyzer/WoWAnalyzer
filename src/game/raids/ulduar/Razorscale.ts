13e710bb98a0da6c042faeecf2eb8911c39c2212
Jazminite <jazminitehs@gmail.com>
2023-03-31 14:43:26 -0400

    Razorscale

--- src/game/raids/ulduar/Razorscale.ts
+++ src/game/raids/ulduar/Razorscale.ts
@@ -1,4 +1,6 @@
 import { Boss } from 'game/raids';
+import DIFFICULTIES from 'game/DIFFICULTIES';
+import { EventType } from 'parser/core/Events';
 
 import Headshot from './images/RazorscaleHeadshot.jpg';
 import Background from './images/Razorscale.jpg';
@@ -9,7 +11,21 @@ const Razorscale: Boss = {
   background: Background,
   headshot: Headshot,
   icon: 'achievement_boss_razorscale',
-  fight: {},
+  fight: {
+    phases: {
+      P2: {
+        key: 'P2',
+        multiple: false,
+        name: 'Phase 2: Razorscale',
+        difficulties: [DIFFICULTIES.NORMAL_RAID],
+        filter: {
+          type: EventType.Health,
+          guid: 33186, // Razorscale
+          health: 99.9,
+        },
+      },
+    },
+  },
 };
 
 export default Razorscale;
