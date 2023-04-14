91cb45a392a742c2e4e9bfdd6f1310bcc2cbfd4a
Jazminite <jazminitehs@gmail.com>
2023-04-14 04:04:55 -0400

    [Classic] Frost Death Knight stub

---
+++ src/analysis/classic/deathknight/frost/CHANGELOG.tsx
@@ -0,0 +1,6 @@
+import { change, date } from 'common/changelog';
+import { jazminite } from 'CONTRIBUTORS';
+
+export default [
+  change(date(2023, 4, 14), 'Initial guide for Classic Frost Death Knight.', jazminite),
+];
