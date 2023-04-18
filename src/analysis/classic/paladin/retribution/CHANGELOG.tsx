1cabf7c5e1f2d07c867de94809195d006670b43d
Jazminite <jazminitehs@gmail.com>
2023-04-18 11:38:31 -0400

    [Classic] Ret Paladin stub

---
+++ src/analysis/classic/paladin/retribution/CHANGELOG.tsx
@@ -0,0 +1,6 @@
+import { change, date } from 'common/changelog';
+import { jazminite } from 'CONTRIBUTORS';
+
+export default [
+  change(date(2023, 4, 18), 'Add Classic Retribution Paladin stub.', jazminite),
+];
