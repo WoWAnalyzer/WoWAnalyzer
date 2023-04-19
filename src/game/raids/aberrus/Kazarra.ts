eb4e649b95bb717b171fca6dd0d365ef3fd0e33e
Richard Harrah <topplethenunnery@gmail.com>
2023-04-19 19:57:28 -0400

    add Aberrus encounter details

---
+++ src/game/raids/aberrus/Kazarra.ts
@@ -0,0 +1,15 @@
+import { Boss } from 'game/raids';
+
+import Background from './backgrounds/Kazarra.jpg';
+import Headshot from './headshots/Kazarra.jpg';
+
+const Kazarra: Boss = {
+  id: 2683,
+  name: 'Kazarra, the Hellforged',
+  background: Background,
+  headshot: Headshot,
+  icon: 'inv_achievement_raiddragon_kazzara',
+  fight: {},
+};
+
+export default Kazarra;
