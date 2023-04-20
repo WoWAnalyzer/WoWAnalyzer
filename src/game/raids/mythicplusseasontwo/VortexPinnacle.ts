b9882af19ce74e7922c5506b4f97d6ba17343aac
Richard Harrah <topplethenunnery@gmail.com>
2023-04-20 14:27:12 -0400

    add M+ season 2 details
    Still missing backgrounds, won't have
    those until the season goes live and we
    have access to the leaderboards' high
    quality images.

---
+++ src/game/raids/mythicplusseasontwo/VortexPinnacle.ts
@@ -0,0 +1,14 @@
+import { Dungeon } from 'game/raids';
+
+import Headshot from './headshots/VortexPinnacle.jpg';
+
+const VortexPinnacle: Dungeon = {
+  id: 10657,
+  name: 'The Vortex Pinnacle',
+  // background: Background,
+  headshot: Headshot,
+  icon: 'achievement_dungeon_skywall',
+  fight: {},
+};
+
+export default VortexPinnacle;
