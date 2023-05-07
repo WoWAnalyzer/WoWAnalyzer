19c32ed8d1a4f155e74b5816a066299df3fbc732
Trevor Moyer <trevorm4@fb.com>
2023-05-06 19:39:32 -0700

    essence burst

--- src/analysis/retail/evoker/preservation/Guide.tsx
+++ src/analysis/retail/evoker/preservation/Guide.tsx
@@ -19,9 +19,10 @@ export default function Guide({ modules, events, info }: GuideProps<typeof Comba
   const isEbBuild = info.combatant.hasTalent(TALENTS_EVOKER.FIELD_OF_DREAMS_TALENT);
   return (
     <>
-      <Section title="Core Spell">
+      <Section title="Core Spells and Buffs">
         {modules.dreamBreath.guideSubsection}
         {modules.spiritBloom.guideSubsection}
+        {modules.essenceBurst.guideSubsection}
         {info.combatant.hasTalent(TALENTS_EVOKER.RESONATING_SPHERE_TALENT) &&
           !isEbBuild &&
           modules.resonatingSphere.guideSubsection}
@@ -33,7 +34,7 @@ export default function Guide({ modules, events, info }: GuideProps<typeof Comba
           info.combatant.hasTalent(TALENTS_EVOKER.EMERALD_COMMUNION_TALENT) &&
           modules.emeraldCommunion.guideSubsection}
       </Section>
-      <Section title="Talents and Buffs">
+      <Section title="Talents">
         {info.combatant.hasTalent(TALENTS_EVOKER.OUROBOROS_TALENT) &&
           modules.ouroboros.guideSubsection}
       </Section>
--- src/analysis/retail/evoker/preservation/modules/talents/EssenceBurst.tsx
+++ src/analysis/retail/evoker/preservation/modules/talents/EssenceBurst.tsx
@@ -4,7 +4,12 @@ import Statistic from 'parser/ui/Statistic';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
-import Events, { EventType, RemoveBuffEvent, RemoveBuffStackEvent } from 'parser/core/Events';
+import Events, {
+  EventType,
+  RefreshBuffEvent,
+  RemoveBuffEvent,
+  RemoveBuffStackEvent,
+} from 'parser/core/Events';
 import { getEssenceBurstConsumeAbility } from '../../normalizers/CastLinkNormalizer';
 import { TALENTS_EVOKER } from 'common/TALENTS';
 import { SPELL_COLORS } from 'analysis/retail/evoker/preservation/constants';
@@ -12,6 +17,13 @@ import DonutChart from 'parser/ui/DonutChart';
 import { SpellLink } from 'interface';
 import { t } from '@lingui/macro';
 import ItemManaGained from 'parser/ui/ItemManaGained';
+import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
+import { RoundedPanel } from 'interface/guide/components/GuideDivs';
+import { BoxRowEntry, PerformanceBoxRow } from 'interface/guide/components/PerformanceBoxRow';
+import CastEfficiencyBar from 'parser/ui/CastEfficiencyBar';
+import { GapHighlight } from 'parser/ui/CooldownBar';
+import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
+import { GUIDE_CORE_EXPLANATION_PERCENT } from '../../Guide';
 
 export const ESSENCE_COSTS: { [name: string]: number } = {
   'Emerald Blossom': 3,
@@ -25,11 +37,19 @@ export const MANA_COSTS: { [name: string]: number } = {
   Disintegrate: 0,
 };
 
+interface CastInfo {
+  spell: number;
+  expired: boolean;
+  refreshed: boolean;
+  timestamp: number;
+}
+
 class EssenceBurst extends Analyzer {
   totalConsumed: number = 0;
   totalExpired: number = 0;
   essenceSaved: number = 0;
   manaSaved: number = 0;
+  casts: CastInfo[] = [];
   consumptionCount: { [name: string]: number } = { 'Emerald Blossom': 0, Echo: 0, Disintegrate: 0 };
 
   constructor(options: Options) {
@@ -49,17 +69,31 @@ class EssenceBurst extends Analyzer {
 
   onBuffRemove(event: RemoveBuffEvent | RemoveBuffStackEvent) {
     const consumeAbility = getEssenceBurstConsumeAbility(event);
+    const info: CastInfo = {
+      timestamp: event.timestamp,
+      expired: false,
+      refreshed: false,
+      spell: 0,
+    };
     if (consumeAbility) {
       const spellName = consumeAbility.ability.name;
+      info.spell = consumeAbility.ability.guid;
       this.totalConsumed += 1;
       this.essenceSaved += ESSENCE_COSTS[spellName];
       this.manaSaved += MANA_COSTS[spellName];
       this.consumptionCount[spellName] += 1;
     } else if (event.type === EventType.RemoveBuff) {
       this.totalExpired += 1;
+      info.expired = true;
     } else {
       this.totalExpired += (event as RemoveBuffStackEvent).stack;
+      info.expired = true;
     }
+    this.casts.push(info);
+  }
+
+  onBuffRefresh(event: RefreshBuffEvent) {
+    this.casts.push({ timestamp: event.timestamp, expired: false, refreshed: true, spell: 0 });
   }
 
   get averageManaSavedForHealingSpells() {
@@ -113,6 +147,75 @@ class EssenceBurst extends Analyzer {
       },
       style: ThresholdStyle.NUMBER,
     };
+  }
+
+  get guideSubsection(): JSX.Element {
+    const explanation = (
+      <p>
+        <b>
+          <SpellLink id={TALENTS_EVOKER.ESSENCE_BURST_TALENT.id} />
+        </b>{' '}
+        is a core buff that you should never let expire or refresh. In general, if you are playing
+        an <SpellLink spell={SPELLS.EMERALD_BLOSSOM} /> focused build then all procs should be used
+        on it and otherwise they should be spent on <SpellLink spell={TALENTS_EVOKER.ECHO_TALENT} />
+        . If you choose to talent into <SpellLink spell={TALENTS_EVOKER.ENERGY_LOOP_TALENT} />, then
+        you should use some procs on <SpellLink id={SPELLS.DISINTEGRATE} />, but this talent should
+        generally not be taken as it is an HPS loss.
+      </p>
+    );
+
+    const entries: BoxRowEntry[] = [];
+    this.casts.forEach((info) => {
+      let value = QualitativePerformance.Good;
+      if (
+        info.spell === null ||
+        (!this.selectedCombatant.hasTalent(TALENTS_EVOKER.ENERGY_LOOP_TALENT) &&
+          info.spell === SPELLS.DISINTEGRATE.id)
+      ) {
+        value = QualitativePerformance.Fail;
+      }
+      const spellString =
+        info.spell === null ? (
+          `Wasted from ${info.expired ? 'expiration' : 'refresh'}`
+        ) : (
+          <>
+            Consume ability: <SpellLink id={info.spell} />
+          </>
+        );
+      const tooltip = (
+        <>
+          Expiration @ {this.owner.formatTimestamp(info.timestamp)}
+          <br />
+          {spellString}
+        </>
+      );
+      entries.push({ value, tooltip });
+    });
+
+    const data = (
+      <div>
+        <RoundedPanel>
+          <strong>
+            <SpellLink id={TALENTS_EVOKER.ESSENCE_BURST_TALENT} /> consumptions
+          </strong>
+          <PerformanceBoxRow values={entries} />
+        </RoundedPanel>
+      </div>
+    );
+
+    return explanationAndDataSubsection(explanation, data, GUIDE_CORE_EXPLANATION_PERCENT);
+  }
+
+  subStatistic() {
+    return (
+      <CastEfficiencyBar
+        spellId={TALENTS_EVOKER.DREAM_BREATH_TALENT.id}
+        gapHighlightMode={GapHighlight.FullCooldown}
+        minimizeIcons
+        slimLines
+        useThresholds
+      />
+    );
   }
 
   suggestions(when: When) {
