1e2a37e1184d69c2ebe74dae9d8a53c5121a1e8b
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2023-05-09 12:44:41 -0400

    Update Dark Evangelism

--- src/analysis/retail/priest/shadow/modules/features/Buffs.tsx
+++ src/analysis/retail/priest/shadow/modules/features/Buffs.tsx
@@ -31,7 +31,13 @@ class Buffs extends CoreAuras {
       },
       {
         spellId: SPELLS.DARK_EVANGELISM_TALENT_BUFF.id,
-        triggeredBySpellId: SPELLS.MIND_FLAY.id,
+        triggeredBySpellId: [
+          SPELLS.MIND_FLAY.id,
+          SPELLS.MIND_FLAY_INSANITY_TALENT_DAMAGE.id,
+          TALENTS.MIND_SPIKE_TALENT.id,
+          SPELLS.MIND_SPIKE_INSANITY_TALENT_DAMAGE.id,
+          TALENTS.VOID_TORRENT_TALENT.id,
+        ],
         enabled: combatant.hasTalent(TALENTS.DARK_EVANGELISM_TALENT),
         timelineHighlight: false,
       },
