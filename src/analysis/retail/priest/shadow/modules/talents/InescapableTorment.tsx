443a2ae68b09d89c39316be6b93fdd55481e5cfe
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2023-05-09 17:02:14 -0400

    Update Inescapable torment

--- src/analysis/retail/priest/shadow/modules/talents/InescapableTorment.tsx
+++ src/analysis/retail/priest/shadow/modules/talents/InescapableTorment.tsx
@@ -8,6 +8,7 @@ import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import UptimeIcon from 'interface/icons/Uptime';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import { INESCAPABLE_TORMENT_EXTENSION } from '../../constants';
 
 class InescapableTorment extends Analyzer {
   damage = 0;
@@ -24,7 +25,9 @@ class InescapableTorment extends Analyzer {
 
   onDamage(event: DamageEvent) {
     this.damage += event.amount + (event.absorbed || 0);
-    this.time += 0.7 * this.selectedCombatant.getTalentRank(TALENTS.INESCAPABLE_TORMENT_TALENT);
+    this.time +=
+      INESCAPABLE_TORMENT_EXTENSION *
+      this.selectedCombatant.getTalentRank(TALENTS.INESCAPABLE_TORMENT_TALENT);
   }
 
   statistic() {
