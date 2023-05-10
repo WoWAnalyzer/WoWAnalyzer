a64925bfb195c75c52f6db79a2015749af788739
Alex Wissmann <wissmania@gmail.com>
2023-05-09 22:08:36 -0700

    change patch compatibility to 10.1.0

--- src/analysis/retail/monk/windwalker/CONFIG.tsx
+++ src/analysis/retail/monk/windwalker/CONFIG.tsx
@@ -10,7 +10,7 @@ const config: Config = {
   contributors: [Durpn, Tenooki],
   expansion: Expansion.Dragonflight,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: null,
+  patchCompatibility: '10.1.0',
   isPartial: true,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
