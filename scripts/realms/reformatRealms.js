8ad2b33838633068792cf640100302f31079d979
Jazminite <jazminitehs@gmail.com>
2023-05-12 02:48:34 -0400

    updates for comments

--- scripts/realms/reformatRealms.js
+++ scripts/realms/reformatRealms.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 function readJson(path) {
   return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
-
 function reformat(json) {
   return json.realms
-    .map(({ name, slug }) => ({ name, slug }))
+    .map(({ name, slug }) => ({
+      name,
+      slug,
+    }))
     .sort((a, b) => a.name.localeCompare(b.name));
 }
-
 function reformatFile(path) {
   return reformat(readJson(path));
 }
@@ -20,10 +21,6 @@ const output = {
   TW: reformatFile('./data/TW.json'),
   US: reformatFile('./data/US.json'),
   CN: reformatFile('./data/CN.json'),
-  classicEU: reformatFile('./data/classicEU.json'),
-  classicKR: reformatFile('./data/classicKR.json'),
-  classicTW: reformatFile('./data/classicTW.json'),
-  classicUS: reformatFile('./data/classicUS.json'),
 };
 
 fs.writeFileSync(
@@ -31,20 +28,6 @@ fs.writeFileSync(
   `// Generated file, changes will be overwritten!
 // eslint-disable
 
-export default {
-${Object.entries(output)
-  .map(
-    ([key, value]) =>
-      `  ${key}: [\n${value
-        .map(
-          (realm) =>
-            `    { name: ${
-              realm.name.includes("'") ? '"' + realm.name + '"' : "'" + realm.name + "'"
-            }, slug: '${realm.slug}' }`,
-        )
-        .join(',\n')},\n  ]`,
-  )
-  .join(',\n')}
-};
+export default ${JSON.stringify(output)};
 `,
 );
