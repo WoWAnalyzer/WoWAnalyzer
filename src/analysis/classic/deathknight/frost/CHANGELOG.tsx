dbba222ee42555aa26968ef9f2404b92dbd778b9
Jazminite <jazminitehs@gmail.com>
2023-05-27 04:34:40 -0400

    update CHANGELOG

--- src/analysis/classic/deathknight/frost/CHANGELOG.tsx
+++ src/analysis/classic/deathknight/frost/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { jazminite } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2023, 5, 25), 'Add missing Frost DK abilities.', jazminite),
   change(date(2023, 4, 14), 'Initial guide for Classic Frost Death Knight.', jazminite),
 ];
