a1c4e61b25adfd58907023f1b676faf34c15e6cc
trevorm4 <trevorm4@illinois.edu>
2023-05-31 22:29:37 -0700

    Move buffs

--- src/common/ITEMS/monk.ts
+++ src/common/ITEMS/monk.ts
@@ -8,16 +8,6 @@ const items = itemIndexableList({
   //region Windwalker
   //endregion
   //region Shared
-  CALL_TO_DOMINANCE: {
-    id: 204202,
-    name: "Neltharion's Call to Dominance",
-    icon: 'inv_10_dungeonjewelry_dragon_trinket_5_bronze',
-  },
-  DOMINEERING_ARROGANCE_BUFF: {
-    id: 411661,
-    name: "Neltharion's Call to Dominance",
-    icon: 'inv_10_dungeonjewelry_dragon_trinket_5_bronze',
-  },
   //endregion
 });
 export default items;
