4bda663ad34fce888979b1234f24b16311e99994
Shon M <him@shonm.com>
2023-06-26 09:32:21 -0600

    Update src/analysis/retail/paladin/retribution/modules/core/CrusaderStrike.tsx
    Co-authored-by: Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>

--- src/analysis/retail/paladin/retribution/modules/core/CrusaderStrike.tsx
+++ src/analysis/retail/paladin/retribution/modules/core/CrusaderStrike.tsx
@@ -9,7 +9,7 @@ class CrusaderStrike extends Analyzer {
   constructor(options: Options) {
     super(options);
 
-    this.active = !this.selectedCombatant.hasTalent(TALENTS.CONSECRATED_BLADE_TALENT);
+    this.active = !this.selectedCombatant.hasTalent(TALENTS.CRUSADING_STRIKES_TALENT);
 
     this.addEventListener(
       Events.cast.by(SELECTED_PLAYER).spell(SPELLS.CRUSADER_STRIKE),
