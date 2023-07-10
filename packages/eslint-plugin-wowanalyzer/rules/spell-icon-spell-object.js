7bb776d0957bafbed6d74999e927b44e0bb31925
Richard Harrah <topplethenunnery@gmail.com>
2023-07-10 12:49:00 -0400

    update remaining usages of spell ID in SpellLink

--- packages/eslint-plugin-wowanalyzer/rules/spell-icon-spell-object.js
+++ packages/eslint-plugin-wowanalyzer/rules/spell-icon-spell-object.js
@@ -19,36 +19,15 @@ module.exports = {
           return;
         }
         const idProp = openingElement.attributes.find((attr) => attr.name.name === 'id');
-        const spellProp = openingElement.attributes.find((attr) => attr.name.name === 'spell');
 
-        if (idProp) {
-          context.report({
-            node: idProp,
-            messageId: 'idInsteadOfSpell',
-            fix(fixer) {
-              return fixer.replaceText(idProp.name, 'spell');
-            },
-          });
+        if (!idProp) {
+          return;
         }
 
-        if (spellProp) {
-          const value = spellProp.value;
-          if (value.type === 'JSXExpressionContainer') {
-            const expression = value.expression;
-            if (expression.type === 'MemberExpression') {
-              const property = expression.property;
-              if (property.name === 'id' || property.name === 'guid') {
-                context.report({
-                  node: property,
-                  messageId: 'nonObjectSpell',
-                  fix(fixer) {
-                    return fixer.removeRange([property.range[0] - 1, property.range[1]]);
-                  },
-                });
-              }
-            }
-          }
-        }
+        context.report({
+          node: idProp,
+          messageId: 'idInsteadOfSpell',
+        });
       },
     };
   },
