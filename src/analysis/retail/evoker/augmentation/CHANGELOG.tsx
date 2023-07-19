b4a33e030a1b25830c597b85f0e96651e354f5cb
Vollmer <kristianvollmer@gmail.com>
2023-07-19 16:34:50 +0200

    Changelog

--- src/analysis/retail/evoker/augmentation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/augmentation/CHANGELOG.tsx
@@ -1,2 +1,6 @@
+import { change, date } from 'common/changelog';
+import { Vollmer } from 'CONTRIBUTORS';
+
 export default [
+    change(date(2023, 7, 19), <>Initial implementation of Augmentation</>, Vollmer),
 ];
