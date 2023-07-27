6e2c826a6227d9d10ea0676e66a75ec1198c3bd3
awildfivreld <12049229+awildfivreld@users.noreply.github.com>
2023-07-27 23:15:35 +0200

    add astral flare to the haste module

--- src/common/SPELLS/encounter.ts
+++ src/common/SPELLS/encounter.ts
@@ -1,5 +1,12 @@
 import Spell from 'common/SPELLS/Spell';
 
-const spells = {} satisfies Record<string, Spell>;
+const spells = {
+  // Sarkareth
+  ASTRAL_FLARE_BUFF: {
+    id: 407576,
+    name: 'Astral Flare',
+    icon: 'sha_ability_mage_firestarter_nightborne',
+  },
+} satisfies Record<string, Spell>;
 
 export default spells;
