73fdb8227afd64f479bd6a12b284c0237cd4df56
Aveline Estié <aa.estie@gmail.com>
2023-07-31 20:02:56 +0200

    redo using generalized cooldown graph component

--- src/analysis/retail/warlock/demonology/modules/guide/Cooldowns.tsx
+++ src/analysis/retail/warlock/demonology/modules/guide/Cooldowns.tsx
@@ -1,59 +1,46 @@
-import { Section, SubSection, useAnalyzer, useInfo } from 'interface/guide';
-import CastEfficiency from 'parser/shared/modules/CastEfficiency';
+import { Section } from 'interface/guide';
 import TALENTS from 'common/TALENTS/warlock';
-import { Talent } from 'common/TALENTS/types';
-import CastEfficiencyBar from 'parser/ui/CastEfficiencyBar';
-import { GapHighlight } from 'parser/ui/CooldownBar';
+import CoreCooldownGraphSubsection, {
+  Cooldown,
+} from 'interface/guide/components/CooldownGraphSubSection';
 
-interface Props {
-  checklist: Talent[];
-}
-
-const COOLDOWNS: Talent[] = [
-  TALENTS.NETHER_PORTAL_TALENT,
-  TALENTS.GRIMOIRE_FELGUARD_TALENT,
-  TALENTS.SUMMON_VILEFIEND_TALENT,
-  TALENTS.SUMMON_DEMONIC_TYRANT_TALENT,
-  TALENTS.DEMONIC_STRENGTH_TALENT,
-  TALENTS.POWER_SIPHON_TALENT,
-  TALENTS.GUILLOTINE_TALENT,
+const cooldowns: Cooldown[] = [
+  {
+    spell: TALENTS.NETHER_PORTAL_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.NETHER_PORTAL_TALENT),
+  },
+  {
+    spell: TALENTS.GRIMOIRE_FELGUARD_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.GRIMOIRE_FELGUARD_TALENT),
+  },
+  {
+    spell: TALENTS.SUMMON_VILEFIEND_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.SUMMON_VILEFIEND_TALENT),
+  },
+  {
+    spell: TALENTS.SUMMON_DEMONIC_TYRANT_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.SUMMON_DEMONIC_TYRANT_TALENT),
+  },
+  {
+    spell: TALENTS.DEMONIC_STRENGTH_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.DEMONIC_STRENGTH_TALENT),
+  },
+  {
+    spell: TALENTS.POWER_SIPHON_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.POWER_SIPHON_TALENT),
+  },
+  {
+    spell: TALENTS.GUILLOTINE_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.GUILLOTINE_TALENT),
+  },
 ];
 
 function Cooldowns() {
   return (
     <Section title="Cooldowns">
-      <p>
-        <strong>Cooldowns</strong> - this graph shows when you used your major cooldowns and how
-        long you waited to use them again. Unless you're holding these for specific raid events, try
-        to use these on as soon as they become available.
-      </p>
-      <CooldownGraphSubsection checklist={COOLDOWNS} />
+      <CoreCooldownGraphSubsection cooldowns={cooldowns} />
     </Section>
   );
 }
-
-const CooldownGraphSubsection = ({ checklist }: Props) => {
-  const info = useInfo();
-  const castEfficiency = useAnalyzer(CastEfficiency);
-  if (!info || !castEfficiency) {
-    return null;
-  }
-
-  return (
-    <SubSection>
-      {checklist
-        .filter((talent) => info.combatant.hasTalent(talent))
-        .map((talent) => (
-          <CastEfficiencyBar
-            key={talent.id}
-            spell={talent}
-            gapHighlightMode={GapHighlight.All}
-            minimizeIcons={(castEfficiency.getCastEfficiencyForSpell(talent)?.casts ?? 0) > 10}
-            useThresholds
-          />
-        ))}
-    </SubSection>
-  );
-};
 
 export default Cooldowns;
