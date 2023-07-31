33093d7bfc1e75d2a709dfc526c63de5f35b2a12
Woliance <34073843+Woliance@users.noreply.github.com>
2023-07-31 03:26:02 +0200

    WIP Guide

---
+++ src/analysis/retail/paladin/protection/modules/core/Defensives/config.tsx
@@ -0,0 +1,7 @@
+import ArdentDefender from './ArdentDefender';
+import GuardianOfAncientKings from './GuardianOfAncientKings';
+import EyeOfTyr from './EyeOfTyr';
+//import Metamorphosis from './Metamorphosis';
+
+export const MAJOR_ANALYZERS = [GuardianOfAncientKings, ArdentDefender, EyeOfTyr] as const;
+export const TIMELINE_ANALYZERS = [] as const;
