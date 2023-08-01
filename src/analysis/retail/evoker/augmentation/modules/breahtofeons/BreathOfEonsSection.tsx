ed79161a5122beca24324bebf9970c80cb95d7e8
Vollmer <kristianvollmer@gmail.com>
2023-08-01 12:24:18 +0200

    Hide Time Skip introduction if not talented

--- src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsSection.tsx
+++ src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsSection.tsx
@@ -38,33 +38,39 @@ const BreathOfEonsSection: React.FC<Props> = ({
 
   return (
     <SubSection title="Breath of Eons">
-      <p>
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> is a powerful cooldown that should be
-        used along side your allies cooldowns, since it's a major damage amplifier.{' '}
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> will replicate damage done by your{' '}
-        <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> targets, it is therefore important to
-        maintain 100% uptime on <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> during your{' '}
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> windows.
-        <br />
-        <br />
-        With <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> talented, you should aim to use{' '}
-        <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> alongside every other{' '}
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} />.{' '}
-        <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> should be used to reduce the cooldown of your
-        empowers, <SpellLink spell={SPELLS.FIRE_BREATH} /> and <SpellLink spell={SPELLS.UPHEAVAL} />{' '}
-        to maximize the amount of <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} /> buffs you have
-        active.
-        <br />
-        <br />
-        You can use the graph below to visualize your buffs:{' '}
-        <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} />,{' '}
-        <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> along with your{' '}
-        <SpellLink spell={SPELLS.TEMPORAL_WOUND_DEBUFF} /> debuffs, for each individual{' '}
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> window. Problem points such as: letting{' '}
-        <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> drop during your{' '}
-        <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> windows, or a mob dying before{' '}
-        <SpellLink spell={SPELLS.TEMPORAL_WOUND_DEBUFF} /> runs out, will be pointed out.
-      </p>
+      <div>
+        <p>
+          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> is a powerful cooldown that should be
+          used along side your allies cooldowns, since it's a major damage amplifier.{' '}
+          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> will replicate damage done by your{' '}
+          <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> targets, it is therefore important to
+          maintain 100% uptime on <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> during your{' '}
+          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> windows.
+          <br />
+        </p>
+        {breathPerformance.timeskipTalented && (
+          <p>
+            With <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> talented, you should aim to use{' '}
+            <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> alongside every other{' '}
+            <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} />.{' '}
+            <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> should be used to reduce the cooldown of
+            your empowers, <SpellLink spell={SPELLS.FIRE_BREATH} /> and{' '}
+            <SpellLink spell={SPELLS.UPHEAVAL} /> to maximize the amount of{' '}
+            <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} /> buffs you have active.
+            <br />
+          </p>
+        )}
+        <p>
+          You can use the graph below to visualize your buffs:{' '}
+          <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} />,{' '}
+          <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> along with your{' '}
+          <SpellLink spell={SPELLS.TEMPORAL_WOUND_DEBUFF} /> debuffs, for each individual{' '}
+          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> window. Problem points such as:
+          letting <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> drop during your{' '}
+          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> windows, or a mob dying before{' '}
+          <SpellLink spell={SPELLS.TEMPORAL_WOUND_DEBUFF} /> runs out, will be pointed out.
+        </p>
+      </div>
       <div className="breath-explanation-container">
         <table className="breath-explanations">
           <tbody>
