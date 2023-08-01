170dceedab4e18b59451b6cba22896a2704d10d1
Vollmer <kristianvollmer@gmail.com>
2023-08-01 11:32:54 +0200

    Implement Breath of Eons module

---
+++ src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsGraph.tsx
@@ -0,0 +1,242 @@
+// BreathOfEonsPlot.tsx
+import React from 'react';
+import { BreathOfEonsWindows, GRAPHBUFFER, SpellTracker } from './BreathOfEonsRotational'; // Import any required types
+import BaseChart, { formatTime } from 'parser/ui/BaseChart';
+import { VisualizationSpec } from 'react-vega';
+import { AutoSizer } from 'react-virtualized';
+import { UnitSpec } from 'vega-lite/build/src/spec';
+import { Field } from 'vega-lite/build/src/channeldef';
+
+type Props = {
+  window: BreathOfEonsWindows;
+  fightStartTime: number;
+  ebonMightCount: SpellTracker[];
+  shiftingSandsCount: SpellTracker[];
+};
+
+const BreathOfEonsPlot: React.FC<Props> = ({
+  window,
+  fightStartTime,
+  ebonMightCount,
+  shiftingSandsCount,
+}) => {
+  const startTime = window.start - GRAPHBUFFER;
+  const endTime = window.end + GRAPHBUFFER;
+
+  /** This is used to filter through event counters to make new ones
+   * that are limited to within the timeframe of our current breath windows.
+   * essentially helps create concentrated Breath window graphs */
+  const filterItems = (items: SpellTracker[]): SpellTracker[] => {
+    let filteredItems: SpellTracker[] = [];
+    let prevCount = 0;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const entry = items[i];
+      const timestamp = entry.timestamp;
+      const count = entry.count;
+
+      if (timestamp < startTime) {
+        filteredItems = [{ timestamp: startTime, count: count }];
+        prevCount = count;
+      }
+
+      if (timestamp >= startTime && timestamp <= endTime) {
+        if (filteredItems.length === 0) {
+          filteredItems = [{ timestamp: startTime, count: 0 }];
+        }
+        prevCount = count;
+        filteredItems.push({ timestamp: timestamp, count: count });
+      } else if (timestamp > endTime) {
+        filteredItems.push({ timestamp: endTime, count: prevCount });
+        break;
+      }
+    }
+    return filteredItems;
+  };
+
+  const filteredEbonCount = filterItems(ebonMightCount);
+  const filteredShiftingSandsCount = filterItems(shiftingSandsCount);
+
+  const xAxis = {
+    field: 'timestamp_shifted',
+    type: 'quantitative' as const,
+    axis: {
+      labelExpr: formatTime('datum.value'),
+      tickCount: 10,
+      grid: false,
+    },
+    scale: {
+      zero: false,
+      nice: false,
+    },
+    title: 'Time',
+  };
+
+  const yAxis = {
+    field: 'count',
+    type: 'quantitative' as const,
+    axis: {
+      gridOpacity: 0.3,
+      format: '~s',
+      title: null,
+    },
+  };
+
+  const line = (dataName: string, label: string): UnitSpec<Field> => ({
+    data: { name: dataName },
+    mark: {
+      type: 'line',
+      interpolate: 'step-after',
+      size: 4,
+    },
+    transform: [
+      { filter: 'isValid(datum.count)' },
+      {
+        calculate: `datum.timestamp - ${fightStartTime}`,
+        as: 'timestamp_shifted',
+      },
+    ],
+    encoding: {
+      color: {
+        datum: label,
+        type: 'nominal',
+      },
+    },
+  });
+
+  const area = (dataName: string, label: string): UnitSpec<Field> => ({
+    data: { name: dataName },
+    mark: {
+      type: 'area',
+      interpolate: 'step-after',
+      line: {
+        strokeWidth: 3,
+      },
+      opacity: 0.2,
+    },
+    transform: [
+      {
+        calculate: `datum.timestamp - ${fightStartTime}`,
+        as: 'timestamp_shifted',
+      },
+    ],
+    encoding: {
+      color: {
+        datum: label,
+        type: 'nominal',
+      },
+    },
+  });
+
+  const point = (dataName: string, label: string, tooltip: string | null): UnitSpec<Field> => ({
+    data: { name: dataName },
+    mark: {
+      type: 'point' as const,
+      shape: 'circle',
+      filled: true,
+      size: 120,
+      opacity: 1,
+    },
+    encoding: {
+      tooltip: {
+        value: tooltip,
+      },
+      color: {
+        datum: label,
+        type: 'nominal',
+      },
+    },
+    transform: [
+      {
+        calculate: `datum.timestamp - ${fightStartTime}`,
+        as: 'timestamp_shifted',
+      },
+    ],
+  });
+
+  const colorRange = [
+    '#736F4E', // Temporal Wounds
+    '#F3A738', // Ebon Might
+    '#F7EC59', // Shifting Sands
+    '#FF6B6B', // Flight Time
+    'red', // Problem Points
+  ];
+  const spec: VisualizationSpec = {
+    encoding: {
+      x: xAxis,
+      y: yAxis,
+      color: {
+        // Color range to make legend behave. Order is
+        scale: { range: colorRange },
+        legend: {
+          symbolOpacity: 1,
+        },
+      },
+    },
+
+    layer: [
+      // Temporal Wounds
+      {
+        ...area('breathCount', 'Temporal Wounds'),
+      },
+      // Ebon Might
+      {
+        ...line('ebonCount', 'Ebon Might'),
+      },
+      // Shifting Sands
+      {
+        ...line('shiftingSandsCount', 'Shifting Sands'),
+      },
+      // Flight Time
+      {
+        ...area('flightData', 'Flight Time'),
+      },
+      // Damage Problem Points
+      {
+        ...point(
+          'damageProblemPoints',
+          'Problem Point',
+          'A mob died before Breath went off, losing you potential damage.',
+        ),
+      },
+      // Ebon Might Problem Points
+      {
+        ...point(
+          'ebonMightProblemPoints',
+          'Problem Point',
+          'You dropped Ebon Might here, this is a major DPS loss!',
+        ),
+      },
+    ],
+  };
+
+  return (
+    <div
+      className="graph-container"
+      style={{
+        width: '100%',
+        minHeight: 200,
+      }}
+    >
+      <AutoSizer>
+        {({ width, height }) => (
+          <BaseChart
+            spec={spec}
+            data={{
+              breathCount: window.breathPerformance.temporalWoundsCounter,
+              ebonCount: filteredEbonCount,
+              damageProblemPoints: window.breathPerformance.damageProblemPoints,
+              ebonMightProblemPoints: window.breathPerformance.ebonMightProblems,
+              flightData: window.flightData,
+              shiftingSandsCount: filteredShiftingSandsCount,
+            }}
+            width={width}
+            height={height}
+          />
+        )}
+      </AutoSizer>
+    </div>
+  );
+};
+
+export default BreathOfEonsPlot;
---
+++ src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsRotational.tsx
@@ -0,0 +1,540 @@
+import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import TALENTS from 'common/TALENTS/evoker';
+import SPELLS from 'common/SPELLS/evoker';
+import Events, {
+  ApplyBuffEvent,
+  ApplyDebuffEvent,
+  CastEvent,
+  EmpowerEndEvent,
+  EventType,
+  RemoveBuffEvent,
+  RemoveDebuffEvent,
+  UpdateSpellUsableEvent,
+  UpdateSpellUsableType,
+} from 'parser/core/Events';
+import {
+  getBreathOfEonsBuffEvents,
+  getBreathOfEonsDamageEvents,
+  getBreathOfEonsDebuffApplyEvents,
+} from '../normalizers/CastLinkNormalizer';
+import SpellUsable from 'parser/shared/modules/SpellUsable';
+import Spell from 'common/SPELLS/dragonflight/potions';
+import BreathOfEonsSection from './BreathOfEonsSection';
+import spells from 'common/SPELLS/dragonflight/trinkets';
+import trinkets from 'common/ITEMS/dragonflight/trinkets';
+
+export type BreathOfEonsWindows = {
+  flightData: SpellTracker[];
+  breathPerformance: BreathWindowPerformance;
+  start: number;
+  end: number;
+};
+
+export type SpellTracker = {
+  timestamp: number;
+  count: number;
+};
+
+export type BreathWindowPerformance = {
+  temporalWoundsCounter: SpellTracker[];
+  ebonMightDroppedDuringBreath: boolean;
+  ebonMightDroppedDuration: number;
+  ebonMightProblems: SpellTracker[];
+  possibleTrinkets: number;
+  trinketUsed: number;
+  possiblePotions: number;
+  potionUsed: number;
+  fireBreaths: number;
+  possibleFireBreaths: number;
+  upheavels: number;
+  possibleUpheavels: number;
+  timeskipTalented: boolean;
+  possibleTimeSkips: number;
+  timeSkips: number;
+  damageProblemPoints: SpellTracker[];
+  earlyDeaths: number;
+  succesfulHits: number;
+  potentialLostDamage: number;
+  damage: number;
+};
+
+/**
+ * Breath of Eons is Augmentations major cooldown, it works as a damage amp for your allies
+ * with Ebon Might up. Therefore it is important to play around this window correctly.
+ *
+ * Currently in this module we will be analysing the buff mangement aspect of the cooldown usages,
+ * along with cast performance (Empowers/Timeskip/Potion/Trinket).
+ *
+ * Essentially keeping track of Ebon Might, Shifting Sands and Temporal Wounds debuffs.
+ * We show this using a graph to help visualize the information.
+ * We mark problem points such as letting Ebon Might drop or mobs with debuff dying early. These
+ * are marked on the graph as red circles.
+ *
+ * Ideally in the future we would like to introduce outside information gathered from fetchWCL.
+ * Some of these things would be either tracking spell usages of your buffed/non buffed teamates,
+ * to figure out if a window should have been shifted.
+ * Even better solution would be to track raid DPS and compare to see if your should have shifted
+ * and/or buffed other players for a stronger burst window.
+ * Prolly easier to just make a WCL component for this count of analysis though.
+ *
+ * TODO: Provide better feedback, estimated damage loss based on lost EM uptime and deadge mobs
+ * TODO: At some point introduce some WCL logic to provide more indepth analysis
+ * TODO: make it prettier? idk not really my forte
+ */
+
+export const GRAPHBUFFER = 3000;
+
+class BreathOfEonsRotational extends Analyzer {
+  static dependencies = {
+    spellUsable: SpellUsable,
+  };
+  protected spellUsable!: SpellUsable;
+
+  windows: BreathOfEonsWindows[] = [];
+
+  activeDebuffs: number = 0;
+  isEbonMightActive: boolean = false;
+
+  breathWindowActive: boolean = false;
+  totalCasts: number = 0;
+  latestEbonMightDrop!: RemoveBuffEvent;
+  latestEbonMightEvent!: RemoveBuffEvent | ApplyBuffEvent;
+
+  ebonMightCounter: number = 0;
+  ebonMightCount: SpellTracker[] = [];
+  shiftingsSandsCounter: number = 0;
+  shiftingSandsCount: SpellTracker[] = [];
+
+  fireBreath = this.selectedCombatant.hasTalent(TALENTS.FONT_OF_MAGIC_AUGMENTATION_TALENT)
+    ? SPELLS.FIRE_BREATH_FONT
+    : SPELLS.FIRE_BREATH;
+
+  upheavel = this.selectedCombatant.hasTalent(TALENTS.FONT_OF_MAGIC_AUGMENTATION_TALENT)
+    ? SPELLS.UPHEAVAL_FONT
+    : SPELLS.UPHEAVAL;
+
+  trackedSpells = [TALENTS.TIME_SKIP_TALENT];
+  empowers = [this.fireBreath, this.upheavel];
+
+  trinketItems = [
+    trinkets.IRIDEUS_FRAGMENT,
+    trinkets.SPOILS_OF_NELTHARUS,
+    trinkets.MIRROR_OF_FRACTURED_TOMORROWS,
+  ];
+
+  trinketSpells = [
+    spells.IRIDEUS_FRAGMENT,
+    spells.SPOILS_OF_NELTHARUS_CRIT,
+    spells.SPOILS_OF_NELTHARUS_HASTE,
+    spells.SPOILS_OF_NELTHARUS_MASTERY,
+    spells.SPOILS_OF_NELTHARUS_VERSATILITY,
+    spells.MIRROR_OF_FRACTURED_TOMORROWS,
+  ];
+
+  trackedPotions = [Spell.ELEMENTAL_POTION_OF_ULTIMATE_POWER, Spell.ELEMENTAL_POTION_OF_POWER];
+
+  foundTrinket = this.selectedCombatant.hasTrinket(trinkets.IRIDEUS_FRAGMENT.id)
+    ? spells.IRIDEUS_FRAGMENT.id
+    : this.selectedCombatant.hasTrinket(trinkets.SPOILS_OF_NELTHARUS.id)
+    ? spells.SPOILS_OF_NELTHARUS_CRIT.id
+    : this.selectedCombatant.hasTrinket(trinkets.MIRROR_OF_FRACTURED_TOMORROWS.id)
+    ? spells.MIRROR_OF_FRACTURED_TOMORROWS.id
+    : undefined;
+
+  constructor(options: Options) {
+    super(options);
+
+    /** DEBUFF EVENTS */
+    this.addEventListener(
+      Events.applydebuff.by(SELECTED_PLAYER).spell(SPELLS.TEMPORAL_WOUND_DEBUFF),
+      (event) => {
+        this.onDebuffApply(event);
+      },
+    );
+    this.addEventListener(
+      Events.removedebuff.by(SELECTED_PLAYER).spell(SPELLS.TEMPORAL_WOUND_DEBUFF),
+      (event) => {
+        this.onDebuffRemove(event);
+      },
+    );
+
+    /** BUFF EVENTS */
+    this.addEventListener(
+      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.EBON_MIGHT_BUFF_EXTERNAL),
+      (event) => {
+        this.onEbonMightApply(event);
+      },
+    );
+    this.addEventListener(
+      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.EBON_MIGHT_BUFF_EXTERNAL),
+      (event) => {
+        this.onEbonMightRemove(event);
+      },
+    );
+
+    this.addEventListener(
+      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.SHIFTING_SANDS_BUFF),
+      (event) => {
+        this.onShiftingSandsApply(event);
+      },
+    );
+    this.addEventListener(
+      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.SHIFTING_SANDS_BUFF),
+      (event) => {
+        this.onShiftingSandsRemove(event);
+      },
+    );
+    /** CAST EVENTS */
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER).spell(TALENTS.BREATH_OF_EONS_TALENT),
+      (event) => {
+        this.onBreathCast(event);
+      },
+    );
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(this.trackedSpells), (event) => {
+      this.onCast(event);
+    });
+    this.addEventListener(Events.empowerEnd.by(SELECTED_PLAYER).spell(this.empowers), (event) => {
+      this.onCast(event);
+    });
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(this.trinketSpells), (event) => {
+      this.onCast(event);
+    });
+    this.addEventListener(Events.cast.by(SELECTED_PLAYER).spell(this.trackedPotions), (event) => {
+      this.onCast(event);
+    });
+    /** SPELL USABLE EVENTS */
+    this.addEventListener(
+      Events.UpdateSpellUsable.by(SELECTED_PLAYER).spell(this.empowers),
+      (event) => {
+        this.onUpdateSpellUsable(event);
+      },
+    );
+    this.addEventListener(
+      Events.UpdateSpellUsable.by(SELECTED_PLAYER).spell(this.trinketSpells),
+      (event) => {
+        this.onUpdateSpellUsable(event);
+      },
+    );
+    /** FIGHT END */
+    this.addEventListener(Events.fightend, this.finalize);
+  }
+
+  private onBreathCast(event: CastEvent) {
+    /** Grab all the related events regarding Breath of Eons
+     * The relevant Debuff events have been linked to the cast event by the CastLinkNormalizer
+     * This makes analysis much easier since we would otherwise need to do a lot of filtering
+     * Due to other Augmentations apparently also wanting to use Breath of Eons alongside you :) */
+    const flightEvents = getBreathOfEonsBuffEvents(event);
+    const flightData: SpellTracker[] = [];
+
+    // Create counter for Flight Time
+    flightEvents.forEach((flightEvent) => {
+      if (flightEvent.type === EventType.ApplyBuff) {
+        flightData.push({ timestamp: flightEvent.timestamp, count: 0 });
+        flightData.push({
+          timestamp: flightEvent.timestamp,
+          count: getBreathOfEonsDebuffApplyEvents(event).length / 2,
+        });
+      } else {
+        flightData.push({ timestamp: flightEvent.timestamp, count: 0 });
+      }
+    });
+
+    // -1 indicates no proper use trinket
+    let trinketReady = this.foundTrinket
+      ? this.spellUsable.isAvailable(this.foundTrinket)
+        ? 1
+        : 0
+      : -1;
+    let trinketUsed = 0;
+    // Check if Trinket was used pre-breath
+    this.trinketSpells.forEach((trinket) => {
+      if (this.selectedCombatant.hasBuff(trinket.id)) {
+        trinketUsed = 1;
+        trinketReady = 1;
+      }
+    });
+
+    let potionReady = this.spellUsable.isAvailable(Spell.ELEMENTAL_POTION_OF_POWER.id) ? 1 : 0;
+    let potionUsed = 0;
+    // Check if Potion was used pre-breath
+    this.trackedPotions.forEach((potion) => {
+      if (this.selectedCombatant.hasBuff(potion.id)) {
+        potionUsed = 1;
+        potionReady = 1;
+      }
+    });
+
+    this.windows.push({
+      flightData: flightData,
+
+      breathPerformance: {
+        temporalWoundsCounter: [],
+        ebonMightDroppedDuringBreath: false,
+        ebonMightDroppedDuration: 0,
+        possibleTrinkets: trinketReady,
+        trinketUsed: trinketUsed,
+        possiblePotions: potionReady,
+        potionUsed: potionUsed,
+        fireBreaths: 0,
+        possibleFireBreaths: this.spellUsable.isAvailable(this.fireBreath.id) ? 1 : 0,
+        upheavels: 0,
+        possibleUpheavels: this.spellUsable.isAvailable(this.upheavel.id) ? 1 : 0,
+        timeskipTalented:
+          this.selectedCombatant.hasTalent(TALENTS.TIME_SKIP_TALENT) &&
+          !this.selectedCombatant.hasTalent(TALENTS.INTERWOVEN_THREADS_TALENT),
+        possibleTimeSkips: this.spellUsable.isAvailable(TALENTS.TIME_SKIP_TALENT.id) ? 1 : 0,
+        timeSkips: 0,
+        damageProblemPoints: [],
+        ebonMightProblems: [],
+        earlyDeaths: 0,
+        succesfulHits: 0,
+        potentialLostDamage: 0,
+        damage: 0,
+      },
+
+      start: 0,
+      end: 0,
+    });
+
+    this.totalCasts = this.totalCasts + 1;
+  }
+
+  /** Track casts inside of Breath Windows */
+  private onCast(event: CastEvent | EmpowerEndEvent) {
+    if (!this.breathWindowActive) {
+      return;
+    }
+    const perfWindow = this.currentPerformanceBreathWindow;
+    if (event.ability.guid === this.fireBreath.id) {
+      perfWindow.fireBreaths += 1;
+    } else if (event.ability.guid === this.upheavel.id) {
+      perfWindow.upheavels += 1;
+    } else if (event.ability.guid === TALENTS.TIME_SKIP_TALENT.id) {
+      perfWindow.timeSkips += 1;
+    } else if (event.ability.guid === this.foundTrinket) {
+      perfWindow.trinketUsed += 1;
+    } else if (
+      event.ability.guid === this.trackedPotions[0].id ||
+      event.ability.guid === this.trackedPotions[1].id
+    ) {
+      perfWindow.potionUsed += 1;
+    }
+  }
+
+  /** Track when spells/trinkets/pots become ready during Breath Windows */
+  private onUpdateSpellUsable(event: UpdateSpellUsableEvent) {
+    if (this.breathWindowActive && event.updateType === UpdateSpellUsableType.EndCooldown) {
+      const perfWindow = this.currentPerformanceBreathWindow;
+      if (event.ability.guid === this.fireBreath.id) {
+        perfWindow.possibleFireBreaths += 1;
+      } else if (event.ability.guid === this.upheavel.id) {
+        perfWindow.possibleUpheavels += 1;
+      } else if (event.ability.guid === this.foundTrinket) {
+        perfWindow.possibleTrinkets += 1;
+      }
+    }
+  }
+
+  /** Keep track of Ebon Might, if it gets applied during an active
+   * window, count the lost uptime.
+   * Push to the Ebon Might counter used for the graph */
+  private onEbonMightApply(event: ApplyBuffEvent) {
+    this.isEbonMightActive = true;
+    this.ebonMightCounter += 1;
+
+    if (
+      this.breathWindowActive &&
+      event.timestamp !== this.latestEbonMightEvent.timestamp &&
+      this.latestEbonMightDrop
+    ) {
+      const droppedDuration = event.timestamp - this.latestEbonMightDrop.timestamp;
+      this.currentPerformanceBreathWindow.ebonMightDroppedDuration += droppedDuration;
+    }
+    this.latestEbonMightEvent = event;
+    this.ebonMightCount.push({ timestamp: event.timestamp, count: this.ebonMightCounter });
+  }
+
+  /** Keep track of Ebon Might, if it gets removed during an active
+   * window, mark it down as a problem point, used for the graph. */
+  private onEbonMightRemove(event: RemoveBuffEvent) {
+    this.isEbonMightActive = false;
+    this.latestEbonMightEvent = event;
+    this.ebonMightCounter -= 1;
+
+    this.ebonMightCount.push({ timestamp: event.timestamp, count: this.ebonMightCounter });
+
+    if (this.breathWindowActive) {
+      const perfWindow = this.currentPerformanceBreathWindow;
+      perfWindow.ebonMightDroppedDuringBreath = true;
+      this.latestEbonMightDrop = event;
+
+      const prevProblem = perfWindow.ebonMightProblems[perfWindow.ebonMightProblems.length - 1];
+
+      const ebonMightProblem = {
+        timestamp: event.timestamp,
+        count: this.ebonMightCounter,
+      };
+
+      // If you drop multiple Ebon Mights at the same time, only push one Problem point
+      if (prevProblem && prevProblem.timestamp === event.timestamp) {
+        perfWindow.ebonMightProblems[perfWindow.ebonMightProblems.length - 1] = ebonMightProblem;
+      } else {
+        perfWindow.ebonMightProblems.push(ebonMightProblem);
+      }
+    }
+  }
+
+  private onShiftingSandsApply(event: ApplyBuffEvent) {
+    this.shiftingsSandsCounter += 1;
+
+    this.shiftingSandsCount.push({
+      timestamp: event.timestamp,
+      count: this.shiftingsSandsCounter,
+    });
+  }
+
+  private onShiftingSandsRemove(event: RemoveBuffEvent) {
+    this.shiftingsSandsCounter -= 1;
+
+    this.shiftingSandsCount.push({
+      timestamp: event.timestamp,
+      count: this.shiftingsSandsCounter,
+    });
+  }
+
+  private onDebuffApply(event: ApplyDebuffEvent) {
+    if (this.currentBreathWindow.start === 0) {
+      this.breathWindowActive = true;
+
+      this.currentPerformanceBreathWindow.temporalWoundsCounter = [
+        {
+          timestamp: event.timestamp,
+          count: 0,
+        },
+      ];
+      this.currentBreathWindow.start = event.timestamp;
+    }
+
+    this.activeDebuffs = this.activeDebuffs + 1;
+
+    this.currentPerformanceBreathWindow.temporalWoundsCounter.push({
+      timestamp: event.timestamp,
+      count: this.activeDebuffs,
+    });
+  }
+
+  /** Here we keep track of removal of Temporal Wounds
+   * we figure out if the removal was because it ran out (good),
+   * or if it was because the mob died early (bad).
+   * If a mob dies early you lose out on ALL the damage gathered within it
+   * so it is a definitive problem point which we mark.
+   *
+   * There is also some calculation of potential damage loss
+   * this is kinda guesstimaty cus prio damage and what not
+   * it could be (somewhat, still have some contributions missing) accurately
+   * be tracked using WCL events.*/
+  private onDebuffRemove(event: RemoveDebuffEvent) {
+    this.activeDebuffs -= 1;
+    const perfWindow = this.currentPerformanceBreathWindow;
+    const breathWindow = this.currentBreathWindow;
+    perfWindow.temporalWoundsCounter.push({
+      timestamp: event.timestamp,
+      count: this.activeDebuffs,
+    });
+
+    const damageEvents = getBreathOfEonsDamageEvents(event);
+    const damageFound = damageEvents.length > 0;
+    if (!damageFound) {
+      perfWindow.damageProblemPoints.push({
+        timestamp: event.timestamp,
+        count: this.activeDebuffs,
+      });
+      perfWindow.earlyDeaths += 1;
+    } else {
+      damageEvents.forEach((damageEvent) => {
+        perfWindow.damage += damageEvent.amount ?? damageEvent.absorbed;
+      });
+      perfWindow.succesfulHits += 1;
+    }
+
+    if (
+      this.activeDebuffs === 0 &&
+      !this.selectedCombatant.hasBuff(TALENTS.BREATH_OF_EONS_TALENT.id)
+    ) {
+      this.breathWindowActive = false;
+
+      const ebonMightDroppedDuringBreath = perfWindow.ebonMightDroppedDuringBreath;
+      const ebonMightDroppedDuration = perfWindow.ebonMightDroppedDuration;
+
+      if (ebonMightDroppedDuringBreath && ebonMightDroppedDuration === 0) {
+        const droppedUptime = event.timestamp - this.latestEbonMightDrop.timestamp;
+        perfWindow.ebonMightDroppedDuration = droppedUptime;
+      }
+
+      breathWindow.breathPerformance = perfWindow;
+      breathWindow.end = event.timestamp;
+
+      perfWindow.temporalWoundsCounter.push({
+        timestamp: event.timestamp + GRAPHBUFFER,
+        count: 0,
+      });
+
+      // Calculate potential damage loss
+      // We add a multiplier to account for prio target damage
+      const PRIO_MULTIPLER = 0.8;
+      const potentialDamagePerTarget = perfWindow.damage / perfWindow.succesfulHits;
+      perfWindow.potentialLostDamage =
+        potentialDamagePerTarget * perfWindow.earlyDeaths * PRIO_MULTIPLER;
+    }
+  }
+
+  private finalize() {
+    // Clean up current window if it runs out during combat end / death etc..
+    const breathWindow = this.currentBreathWindow;
+    if (breathWindow.end === 0) {
+      breathWindow.end = this.owner.fight.end_time;
+
+      // Incase something weird happened make sure we don't make a super long window
+      // Length doesnt really matter too much if you dead so we just
+      // Cap it at 14s.
+      const windowSudoMaxLenght = 1400;
+      if (breathWindow.end - breathWindow.start > windowSudoMaxLenght) {
+        breathWindow.end = breathWindow.start + windowSudoMaxLenght;
+      }
+      this.ebonMightCount.push({
+        timestamp: this.owner.fight.end_time,
+        count: this.ebonMightCounter,
+      });
+      this.currentPerformanceBreathWindow.temporalWoundsCounter.push({
+        timestamp: this.owner.fight.end_time,
+        count: this.activeDebuffs,
+      });
+    }
+  }
+
+  get currentPerformanceBreathWindow() {
+    return this.windows[this.totalCasts - 1].breathPerformance;
+  }
+
+  get currentBreathWindow() {
+    return this.windows[this.totalCasts - 1];
+  }
+
+  guideSubsection(): JSX.Element | null {
+    if (!this.active) {
+      return null;
+    }
+    return (
+      <BreathOfEonsSection
+        windows={this.windows}
+        fightStartTime={this.owner.fight.start_time}
+        ebonMightCount={this.ebonMightCount}
+        shiftingSandsCount={this.shiftingSandsCount}
+      />
+    );
+  }
+}
+
+export default BreathOfEonsRotational;
