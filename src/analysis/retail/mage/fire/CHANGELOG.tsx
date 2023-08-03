923ce2e2c4dc73439d3192936c6acc7914ef59c8
Sharrq <cjgrasty86@gmail.com>
2023-08-02 22:53:49 -0400

    changelog and 10.1.5 support

--- src/analysis/retail/mage/fire/CHANGELOG.tsx
+++ src/analysis/retail/mage/fire/CHANGELOG.tsx
@@ -1,10 +1,18 @@
 import { change, date } from 'common/changelog';
+import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/mage';
 import { SpellLink } from 'interface';
 import { Sharrq, ToppleTheNun } from 'CONTRIBUTORS';
 
 // prettier-ignore
 export default [
+  change(date(2023, 8, 2), <>Marked the spec supported for 10.1.5</>, Sharrq),
+  change(date(2023, 8, 2), <>Added support for <SpellLink spell={TALENTS.UNLEASHED_INFERNO_TALENT} /></>, Sharrq),
+  change(date(2023, 8, 2), <>Added tracking for wasted <SpellLink spell={SPELLS.HOT_STREAK} /> uses while <SpellLink spell={TALENTS.SUN_KINGS_BLESSING_TALENT} /> was ready</>, Sharrq),
+  change(date(2023, 8, 2), <>Updated <SpellLink spell={TALENTS.FEEL_THE_BURN_TALENT} /> tracking</>, Sharrq),
+  change(date(2023, 8, 2), <>Added support for <SpellLink spell={SPELLS.CHARRING_EMBERS_DEBUFF} /></>, Sharrq),
+  change(date(2023, 8, 2), <>Removed <SpellLink spell={TALENTS.PHOENIX_FLAMES_TALENT} /> Pooling</>, Sharrq),
+  change(date(2023, 8, 2), <>Added support for <SpellLink spell={SPELLS.CHARRING_EMBERS_DEBUFF} /></>, Sharrq),
   change(date(2023, 7, 10), 'Remove references to 10.1.5 removed talents.', Sharrq),
   change(date(2023, 7, 3), 'Update SpellLink usage.', ToppleTheNun),
   change(date(2023, 6, 27), <>Added <SpellLink spell={TALENTS.TEMPORAL_WARP_TALENT} /> to list of Bloodlust Buffs.</>, Sharrq),
