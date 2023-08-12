daae6683bcd6ef4597163a60679e7bb76effdd99
Yajinni <yajinni@gmail.com>
2023-08-12 01:52:58 -0400

    big update to abilties file

--- src/analysis/retail/deathknight/blood/modules/Abilities.ts
+++ src/analysis/retail/deathknight/blood/modules/Abilities.ts
@@ -10,18 +10,20 @@ class Abilities extends CoreAbilities {
   spellbook(): SpellbookAbility[] {
     const combatant = this.selectedCombatant;
     return [
+      //Defensives
       {
         spell: TALENTS.ICEBOUND_FORTITUDE_TALENT.id,
         buffSpellId: TALENTS.ICEBOUND_FORTITUDE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.ICEBOUND_FORTITUDE_TALENT),
         category: SPELL_CATEGORY.DEFENSIVE,
-        cooldown: 180,
+        cooldown: combatant.hasTalent(TALENTS.ACCLIMATION_TALENT) ? 120 : 180,
         castEfficiency: {
           suggestion: true,
           recommendedEfficiency: 0.5,
           extraSuggestion: defineMessage({
             id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
             message:
-              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake or to take the edge of big attacks.',
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
           }),
           importance: ISSUE_IMPORTANCE.MINOR,
         },
@@ -30,6 +32,7 @@ class Abilities extends CoreAbilities {
       {
         spell: TALENTS.VAMPIRIC_BLOOD_TALENT.id,
         buffSpellId: TALENTS.VAMPIRIC_BLOOD_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.VAMPIRIC_BLOOD_TALENT),
         category: SPELL_CATEGORY.DEFENSIVE,
         cooldown: 90,
         castEfficiency: {
@@ -38,15 +41,166 @@ class Abilities extends CoreAbilities {
           extraSuggestion: defineMessage({
             id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
             message:
-              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake or to take the edge of big attacks.',
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
           }),
           importance: ISSUE_IMPORTANCE.MINOR,
         },
         timelineSortIndex: 10,
       },
       {
+        spell: TALENTS.ANTI_MAGIC_SHELL_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.ANTI_MAGIC_SHELL_TALENT),
+        buffSpellId: TALENTS.ANTI_MAGIC_SHELL_TALENT.id,
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: combatant.hasTalent(TALENTS.ANTI_MAGIC_BARRIER_TALENT) ? 40 : 60,
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+          importance: ISSUE_IMPORTANCE.MINOR,
+        },
+        timelineSortIndex: 10,
+      },
+      {
+        spell: TALENTS.ANTI_MAGIC_ZONE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.ANTI_MAGIC_ZONE_TALENT),
+        buffSpellId: SPELLS.ANTI_MAGIC_ZONE_TALENT_BUFF.id,
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: 120,
+        gcd: {
+          base: 1500,
+        },
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+          importance: ISSUE_IMPORTANCE.MINOR,
+        },
+        isDefensive: true,
+      },
+      {
+        spell: TALENTS.SACRIFICIAL_PACT_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.SACRIFICIAL_PACT_TALENT),
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: 120,
+        gcd: null,
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+          importance: ISSUE_IMPORTANCE.MINOR,
+        },
+        isDefensive: true,
+      },
+      {
+        spell: TALENTS.DEATH_PACT_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.DEATH_PACT_TALENT),
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: 120,
+        gcd: null,
+        isDefensive: true,
+      },
+      {
+        spell: SPELLS.LICHBORNE.id,
+        enabled: combatant.hasTalent(TALENTS.UNHOLY_ENDURANCE_TALENT), //Provides 15% DR if you take this talent
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: 120,
+        gcd: null,
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+          importance: ISSUE_IMPORTANCE.MINOR,
+        },
+        isDefensive: true,
+      },
+      {
+        spell: TALENTS.CONSUMPTION_TALENT.id,
+        category: SPELL_CATEGORY.SEMI_DEFENSIVE,
+        enabled: combatant.hasTalent(TALENTS.CONSUMPTION_TALENT),
+        cooldown: 45,
+        gcd: {
+          base: 1500,
+        },
+        timelineSortIndex: 5,
+      },
+      {
+        spell: TALENTS.RUNE_TAP_TALENT.id,
+        buffSpellId: TALENTS.RUNE_TAP_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.RUNE_TAP_TALENT),
+        category: SPELL_CATEGORY.DEFENSIVE,
+        cooldown: 25,
+        charges: 2,
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+          importance: ISSUE_IMPORTANCE.MINOR,
+        },
+        timelineSortIndex: 10,
+      },
+      {
+        spell: TALENTS.MARK_OF_BLOOD_TALENT.id,
+        category: SPELL_CATEGORY.DEFENSIVE,
+        enabled: combatant.hasTalent(TALENTS.MARK_OF_BLOOD_TALENT),
+        cooldown: 6,
+        gcd: {
+          base: 1500,
+        },
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.9,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake, to take the edge of big attacks, or heal up quickly after a large hit.',
+          }),
+        },
+        timelineSortIndex: 10,
+      },
+      //Rotational
+      {
+        spell: TALENTS.ABOMINATION_LIMB_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.ABOMINATION_LIMB_TALENT),
+        category: SPELL_CATEGORY.ROTATIONAL,
+        cooldown: 120,
+        gcd: {
+          base: 1500,
+        },
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.9,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.abominationlimb',
+            message:
+              'Should be used on CD. You may delay it a little to not over cap on bone shield charges or if you need its pulling of mobs.',
+          }),
+        },
+      },
+      {
         spell: TALENTS.BLOOD_BOIL_TALENT.id,
         category: SPELL_CATEGORY.ROTATIONAL,
+        enabled: combatant.hasTalent(TALENTS.BLOOD_BOIL_TALENT),
         cooldown: (haste) => 7.5 / (1 + haste),
         gcd: {
           base: 1500,
@@ -63,19 +217,10 @@ class Abilities extends CoreAbilities {
         timelineSortIndex: 4,
       },
       {
-        spell: TALENTS.CONSUMPTION_TALENT.id,
-        category: SPELL_CATEGORY.SEMI_DEFENSIVE,
-        enabled: combatant.hasTalent(TALENTS.CONSUMPTION_TALENT),
-        cooldown: 45,
-        gcd: {
-          base: 1500,
-        },
-        timelineSortIndex: 5,
-      },
-      {
         spell: TALENTS.DANCING_RUNE_WEAPON_TALENT.id,
-        category: SPELL_CATEGORY.SEMI_DEFENSIVE,
+        category: SPELL_CATEGORY.ROTATIONAL,
         buffSpellId: SPELLS.DANCING_RUNE_WEAPON_TALENT_BUFF.id,
+        enabled: combatant.hasTalent(TALENTS.DANCING_RUNE_WEAPON_TALENT),
         gcd: {
           base: 1500,
         },
@@ -85,7 +230,27 @@ class Abilities extends CoreAbilities {
           recommendedEfficiency: 0.9,
           extraSuggestion: defineMessage({
             id: 'deathknight.blood.abilities.extraSuggestion.dancingRuneWeapon',
-            message: 'Should be used as an opener and used on CD for the dps boost.',
+            message:
+              'Should be used as an opener and usally used on CD. You may delay it a little so as to not over cap bone charges to much or if you know there is a large pack of adds incoming.',
+          }),
+        },
+        timelineSortIndex: 9,
+      },
+      {
+        spell: TALENTS.EMPOWER_RUNE_WEAPON_SHARED_TALENT.id,
+        category: SPELL_CATEGORY.ROTATIONAL,
+        //buff spell id later?
+        enabled: combatant.hasTalent(TALENTS.EMPOWER_RUNE_WEAPON_SHARED_TALENT),
+        gcd: {
+          base: 1500,
+        },
+        cooldown: 120,
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.9,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.empowerruneweapon',
+            message: 'Be mindful of the haste/runic power increase and make use of it.',
           }),
         },
         timelineSortIndex: 9,
@@ -109,6 +274,15 @@ class Abilities extends CoreAbilities {
         timelineSortIndex: 6,
       },
       {
+        spell: TALENTS.SOUL_REAPER_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.SOUL_REAPER_TALENT),
+        category: SPELL_CATEGORY.ROTATIONAL,
+        gcd: {
+          base: 1500,
+        },
+        timelineSortIndex: 7,
+      },
+      {
         spell: TALENTS.DEATH_STRIKE_TALENT.id,
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: {
@@ -117,7 +291,9 @@ class Abilities extends CoreAbilities {
         timelineSortIndex: 1,
       },
       {
+        //Check if there is a time to ever use this
         spell: TALENTS.DEATHS_CARESS_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.DEATHS_CARESS_TALENT),
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: {
           base: 1500,
@@ -125,6 +301,7 @@ class Abilities extends CoreAbilities {
         timelineSortIndex: 7,
       },
       {
+        //should this be used without procs?
         spell: SPELLS.DEATH_AND_DECAY.id,
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: {
@@ -145,12 +322,12 @@ class Abilities extends CoreAbilities {
         gcd: {
           base: 1500,
         },
-        enabled: !combatant.hasTalent(TALENTS.RAPID_DECOMPOSITION_TALENT),
         cooldown: 15,
         timelineSortIndex: 5,
       },
       {
         spell: TALENTS.HEART_STRIKE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.HEART_STRIKE_TALENT),
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: {
           base: 1500,
@@ -159,6 +336,7 @@ class Abilities extends CoreAbilities {
       },
       {
         spell: TALENTS.MARROWREND_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.MARROWREND_TALENT),
         buffSpellId: SPELLS.BONE_SHIELD.id,
         category: SPELL_CATEGORY.ROTATIONAL,
         gcd: {
@@ -167,26 +345,74 @@ class Abilities extends CoreAbilities {
         timelineSortIndex: 2,
       },
       {
-        spell: TALENTS.ANTI_MAGIC_SHELL_TALENT.id,
-        buffSpellId: TALENTS.ANTI_MAGIC_SHELL_TALENT.id,
-        category: SPELL_CATEGORY.DEFENSIVE,
-        cooldown: combatant.hasTalent(TALENTS.ANTI_MAGIC_BARRIER_TALENT) ? 40 : 60,
+        spell: TALENTS.RAISE_DEAD_SHARED_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.RAISE_DEAD_SHARED_TALENT),
+        category: SPELL_CATEGORY.ROTATIONAL,
+        cooldown: 120,
+      },
+      {
+        spell: TALENTS.BLOOD_TAP_TALENT.id,
+        category: SPELL_CATEGORY.ROTATIONAL,
+        enabled: combatant.hasTalent(TALENTS.BLOOD_TAP_TALENT),
+        cooldown: 60,
+        charges: 2,
         castEfficiency: {
           suggestion: true,
           recommendedEfficiency: 0.5,
           extraSuggestion: defineMessage({
-            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
-            message:
-              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake or to take the edge of big attacks.',
+            id: 'deathknight.blood.abilities.extraSuggestion.bloodTap',
+            message: 'Use to generate extra runes at opportune times.',
           }),
           importance: ISSUE_IMPORTANCE.MINOR,
         },
         timelineSortIndex: 10,
       },
       {
+        spell: TALENTS.TOMBSTONE_TALENT.id,
+        category: SPELL_CATEGORY.ROTATIONAL,
+        enabled: combatant.hasTalent(TALENTS.TOMBSTONE_TALENT),
+        cooldown: 60,
+        gcd: {
+          base: 1500,
+        },
+        castEfficiency: {
+          suggestion: true,
+          recommendedEfficiency: 0.5,
+          extraSuggestion: defineMessage({
+            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
+            message:
+              'Use this on CD and try to only use it when it consumes 5 boneshield charges and can reduce 20 seconds off dancing rune weapon.',
+          }),
+        },
+        timelineSortIndex: 10,
+      },
+      {
+        spell: TALENTS.BONESTORM_TALENT.id,
+        category: SPELL_CATEGORY.ROTATIONAL_AOE,
+        enabled: combatant.hasTalent(TALENTS.BONESTORM_TALENT),
+        cooldown: 60,
+        gcd: {
+          base: 1500,
+        },
+        timelineSortIndex: 9,
+      },
+      //Utility
+      {
         spell: TALENTS.MIND_FREEZE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.MIND_FREEZE_TALENT),
         category: SPELL_CATEGORY.UTILITY,
         cooldown: 15,
+      },
+      {
+        spell: TALENTS.CHAINS_OF_ICE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.CHAINS_OF_ICE_TALENT),
+        category: SPELL_CATEGORY.UTILITY,
+      },
+      {
+        spell: TALENTS.BLINDING_SLEET_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.BLINDING_SLEET_TALENT),
+        category: SPELL_CATEGORY.UTILITY,
+        cooldown: 60,
       },
       {
         spell: SPELLS.DARK_COMMAND.id,
@@ -221,6 +447,7 @@ class Abilities extends CoreAbilities {
       },
       {
         spell: TALENTS.GOREFIENDS_GRASP_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.GOREFIENDS_GRASP_TALENT),
         category: SPELL_CATEGORY.UTILITY,
         gcd: {
           base: 1500,
@@ -237,6 +464,7 @@ class Abilities extends CoreAbilities {
       },
       {
         spell: TALENTS.ASPHYXIATE_TALENT.id,
+        enabled: combatant.hasTalent(TALENTS.ASPHYXIATE_TALENT),
         category: SPELL_CATEGORY.UTILITY,
         cooldown: 45,
         gcd: {
@@ -245,99 +473,11 @@ class Abilities extends CoreAbilities {
       },
       {
         spell: TALENTS.CONTROL_UNDEAD_TALENT.id,
-        category: SPELL_CATEGORY.OTHERS,
+        enabled: combatant.hasTalent(TALENTS.CONTROL_UNDEAD_TALENT),
+        category: SPELL_CATEGORY.UTILITY,
         gcd: {
           base: 1500,
         },
-      },
-      {
-        spell: SPELLS.RAISE_DEAD_BLOOD_FROST.id,
-        category: SPELL_CATEGORY.COOLDOWNS,
-        cooldown: 120,
-      },
-      {
-        spell: TALENTS.ANTI_MAGIC_ZONE_TALENT.id,
-        buffSpellId: SPELLS.ANTI_MAGIC_ZONE_TALENT_BUFF.id,
-        category: SPELL_CATEGORY.DEFENSIVE,
-        cooldown: 120,
-        gcd: null,
-        isDefensive: true,
-      },
-      {
-        spell: TALENTS.MARK_OF_BLOOD_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL,
-        enabled: combatant.hasTalent(TALENTS.MARK_OF_BLOOD_TALENT),
-        cooldown: 6,
-        gcd: {
-          base: 1500,
-        },
-        timelineSortIndex: 10,
-      },
-      {
-        spell: TALENTS.TOMBSTONE_TALENT.id,
-        category: SPELL_CATEGORY.DEFENSIVE,
-        enabled: combatant.hasTalent(TALENTS.TOMBSTONE_TALENT),
-        cooldown: 60,
-        gcd: {
-          base: 1500,
-        },
-        castEfficiency: {
-          suggestion: true,
-          recommendedEfficiency: 0.5,
-          extraSuggestion: defineMessage({
-            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
-            message:
-              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake or to take the edge of big attacks.',
-          }),
-          importance: ISSUE_IMPORTANCE.MINOR,
-        },
-        timelineSortIndex: 10,
-      },
-      {
-        spell: TALENTS.RUNE_TAP_TALENT.id,
-        buffSpellId: TALENTS.RUNE_TAP_TALENT.id,
-        enabled: combatant.hasTalent(TALENTS.RUNE_TAP_TALENT),
-        category: SPELL_CATEGORY.DEFENSIVE,
-        cooldown: 25,
-        charges: 2,
-        castEfficiency: {
-          suggestion: true,
-          recommendedEfficiency: 0.5,
-          extraSuggestion: defineMessage({
-            id: 'deathknight.blood.abilities.extraSuggestion.defensiveCd',
-            message:
-              'Defensive CDs like this are meant to be used smartly. Use it to smooth regular damage intake or to take the edge of big attacks.',
-          }),
-          importance: ISSUE_IMPORTANCE.MINOR,
-        },
-        timelineSortIndex: 10,
-      },
-      {
-        spell: TALENTS.BLOOD_TAP_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL,
-        enabled: combatant.hasTalent(TALENTS.BLOOD_TAP_TALENT),
-        cooldown: 60,
-        charges: 2,
-        castEfficiency: {
-          suggestion: true,
-          recommendedEfficiency: 0.5,
-          extraSuggestion: defineMessage({
-            id: 'deathknight.blood.abilities.extraSuggestion.bloodTap',
-            message: 'Use to generate extra runes at opportune times.',
-          }),
-          importance: ISSUE_IMPORTANCE.MINOR,
-        },
-        timelineSortIndex: 10,
-      },
-      {
-        spell: TALENTS.BONESTORM_TALENT.id,
-        category: SPELL_CATEGORY.ROTATIONAL_AOE,
-        enabled: combatant.hasTalent(TALENTS.BONESTORM_TALENT),
-        cooldown: 60,
-        gcd: {
-          base: 1500,
-        },
-        timelineSortIndex: 9,
       },
       {
         spell: SPELLS.RUNE_1.id,
@@ -365,21 +505,6 @@ class Abilities extends CoreAbilities {
           return 10 / (1 + haste) / (1 + multiplier);
         },
         charges: 2,
-      },
-
-      // covenants
-      {
-        spell: TALENTS.ABOMINATION_LIMB_TALENT.id,
-        category: SPELL_CATEGORY.COOLDOWNS,
-        cooldown: 120,
-        gcd: {
-          base: 1500,
-        },
-        castEfficiency: {
-          suggestion: true,
-          recommendedEfficiency: 0.9,
-        },
-        enabled: false,
       },
     ];
   }
