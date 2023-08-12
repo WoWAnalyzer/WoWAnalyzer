74f16d6e5cb1a1b198572f899df1bcf0d06eb34b
emallson <emallson@emallson.net>
2023-08-12 17:18:28 -0400

    add pandemic support to buffRemaining

--- src/parser/shared/metrics/apl/conditions/buffRemaining.tsx
+++ src/parser/shared/metrics/apl/conditions/buffRemaining.tsx
@@ -2,43 +2,70 @@ import type Spell from 'common/SPELLS/Spell';
 import { SpellLink } from 'interface';
 import {
   ApplyBuffEvent,
-  RefreshBuffEvent,
-  RemoveBuffEvent,
   ApplyBuffStackEvent,
   EventType,
+  RefreshBuffEvent,
+  RemoveBuffEvent,
 } from 'parser/core/Events';
 import { Condition, tenseAlt } from '../index';
-import { formatTimestampRange, Range } from './util';
+import { buffDuration, DurationData, formatTimestampRange, PandemicData, Range } from './util';
 
+type State = DurationData & {
+  previousEvent: ApplyBuffEvent | RefreshBuffEvent | ApplyBuffStackEvent | RemoveBuffEvent;
+};
+
+/**
+ * Assert that a buff has remaining time within `range`.
+ */
 export default function buffRemaining(
   spell: Spell,
-  buffDuration: number,
+  pandemic: PandemicData,
   range: Range,
-  //If true, then if the buff is missing it will be considered valid if only atMost is specified
+  /**
+   * If true, then if the buff is missing it will be considered valid if only atMost is specified
+   */
   missingCanBeValid: boolean = false,
-): Condition<ApplyBuffEvent | RefreshBuffEvent | RemoveBuffEvent | ApplyBuffStackEvent | null> {
+): Condition<State | undefined> {
   return {
     key: `buffRemaining-${spell.id}`,
-    init: () => null,
+    init: () => undefined,
     update: (state, event) => {
       if (event.type === EventType.ApplyBuff && event.ability.guid === spell.id) {
-        return event;
-      } else if (event.type === EventType.RefreshBuff && event.ability.guid === spell.id) {
-        return event;
+        return {
+          referenceTime: event.timestamp,
+          previousEvent: event,
+          timeRemaining: pandemic.duration,
+        };
+      } else if (
+        (event.type === EventType.RefreshBuff || event.type === EventType.ApplyBuffStack) &&
+        event.ability.guid === spell.id
+      ) {
+        // *sometimes* the game logs both a refresh and an applybuffstack at the same timestamp.
+        // this bit handles not extending the buff twice when that occurs.
+        if (
+          state &&
+          state.previousEvent.type !== event.type &&
+          state.previousEvent.timestamp === event.timestamp
+        ) {
+          return state;
+        }
+        return {
+          referenceTime: event.timestamp,
+          previousEvent: event,
+          timeRemaining: buffDuration(event.timestamp, state, pandemic),
+        };
       } else if (event.type === EventType.RemoveBuff && event.ability.guid === spell.id) {
-        return null;
-      } else if (event.type === EventType.ApplyBuffStack && event.ability.guid === spell.id) {
-        return event;
+        return undefined;
       } else {
         return state;
       }
     },
     validate: (state, event) => {
-      if (state === null) {
+      if (state === undefined) {
         //Buff is not applied so buff has no remaining time so only atMost can be true
         return Boolean(missingCanBeValid && range.atMost && !range.atLeast);
       }
-      const timeRemaining = state.timestamp + buffDuration - event.timestamp;
+      const timeRemaining = buffDuration(event.timestamp, state, pandemic);
       return (
         timeRemaining >= (range.atLeast || 0) && (!range.atMost || timeRemaining <= range.atMost)
       );
