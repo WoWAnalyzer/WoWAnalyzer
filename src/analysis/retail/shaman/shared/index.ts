7a0e6cc77bc05436a80188f3d5d4143899ab52bd
awildfivreld <12049229+awildfivreld@users.noreply.github.com>
2023-08-12 21:25:16 +0200

    add graph

--- src/analysis/retail/shaman/shared/index.ts
+++ src/analysis/retail/shaman/shared/index.ts
@@ -1,10 +1,6 @@
 export { default as ElementalBlast } from './ElementalBlast';
 export { default as AnkhNormalizer } from './core/AnkhNormalizer';
 export { default as FlameShock } from './core/FlameShock';
-export { default as Maelstrom } from './resources/Maelstrom';
-export { default as MaelstromDetails } from './resources/MaelstromDetails';
-export { default as MaelstromTab } from './resources/MaelstromTab';
-export { default as MaelstromTracker } from './resources/MaelstromTracker';
 export { default as AstralShift } from './talents/AstralShift';
 export { default as EarthShield } from './talents/EarthShield';
 export * from './talents/EarthShield';
