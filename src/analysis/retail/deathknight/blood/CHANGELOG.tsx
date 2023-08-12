482ae2721dea033c61208fb869eedca821592dbe
Yajinni <yajinni@gmail.com>
2023-08-12 01:59:24 -0400

    updated change log

--- src/analysis/retail/deathknight/blood/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/blood/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { SpellLink } from 'interface';
 
 
 export default [
+  change(date(2023, 8, 12), 'Large update to the abilities file to include many missing spells and talents.',Yajinni),
   change(date(2023, 8, 11), <><SpellLink spell={talents.RUNE_TAP_TALENT} /> Updated it to show cast efficiency only if the talent was taken.</>,Yajinni),
   change(date(2023, 6, 19), 'Update SpellLink usage.', ToppleTheNun),
   change(date(2023, 4, 8), <><SpellLink spell={talents.DEATH_STRIKE_TALENT} /> casts while not tanking within the next 6 seconds now count as dumping RP.</>, emallson),
