145c21140b8cd8c610457784a3110a8e048f62ef
emallson <emallson@emallson.net>
2023-08-19 18:19:23 -0400

    fix a race condition in useEventParser that comes up with phases a lot
    when using `[value, setValue] = useState()`, calling `setValue` does not
    update the actual contents of `value` until the next time the hook is
    called.
    
    the body of the main `useEffect` in this hook was defensive about that,
    but if you selected a short phase (say 20s long) and then a long phase
    (say 2m long), then the last `useEffect` would run, observe the old
    value of `eventIndex` even after `setEventIndex(0)` along with the fact
    that `eventIndex < normalizedEvents.length`. This would cause it to
    begin processing the events in the new phase that occurred after (in
    this example) 20s into the phase, then call `setEventIndex(currentIndex)`
    with a non-zero value. The next time the hook was called, it would no
    longer be 0 and the processing would not start over from the beginning.
    
    the new setup is both more responsive (`requestIdleCallback` is better
    about letting user input happen) and a bit saner (not so many
    interleaving hooks that can cause re-renders)

--- src/interface/report/hooks/useEventParser.ts
+++ src/interface/report/hooks/useEventParser.ts
@@ -9,12 +9,12 @@ import Fight from 'parser/core/Fight';
 import EventEmitter from 'parser/core/modules/EventEmitter';
 import { PlayerInfo } from 'parser/core/Player';
 import Report from 'parser/core/Report';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const BENCHMARK = false;
 // Picking a correct batch duration is hard. I tried various durations to get the batch sizes to 1 frame, but that results in a lot of wasted time waiting for the next frame. 30ms (33 fps) as well causes a lot of wasted time. 60ms (16fps) seem to have really low wasted time while not blocking the UI anymore than a user might expect.
-const MAX_BATCH_DURATION = 66.67; // ms
+const MAX_BATCH_DURATION = 32; // ms
 const bench = (id: string) => console.time(id);
 const benchEnd = (id: string) => console.timeEnd(id);
 
@@ -57,7 +57,7 @@ const useEventParser = ({
 }: Props) => {
   const [isLoading, setIsLoading] = useState(true);
   const [progress, setProgress] = useState(0);
-  const [eventIndex, setEventIndex] = useState(0);
+  const eventIndexRef = useRef(0);
 
   const location = useLocation();
   const build = getBuild(location.pathname);
@@ -120,23 +120,25 @@ const useEventParser = ({
       //sort now normalized events to avoid new fabricated events like "prepull" casts etc being in incorrect order with casts "kept" from before the filter
       .sort((a, b) => a.timestamp - b.timestamp);
     benchEnd('normalizing events');
-    return result;
-  }, [events, parser]);
-
-  useEffect(() => {
-    setEventIndex(0);
-    setIsLoading(true);
+    eventIndexRef.current = 0;
     setProgress(0);
-  }, [normalizedEvents]);
+    setIsLoading(true);
+    return result;
+  }, [events, parser, eventIndexRef]);
 
   const eventEmitter = useMemo(() => parser?.getModule(EventEmitter), [parser]);
 
-  useEffect(() => {
-    if (parser === null || normalizedEvents === null || eventIndex >= normalizedEvents?.length) {
-      return;
+  const processEventBatch = useCallback(() => {
+    let done = false;
+    if (
+      parser === null ||
+      normalizedEvents === null ||
+      eventIndexRef.current >= normalizedEvents?.length
+    ) {
+      return done;
     }
 
-    let currentIndex = eventIndex;
+    let currentIndex = eventIndexRef.current;
 
     try {
       bench('event loop');
@@ -155,15 +157,46 @@ const useEventParser = ({
       captureException(err as Error);
       throw new EventsParseError(err as Error);
     } finally {
-      setEventIndex(currentIndex);
+      eventIndexRef.current = currentIndex;
       setProgress(Math.min(1, currentIndex / normalizedEvents.length));
       if (currentIndex === normalizedEvents.length) {
+        done = true;
         parser.finish();
         setIsLoading(false);
       }
       benchEnd('event loop');
     }
-  }, [parser, normalizedEvents, eventEmitter, eventIndex]);
+
+    return done;
+  }, [parser, normalizedEvents, eventEmitter]);
+
+  useEffect(() => {
+    // this setup handles event processing with relatively minimal blocking of the UI thread
+    //
+    // `requestIdleCallback` adds our callback to the queue to run the next time the browser is idle, aka not busy with animation or user input.
+    // The `timeout` option causes it to be added to the queue immediately if that does not occur within `timeout` ms (here, 50 ms)
+    //
+    // As long as event processing isn't done, we keep requesting idle callbacks to process batches. Once we're done (or the component is unmounted),
+    // we end the loop by not calling `next()`
+    let isCancelled = false;
+    const next = () =>
+      requestIdleCallback(
+        () => {
+          const done = processEventBatch();
+
+          if (!done && !isCancelled) {
+            next();
+          }
+        },
+        { timeout: 50 },
+      );
+
+    next();
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [processEventBatch]);
 
   return {
     isLoading,
