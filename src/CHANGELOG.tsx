a920d59f0802c9d883c0a5702c72c0a3e57a4ef0
emallson <emallson@emallson.net>
2023-08-19 18:25:50 -0400

    changelog message

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -27,6 +27,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2023, 8, 19), 'Add support for Warcraft Logs\' phase data in new logs.', emallson),
   change(date(2023, 8, 18), 'Update dependencies.', ToppleTheNun),
   change(date(2023, 8, 16), 'Add Classic buffs that effect haste rating', jazminite),
   change(date(2023, 8, 12), 'Add Classic character parse page', jazminite),
@@ -37,7 +38,7 @@ export default [
   change(date(2023, 8, 7), 'Remove some deprecated code.', ToppleTheNun),
   change(date(2023, 8, 1), 'Add Irideus Fragment and Mirror of Fractured Tomorrows trinkets.', Vollmer),
   change(date(2023, 8, 2), 'Add support for buffRemaining in the APL modules.', Putro),
-  change(date(2023, 7, 31), <>Add enchantment check for <ItemLink id={ITEMS.SHADOWED_BELT_CLASP_R3.id} />.</>,  ToppleTheNun),
+  change(date(2023, 7, 31), <>Add enchantment check for <ItemLink id={ITEMS.SHADOWED_BELT_CLASP_R3.id} />.</>, ToppleTheNun),
   change(date(2023, 7, 29), 'Fix another issue loading parses using character search', emallson),
   change(date(2023, 7, 29), 'Fix an issue loading parses using character search', Putro),
   change(date(2023, 7, 27), 'Add Sarkareth haste buff', Awildfivreld),
