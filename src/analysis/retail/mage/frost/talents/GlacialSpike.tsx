34e0f1a9da9a207ad265288b3e579d5d01ad7de5
Sharrq <cjgrasty86@gmail.com>
2023-08-20 23:40:11 -0400

    Glacial Spike

--- src/analysis/retail/mage/frost/talents/GlacialSpike.tsx
+++ src/analysis/retail/mage/frost/talents/GlacialSpike.tsx
@@ -5,11 +5,13 @@ import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/mage';
 import { SpellLink } from 'interface';
 import { TooltipElement } from 'interface';
-import Analyzer, { SELECTED_PLAYER, Options } from 'parser/core/Analyzer';
-import Events, { CastEvent, DamageEvent, FightEndEvent, HasTarget } from 'parser/core/Events';
+import { highlightInefficientCast } from 'interface/report/Results/Timeline/Casts';
+import Analyzer, { Options } from 'parser/core/Analyzer';
+import { EventType } from 'parser/core/Events';
 import { When, ThresholdStyle } from 'parser/core/ParseResults';
 import AbilityTracker from 'parser/shared/modules/AbilityTracker';
-import Enemies, { encodeTargetString } from 'parser/shared/modules/Enemies';
+import Enemies from 'parser/shared/modules/Enemies';
+import EventHistory from 'parser/shared/modules/EventHistory';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
@@ -18,92 +20,52 @@ class GlacialSpike extends Analyzer {
   static dependencies = {
     enemies: Enemies,
     abilityTracker: AbilityTracker,
+    eventHistory: EventHistory,
   };
   protected enemies!: Enemies;
   protected abilityTracker!: AbilityTracker;
-
-  lastCastEvent?: CastEvent;
-  lastCastDidDamage = false;
-  spikeShattered = 0;
-  spikeNotShattered = 0;
+  protected eventHistory!: EventHistory;
 
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.hasTalent(TALENTS.GLACIAL_SPIKE_TALENT);
+  }
 
-    this.addEventListener(
-      Events.cast.by(SELECTED_PLAYER).spell(TALENTS.GLACIAL_SPIKE_TALENT),
-      this.onGlacialSpikeCast,
+  nonShatterCasts = () => {
+    const spikeCasts = this.eventHistory.getEvents(EventType.Cast, {
+      spell: TALENTS.GLACIAL_SPIKE_TALENT,
+    });
+
+    //If Icy Veins was active, disregard it
+    let badCasts = spikeCasts.filter(
+      (gs) => !this.selectedCombatant.hasBuff(TALENTS.ICY_VEINS_TALENT.id, gs.timestamp),
     );
-    this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.GLACIAL_SPIKE_DAMAGE),
-      this.onGlacialSpikeDamage,
-    );
-    this.addEventListener(Events.fightend, this.onFightEnd);
-  }
 
-  onGlacialSpikeCast(event: CastEvent) {
-    if (this.lastCastEvent) {
-      this.flagTimeline(this.lastCastEvent);
-    }
+    //If Glacial Spike was shattered on the main target, disregard it
+    badCasts = badCasts.filter((gs) => {
+      const spikeDamage = this.eventHistory.getEvents(EventType.Damage, {
+        searchBackwards: false,
+        spell: SPELLS.GLACIAL_SPIKE_DAMAGE,
+        count: 1,
+        startTimestamp: gs.timestamp,
+        targetID: gs.targetID,
+        duration: 1500,
+      })[0];
+      const enemy = this.enemies.getEntity(spikeDamage);
+      return (
+        !enemy || !SHATTER_DEBUFFS.some((effect) => enemy.hasBuff(effect.id, spikeDamage.timestamp))
+      );
+    });
 
-    this.lastCastEvent = event;
-    this.lastCastDidDamage = false;
-  }
+    //Highlight bad casts on timeline
+    const tooltip = `You cast Glacial Spike without shattering it. You should only use Glacial Spike if you can shatter it via Winter's Chill or a similar effect, or if Icy Veins is active.`;
+    badCasts.forEach((c) => highlightInefficientCast(c, tooltip));
 
-  onGlacialSpikeDamage(event: DamageEvent) {
-    if (!this.lastCastEvent) {
-      return;
-    }
-    if (!HasTarget(this.lastCastEvent)) {
-      return;
-    }
-
-    const castTarget = encodeTargetString(
-      this.lastCastEvent.targetID,
-      this.lastCastEvent.targetInstance,
-    );
-    const damageTarget = encodeTargetString(event.targetID, event.targetInstance);
-
-    //We dont care about the Glacial Spikes that split to something else via Splitting Ice.
-    if (castTarget !== damageTarget) {
-      return;
-    }
-
-    this.lastCastDidDamage = true;
-    const enemy: any = this.enemies.getEntity(event);
-    if (enemy && SHATTER_DEBUFFS.some((effect) => enemy.hasBuff(effect.id, event.timestamp))) {
-      this.spikeShattered += 1;
-    } else {
-      this.spikeNotShattered += 1;
-      this.flagTimeline(this.lastCastEvent);
-    }
-    this.lastCastEvent = undefined;
-  }
-
-  onFightEnd(event: FightEndEvent) {
-    if (this.lastCastEvent) {
-      this.flagTimeline(this.lastCastEvent);
-    }
-  }
-
-  flagTimeline(event: CastEvent) {
-    if (!this.lastCastEvent) {
-      return;
-    }
-
-    event.meta = event.meta || {};
-    event.meta.isInefficientCast = true;
-    if (this.lastCastDidDamage) {
-      event.meta.inefficientCastReason = `You cast Glacial Spike without shattering it. You should wait until it is frozen or you are able to use a Brain Freeze proc to maximize its damage.`;
-    } else {
-      event.meta.inefficientCastReason =
-        'The target died before Glacial Spike hit it. You should avoid this by casting faster spells on very low-health targets, it is important to not waste potential Glacial Spike damage.';
-    }
-  }
+    return badCasts.length;
+  };
 
   get utilPercentage() {
-    return this.spikeShattered / this.totalCasts || 0;
+    return 1 - this.nonShatterCasts() / this.totalCasts;
   }
 
   get totalCasts() {
@@ -128,7 +90,7 @@ class GlacialSpike extends Analyzer {
         <>
           You cast <SpellLink spell={TALENTS.GLACIAL_SPIKE_TALENT} /> without{' '}
           <SpellLink spell={TALENTS.SHATTER_TALENT} />
-          ing it {this.spikeNotShattered} times. Because it is such a potent ability, it is
+          ing it {this.nonShatterCasts()} times. Because it is such a potent ability, it is
           important to maximize it's damage by only casting it if the target is
           <TooltipElement
             content={
@@ -160,7 +122,7 @@ class GlacialSpike extends Analyzer {
         size="flexible"
         tooltip={
           <>
-            You cast Glacial Spike {this.totalCasts} times, {this.spikeShattered} casts of which
+            You cast Glacial Spike {this.totalCasts} times, {this.nonShatterCasts()} casts of which
             were Shattered
           </>
         }
