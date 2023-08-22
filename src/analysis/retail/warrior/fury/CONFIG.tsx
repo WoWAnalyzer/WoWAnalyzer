79e173eb85ce9aa2a8072ab0c34f603e10b33f7a
Oscar Busk <oscar.busk@gmail.com>
2023-08-23 00:26:02 +0200

    Update the supported patch to be the current patch

--- src/analysis/retail/warrior/fury/CONFIG.tsx
+++ src/analysis/retail/warrior/fury/CONFIG.tsx
@@ -10,7 +10,7 @@ const config: Config = {
   contributors: [Tyndi, Listefano],
   expansion: Expansion.Dragonflight,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '10.0.0',
+  patchCompatibility: '10.1.5',
   isPartial: true,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
