106e9608cc9437e998c6ab2095ca14b9350af8c8
Oscar Busk <oscar.busk@gmail.com>
2023-08-23 00:24:23 +0200

    Fix issue where the forced reset of Raging Blow was not working

--- src/analysis/retail/warrior/fury/modules/features/SpellUsable.ts
+++ src/analysis/retail/warrior/fury/modules/features/SpellUsable.ts
@@ -1,7 +1,7 @@
 import SPELLS from 'common/SPELLS';
 import talents from 'common/TALENTS/warrior';
 import { Options } from 'parser/core/Analyzer';
-import { CastEvent, DamageEvent } from 'parser/core/Events';
+import { AbilityEvent, CastEvent } from 'parser/core/Events';
 import CoreSpellUsable from 'parser/shared/modules/SpellUsable';
 
 const FURY_EXECUTES = [SPELLS.EXECUTE_FURY.id, SPELLS.EXECUTE_FURY_MASSACRE.id];
@@ -30,10 +30,13 @@ class SpellUsable extends CoreSpellUsable {
     }
   }
 
-  beginCooldown(cooldownTriggerEvent: CastEvent | DamageEvent, spellId: number) {
+  beginCooldown(
+    triggeringEvent: AbilityEvent<any>,
+    spellId: number = triggeringEvent.ability.guid,
+  ) {
     if (spellId === SPELLS.RAGING_BLOW.id) {
       // Raging Blow has a 20% chance to reset its own cooldown when cast. The combatlog has no events for this, so we have to do this hack to account for it. This ends the cooldown upon a new cast if it turns out to still be on cooldown so it looks to be working ok.
-      if (this.isOnCooldown(spellId)) {
+      if (!this.isAvailable(spellId)) {
         this.endCooldown(
           spellId,
           this.lastPotentialTriggerForRagingBlow
@@ -41,8 +44,7 @@ class SpellUsable extends CoreSpellUsable {
             : undefined,
         );
       }
-    }
-    if (this.hasSuddenDeath && FURY_EXECUTES.includes(spellId)) {
+    } else if (this.hasSuddenDeath && FURY_EXECUTES.includes(spellId)) {
       if (this.isOnCooldown(spellId) && this.lastExecute) {
         this.executeCdrEvents[this.lastExecute] = this.cooldownRemaining(spellId);
         this.endCooldown(spellId);
@@ -50,7 +52,7 @@ class SpellUsable extends CoreSpellUsable {
     }
 
     // We must do this after ending the cd or it will trigger an error
-    super.beginCooldown(cooldownTriggerEvent, spellId);
+    super.beginCooldown(triggeringEvent, spellId);
   }
 }
 
