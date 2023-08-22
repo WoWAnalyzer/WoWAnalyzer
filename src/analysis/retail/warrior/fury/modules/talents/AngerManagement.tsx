d3b885bccecefb9dbe8cd1c1fd77df1e1f2fd27f
Oscar Busk <oscar.busk@gmail.com>
2023-08-23 01:31:08 +0200

    Update Anger Management module
    Rewrite Anger Management Module to correctly reduce cooldown of Ravager as well as Recklessness. Also rewrite statistic module to show number of extra casts afforded by the reduced CD.

--- src/analysis/retail/warrior/fury/modules/talents/AngerManagement.tsx
+++ src/analysis/retail/warrior/fury/modules/talents/AngerManagement.tsx
@@ -1,31 +1,48 @@
-import { formatNumber } from 'common/format';
-import SPELLS from 'common/SPELLS';
 import talents from 'common/TALENTS/warrior';
+import { formatDuration } from 'common/format';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
+import { SpellLink } from 'interface';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import { calculateMaxCasts } from 'parser/core/EventCalculateLib';
 import Events, { CastEvent } from 'parser/core/Events';
-import SpellUsable from '../features/SpellUsable';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
-import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import Statistic from 'parser/ui/Statistic';
+import SpellUsable from '../features/SpellUsable';
 
 const RAGE_NEEDED_FOR_PROC = 20;
 const CDR_PER_PROC = 1000; // ms
 
-// Example log: /reports/41TzG8jMBNkvxg7f#fight=9&type=damage-done
+// Example log: https://www.warcraftlogs.com/reports/RVwcm3bNzJaXAMqW/#fight=4&source=17
 class AngerManagement extends Analyzer {
   static dependencies = {
     spellUsable: SpellUsable,
   };
 
-  effectiveReduction: number = 0;
-  wastedReduction: number = 0;
-
   protected spellUsable!: SpellUsable;
+
+  private totalRageSpent = 0;
+  private recklessnessCDR = {
+    effective: 0,
+    wasted: 0,
+  };
+  private ravagerCDR = {
+    effective: 0,
+    wasted: 0,
+  };
+
+  private talentAngerManagement: boolean = false;
+  private talentRecklessness: boolean = false;
+  private talentRavager: boolean = false;
 
   constructor(options: Options) {
     super(options);
-    this.active = this.selectedCombatant.hasTalent(talents.ANGER_MANAGEMENT_TALENT);
+
+    this.talentAngerManagement = this.selectedCombatant.hasTalent(talents.ANGER_MANAGEMENT_TALENT);
+    this.talentRecklessness = this.selectedCombatant.hasTalent(talents.RECKLESSNESS_TALENT);
+    this.talentRavager = this.selectedCombatant.hasTalent(talents.RAVAGER_TALENT);
+
+    this.active = this.talentAngerManagement && (this.talentRecklessness || this.talentRavager);
 
     if (!this.active) {
       return;
@@ -45,26 +62,119 @@ class AngerManagement extends Analyzer {
     }
 
     const rageSpent = rage.cost / 10;
+    this.totalRageSpent += rageSpent;
     const reduction = (rageSpent / RAGE_NEEDED_FOR_PROC) * CDR_PER_PROC;
 
-    if (!this.spellUsable.isOnCooldown(SPELLS.RECKLESSNESS.id)) {
-      this.wastedReduction += reduction;
-    } else {
-      const effectiveReduction = this.spellUsable.reduceCooldown(SPELLS.RECKLESSNESS.id, reduction);
-      this.effectiveReduction += effectiveReduction;
-      this.wastedReduction += reduction - effectiveReduction;
+    if (this.talentRavager) {
+      const effectiveReduction = this.spellUsable.reduceCooldown(
+        talents.RAVAGER_TALENT.id,
+        reduction,
+      );
+      this.ravagerCDR.effective += effectiveReduction;
+      this.ravagerCDR.wasted += reduction - effectiveReduction;
+    }
+
+    if (this.talentRecklessness) {
+      const effectiveReduction = this.spellUsable.reduceCooldown(
+        talents.RECKLESSNESS_TALENT.id,
+        reduction,
+      );
+      this.recklessnessCDR.effective += effectiveReduction;
+      this.recklessnessCDR.wasted += reduction - effectiveReduction;
     }
   }
+
+  private extraCasts(
+    /** In ms */
+    baseCooldown: number,
+    /** In ms */
+    cooldownReduction: number,
+  ) {
+    const baseCasts = calculateMaxCasts(baseCooldown / 1000, this.owner.fightDuration);
+    const reducedCasts = calculateMaxCasts(
+      baseCooldown / 1000,
+      this.owner.fightDuration + cooldownReduction,
+    );
+    return Math.floor(reducedCasts - baseCasts);
+  }
+
+  private extraRecklessnessCasts = () =>
+    this.extraCasts(
+      this.spellUsable.fullCooldownDuration(talents.RECKLESSNESS_TALENT.id),
+      this.recklessnessCDR.effective,
+    );
+
+  private extraRavagerCasts = () =>
+    this.extraCasts(
+      this.spellUsable.fullCooldownDuration(talents.RAVAGER_TALENT.id),
+      this.ravagerCDR.effective,
+    );
 
   statistic() {
     return (
       <Statistic
         category={STATISTIC_CATEGORY.TALENTS}
         size="flexible"
-        tooltip={`${formatNumber(this.wastedReduction / 1000)}s missed CDR`}
+        tooltip={
+          <>
+            Spent total of <strong>{this.totalRageSpent} rage</strong>, resulting in a total
+            cooldown reduction of{' '}
+            {formatDuration(
+              this.recklessnessCDR.effective +
+                this.recklessnessCDR.wasted +
+                this.ravagerCDR.effective +
+                this.ravagerCDR.wasted,
+            )}{' '}
+            of which {formatDuration(this.recklessnessCDR.wasted + this.ravagerCDR.wasted)} was
+            wasted.
+            <br />
+            <table className="table table-condensed">
+              <thead>
+                <tr>
+                  <th>Ability</th>
+                  <th>Effective Reduction</th>
+                  <th>Wasted Reduction</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.talentRecklessness && (
+                  <tr>
+                    <td>
+                      <SpellLink spell={talents.RECKLESSNESS_TALENT.id} />
+                    </td>
+                    <td>{formatDuration(this.recklessnessCDR.effective)}</td>
+                    <td>{formatDuration(this.recklessnessCDR.wasted)}</td>
+                  </tr>
+                )}
+                {this.talentRavager && (
+                  <tr>
+                    <td>
+                      <SpellLink spell={talents.RAVAGER_TALENT.id} />
+                    </td>
+                    <td>{formatDuration(this.ravagerCDR.effective)}</td>
+                    <td>{formatDuration(this.ravagerCDR.wasted)}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </>
+        }
       >
         <BoringSpellValueText spell={talents.ANGER_MANAGEMENT_TALENT}>
-          <>{formatNumber(this.effectiveReduction / 1000)}s Recklessness CDR</>
+          {this.talentRecklessness && (
+            <div>
+              <SpellLink spell={talents.RECKLESSNESS_TALENT.id} />
+              <br />
+              {this.extraRecklessnessCasts()} <small>extra casts.</small>
+            </div>
+          )}
+          {this.talentRavager && (
+            <div>
+              <SpellLink spell={talents.RAVAGER_TALENT.id} />
+              <br />
+              {this.extraRavagerCasts()} <small>extra casts.</small>
+            </div>
+          )}
         </BoringSpellValueText>
       </Statistic>
     );
