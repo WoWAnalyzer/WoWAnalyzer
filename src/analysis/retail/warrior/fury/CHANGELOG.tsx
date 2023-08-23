c3da566c7acad730bbb113220ffb6e8c67a524e2
Oscar Busk <oscar.busk@gmail.com>
2023-08-24 01:10:04 +0200

    Add changelog so it stops complaining

--- src/analysis/retail/warrior/fury/CHANGELOG.tsx
+++ src/analysis/retail/warrior/fury/CHANGELOG.tsx
@@ -1,9 +1,10 @@
 import { change, date } from 'common/changelog';
-import { Listefano, CodersKitchen, ToppleTheNun, Ahri } from 'CONTRIBUTORS';
+import { Listefano, CodersKitchen, ToppleTheNun, Ahri, nullDozzer } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 import TALENTS from 'common/TALENTS/warrior';
 
 export default [
+  change(date(2023, 8, 24), 'Massive rewrite to support fury warrior', nullDozzer),
   change(date(2023, 7, 23), <>Normalize extra casts from <SpellLink spell={TALENTS.BERSERKERS_TORMENT_TALENT}/>.</>, ToppleTheNun),
   change(date(2023, 7, 22), 'Update default Fury Warrior log.', Ahri),
   change(date(2023, 7, 8), 'Update SpellLink usage.', ToppleTheNun),
