7df1a13e47d3e5b6aa092cde14e5ba17d216b6ef
Oscar Busk <oscar.busk@gmail.com>
2023-08-23 21:36:44 +0200

    Update RageTracker
    Remove code for tracking getting hit (since it triggers "Rage" spell),
    update rage-per-swing, and handle recklessness rage increase.

--- src/analysis/retail/warrior/fury/modules/core/RageTracker.tsx
+++ src/analysis/retail/warrior/fury/modules/core/RageTracker.tsx
@@ -1,13 +1,16 @@
 import SPELLS from 'common/SPELLS';
+import TALENTS from 'common/TALENTS/warrior';
 import HIT_TYPES from 'game/HIT_TYPES';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { CastEvent, DamageEvent } from 'parser/core/Events';
+import Events, { CastEvent } from 'parser/core/Events';
 import ResourceTracker from 'parser/shared/modules/resources/resourcetracker/ResourceTracker';
 
-const RAGE_GEN_FROM_MELEE_HIT_ICD = 1000; //ms
-const RAGE_PER_MELEE_HIT = 2;
-const RAGE_PER_MELEE_HIT_TAKEN = 3;
+// https://wowpedia.fandom.com/wiki/Rage
+const MH_AUTO_ATTACK_RAGE_PS = 1.75;
+const OH_AUTO_ATTACK_RAGE_PS = 0.875;
+const DEFAULT_SPEED_2H = 3.6;
+const DEFAULT_SPEED_1H = 2.6;
 
 class RageTracker extends ResourceTracker {
   vengeanceRageSaved: number = 0;
@@ -18,11 +21,49 @@ class RageTracker extends ResourceTracker {
   constructor(options: Options) {
     super(options);
     this.resource = RESOURCE_TYPES.RAGE;
-    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MELEE), this.onDamage);
-    this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MELEE),
-      this.onDamageTaken,
-    );
+
+    this.maxResource += this.selectedCombatant.getTalentRank(TALENTS.OVERWHELMING_RAGE_TALENT) * 15;
+
+    this.setupAutoAttackRage();
+  }
+
+  private setupAutoAttackRage() {
+    // While it would be nice to look at the speed or slot for weapons, I don't know if that's possible
+    // so we'll just make the assumption that they have specced correctly if they are using 1h weapons
+    const using1H = this.selectedCombatant.hasTalent(TALENTS.SINGLE_MINDED_FURY_TALENT);
+    let MhAutoAttackRagePerSwing =
+      MH_AUTO_ATTACK_RAGE_PS * (using1H ? DEFAULT_SPEED_1H : DEFAULT_SPEED_2H);
+    let OhAutoATtackRagePerSwing =
+      OH_AUTO_ATTACK_RAGE_PS * (using1H ? DEFAULT_SPEED_1H : DEFAULT_SPEED_2H);
+
+    if (this.selectedCombatant.hasTalent(TALENTS.WAR_MACHINE_FURY_TALENT)) {
+      MhAutoAttackRagePerSwing *= 1.2;
+      OhAutoATtackRagePerSwing *= 1.2;
+    }
+
+    // As a side note, Annihilator rage triggers as energize event, so we don't need to worry about it here
+    // https://www.warcraftlogs.com/reports/N9bWqMnrakAjZdJB#fight=2&type=resources&source=103&spell=101
+
+    // WCL doesn't tell if the attack was MH or OH, so we'll just average the two
+    const AutoAtackRagePerSwing = (MhAutoAttackRagePerSwing + OhAutoATtackRagePerSwing) / 2;
+
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MELEE), (event) => {
+      if (
+        event.hitType === HIT_TYPES.MISS ||
+        event.hitType === HIT_TYPES.PARRY ||
+        event.hitType === HIT_TYPES.DODGE
+      ) {
+        return;
+      }
+
+      const reck = this.selectedCombatant.hasBuff(SPELLS.RECKLESSNESS.id, event.timestamp);
+
+      this.processInvisibleEnergize(
+        SPELLS.RAGE_AUTO_ATTACKS.id,
+        AutoAtackRagePerSwing * (reck ? 2 : 1),
+        event.timestamp,
+      );
+    });
   }
 
   getAdjustedCost(event: CastEvent) {
@@ -32,25 +73,6 @@ class RageTracker extends ResourceTracker {
     }
     cost /= 10;
     return cost;
-  }
-
-  onDamage(event: DamageEvent) {
-    this.processInvisibleEnergize(SPELLS.RAGE_AUTO_ATTACKS.id, RAGE_PER_MELEE_HIT, event.timestamp);
-  }
-
-  onDamageTaken(event: DamageEvent) {
-    if (event.hitType === HIT_TYPES.DODGE || event.hitType === HIT_TYPES.PARRY) {
-      return;
-    }
-
-    if (event.timestamp - this.lastMeleeTaken >= RAGE_GEN_FROM_MELEE_HIT_ICD) {
-      this.processInvisibleEnergize(
-        SPELLS.RAGE_DAMAGE_TAKEN.id,
-        RAGE_PER_MELEE_HIT_TAKEN,
-        event.timestamp,
-      );
-      this.lastMeleeTaken = event.timestamp;
-    }
   }
 }
 
