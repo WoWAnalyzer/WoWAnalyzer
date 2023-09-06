b5f75fd2720594a1411083dc20040be8abb0e98c
Putro <laustsf@gmail.com>
2023-09-06 21:45:43 +0200

    Fix Elemental Blast for Enhancement shamans

--- src/analysis/retail/shaman/enhancement/modules/Abilities.tsx
+++ src/analysis/retail/shaman/enhancement/modules/Abilities.tsx
@@ -413,7 +413,7 @@ class Abilities extends CoreAbilities {
       },
       {
         // This is no error. We actually use the elemental shaman elemental blast spell id.
-        spell: TALENTS_SHAMAN.ELEMENTAL_BLAST_ENHANCEMENT_TALENT.id,
+        spell: TALENTS_SHAMAN.ELEMENTAL_BLAST_ELEMENTAL_TALENT.id,
         enabled: combatant.hasTalent(TALENTS_SHAMAN.ELEMENTAL_BLAST_ENHANCEMENT_TALENT),
         charges:
           combatant.getRepeatedTalentCount(TALENTS_SHAMAN.ELEMENTAL_BLAST_ENHANCEMENT_TALENT) +
