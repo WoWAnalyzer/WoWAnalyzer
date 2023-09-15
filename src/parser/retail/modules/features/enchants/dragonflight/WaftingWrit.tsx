ba18c8be82a125698a9a732ebf899ad9d18ca6a7
Oscar Busk <oscar.busk@gmail.com>
2023-09-15 02:38:23 +0200

    Refactor writs to reuse code

--- src/parser/retail/modules/features/enchants/dragonflight/WaftingWrit.tsx
+++ src/parser/retail/modules/features/enchants/dragonflight/WaftingWrit.tsx
@@ -1,13 +1,8 @@
 import ENCHANTS from 'common/ITEMS/dragonflight/enchants';
 import SPELLS from 'common/SPELLS/dragonflight/enchants';
-import { SpellLink } from 'interface';
-import { HasteIcon } from 'interface/icons';
-import Analyzer, { Options } from 'parser/core/Analyzer';
-import StatTracker from 'parser/shared/modules/StatTracker';
-import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
-import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
-import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
-import Statistic from 'parser/ui/Statistic';
+import { Options } from 'parser/core/Analyzer';
+import STAT from 'parser/shared/modules/features/STAT';
+import Writ from './Writ';
 
 // ================ SAMPLE LOGS ================
 // Wafting Writ R1
@@ -21,72 +16,21 @@ import Statistic from 'parser/ui/Statistic';
 const RANKS = [
   {
     enchant: ENCHANTS.ENCHANT_WEAPON_WAFTING_WRIT_R1,
-    haste: 1185.67,
+    amount: 1185.67,
   },
   {
     enchant: ENCHANTS.ENCHANT_WEAPON_WAFTING_WRIT_R2,
-    haste: 1209.09,
+    amount: 1209.09,
   },
   {
     enchant: ENCHANTS.ENCHANT_WEAPON_WAFTING_WRIT_R3,
-    haste: 1394.51,
+    amount: 1394.51,
   },
 ];
 
-class WaftingWrit extends Analyzer {
-  static dependencies = {
-    statTracker: StatTracker,
-  };
-  protected statTracker!: StatTracker;
-
-  private haste = 0;
-
-  constructor(options: Options & { statTracker: StatTracker }) {
-    super(options);
-    this.statTracker = options.statTracker;
-
-    this.active = RANKS.some((effect) => this.selectedCombatant.hasWeaponEnchant(effect.enchant));
-
-    if (!this.active) {
-      return;
-    }
-    const { mainHand, offHand } = this.selectedCombatant;
-
-    [mainHand.permanentEnchant, offHand.permanentEnchant].forEach((enchantId) => {
-      const effect = RANKS.find((e) => e.enchant.effectId === enchantId);
-      if (effect) {
-        this.haste += effect.haste;
-      }
-    });
-
-    this.statTracker.add(SPELLS.WAFTING_WRIT.id, {
-      haste: this.haste,
-    });
-  }
-
-  statistic() {
-    const totalProcs = this.selectedCombatant.getBuffTriggerCount(SPELLS.WAFTING_WRIT.id);
-    const ppm = (totalProcs / (this.owner.fightDuration / 1000 / 60)).toFixed(1);
-    const uptime =
-      this.selectedCombatant.getBuffUptime(SPELLS.WAFTING_WRIT.id) / this.owner.fightDuration;
-
-    return (
-      <Statistic
-        size="flexible"
-        category={STATISTIC_CATEGORY.ITEMS}
-        position={STATISTIC_ORDER.UNIMPORTANT(1)}
-        tooltip={
-          <>
-            <SpellLink spell={SPELLS.WAFTING_WRIT} /> triggered {totalProcs} times ({ppm} procs per
-            minute), giving {Math.round(this.haste)} haste every time.
-          </>
-        }
-      >
-        <BoringSpellValueText spell={SPELLS.WAFTING_WRIT}>
-          <HasteIcon /> {Math.round(this.haste * uptime)} <small>haste on average</small>
-        </BoringSpellValueText>
-      </Statistic>
-    );
+class WaftingWrit extends Writ {
+  constructor(options: Options) {
+    super(STAT.HASTE, SPELLS.WAFTING_WRIT, RANKS, options);
   }
 }
 
---
+++ src/parser/retail/modules/features/enchants/dragonflight/Writ.tsx
@@ -0,0 +1,86 @@
+import { Enchant } from 'common/ITEMS/Item';
+import Spell from 'common/SPELLS/Spell';
+import SpellLink from 'interface/SpellLink';
+import Analyzer from 'parser/core/Analyzer';
+import { Options } from 'parser/core/EventSubscriber';
+import StatTracker from 'parser/shared/modules/StatTracker';
+import STAT, { getIcon, getName } from 'parser/shared/modules/features/STAT';
+import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
+import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+import Statistic from 'parser/ui/Statistic';
+
+export const SECONDARY_STAT_WRIT_VALUES = {
+  1: 1185.67,
+  2: 1209.09,
+  3: 1394.51,
+} as const;
+
+interface Rank {
+  enchant: Enchant;
+  amount: number;
+}
+
+abstract class Writ extends Analyzer {
+  static dependencies = {
+    statTracker: StatTracker,
+  };
+  protected statTracker!: StatTracker;
+
+  /** The stat that the enchant provides on procc */
+  protected stat: STAT;
+  protected buff: Spell;
+  /** The amount provided each time it proccs */
+  protected amount = 0;
+
+  constructor(stat: STAT, buff: Spell, ranks: Rank[], options: Options) {
+    super(options);
+    this.statTracker = (options as Options & { statTracker: StatTracker }).statTracker;
+    this.stat = stat;
+    this.buff = buff;
+
+    this.active = ranks.some((effect) => this.selectedCombatant.hasWeaponEnchant(effect.enchant));
+    if (!this.active) {
+      return;
+    }
+    const { mainHand, offHand } = this.selectedCombatant;
+
+    [mainHand.permanentEnchant, offHand.permanentEnchant].forEach((enchantId) => {
+      const effect = ranks.find((e) => e.enchant.effectId === enchantId);
+      if (effect) {
+        this.amount += effect.amount;
+      }
+    });
+
+    this.statTracker.add(this.buff.id, {
+      [this.stat]: this.amount,
+    });
+  }
+
+  statistic() {
+    const totalProcs = this.selectedCombatant.getBuffTriggerCount(this.buff.id);
+    const ppm = (totalProcs / (this.owner.fightDuration / 1000 / 60)).toFixed(1);
+    const uptime = this.selectedCombatant.getBuffUptime(this.buff.id) / this.owner.fightDuration;
+    const Icon = getIcon(this.stat);
+
+    return (
+      <Statistic
+        size="flexible"
+        category={STATISTIC_CATEGORY.ITEMS}
+        position={STATISTIC_ORDER.UNIMPORTANT(1)}
+        tooltip={
+          <>
+            <SpellLink spell={this.buff} /> triggered {totalProcs} times ({ppm} procs per minute),
+            giving {Math.round(this.amount)} {getName(this.stat)} every time.
+          </>
+        }
+      >
+        <BoringSpellValueText spell={this.buff}>
+          <Icon /> {Math.round(this.amount * uptime)} <small>{getName(this.stat)} on average</small>
+        </BoringSpellValueText>
+      </Statistic>
+    );
+  }
+}
+
+export default Writ;
