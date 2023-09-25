ba5c2dd07204082ffbe74d491a0055b42a67aa77
emallson <emallson@emallson.net>
2023-09-25 11:41:31 -0400

    fix typo className -> classNames
    the timeout change is just a shortcut

--- src/interface/PremiumLoginPanel.tsx
+++ src/interface/PremiumLoginPanel.tsx
@@ -91,7 +91,7 @@ const PremiumLoginPanel = () => {
   return (
     <div className="panel">
       <div className="panel-body" style={{ padding: '0 15px', position: 'relative' }}>
-        <TransitionGroup className="logged-in" timeout={{ enter: 1000, exit: 1000 }}>
+        <TransitionGroup classNames="logged-in" timeout={1000}>
           {user && <LoggedIn />}
         </TransitionGroup>
 
