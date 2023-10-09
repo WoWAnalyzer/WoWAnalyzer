e6c0f1865f602868cf4f5f33e4338ab88f334501
Jazminite <jazminitehs@gmail.com>
2023-10-09 06:06:48 -0400

    [Classic] Discipline Priest

---
+++ src/analysis/classic/priest/discipline/CHANGELOG.tsx
@@ -0,0 +1,6 @@
+import { change, date } from 'common/changelog';
+import { jazminite } from 'CONTRIBUTORS';
+
+export default [
+  change(date(2023, 10, 9), 'Add initial Classic Discipline Priest spec.', jazminite),
+];
