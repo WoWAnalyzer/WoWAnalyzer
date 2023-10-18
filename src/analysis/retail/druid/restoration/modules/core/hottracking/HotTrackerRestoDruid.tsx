1d4ef903cbb70e6e0e420c39cf96a5ce521cb2d7
Aveline Estié <aa.estie@gmail.com>
2023-10-18 23:38:17 +0200

    [Resto Druid] Dragonflight 10.2 update (#6423)
    Co-authored-by: Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>

--- src/analysis/retail/druid/restoration/modules/core/hottracking/HotTrackerRestoDruid.tsx
+++ src/analysis/retail/druid/restoration/modules/core/hottracking/HotTrackerRestoDruid.tsx
@@ -15,8 +15,6 @@ import {
 } from 'analysis/retail/druid/restoration/constants';
 import { TALENTS_DRUID } from 'common/TALENTS';
 
-const CIRCLE_MULT = 0.85;
-
 class HotTrackerRestoDruid extends HotTracker {
   static dependencies = {
     ...HotTracker.dependencies,
@@ -88,15 +86,12 @@ class HotTrackerRestoDruid extends HotTracker {
   }
 
   _generateHotInfo(): HotInfo[] {
-    const hasCircle = this.selectedCombatant.hasTalent(
-      TALENTS_DRUID.CIRCLE_OF_LIFE_AND_DEATH_SPEC_TALENT,
-    );
     const hasImpRejuv = this.selectedCombatant.hasTalent(
       TALENTS_DRUID.IMPROVED_REJUVENATION_TALENT,
     );
     const hasGermination = this.selectedCombatant.hasTalent(TALENTS_DRUID.GERMINATION_TALENT);
 
-    const globalMult = hasCircle ? CIRCLE_MULT : 1;
+    const globalMult = 1;
     const rejuvDuration = 12000 + (hasImpRejuv ? 3000 : 0) + (hasGermination ? 2000 : 0);
 
     return [
