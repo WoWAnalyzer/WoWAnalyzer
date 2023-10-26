1561b8677e74f84ef71ed957a5ff93e1aeebd896
Putro <laustsf@gmail.com>
2023-10-26 17:42:09 +0200

    [Dwarf] Fix dwarf racial with pets

--- src/parser/shared/modules/racials/dwarf/MightOfTheMountain.tsx
+++ src/parser/shared/modules/racials/dwarf/MightOfTheMountain.tsx
@@ -4,13 +4,14 @@ import HIT_TYPES from 'game/HIT_TYPES';
 import RACES from 'game/RACES';
 import ROLES from 'game/ROLES';
 import { SpellIcon } from 'interface';
-import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Analyzer, { Options, SELECTED_PLAYER, SELECTED_PLAYER_PET } from 'parser/core/Analyzer';
 import Events, { DamageEvent, EventType, HealEvent } from 'parser/core/Events';
 import Abilities from 'parser/core/modules/Abilities';
 import CritEffectBonus, { ValidEvents } from 'parser/shared/modules/helpers/CritEffectBonus';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import StatisticBox from 'parser/ui/StatisticBox';
+import { isPermanentPet } from '../../pets/helpers';
 
 export const CRIT_EFFECT = 0.02;
 
@@ -37,6 +38,7 @@ class MightOfTheMountain extends Analyzer {
     (options.critEffectBonus as CritEffectBonus).hook(this.getCritEffectBonus.bind(this));
     this.addEventListener(Events.heal.by(SELECTED_PLAYER), this.onHeal);
     this.addEventListener(Events.damage.by(SELECTED_PLAYER), this.onDamage);
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER_PET), this.onPetDamage);
   }
 
   isApplicableHeal(event: HealEvent) {
@@ -82,6 +84,24 @@ class MightOfTheMountain extends Analyzer {
       return;
     }
 
+    this.damage += this.critEffectBonus.getDamageContribution(event, CRIT_EFFECT);
+  }
+
+  onPetDamage(event: DamageEvent) {
+    if (!this.isApplicableDamage(event)) {
+      return;
+    }
+    const sourcePet = this.owner.playerPets.find(
+      (pet: { id: number | undefined }) => pet.id === event.sourceID,
+    );
+
+    if (!sourcePet || !isPermanentPet(sourcePet.guid)) {
+      return;
+    }
+
+    //These effects (dwarf & tauren crit mod) apply to both the player and the pet via Apply Player/Pet Aura (202) and the pet inherits from the
+    // player, effectively getting double the mod.
+    this.damage += this.critEffectBonus.getDamageContribution(event, CRIT_EFFECT);
     this.damage += this.critEffectBonus.getDamageContribution(event, CRIT_EFFECT);
   }
 
