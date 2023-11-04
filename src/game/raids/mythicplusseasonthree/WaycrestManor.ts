cdf31b36845f646201e1007a4468955d345f24ed
Aveline Estié <aveline.estie@vntrs.com>
2023-11-04 09:19:41 +0100

    add headshots for pre-existing m+ dungeons

--- src/game/raids/mythicplusseasonthree/WaycrestManor.ts
+++ src/game/raids/mythicplusseasonthree/WaycrestManor.ts
@@ -1,12 +1,12 @@
 import { Dungeon } from 'game/raids';
 
-// import Background from './backgrounds/WaycrestManor.jpg';
+import Background from './backgrounds/WaycrestManor.jpg';
 import Headshot from './headshots/WaycrestManor.jpg';
 
 const WaycrestManor: Dungeon = {
   id: 61862,
   name: 'Waycrest Manor',
-  // background: Background,
+  background: Background,
   headshot: Headshot,
   icon: 'achievement_dungeon_waycrestmannor', // Blizz whyyyy
   fight: {},
