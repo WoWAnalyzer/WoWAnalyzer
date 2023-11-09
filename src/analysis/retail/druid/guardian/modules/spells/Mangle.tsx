ea530b3e883a993c6cfd13e6f417cfe24208cb92
Kelton Finch <keltonfinch@gmail.com>
2023-11-09 10:49:09 -0500

    Druid Guardian - Reactivated Mangle, Thrash, and Moonfire modules including simple guide subsections. Marked ready for 10.2. 10.2 logs added as default for Guardian, Feral, and Resto Druid.

---
+++ src/analysis/retail/druid/guardian/modules/spells/Mangle.tsx
@@ -0,0 +1,23 @@
+import SPELLS from 'common/SPELLS';
+import Analyzer from 'parser/core/Analyzer';
+import SpellLink from 'interface/SpellLink';
+import CastEfficiencyPanel from 'interface/guide/components/CastEfficiencyPanel';
+import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
+
+export default class Mangle extends Analyzer {
+  get guideSubsection() {
+    const explanation = (
+      <p>
+        <strong>
+          <SpellLink spell={SPELLS.MANGLE_BEAR} />
+        </strong>{' '}
+        does direct damage and generates rage. Use it on cooldown. The very short cooldown combined
+        jammed GCDs means 100% usage will be practically impossible, but get as close as you can.
+      </p>
+    );
+
+    const data = <CastEfficiencyPanel spell={SPELLS.MANGLE_BEAR} useThresholds />;
+
+    return explanationAndDataSubsection(explanation, data);
+  }
+}
---
+++ src/analysis/retail/druid/guardian/modules/spells/Thrash.tsx
@@ -0,0 +1,53 @@
+import { formatPercentage } from 'common/format';
+import SPELLS from 'common/SPELLS';
+import { SpellIcon } from 'interface';
+import Analyzer from 'parser/core/Analyzer';
+import Enemies from 'parser/shared/modules/Enemies';
+import BoringValueText from 'parser/ui/BoringValueText';
+import Statistic from 'parser/ui/Statistic';
+import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+import SpellLink from 'interface/SpellLink';
+import CastEfficiencyPanel from 'interface/guide/components/CastEfficiencyPanel';
+import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
+
+const deps = {
+  enemies: Enemies,
+};
+
+export default class Thrash extends Analyzer.withDependencies(deps) {
+  get guideSubsection() {
+    const explanation = (
+      <p>
+        <strong>
+          <SpellLink spell={SPELLS.THRASH_BEAR} />
+        </strong>{' '}
+        is AoE direct damage and a stacking bleed. It generates rage and should be used on cooldown.
+        The very short cooldown combined jammed GCDs means 100% usage will be practically
+        impossible, but get as close as you can.
+      </p>
+    );
+
+    const data = <CastEfficiencyPanel spell={SPELLS.THRASH_BEAR} useThresholds />;
+
+    return explanationAndDataSubsection(explanation, data);
+  }
+
+  statistic() {
+    const thrashUptimePercentage =
+      this.deps.enemies.getBuffUptime(SPELLS.THRASH_BEAR_DOT.id) / this.owner.fightDuration;
+
+    return (
+      <Statistic position={STATISTIC_ORDER.CORE(11)} size="flexible">
+        <BoringValueText
+          label={
+            <>
+              <SpellIcon spell={SPELLS.THRASH_BEAR} /> Thrash uptime{' '}
+            </>
+          }
+        >
+          {`${formatPercentage(thrashUptimePercentage)}%`}
+        </BoringValueText>
+      </Statistic>
+    );
+  }
+}
