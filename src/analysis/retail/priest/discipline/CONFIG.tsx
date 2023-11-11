c75424254228ad5c4181f27a269d7e18b08185d7
Vollmer <kristianvollmer@gmail.com>
2023-11-11 18:04:58 +0100

    revert rogue Disc priest config change

--- src/analysis/retail/priest/discipline/CONFIG.tsx
+++ src/analysis/retail/priest/discipline/CONFIG.tsx
@@ -3,7 +3,7 @@ import Expansion from 'game/Expansion';
 import SPECS from 'game/SPECS';
 import Config from 'parser/Config';
 
-// import CHANGELOG from './CHANGELOG';
+import CHANGELOG from './CHANGELOG';
 
 const config: Config = {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
@@ -37,17 +37,16 @@ const config: Config = {
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
   exampleReport:
     '/report/TDdwqCKRmZzpX8hY/32-Mythic+Kurog+Grimtotem+-+Kill+(8:51)/Xephiroze/standard/overview',
-
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
   spec: SPECS.DISCIPLINE_PRIEST,
   // The contents of your changelog.
-  changelog: [], // CHANGELOG,
+  changelog: CHANGELOG,
   // The CombatLogParser class for your spec.
-  // parser: () =>
-  //   import('./CombatLogParser' /* webpackChunkName: "DisciplinePriest" */).then(
-  //     (exports) => exports.default,
-  //   ),
+  parser: () =>
+    import('./CombatLogParser' /* webpackChunkName: "DisciplinePriest" */).then(
+      (exports) => exports.default,
+    ),
   // The path to the current directory (relative form project root). This is used for generating a GitHub link directly to your spec's code.
   path: __dirname,
 };
