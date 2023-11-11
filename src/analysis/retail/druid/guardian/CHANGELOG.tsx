895ea990683b2a6fb17b0ca736ae0d8c71d8e261
Kelton Finch <keltonfinch@gmail.com>
2023-11-11 14:02:06 -0500

    changelog

--- src/analysis/retail/druid/guardian/CHANGELOG.tsx
+++ src/analysis/retail/druid/guardian/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { SpellLink } from 'interface';
 import SPELLS from 'common/SPELLS';
 
 export default [
+  change(date(2023, 11, 11), <>Added active time graph to Guide.</>, Sref),
   change(date(2023, 11, 9), <>Added simple spell usage stats for <SpellLink spell={SPELLS.MANGLE_BEAR} />, <SpellLink spell={SPELLS.THRASH_BEAR} />, and <SpellLink spell={SPELLS.MOONFIRE_CAST} />. Marked as updated for 10.2.</>, Sref),
   change(date(2023, 6, 20), 'Update SpellLink usage.', ToppleTheNun),
   change(date(2023, 4, 24), <>Fixed a bug where <SpellLink spell={TALENTS_DRUID.PULVERIZE_TALENT} /> uses weren't being correctly detected.</>, Sref),
