c18e8964a12103f784fead96e74e3a9eda0b9754
Oscar Busk <oscar.busk@gmail.com>
2023-11-12 20:13:42 +0100

    Update changelog and config

--- src/analysis/retail/warrior/fury/CHANGELOG.tsx
+++ src/analysis/retail/warrior/fury/CHANGELOG.tsx
@@ -4,7 +4,7 @@ import SpellLink from 'interface/SpellLink';
 import TALENTS from 'common/TALENTS/warrior';
 
 export default [
-  change(date(2023, 8, 24), 'Massive rewrite to support fury warrior', nullDozzer),
+  change(date(2023, 11, 12), 'Massive rewrite to support fury warrior', nullDozzer),
   change(date(2023, 7, 23), <>Normalize extra casts from <SpellLink spell={TALENTS.BERSERKERS_TORMENT_TALENT}/>.</>, ToppleTheNun),
   change(date(2023, 7, 22), 'Update default Fury Warrior log.', Ahri),
   change(date(2023, 7, 8), 'Update SpellLink usage.', ToppleTheNun),
