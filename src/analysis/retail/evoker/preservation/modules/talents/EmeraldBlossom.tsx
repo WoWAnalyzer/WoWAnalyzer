3057053a1bd8f1d16f3d967b9ce86f87cd4819e9
trevorm4 <trevorm4@illinois.edu>
2023-11-11 18:49:02 -0800

    Add emerald blossom graphic

--- src/analysis/retail/evoker/preservation/modules/talents/EmeraldBlossom.tsx
+++ src/analysis/retail/evoker/preservation/modules/talents/EmeraldBlossom.tsx
@@ -8,10 +8,15 @@ import TalentSpellText from 'parser/ui/TalentSpellText';
 import { TALENTS_EVOKER } from 'common/TALENTS';
 import { ThresholdStyle, When } from 'parser/core/ParseResults';
 import ItemHealingDone from 'parser/ui/ItemHealingDone';
-import { getHealEvents } from '../../normalizers/CastLinkNormalizer';
+import { getHealEvents, isFromFieldOfDreams } from '../../normalizers/CastLinkNormalizer';
 import { SpellLink } from 'interface';
-import { formatNumber } from 'common/format';
+import { formatNumber, formatPercentage } from 'common/format';
 import { defineMessage } from '@lingui/macro';
+import { BoxRowEntry, PerformanceBoxRow } from 'interface/guide/components/PerformanceBoxRow';
+import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
+import { RoundedPanel } from 'interface/guide/components/GuideDivs';
+import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
+import { GUIDE_CORE_EXPLANATION_PERCENT } from '../../Guide';
 
 const BOUNTIFUL_ADDITIONAL_TARGETS = 2;
 const BASE_TARGETS = 3;
@@ -25,8 +30,10 @@ class EmeraldBlossom extends Analyzer {
   totalHealing: number = 0;
   totalOverhealing: number = 0;
   countedTimestamps: Set<number> = new Set<number>();
-
+  castEntries: BoxRowEntry[] = [];
   hasBountiful: boolean = false;
+  goodThreshold: number = 0;
+  perfectThreshold: number = 0;
 
   constructor(options: Options) {
     super(options);
@@ -35,6 +42,10 @@ class EmeraldBlossom extends Analyzer {
       Events.heal.by(SELECTED_PLAYER).spell(SPELLS.EMERALD_BLOSSOM),
       this.onHealBatch,
     );
+    this.perfectThreshold =
+      3 + this.selectedCombatant.getTalentRank(TALENTS_EVOKER.BOUNTIFUL_BLOOM_TALENT) * 2;
+    this.goodThreshold =
+      2 + this.selectedCombatant.getTalentRank(TALENTS_EVOKER.BOUNTIFUL_BLOOM_TALENT) * 2;
   }
 
   get averageNumTargets() {
@@ -43,6 +54,41 @@ class EmeraldBlossom extends Analyzer {
 
   get averageExtraTargets() {
     return this.extraBountifulHits / this.numBlossoms;
+  }
+
+  processBatch(events: HealEvent[]) {
+    if (events.some((ev) => isFromFieldOfDreams(ev))) {
+      return;
+    }
+    let value = QualitativePerformance.Perfect;
+    let effective = 0;
+    let overheal = 0;
+    let timestamp = events.at(0)!.timestamp;
+    events.forEach((ev) => {
+      effective += ev.amount;
+      overheal = ev.overheal || 0;
+      timestamp = timestamp < ev.timestamp ? timestamp : ev.timestamp;
+    });
+    const overhealPercent = overheal / (overheal + effective);
+    if (events.length < this.goodThreshold || overhealPercent > 0.8) {
+      value = QualitativePerformance.Fail;
+    } else if (overhealPercent > 0.7) {
+      value = QualitativePerformance.Ok;
+    } else if (events.length < this.perfectThreshold || overhealPercent > 0.6) {
+      value = QualitativePerformance.Good;
+    }
+    const tooltip = (
+      <>
+        @ <strong>{this.owner.formatTimestamp(timestamp)}</strong>, Targets hit:{' '}
+        <strong>{events.length}</strong>
+        <br />
+        <>
+          Healing: {formatNumber(effective)} ({formatPercentage(overhealPercent)}% overheal)
+        </>
+        <br />
+      </>
+    );
+    this.castEntries.push({ value, tooltip });
   }
 
   // batch process all healevents for single cast to easily decide which to attribute to bountiful
@@ -66,6 +112,7 @@ class EmeraldBlossom extends Analyzer {
       }
     }
     this.countedTimestamps.add(event.timestamp);
+    this.processBatch(allHealingEvents);
   }
 
   get suggestionThresholds() {
@@ -96,6 +143,56 @@ class EmeraldBlossom extends Analyzer {
         )
         .recommended(`at least ${recommended} targets hit recommended`),
     );
+  }
+
+  get guideSubsection(): JSX.Element {
+    const styleObj = {
+      fontSize: 20,
+    };
+    const styleObjInner = {
+      fontSize: 15,
+    };
+    const explanation = (
+      <p>
+        <SpellLink spell={SPELLS.EMERALD_BLOSSOM} /> is the core spell in the Blossom talent build
+        for Preservation Evokers. It is critical to cast{' '}
+        <SpellLink spell={SPELLS.EMERALD_BLOSSOM} /> on targets that are nearby other allies (more
+        often than not this is a melee) so that you can fully utilize its healing, while trying to
+        not overheal in the process.
+      </p>
+    );
+    const data = (
+      <div>
+        <RoundedPanel>
+          <div>
+            <strong>
+              <SpellLink spell={SPELLS.EMERALD_BLOSSOM_CAST} /> casts
+            </strong>{' '}
+            <small>
+              {' '}
+              - Blue is a perfect cast with {this.perfectThreshold} targets hit, Green is a good
+              cast with {this.goodThreshold} or more with moderate to low overhealing, Yellow is an
+              ok cast with a moderate amount of overheal, and Red is a bad cast with high overheal
+              or few targets hit. This graphic does not include{' '}
+              <SpellLink spell={TALENTS_EVOKER.FIELD_OF_DREAMS_TALENT} /> due to their inherent
+              randomness.
+            </small>
+            <PerformanceBoxRow values={this.castEntries} />
+          </div>
+          <div style={styleObj}>
+            <small style={styleObjInner}>
+              <SpellLink spell={SPELLS.EMERALD_BLOSSOM} /> -{' '}
+            </small>
+            <strong>{this.averageNumTargets.toFixed(2)}</strong>{' '}
+            <small>
+              average targets hit per <SpellLink spell={SPELLS.EMERALD_BLOSSOM} />
+            </small>
+          </div>
+        </RoundedPanel>
+      </div>
+    );
+
+    return explanationAndDataSubsection(explanation, data, GUIDE_CORE_EXPLANATION_PERCENT);
   }
 
   statistic() {
