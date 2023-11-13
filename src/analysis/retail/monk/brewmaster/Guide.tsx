b6700319d75a217c1e5f6c0b3347dc48e1861d81
emallson <emallson@emallson.net>
2023-11-13 16:46:58 -0500

    add ABC section to brew guide

--- src/analysis/retail/monk/brewmaster/Guide.tsx
+++ src/analysis/retail/monk/brewmaster/Guide.tsx
@@ -20,6 +20,9 @@ import { Highlight } from 'interface/Highlight';
 import BlackoutComboSection from './modules/spells/BlackoutCombo/BlackoutComboSection';
 import BrewAplSummary from './modules/core/AplCheck/BrewAplSummary';
 import * as explainSpendCooldowns from './modules/core/AplCheck/explainSpendCooldowns';
+import ActiveTimeGraph from 'parser/ui/ActiveTimeGraph';
+import { formatPercentage } from 'common/format';
+import PerformanceStrong from 'interface/PerformanceStrong';
 
 const explainers = {
   explainSCK,
@@ -57,6 +60,40 @@ export default function Guide({ modules, events, info }: GuideProps<typeof Comba
             summary={BrewAplSummary}
             apl={AplCheck.apl(info)}
             violationExplainers={explainers}
+          />
+        </SubSection>
+        <SubSection title="Always Be Casting">
+          <Explanation>
+            <p>
+              Brewmaster is currently a high-APM spec, with the ability to use an ability every time
+              your global cooldown (GCD) ends. The only talent that changes this is{' '}
+              <SpellLink spell={talents.PRESS_THE_ADVANTAGE_TALENT} />, which introduces downtime
+              where you would otherwise press <SpellLink spell={SPELLS.TIGER_PALM} /> in
+              single-target. However, even with{' '}
+              <SpellLink spell={talents.PRESS_THE_ADVANTAGE_TALENT}>PtA</SpellLink>, you can still
+              fill <em>almost</em> every GCD with an ability.
+            </p>
+            <p>
+              It is better for you to use an ability&mdash;<em>any</em> ability&mdash;each GCD than
+              to think carefully about each button press and have gaps between ability uses. You
+              should <strong>Always Be Casting</strong>. With practice, you will build the skills
+              needed to both use the correct abilities and do so without rotational gaps.
+            </p>
+            <p>
+              The chart below should generally be near <strong>100%</strong> during periods where
+              you are able to attack the boss.
+            </p>
+          </Explanation>
+          <p>
+            Active Time:{' '}
+            <PerformanceStrong performance={modules.alwaysBeCasting.DowntimePerformance}>
+              {formatPercentage(modules.alwaysBeCasting.activeTimePercentage, 1)}%
+            </PerformanceStrong>
+          </p>
+          <ActiveTimeGraph
+            activeTimeSegments={modules.alwaysBeCasting.activeTimeSegments}
+            fightStart={info.fightStart}
+            fightEnd={info.fightEnd}
           />
         </SubSection>
         <BlackoutComboSection />
