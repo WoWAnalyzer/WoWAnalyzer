086b6f951e06211203249ce6bc9d88736ca7ed27
LucasLevyOB <oliveiralucaslevy@gmail.com>
2023-11-15 19:37:32 -0300

    docs: add contribuitor and changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -25,12 +25,14 @@ import {
   Bigsxy,
   Seriousnes,
   Arlie,
+  LucasLevyOB,
 } from 'CONTRIBUTORS';
 import { ItemLink } from 'interface';
 import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2023, 11, 15), 'Refactor Code Smell Missing Union Type.', LucasLevyOB),
   change(date(2023, 11, 15), 'Add backgrounds for Dawn of the Infinites, Everbloom, and Throne of the Tides.', ToppleTheNun),
   change(date(2023, 11, 13), <>Add stat tracking and statistics for <ItemLink id={ITEMS.ELEMENTAL_LARIAT.id}/>.</>, nullDozzer),
   change(date(2023, 11, 7), 'Fix Classic class / spec ranking ids.', jazminite),
