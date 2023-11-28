9a5c0b77dc1eeea0ec87efe98fe65bccbe66478c
emallson <emallson@emallson.net>
2023-11-28 10:44:22 -0500

    remove soulbindTraits/conduits and re-enable the holy priest analyzer

--- src/analysis/retail/priest/holy/CONFIG.tsx
+++ src/analysis/retail/priest/holy/CONFIG.tsx
@@ -41,10 +41,10 @@ const config: Config = {
   // The contents of your changelog.
   changelog: [], // CHANGELOG,
   // The CombatLogParser class for your spec.
-  // parser: () =>
-  //   import('./CombatLogParser' /* webpackChunkName: "HolyPriest" */).then(
-  //     (exports) => exports.default,
-  //   ),
+  parser: () =>
+    import('./CombatLogParser' /* webpackChunkName: "HolyPriest" */).then(
+      (exports) => exports.default,
+    ),
   // The path to the current directory (relative form project root). This is used for generating a GitHub link directly to your spec's code.
   path: __dirname,
 };
