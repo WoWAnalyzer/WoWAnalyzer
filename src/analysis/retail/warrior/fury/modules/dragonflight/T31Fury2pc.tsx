5f38caf39a551d9c519f0d61622459056440ec71
Oscar Busk <oscar.busk@gmail.com>
2023-12-24 00:47:47 +0100

    Improve tracking of 2pc damage boosts

--- src/analysis/retail/warrior/fury/modules/dragonflight/T31Fury2pc.tsx
+++ src/analysis/retail/warrior/fury/modules/dragonflight/T31Fury2pc.tsx
@@ -1,11 +1,11 @@
 import SPELLS from 'common/SPELLS/warrior';
 import TALENTS from 'common/TALENTS/warrior';
-import { formatNumber } from 'common/format';
+import { formatNumber, formatPercentage } from 'common/format';
 import { TIERS } from 'game/TIERS';
 import { SpellLink } from 'interface';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import { calculateEffectiveDamage } from 'parser/core/EventCalculateLib';
-import Events, { CastEvent, DamageEvent } from 'parser/core/Events';
+import Events, { Ability, CastEvent, DamageEvent } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
@@ -31,12 +31,12 @@ export default class T31Fury2pc extends Analyzer {
 
     this.addEventListener(
       Events.cast.by(SELECTED_PLAYER).spell(SPELLS.ODYNS_FURY),
-      () => (this.boostedOdynsFuryCount += 1),
+      this.odynsFuryCast,
     );
     if (this.selectedCombatant.hasTalent(TALENTS.TITANS_TORMENT_TALENT)) {
       this.addEventListener(
         Events.cast.by(SELECTED_PLAYER).spell(SPELLS.AVATAR_SHARED),
-        () => (this.boostedTitansTormentCount += 1),
+        this.avatarCast,
       );
     }
     this.addEventListener(
@@ -50,21 +50,46 @@ export default class T31Fury2pc extends Analyzer {
       this.bloodthirstCast,
     );
     this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER).spell([SPELLS.BLOODTHIRST, SPELLS.BLOODBATH]),
+      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.BLOODTHIRST),
       this.bloodthirstDamage,
+    );
+    this.addEventListener(
+      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.BLOODBATH),
+      this.bloodbathDamage,
     );
   }
 
+  /**
+   * Track if we cast Avatar or Odyns Fury last. This is to correctly track boosted damage to Titans Torment.
+   */
+  lastOFCast: Ability | null = null;
   boostedTitansTormentCount = 0;
+  boostedTitansTormentDamage = 0;
   boostedOdynsFuryCount = 0;
   boostedOdynsFuryDamage = 0;
   boostedBloodthirstCount = 0;
   boostedBloodthirstDamage = 0;
+  boostedBloodbathCount = 0;
+  boostedBloodbathDamage = 0;
 
   mainTarget: { id: number; instance?: number } | null = null;
 
+  private odynsFuryCast(event: CastEvent) {
+    this.boostedOdynsFuryCount += 1;
+    this.lastOFCast = event.ability;
+  }
+
+  private avatarCast(event: CastEvent) {
+    this.boostedTitansTormentCount += 1;
+    this.lastOFCast = event.ability;
+  }
+
   private odynsFuryDamage(event: DamageEvent) {
-    this.boostedOdynsFuryDamage += calculateEffectiveDamage(event, ODYNS_FURY_DMG_INCREASE);
+    if (this.lastOFCast?.guid === SPELLS.AVATAR_SHARED.id) {
+      this.boostedTitansTormentDamage += calculateEffectiveDamage(event, ODYNS_FURY_DMG_INCREASE);
+    } else {
+      this.boostedOdynsFuryDamage += calculateEffectiveDamage(event, ODYNS_FURY_DMG_INCREASE);
+    }
   }
 
   private bloodthirstCast(event: CastEvent) {
@@ -96,6 +121,19 @@ export default class T31Fury2pc extends Analyzer {
     }
   }
 
+  private bloodbathDamage(event: DamageEvent) {
+    // If damage is done to the main target after casting bloodbath with buff, we count damage.
+    if (
+      this.mainTarget !== null &&
+      event.targetID === this.mainTarget.id &&
+      event.targetInstance === this.mainTarget.instance
+    ) {
+      this.boostedBloodbathCount += 1;
+      this.boostedBloodbathDamage += calculateEffectiveDamage(event, BLOODTHIRST_DMG_INCREASE);
+      this.mainTarget = null;
+    }
+  }
+
   statistic() {
     return (
       <Statistic
@@ -105,34 +143,96 @@ export default class T31Fury2pc extends Analyzer {
         tooltip={
           <>
             <div>
-              A total of {this.boostedOdynsFuryCount + this.boostedTitansTormentCount}{' '}
-              {this.boostedTitansTormentCount > 0 ? (
+              All <SpellLink spell={SPELLS.ODYNS_FURY} /> deal{' '}
+              {formatPercentage(ODYNS_FURY_DMG_INCREASE, 0)}% more damage.
+            </div>
+            <div>
+              After each <SpellLink spell={SPELLS.ODYNS_FURY} />, the following 3{' '}
+              <SpellLink spell={SPELLS.BLOODTHIRST} />{' '}
+              {this.selectedCombatant.hasTalent(TALENTS.RECKLESS_ABANDON_TALENT) ? (
                 <>
-                  ({this.boostedOdynsFuryCount} <SpellLink spell={SPELLS.ODYNS_FURY} /> +{' '}
-                  {this.boostedTitansTormentCount}{' '}
-                  <SpellLink spell={TALENTS.TITANS_TORMENT_TALENT} />)
+                  {' '}
+                  or <SpellLink spell={SPELLS.BLOODBATH} />
                 </>
               ) : (
                 ''
               )}{' '}
-              casts was increased with a total of {formatNumber(this.boostedOdynsFuryDamage)}{' '}
-              damage.
+              deal {formatPercentage(BLOODTHIRST_DMG_INCREASE, 0)}% increased damage and have 100%
+              increased crit chance. Attacks cleaved to other targets with{' '}
+              <SpellLink spell={TALENTS.IMPROVED_WHIRLWIND_TALENT} /> is not affected.
             </div>
-            <div>
-              A total of {this.boostedBloodthirstCount} <SpellLink spell={SPELLS.BLOODTHIRST} />/
-              <SpellLink spell={SPELLS.BLOODBATH} /> casts was increased with a total of{' '}
-              {formatNumber(this.boostedBloodthirstDamage)} damage.{' '}
-              <b>This does not account for the incrreased crit chance!</b>
-            </div>
-            <div>
-              These combined for a total of{' '}
-              {formatNumber(this.boostedBloodthirstDamage + this.boostedOdynsFuryDamage)} damage.
-            </div>
+            <table className="table table-condensed">
+              <thead>
+                <tr>
+                  <th>Spell</th>
+                  <th>Affected Casts</th>
+                  <th>Damage Gained</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.boostedOdynsFuryCount > 0 && (
+                  <tr>
+                    <td>
+                      <SpellLink spell={SPELLS.ODYNS_FURY} />
+                    </td>
+                    <td>{this.boostedOdynsFuryCount}</td>
+                    <td>{formatNumber(this.boostedOdynsFuryDamage)}</td>
+                  </tr>
+                )}
+                {this.boostedTitansTormentCount > 0 && (
+                  <tr>
+                    <td>
+                      <SpellLink spell={TALENTS.TITANS_TORMENT_TALENT} />
+                    </td>
+                    <td>{this.boostedTitansTormentCount}</td>
+                    <td>{formatNumber(this.boostedTitansTormentDamage)}</td>
+                  </tr>
+                )}
+                {this.boostedBloodbathCount > 0 && (
+                  <tr>
+                    <td>
+                      <SpellLink spell={SPELLS.BLOODBATH} />
+                    </td>
+                    <td>{this.boostedBloodbathCount}</td>
+                    <td>{formatNumber(this.boostedBloodbathDamage)}*</td>
+                  </tr>
+                )}
+                <tr>
+                  <td>
+                    <SpellLink spell={SPELLS.BLOODTHIRST} />
+                  </td>
+                  <td>{this.boostedBloodthirstCount}</td>
+                  <td>
+                    {formatNumber(this.boostedBloodthirstDamage)}
+                    {this.boostedBloodthirstDamage > 0 ? '*' : ''}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Total</th>
+                  <th></th>
+                  <th>
+                    {formatNumber(
+                      this.boostedBloodthirstDamage +
+                        this.boostedBloodbathDamage +
+                        this.boostedOdynsFuryDamage +
+                        this.boostedTitansTormentDamage,
+                    )}
+                  </th>
+                </tr>
+              </tbody>
+            </table>
+            <div>* increased damage does not account for the increased crit chance</div>
           </>
         }
       >
         <BoringSpellValueText spell={SPELLS.T31_FURY_2P_BONUS}>
-          <ItemDamageDone amount={this.boostedBloodthirstDamage + this.boostedOdynsFuryDamage} />
+          <ItemDamageDone
+            amount={
+              this.boostedBloodthirstDamage +
+              this.boostedBloodbathDamage +
+              this.boostedOdynsFuryDamage
+            }
+          />
         </BoringSpellValueText>
       </Statistic>
     );
