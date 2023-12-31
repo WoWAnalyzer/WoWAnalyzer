2a87b3e63b09a34431f71a4850e3ea9e5d75cc4f
Vollmer <kristianvollmer@gmail.com>
2023-12-31 21:17:55 +0100

    Add SpellEssenceCost module

--- src/analysis/retail/evoker/shared/index.ts
+++ src/analysis/retail/evoker/shared/index.ts
@@ -8,4 +8,5 @@ export {
   getWastedEssenceBurst,
 } from './modules/normalizers/LeapingFlamesNormalizer';
 export { default as LeapingFlames } from './modules/talents/LeapingFlames';
+export { default as SpellEssenceCost } from './modules/core/essence/SpellEssenceCost';
 export * from './constants';
