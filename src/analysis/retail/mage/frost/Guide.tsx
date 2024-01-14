438fbd4c0b0241c7adce08439999aa58d39456c4
Eduardo Rosselot <earosselot@gmail.com>
2024-01-14 16:44:31 +0100

    added Icy Veins guide module

--- src/analysis/retail/mage/frost/Guide.tsx
+++ src/analysis/retail/mage/frost/Guide.tsx
@@ -7,42 +7,49 @@ import ActiveTimeGraph from 'parser/ui/ActiveTimeGraph';
 import { SpellLink } from 'interface';
 import TALENTS from 'common/TALENTS/mage';
 
+export const GUIDE_CORE_EXPLANATION_PERCENT = 50;
+
 export default function Guide({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
+  const alwaysBeCastingSubsection = (
+    <SubSection title="Active Time">
+      <Explanation>
+        <b>
+          Continuously casting throughout an encounter is the single most important thing for
+          achieving good DPS as a caster.
+        </b>
+        <p>
+          As mages we have <SpellLink spell={TALENTS.ICE_FLOES_TALENT} /> or{' '}
+          <SpellLink spell={TALENTS.SHIMMER_TALENT} />
+          to continue casting while dealing with mechanics that require movement.
+        </p>
+        <p>
+          Some fights have unavoidable downtime due to phase transitions and the like, so in these
+          cases 0% downtime will not be possible - do the best you can.
+        </p>
+      </Explanation>
+      <p>
+        Active Time:{' '}
+        <PerformanceStrong performance={modules.alwaysBeCasting.DowntimePerformance}>
+          {formatPercentage(modules.alwaysBeCasting.activeTimePercentage, 1)}%
+        </PerformanceStrong>{' '}
+        Cancelled Casts:{' '}
+        <PerformanceStrong performance={modules.cancelledCasts.CancelledPerformance}>
+          {formatPercentage(modules.cancelledCasts.cancelledPercentage, 1)}%
+        </PerformanceStrong>{' '}
+      </p>
+      <ActiveTimeGraph
+        activeTimeSegments={modules.alwaysBeCasting.activeTimeSegments}
+        fightStart={info.fightStart}
+        fightEnd={info.fightEnd}
+      />
+    </SubSection>
+  );
+
   return (
     <>
       <Section title="Core">
-        <SubSection title="Active Time">
-          <Explanation>
-            <b>
-              Continuously casting throughout an encounter is the single most important thing for
-              achieving good DPS as a caster.
-            </b>
-            <p>
-              As mages we have <SpellLink spell={TALENTS.ICE_FLOES_TALENT} /> or{' '}
-              <SpellLink spell={TALENTS.SHIMMER_TALENT} />
-              to continue casting while dealing with mechanics that require movement.
-            </p>
-            <p>
-              Some fights have unavoidable downtime due to phase transitions and the like, so in
-              these cases 0% downtime will not be possible - do the best you can.
-            </p>
-          </Explanation>
-          <p>
-            Active Time:{' '}
-            <PerformanceStrong performance={modules.alwaysBeCasting.DowntimePerformance}>
-              {formatPercentage(modules.alwaysBeCasting.activeTimePercentage, 1)}%
-            </PerformanceStrong>{' '}
-            Cancelled Casts:{' '}
-            <PerformanceStrong performance={modules.cancelledCasts.CancelledPerformance}>
-              {formatPercentage(modules.cancelledCasts.cancelledPercentage, 1)}%
-            </PerformanceStrong>{' '}
-          </p>
-          <ActiveTimeGraph
-            activeTimeSegments={modules.alwaysBeCasting.activeTimeSegments}
-            fightStart={info.fightStart}
-            fightEnd={info.fightEnd}
-          />
-        </SubSection>
+        {alwaysBeCastingSubsection}
+        {modules.icyVeins.guideSubsection}
       </Section>
     </>
   );
