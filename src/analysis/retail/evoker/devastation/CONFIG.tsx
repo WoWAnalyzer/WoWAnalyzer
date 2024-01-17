004a289a089af334cba400cd9fd850209c243306
Vollmer <kristianvollmer@gmail.com>
2024-01-17 10:38:19 +0100

    Bump Devastation to 10.2.5

--- src/analysis/retail/evoker/devastation/CONFIG.tsx
+++ src/analysis/retail/evoker/devastation/CONFIG.tsx
@@ -10,7 +10,7 @@ const config: Config = {
   contributors: [Vireve, Tyndi, Vollmer],
   expansion: Expansion.Dragonflight,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '10.2.0',
+  patchCompatibility: '10.2.5',
   isPartial: false,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
@@ -28,8 +28,7 @@ const config: Config = {
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport:
-    '/report/6RgwY1MV3CcJv792/25-Mythic+Scalecommander+Sarkareth+-+Kill+(7:06)/Nimeji/standard',
+  exampleReport: '/report/PQcAhw3LzpnrxFGJ/18-Mythic+Volcoross+-+Kill+(3:10)/Gilgvoker/standard',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
