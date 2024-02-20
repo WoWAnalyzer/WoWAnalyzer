e1305bd32aa8d1a46a6246bcbf8ca7d0fd21e30f
ZyerTCoder <protekthorwow@gmail.com>
2024-02-20 19:24:57 +0000

    cleaned up the guide

--- src/analysis/retail/warlock/demonology/Guide.tsx
+++ src/analysis/retail/warlock/demonology/Guide.tsx
@@ -1,17 +1,14 @@
 import { GuideProps, Section } from 'interface/guide';
 import CombatLogParser from './CombatLogParser';
 import PreparationSection from 'interface/guide/components/Preparation/PreparationSection';
-// import Rotation from './modules/guide/Rotation';
 import CooldownSubsection from './modules/guide/CooldownsSubsection';
 import ResourceUsage from './modules/guide/ResourceUsage';
-// import CooldownUsage from 'parser/core/MajorCooldowns/CooldownUsage';
 
 export default function Guide({ modules, events, info }: GuideProps<typeof CombatLogParser>) {
   return (
     <>
       <CooldownSection modules={modules} events={events} info={info} />
       <ResourceUsage modules={modules} events={events} info={info} />
-      {/* <Rotation modules={modules} events={events} info={info} /> */}
       <PreparationSection />
     </>
   );
@@ -21,7 +18,6 @@ function CooldownSection({ modules }: GuideProps<typeof CombatLogParser>) {
   return (
     <Section title="Cooldowns">
       <CooldownSubsection />
-      {/* <CooldownUsage analyzer={modules.summonDemonicTyrant} title="Summon Demonic Tyrant"/> */}
       {modules.summonDemonicTyrant.guideSubsection}
     </Section>
   );
