62a7a042f24b3ff3e7d3860be1ed2b9a55e6a547
Richard Harrah <topplethenunnery@gmail.com>
2024-03-08 00:30:24 -0500

    fix TS2612 findings

--- src/analysis/classic/shaman/shared/totems/WaterTotems.tsx
+++ src/analysis/classic/shaman/shared/totems/WaterTotems.tsx
@@ -1,16 +1,12 @@
 import { Options } from 'parser/core/Analyzer';
 
 import { TotemElements } from './totemConstants';
-import TotemTracker from '../TotemTracker';
 import BaseTotem from './BaseTotem';
 
 export default class WaterTotems extends BaseTotem {
   static dependencies = {
     ...BaseTotem.dependencies,
-    totemTracker: TotemTracker,
   };
-
-  protected totemTracker!: TotemTracker;
 
   constructor(options: Options) {
     super(options, TotemElements.Water);
