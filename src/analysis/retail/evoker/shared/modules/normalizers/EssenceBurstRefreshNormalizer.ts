c6ce2b430dc8e25c65fe370030c26d2fe6ac4723
Vollmer <kristianvollmer@gmail.com>
2024-04-08 12:04:11 +0200

    refactor EssenceBurstRefreshNormalizer so it removes the proper abilities

--- src/analysis/retail/evoker/shared/modules/normalizers/EssenceBurstRefreshNormalizer.ts
+++ src/analysis/retail/evoker/shared/modules/normalizers/EssenceBurstRefreshNormalizer.ts
@@ -9,39 +9,29 @@ const MAX_DELAY = 25;
  * This normalizer removes the refresh event because it messes up analysis.
  */
 class EssenceBurstRefreshNormalizer extends EventsNormalizer {
-  isEbEvent(event: AnyEvent) {
-    return (
-      (event.type === EventType.RemoveBuffStack || event.type === EventType.RefreshBuff) &&
-      EB_BUFF_IDS.includes(event.ability.guid)
-    );
-  }
   normalize(events: AnyEvent[]) {
     const fixedEvents: AnyEvent[] = [];
-    let skipNext = false;
-    events.forEach((event, eventIndex) => {
-      if (skipNext) {
-        skipNext = false;
-      } else if (this.isEbEvent(event)) {
-        const castTimestamp = event.timestamp;
-        // Look ahead through the events to see if there is a Refresh/RemoveStack at nearly same timestamp
-        for (
-          let nextEventIndex = eventIndex;
-          nextEventIndex < events.length - 1;
-          nextEventIndex += 1
-        ) {
-          const nextEvent = events[nextEventIndex];
-          if (this.isEbEvent(nextEvent) && nextEvent.timestamp - castTimestamp <= MAX_DELAY) {
-            // Only keep the RemoveBuffStack event
-            if (nextEvent.type === EventType.RemoveBuffStack) {
-              fixedEvents.push(nextEvent);
-            } else {
-              fixedEvents.push(event);
-            }
-            skipNext = true;
-            break;
-          }
-        }
-      } else {
+    let lastEbRemoveBuffStack = 0;
+
+    events.forEach((event) => {
+      if (event.type !== EventType.RemoveBuffStack && event.type !== EventType.RefreshBuff) {
+        fixedEvents.push(event);
+        return;
+      }
+      if (!EB_BUFF_IDS.includes(event.ability.guid)) {
+        fixedEvents.push(event);
+        return;
+      }
+
+      if (event.type === EventType.RemoveBuffStack) {
+        // We always want this event
+        fixedEvents.push(event);
+        lastEbRemoveBuffStack = event.timestamp;
+        return;
+      }
+
+      const diff = event.timestamp - lastEbRemoveBuffStack;
+      if (diff > MAX_DELAY) {
         fixedEvents.push(event);
       }
     });
