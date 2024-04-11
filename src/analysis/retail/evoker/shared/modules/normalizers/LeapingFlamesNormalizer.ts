84d332b90384da78ae3989ef157a674e33d23f50
Vollmer <kristianvollmer@gmail.com>
2024-04-11 14:44:31 +0200

    Implement maxLinks check for LEAPING_FLAMES_HITS cast link check

--- src/analysis/retail/evoker/shared/modules/normalizers/LeapingFlamesNormalizer.ts
+++ src/analysis/retail/evoker/shared/modules/normalizers/LeapingFlamesNormalizer.ts
@@ -12,12 +12,16 @@ import {
   HealEvent,
   RemoveBuffEvent,
   AnyEvent,
+  ApplyBuffStackEvent,
 } from 'parser/core/Events';
 import { encodeEventTargetString } from 'parser/shared/modules/Enemies';
+import LeapingFlamesBuffNormalizer from './LeapingFlamesBuffNormalizer';
 
 export const LEAPING_FLAMES_HITS = 'leapingFlamesHits';
 export const LEAPING_FLAMES_CONSUME = 'leapingFlamesConsume';
+export const LEAPING_FLAMES_BUFF = 'leapingFlamesBuff';
 const LEAPING_FLAMES_CONSUME_BUFFER = 35;
+const LEAPING_FLAMES_BUFF_BUFFER = 30_000;
 
 export const LIVING_FLAME_CAST_HIT = 'livingFlameCastHit';
 const LIVING_FLAME_HIT_BUFFER = 1000; // LF has travel time
@@ -39,6 +43,26 @@ const EVENT_LINKS: EventLink[] = [
     anyTarget: true,
     forwardBufferMs: LEAPING_FLAMES_CONSUME_BUFFER,
     backwardBufferMs: LEAPING_FLAMES_CONSUME_BUFFER,
+    isActive(c) {
+      return c.hasTalent(TALENTS.LEAPING_FLAMES_TALENT);
+    },
+  },
+  {
+    linkRelation: LEAPING_FLAMES_BUFF,
+    reverseLinkRelation: LEAPING_FLAMES_BUFF,
+    linkingEventId: SPELLS.LEAPING_FLAMES_BUFF.id,
+    linkingEventType: [EventType.ApplyBuff, EventType.ApplyBuffStack],
+    referencedEventId: SPELLS.LEAPING_FLAMES_BUFF.id,
+    referencedEventType: EventType.RemoveBuff,
+    forwardBufferMs: LEAPING_FLAMES_BUFF_BUFFER,
+    anyTarget: true,
+    maximumLinks: 1,
+    additionalCondition(_linkingEvent, referencedEvent) {
+      /** Condition to ensure that we only ever link unique hits to cast
+       * LF has travel time so you can technically cast back-to-back before
+       * the first LF hits */
+      return !HasRelatedEvent(referencedEvent, LEAPING_FLAMES_BUFF);
+    },
     isActive(c) {
       return c.hasTalent(TALENTS.LEAPING_FLAMES_TALENT);
     },
@@ -73,13 +97,29 @@ const EVENT_LINKS: EventLink[] = [
     // can be seen here at 0:30
     // /report/vDVJKnwGmAM9tCQN/48-Mythic+Volcoross+-+Kill+(2:49)/Vollmer/standard/events
     backwardBufferMs: BACKWARDS_BUFFER,
+    maximumLinks: (c) =>
+      c.hasTalent(TALENTS.FONT_OF_MAGIC_AUGMENTATION_TALENT) ||
+      c.hasTalent(TALENTS.FONT_OF_MAGIC_DEVASTATION_TALENT) ||
+      c.hasTalent(TALENTS.FONT_OF_MAGIC_PRESERVATION_TALENT)
+        ? 4
+        : 3,
     additionalCondition(linkingEvent, referencedEvent) {
       if (!HasRelatedEvent(linkingEvent, LEAPING_FLAMES_CONSUME)) {
         return false;
       }
 
+      /** To ensure we don't over attribute hits to Leaping Flames
+       * we need to check for amount of targets hit, and max possible */
       const previousLeapingHits = getLeapingEvents(linkingEvent as CastEvent);
+      const maxPossibleHits = leapingFlamesStackCount(linkingEvent);
+      if (previousLeapingHits.length >= maxPossibleHits) {
+        return false;
+      }
+
+      /** Make sure we don't attribute to the main target */
       const curTarget = encodeEventTargetString(referencedEvent);
+      /** Talents like Pupil of Alexstrasza will also cleave LF
+       * so we just need to make sure we don't double dip the same target */
       const targetHitBefore = previousLeapingHits.some(
         (prevEvent) => encodeEventTargetString(prevEvent) === curTarget,
       );
@@ -93,6 +133,11 @@ const EVENT_LINKS: EventLink[] = [
 ];
 
 class LeapingFlamesNormalizer extends EventLinkNormalizer {
+  static dependencies = {
+    ...EventLinkNormalizer.dependencies,
+    leapingFlamesBuffNormalizer: LeapingFlamesBuffNormalizer,
+  };
+
   constructor(options: Options) {
     super(options, EVENT_LINKS);
   }
@@ -129,6 +174,20 @@ export function getLeapingCast(event: RemoveBuffEvent): CastEvent | undefined {
     LEAPING_FLAMES_CONSUME,
     (e): e is CastEvent => e.type === EventType.Cast,
   );
+}
+
+function leapingFlamesStackCount(event: AnyEvent): number {
+  const leapingRemove = GetRelatedEvent(event, LEAPING_FLAMES_CONSUME);
+  if (!leapingRemove) {
+    return 1;
+  }
+  const applyEvent = GetRelatedEvent<ApplyBuffStackEvent>(leapingRemove, LEAPING_FLAMES_BUFF);
+
+  if (!applyEvent || applyEvent.type !== EventType.ApplyBuffStack) {
+    return 1;
+  }
+
+  return applyEvent.stack;
 }
 
 export default LeapingFlamesNormalizer;
