a08a495ffc50eb2bdd044e3f2fe9f1f2d0abad9d
emallson <emallson@emallson.net>
2024-04-12 14:13:45 -0400

    useReport/usePlayer also throw

--- src/interface/report/ConfigContext.tsx
+++ src/interface/report/ConfigContext.tsx
@@ -5,8 +5,29 @@ import getConfig from 'parser/getConfig';
 import { wclGameVersionToExpansion } from 'game/VERSIONS';
 
 export const useMaybeConfig = (): Config | undefined => {
-  const { report } = useReport();
-  const { player, combatant } = usePlayer();
+  // this mess of try/catch papers over some historical choices to throw for these contexts instead of allowing them to return undefined.
+  // that refactor is...big enough that i'm not doing it today
+  let report = undefined;
+  try {
+    const data = useReport();
+    report = data.report;
+  } catch {
+    /* do nothing */
+  }
+  let player = undefined;
+    let combatant = undefined;
+
+  try {
+    const playerData = usePlayer();
+    player = playerData.player;
+    combatant = playerData.combatant;
+  } catch {
+    /* do nothing */
+  }
+
+  if (!report || !player || !combatant) {
+    return undefined;
+  }
   return getConfig(
     wclGameVersionToExpansion(report.gameVersion),
     combatant.specID,
