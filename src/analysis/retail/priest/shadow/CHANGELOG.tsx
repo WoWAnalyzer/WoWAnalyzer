baab0be82a60e8ef1bd63b9a5750f61913584579
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2024-04-23 12:32:26 -0400

    Season 4 tier statistic

--- src/analysis/retail/priest/shadow/CHANGELOG.tsx
+++ src/analysis/retail/priest/shadow/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { DoxAshe, Havoc, Sref, ToppleTheNun } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2024, 4, 22), <>Add statistics for the two and four piece Season 4 tier set</>,DoxAshe),
   change(date(2024, 4, 18), <>Add statistics for <SpellLink spell={TALENTS.PHANTASMAL_PATHOGEN_TALENT}/> and <SpellLink spell={TALENTS.TORMENTED_SPIRITS_TALENT}/> </>,DoxAshe),
   change(date(2024, 4, 16), <>Fix broken icons, update sugestion thresholds and remove outdated checklist view</>,DoxAshe),
   change(date(2024, 3, 21), <>Fix error in <SpellLink spell={TALENTS.SURGE_OF_INSANITY_TALENT}/> </>,DoxAshe),
