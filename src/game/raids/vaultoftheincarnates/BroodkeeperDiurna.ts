3d2478b8c5fa04c9a1f67ac641fae3383b8f9bba
Richard Harrah <topplethenunnery@gmail.com>
2024-04-24 00:13:58 -0400

    add missing headshots for VotI

--- src/game/raids/vaultoftheincarnates/BroodkeeperDiurna.ts
+++ src/game/raids/vaultoftheincarnates/BroodkeeperDiurna.ts
@@ -1,11 +1,13 @@
 import type { Boss } from 'game/raids';
 
-import Background from './images/broodkeeper.jpg';
+import Background from './backgrounds/broodkeeper.jpg';
+import Headshot from './headshots/BroodkeeperDiurna.jpg';
 
 const BroodkeeperDiurna: Boss = {
   id: 2614,
   name: 'Broodkeeper Diurna',
   background: Background,
+  headshot: Headshot,
   icon: 'achievement_raidprimalist_diurna',
   fight: {},
 };
