48893757378eada6f8a2f43c4bf4f6748d1d1846
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2024-04-29 19:28:14 -0400

    Add Mastermind Statistic

--- src/analysis/retail/priest/shadow/modules/talents/Mastermind.tsx
+++ src/analysis/retail/priest/shadow/modules/talents/Mastermind.tsx
@@ -3,7 +3,10 @@ import SPELLS from 'common/SPELLS';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import { ApplyBuffStackEvent, DamageEvent } from 'parser/core/Events';
 import Events from 'parser/core/Events';
-import { calculateEffectiveDamageFromCritIncrease } from 'parser/core/EventCalculateLib';
+import {
+  calculateEffectiveDamage,
+  calculateEffectiveDamageFromCritIncrease,
+} from 'parser/core/EventCalculateLib';
 import StatTracker from 'parser/shared/modules/StatTracker';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
@@ -11,7 +14,10 @@ import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import HIT_TYPES from 'game/HIT_TYPES';
 
-class MindMelt extends Analyzer {
+import { MASTERMIND_CRIT_CHANCE_PER_RANK } from '../../constants';
+import { MASTERMIND_CRIT_DAMAGE_PER_RANK } from '../../constants';
+
+class Mastermind extends Analyzer {
   static dependencies = {
     statTracker: StatTracker,
   };
@@ -20,28 +26,35 @@ class MindMelt extends Analyzer {
 
   damage = 0;
   buffStacks = 0;
-  multiplierCritMindMelt = 0.2; //20% per stack
+
+  mastermindCritChance =
+    MASTERMIND_CRIT_CHANCE_PER_RANK *
+    this.selectedCombatant.getTalentRank(TALENTS.MASTERMIND_TALENT); //increase in crit chance
+  mastermindCritDamage =
+    MASTERMIND_CRIT_DAMAGE_PER_RANK *
+    this.selectedCombatant.getTalentRank(TALENTS.MASTERMIND_TALENT); //increase in crit damage
 
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.hasTalent(TALENTS.MIND_MELT_TALENT);
 
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MIND_BLAST), this.onSpell);
     this.addEventListener(
-      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.MIND_MELT_TALENT_BUFF),
-      this.onBuffApplied,
+      Events.damage.by(SELECTED_PLAYER).spell(TALENTS.SHADOW_WORD_DEATH_TALENT),
+      this.onSpell,
+    );
+    this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MIND_FLAY), this.onSpell);
+    this.addEventListener(
+      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MIND_FLAY_INSANITY_TALENT_DAMAGE),
+      this.onSpell,
     );
     this.addEventListener(
-      Events.applybuffstack.by(SELECTED_PLAYER).spell(SPELLS.MIND_MELT_TALENT_BUFF),
-      this.onBuffStack,
+      Events.damage.by(SELECTED_PLAYER).spell(TALENTS.MIND_SPIKE_TALENT),
+      this.onSpell,
     );
     this.addEventListener(
-      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.MIND_MELT_TALENT_BUFF),
-      this.onBuffRemoved,
-    );
-
-    this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MIND_BLAST),
-      this.onMindBlast,
+      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MIND_SPIKE_INSANITY_TALENT_DAMAGE),
+      this.onSpell,
     );
   }
 
@@ -55,14 +68,18 @@ class MindMelt extends Analyzer {
     this.buffStacks = 0;
   }
 
-  onMindBlast(event: DamageEvent) {
+  onSpell(event: DamageEvent) {
     if (event.hitType === HIT_TYPES.CRIT) {
       //only crit events should be sent to effectiveDamageFromCritIncrease,
+
       this.damage += calculateEffectiveDamageFromCritIncrease(
+        //Extra damage from having extra crit chance
         event,
         this.statTracker.currentCritPercentage,
-        this.buffStacks * this.multiplierCritMindMelt,
+        this.mastermindCritChance,
       );
+
+      this.damage += calculateEffectiveDamage(event, this.mastermindCritDamage); //Extra damage from having extra crit damage
     }
   }
 
@@ -71,7 +88,7 @@ class MindMelt extends Analyzer {
       <Statistic
         category={STATISTIC_CATEGORY.TALENTS}
         size="flexible"
-        tooltip="This is the damage gained from the critical increase"
+        tooltip="This is the damage gained from the critical chance and damage increase"
       >
         <BoringSpellValueText spell={TALENTS.MIND_MELT_TALENT}>
           <div>
@@ -83,4 +100,4 @@ class MindMelt extends Analyzer {
   }
 }
 
-export default MindMelt;
+export default Mastermind;
