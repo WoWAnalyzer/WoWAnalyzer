6e28a3911b2929e08a817e773faf3774d089751c
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-05-03 08:38:22 +0100

    [Discipline] Formatting Fix

--- src/analysis/retail/priest/discipline/modules/spells/TwilightEquilibrium/TwilightEquilibrium.tsx
+++ src/analysis/retail/priest/discipline/modules/spells/TwilightEquilibrium/TwilightEquilibrium.tsx
@@ -102,8 +102,11 @@ class TwilightEquilibrium extends Analyzer {
   checkDamageEvent(event: DamageEvent) {
     // Shadow Smite is buffed by Holy TE's Whitelist and has no associated cast.
     if (event.ability.guid === SPELLS.SHADOW_SMITE.id) {
-      if (this.selectedCombatant.hasBuff(SPELLS.TWILIGHT_EQUILIBRIUM_HOLY_BUFF.id, event.timestamp))
-        {return true;}
+      if (
+        this.selectedCombatant.hasBuff(SPELLS.TWILIGHT_EQUILIBRIUM_HOLY_BUFF.id, event.timestamp)
+      ) {
+        return true;
+      }
 
       return false;
     }
