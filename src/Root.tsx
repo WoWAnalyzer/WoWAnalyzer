6a584016abc28a6a6d0b8871c90954b7352036e9
Richard Harrah <topplethenunnery@gmail.com>
2024-06-03 22:27:00 -0400

    replace react-helmet with react-helmet-async

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,18 +5,21 @@ import { store } from 'store';
 import RootErrorBoundary from 'interface/RootErrorBoundary';
 
 import I18nProvider from './localization/I18nProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 interface Props {
   children?: ReactNode;
 }
 
 const Root = ({ children }: Props) => (
-  <ReduxProvider store={store}>
-    <I18nProvider>
-      {/* We need to place the error boundary inside all providers since it uses i18n for localized messages. */}
-      <RootErrorBoundary>{children ?? <App />}</RootErrorBoundary>
-    </I18nProvider>
-  </ReduxProvider>
+  <HelmetProvider>
+    <ReduxProvider store={store}>
+      <I18nProvider>
+        {/* We need to place the error boundary inside all providers since it uses i18n for localized messages. */}
+        <RootErrorBoundary>{children ?? <App />}</RootErrorBoundary>
+      </I18nProvider>
+    </ReduxProvider>
+  </HelmetProvider>
 );
 
 export default Root;
