653311bd364166a701c6bb6a91205a743eb13e40
David Smith <emallson@emallson.net>
2024-06-10 11:18:54 -0400

    initial bdk setup (no spells yet)

---
+++ src/analysis/classic/deathknight/blood/CHANGELOG.tsx
@@ -0,0 +1,8 @@
+import { change, date } from 'common/changelog';
+import { jazminite } from 'CONTRIBUTORS';
+
+export default [
+  // Remove these entries and add your own
+  change(date(2023, 2, 3), 'Small text update to Component Checklist.', jazminite),
+  change(date(2023, 2, 3), 'Add Classic Melee spec template.', jazminite),
+];
