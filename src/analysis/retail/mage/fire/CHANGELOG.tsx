699b5a4db5aa06b514b8a8f9b034a17eab550f70
Sharrq <cjgrasty86@gmail.com>
2024-06-16 16:29:58 -0400

    changelog

--- src/analysis/retail/mage/fire/CHANGELOG.tsx
+++ src/analysis/retail/mage/fire/CHANGELOG.tsx
@@ -3,5 +3,7 @@ import { Sharrq } from 'CONTRIBUTORS';
 
 // prettier-ignore
 export default [
-  change(date(2024, 6, 16), 'Initial The War Within support', Sharrq),
+  change(date(2024, 6, 16), <>Updated the Fire Spec Spellbook and the Mage Class Spellbook.</>, Sharrq),
+  change(date(2024, 6, 16), <>Remove Living Bomb, Searing Touch, and Charring Embers</>, Sharrq),
+  change(date(2024, 6, 16), <>Initial The War Within support</>, Sharrq),
 ];
