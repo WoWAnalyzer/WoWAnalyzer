064eb27d39bc87550cdd5905ee7f9a39fa18862b
vohrr <matthew.d.pingel@gmail.com>
2024-06-16 11:17:16 -0400

    Remove unecessary conditions on VV event link. More suggestion threshold cleanup. visual cleanup on mana tea

--- src/analysis/retail/monk/mistweaver/normalizers/EventLinks/VivifyEventLinks.ts
+++ src/analysis/retail/monk/mistweaver/normalizers/EventLinks/VivifyEventLinks.ts
@@ -1,12 +1,11 @@
 import SPELLS from 'common/SPELLS';
 import { EventLink } from 'parser/core/EventLinkNormalizer';
-import { CastEvent, EventType, HasRelatedEvent, RemoveBuffEvent } from 'parser/core/Events';
+import { EventType } from 'parser/core/Events';
 import {
   VIVIFY,
   CAST_BUFFER_MS,
   ZEN_PULSE_VIVIFY,
   VIVACIOUS_VIVIFICATION,
-  VIVACIOUS_VIVIFICATION_CAST,
 } from './EventLinkConstants';
 import { TALENTS_MONK } from 'common/TALENTS';
 
@@ -43,26 +42,6 @@ export const VIVIFY_EVENT_LINKS: EventLink[] = [
     backwardBufferMs: 50,
     anyTarget: true,
     maximumLinks: 1,
-    additionalCondition(linkingEvent, referencedEvent) {
-      return HasRelatedEvent(referencedEvent, VIVACIOUS_VIVIFICATION_CAST);
-    },
-    isActive(c) {
-      return c.hasTalent(TALENTS_MONK.VIVACIOUS_VIVIFICATION_TALENT);
-    },
-  },
-  {
-    linkRelation: VIVACIOUS_VIVIFICATION_CAST,
-    linkingEventId: SPELLS.VIVIFICATION_BUFF.id,
-    linkingEventType: [EventType.RemoveBuff],
-    referencedEventId: SPELLS.VIVIFY.id,
-    referencedEventType: [EventType.Cast],
-    forwardBufferMs: 50,
-    backwardBufferMs: 50,
-    maximumLinks: 1,
-    anyTarget: true,
-    additionalCondition(linkingEvent, referencedEvent) {
-      return (linkingEvent as RemoveBuffEvent).sourceID === (referencedEvent as CastEvent).sourceID;
-    },
     isActive(c) {
       return c.hasTalent(TALENTS_MONK.VIVACIOUS_VIVIFICATION_TALENT);
     },
