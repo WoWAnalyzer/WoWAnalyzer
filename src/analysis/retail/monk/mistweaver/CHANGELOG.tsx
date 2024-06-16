c8d20c2ce6cfe19515b2a45185638e747258ff84
vohrr <matthew.d.pingel@gmail.com>
2024-06-16 13:12:55 -0400

    another changelog

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { Vohrr } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2024, 6, 16), <>Added <SpellLink spell={TALENTS_MONK.ZEN_PULSE_TALENT}/>.</>, Vohrr),
   change(date(2024, 6, 16), <>Removed old checklist and suggestion thresholds. Updated <SpellLink spell={SPELLS.VIVIFY}/> event linking to fix cast entry bugs. Added healing boost statistic for <SpellLink spell={TALENTS_MONK.VIVACIOUS_VIVIFICATION_TALENT}/>.</>, Vohrr),
   change(date(2024, 6, 14), <>Added <SpellLink spell={TALENTS_MONK.CRANE_STYLE_TALENT}/> analysis.</>, Vohrr),
   change(date(2024, 6, 14), <>Update <SpellLink spell={SPELLS.GUSTS_OF_MISTS}/> source breakdown to include <SpellLink spell={TALENTS_MONK.JADEFIRE_STOMP_TALENT}/> and <SpellLink spell={TALENTS_MONK.CRANE_STYLE_TALENT}/>. </>, Vohrr),
