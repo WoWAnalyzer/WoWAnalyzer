d8b23bb54547c9116f6c5b6507aee96959c0561b
vohrr <matthew.d.pingel@gmail.com>
2024-06-15 20:12:05 -0400

    reorganize event link constants and add new

--- src/analysis/retail/monk/mistweaver/normalizers/EventLinks/EventLinkConstants.ts
+++ src/analysis/retail/monk/mistweaver/normalizers/EventLinks/EventLinkConstants.ts
@@ -1,3 +1,4 @@
+//Renewing Mist
 export const APPLIED_HEAL = 'AppliedHeal';
 export const FORCE_BOUNCE = 'ForceBounce';
 export const OVERHEAL_BOUNCE = 'OverhealBounce';
@@ -5,9 +6,16 @@ export const BOUNCED = 'Bounced';
 export const FROM_DANCING_MISTS = 'FromDM';
 export const SOURCE_APPLY = 'SourceApply';
 export const FROM_HARDCAST = 'FromHardcast';
-export const FROM_MISTY_PEAKS = 'FromMistyPeaks';
-export const FROM_MISTS_OF_LIFE = 'FromMOL';
 export const FROM_RAPID_DIFFUSION = 'FromRD'; // can be linked to env mist or rsk cast
+export const FROM_MISTS_OF_LIFE = 'FromMOL';
+export const RAPID_DIFFUSION_BUFFER_MS = 300;
+export const DANCING_MIST_BUFFER_MS = 250;
+export const MAX_REM_DURATION = 77000;
+
+//Enveloping Mist
+export const FROM_MISTY_PEAKS = 'FromMistyPeaks';
+
+//Mastery
 export const ENVELOPING_MIST_GOM = 'EnvGOM';
 export const RENEWING_MIST_GOM = 'ReMGOM';
 export const VIVIFY_GOM = 'ViVGOM';
@@ -16,15 +24,29 @@ export const SHEILUNS_GIFT_GOM = 'SGGOM';
 export const SHEILUNS_GIFT = 'SheilunsGift';
 export const EXPEL_HARM_GOM = 'EHGOM';
 export const SOOM_GOM = 'SoomGOM';
+export const JFS_GOM = 'JadefireStompGOM';
+export const CRANE_STYLE_GOM = 'CraneStyleGOM';
+
+//Vivify
 export const VIVIFY = 'Vivify';
-export const CALMING_COALESCENCE = 'Calming Coalescence';
+export const VIVACIOUS_VIVIFICATION = 'VivaciousVivification';
+export const ZEN_PULSE_VIVIFY = 'ZenPulseVivify';
+export const CHI_WAVE_VIVIFY = 'ChiWaveVivify';
+
+//Refreshing Jade Wind
+export const RJW_TFT = 'RefreshingJadeWindTFT';
+export const RJW_CELESTIAL = 'RefreshingJadeWindCelestial';
+
+//Mana Tea
 export const MANA_TEA_CHANNEL = 'MTChannel';
 export const MANA_TEA_CAST_LINK = 'MTLink';
 export const MT_BUFF_REMOVAL = 'MTStack';
 export const LIFECYCLES = 'Lifecycles';
 export const MT_STACK_CHANGE = 'MTStackChange';
-export const RAPID_DIFFUSION_BUFFER_MS = 300;
-export const DANCING_MIST_BUFFER_MS = 250;
-export const CAST_BUFFER_MS = 100;
 export const MAX_MT_CHANNEL = 25000;
-export const MAX_REM_DURATION = 77000;
+export const MANA_TEA_YULONS_WHISPER = 'MTYulonsWhisper';
+
+//Misc
+export const CALMING_COALESCENCE = 'Calming Coalescence';
+export const CAST_BUFFER_MS = 100;
+export const CHI_WAVE_RSK = 'ChiWaveRSK';
