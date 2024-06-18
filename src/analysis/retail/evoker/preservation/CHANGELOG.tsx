7ee60322e92e3e4c98078b275e4dda35af56b7f6
Harrek <sergiomm.like@gmail.com>
2024-06-17 19:36:26 -0600

    Update CHANGELOG.tsx

--- src/analysis/retail/evoker/preservation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/preservation/CHANGELOG.tsx
@@ -1,7 +1,10 @@
 import { change, date } from 'common/changelog';
-import { Trevor} from 'CONTRIBUTORS';
+import { Trevor, Harrek} from 'CONTRIBUTORS';
+import { SpellLink } from 'interface';
+import { TALENTS_EVOKER } from 'common/TALENTS';
 
 export default [
+  change(date(2024, 6, 17), <>Add <SpellLink spell={TALENTS_EVOKER.TITANS_GIFT_TALENT} /> module</>, Harrek),
   change(date(2024, 6, 16), <>Add T32 tier set module</>, Trevor),
   change(date(2024, 6, 16), <>Split up linking normalizer files</>, Trevor),
   change(date(2024, 6, 16), <>Cleanup old tier sets</>, Trevor),
