24d2dbee212237767d985db3f04031fe99214085
trevorm4 <trevorm4@illinois.edu>
2024-06-19 19:50:15 -0700

    changelog

--- src/analysis/retail/evoker/preservation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/preservation/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { Trevor} from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2024, 6, 19), <>Add <SpellLink spell={TALENTS_EVOKER.ENGULF_TALENT}/> to <SpellLink spell={TALENTS_EVOKER.ECHO_TALENT}/> modules</>, Trevor),
   change(date(2024, 6, 19), <>Implement <SpellLink spell={TALENTS_EVOKER.EXPANDED_LUNGS_TALENT}/> module</>, Trevor),
   change(date(2024, 6, 19), <>Remove <SpellLink spell={TALENTS_EVOKER.REWIND_TALENT}/> from <SpellLink spell={TALENTS_EVOKER.ENGULF_TALENT}/> module</>, Trevor),
   change(date(2024, 6, 19), <>Improve accuracy of T32 tier set module</>, Trevor),
