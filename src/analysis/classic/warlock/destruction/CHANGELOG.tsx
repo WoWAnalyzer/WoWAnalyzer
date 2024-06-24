0e0f3a65a3d2b165640e249c03795bd16bf7b5ed
emallson <emallson@emallson.net>
2024-06-24 11:16:56 -0400

    review comments

--- src/analysis/classic/warlock/destruction/CHANGELOG.tsx
+++ src/analysis/classic/warlock/destruction/CHANGELOG.tsx
@@ -2,6 +2,5 @@ import { change, date } from 'common/changelog';
 import { emallson } from 'CONTRIBUTORS';
 
 export default [
-  // REMOVE THIS ENTRY AND ADD YOUR OWN
   change(date(2024, 6, 21), 'Implemented core support for Destruction Warlock', emallson),
 ];
