bd8df757204a056a3aec6c1409d7ccb67b0dcbd3
emallson <emallson@emallson.net>
2024-06-26 19:37:06 -0400

    maybe (?) preemptively fix e2e
    its a pain to run on this computer so...i'm not

--- scripts/e2e/generate-configs.ts
+++ scripts/e2e/generate-configs.ts
@@ -89,9 +89,12 @@ export const generateConfigs = async () => {
               cfg[property.key.name] = property.value.property.name;
             }
             break;
-          case 'isPartial':
-            if (property.value.type === 'BooleanLiteral') {
-              cfg[property.key.name] = property.value.value;
+          case 'supportLevel':
+            if (
+              property.value.type === 'MemberExpression' &&
+              property.value.property.type === 'Identifier'
+            ) {
+              cfg.isPartial = property.value.property.name === 'Unmaintained';
             }
         }
       }
