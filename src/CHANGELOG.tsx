31a4d59fb1f1cac2aba278cce3c01605ab5f06c1
Kelton Finch <keltonfinch@gmail.com>
2024-07-16 19:22:36 -0400

    Changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -36,6 +36,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 7, 16), 'Fixed an issue where naturally regenerating resources were not showing correctly on resource graphs', Sref),
   change(date(2024, 7, 16), 'Update Shaman spells for Classic Cataclysm', jazminite),
   change(date(2024, 7, 15), 'Significantly improve loading time for M+ logs', emallson),
   change(date(2024, 7, 10), 'Improve debugging information for Global Cooldown tracking', emallson),
