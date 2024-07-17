25a3a69a9ad71e2bf4e4cf2e26e1c013ff311636
Jazminite <65823478+jazminite@users.noreply.github.com>
2024-07-17 16:47:37 -0400

    [Classic] Cata Warlock Affliction (#6870)
    * [Classic] Cata Warlock Affliction
    
    * remove pages from config

--- src/analysis/classic/warlock/affliction/CHANGELOG.tsx
+++ src/analysis/classic/warlock/affliction/CHANGELOG.tsx
@@ -2,9 +2,5 @@ import { change, date } from 'common/changelog';
 import { jazminite } from 'CONTRIBUTORS';
 
 export default [
-  change(date(2023, 12, 5), 'Add Shadow Trance and Eradication buffs', jazminite),
-  change(date(2023, 4, 2), 'Remove static entries for Healthstone use in favor of HealthstoneChecker.', jazminite),
-  change(date(2022, 12, 30), 'Add trinkets to Buffs for timeline highlight.', jazminite),
-  change(date(2022, 11, 14), 'Add channeling spells to improve downtime detection.', jazminite),
-  change(date(2022, 11, 13), 'Separate out Classic Affliction Warlock.', jazminite),
+  change(date(2024, 7, 16), 'Add foundation guide for Cata Classic Warlock Affliction spec.', jazminite),
 ];
