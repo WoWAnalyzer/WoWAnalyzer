61443c0eef8a54a93f773cdd2c9724534e12cde2
Jazminite <jazminitehs@gmail.com>
2024-07-17 07:21:20 -0400

    [Classic] Cata Warlock Affliction

--- src/analysis/classic/warlock/affliction/CONFIG.tsx
+++ src/analysis/classic/warlock/affliction/CONFIG.tsx
@@ -2,34 +2,28 @@ import { jazminite } from 'CONTRIBUTORS';
 import GameBranch from 'game/GameBranch';
 import SPECS from 'game/SPECS';
 import type Config from 'parser/Config';
-
 import CHANGELOG from './CHANGELOG';
-import AlertInfo from 'interface/AlertInfo';
 import { SupportLevel } from 'parser/Config';
+import AlertInfo from 'interface/AlertInfo';
 
 const CONFIG: Config = {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
   contributors: [jazminite],
   branch: GameBranch.Classic,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: null,
-  supportLevel: SupportLevel.Unmaintained,
+  patchCompatibility: '4.4.0',
+  supportLevel: SupportLevel.Foundation,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
     <>
-      Welcome! Thank you for using this tool to improve your overall Affliction Warlock game play.
-      The tool is under active development and may provide suggestions that are strange.
+      Welcome! Thanks for checking out WoWAnalyzer.
       <br />
-      If you have any suggestions or comments, please submit a{' '}
-      <a
-        href="https://github.com/WoWAnalyzer/WoWAnalyzer/issues/new"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub issue
-      </a>{' '}
-      . Thanks and we hope you enjoy the tool!
+      <br />
+      Classic Cataclysm support is still a Work in Progress. This spec guide is a stub.
+      <br />
+      See the public GitHub repo or join our community Discord for information about contributing.
+      Thanks!
     </>
   ),
   pages: {
@@ -38,7 +32,7 @@ const CONFIG: Config = {
     },
   },
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport: '/report/dyg2xpXQ9DNh1kzT/10-Normal+Patchwerk+-+Kill+(2:05)/Fon',
+  exampleReport: '/report/QNAPvryX6F9gKGx8/8-Heroic+Magmaw+-+Kill+(2:59)/Deepdrei',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
