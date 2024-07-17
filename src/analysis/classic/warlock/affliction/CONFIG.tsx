7cc5e49561269e4659147afd745d5989d0d7fb66
Jazminite <jazminitehs@gmail.com>
2024-07-17 07:42:34 -0400

    remove pages from config

--- src/analysis/classic/warlock/affliction/CONFIG.tsx
+++ src/analysis/classic/warlock/affliction/CONFIG.tsx
@@ -4,7 +4,6 @@ import SPECS from 'game/SPECS';
 import type Config from 'parser/Config';
 import CHANGELOG from './CHANGELOG';
 import { SupportLevel } from 'parser/Config';
-import AlertInfo from 'interface/AlertInfo';
 
 const CONFIG: Config = {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
@@ -26,11 +25,6 @@ const CONFIG: Config = {
       Thanks!
     </>
   ),
-  pages: {
-    overview: {
-      notes: <AlertInfo>Classic Cataclysm support is still a Work in Progress.</AlertInfo>,
-    },
-  },
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
   exampleReport: '/report/QNAPvryX6F9gKGx8/8-Heroic+Magmaw+-+Kill+(2:59)/Deepdrei',
 
