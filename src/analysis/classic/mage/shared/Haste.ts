f1af2ff885da2b6388a3155a152e81a2468528c1
Jazminite <jazminitehs@gmail.com>
2024-07-21 08:42:51 -0400

    add haste buff

--- src/analysis/classic/mage/shared/Haste.ts
+++ src/analysis/classic/mage/shared/Haste.ts
@@ -1,10 +1,11 @@
 import CoreHaste, { DEFAULT_HASTE_BUFFS } from 'parser/shared/modules/Haste';
-import SPELLS from 'common/SPELLS/classic';
+import SPELLS from 'common/SPELLS/classic/mage';
 
 class Haste extends CoreHaste {
   hasteBuffs = {
     ...DEFAULT_HASTE_BUFFS,
     [SPELLS.ICY_VEINS.id]: 0.2,
+    [SPELLS.PYROMANIAC.id]: 0.05,
   };
 }
 
