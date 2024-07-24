3be14ffd6a5d7d74db255ef2cfa725f8fb94e2e0
Kelton Finch <keltonfinch@gmail.com>
2024-07-23 20:24:55 -0400

    Druid Balance - changelog

--- src/analysis/retail/druid/balance/CHANGELOG.tsx
+++ src/analysis/retail/druid/balance/CHANGELOG.tsx
@@ -1,6 +1,9 @@
 import { change, date } from 'common/changelog';
 import { Sref } from 'CONTRIBUTORS';
+import { SpellLink } from 'interface';
+import { TALENTS_DRUID } from 'common/TALENTS';
 
 export default [
-  change(date(2024, 7, 14), <>Activating Balance Druid analyzer for The War Within! Hero talent analyzers not yet implemented.</>, Sref),
+  change(date(2024, 7, 23), <>Reworked statistics for <SpellLink spell={TALENTS_DRUID.SOUL_OF_THE_FOREST_BALANCE_TALENT} />, <SpellLink spell={TALENTS_DRUID.TWIN_MOONS_TALENT} />, and <SpellLink spell={TALENTS_DRUID.SUNDERED_FIRMAMENT_TALENT} /> to be more clear.</>, Sref),
+  change(date(2024, 7, 23), <>Activating Balance Druid analyzer for The War Within! Hero talent analyzers not yet implemented.</>, Sref),
 ];
