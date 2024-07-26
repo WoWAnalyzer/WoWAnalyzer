c01638b80b0fbf29f9a0fbb6a94721041f952f30
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2024-07-25 22:12:18 -0400

    fix partial support on spec page (#6897)

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -38,6 +38,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 7, 22), 'Fix partial support indication on spec list.', ToppleTheNun),
   change(date(2024, 7, 22), 'Update Paladin spells for Classic Cataclysm', Ethelis),
   change(date(2024, 7, 19), 'Update Racials for Classic Cataclysm', jazminite),
   change(date(2024, 7, 18), 'Clean up padding on specs with core support', emallson),
