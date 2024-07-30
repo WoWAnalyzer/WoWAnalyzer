cf0bd41aba6ff786a9fe5389440e7f7e6ad712b5
Eduardo Rosselot <earosselot@gmail.com>
2024-07-30 11:54:42 +0200

    update changelog

--- src/analysis/retail/mage/frost/CHANGELOG.tsx
+++ src/analysis/retail/mage/frost/CHANGELOG.tsx
@@ -1,11 +1,11 @@
 import { change, date } from 'common/changelog';
-import SPELLS from 'common/SPELLS';
 import { SpellLink } from 'interface';
 import { Earosselot } from 'CONTRIBUTORS';
+import TALENTS from 'common/TALENTS/mage';
 
 // prettier-ignore
 export default [
+  change(date(2024, 7, 30), <>Solved bug when not taking <SpellLink spell={TALENTS.RAY_OF_FROST_TALENT} />.</>, Earosselot),
   change(date(2024, 7, 30), <>Added Icy Veins to guides on About Section.</>, Earosselot),
-  change(date(2024, 7, 30), <>Added support for <SpellLink spell={SPELLS.DEATHS_CHILL_BUFF} /> and <SpellLink spell={SPELLS.PERMAFROST_LANCES_BUFF} />.</>, Earosselot),
   change(date(2024, 7, 30), <>Initial The War Within support</>, Earosselot),
 ];
