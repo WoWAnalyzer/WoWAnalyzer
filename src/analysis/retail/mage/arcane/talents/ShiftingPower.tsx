d5c4e80ed0f228d7cdf5dd7dc7f49bd1ff969500
Kelton Finch <keltonfinch@gmail.com>
2024-07-31 11:45:04 -0400

    Mage Arcane - rewrote Shifting Power guide section to do more checks and to have a per-cast breakdown

--- src/analysis/retail/mage/arcane/talents/ShiftingPower.tsx
+++ src/analysis/retail/mage/arcane/talents/ShiftingPower.tsx
@@ -1,27 +1,26 @@
 import TALENTS from 'common/TALENTS/mage';
 import Analyzer, { SELECTED_PLAYER, Options } from 'parser/core/Analyzer';
 import Events, { CastEvent, GetRelatedEvents } from 'parser/core/Events';
-import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
-import { BoxRowEntry } from 'interface/guide/components/PerformanceBoxRow';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
+import Enemies from 'parser/shared/modules/Enemies';
+import SPELLS from 'common/SPELLS';
 
-class ShiftingPowerArcane extends Analyzer {
+export const MAX_TICKS = 4;
+
+export default class ShiftingPowerArcane extends Analyzer {
   static dependencies = {
     spellUsable: SpellUsable,
+    enemies: Enemies,
   };
 
   protected spellUsable!: SpellUsable;
+  protected enemies!: Enemies;
 
   hasArcaneSurge: boolean = this.selectedCombatant.hasTalent(TALENTS.ARCANE_SURGE_TALENT);
   hasTouchOfTheMagi: boolean = this.selectedCombatant.hasTalent(TALENTS.TOUCH_OF_THE_MAGI_TALENT);
   hasEvocation: boolean = this.selectedCombatant.hasTalent(TALENTS.EVOCATION_TALENT);
 
-  shiftingPower: {
-    cast: number;
-    ticks: CastEvent[];
-    spellsReduced: { arcaneSurge: boolean; touchOfTheMagi: boolean; evocation: boolean };
-    usage?: BoxRowEntry;
-  }[] = [];
+  casts: ShiftingPowerCast[] = [];
 
   constructor(options: Options) {
     super(options);
@@ -30,54 +29,37 @@ class ShiftingPowerArcane extends Analyzer {
       Events.cast.by(SELECTED_PLAYER).spell(TALENTS.SHIFTING_POWER_TALENT),
       this.onShiftingPower,
     );
-    this.addEventListener(Events.fightend, this.onFightEnd);
   }
 
   onShiftingPower(event: CastEvent) {
-    const ticks: CastEvent[] = GetRelatedEvents(event, 'SpellTick');
+    const ordinal = this.casts.length + 1;
+    const timestamp = event.timestamp;
+    const ticks = GetRelatedEvents(event, 'SpellTick').length;
+    const spellsReduced = {
+      arcaneSurge: this.spellUsable.isOnCooldown(TALENTS.ARCANE_SURGE_TALENT.id),
+      touchOfTheMagi: this.spellUsable.isOnCooldown(TALENTS.TOUCH_OF_THE_MAGI_TALENT.id),
+      evocation: this.spellUsable.isOnCooldown(TALENTS.EVOCATION_TALENT.id),
+    };
+    const cdsActive = {
+      arcaneSurge: this.selectedCombatant.hasBuff(SPELLS.ARCANE_SURGE_BUFF.id),
+      touchOfTheMagi: this.enemies.hasBuffOnAny(SPELLS.TOUCH_OF_THE_MAGI_DEBUFF.id),
+      siphonStorm: this.selectedCombatant.hasBuff(SPELLS.SIPHON_STORM_BUFF.id),
+    };
 
-    this.shiftingPower.push({
-      cast: event.timestamp,
-      ticks: ticks || [],
-      spellsReduced: {
-        arcaneSurge:
-          this.hasArcaneSurge && this.spellUsable.isOnCooldown(TALENTS.ARCANE_SURGE_TALENT.id),
-        touchOfTheMagi:
-          this.hasTouchOfTheMagi &&
-          this.spellUsable.isOnCooldown(TALENTS.TOUCH_OF_THE_MAGI_TALENT.id),
-        evocation: this.hasEvocation && this.spellUsable.isOnCooldown(TALENTS.EVOCATION_TALENT.id),
-      },
+    this.casts.push({
+      ordinal,
+      timestamp,
+      ticks,
+      spellsReduced,
+      cdsActive,
     });
   }
-
-  onFightEnd() {
-    this.analyzeShiftingPowers();
-  }
-
-  analyzeShiftingPowers = () => {
-    this.shiftingPower.forEach((s) => {
-      if (this.hasArcaneSurge && !s.spellsReduced.arcaneSurge) {
-        s.usage = {
-          value: QualitativePerformance.Fail,
-          tooltip: `Arcane Surge was not on cooldown.`,
-        };
-      } else if (this.hasTouchOfTheMagi && !s.spellsReduced.touchOfTheMagi) {
-        s.usage = {
-          value: QualitativePerformance.Fail,
-          tooltip: `Touch of the Magi was not on cooldown.`,
-        };
-      } else if (this.hasEvocation && !s.spellsReduced.evocation) {
-        s.usage = { value: QualitativePerformance.Fail, tooltip: `Evocation was not on cooldown.` };
-      } else if (s.ticks.length < 4) {
-        s.usage = {
-          value: QualitativePerformance.Fail,
-          tooltip: `Missed ${4 - s.ticks.length} ticks`,
-        };
-      } else {
-        s.usage = { value: QualitativePerformance.Fail, tooltip: `No Arcane Surge cast found` };
-      }
-    });
-  };
 }
 
-export default ShiftingPowerArcane;
+export interface ShiftingPowerCast {
+  ordinal: number;
+  timestamp: number;
+  ticks: number;
+  spellsReduced: { arcaneSurge: boolean; touchOfTheMagi: boolean; evocation: boolean };
+  cdsActive: { arcaneSurge: boolean; touchOfTheMagi: boolean; siphonStorm: boolean };
+}
