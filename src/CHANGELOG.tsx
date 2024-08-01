7e58726142245708bcf99fc78bfd0ad563ff53b0
Jazminite <jazminitehs@gmail.com>
2024-08-01 09:30:51 -0400

    [Classic] Cata P1 Trinkets

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -38,6 +38,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 8, 1), 'Add Classic Cataclysm Phase 1 trinkets.', jazminite),
   change(date(2024, 7, 27), "Fixed an issue where fully supported specs on spec list weren't displaying their maintainer.", Sref),
   change(date(2024, 7, 22), 'Fix partial support indication on spec list.', ToppleTheNun),
   change(date(2024, 7, 22), 'Update Paladin spells for Classic Cataclysm', Ethelis),
