7e5f93b2831fc5d38477210e555035da0fe7e716
Ypp <lavedrine.cyril@gmail.com>
2024-08-12 19:10:30 +0200

    Added commentary in changelog

--- src/analysis/retail/shaman/restoration/CHANGELOG.tsx
+++ src/analysis/retail/shaman/restoration/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import {
 
 // prettier-ignore
 export default [
+  change(date(2024, 8, 12), <>Deletion of Dragonflight tier sets from the code</>, Ypp),
   change(date(2024, 8, 9), <>Initial support for Restoration Shaman in The War Within</>, Ypp),
   change(date(2024, 7, 27), <>Partial update for 11.0.0</>, Seriousnes),
 ];
