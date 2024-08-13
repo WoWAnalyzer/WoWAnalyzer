c3429180d76ec3e714c487f88fbfccd3ffd619bd
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2024-08-13 17:27:08 -0400

    Update Dot Uptimes to use uptimeBarSubStatistic

--- src/analysis/retail/priest/shadow/modules/features/DotUptimes.tsx
+++ src/analysis/retail/priest/shadow/modules/features/DotUptimes.tsx
@@ -9,7 +9,6 @@ import TALENTS from 'common/TALENTS/priest';
 import DevouringPlague from '../spells/DevouringPlague';
 import ShadowWordPain from '../spells/ShadowWordPain';
 import VampiricTouch from '../spells/VampiricTouch';
-import DarkEvangelism from '../talents/DarkEvangelism';
 
 import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
 import { RoundedPanel } from 'interface/guide/components/GuideDivs';
@@ -19,23 +18,10 @@ class DotUptimes extends Analyzer {
     vampiricTouch: VampiricTouch,
     shadowWordPain: ShadowWordPain,
     devouringPlague: DevouringPlague,
-    darkEvangelism: DarkEvangelism,
   };
   protected vampiricTouch!: VampiricTouch;
   protected shadowWordPain!: ShadowWordPain;
   protected devouringPlague!: DevouringPlague;
-  protected darkEvangelism!: DarkEvangelism;
-
-  statistic() {
-    return (
-      <StatisticBar wide position={STATISTIC_ORDER.CORE(1)}>
-        {this.vampiricTouch.subStatistic()}
-        {this.shadowWordPain.subStatistic()}
-        {this.devouringPlague.subStatistic()}
-        {this.darkEvangelism.active && this.darkEvangelism.subStatistic()}
-      </StatisticBar>
-    );
-  }
 
   get guideSubsection() {
     const explanation = (
@@ -78,21 +64,14 @@ class DotUptimes extends Analyzer {
     return explanationAndDataSubsection(explanation, data);
   }
 
-  get guideSubsectionDE() {
-    const explanation = (
-      <p>
-        This is the uptime of <SpellLink spell={SPELLS.DARK_EVANGELISM_TALENT_BUFF} />. You should
-        not adjust your rotation to maintain this buff.
-      </p>
+  statistic() {
+    return (
+      <StatisticBar wide position={STATISTIC_ORDER.CORE(1)}>
+        {this.vampiricTouch.subStatistic()}
+        {this.shadowWordPain.subStatistic()}
+        {this.devouringPlague.subStatistic()}
+      </StatisticBar>
     );
-
-    const data = (
-      <RoundedPanel>
-        {this.darkEvangelism.active && this.darkEvangelism.subStatistic()}
-      </RoundedPanel>
-    );
-
-    return explanationAndDataSubsection(explanation, data);
   }
 }
 
--- src/analysis/retail/priest/shadow/modules/spells/DevouringPlague.tsx
+++ src/analysis/retail/priest/shadow/modules/spells/DevouringPlague.tsx
@@ -1,11 +1,8 @@
-import { formatPercentage } from 'common/format';
 import TALENTS from 'common/TALENTS/priest';
 import Analyzer from 'parser/core/Analyzer';
 import Enemies from 'parser/shared/modules/Enemies';
-import UptimeBar, { Uptime } from 'parser/ui/UptimeBar';
 import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
-import { SpellIcon } from 'interface';
-import { PerformanceStrong } from 'analysis/retail/priest/shadow/modules/guide/ExtraComponents';
+import uptimeBarSubStatistic from 'parser/ui/UptimeBarSubStatistic';
 
 const BAR_COLOR = '#9933cc';
 
@@ -40,43 +37,12 @@ class DevouringPlague extends Analyzer {
   }
 
   subStatistic() {
-    const fight = this.owner.fight;
-    const totalFightTime = fight.end_time - fight.start_time;
-    const primaryUptime = this.getCombinedUptime(this.uptimeHistory);
-
-    return (
-      <div className="flex-main multi-uptime-bar">
-        <div className="flex main-bar">
-          <div className="flex-sub bar-label">
-            <SpellIcon
-              key={'Icon-' + TALENTS.DEVOURING_PLAGUE_TALENT.name}
-              spell={TALENTS.DEVOURING_PLAGUE_TALENT}
-            />{' '}
-            <PerformanceStrong performance={this.DowntimePerformance}>
-              {' '}
-              {this.formatPercentUptime(primaryUptime, totalFightTime)}{' '}
-            </PerformanceStrong>{' '}
-          </div>
-          <div className="flex-main chart">
-            <UptimeBar
-              timeTooltip
-              uptimeHistory={this.uptimeHistory}
-              start={fight.start_time}
-              end={fight.end_time}
-              barColor={BAR_COLOR}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  getCombinedUptime(uptimes: Uptime[]): number {
-    return uptimes.reduce((acc, up) => acc + up.end - up.start, 0);
-  }
-
-  formatPercentUptime(uptime: number, total: number): string {
-    return formatPercentage(uptime / total, 0) + '%';
+    return uptimeBarSubStatistic(this.owner.fight, {
+      spells: [TALENTS.DEVOURING_PLAGUE_TALENT],
+      uptimes: this.uptimeHistory,
+      color: BAR_COLOR,
+      perf: this.DowntimePerformance,
+    });
   }
 }
 
