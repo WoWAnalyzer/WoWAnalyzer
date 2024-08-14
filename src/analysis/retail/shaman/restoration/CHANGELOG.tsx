c9f96996b556f3c5f3eb2cb78e0648f072586d52
Texleretour <nathan.hostens@gmail.com>
2024-08-14 20:32:11 +0200

    upgrade changelog

--- src/analysis/retail/shaman/restoration/CHANGELOG.tsx
+++ src/analysis/retail/shaman/restoration/CHANGELOG.tsx
@@ -1,3 +1,6 @@
+import { SpellLink } from 'interface'
+import { TALENTS_SHAMAN } from 'common/TALENTS';
+import SPELLS from 'common/SPELLS'
 import { change, date } from 'common/changelog';
 import {
   Seriousnes,
@@ -5,10 +8,11 @@ import {
   Texleretour,
 } from 'CONTRIBUTORS';
 
+
 // prettier-ignore
 export default [
-  change(date(2024, 8, 14), <>New statistic: Mana saved from SWTT, fixed healing surge mana cost for TWW, fixed NS edge case with SWTT</>, Texleretour),
-  change(date(2024, 8, 13), <>New statistic: Mana saved from Nature's Swiftness</>, Texleretour),
+  change(date(2024, 8, 14), <>Added mana saved from <SpellLink spell={TALENTS_SHAMAN.SPIRITWALKERS_TIDAL_TOTEM_TALENT} /> statistic, scaled <SpellLink spell={SPELLS.HEALING_SURGE} /> mana cost for The War Within and fixed <SpellLink spell={TALENTS_SHAMAN.NATURES_SWIFTNESS_TALENT} /> statistic</>, Texleretour),
+  change(date(2024, 8, 13), <>Added mana saved from <SpellLink spell={TALENTS_SHAMAN.NATURES_SWIFTNESS_TALENT} /> statistic</>, Texleretour),
   change(date(2024, 8, 9), <>Initial support for Restoration Shaman in The War Within</>, Ypp),
   change(date(2024, 7, 27), <>Partial update for 11.0.0</>, Seriousnes),
 ];
