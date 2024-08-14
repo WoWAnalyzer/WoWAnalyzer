c42aa6382d82bbeb90e2169a481620123ec5cf27
Texleretour <nathan.hostens@gmail.com>
2024-08-14 15:24:51 +0200

    changelog entry

--- src/analysis/retail/shaman/restoration/CHANGELOG.tsx
+++ src/analysis/retail/shaman/restoration/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import {
 
 // prettier-ignore
 export default [
+  change(date(2024, 8, 14), <>New statistic: Mana saved from SWTT, fixed healing surge mana cost for TWW, fixed NS edge case with SWTT</>, Texleretour),
   change(date(2024, 8, 13), <>New statistic: Mana saved from Nature's Swiftness</>, Texleretour),
   change(date(2024, 8, 9), <>Initial support for Restoration Shaman in The War Within</>, Ypp),
   change(date(2024, 7, 27), <>Partial update for 11.0.0</>, Seriousnes),
