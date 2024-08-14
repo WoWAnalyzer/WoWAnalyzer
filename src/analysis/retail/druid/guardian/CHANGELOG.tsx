9130dd625b22b9498302f98f7006a28e0d666765
Kelton Finch <keltonfinch@gmail.com>
2024-08-14 08:40:35 -0400

    Fix changelog date

--- src/analysis/retail/druid/guardian/CHANGELOG.tsx
+++ src/analysis/retail/druid/guardian/CHANGELOG.tsx
@@ -4,7 +4,7 @@ import { SpellLink } from 'interface';
 import SPELLS from 'common/SPELLS';
 
 export default [
-  change(date(2024, 8, 22), <>Updated spells to account for 11.0.2 balance patch.</>, Sref),
+  change(date(2024, 8, 14), <>Updated spells to account for 11.0.2 balance patch.</>, Sref),
   change(date(2024, 8, 3), <>Added Offensive Cooldowns section to Guide.</>, Sref),
   change(date(2024, 7, 29), <>Added <SpellLink spell={SPELLS.SWIPE_BEAR} /> subsection to Guide.</>, Sref),
   change(date(2024, 7, 14), <>Activating Guardian Druid analyzer for The War Within! Hero talent analyzers not yet implemented.</>, Sref),
