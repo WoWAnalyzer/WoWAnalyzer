4d8c57aef9ec0a35d05817b30ac1d892ecac11b4
Vollmer <kristianvollmer@gmail.com>
2024-08-15 14:04:15 +0200

    add to Deva & Aug

--- src/analysis/retail/evoker/augmentation/CombatLogParser.ts
+++ src/analysis/retail/evoker/augmentation/CombatLogParser.ts
@@ -60,6 +60,7 @@ import {
   MeltArmor,
   MassDisintegrate,
   MightOfTheBlackDragonflight,
+  ExtendedBattle,
 } from 'analysis/retail/evoker/shared';
 
 class CombatLogParser extends MainCombatLogParser {
@@ -124,6 +125,7 @@ class CombatLogParser extends MainCombatLogParser {
     // hero talents
     mightOfTheBlackDragonflight: MightOfTheBlackDragonflight,
     meltArmor: MeltArmor,
+    extendedBattle: ExtendedBattle,
 
     // Features
     buffTrackerGraph: BuffTrackerGraph,
