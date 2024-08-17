743dec0825e8c4cb01c69126705ca0066652ed3a
Jazminite <jazminitehs@gmail.com>
2024-08-17 16:48:21 -0400

    update CHANGELOGs

--- src/analysis/classic/_templates/healer/CHANGELOG.tsx
+++ src/analysis/classic/_templates/healer/CHANGELOG.tsx
@@ -3,7 +3,7 @@ import { jazminite } from 'CONTRIBUTORS';
 
 export default [
   // Remove this entry and add your own
-  change(date(2024, 8, 17), 'Update Config.tsx to use foundation guide default for spec status message.', jazminite),
-  change(date(2024, 8, 9), 'Remove old comment from Config.tsx', jazminite),
+  change(date(2024, 8, 17), 'Update CONFIG.tsx to use foundation guide default for spec status message.', jazminite),
+  change(date(2024, 8, 9), 'Remove old comment from CONFIG.tsx', jazminite),
   change(date(2022, 12, 29), 'Add Classic healing spec template.', jazminite),
 ];
