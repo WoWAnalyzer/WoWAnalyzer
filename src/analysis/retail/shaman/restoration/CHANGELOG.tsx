9d1d519e8b99f4be4c38a411bcbdc2fc7415d6fe
Ypp <boss@brocoli-cosmique.net>
2024-08-17 23:24:22 +0200

    [Restoration Shaman] Removed Dragonflight Tier set files and references (#6946)
    * [TWW] Initial support for Resto Shaman
    
    * Update CHANGELOG.tsx
    
    * Updated for change request
    
    * Remove file references from tsconfig according to wiki
    
    * Remove T30 and T31 files and references
    
    * Added commentary in changelog
    
    ---------
    
    Co-authored-by: Ypp <lavedrine.cyril@gmail.com>

--- src/analysis/retail/shaman/restoration/CHANGELOG.tsx
+++ src/analysis/retail/shaman/restoration/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import {
 
 // prettier-ignore
 export default [
+  change(date(2024, 8, 14), <>Deletion of Dragonflight tier sets from the code</>, Ypp),
   change(date(2024, 8, 13), <>New statistic: Mana saved from Nature's Swiftness</>, Texleretour),
   change(date(2024, 8, 9), <>Initial support for Restoration Shaman in The War Within</>, Ypp),
   change(date(2024, 7, 27), <>Partial update for 11.0.0</>, Seriousnes),
