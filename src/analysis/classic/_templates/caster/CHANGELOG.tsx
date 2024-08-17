1656eea04c042fef29d41e9e28959fce03c2bc14
Jazminite <jazminitehs@gmail.com>
2024-08-17 16:44:03 -0400

    [Classic] Templates Config

--- src/analysis/classic/_templates/caster/CHANGELOG.tsx
+++ src/analysis/classic/_templates/caster/CHANGELOG.tsx
@@ -3,6 +3,7 @@ import { jazminite } from 'CONTRIBUTORS';
 
 export default [
   // When copying this file, REMOVE THESE ENTRIES AND ADD YOUR OWN
+  change(date(2024, 8, 17), 'Update Config.tsx to use foundation guide default for spec status message.', jazminite),
   change(date(2024, 8, 9), 'Remove old comment from Config.tsx', jazminite),
   change(date(2024, 7, 6), 'Add back Always Be Casting module without suggestions.', jazminite),
   change(date(2024, 6, 17), 'Add Cata Classic Caster spec template', jazminite),
