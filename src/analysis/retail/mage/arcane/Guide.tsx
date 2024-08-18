5784a977132bba5eba364f6a9fce182d1e2dbe2a
Sharrq <cjgrasty86@gmail.com>
2024-08-18 18:10:36 -0400

    name drop Porom

--- src/analysis/retail/mage/arcane/Guide.tsx
+++ src/analysis/retail/mage/arcane/Guide.tsx
@@ -82,13 +82,13 @@ export default function Guide({ modules, events, info }: GuideProps<typeof Comba
     <>
       <Section title="Preface & Disclaimers">
         <>
-          The analysis in this guide is provided in collaboration with the staff of the{' '}
-          <a href="https://discord.gg/makGfZA">Altered Time</a> Mage Discord. When reviewing this
-          information, keep in mind that WoWAnalyzer is limited to the information that is present
-          in your combat log. As a result, we have no way of knowing if you were intentionally doing
-          something suboptimal because the fight or strat required it (such as Forced Downtime or
-          holding cooldowns for a burn phase). Because of this, we recommend comparing your analysis
-          against a top 100 log for the same boss.
+          The analysis in this guide is provided in collaboration with Porom and the rest of the
+          staff of the <a href="https://discord.gg/makGfZA">Altered Time</a> Mage Discord. When
+          reviewing this information, keep in mind that WoWAnalyzer is limited to the information
+          that is present in your combat log. As a result, we have no way of knowing if you were
+          intentionally doing something suboptimal because the fight or strat required it (such as
+          Forced Downtime or holding cooldowns for a burn phase). Because of this, we recommend
+          comparing your analysis against a top 100 log for the same boss.
           <br />
           <br />
           For additional assistance in improving your gameplay, or to have someone look more in
