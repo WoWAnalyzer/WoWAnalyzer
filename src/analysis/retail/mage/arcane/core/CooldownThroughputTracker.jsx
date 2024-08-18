b26aaa2b38640930e53b9cfd3256fe1efc53d473
Sharrq <cjgrasty86@gmail.com>
2024-08-18 18:19:32 -0400

    add touch to cooldown throughout tracking

--- src/analysis/retail/mage/arcane/core/CooldownThroughputTracker.jsx
+++ src/analysis/retail/mage/arcane/core/CooldownThroughputTracker.jsx
@@ -3,12 +3,19 @@ import CoreCooldownThroughputTracker, {
   BUILT_IN_SUMMARY_TYPES,
 } from 'parser/shared/modules/CooldownThroughputTracker';
 import GameBranch from 'game/GameBranch';
+import SPELLS from 'common/SPELLS';
 
 class CooldownThroughputTracker extends CoreCooldownThroughputTracker {
   static cooldownSpells = [
     ...CoreCooldownThroughputTracker.cooldownSpells,
     {
       spell: TALENTS.ARCANE_SURGE_TALENT.id,
+      startBufferMS: 4000,
+      summary: [BUILT_IN_SUMMARY_TYPES.DAMAGE],
+      branch: GameBranch.Retail,
+    },
+    {
+      spell: SPELLS.TOUCH_OF_THE_MAGI_DEBUFF.id,
       startBufferMS: 4000,
       summary: [BUILT_IN_SUMMARY_TYPES.DAMAGE],
       branch: GameBranch.Retail,
