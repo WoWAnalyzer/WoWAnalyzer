27971ba54d8aa64d7b4e361d940ee60486b6c783
Kelton Finch <keltonfinch@gmail.com>
2024-08-20 11:17:33 -0400

    Mage Arcane - changelog entry and updated contributors

--- src/analysis/retail/mage/arcane/CONFIG.tsx
+++ src/analysis/retail/mage/arcane/CONFIG.tsx
@@ -1,4 +1,4 @@
-import { Sharrq, SyncSubaru } from 'CONTRIBUTORS';
+import { Sharrq, Sref } from 'CONTRIBUTORS';
 import GameBranch from 'game/GameBranch';
 import SPECS from 'game/SPECS';
 import Config, { SupportLevel } from 'parser/Config';
@@ -8,7 +8,7 @@ import AlertWarning from 'interface/AlertWarning';
 
 const config: Config = {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
-  contributors: [Sharrq, SyncSubaru],
+  contributors: [Sharrq, Sref],
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
   patchCompatibility: '11.0.2',
