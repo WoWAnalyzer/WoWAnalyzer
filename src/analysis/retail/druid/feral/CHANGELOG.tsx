df018fe6bfb640a916bc188390409076c06ca65f
Kelton Finch <keltonfinch@gmail.com>
2024-08-22 13:18:05 -0400

    Druid Feral - Convoke tracker should detect Ravages

--- src/analysis/retail/druid/feral/CHANGELOG.tsx
+++ src/analysis/retail/druid/feral/CHANGELOG.tsx
@@ -4,6 +4,7 @@ import { SpellLink } from 'interface';
 import { TALENTS_DRUID } from 'common/TALENTS/druid';
 
 export default [
+  change(date(2024, 8, 22), <><SpellLink spell={TALENTS_DRUID.CONVOKE_THE_SPIRITS_TALENT}/> tracker should now correctly detect procced <SpellLink spell={TALENTS_DRUID.RAVAGE_TALENT}/> casts.</>, Sref),
   change(date(2024, 8, 17), <>Marked updated for 11.0.2 and updated the spec's 'About' page.</>, Sref),
   change(date(2024, 8, 14), <>Updated spells to account for 11.0.2 balance patch.</>, Sref),
   change(date(2024, 7, 22), <>Refactored various modules to correctly handle <SpellLink spell={TALENTS_DRUID.RAVAGE_TALENT}/>. More updates for The War Within talent changes.</>, Sref),
