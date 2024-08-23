71e80d6911da9932a599538d64a633a0d5c42cb6
Kelton Finch <keltonfinch@gmail.com>
2024-08-23 10:19:16 -0400

    Druid Feral - Convoke tracker should detect Ravages (#6978)

--- src/analysis/retail/druid/feral/CHANGELOG.tsx
+++ src/analysis/retail/druid/feral/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { TALENTS_DRUID } from 'common/TALENTS/druid';
 import SPELLS from 'common/SPELLS';
 
 export default [
+  change(date(2024, 8, 22), <><SpellLink spell={TALENTS_DRUID.CONVOKE_THE_SPIRITS_TALENT}/> tracker should now correctly detect procced <SpellLink spell={TALENTS_DRUID.RAVAGE_TALENT}/> casts.</>, Sref),
   change(date(2024, 8, 22), <>Cleaner display of <SpellLink spell={SPELLS.RAKE}/>, <SpellLink spell={SPELLS.RIP}/>, <SpellLink spell={SPELLS.FEROCIOUS_BITE}/>, and <SpellLink spell={TALENTS_DRUID.SUDDEN_AMBUSH_TALENT}/> sections in Guide.</>, Sref),
   change(date(2024, 8, 17), <>Marked updated for 11.0.2 and updated the spec's 'About' page.</>, Sref),
   change(date(2024, 8, 14), <>Updated spells to account for 11.0.2 balance patch.</>, Sref),
