16d22feec19577db4e7c016b4a783c0bc8a6ccc1
Eduardo Rosselot <32552126+earosselot@users.noreply.github.com>
2024-08-28 20:46:37 +0200

    Spellfrost teachings Frozen Orb CD reset support (#6984)
    Co-authored-by: J David Smith <emallson@emallson.net>

--- src/analysis/retail/mage/frost/CHANGELOG.tsx
+++ src/analysis/retail/mage/frost/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import TALENTS from 'common/TALENTS/mage';
 
 // prettier-ignore
 export default [
+  change(date(2024, 8, 25), <>Added support for <SpellLink spell={TALENTS.SPELLFROST_TEACHINGS_TALENT} />.</>, Earosselot),
   change(date(2024, 8, 23), <>Adding APL for Spellslinger Frost.</>, Earosselot),
   change(date(2024, 7, 30), <>Solved bug when not taking <SpellLink spell={TALENTS.RAY_OF_FROST_TALENT} />.</>, Earosselot),
   change(date(2024, 7, 30), <>Added Icy Veins to guides on About Section.</>, Earosselot),
