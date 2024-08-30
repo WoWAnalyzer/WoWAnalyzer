7573c22466efc41b092abf44c77be4683e609665
Jazminite <65823478+jazminite@users.noreply.github.com>
2024-08-30 09:20:52 -0400

    [Classic] Cata Healer Template (#6993)

--- src/analysis/classic/_templates/healer/modules/features/AlwaysBeCasting.tsx
+++ src/analysis/classic/_templates/healer/modules/features/AlwaysBeCasting.tsx
@@ -1,6 +1,4 @@
-import { defineMessage } from '@lingui/macro';
-import { formatPercentage } from 'common/format';
-import { When } from 'parser/core/ParseResults';
+import { ThresholdStyle } from 'parser/core/ParseResults';
 import CoreAlwaysBeCastingHealing from 'parser/shared/modules/AlwaysBeCastingHealing';
 import SPELLS from 'common/SPELLS/classic';
 
@@ -10,24 +8,16 @@ class AlwaysBeCasting extends CoreAlwaysBeCastingHealing {
     SPELLS.HEALING_TOUCH.id,
   ];
 
-  suggestions(when: When) {
-    const deadTimePercentage = this.totalTimeWasted / this.owner.fightDuration;
-
-    when(deadTimePercentage)
-      .isGreaterThan(0.25)
-      .addSuggestion((suggest, actual, recommended) =>
-        suggest('Your downtime can be improved. Try to Always Be Casting (ABC).')
-          .icon('spell_mage_altertime')
-          .actual(
-            defineMessage({
-              id: 'shared.suggestions.alwaysBeCasting.downtime',
-              message: `${formatPercentage(actual)}% downtime`,
-            }),
-          )
-          .recommended(`<${formatPercentage(recommended)}% is recommended`)
-          .regular(recommended + 0.05)
-          .major(recommended + 0.05),
-      );
+  get downtimeSuggestionThresholds() {
+    return {
+      actual: this.downtimePercentage,
+      isGreaterThan: {
+        minor: 0.2,
+        average: 0.35,
+        major: 1,
+      },
+      style: ThresholdStyle.PERCENTAGE,
+    };
   }
 }
 
