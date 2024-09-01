81ebab7956fae38a3c48ed6701c55cb1ac4fedde
Sean Smith <ranky26@gmail.com>
2024-09-01 18:41:00 +1000

    new stormbringer APLs

--- src/analysis/retail/shaman/enhancement/modules/apl/AplCheck.tsx
+++ src/analysis/retail/shaman/enhancement/modules/apl/AplCheck.tsx
@@ -1,83 +1,14 @@
-import TALENTS from 'common/TALENTS/shaman';
 import { suggestion } from 'parser/core/Analyzer';
 import { AnyEvent } from 'parser/core/Events';
-import aplCheck, { Apl, build, CheckResult, PlayerInfo, Rule } from 'parser/shared/metrics/apl';
-import {
-  and,
-  buffPresent,
-  debuffMissing,
-  describe,
-  or,
-} from 'parser/shared/metrics/apl/conditions';
+import aplCheck, { Apl, build, CheckResult, PlayerInfo } from 'parser/shared/metrics/apl';
 import annotateTimeline from 'parser/shared/metrics/apl/annotate';
-import { SpellLink } from 'interface';
-import SPELLS from 'common/SPELLS';
-import { AtLeastFiveMSW, MaxStacksMSW, minimumMaelstromWeaponStacks } from './Conditions';
+import { getBuild } from './AplSelector';
 /**
  * Based on https://www.icy-veins.com/wow/enhancement-shaman-pve-dps-guide
  */
 
 export const apl = (info: PlayerInfo): Apl => {
-  const rules: Rule[] = [];
-
-  rules.push(
-    {
-      spell: SPELLS.TEMPEST_CAST,
-      condition: describe(and(buffPresent(SPELLS.TEMPEST_BUFF), MaxStacksMSW), () => (
-        <>
-          available and at 10 <SpellLink spell={SPELLS.MAELSTROM_WEAPON_BUFF} /> stacks
-        </>
-      )),
-    },
-    {
-      spell: SPELLS.LIGHTNING_BOLT,
-      condition: MaxStacksMSW,
-    },
-    {
-      spell: TALENTS.ELEMENTAL_BLAST_ELEMENTAL_TALENT,
-      condition: and(
-        minimumMaelstromWeaponStacks(8),
-        describe(
-          or(
-            buffPresent(SPELLS.ELEMENTAL_SPIRITS_BUFF_MOLTEN_WEAPON),
-            buffPresent(SPELLS.ELEMENTAL_SPIRITS_BUFF_ICY_EDGE),
-            buffPresent(SPELLS.ELEMENTAL_SPIRITS_BUFF_CRACKLING_SURGE),
-          ),
-          () => (
-            <>
-              any <SpellLink spell={TALENTS.ELEMENTAL_SPIRITS_TALENT} /> active
-            </>
-          ),
-        ),
-      ),
-    },
-    {
-      spell: SPELLS.FLAME_SHOCK,
-      condition: debuffMissing(SPELLS.FLAME_SHOCK),
-    },
-    {
-      spell: TALENTS.LAVA_LASH_TALENT,
-      condition: buffPresent(SPELLS.HOT_HAND_BUFF),
-    },
-    {
-      spell: SPELLS.LIGHTNING_BOLT,
-      condition: AtLeastFiveMSW,
-    },
-    TALENTS.ICE_STRIKE_TALENT,
-    {
-      spell: TALENTS.FROST_SHOCK_TALENT,
-      condition: buffPresent(SPELLS.HAILSTORM_BUFF),
-    },
-    TALENTS.LAVA_LASH_TALENT,
-    TALENTS.STORMSTRIKE_TALENT,
-    TALENTS.SUNDERING_TALENT,
-    TALENTS.CRASH_LIGHTNING_TALENT,
-    SPELLS.FLAME_SHOCK,
-  );
-
-  const apl = build(rules);
-
-  return apl;
+  return getBuild(info.combatant) ?? build([]);
 };
 
 export const check = (events: AnyEvent[], info: PlayerInfo): CheckResult => {
---
+++ src/analysis/retail/shaman/enhancement/modules/apl/StormbringerElementalist.tsx
@@ -0,0 +1,68 @@
+import Combatant from 'parser/core/Combatant';
+import { Apl, build, Rule } from 'parser/shared/metrics/apl';
+import SPELLS from 'common/SPELLS/shaman';
+import TALENTS from 'common/TALENTS/shaman';
+import SpellLink from 'interface/SpellLink';
+import {
+  and,
+  buffPresent,
+  repeatableBuffPresent,
+  debuffMissing,
+  describe,
+} from 'parser/shared/metrics/apl/conditions';
+import { MaxStacksMSW, minimumMaelstromWeaponStacks, AtLeastFiveMSW } from './Conditions';
+
+export function stormbringerElementalist(combatant: Combatant): Apl {
+  const rules: (Rule | false)[] = [
+    {
+      spell: SPELLS.TEMPEST_CAST,
+      condition: describe(and(buffPresent(SPELLS.TEMPEST_BUFF), MaxStacksMSW), () => (
+        <>
+          available and at 10 <SpellLink spell={SPELLS.MAELSTROM_WEAPON_BUFF} /> stacks
+        </>
+      )),
+    },
+    {
+      spell: TALENTS.ELEMENTAL_BLAST_ELEMENTAL_TALENT,
+      condition: and(
+        minimumMaelstromWeaponStacks(8),
+        repeatableBuffPresent(
+          [
+            SPELLS.ELEMENTAL_SPIRITS_BUFF_MOLTEN_WEAPON,
+            SPELLS.ELEMENTAL_SPIRITS_BUFF_ICY_EDGE,
+            SPELLS.ELEMENTAL_SPIRITS_BUFF_CRACKLING_SURGE,
+          ],
+          { atLeast: 4 },
+        ),
+      ),
+    },
+    {
+      spell: SPELLS.LIGHTNING_BOLT,
+      condition: MaxStacksMSW,
+    },
+    {
+      spell: SPELLS.FLAME_SHOCK,
+      condition: debuffMissing(SPELLS.FLAME_SHOCK),
+    },
+    {
+      spell: TALENTS.LAVA_LASH_TALENT,
+      condition: buffPresent(SPELLS.HOT_HAND_BUFF),
+    },
+    {
+      spell: SPELLS.LIGHTNING_BOLT,
+      condition: AtLeastFiveMSW,
+    },
+    TALENTS.ICE_STRIKE_TALENT,
+    {
+      spell: TALENTS.FROST_SHOCK_TALENT,
+      condition: buffPresent(SPELLS.HAILSTORM_BUFF),
+    },
+    TALENTS.LAVA_LASH_TALENT,
+    TALENTS.STORMSTRIKE_TALENT,
+    TALENTS.SUNDERING_TALENT,
+    TALENTS.CRASH_LIGHTNING_TALENT,
+    SPELLS.FLAME_SHOCK,
+  ];
+
+  return build(rules.filter((rule) => rule !== false) as Rule[]);
+}
