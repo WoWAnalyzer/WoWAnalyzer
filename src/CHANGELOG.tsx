de855c031a7823818d690172c395f617538027ff
Vollmer <kristianvollmer@gmail.com>
2024-09-06 14:33:54 +0200

    core changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -35,6 +35,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 9, 6), "Add missing supportID property to DamageEvent interface", Vollmer),
   change(date(2024, 9, 4), 'Updates to Active Time calculation to avoid cases where overcount could occur.', Sref),
   change(date(2024, 9, 3), "Fixed a bug where the Death Recap wouldn't detect some defensive buffs", Sref),
   change(date(2024, 9, 3), 'Remove AbilitiesMissing module.', ToppleTheNun),
