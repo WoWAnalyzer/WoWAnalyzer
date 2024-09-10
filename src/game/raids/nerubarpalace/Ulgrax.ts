46f9c6036b49866c2a1a34c6c60840bfb52b1ffc
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2024-09-09 20:11:25 -0400

    [core] add S1 raid/dungeons, clean out old raids (#7008)

---
+++ src/game/raids/nerubarpalace/Ulgrax.ts
@@ -0,0 +1,6 @@
+import { buildBoss } from 'game/raids/builders';
+
+export const Ulgrax = buildBoss({
+  id: 2902,
+  name: 'Ulgrax the Devourer',
+});
--- src/game/raids/nerubarpalace/index.ts
+++ src/game/raids/nerubarpalace/index.ts
@@ -1,7 +1,26 @@
 import type { Raid } from 'game/raids';
 
+import { Ansurek } from './Ansurek';
+import { BloodboundHorror } from './BloodboundHorror';
+import { Kyveza } from './Kyveza';
+import { Ovinax } from './Ovinax';
+import { Rashanan } from './Rashanan';
+import { Sikran } from './Sikran';
+import { SilkenCourt } from './SilkenCourt';
+import { Ulgrax } from './Ulgrax';
+import background from './backgrounds/NerubarPalace.jpg';
+
 export default {
   name: "Nerub'ar Palace",
-  background: undefined,
-  bosses: {},
+  background,
+  bosses: {
+    Ansurek,
+    BloodboundHorror,
+    Kyveza,
+    Ovinax,
+    Rashanan,
+    Sikran,
+    SilkenCourt,
+    Ulgrax,
+  },
 } satisfies Raid;
