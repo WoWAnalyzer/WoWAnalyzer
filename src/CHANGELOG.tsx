599d750ac06c405f1a1180babcc268029060d4a6
Richard Harrah <topplethenunnery@gmail.com>
2024-09-09 22:06:59 -0400

    [core] fix character search showing UNKNOWN

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -35,6 +35,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 9, 9), 'Fix character search showing UNKNOWN instead of THE WAR WITHIN.', ToppleTheNun),
   change(date(2024, 9, 8), "Add Nerub'ar Palace bosses and season 1 M+ dungeons.", ToppleTheNun),
   change(date(2024, 9, 5), 'Update Rogue spells for Classic Cataclysm', jazminite),
   change(date(2024, 9, 4), 'Updates to Active Time calculation to avoid cases where overcount could occur.', Sref),
