158b3a7db0662e2568c0f8a0b9332fc8b06ffab9
trevorm4 <trevorm4@illinois.edu>
2024-09-10 20:15:42 -0700

    Fix revival loading for jade bond

--- src/analysis/retail/monk/mistweaver/Guide.tsx
+++ src/analysis/retail/monk/mistweaver/Guide.tsx
@@ -42,8 +42,7 @@ export default function Guide({ modules, events, info }: GuideProps<typeof Comba
         {info.combatant.hasTalent(TALENTS_MONK.INVOKE_CHI_JI_THE_RED_CRANE_TALENT)
           ? modules.invokeChiJi.guideCastBreakdown
           : modules.invokeYulon.guideCastBreakdown}
-        {(info.combatant.hasTalent(TALENTS_MONK.JADE_BOND_TALENT) ||
-          info.combatant.hasTalent(TALENTS_MONK.SHAOHAOS_LESSONS_TALENT)) &&
+        {info.combatant.hasTalent(TALENTS_MONK.SHAOHAOS_LESSONS_TALENT) &&
           modules.revival.guideCastBreakdown}
         {/* {info.combatant.hasTalent(TALENTS_MONK.MANA_TEA_TALENT) &&
           modules.manaTea.guideCastBreakdown} */}
