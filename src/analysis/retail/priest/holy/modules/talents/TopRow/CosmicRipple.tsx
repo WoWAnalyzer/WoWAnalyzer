6baeb67b9505fc1a22c89ae3be7c0fef5682d62e
Liavre <rpluegge@outlook.com>
2024-09-11 23:45:25 -0700

    Eol attrib implementation (#7011)
    * working EoL display
    
    * implemented EOLAttrib spec wide to account for mastery which previously not accounted for in most modules
    
    * actually display this module
    
    * resolving github comments
    
    * replaced most <br/> with <div>
    
    * added essenc devourer heal, not spell to EOL blacklist
    
    * removed WCP guide as per WCP moderator request
    
    * constant fix
    
    * constant fix 2
    
    * talent was just always wrong
    
    * fixed legacy lightwell module to include absorbed as effective healing
    
    * verbose changelog
    
    * minor bug fix ( = shouldve been += )
    
    * more formatting changes

--- src/analysis/retail/priest/holy/modules/talents/TopRow/CosmicRipple.tsx
+++ src/analysis/retail/priest/holy/modules/talents/TopRow/CosmicRipple.tsx
@@ -1,5 +1,5 @@
 import SPELLS from 'common/SPELLS';
-import TALENTS from 'common/TALENTS/priest';
+import TALENTS, { TALENTS_PRIEST } from 'common/TALENTS/priest';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { HealEvent } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
@@ -7,9 +7,18 @@ import ItemHealingDone from 'parser/ui/ItemHealingDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
+import EOLAttrib from '../../core/EchoOfLightAttributor';
+import SpellLink from 'interface/SpellLink';
+import ItemPercentHealingDone from 'parser/ui/ItemPercentHealingDone';
 
 // Example Log: /report/C2NGDav6KHgc8ZWd/28-Mythic+Taloc+-+Kill+(7:07)/13-Ariemah
 class CosmicRipple extends Analyzer {
+  static dependencies = {
+    eolAttrib: EOLAttrib,
+  };
+  protected eolAttrib!: EOLAttrib;
+  eolContrib = 0;
+
   totalHealing = 0;
   overhealing = 0;
   absorbed = 0;
@@ -30,6 +39,7 @@ class CosmicRipple extends Analyzer {
     this.overhealing += event.overheal || 0;
     this.totalHealing += (event.amount || 0) + (event.absorbed || 0);
     this.totalHits += 1;
+    this.eolContrib += this.eolAttrib.getEchoOfLightHealingAttrib(event);
 
     if (event.timestamp - this.lastRippleTimeStamp > 1000) {
       this.totalRipples += 1;
@@ -43,9 +53,22 @@ class CosmicRipple extends Analyzer {
         size="flexible"
         category={STATISTIC_CATEGORY.TALENTS}
         position={STATISTIC_ORDER.OPTIONAL(3)}
+        tooltip={
+          <>
+            Breakdown:{' '}
+            <div>
+              <SpellLink spell={TALENTS_PRIEST.COSMIC_RIPPLE_TALENT} />:{' '}
+              <ItemPercentHealingDone amount={this.totalHealing}></ItemPercentHealingDone>{' '}
+            </div>
+            <div>
+              <SpellLink spell={SPELLS.ECHO_OF_LIGHT_MASTERY} />:{' '}
+              <ItemPercentHealingDone amount={this.eolContrib}></ItemPercentHealingDone>{' '}
+            </div>
+          </>
+        }
       >
         <BoringSpellValueText spell={SPELLS.COSMIC_RIPPLE_HEAL}>
-          <ItemHealingDone amount={this.totalHealing} />
+          <ItemHealingDone amount={this.totalHealing + this.eolContrib} />
         </BoringSpellValueText>
       </Statistic>
     );
