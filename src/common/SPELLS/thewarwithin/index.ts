c4b341f1d81edde289fc807aef1d79c4ef953988
Sean Smith <ranky26@gmail.com>
2024-09-15 09:44:07 +1000

    adding many TWW weapon enchants and effects, removing old DF item analysis

---
+++ src/common/SPELLS/thewarwithin/index.ts
@@ -0,0 +1,6 @@
+import safeMerge from 'common/safeMerge';
+import Enchants from './enchants';
+
+const spells = safeMerge(Enchants);
+
+export default spells;
---
+++ src/parser/retail/modules/items/thewarwithin/index.tsx
@@ -0,0 +1,4 @@
+export { default as AuthorityOfRadiantPower } from './enchants/AuthorityOfRadiantPower';
+export { default as AuthorityOfStorms } from './enchants/AuthorityOfStorms';
+export { default as AuthorityOfTheDepths } from './enchants/AuthorityOfTheDepths';
+export { default as StormridersFury } from './enchants/StormridersFury';
