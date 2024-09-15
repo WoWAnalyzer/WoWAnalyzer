fa09f4767605b7b10d67f8f1817a0d2022f1d903
Oscar Busk <oscar.busk@gmail.com>
2024-09-15 23:36:14 +0200

    Update HealthPotion module for TWW

---
+++ src/common/SPELLS/thewarwithin/potions.ts
@@ -0,0 +1,11 @@
+import Spell from '../Spell';
+
+const Potions = {
+  ALGARI_HEALING_POTION: {
+    name: 'Algari Healing Potion',
+    id: 431416,
+    icon: 'inv_potion_49',
+  },
+} satisfies Record<string, Spell>;
+
+export default Potions;
