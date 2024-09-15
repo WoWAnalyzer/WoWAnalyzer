f74aef81c8375c6b09f520f40b87fd09921036b2
Oscar Busk <oscar.busk@gmail.com>
2024-09-15 23:32:10 +0200

    [raids] Add Ulgrax spells to CASTS_THAT_ARENT_CASTS
    This is so that the ability module does not try to track it, and does not complain unecessarily.

--- src/common/SPELLS/thewarwithin/index.ts
+++ src/common/SPELLS/thewarwithin/index.ts
@@ -2,7 +2,8 @@ import safeMerge from 'common/safeMerge';
 import Enchants from './enchants';
 import Embellishments from './embellishments';
 import Others from './others';
+import Raids from './raids';
 
-const spells = safeMerge(Enchants, Embellishments, Others);
+const spells = safeMerge(Enchants, Embellishments, Others, Raids);
 
 export default spells;
