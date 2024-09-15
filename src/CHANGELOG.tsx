d2bd5cc850be88084c297ce1c813331fd5c9837a
Putro <29204244+Pewtro@users.noreply.github.com>
2024-09-16 00:25:17 +0200

    [Core] Update spellAvailable to properly adjust behaviour to inverse
    Additionally move it to an options object

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -35,6 +35,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 9, 16), 'Updated spellAvailable APL function to properly adjust validation behaviour based on inverse options, and turn it into an options object rather than a straight boolean', Putro),
   change(date(2024, 9, 15), 'Adding TWW weapon enchants, removing DF-specific items (e.g. Fyralath, Call To Dominance, Voice of the Silent Star, etc)', Seriousnes),
   change(date(2024, 9, 12), 'Fixed crash when analyzing reports where 0 procs of certain effects ocurred.', emallson),
   change(date(2024, 9, 12), 'Add support for Earthen characters.', ToppleTheNun),
