cb195c475288671de107485baf16aa9d0a0c91bb
Oscar Busk <oscar.busk@gmail.com>
2024-09-15 23:34:02 +0200

    [trinkets] Add signeet of the priory cast to abilties

--- src/common/SPELLS/thewarwithin/index.ts
+++ src/common/SPELLS/thewarwithin/index.ts
@@ -3,7 +3,8 @@ import Enchants from './enchants';
 import Embellishments from './embellishments';
 import Others from './others';
 import Raids from './raids';
+import Trinkets from './trinkets';
 
-const spells = safeMerge(Enchants, Embellishments, Others, Raids);
+const spells = safeMerge(Enchants, Embellishments, Others, Raids, Trinkets);
 
 export default spells;
---
+++ src/common/SPELLS/thewarwithin/trinkets.ts
@@ -0,0 +1,12 @@
+import Spell from '../Spell';
+
+const spells = {
+  // Signet of the Priory
+  BOLSTERING_LIGHT: {
+    id: 443531,
+    name: 'Bolstering Light',
+    icon: 'inv_arathordungeon_signet_color1',
+  },
+} satisfies Record<string, Spell>;
+
+export default spells;
