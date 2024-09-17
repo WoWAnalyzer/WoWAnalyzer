385dae24c26634ba5b011701c088baadde86c1cd
Oscar Busk <oscar.busk@gmail.com>
2024-09-17 03:19:38 +0200

    Update foodchecker for tww

---
+++ src/common/SPELLS/thewarwithin/food.ts
@@ -0,0 +1,367 @@
+import Spell from '../Spell';
+
+const food = {
+  /**
+   * 30 Fishing Skill, 150 Perception
+   */
+  GHOULFISH_DELIGHT: {
+    id: 456406,
+    name: 'Ghoulfish Delight',
+    icon: 'inv_cooking_90_cinnamonbonefishstew',
+  },
+  /**
+   * Supposed to give Lowest Secondary Stat and 1% size
+   *
+   * But right now resotres 0 mana per second until you stand up.
+   */
+  PROTEIN_SLURP: {
+    id: 456576,
+    name: 'Protein Slurp',
+    icon: 'inv_cooking_90_pickledmeatsmoothie_color03',
+  },
+  /** 582 Speed */
+  EXQUISITELY_EVISERATED_MUSCLE: {
+    id: 455369,
+    name: 'Exquisitely Eviscerated Muscle',
+    icon: 'inv_misc_food_84_roastclefthoof',
+  },
+
+  /**
+   * 469 Lowest Secondary Stat
+   */
+  WELL_FED_EVERYTHING_STEW: {
+    id: 461958,
+    name: 'Everything Stew',
+    icon: 'inv_cooking_10_draconicdelicacies',
+  },
+  /**
+   * 469 Lowest Secondary Stat
+   */
+  HEARTY_WELL_FED_EVERYTHING_STEW: {
+    id: 462182,
+    name: 'Hearty Everything Stew',
+    icon: 'inv_cooking_10_draconicdelicacies',
+  },
+  /**
+   * 469 Highest Secondary Stat
+   *
+   * - The Sushi Special
+   * - Beledar's Bounty
+   * - Jester's Board
+   * - Empress' Farewell
+   * - Outsider's Provisions
+   */
+  WELL_FED_HIGHEST_SECONDARY: {
+    id: 462181,
+    name: 'Hearty Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 469 Highest Secondary Stat
+   *
+   * - Hearty Sushi Special
+   * - Hearty Beledar's Bounty
+   * - Hearty Jester's Board
+   * - Hearty Empress' Farewell
+   * - Hearty Outsider's Provisions
+   */
+  HEARTY_WELL_FED_HIGHEST_SECONDARY: {
+    id: 462183,
+    name: 'Hearty Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 446 Primary Stat
+   *
+   * - Feast of the Divine Day
+   * - Feast of the Midnight Masquerade
+   */
+  WELL_FED_PRIMARY_FEAST: {
+    id: 457284,
+    name: 'Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 446 Primary Stat
+   *
+   * - Hearty Feast of the Divine Day
+   * - Hearty Feast of the Midnight Masquerade
+   */
+  HEARTY_WELL_FED_PRIMARY_FEAST: {
+    id: 462210,
+    name: 'Hearty Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 328 Lowest Secondary Stat (15 min)
+   *
+   * - Simple Stew
+   * - Skewered Fillet
+   * - Unseasoned Field Steak
+   * - Spongey Scramble
+   * - Roasted Mycobloom
+   */
+  WELL_FED_SNACKS: {
+    id: 461943,
+    name: 'Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 328 Lowest Secondary Stat (15 min)
+   *
+   * - Hearty Simple Stew
+   * - Hearty Skewered Fillet
+   * - Hearty Unseasoned Field Steak
+   * - Hearty Spongey Scramble
+   * - Hearty Roasted Mycobloom
+   */
+  HEARTY_WELL_FED_SNACKS: {
+    id: 462190,
+    name: 'Hearty Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 328 Lowest Secondary Stat (30 min)
+   *
+   * - Coreway Kabob
+   * - Flash Fire Fillet
+   * - Hallowfall Chili
+   * - Pan Seared Mycobloom
+   */
+  WELL_FED_QUICK_AND_EASY: {
+    id: 461939,
+    name: 'Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /**
+   * 328 Lowest Secondary Stat (30 min)
+   *
+   * - Hearty Coreway Kabob
+   * - Hearty Flash Fire Fillet
+   * - Hearty Hallowfall Chili
+   * - Hearty Pan Seared Mycobloom
+   */
+  HEARTY_WELL_FED_QUICK_AND_EASY: {
+    id: 462194,
+    name: 'Hearty Well Fed',
+    icon: 'spell_misc_food',
+  },
+  /** 235 Crit Mastery */
+  SALT_BAKED_SEAFOOD: {
+    id: 461835,
+    name: 'Salt Baked Seafood',
+    icon: 'inv_misc_food_draenor_saltedskulker',
+  },
+  /** 235 Crit Mastery */
+  HEARTY_SALT_BAKED_SEAFOOD: {
+    id: 462208,
+    name: 'Hearty Salt Baked Seafood',
+    icon: 'inv_misc_food_draenor_saltedskulker',
+  },
+  /** 235 Versatility Mastery */
+  MARINATED_TENDERLOINS: {
+    id: 461845,
+    name: 'Marinated Tenderloins',
+    icon: 'inv_misc_food_meat_cooked_02',
+  },
+  /** 235 Versatility Mastery */
+  HEARTY_MARINATED_TENDERLOINS: {
+    id: 462209,
+    name: 'Hearty Marinated Tenderloins',
+    icon: 'inv_misc_food_meat_cooked_02',
+  },
+  /** 235 Crit Versatility */
+  FISH_AND_CHIPS: {
+    id: 461854,
+    name: 'Fish and Chips',
+    icon: 'inv_cooking_80_swampfishnchips',
+  },
+  /** 235 Crit Versatility */
+  HEARTY_FISH_AND_CHIPS: {
+    id: 462207,
+    name: 'Hearty Fish and Chips',
+    icon: 'inv_cooking_80_swampfishnchips',
+  },
+  /** 235 Haste Mastery */
+  CHIPPY_TEA: {
+    id: 461855,
+    name: 'Chippy Tea',
+    icon: 'inv_misc_food_vendor_roastedbarlytea',
+  },
+  /** 235 Haste Mastery */
+  HEARTY_CHIPPY_TEA: {
+    id: 462206,
+    name: 'Hearty Chippy Tea',
+    icon: 'inv_misc_food_vendor_roastedbarlytea',
+  },
+  /** 235 Haste Versatility */
+  SWEET_AND_SPICY_SOUP: {
+    id: 461856,
+    name: 'Sweet and Spicy Soup',
+    icon: 'inv_misc_food_vendor_tangypeachyogurt',
+  },
+  /** 235 Haste Versatility */
+  HEARTY_SWEET_AND_SPICY_SOUP: {
+    id: 462205,
+    name: 'Hearty Sweet and Spicy Soup',
+    icon: 'inv_misc_food_vendor_tangypeachyogurt',
+  },
+  /** 235 Haste Crit */
+  DEEPFIN_PATTY: {
+    id: 461857,
+    name: 'Deepfin Patty',
+    icon: 'inv_misc_food_vendor_poundedricecakes',
+  },
+  /** 235 Haste Crit */
+  HEARTY_DEEPFIN_PATTY: {
+    id: 462204,
+    name: 'Hearty Deepfin Patty',
+    icon: 'inv_misc_food_vendor_poundedricecakes',
+  },
+  /** 328 Mastery */
+  SALTY_DOG: {
+    id: 461858,
+    name: 'Salty Dog',
+    icon: 'inv_cooking_81_honeypotpie',
+  },
+  /** 328 Mastery */
+  HEARTY_SALTY_DOG: {
+    id: 462203,
+    name: 'Hearty Salty Dog',
+    icon: 'inv_cooking_81_honeypotpie',
+  },
+  /** 328 Versatility */
+  GINGER_GLAZED_FILLET: {
+    id: 461859,
+    name: 'Ginger Glazed Fillet',
+    icon: 'inv_cooking_82_moistfillet',
+  },
+  /** 328 Versatility */
+  HEARTY_GINGER_GLAZED_FILLET: {
+    id: 462202,
+    name: 'Hearty Ginger Glazed Fillet',
+    icon: 'inv_cooking_82_moistfillet',
+  },
+  /** 328 Critical Strike */
+  FIERY_FISH_STICKS: {
+    id: 461860,
+    name: 'Fiery Fish Sticks',
+    icon: 'inv_misc_fish_18',
+  },
+  /** 328 Critical Strike */
+  HEARTY_FIERY_FISH_STICKS: {
+    id: 462201,
+    name: 'Hearty Fiery Fish Sticks',
+    icon: 'inv_misc_fish_18',
+  },
+  /** 328 Haste */
+  ZESTY_NIBBLERS: {
+    id: 461861,
+    name: 'Zesty Nibblers',
+    icon: 'inv_misc_food_86_basilisk',
+  },
+  /** 328 Haste */
+  HEARTY_ZESTY_NIBBLERS: {
+    id: 462200,
+    name: 'Hearty Zesty Nibblers',
+    icon: 'inv_misc_food_86_basilisk',
+  },
+  /** 446 Stamina, 223 Intellect */
+  STUFFED_CAVE_PEPPERS: {
+    id: 461922,
+    name: 'Stuffed Cave Peppers',
+    icon: 'inv_cooking_90_smuggledproduce',
+  },
+  /** 446 Stamina, 223 Intellect */
+  HEARTY_STUFFED_CAVE_PEPPERS: {
+    id: 462199,
+    name: 'Hearty Stuffed Cave Peppers',
+    icon: 'inv_cooking_90_smuggledproduce',
+  },
+  /** 446 Stamina, 223 Agility */
+  MYCOBLOOM_RISOTTO: {
+    id: 461924,
+    name: 'Mycobloom Risotto',
+    icon: 'inv_cooking_80_sailorspie',
+  },
+  /** 446 Stamina, 223 Agility */
+  HEARTY_MYCOBLOOM_RISOTTO: {
+    id: 462198,
+    name: 'Hearty Mycobloom Risotto',
+    icon: 'inv_cooking_80_sailorspie',
+  },
+  /** 446 Stamina, 223 Strength */
+  SIZZLING_HONEY_ROAST: {
+    id: 461925,
+    name: 'Sizzling Honey Roast',
+    icon: 'inv_cooking_100_roastduck',
+  },
+  /** 446 Stamina, 156(?) Strength */
+  HEARTY_SIZZLING_HONEY_ROAST: {
+    id: 462197,
+    name: 'Hearty Sizzling Honey Roast',
+    icon: 'inv_cooking_100_roastduck',
+  },
+  /** 892 Stamina */
+  ANGLERS_DELIGHT: {
+    id: 461927,
+    name: "Angler's Delight",
+    icon: 'inv_cooking_100_revengeservedcold_color04',
+  },
+  /** 892 Stamina */
+  HEARTY_ANGLERS_DELIGHT: {
+    id: 462196,
+    name: "Hearty Angler's Delight",
+    icon: 'inv_cooking_100_revengeservedcold_color04',
+  },
+  /** 625 Stamina */
+  TENDER_TWILIGHT_JERKY: {
+    id: 461946,
+    name: 'Tender Twilight Jerky',
+    icon: 'inv_misc_food_79',
+  },
+  /** 625 Stamina */
+  HEARTY_TENDER_TWILIGHT_JERKY: {
+    id: 462187,
+    name: 'Hearty Tender Twilight Jerky',
+    icon: 'inv_misc_food_79',
+  },
+  /** 312 Stamina, 156 Strength */
+  MEAT_AND_POTATOES: {
+    id: 461947,
+    name: 'Meat and Potatoes',
+    icon: 'inv_misc_food_cooked_eternalblossomfish',
+  },
+  /** 312 Stamina, 156 Strength */
+  HEARTY_MEAT_AND_POTATOES: {
+    id: 462186,
+    name: 'Hearty Meat and Potatoes',
+    icon: 'inv_misc_food_cooked_eternalblossomfish',
+  },
+  /** 312 Stamina, 156 Agility */
+  RIB_STICKERS: {
+    id: 461948,
+    name: 'Rib Stickers',
+    icon: 'inv_misc_food_88_ravagernuggets',
+  },
+  /** 312 Stamina, 156 Agility */
+  HEARTY_RIB_STICKERS: {
+    id: 462185,
+    name: 'Hearty Rib Stickers',
+    icon: 'inv_misc_food_88_ravagernuggets',
+  },
+  /** 312 Stamina, 156 Intellect */
+  SWEET_AND_SOUR_MEATBALLS: {
+    id: 461949,
+    name: 'Sweet and Sour Meatballs',
+    icon: 'inv_misc_food_mango_ice',
+  },
+  /** 312 Stamina, 156 Intellect */
+  HEARTY_SWEET_AND_SOUR_MEATBALLS: {
+    id: 462184,
+    name: 'Hearty Sweet and Sour Meatballs',
+    icon: 'inv_misc_food_mango_ice',
+  },
+} satisfies Record<string, Spell>;
+
+export default food;
--- src/common/SPELLS/thewarwithin/index.ts
+++ src/common/SPELLS/thewarwithin/index.ts
@@ -1,11 +1,12 @@
 import safeMerge from 'common/safeMerge';
 import Enchants from './enchants';
 import Embellishments from './embellishments';
+import Food from './food';
 import Others from './others';
 import Potions from './potions';
 import Raids from './raids';
 import Trinkets from './trinkets';
 
-const spells = safeMerge(Enchants, Embellishments, Others, Potions, Raids, Trinkets);
+const spells = safeMerge(Enchants, Embellishments, Food, Others, Potions, Raids, Trinkets);
 
 export default spells;
