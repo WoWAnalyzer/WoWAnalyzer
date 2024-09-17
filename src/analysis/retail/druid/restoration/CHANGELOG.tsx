ea15a225bd23493f33843af85ea0b9c3452a73bd
Kelton Finch <keltonfinch@gmail.com>
2024-09-16 21:59:13 -0400

    Druid Resto - removed mana saved info from ToL as it was causing problems (#7024)
    Co-authored-by: J David Smith <emallson@emallson.net>

--- src/analysis/retail/druid/restoration/CHANGELOG.tsx
+++ src/analysis/retail/druid/restoration/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import SPELLS from 'common/SPELLS';
 import { TALENTS_DRUID } from 'common/TALENTS';
 
 export default [
+  change(date(2024, 9, 13), <>Removed mana saved attribution from <SpellLink spell={TALENTS_DRUID.INCARNATION_TREE_OF_LIFE_TALENT}/> tracking.</>, Sref),
   change(date(2024, 9, 13), <>Added statistics for <SpellLink spell={TALENTS_DRUID.GERMINATION_TALENT}/> and  <SpellLink spell={TALENTS_DRUID.THRIVING_VEGETATION_TALENT}/>. Fixed an issue where  <SpellLink spell={TALENTS_DRUID.RAMPANT_GROWTH_TALENT}/> statistic was undercounting. </>, Sref),
   change(date(2024, 9, 3), <>Fixed numbers for <SpellLink spell={TALENTS_DRUID.PHOTOSYNTHESIS_TALENT}/> self Lifebloom. Fixed calculation issues in <SpellLink spell={TALENTS_DRUID.ABUNDANCE_TALENT}/> statistic and updated tooltip.</>, Sref),
   change(date(2024, 8, 23), <>Cleaner display of <SpellLink spell={SPELLS.WILD_GROWTH}/>, <SpellLink spell={SPELLS.REGROWTH}/>, <SpellLink spell={SPELLS.SWIFTMEND}/>, and <SpellLink spell={TALENTS_DRUID.SOUL_OF_THE_FOREST_RESTORATION_TALENT}/> sections in Guide. Added <SpellLink spell={SPELLS.SWIFTMEND}/> cast efficiency tracking. Tweaked Guide text. </>, Sref),
