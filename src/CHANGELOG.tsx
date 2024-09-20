8691074f980b26bfdcfc6d5fe4d7032ab44ef1e8
Aveline Estié <aa.estie@gmail.com>
2024-09-20 21:21:29 +0200

    only run click function once

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -36,6 +36,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 9, 20), 'Ensured that LazyLoadStatisticBox is only requesting the results once.', Arlie),
   change(date(2024, 9, 17), 'Updated Class Guide links for Wowhead to point to correct link.', Taevis),
   change(date(2024, 9, 16), 'Updated spellAvailable APL function to properly adjust validation behaviour based on inverse options, and turn it into an options object rather than a straight boolean', Putro),
   change(date(2024, 9, 15), 'Adding TWW weapon enchants, removing DF-specific items (e.g. Fyralath, Call To Dominance, Voice of the Silent Star, etc)', Seriousnes),
