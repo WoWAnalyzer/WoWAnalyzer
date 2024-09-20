ecf4cdd921a246fa0eccc4c6cb6cd57474ea57a8
Oscar Busk <oscar.busk@gmail.com>
2024-09-21 01:09:34 +0200

    Update Buffs that show on the timeline

--- src/analysis/retail/warrior/fury/modules/features/Buffs.ts
+++ src/analysis/retail/warrior/fury/modules/features/Buffs.ts
@@ -1,10 +1,13 @@
-import SPELLS from 'common/SPELLS';
+// import POTIONS from 'common/SPELLS/thewarwithin/potions';
+import SPELLS from 'common/SPELLS/warrior';
+import TALENTS from 'common/TALENTS/warrior';
 import BLOODLUST_BUFFS from 'game/BLOODLUST_BUFFS';
+import { SpellbookAura } from 'parser/core/modules/Aura';
 import CoreAuras from 'parser/core/modules/Auras';
 
 class Buffs extends CoreAuras {
   auras() {
-    //const combatant = this.selectedCombatant;
+    const hasTalent = this.selectedCombatant.hasTalent.bind(this.selectedCombatant);
 
     // This should include ALL buffs that can be applied by your spec.
     // This data can be used by various kinds of modules to improve their results, and modules added in the future may rely on buffs that aren't used today.
@@ -13,15 +16,38 @@ class Buffs extends CoreAuras {
         spellId: Object.keys(BLOODLUST_BUFFS).map((item) => Number(item)),
         timelineHighlight: true,
       },
+      // {
+      //   spellId: Object.entries(POTIONS).map(([, { id }]) => id),
+      //   timelineHighlight: true,
+      // },
+
       {
         spellId: SPELLS.ENRAGE.id,
+        enabled:
+          hasTalent(TALENTS.RAMPAGE_TALENT) ||
+          hasTalent(TALENTS.BLOODTHIRST_TALENT) ||
+          hasTalent(TALENTS.ONSLAUGHT_TALENT),
         timelineHighlight: true,
+        triggeredBySpellId: [SPELLS.RAMPAGE.id, SPELLS.BLOODTHIRST.id, SPELLS.ONSLAUGHT.id],
+      },
+      {
+        spellId: SPELLS.FRENZY.id,
+        enabled: hasTalent(TALENTS.FRENZY_TALENT),
+        timelineHighlight: true,
+        triggeredBySpellId: SPELLS.RAMPAGE.id,
       },
       {
         spellId: SPELLS.RECKLESSNESS.id,
+        enabled:
+          hasTalent(TALENTS.RECKLESSNESS_TALENT) || hasTalent(TALENTS.BERSERKERS_TORMENT_TALENT),
         timelineHighlight: true,
       },
-    ];
+      {
+        spellId: SPELLS.AVATAR_SHARED.id,
+        enabled: hasTalent(TALENTS.AVATAR_SHARED_TALENT),
+        timelineHighlight: true,
+      },
+    ] satisfies SpellbookAura[];
   }
 }
 
