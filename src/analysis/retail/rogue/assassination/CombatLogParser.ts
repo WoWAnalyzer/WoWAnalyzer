dc0fff0177f814ca57a180ac4515c84ce4f639d8
SebShady42 <30671737+SebShady42@users.noreply.github.com>
2024-09-22 11:57:07 -0700

    spec/assassination: initial tww compatibility

--- src/analysis/retail/rogue/assassination/CombatLogParser.ts
+++ src/analysis/retail/rogue/assassination/CombatLogParser.ts
@@ -25,7 +25,6 @@ import BuilderUse from './modules/core/BuilderUse';
 import FinisherUse from './modules/core/FinisherUse';
 import HitCountAoE from './modules/core/HitCountAoE';
 import Envenom from './modules/spells/Envenom';
-import Sepsis from './modules/talents/Sepsis';
 import ThistleTea from './modules/talents/ThistleTea';
 import Guide from './Guide';
 import Mutilate from './modules/spells/Mutilate';
@@ -73,7 +72,6 @@ class CombatLogParser extends CoreCombatLogParser {
 
     // Talents
     crimsonTempestUptimeAndSnapshots: CrimsonTempestUptimeAndSnapshots,
-    sepsis: Sepsis,
     thistleTea: ThistleTea,
     kingsbane: Kingsbane,
 
