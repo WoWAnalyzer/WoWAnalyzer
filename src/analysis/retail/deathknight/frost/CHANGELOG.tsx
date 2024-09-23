fcc98f70f824b6f1db30b119d9a717a8840187bd
robertdiasio <126981667+robertdiasio@users.noreply.github.com>
2024-09-23 18:52:10 -0400

    Bug fix
    Commenting out imports that are throwing warning errors. These imports will be required later.

--- src/analysis/retail/deathknight/frost/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/frost/CHANGELOG.tsx
@@ -1,8 +1,8 @@
 import { change, date } from 'common/changelog';
-import spells from 'common/SPELLS/deathknight';
-import talents from 'common/TALENTS/deathknight';
-import { Boohbah, Khazak, ToppleTheNun } from 'CONTRIBUTORS';
-import { SpellLink } from 'interface';
+//import spells from 'common/SPELLS/deathknight';
+//import talents from 'common/TALENTS/deathknight';
+import { Boohbah } from 'CONTRIBUTORS';
+//import { SpellLink } from 'interface';
 
 export default [
   change(date(2024, 9, 21), 'Updates for 11.0.2: General bug fixes and tweaks to ensure spec matches live - Boohbah', Boohbah),
