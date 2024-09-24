74dbb476ca3774bc840aa69d51db79e7bb5586d3
Sean Smith <ranky26@gmail.com>
2024-09-25 08:56:52 +1000

    [Core]/[Shaman] Weapon Enhancement Checker (#7062)
    * fixing weapon enhancement checker for shamans
    
    * call of the ancestor module refeferencing incorrect talent

--- src/analysis/retail/shaman/elemental/CHANGELOG.tsx
+++ src/analysis/retail/shaman/elemental/CHANGELOG.tsx
@@ -7,6 +7,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2024, 9, 23), <>Corrected talent link from <i>Heed My Call</i> to <SpellLink spell={TALENTS.ANCIENT_FELLOWSHIP_TALENT}/></>, Seriousnes),
   change(date(2024, 9, 22), <>Added Farseer <SpellLink spell={TALENTS.CALL_OF_THE_ANCESTORS_TALENT} /> analysis.</>, Seriousnes),
   change(date(2024, 9, 17), <>Updated <SpellLink spell={TALENTS.CHAIN_LIGHTNING_TALENT} /> inefficient target count from 3 -&gt; 2</>, Seriousnes),
   change(date(2024, 9, 14), <>Updating <SpellLink spell={TALENTS.STORMKEEPER_TALENT} /> and spender window analysis, added support for <SpellLink spell={TALENTS.FLASH_OF_LIGHTNING_TALENT} /> </>, Seriousnes),
