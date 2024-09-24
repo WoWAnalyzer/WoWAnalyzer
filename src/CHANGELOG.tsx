b0b93c2b897267d2bc977a3f6affda5066a5e259
Danny Janse <d.janse97@gmail.com>
2024-09-25 00:10:19 +0100

    TWW Character Profile - Enchants and Gems (#7074)
    * feat: TWW Character Profile Enchants and Gems
    
    * feat: TWW Character Profile Enchants and Gems
    
    ---------
    
    Co-authored-by: Danny Janse <danny.janse@synetic.nl>
    Co-authored-by: J David Smith <emallson@emallson.net>

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -26,6 +26,7 @@ import {
   Tialyss,
   ToppleTheNun,
   Trevor,
+  Vetyst,
   Vohrr,
   Vollmer,
   ZiayaKens,
@@ -37,6 +38,7 @@ import SpellLink from 'interface/SpellLink';
 // prettier-ignore
 export default [
   change(date(2024, 9, 25), 'Show statistic for Tauren crit damage racial. Add all active racials to spellbook, correctly tracking cooldowns.', nullDozzer),
+  change(date(2024, 9, 24), 'Update character profile to show gem icons and enchant effects.', Vetyst),
   change(date(2024, 9, 23), 'Fix temporary weapon enchants for all shaman specs', Seriousnes),
   change(date(2024, 9, 23), 'Fix a bug in table filtering. Notably: this fixes a bug with Brewmaster Purifying Brew active tanking time stats.', emallson),
   change(date(2024, 9, 21), 'Update all checks for enchants and consumables for The War Within. Also show more information when suggesting to improve enchants.', nullDozzer),
