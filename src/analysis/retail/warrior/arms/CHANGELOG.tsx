2dd0588a26909d55b5a93bfa676576a2b505cb49
Oscar Busk <oscar.busk@gmail.com>
2024-09-26 23:06:10 +0200

    Fix/update changelog

--- src/analysis/retail/warrior/arms/CHANGELOG.tsx
+++ src/analysis/retail/warrior/arms/CHANGELOG.tsx
@@ -1,8 +1,10 @@
-import { change, date } from 'common/changelog';
 import { manu310891, nullDozzer } from 'CONTRIBUTORS';
+import TALENTS from 'common/TALENTS/warrior';
+import { change, date } from 'common/changelog';
+import SpellLink from 'interface/SpellLink';
 
 export default [
-  change(date(2024, 9, 25), <>Add <SpellLink spell={TALENTS.INTERVENE_TALENT} /> to spellbook. Cooldown adjustments when specced into <SpellLink spell={TALENTS.HONED_REFLEXES_TALENT} />.</>, nullDozzer),
+  change(date(2024, 9, 26), <>Add <SpellLink spell={TALENTS.INTERVENE_TALENT} /> to spellbook. Cooldown adjustments when specced into <SpellLink spell={TALENTS.HONED_REFLEXES_TALENT} />.</>, nullDozzer),
   change(date(2024, 9, 18), 'Fix various rage bugs! Fix bladestorm not being tracked.', nullDozzer),
   change(date(2024, 9, 7), 'Greatly improved tracking of rage generation and sources of rage. Visualized by showing a graph of Rage in the Rage usage tab.', nullDozzer),
   change(date(2024, 8, 15),"Enabled spec, adjusted cooldowns and fixed skullsplitter talent", manu310891),
--- src/analysis/retail/warrior/fury/CHANGELOG.tsx
+++ src/analysis/retail/warrior/fury/CHANGELOG.tsx
@@ -2,7 +2,7 @@ import { nullDozzer } from 'CONTRIBUTORS';
 import { change, date } from 'common/changelog';
 
 export default [
-  change(date(2024, 9, 25), 'Many improvements to cooldown and haste tracking.', nullDozzer),
+  change(date(2024, 9, 26), 'Many improvements to cooldown and haste tracking.', nullDozzer),
   change(date(2024, 9, 18), 'Fix various rage bugs! Add some missing spells to spellbook.', nullDozzer),
   change(date(2024, 9, 7), 'Greatly improved tracking of rage generation and sources of rage. Visualized by showing a graph of Rage in the Rage usage tab.', nullDozzer),
   change(date(2024, 8, 26), 'Prepare Fury for War Within. Update talents, spells and cooldowns.', nullDozzer),
