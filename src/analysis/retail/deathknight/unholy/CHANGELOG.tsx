d8266cfe95c2ccaaaa1bc136e727a609fdfb620a
Danny Janse <d.janse97@gmail.com>
2024-10-04 16:34:27 +0200

    feature(TTW): Enable Core Foundation of Unholy DK

--- src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
@@ -2,5 +2,5 @@ import { change, date } from 'common/changelog';
 import { Vetyst } from 'CONTRIBUTORS';
 
 export default [
-  change(date(2024, 10, 4), 'Enable Core Foundation of Unholy DK for TTW.', Vetyst),
+  change(date(2024, 10, 4), 'Enable Core Foundation of Unholy DK for TWW.', Vetyst),
 ];
