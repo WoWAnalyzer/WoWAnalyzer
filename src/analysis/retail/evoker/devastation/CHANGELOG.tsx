54a18a64b39aec6ccaf635b0e25e326674c5f182
Vollmer <kristianvollmer@gmail.com>
2024-10-04 15:07:14 +0200

    [Evoker] Fix an issue with MajorDefensive module when recieving Renewing Blaze externally (#7109)
    * fix DefensiveNormalizer to only normalize selectedCombatant events
    
    * changelogs

--- src/analysis/retail/evoker/devastation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/devastation/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { TALENTS_EVOKER } from 'common/TALENTS/evoker';
 import SPELLS from 'common/SPELLS/evoker';
 
 export default [
+  change(date(2024, 10, 4), <>Fix an issue with external <SpellLink spell={TALENTS_EVOKER.RENEWING_BLAZE_TALENT}/> for MajorDefensive module</>, Vollmer),
   change(date(2024, 9, 10), "Update various Modules & Guide Sections for TWW S1", Vollmer),
   change(date(2024, 9, 6), <>Implement <SpellLink spell={TALENTS_EVOKER.WINGLEADER_TALENT}/> module</>, Vollmer),
   change(date(2024, 9, 6), <>Update MajorDefensive module for <SpellLink spell={TALENTS_EVOKER.LIFECINDERS_TALENT}/> and <SpellLink spell={TALENTS_EVOKER.HARDENED_SCALES_TALENT}/></>, Vollmer), 
