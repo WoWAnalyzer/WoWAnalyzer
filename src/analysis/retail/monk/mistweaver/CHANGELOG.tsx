8a947f826a314df6cc1f5eddd019ba1cefb0a1a3
trevorm4 <trevorm4@illinois.edu>
2024-10-06 19:50:09 -0700

    Add Coalesence module

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { Trevor, Vohrr } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date (2024, 10, 6), <>Add <SpellLink spell={TALENTS_MONK.COALESCENCE_TALENT}/> statistic</>, Trevor),
   change(date (2024, 9, 28), <>Fix crash in <SpellLink spell={TALENTS_MONK.CELESTIAL_CONDUIT_TALENT}/> when it failed to hit any allies.</>, Trevor),
   change(date (2024, 9, 24), <>Fix <SpellLink spell={TALENTS_MONK.CELESTIAL_CONDUIT_TALENT}/> event linking</>, Trevor),
   change(date (2024, 9, 24), <>Fix another crash condition</>, Trevor),
