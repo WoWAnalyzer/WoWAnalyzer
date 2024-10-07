b19528c4ea26b722159ca1fcde8eaa3f94d835ee
Danny Janse <d.janse97@gmail.com>
2024-10-07 21:51:55 +0200

    feature(TWW): Correct GCD and Cooldown for AMS (#7126)
    Co-authored-by: Danny Janse <danny.janse@synetic.nl>

--- src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
@@ -1,6 +1,10 @@
 import { change, date } from 'common/changelog';
 import { Vetyst } from 'CONTRIBUTORS';
+import { SpellLink } from 'interface';
+import SPELLS from 'common/SPELLS';
+import TALENTS from 'common/TALENTS/deathknight';
 
 export default [
+  change(date(2024, 10, 7), <>Correct GCD and cooldown of <SpellLink spell={SPELLS.ANTI_MAGIC_SHELL.id} /> when paired with <SpellLink spell={TALENTS.ANTI_MAGIC_BARRIER_TALENT.id} /> and <SpellLink spell={TALENTS.UNYIELDING_WILL_TALENT.id} />.</>, Vetyst),
   change(date(2024, 10, 4), 'Enable Core Foundation of Unholy DK for TWW.', Vetyst),
 ];
