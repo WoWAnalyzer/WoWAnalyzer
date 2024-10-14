2a77fb0952febcc3729f8b86ee8e3191facacf7f
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2024-10-14 12:36:22 -0400

    Update CONFIG.tsx

--- src/analysis/retail/priest/shadow/CONFIG.tsx
+++ src/analysis/retail/priest/shadow/CONFIG.tsx
@@ -11,7 +11,7 @@ const config: Config = {
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
   patchCompatibility: '11.0.2',
-  supportLevel: SupportLevel.MaintainedPartial,
+  supportLevel: SupportLevel.MaintainedFull,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
@@ -39,7 +39,8 @@ const config: Config = {
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport: 'report/WRBVrQfGyvaJjhzm/2-Heroic+Terros+-+Kill+(4:22)/Doxashe/standard',
+  exampleReport:
+    'report/tnNGvpWfbJ1wZdTX/12-Heroic+Ulgrax+the+Devourer+-+Kill+(7:36)/Doxashe/standard',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
