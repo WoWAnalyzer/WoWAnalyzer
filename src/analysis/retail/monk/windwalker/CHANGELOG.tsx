604431865da8d989c1f425d82c94b733c5bf8477
Sam Voss <sam.voss@gmail.com>
2024-10-26 12:18:44 -0700

    windwalker: fix mastery not tracking SEF / WDP correctly
    Mastery was not tracking the correct spell ID for WDP, and was not
    tracking SEF at all.
    
    Signed-off-by: Sam Voss <sam.voss@gmail.com>

--- src/analysis/retail/monk/windwalker/CHANGELOG.tsx
+++ src/analysis/retail/monk/windwalker/CHANGELOG.tsx
@@ -1,6 +1,9 @@
 import { change, date } from 'common/changelog';
+import { TALENTS_MONK } from 'common/TALENTS';
 import { Durpn } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2024, 10, 16), <>Fix Mastery tracking for <SpellLink spell={TALENTS_MONK.WHIRLING_DRAGON_PUNCH_TALENT} /> and <SpellLink spell={TALENTS_MONK.STORM_EARTH_AND_FIRE_TALENT} /></>, Durpn),
   change(date(2024, 9, 15), <>Initial Update for The War Within</>, Durpn),
 ];
