ec4335d2aab82d9b420e627b6f234b9cce7829b4
emallson <emallson@emallson.net>
2024-11-02 12:44:15 -0400

    merge conflicts

--- src/analysis/retail/warrior/protection/modules/Abilities.ts
+++ src/analysis/retail/warrior/protection/modules/Abilities.ts
@@ -140,7 +140,7 @@ class Abilities extends CoreAbilities {
         spell: [TALENTS.INTERVENE_TALENT.id, SPELLS.INTERVENE_BUFF.id, SPELLS.INTERVENE_CHARGE.id],
         enabled: combatant.hasTalent(TALENTS.INTERVENE_TALENT),
         category: SPELL_CATEGORY.UTILITY,
-        cooldown: 30 * combatant.hasTalent(TALENTS.HONED_REFLEXES_PROTECTION_TALENT ? 0.95 : 1),
+        cooldown: 30 * (combatant.hasTalent(TALENTS.HONED_REFLEXES_TALENT) ? 0.95 : 1),
       },
       {
         spell: TALENTS.RALLYING_CRY_TALENT.id,
@@ -172,7 +172,7 @@ class Abilities extends CoreAbilities {
         enabled: combatant.hasTalent(TALENTS.SPELL_REFLECTION_TALENT),
 
         category: SPELL_CATEGORY.DEFENSIVE,
-        cooldown: 20 * (combatant.hasTalent(TALENTS.HONED_REFLEXES_PROTECTION_TALENT) ? 0.95 : 1),
+        cooldown: 20 * (combatant.hasTalent(TALENTS.HONED_REFLEXES_TALENT) ? 0.95 : 1),
       },
       {
         spell: TALENTS.HEROIC_LEAP_TALENT.id,
@@ -342,7 +342,7 @@ class Abilities extends CoreAbilities {
         category: SPELL_CATEGORY.DEFENSIVE,
         cooldown:
           (210 - (combatant.hasTalent(TALENTS.DEFENDERS_AEGIS_TALENT) ? 30 : 0)) *
-          combatant.hasTalent(TALENTS.HONED_REFLEXES_PROTECTION_TALENT ? 0.95 : 1),
+          (combatant.hasTalent(TALENTS.HONED_REFLEXES_TALENT) ? 0.95 : 1),
         charges: 1 + (combatant.hasTalent(TALENTS.DEFENDERS_AEGIS_TALENT) ? 1 : 0),
       },
       {
