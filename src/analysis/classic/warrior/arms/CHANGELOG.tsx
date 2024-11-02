1d0328994d5d0791dc229a266a6b7a35b606790b
emallson <emallson@emallson.net>
2024-11-02 16:24:32 -0400

    changelog entries

--- src/analysis/classic/warrior/arms/CHANGELOG.tsx
+++ src/analysis/classic/warrior/arms/CHANGELOG.tsx
@@ -3,6 +3,8 @@ import { emallson } from 'CONTRIBUTORS';
 
 // prettier-ignore
 export default [
+  change(date(2024, 11, 2), 'Added compensation for unavoidable rotational downtime to Active Time estimation', emallson),
+  change(date(2024, 11, 2), 'Adjust GCDs for Arms Warrior to ignore Haste', emallson),
   change(date(2024, 8, 8), 'Adjust active time section of the Arms Warrior guide', emallson),
   change(date(2024, 8, 4), 'Added foundational support for Arms Warrior', emallson),
 ];
