1967368a7924b90d55b624de216827389fe583ef
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2024-11-02 23:55:51 -0400

    Update EnergyCompression.tsx

--- src/analysis/retail/priest/shadow/modules/talents/Archon/EnergyCompression.tsx
+++ src/analysis/retail/priest/shadow/modules/talents/Archon/EnergyCompression.tsx
@@ -29,8 +29,6 @@ class EnergyCompression extends Analyzer {
   constructor(options: Options) {
     super(options);
 
-    this.active = this.selectedCombatant.hasTalent(TALENTS.ENERGY_COMPRESSION_TALENT);
-
     this.addEventListener(
       Events.damage.by(SELECTED_PLAYER).spell(SPELLS.SHADOW_HALO_DAMAGE),
       this.onHalo,
