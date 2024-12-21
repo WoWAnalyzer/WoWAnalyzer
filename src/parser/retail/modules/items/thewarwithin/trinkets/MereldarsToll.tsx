9de60767061996205bde5dc65469d5f5d96d1dc3
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2024-12-21 17:04:25 +0000

    Run Prettier

--- src/parser/retail/modules/items/thewarwithin/trinkets/MereldarsToll.tsx
+++ src/parser/retail/modules/items/thewarwithin/trinkets/MereldarsToll.tsx
@@ -13,79 +13,81 @@ import { DamageIcon, VersatilityIcon, InformationIcon } from 'interface/icons';
 import Combatants from 'parser/shared/modules/Combatants';
 import { calculateSecondaryStatDefault } from 'parser/core/stats';
 
-
 /**
  * Based on the stats provided on wowhead.
  *
  * https://www.wowhead.com/item=219313/mereldars-toll
  */
- const MERELDARS_TOLL_BASE_ILVL = 437;
- const MERELDARS_TOLL_BASE_GAIN = 658;
-
+const MERELDARS_TOLL_BASE_ILVL = 437;
+const MERELDARS_TOLL_BASE_GAIN = 658;
 
 export default class MereldarsToll extends Analyzer.withDependencies({
   abilities: Abilities,
   combatants: Combatants,
 }) {
+  protected damage: number = 0;
+  protected externalUptimeTotal: number = 0;
 
-    protected damage: number = 0;
-    protected externalUptimeTotal: number = 0;
+  constructor(options: Options) {
+    super(options);
 
-    constructor(options: Options) {
-        super(options);
-
-        this.active = this.selectedCombatant.hasTrinket(ITEMS.MERELDARS_TOLL.id);
-        if (!this.active) {
-            return;
-        }
-
-        this.deps.abilities.add({
-            spell: SPELLS.MERELDARS_TOLL_USE.id,
-            category: SPELL_CATEGORY.ITEMS,
-            cooldown: 90,
-            castEfficiency: {
-                suggestion: true,
-                recommendedEfficiency: 0.9,
-            },
-        });
-
-        this.addEventListener(Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MERELDARS_TOLL_DAMAGE), this.onDamage);
+    this.active = this.selectedCombatant.hasTrinket(ITEMS.MERELDARS_TOLL.id);
+    if (!this.active) {
+      return;
     }
 
-    private onDamage(event: DamageEvent) {
-        this.damage += event.amount + (event.absorbed || 0);
-    }
+    this.deps.abilities.add({
+      spell: SPELLS.MERELDARS_TOLL_USE.id,
+      category: SPELL_CATEGORY.ITEMS,
+      cooldown: 90,
+      castEfficiency: {
+        suggestion: true,
+        recommendedEfficiency: 0.9,
+      },
+    });
 
-    statistic() {
-      const externalUptime = this.deps.combatants.getBuffUptime(SPELLS.MERELDARS_TOLL_VERS.id)
-      const externalUptimePercentage = externalUptime / this.owner.fightDuration;
-      const players = Object.values(this.deps.combatants.players);
-      this.externalUptimeTotal = 0
-      players.forEach((player) => {
-        this.externalUptimeTotal += player.getBuffUptime(SPELLS.MERELDARS_TOLL_VERS.id)
-      });
-      const externalUptimeTotalPercentage = this.externalUptimeTotal / this.owner.fightDuration;
-      const versBuff: number = calculateSecondaryStatDefault(
-        MERELDARS_TOLL_BASE_ILVL,
-        MERELDARS_TOLL_BASE_GAIN,
-        this.selectedCombatant.getTrinket(ITEMS.MERELDARS_TOLL.id)!.itemLevel,
-      );
-      const averageVers: number = versBuff * externalUptimeTotalPercentage
+    this.addEventListener(
+      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.MERELDARS_TOLL_DAMAGE),
+      this.onDamage,
+    );
+  }
 
-      return (
-        <Statistic
-          position={STATISTIC_ORDER.OPTIONAL(99)}
-          category={STATISTIC_CATEGORY.ITEMS}
-          size="flexible"
-        >
+  private onDamage(event: DamageEvent) {
+    this.damage += event.amount + (event.absorbed || 0);
+  }
+
+  statistic() {
+    const externalUptime = this.deps.combatants.getBuffUptime(SPELLS.MERELDARS_TOLL_VERS.id);
+    const externalUptimePercentage = externalUptime / this.owner.fightDuration;
+    const players = Object.values(this.deps.combatants.players);
+    this.externalUptimeTotal = 0;
+    players.forEach((player) => {
+      this.externalUptimeTotal += player.getBuffUptime(SPELLS.MERELDARS_TOLL_VERS.id);
+    });
+    const externalUptimeTotalPercentage = this.externalUptimeTotal / this.owner.fightDuration;
+    const versBuff: number = calculateSecondaryStatDefault(
+      MERELDARS_TOLL_BASE_ILVL,
+      MERELDARS_TOLL_BASE_GAIN,
+      this.selectedCombatant.getTrinket(ITEMS.MERELDARS_TOLL.id)!.itemLevel,
+    );
+    const averageVers: number = versBuff * externalUptimeTotalPercentage;
+
+    return (
+      <Statistic
+        position={STATISTIC_ORDER.OPTIONAL(99)}
+        category={STATISTIC_CATEGORY.ITEMS}
+        size="flexible"
+      >
         <BoringItemValueText item={ITEMS.MERELDARS_TOLL}>
-          <DamageIcon /> {formatNumber(this.owner.getPerSecond(this.damage))} <small>direct DPS</small>
+          <DamageIcon /> {formatNumber(this.owner.getPerSecond(this.damage))}{' '}
+          <small>direct DPS</small>
           <p></p>
           <VersatilityIcon /> {formatNumber(averageVers)} <small>average Versatility granted</small>
           <p></p>
-          <InformationIcon />  {formatPercentage(externalUptimePercentage, 1)}% <small>Versatility buff uptime</small>
+          <InformationIcon /> {formatPercentage(externalUptimePercentage, 1)}%{' '}
+          <small>Versatility buff uptime</small>
         </BoringItemValueText>
-    </Statistic>
-        );
-      }
+      </Statistic>
+    );
+  }
 }
