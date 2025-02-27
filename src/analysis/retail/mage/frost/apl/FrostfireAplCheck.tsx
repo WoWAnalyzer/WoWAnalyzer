25939e7ad0ceb9587341dd1703b18d0ac789b9fa
Eduardo Rosselot <earosselot@gmail.com>
2025-02-27 01:11:52 +0100

    separated flurry conditions

--- src/analysis/retail/mage/frost/apl/FrostfireAplCheck.tsx
+++ src/analysis/retail/mage/frost/apl/FrostfireAplCheck.tsx
@@ -2,72 +2,47 @@ import aplCheck, { build } from 'parser/shared/metrics/apl';
 import TALENTS from 'common/TALENTS/mage';
 import * as cnd from 'parser/shared/metrics/apl/conditions';
 import SPELLS from 'common/SPELLS';
-import SpellLink from 'interface/SpellLink';
 import * as apl from './FrostAplCommons';
-
-// flurry,if=buff.icicles.react<5&remaining_winters_chill=0&debuff.winters_chill.down&prev_gcd.1.glacial_spike
-// flurry,if=buff.icicles.react<5&remaining_winters_chill=0&debuff.winters_chill.down&prev_gcd.1.comet_storm
-// flurry,if=buff.icicles.react<5&remaining_winters_chill=0&debuff.winters_chill.down&prev_gcd.1.frostfire_bolt
-// flurry,if=buff.icicles.react<5&remaining_winters_chill=0&debuff.winters_chill.down&buff.excess_frost.react=2
-// flurry,if=buff.icicles.react<5&buff.brain_freeze.react&buff.excess_fire.react
-
-// 1. Flurry when (!WC and (after FFb/GS/CS or at 2 ExFr)) or BF+ExFi
-// 2. GS
-// 3. IL with FoF or WC
-// 4. FFb
 
 const lessThanFourIcicles = cnd.buffStacks(SPELLS.ICICLES_BUFF, { atMost: 4 });
 const precastFrostfireBolt = cnd.lastSpellCast(TALENTS.FROSTFIRE_BOLT_TALENT);
 const precastCommetStorm = cnd.lastSpellCast(TALENTS.COMET_STORM_TALENT);
 const excessFrostTwoStacks = cnd.buffStacks(SPELLS.EXCESS_FROST_BUFF, { atLeast: 2 });
 const excessFire = cnd.buffPresent(SPELLS.EXCESS_FIRE_BUFF);
-const brainFreeze = cnd.buffPresent(TALENTS.BRAIN_FREEZE_TALENT);
+const brainFreeze = cnd.buffPresent(SPELLS.BRAIN_FREEZE_BUFF);
 
-const flurryFfCondition = cnd.or(
-  cnd.and(cnd.debuffMissing(SPELLS.WINTERS_CHILL), apl.precastGlacialSpike),
-  cnd.and(
-    lessThanFourIcicles,
-    cnd.or(
-      cnd.and(
-        cnd.debuffMissing(SPELLS.WINTERS_CHILL),
-        cnd.or(
-          precastFrostfireBolt,
-          apl.precastGlacialSpike,
-          precastCommetStorm,
-          excessFrostTwoStacks,
-        ),
-      ),
-      cnd.and(excessFire, brainFreeze),
-    ),
-  ),
+const flurryGSCondition = cnd.and(cnd.debuffMissing(SPELLS.WINTERS_CHILL), apl.precastGlacialSpike);
+
+const flurryFfbCsCondition = cnd.and(
+  lessThanFourIcicles,
+  cnd.debuffMissing(SPELLS.WINTERS_CHILL),
+  cnd.or(precastFrostfireBolt, precastCommetStorm),
 );
 
-const flurryFfDescription = (
-  <>
-    you have less than 4 <SpellLink spell={SPELLS.ICICLES_BUFF} /> and either:
-    <ul>
-      <li>
-        no <SpellLink spell={SPELLS.WINTERS_CHILL} /> on target and just cast{' '}
-        <SpellLink spell={TALENTS.FROSTFIRE_BOLT_TALENT} />,{' '}
-        <SpellLink spell={TALENTS.GLACIAL_SPIKE_TALENT} /> or{' '}
-        <SpellLink spell={TALENTS.COMET_STORM_TALENT} />
-      </li>
-      <li>
-        no <SpellLink spell={SPELLS.WINTERS_CHILL} /> on target and have 2 stacks of{' '}
-        <SpellLink spell={SPELLS.EXCESS_FROST_BUFF} />
-      </li>
-      <li>
-        or have <SpellLink spell={SPELLS.EXCESS_FIRE_BUFF} /> and{' '}
-        <SpellLink spell={TALENTS.BRAIN_FREEZE_TALENT} />
-      </li>
-    </ul>
-  </>
+const flurryExFrostCondition = cnd.and(
+  lessThanFourIcicles,
+  cnd.debuffMissing(SPELLS.WINTERS_CHILL),
+  excessFrostTwoStacks,
 );
+
+const FlurryExFireCondition = cnd.and(excessFire, brainFreeze);
 
 export const frostfireApl = build([
   {
     spell: TALENTS.FLURRY_TALENT,
-    condition: cnd.describe(flurryFfCondition, (tense) => flurryFfDescription),
+    condition: flurryGSCondition,
+  },
+  {
+    spell: TALENTS.FLURRY_TALENT,
+    condition: flurryFfbCsCondition,
+  },
+  {
+    spell: TALENTS.FLURRY_TALENT,
+    condition: flurryExFrostCondition,
+  },
+  {
+    spell: TALENTS.FLURRY_TALENT,
+    condition: FlurryExFireCondition,
   },
   {
     spell: TALENTS.GLACIAL_SPIKE_TALENT,
