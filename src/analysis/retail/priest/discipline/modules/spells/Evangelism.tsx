e89c357dec6c2bbfad38263048ca028f90f62fa2
Oisin O'Dochartaigh <oisinpodoherty@gmail.com>
2025-03-16 22:18:58 +0100

    - Introduced tracking for Evangelism healing in the Evangelism module and update evang healing to reflect this

--- src/analysis/retail/priest/discipline/modules/spells/Evangelism.tsx
+++ src/analysis/retail/priest/discipline/modules/spells/Evangelism.tsx
@@ -10,6 +10,8 @@ import isAtonement from '../core/isAtonement';
 import Atonement from './Atonement';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
+import SPELLS from 'common/SPELLS';
+import SpellLink from 'interface/SpellLink';
 
 const EVANGELISM_DURATION = 6;
 const EVANGELISM_DURATION_MS = EVANGELISM_DURATION * 1000;
@@ -20,6 +22,7 @@ class Evangelism extends Analyzer {
   };
   _previousEvangelismCast: CastEvent | null = null;
   protected atonementModule!: Atonement;
+  evangelismActiveHealing = 0;
 
   constructor(options: Options) {
     super(options);
@@ -29,6 +32,10 @@ class Evangelism extends Analyzer {
       this.onCast,
     );
     this.addEventListener(Events.heal.by(SELECTED_PLAYER), this.onHeal);
+    this.addEventListener(
+      Events.heal.by(SELECTED_PLAYER).spell(SPELLS.EVANGELISM_HEAL),
+      this.onEvangelismHeal,
+    );
   }
 
   _evangelismStatistics: {
@@ -43,6 +50,10 @@ class Evangelism extends Analyzer {
     return Object.keys(this._evangelismStatistics)
       .map(Number)
       .map((key: number) => this._evangelismStatistics[key]);
+  }
+
+  onEvangelismHeal(event: HealEvent) {
+    this.evangelismActiveHealing += event.amount + (event.absorbed || 0);
   }
 
   onCast(event: CastEvent) {
@@ -86,6 +97,15 @@ class Evangelism extends Analyzer {
         position={STATISTIC_ORDER.CORE(0)}
         size="flexible"
         category={STATISTIC_CATEGORY.TALENTS}
+        tooltip={
+          <>
+            <SpellLink spell={TALENTS_PRIEST.EVANGELISM_TALENT} /> Active Healing:{' '}
+            {formatNumber(this.evangelismActiveHealing)}
+            <br />
+            <SpellLink spell={SPELLS.ATONEMENT_HEAL_NON_CRIT} /> extension healing:{' '}
+            {formatNumber(evangelismStatistics.reduce((p, c) => p + c.healing, 0))}
+          </>
+        }
         dropdown={
           <table className="table table-condensed">
             <thead>
@@ -110,7 +130,11 @@ class Evangelism extends Analyzer {
         }
       >
         <BoringSpellValueText spell={TALENTS_PRIEST.EVANGELISM_TALENT}>
-          <ItemHealingDone amount={evangelismStatistics.reduce((p, c) => p + c.healing, 0)} />
+          <ItemHealingDone
+            amount={
+              evangelismStatistics.reduce((p, c) => p + c.healing, 0) + this.evangelismActiveHealing
+            }
+          />
         </BoringSpellValueText>
       </Statistic>
     );
