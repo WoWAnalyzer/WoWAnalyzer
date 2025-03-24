56f480e5a23600536d4d0fe768488653a3d88c18
trevorm4 <trevorm4@illinois.edu>
2025-03-23 22:15:03 -0700

    Comments

--- src/analysis/retail/monk/mistweaver/modules/spells/JadeEmpowerment.tsx
+++ src/analysis/retail/monk/mistweaver/modules/spells/JadeEmpowerment.tsx
@@ -4,53 +4,73 @@ import { SpellLink } from 'interface';
 import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
 import { RoundedPanel } from 'interface/guide/components/GuideDivs';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { ApplyBuffEvent, RefreshBuffEvent } from 'parser/core/Events';
+import Events, { CastEvent, RefreshBuffEvent } from 'parser/core/Events';
 import { GUIDE_CORE_EXPLANATION_PERCENT } from '../../Guide';
-import JadefireTeachings from './JadefireTeachings';
 import { BoxRowEntry, PerformanceBoxRow } from 'interface/guide/components/PerformanceBoxRow';
-import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
+import { getLowestPerf, QualitativePerformance } from 'parser/ui/QualitativePerformance';
+import { PerformanceMark } from 'interface/guide';
 
 class JadeEmpowerment extends Analyzer {
-  protected jft!: JadefireTeachings;
   wastedCharges: number = 0;
   castEntries: BoxRowEntry[] = [];
+  hasJFT: boolean = false;
 
   constructor(options: Options) {
     super(options);
-    this.active = this.selectedCombatant.hasTalent(TALENTS_MONK.CHI_BURST_SHARED_TALENT);
+    this.active = this.selectedCombatant.hasTalent(TALENTS_MONK.JADE_EMPOWERMENT_TALENT);
+    this.hasJFT = this.selectedCombatant.hasTalent(TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT);
     this.addEventListener(
       Events.refreshbuff.by(SELECTED_PLAYER).spell(SPELLS.JADE_EMPOWERMENT_BUFF),
       this.onRefresh,
     );
     this.addEventListener(
-      Events.applybuff.by(SELECTED_PLAYER).spell(SPELLS.JADE_EMPOWERMENT_BUFF),
-      this.onApply,
+      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.CRACKLING_JADE_LIGHTNING),
+      this.onCast,
     );
   }
 
-  onApply(event: ApplyBuffEvent) {
-    this.castEntries.push({
-      value: QualitativePerformance.Good,
-      tooltip: (
-        <>
-          <SpellLink spell={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT} /> cast @{' '}
+  onCast(event: CastEvent) {
+    const perfs: QualitativePerformance[] = [
+      this.selectedCombatant.hasBuff(SPELLS.JADE_EMPOWERMENT_BUFF)
+        ? QualitativePerformance.Good
+        : QualitativePerformance.Fail,
+    ];
+    if (this.hasJFT) {
+      perfs.push(
+        this.selectedCombatant.hasBuff(SPELLS.JT_BUFF)
+          ? QualitativePerformance.Good
+          : QualitativePerformance.Fail,
+      );
+    }
+    const tooltip = (
+      <>
+        <div>
+          <SpellLink spell={SPELLS.CRACKLING_JADE_LIGHTNING} /> cast @{' '}
           {this.owner.formatTimestamp(event.timestamp)}
-        </>
-      ),
+        </div>
+        <br />
+        <div>
+          {this.hasJFT && (
+            <>
+              <SpellLink spell={TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT} /> active:{' '}
+              <PerformanceMark perf={perfs.at(-1)!} />
+            </>
+          )}
+        </div>
+        <div>
+          <SpellLink spell={SPELLS.JADE_EMPOWERMENT_BUFF} /> active:{' '}
+          <PerformanceMark perf={perfs[0]} />
+        </div>
+      </>
+    );
+    this.castEntries.push({
+      value: getLowestPerf(perfs),
+      tooltip: tooltip,
     });
   }
 
   onRefresh(event: RefreshBuffEvent) {
     this.wastedCharges += 1;
-    this.castEntries.push({
-      value: QualitativePerformance.Good,
-      tooltip: (
-        <>
-          <SpellLink spell={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT} /> cast @{' '}
-          {this.owner.formatTimestamp(event.timestamp)}
-        </>
-      ),
-    });
   }
 
   get guideSubsection(): JSX.Element {
@@ -61,14 +81,11 @@ class JadeEmpowerment extends Analyzer {
         </b>{' '}
         empowers your next <SpellLink spell={SPELLS.CRACKLING_JADE_LIGHTNING} /> after pressing{' '}
         <SpellLink spell={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT} />. It is critical to avoid
-        overcapping charges of the buff and ideally use it during damage to get effective healing
-        via{' '}
+        overcapping charges of the buff, ensure that{' '}
+        <SpellLink spell={TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT} /> is active if talented, and
+        ideally use it during damage to get effective healing via{' '}
         <SpellLink
-          spell={
-            this.selectedCombatant.hasTalent(TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT)
-              ? TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT
-              : SPELLS.ANCIENT_TEACHINGS
-          }
+          spell={this.hasJFT ? TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT : SPELLS.ANCIENT_TEACHINGS}
         />
         .
       </p>
@@ -89,12 +106,6 @@ class JadeEmpowerment extends Analyzer {
             <div style={styleObj}>
               <b>{this.wastedCharges}</b> <small style={styleObjInner}>wasted buffs</small>
             </div>
-            <small>
-              Green indicates an unwasted stack while red indicates you wasted a stack by casting{' '}
-              <SpellLink spell={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT} /> when already at 2 charges
-              of <SpellLink spell={SPELLS.JADE_EMPOWERMENT_BUFF} />
-              {}
-            </small>
             <PerformanceBoxRow values={this.castEntries} />
           </div>
         </RoundedPanel>
