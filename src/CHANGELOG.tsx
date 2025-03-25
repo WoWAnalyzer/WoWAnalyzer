f1fcaec28101814acdec4e4619981555183928d2
emallson <emallson@emallson.net>
2025-03-25 17:18:44 -0400

    changelog entry

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -41,6 +41,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2025, 3, 25), 'Expanded new Foundation downtime section to caster and healer specs.', emallson),
   change(date(2025, 3, 10), 'Added ability data for Mug\'Zee and Gallywix to the Foundation downtime section', emallson),
   change(date(2025, 3, 8), 'Add missing patch 11.1.0 details.', ToppleTheNun),
   change(date(2025, 2, 26), <>Revised Foundation downtime section for melee specs.</>, emallson),
