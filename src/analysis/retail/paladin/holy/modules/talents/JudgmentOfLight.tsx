094ec2c97c0b86996b8d3b8eab947a1aa3c9fd85
swirl <180882683+lolswirl@users.noreply.github.com>
2025-05-16 18:19:55 -0400

    modernize to TalentSpellText

--- src/analysis/retail/paladin/holy/modules/talents/JudgmentOfLight.tsx
+++ src/analysis/retail/paladin/holy/modules/talents/JudgmentOfLight.tsx
@@ -1,12 +1,11 @@
-import { formatNumber } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/paladin';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { CastEvent, HealEvent } from 'parser/core/Events';
-import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import { STATISTIC_ORDER } from 'parser/ui/StatisticBox';
+import TalentSpellText from 'parser/ui/TalentSpellText';
 
 const JUDGEMENT_HEALS = 5;
 
@@ -18,9 +17,7 @@ class JudgmentOfLight extends Analyzer {
   constructor(options: Options) {
     super(options);
     this.active = this.selectedCombatant.hasTalent(TALENTS.JUDGMENT_OF_LIGHT_TALENT);
-    if (!this.active) {
-      return;
-    }
+
     this.addEventListener(
       Events.cast.by(SELECTED_PLAYER).spell(SPELLS.JUDGMENT_CAST_HOLY),
       this.resetCounter,
@@ -46,10 +43,14 @@ class JudgmentOfLight extends Analyzer {
   statistic() {
     this.wasted += JUDGEMENT_HEALS - this.counter;
     return (
-      <Statistic position={STATISTIC_ORDER.CORE(10)} category={STATISTIC_CATEGORY.TALENTS}>
-        <BoringSpellValueText spell={TALENTS.JUDGMENT_OF_LIGHT_TALENT}>
-          {formatNumber(this.wasted)} Wasted Stacks
-        </BoringSpellValueText>
+      <Statistic
+        position={STATISTIC_ORDER.CORE(10)}
+        size="flexible"
+        category={STATISTIC_CATEGORY.TALENTS}
+      >
+        <TalentSpellText talent={TALENTS.JUDGMENT_OF_LIGHT_TALENT}>
+          {this.wasted} <small>wasted stacks</small>
+        </TalentSpellText>
       </Statistic>
     );
   }
