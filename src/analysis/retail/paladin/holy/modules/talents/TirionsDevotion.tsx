620d72c1d7b9d80cc0c622e29188f35c948c193c
swirl <180882683+lolswirl@users.noreply.github.com>
2025-05-16 18:10:39 -0400

    modernize to TalentSpellText and ItemCooldownReduction elements

--- src/analysis/retail/paladin/holy/modules/talents/TirionsDevotion.tsx
+++ src/analysis/retail/paladin/holy/modules/talents/TirionsDevotion.tsx
@@ -4,13 +4,13 @@ import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, { CastEvent } from 'parser/core/Events';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
 import Statistic from 'parser/ui/Statistic';
-import BoringValueText from 'parser/ui/BoringValueText';
-import { SpellLink } from 'interface';
+import { SpellIcon, SpellLink } from 'interface';
 import SpellUsable from 'parser/shared/modules/SpellUsable';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
-import { formatDuration } from 'common/format';
 import Spell from 'common/SPELLS/Spell';
 import { getLayOnHandsSpell, getWordofGlorySpell } from 'analysis/retail/paladin/shared/constants';
+import TalentSpellText from 'parser/ui/TalentSpellText';
+import ItemCooldownReduction from 'parser/ui/ItemCooldownReduction';
 
 class TirionsDevotion extends Analyzer {
   static dependencies = {
@@ -69,21 +69,17 @@ class TirionsDevotion extends Analyzer {
         size="flexible"
         tooltip={
           <>
-            Effective CDR: {formatDuration(this.effectiveCDR)}
-            <br />
-            Wasted CDR: {formatDuration(this.wastedCDR)}
+            Holy Power spent while <SpellLink spell={TALENTS.LAY_ON_HANDS_TALENT} /> was not on CD:{' '}
+            {this.wastedCDR / this.cdrPerHolyPower / 1000}
           </>
         }
       >
-        <BoringValueText
-          label={
-            <>
-              <SpellLink spell={TALENTS.TIRIONS_DEVOTION_HOLY_TALENT} />
-            </>
-          }
-        >
-          {formatDuration(this.effectiveCDR)} <small>Total CDR</small>
-        </BoringValueText>
+        <TalentSpellText talent={TALENTS.TIRIONS_DEVOTION_HOLY_TALENT}>
+          <div>
+            <SpellIcon spell={TALENTS.LAY_ON_HANDS_TALENT} />{' '}
+            <ItemCooldownReduction effective={this.effectiveCDR} waste={this.wastedCDR} />
+          </div>
+        </TalentSpellText>
       </Statistic>
     );
   }
