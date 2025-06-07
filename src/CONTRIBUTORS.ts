ef1544e6a0542c5e702e4f3f030cba171b55c6f4
brandrewsss <brandrewsss@gmail.com>
2025-06-06 22:37:38 -0400

    Added missing changelog entry for PR #7376

--- src/CONTRIBUTORS.ts
+++ src/CONTRIBUTORS.ts
@@ -2724,7 +2724,25 @@ export const Leftyxiv: Contributor = {
     {
       name: 'Magicmushies',
       spec: SPECS.DEMONOLOGY_WARLOCK,
-      link: 'https://www.warcraftlogs.com/character/us/area-52/magicmushies?zone=43',
+      link: 'https://worldofwarcraft.blizzard.com/en-us/character/us/area-52/magicmushies',
+    },
+  ],
+};
+
+export const Brandrewsss: Contributor = {
+  nickname: 'Brandrews',
+  github: 'brandrewssss',
+  avatar: avatar('brandrews.jpeg'),
+  mains: [
+    {
+      name: 'Brandrews',
+      spec: SPECS.MARKSMANSHIP_HUNTER,
+      link: 'https://worldofwarcraft.blizzard.com/en-us/character/us/thrall/brandrews',
+    },
+    {
+      name: 'Brandrewsdk',
+      spec: SPECS.UNHOLY_DEATH_KNIGHT,
+      link: 'https://worldofwarcraft.blizzard.com/en-us/character/us/thrall/brandrewsdk',
     },
   ],
 };
