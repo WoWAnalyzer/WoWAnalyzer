270d3e21be3b024d6f346b10d973bcd233ac9b1e
J David Smith <emallson@emallson.net>
2025-06-17 12:38:35 -0400

    Revert "Fix Soul Strike overcapping tracking for Demonology Warlock (#7378)"
    This reverts commit b272daf4bc9871a21ad1db2c905ebae71794e3e4.

--- src/analysis/retail/warlock/demonology/modules/talents/SoulStrike.tsx
+++ src/analysis/retail/warlock/demonology/modules/talents/SoulStrike.tsx
@@ -2,12 +2,11 @@ import { formatThousands } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/warlock';
 import Analyzer, { Options, SELECTED_PLAYER_PET } from 'parser/core/Analyzer';
-import Events, { DamageEvent, ResourceChangeEvent } from 'parser/core/Events';
+import Events, { DamageEvent } from 'parser/core/Events';
 import BoringSpellValueText from 'parser/ui/BoringSpellValueText';
 import ItemDamageDone from 'parser/ui/ItemDamageDone';
 import Statistic from 'parser/ui/Statistic';
 import STATISTIC_CATEGORY from 'parser/ui/STATISTIC_CATEGORY';
-import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 
 import SoulShardTracker from '../resources/SoulShardTracker';
 
@@ -18,8 +17,6 @@ class SoulStrike extends Analyzer {
 
   soulShardTracker!: SoulShardTracker;
   damage = 0;
-  totalCasts = 0;
-  wastedShards = 0;
 
   constructor(options: Options) {
     super(options);
@@ -28,56 +25,24 @@ class SoulStrike extends Analyzer {
       Events.damage.by(SELECTED_PLAYER_PET).spell(SPELLS.SOUL_STRIKE_DAMAGE),
       this.handleSoulStrikeDamage,
     );
-    this.addEventListener(
-      Events.resourcechange.by(SELECTED_PLAYER_PET).spell(SPELLS.SOUL_STRIKE_SHARD_GEN),
-      this.handleSoulStrikeShardGen,
-    );
   }
 
   handleSoulStrikeDamage(event: DamageEvent) {
     this.damage += event.amount + (event.absorbed || 0);
   }
 
-  handleSoulStrikeShardGen(event: ResourceChangeEvent) {
-    if (event.resourceChangeType === RESOURCE_TYPES.SOUL_SHARDS.id) {
-      this.totalCasts += 1;
-
-      // Convert waste from fragments (tenths) to whole shards
-      // Each shard = 10 fragments, so divide by 10
-      if (event.waste > 0) {
-        this.wastedShards += event.waste / 10;
-      }
-    }
-  }
-
-  get shardsGenerated() {
-    return this.totalCasts;
-  }
-
   statistic() {
+    const shardsGained = this.soulShardTracker.getGeneratedBySpell(SPELLS.SOUL_STRIKE_SHARD_GEN.id);
     return (
       <Statistic
         category={STATISTIC_CATEGORY.TALENTS}
         size="flexible"
-        tooltip={
-          <ul>
-            <li>{formatThousands(this.damage)} damage</li>
-            {this.wastedShards > 0 && <li>{this.wastedShards} shards wasted due to overcapping</li>}
-          </ul>
-        }
+        tooltip={`${formatThousands(this.damage)} damage`}
       >
         <BoringSpellValueText spell={TALENTS.SOUL_STRIKE_TALENT}>
           <ItemDamageDone amount={this.damage} />
-          <ul>
-            <li>
-              {this.shardsGenerated} <small>shards generated</small>
-            </li>
-            {this.wastedShards > 0 && (
-              <li>
-                {this.wastedShards} <small>shards wasted</small>
-              </li>
-            )}
-          </ul>
+          <br />
+          {shardsGained} <small>Shards generated</small>
         </BoringSpellValueText>
       </Statistic>
     );
