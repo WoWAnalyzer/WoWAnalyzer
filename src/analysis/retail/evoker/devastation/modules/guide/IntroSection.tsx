853174518b805f1fb82343c34d5877a72cd811a5
Vollmer <kristianvollmer@gmail.com>
2025-06-20 11:32:41 +0200

    update intro section for Devastation

--- src/analysis/retail/evoker/devastation/modules/guide/IntroSection.tsx
+++ src/analysis/retail/evoker/devastation/modules/guide/IntroSection.tsx
@@ -10,15 +10,14 @@ export function IntroSection() {
           Wowhead
         </a>
         , <a href="https://www.icy-veins.com/wow/devastation-evoker-pve-dps-guide">Icy Veins</a>,{' '}
-        <a href="https://jereico.com/devastation-evoker-guide/">Jereico's Evoker Compendium</a> and{' '}
-        <a href="https://discord.com/invite/evoker">Evoker Discord</a>.
+        and <a href="https://discord.com/invite/evoker">Evoker Discord</a>.
       </p>
       <p>
         The accuracy and problems pointed out here are <b>guidelines</b> and don't factor in raid
         conditions or edge cases. To find a good measure of success, you should compare your results
         to other top Evokers in the same fight with Warcraft Logs (e.g{' '}
-        <a href="https://www.warcraftlogs.com/zone/rankings/31#boss=2639&class=Evoker&spec=Devastation">
-          Mythic Terros Top 100
+        <a href="https://www.warcraftlogs.com/zone/rankings/42?boss=3015&class=Evoker&spec=Devastation">
+          Mythic Mug'Zee Top 100
         </a>
         ).
       </p>
