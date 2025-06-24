559626d4d9963059a2c2f8ee104a7c7865c01b2f
brandrewsss <brandrewsss@gmail.com>
2025-06-23 22:28:21 -0400

    Updated Unholy Death Knight changelog

--- src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/deathknight';
 
 export default [
+  change(date(2025, 6, 23), 'Update Unholy Death Knight Buffs for Patch 11.1.5', Brandrewsss),
   change(date(2025, 6, 8), 'Update Unholy Death Knight Abilities and Talents for Patch 11.1.5', Brandrewsss),
   change(date(2024, 12, 9), 'Update spec config to reflect lack of long term maintainers', Khazak),
   change(date(2024, 10, 7), <>Correct GCD and cooldown of <SpellLink spell={SPELLS.ANTI_MAGIC_SHELL.id} /> when paired with <SpellLink spell={TALENTS.ANTI_MAGIC_BARRIER_TALENT.id} /> and <SpellLink spell={TALENTS.UNYIELDING_WILL_TALENT.id} />.</>, Vetyst),
