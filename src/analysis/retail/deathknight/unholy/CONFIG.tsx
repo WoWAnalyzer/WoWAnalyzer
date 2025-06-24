c13ac43c3b9bd3938f563a12608bc0009100d463
brandrewsss <brandrewsss@gmail.com>
2025-06-24 13:16:55 -0400

    Updated About Unholy Death Knight

--- src/analysis/retail/deathknight/unholy/CONFIG.tsx
+++ src/analysis/retail/deathknight/unholy/CONFIG.tsx
@@ -3,39 +3,47 @@ import SPECS from 'game/SPECS';
 import Config, { SupportLevel } from 'parser/Config';
 import CHANGELOG from './CHANGELOG';
 import { Brandrewsss } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
+import SPELLS from 'common/SPELLS/deathknight';
+import TALENTS from 'common/TALENTS/deathknight';
 
 const config: Config = {
   // The people that have contributed to this spec recently. People don't have to sign up to be long-time maintainers to be included in this list. If someone built a large part of the spec or contributed something recently to that spec, they can be added to the contributors list. If someone goes MIA, they may be removed after major changes or during a new expansion.
   contributors: [Brandrewsss],
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '11.1.2',
+  patchCompatibility: '11.1.5',
   supportLevel: SupportLevel.Foundation,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
     <>
-      More resources for Unholy:
+      Unholy Death Knights are disease specialists and undead masters, they are literally turning
+      rot and plague into damage. The specialization focuses on spreading and bursting{' '}
+      <SpellLink spell={SPELLS.FESTERING_WOUND} />, maintaining{' '}
+      <SpellLink spell={SPELLS.VIRULENT_PLAGUE} />, and syncing powerful cooldowns like{' '}
+      <SpellLink spell={TALENTS.DARK_TRANSFORMATION_TALENT} /> and{' '}
+      <SpellLink spell={TALENTS.APOCALYPSE_TALENT} /> for massive burst windows.
       <br />
-      <a href="https://discord.gg/acherus" target="_blank" rel="noopener noreferrer">
+      <br />
+      Efficient resource use and cooldown alignment are key. Whether in AoE or single-target, your
+      damage hinges on controlling your minions, maximizing uptime on diseases, and capitalizing on
+      procs and runic power spikes.
+      <br />
+      <br />
+      If you have any feedback, find any issues, or have something you would like to see added, you
+      can open an issue on{' '}
+      <a href="https://github.com/WoWAnalyzer/WoWAnalyzer/issues/new">GitHub</a> or contact us on{' '}
+      <a href="https://discord.gg/AxphPxU">Discord</a>.
+      <br />
+      <br />
+      Make sure to check out the <a href="https://discord.gg/acherus">
         Death Knight Class Discord
       </a>{' '}
+      if you need more specific advice or a more detailed guide than the ones available on{' '}
+      <a href="https://www.icy-veins.com/wow/unholy-death-knight-pve-dps-guide">Icy Veins</a> or{' '}
+      <a href="https://www.wowhead.com/unholy-death-knight-guide">Wowhead</a>.
       <br />
-      <a
-        href="https://www.wowhead.com/guide/classes/death-knight/unholy/overview-pve-dps"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Wowhead Guide
-      </a>{' '}
-      <br />
-      <a
-        href="https://www.icy-veins.com/wow/unholy-death-knight-pve-dps-guide"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Icy Veins Guide
-      </a>{' '}
       <br />
     </>
   ),
