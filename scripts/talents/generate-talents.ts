5f2bf76a8143bb5b74627b235ef5695022884672
emallson <emallson@emallson.net>
2025-08-09 11:02:43 -0400

    live talent update pre-manaforge

--- scripts/talents/generate-talents.ts
+++ scripts/talents/generate-talents.ts
@@ -22,7 +22,7 @@ import {
   TalentNode,
 } from './talent-tree-types';
 
-const LIVE_WOW_BUILD_NUMBER = '11.1.5.60822';
+const LIVE_WOW_BUILD_NUMBER = '11.2.0.62438';
 const LIVE_TALENT_DATA_URL = 'https://www.raidbots.com/static/data/live/talents.json';
 const LIVE_SPELLPOWER_DATA_URL = `https://wago.tools/db2/SpellPower/csv?build=${LIVE_WOW_BUILD_NUMBER}`;
 const PTR_WOW_BUILD_NUMBER = '11.2.0.62136';
