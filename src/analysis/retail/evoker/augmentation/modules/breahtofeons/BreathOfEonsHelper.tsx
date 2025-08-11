5240db4834d40b14066705de4590591c0852cdcb
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2025-08-11 21:09:06 +0100

    Move abilityFilter

--- src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsHelper.tsx
+++ src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsHelper.tsx
@@ -22,6 +22,7 @@ import {
   ABILITY_BLACKLIST,
   ABILITY_NO_BOE_SCALING,
   ABILITY_NO_SCALING,
+  ABILITY_NO_EM_SCALING,
 } from '../util/abilityFilter';
 
 interface Props {
@@ -64,14 +65,7 @@ const BreathOfEonsHelper: React.FC<Props> = ({ windows, fightStartTime, fightEnd
   /** Generate filter so we only get class abilities
    * that can accumulate into BoE */
   const filter = useMemo(() => {
-    const abilityFilter = [
-      ...ABILITY_BLACKLIST,
-      ...ABILITY_NO_BOE_SCALING,
-      ...ABILITY_NO_SCALING,
-    ].join(',');
-
     const filter = `type = "damage" 
-    AND not ability.id in (${abilityFilter}) 
     AND (target.id != source.id)
     AND target.id not in(169428, 169430, 169429, 169426, 169421, 169425, 168932)
     AND not (target.id = source.owner.id)
@@ -127,8 +121,16 @@ const BreathOfEonsHelper: React.FC<Props> = ({ windows, fightStartTime, fightEnd
       40,
     )) as DamageEvent[];
 
+    const ABILITY_FILTER = new Set<number>([
+      ...ABILITY_NO_BOE_SCALING,
+      ...ABILITY_BLACKLIST,
+      ...ABILITY_NO_SCALING,
+      ...ABILITY_NO_EM_SCALING,
+    ]);
+    const events = response.filter((event) => !ABILITY_FILTER.has(event.ability.guid));
+
     return {
-      events: response as DamageEvent[],
+      events,
       start,
       end,
     };
