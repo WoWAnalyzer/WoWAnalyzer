1bfe0917b5d2ef6d1cd4333f8f9653b7db8bd204
emallson <emallson@emallson.net>
2025-08-18 16:00:26 -0400

    add timeline abilities for Manaforge

--- src/game/raids/manaforge-omega/Loomithar.ts
+++ src/game/raids/manaforge-omega/Loomithar.ts
@@ -5,4 +5,32 @@ export default buildBoss({
   id: 3131,
   name: "Loom'ithar",
   background,
+  timeline: {
+    abilities: [
+      {
+        id: 1227263, // Piercing Strand (tank beam),
+        type: 'cast',
+      },
+      {
+        id: 1237272, // Lair Weaving (ring spawn)
+        type: 'cast',
+      },
+      {
+        id: 1227226, // Writhing Wave (p2 frontal)
+        type: 'cast',
+      },
+      {
+        id: 1227784, // Arcane Outrage
+        type: 'cast',
+      },
+    ],
+    debuffs: [
+      {
+        id: 1226311, // Infusion Tether
+      },
+      {
+        id: 1227784, // Arcane Outrage
+      },
+    ],
+  },
 });
