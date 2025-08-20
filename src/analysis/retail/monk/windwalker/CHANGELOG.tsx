e5f609eb95519aca5731ef480e5652eb01bf9e41
Sam Voss <sam.voss@gmail.com>
2025-08-20 10:39:57 -0700

    windwalker: set spec compat to 11.2
    Signed-off-by: Sam Voss <sam.voss@gmail.com>

--- src/analysis/retail/monk/windwalker/CHANGELOG.tsx
+++ src/analysis/retail/monk/windwalker/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { Durpn, emallson } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2025, 8, 20), <>Mark 11.2 as supported</>, Durpn),
   change(date(2025, 7, 18), <>Correctly mark 11.1.7 as supported</>, Durpn),
   change(date(2025, 6, 20), <>Add <SpellLink spell={TALENTS_MONK.SLICING_WINDS_TALENT} /> as tracked spell</>, Durpn),
   change(date(2025, 4, 9), <>Add <SpellLink spell={TALENTS_MONK.CELESTIAL_CONDUIT_TALENT} /> to mastery tracking</>, Durpn),
