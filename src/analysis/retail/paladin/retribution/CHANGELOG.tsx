1deeb889936af6b888025ab83d2a535d9b36a59b
Texleretour <nathan.hostens@gmail.com>
2025-08-22 20:03:19 +0200

    add changelog entry

--- src/analysis/retail/paladin/retribution/CHANGELOG.tsx
+++ src/analysis/retail/paladin/retribution/CHANGELOG.tsx
@@ -2,6 +2,7 @@ import { change, date } from 'common/changelog';
 import { Texleretour, Vetyst } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2025, 8, 22), 'Add Expurgation uptime bar and add Divine Hammer to the Cooldown Graph', Texleretour),
   change(date(2025, 8, 4), 'Few bugfixes and old modules cleaning', Texleretour),
   change(date(2025, 6, 9), 'Basic 11.1.5 support', Vetyst),
 ];
