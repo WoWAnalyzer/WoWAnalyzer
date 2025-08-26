444fb2dfb27976e8342cdcc9b7d296110248a228
Texleretour <nathan.hostens@gmail.com>
2025-08-26 20:14:34 +0200

    add changelog entry, bump spec to 11.2

--- src/analysis/retail/paladin/retribution/CHANGELOG.tsx
+++ src/analysis/retail/paladin/retribution/CHANGELOG.tsx
@@ -1,7 +1,10 @@
 import { change, date } from 'common/changelog';
+import { TALENTS_PALADIN } from 'common/TALENTS';
 import { Texleretour, Vetyst } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2025, 8, 22), <>Add {<SpellLink spell={TALENTS_PALADIN.WAKE_OF_ASHES_TALENT} />} cast analysis.</>, Texleretour),
   change(date(2025, 8, 22), 'Add Expurgation uptime bar and add Divine Hammer to the Cooldown Graph', Texleretour),
   change(date(2025, 8, 4), 'Few bugfixes and old modules cleaning', Texleretour),
   change(date(2025, 6, 9), 'Basic 11.1.5 support', Vetyst),
