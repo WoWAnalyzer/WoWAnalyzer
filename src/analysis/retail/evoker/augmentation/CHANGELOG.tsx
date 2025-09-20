85c56040567eea275a7ab66c7c18bc3015da3d8c
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2025-09-20 10:16:06 +0100

    Update CHANGELOG.tsx

--- src/analysis/retail/evoker/augmentation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/augmentation/CHANGELOG.tsx
@@ -4,7 +4,8 @@ import { SpellLink } from 'interface';
 import TALENTS from 'common/TALENTS/evoker';
 import SPELLS from 'common/SPELLS';
 
-export default [
+export default [  
+  change(date(2025, 9, 20), <>Update filter for <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT}/> to remove a Dimensius mechanic.</>, KYZ),
   change(date(2025, 8, 29), <>Update <SpellLink spell={TALENTS.EBON_MIGHT_TALENT}/> module to be more lenient with pandemic duration.</>, KYZ),
   change(date(2025, 8, 22), "Update example report for 11.2.0", Vollmer),
   change(date(2025, 8, 11), <>Update filter for <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT}/> to make module more reliably load.</>, KYZ),
