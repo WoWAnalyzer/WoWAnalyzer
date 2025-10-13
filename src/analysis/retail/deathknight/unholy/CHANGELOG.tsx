14ae6d1afb59283b814cda7d33239361dd83ae3a
Brandon Andrews <brandrewsss@gmail.com>
2025-10-12 20:17:35 -0400

    Updated Changelog

--- src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/deathknight';
 
 export default [
+  change(date(2025, 10, 12), 'Updated Unholy Death Knight Analyzer to Guide layout and updated folder structure', Brandrewsss),
   change(date(2025, 6, 23), 'Update Unholy Death Knight Buffs for Patch 11.1.5', Brandrewsss),
   change(date(2025, 6, 8), 'Update Unholy Death Knight Abilities and Talents for Patch 11.1.5', Brandrewsss),
   change(date(2024, 12, 9), 'Update spec config to reflect lack of long term maintainers', Khazak),
