40a66baedee63b9975084c34ec18b536c6c2b899
Sean Smith <ranky26@gmail.com>
2025-10-15 17:11:23 +1000

    Mark ele as compatible for S3/11.2.5

--- src/analysis/retail/shaman/elemental/CHANGELOG.tsx
+++ src/analysis/retail/shaman/elemental/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2025, 10, 15), <>Bump to S3 compatibility</>, Seriousnes),
   change(date(2025, 7, 8), <>Disable <SpellLink spell={TALENTS.MASTER_OF_THE_ELEMENTS_ELEMENTAL_TALENT} /></>, Seriousnes),
   change(date(2025, 4, 25), <>Fix <SpellLink spell={TALENTS.ASCENDANCE_ELEMENTAL_TALENT} /> and <SpellLink spell={TALENTS.CALL_OF_THE_ANCESTORS_TALENT} /> timelines, and <SpellLink spell={TALENTS.ASCENDANCE_ELEMENTAL_TALENT} /> missing from main timeline</>, Seriousnes),
   change(date(2025, 4, 21), <>Update example log.</>, Vollmer),
