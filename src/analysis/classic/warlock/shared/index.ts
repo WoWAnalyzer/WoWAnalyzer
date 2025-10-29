d88456a86ddfcb0b1da82dd5022693724cf67c97
Jazminite <65823478+jazminite@users.noreply.github.com>
2025-10-29 16:52:26 -0400

    [Classic] Affliction Warlock (#7498)

--- src/analysis/classic/warlock/shared/index.ts
+++ src/analysis/classic/warlock/shared/index.ts
@@ -1,4 +1,4 @@
-export { default as Haste } from './Haste';
 export { default as DemonicCirclesCreated } from './DemonicCirclesCreated';
+export { default as Haste } from './Haste';
 export { default as Pets } from './Pets';
 export { default as Talents } from './Talents';
