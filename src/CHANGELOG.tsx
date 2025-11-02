6291fcb1f4674f885285ce2e0a517a10541d8d5f
Sharrq <cjgrasty86@gmail.com>
2025-11-01 20:30:48 -0400

    changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -45,6 +45,8 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2025, 11,  1), 'Reqorked CastEfficiencyBar to fit the new style and to account for spells with charges.', Sharrq),  
+  change(date(2025, 11,  1), 'Added additional Guide Components for IntensityChart, IntensityBar, and DamageContribution.', Sharrq),  
   change(date(2025, 10,  14), 'Update CastEfficiencyBar to accept a spell object.', Sharrq),  
   change(date(2025, 10,  22), 'Add and Update Guide Components for CastSummary, CastDetail, CastSequence, StatisticCard, and BuffUptimeBar.', Sharrq),
   change(date(2025, 10,  14), 'Replace Babel with oxc and swc.', ToppleTheNun),
