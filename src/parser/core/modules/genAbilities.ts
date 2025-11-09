f5366c7ae7758b7f183ec32d2f817abcf0fee25d
emallson <emallson@emallson.net>
2025-11-09 12:32:13 -0500

    prevent infinite recursion with spells that are granted by and override the same spell

--- src/parser/core/modules/genAbilities.ts
+++ src/parser/core/modules/genAbilities.ts
@@ -126,7 +126,14 @@ function checkEnabled(
 
   // check if another spell overrides this one *and* is statically enabled
   for (const other of Object.values(allSpells)) {
-    if (other.overrides === spell.id && checkEnabled(other, combatant, allSpells)) {
+    if (
+      other.overrides === spell.id &&
+      // if a spell is both granted by and overrides the same spell, don't check it.
+      // this prevents infinite recursion.
+      // an example of this is the Flying Serpent Kick slam that you can use while using FSK.
+      (!('grantedBy' in other) || other.grantedBy !== spell.id) &&
+      checkEnabled(other, combatant, allSpells)
+    ) {
       return false;
     }
   }
