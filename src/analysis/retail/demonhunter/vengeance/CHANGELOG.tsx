dd5345bf9fab6570a6d51db5c12119a66b1d4730
David Fernandez <quaarkz@gmail.com>
2025-11-10 04:02:55 +0100

    Included the Demon Soul ID for FS and updated it in DemonSoulBuff.tsx, might aswell check how the added damage is calculated because my numbers doesnt add up, 4-5% of damage amp in ~50% uptime and obviously lined with cooldowns sounds bad to me but idk

--- src/analysis/retail/demonhunter/vengeance/CHANGELOG.tsx
+++ src/analysis/retail/demonhunter/vengeance/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import TALENTS from 'common/TALENTS/demonhunter';
 
 // prettier-ignore
 export default [
+  change(date(2025, 11, 10), <>Fix Demon Soul increased damage calculation.</>, Quaarkz),
   change(date(2025, 4, 21), <>Update example log.</>, Vollmer),
   change(date(2024, 9, 23), <>Clean up <SpellLink spell={TALENTS.FRACTURE_TALENT} /> analyzer.</>, ToppleTheNun),
   change(date(2024, 10, 17), 'Untethered Fury talent taken into consideration for Fracture analysis.', Quaarkz),
