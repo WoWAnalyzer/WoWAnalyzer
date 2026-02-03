3c08eea046339e3a25eb8993e7f86fb4a2cd3000
Sean Smith <ranky26@gmail.com>
2026-02-03 19:20:54 +1000

    Update Elemental ability cooldowns

--- src/analysis/retail/shaman/elemental/CHANGELOG.tsx
+++ src/analysis/retail/shaman/elemental/CHANGELOG.tsx
@@ -1,7 +1,10 @@
 import { change, date } from 'common/changelog';
+import { TALENTS_SHAMAN } from 'common/TALENTS';
 import { Seriousnes } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [  
+  change(date(2026, 2, 3), <>Updating <SpellLink spell={TALENTS_SHAMAN.STORMKEEPER_TALENT} /> sources of CDR</>, Seriousnes),
   change(date(2025, 12, 17), <>Updated for Midnight</>, Seriousnes)
 ];
