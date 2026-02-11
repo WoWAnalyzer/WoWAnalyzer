64971cc9785181da0dfa5e752858f34af78e973c
Brandon Andrews <brandrewsss@gmail.com>
2026-02-11 12:24:32 -0500

    Added major cooldowns to the Cooldown Tracking section

--- src/analysis/retail/deathknight/unholy/modules/guide/CooldownSection.tsx
+++ src/analysis/retail/deathknight/unholy/modules/guide/CooldownSection.tsx
@@ -8,6 +8,14 @@ const COOLDOWNS: Cooldown[] = [
     spell: TALENTS.ARMY_OF_THE_DEAD_TALENT,
     isActive: (c) => c.hasTalent(TALENTS.ARMY_OF_THE_DEAD_TALENT),
   },
+  {
+    spell: TALENTS.DARK_TRANSFORMATION_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.DARK_TRANSFORMATION_TALENT),
+  },
+  {
+    spell: TALENTS.PUTREFY_TALENT,
+    isActive: (c) => c.hasTalent(TALENTS.PUTREFY_TALENT),
+  },
 ];
 
 export default function Cooldowns() {
