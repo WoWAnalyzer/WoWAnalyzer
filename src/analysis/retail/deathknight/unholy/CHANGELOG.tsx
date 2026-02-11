a2aadafeca511da1a3388143f83377aa8d4e7bd1
Brandon Andrews <brandrewsss@gmail.com>
2026-02-11 17:23:03 -0500

    Added changelog entry

--- src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/unholy/CHANGELOG.tsx
@@ -1,6 +1,9 @@
 import { change, date } from 'common/changelog';
 import { Brandrewsss } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
+import TALENTS from 'common/TALENTS/deathknight';
 
 export default [
-  change(date(2026, 2, 5), <>Initial Update for Midnight</>, Brandrewsss)
+  change(date(2026, 2, 11), <>Added Cooldown Tracking and Throughput Tracker for <SpellLink spell={TALENTS.DARK_TRANSFORMATION_TALENT} />, <SpellLink spell={TALENTS.ARMY_OF_THE_DEAD_TALENT} />, and <SpellLink spell={TALENTS.PUTREFY_TALENT} />.</>, Brandrewsss),
+  change(date(2026, 2, 5), <>Initial Update for Midnight.</>, Brandrewsss),
 ];
