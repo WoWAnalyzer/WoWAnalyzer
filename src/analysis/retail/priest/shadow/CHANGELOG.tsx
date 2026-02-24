05a0ab8a0cbbfdb218e6b29cf6de693c37c6e823
DoxAshe <103148620+DoxAshe@users.noreply.github.com>
2026-02-24 16:27:18 -0500

    Fix guide view of Collapsing void

--- src/analysis/retail/priest/shadow/CHANGELOG.tsx
+++ src/analysis/retail/priest/shadow/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { DoxAshe } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 
 export default [
+  change(date(2026, 2, 24), <>Fix issues in guide view with <SpellLink spell={TALENTS.COLLAPSING_VOID_TALENT}/> </>,DoxAshe),
   change(date(2026, 2, 23), <>Fix issues in guide view with <SpellLink spell={TALENTS.SHADOW_WORD_MADNESS_TALENT}/> and <SpellLink spell={TALENTS.VOIDFORM_TALENT}/></>,DoxAshe),
   change(date(2026, 1, 10), <>Enable spec for Midnight</>,DoxAshe),
 ];
