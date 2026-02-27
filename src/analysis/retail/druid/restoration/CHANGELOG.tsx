9f54b56b6a8caf77e66adc5bdbdb1dfe8488f56e
Jake <bartljak@gmail.com>
2026-02-26 21:11:47 -0700

    update changelog

--- src/analysis/retail/druid/restoration/CHANGELOG.tsx
+++ src/analysis/retail/druid/restoration/CHANGELOG.tsx
@@ -1,10 +1,11 @@
 import { change, date } from 'common/changelog';
-import { Oratio, Sref, Vollmer } from 'CONTRIBUTORS';
+import { Oratio, Sref, Vollmer, squided } from 'CONTRIBUTORS';
 import { SpellLink } from 'interface';
 import SPELLS from 'common/SPELLS';
 import { TALENTS_DRUID } from 'common/TALENTS';
 
 export default [
+  change(date(2026, 2, 26), <>Activating Resto Druid analyzer for Midnight! Full support is not yet implemented, but partial support is there.</>, squided),
   change(date(2025, 8, 15), <>Added statistic for <SpellLink spell={TALENTS_DRUID.ROOT_NETWORK_TALENT}/>, and added <SpellLink spell={SPELLS.SYMBIOTIC_BLOOMS_WILDSTALKER}/> to HoT graph.</>, Sref),
   change(date(2025, 7, 3), <>Updates <SpellLink spell={SPELLS.INNERVATE}/> guide section to evaluate based on ramp active time instead of flat mana saved. Fixed CDR from Dreamstate. Added active time evaluation to guide.</>, Sref),
   change(date(2025, 7, 1), <>Innervate bug fix. Also updated the recommended mana saved value.</>, Oratio),
