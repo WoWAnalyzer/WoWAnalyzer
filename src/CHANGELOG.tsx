423aacc2a989ced9ece9a5eb8f6b87750066c5a4
Sharrq <cjgrasty86@gmail.com>
2026-03-06 16:41:49 -0500

    more changelogs

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -30,6 +30,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2026, 3, 6), 'Added a Stacked Bar component visual to use in Guides.', Sharrq),
   change(date(2026, 2, 28), 'Rework the visuals of CastDetail, CastSummary, and CastOverview.', Sharrq),
   change(date(2026, 2, 10), 'Prevent duplicate GCDs triggering for normalized channels', Vollmer),
   change(date(2026, 2, 1), 'Fix playwright tests for specs with partial support', Vollmer),
