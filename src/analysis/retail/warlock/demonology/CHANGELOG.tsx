1f29c10022f30c85c9fda9f00c760e04441b4db8
Katorri <katogaming88@gmail.com>
2026-03-11 18:38:30 -0400

    quick fix for Soul Shard graph

--- src/analysis/retail/warlock/demonology/CHANGELOG.tsx
+++ src/analysis/retail/warlock/demonology/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import { Katorri} from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date( 2026, 3, 11), "Fix to show Soul Shard Graph again", Katorri),
   change(date(2026, 3, 6), "Add tracking for Grimoire: Imp Lord and Grimoire: Fel Ravager. Removed Demonic Tyrant Cooldown subsection as it was out of date.", Katorri),
   change(date(20226, 3, 7), <>Create <SpellLink spell={SPELLS.SUMMON_DEMONIC_TYRANT}></SpellLink> Analyzer to detail Demonic Tyrant windows.</>, Katorri),
   change(date(2026, 2, 28), "Initial pass to enable Midnight Demonology. In a very rough state, and some info is out of date.", Katorri)
