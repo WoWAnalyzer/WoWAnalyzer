1cb655a2720db8b9f3aafe5227ad0e40c29e3dd2
yajinni <Yajinni@users.noreply.github.com>
2026-03-15 17:37:27 -0400

    Update Blood DK config to Foundation support and enable parser

--- src/analysis/retail/deathknight/blood/modules/features/BoneShieldTimesByStacks.ts
+++ src/analysis/retail/deathknight/blood/modules/features/BoneShieldTimesByStacks.ts
@@ -16,7 +16,6 @@ import SpellUsable from 'parser/shared/modules/SpellUsable';
 const MAX_BONE_SHIELD_STACKS = 15;
 const BONE_SHIELD_DURATION_MS = 30 * 1000;
 const DRW_COOLDOWN_REDUCTION_MS = 5000;
-const BLOOD_TAP_COOLDOWN_REDUCTION_MS = 2000;
 /**
  boneShieldTimesByStacks() returns an array with the durations of each BS charge
  */
@@ -85,7 +84,6 @@ class BoneShieldStacksBySeconds extends Analyzer {
     const didExpire = nextStacks === 0 ? this.didExpire(event) : false;
     if (nextStacks < this.lastBoneShieldStack && !didExpire) {
       this.reduceDRWCooldown(nextStacks - this.lastBoneShieldStack);
-      this.reduceBloodTapCooldown(nextStacks - this.lastBoneShieldStack);
     }
     this.lastBoneShieldStack = currentStacks(event);
   }
@@ -141,20 +139,6 @@ class BoneShieldStacksBySeconds extends Analyzer {
       const effectiveReduction = this.spellUsable.reduceCooldown(reducedSpellID, reduction);
       this.totalDRWCooldownReduction += effectiveReduction;
       this.totalDRWCooldownReductionWasted += reduction - effectiveReduction;
-    }
-  }
-
-  reduceBloodTapCooldown(stackDiff: number) {
-    if (!this.selectedCombatant.hasTalent(TALENTS.BLOOD_TAP_TALENT)) {
-      return;
-    }
-    if (stackDiff >= 0) {
-      return;
-    }
-    const reduction = -stackDiff * BLOOD_TAP_COOLDOWN_REDUCTION_MS;
-    const reducedSpellID = TALENTS.BLOOD_TAP_TALENT.id;
-    if (this.spellUsable.isOnCooldown(reducedSpellID)) {
-      this.spellUsable.reduceCooldown(reducedSpellID, reduction);
     }
   }
 }
