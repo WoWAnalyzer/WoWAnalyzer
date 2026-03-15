73bc64bdbf86b82ae1bcb6c81616d3c2d2103028
yajinni <Yajinni@users.noreply.github.com>
2026-03-15 18:20:34 -0400

    Fix typo in Blood DK CHANGELOG

--- src/analysis/retail/deathknight/blood/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/blood/CHANGELOG.tsx
@@ -5,12 +5,6 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
-  change(date(2026, 3, 15), <>Cleaned up the code removing talents/spells that don't exist and fixed some others so that it can load logs properly no at a basic level.</>, Yajinni),
-  change(date(2025, 9, 29), <>Updated cooldown and CDR of <SpellLink spell={talents.ANTI_MAGIC_ZONE_TALENT} />.</>, Arlie),
-  change(date(2024, 11, 20), <>Add basic support for San'layn abilities.</>, emallson),
-  change(date(2024, 10, 5), <>Added warning about repeated <SpellLink spell={talents.DEATH_STRIKE_TALENT} /> casts.</>, emallson),
-  change(date(2024, 10, 5), <>Removed Dragonflight rotational analysis.</>, emallson),
-  change(date(2024, 10, 5), <>Fixed handling of <SpellLink spell={talents.EXTERMINATE_TALENT} /> cost reduction and Deathbringer cooldown reduction effects.</>, emallson),
-  change(date(2024, 8, 12), <>Fixed crash when <SpellLink spell={talents.RAPID_DECOMPOSITION_TALENT} /> was not selected.</>, emallson),
-  change(date(2024, 7, 28), 'Basic updates for The War Within', emallson),
+  change(date(2026, 3, 15), <>Cleaned up the code removing talents/spells that don't exist and fixed some others so that it can load logs properly at a basic level.</>, Yajinni),
+
 ];
