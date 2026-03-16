3162662a96fcc0c16aedab174721898930f94168
Alexander Leon <1995.Alexander.Leon@gmail.com>
2026-03-15 19:29:49 -0700

    added missing change log changes

--- src/analysis/retail/druid/feral/CHANGELOG.tsx
+++ src/analysis/retail/druid/feral/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import {  Drowzen } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2026, 3, 15), "Added initial Chomp and Frantic Frenzy Support", Drowzen),
   change(date(2026, 3, 7,), "Initial Midnight update to activate Feral", Drowzen),
 ];
