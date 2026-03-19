1cf0e034f4c3334c39eade455c4b24f2506da149
Kivlov <lordkiv@hotmail.com>
2026-03-19 00:40:34 -0300

    Remove some break tags

--- src/analysis/retail/hunter/survival/CONFIG.tsx
+++ src/analysis/retail/hunter/survival/CONFIG.tsx
@@ -15,32 +15,30 @@ const config: Config = {
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
   description: (
     <>
-      Hello and welcome to the Survival Hunter analyzer! We hope that the suggestions and statistics
-      will be helpful in improving your overall performance. Try and focus on improving only a few
-      things at a time, until those become ingrained in your muscle memory so as to not be
-      concentrating on many different things.
-      <br />
-      <br />
-      If you want to learn more about Survival Hunters, join the Hunter community on the Trueshot
-      Lodge Discord:{' '}
-      <a href="https://www.discord.gg/trueshot" target="_blank" rel="noopener noreferrer">
-        discord.gg/trueshot
-      </a>
-      . The <kbd>#Survival</kbd> channel has a lot of helpful people, and if you post your logs in{' '}
-      <kbd>#log-reviews</kbd>, you can expect to get some good pointers for improvement from the
-      community. The <kbd>#Survival</kbd> channel is also a great place to post feedback for this
-      analyzer, as I'll be very likely to see it there. <br />
-      <br />
-      The best guides available currently are the guides on{' '}
-      <a href="https://www.wowhead.com/guide/classes/hunter/survival/overview-pve-dps">
-        {' '}
-        WoWHead or{' '}
-      </a>{' '}
-      <a href="https://www.icy-veins.com/wow/survival-hunter-pve-dps-guide">Icy-Veins</a>. WoWHead
-      is maintained by Thyminde and Doolb and Icy-Veins by Azortharion, and they are constantly
-      fact-checked by community-members, and improved upon on an almost weekly basis.
-      <br />
-      <br />
+      <p>
+        Hello and welcome to the Survival Hunter analyzer! We hope that the suggestions and
+        statistics will be helpful in improving your overall performance. Try and focus on improving
+        only a few things at a time, until those become ingrained in your muscle memory so as to not
+        be concentrating on many different things.
+      </p>
+      <p>
+        If you want to learn more about Survival Hunters, join the Hunter community on the Trueshot
+        Lodge Discord:{' '}
+        <a href="https://www.discord.gg/trueshot" target="_blank" rel="noopener noreferrer">
+          discord.gg/trueshot
+        </a>
+        . The <kbd>#Survival</kbd> channel has a lot of helpful people, and if you post your logs in{' '}
+        <kbd>#log-reviews</kbd>, you can expect to get some good pointers for improvement from the
+        community. The <kbd>#Survival</kbd> channel is also a great place to post feedback for this
+        analyzer, as I'll be very likely to see it there.
+      </p>
+      <p>
+        The best guides available currently are the guides on{' '}
+        <a href="https://www.wowhead.com/guide/classes/hunter/survival/overview-pve-dps">WoWHead</a>{' '}
+        or <a href="https://www.icy-veins.com/wow/survival-hunter-pve-dps-guide">Icy-Veins</a>.
+        WoWHead is maintained by Thyminde and Doolb and Icy-Veins by Azortharion, and they are
+        constantly fact-checked by community-members, and improved upon on an almost weekly basis.
+      </p>
     </>
   ),
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
