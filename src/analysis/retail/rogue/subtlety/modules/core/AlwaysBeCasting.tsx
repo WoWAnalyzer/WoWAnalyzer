e6c2e168a3d6008a2724f4518ba79a3f8b2d8aa4
Eduardo Rosselot <earosselot@gmail.com>
2026-03-24 13:16:43 -0300

    delete unnecesary file

--- src/analysis/retail/rogue/subtlety/modules/core/AlwaysBeCasting.tsx
+++
@@ -1,8 +0,0 @@
-// import CoreAlwaysBeCasting from 'parser/shared/modules/AlwaysBeCasting';
-// import STATISTIC_ORDER from 'parser/ui/STATISTIC_ORDER';
-
-// class AlwaysBeCasting extends CoreAlwaysBeCasting {
-//   position = STATISTIC_ORDER.CORE(1);
-// }
-
-// export default AlwaysBeCasting;
