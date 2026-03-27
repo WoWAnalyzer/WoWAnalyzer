7dc34a6ca370e6839f0d2e79e8479b071991d048
Harrek <sergiomm.like@gmail.com>
2026-03-26 20:30:42 -0600

    Changelog update

--- src/analysis/retail/shaman/restoration/CHANGELOG.tsx
+++ src/analysis/retail/shaman/restoration/CHANGELOG.tsx
@@ -1,8 +1,11 @@
 import { change, date } from 'common/changelog';
 import { Harrek, Texleretour } from 'CONTRIBUTORS';
+import TALENTS from 'common/TALENTS/shaman';
+import { SpellLink } from 'interface';
 
 // prettier-ignore
 export default [
+  change(date(2026, 3, 26), <>Updated guide, added <SpellLink spell={TALENTS.ASCENDANCE_RESTORATION_TALENT} /> and <SpellLink spell={TALENTS.HEALING_STREAM_TOTEM_RESTORATION_TALENT} /> modules</>, Harrek),
   change(date(2026, 3, 20), "Updates for raid launch", Texleretour),
   change(date(2026, 1, 11), <>Initial support for 12.0</>, Harrek)
 ];
