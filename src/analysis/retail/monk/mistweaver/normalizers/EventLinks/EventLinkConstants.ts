eb5f8294802d74dbc9f37b284a2e0b1f0be78bc7
swirl <180882683+lolswirl@users.noreply.github.com>
2026-03-29 02:33:37 -0400

    add consumption perf to spiritfont module

--- src/analysis/retail/monk/mistweaver/normalizers/EventLinks/EventLinkConstants.ts
+++ src/analysis/retail/monk/mistweaver/normalizers/EventLinks/EventLinkConstants.ts
@@ -75,6 +75,7 @@ export const RUSHING_WIND_KICK = 'RWK';
 // Spiritfont
 export const SPIRITFONT_PROC = 'SpiritfontProc';
 export const SPIRITFONT_TFT = 'SpiritfontTFT';
+export const SPIRITFONT_CONSUMED = 'SpiritfontConsumed';
 
 // Soothing Mist Channel
 export const SOOTHING_MIST_CHANNEL_START = 'SoothingMistChannelStart';
