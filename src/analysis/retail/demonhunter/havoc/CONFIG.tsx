11a355ab59003cbf12f0298693d36475ce581a98
Hezaerd <hezaerd@hezaerd.com>
2026-04-02 13:55:09 -0400

    update patch compatibility for havoc to version 12.0.1

--- src/analysis/retail/demonhunter/havoc/CONFIG.tsx
+++ src/analysis/retail/demonhunter/havoc/CONFIG.tsx
@@ -10,7 +10,7 @@ const CONFIG: Config = {
   contributors: [Topple],
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '12.0.0',
+  patchCompatibility: '12.0.1',
   supportLevel: SupportLevel.MaintainedPartial,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
