4538640587919f8a4c1f78642ba55cbf1fed67f7
Harrek <sergiomm.like@gmail.com>
2026-04-04 17:24:34 -0600

    Fix stasis table

--- src/analysis/retail/evoker/preservation/modules/talents/Stasis.tsx
+++ src/analysis/retail/evoker/preservation/modules/talents/Stasis.tsx
@@ -91,7 +91,7 @@ class Stasis extends Analyzer {
     return (
       <div>
         <SpellLink key={key} spell={spellPair[0]} /> @ {this.owner.formatTimestamp(spellPair[1])}
-      </>
+      </div>
     );
   }
 
@@ -414,22 +414,14 @@ class Stasis extends Analyzer {
         wide
       >
         <SpellLink spell={TALENTS_EVOKER.STASIS_TALENT} /> <small>spell breakdown</small>
-        <table className="table table-condensed">
-          <thead>
-            <tr>
-              <th>Cast #</th>
-              <th>Cast Time</th>
-              <th>Consume Time</th>
-              <th>Spells</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.stasisInfos.map((info, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>{this.owner.formatTimestamp(info.castTime)}</td>
-                <td>{this.owner.formatTimestamp(info.consumeTime)}</td>
-                <td>{info.spells.map((spellPair, idx2) => this.getSpellLink(idx2, spellPair))}</td>
+        <div>
+          <table className="table table-condensed">
+            <thead>
+              <tr>
+                <th>Cast #</th>
+                <th>Cast Time</th>
+                <th>Consume Time</th>
+                <th>Spells</th>
               </tr>
             </thead>
             <tbody>
