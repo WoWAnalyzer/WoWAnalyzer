e3fac7e5b6a8647f3869bf6c930ef9742953fe2b
Sharrq <cjgrasty86@gmail.com>
2026-04-04 22:43:23 -0400

    Update spec compat and warning

--- src/analysis/retail/mage/arcane/CONFIG.tsx
+++ src/analysis/retail/mage/arcane/CONFIG.tsx
@@ -11,7 +11,7 @@ const config: Config = {
   contributors: [Sharrq],
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '12.0.0',
+  patchCompatibility: '12.0.1',
   supportLevel: SupportLevel.MaintainedFull,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
@@ -59,8 +59,8 @@ const config: Config = {
     overview: {
       notes: (
         <AlertWarning>
-          This spec is not being updated for The War Within or Midnight Prepatch. All analysis is in
-          preparation of Midnight's full release.
+          This spec has been fully updated for Midnight (As of April 4). If anything is missing or
+          incorrect, please ping <code>@Sharrq</code> in the Altered Time Discord.
         </AlertWarning>
       ),
     },
