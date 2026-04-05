ba2662a10762be30fdee9df5a9ffb953ebf7668a
Sharrq <cjgrasty86@gmail.com>
2026-04-04 23:38:44 -0400

    Updated sample guides

--- src/analysis/retail/mage/arcane/CONFIG.tsx
+++ src/analysis/retail/mage/arcane/CONFIG.tsx
@@ -66,8 +66,7 @@ const config: Config = {
     },
   },
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport:
-    '/report/YG9R3NkhwmBDv871/28-Heroic+Sikran,+Captain+of+the+Sureki+-+Kill+(3:37)/Coeus/standard',
+  exampleReport: '/report/ar37HGqfWFLNk8w6/26-Heroic+Vorasius+-+Kill+(4:07)/189-Alysseda/standard',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
