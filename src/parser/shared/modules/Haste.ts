1fdc8a0e19b0bdb268a423965c73c17370a99106
tex <nathan.hostens@gmail.com>
2026-04-06 16:24:21 +0200

    update haste constants

--- src/parser/shared/modules/Haste.ts
+++ src/parser/shared/modules/Haste.ts
@@ -77,9 +77,9 @@ const DEFAULT_HASTE_BUFFS: HasteBuffMap = {
     hastePerStack: 0.01,
   },
   [SPELLS.RUSH_OF_LIGHT.id]: 0.05,
-  [SPELLS.UNDISPUTED_RULING_BUFF.id]: 0.15,
+  [SPELLS.UNDISPUTED_RULING_BUFF.id]: 0.12,
   [SPELLS.CRUSADE.id]: {
-    hastePerStack: 0.03,
+    hastePerStack: 0.02,
   },
 
   //region Priest
