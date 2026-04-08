f698ef2f0e973e30241ee2b9299e0721f39cda38
Putro <29204244+Pewtro@users.noreply.github.com>
2026-04-09 00:15:53 +0200

    Apply suggestion from @Pewtro

--- src/analysis/retail/hunter/beastmastery/CHANGELOG.tsx
+++ src/analysis/retail/hunter/beastmastery/CHANGELOG.tsx
@@ -1,7 +1,7 @@
 import { change, date } from 'common/changelog';
 import { Putro, SheenMachine} from 'CONTRIBUTORS';
 export default [
+  change(date(2026, 4, 9), "Remove Kill Shot module", SheenMachine),
   change(date(2026, 4, 4), "Begin removing some outdated modules and components that are no longer relevant in Midnight", Putro),
   change(date(2026, 4, 2) , "Enable the spec to load in 12.0.5", Putro),
-  change(date(2026, 3, 25), "Ability pruning for Beast Mastery.", SheenMachine),
 ];
