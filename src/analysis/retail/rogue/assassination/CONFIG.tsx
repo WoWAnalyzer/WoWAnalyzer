035ff84ad9634f6f5b5555475e357917db6b8e2d
Extrasza <mateusatiliob@outlook.com>
2026-04-11 12:53:22 -0300

    revert exampleReport

--- src/analysis/retail/rogue/assassination/CONFIG.tsx
+++ src/analysis/retail/rogue/assassination/CONFIG.tsx
@@ -47,7 +47,8 @@ const config: Config = {
     },
   },
   // A recent example report to see interesting parts of the spec. Will be shown on the homepage.
-  exampleReport: 'report/bFdKwPV8XWJTvN97/12-Mythic+Vorasius+-+Kill+(6:07)/63-Whispyr/standard',
+  exampleReport:
+    '/report/fYWBL3AtVXTPmkMJ/24-Mythic+One-Armed+Bandit+-+Kill+(6:46)/Violent/standard',
 
   // Don't change anything below this line;
   // The current spec identifier. This is the only place (in code) that specifies which spec this parser is about.
