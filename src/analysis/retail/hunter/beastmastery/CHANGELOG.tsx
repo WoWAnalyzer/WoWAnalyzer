a864fa146b639808f85b2cb3300ce111e83697f5
Sean Whilden <whildensr@gmail.com>
2026-04-14 12:58:22 -0500

    [Hunter] Remove multishot for BM hunter (#7819)
    * commit
    
    * updated label for stats summary
    
    * Update CHANGELOG.tsx
    
    * remove Dire Beast module and update changelog
    
    * remove direBeast from parser
    
    * remove multishot from consts and abilities
    
    * Update BeastCleave to trigger off Wild Thrash instead of multi-shot
    
    * remove bloodshed
    
    * remove arcane shot from rotational abilities
    
    * Update Abilities.tsx
    
    * Update CHANGELOG.tsx

--- src/analysis/retail/hunter/beastmastery/CHANGELOG.tsx
+++ src/analysis/retail/hunter/beastmastery/CHANGELOG.tsx
@@ -1,6 +1,7 @@
 import { change, date } from 'common/changelog';
 import { Putro, SheenMachine} from 'CONTRIBUTORS';
 export default [
+  change(date(2026, 4, 14), "Remove more outdated spells and modules", SheenMachine),
   change(date(2026, 4, 12), "Remove Dire Beast module", SheenMachine),
   change(date(2026, 4, 9), "Remove Kill Shot module", SheenMachine),
   change(date(2026, 4, 4), "Begin removing some outdated modules and components that are no longer relevant in Midnight", Putro),
