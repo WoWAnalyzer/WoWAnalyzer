bb237566da6a4024d6c49bbffcc2fc00e3d34082
Jake <bartljak@gmail.com>
2026-04-14 21:45:17 -0600

    fix bug in power of nature

--- src/analysis/retail/druid/restoration/CHANGELOG.tsx
+++ src/analysis/retail/druid/restoration/CHANGELOG.tsx
@@ -5,6 +5,7 @@ import SPELLS from 'common/SPELLS';
 import { TALENTS_DRUID } from 'common/TALENTS';
 
 export default [
+  change(date(2026, 4, 14), <>Fixed bug in statistic for <SpellLink spell={TALENTS_DRUID.POWER_OF_NATURE_TALENT}/>.</>, squided),
   change(date(2026, 4, 10), <>Fixed bug in statistic for <SpellLink spell={TALENTS_DRUID.STRATEGIC_INFUSION_TALENT}/>.</>, squided),
   change(date(2026, 3, 31), <>Remove recent tranquility check from convoke guide. Fix Abundancy module to account for Intensity crit bonus. Add Intensity module.</>, squided),
   change(date(2026, 3, 23), <>Update mana efficiency calculations and cooldowns tab for Midnight.</>, squided),
