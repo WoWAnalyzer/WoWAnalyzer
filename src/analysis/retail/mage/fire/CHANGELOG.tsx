1ce2c9e20e14af920c99f67cbbc57c12f7f7efcf
Sharrq <cjgrasty86@gmail.com>
2026-04-21 15:40:05 -0400

    Update Arcane/Fire to 12.0.5

--- src/analysis/retail/mage/fire/CHANGELOG.tsx
+++ src/analysis/retail/mage/fire/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import { Thias, Sharrq } from 'CONTRIBUTORS';
 
 // prettier-ignore
 export default [
+  change(date(2026, 4, 21), <>Updated Spec Compatability to 12.0.5.</>, Sharrq),
   change(date(2026, 4, 17), <>Temporarily fix crash of <SpellLink spell={SPELLS.HEAT_SHIMMER_BUFF} /> by extending the buffer</>, Thias),
   change(date(2026, 3, 20), <>Fixed an issue that prevented <SpellLink spell={TALENTS.FLAMESTRIKE_2_FIRE_TALENT} /> from being detected as a <SpellLink spell={SPELLS.HOT_STREAK} /> spender if the player chose the talent to cast <SpellLink spell={TALENTS.FLAMESTRIKE_2_FIRE_TALENT} /> at your target instead of your cursor.</>, Sharrq),
   change(date(2026, 3, 20), <>Fixed an issue that caused some <SpellLink spell={SPELLS.FIRE_BLAST.id} /> casts to not detect that <SpellLink spell={TALENTS.COMBUSTION_TALENT} /> was active.</>, Sharrq),
