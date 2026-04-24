456ccb3058cbbde557d96b414eefc925aaceb006
anon <anon@example.org>
2026-04-24 00:00:00 +0000

    fix(hunter): remove spell usable for BM hunter
    This is no longer needed with the changes to the analyzers now
    handling the cooldowns themselves.

--- src/analysis/retail/hunter/beastmastery/modules/core/SpellUsable.tsx
+++ src/analysis/retail/hunter/beastmastery/modules/core/SpellUsable.tsx
@@ -1,80 +1,9 @@
-import SPELLS from 'common/SPELLS';
-import TALENTS from 'common/TALENTS/hunter';
-import HIT_TYPES from 'game/HIT_TYPES';
-import { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, {
-  AbilityEvent,
-  AnyEvent,
-  CastEvent,
-  DamageEvent,
-  EventType,
-} from 'parser/core/Events';
 import CoreSpellUsable from 'parser/shared/modules/SpellUsable';
 
 class SpellUsable extends CoreSpellUsable {
   static dependencies = {
     ...CoreSpellUsable.dependencies,
   };
-
-  lastPotentialTriggerForBarbedShotReset: AnyEvent | null = null;
-  lastPotentialTriggerForKillCommandReset: AnyEvent | null = null;
-
-  private _tierCanResetBarbedShot = false;
-  private _barbedShotResetsFromT29 = 0;
-
-  get barbedShotResetsFromT29() {
-    return this._barbedShotResetsFromT29;
-  }
-
-  constructor(options: Options) {
-    super(options);
-    this.addEventListener(
-      Events.damage.by(SELECTED_PLAYER).spell(SPELLS.AUTO_SHOT),
-      this.onAutoShotDamage,
-    );
-  }
-
-  onAutoShotDamage(event: DamageEvent) {
-    if (event.hitType === HIT_TYPES.CRIT) {
-      this.lastPotentialTriggerForBarbedShotReset = event;
-      this._tierCanResetBarbedShot = false;
-    }
-  }
-
-  onCast(event: CastEvent) {
-    const spellId = event.ability.guid;
-    if (spellId === TALENTS.BARBED_SHOT_TALENT.id) {
-      this.lastPotentialTriggerForBarbedShotReset = null;
-      if (this.selectedCombatant.hasTalent(TALENTS.WAR_ORDERS_TALENT)) {
-        this.lastPotentialTriggerForKillCommandReset = event;
-      }
-    }
-    super.onCast(event);
-  }
-
-  beginCooldown(triggerEvent: AbilityEvent<EventType>, _spellId: number) {
-    if (triggerEvent.type === EventType.FreeCast) {
-      //Ignore FreeCast events as they are events that have been modified or fabricated
-      //They indicate that a different spell caused it to cast
-      return;
-    }
-    const spellId = triggerEvent.ability.guid;
-    if (spellId === TALENTS.BARBED_SHOT_TALENT.id) {
-      if (this.isOnCooldown(spellId) && this.chargesAvailable(spellId) === 0) {
-        if (this._tierCanResetBarbedShot) {
-          this._barbedShotResetsFromT29 += 1;
-        }
-        this.endCooldown(spellId, this.lastPotentialTriggerForBarbedShotReset?.timestamp);
-      }
-    }
-    if (spellId === TALENTS.KILL_COMMAND_BEAST_MASTERY_TALENT.id) {
-      if (this.isOnCooldown(spellId) && this.chargesAvailable(spellId) === 0) {
-        this.endCooldown(spellId, this.lastPotentialTriggerForKillCommandReset?.timestamp);
-      }
-    }
-
-    super.beginCooldown(triggerEvent, spellId);
-  }
 }
 
 export default SpellUsable;
