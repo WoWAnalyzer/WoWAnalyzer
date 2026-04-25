64bbf189f8e207d36a112c658d78fb52a818e38c
Mathias V. Nielsen <1547127+math280h@users.noreply.github.com>
2026-04-25 18:40:13 -0400

    chore: correct changelog

--- src/CHANGELOG.tsx
+++ src/CHANGELOG.tsx
@@ -38,7 +38,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
-  change(date(2026, 4, 25), 'Fix some extremely long loading times by improving probability calculations from o(n^3) to o(n^2).', Thias),
+  change(date(2026, 4, 25), 'Fix certain extremely long loading times by improving probability calculations from o(n^3) to o(n^2).', Thias),
   change(date(2026, 4, 22), 'Add Midnight raid boss abilities to uptime timelines.', emallson),
   change(date(2026, 4, 21), 'Improve glyph loading by pre-loading during initialization instead of lazy load', Thias),
   change(date(2026, 4, 20), "Regenerate talents for 12.0.5.", Vollmer),
