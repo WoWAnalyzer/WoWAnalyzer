608ac2b23207fac25a3ad911c3eef6f933c0c075
tinkerer12345 <130654988+tinkerer12345@users.noreply.github.com>
2026-05-01 02:44:43 +0100

    Update CHANGELOG.tsx

--- src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
+++ src/analysis/retail/rogue/outlaw/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { Silverkage } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2026, 5, 1), "Support for Deadly Pursuit", Silverkage),
   change(date(2025, 4, 17), "Update Outlaw Rogue for 12.0.1", Silverkage),
 ];
