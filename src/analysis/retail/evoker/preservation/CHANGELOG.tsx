26a41067ca3d71a3168c3147aaa6363e4725e99b
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2026-05-03 18:23:46 +0100

    Update Changelogs

--- src/analysis/retail/evoker/preservation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/preservation/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2026, 5, 3), <>Implement <SpellLink spell={TALENTS_EVOKER.TEMPORAL_BURST_TALENT} /> CDR.</>, KYZ),
   change(date(2026, 4, 27), <>Update <SpellLink spell={TALENTS_EVOKER.LEAPING_FLAMES_TALENT} /> module to include <SpellLink spell={SPELLS.CHRONO_FLAME_CAST} /> damage and healing.</>, KYZ),
   change(date(2026, 4, 23), <>Update <SpellLink spell={TALENTS_EVOKER.FIELD_OF_DREAMS_TALENT} /> module, implement <SpellLink spell={TALENTS_EVOKER.FLUTTERING_SEEDLINGS_TALENT} /> analysis.</>, Harrek),
   change(date(2026, 4, 3), <>Update <SpellLink spell={SPELLS.MERITHRAS_BLESSING_CAST} /> analysis.</>, Harrek),
