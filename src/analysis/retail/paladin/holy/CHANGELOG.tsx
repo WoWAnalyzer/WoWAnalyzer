39d33711f85be8d5b52c00c2f06e262d1a96cefe
Bram Devries <bram@madewithlove.com>
2026-07-16 22:57:07 +0200

    [Holy Paladin] Add changelog entries for the Midnight fixes

--- src/analysis/retail/paladin/holy/CHANGELOG.tsx
+++ src/analysis/retail/paladin/holy/CHANGELOG.tsx
@@ -1,9 +1,12 @@
 import { change, date } from 'common/changelog';
 import { TALENTS_PALADIN } from 'common/TALENTS/paladin';
-import { swirl } from 'CONTRIBUTORS';
+import { swirl, Taleria } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2026, 7, 16), <>Fixed <SpellLink spell={TALENTS_PALADIN.LIGHT_OF_DAWN_TALENT} /> healing not being tracked at all, which also restores its beacon transfer, overhealing, mastery contribution and healing per Holy Power.</>, Taleria),
+  change(date(2026, 7, 16), <>Fixed <SpellLink spell={TALENTS_PALADIN.RECLAMATION_TALENT} /> reporting no healing.</>, Taleria),
+  change(date(2026, 7, 16), <>Removed the <SpellLink spell={TALENTS_PALADIN.SOLAR_GRACE_TALENT} />, <SpellLink spell={TALENTS_PALADIN.BLESSING_OF_ANSHE_TALENT} /> and <SpellLink spell={TALENTS_PALADIN.SECOND_SUNRISE_TALENT} /> statistics, as they no longer reflect how those talents work in Midnight.</>, Taleria),
   change(date(2026, 1, 26), <>Updated <SpellLink spell={TALENTS_PALADIN.RISING_SUNLIGHT_TALENT} />, <SpellLink spell={TALENTS_PALADIN.CRUSADERS_MIGHT_TALENT} />, and added initial <SpellLink spell={TALENTS_PALADIN.BEACON_OF_THE_SAVIOR_1_HOLY_TALENT} /> setup.</>, swirl),
   change(date(2026, 1, 3), <>Initial Holy Paladin support for Midnight.</>, swirl),
 ];
