2da8926a3a9de277774c74b2d3c6219d03aa653f
krealle <vollmer@archon.gg>
2026-07-27 14:19:04 +0200

    add Midnight Season 2 raid/m+

--- src/game/raids/index.ts
+++ src/game/raids/index.ts
@@ -2,8 +2,10 @@ import { Spec } from 'game/SPECS';
 import { Race } from 'game/RACES';
 import PhaseConfig from 'parser/core/PhaseConfig';
 import MythicPlusSeasonOne from 'game/raids/mythicplusseasonone';
+import MythicPlusSeasonTwo from 'game/raids/mythicplusseasontwo';
 import VSDRMQD from 'game/raids/vs_dr_mqd';
 import Sporefall from 'game/raids/sporefall';
+import VenomousAbyss from 'game/raids/venomous_abyss';
 import {
   msv as MogushanVaults,
   hof as HeartOfFear,
@@ -69,11 +71,13 @@ export interface Phase extends PhaseConfig {
 
 const dungeons = {
   // Midnight
+  MythicPlusSeasonTwo,
   MythicPlusSeasonOne,
 };
 
 const raids = {
   // Midnight
+  VenomousAbyss,
   VSDRMQD,
   Sporefall,
   // Mists of Pandaria (Classic)
