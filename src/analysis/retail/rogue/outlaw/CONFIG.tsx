f1119b455eac043b83945d88048b808c0d3fae73
Texleretour <72305552+Texleretour@users.noreply.github.com>
2026-08-07 02:51:05 +0200

    12.0.7 bump and changelog

--- src/analysis/retail/rogue/outlaw/CONFIG.tsx
+++ src/analysis/retail/rogue/outlaw/CONFIG.tsx
@@ -10,7 +10,7 @@ const config: Config = {
   contributors: [Silverkage],
   branch: GameBranch.Retail,
   // The WoW client patch this spec was last updated.
-  patchCompatibility: '12.0.1',
+  patchCompatibility: '12.0.7',
   supportLevel: SupportLevel.MaintainedPartial,
   // Explain the status of this spec's analysis here. Try to mention how complete it is, and perhaps show links to places users can learn more.
   // If this spec's analysis does not show a complete picture please mention this in the `<Warning>` component.
@@ -45,10 +45,10 @@ const config: Config = {
   // The contents of your changelog.
   changelog: CHANGELOG,
   // The CombatLogParser class for your spec.
-  // parser: () =>
-  //   import('./CombatLogParser' /* webpackChunkName: "OutlawRogue" */).then(
-  //     (exports) => exports.default,
-  //   ),
+  parser: () =>
+    import('./CombatLogParser' /* webpackChunkName: "OutlawRogue" */).then(
+      (exports) => exports.default,
+    ),
   // The path to the current directory (relative form project root). This is used for generating a GitHub link directly to your spec's code.
   path: import.meta.url,
 };
