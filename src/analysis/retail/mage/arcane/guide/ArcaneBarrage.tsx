11cdfa51673eb34308d27b0ccd7b5765e22ffb9a
Sharrq <cjgrasty86@gmail.com>
2026-08-15 01:05:18 -0400

    Arcane Barrage

--- src/analysis/retail/mage/arcane/guide/ArcaneBarrage.tsx
+++ src/analysis/retail/mage/arcane/guide/ArcaneBarrage.tsx
@@ -16,7 +16,7 @@ import CastDetail, {
 import Analyzer from 'parser/core/Analyzer';
 import ArcaneBarrage, { ArcaneBarrageData } from '../analyzers/ArcaneBarrage';
 import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
-import { CastEvaluation, TipBox } from 'interface/guide/components';
+import { CastEvaluation } from 'interface/guide/components';
 
 class ArcaneBarrageGuide extends Analyzer {
   static dependencies = {
@@ -44,7 +44,7 @@ class ArcaneBarrageGuide extends Analyzer {
     const hasMaxCharges = cast.charges >= this.MAX_ARCANE_CHARGES;
     const hasNoMana = cast.mana !== undefined && cast.mana <= this.NO_MANA_THRESHOLD;
     const hasClearcasting = cast.activeBuffs.includes(SPELLS.CLEARCASTING_ARCANE.id);
-    const hasOPMissiles = cast.activeBuffs.includes(SPELLS.OVERPOWERED_MISSILES_BUFF.id);
+    const hasArcaneSoulBuff = cast.activeBuffs.includes(SPELLS.ARCANE_SOUL_BUFF.id);
 
     // NO MANA
     if (cast.mana && hasNoMana) {
@@ -56,7 +56,7 @@ class ArcaneBarrageGuide extends Analyzer {
     }
 
     // FAIL CONDITIONS
-    if (cast.touchCD < 5000 && cast.touchCD > 0) {
+    if (cast.touchCD < 5000 && cast.touchCD > 500) {
       return {
         timestamp: cast.cast.timestamp,
         performance: QualitativePerformance.Fail,
@@ -65,136 +65,113 @@ class ArcaneBarrageGuide extends Analyzer {
     }
 
     // PERFECT CONDITIONS
-    if (this.isSpellslinger && cast.salvoStacks === 20 && (hasMaxCharges || this.hasOrbBarrage)) {
+    if (this.isSpellslinger && cast.salvoStacks === 20 && hasMaxCharges) {
       return {
         timestamp: cast.cast.timestamp,
         performance: QualitativePerformance.Perfect,
-        reason: `Had 20 Arcane Salvo Stacks and ${hasMaxCharges ? '4 Arcane Charges.' : 'Orb Barrage Talented.'}`,
+        reason: `Had 4 Arcane Charges and 20 Arcane Salvo stacks.`,
       };
     }
 
-    // GOOD CONDITIONS
-    if (this.isSpellslinger && cast.touchApply && cast.barrageBefore) {
+    if (this.isSpellslinger && cast.salvoStacks >= 19 && this.hasOrbBarrage && hasMaxCharges) {
       return {
         timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Barrage was cast immediately before Touch of the Magi.`,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges, ${cast.salvoStacks} Arcane Salvo stacks, and Orb Barrage talented.`,
       };
     }
 
-    if (this.isSpellslinger && cast.touchApply && cast.barrageAfter) {
+    if (this.isSpellslinger && cast.touchCD === 0 && cast.salvoStacks >= 15 && hasMaxCharges) {
       return {
         timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Barrage was cast immediately after Touch of the Magi.`,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges, ${cast.salvoStacks} Arcane Salvo stacks, and Touch of the Magi was available`,
+      };
+    }
+
+    if (this.isSunfury && cast.salvoStacks >= 12 && hasClearcasting && hasMaxCharges) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges, Clearcasting, and ${cast.salvoStacks} Arcane Salvo stacks`,
       };
     }
 
     if (
-      this.isSpellslingerMissile &&
+      this.isSunfury &&
+      cast.salvoStacks >= 12 &&
       hasMaxCharges &&
-      hasOPMissiles &&
-      hasClearcasting &&
-      cast.salvoStacks >= 5
-    ) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Had 4 Arcane Charges, Overpowered Missiles, Clearcasting, and ${cast.salvoStacks} Arcane Salvo Stacks.`,
-      };
-    }
-
-    if (
-      this.isSpellslingerOrb &&
-      cast.touchRemaining &&
-      cast.touchRemaining < 2000 &&
-      cast.salvoStacks >= 15
-    ) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Touch of the Magi about to end (${formatDurationMillisMinSec(cast.touchRemaining)}) with ${cast.salvoStacks} Arcane Salvo stacks.`,
-      };
-    }
-
-    if (
-      this.isSpellslingerOrb &&
-      cast.surgeRemaining &&
-      cast.surgeRemaining < 2000 &&
-      cast.salvoStacks >= 15
-    ) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Arcane Surge about to end (${formatDurationMillisMinSec(cast.surgeRemaining)}) with ${cast.salvoStacks} Arcane Salvo stacks.`,
-      };
-    }
-
-    if (this.isSunfury && cast.salvoStacks >= 25) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks and 4 Arcane Charges.`,
-      };
-    }
-
-    if (this.isSunfury && cast.barrageAfter) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Touch of the Magi cast immediately before Arcane Barrage.`,
-      };
-    }
-
-    if (this.isSunfury && cast.touchRemaining && cast.touchRemaining < 2000) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Touch of the Magi ends in ${formatDurationMillisMinSec(cast.touchRemaining)}.`,
-      };
-    }
-
-    if (this.isSunfury && cast.activeBuffs.includes(SPELLS.ARCANE_SOUL_BUFF.id)) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Had Arcane Soul.`,
-      };
-    }
-
-    if (
-      this.isSunfury &&
-      !cast.activeBuffs.includes(SPELLS.TOUCH_OF_THE_MAGI_DEBUFF.id) &&
-      !cast.activeBuffs.includes(SPELLS.ARCANE_SURGE_BUFF.id) &&
-      cast.salvoStacks < 19
-    ) {
-      return {
-        timestamp: cast.cast.timestamp,
-        performance: QualitativePerformance.Good,
-        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks without Touch of the Magi or Arcane Surge.`,
-      };
-    }
-
-    if (
-      this.isSunfury &&
-      !cast.activeBuffs.includes(SPELLS.TOUCH_OF_THE_MAGI_DEBUFF.id) &&
-      !cast.activeBuffs.includes(SPELLS.ARCANE_SURGE_BUFF.id) &&
-      cast.targetsHit >= 3 &&
+      cast.targetsHit >= 5 &&
       cast.arcaneOrbAvail
     ) {
       return {
         timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges, hit ${cast.targetsHit} targets, and had Arcane Orb available.`,
+      };
+    }
+
+    if (
+      this.isSunfury &&
+      cast.salvoStacks >= 12 &&
+      hasMaxCharges &&
+      cast.targetsHit >= 5 &&
+      cast.arcanePulseAvail
+    ) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges, hit ${cast.targetsHit} targets, and had Arcane Pulse available.`,
+      };
+    }
+
+    // GOOD CONDITIONS
+    if (this.isSpellslinger && cast.salvoStacks === 20) {
+      return {
+        timestamp: cast.cast.timestamp,
         performance: QualitativePerformance.Good,
-        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks and an Arcane Orb charge without Touch of the Magi or Arcane Surge.`,
+        reason: `Had ${cast.charges} Arcane Charges and 20 Arcane Salvo stacks.`,
+      };
+    }
+
+    if (this.isSunfury && hasArcaneSoulBuff) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had an Arcane Soul proc.`,
+      };
+    }
+
+    if (this.isSunfury && cast.salvoStacks === 25 && hasMaxCharges) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Perfect,
+        reason: `Had 4 Arcane Charges and 25 Arcane Salvo stacks.`,
       };
     }
 
     // OK CONDITIONS
-    if (this.isSpellslinger && cast.salvoStacks < 20) {
+    if (this.isSpellslinger && cast.salvoStacks < 15 && cast.touchCD > 5000) {
       return {
         timestamp: cast.cast.timestamp,
         performance: QualitativePerformance.Ok,
-        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks.`,
+        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks with ${formatDurationMillisMinSec(cast.touchCD)} CD remaining on Touch of the Magi.`,
+      };
+    }
+
+    if (this.isSpellslinger && cast.salvoStacks < 20 && !this.hasOrbBarrage) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Ok,
+        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks without Orb Barrage.`,
+      };
+    }
+
+    if (this.isSunfury && !hasClearcasting && cast.salvoStacks < 12) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Ok,
+        reason: `Had ${cast.salvoStacks} Arcane Salvo stacks without Clearcasting.`,
       };
     }
 
@@ -203,6 +180,23 @@ class ArcaneBarrageGuide extends Analyzer {
         timestamp: cast.cast.timestamp,
         performance: QualitativePerformance.Ok,
         reason: `Had ${cast.salvoStacks} Arcane Salvo stacks.`,
+      };
+    }
+
+    if (this.isSunfury && cast.targetsHit < 5 && (cast.arcaneOrbAvail || cast.arcanePulseAvail)) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Ok,
+        reason: `Had Arcane Orb or Arcane Pulse, but only hit ${cast.targetsHit} targets.`,
+      };
+    }
+
+    // FAIL
+    if (this.isSunfury && cast.targetsHit < 5) {
+      return {
+        timestamp: cast.cast.timestamp,
+        performance: QualitativePerformance.Fail,
+        reason: `Hit ${cast.targetsHit} targets hit with no other benefits.`,
       };
     }
 
@@ -215,95 +209,55 @@ class ArcaneBarrageGuide extends Analyzer {
   }
 
   get guideSubsection(): JSX.Element {
-    const arcaneBlast = <SpellLink spell={SPELLS.ARCANE_BLAST} />;
     const arcaneCharge = <SpellLink spell={SPELLS.ARCANE_CHARGE} />;
     const touchOfTheMagi = <SpellLink spell={TALENTS.TOUCH_OF_THE_MAGI_TALENT} />;
     const arcaneBarrage = <SpellLink spell={SPELLS.ARCANE_BARRAGE} />;
     const clearcasting = <SpellLink spell={SPELLS.CLEARCASTING_ARCANE} />;
+    const arcanePulse = <SpellLink spell={TALENTS.ARCANE_PULSE_TALENT} />;
     const arcaneOrb = <SpellLink spell={SPELLS.ARCANE_ORB} />;
     const arcaneSalvo = <SpellLink spell={TALENTS.ARCANE_SALVO_TALENT} />;
-    const overpoweredMissiles = <SpellLink spell={TALENTS.OVERPOWERED_MISSILES_TALENT} />;
     const orbBarrage = <SpellLink spell={TALENTS.ORB_BARRAGE_TALENT} />;
-    const arcaneSurge = <SpellLink spell={TALENTS.ARCANE_SURGE_TALENT} />;
     const arcaneSoul = <SpellLink spell={SPELLS.ARCANE_SOUL_BUFF} />;
-    const gloriousIncandescence = <SpellLink spell={TALENTS.GLORIOUS_INCANDESCENCE_TALENT} />;
 
     const explanation = (
       <>
         <p>
           <b>{arcaneBarrage}</b> is your {arcaneCharge} spender, removing the associated increased
           mana costs and damage. In order to maintain the damage increase as long as possible, you
-          should only cast {arcaneBarrage} under the below conditions, which are tailored to your
-          current talent build.
+          should only cast {arcaneBarrage} under the below conditions, and should always aim to have
+          4 {arcaneCharge}s before casting {arcaneBarrage}.
         </p>
         <p>
           Regardless of the below, if {touchOfTheMagi} will be available in the next 4-5 seconds,
           you should hold {arcaneBarrage} for {touchOfTheMagi}.
         </p>
-        {this.isSpellslingerMissile && (
+        {this.isSpellslinger && (
           <ul>
             <li>
-              You have 20 stacks of {arcaneSalvo} and either 4 {arcaneCharge}s or have the{' '}
-              {orbBarrage} talent.
+              You have 20 stacks of {arcaneSalvo} (or 19 stacks with {orbBarrage}).
             </li>
             <li>
-              You just casted, or are about to cast, {touchOfTheMagi} ({arcaneBarrage} should be
-              within a GCD of {touchOfTheMagi}, either before it or after it).
+              You have at least 15 stacks of {arcaneSalvo} and {touchOfTheMagi} is ready.
             </li>
-            <li>You don't have enough mana for {arcaneBlast}</li>
-            <li>
-              You have 4 {arcaneCharge}s, an {overpoweredMissiles} and {clearcasting} proc, and at
-              least 5 {arcaneSalvo} stacks.
-            </li>
-          </ul>
-        )}
-        {this.isSpellslingerOrb && (
-          <ul>
-            <li>
-              You have 20 stacks of {arcaneSalvo} and either 4 {arcaneCharge}s or have the{' '}
-              {orbBarrage} talent.
-            </li>
-            <li>
-              You just casted, or are about to cast, {touchOfTheMagi} ({arcaneBarrage} should be
-              within a GCD of {touchOfTheMagi}, either before it or after it).
-            </li>
-            <li>You don't have enough mana for {arcaneBlast}</li>
-            <li>
-              {arcaneSurge} or {touchOfTheMagi} will end in the next 1-2 seconds and you have 15 or
-              more {arcaneSalvo} stacks.
-            </li>
+            <li>You are out of mana.</li>
           </ul>
         )}
         {this.isSunfury && (
           <ul>
+            <li>{arcaneSoul} is active.</li>
+            <li>You have 25 stacks of {arcaneSalvo}.</li>
             <li>
-              You have 4 {arcaneCharge}s and 25 stacks of {arcaneSalvo}
+              You have at least 12 stacks of {arcaneSalvo} and a {clearcasting} proc.
             </li>
             <li>
-              Your last cast was {touchOfTheMagi} or the {touchOfTheMagi} debuff will end in 1-2
-              seconds.
-            </li>
-            <li>You have {arcaneSoul}.</li>
-            <li>You don't have enough mana for {arcaneBlast}</li>
-            <li>
-              You have 4 {arcaneCharge}s, are not in a burn phase ({touchOfTheMagi} and{' '}
-              {arcaneSurge} are not active), and &lt; 19 stacks of {arcaneSalvo}.
+              You have at least 8 stacks of {arcaneSalvo} and {touchOfTheMagi} is ready.
             </li>
             <li>
-              You have 4 {arcaneCharge}s, are not in a burn phase ({touchOfTheMagi} and{' '}
-              {arcaneSurge} are not active), {arcaneBarrage} will hit 3 or more enemies, and you
-              have a charge of {arcaneOrb} available.
+              You have 12 or more stacks of {arcaneSalvo}, {arcanePulse} or {arcaneOrb} is
+              available, and you can hit 5 or more targets with {arcanePulse} or {arcaneOrb}.
             </li>
+            <li>You are out of mana.</li>
           </ul>
-        )}
-        {this.isSunfury && (
-          <p>
-            <TipBox type="note" title="Meteorites">
-              If you are close to a multiple of 6 {arcaneSalvo} stacks (6, 12, 18), it is beneficial
-              to hold {arcaneBarrage} until you are above that threshold to maximize the number of
-              meteorites generated by {gloriousIncandescence}.
-            </TipBox>
-          </p>
         )}
       </>
     );
@@ -360,7 +314,7 @@ class ArcaneBarrageGuide extends Analyzer {
         cast.touchCD
           ? {
               label: 'Touch CD',
-              value: formatDuration(cast.touchCD),
+              value: formatDuration(cast.touchCD, 1),
               tooltip: `Cooldown Remaining on Touch of the Magi`,
             }
           : undefined,
