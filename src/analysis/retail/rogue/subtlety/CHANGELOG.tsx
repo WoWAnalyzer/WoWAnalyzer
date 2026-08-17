e61e20700853dcd53f11f6ee6ce23db3a46b5764
Eduardo Rosselot <earosselot@gmail.com>
2026-08-17 19:15:29 -0300

    sublety 12.1 changes

--- src/analysis/retail/rogue/subtlety/CHANGELOG.tsx
+++ src/analysis/retail/rogue/subtlety/CHANGELOG.tsx
@@ -1,8 +1,17 @@
 import { change, date } from 'common/changelog';
 import { Earosselot } from 'CONTRIBUTORS';
 import SHARED_CHANGELOG from 'analysis/retail/rogue/shared/CHANGELOG';
+import TALENTS from 'common/TALENTS/rogue';
+import { SpellLink } from 'interface';
 
 export default [
+  change(
+    date(2026, 8, 17),
+    <>
+      Add <SpellLink spell={TALENTS.GOREMAWS_BITE_TALENT} /> to the ability list
+    </>,
+    Earosselot,
+  ),
   change(date(2026, 3, 23), <>Fix Shadow Blades Module</>, Earosselot),
   change(date(2026, 3, 23), <>Adding Shadow Dance Module</>, Earosselot),
   change(date(2026, 3, 20), <>Enable Sublety for midnight</>, Earosselot),
