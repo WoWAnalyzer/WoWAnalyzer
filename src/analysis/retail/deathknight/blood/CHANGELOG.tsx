376f576197453911e4632ad4cc89294f69c3611d
Gazh <axel.bruneaux@epitech.eu>
2026-08-29 11:43:43 +0200

    Fix date for visceralStrength tracking entry

--- src/analysis/retail/deathknight/blood/CHANGELOG.tsx
+++ src/analysis/retail/deathknight/blood/CHANGELOG.tsx
@@ -7,7 +7,7 @@ const visceralStrength = <SpellLink spell={talents.VISCERAL_STRENGTH_TALENT} />
 
 // prettier-ignore
 export default [
-  change(date(2026, 8, 229), <>Added {visceralStrength} tracking</>, Gazh),
+  change(date(2026, 8, 29), <>Added {visceralStrength} tracking</>, Gazh),
   change(date(2026, 8, 27), <>Added basic 12.1 support and <SpellLink spell={talents.ABOMINATION_LIMB_TALENT}></SpellLink> tracking</>, Badkad),
   change(date(2026, 4, 24), <>Added <SpellLink spell={talents.UMBILICUS_ETERNUS_TALENT}/> to timeline .</>, Badkad),
   change(date(2026, 4, 10), <>Enabled Changelog.</>, Badkad),
