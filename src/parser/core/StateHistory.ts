0923a8ae62d349e3e51f686efa811bf5da197ba2
emallson <emallson@emallson.net>
2026-09-03 10:34:13 -0400

    clarify StateHistory comment

--- src/parser/core/StateHistory.ts
+++ src/parser/core/StateHistory.ts
@@ -106,7 +106,7 @@ export default class StateHistory<State extends { timestamp: number }> {
   slice(start: number, end: number, expand = false): StateHistory<State> {
     this.ensureSort();
 
-    // handle the pathological cases: start > end of data and end < start of data
+    // handle the pathological cases: start > end of data or end < start of data
     if (
       (this._data.at(-1) && start > this._data.at(-1)!.timestamp) ||
       (this._data[0] && end < this._data[0].timestamp)
