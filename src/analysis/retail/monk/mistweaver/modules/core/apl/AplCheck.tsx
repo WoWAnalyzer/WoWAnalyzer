a7141a2f5af89235a3206c747e7106defd14612d
swirl <180882683+lolswirl@users.noreply.github.com>
2026-09-05 12:20:00 -0400

    apl changes

--- src/analysis/retail/monk/mistweaver/modules/core/apl/AplCheck.tsx
+++ src/analysis/retail/monk/mistweaver/modules/core/apl/AplCheck.tsx
@@ -84,7 +84,103 @@ const SPIRITFONT_PROC = {
 
 const MANA_TEA_20_STACKS = {
   spell: SPELLS.MANA_TEA_CAST,
-  condition: cnd.buffStacks(SPELLS.MANA_TEA_STACK, { atLeast: 20, atMost: 20 }),
+  condition: cnd.and(
+    cnd.buffStacks(SPELLS.MANA_TEA_STACK, { atLeast: 20, atMost: 20 }),
+    mwCnd.manaPercent({ atMost: 99 }),
+  ),
+};
+
+const MANA_TEA_ANY_STACKS = {
+  spell: SPELLS.MANA_TEA_CAST,
+  condition: cnd.optionalRule(
+    cnd.and(
+      cnd.buffStacks(SPELLS.MANA_TEA_STACK, { atLeast: 1 }),
+      mwCnd.manaPercent({ atMost: 99 }),
+    ),
+  ),
+};
+
+const REM_AFTER_THUNDER_FOCUS_TEA = {
+  spell: SPELLS.RENEWING_MIST_CAST,
+  condition: cnd.describe(cnd.lastSpellCast(talents.THUNDER_FOCUS_TEA_TALENT), (tense) => (
+    <>
+      {' '}
+      you cast <SpellLink spell={talents.THUNDER_FOCUS_TEA_TALENT} />
+    </>
+  )),
+};
+
+const SPIRITFONT_OVERCAP = {
+  spell: talents.ENVELOPING_MIST_TALENT,
+  condition: cnd.optionalRule(
+    cnd.buffStacks(SPELLS.SPIRITFONT_BUFF, { atLeast: 2, atMost: 2 }),
+    <>
+      <div>
+        <b>This does not mean you should only spend at 2 stacks.</b>{' '}
+      </div>
+      <div>
+        <SpellLink spell={talents.THUNDER_FOCUS_TEA_TALENT} /> grants you a{' '}
+        <SpellLink spell={SPELLS.SPIRITFONT_BUFF} /> stack on cast, so if you're already sitting at
+        2, spend at least one beforehand to avoid overcapping. Continue to use this regularly (later
+        in the priority list) with damage on your group.
+      </div>
+    </>,
+    'to avoid overcapping',
+  ),
+};
+
+// "injured" is harder to quantify here, but we
+// model this elsewhere on the analyzer
+const VIVIFY_INJURED_RAID = {
+  spell: SPELLS.VIVIFY,
+  condition: cnd.optionalRule(
+    cnd.describe(
+      mwCnd.targetsHealed(
+        { atLeast: 8 },
+        {
+          lookahead: 750,
+          targetSpell: SPELLS.INVIGORATING_MISTS_HEAL,
+          targetType: EventType.Heal,
+        },
+      ),
+      (tense) => (
+        <>
+          you {tenseAlt(tense, 'have', 'had')} at least 8 active{' '}
+          <SpellLink spell={SPELLS.RENEWING_MIST_CAST} />s
+        </>
+      ),
+    ),
+    undefined,
+    'with an injured raid',
+  ),
+};
+
+// this will always be true, since spiritfont
+// has a "won't activate unless injured" component
+const SPIRITFONT_PROC_INJURED_RAID = {
+  spell: talents.ENVELOPING_MIST_TALENT,
+  condition: cnd.optionalRule(
+    cnd.buffStacks(SPELLS.SPIRITFONT_BUFF, { atLeast: 1, atMost: 1 }),
+    undefined,
+    'with an injured raid',
+  ),
+};
+
+const BLACKOUT_KICK_FILLER = {
+  spell: SPELLS.BLACKOUT_KICK,
+  condition: cnd.optionalRule(cnd.spellAvailable(SPELLS.BLACKOUT_KICK)),
+};
+
+const TOTM_NOT_CAPPED = cnd.buffStacks(SPELLS.TEACHINGS_OF_THE_MONASTERY, {
+  atLeast: 0,
+  atMost: 3,
+});
+
+const TIGER_PALM_FILLER = {
+  spell: SPELLS.TIGER_PALM,
+  condition: cnd.optionalRule(
+    cnd.and(cnd.spellAvailable(SPELLS.BLACKOUT_KICK, { inverse: true }), TOTM_NOT_CAPPED),
+  ),
 };
 
 const commonTop = [
@@ -131,9 +227,9 @@ const commonTop = [
   MANA_TEA_20_STACKS,
 ];
 
-const RM_AT_CORE = [ZP_VIVIFY_5_REMS, VIVIFY_8_REMS];
+const RM_JFT_CORE = [ZP_VIVIFY_5_REMS, VIVIFY_8_REMS];
 
-const rotation_rm_at_sg = build([
+const rotation_rm_jft = build([
   {
     spell: [SPELLS.RENEWING_MIST_CAST, talents.RISING_SUN_KICK_TALENT],
     condition: cnd.describe(cnd.lastSpellCast(talents.THUNDER_FOCUS_TEA_TALENT), (tense) => (
@@ -144,38 +240,31 @@ const rotation_rm_at_sg = build([
     )),
   },
   ...commonTop,
-  ...RM_AT_CORE,
+  ...RM_JFT_CORE,
   BLACKOUT_KICK,
   REM_REMAINING,
   {
     spell: SPELLS.TIGER_PALM,
-    condition: cnd.optionalRule(
-      cnd.buffStacks(SPELLS.TEACHINGS_OF_THE_MONASTERY, { atLeast: 0, atMost: 4 }),
-    ),
+    condition: cnd.optionalRule(TOTM_NOT_CAPPED),
   },
 ]);
 
-const rotation_rm_rwk_sg = build([
+const rotation_rm_rwk = build([
+  talents.RUSHING_WIND_KICK_MISTWEAVER_TALENT,
   {
     spell: SPELLS.RENEWING_MIST_CAST,
-    condition: cnd.describe(cnd.lastSpellCast(talents.THUNDER_FOCUS_TEA_TALENT), (tense) => (
-      <>
-        {' '}
-        you cast <SpellLink spell={talents.THUNDER_FOCUS_TEA_TALENT} />
-      </>
-    )),
+    condition: cnd.spellCharges(SPELLS.RENEWING_MIST_CAST, { atLeast: 3 }),
   },
-  ...commonTop,
-  ZP_VIVIFY_5_REMS,
-  VIVIFY_8_REMS,
+  MANA_TEA_20_STACKS,
+  SPIRITFONT_OVERCAP,
+  REM_AFTER_THUNDER_FOCUS_TEA,
   REM_REMAINING,
-  BLACKOUT_KICK,
-  {
-    spell: SPELLS.TIGER_PALM,
-    condition: cnd.optionalRule(
-      cnd.buffStacks(SPELLS.TEACHINGS_OF_THE_MONASTERY, { atLeast: 0, atMost: 4 }),
-    ),
-  },
+  VIVIFY_INJURED_RAID,
+  SPIRITFONT_PROC_INJURED_RAID,
+  BLACK_OX_PROC,
+  MANA_TEA_ANY_STACKS,
+  BLACKOUT_KICK_FILLER,
+  TIGER_PALM_FILLER,
 ]);
 
 const rotation_fallback = build([...commonTop]);
@@ -208,8 +297,8 @@ export const chooseApl = (info: PlayerInfo): MistweaverApl => {
 };
 
 const apls: Record<MistweaverApl, Apl> = {
-  [MistweaverApl.RisingMistJadefireTeachings]: rotation_rm_at_sg,
-  [MistweaverApl.RisingMistRushingWindKick]: rotation_rm_rwk_sg,
+  [MistweaverApl.RisingMistJadefireTeachings]: rotation_rm_jft,
+  [MistweaverApl.RisingMistRushingWindKick]: rotation_rm_rwk,
   [MistweaverApl.WayOfTheCrane]: rotation_fallback,
   [MistweaverApl.TearOfMorning]: rotation_fallback,
   [MistweaverApl.Fallback]: rotation_fallback,
