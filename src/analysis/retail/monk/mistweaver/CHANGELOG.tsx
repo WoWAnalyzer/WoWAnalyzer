06ed6edb93faab0a4dbbea56f2cd1f47881a786b
swirl <180882683+lolswirl@users.noreply.github.com>
2026-09-09 11:19:19 -0400

    changelog

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -8,6 +8,7 @@ import SpellLink from 'interface/SpellLink';
 
 // prettier-ignore
 export default [
+  change(date(2026, 9, 9), <>Cleaned up cast efficiencies from replaced/removed spells.</>, swirl),
   change(date(2026, 9, 8), <>Fixed <SpellLink spell={TALENTS_MONK.MANA_TEA_TALENT}/> not being recognized as completed when a fight ends during a channel.</>, swirl),
   change(date(2026, 9, 5), <>Added Defensive and Active Time tracking to Overview tab.</>, swirl),
   change(date(2026, 9, 5), <>Adjusted APL conditions for 12.1.</>, swirl),
