7683f93c2670fafb860f5af9120864f0388b3fad
Katorri <katogaming88@gmail.com>
2026-09-21 18:29:05 -0400

    remove `sawApplyDebuff` and replace with `break`

--- src/analysis/retail/warlock/destruction/modules/normalizers/HavocPrepullNormalizer.ts
+++ src/analysis/retail/warlock/destruction/modules/normalizers/HavocPrepullNormalizer.ts
@@ -5,7 +5,6 @@ import EventsNormalizer from 'parser/core/EventsNormalizer';
 class HavocPrepullNormalizer extends EventsNormalizer {
   normalize(events: AnyEvent[]): AnyEvent[] {
     const fightStartTimestamp = this.owner.fight.start_time;
-    let sawApplyDebuff = false;
     const fabricatedEvents: (ApplyDebuffEvent | CastEvent)[] = [];
 
     for (const event of events) {
@@ -13,34 +12,32 @@ class HavocPrepullNormalizer extends EventsNormalizer {
 
       if (event.type === EventType.ApplyDebuff) {
         if (event.ability?.guid !== SPELLS.HAVOC.id) continue;
-        sawApplyDebuff = true;
-      } else if (event.type === EventType.RemoveDebuff && !sawApplyDebuff) {
+        break;
+      } else if (event.type === EventType.RemoveDebuff) {
         if (event.ability?.guid !== SPELLS.HAVOC.id) continue;
-        if (!sawApplyDebuff) {
-          fabricatedEvents.push({
-            type: EventType.ApplyDebuff,
-            ability: event.ability,
-            sourceID: event.sourceID,
-            sourceIsFriendly: event.sourceIsFriendly,
-            targetID: event.targetID,
-            targetInstance: event.targetInstance,
-            targetIsFriendly: event.targetIsFriendly,
-            timestamp: fightStartTimestamp,
-            __fabricated: true,
-          });
-          fabricatedEvents.push({
-            type: EventType.Cast,
-            ability: event.ability,
-            sourceID: event.sourceID ?? this.owner.playerId,
-            sourceIsFriendly: event.sourceIsFriendly,
-            targetID: event.targetID,
-            targetInstance: event.targetInstance,
-            targetIsFriendly: event.targetIsFriendly,
-            timestamp: fightStartTimestamp,
-            __fabricated: true,
-          });
-          sawApplyDebuff = true; // only fabricate once
-        }
+        fabricatedEvents.push({
+          type: EventType.ApplyDebuff,
+          ability: event.ability,
+          sourceID: event.sourceID,
+          sourceIsFriendly: event.sourceIsFriendly,
+          targetID: event.targetID,
+          targetInstance: event.targetInstance,
+          targetIsFriendly: event.targetIsFriendly,
+          timestamp: fightStartTimestamp,
+          __fabricated: true,
+        });
+        fabricatedEvents.push({
+          type: EventType.Cast,
+          ability: event.ability,
+          sourceID: event.sourceID ?? this.owner.playerId,
+          sourceIsFriendly: event.sourceIsFriendly,
+          targetID: event.targetID,
+          targetInstance: event.targetInstance,
+          targetIsFriendly: event.targetIsFriendly,
+          timestamp: fightStartTimestamp,
+          __fabricated: true,
+        });
+        break;
       }
     }
     return [...fabricatedEvents, ...events];
